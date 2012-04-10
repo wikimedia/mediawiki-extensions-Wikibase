d927bd71f391cb0b269001b4155d90344cc748fd
jeroendedauw <jeroendedauw@gmail.com>
2012-04-10 17:09:01 +0200

    rename fixes
    Change-Id: Ia457e4415a9569a8c803dccc2f2267357fcf4473

--- repo/Wikibase.php
+++ repo/Wikibase.php
@@ -27,12 +27,12 @@ if ( version_compare( $wgVersion, '1.19c', '<' ) ) { // Needs to be 1.19c becaus
 	die( '<b>Error:</b> Wikibase requires MediaWiki 1.19 or above.' );
 }
 
-define( 'WDR_VERSION', '0.1 alpha' );
+define( 'WB_VERSION', '0.1 alpha' );
 
 $wgExtensionCredits['other'][] = array(
 	'path' => __FILE__,
 	'name' => 'Wikibase',
-	'version' => WDR_VERSION,
+	'version' => WB_VERSION,
 	'author' => array(
 		'The Wikidata team', // TODO: link?
 	),
