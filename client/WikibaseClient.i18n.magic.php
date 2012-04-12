0e80e2a2c04f6fad5392dbbb178fe89c7df7e876
Nikola Smolenski <smolensk@eunet.rs>
2012-04-12 12:28:37 +0200

    Internationalization file with magic words

---
+++ client/WikibaseClient.i18n.magic.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Internationalization file with magic words for the Wikibase Client extension.
+ *
+ * @since 0.1
+ *
+ * @file WikibaseClient.i18n.php
+ * @ingroup Wikibase
+ *
+ * @licence GNU GPL v2+
+ */
+
+$magicWords = array();
+
+/** English
+ * @author Nikola Smolenski
+ */
+$magicWords['en'] = array(
+	'noexternalinterlang' => array(0, 'noexternalinterlang'),
+);
