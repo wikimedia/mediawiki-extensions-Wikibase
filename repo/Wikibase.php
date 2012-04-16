a44eb105cdefc19f79769e69d11e9a2615df3f03
Jens Ohlig <jens.ohlig@wikimedia.de>
2012-04-16 19:10:49 +0200

    Typo.

--- repo/Wikibase.php
+++ repo/Wikibase.php
@@ -104,7 +104,7 @@ $wgHooks['PageContentLanguage'][]					= 'WikibaseHooks::onPageContentLanguage';
 // Resource loader modules
 $moduleTemplate = array(
 	'localBasePath' => dirname( __FILE__ ) . '/resources',
-	'remoteExtPath' => ( ( string ) end  ( explode( DIRECTORY_SEPERATOR, dirname( __FILE__ ) ) ) ) . '/resources'
+	'remoteExtPath' => ( ( string ) end  ( explode( DIRECTORY_SEPARATOR, dirname( __FILE__ ) ) ) ) . '/resources'
 );
 
 $wgResourceModules['wikibase'] = $moduleTemplate + array(
