e448fbde75028bb5601cc888227765f668159c1d
Daniel Werner <daniel.werner@wikimedia.de>
2012-04-16 19:06:41 +0200

    strict error fix

--- repo/Wikibase.php
+++ repo/Wikibase.php
@@ -104,7 +104,7 @@ $wgHooks['PageContentLanguage'][]					= 'WikibaseHooks::onPageContentLanguage';
 // Resource loader modules
 $moduleTemplate = array(
 	'localBasePath' => dirname( __FILE__ ) . '/resources',
-	'remoteExtPath' => end( explode( '/', dirname( __FILE__ ) ) ) . '/resources' ,
+	'remoteExtPath' => preg_replace( '%.*\\\(.+?)%', '$1', dirname( __FILE__ ) ) . '/resources' ,
 );
 
 $wgResourceModules['wikibase'] = $moduleTemplate + array(
