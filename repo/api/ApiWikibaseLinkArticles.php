5467450a0d31233fda8da0c285daac6aeac49d78
jeblad <john.blad@wikimedia.de>
2012-04-17 11:47:26 +0200

    Updated for ModifyItem

--- repo/api/ApiWikibaseLinkArticles.php
+++ repo/api/ApiWikibaseLinkArticles.php
@@ -14,63 +14,24 @@
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  * @author John Erling Blad < jeblad@gmail.com >
  */
-class ApiWikibaseLinkArticles extends ApiBase {
+class ApiWikibaseLinkArticles extends ApiWikibaseModifyItem {
 
 	public function __construct( $main, $action ) {
 		parent::__construct( $main, $action );
 	}
 
 	/**
-	 * Main method. Does the actual work and sets the result.
+	 * Actually modify the item.
 	 *
 	 * @since 0.1
+	 *
+	 * @param WikibaseItem $item
+	 * @param array $params
+	 *
+	 * @return boolean Success indicator
 	 */
-	public function execute() {
-		// TODO: implement
-	}
-
-	public function needsToken() {
-		return !WBSettings::get( 'apiInDebug' );
-	}
-
-	public function mustBePosted() {
-		return !WBSettings::get( 'apiInDebug' );
-	}
-
-	public function getAllowedParams() {
-		return array(
-			'language-from' => array(
-				ApiBase::PARAM_TYPE => WikibaseUtils::getLanguageCodes(),
-				ApiBase::PARAM_REQUIRED => true,
-			),
-			'title-from' => array(
-				ApiBase::PARAM_TYPE => 'string',
-				ApiBase::PARAM_REQUIRED => true,
-			),
-			'language-to' => array(
-				ApiBase::PARAM_TYPE => WikibaseUtils::getLanguageCodes(),
-				ApiBase::PARAM_REQUIRED => true,
-			),
-			'title-to' => array(
-				ApiBase::PARAM_TYPE => 'string',
-				ApiBase::PARAM_REQUIRED => true,
-			),
-		);
-	}
-
-	public function getParamDescription() {
-		return array(
-			'language-from' => 'Language code of the external page it is linked from',
-			'title-from' => 'Title of the page it is linked from',
-			'language-to' => 'Language code of the external page it is linked to',
-			'title-to' => 'Title of the page it is linked to',
-		);
-	}
-
-	public function getDescription() {
-		return array(
-			'API module to associate two articles on external Mediawiki pages with a Wikibase item.'
-		);
+	protected function modifyItem( WikibaseItem &$item, array $params ) {
+		return $item->addSiteLink( $params['site_to'], $params['title_to'], !$params['noupdate'] );
 	}
 
 	public function getPossibleErrors() {
@@ -78,15 +39,83 @@ class ApiWikibaseLinkArticles extends ApiBase {
 		) );
 	}
 
-	protected function getExamples() {
+	/**
+	 * Make sure the required parameters are provided and that they are valid.
+	 *
+	 * @since 0.1
+	 *
+	 * @param array $params
+	 */
+	protected function validateParameters( array $params ) {
+		$params['title'] = $params['title_from'];
+		$params['site'] = $params['site_from'];
+	}
+
+	public function getAllowedParams() {
+		return array_merge( parent::getAllowedParams(), array(
+			'site' => array(
+				ApiBase::PARAM_TYPE => WikibaseUtils::getSiteIdentifiers(),
+			),
+			'site-from' => array(
+				ApiBase::PARAM_TYPE => WikibaseUtils::getSiteIdentifiers(),
+				ApiBase::PARAM_REQUIRED => true,
+			),
+			'title-from' => array(
+				ApiBase::PARAM_TYPE => 'string',
+				ApiBase::PARAM_REQUIRED => true,
+			),
+			'site-to' => array(
+				ApiBase::PARAM_TYPE => WikibaseUtils::getSiteIdentifiers(),
+				ApiBase::PARAM_REQUIRED => true,
+			),
+			'title-to' => array(
+				ApiBase::PARAM_TYPE => 'string',
+				ApiBase::PARAM_REQUIRED => true,
+			),
+			'noupdate' => array(
+				ApiBase::PARAM_TYPE => 'boolean',
+				ApiBase::PARAM_DFLT => false,
+			),
+		) );
+	}
+
+
+	public function getParamDescription() {
+		return array_merge( parent::getParamDescription(), array(
+			'site-from' => array( 'Site code of the external page it is linked from.',
+				"Use together with 'title-from'. Can also be used as 'site'."
+			),
+			'title-from' => array( 'Title of the page it is linked from',
+				"Use together with 'site-from'. Can also be used as 'title'."
+			),
+			'site-to' => array( 'Site code of the external page it is linked to.',
+				"Use together with 'title-to'."
+			),
+			'title-to' => array( 'Title of the page it is linked to.',
+				"Use together with 'site-to'."
+			),
+			'noupdate' => 'Indicates that if a link to the specified site already exists, it should not be updated to use the provided page',
+			//'change' => 'Inidcates if you are adding or removing the link',
+		) );
+	}
+
+	public function getDescription() {
 		return array(
-			'api.php?action=wblinkarticles&language-from=en&title-from=Norway&language-to=no&title-to=Norge'
-				=> 'Set link from title "Norway" for page in English Wikipedia to "Norge" in Norwegian Wikipedia', // TODO: Still references Wikipedia
+			'API module to use one article (site_from, title_from) to link to another article (site_to, title_to).'
 		);
 	}
 
+	protected function getExamples() {
+		return array(
+			'api.php?action=wblinkarticles&site-from=en&title-from=Norway&site-to=no&title-to=Norge'
+				=> array( 'Set link from title "Norway" for page in English Wikipedia to "Norge" in Norwegian Wikipedia.', // TODO: Still references Wikipedia
+					"Actual form and type for 'site' is not final."
+				),
+			);
+	}
+
 	public function getHelpUrls() {
-		return 'https://www.mediawiki.org/wiki/API:Wikidata#LinkArticles';
+		return 'https://www.mediawiki.org/wiki/API:Wikidata#wblinkarticles';
 	}
 
 	public function getVersion() {
