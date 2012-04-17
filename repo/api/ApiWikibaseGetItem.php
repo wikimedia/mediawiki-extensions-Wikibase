df23d1ce096696e1522279d3d74d80d7e122d629
jeblad <john.blad@wikimedia.de>
2012-04-17 10:48:31 +0200

    Just a newline to get a change in the file

--- repo/api/ApiWikibaseGetItem.php
+++ repo/api/ApiWikibaseGetItem.php
@@ -19,7 +19,6 @@ class ApiWikibaseGetItem extends ApiBase {
 		parent::__construct( $main, $action );
 	}
 
-
 	/**
 	 * Main method. Does the actual work and sets the result.
 	 *
@@ -32,43 +31,45 @@ class ApiWikibaseGetItem extends ApiBase {
 			$this->dieUsage( wfMsg( 'wikibase-api-id-xor-wikititle' ), 'id-xor-wikititle' );
 		}
 
-		if ( !isset( $params['id'] ) ) {
-			$params['id'] = WikibaseItem::getIdForSiteLink( $params['wiki'], $params['title '] );
-		}
-
 		$success = false;
-		$result = $this->getResult();
+
+		if ( !isset( $params['id'] ) ) {
+			$params['id'] = WikibaseItem::getIdForSiteLink( $params['site'], $params['title'] );
+
+			if ( $params['id'] === false ) {
+				$this->dieUsage( wfMsg( 'wikibase-api-no-such-item' ), 'no-such-item' );
+			}
+		}
 
 		$page = WikibaseUtils::getWikiPageForId( $params['id'] );
-		if ( !isset( $page ) ) {
-			$this->dieUsage( wfMsg( 'wikibase-api-invalid-id' ), 'invalid-id' );
-		}
-		
-		$content = $page->getContent();
-		if ( !isset( $content ) ) {
-			$this->dieUsage( wfMsg( 'wikibase-api-invalid-content' ), 'invalid-content' );
-		}
+		$content = $page->getContent(); // TODO: The call to getContent is not implemented (?)
 		
 		if ( $content->getModelName() === CONTENT_MODEL_WIKIBASE ) {
 			$item = $content->getItem();
-			$success = (bool)$item;
 			
-			$langcodes = WikibaseUtils::getLanguageCodes();
+			$sitelinks = $item->getSiteLinks();
+			$this->getResult()->addValue(
+			 	'page', 
+				'sitelinks',
+				(int)$success
+			);
 			
-			$labels = $item->getLabels( $langcodes );
-			if ( $labels ) {
-				$result->addValue( 'page', 'labels', $labels );
-			}
+			$languages = WikibaseUtils::getLanguageCodes();
 			
-			$descriptions = $item->getDescriptions( $langcodes );
-			if ( $descriptions ) {
-				$result->addValue( 'page', 'descriptions', $descriptions );
-			}
+			$labels = $item->getLabels($languages); // TODO: Set specific languages
+			$this->getResult()->addValue(
+			 	'page', 
+				'labels',
+				$labels
+			);
 			
-			$sitelinks = $item->getSiteLinks( $langcodes );
-			if ( $sitelinks ) {
-				$result->addValue( 'page', 'sitelinks', $sitelinks );
-			}
+			$descriptions = $item->getDescriptions($languages); // TODO: Set specific languages
+			$this->getResult()->addValue(
+			 	'page', 
+				'descriptions',
+				$descriptions
+			);
+			$success = true;
 		}
 		else {
 			$this->dieUsage( wfMsg( 'wikibase-api-invalid-contentmodel' ), 'invalid-contentmodel' );
@@ -85,14 +86,14 @@ class ApiWikibaseGetItem extends ApiBase {
 		return array(
 			'id' => array(
 				ApiBase::PARAM_TYPE => 'integer',
-				ApiBase::PARAM_ISMULTI => true,
+				/*ApiBase::PARAM_ISMULTI => true,*/
 			),
 			'site' => array(
 				ApiBase::PARAM_TYPE => WikibaseUtils::getSiteIdentifiers(),
 			),
 			'title' => array(
 				ApiBase::PARAM_TYPE => 'string',
-				ApiBase::PARAM_ISMULTI => true,
+				/*ApiBase::PARAM_ISMULTI => true,*/
 			),
 			'language' => array(
 				ApiBase::PARAM_TYPE => WikibaseUtils::getLanguageCodes(),
@@ -119,8 +120,7 @@ class ApiWikibaseGetItem extends ApiBase {
 
 	public function getPossibleErrors() {
 		return array_merge( parent::getPossibleErrors(), array(
-			array( 'code' => 'id-xor-wikititle', 'info'
-				=> 'You need to either provide the item id or the title of a corresponding page and the identifier for the wiki this page is on' ),
+			array( 'code' => 'id-xor-wikititle', 'info' => 'You need to either provide the item id or the title of a corresponding page and the identifier for the wiki this page is on' ),
 		) );
 	}
 
@@ -131,19 +131,17 @@ class ApiWikibaseGetItem extends ApiBase {
 			'api.php?action=wbgetitem&id=42&language=en'
 				=> 'Get item number 42 with english language',
 /*
-			// multiple hits by arguments to a non-plural parameter
+			// takes multiple values without using plural form
 			'api.php?action=wbgetitem&id=4|2'
 				=> 'Get item number 4 and 2 with default (user?) language',
-*/
-/*
-			// multiple hits by arguments to a non-plural parameter
+			// takes multiple values without using plural form
 			'api.php?action=wbgetitem&id=4|2&language=en'
 				=> 'Get item number 4 and 2 with enlish language',
 */
 			'api.php?action=wbgetitem&site=en&title=Berlin&language=en'
 				=> 'Get the item associated to page Berlin on the site identified by "en"',
 /*
-			// multiple hits by arguments to a non-plural parameter
+			// takes multiple values without using plural form
 			'api.php?action=wbgetitem&site=en&title=Berlin|Foobar&language=en'
 				=> 'Get the items associated to pages Berlin and Foobar on the site identified by "en"',
 */
