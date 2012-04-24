18330af679b786e1707af1d557f35f3456dcf95b
jeblad <john.blad@wikimedia.de>
2012-04-24 12:24:47 +0200

    Changed according to present API defs, except naming of ids, sites, titles

--- repo/includes/api/ApiWikibaseGetItem.php
+++ repo/includes/api/ApiWikibaseGetItem.php
@@ -13,7 +13,7 @@
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  * @author John Erling Blad < jeblad@gmail.com >
  */
-class ApiWikibaseGetItem extends ApiBase {
+class ApiWikibaseGetItems extends ApiBase {
 
 	public function __construct( $main, $action ) {
 		parent::__construct( $main, $action );
@@ -27,41 +27,57 @@ class ApiWikibaseGetItem extends ApiBase {
 	public function execute() {
 		$params = $this->extractRequestParams();
 
-		if ( !( isset( $params['id'] ) XOR ( isset( $params['site'] ) && isset( $params['title'] ) ) ) ) {
+		if ( !( isset( $params['ids'] ) XOR ( isset( $params['sites'] ) && isset( $params['titles'] ) ) ) ) {
 			$this->dieUsage( wfMsg( 'wikibase-api-id-xor-wikititle' ), 'id-xor-wikititle' );
 		}
 
 		$success = false;
 
-		if ( !isset( $params['id'] ) ) {
-			$params['id'] = WikibaseItem::getIdForSiteLink( $params['site'], $params['title'] );
-
-			if ( $params['id'] === false ) {
+		if ( !isset( $params['ids'] ) ) {
+			$params['ids'] = array();
+			if ( count($params['sites']) === 1 ) {
+				foreach ($params['titles'] as $title) {
+					$params['ids'] = WikibaseItem::getIdForSiteLink( $params['sites'], $title );
+				}
+			}
+			elseif ( count($params['titles']) === 1 ) {
+				foreach ($params['sites'] as $site) {
+					$params['ids'] = WikibaseItem::getIdForSiteLink( $sites, $params['titles'] );
+				}
+			}
+			else {
+				$this->dieUsage( wfMsg( 'wikibase-api-id-xor-wikititle' ), 'id-xor-wikititle' );
+			}
+			
+			if ( count($params['ids']) === 0 ) {
 				$this->dieUsage( wfMsg( 'wikibase-api-no-such-item' ), 'no-such-item' );
 			}
 		}
 
-		$page = WikibaseItem::getWikiPageForId( $params['id'] );
-
-		if ( $page->exists() ) {
-			$item = $page->getContent();
-		}
-		else {
-			$this->dieUsage( wfMsg( 'wikibase-api-no-such-item-id' ), 'no-such-item-id' );
-		}
-
 		$languages = WikibaseUtils::getLanguageCodes();
-
+		
 		$this->getResult()->addValue(
 			null,
-			'page',
-			array(
-			 	'id' => $params['id'],
-				'sitelinks' => $item->getSiteLinks(),
-				'descriptions' => $item->getDescriptions($languages),
-				'labels' => $item->getLabels($languages),
-			)
+			'items',
+			array()
 		);
+		
+		foreach ($params['ids'] as $id) {
+			WikibaseItem::getWikiPageForId( $id );
+			if ($page->exists()) {
+				$item = $page->getContent();
+				$this->getResult()->addValue(
+					'items',
+					"{$id}",
+					array(
+					 	'id' => $id,
+						'sitelinks' => $item->getSiteLinks(),
+						'descriptions' => $item->getDescriptions($languages),
+						'labels' => $item->getLabels($languages),
+					)
+				);
+			}
+		}
 		
 		$success = true;
 
@@ -74,16 +90,17 @@ class ApiWikibaseGetItem extends ApiBase {
 
 	public function getAllowedParams() {
 		return array(
-			'id' => array(
+			'ids' => array(
 				ApiBase::PARAM_TYPE => 'integer',
-				/*ApiBase::PARAM_ISMULTI => true,*/
+				ApiBase::PARAM_ISMULTI => true,
 			),
-			'site' => array(
+			'sites' => array(
 				ApiBase::PARAM_TYPE => WikibaseUtils::getSiteIdentifiers(),
+				ApiBase::PARAM_ISMULTI => true,
 			),
-			'title' => array(
+			'titles' => array(
 				ApiBase::PARAM_TYPE => 'string',
-				/*ApiBase::PARAM_ISMULTI => true,*/
+				ApiBase::PARAM_ISMULTI => true,
 			),
 			'language' => array(
 				ApiBase::PARAM_TYPE => WikibaseUtils::getLanguageCodes(),
@@ -94,13 +111,13 @@ class ApiWikibaseGetItem extends ApiBase {
 
 	public function getParamDescription() {
 		return array(
-			'id' => 'The ID of the item to get the data from',
+			'ids' => 'The ID of the item to get the data from',
 			'language' => 'By default the internationalized values are returned in all available languages.
 						This parameter allows filtering these down to one or more languages by providing their language codes.',
-			'title' => array( 'The title of the corresponding page',
+			'titles' => array( 'The title of the corresponding page',
 				"Use together with 'site'."
 			),
-			'site' => array( 'Identifier for the site on which the corresponding page resides',
+			'sites' => array( 'Identifier for the site on which the corresponding page resides',
 				"Use together with 'title'."
 			),
 		);
@@ -114,18 +131,18 @@ class ApiWikibaseGetItem extends ApiBase {
 
 	public function getPossibleErrors() {
 		return array_merge( parent::getPossibleErrors(), array(
-			array( 'code' => 'id-xor-wikititle', 'info' => 'You need to either provide the item id or the title of a corresponding page and the identifier for the wiki this page is on' ),
+			array( 'code' => 'id-xor-wikititle', 'info' => 'You need to either provide the item ids or the titles of a corresponding page and the identifier for the wiki this page is on' ),
 			array( 'code' => 'no-such-item-id', 'info' => 'Could not find an existing item for this id' ),
 		) );
 	}
 
 	protected function getExamples() {
 		return array(
-			'api.php?action=wbgetitem&id=42'
+			'api.php?action=wbgetitem&ids=42'
 			=> 'Get item number 42 with default (user?) language',
-			'api.php?action=wbgetitem&id=42&language=en'
+			'api.php?action=wbgetitem&ids=42&language=en'
 			=> 'Get item number 42 with english language',
-			'api.php?action=wbgetitem&site=en&title=Berlin&language=en'
+			'api.php?action=wbgetitem&sites=en&titles=Berlin&language=en'
 			=> 'Get the item associated to page Berlin on the site identified by "en"',
 		);
 	}
