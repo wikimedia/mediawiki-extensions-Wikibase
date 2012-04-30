21c79e43a9d04fa3f560ced6b9c153885b6da034
Tobias Gritschacher <tobias.gritschacher@wikimedia.de>
2012-04-30 16:49:50 +0200

    added ruby label ui selenium test

--- repo/tests/selenium_ruby/lib/pages/item_page.rb
+++ repo/tests/selenium_ruby/lib/pages/item_page.rb
@@ -6,7 +6,35 @@ class ItemPage
   item_id = '10';
   
   #page_url "http://localhost/mediawiki/index.php/Data:q#{@item_id}"
-  page_url "http://localhost/mediawiki/index.php/Data:q" + item_id
-  h1(:firstHeading, :id => 'firstHeading')
-  span(:itemLabelSpan, :xpath => "//h1[@id='firstHeading']/span");
+  #page_url "http://localhost/mediawiki/index.php/Data:q" + item_id
+  page_url "http://localhost/mediawiki/index.php/Data:q10"
+  
+  h1(:firstHeading, :id => "firstHeading")
+  span(:itemLabelSpan, :xpath => "//h1[@id='firstHeading']/span")
+  # span(:itemLabelSpan, :css => "h1#firstHeading > span")
+  # link(:editLink, :link_text => "edit", :index => 0)
+  link(:editLink, :css => 
+    "h1#firstHeading > 
+    div.wb-ui-propertyedittoolbar > 
+    div.wb-ui-propertyedittoolbar-group > 
+    div.wb-ui-propertyedittoolbar-group > 
+    a.wb-ui-propertyedittoolbar-button:nth-child(1)")
+  text_field(:valueInputField, :class => "wb-ui-propertyedittoolbar-editablevalueinterface", :index => 0)
+  # link(:cancelLink, :class => "wb-ui-propertyedittoolbar-button", :link_text => "cancel", :index => 0)
+  link(:cancelLink, :css => "h1#firstHeading > 
+    div.wb-ui-propertyedittoolbar > 
+    div.wb-ui-propertyedittoolbar-group > 
+    div.wb-ui-propertyedittoolbar-group > 
+    a.wb-ui-propertyedittoolbar-button:nth-child(2)")
+  link(:saveLinkDisabled, :css => "h1#firstHeading > 
+    div.wb-ui-propertyedittoolbar > 
+    div.wb-ui-propertyedittoolbar-group > 
+    div.wb-ui-propertyedittoolbar-group > 
+    span.wb-ui-propertyedittoolbar-button-disabled:nth-child(1)")
+  link(:saveLink, :css => "h1#firstHeading > 
+    div.wb-ui-propertyedittoolbar > 
+    div.wb-ui-propertyedittoolbar-group > 
+    div.wb-ui-propertyedittoolbar-group > 
+    a.wb-ui-propertyedittoolbar-button:nth-child(1)")
+  
 end
--- repo/tests/selenium_ruby/spec/item/item_spec.rb
+++ repo/tests/selenium_ruby/spec/item/item_spec.rb
@@ -1,15 +1,57 @@
-
 require 'spec_helper'
 
 describe "Check for labels" do
   context "Check for firstHeading" do
-    it "should check for firstHeading" do 
-      item_id = visit_page(AddItemPage).create_new_item("mytestitem")
+    it "should check for firstHeading" do
+      #item_id = visit_page(AddItemPage).create_new_item("mytestitem")
       visit_page(ItemPage)
       @current_page.firstHeading.should be_true
       @current_page.itemLabelSpan.should be_true
-      @current_page.itemLabelSpan.should == "Austria"
+      current_label = @current_page.itemLabelSpan
+      changed_label = current_label + "_fooo"
+      @current_page.itemLabelSpan.should == current_label
+      @current_page.editLink?.should be_true
+      @current_page.cancelLink?.should be_false
+      @current_page.editLink
+      @current_page.editLink?.should be_false
+      @current_page.cancelLink?.should be_true
+      @current_page.saveLinkDisabled?.should be_true
+      @current_page.valueInputField.should be_true
+      @current_page.valueInputField.clear
+      @current_page.valueInputField = changed_label
+      @current_page.saveLink?.should be_true
+      @current_page.cancelLink
+      @current_page.editLink?.should be_true
+      @current_page.cancelLink?.should be_false
+      @current_page.itemLabelSpan.should == current_label
+      @current_page.editLink
+      @current_page.valueInputField.clear
+      @current_page.valueInputField = changed_label
+      @current_page.saveLink
+      @current_page.itemLabelSpan.should == changed_label
+
+      # TODO: put this into a helper function to be reused
+      while (script = @browser.execute_script("return jQuery.active")) == 1 do
+        sleep(1.0/3)
+      end
+
+      visit_page(ItemPage)
+      @current_page.itemLabelSpan.should == changed_label
+
+      @current_page.editLink
+      @current_page.valueInputField.clear
+      @current_page.valueInputField = current_label
+      @current_page.saveLink
+      @current_page.itemLabelSpan.should == current_label
+
+      # TODO: put this into a helper function to be reused
+      while (script = @browser.execute_script("return jQuery.active")) == 1 do
+        sleep(1.0/3)
+      end
+
+      visit_page(ItemPage)
+      @current_page.itemLabelSpan.should == current_label
     end
   end
-  
+
 end
