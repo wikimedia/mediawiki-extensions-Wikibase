18d93a73cdcdb7e15945b64db84a198e9f497db3
Tobias Gritschacher <tobias.gritschacher@wikimedia.de>
2012-04-30 18:36:52 +0200

    more selenium ruby testing

--- repo/tests/selenium_ruby/lib/pages/item_page.rb
+++ repo/tests/selenium_ruby/lib/pages/item_page.rb
@@ -3,35 +3,34 @@ class ItemPage
   #def initialize(item_id)
   #  @item_id = item_id
   #end
-  item_id = '10';
+  # item_id = '10';
   
-  #page_url "http://localhost/mediawiki/index.php/Data:q#{@item_id}"
-  #page_url "http://localhost/mediawiki/index.php/Data:q" + item_id
+  # page_url "http://localhost/mediawiki/index.php/Data:q#{@item_id}"
+  # page_url "http://localhost/mediawiki/index.php/Data:q" + item_id
   page_url "http://localhost/mediawiki/index.php/Data:q10"
+  # page_url "http://localhost/mediawiki/index.php/Data:q" + @item_id
   
   h1(:firstHeading, :id => "firstHeading")
   span(:itemLabelSpan, :xpath => "//h1[@id='firstHeading']/span")
-  # span(:itemLabelSpan, :css => "h1#firstHeading > span")
-  # link(:editLink, :link_text => "edit", :index => 0)
-  link(:editLink, :css => 
+  
+  link(:editLabelLink, :css => 
     "h1#firstHeading > 
     div.wb-ui-propertyedittoolbar > 
     div.wb-ui-propertyedittoolbar-group > 
     div.wb-ui-propertyedittoolbar-group > 
     a.wb-ui-propertyedittoolbar-button:nth-child(1)")
-  text_field(:valueInputField, :class => "wb-ui-propertyedittoolbar-editablevalueinterface", :index => 0)
-  # link(:cancelLink, :class => "wb-ui-propertyedittoolbar-button", :link_text => "cancel", :index => 0)
-  link(:cancelLink, :css => "h1#firstHeading > 
+  text_field(:labelInputField, :class => "wb-ui-propertyedittoolbar-editablevalueinterface", :index => 0)
+  link(:cancelLabelLink, :css => "h1#firstHeading > 
     div.wb-ui-propertyedittoolbar > 
     div.wb-ui-propertyedittoolbar-group > 
     div.wb-ui-propertyedittoolbar-group > 
     a.wb-ui-propertyedittoolbar-button:nth-child(2)")
-  link(:saveLinkDisabled, :css => "h1#firstHeading > 
+  link(:saveLabelLinkDisabled, :css => "h1#firstHeading > 
     div.wb-ui-propertyedittoolbar > 
     div.wb-ui-propertyedittoolbar-group > 
     div.wb-ui-propertyedittoolbar-group > 
     span.wb-ui-propertyedittoolbar-button-disabled:nth-child(1)")
-  link(:saveLink, :css => "h1#firstHeading > 
+  link(:saveLabelLink, :css => "h1#firstHeading > 
     div.wb-ui-propertyedittoolbar > 
     div.wb-ui-propertyedittoolbar-group > 
     div.wb-ui-propertyedittoolbar-group > 
