861dc76389183ecfe2f1f0d852ebfb1b743a7d04
Daniel Werner <daniel.werner@wikimedia.de>
2012-05-02 20:37:49 +0200

    Further label tests added

--- repo/tests/qunit/wikibase.ui.Toolbar.Label.tests.js
+++ repo/tests/qunit/wikibase.ui.Toolbar.Label.tests.js
@@ -29,7 +29,7 @@
 			equal(
 				this.label._elem,
 				null,
-				'destroyed button'
+				'destroyed label'
 			);
 		}
 
@@ -52,6 +52,104 @@
 			this.label.getContent()[0],
 			jQueryObj[0], // compare with containing node
 			'Content equals the content set before'
+		);
+
+	} );
+
+	test( 'disable and enable', function() {
+
+		equal(
+			this.label.isDisabled(),
+			false,
+			'not yet disabled'
+		);
+
+		equal(
+			this.label.setDisabled(),
+			true,
+			'disable, state changed'
+		);
+
+		equal(
+			this.label.setDisabled( true ),
+			false,
+			'disable but disabled already, state not changed'
+		);
+
+		equal(
+			this.label.isDisabled(),
+			true,
+			'disabled now'
+		);
+
+		equal(
+			this.label.setDisabled( false ),
+			true,
+			'enable, state changed'
+		);
+
+		equal(
+			this.label.setDisabled( false ),
+			false,
+			'enable but enabled already, state not changed'
+		);
+
+		equal(
+			this.label.isDisabled(),
+			false,
+			'disabled now'
+		);
+	} );
+
+	test( 'EVENTS: beforeDisable and beforeEnable', function() {
+		// set events:
+		this.label.beforeDisable = function() {	return false; }
+		this.label.beforeEnable = function() {	return false; }
+
+		equal(
+			this.label.setDisabled(),
+			false,
+			'event beforeDisabled return value will cancel disable command'
+		);
+
+		equal(
+			this.label.isDisabled(),
+			false,
+			'still enabled'
+		);
+
+		this.label.beforeDisable = null;
+
+		equal(
+			this.label.setDisabled(),
+			true,
+			'event beforeDisabled removed, disabled'
+		);
+
+		equal(
+			this.label.setDisabled( false ),
+			false,
+			'try to enable, beforeEnabled will prevent'
+		);
+
+		equal(
+			this.label.isDisabled(),
+			true,
+			'still disabled'
+		);
+
+		this.label.beforeEnable = null;
+
+		equal(
+			this.label.setDisabled( false ),
+			true,
+			'event beforeEnable removed, enable'
+		);
+
+		equal(
+			this.label.isDisabled(),
+			false,
+			'enabled'
 		);
 
 	} );
