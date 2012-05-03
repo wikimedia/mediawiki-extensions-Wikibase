5a08ebd68c159a4562a410fb8161ac3eecab2595
Tobias Gritschacher <tobias.gritschacher@wikimedia.de>
2012-05-03 16:31:53 +0200

    working on selenium tests for sitelinks

--- repo/tests/selenium_ruby/spec/item/sitelinks_spec.rb
+++ repo/tests/selenium_ruby/spec/item/sitelinks_spec.rb
@@ -7,6 +7,9 @@ describe "Check functionality of add/edit/remove sitelinks" do
       visit_page(ItemPage)
       @current_page.sitelinksTable?.should be_true
       @current_page.addSitelinkLink?.should be_true
+      @current_page.siteLinkCounter?.should be_true
+      
+      
       # @current_page.siteIdCell?.should be_true
 
       # TODO: implement tests for sitelinks
