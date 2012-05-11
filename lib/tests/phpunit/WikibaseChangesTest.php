145e4c7d64cd883f634a7f898e0a516aff10f839
jeroendedauw <jeroendedauw@gmail.com>
2012-05-11 15:44:52 +0200

    base for WikibaseChanges tests and some test cleanup
    Change-Id: Ie8b3b7c7bc8a25f6a5a07709a895a030cb9b9119

--- lib/tests/phpunit/WikibaseChangesTest.php
+++ lib/tests/phpunit/WikibaseChangesTest.php
@@ -1,8 +1,41 @@
 <?php
+
 /**
- * Created by JetBrains PhpStorm.
- * User: j
- * Date: 5/10/12
- * Time: 7:59 PM
- * To change this template use File | Settings | File Templates.
+ * Tests for the WikibaseChanges class.
+ *
+ * @file
+ * @since 0.1
+ *
+ * @ingroup WikibaseLib
+ * @ingroup Test
+ *
+ * The database group has as a side effect that temporal database tables are created. This makes
+ * it possible to test without poisoning a production database.
+ * @group Database
+ *
+ * Some of the tests takes more time, and needs therefor longer time before they can be aborted
+ * as non-functional. The reason why tests are aborted is assumed to be set up of temporal databases
+ * that hold the first tests in a pending state awaiting access to the database.
+ * @group medium
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
+class WikibaseChangesTest extends MediaWikiTestCase {
+
+	public function newFromArrayProvider() {
+		return array(
+			array(),
+
+		);
+	}
+
+	/**
+	 * @dataProvider newFromArraysProvider
+	 */
+	public function testNewFromArray( array $data ) {
+
+	}
+
+}
+	
--- lib/tests/phpunit/WikibaseMapDiffTest.php
+++ lib/tests/phpunit/WikibaseMapDiffTest.php
@@ -3,19 +3,6 @@
 /**
  * Tests for the WikibaseMapDiff class.
  *
- * The tests are using "Database" to get its own set of temporal tables.
- * This is nice so we avoid poisoning an existing database.
- *
- * The tests are using "medium" so they are able to run alittle longer before they are killed.
- * Without this they will be killed after 1 second, but the setup of the tables takes so long
- * time that the first few tests get killed.
- *
- * The tests are doing some assumptions on the id numbers. If the database isn't empty when
- * when its filled with test items the ids will most likely get out of sync and the tests will
- * fail. It seems impossible to store the item ids back somehow and at the same time not being
- * dependant on some magically correct solution. That is we could use GetItemId but then we
- * would imply that this module in fact is correct.
- *
  * @file
  * @since 0.1
  *
