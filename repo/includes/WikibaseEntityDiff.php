ad9d4e2362de3620f76301218b4bef4aeaf29869
jeroendedauw <jeroendedauw@gmail.com>
2012-05-18 17:39:42 +0200

    match naming changes in lib
    Change-Id: If83aa1cd80e77d8f5f11466b5b1904426a03a0eb

--- repo/includes/WikibaseEntityDiff.php
+++ repo/includes/WikibaseEntityDiff.php
@@ -11,7 +11,7 @@
  * @licence GNU GPL v2+
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
-abstract class WikibaseEntityDiff extends ContentDiff {
+abstract class WikibaseEntityDiff {
 
 
 
