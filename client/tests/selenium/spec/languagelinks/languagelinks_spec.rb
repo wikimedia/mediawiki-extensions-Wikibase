91c881c105199d5a98b2312c89f957b797ca3a01
Tobias Gritschacher <tobias.gritschacher@wikimedia.de>
2012-05-22 15:48:49 +0200

    selenium: refactored client sitelinks tests to work again

---
+++ client/tests/selenium/spec/languagelinks/languagelinks_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe "Check if language links are displayed for a specific article" do
+  context "Check for display of language links" do
+    it "should check that language links are displayed" do
+      visit_page(ArticlePage)
+      
+      @current_page.langLinks?.should be_true
+      @current_page.langLinksList?.should be_true
+      
+      @current_page.count_language_links.should >= WIKI_MIN_LANGUAGE_LINKS
+
+      @current_page.germanWikiLink?.should be_true
+      @current_page.germanWikiLink
+      @current_page.wikiArticleTitle.should == WIKI_ARTICLE_TO_TEST
+      
+    end
+  end
+end
+
