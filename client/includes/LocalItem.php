8478c25137f9fb32d5c6030dd6878ab613ddaafb
jeroendedauw <jeroendedauw@gmail.com>
2012-05-24 18:13:08 +0200

    Adding classes to interact and represent entries in the local items table
    Change-Id: I448d130d21503d5e6bb976e491c976913c3d447a

---
+++ client/includes/LocalItem.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Wikibase;
+use ORMRow;
+
+/**
+ * Class representing a single local item (ie a row in the wbc_local_items).
+ *
+ * TODO: would be nice if this thing could use the decorator pattern and actually be
+ * and extension to the item class, but for that we need an IItem interface in the lib.
+ *
+ * @since 0.1
+ *
+ * @file
+ * @ingroup WikibaseClient
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class LocalItem extends ORMRow {
+
+	/**
+	 * @since 0.1
+	 *
+	 * @return Item
+	 */
+	public function getItem() {
+		return $this->getField( 'item' );
+	}
+
+}
