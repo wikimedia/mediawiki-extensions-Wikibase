82ed6acfc41c43b2bb749dcae9c0aeab29d965f0
jeroendedauw <jeroendedauw@gmail.com>
2012-05-24 23:23:23 +0200

    adding tests for LocalItem class
    Change-Id: I7f98746bbb85f6c0e9bf87a86ce5b7b7e721d0ad

---
+++ client/tests/phpunit/includes/LocalItemTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Wikibase\Test;
+use Wikibase\Diff as Diff;
+
+/**
+ * Tests for the Diff class.
+ *
+ * @file
+ * @since 0.1
+ *
+ * @ingroup WikibaseLib
+ * @ingroup Test
+ * @group Wikibase
+ * @group WikibaseLib
+ * @group WikibaseDiff
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class DiffTest extends \MediaWikiTestCase {
+
+
+
+}
+	
