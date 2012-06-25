a1ed5adc977fe69ab8ff9f5065655e5ef4c5af9a
daniel <daniel.kinzler@wikimedia.de>
2012-06-25 13:49:04 +0200

    make the edit action the same as the view action
    Change-Id: I0b811e08040e0edafb323f2d1e10693041eb500c

--- repo/includes/actions/EditItemAction.php
+++ repo/includes/actions/EditItemAction.php
@@ -14,40 +14,10 @@ namespace Wikibase;
  * @ingroup Action
  *
  * @licence GNU GPL v2+
- * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ * @author Daniel Kinzler
  */
-class EditItemAction extends \FormlessAction {
+class EditItemAction extends ViewItemAction {
 
-	/**
-	 * (non-PHPdoc)
-	 * @see Action::getName()
-	 */
-	public function getName() {
-		return 'edit';
-	}
-
-	/**
-	 * (non-PHPdoc)
-	 * @see FormlessAction::onView()
-	 */
-	public function onView() {
-		$content = $this->getContext()->getWikiPage()->getContent();
-
-		if ( is_null( $content ) ) {
-			// TODO: redirect to create special
-		}
-		else {
-			// TODO: switch on type of content
-		}
-		return 'TODO: edit UI';
-	}
-
-	/**
-	 * (non-PHPdoc)
-	 * @see Action::getDescription()
-	 */
-	protected function getDescription() {
-		return '';
-	}
+	//nothing to see here...
 
 }
