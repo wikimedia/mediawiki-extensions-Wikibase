ba2f8432adf765ba9022059cd17ce021989ca770
jeblad <john.blad@wikimedia.de>
2012-06-27 19:53:37 +0200

    Prevent direct editing of item through API.
    Patchset 4: Removed some whitespace
    
    Patchset 3: Rebase
    
    Patchset 2: Fixed use of a referenced variable (it was passed as value) and did a rebase.
    There is a failure in Wikibase\Test\ApiLinkSiteTest::testTitleToPage during test, but
    this is unrelated and fixed in 13492.
    
    Squashed commit of the following:
    
    commit 6321236ada0ad2f3f1613c6a41623c9bb798ff19
    Author: jeblad <john.blad@wikimedia.de>
    Date:   Wed Jun 27 19:47:19 2012 +0200
    
        Fixed the session thingy. About to squash now...
    
        Change-Id: If30eff135ca1d29f11491a013cb439f87b3a0a6e
    
    commit 2d18cedc04d117c521cd6cc7d2e94416b0d5e8e4
    Author: jeblad <john.blad@wikimedia.de>
    Date:   Tue Jun 26 10:47:34 2012 +0200
    
        Prevent direct editing of items with tests.
    
        Change-Id: I2f110190afb1ce367d321b509eb0c0401692fa9f
    
    Change-Id: Ie98b8e460f9d519389d005ed1d81c8595e6dfc47

---
+++ repo/tests/phpunit/includes/api/ApiEditPageTest.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Wikibase\Test;
+use ApiTestCase, ApiTestUser;
+use Wikibase\Settings as Settings;
+
+/**
+ * Tests for blocking of direct editing.
+ *
+ * @file
+ * @since 0.1
+ *
+ * @ingroup Wikibase
+ * @ingroup Test
+ *
+ * @licence GNU GPL v2+
+ * @author John Erling Blad < jeblad@gmail.com >
+ *
+ * @group API
+ * @group Wikibase
+ * @group WikibaseAPI
+ *
+ * The database group has as a side effect that temporal database tables are created. This makes
+ * it possible to test without poisoning a production database.
+ * @group Database
+ *
+ * Some of the tests takes more time, and needs therefor longer time before they can be aborted
+ * as non-functional. The reason why tests are aborted is assumed to be set up of temporal databases
+ * that hold the first tests in a pending state awaiting access to the database.
+ * @group medium
+ */
+class ApiEditPageTest extends ApiTestCase {
+
+	protected static $baseOfItemIds = 0;
+	protected static $usepost;
+	protected static $usetoken;
+	protected static $userights;
+	protected static $data;
+	protected static $edittoken;
+
+	function setUp() {
+		global $wgUser;
+		parent::setUp();
+		$this->doLogin();
+
+		$dbw = wfGetDB( DB_MASTER );
+		$dbw->query( 'TRUNCATE TABLE ' . $dbw->tableName( 'sites' ) );
+		\Wikibase\Utils::insertSitesForTests();
+		self::$usepost = Settings::get( 'apiInDebug' ) ? Settings::get( 'apiDebugWithPost' ) : true;
+		self::$usetoken = Settings::get( 'apiInDebug' ) ? Settings::get( 'apiDebugWithTokens' ) : true;
+		self::$userights = Settings::get( 'apiInDebug' ) ? Settings::get( 'apiDebugWithRights' ) : true;
+
+		ApiTestCase::$users['wbeditor'] = new ApiTestUser(
+			'Apitesteditor',
+			'Api Test Editor',
+			'api_test_editor@example.com',
+			array( 'wbeditor' )
+		);
+		//$wgUser = self::$users['wbeditor']->user;
+	}
+
+	/**
+	 * @group API
+	 */
+	function testEdit(  ) {
+
+		$data = $this->doApiRequest( array(
+			'action' => 'login',
+			'lgname' => self::$users['wbeditor']->username,
+			'lgpassword' => self::$users['wbeditor']->password
+		),
+			null,
+			false,
+			self::$users['wbeditor']->user );
+
+		$data = $this->doApiRequest(
+			array(
+				'action' => 'login',
+				'lgtoken' => $data[0]['login']['token'],
+				'lgname' => self::$users['wbeditor']->username,
+				'lgpassword' => self::$users['wbeditor']->password
+			),
+			null,
+			false,
+			self::$users['wbeditor']->user
+		);
+
+		$data = $this->doApiRequest(
+			array(
+				'action' => 'query',
+				'titles' => 'Main Page',
+				'intoken' => 'edit',
+				'prop' => 'info'
+			),
+			null,
+			false,
+			self::$users['sysop']->user
+		);
+
+		$pages = array_values( $data[0]['query']['pages'] );
+		$pageinfo = array_pop( $pages );
+
+		$this->assertEquals(
+			34, strlen( $pageinfo["edittoken"] ),
+			"The length of the token is not 34 chars"
+		);
+		$this->assertRegExp(
+			'/\+\\\\$/', $pageinfo["edittoken"],
+			"The final chars of the token is not '+\\'"
+		);
+
+		print_r($pageinfo);
+		print "\nflush\n";
+
+		$data = $this->doApiRequest(
+			array(
+				'action' => 'edit',
+				'title' => 'Main Page',
+				'text' => 'new text',
+				'token' => $pageinfo["edittoken"]
+			),
+			null,
+			false,
+			self::$users['sysop']->user
+		);
+
+		print_r($data[0]);
+		print "\nflush\n";
+
+		$this->assertArrayHasKey( 'edit', $data[0], "Check whatever" );
+		$this->assertArrayHasKey( 'result', $data[0]['edit'], "Check result" );
+		$this->assertEquals( 'Success', $data[0]['edit']['result'], "Check success report" );
+
+		return $data;
+	}
+}
