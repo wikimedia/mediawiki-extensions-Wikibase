7e214b73257f4c2fb9e78ea3db188cd8162648e3
jeroendedauw <jeroendedauw@gmail.com>
2012-07-02 12:43:00 +0200

    Added some missing class docs
    Change-Id: Ie536229a9e17142081651397286a2ff7b967ae46

--- repo/includes/EntityContent.php
+++ repo/includes/EntityContent.php
@@ -2,6 +2,17 @@
 
 namespace Wikibase;
 
+/**
+ * Abstract content object for articles representing Wikibase entities.
+ *
+ * @since 0.1
+ *
+ * @file
+ * @ingroup Wikibase
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
 abstract class EntityContent extends \AbstractContent {
 
 
