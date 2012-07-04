84ca7f378f31c45d96f0053ef7b828f2a21d0a7d
Tobias Gritschacher <tobias.gritschacher@wikimedia.de>
2012-07-04 19:17:58 +0200

    added basic selenium testing for search functionality
    patchset 2: fixed test which broke after
    https://gerrit.wikimedia.org/r/#/c/14203/ was merged in
    
    Change-Id: I54be6f39c90365661d00be5fd95d69e59584d41d

---
+++ repo/tests/selenium/lib/pages/search_page.rb
@@ -0,0 +1,29 @@
+# -*- encoding : utf-8 -*-
+# Wikidata UI tests
+#
+# Author:: Tobias Gritschacher (tobias.gritschacher@wikimedia.de)
+# License:: GNU GPL v2+
+#
+# page object for search page
+
+class SearchPage < NewItemPage
+  include PageObject
+  page_url WIKI_URL + "index.php/Special:Search"
+  text_field(:searchText, :id => "searchText")
+  button(:searchSubmit, :text => "Search")
+  div(:searchResultDiv, :class => "searchresults")
+  unordered_list(:searchResults, :class => "mw-search-results")
+  paragraph(:noResults, :class => "mw-search-nonefound")
+  span(:firstResultLabelSpan, :class => "wb-itemlink-label")
+  span(:firstResultIdSpan, :class => "wb-itemlink-id")
+  span(:firstResultSearchMatch, :class => "searchmatch")
+  link(:firstResultLink, :xpath => "//div[@class='mw-search-result-heading']/a")
+
+  def countSearchResults
+    count = 0
+    searchResults_element.each do |resultElem|
+      count = count+1
+    end
+    return count
+  end
+end
