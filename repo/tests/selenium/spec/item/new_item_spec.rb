4efaa733ed24c44a04223d924ed9ea57bb96b295
Daniel Werner <daniel.werner@wikimedia.de>
2012-07-06 17:46:03 +0200

    Special:CreateItem keeps the language now when redirecting to items page after creation - patchset 2: more readable var name and using url encode now. - patchset 3: added selenium test to check the functionality
    Change-Id: Ib4cc4ac57f4ba621959924dd9836f1a3deaddf55

--- repo/tests/selenium/spec/item/new_item_spec.rb
+++ repo/tests/selenium/spec/item/new_item_spec.rb
@@ -25,5 +25,26 @@ describe "Check functionality of create new item" do
       end
     end
   end
+
+  context "Check for correct redirect on create new item" do
+    it "should check that the redirect preservs the correct uselang parameter" do
+      initial_label = generate_random_string(10)
+      visit_page(NewItemPage) do |page|
+        page.wait_for_item_to_load
+      end
+      on_page(LanguageSelectorPage) do |page|
+        page.sttlLinkDe
+        page.wait_for_item_to_load
+        page.labelInputField= initial_label
+        page.saveLabelLink
+        ajax_wait
+        page.wait_for_api_callback
+        page.wait_for_new_item_creation
+        page.wait_for_item_to_load
+        page.itemLabelSpan.should == initial_label
+        page.viewTabLink_element.text == "Lesen"
+      end
+    end
+  end
 end
 
