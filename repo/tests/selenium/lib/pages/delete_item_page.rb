f9ffe86338f37fa99e8ab1d2bf77bdbc3cc930a2
Tobias Gritschacher <tobias.gritschacher@wikimedia.de>
2012-07-12 11:32:49 +0200

    added selenium test to check proper item deletion
    Change-Id: I3b94d395fcc947dcb5a10f3ddd547dd3d2406463

---
+++ repo/tests/selenium/lib/pages/delete_item_page.rb
@@ -0,0 +1,18 @@
+# -*- encoding : utf-8 -*-
+# Wikidata UI tests
+#
+# Author:: Tobias Gritschacher (tobias.gritschacher@wikimedia.de)
+# License:: GNU GPL v2+
+#
+# page object for delete item action
+
+class DeleteItemPage < NewItemPage
+  include PageObject
+
+  button(:delete, :id => 'wpConfirmB')
+
+  def delete_item
+    navigate_to(@@item_url + "?action=delete")
+    delete
+  end
+end
