0e1ce1e630991cfb1d454087396f58dc05ffb718
jeblad <john.blad@wikimedia.de>
2012-07-12 18:50:20 +0200

    Added flags to ItemContent to make it respect botflags.
    Patchset 2: Some changes to ApiModiyItem.php was not included.
    
    Change-Id: Icd0f8759fc5d9f31ecac3a229b08fb8f684546a8

---
+++ repo/tests/phpunit/includes/api/ApiBotEditTest.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace Wikibase\Test;
+use ApiTestCase, ApiTestUser;
+use Wikibase\Settings as Settings;
+
+/**
+ * Tests for the ApiWikibase class.
+ *
+ * This testset only checks the validity of the calls and correct handling of tokens and users.
+ * Note that we creates an empty database and then starts manipulating testusers.
+ *
+ * BE WARNED: the tests depend on execution order of the methods and the methods are interdependent,
+ * so stuff will fail if you move methods around or make changes to them without matching changes in
+ * all methods depending on them.
+ *
+ * @file
+ * @since 0.1
+ *
+ * @ingroup Wikibase
+ * @ingroup Test
+ *
+ * @licence GNU GPL v2+
+ * @author John Erling Blad < jeblad@gmail.com >
+ * 
+ * @group API
+ * @group Wikibase
+ * @group WikibaseAPI
+ * @group ApiBotEditTest
+ *
+ * The database group has as a side effect that temporal database tables are created. This makes
+ * it possible to test without poisoning a production database.
+ * @group Database
+ *
+ * Some of the tests takes more time, and needs therefor longer time before they can be aborted
+ * as non-functional. The reason why tests are aborted is assumed to be set up of temporal databases
+ * that hold the first tests in a pending state awaiting access to the database.
+ * @group medium
+ */
+class ApiBotEditTest extends \ApiTestCase {
+
+	protected static $baseOfItemIds = 0;
+	protected static $usepost;
+	protected static $usetoken;
+	protected static $userights;
+
+	public function setUp() {
+		global $wgUser;
+		parent::setUp();
+
+		\Wikibase\Utils::insertSitesForTests();
+
+		self::$usepost = Settings::get( 'apiInDebug' ) ? Settings::get( 'apiDebugWithPost' ) : true;
+		self::$usetoken = Settings::get( 'apiInDebug' ) ? Settings::get( 'apiDebugWithTokens' ) : true;
+		self::$userights = Settings::get( 'apiInDebug' ) ? Settings::get( 'apiDebugWithRights' ) : true;
+
+		ApiTestCase::$users['wbbot'] = new ApiTestUser(
+			'Apitestbot',
+			'Api Test Bot',
+			'api_test_bot@example.com',
+			array( 'bot' )
+		);
+		$wgUser = self::$users['wbbot']->user;
+
+		// now we have to do the login with the previous user
+		$data = $this->doApiRequest( array(
+			'action' => 'login',
+			'lgname' => self::$users['wbbot']->username,
+			'lgpassword' => self::$users['wbbot']->password
+		) );
+
+		$token = $data[0]['login']['token'];
+
+		$this->doApiRequest(
+			array(
+				'action' => 'login',
+				'lgtoken' => $token,
+				'lgname' => self::$users['wbbot']->username,
+				'lgpassword' => self::$users['wbbot']->password
+			),
+			$data
+		);
+	}
+
+	/**
+	 * This initiates a cascade that fails if there are no
+	 * production-like environment
+	 * @group API
+	 */
+	function testTokensAndRights() {
+		// check if there is a production-like environment available
+		if (!self::$usetoken || !self::$userights) {
+			$this->markTestSkipped(
+				"The current setup does not include use of tokens or user rights"
+			);
+		}
+		// make sure execution pass through at least one assertion
+		else {
+			$this->assertTrue( true, "Make phpunit happy" );
+		}
+	}
+
+	/**
+	 * @group API
+	 * @depends testTokensAndRights
+	 */
+	function testSetItemTop() {
+		$req = array();
+		if ( Settings::get( 'apiInDebug' ) ? Settings::get( 'apiDebugWithTokens', false ) : true ) {
+			$first = $this->doApiRequest(
+				array(
+					'action' => 'wbsetitem',
+					'gettoken' => '' ),
+				null,
+				false,
+				self::$users['wbbot']->user
+			);
+
+			$req['token'] = $first[0]['wbsetitem']['itemtoken'];
+		}
+		$req = array_merge(
+			$req,
+			array(
+				'action' => 'wbsetitem',
+				'summary' => 'Some reason',
+				'data' => '{}',
+			)
+		);
+
+		$second = $this->doApiRequest( $req, null, false, self::$users['wbbot']->user );
+		//print_r($second[0]);
+		$this->assertArrayHasKey( 'success', $second[0],
+			"Must have an 'success' key in the second result from the API" );
+		$this->assertArrayHasKey( 'item', $second[0],
+			"Must have an 'item' key in the second result from the API" );
+		$this->assertArrayHasKey( 'id', $second[0]['item'],
+			"Must have an 'id' key in the 'item' from the second result from the API" );
+		self::$baseOfItemIds = $second[0]['item']['id'];
+	}
+	/**
+	 * @group API
+	 * @depends testTokensAndRights
+	 * @dataProvider providerCreateItem
+	 */
+	function testCreateItem( $id, $bot, $new, $data ) {
+		$myid = self::$baseOfItemIds + $id;
+		$req = array();
+		if ( Settings::get( 'apiInDebug' ) ? Settings::get( 'apiDebugWithTokens', false ) : true ) {
+			$first = $this->doApiRequest(
+				array(
+					'action' => 'wbsetitem',
+					'gettoken' => ''
+				),
+				null,
+				false,
+				self::$users['wbbot']->user
+			);
+
+			$req['token'] = $first[0]['wbsetitem']['itemtoken'];
+		}
+
+		$req = array_merge(
+			$req,
+			array(
+				'action' => 'wbsetitem',
+				'summary' => 'Some reason',
+				'data' => $data,
+			)
+		);
+		if ( !$new ) {
+			$req['id'] = $myid;
+		}
+		if ( $bot ) {
+			$req['bot'] = true;
+		}
+
+		$second = $this->doApiRequest( $req, null, false, self::$users['wbbot']->user );
+		//print_r($second[0]);
+
+		$this->assertArrayHasKey( 'success', $second[0],
+			"Must have an 'success' key in the second result from the API" );
+		$this->assertArrayHasKey( 'item', $second[0],
+			"Must have an 'item' key in the second result from the API" );
+		$this->assertArrayHasKey( 'id', $second[0]['item'],
+			"Must have an 'id' key in the 'item' from the second result from the API" );
+		$this->assertEquals( $myid, $second[0]['item']['id'],
+			"Must have the value '{$myid}' for the 'id' in the result from the API" );
+
+		$req = array(
+				'action' => 'query',
+				'list' => 'recentchanges',
+				'rcprop' => 'title|flags'
+		);
+		$third = $this->doApiRequest( $req, null, false, self::$users['wbbot']->user );
+
+		$this->assertArrayHasKey( 'query', $third[0],
+			"Must have a 'query' key in the third result from the API" );
+		$this->assertArrayHasKey( 'recentchanges', $third[0]['query'],
+			"Must have a 'recentchanges' key in 'query' subset of the third result from the API" );
+		$this->assertArrayHasKey( '0', $third[0]['query']['recentchanges'],
+			"Must have a '0' key in 'recentchanges' subset of the third result from the API" );
+		$this->assertTrue( $new == array_key_exists( 'new', $third[0]['query']['recentchanges']['0'] ),
+			"Must" . ( $new ? '' : ' not ' ) . "have a 'new' key in the rc-entry of the third result from the API" );
+		$this->assertTrue( $bot == array_key_exists( 'bot', $third[0]['query']['recentchanges']['0'] ),
+			"Must" . ( $bot ? '' : ' not ' ) . "have a 'bot' key in the rc-entry of the third result from the API" );
+	}
+
+	function providerCreateItem() {
+		//$idx = self::$baseOfItemIds;
+		$idx = 0;
+		return array(
+			array( ++$idx, false, true, "{}" ),
+			array( $idx, true, false, '{ "labels": { "nn":"Karasjok" } }' ),
+			array( $idx, false, false, '{ "descriptions": { "nn":"Small place in Finnmark" } }' ),
+			array( ++$idx, true, true, "{}" ),
+			array( $idx, true, false, '{ "labels": { "nn":"Kautokeino" } }' ),
+			array( $idx, false, false, '{ "descriptions": { "nn":"Small place in Finnmark" } }' ),
+		);
+	}
+
+}
