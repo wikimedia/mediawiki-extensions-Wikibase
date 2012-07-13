84bf25f5ba92c5ccd02dce3b0310b7828d8d718a
Henning Snater <henning.snater@wikimedia.de>
2012-07-13 16:32:45 +0200

    disabling simultaneous editing
    patchset 2: fix for multiple empty values
    patchset 3: checking for pending value and minor fixes
    patchset 4: preventing event from being fired too often
    patchset 6: applying changes of patch sets 3 and 4 again after obviously
    being lost by rebasing
    
    Change-Id: If8abaf8bb3d3fab780775ef85011b8537ec5b2d3

--- repo/tests/qunit/wikibase.ui.PropertyEditTool.EditableValue.tests.js
+++ repo/tests/qunit/wikibase.ui.PropertyEditTool.EditableValue.tests.js
@@ -149,6 +149,83 @@
 	} );
 
 
+	test( 'dis- and enabling', function() {
+
+		equal(
+			this.editableValue.isEnabled(),
+			true,
+			'is enabled'
+		);
+
+		equal(
+			this.editableValue.isDisabled(),
+			false,
+			'not disabled'
+		);
+
+		equal(
+			this.editableValue.disable(),
+			true,
+			'disabling'
+		);
+
+		equal(
+			this.editableValue.isDisabled(),
+			true,
+			'disabled'
+		);
+
+		equal(
+			this.editableValue.isEnabled(),
+			false,
+			'not enabled'
+		);
+
+		equal(
+			this.editableValue.getToolbar().enable(),
+			true,
+			'enabling toolbar'
+		);
+
+		equal(
+			this.editableValue.getElementsState(),
+			wikibase.ui.ELEMENT_STATE.MIXED,
+			'mixed state'
+		);
+
+		equal(
+			this.editableValue.isDisabled(),
+			false,
+			'not disabled'
+		);
+
+		equal(
+			this.editableValue.isEnabled(),
+			false,
+			'not enabled'
+		);
+
+		equal(
+			this.editableValue.enable(),
+			true,
+			'enabling'
+		);
+
+		equal(
+			this.editableValue.isEnabled(),
+			true,
+			'is enabled'
+		);
+
+		equal(
+			this.editableValue.isDisabled(),
+			false,
+			'is not disabled'
+		);
+
+	} );
+
+
 	test( 'edit', function() {
 
 		equal(
