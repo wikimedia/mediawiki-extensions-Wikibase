5fbe4079a2f5634de543c239c958c2b9b709ab09
jeblad <john.blad@wikimedia.de>
2012-08-01 08:26:10 +0200

    Forms for SpecialItemByLabel and SpecialItemByTitle
    The forms are not very nice, its just a quick and dirty solution
    to get a fallback when the URLs are not identifying anything.
    The code should probably do qualification of site and language,
    and it could be nice to have autocompletion.
    
    The special page for create item needs a better solution, there
    will be addd a few bugs for that page.
    
    The disambiguation needs some further refinement. For example it
    should show the language and it should probably redirect to the
    special page with correct URL.
    
    There is a bugfix in SpecialByTitle, the present page crashes
    with an error message.
    
    The patchset needs tests, but those are probably best implemented
    as Selenium tests.
    
    Patchset 2: Cleanup and removed some unrelated stuff.
    
    Patchset 3: Commented out parts of SpecialItemByLabel, but this
    creates a really weird use case where a user can access a link
    with a description, which fails, then end up at the form, and
    post that, and will then find the page.
    
    Patchset whatever: Moved html escaping.
    
    Patchset 5: Some changes in use of messages in ItemDisambiguation,
    shifted to use uf item id insted of arrow as link text, shifted
    to use of post as action in forms, removed use of description,
    removed an unused code line.
    
    Change-Id: I6879e900808f97b6df16f9fb9e6fff32ba7b7be8

--- repo/includes/specials/SpecialItemByLabel.php
+++ repo/includes/specials/SpecialItemByLabel.php
@@ -33,35 +33,101 @@ class SpecialItemByLabel extends SpecialItemResolver {
 	public function execute( $subPage ) {
 		parent::execute( $subPage );
 
-		if ( $this->subPage === '' ) {
-			// TODO: display a message that the user needs to provide language+label and possibly some fancy input UI
+		// Setup
+		$request = $this->getRequest();
+		$parts = ( $this->subPage === '' ) ? array() : explode( '/', $this->subPage, 2 );
+		$language = $request->getVal( 'language', isset( $parts[0] ) ? $parts[0] : '' );
+		if ( $language === '' ) {
+			$language = null;
 		}
-
-		// TODO: document that the label cannot have slashes (or pick other separator)
-		$parts = explode( '/', $this->subPage, 3 );
-
-		if ( count( $parts ) == 1 ) {
-			// TODO: display a message that the user needs to provide  the label and possibly some fancy input UI
+		$label = $request->getVal( 'label', isset( $parts[1] ) ? $parts[1] : '' );
+		if ( $label === '' ) {
+			$label = null;
 		}
-		else {
-			$itemContents = call_user_func_array( array( \Wikibase\ItemHandler::singleton(), 'getFromLabel' ), $parts );
+		$itemContents = array();
 
-			if ( $itemContents === array() ) {
-				// TODO: display that there are no matching items and possibly some fancy input UI
-			}
-			elseif ( count( $itemContents ) !== 1 ) {
-				$this->getOutput()->setPageTitle( $this->msg( 'wikibase-disambiguation-title', $parts[1] )->escaped() );
-				$this->displayDisambiguationPage( $itemContents, $parts[0] );
-			}
-			else {
+		// Create an item view
+		if ( isset( $language ) && isset( $label ) ) {
+			$itemContents = \Wikibase\ItemHandler::singleton()->getFromLabel( $language, $label );
+			//$itemContents = call_user_func_array(
+			//	array( \Wikibase\ItemHandler::singleton(), 'getFromLabel' ),
+			//	array( $language, $label, $description )
+			//);
+			if ( count( $itemContents ) === 1 && $this->getRequest()->wasPosted() ) {
 				$this->displayItem( $itemContents[0] );
 			}
+			if ( 0 < count( $itemContents ) ) {
+				$this->getOutput()->setPageTitle( $this->msg( 'wikibase-disambiguation-title', $label )->escaped() );
+				$this->displayDisambiguationPage( $itemContents, $language );
+			}
+		}
+
+		// If there is no item content post the switch form
+		if ( $itemContents === array() ) {
+			$this->switchForm( $language, $label );
 		}
 	}
 
+	/**
+	 * Display disambiguation page.
+	 *
+	 * @since 0.1
+	 *
+	 * @param array $items
+	 * @param string $langCode
+	 */
 	protected function displayDisambiguationPage( array /* of ItemContent */ $items, $langCode ) {
 		$disambiguationList = new Wikibase\ItemDisambiguation( $items, $langCode, $this->getContext() );
 		$disambiguationList->display();
+	}
+
+	/**
+	 * Output a form to allow searching for a page
+	 *
+	 * @since 0.1
+	 *
+	 * @param string|null $site
+	 * @param string|null $page
+	 */
+	protected function switchForm( $langCode, $label ) {
+		global $wgScript;
+
+		// Misuse language name to figure out if the code is defined
+		$languageName = \Wikibase\Utils::fetchLanguageName( $langCode );
+
+		if ( isset( $langCode ) || isset( $label ) ) {
+			$this->getOutput()->addHTML(
+				Html::openElement( 'div' )
+				. $this->msg( $languageName !== $langCode ? 'wikibase-itembylabel-nothing-found' : 'wikibase-itembylabel-invalid-langcode' )
+					->params( $langCode, $label )
+					->parse()
+				. Html::closeElement( 'div' )
+			);
+		}
+		$this->getOutput()->addHTML(
+			Html::openElement( 'form', array( 'method' => 'post', 'action' => $wgScript, 'name' => 'itembylabel', 'id' => 'mw-itembylabel-form1' ) )
+			. Html::hidden( 'title',  $this->getTitle()->getPrefixedText() )
+			. Xml::fieldset( $this->msg( 'wikibase-itembylabel-lookup-fieldset' )->text() )
+			. Xml::inputLabel( $this->msg( 'wikibase-itembylabel-lookup-language' )->text(), 'language', 'languagename', 12, $langCode ? $langCode : '' )
+			. Xml::inputLabel( $this->msg( 'wikibase-itembylabel-lookup-label' )->text(), 'label', 'labelname', 36, $label ? $label : '' )
+			. Xml::submitButton( $this->msg( 'wikibase-itembylabel-submit' )->text() )
+			. Html::closeElement( 'fieldset' )
+			. Html::closeElement( 'form' )
+		);
+		$this->getOutput()->addHTML(
+			Html::openElement( 'div' )
+			. $this->msg( 'wikibase-itembylabel-description' )->text()
+			. Html::closeElement( 'div' )
+		);
+		if ( isset( $langCode ) && isset( $label ) ) {
+			$this->getOutput()->addHTML(
+				Html::openElement( 'div' )
+				. $this->msg( 'wikibase-itembylabel-create' )
+					->params( $langCode ? $langCode : '', $label ? $label : '' )
+					->parse()
+				. Html::closeElement( 'div' )
+			);
+		}
 	}
 
 }
