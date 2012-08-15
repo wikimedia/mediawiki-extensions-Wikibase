6dee67056c5365a15f6872a61f09a4ca3886ad6f
jeroendedauw <jeroendedauw@gmail.com>
2012-08-15 21:23:56 +0200

    Adding core classes for phase2 data model
    Step one of three:
    
    * define interfaces
    * write tests
    * implement
    
    ps3: commented out new methods in Entity interface as they will only be implemented later
    
    Change-Id: Ie11c69c0c32ca2a82e658a7ef3b3b4d6af2e2822

---
+++ lib/includes/snak/PropertySomeValueSnak.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Wikibase;
+
+/**
+ * Class representing a property some value snak.
+ * See https://meta.wikimedia.org/wiki/Wikidata/Data_model#PropertyNoValueSnak
+ *
+ * @since 0.1
+ *
+ * @file
+ * @ingroup Wikibase
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class PropertySomeValueSnak extends PropertySnakObject {
+
+
+
+}
