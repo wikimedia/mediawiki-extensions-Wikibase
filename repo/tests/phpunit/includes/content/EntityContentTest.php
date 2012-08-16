1ca214ae4a88ce058f967cbf7c4a467d453af161
jeblad <john.blad@wikimedia.de>
2012-08-16 09:22:43 +0200

    (Bug 37385) Initial commit for checking edit conflicts [DO NOT MERGE]
    Checks if the user is the last one to edit after a given revision,
    and only stores the edit if the the checks turns out as true.
    
    The patchset contains necessary changes to pass on revision ids
    from the user interface, but for now only the revision set in the
    page config is used for conflict detection. The reported revision
    id can be used for individual fields, but then it can only be used
    for patching the item for that specific field.
    
    Patchset 2: Bunch of additional stuff.
    
    Patchset 3: Cleanup
    
    Change-Id: I67f2f225029e3a39af027193a999e20cb7338a66

---
+++ repo/tests/phpunit/includes/content/EntityContentTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Wikibase\Test;
+use \Wikibase\EntityContent as EntityContent;
+use \Wikibase\ItemContent as ItemContent;
+use \Wikibase\Item as Item;
+
+/**
+ * Tests for the Wikibase\EntityContent class.
+ *
+ * @file
+ * @since 0.1
+ *
+ * @ingroup Wikibase
+ * @ingroup Test
+ *
+ * @group Wikibase
+ * @group WikibaseEntity
+ * @group WikibaseRepo
+ *
+ * @group Database
+ * ^--- important, causes temporary tables to be used instead of the real database
+ *
+ * @licence GNU GPL v2+
+ * @author John Erling Blad < jeblad@gmail.com >
+ */
+class EntityContentTest extends \MediaWikiTestCase {
+
+	/**
+	 * Because the database is somehow reset between calls we do it all in the test method.
+	 * This doesn't test failing cases, mainly because something weird going on with the revision table.
+	 */
+	public function testEditEntity() {
+		$this->markTestIncomplete( "Needs a test user before it can be run" );
+		return;
+		// EntityContent is abstract so we use the subclass ItemContent
+		$item = \Wikibase\ItemContent::newEmpty();
+		$item->save();
+		$page = $item->getWikiPage();
+		$lastRevId = $page->getRevision()->getId();
+		$userId = $page->getRevision()->getUser(); // this is 0!
+
+		$sets = array(
+			array( 0, false, false ),
+			array( 0, $lastRevId, false ),
+			array( $userId, false, false ),
+			array( $userId, $lastRevId, true ),
+		);
+
+		for ( $i = 0; $i < 60; $i++ ) {
+			$item->reload();
+			$item->getItem()->setLabel('en', "Test $i" );
+			$status = $item->save();
+			$this->assertTrue( $status->isOK() );
+		}
+		$page = $item->getWikiPage();
+
+		foreach ( $sets as $args ) {
+			list( $userId, $lastRevId, $expected ) = $args;
+			$res = $item->userWasLastToEdit( $userId, $lastRevId );
+		}
+	}
+}
