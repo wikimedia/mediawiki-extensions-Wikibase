4c53f7970f40e487d93f4b733ad0a0d9b5b6a4d3
jeroendedauw <jeroendedauw@gmail.com>
2012-08-20 13:14:55 +0200

    Implemented Claim and added tests
    Change-Id: Id259ba96fefd4dc1d7fe03a4898d64e30555b8a3

--- lib/includes/ClaimObject.php
+++ lib/includes/ClaimObject.php
@@ -14,7 +14,7 @@ namespace Wikibase;
  * @licence GNU GPL v2+
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
-class ClaimObject implements  Claim {
+class ClaimObject implements Claim {
 
 	/**
 	 * @since 0.1
@@ -24,10 +24,69 @@ class ClaimObject implements  Claim {
 	protected $mainSnak;
 
 	/**
+	 * The property snaks that are qualifiers for this claim.
+	 *
 	 * @since 0.1
 	 *
-	 * @var Snaks (each element being a PropertySnak)
+	 * @var Snaks
 	 */
 	protected $qualifiers;
+
+	/**
+	 * Constructor.
+	 *
+	 * @since 0.1
+	 *
+	 * @param Snak $mainSnak
+	 * @param null|Snaks $qualifiers All elements in the Snaks object must be PropertySnak objects
+	 */
+	public function __construct( Snak $mainSnak, Snaks $qualifiers = null ) {
+		$this->mainSnak = $mainSnak;
+		$this->qualifiers = $qualifiers === null ? new SnakList() : $qualifiers;
+	}
+
+	/**
+	 * @see Claim::getMainSnak
+	 *
+	 * @since 0.1
+	 *
+	 * @return Snak
+	 */
+	public function getMainSnak() {
+		return $this->mainSnak;
+	}
+
+	/**
+	 * @see Claim::setMainSnak
+	 *
+	 * @since 0.1
+	 *
+	 * @param Snak $mainSnak
+	 */
+	public function setMainSnak( Snak $mainSnak ) {
+		$this->mainSnak = $mainSnak;
+	}
+
+	/**
+	 * @see Claim::getQualifiers
+	 *
+	 * @since 0.1
+	 *
+	 * @return Snaks
+	 */
+	public function getQualifiers() {
+		return $this->qualifiers;
+	}
+
+	/**
+	 * @see Claim::setQualifiers
+	 *
+	 * @since 0.1
+	 *
+	 * @param Snaks $propertySnaks
+	 */
+	public function setQualifiers( Snaks $propertySnaks ) {
+		$this->qualifiers = $propertySnaks;
+	}
 
 }
