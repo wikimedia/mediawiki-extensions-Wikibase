3dc67d93cb9c202cbccf65a9810701ef64779f0f
jeroendedauw <jeroendedauw@gmail.com>
2012-08-21 21:05:33 +0200

    Implementing abstract store interface [DO NOT MERGE]
    This is not fully implemented yet and currently breaks stuff. On gerrit so people can review.
    
    Change-Id: I6ee67007db17053357e5eca46f0e0c7cdb4d6b09

---
+++ repo/includes/store/Store.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Wikibase;
+
+/**
+ * Store interface. All interaction with store Wikibase does on top
+ * of storing pages and associated core MediaWiki indexing is done
+ * throuhg this interface.
+ *
+ * @since 0.1
+ *
+ * @file
+ * @ingroup Wikibase
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+interface Store {
+
+	/**
+	 * Returns an instance of the store.
+	 *
+	 * @since 0.1
+	 *
+	 * @return Store
+	 */
+	public function singleton();
+
+	/**
+	 * Returns a new EntityDeletionHandler for this store.
+	 *
+	 * @since 0.1
+	 *
+	 * @return EntityDeletionHandler
+	 */
+	public function newEntityDeletionHandler();
+
+	/**
+	 * Returns a new EntityUpdateHandler for this store.
+	 *
+	 * @since 0.1
+	 *
+	 * @return EntityUpdateHandler
+	 */
+	public function newEntityUpdateHandler();
+
+	/**
+	 * Removes all data from the store.
+	 *
+	 * @since 0.1
+	 */
+	public function clear();
+
+	// TODO
+	public function getIdsForLabel( $language, $label, $description = null );
+
+}
