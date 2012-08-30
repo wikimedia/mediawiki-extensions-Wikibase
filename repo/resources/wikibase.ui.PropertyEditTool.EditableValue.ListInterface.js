62dec51b0db6fb89c50838711f2b65fa6a763e7e
Daniel Werner <daniel.werner@wikimedia.de>
2012-08-31 01:04:27 +0200

    Fixed bug where aliases would be broader than grid layout
    The max-width for aliases in edit mode was set to 1000. This is now flexible and set to the current width of the aliases <ul> list. We also do this max-width calculation for the helper tag provided by the tag-a-data widget, this was not done before but nobody noticed because the default here was 1000 as well.
    
    patchset 2: rebase & fixed conflicts
    
    Change-Id: Ic023f5662ae98dbeb3835392194f2d0887067c7c

--- repo/resources/wikibase.ui.PropertyEditTool.EditableValue.ListInterface.js
+++ repo/resources/wikibase.ui.PropertyEditTool.EditableValue.ListInterface.js
@@ -38,26 +38,36 @@ wb.ui.PropertyEditTool.EditableValue.ListInterface = wb.utilities.inherit( $PARE
 		.css() function would return unexpected results
 		*/
 		var self = this;
-		$.each( this._getTagadata().tagList.children( 'li' ).find( 'input' ), function( i, input ) {
-			if ( $( input ).inputAutoExpand ) {
-				$( input ).inputAutoExpand( {
-					expandOnResize: false,
-					maxWidth: 1000
-					/* // TODO/FIXME: both solutions are not perfect, when tag larger than available space either the
-					   // input will be auto-resized and not show the whole text or we still show the whole tag but it
-					   // will break the site layout. A solution would be replacing input with textarea.
-					maxWidth: function() {
-						var tagList = self._getTagadata().tagList;
-						var origCssDisplay = tagList.css( 'display' );
-						tagList.css( 'display', 'block' );
-						var width = tagList.width();
-						tagList.css( 'display', origCssDisplay );
-						return width;
-					}
-					*/
-				} );
-			}
-		} );
+		if ( $.fn.inputAutoExpand ) {
+			var expansionOptions = {
+				expandOnResize: false,
+				maxWidth: function() {
+					// TODO/FIXME: figure out why this requires at least -17, can't be because of padding + border
+					// which is only 10 in total
+					return self._inputElem.width() - 20;
+				}
+				/* // TODO/FIXME: both solutions are not perfect, when tag larger than available space either the
+				 // input will be auto-resized and not show the whole text or we still show the whole tag but it
+				 // will break the site layout. A solution would be replacing input with textarea.
+				 maxWidth: function() {
+				 var tagList = self._getTagadata().tagList;
+				 var origCssDisplay = tagList.css( 'display' );
+				 tagList.css( 'display', 'block' );
+				 var width = tagList.width();
+				 tagList.css( 'display', origCssDisplay );
+				 return width;
+				 }
+				 */
+			};
+
+			// calculate size for all input elements initially:
+			this._getTagadata().tagList.children( 'li' ).find( 'input' ).inputAutoExpand( expansionOptions );
+
+			// also make sure that new helper tags will calculate size correctly:
+			this._inputElem.on( 'tagadatahelpertagadded', function( e, tag ) {
+				$( tag ).find( 'input' ).inputAutoExpand( expansionOptions )
+			} );
+		}
 	},
 
 	/**
