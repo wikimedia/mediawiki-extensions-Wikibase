20a5f4d1a4fd64304418223e708df39d7241f147
Tobias Gritschacher <tobias.gritschacher@wikimedia.de>
2012-08-31 12:48:06 +0200

    selenium tests for undelete
    - just some undelete tests
    - additional tests for checking correct behaviour on conflicts while
      undeleting
    
    patchset 2: added tests for checking conflicts on undelete
    
    Change-Id: I47833dc5be53b3cc533e8e9bafc9318d2bddaaab

---
+++ selenium/lib/pages/undelete_item_page.rb
@@ -0,0 +1,20 @@
+# -*- encoding : utf-8 -*-
+# Wikidata UI tests
+#
+# Author:: Tobias Gritschacher (tobias.gritschacher@wikimedia.de)
+# License:: GNU GPL v2+
+#
+# page object for undelete special page
+
+class UndeleteItemPage < ItemPage
+  include PageObject
+
+  button(:undelete, :id => 'mw-undelete-submit')
+  div(:undeleteErrorDiv, :class => "error")
+  link(:conflictingItemLink, :xpath => "//div[contains(@class, 'error')]/ul/li/a[2]")
+
+  def undelete_item(item_id)
+    navigate_to(WIKI_REPO_URL + "Special:Undelete/Data:Q" + item_id)
+    undelete
+  end
+end
