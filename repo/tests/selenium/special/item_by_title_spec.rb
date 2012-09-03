495cf8eff90bc3455e5518e26dda6aa5e546e540
Tobias Gritschacher <tobias.gritschacher@wikimedia.de>
2012-09-03 17:55:45 +0200

    selenium test for ItemByTitle special page
    patchset 2: rebased & solved conflicts in Rakefile
    
    Change-Id: Ie468ba683b4322991050dbcd5c8ad38f6556ed7a

---
+++ repo/tests/selenium/special/item_by_title_spec.rb
@@ -0,0 +1,59 @@
+# Wikidata UI tests
+#
+# Author:: Tobias Gritschacher (tobias.gritschacher@wikimedia.de)
+# License:: GNU GPL v2+
+#
+# tests for ItemByTitle special page
+
+require 'spec_helper'
+
+label = "Malaysia"
+description = "Country in Southeast Asia"
+sitelinks = [["en", "Malaysia"], ["fr", "Malaisie"]]
+
+describe "Check ItemByTitle special page" do
+
+  context "item by title test setup" do
+    it "should create item, enter label, description and add en/fr sitelinks" do
+      visit_page(ItemPage) do |page|
+        page.uls_switch_language("english")
+        page.wait_for_item_to_load
+        page.create_new_item(label, description)
+        page.wait_for_item_to_load
+        page.wait_for_sitelinks_to_load
+        page.add_sitelinks(sitelinks)
+      end
+    end
+  end
+
+  context "item by title functionality test" do
+    it "should find an item through ItemByTitle special page" do
+      visit_page(ItemByTitlePage) do |page|
+        page.itemByTitleSiteField = sitelinks[0][0] + "wiki"
+        page.itemByTitlePageField = sitelinks[0][1]
+        page.itemByTitleSubmit
+        page.wait_for_item_to_load
+        page.itemLabelSpan.should == label
+        page.itemDescriptionSpan.should == description
+        @browser.back
+        page.itemByTitleSiteField = sitelinks[1][0] + "wiki"
+        page.itemByTitlePageField = sitelinks[1][1]
+        page.itemByTitleSubmit
+        page.wait_for_item_to_load
+        page.itemLabelSpan.should == label
+        page.itemDescriptionSpan.should == description
+      end
+    end
+  end
+
+  context "item by title test teardown" do
+    it "should remove all sitelinks" do
+      on_page(ItemPage) do |page|
+        page.navigate_to_item
+        page.wait_for_item_to_load
+        page.wait_for_sitelinks_to_load
+        page.remove_all_sitelinks
+      end
+    end
+  end
+end
