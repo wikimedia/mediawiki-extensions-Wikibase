b9b4e82629e4dfe31f161e7b8a5f8e6a45bd5454
daniel <daniel.kinzler@wikimedia.de>
2012-09-04 10:16:26 +0200

    Make permission checks via EditEntity.
    Refactored permission checks in EntityContent, integrated them into
    EditEntity, wroite test cases to make sure permissions are always
    checked and also work for items that have not yet been saved.
    
    Change-Id: I01ef4b6860a5679eb775e681bff2aa2c527de8bb

--- repo/tests/phpunit/includes/api/ApiModifyItemBase.php
+++ repo/tests/phpunit/includes/api/ApiModifyItemBase.php
@@ -448,6 +448,34 @@ abstract class ApiModifyItemBase extends ApiTestCase {
 	}
 
 	/**
+	 * Utility function for applying a set of permissions to $wgGroupPermissions.
+	 * Automatically resets the rights cache for $wgUser.
+	 * No measures are taken to restore the original permissions later, this is up to the caller.
+	 *
+	 * @param $permissions
+	 */
+	public static function applyPermissions( $permissions ) {
+		global $wgGroupPermissions;
+		global $wgUser;
+
+		if ( !$permissions ) {
+			return;
+		}
+
+		foreach ( $permissions as $group => $rights ) {
+			if ( !empty( $wgGroupPermissions[ $group ] ) ) {
+				$wgGroupPermissions[ $group ] = array_merge( $wgGroupPermissions[ $group ], $rights );
+			} else {
+				$wgGroupPermissions[ $group ] = $rights;
+			}
+		}
+
+		# reset rights cache
+		$wgUser->addGroup( "dummy" );
+		$wgUser->removeGroup( "dummy" );
+	}
+
+	/**
 	 * Utility function for converting an array from "deep" (indexed) to "flat" (keyed) structure.
 	 * Arrays that already use a flat structure are left unchanged.
 	 *
