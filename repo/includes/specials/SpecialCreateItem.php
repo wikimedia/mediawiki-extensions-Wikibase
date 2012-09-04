444368255fb5251a626be94dd7bf775c88a8f290
Jens Ohlig <jens.ohlig@wikimedia.de>
2012-09-04 19:50:15 +0200

    (39581) Special:CreateItem first iteration [DO NOT MERGE]
    - using a form instead of an empty element
    
    Change-Id: I715ac9fb1887eb81651249058df5b782ebb5e777

--- repo/includes/specials/SpecialCreateItem.php
+++ repo/includes/specials/SpecialCreateItem.php
@@ -10,6 +10,7 @@
  *
  * @licence GNU GPL v2+
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ * @author Jens Ohlig
  */
 class SpecialCreateItem extends SpecialWikibasePage {
 
@@ -36,10 +37,37 @@ class SpecialCreateItem extends SpecialWikibasePage {
 		$this->checkPermissions();
 		$this->outputHeader();
 
-		$options = ParserOptions::newFromContext( $this->getContext() );
-		$options->setEditSection( true ); //NOTE: editing must be enabled
+		$request = $this->getRequest();
+		$parts = ( $this->subPage === '' ) ? array() : explode( '/', $this->subPage, 2 );
+		$label = $request->getVal( 'label', isset( $parts[0] ) ? $parts[0] : '' );
+		$description = $request->getVal( 'description', isset( $parts[1] ) ? $parts[1] : '' );
+		$lang = 'en';
 
-		$view = new Wikibase\ItemView( $this->getContext() );
-		$view->render( Wikibase\ItemContent::newEmpty(), $this->getOutput(), $options );
+		if ( ( isset( $label ) && $label != '' ) || ( isset( $description ) && $description != '' ) ) {
+			// TODO: sanity check, escaping, etc..
+			$itemContent = \Wikibase\ItemContent::newEmpty();
+			$itemContent->getEntity()->setLabel( $lang, $label );
+			$itemContent->getEntity()->setDescription( $lang, $description );
+			// TODO: thats not the way we should do it I think ( should use EditEntity::attemptSave() )
+			$itemContent->save();
+			// TODO: redirect to item page
+		} else {
+			$this->createItemForm( $label, $description );
+		}
+	}
+
+	public function createItemForm( $label, $description ) {
+		$this->getOutput()->addHTML(
+				$this->msg( 'wikibase-createitem-intro' )->text()
+				. Html::openElement( 'form', array( 'method' => 'get', 'action' => "", 'name' => 'createitem', 'id' => 'mw-createitem-form1' ) )
+				. Xml::fieldset( $this->msg( 'wikibase-createitem-fieldset' )->text() )
+				. Xml::inputLabel( $this->msg( 'wikibase-createitem-label' )->text(), 'label', '', 12, $label ? htmlspecialchars( $label ) : '' )
+				. Xml::closeElement('br')
+				. Xml::inputLabel( $this->msg( 'wikibase-createitem-description' )->text(), 'description', '', 36, $description ? htmlspecialchars( $description ) : '' )
+				. Xml::closeElement( 'br' )
+				. Xml::submitButton( $this->msg( 'wikibase-createitem-submit' )->text() )
+				. Html::closeElement( 'fieldset' )
+				. Html::closeElement( 'form' )
+		);
 	}
 }
