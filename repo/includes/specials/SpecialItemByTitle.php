dc50325e138c282998ca8d61fbcb0295127944a2
Jens Ohlig <jens.ohlig@wikimedia.de>
2012-09-07 12:02:35 +0200

    Special:ItemByTitle -- needless text removed.
    Change-Id: I6d2b083d82107d249c4defb6d0d468d2fc744b01

--- repo/includes/specials/SpecialItemByTitle.php
+++ repo/includes/specials/SpecialItemByTitle.php
@@ -14,104 +14,91 @@
  */
 class SpecialItemByTitle extends SpecialItemResolver {
 
-	/**
-	 * Constructor.
-	 *
-	 * @since 0.1
-	 */
-	public function __construct() {
-		parent::__construct( 'ItemByTitle' );
-	}
+  /**
+   * Constructor.
+   *
+   * @since 0.1
+   */
+  public function __construct() {
+    parent::__construct( 'ItemByTitle' );
+  }
 
-	/**
-	 * Main method.
-	 *
-	 * @since 0.1
-	 *
-	 * @param string|null $subPage
-	 *
-	 * @return boolean
-	 */
-	public function execute( $subPage ) {
-		parent::execute( $subPage );
+  /**
+   * Main method.
+   *
+   * @since 0.1
+   *
+   * @param string|null $subPage
+   *
+   * @return boolean
+   */
+  public function execute( $subPage ) {
+    parent::execute( $subPage );
 
-		// Setup
-		$request = $this->getRequest();
-		$parts = ( $this->subPage === '' ) ? array() : explode( '/', $this->subPage, 2 );
-		$siteId = $request->getVal( 'site', isset( $parts[0] ) ? $parts[0] : '' );
-		$page = $request->getVal( 'page', isset( $parts[1] ) ? $parts[1] : '' );
-		$itemContent = null;
+    // Setup
+    $request = $this->getRequest();
+    $parts = ( $this->subPage === '' ) ? array() : explode( '/', $this->subPage, 2 );
+    $siteId = $request->getVal( 'site', isset( $parts[0] ) ? $parts[0] : '' );
+    $page = $request->getVal( 'page', isset( $parts[1] ) ? $parts[1] : '' );
+    $itemContent = null;
 
-		// Create an item view
-		if ( isset( $siteId ) && isset( $page ) ) {
-			$itemContent = \Wikibase\ItemHandler::singleton()->getFromSiteLink( $siteId, $page );
-			if ( $itemContent !== null ) {
-				$itemUrl = $itemContent->getTitle()->getFullUrl();
-				$this->getOutput()->redirect( $itemUrl );
-			}
-		}
+    // Create an item view
+    if ( isset( $siteId ) && isset( $page ) ) {
+      $itemContent = \Wikibase\ItemHandler::singleton()->getFromSiteLink( $siteId, $page );
+      if ( $itemContent !== null ) {
+        $itemUrl = $itemContent->getTitle()->getFullUrl();
+        $this->getOutput()->redirect( $itemUrl );
+      }
+    }
 
-		// If there is no item content post the switch form
-		if ( $itemContent === null ) {
-			$this->switchForm( $siteId, $page );
-		}
-	}
+    // If there is no item content post the switch form
+    if ( $itemContent === null ) {
+      $this->switchForm( $siteId, $page );
+    }
+  }
 
-	/**
-	 * Output a form to allow searching for a page
-	 *
-	 * @since 0.1
-	 *
-	 * @param string|null $site
-	 * @param string|null $page
-	 */
-	protected function switchForm( $siteId, $page ) {
-		global $wgScript;
+  /**
+   * Output a form to allow searching for a page
+   *
+   * @since 0.1
+   *
+   * @param string|null $site
+   * @param string|null $page
+   */
+  protected function switchForm( $siteId, $page ) {
+    global $wgScript;
 
-		$siteExists = \Sites::singleton()->getSites()->hasSite( $siteId );
+    $siteExists = \Sites::singleton()->getSites()->hasSite( $siteId );
 
-		if ( isset( $siteId ) || isset( $page ) ) {
-			$this->getOutput()->addHTML(
-				Html::openElement( 'div' )
-				. $this->msg( $siteExists ? 'wikibase-itembytitle-nothing-found' : 'wikibase-itembytitle-invalid-site' )
-					->params( htmlspecialchars( $siteId ), htmlspecialchars( $page ) )
-					->parse()
-				. Html::closeElement( 'div' )
-			);
-		}
-		$sites = \Wikibase\ItemView::getSiteDetails();
-		$this->getOutput()->addJsConfigVars( 'wbSiteDetails', $sites );
-		$this->getOutput()->addModules( 'wikibase.ui.PropertyEditTool' );
+    $sites = \Wikibase\ItemView::getSiteDetails();
+    $this->getOutput()->addJsConfigVars( 'wbSiteDetails', $sites );
+    $this->getOutput()->addModules( 'wikibase.ui.PropertyEditTool' );
 
-		$this->getOutput()->addHTML(
-			Html::openElement( 'form', array( 'method' => 'get', 'action' => $wgScript, 'name' => 'itembytitle', 'id' => 'mw-itembytitle-form1' ) )
-			. Html::hidden( 'title',  $this->getTitle()->getPrefixedText() )
-			. Xml::fieldset( $this->msg( 'wikibase-itembytitle-lookup-fieldset' )->text() )
-			. Xml::inputLabel( $this->msg( 'wikibase-itembytitle-lookup-site' )->text(), 'site', 'wb-itembytitle-sitename', 12, $siteId ? htmlspecialchars( $siteId ) : '' )
-			. Xml::inputLabel( $this->msg( 'wikibase-itembytitle-lookup-page' )->text(), 'page', 'pagename', 36, $page ? htmlspecialchars( $page ) : '' )
-			. Xml::submitButton( $this->msg( 'wikibase-itembytitle-submit' )->text() )
-			. Html::closeElement( 'fieldset' )
-			. Html::closeElement( 'form' )
-		);
-		$this->getOutput()->addHTML(
-			Html::openElement( 'div' )
-			. $this->msg( 'wikibase-itembytitle-description' )->text()
-			. Html::closeElement( 'div' )
-		);
-		if ( $siteExists && isset( $page ) ) {
-			$this->getOutput()->addHTML(
-				Html::openElement( 'div' )
-				. $this->msg( 'wikibase-itembytitle-create' )
-					->params(
-						wfUrlencode( $siteId ? $siteId : '' ),
-						wfUrlencode( $page ? $page : '' ),
-						$page ? $page : ''
-					)
-					->parse()
-				. Html::closeElement( 'div' )
-			);
-		}
+    $this->getOutput()->addHTML(
+      Html::openElement( 'form', array( 'method' => 'get', 'action' => $wgScript, 'name' => 'itembytitle', 'id' => 'mw-itembytitle-form1' ) )
+      . Html::hidden( 'title',  $this->getTitle()->getPrefixedText() )
+      . Xml::fieldset( $this->msg( 'wikibase-itembytitle-lookup-fieldset' )->text() )
+      . Xml::inputLabel( $this->msg( 'wikibase-itembytitle-lookup-site' )->text(), 'site', 'wb-itembytitle-sitename', 12, $siteId ? htmlspecialchars( $siteId ) : '' )
+      . Xml::inputLabel( $this->msg( 'wikibase-itembytitle-lookup-page' )->text(), 'page', 'pagename', 36, $page ? htmlspecialchars( $page ) : '' )
+      . Xml::submitButton( $this->msg( 'wikibase-itembytitle-submit' )->text() )
+      . Html::closeElement( 'fieldset' )
+      . Html::closeElement( 'form' )
+    );
+    if ( $siteExists && isset( $page ) ) {
+      $this->getOutput()->addHTML(
+        Html::openElement( 'div' )
+        . $this->msg( 'wikibase-itembytitle-create' )
+          ->params(
+            wfUrlencode( $siteId ? $siteId : '' ),
+            wfUrlencode( $page ? $page : '' ),
+            $page ? $page : ''
+          )
+          ->parse()
+        . Html::closeElement( 'div' )
+      );
+    }
 
-	}
+  }
 
 }
+
