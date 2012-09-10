ad1cefe02cbe9ca7788d47d23cdf68c0b2181c7f
Daniel Werner <daniel.werner@wikimedia.de>
2012-09-10 17:17:03 +0200

    Moved special page stuff from startup to their own files
    This is about Special:ItemByTitle and Special:ItemDisambiguation who have their own files for their JavaScript code now.
    
    Change-Id: Ifcd8f7458ded7ff11a6f07d9200a717cdb741596

--- repo/includes/specials/SpecialItemDisambiguation.php
+++ repo/includes/specials/SpecialItemDisambiguation.php
@@ -61,11 +61,7 @@ class SpecialItemDisambiguation extends SpecialItemResolver {
 				$this->displayDisambiguationPage( $itemContents, $language );
 			}
 		}
-
-		// If there is no item content post the switch form
-		if ( $itemContents === array() ) {
-			$this->switchForm( $language, $label );
-		}
+		$this->switchForm( $language, $label );
 	}
 
 	/**
@@ -92,23 +88,11 @@ class SpecialItemDisambiguation extends SpecialItemResolver {
 	protected function switchForm( $langCode, $label ) {
 		global $wgScript;
 
-		// Misuse language name to figure out if the code is defined
-		$languageName = \Wikibase\Utils::fetchLanguageName( $langCode );
-
-		if ( isset( $langCode ) || isset( $label ) ) {
-			$this->getOutput()->addHTML(
-				Html::openElement( 'div' )
-				. $this->msg( $languageName !== $langCode ? 'wikibase-itemdisambiguation-nothing-found' : 'wikibase-itemdisambiguation-invalid-langcode' )
-					->params( $langCode, $label )
-					->parse()
-				. Html::closeElement( 'div' )
-			);
-		}
 		$sites = \Wikibase\ItemView::getSiteDetails();
 
 		// The next two lines are here for the site ID autocompletion
-		$this->getOutput()->addJsConfigVars( 'wbSiteDetails', $sites );   // TODO: This should really be in a Resource loader module and not here.
-		$this->getOutput()->addModules( 'wikibase.ui.PropertyEditTool' ); // TODO: startup.js should be separated in a presenter loading edit tools for actual items (usually on item pages) and a presenter for special pages or even one for each special page.
+		$this->getOutput()->addJsConfigVars( 'wbSiteDetails', $sites ); // TODO: This should really be in a Resource loader module and not here.
+		$this->getOutput()->addModules( 'wikibase.special.itemDisambiguation' );
 
 		$this->getOutput()->addHTML(
 			Html::openElement( 'form', array( 'method' => 'get', 'action' => $wgScript, 'name' => 'itemdisambiguation', 'id' => 'wb-itemdisambiguation-form1' ) )
@@ -120,24 +104,6 @@ class SpecialItemDisambiguation extends SpecialItemResolver {
 			. Html::closeElement( 'fieldset' )
 			. Html::closeElement( 'form' )
 		);
-		$this->getOutput()->addHTML(
-			Html::openElement( 'div' )
-			. $this->msg( 'wikibase-itemdisambiguation-description' )->text()
-			. Html::closeElement( 'div' )
-		);
-		if ( isset( $langCode ) && isset( $label ) ) {
-			$this->getOutput()->addHTML(
-				Html::openElement( 'div' )
-				. $this->msg( 'wikibase-itemdisambiguation-create' )
-					->params(
-						wfUrlencode( $langCode ? $langCode : '' ),
-						wfUrlencode( $label ? $label : '' ),
-						$label ? $label : ''
-					)
-					->parse()
-				. Html::closeElement( 'div' )
-			);
-		}
 	}
 
 }
