d7c79898dd0fc2f6a5cade658d22819271cd863c
Daniel Werner <daniel.werner@wikimedia.de>
2012-09-10 17:17:03 +0200

    Moved special page stuff from startup to their own files
    This is about Special:ItemByTitle and Special:ItemDisambiguation who have their own files for their JavaScript code now.
    
    Patch set 3: Rebase.
    
    Change-Id: Ifcd8f7458ded7ff11a6f07d9200a717cdb741596

---
+++ repo/resources/wikibase.special/wikibase.special.itemByTitle.js
@@ -0,0 +1,39 @@
+/**
+ * JavaScript for 'wikibase' extension special page 'ItemByTitle'
+ *
+ * @since 0.1
+ * @file
+ * @ingroup Wikibase
+ *
+ * @licence GNU GPL v2+
+ * @author Jens Ohlig
+ */
+( function( $, mw, wb, undefined ) {
+	'use strict';
+
+	$( document ).ready( function() {
+		if( ( mw.config.get( 'wgCanonicalSpecialPageName' ) !== 'ItemByTitle' ) || ( $.uls === undefined ) ) {
+			return; // not the right special page or ULS dependency unavailable
+		}
+
+		// this will build a drop-down for the language selection:
+		var siteList = new Array();
+		for ( var siteId in wb.getSites() ) {
+			var site = wb.getSite( siteId );
+			siteList.push( {
+				'label': site.getName() + ' (' + site.getId() + ')',
+				'value': site.getName() + ' (' + site.getId() + ')',
+			} );
+		}
+		$( '#wb-itembytitle-sitename' ).wikibaseAutocomplete( { "source": siteList } );
+		// Hackety hack hack...
+		// On submit, replace human readable value like "English (en)" with actual sitename ("enwiki")
+		$( '#wb-itembytitle-form1' ).submit( function() {
+			var langID = String( $( '#wb-itembytitle-sitename' ).val().replace(/.*\(|\).*/gi,'') );
+			if ( wb._siteList[langID].getGlobalSiteId() !== undefined ) {
+				$( '#wb-itembytitle-sitename' ).val( wb._siteList[langID].getGlobalSiteId() );
+			}
+		});
+	} );
+
+} )( jQuery, mediaWiki, wikibase );
