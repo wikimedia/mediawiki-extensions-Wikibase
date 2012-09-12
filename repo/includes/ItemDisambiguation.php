8e0bfc2da4e0f042747bb14c3912ef5f38fdfb46
jeblad <john.blad@wikimedia.de>
2012-09-12 20:06:47 +0200

    (Bug 40132) Added language fallbacks for labels and description
    This is for discussion, it might not be so after final discussion.
    
    The additional marked language link goes to the item with an added
    "uselang". A variation could be to use the label in the search lang
    or the aliases.
    
    Note that the entries will have similar labels in the search language
    unless they are found through the aliases.
    
    Patchset 2: Cleanup
    
    Change-Id: I35d0849dcd8125469049c1917414cd86c48c148c

--- repo/includes/ItemDisambiguation.php
+++ repo/includes/ItemDisambiguation.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Wikibase;
-use IContextSource, MWException;
+use Language, IContextSource, MWException;
 
 /**
  * Class representing the disambiguation of a list of WikibaseItems.
@@ -22,6 +22,10 @@ class ItemDisambiguation extends \ContextSource {
 	 */
 	protected $items;
 
+	/**
+	 * @since 0.1
+	 * @var string
+	 */
 	protected $langCode;
 
 	/**
@@ -42,10 +46,16 @@ class ItemDisambiguation extends \ContextSource {
 		if ( !is_null( $context ) ) {
 			$this->setContext( $context );
 		}
+	}
 
-		//if ( count( $this->items ) < 2 ) {
-		//	throw new MWException( 'Cannot disambiguate less then 2 items!' );
-		//}
+	protected static function getLangStore( $langCode ) {
+		static $langStore = array();
+
+		if ( !isset( $langStore[$langCode] ) ) {
+			$langStore[$langCode] = array_merge( array( $langCode ), Language::getFallbacksFor( $langCode ) );
+		}
+
+		return $langStore[$langCode];
 	}
 
 	/**
@@ -56,45 +66,120 @@ class ItemDisambiguation extends \ContextSource {
 	 * @return string
 	 */
 	public function getHTML() {
-		//global $wgContLang;
-		$langCode = $this->langCode;
 
+		$html = '';
+		foreach ( $this->items as /* ItemContent */ $itemContent ) {
+			$innerHtml = $this->getHtmlForLabel( $itemContent, self::getLangStore( $this->getLanguage()->getCode() ) );
+			$innerHtml .= $this->getHtmlForLanguage( $itemContent, $this->langCode );
+			$innerHtml .= $this->getHtmlForDescription( $itemContent, self::getLangStore( $this->getLanguage()->getCode() ) );
+
+			$html .= \Html::rawElement(
+				'li',
+				array( 'class' => 'wikibase-disambiguation' ),
+				$innerHtml
+			);
+		}
+
+		return \Html::rawElement(
+			'ul',
+			array( 'class' => 'wikibase-disambiguation' ),
+			$html
+		);
+	}
+
+	/**
+	 * Create HTML for display of the label in the item disambiguation list
+	 *
+	 * @since 0.1
+	 */
+	protected function getHtmlForLabel( $itemContent, $userLanguages ) {
+
+		// Get the label for the first languages on the chain
+		// that doesn't fail, use a fallback if everything fails. This could
+		// use the user supplied list of acceptable languages as a filter.
+		list( $labelCode, $labelText, $labelLang) =
+			Utils::lookupMultilangText(
+				$itemContent->getItem()->getLabels( $userLanguages ),
+				$userLanguages,
+				array(
+					$this->getLanguage()->getCode(),
+					wfMessage( 'wikibase-itemlink-id-wrapper' )->params( $itemContent->getTitle()->getText() )->escaped(),
+					$this->getLanguage()
+				)
+			);
+
+		// Return the initial link in the users own language
 		return
-			'<ul class="wikibase-disambiguation">' .
-				implode( '', array_map(
-					function( ItemContent $item ) use ( $langCode ) {
-						global $wgLang;
-						// Figure out which description to use while identifying the item
-						list( $descriptionCode, $descriptionText, $descriptionLang) =
-							\Wikibase\Utils::lookupUserMultilangText(
-								$item->getItem()->getDescriptions(),
-								\Wikibase\Utils::languageChain( $langCode ),
-								array( $langCode, '', \Language::factory( $langCode ) )
-							);
-						return \Html::rawElement(
-							'li',
-							array( 'class' => 'wikibase-disambiguation' ),
-							\Linker::link(
-								$item->getTitle(),
-								// FIXME: Need a more general way to figure out the "q" thingy.
-								// This should REALLY be something more elegant, but it is sufficient for now.
-								\Html::openElement( 'span', array( 'class' => 'wb-itemlink-id' ) )
-								. $item->getItem()->getLabel( $langCode  ) ? $item->getItem()->getLabel( $langCode ) : wfMessage( 'wikibase-itemlink-id-wrapper' )->params( 'q' . $item->getItem()->getId() )->escaped()
-								. \Html::closeElement( 'span' )
-							)
-							. \Html::openElement( 'span', array( 'class' => 'wb-itemlink-query-lang' ) )
-							// really ugly way to add parenthesis... ;/
-							. '&nbsp;[' . \Linker::link( $item->getTitle(), $langCode, array(), array( 'uselang' => $langCode ) ) . ']'
-							. \Html::closeElement( 'span' )
-							. wfMessage( 'colon-separator' )->escaped()
-							. \Html::openElement( 'span', array( 'class' => 'wb-itemlink-description', 'lang' => $descriptionLang->getCode(), 'dir' => $descriptionLang->getDir() ) )
-							. htmlspecialchars( $descriptionText )
-							. \Html::closeElement( 'span' )
-						);
-					},
-					$this->items
-				) ).
-			'</ul>';
+			\Linker::link(
+				$itemContent->getTitle(),
+				\Html::element(
+					'span',
+					array( 'class' => 'wb-itemlink-id', 'lang' => htmlspecialchars( $labelCode ) ),
+					htmlspecialchars( $labelText )
+				)
+			);
+	}
+
+	/**
+	 * Create HTML for display of the language bracket in the item disambiguation list
+	 *
+	 * @since 0.1
+	 */
+	protected function getHtmlForLanguage( $itemContent, $queryLanguage ) {
+
+		// Return the link for the query language
+		return
+			\Html::rawElement(
+				'span',
+				array( 'class' => 'wb-itemlink-query-lang' ),
+				wfMessage( 'wikibase-language-id-wrapper' )->rawParams(
+					\Linker::link(
+						$itemContent->getTitle(),
+						htmlspecialchars( $queryLanguage ),
+						array(),
+						array( 'uselang' => htmlspecialchars( $queryLanguage ) )
+					)
+				)->escaped()
+			);
+	}
+
+	/**
+	 * Create HTML for display of the description in the item disambiguation list
+	 *
+	 * @since 0.1
+	 */
+	protected function getHtmlForDescription( $itemContent, $userLanguages ) {
+
+		// Get the description for the first language found on the chain
+		// that doesn't fail, use a fallback if everything fails. This could
+		// use the user supplied list of acceptable languages as a filter.
+		list( $descriptionCode, $descriptionText, $descriptionLang) =
+			Utils::lookupMultilangText(
+				$itemContent->getItem()->getDescriptions( $userLanguages ),
+				$userLanguages,
+				array(
+					$this->getLanguage()->getCode(),
+					'',
+					$this->getLanguage()
+				)
+			);
+
+		// Return the description in the users own language
+		return
+			\Html::rawElement(
+				'span',
+				array( 'class' => 'wb-itemlink-separator' ),
+				wfMessage( 'colon-separator' )->escaped()
+			)
+			. \Html::element(
+				'span',
+				array(
+					'class' => 'wb-itemlink-description',
+					'lang' => $descriptionLang->getCode(),
+					'dir' => $descriptionLang->getDir()
+				),
+				htmlspecialchars( $descriptionText )
+			);
 	}
 
 	/**
