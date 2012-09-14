9fb7c86d7a31f75e1e3a310706aa05f3397d5cec
daniel <daniel.kinzler@wikimedia.de>
2012-09-14 17:23:58 +0200

    500x speedup for importInterlang
    Change-Id: I9332e071d05e9600590b98fd7c0ae124b26a8822

--- repo/maintenance/importInterlang.php
+++ repo/maintenance/importInterlang.php
@@ -28,6 +28,8 @@ class importInterlang extends Maintenance {
 	protected $api;
 	protected $verbose = false;
 	protected $ignore_errors = false;
+	protected $skip = 0;
+	protected $only = 0;
 
 	public function __construct() {
 		$this->mDescription = "Import interlanguage links in Wikidata.\n\nThe links may be created by extractInterlang.sql";
@@ -36,9 +38,8 @@ class importInterlang extends Maintenance {
 		$this->addOption( 'only', "Only import the specific entry from the import file" );
 		$this->addOption( 'verbose', "Print API requests and responses" );
 		$this->addOption( 'ignore-errors', "Ignore API errors" );
-		$this->addArg( 'lang', "Language code of the base wiki", true );
+		$this->addArg( 'lang', "The source wiki's language code", true );
 		$this->addArg( 'filename', "File with interlanguage links", true );
-		$this->addArg( 'api', "Base API url", true );
 
 		parent::__construct();
 	}
@@ -50,17 +51,27 @@ class importInterlang extends Maintenance {
 		$this->only = (int)$this->getOption( 'only' );
 		$lang = $this->getArg( 0 );
 		$filename = $this->getArg( 1 );
-		$this->api = $this->getArg( 2 );
 
 		$file = fopen( $filename, "r" );
+
+		if ( !$file ) {
+			$this->doPrint( "ERROR: failed to open `$filename`" );
+			return;
+		}
+
 		fgets( $file ); // We don't need the first line with column names.
-		$current = ""; $current_id = false;
+
+		$current = null;
+		$current_links = array();
 		$count = 0;
 		while( $link = fgetcsv( $file, 0, "\t" ) ) {
 			try {
 				if( $link[0] !== $current ) {
+					if ( !empty( $current_links ) ) {
+						$this->createItem( $current_links );
+					}
+
 					$count++;
-					$current = $link[0];
 					if ( ( $this->skip !== 0 ) && ( $this->skip > $count ) ) {
 						continue;
 					}
@@ -70,12 +81,16 @@ class importInterlang extends Maintenance {
 						}
 						continue;
 					}
-					$this->maybePrint( "New item: $current" );
-					$current_id = $this->createItem( $lang, $current );
+
+					$current = $link[0];
+					$this->maybePrint( "Processing `$current`" );
+
+					$current_links = array(
+						$lang => $current
+					);
 				}
-				if ( $current_id ) {
-					$this->addLink( $link[1], $link[2], $current_id );
-				}
+
+				$current_links[ $link[1] ] = $link[2];
 			} catch( importInterlangException $e ) {
 				if( !$this->ignore_errors ) {
 					throw $e;
@@ -86,103 +101,40 @@ class importInterlang extends Maintenance {
 			}
 		}
 
+		if ( !empty( $current_links ) ) {
+			$this->createItem( $current_links );
+		}
+
 		echo "Done!\n";
 	}
 
-	protected function createItem( $lang, $link ) {
-		$link = self::niceLink( $link );
+	protected function createItem( $links ) {
+		$item = \Wikibase\ItemObject::newEmpty();
 
-		$api_response = $this->callAPI( $this->api . "?action=wbgetitems&format=php&sites=" . urlencode( $lang ) . "wiki" . "&titles=" . urlencode( $link ) );
-		if( $this->verbose ) {
-			print_r ( $api_response );
-		}
-		if( isset( $api_response['error'] ) ) {
-			if( $api_response['error']['code'] !== 'no-such-item' ) {
-				throw new importInterlangException( $api_response['error']['info'] );
-			}
-		} else {
-			if( isset( $api_response['success'] ) && $api_response['success'] ) {
-				$found = array_filter( $api_response['items'], function( $item ) { return !isset( $item['missing'] ); } );
-				if ( count($found) > 1 ) {
-					throw new importInterlangException( "several hits: " . count($found) );
-				}
-				$item = array_pop( $found );
-				if ( isset( $item['id'] ) && $item['id'] !== 0 ) {
-					$this->addLink( $lang, $link, $item['id'] );
-					$this->maybePrint( "The ID#2 is now: " . $item['id'] );
-					return $item['id'];
-				}
-			} else {
-				throw new importInterlangException( "no success" );
+		foreach ( $links as $lang => $title ) {
+			$name = strtr( $title, "_", " " );
+			$label = preg_replace( '/ *\(.*\)$/u', '', $name );
+
+			$item->setLabel( $lang, $label );
+
+			$siteLink = \Wikibase\SiteLink::newFromText( $lang . 'wiki',  $name );
+
+			if ( $siteLink ) {
+				$item->addSiteLink( $siteLink );
 			}
 		}
 
-		// We only reach this if we have received an error, and the error was no-such-item
-		// Note: After the changes we will reach this point if no valid items are found,
-		// that is they are "missing"
-		$api_response = $this->callAPI( $this->api . "?action=wbsetitem&data=%7B%7D&format=php" );
-		if( $this->verbose ) {
-			print_r ( $api_response );
-		}
-		if( isset( $api_response['error'] ) ) {
-			throw new importInterlangException( $api_response['error']['info'] );
-		}
-		if( isset( $api_response['success'] ) && $api_response['success'] ) {
-			$this->addLink( $lang, $link, $api_response['item']['id'] );
-			$this->maybePrint( "The ID is now: " . $api_response['item']['id'] );
-			return $api_response['item']['id'];
-		} else {
-			throw new importInterlangException( "no success" );
-		}
-	}
+		$content = \Wikibase\ItemContent::newFromItem( $item );
 
-	protected function addLink( $lang, $link, $id ) {
-		print( "$lang: $link\n" );
-		// If a link is empty (which is a valid MediaWiki interlanguage link), fail silently.
-		if( $link === "" ) {
-			$this->maybePrint( "Skipping empty link." );
-			return;
-		}
-		if( $id === "" ) {
-			$this->maybePrint( "Skipping empty id." );
-			return;
-		}
-		$label = self::niceLink( $link );
+		try {
+			$status = $content->save( "imported", null, EDIT_NEW );
 
-		$api_response = $this->callAPI( $this->api . "?action=wbsetsitelink&format=php&id=" . urlencode( $id )	. "&linksite=" . urlencode( $lang ) . "wiki" . "&linktitle=" . urlencode( $link ) );
-		if( $this->verbose ) {
-				print_r ( $api_response );
+			if ( !$status->isOK() ) {
+				$this->doPrint( "ERROR: " . strtr( $status->getMessage(), "\n", " " ) );
+			}
+		} catch ( MWException $ex ) {
+			$this->doPrint( "ERROR: " . strtr( $ex->getMessage(), "\n", " " ) );
 		}
-		if( isset( $api_response['error'] ) ) {
-			throw new importInterlangException( $api_response['error']['info'] );
-		}
-
-		$api_response = $this->callAPI( $this->api . "?action=wbsetlabel&format=php&id=" . urlencode( $id ) . "&language=" . urlencode( $lang ) . "&value=" . urlencode( $label ) );
-		if( isset( $api_response['error'] ) ) {
-			throw new importInterlangException( $api_response['error']['info'] );
-		}
-	}
-
-	/**
-	 * Call the API, return the results.
-	 */
-	protected function callAPI( $url ) {
-		$this->maybePrint( $url );
-		$api_response = Http::post( $url );
-		$this->maybePrint( "API response is: " . $api_response . "\n" );
-		$api_response = unserialize( $api_response );
-		$this->maybePrint( $api_response );
-		if( empty( $api_response ) ) {
-			throw new importInterlangException( "API returned invalid response" );
-		}
-		return $api_response;
-	}
-
-	/**
-	 * Make a nicer link (convert _ to	).
-	 */
-	protected static function niceLink( $link ) {
-		return strtr( $link, "_", " " );
 	}
 
 	/**
@@ -190,13 +142,20 @@ class importInterlang extends Maintenance {
 	 */
 	protected function maybePrint( $a ) {
 		if( $this->verbose ) {
-			if( is_bool( $a ) ) {
-				echo $a? "true\n": "false\n";
-			} elseif( is_scalar( $a ) ) {
-				echo $a . (substr( $a, -1 ) != "\n"? "\n": "");
-			} else {
-				print_r( $a );
-			}
+			$this->doPrint( $a );
+		}
+	}
+
+	/**
+	 * Print a scalar, array or object
+	 */
+	protected function doPrint( $a ) {
+		if( is_bool( $a ) ) {
+			echo $a? "true\n": "false\n";
+		} elseif( is_scalar( $a ) ) {
+			echo trim( strval( $a ) ) . "\n";
+		} else {
+			print_r( $a );
 		}
 	}
 
