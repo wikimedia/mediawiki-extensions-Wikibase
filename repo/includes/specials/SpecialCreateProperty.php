a85116aae968764d01c10caaf3bee77e8ab8407a
jeblad <john.blad@wikimedia.de>
2012-09-14 12:47:45 +0200

    (Bug 40227) Initial commit for SpecialCreateEntity
    This refactors the SpecialCreateItem into two classes for
    item and entity, and also add a class for creating properties.
    
    Note that the PropertyContent needs updating, but after the
    update the ItemView will fail. That is on-going work so let
    this "bug" be for now. Due to this the property will be saved
    (and viewed) as items which is somewhat weird.
    
    ItemView needs to be updated so it handles the view correctly,
    or rater a new EntityView must be made with a factory method.
    
    There are still missing things in the formatting of the page,
    and there are no eye-candy like drop-down datatype lists.
    
    - patch set 2: generalized css and added skeleton for data type select box
    
    - patch set 3: Added a bit of config stuff to be able to test
    Change-Id: I47d0e2af32ca55d629dd85c3a230ac4d37c9f6af

---
+++ repo/includes/specials/SpecialCreateProperty.php
@@ -0,0 +1,111 @@
+<?php
+
+/**
+ * Page for creating new Wikibase properties.
+ *
+ * @since 0.1
+ *
+ * @file 
+ * @ingroup Wikibase
+ *
+ * @licence GNU GPL v2+
+ * @author John Erling Blad < jeblad@gmail.com >
+ */
+class SpecialCreateProperty extends SpecialCreateEntity {
+
+	/**
+	 * @var string
+	 */
+	protected $datatype = null;
+
+	/**
+	 * Constructor.
+	 *
+	 * @since 0.1
+	 */
+	public function __construct() {
+		parent::__construct( 'CreateProperty' );
+	}
+
+	/**
+	 * @see SpecialCreateEntity::prepareArguments()
+	 */
+	protected function prepareArguments() {
+		parent::prepareArguments();
+		$this->datatype = $this->getRequest()->getVal( 'datatype', isset( $this->parts[2] ) ? $this->parts[2] : '' );
+		return true;
+	}
+
+	/**
+	 * @see SpecialCreateEntity::hasSufficientArguments()
+	 */
+	protected function hasSufficientArguments() {
+		// TODO: Needs refinement
+		return parent::hasSufficientArguments() && ( $this->datatype !== '' );
+	}
+
+	/**
+	 * @see SpecialCreateEntity::createEntity()
+	 */
+	protected function createEntity() {
+		return \Wikibase\PropertyContent::newEmpty();
+	}
+
+	/**
+	 * @see SpecialCreateEntity::modifyEntity()
+	 */
+	protected function modifyEntity( \Wikibase\EntityContent &$entity ) {
+		$status = parent::modifyEntity( $entity );
+		$lang = $this->getLanguage()->getCode();
+		if ( $this->datatype !== '' ) {
+			// how to set this and where to get it
+			//$entity->getEntity()->setDatatype( $lang, $this->datatype );
+		}
+		return $status;
+	}
+
+	/**
+	 * @see SpecialCreateEntity::additionalFormElements()
+	 */
+	protected function additionalFormElements() {
+		return parent::additionalFormElements()
+			. Html::element(
+				'label',
+				array(
+					'for' => 'wb-createproperty-datatype',
+					'class' => 'wb-label'
+				),
+				$this->msg( 'wikibase-createproperty-datatype' )->text()
+			)
+			. $this->getDataTypes()
+			. Html::element( 'br' );
+	}
+
+	protected function getDataTypes() {
+		$html = '';
+		foreach ( \Wikibase\Settings::get( 'testDataTypes' ) as $option ) {
+			$html .= Html::element(
+				'option',
+				array(),
+				$option
+			);
+		}
+		return
+			\Html::rawElement(
+				'select',
+				array(
+					'id' => 'wb-createproperty-datatype',
+					'class' => 'wb-select'
+				),
+				$html
+			);
+	}
+
+	/**
+	 * @see SpecialCreateEntity::getLegend()
+	 */
+	protected function getLegend() {
+		return $this->msg( 'wikibase-createproperty-fieldset' );
+	}
+
+}
