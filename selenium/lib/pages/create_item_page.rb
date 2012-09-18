029f0bb205e8e34589b53ad403e16c637679685e
Tobias Gritschacher <tobias.gritschacher@wikimedia.de>
2012-09-18 10:02:20 +0200

    made item id prefix configurable in selenium tests
    Change-Id: Ia1a7ef5b8c7eab4ccab7f28def93e71f138970fe

--- selenium/lib/pages/create_item_page.rb
+++ selenium/lib/pages/create_item_page.rb
@@ -20,8 +20,7 @@ class CreateItemPage < ItemPage
     createItemSubmit
     wait_for_item_to_load
     @@item_url = current_url
-    #@@item_id = @@item_url[@@item_url.index('/Q')+2..-1]
-    query_string = "/" + ITEM_NAMESPACE + "Q"
+    query_string = "/" + ITEM_NAMESPACE + ITEM_ID_PREFIX
     @@item_id = @@item_url[@@item_url.index(query_string)+query_string.length..-1]
     return @@item_id
   end
--- selenium/lib/pages/noexisting_item_page.rb
+++ selenium/lib/pages/noexisting_item_page.rb
@@ -10,7 +10,7 @@ require 'ruby_selenium'
 
 class NonExistingItemPage < RubySelenium
   include PageObject
-  page_url WIKI_REPO_URL + ITEM_NAMESPACE + "Qxy"
+  page_url WIKI_REPO_URL + ITEM_NAMESPACE + ITEM_ID_PREFIX + "xy"
 
   span(:firstHeading, :xpath => "//h1[@id='firstHeading']/span")
   link(:specialLogLink, :css => "div#mw-content-text > div > p > span > a:nth-child(1)")
