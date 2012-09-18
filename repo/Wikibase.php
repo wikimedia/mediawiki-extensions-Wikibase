bc0de4e6abceb9af404a04ee0e12526f94049300
Daniel Werner <daniel.werner@wikimedia.de>
2012-09-18 17:02:42 +0200

    (bug 40229) Introduce PropertyView (DO NOT MERGE)
    Change-Id: I22222ec2cf85d96cc81025c451478e52681ce6a4

--- repo/Wikibase.php
+++ repo/Wikibase.php
@@ -90,6 +90,7 @@ $wgAutoloadClasses['Wikibase\ItemContentDiffView'] 		= $dir . 'includes/ItemCont
 $wgAutoloadClasses['Wikibase\ItemDisambiguation'] 		= $dir . 'includes/ItemDisambiguation.php';
 $wgAutoloadClasses['Wikibase\EntityView']				= $dir . 'includes/EntityView.php';
 $wgAutoloadClasses['Wikibase\ItemView'] 				= $dir . 'includes/ItemView.php';
+$wgAutoloadClasses['Wikibase\PropertyView']				= $dir . 'includes/PropertyView.php';
 $wgAutoloadClasses['Wikibase\Autocomment'] 				= $dir . 'includes/Autocomment.php';
 $wgAutoloadClasses['Wikibase\EditEntity'] 				= $dir . 'includes/EditEntity.php';
 
