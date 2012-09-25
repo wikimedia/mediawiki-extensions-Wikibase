f559ce92315b507b0ccd5d9c54af8e7f6ebb3305
jeblad <john.blad@wikimedia.de>
2012-09-25 12:02:08 +0200

    (Bug 40230) Fix for permissions
    Small fix to generalize permissions to use EntityContent instead
    of ItemContent.
    
    Change-Id: Icebfaee5f3acab7f2b64a47af1898d0118d399df

--- repo/includes/api/Api.php
+++ repo/includes/api/Api.php
@@ -349,16 +349,16 @@ abstract class Api extends \ApiBase {
 	 * @return Status the check's result
 	 * @todo: use this also to check for read access in ApiGetItems, etc
 	 */
-	public function checkPermissions( ItemContent $itemContent, User $user, array $params ) {
+	public function checkPermissions( EntityContent $entityContent, User $user, array $params ) {
 		if ( Settings::get( 'apiInDebug' ) && !Settings::get( 'apiDebugWithRights', false ) ) {
 			return Status::newGood();
 		}
 
-		$permissions = $this->getRequiredPermissions( $itemContent->getItem(), $params );
+		$permissions = $this->getRequiredPermissions( $entityContent->getEntity(), $params );
 		$status = Status::newGood();
 
 		foreach ( $permissions as $perm ) {
-			$permStatus = $itemContent->checkPermission( $perm, $user, true );
+			$permStatus = $entityContent->checkPermission( $perm, $user, true );
 			$status->merge( $permStatus );
 		}
 
