4bbc003ed37d50e880e39b66ce5ec0ff530dc3c1
Tim Starling <tstarling@wikimedia.org>
2012-09-27 16:24:08 +1000

    Some code cleanup
    * Removed ASCII art
    * Removed commented-out code in onSchemaUpdate() and updated the
      callback appropriately.
    
    Conflicts:
    	lib/includes/Utils.php
    
    Change-Id: I21f62b375a0ede9b91f72856cc8f2cfc974f0324

--- lib/includes/Utils.php
+++ lib/includes/Utils.php
@@ -58,26 +58,13 @@ final class Utils {
 	 * Inserts some sites into the sites table.
 	 *
 	 * @since 0.1
-	 *
-	 * @param lame $databaseUpdaterIsFullOfFail
-	 * @param callable|null $outputFunction
 	 */
-	public static function insertDefaultSites( $databaseUpdaterIsFullOfFail, $outputFunction = null ) {
+	public static function insertDefaultSites() {
 		if ( \SitesTable::singleton()->count() > 0 ) {
 			return;
 		}
 
-		if ( is_null( $outputFunction ) ) {
-			$outputMessage = function( $message ) {};
-		}
-		else {
-			// Compatibility with PHP < 5.4.
-			$outputMessage = function( $message ) use ( $outputFunction ) {
-				call_user_func( $outputFunction, $message );
-			};
-		}
-
-		$outputMessage( "No sites present yet, fetching from meta.wikimedia.org to populate sites table\n" );
+		// No sites present yet, fetching from meta.wikimedia.org to populate sites table
 
 		$languages = \FormatJson::decode(
 			\Http::get( 'http://meta.wikimedia.org/w/api.php?action=sitematrix&format=json' ),
@@ -96,7 +83,7 @@ final class Utils {
 
 		wfGetDB( DB_MASTER )->begin();
 
-		$outputMessage( "Inserting obtained sites...\n" );
+		// Inserting obtained sites...
 
 		foreach ( $languages['sitematrix'] as $language ) {
 			if ( is_array( $language ) && array_key_exists( 'code', $language ) && array_key_exists( 'site', $language ) ) {
