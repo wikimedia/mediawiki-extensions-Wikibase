19f6dd7bdaec47d4254a847896aea52eb745c18e
jeblad <john.blad@wikimedia.de>
2012-10-09 13:47:08 +0200

    Fix timeout problems in tests for store
    Change-Id: I38b2c3e07514bacd7029929c78bdbd7719aa8b61

--- repo/tests/phpunit/includes/store/sql/SqlIdGeneratorTest.php
+++ repo/tests/phpunit/includes/store/sql/SqlIdGeneratorTest.php
@@ -31,6 +31,11 @@ use Wikibase\IdGenerator as IdGenerator;
  * @group WikibaseStore
  * @group Database
  *
+ * Some of the tests takes more time, and needs therefor longer time before they can be aborted
+ * as non-functional. The reason why tests are aborted is assumed to be set up of temporal databases
+ * that hold the first tests in a pending state awaiting access to the database.
+ * @group medium
+ *
  * @licence GNU GPL v2+
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
