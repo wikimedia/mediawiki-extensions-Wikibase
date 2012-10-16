8138fd73756a0f1cfd7bd1c98110c8479c28ba2c
jeblad <john.blad@wikimedia.de>
2012-10-16 16:11:51 +0200

    (Bug 40392) First commit of wbsearchentities [DO NOT MERGE]
    This commit is mostly just to get the parameters and result format
    right, a lot of the business code is missing and replaced by example
    data.
    
    Change-Id: I60d74f34ebf4e1bcdaaec982dc0a5597183e6026

---
+++ repo/includes/api/ApiSearchEntities.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace Wikibase;
+use ApiBase;
+
+/**
+ * API module to search for Wikibase entities.
+ *
+ * @since 0.2
+ *
+ * @file
+ * @ingroup Wikibase
+ * @ingroup API
+ *
+ * @licence GNU GPL v2+
+ * @author John Erling Blad < jeblad@gmail.com >
+ */
+class ApiSearchEntities extends ApiBase {
+
+	/**
+	 * @see ApiBase::execute()
+	 */
+	public function execute() {
+		$params = $this->extractRequestParams();
+
+		// TODO: implement access to search
+		// this should be something similar to
+		// Change I6ea1f848: New function getFromTerm that searches over both aliases and labels
+		// https://gerrit.wikimedia.org/r/#/c/25006/
+		$hits = array(
+			array(
+				1,
+				array( 'en' => array( 'language' => 'en', 'value' => 'Foo' ) ),
+				array( 'en' => array( 'language' => 'en', 'value' => 'Some description about Foo' ) ),
+				null,
+				0.9
+			),
+			array(
+				2,
+				array( 'en' => array( 'language' => 'en', 'value' => 'Bar' ) ),
+				array( 'en' => array( 'language' => 'en', 'value' => 'Some description about Bar' ) ),
+				null,
+				0.8
+			),
+		);
+
+		$this->getResult()->addValue(
+			null,
+			'searchinfo',
+			array(
+				'totalhits' => count( $hits )
+			)
+		);
+
+		$this->getResult()->addValue(
+			null,
+			'search',
+			array()
+		);
+
+		$entries = array();
+		foreach ( $hits as $hit ) {
+			$entry = array();
+
+			$entry['score'] = $hit[4];
+			$entry['id'] = $hit[0];
+
+			if ( isset( $hit[1] ) ) {
+				$entry['labels'] = $hit[1];
+			}
+
+			if ( isset( $hit[2] ) ) {
+				$entry['descriptions'] = $hit[2];
+			}
+
+			if ( isset( $hit[3] ) ) {
+				$entry['aliases'] = $hit[3];
+			}
+
+			$entries[] = $entry;
+		}
+
+		$this->getResult()->addValue(
+			null,
+			'search',
+			$entries
+		);
+
+		$this->getResult()->addValue(
+			null,
+			'success',
+			(int)true
+		);
+	}
+
+	/**
+	 * @see ApiBase::getAllowedParams
+	 */
+	public function getAllowedParams() {
+		return array(
+			'text' => array(
+				ApiBase::PARAM_TYPE => 'string',
+				ApiBase::PARAM_REQUIRED => true,
+			),
+			'language' => array(
+				ApiBase::PARAM_TYPE => Utils::getLanguageCodes(),
+				ApiBase::PARAM_REQUIRED => true,
+			),
+			'type' => array(
+				ApiBase::PARAM_TYPE => EntityFactory::singleton()->getEntityTypes(),
+				ApiBase::PARAM_DFLT => 'item',
+			),
+			'limit' => array(
+				ApiBase::PARAM_TYPE => 'integer',
+				ApiBase::PARAM_DFLT => 0,
+			),
+		);
+	}
+
+	/**
+	 * @see ApiBase::getParamDescription
+	 */
+	public function getParamDescription() {
+		return array(
+			'text' => 'Search for this initial text.',
+			'language' => 'Search within this language.',
+			'type' => 'Search for this type of entity.',
+			'limit' => array( 'Limit to this number of non-exact matches',
+				"The value '0' will return all found matches." ),
+		);
+	}
+
+	/**
+	 * @see ApiBase::getDescription
+	 */
+	public function getDescription() {
+		return array(
+			'API module to search for entities.'
+		);
+	}
+
+	/**
+	 * @see ApiBase::getPossibleErrors()
+	 */
+	public function getPossibleErrors() {
+		return array_merge( parent::getPossibleErrors(), array(
+		) );
+	}
+
+	/**
+	 * @see ApiBase::getExamples
+	 */
+	protected function getExamples() {
+		return array(
+			'api.php?action=wbsearchentities&text=abc&language=en'
+			=> 'Search for "abc" in English language, with defaults for type and limit.',
+		);
+	}
+
+	/**
+	 * @see ApiBase::getHelpUrls
+	 */
+	public function getHelpUrls() {
+		return 'https://www.mediawiki.org/wiki/Extension:Wikibase/API#wbsearchentity';
+	}
+
+	/**
+	 * @see ApiBase::getVersion
+	 */
+	public function getVersion() {
+		return __CLASS__ . '-' . WB_VERSION;
+	}
+
+}
