b2e486d195adb16a1c65077f5f77b836acfe6074
Jens Ohlig <jens.ohlig@wikimedia.de>
2012-10-16 16:17:28 +0200

    PHPUnit test for PropertyDiff -- now running under phpunit --strict
    Change-Id: I4d915776caff9b42013a51fbe7973d5547276ae5

--- lib/WikibaseLib.hooks.php
+++ lib/WikibaseLib.hooks.php
@@ -132,6 +132,8 @@ final class LibHooks {
 			'claim/ClaimList',
 			'claim/ClaimObject',
 
+
+			'entity/EntityDiff',
 			'entity/EntityFactory',
 
 			'item/ItemDiff',
@@ -140,6 +142,7 @@ final class LibHooks {
 			'item/ItemNewFromArray',
 			'item/ItemObject',
 
+			'property/PropertyDiff',
 			'property/PropertyObject',
 
 			'query/QueryObject',
---
+++ lib/tests/phpunit/property/PropertyDiffTest.php
@@ -0,0 +1,53 @@
+<?php
+namespace Wikibase\Test;
+use \Wikibase\Entity;
+/**
+ * Tests for the Wikibase\PropertyObject deriving classes.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @since 0.1
+ *
+ * @ingroup WikibaseLib
+ * @ingroup Test
+ *
+ * @group Wikibase
+ * @group WikibaseLib
+ *
+ * @licence GNU GPL v2+
+ * @author Jens Ohlig <jens.ohlig@wikimedia.de>
+ */
+
+class PropertyDiffTest extends EntityDiffTest {
+
+	public function provideApplyData() {
+		return $this::makeData( "Property" );
+	}
+
+	/**
+	 *
+	 * @dataProvider provideApplyData
+	 */
+	public function testApply( Entity $a, Entity $b ) {
+		$diff = $a->getDiff( $b );
+		$diff->apply( $a );
+
+		$this->assertArrayEquals( $a->getLabels(), $b->getLabels() );
+		$this->assertArrayEquals( $a->getDescriptions(), $b->getDescriptions() );
+		$this->assertArrayEquals( $a->getAllAliases(), $b->getAllAliases() );
+	}
+}
