b87b7ad687f80f3c8d8f322c1ee472a66d922b01
Henning Snater <henning.snater@wikimedia.de>
2012-10-19 16:43:40 +0200

    (bug 40887) HTML template provider
    First iteration of implementing a mechanism to provide HTML templates commonly used in server-side HTML generation and client-side JavaScript processing. For the moment, only site link and edit section templates are defined and used while generating HTML in PHP. Other templates as well as the JavaScript implementation will follow.
    
    - patch set 2: split tests into two separate files, prefixed template keys
    
    Change-Id: I51578dfb7d5ddadea181b5050103e6bc5802fa20

---
+++ lib/includes/HtmlTemplateStore.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Wikibase;
+
+/**
+ * Allows storing and accessing of HTML templates (e.g. snippets commonly used in server-side HTML
+ * generation and client-side JavaScript processing).
+ *
+ * @since 0.2
+ *
+ * @file
+ * @ingroup Wikibase
+ *
+ * @licence GNU GPL v2+
+ * @author H. Snater <mediawiki@snater.com>
+ */
+
+/**
+ * Stores plain HTML templates.
+ */
+class HtmlTemplateStore {
+	/**
+	 * @var array
+	 */
+	private $templates;
+
+	/**
+	 * Gets the array containing all templates.
+	 *
+	 * @return array
+	 */
+	public function getTemplates() {
+		return $this->templates;
+	}
+
+	/**
+	 * Gets a specific template.
+	 *
+	 * @param string $key
+	 * @return string
+	 */
+	public function getTemplate( $key ) {
+		return $this->templates[$key];
+	}
+
+	/**
+	 * Adds multiple templates to the store.
+	 *
+	 * @param array $templates
+	 */
+	public function addTemplates( $templates ) {
+		foreach ( $templates AS $key => $html ) {
+			$this->addTemplate( $key, $html );
+		}
+	}
+
+	/**
+	 * Adds a single template to the store.
+	 *
+	 * @param string $key
+	 * @param string $html
+	 */
+	public function addTemplate( $key, $html ) {
+		$this->templates[$key] = $html;
+	}
+
+	/**
+	 * Singleton pattern integration.
+	 *
+	 * @return HtmlTemplateStore
+	 */
+	public static function singleton() {
+		static $instance = false;
+
+		if ( $instance === false ) {
+			$instance = new static();
+		}
+
+		return $instance;
+	}
+}
+
+/**
+ * Represents a HTML template that can contain placeholders just like MediWiki messages.
+ */
+class HtmlTemplate extends \Message {
+
+	/**
+	 * Fetch a HTML template from the template store.
+	 * @see \Message.fetchMessage()
+	 *
+	 * @return string HTML template
+	 */
+	function fetchMessage() {
+		if ( !isset( $this->message ) ) {
+			$cache = HtmlTemplateStore::singleton();
+			$this->message = $cache->getTemplate( $this->key );
+		}
+		return $this->message;
+	}
+
+}
---
+++ lib/tests/phpunit/HtmlTemplateStoreTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Wikibase\Test;
+use Wikibase\HtmlTemplateStore as HtmlTemplateStore;
+
+/**
+ * Tests for the Wikibase\HtmlTemplateStore class.
+ *
+ * @file
+ * @since 0.1
+ *
+ * @ingroup Wikibase
+ * @ingroup Test
+ *
+ * @group Wikibase
+ * @group WikibaseLib
+ *
+ * @licence GNU GPL v2+
+ * @author H. Snater <mediawiki@snater.com>
+ */
+class HtmlTemplateStoreTest extends \MediaWikiTestCase {
+
+	/**
+	 * @group WikibaseLib
+	 * @dataProvider providerAddTemplate
+	 */
+	public function testAddTemplate( $html ) {
+		HtmlTemplateStore::singleton()->addTemplate( 'tmpl1', $html );
+		$this->assertEquals(
+			HtmlTemplateStore::singleton()->getTemplate( 'tmpl1' ),
+			$html
+		);
+	}
+
+	public function providerAddTemplate() {
+		return array(
+			array( '<div>$1</div>' )
+		);
+	}
+
+
+	/**
+	 * @group WikibaseLib
+	 * @dataProvider providerAddTemplates
+	 */
+	public function testAddTemplates( $data ) {
+		HtmlTemplateStore::singleton()->addTemplates( $data );
+
+		$templates = HtmlTemplateStore::singleton()->getTemplates();
+		foreach( $data AS $key => $html ) {
+			$this->assertEquals(
+				$templates[$key],
+				$html
+			);
+		}
+	}
+
+	public function providerAddTemplates() {
+		return array(
+			array(
+				array( 'tmpl2' => '<div>$1</div>' ),
+				array( 'tmpl3' => '<div><div>$1</div></div>' )
+			)
+		);
+	}
+
+}
