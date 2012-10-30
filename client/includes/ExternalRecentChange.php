fa85a5cdc1887061b0170e5566f48d5cdc0e68f0
aude <aude.wiki@gmail.com>
2012-10-30 18:19:46 +0000

    cleaning up recent changes code, ExternalRecentChange stuff to handle more stuff
    Change-Id: Ide8b6420861863ff977e5819993aa1b762866749

--- client/includes/ExternalRecentChange.php
+++ client/includes/ExternalRecentChange.php
@@ -1,34 +1,63 @@
 <?php
 
 namespace Wikibase;
-use RecentChange;
 
 class ExternalRecentChange {
 
+	public $mAttribs = array();
+
 	/**
-	 * @param $timestamp
-	 * @param $title
-	 * @param $user
-	 * @param $actionComment
-	 * @param $ip
-	 * @param $type
-	 * @param $action
-	 * @param $target
-	 * @param $logComment
-	 * @param $params
-	 * @param int $newId
-	 * @param string $actionCommentIRC
+	 * @param array $attribs
+	 * @param \Title $title
 	 *
-	 * TODO: simplify creating entries for external changes
-	 * @return \RecentChange
+	 * @return ExternalRecentChange
 	 */
-	public static function newExternalLogEntry( $timestamp, &$title, &$user, $actionComment, $ip,
-		$type, $action, $target, $logComment, $params, $newId=0, $actionCommentIRC='' ) {
-
-		$rc = RecentChange::newLogEntry( $timestamp, &$title, &$user, $actionComment, $ip,
-			$type, $action, $target, $logComment, $params, $newId=0, $actionCommentIRC='' );
-
+	public static function newFromAttribs( $attribs, $title ) {
+		$rc = new ExternalRecentChange;
+		$rc->buildAttributes( $attribs, $title );
 		return $rc;
+	}
+
+	/**
+	 * @param array @attribs
+	 * @param \Title $title
+	 */
+	protected function buildAttributes( $attribs, $title ) {
+		$changeInfo = $attribs['wikibase-repo-change'];
+
+		$bot = false;
+		if ( array_key_exists( 'rc_bot', $changeInfo ) ) {
+			$bot = $changeInfo['rc_bot'];
+		}
+
+        $this->mAttribs = array(
+			'rc_namespace'  => $title->getNamespace(),
+			'rc_title'      => $title->getDBkey(),
+			'rc_user' => \User::newFromId( 0 ),
+			'rc_user_text' => $changeInfo['rc_user_text'],
+			'rc_type' => RC_EXTERNAL,
+			'rc_minor' => true, // for now, always consider these minor
+			'rc_bot' => $bot,
+			'rc_old_len' => $title->getLength(),
+			'rc_new_len' => $title->getLength(),
+			'rc_this_oldid' => $title->getLatestRevID(),
+			'rc_last_oldid' => $title->getLatestRevID(),
+			'rc_params' => serialize( $attribs ),
+			'rc_cur_id' => $changeInfo['rc_curid'],
+			'rc_comment' => '',
+			'rc_timestamp' => $changeInfo['time'],
+			'rc_cur_time' => $changeInfo['time'],
+			'rc_log_action' => ''
+		);
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function save() {
+		$dbw = wfGetDB( DB_MASTER );
+		$res = $dbw->insert( 'recentchanges', $this->mAttribs, __METHOD__ );
+		return $res;
 	}
 
 }
