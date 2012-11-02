6d09e57e99df21333bdc7740a6f2d42158909f28
Tpt <thomaspt@hotmail.fr>
2012-11-02 09:01:46 +0100

    (bug 40509) Make Special:CreateItem handle site and page params.
    Change-Id: I5a886b0b83f5af70f72125e79d23b024fc1b3c93

--- repo/includes/specials/SpecialCreateItem.php
+++ repo/includes/specials/SpecialCreateItem.php
@@ -13,6 +13,17 @@
  */
 class SpecialCreateItem extends SpecialCreateEntity {
 
+
+	/**
+	 * @var string|null
+	 */
+	protected $site = null;
+
+	/**
+	 * @var string|null
+	 */
+	protected $page = null;
+
 	/**
 	 * Constructor.
 	 *
@@ -23,10 +34,101 @@ class SpecialCreateItem extends SpecialCreateEntity {
 	}
 
 	/**
+	 * @see SpecialCreateEntity::prepareArguments()
+	 */
+	protected function prepareArguments() {
+		parent::prepareArguments();
+		$this->site = $this->getRequest()->getVal( 'site', null );
+		$this->page = $this->getRequest()->getVal( 'page', null );
+		return true;
+	}
+
+	/**
 	 * @see SpecialCreateEntity::createEntity()
 	 */
 	protected function createEntity() {
 		return \Wikibase\ItemContent::newEmpty();
+	}
+
+	/**
+	 * @see SpecialCreateEntity::modifyEntity()
+	 */
+	protected function modifyEntity( \Wikibase\EntityContent &$itemContent ) {
+		$status = parent::modifyEntity( $itemContent );
+
+		if ( $this->site !== null && $this->page !== null ) {
+			$site = \Sites::singleton()->getSite( $this->site );
+			if ( $site === false ) {
+				$status->error( 'wikibase-createitem-not-recognized-siteid' );
+				return $status;
+			}
+
+			$page = $site->normalizePageName( $this->page );
+			if ( $page === false ) {
+				$status->error( 'wikibase-createitem-no-external-page' );
+				return $status;
+			}
+
+			$link = new \Wikibase\SiteLink( $site, $page );
+			$ret = $itemContent->getItem()->addSiteLink( $link, 'add' );
+			if ( $ret === false ) {
+				$status->error( 'wikibase-createitem-add-sitelink-failed' );
+				return $status;
+			}
+		}
+
+		return $status;
+	}
+
+	/**
+	 * @see SpecialCreateEntity::additionalFormElements()
+	 */
+	protected function additionalFormElements() {
+		if ( $this->site === null || $this->page === null ) {
+			return parent::additionalFormElements();
+		}
+
+		return parent::additionalFormElements()
+		. Html::element(
+			'site',
+			array(
+				'for' => 'wb-createitem-site',
+				'class' => 'wb-label'
+			),
+			$this->msg( 'wikibase-createitem-site' )->text()
+		)
+		. Html::input(
+			'site',
+			$this->site,
+			'text',
+			array(
+				'id' => 'wb-createitem-site',
+				'size' => 12,
+				'class' => 'wb-input',
+				'readonly' => 'readonly'
+			)
+		)
+		. Html::element( 'br' )
+		. Html::element(
+			'page',
+			array(
+				'for' => 'wb-createitem-page',
+				'class' => 'wb-label'
+			),
+			$this->msg( 'wikibase-createitem-page' )->text()
+		)
+		. Html::input(
+			'page',
+			$this->page,
+			'text',
+			array(
+				'id' => 'wb-createitem-page',
+				'size' => 12,
+				'class' => 'wb-input',
+				'readonly' => 'readonly'
+			)
+		)
+		. Html::element( 'br' );
 	}
 
 	/**
