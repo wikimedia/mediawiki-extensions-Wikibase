e70c5ada7180e6f55796765d601e24b7a0d90a6e
Jens Ohlig <jens.ohlig@wikimedia.de>
2012-11-06 11:01:55 +0100

    Added TermMatchScoreCalculator
    Patchset 2: Some doc and a slight speedup
    
    Change-Id: I337cb4ef0d469e849f9da817facd2f79e1c81cfe

---
+++ repo/includes/store/TermMatchScoreCalculator.php
@@ -0,0 +1,64 @@
+<?php
+namespace Wikibase;
+
+/**
+ * Calculates and stores score for term search
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @since 0.3
+ *
+ * @file
+ * @ingroup Wikibase
+ *
+ * @licence GNU GPL v2+
+ * @author Jens Ohlig < jens.ohlig@wikimedia.de >
+ * @author John Erling Blad < jeblad@gmail.com >
+ */
+
+class TermMatchScoreCalculator {
+	protected $entry;
+	protected $searchLength;
+
+	/**
+	 * Constructor
+	 *
+	 * @param array entry
+	 * @param string $search
+	 * @since 0.3
+	 */
+	public function __construct( array $entry, $search ) {
+		$this->entry = $entry;
+		$this->searchLength = strlen( $search );
+	}
+
+	/**
+	 * Calculate score
+	 *
+	 * @since 0.3
+	 * @returns integer $score
+	 */
+	public function calculateScore() {
+		$score = $this->searchLength / strlen( $this->entry['label'] );
+		foreach ( $this->entry['aliases'] as $alias ) {
+			$aliasscore = $this->searchLength / strlen( $alias );
+			if ( $aliasscore > $score ) {
+				$score = $aliasscore;
+			}
+		}
+		return $score;
+	}
+}
