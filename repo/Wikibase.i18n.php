bc60c2e8dc0ec9b10241abc5f125b52d65587dda
Tpt <thomaspt@hotmail.fr>
2012-11-06 22:15:58 +0100

    Merge 942d5544a3ecf297d2ffd77fa273d668dd0e616c from master
    Change-Id: I818301cf743a8dc58f8bdc445dcf0686691d9a09

--- repo/Wikibase.i18n.php
+++ repo/Wikibase.i18n.php
@@ -137,7 +137,13 @@ $messages['en'] = array(
 	'wikibase-setlabel-invalid-id' => 'The ID "$1" is unknown to the system. Please use a valid entity ID.',
 	'special-listdatatypes' => 'List of all datatypes available',
 	'wikibase-listdatatypes-intro' => 'This is a list of all datatypes currently in use on this installation:',
-
+	'wikibase-history-title-with-label' => 'Revision history of "$2" ($1)',
+	'wikibase-history-title-without-label' => 'Revision history of ($1)',
+	'special-entitieswithoutlabel' => 'List of the entities without label',
+	'wikibase-entitieswithoutlabel-legend' => 'Get list of the entities without label',
+	'wikibase-entitieswithoutlabel-label-language' => 'Language:',
+	'wikibase-entitieswithoutlabel-submit' => 'Find',
+	'wikibase-entitieswithoutlabel-invalid-language' => '"$1" is not a valid language code.',
 
 	// API errors - these messages should not be translated
 	'wikibase-api-aliases-invalid-list' => 'Use either one of "set", "add" and "remove" parameters.', # Do not translate
@@ -392,6 +398,17 @@ Parameters:
 	'wikibase-setlabel-invalid-id' => 'Response informing that the selected entity ID is not valid. $1 is the invalid ID.',
 	'special-listdatatypes' => 'This special page returns a list of available datatypes (such as commonsMedia)',
 	'wikibase-listdatatypes-intro' => 'Intro text for the ListDatatypes special page.',
+	'wikibase-history-title-with-label' => 'The title of the history page with label and prefixed id
+* $1 is the prefixed id, the id is usually in parenthesis
+* $2 is the localized label, it is usually in quotes',
+	'wikibase-history-title-without-label' => 'The title of the history page with only a prefixed id
+* $1 is the prefixed id, the id is usually in parenthesis',
+	'special-entitieswithoutlabel' => 'This special page returns a list of entities without label for a given language',
+	'wikibase-entitieswithoutlabel-legend' => 'Legend of the form that allow to change the language.',
+	'wikibase-entitieswithoutlabel-label-language' => 'Label for the input field to change the language.',
+	'wikibase-entitieswithoutlabel-submit' => 'Label for the button that activate the action.',
+	'wikibase-entitieswithoutlabel-invalid-language' => 'Error message shown when the language code passed in parameter is invalid. $1 is invalid language code.',
+
 	'wikibase-api-aliases-invalid-list' => '!!DO NOT TRANSLATE!! This is an error message for a situation where the arguments to the API is inconsistent. Usually this should never be shown to the user, unless there are some exceptional error conditions.
 
 {{doc-important|Do not translate the names of the arguments: "set", "add" and "remove"}}',
