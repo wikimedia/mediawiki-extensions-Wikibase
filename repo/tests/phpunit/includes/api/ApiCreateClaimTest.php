4c901bcb46ba976cd3971a1e0fd19475c0a43f4c
Jeroen De Dauw <jeroendedauw@gmail.com>
2012-11-21 20:24:22 +0100

    Added initial code for testing failures in the create claim API
    Change-Id: Id9cdc4d6ff22131734f570bdb7844556e9a1c5fa

--- repo/tests/phpunit/includes/api/ApiCreateClaimTest.php
+++ repo/tests/phpunit/includes/api/ApiCreateClaimTest.php
@@ -75,4 +75,108 @@ class ApiCreateClaimTest extends \ApiTestCase {
 		$this->assertEquals( 'somevalue', $claim['mainsnak']['snaktype'] );
 	}
 
+	public function invalidRequestProvider() {
+		$entity = \Wikibase\ItemObject::newEmpty();
+		$content = new \Wikibase\ItemContent( $entity );
+		$content->save( '', null, EDIT_NEW );
+		$entity = $content->getEntity();
+
+		$dataTypes = \Wikibase\Settings::get( 'dataTypes' );
+		$property = \Wikibase\PropertyObject::newFromType( reset( $dataTypes ) );
+		$content = new \Wikibase\PropertyContent( $property );
+		$content->save( '', null, EDIT_NEW );
+		$property = $content->getEntity();
+
+		$argLists = array();
+
+		$params = array(
+			'action' => 'wbcreateclaim',
+			'entity' => 'q0',
+			'snaktype' => 'value',
+			'property' => $property->getPrefixedId(),
+			'value' => 'foo',
+		);
+
+		$argLists[] = array( 'cant-load-entity-content', $params );
+
+		$params = array(
+			'action' => 'wbcreateclaim',
+			'entity' => $entity->getPrefixedId(),
+			'snaktype' => 'value',
+			'property' => 'q0',
+			'value' => 'foo',
+		);
+
+		$argLists[] = array( 'unknownerror', $params );
+
+		$params = array(
+			'action' => 'wbcreateclaim',
+			'entity' => $entity->getPrefixedId(),
+			'snaktype' => 'value',
+			'property' => 'q0',
+			'value' => 'foo',
+		);
+
+		$argLists[] = array( 'unknownerror', $params );
+
+		$params = array(
+			'action' => 'wbcreateclaim',
+			'entity' => $entity->getPrefixedId(),
+			'snaktype' => 'hax',
+			'property' => $property->getPrefixedId(),
+			'value' => 'foo',
+		);
+
+		$argLists[] = array( 'unknown_snaktype', $params );
+
+		foreach ( array( 'entity', 'snaktype', 'property' ) as $requiredParam ) {
+			$params = array(
+				'action' => 'wbcreateclaim',
+				'entity' => $entity->getPrefixedId(),
+				'snaktype' => 'value',
+				'property' => $property->getPrefixedId(),
+				'value' => 'foo',
+			);
+
+			unset( $params[$requiredParam] );
+
+			$argLists[] = array( 'no' . $requiredParam, $params );
+		}
+
+		$params = array(
+			'action' => 'wbcreateclaim',
+			'entity' => $entity->getPrefixedId(),
+			'snaktype' => 'hax',
+			'property' => $property->getPrefixedId(),
+		);
+
+		$argLists[] = array( 'claim-value-missing', $params );
+
+		return array();
+		// TODO: during PHPUnit run the tests are thrown as usage exceptions
+		// which makes the test think there is an error and fail.
+		
+		//return $argLists;
+	}
+
+	/**
+	 * @dataProvider invalidRequestProvider
+	 *
+	 * @param string $errorCode
+	 * @param array $params
+	 */
+	public function testInvalidRequest( $errorCode, array $params ) {
+		list( $resultArray, ) = $this->doApiRequest( $params );
+
+		$this->assertInternalType( 'array', $resultArray, 'top level element is an array' );
+		$this->assertArrayHasKey( 'claim', $resultArray, 'top level element has an error key' );
+
+		$error = $resultArray['error'];
+
+		$this->assertArrayHasKey( 'code', $error, 'error element has a code key' );
+		$this->assertArrayHasKey( 'info', $error, 'error element has an info key' );
+
+		$this->assertEquals( $errorCode, $error['code'] );
+	}
+
 }
