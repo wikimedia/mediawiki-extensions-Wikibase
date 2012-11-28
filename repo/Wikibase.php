wikimedia/mediawiki-extensions-Wikibase

commit 05d6180c2655cdd842f443441ad070940160c697

Adding setup files for experimental/testing.
Adding setup files for enabling experimental features and testing options.
These can be used in LocalSettings.php instead of the plain Wikibase.php
resp. WikibaseClient.php.

Incomplete features should be configured or enabled only in the
Wikibase*.experimental.php files.

Change-Id: I391c379435437a328aad1bdeb156d3b2bf1ff18b

repo/Wikibase.php
@@ -32,7 +32,7 @@ if ( version_compare( $wgVersion, '1.20c', '<' ) ) { // Needs to be 1.20c becaus
 
 // Include the WikibaseLib extension if that hasn't been done yet, since it's required for Wikibase to work.
 if ( !defined( 'WBL_VERSION' ) ) {
-	@include_once( __DIR__ . '/../lib/WikibaseLib.php' );
+	require_once( __DIR__ . '/../lib/WikibaseLib.php' );
 }
 
 if ( !defined( 'WBL_VERSION' ) ) { // No version constant to check against :/

client/WikibaseClient.experimental.php
@@ -1,0 +1,20 @@
+<?php
+
+/**
+ * Initialization file for EXPERIMENTAL features of the Wikibase Client extension.
+ *
+ * This file can be included in LocalSettings.php instead of including WikibaseClient.php,
+ * in case all experimental features should be enabled.
+ */
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die( 'Not an entry point.' );
+}
+
+// include the regular wikibase stuff
+require_once( __DIR__ . '/WikibaseClient.php' );
+
+// include the experimental wikibase lib stuff
+require_once( __DIR__ . '/../lib/WikibaseLib.experimental.php' );
+
+// enable, register and/or configure experimental features here!

client/WikibaseClient.testing.php
@@ -1,0 +1,21 @@
+<?php
+
+/**
+ * Initialization file for TESTING settings for the Wikibase Client extension.
+ *
+ * This file can be included in LocalSettings.php instead of including WikibaseClient.php,
+ * to set up WikibaseClient for testing.
+ */
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die( 'Not an entry point.' );
+}
+
+// include the experimental wikibase stuff
+require_once( __DIR__ . '/WikibaseClient.experimental.php' );
+
+// include the testing wikibase lib stuff
+require_once( __DIR__ . '/../lib/WikibaseLib.testing.php' );
+
+
+// put any configuration for testing here!

lib/WikibaseLib.experimental.php
@@ -1,0 +1,17 @@
+<?php
+
+/**
+ * Initialization file for EXPERIMENTAL features of the Wikibase Client extension.
+ *
+ * This file can be included in LocalSettings.php instead of including WikibaseClient.php,
+ * in case all experimental features should be enabled.
+ */
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die( 'Not an entry point.' );
+}
+
+// include the regular wikibase stuff
+require_once( __DIR__ . '/WikibaseLib.php' );
+
+// enable, register and/or configure experimental features here!

lib/WikibaseLib.testing.php
@@ -1,0 +1,32 @@
+<?php
+
+/**
+ * Initialization file for TESTING settings for the Wikibase Lib.
+ *
+ * This file can be included in LocalSettings.php instead of including WikibaseLib.php,
+ * to set up WikibaseLib for testing.
+ */
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die( 'Not an entry point.' );
+}
+
+// include the experimental wikibase stuff
+require_once( __DIR__ . '/WikibaseLib.experimental.php' );
+
+// common settings for testing / debugging
+error_reporting( E_ALL | E_STRICT );
+ini_set( 'display_errors', 1 );
+
+$wgMessageCacheType = CACHE_NONE;
+
+$wgShowExceptionDetails = true;
+$wgShowSQLErrors        = true;
+$wgDebugDumpSql         = true;
+$wgShowDBErrorBacktrace = true;
+$wgDevelopmentWarnings  = true;
+$wgDebugTimestamps      = true;
+$wgDebugToolbar = true;
+
+
+// put any additional configuration for testing here!

repo/Wikibase.experimental.php
@@ -1,0 +1,20 @@
+<?php
+
+/**
+ * Initialization file for EXPERIMENTAL features of the Wikibase extension.
+ *
+ * This file can be included in LocalSettings.php instead of including Wikibase.php,
+ * in case all experimental features should be enabled.
+ */
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die( 'Not an entry point.' );
+}
+
+// include the regular wikibase stuff
+require_once( __DIR__ . '/Wikibase.php' );
+
+// include the experimental wikibase lib stuff
+require_once( __DIR__ . '/../lib/WikibaseLib.experimental.php' );
+
+// enable, register and/or configure experimental features here!

repo/Wikibase.testing.php
@@ -1,0 +1,21 @@
+<?php
+
+/**
+ * Initialization file for TESTING settings for the Wikibase extension.
+ *
+ * This file can be included in LocalSettings.php instead of including Wikibase.php,
+ * to set up Wikibase for testing.
+ */
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die( 'Not an entry point.' );
+}
+
+// include the experimental wikibase stuff
+require_once( __DIR__ . '/Wikibase.experimental.php' );
+
+// include the testing wikibase lib stuff
+require_once( __DIR__ . '/../lib/WikibaseLib.testing.php' );
+
+
+// put any configuration for testing here!

commit a5c33e972ace3ec7566e61dddee38636c80f544d

Restore magic loading of lib when present, broken in 05d6180c2655cdd842f443441ad070940160c697
Change-Id: I5384fe29148ad54fc506cce5ef831c05b7499529

repo/Wikibase.php
@@ -33,7 +33,7 @@ if ( version_compare( $wgVersion, '1.20c', '<' ) ) { // Needs to be 1.20c becaus
 
 // Include the WikibaseLib extension if that hasn't been done yet, since it's required for Wikibase to work.
 if ( !defined( 'WBL_VERSION' ) ) {
-	require_once( __DIR__ . '/../lib/WikibaseLib.php' );
+	@include_once( __DIR__ . '/../lib/WikibaseLib.php' );
 }
 
 if ( !defined( 'WBL_VERSION' ) ) { // No version constant to check against :/