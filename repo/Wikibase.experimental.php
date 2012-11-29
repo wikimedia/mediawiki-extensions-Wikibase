281c11bf088f69046eaf46586fe465fae7992a4c
Jens Ohlig <jens.ohlig@wikimedia.de>
2012-11-29 13:53:41 +0100

    Moved API methods with claims to experimental.
    Change-Id: I0568616763bf178500384a66e7f417f2e372b0fd

--- repo/Wikibase.experimental.php
+++ repo/Wikibase.experimental.php
@@ -18,3 +18,13 @@ require_once( __DIR__ . '/Wikibase.php' );
 require_once( __DIR__ . '/../lib/WikibaseLib.experimental.php' );
 
 // enable, register and/or configure experimental features here!
+$wgAutoloadClasses['Wikibase\ApiCreateClaim'] 			= $dir . 'includes/api/ApiCreateClaim.php';
+$wgAutoloadClasses['Wikibase\ApiGetClaims'] 			= $dir . 'includes/api/ApiGetClaims.php';
+$wgAutoloadClasses['Wikibase\ApiRemoveClaims'] 			= $dir . 'includes/api/ApiRemoveClaims.php';
+$wgAutoloadClasses['Wikibase\ApiSetClaimValue'] 		= $dir . 'includes/api/ApiSetClaimValue.php';
+$wgAutoloadClasses['Wikibase\ApiSetReference'] 			= $dir . 'includes/api/ApiSetReference.php';
+$wgAPIModules['wbcreateclaim'] 						= 'Wikibase\ApiCreateClaim';
+$wgAPIModules['wbgetclaims'] 						= 'Wikibase\ApiGetClaims';
+$wgAPIModules['wbremoveclaims'] 					= 'Wikibase\ApiRemoveClaims';
+$wgAPIModules['wbsetclaimvalue'] 					= 'Wikibase\ApiSetClaimValue';
+$wgAPIModules['wbsetreference'] 					= 'Wikibase\ApiSetReference';
