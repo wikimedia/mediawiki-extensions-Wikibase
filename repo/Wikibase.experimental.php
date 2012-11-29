b41e5852296afdca49a136615d5df81a76ccd99d
daniel <daniel.kinzler@wikimedia.de>
2012-11-29 15:16:19 +0100

    (bug 42539) Move more stuff to experimental setup.
    Move remaining phase 2 API modules and some special pages to
    the experimental setup, so they are not available in production.
    
    Change-Id: I650ac890f222aad430bb0d4da34dc5c5da1b8b8f

--- repo/Wikibase.experimental.php
+++ repo/Wikibase.experimental.php
@@ -25,12 +25,20 @@ $wgAutoloadClasses['Wikibase\ApiGetClaims'] 			= $dir . 'includes/api/ApiGetClai
 $wgAutoloadClasses['Wikibase\ApiRemoveClaims'] 			= $dir . 'includes/api/ApiRemoveClaims.php';
 $wgAutoloadClasses['Wikibase\ApiSetClaimValue'] 		= $dir . 'includes/api/ApiSetClaimValue.php';
 $wgAutoloadClasses['Wikibase\ApiSetReference'] 			= $dir . 'includes/api/ApiSetReference.php';
+$wgAutoloadClasses['Wikibase\Api\RemoveReferences'] 	= $dir . 'includes/api/RemoveReferences.php';
+$wgAutoloadClasses['Wikibase\Api\SetStatementRank'] 	= $dir . 'includes/api/SetStatementRank.php';
+
+$wgAutoloadClasses['SpecialListDatatypes'] 				= $dir . 'includes/specials/SpecialListDatatypes.php';
+$wgAutoloadClasses['SpecialNewProperty'] 				= $dir . 'includes/specials/SpecialNewProperty.php';
+$wgAutoloadClasses['SpecialEntityData'] 				= $dir . 'includes/specials/SpecialEntityData.php';
 
 $wgAPIModules['wbcreateclaim'] 						= 'Wikibase\ApiCreateClaim';
 $wgAPIModules['wbgetclaims'] 						= 'Wikibase\ApiGetClaims';
 $wgAPIModules['wbremoveclaims'] 					= 'Wikibase\ApiRemoveClaims';
 $wgAPIModules['wbsetclaimvalue'] 					= 'Wikibase\ApiSetClaimValue';
 $wgAPIModules['wbsetreference'] 					= 'Wikibase\ApiSetReference';
+$wgAPIModules['wbremovereferences'] 				= 'Wikibase\Api\RemoveReferences';
+$wgAPIModules['wbsetstatementrank'] 				= 'Wikibase\Api\SetStatementRank';
 
 $wgSpecialPages['EntityData'] 						= 'SpecialEntityData';
 $wgSpecialPages['NewProperty'] 						= 'SpecialNewProperty';
