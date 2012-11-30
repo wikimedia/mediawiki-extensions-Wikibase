0e1b6ca6e71f741fe92035cee025012f835122aa
Henning Snater <henning.snater@wikimedia.de>
2012-11-30 14:34:08 +0100

    JavaScript template engine function returning jQuery object
    Instead of a string, mw.templates() returns a jQuery object and checks if the constructed HTML would actually be valid. Comes along with some QUnit tests.
    
    Change-Id: I72e2e22e1627bab98acf0562d2399bd74b109085

--- repo/resources/wikibase.ui.entityViewInit.js
+++ repo/resources/wikibase.ui.entityViewInit.js
@@ -98,7 +98,7 @@
 			// removing site links heading to rebuild it with value counter
 			$( 'table.wb-sitelinks' ).each( function() {
 				$( this ).before(
-					$( mw.template( 'wb-section-heading', mw.msg( 'wikibase-sitelinks' ) ) )
+					mw.template( 'wb-section-heading', mw.msg( 'wikibase-sitelinks' ) )
 					.append(
 						$( '<span/>' )
 						.attr( 'id', 'wb-item-' + mw.config.get('wbEntityId') + '-sitelinks-counter' )
