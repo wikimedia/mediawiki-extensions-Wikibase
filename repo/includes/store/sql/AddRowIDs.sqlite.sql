0db71b0348df69d2f393b20fdde4e30b9c006f40
Reedy <reedy@wikimedia.org>
2012-12-07 19:49:47 +0000

    Followup to 36780, add new columns FIRST
    Change-Id: Ibcc2c36d75ac4d302a5fa308d1685dc2af054022

--- repo/includes/store/sql/AddRowIDs.sqlite.sql
+++ repo/includes/store/sql/AddRowIDs.sqlite.sql
@@ -1,37 +1,24 @@
--- MySQL version of the database schema for the Wikibase extension.
--- Licence: GNU GPL v2+
+-- Create unique row IDs, so we can efficiently update large tables later.
 
-
--- TODO: figure out which lenghts to use for some of the varchar fields.
-
-
--- Unique ID generator.
-CREATE TABLE IF NOT EXISTS /*_*/wb_id_counters (
-  id_value                   INT unsigned        NOT NULL,
-  id_type                    VARBINARY(32)       NOT NULL
-) /*$wgDBTableOptions*/;
-
-CREATE UNIQUE INDEX /*i*/wb_id_counters_type ON /*_*/wb_id_counters (id_type);
-
-
-
--- Derived storage.
--- Links site+title pairs to item ids.
-CREATE TABLE IF NOT EXISTS /*_*/wb_items_per_site (
+CREATE TABLE /*_*/wb_items_per_site_tmp (
   ips_row_id                 INT unsigned        NOT NULL PRIMARY KEY AUTO_INCREMENT, -- row ID
   ips_item_id                INT unsigned        NOT NULL, -- Id of the item
   ips_site_id                VARBINARY(32)       NOT NULL, -- Site identifier (global)
   ips_site_page              VARCHAR(255)        NOT NULL -- Title of the page
 ) /*$wgDBTableOptions*/;
 
+INSERT INTO /*_*/wb_items_per_site_tmp( ips_item_id, ips_site_id, ips_site_page )
+  SELECT ips_item_id, ips_site_id, ips_site_page FROM wb_items_per_site;
+
+DROP TABLE /*_*/wb_items_per_site;
+
+ALTER TABLE /*_*/wb_items_per_site_tmp RENAME TO /*_*/wb_items_per_site;
+
 CREATE UNIQUE INDEX /*i*/wb_ips_item_site_page ON /*_*/wb_items_per_site (ips_site_id, ips_site_page);
 CREATE INDEX /*i*/wb_ips_site_page ON /*_*/wb_items_per_site (ips_site_page);
 CREATE INDEX /*i*/wb_ips_item_id ON /*_*/wb_items_per_site (ips_item_id);
 
-
-
--- Lookup table for entity terms (ie labels, aliases, descriptions).
-CREATE TABLE IF NOT EXISTS /*_*/wb_terms (
+CREATE TABLE /*_*/wb_terms_tmp (
   term_row_id                INT unsigned        NOT NULL PRIMARY KEY AUTO_INCREMENT, -- row ID
   term_entity_id             INT unsigned        NOT NULL, -- Id of the entity
   term_entity_type           VARBINARY(32)       NOT NULL, -- Type of the entity
@@ -41,19 +28,16 @@ CREATE TABLE IF NOT EXISTS /*_*/wb_terms (
   term_search_key            VARCHAR(255) binary NOT NULL -- The term text, lowercase for case-insensitive lookups
 ) /*$wgDBTableOptions*/;
 
+INSERT INTO /*_*/wb_terms_tmp( term_entity_id, term_entity_type, term_language, term_type, term_text, term_search_key )
+  SELECT term_entity_id, term_entity_type, term_language, term_type, term_text, term_search_key FROM wb_terms;
+
+DROP TABLE /*_*/wb_terms;
+
+ALTER TABLE /*_*/wb_terms_tmp RENAME TO /*_*/wb_terms;
+
 CREATE INDEX /*i*/wb_terms_entity_id ON /*_*/wb_terms (term_entity_id);
 CREATE INDEX /*i*/wb_terms_entity_type ON /*_*/wb_terms (term_entity_type);
 CREATE INDEX /*i*/wb_terms_language ON /*_*/wb_terms (term_language);
 CREATE INDEX /*i*/wb_terms_type ON /*_*/wb_terms (term_type);
 CREATE INDEX /*i*/wb_terms_text ON /*_*/wb_terms (term_text);
 CREATE INDEX /*i*/wb_terms_search_key ON /*_*/wb_terms (term_search_key);
-
--- Links id+type to page ids.
-CREATE TABLE IF NOT EXISTS /*_*/wb_entity_per_page (
-  epp_entity_id                  INT unsigned        NOT NULL, -- Id of the entity
-  epp_entity_type                VARBINARY(32)       NOT NULL, -- Type of the entity
-  epp_page_id                    INT unsigned        NOT NULL -- Id of the page that store the entity
-) /*$wgDBTableOptions*/;
-
-CREATE UNIQUE INDEX /*i*/wb_epp_entity ON /*_*/wb_entity_per_page (epp_entity_id, epp_entity_type);
-CREATE UNIQUE INDEX /*i*/wb_epp_page ON /*_*/wb_entity_per_page (epp_page_id);
