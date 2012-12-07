c9de14aa2d373c8ebf3b8d2d01b27cc7abcfef89
Reedy <reedy@wikimedia.org>
2012-12-07 19:49:47 +0000

    Followup to 36780, add new columns FIRST
    Change-Id: Ibcc2c36d75ac4d302a5fa308d1685dc2af054022

--- repo/includes/store/sql/AddRowIDs.sql
+++ repo/includes/store/sql/AddRowIDs.sql
@@ -2,8 +2,8 @@
 
 ALTER TABLE /*_*/wb_items_per_site
 ADD ips_row_id
-INTEGER PRIMARY KEY auto_increment;
+INTEGER PRIMARY KEY auto_increment FIRST;
 
 ALTER TABLE /*_*/wb_terms
 ADD COLUMN term_row_id
-INTEGER PRIMARY KEY auto_increment;
+INTEGER PRIMARY KEY auto_increment FIRST;
