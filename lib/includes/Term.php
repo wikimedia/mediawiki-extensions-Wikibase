13231768d31cb59adf32ee0d88caeb9d3c00f7a1
daniel <daniel.kinzler@wikimedia.de>
2012-12-11 17:28:50 +0100

    (bug 42547) don't re-save terms if nothing changed.
    Change-Id: I442ee8dfb6eca531b3ebe213d25427d38731d158

--- lib/includes/Term.php
+++ lib/includes/Term.php
@@ -46,7 +46,15 @@ class Term {
 	 *
 	 * @var array
 	 */
-	protected $fields;
+	protected $fields = array();
+
+	protected static $fieldNames = array(
+		'entityType',
+		'entityId',
+		'termType',
+		'termLanguage',
+		'termText',
+	);
 
 	/**
 	 * @since 0.2
@@ -218,7 +226,65 @@ class Term {
 	 * @return string
 	 */
 	public static function normalizeText( $text ) {
-		return strtolower( Utils::squashToNFC( $text ) );
+		return mb_strtolower( Utils::squashToNFC( $text ) );
 	}
 
+	/**
+	 * Returns true if this Term object is equals to $that. This Term object is considered
+	 * equal to $that if $that is also an instance of Term, and $that->fields contains the
+	 * same values for the same fields as $this->fields.
+	 *
+	 * @param mixed $that The object to check for equality.
+	 *
+	 * @return bool If $that is equal to this Term object.
+	 */
+	public function equals( $that ) {
+		if ( $this === $that ) {
+			return true;
+		} else if ( !( $that instanceof Term ) ) {
+			return false;
+		} else {
+			if ( count( $this->fields ) != count( $that->fields ) ) {
+				return false;
+			}
+
+			/* @var Term $that */
+			foreach ( $this->fields as $k => $v ) {
+				if ( !isset( $that->fields[$k] ) || $that->fields[$k] !== $v ) {
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * Imposes an canonical but arbitrary order on Term objects.
+	 * Useful for sorting lists of terms for comparison.
+	 *
+	 * @param Term $a
+	 * @param Term $b
+	 *
+	 * @return int Returns 1 if $a is greater than $b, -1 if $b is greater than $a, and 0 otherwise.
+	 */
+	public static function compare( Term $a, Term $b ) {
+		foreach ( self::$fieldNames as $n ) {
+			if ( !isset( $a->fields[$n] ) ) {
+				if ( isset( $b->fields[$n] ) ) {
+					return -1;
+				}
+			} elseif ( !isset( $b->fields[$n] ) ) {
+				if ( isset( $a->fields[$n] ) ) {
+					return 1;
+				}
+			} elseif ( $a->fields[$n] > $b->fields[$n] ) {
+				return 1;
+			} elseif ( $a->fields[$n] < $b->fields[$n] ) {
+				return -1;
+			}
+		}
+
+		return 0;
+	}
 }
