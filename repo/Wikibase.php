2faf27608c1b5d4f2db85c033ffbfacbdab3cec6
jeroendedauw <jeroendedauw@gmail.com>
2012-12-17 02:13:08 +0100

    Moved data type selector ui code into own class to facilitate testing and reuse and make special newproperty simpler
    Change-Id: I3cfd438b34f2a7a2bcfa22a25b7f45c63485bd75

--- repo/Wikibase.php
+++ repo/Wikibase.php
@@ -94,6 +94,7 @@ $wgAutoloadClasses['Wikibase\RepoHooks'] 				= $dir . 'Wikibase.hooks.php';
 
 // includes
 $wgAutoloadClasses['Wikibase\Autocomment']				= $dir . 'includes/Autocomment.php';
+$wgAutoloadClasses['Wikibase\DataTypeSelector']			= $dir . 'includes/DataTypeSelector.php';
 $wgAutoloadClasses['Wikibase\EditEntity'] 				= $dir . 'includes/EditEntity.php';
 $wgAutoloadClasses['Wikibase\EntityContentDiffView'] 	= $dir . 'includes/EntityContentDiffView.php';
 $wgAutoloadClasses['Wikibase\ItemContentDiffView'] 		= $dir . 'includes/ItemContentDiffView.php';
