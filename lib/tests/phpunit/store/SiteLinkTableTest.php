344d9186d97a5d84addafe15a061612026bd2cff
aude <aude.wiki@gmail.com>
2012-12-19 01:31:22 +0100

    (minor) add some tests for SiteLinkTable
    Change-Id: Ic1f5913aca99a634d5228d4d51adaf40fb4fcecf

--- lib/tests/phpunit/store/SiteLinkTableTest.php
+++ lib/tests/phpunit/store/SiteLinkTableTest.php
@@ -2,6 +2,8 @@
 
 namespace Wikibase\Test;
 use \Wikibase\SiteLinkTable;
+use \Wikibase\SiteLink;
+use \Wikibase\Item;
 
 /**
  * Tests for the Wikibase\SiteLinkTable class.
@@ -35,9 +37,92 @@ class SiteLinkTableTest extends \MediaWikiTestCase {
 	public function testConstructor( $tableName ) {
 		$instance = new SiteLinkTable( $tableName, false );
 
+		// @todo: what kind of test is this?
 		$this->assertTrue( true );
 
 		// TODO: migrate tests from ItemDeletionUpdate and ItemStructuredSave
 	}
 
+	public function itemProvider() {
+		$items = array();
+
+		$item = Item::newEmpty();
+		$item->setId( new \Wikibase\EntityId( Item::ENTITY_TYPE, 1 ) );
+		$item->setLabel( 'en', 'Beer' );
+
+		$sitelinks = array(
+			'cswiki' => 'Pivo',
+			'enwiki' => 'Beer',
+			'jawiki' => 'ビール'
+		);
+
+		foreach( $sitelinks as $site => $page ) {
+			$item->addSiteLink( SiteLink::newFromText( $site, $page ) );
+		}
+
+		$items[] = $item;
+
+		return array( $items );
+	}
+
+	/**
+	 * @dataProvider itemProvider
+	 */
+	public function testSaveLinksOfItem( $item ) {
+		if ( defined( 'WB_VERSION' ) ) {
+			$siteLinkTable = \Wikibase\StoreFactory::getStore( 'sqlstore' )->newSiteLinkCache();
+		}
+
+		if ( defined( 'WBC_VERSION' ) ) {
+			$this->markTestSkipped( "Skipping because you're running it on a WikibaseClient instance." );
+		}
+
+		$res = $siteLinkTable->saveLinksOfItem( $item );
+		$this->assertTrue( $res );
+	}
+
+	/**
+	 * @depends testSaveLinksOfItem
+	 * @dataProvider itemProvider
+	 */
+	 public function testGetSiteLinksOfItem( $item ) {
+	 	$siteLinkTable = \Wikibase\StoreFactory::getStore( 'sqlstore' )->newSiteLinkCache();
+
+		$siteLinks = $siteLinkTable->getSiteLinksForItem( $item->getId() );
+
+		$this->assertEquals(
+			$item->getSiteLinks(),
+			$siteLinks
+		);
+	}
+
+	/**
+	 * @depends testSaveLinksOfItem
+	 * @dataProvider itemProvider
+	 */
+	public function testCountLinks( $item ) {
+		$siteLinkTable = \Wikibase\StoreFactory::getStore( 'sqlstore' )->newSiteLinkCache();
+
+		$this->assertEquals(
+			count( $item->getSiteLinks() ),
+			$siteLinkTable->countLinks( array( $item->getId()->getNumericId() ) )
+		);
+	}
+
+	/**
+	 * @depends testCountLinks
+	 * @dataProvider itemProvider
+	 */
+	 public function testDeleteLinksOfItem( $item ) {
+	 	$siteLinkTable = \Wikibase\StoreFactory::getStore( 'sqlstore' )->newSiteLinkCache();
+
+		$this->assertTrue(
+			$siteLinkTable->deleteLinksOfItem( $item->getId() )
+		);
+
+		$this->assertEquals(
+			array(),
+			$siteLinkTable->getSiteLinksForItem( $item->getId() )
+		);
+	}
 }
