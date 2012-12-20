88c3833866e0dfaf3e3e8e33d313b208fc747bdd
aude <aude.wiki@gmail.com>
2012-12-20 17:55:23 +0000

    (bug 43303) Make recent changes respect Wikibase Client namespace setting
    Change-Id: I532fe7943e70d693fb5330c61c15808d727dca1b

--- client/WikibaseClient.hooks.php
+++ client/WikibaseClient.hooks.php
@@ -251,7 +251,10 @@ final class ClientHooks {
 			// sitelinks could in theory be handled without re-parsing the page, but
 			// would still need to purge the squid cache.
 			foreach ( array_unique( $pagesToUpdate ) as $page ) {
-				self::updatePage( $page, $change, false );
+				$title = \Title::newFromText( $page );
+				if ( in_array( $title->getNamespace(), Settings::get( 'namespaces' ) ) ) {
+					self::updatePage( $title, $change, false );
+				}
 			}
 		}
 
@@ -269,10 +272,8 @@ final class ClientHooks {
 	 *
 	 * @return bool
 	 */
-	protected static function updatePage( $titleText, Change $change ) {
+	protected static function updatePage( $title, Change $change ) {
 		wfProfileIn( __METHOD__ );
-
-		$title = \Title::newFromText( $titleText );
 
 		if ( !$title->exists() ) {
 			wfProfileOut( __METHOD__ );
