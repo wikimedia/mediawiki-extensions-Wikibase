77df7eecd51cd0e7ca5be1f68db8d079d5abe0bd
Bene <benestar.wikimedia@googlemail.com>
2013-01-11 22:06:11 +0100

    (bug 43870 - bug 43871 - bug 43872) New Special Pages to edit entities without javascript
    New baseclass "SpecialSetEntity" and subclasses "SpecialSetLabel",
    "SpecialSetDescription", "SpecialSetAliases" and "SpecialSetSiteLink"
    
    patchset 16: rebased & resolved conflicts
    patchset 17: adjusted selenium test for Special:SetLabel
    patchset 23: SpecialSetSiteLink now depends on siteid instead of language
    
    Change-Id: Ib3650cf8cba0eb4014f33b27bfd07fba1a3b7767

---
+++ repo/includes/specials/SpecialSetSiteLink.php
@@ -0,0 +1,181 @@
+<?php
+
+use Wikibase\SiteLink;
+
+/**
+ * Special page for setting the sitelink of a Wikibase entity.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @since 0.4
+ *
+ * @file
+ * @ingroup WikibaseRepo
+ *
+ * @licence GNU GPL v2+
+ * @author Bene* < benestar.wikimedia@googlemail.com >
+ */
+class SpecialSetSiteLink extends SpecialSetEntity {
+
+	/**
+	 * Constructor
+	 *
+	 * @since 0.4
+	 */
+	public function __construct() {
+		parent::__construct( 'SetSiteLink' );
+	}
+
+	/**
+	 * Returns the posted site id.
+	 *
+	 * @since 0.4
+	 *
+	 * @param array $parts the parts of the subpage
+	 * @return string
+	 */
+	function getPostedLanguage( $parts ) {
+		$language = $this->getRequest()->getVal( 'site', isset( $parts[1] ) ? $parts[1] : '' );
+		if( $language === '' ) {
+			$language = $this->getRequest()->getVal( 'language' ); // Fix for posted requests
+		}
+		return $language;
+	}
+
+	/**
+	 * @see SpecialSetEntity::getPostedValue()
+	 *
+	 * @since 0.4
+	 *
+	 * @return string
+	 */
+	protected function getPostedValue() {
+		return $this->getRequest()->getVal( 'sitelink' );
+	}
+
+	/**
+	 * Checks if the site id is ok.
+	 *
+	 * @since 0.4
+	 */
+	protected function checkLanguage( $siteid ) {
+		if( \Sites::singleton()->getSite( $siteid ) === false ) {
+			$this->showError( $this->msg( 'wikibase-setsitelink-invalid-site', $siteid )->text() );
+		}
+	}
+
+	/**
+	 * Returns the full intro when both id and site are set.
+	 *
+	 * @since 0.4
+	 *
+	 * @param \Wikibase\EntityContent $entityContent the entity to have the value set
+	 * @param string $site
+	 */
+	protected function getIntrofull( $entityContent, $site ) {
+		return $this->msg(
+			'wikibase-' . strtolower( $this->getName() ) . '-introfull',
+			$entityContent->getTitle()->getPrefixedText(),
+			'[' . \Sites::singleton()->getSite( $site )->getPageUrl( '' ) . ' ' . $site . ']'
+		)->parse();
+	}
+
+	/**
+	 * @see SpecialSetEntity::getLanguageForm()
+	 *
+	 * @since 0.4
+	 *
+	 * @return string
+	 */
+	protected function getLanguageForm( $siteid ) {
+		return Html::element(
+			'label',
+			array(
+				'for' => 'wb-setentity-site',
+				'class' => 'wb-label'
+			),
+			$this->msg( 'wikibase-setsitelink-site' )->text()
+		)
+		. Html::input(
+			'site',
+			$siteid == $this->getLanguage()->getCode() ? '' : $siteid,
+			'text',
+			array(
+				'class' => 'wb-input',
+				'id' => 'wb-setentity-site'
+			)
+		);
+	}
+
+	/**
+	 * @see SpecialSetEntity::getValue()
+	 *
+	 * @since 0.4
+	 *
+	 * @param \Wikibase\EntityContent $entityContent
+	 * @param string $language
+	 *
+	 * @return string
+	 */
+	protected function getValue( $entityContent, $siteid ) {
+		if( $entityContent === null ) {
+			return '';
+		}
+		$sitelink = $entityContent->getEntity()->getSitelink( $siteid );
+		if( $sitelink === null ) {
+			return '';
+		}
+		return $sitelink->getPage();
+	}
+
+	/**
+	 * @see SpecialSetEntity::setValue()
+	 *
+	 * @since 0.4
+	 *
+	 * @param \Wikibase\EntityContent $entityContent
+	 * @param string $language
+	 * @param string $value
+	 * @param string &$summary The summary for this edit will be saved here.
+	 *
+	 * @return Status
+	 */
+	protected function setValue( $entityContent, $siteid, $value, &$summary ) {
+		$site = \Sites::singleton()->getSite( $siteid );
+		$status = \Status::newGood();
+
+		if( $site === false ) {
+			$status->error( 'wikibase-setsitelink-invalid-site', $siteid );
+			return $status;
+		}
+		if ( $site->normalizePageName( $value ) === false && $value !== '' ) {
+			$status->error( 'wikibase-error-ui-no-external-page' );
+			return $status;
+		}
+
+		if( $value === '' ) {
+			$entityContent->getEntity()->removeSitelink( $siteid );
+			$i18n = 'wbsetsitelink-remove';
+		}
+		else {
+			$siteLink = new SiteLink( $site, $value );
+			$entityContent->getEntity()->addSiteLink( $siteLink, 'set' );
+			$i18n = 'wbsetsitelink-set';
+		}
+		$summary = $this->getSummary( $siteid, $value, $i18n );
+		return $status;
+	}
+}
