cf1b5f1448e18e4561bb45ffb45776ca12608824
aude <aude.wiki@gmail.com>
2013-01-11 07:45:40 +0000

    add toggle for wikibase edits in watchlist [DO NOT MERGE!]
    - not quite done yet
    
    Change-Id: I62d961bcae8a3b601350551827868591217b02d2

--- client/resources/Resources.php
+++ client/resources/Resources.php
@@ -14,6 +14,20 @@ return call_user_func( function() {
 		'wikibase.client.page-move' => $moduleTemplate + array(
 			'styles' => 'wikibase.client.page-move.css'
 		),
+		'wikibase-client.watchlist.css' => $moduleTemplate + array(
+			'styles' => 'wikibase-client.watchlist.css',
+			'position' => 'top',
+		),
+		'wikibase-client.watchlist' => $moduleTemplate + array(
+			'scripts' => array(
+				'wikibase-client.watchlist.js'
+			),
+			'messages' => array(
+				'hide',
+				'show',
+				'wbc-rc-hide-wikidata',
+			),
+		),
 	);
 
 } );
