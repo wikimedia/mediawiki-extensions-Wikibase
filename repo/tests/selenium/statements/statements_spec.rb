13b4e1701208336dace24e5c50946f7b10be26c2
Tobias Gritschacher <tobias.gritschacher@wikimedia.de>
2013-01-18 12:47:45 +0100

    (testing) removing workarounds for bug 43200 in selenium tests
    - bug 43200 is fixed, so we can actually test adding multiple
    statements & claims
    
    Change-Id: I8fb2776e8e9e3a3a6668b67d8a5aad1460f2ad69

--- repo/tests/selenium/statements/statements_spec.rb
+++ repo/tests/selenium/statements/statements_spec.rb
@@ -57,7 +57,7 @@ describe "Check statements UI", :experimental => true do
   end
 
   context "Check statements UI" do
-    it "should check statement buttons behaviour" do
+    xit "should check statement buttons behaviour" do
       on_page(ItemPage) do |page|
         page.navigate_to items[0]["url"]
         page.wait_for_entity_to_load
@@ -77,7 +77,7 @@ describe "Check statements UI", :experimental => true do
         page.statementValueInput?.should be_false
       end
     end
-    it "should check entity suggestor behaviour" do
+    xit "should check entity suggestor behaviour" do
       on_page(ItemPage) do |page|
         page.navigate_to items[0]["url"]
         page.wait_for_entity_to_load
@@ -118,7 +118,7 @@ describe "Check statements UI", :experimental => true do
         page.statementValueInput?.should be_false
       end
     end
-    it "should check adding a statement" do
+    xit "should check adding a statement" do
       on_page(ItemPage) do |page|
         page.navigate_to items[0]["url"]
         page.wait_for_entity_to_load
@@ -146,7 +146,7 @@ describe "Check statements UI", :experimental => true do
         page.entityLabelSpan.should == properties_cm[0]["label"]
       end
     end
-    it "should check removing of claim/statement" do
+    xit "should check removing of claim/statement" do
       on_page(ItemPage) do |page|
         page.navigate_to items[0]["url"]
         page.wait_for_entity_to_load
@@ -166,7 +166,7 @@ describe "Check statements UI", :experimental => true do
         page.statement1ClaimValue1?.should be_false
       end
     end
-    it "should check ESC/RETURN button behaviour" do
+    xit "should check ESC/RETURN button behaviour" do
       on_page(ItemPage) do |page|
         page.navigate_to items[0]["url"]
         page.wait_for_entity_to_load
@@ -227,10 +227,9 @@ describe "Check statements UI", :experimental => true do
         page.statement1ClaimValue2.should == values[1]
         page.statement1ClaimValue3.should == values[2]
         @browser.refresh
-        # TODO: bug 43200 - wbcreateclaim overrides existing claim with new one when not reloading page before adding new claim
-        #page.statement1ClaimValue1.should == values[0]
-        #page.statement1ClaimValue2.should == values[1]
-        #page.statement1ClaimValue3.should == values[2]
+        page.statement1ClaimValue1.should == values[0]
+        page.statement1ClaimValue2.should == values[1]
+        page.statement1ClaimValue3.should == values[2]
         page.remove_all_claims
         page.statement1ClaimValue1?.should be_false
         page.statement1Name?.should be_false
@@ -253,10 +252,9 @@ describe "Check statements UI", :experimental => true do
         page.statement1ClaimValue2.should == values[1]
         page.statement1ClaimValue3.should == values[2]
         @browser.refresh
-        # TODO: bug 43200 - wbcreateclaim overrides existing claim with new one when not reloading page before adding new claim
-        #page.statement1ClaimValue1.should == values[0]
-        #page.statement1ClaimValue2.should == values[1]
-        #page.statement1ClaimValue3.should == values[2]
+        page.statement1ClaimValue1.should == values[0]
+        page.statement1ClaimValue2.should == values[1]
+        page.statement1ClaimValue3.should == values[2]
         page.remove_all_claims
       end
     end
@@ -278,9 +276,8 @@ describe "Check statements UI", :experimental => true do
         page.add_statement(properties_cm[1]["label"], statement2_values[1])
         page.statement2Name.should == properties_cm[1]["label"]
         page.statement2ClaimValue2.should == statement2_values[1]
-        # TODO: bug 43200 - removing claims fails, cause they are not existing because of bug 43200
-        #page.remove_all_claims
-        #page.statement1Name?.should be_false
+        page.remove_all_claims
+        page.statement1Name?.should be_false
       end
     end
     it "should check button behaviour when editing a statement" do
@@ -289,7 +286,6 @@ describe "Check statements UI", :experimental => true do
         page.wait_for_entity_to_load
         # make sure no claims displayed on page
         page.remove_all_claims
-        @browser.refresh # because of bug 43200
         page.add_statement(properties_cm[0]["label"], statement_value)
         page.editFirstStatement
         page.saveStatement?.should be_true
@@ -318,7 +314,7 @@ describe "Check statements UI", :experimental => true do
         page.statement1ClaimValue1.should == statement_value
       end
     end
-    it "should check editing a statement" do
+    xit "should check editing a statement" do
       on_page(ItemPage) do |page|
         page.navigate_to items[0]["url"]
         page.wait_for_entity_to_load
