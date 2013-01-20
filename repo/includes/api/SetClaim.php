00789d0b00b3a11ee2c28b9f641ca83e88769443
jeroendedauw <jeroendedauw@gmail.com>
2013-01-20 15:38:04 +0100

    New setclaim API module that allows creating or updating an entire claim given JSON serialization [DO NOT MERGE]
    Change-Id: I571e5b2e4ed6b492f2cfc0fb25c2af5777ae39af

---
+++ repo/includes/api/SetClaim.php
@@ -0,0 +1,254 @@
+<?php
+
+namespace Wikibase\Repo\Api;
+use MWException;
+
+use ApiBase;
+
+use Wikibase\Entity;
+use Wikibase\EntityContent;
+use Wikibase\Claim;
+use Wikibase\EditEntity;
+use Wikibase\EntityId;
+use Wikibase\EntityContentFactory;
+
+/**
+ * API module for creating or updating an entire Claim.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @since 0.4
+ *
+ * @ingroup WikibaseRepo
+ * @ingroup API
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class ApiSetClaim extends \Wikibase\Api {
+
+	// TODO: rights
+
+	/**
+	 * @see ApiBase::execute
+	 *
+	 * @since 0.4
+	 */
+	public function execute() {
+		$content = $this->getEntityContent();
+
+		$claim = $this->getClaimFromRequest();
+
+		$this->setClaim( $content->getEntity(), $claim );
+
+		$this->saveChanges( $content );
+
+		$this->outputClaim( $claim );
+	}
+
+	/**
+	 * @since 0.4
+	 *
+	 * @return Claim
+	 */
+	protected function getClaimFromRequest() {
+		$serializerFactory = new \Wikibase\Lib\Serializers\SerializerFactory();
+		$unserializer = $serializerFactory->newUnserializerForClass( 'Wikibase\Claim' );
+
+		$params = $this->extractRequestParams();
+		$claim = $unserializer->newFromSerialization( \FormatJson::decode( $params['claim'], true ) );
+
+		assert( $claim instanceof Claim );
+
+		return $claim;
+	}
+
+	/**
+	 * @since 0.4
+	 *
+	 * @param Entity $entity
+	 * @param Claim $claim
+	 */
+	protected function setClaim( Entity $entity, Claim $claim ) {
+		$claims = new \Wikibase\Claims( $entity->getClaims() );
+
+		if ( $claim->getGuid() === null || !$claims->hasClaimWithGuid( $claim ) ) {
+			$claim = $entity->newClaim( $claim );
+		}
+
+		$claims->addClaim( $claim );
+
+		$entity->setClaims( $claims );
+	}
+
+	/**
+	 * @since 0.4
+	 *
+	 * @return EntityContent
+	 */
+	protected function getEntityContent() {
+		$params = $this->extractRequestParams();
+
+		// FIXME: derp injection of parser
+		$entityId = EntityId::newFromPrefixedId( Entity::getIdFromClaimGuid( $params['claim'] ) );
+		$entityTitle = EntityContentFactory::singleton()->getTitleForId( $entityId );
+
+		if ( $entityTitle === null ) {
+			$this->dieUsage( 'No such entity', 'setclaim-entity-not-found' );
+		}
+
+		$baseRevisionId = isset( $params['baserevid'] ) ? intval( $params['baserevid'] ) : null;
+
+		return $this->loadEntityContent( $entityTitle, $baseRevisionId );
+	}
+
+	/**
+	 * @since 0.4
+	 *
+	 * @param EntityContent $content
+	 */
+	protected function saveChanges( EntityContent $content ) {
+		$params = $this->extractRequestParams();
+
+		$baseRevisionId = isset( $params['baserevid'] ) ? intval( $params['baserevid'] ) : null;
+		$baseRevisionId = $baseRevisionId > 0 ? $baseRevisionId : false;
+		$editEntity = new EditEntity( $content, $this->getUser(), $baseRevisionId, $this->getContext() );
+
+		$status = $editEntity->attemptSave(
+			'', // TODO: automcomment
+			EDIT_UPDATE,
+			isset( $params['token'] ) ? $params['token'] : ''
+		);
+
+		if ( !$status->isGood() ) {
+			$this->dieUsage( 'Failed to save the change', 'setclaim-save-failed' );
+		}
+
+		$statusValue = $status->getValue();
+
+		if ( isset( $statusValue['revision'] ) ) {
+			$this->getResult()->addValue(
+				'pageinfo',
+				'lastrevid',
+				(int)$statusValue['revision']->getId()
+			);
+		}
+	}
+
+	/**
+	 * @since 0.4
+	 *
+	 * @param Claim $claim
+	 */
+	protected function outputClaim( Claim $claim ) {
+		$serializerFactory = new \Wikibase\Lib\Serializers\SerializerFactory();
+		$serializer = $serializerFactory->newSerializerForObject( $claim );
+		$serializer->getOptions()->setIndexTags( $this->getResult()->getIsRawMode() );
+
+		$this->getResult()->addValue(
+			null,
+			'claim',
+			$serializer->getSerialized( $claim )
+		);
+	}
+
+	/**
+	 * @see ApiBase::getAllowedParams
+	 *
+	 * @since 0.4
+	 *
+	 * @return array
+	 */
+	public function getAllowedParams() {
+		return array(
+			'claim' => array(
+				ApiBase::PARAM_TYPE => 'string',
+				ApiBase::PARAM_REQUIRED => true,
+			),
+			'token' => null,
+			'baserevid' => array(
+				ApiBase::PARAM_TYPE => 'integer',
+			),
+		);
+	}
+
+	/**
+	 * @see ApiBase::getParamDescription
+	 *
+	 * @since 0.4
+	 *
+	 * @return array
+	 */
+	public function getParamDescription() {
+		return array(
+			'claim' => 'Claim serialization',
+			'token' => 'An "edittoken" token previously obtained through the token module (prop=info).',
+			'baserevid' => array( 'The numeric identifier for the revision to base the modification on.',
+				"This is used for detecting conflicts during save."
+			),
+		);
+	}
+
+	/**
+	 * @see ApiBase::getDescription
+	 *
+	 * @since 0.4
+	 *
+	 * @return string
+	 */
+	public function getDescription() {
+		return array(
+			'API module for creating or updating an entire Claim.'
+		);
+	}
+
+	/**
+	 * @see ApiBase::getExamples
+	 *
+	 * @since 0.4
+	 *
+	 * @return array
+	 */
+	protected function getExamples() {
+		return array(
+			'api.php?action=setclaim&claim={json-stuff}&baserevid=9042&token=foobar'
+			// 'ex' => 'desc'
+		);
+	}
+
+	/**
+	 * @see ApiBase::getHelpUrls
+	 *
+	 * @since 0.4
+	 *
+	 * @return string
+	 */
+	public function getHelpUrls() {
+		return 'https://www.mediawiki.org/wiki/Extension:Wikibase/API#wbsetclaimvalue';
+	}
+
+	/**
+	 * @see ApiBase::getVersion
+	 *
+	 * @since 0.4
+	 *
+	 * @return string
+	 */
+	public function getVersion() {
+		return __CLASS__ . '-' . WB_VERSION;
+	}
+
+}
