063e966223f83071147720cfe0383eed1776e9e4
Henning Snater <henning.snater@wikimedia.de>
2013-01-22 12:54:17 +0100

    New RepoApiError constructor handling wikibase API errors
    Change set integrates new RepoApiError within claimview/claimlistview widgets. Integration of
    EditableValue and its derivatives will be done in another change set.
    
    Change-Id: Iccfbd64e390d38595d8b9f7939d505e043350206

--- lib/resources/Resources.php
+++ lib/resources/Resources.php
@@ -89,10 +89,36 @@ return call_user_func( function() {
 				'wikibase.store/wikibase.RepoApi.js',
 			),
 			'dependencies' => array(
+				'jquery.json',
 				'user.tokens',
 				'wikibase.datamodel',
 				'wikibase.repoAccess',
-				'jquery.json'
+				'wikibase.RepoApiError',
+			)
+		),
+
+		'wikibase.RepoApiError' => $moduleTemplate + array(
+			'scripts' => array(
+				'wikibase.store/wikibase.RepoApiError.js',
+			),
+			'messages' => array(
+				'wikibase-error-unexpected',
+				'wikibase-error-save-generic',
+				'wikibase-error-remove-generic',
+				'wikibase-error-save-timeout',
+				'wikibase-error-remove-timeout',
+				'wikibase-error-ui-client-error',
+				'wikibase-error-ui-no-external-page',
+				'wikibase-error-ui-cant-edit',
+				'wikibase-error-ui-no-permissions',
+				'wikibase-error-ui-link-exists',
+				'wikibase-error-ui-session-failure',
+				'wikibase-error-ui-edit-conflict',
+				'wikibase-error-ui-edit-conflict',
+			),
+			'dependencies' => array(
+				'wikibase',
+				'wikibase.utilities',
 			)
 		),
 
