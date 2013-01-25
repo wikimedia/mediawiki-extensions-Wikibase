09619102194999ab1155276ad84f505458baeefe
Tobias Gritschacher <tobias.gritschacher@wikimedia.de>
2013-01-25 16:04:28 +0100

    (testing) added selenium test for setAliases special page
    Change-Id: I58c06d0f6f34999229faa57445050048ebbb93fd

--- repo/tests/selenium/special/set_entity_spec.rb
+++ repo/tests/selenium/special/set_entity_spec.rb
@@ -4,26 +4,32 @@
 # Author:: Tobias Gritschacher (tobias.gritschacher@wikimedia.de)
 # License:: GNU GPL v2+
 #
-# tests for special pages to set label, description, etc.
+# tests for special pages to set label, description, aliases.
 
 require 'spec_helper'
 
 label = generate_random_string(10)
 description = generate_random_string(20)
+aliases = [generate_random_string(8), generate_random_string(8)]
 label_de = generate_random_string(10)
 label_de_different = generate_random_string(10)
 description_de = generate_random_string(20)
 description_de_different = generate_random_string(20)
+alias_de = generate_random_string(8)
 language_code_de = "de"
 item_id = ""
 
-describe "Check special pages to set entity label, description, etc." do
+describe "Check special pages to set entity label, description, aliases." do
   before :all do
     # set up: create item
     visit_page(CreateItemPage) do |page|
       page.create_new_item(label, description)
     end
     on_page(ItemPage) do |page|
+      page.navigate_to_item
+      page.uls_switch_language("en", "English")
+      page.wait_for_entity_to_load
+      page.add_aliases(aliases)
       item_id = page.get_item_id
     end
   end
@@ -33,7 +39,7 @@ describe "Check special pages to set entity label, description, etc." do
       visit_page(SetLabelPage) do |page|
         page.idField = ITEM_ID_PREFIX + item_id
         page.languageField = language_code_de
-        page.labelField = label_de
+        page.valueField = label_de
         page.setLabelSubmit
         page.wait_for_entity_to_load
         page.navigate_to_item
@@ -46,7 +52,7 @@ describe "Check special pages to set entity label, description, etc." do
       visit_page(SetLabelPage) do |page|
         page.idField = ITEM_ID_PREFIX + item_id
         page.languageField = language_code_de
-        page.labelField = label_de_different
+        page.valueField = label_de_different
         page.setLabelSubmit
         page.wait_for_entity_to_load
         page.navigate_to_item
@@ -62,7 +68,7 @@ describe "Check special pages to set entity label, description, etc." do
       visit_page(SetDescriptionPage) do |page|
         page.idField = ITEM_ID_PREFIX + item_id
         page.languageField = language_code_de
-        page.labelField = description_de
+        page.valueField = description_de
         page.setDescriptionSubmit
         page.wait_for_entity_to_load
         page.navigate_to_item
@@ -75,13 +81,42 @@ describe "Check special pages to set entity label, description, etc." do
       visit_page(SetDescriptionPage) do |page|
         page.idField = ITEM_ID_PREFIX + item_id
         page.languageField = language_code_de
-        page.labelField = description_de_different
+        page.valueField = description_de_different
         page.setDescriptionSubmit
         page.wait_for_entity_to_load
         page.navigate_to_item
         page.uls_switch_language("de", "deutsch")
         page.wait_for_entity_to_load
         page.entityDescriptionSpan.should == description_de_different
+      end
+    end
+  end
+
+  context "SetAliases functionality test" do
+    it "should set label for " + language_code_de do
+      on_page(ItemPage) do |page|
+        page.navigate_to_item
+        page.uls_switch_language("en", "English")
+        page.wait_for_entity_to_load
+        page.count_existing_aliases.should == 2
+        page.uls_switch_language("de", "Deutsch")
+        page.wait_for_entity_to_load
+        page.count_existing_aliases.should == 0
+      end
+      visit_page(SetAliasesPage) do |page|
+        page.idField = ITEM_ID_PREFIX + item_id
+        page.languageField = language_code_de
+        page.valueField = alias_de
+        page.setAliasesSubmit
+        page.wait_for_entity_to_load
+        page.navigate_to_item
+        page.uls_switch_language("de", "deutsch")
+        page.wait_for_entity_to_load
+        page.count_existing_aliases.should == 1
+        page.get_nth_alias(1).text.should == alias_de
+        page.uls_switch_language("en", "English")
+        page.wait_for_entity_to_load
+        page.count_existing_aliases.should == 2
       end
     end
   end
