a40fb2cdedee922b590da57f2eaca4e830daea80
Tobias Gritschacher <tobias.gritschacher@wikimedia.de>
2013-01-25 14:55:59 +0100

    (hotfix) fixing SetLabel Selenium tests after refactoring
    - after the refactoring of setLabel special page, the selenium tests
    need to be adjusted
    
    Change-Id: I6cf2fcc03706a80080fd2f27af8684371b9fc1ce

---
+++ selenium/lib/pages/set_entity_page.rb
@@ -0,0 +1,16 @@
+# -*- encoding : utf-8 -*-
+# Wikidata UI tests
+#
+# Author:: Anja Jentzsch (anja.jentzsch@wikimedia.de)
+# License:: GNU GPL v2+
+#
+# page object for SetEntity base class
+
+class SetEntityPage < ItemPage
+  include PageObject
+
+  text_field(:idField, :id => "wb-setentity-id")
+  text_field(:languageField, :id => "wb-setentity-language")
+  text_field(:labelField, :id => "wb-setentity-value")
+
+end
