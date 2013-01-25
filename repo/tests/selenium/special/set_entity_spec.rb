a62fbc1120c69729a6fed48c2e9ddc3550080c21
Tobias Gritschacher <tobias.gritschacher@wikimedia.de>
2013-01-25 15:16:46 +0100

    (testing) added Selenium tests for setDescription special page
    - extended and refactored setLabel test to be able to test
    setDescription and others too
    
    Change-Id: Ie4f7067b2e3c9f743fe7e312e9712261ead9d8fb

--- repo/tests/selenium/special/set_entity_spec.rb
+++ repo/tests/selenium/special/set_entity_spec.rb
@@ -1,9 +1,10 @@
 # Wikidata UI tests
 #
 # Author:: Anja Jentzsch (anja.jentzsch@wikimedia.de)
+# Author:: Tobias Gritschacher (tobias.gritschacher@wikimedia.de)
 # License:: GNU GPL v2+
 #
-# tests for SetLabel special page
+# tests for special pages to set label, description, etc.
 
 require 'spec_helper'
 
@@ -11,10 +12,12 @@ label = generate_random_string(10)
 description = generate_random_string(20)
 label_de = generate_random_string(10)
 label_de_different = generate_random_string(10)
+description_de = generate_random_string(20)
+description_de_different = generate_random_string(20)
 language_code_de = "de"
 item_id = ""
 
-describe "Check SetLabel special page" do
+describe "Check special pages to set entity label, description, etc." do
   before :all do
     # set up: create item
     visit_page(CreateItemPage) do |page|
@@ -50,6 +53,35 @@ describe "Check SetLabel special page" do
         page.uls_switch_language("de", "deutsch")
         page.wait_for_entity_to_load
         page.entityLabelSpan.should == label_de_different
+      end
+    end
+  end
+
+  context "SetDescription functionality test" do
+    it "should set label for " + language_code_de do
+      visit_page(SetDescriptionPage) do |page|
+        page.idField = ITEM_ID_PREFIX + item_id
+        page.languageField = language_code_de
+        page.labelField = description_de
+        page.setDescriptionSubmit
+        page.wait_for_entity_to_load
+        page.navigate_to_item
+        page.uls_switch_language("de", "deutsch")
+        page.wait_for_entity_to_load
+        page.entityDescriptionSpan.should == description_de
+      end
+    end
+    it "should change description for " + language_code_de do
+      visit_page(SetDescriptionPage) do |page|
+        page.idField = ITEM_ID_PREFIX + item_id
+        page.languageField = language_code_de
+        page.labelField = description_de_different
+        page.setDescriptionSubmit
+        page.wait_for_entity_to_load
+        page.navigate_to_item
+        page.uls_switch_language("de", "deutsch")
+        page.wait_for_entity_to_load
+        page.entityDescriptionSpan.should == description_de_different
       end
     end
   end
