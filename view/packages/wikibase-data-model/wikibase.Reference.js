b27a27c61c0b74703c9528621d5f0cb6e506bc6d
Daniel Werner <daniel.werner@wikimedia.de>
2013-01-29 00:32:35 +0100

    (bug 43291) introduction of jQuery.wikibase.statementview
    inherits from claimview, displayed in claimlistview on item entity pages. For now same
    functionality as claims but different object. Introduced new template for this view.
    
    - patchset 2: adjustments to selneium test
    - patchset 5: adjusted JS wb.Statement.newFromJSON to new serialized statements structure.
    
    Change-Id: Id74cde0758eb9d1759244e6ab2a89c8d1bb90959

--- view/packages/wikibase-data-model/wikibase.Reference.js
+++ view/packages/wikibase-data-model/wikibase.Reference.js
@@ -14,17 +14,41 @@
 	 * @see https://meta.wikimedia.org/wiki/Wikidata/Data_model#ReferenceRecords
 	 *
 	 * @param {wb.Snak[]|wb.Snak|wb.SnakList} [snaks]
+	 * @param {string} [hash] The hash of the Reference, required when API is used to change or
+	 *        remove a certain Reference. In the PHP object model, the hash of a Reference changes
+	 *        always when the Reference changes. In the JavaScript Reference object the hash will
+	 *        not change but remain the same.
+	 *
+	 * TODO: get rid of 'hash' parameter and introduce a method to generate the hash, but make sure
+	 *       it will be the same as it would be for a Reference in PHP.
 	 */
-	wb.Reference = function( snaks ) {
+	wb.Reference = function( snaks, hash ) {
 		this.setSnaks( snaks );
+		this._hash = hash;
 	};
 
 	wb.Reference.prototype = {
 		/**
-		 * @type wb.Snak[]
+		 * @type string|null
+		 */
+		_hash: null,
+
+		/**
+		 * @type wb.SnakList
 		 * @todo think about implementing a SnakList rather than having an Array here
 		 */
 		_snaks: null,
+
+		/**
+		 * Will return the hash set for the reference initially in the constructor. This is required
+		 * when changing a reference via the API and differs from the PHP's Reference's getHash.
+		 * @since 0.4
+		 *
+		 * @return string|null
+		 */
+		getHash: function() {
+			return this._hash;
+		},
 
 		/**
 		 * Returns a list of the Snaks.
@@ -47,8 +71,9 @@
 
 	wb.Reference.newFromJSON = function( json ) {
 		return new wb.Reference(
-			wb.SnakList.newFromJSON( json )
+			wb.SnakList.newFromJSON( json.snaks ),
+			json.hash
 		);
-	}
+	};
 
 }( wikibase, jQuery ) );
