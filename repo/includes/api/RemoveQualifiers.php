ce52dd211ef604768f8a8b03c880745baa02192f
jeblad <john.blad@wikimedia.de>
2013-01-30 01:23:45 +0100

    Change API namespaces and prefixes
    Change-Id: Id5aca089151063022dd0468e37589b54f78c7e3e

--- repo/includes/api/RemoveQualifiers.php
+++ repo/includes/api/RemoveQualifiers.php
@@ -1,15 +1,14 @@
 <?php
 
-namespace Wikibase\Repo\Api;
+namespace Wikibase\Api;
 
-use ApiBase;
-use MWException;
+use ApiBase, MWException;
 
-use Wikibase\EntityContent;
+//use Wikibase\EditEntity;
 use Wikibase\EntityId;
 use Wikibase\Entity;
+use Wikibase\EntityContent;
 use Wikibase\EntityContentFactory;
-use Wikibase\EditEntity;
 use Wikibase\Claim;
 use Wikibase\Settings;
 
@@ -39,7 +38,7 @@ use Wikibase\Settings;
  * @licence GNU GPL v2+
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
-class RemoveQualifiers extends \Wikibase\Api {
+class RemoveQualifiers extends Api {
 
 	// TODO: automcomment
 	// TODO: example
@@ -48,7 +47,7 @@ class RemoveQualifiers extends \Wikibase\Api {
 	// TODO: claim uniqueness
 
 	/**
-	 * @see ApiBase::execute
+	 * @see \ApiBase::execute
 	 *
 	 * @since 0.3
 	 */
@@ -67,7 +66,7 @@ class RemoveQualifiers extends \Wikibase\Api {
 	/**
 	 * @since 0.3
 	 *
-	 * @return EntityContent
+	 * @return \Wikibase\EntityContent
 	 */
 	protected function getEntityContent() {
 		$params = $this->extractRequestParams();
@@ -87,7 +86,7 @@ class RemoveQualifiers extends \Wikibase\Api {
 	/**
 	 * @since 0.3
 	 *
-	 * @param Entity $entity
+	 * @param \Wikibase\Entity $entity
 	 */
 	protected function doRemoveQualifiers( Entity $entity ) {
 		$params = $this->extractRequestParams();
@@ -109,10 +108,10 @@ class RemoveQualifiers extends \Wikibase\Api {
 	/**
 	 * @since 0.3
 	 *
-	 * @param Entity $entity
+	 * @param \Wikibase\Entity $entity
 	 * @param string $claimGuid
 	 *
-	 * @return Claim
+	 * @return \Wikibase\Claim
 	 */
 	protected function getClaim( Entity $entity, $claimGuid ) {
 		if ( !$entity->getClaims()->hasClaimWithGuid( $claimGuid ) ) {
@@ -129,14 +128,14 @@ class RemoveQualifiers extends \Wikibase\Api {
 	/**
 	 * @since 0.3
 	 *
-	 * @param EntityContent $content
+	 * @param \Wikibase\EntityContent $content
 	 */
 	protected function saveChanges( EntityContent $content ) {
 		$params = $this->extractRequestParams();
 
 		$baseRevisionId = isset( $params['baserevid'] ) ? intval( $params['baserevid'] ) : null;
 		$baseRevisionId = $baseRevisionId > 0 ? $baseRevisionId : false;
-		$editEntity = new EditEntity( $content, $this->getUser(), $baseRevisionId, $this->getContext() );
+		$editEntity = new \Wikibase\EditEntity( $content, $this->getUser(), $baseRevisionId, $this->getContext() );
 
 		$status = $editEntity->attemptSave(
 			'', // TODO: automcomment
@@ -160,7 +159,7 @@ class RemoveQualifiers extends \Wikibase\Api {
 	}
 
 	/**
-	 * @see ApiBase::getAllowedParams
+	 * @see \ApiBase::getAllowedParams
 	 *
 	 * @since 0.3
 	 *
@@ -185,7 +184,7 @@ class RemoveQualifiers extends \Wikibase\Api {
 	}
 
 	/**
-	 * @see ApiBase::getParamDescription
+	 * @see \ApiBase::getParamDescription
 	 *
 	 * @since 0.3
 	 *
@@ -204,7 +203,7 @@ class RemoveQualifiers extends \Wikibase\Api {
 	}
 
 	/**
-	 * @see ApiBase::getDescription
+	 * @see \ApiBase::getDescription
 	 *
 	 * @since 0.3
 	 *
@@ -217,7 +216,7 @@ class RemoveQualifiers extends \Wikibase\Api {
 	}
 
 	/**
-	 * @see ApiBase::getExamples
+	 * @see \ApiBase::getExamples
 	 *
 	 * @since 0.3
 	 *
@@ -231,7 +230,7 @@ class RemoveQualifiers extends \Wikibase\Api {
 	}
 
 	/**
-	 * @see ApiBase::getHelpUrls
+	 * @see \ApiBase::getHelpUrls
 	 *
 	 * @since 0.3
 	 *
@@ -242,7 +241,7 @@ class RemoveQualifiers extends \Wikibase\Api {
 	}
 
 	/**
-	 * @see ApiBase::getVersion
+	 * @see \ApiBase::getVersion
 	 *
 	 * @since 0.3
 	 *
---
+++ repo/tests/phpunit/includes/api/CreateClaimTest.php
@@ -0,0 +1,269 @@
+<?php
+
+namespace Wikibase\Test;
+use Wikibase\Entity;
+
+/**
+ * Unit tests for the Wikibase\ApiCreateClaim class.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @since 0.3
+ *
+ * @ingroup WikibaseRepoTest
+ *
+ * @group API
+ * @group Database
+ * @group Wikibase
+ * @group WikibaseAPI
+ * @group WikibaseRepo
+ * @group CreateClaimTest
+ *
+ * @group medium
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class ApiCreateClaimTest extends \ApiTestCase {
+
+	protected static function getNewEntityAndProperty() {
+		$entity = \Wikibase\Item::newEmpty();
+		$content = new \Wikibase\ItemContent( $entity );
+		$content->save( '', null, EDIT_NEW );
+		$entity = $content->getEntity();
+
+		$property = \Wikibase\Property::newFromType( 'commonsMedia' );
+		$content = new \Wikibase\PropertyContent( $property );
+		$content->save( '', null, EDIT_NEW );
+		$property = $content->getEntity();
+
+		return array( $entity, $property );
+	}
+
+	protected function assertRequestValidity( $resultArray ) {
+		$this->assertInternalType( 'array', $resultArray, 'top level element is an array' );
+		$this->assertArrayHasKey( 'claim', $resultArray, 'top level element has a claim key' );
+		$this->assertArrayNotHasKey( 'lastrevid', $resultArray['claim'], 'claim has a lastrevid key' );
+
+		$this->assertArrayHasKey( 'pageinfo', $resultArray, 'top level element has a pageinfo key' );
+		$this->assertArrayHasKey( 'lastrevid', $resultArray['pageinfo'], 'pageinfo has a lastrevid key' );
+	}
+
+	public function testValidRequest() {
+		/**
+		 * @var Entity $entity
+		 * @var Entity $property
+		 */
+		list( $entity, $property ) = self::getNewEntityAndProperty();
+
+		$params = array(
+			'action' => 'wbcreateclaim',
+			'entity' => $entity->getPrefixedId(),
+			'snaktype' => 'value',
+			'property' => $property->getPrefixedId(),
+			'value' => '"foo"',
+			'token' => $GLOBALS['wgUser']->getEditToken()
+		);
+
+		list( $resultArray, ) = $this->doApiRequest( $params );
+
+		$this->assertRequestValidity( $resultArray );
+
+		$claim = $resultArray['claim'];
+
+		foreach ( array( 'id', 'mainsnak', 'type', 'rank' ) as $requiredKey ) {
+			$this->assertArrayHasKey( $requiredKey, $claim, 'claim has a "' . $requiredKey . '" key' );
+		}
+
+		$entityId = \Wikibase\Entity::getIdFromClaimGuid( $claim['id'] );
+
+		$this->assertEquals( $entity->getPrefixedId(), $entityId );
+
+		$this->assertEquals( 'value', $claim['mainsnak']['snaktype'] );
+
+		$entityContent = \Wikibase\EntityContentFactory::singleton()->getFromId( $entity->getId() );
+
+		$this->assertTrue( $entityContent->getEntity()->hasClaimWithGuid( $claim['id'] ) );
+	}
+
+	public function invalidRequestProvider() {
+		$argLists = array();
+
+		$params = array(
+			'action' => 'wbcreateclaim',
+			'entity' => 'q0',
+			'snaktype' => 'value',
+			'property' => '-',
+			'value' => '"foo"',
+		);
+
+		$argLists[] = array( 'cant-load-entity-content', $params );
+
+		$params = array(
+			'action' => 'wbcreateclaim',
+			'entity' => '-',
+			'snaktype' => 'value',
+			'property' => 'q0',
+			'value' => '"foo"',
+		);
+
+		$argLists[] = array( 'unknownerror', $params );
+
+		$params = array(
+			'action' => 'wbcreateclaim',
+			'entity' => '-',
+			'snaktype' => 'hax',
+			'property' => '-',
+			'value' => '"foo"',
+		);
+
+		$argLists[] = array( 'unknown_snaktype', $params );
+
+		foreach ( array( 'entity', 'snaktype' ) as $requiredParam ) {
+			$params = array(
+				'action' => 'wbcreateclaim',
+				'entity' => '-',
+				'snaktype' => 'value',
+				'property' => '-',
+				'value' => '"foo"',
+			);
+
+			unset( $params[$requiredParam] );
+
+			$argLists[] = array( 'no' . $requiredParam, $params );
+		}
+
+		$params = array(
+			'action' => 'wbcreateclaim',
+			'entity' => '-',
+			'snaktype' => 'value',
+			'value' => '"foo"',
+		);
+
+		$argLists[] = array( 'claim-property-id-missing', $params );
+
+		$params = array(
+			'action' => 'wbcreateclaim',
+			'entity' => '-',
+			'snaktype' => 'value',
+			'property' => '-',
+		);
+
+		$argLists[] = array( 'claim-value-missing', $params );
+
+		return $argLists;
+	}
+
+	public static function getEntityAndPropertyForInvalid() {
+		static $array = null;
+
+		if ( $array === null ) {
+			$array = self::getNewEntityAndProperty();
+		}
+
+		return $array;
+	}
+
+	/**
+	 * @dataProvider invalidRequestProvider
+	 *
+	 * @param string $errorCode
+	 * @param array $params
+	 */
+	public function testInvalidRequest( $errorCode, array $params ) {
+		/**
+		 * @var Entity $entity
+		 * @var Entity $property
+		 */
+		list( $entity, $property ) = self::getEntityAndPropertyForInvalid();
+
+		$params['token'] = $GLOBALS['wgUser']->getEditToken();
+
+		if ( array_key_exists( 'entity', $params ) && $params['entity'] === '-' ) {
+			$params['entity'] = $entity->getPrefixedId();
+		}
+
+		if ( array_key_exists( 'property', $params ) && $params['property'] === '-' ) {
+			$params['property'] = $property->getPrefixedId();
+		}
+
+		try {
+			$this->doApiRequest( $params );
+			$this->assertFalse( true, 'Invalid request should raise an exception' );
+		}
+		catch ( \UsageException $e ) {
+			$this->assertEquals( $errorCode, $e->getCodeString(), 'Invalid request raised correct error' );
+		}
+
+		$entityContent = \Wikibase\EntityContentFactory::singleton()->getFromId( $entity->getId() );
+
+		$this->assertFalse( $entityContent->getEntity()->hasClaims() );
+	}
+
+	public function testMultipleRequests() {
+		/**
+		 * @var Entity $entity
+		 * @var Entity $property
+		 */
+		list( $entity, $property ) = self::getNewEntityAndProperty();
+
+		$params = array(
+			'action' => 'wbcreateclaim',
+			'entity' => $entity->getPrefixedId(),
+			'snaktype' => 'value',
+			'property' => $property->getPrefixedId(),
+			'value' => '"foo"',
+			'token' => $GLOBALS['wgUser']->getEditToken()
+		);
+
+		list( $resultArray, ) = $this->doApiRequest( $params );
+
+		$this->assertRequestValidity( $resultArray );
+
+		$revId = $resultArray['pageinfo']['lastrevid'];
+
+		$firstGuid = $resultArray['claim']['id'];
+
+		$params = array(
+			'action' => 'wbcreateclaim',
+			'entity' => $entity->getPrefixedId(),
+			'snaktype' => 'value',
+			'property' => $property->getPrefixedId(),
+			'value' => '"bar"',
+			'token' => $GLOBALS['wgUser']->getEditToken(),
+			'baserevid' => $revId
+		);
+
+		list( $resultArray, ) = $this->doApiRequest( $params );
+
+		$this->assertRequestValidity( $resultArray );
+
+		$newRevId = $resultArray['pageinfo']['lastrevid'];
+
+		$secondGuid = $resultArray['claim']['id'];
+
+		$this->assertTrue( (int)$revId < (int)$newRevId );
+
+		$this->assertNotEquals( $firstGuid, $secondGuid );
+
+		$entityContent = \Wikibase\EntityContentFactory::singleton()->getFromId( $entity->getId() );
+
+		$this->assertTrue( $entityContent->getEntity()->hasClaimWithGuid( $firstGuid ) );
+		$this->assertTrue( $entityContent->getEntity()->hasClaimWithGuid( $secondGuid ) );
+	}
+
+}
