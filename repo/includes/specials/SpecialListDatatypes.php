26b536f3eaae683835ae569b5fce8394d99ff20e
jeblad <john.blad@wikimedia.de>
2013-02-07 00:51:30 +0100

    Add dt/dd msgs for of the datatypes in Special:ListDatatypes
    These messages are used at the "List all datatypes" -special page.
    In those cases some datatypes exists that can be added as configuration,
    the messages can be set upp manually in as local system messages.
    
    Change-Id: I66ed2cf6e82d50fc0904c89d37e09daa88a2a6ad

--- repo/includes/specials/SpecialListDatatypes.php
+++ repo/includes/specials/SpecialListDatatypes.php
@@ -39,13 +39,33 @@ class SpecialListDatatypes extends SpecialWikibasePage {
 	}
 	public function execute( $subPage ) {
 		parent::execute( $subPage );
+
 		$this->getOutput()->addHTML( $this->msg( 'wikibase-listdatatypes-intro' ) );
-		$this->getOutput()->addHTML( Html::openElement( 'ul' ));
+		$this->getOutput()->addHTML( Html::openElement( 'dl' ));
+
+		// some of the datatype descriptions
+		// 'wikibase-listdatatypes-wikibase-item-head'
+		// 'wikibase-listdatatypes-wikibase-item-body'
+		// 'wikibase-listdatatypes-commonsmedia-head'
+		// 'wikibase-listdatatypes-commonsmedia-body'
+		// 'wikibase-listdatatypes-geo-coordinate-head'
+		// 'wikibase-listdatatypes-geo-coordinate-body'
+		// 'wikibase-listdatatypes-quantity-head'
+		// 'wikibase-listdatatypes-quantity-body'
+		// 'wikibase-listdatatypes-monolingual-text-head'
+		// 'wikibase-listdatatypes-monolingual-text-body'
+		// 'wikibase-listdatatypes-multilingual-text-head'
+		// 'wikibase-listdatatypes-multilingual-text-body'
+		// 'wikibase-listdatatypes-time-head'
+		// 'wikibase-listdatatypes-text-body'
+
 		foreach (\Wikibase\Settings::get( 'dataTypes' ) as $dataTypeId ) {
-			$this->getOutput()->addHTML( Html::openElement( 'li' ));
-			$this->getOutput()->addHTML( htmlspecialchars( $dataTypeId ) );
-			$this->getOutput()->addHTML( Html::closeElement( 'li' ));
+			$baseKey = 'wikibase-listdatatypes-' . mb_strtolower( $dataTypeId );
+			$out = Html::rawElement( 'dt', array(), $this->msg( $baseKey . '-head' )->parse() )
+			. Html::rawElement( 'dd', array(), $this->msg( $baseKey . '-body' )->parse() );
+			$this->getOutput()->addHTML( $out );
 		}
-		$this->getOutput()->addHTML( Html::closeElement( 'ul' ));
+
+		$this->getOutput()->addHTML( Html::closeElement( 'dl' ));
 	}
 }
