d9ff7467e746f1476cc76b1b0530fb6c475b1a70
jeroendedauw <jeroendedauw@gmail.com>
2013-02-07 22:40:00 +0100

    Step towards splitting the settings of lib, repo and client into dedicated interfaces
    Change-Id: I8313756e94a4dc953ced1d3dacaa20aa4afbc96f

---
+++ lib/config/WikibaseLib.experimental.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * This file holds registration of experimental features part of the WikibaseLib extension.
+ *
+ * This file is NOT an entry point the WikibaseLib extension. Use WikibaseLib.php.
+ * It should furthermore not be included from outside the extension.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @since 0.4
+ *
+ * @file
+ * @ingroup WikibaseLib
+ *
+ * @licence GNU GPL v2+
+ */
+
+if ( !defined( 'WBL_VERSION' ) || !defined( 'WB_EXPERIMENTAL_FEATURES' ) ) {
+	die( 'Not an entry point.' );
+}
+
+global $wgWBSettings;
+
+$wgWBSettings['dataTypes'] = array_merge( $wgWBSettings['dataTypes'], array(
+	'geo-coordinate',
+	'quantity',
+	'monolingual-text',
+	'multilingual-text',
+	'time',
+) );
--- repo/config/Wikibase.experimental.php
+++ repo/config/Wikibase.experimental.php
@@ -1,12 +1,27 @@
 <?php
 
 /**
- * This file holds registration of experimental features part of the Wikibase extension.
+ * This file holds registration of experimental features part of the Wikibase Repo extension.
  *
  * This file is NOT an entry point the Wikibase extension. Use Wikibase.php.
  * It should furthermore not be included from outside the extension.
  *
- * @since 0.3
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @since 0.4
  *
  * @file
  * @ingroup WikibaseRepo
@@ -14,10 +29,11 @@
  * @licence GNU GPL v2+
  */
 
-if ( !defined( 'WB_VERSION' ) ) {
+if ( !defined( 'WB_VERSION' ) || !defined( 'WB_EXPERIMENTAL_FEATURES' ) ) {
 	die( 'Not an entry point.' );
 }
-$dir = __DIR__ . '/';
+
+$dir = __DIR__ . '/../';
 
 $wgAutoloadClasses['Wikibase\Repo\Api\RemoveQualifiers'] 	= $dir . 'includes/api/RemoveQualifiers.php';
 $wgAutoloadClasses['Wikibase\Repo\Api\SetQualifier'] 		= $dir . 'includes/api/SetQualifier.php';
@@ -29,6 +45,8 @@ $wgAPIModules['wbsetqualifier'] 					= 'Wikibase\Repo\Api\SetQualifier';
 $wgAPIModules['wbsetstatementrank'] 				= 'Wikibase\Api\SetStatementRank';
 
 $wgSpecialPages['EntityData'] 						= 'SpecialEntityData';
+
+unset( $dir );
 
 /**
  * Hook to add PHPUnit test cases.
@@ -50,7 +68,7 @@ $wgHooks['UnitTestsList'][] = function( array &$files ) {
 	);
 
 	foreach ( $testFiles as $file ) {
-		$files[] = __DIR__ . '/tests/phpunit/includes/' . $file . 'Test.php';
+		$files[] = __DIR__ . '/../tests/phpunit/includes/' . $file . 'Test.php';
 	}
 
 	return true;
