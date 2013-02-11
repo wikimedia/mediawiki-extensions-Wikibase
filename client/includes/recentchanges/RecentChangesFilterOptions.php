7af42ecf2be6740ccc595fb083ab7cd897ec8fa6
aude <aude.wiki@gmail.com>
2013-02-11 11:21:01 +0000

    Omit wikidata changes from enhanced changes
    - temporary until we have patch in for supporting enhanced changes properly
    
    Change-Id: I567a697fa14d76fa2cac5cb2abe9120955858372

--- client/includes/recentchanges/RecentChangesFilterOptions.php
+++ client/includes/recentchanges/RecentChangesFilterOptions.php
@@ -67,6 +67,22 @@ class RecentChangesFilterOptions {
 	}
 
 	/**
+	 * Is the enhanced changes format used?
+	 *
+	 * @note this is temporary and we will support enhanced changes in the near future
+ 	 *
+	 * @since 0.4
+	 *
+	 * @return bool
+	 */
+	protected function isEnhancedChanges() {
+		// @todo evil globals, though the recent changes and watchlist query hooks
+		// so nor provide access to context
+		global $wgRequest, $wgUser;
+		return $wgRequest->getBool( 'enhanced', $wgUser->getOption( 'usenewrc' ) );
+	}
+
+	/**
 	 * Do we show wikibase edits in recent changes?
 	 *
 	 * @since 0.4
@@ -74,7 +90,7 @@ class RecentChangesFilterOptions {
 	 * @return bool
 	 */
 	public function showWikibaseEdits() {
-		if ( $this->isDisabled() || $this->hideWikibase() ) {
+		if ( $this->isDisabled() || $this->hideWikibase() || $this->isEnhancedChanges() ) {
 			return false;
 		}
 		return true;
