ac890e40c0469a410abd01be249689a616102574
Tobias Gritschacher <tobias.gritschacher@wikimedia.de>
2013-02-11 14:50:25 +0100

    (bug 44095) enhanced claim diff visualizaion including refs
    - still todo: qualifiers, ranks.
    
    squashed:
    
    (bug 44095) visualize claim differences
    
    This commit is a follow up to 5fc812442e19d55a7d1c2072ba8a839ce7d1f067.
    It tackles some design issues and adds tests, while removing the
    preliminary visualization code, which needs to be re-added.
    
    Change-Id: I7f8645df860b1860b4016003f52e7959af685a25

---
+++ lib/includes/entity/EntityDiffVisualizer.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace Wikibase;
+
+use IContextSource;
+use Html;
+use MWException;
+use Diff\Diff;
+use Diff\DiffOp;
+use Diff\DiffOpChange;
+use Diff\DiffOpAdd;
+use Diff\DiffOpRemove;
+
+/**
+ * Class for generating views of EntityDiff objects.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @since 0.4
+ *
+ * @file
+ * @ingroup WikibaseLib
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ * @author Tobias Gritschacher < tobias.gritschacher@wikimedia.de >
+ */
+class EntityDiffVisualizer {
+
+	/**
+	 * @since 0.4
+	 *
+	 * @var IContextSource
+	 */
+	private $context;
+
+	/**
+	 * @since 0.4
+	 *
+	 * @var ClaimDiffer|null
+	 */
+	private $claimDiffer;
+
+	/**
+	 * @since 0.4
+	 *
+	 * @var ClaimDifferenceVisualizer|null
+	 */
+	private $claimDiffVisualizer;
+
+	/**
+	 * Constructor.
+	 *
+	 * @since 0.4
+	 *
+	 * @param IContextSource $contextSource
+	 * @param ClaimDiffer $claimDiffer
+	 * @param ClaimDifferenceVisualizer $claimDiffView
+	 */
+	public function __construct( IContextSource $contextSource, ClaimDiffer $claimDiffer, ClaimDifferenceVisualizer $claimDiffView ) {
+		$this->context = $contextSource;
+		$this->claimDiffer = $claimDiffer;
+		$this->claimDiffVisualizer = $claimDiffView;
+	}
+
+	/**
+	 * Generates and returns an HTML visualization of the provided EntityDiff.
+	 *
+	 * @since 0.4
+	 *
+	 * @param EntityDiff $diff
+	 *
+	 * @return string
+	 */
+	public function visualizeDiff( EntityDiff $diff ) {
+		$html = '';
+
+		$termDiffVisualizer = new DiffView(
+			array(),
+			new Diff( array(
+				'label' => $diff->getLabelsDiff(),
+				'aliases' => $diff->getAliasesDiff(),
+				'description' => $diff->getDescriptionsDiff(),
+			), true ),
+			$this->context
+		);
+
+		$html .= $termDiffVisualizer->getHtml();
+
+		foreach ( $diff->getClaimsDiff() as $claimDiffOp ) {
+			$html .= $this->getClaimDiffHtml( $claimDiffOp );
+		}
+
+		// FIXME: this does not belong here as it is specific to items
+		if ( $diff instanceof ItemDiff ) {
+			$termDiffVisualizer = new DiffView(
+				array(),
+				new Diff( array(
+					'links' => $diff->getSiteLinkDiff(),
+				), true ),
+				$this->context
+			);
+
+			$html .= $termDiffVisualizer->getHtml();
+		}
+
+		return $html;
+	}
+
+	/**
+	 * Returns the HTML for a single claim DiffOp.
+	 *
+	 * @since 0.4
+	 *
+	 * @param DiffOp $claimDiffOp
+	 *
+	 * @return string
+	 * @throws MWException
+	 */
+	protected function getClaimDiffHtml( DiffOp $claimDiffOp ) {
+		if ( $claimDiffOp instanceof DiffOpChange ) {
+			$claimDifference = $this->claimDiffer->diffClaims( $claimDiffOp->getOldValue(), $claimDiffOp->getNewValue() );
+			return $this->claimDiffVisualizer->visualizeDiff( $claimDifference );
+		}
+
+		if ( $claimDiffOp instanceof DiffOpAdd || $claimDiffOp instanceof DiffOpRemove ) {
+			$claim = $claimDiffOp instanceof DiffOpAdd ? $claimDiffOp->getNewValue() : $claimDiffOp->getOldValue();
+			return $this->getClaimHtml( $claim );
+		}
+
+		throw new MWException( 'Encountered an unexpected diff operation type for a claim' );
+	}
+
+	/**
+	 * Returns the HTML for a single Claim.
+	 *
+	 * @since 0.4
+	 *
+	 * @param Claim $claim
+	 *
+	 * @return string
+	 */
+	protected function getClaimHtml( Claim $claim ) {
+		return 'TODO'; // TODO
+	}
+
+}
---
+++ lib/tests/phpunit/claim/ClaimDifferenceVisualizerTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Wikibase\Test;
+
+use Wikibase\ClaimDifferenceVisualizer;
+use Wikibase\ClaimDifference;
+
+/**
+ * Tests for the Wikibase\ClaimDifferenceVisualizer class.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @since 0.4
+ *
+ * @ingroup WikibaseLib
+ * @ingroup Test
+ *
+ * @group Wikibase
+ * @group WikibaseLib
+ * @group WikibaseClaim
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class ClaimDifferenceVisualizerTest extends \MediaWikiTestCase {
+
+	public function visualizeDiffProvider() {
+		$differences = array();
+
+		$differences[] = new ClaimDifference();
+
+		$differences[] = new ClaimDifference(
+			new \Diff\DiffOpChange(
+				new \Wikibase\PropertyNoValueSnak( 42 ),
+				new \Wikibase\PropertyNoValueSnak( 43 )
+			),
+			new \Diff\Diff( array(
+				new \Diff\DiffOpAdd( new \Wikibase\PropertySomeValueSnak( 44 ) ),
+				new \Diff\DiffOpRemove( new \Wikibase\PropertyValueSnak( 45, new \DataValues\StringValue( 'foo' ) ) ),
+				new \Diff\DiffOpChange( new \Wikibase\PropertySomeValueSnak( 46 ), new \Wikibase\PropertySomeValueSnak( 47 ) ),
+			) )
+		);
+
+		$differences[] = new ClaimDifference(
+			new \Diff\DiffOpChange(
+				new \Wikibase\PropertyNoValueSnak( 42 ),
+				new \Wikibase\PropertyNoValueSnak( 43 )
+			),
+			null,
+			new \Diff\Diff( array(
+				new \Diff\DiffOpAdd( new \Wikibase\Reference() ),
+				new \Diff\DiffOpRemove( new \Wikibase\Reference( new \Wikibase\SnakList( array( new \Wikibase\PropertyNoValueSnak( 50 ) ) ) ) ),
+			) )
+		);
+
+		$differences[] = new ClaimDifference(
+			null,
+			null,
+			null,
+			new \Diff\DiffOpChange( \Wikibase\Statement::RANK_DEPRECATED, \Wikibase\Statement::RANK_PREFERRED )
+		);
+
+		return $this->arrayWrap( $differences );
+	}
+
+	/**
+	 * @dataProvider visualizeDiffProvider
+	 *
+	 * @param ClaimDifference $claimDifference
+	 */
+	public function testVisualizeDiff( ClaimDifference $claimDifference ) {
+		$differenceVisualizer = new ClaimDifferenceVisualizer( new \Wikibase\CachingEntityLoader() );
+
+		$visualization = $differenceVisualizer->visualizeDiff( $claimDifference );
+
+		$this->assertInternalType( 'string', $visualization );
+	}
+
+}
