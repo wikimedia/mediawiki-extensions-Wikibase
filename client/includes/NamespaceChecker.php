31e89d5f2ee9643459c11fd4d74a3ea4ff52aabb
aude <aude.wiki@gmail.com>
2013-02-11 13:33:33 +0000

    (bug 44873) Add setting to exclude namespaces
    - make it default to include all namespaces, but with option to exclude some
    - it's insane to specify settings for all wikipedias to include namespaces;
    much better to have wikibase for all namespaces by default, and we can exclude
    some if requested.
    
    Change-Id: I1a96321d43beb621d5a28ea024fb46477e0abafc

---
+++ client/includes/NamespaceChecker.php
@@ -0,0 +1,76 @@
+<?php
+namespace Wikibase;
+
+/**
+ * Checks if a namespace in Wikibase Client shall have wikibase links, etc., based on settings
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @since 0.4
+ *
+ * @file
+ * @ingroup WikibaseClient
+ *
+ * @licence GNU GPL v2+
+ * @author Katie Filbert < aude.wiki@gmail.com >
+ */
+class NamespaceChecker {
+
+	protected $excluded;
+
+	protected $enabled;
+
+	/**
+	 * @since 0.4
+	 *
+	 * @param $excluded[]
+	 * @param $enabled[]
+	 *
+	 * @throws \MWException
+	 */
+	public function __construct( array $excluded, $enabled ) {
+		$this->excluded = $excluded;
+
+		if ( $enabled !== false && ! is_array( $enabled ) ) {
+			throw new \MWException( '$enabled parameter is invalid.' );
+		}
+
+		$this->enabled = $enabled;
+	}
+
+	/**
+	 * Per the settings, does the namespace have wikibase enabled?
+	 *
+	 * @since 0.4
+	 *
+	 * @param int $namespace
+	 *
+	 * @return bool
+	 */
+	public function isWikibaseEnabled( $namespace ) {
+		if( !is_int( $namespace ) ) {
+			wfDebug( 'Invalid namespace in Wikibase namespace checker.' );
+			return false;
+		} else if ( in_array( $namespace, $this->excluded ) ) {
+			return false;
+		} else if ( is_array( $this->enabled ) && !in_array( $namespace, $this->enabled ) ) {
+			return false;
+		}
+
+		return true;
+	}
+
+}
