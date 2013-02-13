7a45c185e13d1afce92aae373495636579361469
jeroendedauw <jeroendedauw@gmail.com>
2013-02-13 14:50:35 +0100

    Query engine [DO NOT MERGE]
    Change-Id: I1de75a9a3ed40b04f77443f035aa9ead8816ccf5

---
+++ repo/includes/Query/SQLEngine/SQLQueryEngine.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Wikibase\Repo\Query\SQLEngine;
+
+use Ask\Language\Query;
+use Wikibase\Repo\Query\QueryEngineResult;
+use Wikibase\Repo\Query\QueryEngine;
+
+class SQLQueryEngine implements QueryEngine {
+
+	
+
+	public function __construct() {
+
+	}
+
+	/**
+	 * @see QueryEngine::runQuery
+	 *
+	 * @param Query $query
+	 *
+	 * @return QueryEngineResult
+	 */
+	public function runQuery( Query $query ) {
+
+	}
+
+}
