229ac1f9326537d5a8b2b5b81723da262afd78b5
daniel <daniel.kinzler@wikimedia.de>
2013-02-14 16:25:47 +0100

    Make language names available in other languages.
    Requires the CLDR extension for full functionality.
    
    Change-Id: I7125109c13a351147c0ea8917332d1c08da7096f

--- lib/includes/Utils.php
+++ lib/includes/Utils.php
@@ -27,6 +27,7 @@ use Sanitizer, UtfNormal, Language, SiteList, SiteSQLStore;
  * @ingroup WikibaseLib
  *
  * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  * @author Tobias Gritschacher
  * @author Jens Ohlig < jens.ohlig@wikimedia.de >
  */
@@ -56,11 +57,19 @@ final class Utils {
 	 * @since 0.1
 	 *
 	 * @param string $languageCode
+	 * @param string|null $inLanguage
 	 *
 	 * @return string
 	 */
-	public static function fetchLanguageName( $languageCode ) {
-		$languageName = \Language::fetchLanguageName( str_replace( '_', '-', $languageCode ) );
+	public static function fetchLanguageName( $languageCode, $inLanguage = null ) {
+		$languageCode = str_replace( '_', '-', $languageCode );
+		if ( isset( $inLanguage ) ) {
+			$inLanguage = str_replace( '_', '-', $inLanguage );
+			$languageName = \Language::fetchLanguageName( $languageCode, $inLanguage );
+		}
+		else {
+			$languageName = \Language::fetchLanguageName( $languageCode );
+		}
 		if ( $languageName == '' ) {
 			$languageName = $languageCode;
 		}
