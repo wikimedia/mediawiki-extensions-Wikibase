84d828924b604eba67084d3e5a9dd4cf59f0be30
aude <aude.wiki@gmail.com>
2013-02-18 11:47:41 +0000

    Simple diff visualization
    - todo: make sure all cases are covered, such as reference change
    - further design improvements
    - tests
    - there's opportunity for DiffView and other places to share code
    
    Change-Id: I4459f274b092a4c0aa707af8bafd175775b2c51e

--- lib/includes/claim/ClaimDifferenceVisualizer.php
+++ lib/includes/claim/ClaimDifferenceVisualizer.php
@@ -1,9 +1,7 @@
 <?php
-
 namespace Wikibase;
 
 use Html;
-use Diff;
 
 /**
  * Class for generating HTML for Claim Diffs.
@@ -30,6 +28,7 @@ use Diff;
  *
  * @licence GNU GPL v2+
  * @author Tobias Gritschacher < tobias.gritschacher@wikimedia.de >
+ * @author Katie Filbert < aude.wiki@gmail.com >
  */
 class ClaimDifferenceVisualizer {
 
@@ -46,9 +45,11 @@ class ClaimDifferenceVisualizer {
 	 * @since 0.4
 	 *
 	 * @param EntityLookup $entityLookup
+	 * @param string $langCode
 	 */
-	public function __construct( $entityLookup ) {
+	public function __construct( $entityLookup, $langCode ) {
 		$this->entityLookup = $entityLookup;
+		$this->langCode = $langCode;
 	}
 
 	/**
@@ -58,8 +59,235 @@ class ClaimDifferenceVisualizer {
 	 *
 	 * @return string
 	 */
-	public function visualizeDiff( ClaimDifference $claimDifference ) {
-		return 'TODO'; // TODO
+	public function visualizeDiff( ClaimDifference $claimDifference, $langCode ) {
+		$html = '';
+
+		if ( $claimDifference->getMainSnakChange() !== null ) {
+			$mainSnakChange = $claimDifference->getMainSnakChange();
+			$valueFormatter = new DiffOpValueFormatter(
+				// todo: should shoe specific headers for both columns
+				$this->getMainSnakHeader( $mainSnakChange->getNewValue() ),
+				$this->getMainSnakValue( $mainSnakChange->getOldValue() ),
+				$this->getMainSnakValue( $mainSnakChange->getNewValue() )
+			);
+			$html .= $valueFormatter->generateHtml();
+		}
+
+		if ( $claimDifference->getRankChange() !== null ) {
+			$rankChange = $claimDifference->getRankChange();
+			$valueFormatter = new DiffOpValueFormatter(
+				wfMessage( 'wikibase-diffview-rank' ),
+				$rankChange->getOldValue(),
+				$rankChange->getNewValue()
+			);
+			$html .= $valueFormatter->generateHtml();
+		}
+
+		// TODO: html for qualifier changes
+
+		if ( $claimDifference->getReferenceChanges() !== null ) {
+			$referenceChanges = $claimDifference->getReferenceChanges();
+
+			// somehow changing a reference value is treated as a diffop add and diffop remove
+			// for diff visualization, it should be more like a change
+			// @todo assert that both reference changes refer to the same reference
+			if ( count( $referenceChanges ) === 2 ) {
+
+				$oldValue = $newValue = null;
+
+				foreach( $referenceChanges as $referenceChange ) {
+					if ( $referenceChange instanceof \Diff\DiffOpAdd ) {
+						$newValue = $referenceChange->getNewValue();
+					} else if ( $referenceChange instanceof \Diff\DiffOpRemove ) {
+						$oldValue = $referenceChange->getOldValue();
+					}
+				}
+
+				$html .= $this->getRefHtml( $oldValue, $newValue, 'change' );
+			} else {
+				foreach( $referenceChanges as $referenceChange ) {
+					if ( $referenceChange instanceof \Diff\DiffOpAdd ) {
+						$html .= $this->getRefHtml( null, $referenceChange->getNewValue(), 'add' );
+					} else if ( $referenceChange instanceof \Diff\DiffOpRemove ) {
+						$html .= $this->getRefHtml( $referenceChange->getOldValue(), null, 'remove' );
+					} else if ( $referenceChange instanceof \Diff\DiffOpChange ) {
+						$html .= $this->getRefHtml( $referenceChange->getOldValue(),
+							$reference->getNewValue(), 'change' );
+					}
+				}
+			}
+		}
+
+		return $html;
+	}
+
+	/**
+	 * Format a snak
+	 *
+	 * @since 0.4
+	 *
+	 * @param Snak|null $oldSnak
+	 * @param Snak|null $newSnak
+	 * @param string $prependHeader
+	 *
+	 * @return string
+	 */
+	public function getSnakHtml( $oldSnak, $newSnak, $prependHeader = null ) {
+		$snakHeader = '';
+		// @todo fix ugly cruft!
+		if ( $prependHeader !== null ) {
+			$snakHeader = $prependHeader;
+		}
+
+		if ( $newSnak instanceof Snak || $oldSnak instanceof Snak ) {
+			$headerSnak = $newSnak instanceof Snak ? $newSnak : $oldSnak;
+			$snakHeader .= $this->getMainSnakHeader( $headerSnak );
+		} else {
+			// something went wrong
+			throw new \MWException( 'Snak parameters not provided.' );
+		}
+
+		$oldValue = null;
+		$newValue = null;
+
+		if ( $oldSnak instanceof Snak ) {
+			$oldValue = $this->getMainSnakValue( $oldSnak );
+		}
+
+		if ( $newSnak instanceof Snak ) {
+			$newValue = $this->getMainSnakValue( $newSnak );
+		}
+
+		$valueFormatter = new DiffOpValueFormatter( $snakHeader, $oldValue, $newValue );
+
+		return $valueFormatter->generateHtml();
+	}
+
+	/**
+	 * Get formatted SnakList
+	 *
+	 * @since 0.4
+	 *
+	 * @param SnakList $snakList
+	 *
+	 * @return string
+	 */
+	 protected function getSnakListHtml( $snakList ) {
+		$html = '';
+
+		foreach ( $snakList as $snak ) {
+
+			if ( $html !== '' ) {
+				$html .= Html::rawElement( 'br', array(), '' );
+			}
+			// @fixme
+			$html .= $this->getMainSnakValue( $snak );
+		}
+
+		return $html;
+	}
+
+	/**
+	 * Get formatted header for a snak
+	 *
+	 * @since 0.4
+	 *
+	 * @param Snak $mainSnak
+	 *
+	 * @return string
+ 	 */
+	protected function getMainSnakHeader( Snak $mainSnak ) {
+		$propertyId = $mainSnak->getPropertyId();
+		$property = $this->entityLookup->getEntity( $propertyId );
+		$dataTypeLabel = $property->getDataType()->getLabel( $this->langCode );
+
+		$label = $property->getLabel( $this->langCode );
+		$propertyLabel = $label !== false ? $label : $property->getPrefixedId();
+
+		$headerText = wfMessage( 'wikibase-entity-property' ) . ' / ' . $dataTypeLabel . ' / ' . $label;
+
+		return $headerText;
+	}
+
+	/**
+	 * Get main snak value in string form
+	 *
+	 * @since 0.4
+	 *
+	 * @param Snak $snak
+	 *
+	 * @return string
+ 	 */
+	protected function getMainSnakValue( Snak $snak ) {
+		$snakType = $snak->getType();
+
+		if ( $snakType === 'value' ) {
+			$dataValue = $snak->getDataValue();
+
+			// FIXME!
+			if ( $dataValue instanceof EntityId ) {
+				$diffValueString = $this->getEntityLabel( $dataValue );
+			} else {
+				$diffValueString = $dataValue->getValue();
+			}
+
+			return $diffValueString;
+		}
+
+		// fixme, error handling for unknown snak value types
+		return '';
+	}
+
+	/**
+	 * Get an entity label
+	 *
+	 * @since 0.4
+	 *
+	 * @param EntityId $entityId
+	 *
+	 * @return string
+	 */
+	protected function getEntityLabel( EntityId $entityId  ) {
+		$label = $entityId->getPrefixedId();
+
+		$lookedUpLabel = $this->entityLookup->getEntity( $entityId )->getLabel( $this->langCode );
+
+		if ( $lookedUpLabel !== false ) {
+			$label = $lookedUpLabel;
+		}
+
+		return $label;
+	}
+
+	/**
+	 * Format reference change
+	 *
+	 * @since 0.4
+	 *
+	 * @param $oldRef Reference|null
+	 * @param $newRef Reference|null
+	 * @param $opType string
+	 *
+	 * @return string
+	 */
+	protected function getRefHtml( $oldRef, $newRef, $opType ) {
+		$html = $oldHtml = $newHtml = '';
+
+		if ( $oldRef !== null ) {
+			$oldHtml .= $this->getSnakListHtml( $oldRef->getSnaks() );
+		}
+
+		if ( $newRef !== null ) {
+			$newHtml .= $this->getSnakListHtml( $newRef->getSnaks() );
+		}
+
+		$valueFormatter = new DiffOpValueFormatter(
+			wfMessage( 'wikibase-diffview-reference' ),
+			$oldHtml,
+			$newHtml
+		);
+
+		return $valueFormatter->generateHtml();
 	}
 
 }
