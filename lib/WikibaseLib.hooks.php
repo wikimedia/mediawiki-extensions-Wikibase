6abd8e64d42dbd60dacd0acb3d535baab328f9c0
Daniel Werner <daniel.werner@wikimedia.de>
2013-02-20 13:22:36 +0100

    Removes jquery.inputAutoExpand module since it went into DataTypes extension
    Moved into DataTypes extension in I21530780db401e8488d60a014161ab1629955780.
    
    Change-Id: I530ab6647bb8eb003146573401b438408c319494

--- lib/WikibaseLib.hooks.php
+++ lib/WikibaseLib.hooks.php
@@ -165,7 +165,6 @@ final class LibHooks {
 				'tests/qunit/wikibase.utilities/wikibase.utilities.jQuery.ui.tests.js',
 				'tests/qunit/wikibase.utilities/wikibase.utilities.jQuery.ui.tagadata.tests.js',
 
-				'tests/qunit/jquery/jquery.inputAutoExpand.tests.js',
 				'tests/qunit/jquery.ui/jquery.ui.suggester.tests.js',
 				'tests/qunit/jquery.ui/jquery.ui.entityselector.tests.js',
 				'tests/qunit/jquery.wikibase/jquery.wikibase.siteselector.tests.js',
