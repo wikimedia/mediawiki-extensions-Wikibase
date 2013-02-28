156a2b5c36a3a126813f1b7acc12515a6f7c0815
Daniel Werner <daniel.werner@wikimedia.de>
2013-02-28 20:29:25 +0100

    Introduction of wb.Entity and related constructors and its usage in our code
    Constructors we need for first step towards proper entity store interface which we can use in
    entityview widget and others to get rid of global revision and entity stores. This patch set will
    not make any real use of any of that new code in existing code yet.
    
    * wb.Entity, wb.Item and wb.Property implementations which will replace loose array structures we
      are dealing with currently.
    * initial implementation of wb.store.FetchedContent which represents some wiki content plus meta
      data such as wiki page title and fetched revision.
    * new module "wikibase.serialization" which holds serialization logic to unserialize
      wb.Entity and wb.store.FetchedContent from JSON.
    
    Change-Id: I1c8912d8e82ff1b563a6e8861f73bfb223caa696

---
+++ view/packages/wikibase-serialization/serialization.Unserializer.js
@@ -0,0 +1,68 @@
+/**
+ * @file
+ * @ingroup WikibaseLib
+ * @licence GNU GPL v2+
+ * @author Daniel Werner < daniel.werner@wikimedia.de >
+ */
+( function( wb, $ ) {
+	'use strict';
+
+	var MODULE = wb.serialization;
+
+	/**
+	 * Base for unserializers.
+	 *
+	 * @param {Object} [options]
+	 *
+	 * @constructor
+	 * @abstract
+	 * @since 0.4
+	 */
+	var SELF = MODULE.Unserializer = function WbUnserializer( options ) {
+		if( options ) {
+			this.setOptions( options );
+		} else {
+			this._options = {};
+		}
+	};
+
+	SELF.prototype = {
+		/**
+		 * @type Object
+		 */
+		_options: null,
+
+		/**
+		 * Constructs the original object from the provided serialization.
+		 *
+		 * @since 0.4
+		 *
+		 * @param {Object} serialization
+		 */
+		unserialize: wb.utilities.abstractFunction,
+
+		/**
+		 * Sets the unserializer's options without just keeping a reference to the given object.
+		 *
+		 * @since 0.4
+		 *
+		 * @param options
+		 */
+		setOptions: function( options ) {
+			this._options = $.extend( {}, options );
+		},
+
+		/**
+		 * Returns the unserializer's options. Changing the returned object will have no affect on the
+		 * unserializer's actual options until they are set via setOptions.
+		 *
+		 * @since 0.4
+		 *
+		 * @return Object
+		 */
+		getOptions: function() {
+			return $.extend( {}, this._options );
+		}
+	};
+
+}( wikibase, jQuery ) );
