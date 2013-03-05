aebdb1ac343202fb9e04052e12f1aac2906a266d
jeroendedauw <jeroendedauw@gmail.com>
2013-03-05 02:45:46 +0100

    Added StringHandler
    Change-Id: I5a7d4187a7e9df674faf44fecca01a46f7b56444

---
+++ repo/includes/Query/SQLStore/DVHandler/StringHandler.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace Wikibase\Repo\Query\SQLStore\DVHandler;
+
+use Wikibase\Repo\Query\SQLStore\DataValueHandler;
+use Wikibase\Repo\Database\TableDefinition;
+use Wikibase\Repo\Database\FieldDefinition;
+use DataValues\DataValue;
+use DataValues\StringValue;
+use InvalidArgumentException;
+
+/**
+ * Represents the mapping between Wikibase\StringValue and
+ * the corresponding table in the store.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @since wd.qe
+ *
+ * @file
+ * @ingroup WikibaseSQLStore
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class StringHandler extends DataValueHandler {
+
+	/**
+	 * @see DataValueHandler::getTableDefinition
+	 *
+	 * @since wd.qe
+	 *
+	 * @return TableDefinition
+	 */
+	public function getTableDefinition() {
+		$fields = array(
+			new FieldDefinition( 'value', FieldDefinition::TYPE_TEXT, false ),
+		);
+
+		return new TableDefinition( 'string', $fields );
+	}
+
+	/**
+	 * @see DataValueHandler::getValueFieldName
+	 *
+	 * @since wd.qe
+	 *
+	 * @return string
+	 */
+	public function getValueFieldName() {
+		return 'value';
+	}
+
+	/**
+	 * @see DataValueHandler::getSortFieldName
+	 *
+	 * @since wd.qe
+	 *
+	 * @return string
+	 */
+	public function getSortFieldName() {
+		return 'value';
+	}
+
+	/**
+	 * @see DataValueHandler::newDataValueFromValueField
+	 *
+	 * @since wd.qe
+	 *
+	 * @param $valueFieldValue // TODO: mixed or string?
+	 *
+	 * @return DataValue
+	 */
+	public function newDataValueFromValueField( $valueFieldValue ) {
+		return new StringValue( $valueFieldValue );
+	}
+
+	/**
+	 * @see DataValueHandler::getLabelFieldName
+	 *
+	 * @since wd.qe
+	 *
+	 * @return string|null
+	 */
+	public function getLabelFieldName() {
+		return null;
+	}
+
+	/**
+	 * @see DataValueHandler::getWhereConditions
+	 *
+	 * @since wd.qe
+	 *
+	 * @param DataValue $value
+	 *
+	 * @return array
+	 * @throws InvalidArgumentException
+	 */
+	public function getWhereConditions( DataValue $value ) {
+		if ( !( $value instanceof StringValue ) ) {
+			throw new InvalidArgumentException( 'Value is not a StringValue' );
+		}
+
+		return array(
+			'value' => $value->getValue(),
+		);
+	}
+
+	/**
+	 * @see DataValueHandler::getInsertValues
+	 *
+	 * @since wd.qe
+	 *
+	 * @param DataValue $value
+	 *
+	 * @return array
+	 * @throws InvalidArgumentException
+	 */
+	public function getInsertValues( DataValue $value ) {
+		if ( !( $value instanceof StringValue ) ) {
+			throw new InvalidArgumentException( 'Value is not a StringValue' );
+		}
+
+		$values = array(
+			'value' => $value->getValue(),
+		);
+
+		return $values;
+	}
+
+}
