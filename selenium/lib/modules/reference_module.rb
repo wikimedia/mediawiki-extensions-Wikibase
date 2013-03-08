ec066e6198aaad3278c7dea2aa6aca293149cf5d
Tobias Gritschacher <tobias.gritschacher@wikimedia.de>
2013-03-08 16:32:33 +0100

    (testing) adding selenium tests for deleted-property/item-handling in UI
    Change-Id: Ia84b94cb3ea74ed7f8b2b02686528d6956e4a914

--- selenium/lib/modules/reference_module.rb
+++ selenium/lib/modules/reference_module.rb
@@ -21,7 +21,9 @@ module ReferencePage
   div(:reference2Value, :xpath => "//div[contains(@class, 'wb-referenceview')][2]/div[contains(@class, 'wb-reference-snaks')]/div[contains(@class, 'wb-snak-value-container')]/div[contains(@class, 'wb-snak-value')]/div/div")
   div(:reference3Value, :xpath => "//div[contains(@class, 'wb-referenceview')][3]/div[contains(@class, 'wb-reference-snaks')]/div[contains(@class, 'wb-snak-value-container')]/div[contains(@class, 'wb-snak-value')]/div/div")
   link(:reference1ValueLink, :xpath => "//div[contains(@class, 'wb-referenceview')][1]/div[contains(@class, 'wb-reference-snaks')]/div[contains(@class, 'wb-snak-value-container')]/div[contains(@class, 'wb-snak-value')]/div/div/a")
-  text_area(:referenceValueInput, :xpath => "//div[contains(@class, 'valueview-ineditmode')]/div/a/textarea[contains(@class, 'valueview-input')]")
+  # TODO: could this lead to problems? for CM & item type properties there is an additional "a" element around the textbox; this is not the case for string type properies
+  #text_area(:referenceValueInput, :xpath => "//div[contains(@class, 'valueview-ineditmode')]/div/a/textarea[contains(@class, 'valueview-input')]")
+  text_area(:referenceValueInput, :class => "valueview-input")
   link(:saveReference, :xpath => "//div[contains(@class, 'wb-claim-toolbar')]/span/span/span[contains(@class, 'wb-ui-toolbar-editgroup-ineditmode')]/span/a[text()='save']")
   link(:cancelReference, :xpath => "//div[contains(@class, 'wb-claim-toolbar')]/span/span/span[contains(@class, 'wb-ui-toolbar-editgroup-ineditmode')]/span/a[text()='cancel']")
   link(:removeReference, :xpath => "//div[contains(@class, 'wb-claim-toolbar')]/span/span/span[contains(@class, 'wb-ui-toolbar-editgroup-ineditmode')]/span/a[text()='remove']")
@@ -38,6 +40,19 @@ module ReferencePage
     wait_until do
       referenceListItems_element.visible?
     end
+  end
+
+  def add_reference_to_first_claim(property, value)
+    addReferenceToFirstClaim
+    self.entitySelectorInput = property
+    ajax_wait
+    wait_for_entity_selector_list
+    wait_for_reference_value_box
+    self.referenceValueInput = value
+    ajax_wait
+    saveReference
+    ajax_wait
+    wait_for_statement_request_finished
   end
 
 end
