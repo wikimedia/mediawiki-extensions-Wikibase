76fdd2f21f7bbee32161b188a529efd909d71763
Henning Snater <henning.snater@wikimedia.de>
2013-03-12 08:01:05 +0100

    (bug 44683) Implementing missing equals() and toJSON() methods
    This change set implements some equals() and toJSON() methods for
    wb.Reference, wb.Claim and wb.Statement not yet implemented but
    required for implementing the setClaim API method.
    
    - patch set 4: rebase
    - patch set 6: Optimized QUnit tests
    
    Change-Id: I0e1d341549e253bfb411bc61960bbd57ad569c84

--- view/packages/wikibase-data-model/Wikibase.claim.tests.js
+++ view/packages/wikibase-data-model/Wikibase.claim.tests.js
@@ -7,12 +7,13 @@
  *
  * @licence GNU GPL v2+
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ * @author H. Snater < mediawiki@snater.com >
  */
 
 ( function( wb, dv, $, QUnit ) {
 	'use strict';
 
-	QUnit.module( 'wikibase.datamodel.claim.js', QUnit.newMwEnvironment() );
+	QUnit.module( 'wikibase.datamodel.Claim', QUnit.newWbEnvironment() );
 
 	QUnit.test( 'constructor', function( assert ) {
 		var argumentLists = [
@@ -75,6 +76,91 @@
 				'Main snak type is correct'
 			);
 		} );
+	} );
+
+	QUnit.test( 'toJSON()', function( assert ) {
+		var claim = new wb.Claim( new wb.PropertyValueSnak( 42, new dv.StringValue( '~=[,,_,,]:3' ) ) );
+
+		assert.ok(
+			claim.equals( wb.Claim.newFromJSON( claim.toJSON() ) ),
+			'Exported simple claim to JSON.'
+		);
+
+		claim = new wb.Claim(
+			new wb.PropertyNoValueSnak( 42 ),
+			new wb.SnakList(
+				[
+					new wb.PropertyNoValueSnak( 9001 ),
+					new wb.PropertySomeValueSnak( 42 ),
+					new wb.PropertyValueSnak( 23, new dv.StringValue( '~=[,,_,,]:3' ) )
+				]
+			)
+		);
+
+		assert.ok(
+			claim.equals( wb.Claim.newFromJSON( claim.toJSON() ) ),
+			'Exported complex claim to JSON.'
+		);
+	} );
+
+	QUnit.test( 'equals()', function( assert ) {
+		var claims = [
+			new wb.Claim( new wb.PropertyValueSnak( 42, new dv.StringValue( 'string' ) ) ),
+			new wb.Claim(
+				new wb.PropertyValueSnak( 42, new dv.StringValue( 'string' ) ),
+				new wb.SnakList(
+					[
+						new wb.PropertyValueSnak( 2, new dv.StringValue( 'some string' ) ),
+						new wb.PropertySomeValueSnak( 9001 )
+					]
+				)
+			),
+			new wb.Claim( new wb.PropertyValueSnak( 42, new dv.StringValue( 'other string' ) ) ),
+			new wb.Claim( new wb.PropertySomeValueSnak( 9001 ) ),
+			new wb.Claim(
+				new wb.PropertyValueSnak( 42, new dv.StringValue( 'string' ) ),
+				new wb.SnakList(
+					[
+						new wb.PropertyValueSnak( 43, new dv.StringValue( 'some string' ) ),
+						new wb.PropertySomeValueSnak( 9001 )
+					]
+				)
+			)
+		];
+
+		// Compare claims:
+		$.each( claims, function( i, claim ) {
+			var clonedClaim = wb.Claim.newFromJSON( claim.toJSON() );
+
+			// Check if "cloned" claim is equal:
+			assert.ok(
+				claim.equals( clonedClaim ),
+				'Verified claim "' + i + '" on equality.'
+			);
+
+			// Compare to all other claims:
+			$.each( claims, function( j, otherClaim ) {
+				if ( j !== i ) {
+					assert.ok(
+						!claim.equals( otherClaim ),
+						'Claim "' + i + '" is not equal to claim "'+ j + '".'
+					);
+				}
+			} );
+
+		} );
+
+		// Compare claim to statement:
+		var claim = new wb.Claim( new wb.PropertyValueSnak( 42, new dv.StringValue( 'string' ) ) ),
+			statement = new wb.Statement(
+				new wb.PropertyValueSnak( 42, new dv.StringValue( 'string' ) )
+			);
+
+		assert.ok(
+			claim.equals( statement ),
+			'Claim equals statement that received the same initialization parameters.'
+		);
+
 	} );
 
 }( wikibase, dataValues, jQuery, QUnit ) );
