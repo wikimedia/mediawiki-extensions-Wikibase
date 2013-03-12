69b88d56d72a5e97ea3aebed7676c9fd57789935
jeblad <john.blad@wikimedia.de>
2013-03-12 16:25:41 +0100

    (Bug 43994) New Special:UnconnectedPages
    This special page "Special:UnconnectedPages" uses a page prop "wikibase_item"
    that holds the id for the item at the repository.
    
    Later it could be other tables that are better suited, and updated faster,
    than the page_props table.
    
    The actual page prop in use is defined in
    https://gerrit.wikimedia.org/r/#/c/51179/
    
    Patchset 3: Rebased to use the last additions for page props
    Patchset 4: Tryed out Daniels proposal
    Patchset 5: Rebase to fix an error due to old changes
    Patchset 6: Fix of bug introduced by "JOIN" should be "LEFT JOIN"
                Also fix of whitespace (carriage return).
    Patchset 7: More cleanup
    Patchset 8: More cleanup - where do those carriage return comes from??
    
    Change-Id: I5cfab43ee8c7feff79c8b4bda0183142dc3e2c98

---
+++ client/includes/specials/SpecialUnconnectedPages.php
@@ -0,0 +1,300 @@
+<?php
+
+/**
+ * List client pages that is not connected to repository items.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @since 0.4
+ *
+ * @file
+ * @ingroup WikibaseClient
+ *
+ * @licence GNU GPL v2+
+ * @author John Erling Blad < jeblad@gmail.com >
+ */
+class SpecialUnconnectedPages extends SpecialWikibaseQueryPage {
+
+	/**
+	 * The title as a string to start search at
+	 *
+	 * @since 0.4
+	 *
+	 * @var string
+	 */
+	protected $startPage = '';
+
+	/**
+	 * The startPage as a title to start search at
+	 *
+	 * @since 0.4
+	 *
+	 * @var Title
+	 */
+	protected $startTitle = null;
+
+	/**
+	 * The namespaceChecker
+	 *
+	 * @since 0.4
+	 *
+	 * @var NamespaceChecker
+	 */
+	protected $namespaceChecker = null;
+
+	/**
+	 * If the search should only include pages with iw-links
+	 *
+	 * @since 0.4
+	 *
+	 * @var string
+	 */
+	protected $iwData = '';
+
+	public function __construct() {
+		parent::__construct( 'UnconnectedPages' );
+
+	}
+
+	/**
+	 * @see SpecialWikibasePage::execute
+	 *
+	 * @since 0.4
+	 *
+	 * @param string $subPage
+	 * @return boolean
+	 */
+	public function execute( $subPage ) {
+		if ( !parent::execute( $subPage ) ) {
+			return false;
+		}
+
+		# 10 seconds server-side caching max
+		$this->getOutput()->setSquidMaxage( 10 );
+
+		$output = $this->getOutput();
+		$request = $this->getRequest();
+
+		$this->startPage = $request->getText( 'page', $subPage );
+		if ( $this->startPage !== '' ) {
+			$this->startTitle = \Title::newFromText( $this->startPage );
+		}
+
+		$this->iwData = $request->getText( 'iwdata', '' );
+
+		$out = '';
+
+		if ( $this->startPage !== '' && $this->startTitle === null ) {
+			$out .= Html::element( 'div', array(), $this->msg( 'wikibase-unconnectedpages-page-warning' )->text() );
+		}
+
+		$out .= Html::openElement(
+			'form',
+			array(
+				'action' => $this->getTitle()->getLocalURL(),
+				'name' => 'unconnectedpages',
+				'id' => 'wbc-unconnectedpages-form'
+			)
+		)
+		. Html::openElement( 'fieldset' )
+		. Html::element( 'legend', array(), $this->msg( 'wikibase-unconnectedpages-legend' )->text() )
+		. Html::openElement( 'p' )
+		. Html::element( 'label', array( 'for' => 'language' ), $this->msg( 'wikibase-unconnectedpages-page' )->text() )
+		. ' '
+		. Html::input(
+			'page',
+			$this->startPage,
+			'text',
+			array(
+				'id' => 'page'
+			)
+		)
+		. Html::input(
+			'submit',
+			$this->msg( 'wikibase-unconnectedpages-submit' )->text(),
+			'submit',
+			array(
+				'id' => 'wbc-unconnectedpages-submit',
+				'class' => 'wbc-input-button'
+			)
+		)
+		. ' '
+		. Html::input(
+			'iwdata',
+			'only',
+			'checkbox',
+			array(
+				'id' => 'wbc-unconnectedpages-iwdata',
+				'class' => 'wbc-input-button',
+				$this->iwData === 'only' ? 'checked' : 'unchecked' => ''
+			)
+		)
+		. ' '
+		. Html::element( 'label', array( 'for' => 'wbc-unconnectedpages-iwdata' ), $this->msg( 'wikibase-unconnectedpages-iwdata-label' )->text() )
+		. Html::closeElement( 'p' )
+		. Html::closeElement( 'fieldset' )
+		. Html::closeElement( 'form' );
+		$output->addHTML( $out );
+
+		$this->showQuery();
+	}
+
+	/**
+	 * Set the NamespaceChecker
+	 *
+	 * @since 0.4
+	 *
+	 * @param \Wikibase\NamespaceChecker $namespaceChecker
+	 */
+	public function setNamespaceChecker( \Wikibase\NamespaceChecker $namespaceChecker ) {
+		$this->namespaceChecker = $namespaceChecker;
+	}
+
+	/**
+	 * Get the NamespaceChecker
+	 *
+	 * @since 0.4
+	 *
+	 * @return \Wikibase\NamespaceChecker
+	 */
+	public function getNamespaceChecker() {
+		if ( $this->namespaceChecker === null ) {
+			$this->namespaceChecker = new \Wikibase\NamespaceChecker(
+				\Wikibase\Settings::get( 'excludeNamespaces' ),
+				\Wikibase\Settings::get( 'namespaces' )
+			);
+		}
+		return $this->namespaceChecker;
+	}
+
+	/**
+	 * Build conditionals for namespace
+	 *
+	 * @since 0.4
+	 *
+	 * @param DatabaseBase $dbr
+	 * @param Title $title
+	 * @param \Wikibase\NamespaceChecker $checker
+	 * @return string[]
+	 */
+	public function buildConditionals( $dbr, Title $title = null, $checker = null ) {
+		if ( !isset( $title ) ) {
+			$title = $this->startTitle;
+		}
+		if ( !isset( $checker ) ) {
+			$checker = $this->getNamespaceChecker();
+		}
+		if ( $title !== null ) {
+			$conds[] = 'page_title >= ' . $dbr->addQuotes( $title->getText() );
+			$conds[] = 'page_namespace = ' . $title->getNamespace();
+		}
+		$conds[] = 'page_namespace IN (' . implode(',', $checker->getWikibaseNamespaces() ) . ')';
+		return $conds;
+	}
+
+	/**
+	 * @see SpecialWikibaseQueryPage::getResult
+	 *
+	 * @since 0.4
+	 *
+	 * @param integer $offset Start to include at number of entries from the start title
+	 * @param integer $limit Stop at number of entries after start of inclusion
+	 *
+	 * @return Array[]
+	 */
+	public function getResult( $offset = 0, $limit = 0 ) {
+
+		$dbr = wfGetDB( DB_SLAVE );
+
+		$conds = $this->buildConditionals( $dbr );
+		$conds[] = "page_is_redirect = FALSE";
+		$conds[] = "pp_propname IS NULL";
+		if ( $this->iwData === 'only' ) {
+			$conds[] = 'll_from IS NOT NULL';
+		}
+
+		$rows = $dbr->select(
+			array(
+				'page',
+				'page_props',
+				'langlinks'
+			),
+			array(
+				'page_namespace',
+				'page_title',
+				'page_id',
+				'page_len',
+				'page_is_redirect',
+				'page_num_iwlinks' => 'count(ll_from)'
+			),
+			$conds,
+			__METHOD__,
+			array(
+				'OFFSET' => $offset,
+				'LIMIT' => $limit,
+				'GROUP BY' => 'page_namespace, page_title',
+				'ORDER BY' => 'page_namespace, page_title',
+				'USE INDEX' => array( 'page' => 'name_title' )
+			),
+			array(
+				'page_props' => array( 'LEFT JOIN', array( 'page_id = pp_page', "pp_propname = 'wikibase_item'" ) ),
+				'langlinks' => array( 'LEFT JOIN', 'll_from = page_id' )
+			)
+		);
+
+		$entries = array();
+		foreach ( $rows as $row ) {
+			$title = \Title::newFromRow( $row );
+			$numIWLinks = $row->page_num_iwlinks;
+			$entries[] = array( 'title' => $title, 'num' => $numIWLinks);
+		}
+		return $entries;
+	}
+
+	/**
+	 * @see SpecialWikibaseQueryPage::formatRow
+	 *
+	 * @since 0.4
+	 *
+	 * @param $entry The entry is for this call a row from the select in getResult
+	 *
+	 * @return string|null
+	 */
+	protected function formatRow( $entry ) {
+		try {
+			$out = Linker::linkKnown( $entry['title'] );
+			if ( $entry['num'] > 0 ) {
+				$out .= ' ' . $this->msg( 'wikibase-unconnectedpages-format-row', $entry['num'] )->text();
+			}
+			return $out;
+		} catch ( MWException $e ) {
+			wfWarn( "Error formatting result row: " . $e->getMessage() );
+			return false;
+		}
+	}
+
+	/**
+	 * @see SpecialWikibaseQueryPage::getTitleForNavigation
+	 *
+	 * @since 0.4
+	 *
+	 * @return Title
+	 */
+	protected function getTitleForNavigation() {
+		return $this->getTitle( $this->startPage );
+	}
+
+}
---
+++ client/tests/phpunit/includes/specials/SpecialUnconnectedPagesTest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Wikibase\Test;
+
+/**
+ * Tests for the SpecialUnconnectedPages class.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @since 0.4
+ *
+ * @ingroup WikibaseClientTest
+ * @ingroup Test
+ *
+ * @group WikibaseClient
+ * @group SpecialPage
+ * @group WikibaseSpecialPage
+ * @group WikibaseSpecialUnconnectedPages
+ *
+ * @licence GNU GPL v2+
+ * @author John Erling Blad < jeblad@gmail.com >
+ */
+class SpecialUnconnectedPagesTest extends SpecialPageTestBase {
+
+	protected function newSpecialPage() {
+		return new \SpecialUnconnectedPages();
+	}
+
+	public function testExecute(  ) {
+		//TODO: Actually verify that the output is correct.
+		//      Currently this just tests that there is no fatal error.
+		list( $output, ) = $this->executeSpecialPage( '' );
+		$this->assertTrue( true, 'Calling execute without any subpage value' );
+	}
+
+	/**
+	 * @dataProvider provideNamespaceChecker
+	 */
+	public function testNamespaceChecker( $namespace, $expected ) {
+		$page = $this->newSpecialPage();
+		$checker = new \Wikibase\NamespaceChecker( array( 2, 4 ), array( 0 ) );
+		$page->setNamespaceChecker( $checker );
+		$this->assertEquals( $expected, $page->getNamespaceChecker()->isWikibaseEnabled( $namespace ) );
+	}
+
+	public function provideNamespaceChecker() {
+		return array(
+			array( 0, true ),  // #0
+			array( 1, false ), // #1
+			array( 2, false ), // #2
+			array( 3, false ), // #3
+			array( 4, false ), // #4
+			array( 5, false ), // #5
+			array( 6, false ), // #6
+			array( 7, false ), // #7
+		);
+	}
+
+	/**
+	 *  @dataProvider provideBuildConditionals
+	 */
+	public function testBuildConditionals( $text, $expected ) {
+		$page = $this->newSpecialPage();
+		$title = \Title::newFromText( $text);
+		$checker = new \Wikibase\NamespaceChecker( array( 2, 4 ), array( 0 ) );
+		$dbr = wfGetDB( DB_SLAVE );
+		$this->assertEquals( $expected, $page->buildConditionals( $dbr, $title, $checker ) );
+	}
+
+	public function provideBuildConditionals() {
+		return array(
+			array( 'foo', array( "page_title >= 'Foo'", "page_namespace = 0", 'page_namespace IN (0)' ) ),
+			array( ':foo', array( "page_title >= 'Foo'", "page_namespace = 0", 'page_namespace IN (0)' ) ),
+			array( 'en:foo', array( "page_title >= 'foo'", "page_namespace = 0", 'page_namespace IN (0)' ) ),
+			array( 'user:foo', array( "page_title >= 'Foo'", "page_namespace = 2", 'page_namespace IN (0)' ) ),
+			array( 'user talk:foo', array( "page_title >= 'Foo'", "page_namespace = 3", 'page_namespace IN (0)' ) ),
+		);
+	}
+
+}
