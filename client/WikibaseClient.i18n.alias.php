fd808b5c54ce74e4c09f7b44c2e90a0da8106ac6
jeblad <john.blad@wikimedia.de>
2013-03-12 16:25:41 +0100

    (Bug 43994) New Special:UnconnectedPages
    This special page "Special:UnconnectedPages" uses a page prop "wikibase_item"
    that holds the id for the item at the repository.
    
    Later it could be other tables that are better suited, and updated faster,
    than the page_props table.
    
    The actual page prop in use is defined in
    https://gerrit.wikimedia.org/r/#/c/51179/
    
    Patchset 3: Rebased to use the last additions for page props
    Patchset 4: Tryed out Daniels proposal
    
    Change-Id: I5cfab43ee8c7feff79c8b4bda0183142dc3e2c98

---
+++ client/WikibaseClient.i18n.alias.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Aliases for the special pages of the Wikibase extension.
+ *
+ * @since 0.1
+ *
+ * @file
+ * @ingroup WikibaseRepo
+ *
+ * @licence GNU GPL v2+
+ * @author John Erling Blad < jeblad@gmail.com >
+ */
+
+$specialPageAliases = array();
+
+/** English (English) */
+$specialPageAliases['en'] = array(
+	'UnconnectedPages' => array( 'UnconnectedPages', 'WithoutConnection', 'WithoutSitelinks' ),
+);
