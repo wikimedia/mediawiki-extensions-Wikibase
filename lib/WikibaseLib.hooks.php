de0aa4968a5dd8e6113d7e0a3f8c9cd5b8211ad7
Henning Snater <henning.snater@wikimedia.de>
2013-03-13 10:39:42 +0100

    (bug 44683) GuidGenerator JavaScript implementation
    - patch set 2: Passing prefixed id to claim guid generator as long as the backend does not
    deliver a serialized EntityId object along with a serialized Entity.
    - patch set 3: rebase
    - patch set 4: rebase
    - patch set 5: Split claim GUID generator from plain GUID generator addressing comments on
    patch set 3.
    
    Change-Id: Ica945337f52110cad9f2febfaf52e695b6e302fe

--- lib/WikibaseLib.hooks.php
+++ lib/WikibaseLib.hooks.php
@@ -143,6 +143,8 @@ final class LibHooks {
 				'tests/qunit/wikibase.ui.Toolbar.Button.tests.js',
 				'tests/qunit/wikibase.ui.Tooltip.tests.js',
 
+				'tests/qunit/wikibase.utilities/wikibase.utilities.ClaimGuidGenerator.tests.js',
+				'tests/qunit/wikibase.utilities/wikibase.utilities.GuidGenerator.tests.js',
 				'tests/qunit/wikibase.utilities/wikibase.utilities.newExtension.tests.js',
 				'tests/qunit/wikibase.utilities/wikibase.utilities.ObservableObject.tests.js',
 				'tests/qunit/wikibase.utilities/wikibase.utilities.ui.StatableObject.tests.js',
@@ -165,6 +167,8 @@ final class LibHooks {
 				'wikibase.parsers',
 				'wikibase.store',
 				'wikibase.utilities',
+				'wikibase.utilities.ClaimGuidGenerator',
+				'wikibase.utilities.GuidGenerator',
 				'wikibase.utilities.jQuery',
 				'wikibase.ui.Toolbar',
 				'wikibase.ui.PropertyEditTool',
