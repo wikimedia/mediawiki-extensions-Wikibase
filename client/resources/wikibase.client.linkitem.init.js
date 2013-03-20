99fe2c2856ce9970c8b0c1e52fb4acce0eb9ce10
Marius Hoch <hoo@online.de>
2013-03-20 02:30:45 +0100

    Refactor and lazy load jquery.wikibase.linkitem
    While I was on it I changed wbclient.LinkItem into a jQuery
    widget (that's why I had to rename it). This makes the whole
    thing much more flexible.
    
    Change-Id: I9a03c3d974f181e8eadaa30b673e7c662033cb1c

---
+++ client/resources/wikibase.client.linkitem.init.js
@@ -0,0 +1,51 @@
+/**
+* JavaScript that lazy loads the widget for linking articles on the client
+*
+* @since 0.4
+*
+* Author: Marius Hoch hoo@online.de
+*/
+( function( mw, $ ) {
+	'use strict';
+	/**
+	 * Displays the link which loads wbclient.LinkItem
+	 *
+	 */
+	$( document ).ready( function() {
+		$( '#wbc-linkToItem' )
+			.empty()
+			.append(
+				$( '<a>' )
+				.attr( {
+					href: '#',
+					id: 'wbc-linkToItem-link'
+				} )
+				.text( mw.msg( 'wikibase-linkitem-addlinks' ) )
+				.click( function( event ) {
+					var $spinner = $.createSpinner(),
+						$linkItemLink = $( this );
+
+					$linkItemLink
+						.hide()
+						.after( $spinner );
+
+					event.preventDefault();
+
+					mw.loader.using(
+						'jquery.wikibase.linkitem',
+						function() {
+							$spinner.remove();
+							$linkItemLink.show().linkitem();
+						},
+						function() {
+							// Failure: This isn't very likely, but who knows
+							$spinner.remove();
+							$linkItemLink.show();
+							mw.notify( mw.msg( 'unknown-error' ) );
+						}
+					);
+				} )
+			);
+		$( '#p-lang' ).show();
+	} );
+} )( mediaWiki, jQuery );
