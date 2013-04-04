2671b54c6174c704d77b054f77c64dbb5d3c251c
jeroendedauw <jeroendedauw@gmail.com>
2013-04-04 05:00:54 +0200

    Cleanup of DataType related formatting code
    Fixed a bunch of broken code, removed some not used code, rewrote some messy code and added a lot of tests
    
    * Moved code from EntityIdFormatter to EntityIdLabelFormatter
    * Added new EntityIdFormatter that just formats an EntityId (complimentary with EntityIdParser), plus associated tests
    * Deleted unused and broken SnakFormatter
    * Deleted pointless, badly written and non-tested StringFormatter
    * Deleted pointless and badly written WikibaseFormatterFactory
    * Added tests for existing EntityId formatting code
    * Added tests for existing PropertyParserFunction code
    * Added top level factory in client to improve dependency management
    * Added new PropertyDataTypeLookup interface plus both mock and EntityLoader using implementations, plus tests
    * Added new SnaksFormatter class replacing broken code in PropertyParserFunction
    * Fixed various documentation issues
    * Did misc cleanup work in modified code
    
    Change-Id: I0067e1cba4141b7612c7d9d5bcb18352dd812984

---
+++ lib/tests/phpunit/SnakFormatterTest.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace Wikibase\Lib\Test;
+
+use DataTypes\DataType;
+use DataTypes\DataTypeFactory;
+use DataValues\StringValue;
+use ValueFormatters\FormatterOptions;
+use ValueParsers\ParserOptions;
+use Wikibase\EntityId;
+use Wikibase\Item;
+use Wikibase\Lib\EntityIdFormatter;
+use Wikibase\Lib\EntityIdParser;
+use Wikibase\Lib\InMemoryDataTypeLookup;
+use Wikibase\Lib\SnakFormatter;
+use Wikibase\Lib\TypedValueFormatter;
+use Wikibase\Property;
+use Wikibase\PropertyValueSnak;
+use Wikibase\Test\MockRepository;
+
+/**
+ * Tests for the Wikibase\Lib\SnakFormatter class.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @since 0.4
+ *
+ * @file
+ * @ingroup WikibaseLib
+ * @ingroup Test
+ *
+ * @group WikibaseLib
+ * @group SnakFormatterTest
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class SnakFormatterTest extends \PHPUnit_Framework_TestCase {
+
+	private $stringPropertyId = 572106;
+
+	private $itemPropertyId = 1730;
+
+	private function newPropertyDataTypeLookup() {
+		$lookup = new InMemoryDataTypeLookup();
+
+		$lookup->setDataTypeForProperty(
+			new EntityId( Property::ENTITY_TYPE, $this->stringPropertyId ),
+			'string-datatype'
+		);
+
+		$lookup->setDataTypeForProperty(
+			new EntityId( Property::ENTITY_TYPE, $this->itemPropertyId ),
+			'wikibase-item-datatype'
+		);
+
+		return $lookup;
+	}
+
+	private function newFormatter() {
+		return new SnakFormatter(
+			$this->newPropertyDataTypeLookup(),
+			new TypedValueFormatter(),
+			DataTypeFactory::newFromTypes( array(
+				$this->newStringDataType(),
+				$this->newItemDataType()
+		 	) )
+		);
+	}
+
+	private function newStringDataType() {
+		return new DataType(
+			'string-datatype',
+			'string',
+			array(),
+			array(),
+			array()
+		);
+	}
+
+	private function newItemDataType() {
+		$formatterOptions = new FormatterOptions( array(
+			EntityIdParser::OPT_PREFIX_MAP => array(
+				Item::ENTITY_TYPE => 'q'
+			)
+		) );
+
+		$parserOptions = new ParserOptions( array(
+			EntityIdFormatter::OPT_PREFIX_MAP => array(
+				'q' => Item::ENTITY_TYPE
+			)
+		) );
+
+		return new DataType(
+			'wikibase-item-datatype',
+			'wikibase-entityid',
+			array( new EntityIdParser( $parserOptions ) ),
+			array( new EntityIdFormatter( $formatterOptions ) ),
+			array()
+		);
+	}
+
+	public function testFormatNoSnaks() {
+		$formatted = $this->newFormatter()->formatSnaks( array() );
+
+		$this->assertInternalType( 'array', $formatted );
+		$this->assertEmpty( $formatted );
+	}
+
+	private function getStrings() {
+		return array(
+			'',
+			'foo BAR baz!  ',
+			'~=[,,_,,]:3'
+		);
+	}
+
+	public function stringProvider() {
+		$argLists = array();
+
+		foreach ( $this->getStrings() as $string ) {
+			$argLists[] = array( $string );
+		}
+
+		return $argLists;
+	}
+
+	/**
+	 * @dataProvider stringProvider
+	 */
+	public function testFormatOnePropertyValueSnakWithString( $expected ) {
+		$propertyValueSnak = new PropertyValueSnak(
+			$this->stringPropertyId,
+			new StringValue( $expected )
+		);
+
+		$formatted = $this->newFormatter()->formatSnaks( array( $propertyValueSnak ) );
+		$this->assertFormatSnaksReturnType( $formatted );
+		$this->assertCount( 1, $formatted );
+
+		$actual = $formatted[0];
+		$this->assertEquals( $expected, $actual );
+	}
+
+	private function assertFormatSnaksReturnType( $returnValue ) {
+		$this->assertInternalType( 'array', $returnValue );
+		$this->assertContainsOnly( 'string', $returnValue );
+	}
+
+	public function testFormatMultipleValues() {
+		$propertyValueSnaks = array();
+
+		foreach ( $this->getStrings() as $string ) {
+			$propertyValueSnaks[] = new PropertyValueSnak(
+				$this->stringPropertyId,
+				new StringValue( $string )
+			);
+		}
+
+		$propertyValueSnaks[] = new PropertyValueSnak(
+			$this->itemPropertyId,
+			new EntityId( Item::ENTITY_TYPE, 1337 )
+		);
+
+		$formatted = $this->newFormatter()->formatSnaks( $propertyValueSnaks );
+		$this->assertFormatSnaksReturnType( $formatted );
+
+		$expected = array_merge(
+			$this->getStrings(),
+			array( 'q1337' )
+		);
+
+		$this->assertSameSize( $expected, $formatted );
+		$this->assertEquals( $expected, $formatted );
+	}
+
+}
