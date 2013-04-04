ed27901c015eb1797dd0e7af5a4cff5113bf53c1
jeroendedauw <jeroendedauw@gmail.com>
2013-04-04 05:00:54 +0200

    Cleanup of DataType related formatting code
    Fixed a bunch of broken code, removed some not used code, rewrote some messy code and added a lot of tests
    
    * Moved code from EntityIdFormatter to EntityIdLabelFormatter
    * Added new EntityIdFormatter that just formats an EntityId (complimentary with EntityIdParser), plus associated tests
    * Deleted unused and broken SnakFormatter
    * Deleted pointless, badly written and non-tested StringFormatter
    * Deleted pointless and badly written WikibaseFormatterFactory
    * Added tests for existing EntityId formatting code
    * Added tests for existing PropertyParserFunction code
    * Added top level factory in client to improve dependency management
    * Added new PropertyDataTypeLookup interface plus both mock and EntityLoader using implementations, plus tests
    * Added new SnaksFormatter class replacing broken code in PropertyParserFunction
    * Fixed various documentation issues
    * Did misc cleanup work in modified code
    
    Change-Id: I0067e1cba4141b7612c7d9d5bcb18352dd812984

---
+++ lib/tests/phpunit/formatters/EntityIdFormatterTest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Wikibase\Test;
+
+use ValueFormatters\FormatterOptions;
+use Wikibase\EntityId;
+use Wikibase\Lib\EntityIdFormatter;
+use Wikibase\Lib\EntityIdLabelFormatter;
+use Wikibase\Item;
+use Wikibase\Property;
+
+/**
+ * Unit tests for the Wikibase\Lib\EntityIdFormatter class.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @since 0.4
+ *
+ * @ingroup WikibaseLibTest
+ *
+ * @group ValueFormatters
+ * @group DataValueExtensions
+ * @group WikibaseLib
+ * @group EntityIdFormatterTest
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class EntityIdFormatterTest extends \PHPUnit_Framework_TestCase {
+
+	protected function newEntityIdFormatter() {
+		$options = new FormatterOptions();
+		$options->setOption(
+			EntityIdFormatter::OPT_PREFIX_MAP,
+			array(
+				 Item::ENTITY_TYPE => 'I',
+				 Property::ENTITY_TYPE => 'PropERTY',
+			)
+		);
+
+		return new EntityIdFormatter( $options );
+	}
+
+	/**
+	 * @since 0.4
+	 *
+	 * @return array
+	 */
+	public function validProvider() {
+		$argLists = array();
+
+		$argLists[] = array( new EntityId( Item::ENTITY_TYPE, 42 ), 'I42' );
+		$argLists[] = array( new EntityId( Item::ENTITY_TYPE, 9001 ), 'I9001' );
+		$argLists[] = array( new EntityId( Item::ENTITY_TYPE, 1 ), 'I1' );
+
+		$argLists[] = array( new EntityId( Property::ENTITY_TYPE, 42 ), 'PropERTY42' );
+		$argLists[] = array( new EntityId( Property::ENTITY_TYPE, 9001 ), 'PropERTY9001' );
+		$argLists[] = array( new EntityId( Property::ENTITY_TYPE, 1 ), 'PropERTY1' );
+
+		return $argLists;
+	}
+
+	/**
+	 * @dataProvider validProvider
+	 *
+	 * @param EntityId $entityId
+	 * @param string $expectedString
+	 */
+	public function testParseWithValidArguments( EntityId $entityId, $expectedString ) {
+		$formatter = $this->newEntityIdFormatter();
+
+		$formattingResult = $formatter->format( $entityId );
+		$this->assertInstanceOf( 'ValueFormatters\Result', $formattingResult );
+		$this->assertTrue( $formattingResult->isValid(), 'Formatting result should be valid' );
+
+		$this->assertInternalType( 'string', $formattingResult->getValue() );
+		$this->assertEquals( $expectedString, $formattingResult->getValue() );
+	}
+
+}
