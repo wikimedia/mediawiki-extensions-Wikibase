c856fed3a8879d22d46934a7ac21823fddee624f
jeroendedauw <jeroendedauw@gmail.com>
2013-04-04 05:00:54 +0200

    Cleanup of DataType related formatting code
    Fixed a bunch of broken code, removed some not used code, rewrote some messy code and added a lot of tests
    
    * Moved code from EntityIdFormatter to EntityIdLabelFormatter
    * Added new EntityIdFormatter that just formats an EntityId (complimentary with EntityIdParser), plus associated tests
    * Deleted unused and broken SnakFormatter
    * Deleted pointless, badly written and non-tested StringFormatter
    * Deleted pointless and badly written WikibaseFormatterFactory
    * Added tests for existing EntityId formatting code
    * Added tests for existing PropertyParserFunction code
    * Added top level factory in client to improve dependency management
    * Added new PropertyDataTypeLookup interface plus both mock and EntityLoader using implementations, plus tests
    * Added new SnaksFormatter class replacing broken code in PropertyParserFunction
    * Fixed various documentation issues
    * Did misc cleanup work in modified code
    
    Change-Id: I0067e1cba4141b7612c7d9d5bcb18352dd812984

---
+++ lib/includes/EntityRetrievingDataTypeLookup.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Wikibase\Lib;
+
+use DataTypes\DataType;
+use InvalidArgumentException;
+use Wikibase\EntityId;
+use Wikibase\EntityLookup;
+use Wikibase\Property;
+
+/**
+ * PropertyDataTypeLookup that uses an EntityLookup to find the
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @since 0.4
+ *
+ * @file
+ * @ingroup WikibaseLib
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class EntityRetrievingDataTypeLookup implements PropertyDataTypeLookup {
+
+	/**
+	 * @var EntityLookup
+	 */
+	private $entityLookup;
+
+	public function __construct( EntityLookup $entityLookup ) {
+		$this->entityLookup = $entityLookup;
+	}
+
+	/**
+	 * @since 0.4
+	 *
+	 * @param EntityId $propertyId
+	 *
+	 * @return string
+	 */
+	public function getDataTypeIdForProperty( EntityId $propertyId ) {
+		$this->verifyIdIsOfAProperty( $propertyId );
+		return $this->getProperty( $propertyId )->getDataTypeId();
+	}
+
+	private function verifyIdIsOfAProperty( EntityId $propertyId ) {
+		if ( $propertyId->getEntityType() !== Property::ENTITY_TYPE ) {
+			throw new InvalidArgumentException( '$propertyId with non-property entity type provided' );
+		}
+	}
+
+	/**
+	 * @param EntityId $propertyId
+	 *
+	 * @return Property
+	 * @throws PropertyNotFoundExtension
+	 */
+	private function getProperty( EntityId $propertyId ) {
+		$property = $this->entityLookup->getEntity( $propertyId );
+
+		if ( $property === null ) {
+			throw new PropertyNotFoundExtension();
+		}
+
+		assert( $property instanceof Property );
+		return $property;
+	}
+
+}
+
+class PropertyNotFoundExtension extends \RuntimeException {
+
+
+
+}
