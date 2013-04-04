fe8f24f7893e6dcee59bd26a12b447a0395742c6
jeroendedauw <jeroendedauw@gmail.com>
2013-04-04 05:00:54 +0200

    Cleanup of DataType related formatting code
    Fixed a bunch of broken code, removed some not used code, rewrote some messy code and added a lot of tests
    
    * Moved code from EntityIdFormatter to EntityIdLabelFormatter
    * Added new EntityIdFormatter that just formats an EntityId (complimentary with EntityIdParser), plus associated tests
    * Deleted unused and broken SnakFormatter
    * Deleted pointless, badly written and non-tested StringFormatter
    * Deleted pointless and badly written WikibaseFormatterFactory
    * Added tests for existing EntityId formatting code
    * Added tests for existing PropertyParserFunction code
    * Added top level factory in client to improve dependency management
    * Added new PropertyDataTypeLookup interface plus both mock and EntityLoader using implementations, plus tests
    * Added new SnaksFormatter class replacing broken code in PropertyParserFunction
    * Fixed various documentation issues
    * Did misc cleanup work in modified code
    
    Change-Id: I0067e1cba4141b7612c7d9d5bcb18352dd812984

---
+++ lib/includes/TypedValueFormatter.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Wikibase\Lib;
+
+use DataTypes\DataType;
+use DataValues\DataValue;
+use ValueFormatters\ValueFormatter;
+
+/**
+ * Provides a string representation for a DataValue given its associated DataType.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @since 0.4
+ *
+ * @file
+ * @ingroup WikibaseLib
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class TypedValueFormatter {
+
+	public function formatToString( DataValue $dataValue, DataType $dataType ) {
+		// TODO: update this code to obtain the string formatter as soon as corresponding changes
+		// in the DataTypes library have been made.
+
+		$valueFormatters = $dataType->getFormatters();
+		$valueFormatter = reset( $valueFormatters );
+
+		if ( $valueFormatter === false ) {
+			$value = $dataValue->getValue();
+
+			if ( is_string( $value ) ) {
+				return $value;
+			}
+
+			// TODO: implement: error message or other error handling
+			return 'ERROR: TODO: getValue returned non-string, so DataType is missing formatter';
+		}
+
+		/**
+		 * @var ValueFormatter $valueFormatter
+		 */
+		$formattingResult = $valueFormatter->format( $dataValue );
+
+		if ( $formattingResult->isValid() ) {
+			return $formattingResult->getValue();
+		}
+
+		// TODO: implement: error message or other error handling
+		return 'ERROR: TODO: formatting failed';
+	}
+
+}
