77edb67dfeb11937eb8975c13ec8a27083469087
Marius Hoch <hoo@online.de>
2013-04-08 21:29:04 +0200

    Refactor wikibase.store - introduce AbstractedRepoApi
    * Remove the useless wikibase.EntityStore.js
    * Create a new module wikibase.RepoApi which holds
      basic API functions (useful for the client
      as we have way less dependencies)
    * Create wikibase.AbstractedRepoApi which extends
      the above with abstraction features (useful for
      the repo)
    * Added missing dependencies for wb.utilities
      while I was on it
    
    Change-Id: If0f4ba3a3b4ff28330860ab308e71c8afaca6a6b

--- lib/resources/Resources.php
+++ lib/resources/Resources.php
@@ -126,16 +126,9 @@ return call_user_func( function() {
 			'scripts' => array(
 				'wikibase.store/store.js',
 				'wikibase.store/store.FetchedContent.js',
-				'wikibase.store/wikibase.EntityStore.js', // TODO: get rid of this
-				'wikibase.store/wikibase.RepoApi.js', // TODO: move
 			),
 			'dependencies' => array(
-				'jquery.json',
-				'user.tokens',
-				'wikibase.datamodel',
-				'wikibase.serialization.entities',
-				'wikibase.repoAccess',
-				'wikibase.RepoApiError',
+				'wikibase',
 				'mediawiki.Title',
 			)
 		),
@@ -150,9 +143,33 @@ return call_user_func( function() {
 			)
 		),
 
+		'wikibase.AbstractedRepoApi' => $moduleTemplate + array(
+			'scripts' => array(
+				'wikibase.RepoApi/wikibase.AbstractedRepoApi.js',
+			),
+			'dependencies' => array(
+				'wikibase.datamodel',
+				'wikibase.serialization.entities',
+				'wikibase.RepoApi',
+				'wikibase.utilities',
+			)
+		),
+
+		'wikibase.RepoApi' => $moduleTemplate + array(
+			'scripts' => array(
+				'wikibase.RepoApi/wikibase.RepoApi.js',
+			),
+			'dependencies' => array(
+				'jquery.json',
+				'user.tokens',
+				'wikibase.repoAccess',
+				'wikibase',
+			)
+		),
+
 		'wikibase.RepoApiError' => $moduleTemplate + array(
 			'scripts' => array(
-				'wikibase.store/wikibase.RepoApiError.js',
+				'wikibase.RepoApi/wikibase.RepoApiError.js',
 			),
 			'messages' => array(
 				'wikibase-error-unexpected',
@@ -186,6 +203,8 @@ return call_user_func( function() {
 				'wikibase.utilities/wikibase.utilities.ui.css',
 			),
 			'dependencies' => array(
+				'dataValues',
+				'dataValues.util',
 				'wikibase',
 				'jquery.tipsy',
 				'mediawiki.language',
@@ -345,7 +364,7 @@ return call_user_func( function() {
 				'wikibase.utilities',
 				'wikibase.utilities.jQuery',
 				'wikibase.utilities.jQuery.ui.tagadata',
-				'wikibase.store',
+				'wikibase.AbstractedRepoApi',
 			),
 			'messages' => array(
 				'wikibase-cancel',
@@ -524,6 +543,7 @@ return call_user_func( function() {
 				'wikibase.datamodel',
 				'mw.ext.valueView',
 				'jquery.valueview.experts.wikibase',
+				'wikibase.AbstractedRepoApi',
 				'wikibase.store', // required for getting datatype from entityselector selected property
 				'mediawiki.legacy.shared',
 				'jquery.ui.TemplatedWidget'
@@ -549,6 +569,7 @@ return call_user_func( function() {
 				'jquery.wikibase/jquery.wikibase.claimview.js'
 			),
 			'dependencies' => array(
+				'wikibase.AbstractedRepoApi',
 				'jquery.wikibase.snakview'
 			),
 			'messages' => array(
@@ -565,6 +586,7 @@ return call_user_func( function() {
 			'dependencies' => array(
 				'jquery.wikibase.snaklistview',
 				'jquery.wikibase.toolbarcontroller',
+				'wikibase.AbstractedRepoApi'
 			)
 		),
 
@@ -577,6 +599,7 @@ return call_user_func( function() {
 				'jquery.wikibase.listview',
 				'jquery.wikibase.referenceview',
 				'jquery.wikibase.toolbarcontroller',
+				'wikibase.AbstractedRepoApi',
 				'wikibase.utilities',
 			),
 			'messages' => array(
