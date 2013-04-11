3a9fd10ddcebf3c214982523e5483f0c61d539db
jeroendedauw <jeroendedauw@gmail.com>
2013-04-11 19:50:29 +0200

    Removing me as author since this code changed into something I do not want to be associated with
    Change-Id: Ide0ea995587d8613ca3e4dabeae26368de5f6e0c

--- client/includes/ChangeHandler.php
+++ client/includes/ChangeHandler.php
@@ -28,7 +28,6 @@ namespace Wikibase;
  * @ingroup WikibaseLib
  *
  * @licence GNU GPL v2+
- * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  * @author Daniel Kinzler
  *
  * @fixme: ChangeNotification, ChangeHandler, ClientHooks::onWikibasePollHandle
