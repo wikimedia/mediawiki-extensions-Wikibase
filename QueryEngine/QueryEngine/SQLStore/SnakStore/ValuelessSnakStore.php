d496b488257a2d5db5ecb8d61c89e3ecc6a100d7
jeroendedauw <jeroendedauw@gmail.com>
2013-04-15 16:57:39 +0200

    Added internal snak type restriction to ValuelessSnakStore
    Change-Id: Ic4b41aeef3901a2ae62941648e125f32f7394e56

--- QueryEngine/QueryEngine/SQLStore/SnakStore/ValuelessSnakStore.php
+++ QueryEngine/QueryEngine/SQLStore/SnakStore/ValuelessSnakStore.php
@@ -34,21 +34,29 @@ class ValuelessSnakStore extends SnakStore {
 
 	protected $queryInterface;
 	protected $tableName;
+	protected $internalSnakType;
 
-	public function __construct( QueryInterface $queryInterface, $tableName ) {
+	public function __construct( QueryInterface $queryInterface, $tableName, $internalSnakType ) {
 		$this->queryInterface = $queryInterface;
 		$this->tableName = $tableName;
+		$this->internalSnakType = $internalSnakType;
 	}
 
 	public function canStore( SnakRow $snakRow ) {
-		return $snakRow instanceof ValuelessSnakRow;
+		return $snakRow instanceof ValuelessSnakRow
+			&& $snakRow->getInternalSnakType() === $this->internalSnakType;
 	}
 
 	public function storeSnakRow( SnakRow $snakRow ) {
-		if ( !( $snakRow instanceof ValuelessSnakRow ) ) {
-			throw new InvalidArgumentException( 'Can only store ValuelessSnakRow in ValuelessSnakStore' );
+		if ( !$this->canStore( $snakRow ) ) {
+			throw new InvalidArgumentException(
+				"Can only store ValuelessSnakRow with internal snak type '$this->internalSnakType' in ValuelessSnakStore"
+			);
 		}
 
+		/**
+		 * @var ValuelessSnakRow $snakRow
+		 */
 		$this->queryInterface->insert(
 			$this->tableName,
 			array(
