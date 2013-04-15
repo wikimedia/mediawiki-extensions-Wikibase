dd4d553c0fde3d905ed606b9eb2dd5139044ad79
daniel <daniel.kinzler@wikimedia.de>
2013-04-15 15:23:52 +0200

    (Bug 47288) Abstraction layer for usage tracking.
    This introduces the EntityUsageIndex for tracking which client
     pages used which entity.
    
    Change-Id: I50dbac5760ec5cb331d3372545f1eac4e53a19bd

--- lib/WikibaseLib.classes.php
+++ lib/WikibaseLib.classes.php
@@ -116,6 +116,7 @@ return call_user_func( function() {
 		'Wikibase\ChunkAccess' => 'includes/store/ChunkAccess.php',
 		'Wikibase\EntityLookup' => 'includes/store/EntityLookup.php',
 		'Wikibase\PropertyLabelResolver' => 'includes/store/PropertyLabelResolver.php',
+		'Wikibase\EntityUsageIndex' => 'includes/store/EntityUsageIndex.php',
 		'Wikibase\SiteLinkCache' => 'includes/store/SiteLinkCache.php',
 		'Wikibase\SiteLinkLookup' => 'includes/store/SiteLinkLookup.php',
 		'Wikibase\TermIndex' => 'includes/store/TermIndex.php',
--- lib/WikibaseLib.hooks.php
+++ lib/WikibaseLib.hooks.php
@@ -63,6 +63,7 @@ final class LibHooks {
 			'store/SiteLinkTable',
 			'store/WikiPageEntityLookup',
 			'store/CachingEntityLoader',
+			'store/EntityUsageIndex',
 
 			'store/TermPropertyLabelResolver',
 
