89d2ef60dc661fd1483ef7a2f25e3efdbf7df00d
jeroendedauw <jeroendedauw@gmail.com>
2013-04-26 17:29:27 +0200

    Another attempt to make the tests run on Jenkins
    Change-Id: I880af722a710bc4be92cedb456dd13623597fe7d

--- Wikibase.php
+++ Wikibase.php
@@ -18,8 +18,12 @@
 
 define( 'WB_EXPERIMENTAL_FEATURES', true );
 
+require_once __DIR__ . '/Query/ExampleSettings.php';
+
+require_once __DIR__ . '/DataModel/DataModel.php';
 require_once __DIR__ . '/lib/WikibaseLib.php';
 require_once __DIR__ . '/repo/Wikibase.php';
+require_once __DIR__ . '/Query/WikibaseQuery.php';
 //require_once __DIR__ . '/client/WikibaseClient.php';
 
 //require_once __DIR__ . '/client/ExampleSettings.php';
