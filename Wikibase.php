1c6197341239c85db132e436397c663205f163c6
Antoine Musso <hashar@free.fr>
2013-04-29 16:46:49 +0200

    Experimental config switcher for WMF Jenkins jobs
    Wikibase would need a client and a server job that would let tweak the
    default configuration settings.  I have created two Jenkins jobs that
    launch the full PHPUnit file and named:
    
    - mwext-Wikibase-client-tests
    - mwext-Wikibase-server-tests
    
    Jenkins provide the job name as the JOB_NAME environnement variable. One
    can thus detect which job is being run and tweak the PHPUnit setting.
    For the client job, we insert a `--exclude-group WikidataClient`.
    
    This also includes an evil hack to populate the sites table since
    there are some tests that require this
    
    Change-Id: Ide41a0b9003a9627f5a86c0514cc61d684b5e6aa

--- Wikibase.php
+++ Wikibase.php
@@ -15,7 +15,6 @@
  * @licence GNU GPL v2+
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
-
 define( 'WB_EXPERIMENTAL_FEATURES', true );
 
 require_once __DIR__ . '/Query/ExampleSettings.php';
@@ -24,7 +23,39 @@ require_once __DIR__ . '/DataModel/DataModel.php';
 require_once __DIR__ . '/lib/WikibaseLib.php';
 require_once __DIR__ . '/repo/Wikibase.php';
 require_once __DIR__ . '/Query/WikibaseQuery.php';
-//require_once __DIR__ . '/client/WikibaseClient.php';
 
-//require_once __DIR__ . '/client/ExampleSettings.php';
 require_once __DIR__ . '/repo/ExampleSettings.php';
+
+// Temporary hack that populates the sites table since there are some tests that require this to have happened
+require_once __DIR__ . '/lib/maintenance/populateSitesTable.php';
+$wgExtensionFunctions[] = function() {
+	$evilStuff = new PopulateSitesTable();
+	$evilStuff->execute();
+};
+
+# Let JenkinsAdapt our test suite when run under Jenkins
+$jenkins_job_name = getenv( 'JOB_NAME' );
+if( PHP_SAPI === 'cli' && $jenkins_job_name !== false ) {
+
+	switch( $jenkins_job_name) {
+
+	case 'mwext-Wikibase-client-tests':
+	break;
+
+	case 'mwext-Wikibase-server-tests':
+		# Pretends we asked PHPUnit to exclude WikidataClient group,
+		# this is done by inserting an --exclude-group option just after the
+		# command line.
+		$_SERVER['argv'] = array_merge(
+			$_SERVER['argv'],
+			array(
+				'--group', 'Diff,Ask,DataValueExtensions,Wikibase',
+				'--exclude-group', 'WikibaseClient',
+			)
+		);
+
+	break;
+	}
+}
+// Avoid polluting the global namespace
+unset( $jenkins_job_name, $cmd );
