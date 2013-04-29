d160f22d7e2453559557fb40f05c1a7da03b5652
Antoine Musso <hashar@free.fr>
2013-04-29 16:46:49 +0200

    experimental config switcher for WMF Jenkins jobs
    Wikibase would need a client and a server job that would let tweak the
    default configuration settings.  I have created two Jenkins jobs that
    launch the full PHPUnit file and named:
    
    - mwext-Wikibase-client-tests
    - mwext-Wikibase-server-tests
    
    Jenkins provide the job name as the JOB_NAME environnement variable. One
    can thus detect which job is being run and tweak the PHPUnit setting.
    For the client job, we insert a `--exclude-group WikidataClient`.
    
    Change-Id: Ide41a0b9003a9627f5a86c0514cc61d684b5e6aa

--- Wikibase.php
+++ Wikibase.php
@@ -15,7 +15,6 @@
  * @licence GNU GPL v2+
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
-
 define( 'WB_EXPERIMENTAL_FEATURES', true );
 
 require_once __DIR__ . '/Query/ExampleSettings.php';
@@ -28,3 +27,53 @@ require_once __DIR__ . '/Query/WikibaseQuery.php';
 
 //require_once __DIR__ . '/client/ExampleSettings.php';
 require_once __DIR__ . '/repo/ExampleSettings.php';
+
+
+// Temporary hack that populates the sites table since there are some tests that require this to have happened
+/*
+require_once __DIR__ . '/lib/maintenance/populateSitesTable.php';
+$wgExtensionFunctions[] = function() {
+	$evilStuff = new PopulateSitesTable();
+	$evilStuff->execute();
+};
+ */
+
+
+# Let JenkinsAdapt our test suite when run under Jenkins
+$jenkins_job_name = getenv( 'JOB_NAME' );
+if( PHP_SAPI === 'cli' && $jenkins_job_name !== false ) {
+
+	# FIXME: remove debugging statements
+	var_dump( $_SERVER['argv'] );
+
+	switch( $jenkins_job_name) {
+
+	case 'mwext-Wikibase-client-tests':
+	break;
+
+	case 'mwext-Wikibase-server-tests':
+		# Pretends we asked PHPUnit to exclude WikidataClient group,
+		# this is done by inserting an --exclude-group option just after the
+		# command line.
+
+		# Save up 'php' and the 'phpunit.php' part
+		$cmd[] = array_shift( $_SERVER['argv'] );
+
+		# Insert in as first parameters our specific requirements
+		$_SERVER['argv'] = array_merge(
+			$cmd,
+			array(
+				'--group', 'Diff,Ask,DataValueExtensions,Wikibase',
+				'--exclude-group', 'WikibaseClient',
+			),
+			$_SERVER['argv']
+		);
+
+		# FIXME: remove debugging statements
+		var_dump( $_SERVER['argv'] );
+		var_dump( 'FINAL COMMAND: ' . join($_SERVER['argv'], ' ' ) );
+	break;
+	}
+}
+// Avoid polluting the global namespace
+unset( $jenkins_job_name, $cmd );
