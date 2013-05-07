ec3a559e7c0d2b499158281c39487280bd2aeb86
Tobias Gritschacher <tobias.gritschacher@wikimedia.de>
2013-05-07 15:34:36 +0200

    Use cucumber for selenium tests (WIP, DNM)
    - this is the initial commit towards the use of cucumber
      for the selenium tests
    - it contains a minimum of setup code and one single testcase
    - old tests will be migrated over the time in several changesets
    - new tests should only be written using the new cucumber framework
    
    Bug: 48142
    Change-Id: I4dd7d8d868e623e8ee606d1bb3e19297b26eb374

---
+++ selenium_cuc/features/support/modules/alias_module.rb
@@ -0,0 +1,61 @@
+# -*- encoding : utf-8 -*-
+# Wikidata UI tests
+#
+# Author:: Tobias Gritschacher (tobias.gritschacher@wikimedia.de)
+# License:: GNU GPL v2+
+#
+# module for aliases page
+
+module AliasPage
+  include PageObject
+  # aliases UI elements
+  div(:aliasesDiv, :class => "wb-aliases")
+  span(:aliasesTitle, :class => "wb-aliases-label")
+  unordered_list(:aliasesList, :class => "wb-aliases-container")
+  link(:addAliases, :xpath => "//div[contains(@class, 'wb-aliases')]/span[contains(@class, 'wb-ui-propertyedittool-toolbar')]/span/a[text()='add']")
+  span(:addAliasesDisabled, :xpath => "//div[contains(@class, 'wb-aliases')]/span[contains(@class, 'wb-ui-propertyedittool-toolbar')]/span/span")
+  link(:editAliases, :xpath => "//div[contains(@class, 'wb-aliases')]/div/span[contains(@class, 'wb-ui-propertyedittool-editablevalue')]/span/span/span/span/a[text()='edit']")
+  link(:saveAliases, :xpath => "//div[contains(@class, 'wb-aliases')]/div/span[contains(@class, 'wb-ui-propertyedittool-editablevalue')]/span/span/span/span/a[text()='save']")
+  link(:cancelAliases, :xpath => "//div[contains(@class, 'wb-aliases')]/div/span[contains(@class, 'wb-ui-propertyedittool-editablevalue')]/span/span/span/span/a[text()='cancel']")
+  text_field(:aliasesInputFirst, :xpath => "//li[contains(@class, 'wb-aliases-alias')]/span/input")
+  link(:aliasesInputFirstRemove, :xpath => "//li[contains(@class, 'wb-aliases-alias')]/a[contains(@class, 'tagadata-close')]")
+  text_field(:aliasesInputEmpty, :xpath => "//li[contains(@class, 'tagadata-choice-empty')]/span/input")
+  text_field(:aliasesInputModified, :xpath => "//li[contains(@class, 'tagadata-choice-modified')]/span/input")
+  text_field(:aliasesInputEqual, :xpath => "//li[contains(@class, 'tagadata-choice-equal')]/span/input")
+  link(:aliasesInputRemove, :xpath => "//li[contains(@class, 'tagadata-choice-modified')]/a[contains(@class, 'tagadata-close')]")
+
+  # aliases methods
+  def count_existing_aliases
+    count = 0
+    if aliasesList? == false
+      return 0
+    end
+    aliasesList_element.each do |aliasElem|
+      count = count+1
+    end
+    count
+  end
+
+  def get_nth_alias n
+    count = 1
+    if aliasesList_element.exists?
+      aliasesList_element.each do |aliasElem|
+        if count == n
+          return aliasElem
+        end
+        count = count+1
+      end
+    end
+    false
+  end
+
+  def add_aliases(aliases)
+    addAliases
+    aliases.each do |ali|
+      self.aliasesInputEmpty= ali
+    end
+    saveAliases
+    ajax_wait
+    wait_for_api_callback
+  end
+end
---
+++ selenium_cuc/features/support/modules/sitelink_module.rb
@@ -0,0 +1,99 @@
+# -*- encoding : utf-8 -*-
+# Wikidata UI tests
+#
+# Author:: Tobias Gritschacher (tobias.gritschacher@wikimedia.de)
+# License:: GNU GPL v2+
+#
+# module for sitelinks page
+
+module SitelinkPage
+  include PageObject
+  # sitelinks UI elements
+  table(:sitelinksTable, :class => "wb-sitelinks")
+  element(:sitelinksTableColumnHeaders, :tr, :class => "wb-sitelinks-columnheaders")
+  element(:sitelinksHeaderLanguage, :th, :class => "wb-sitelinks-sitename")
+  element(:sitelinksHeaderCode, :th, :class => "wb-sitelinks-siteid")
+  element(:sitelinksHeaderLink, :th, :class => "wb-sitelinks-link")
+  element(:sitelinksTableBody, :tbody, :xpath => "//table[contains(@class, 'wb-sitelinks')]/tbody")
+  element(:sitelinksTableLanguage, :td, :index => 1)
+  link(:addSitelinkLink, :css => "table.wb-sitelinks > tfoot > tr > td > span.wb-ui-toolbar > span.wb-ui-toolbar-group > a.wb-ui-toolbar-button:nth-child(1)")
+  span(:siteLinkCounter, :class => "wb-ui-propertyedittool-counter")
+  text_field(:siteIdInputField, :xpath => "//table[contains(@class, 'wb-sitelinks')]/tfoot/tr/td[1]/input")
+  text_field(:pageInputField, :xpath => "//table[contains(@class, 'wb-sitelinks')]/tfoot/tr/td[contains(@class, 'wb-sitelinks-link')]/input")
+  text_field(:pageInputFieldExistingSiteLink, :xpath => "//table[contains(@class, 'wb-sitelinks')]/tbody/tr/td[contains(@class, 'wb-sitelinks-link')]/input")
+  span(:saveSitelinkLinkDisabled, :class => "wb-ui-toolbar-button-disabled")
+  unordered_list(:siteIdAutocompleteList, :class => "wikibase-siteselector-list")
+  #todo: this is not a nice way to get the suggestion list, we should find a better way
+  unordered_list(:pageAutocompleteList, :xpath => "//ul[@class='ui-autocomplete ui-menu ui-widget ui-widget-content ui-corner-all ui-suggester-list']")
+  unordered_list(:editSitelinkAutocompleteList, :xpath => "//ul[@class='ui-autocomplete ui-menu ui-widget ui-widget-content ui-corner-all ui-suggester-list']")
+  link(:saveSitelinkLink, :text => "save")
+  link(:cancelSitelinkLink, :text => "cancel")
+  link(:removeSitelinkLink, :text => "remove")
+  link(:editSitelinkLink, :xpath => "//td[contains(@class, 'wb-ui-propertyedittool-editablevalue-toolbarparent')]/span/span/span/a")
+  link(:englishEditSitelinkLink, :xpath => "//tr[contains(@class, 'wb-sitelinks-en')]/td[4]/span/span/span/a")
+  link(:pageArticleNormalized, :css => "td.wb-sitelinks-link-sr > a")
+  link(:germanSitelink, :xpath => "//td[contains(@class, 'wb-sitelinks-link-de')]/a")
+  link(:englishSitelink, :xpath => "//td[contains(@class, 'wb-sitelinks-link-en')]/a")
+  span(:articleTitle, :xpath => "//h1[@id='firstHeading']/span")
+  # sitelinks methods
+  def get_number_of_sitelinks_from_counter
+    wait_until do
+      siteLinkCounter?
+    end
+    scanned = siteLinkCounter.scan(/\(([^)]+)\)/)
+    integerValue = scanned[0][0].to_i()
+    return integerValue
+  end
+
+  def get_nth_element_in_autocomplete_list(list, n)
+    count = 0
+    list.each do |listItem|
+      count = count+1
+      if count == n
+        return listItem
+      end
+    end
+    return false
+  end
+
+  def get_text_from_sitelist_table(x, y)
+    return sitelinksTable_element[x][y].text
+  end
+
+  def count_existing_sitelinks
+    if sitelinksTableColumnHeaders? == false
+      return 0
+    end
+    count = 0
+    sitelinksTable_element.each do |tableRow|
+      count = count+1
+    end
+    return count - 2 # subtracting the table header row and the footer row
+  end
+
+  def add_sitelinks(sitelinks)
+    sitelinks.each do |sitelink|
+      addSitelinkLink
+      self.siteIdInputField= sitelink[0]
+      wait_until do
+        self.pageInputField_element.enabled?
+      end
+      self.pageInputField= sitelink[1]
+      saveSitelinkLink
+      ajax_wait
+      wait_for_api_callback
+    end
+  end
+
+  def remove_all_sitelinks
+    count = 0
+    number_of_sitelinks = get_number_of_sitelinks_from_counter
+    while count < (number_of_sitelinks)
+      editSitelinkLink
+      removeSitelinkLink
+      ajax_wait
+      wait_for_api_callback
+      count = count + 1
+    end
+  end
+end
