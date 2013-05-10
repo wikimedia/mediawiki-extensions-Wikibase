dfd6e0708f56e44265f39e3464fc165424427e5e
jeroendedauw <jeroendedauw@gmail.com>
2013-05-10 15:32:31 +0200

    Added PHPUnit config and boostrtap files
    Change-Id: I94f21c34e7a142a6f5376771750531f98e858659

---
+++ view/packages/wikibase-data-values/tests/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * PHPUnit test bootstrap file for the DataValues library.
+ *
+ * @since 0.1
+ *
+ * @file
+ * @ingroup DataValues
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+
+require_once( __DIR__ . '/../DataValues.php' );
