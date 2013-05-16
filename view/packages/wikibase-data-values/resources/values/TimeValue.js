df01323a02056330ecc8aa1289c56f36996b1694
Daniel Werner <daniel.werner@wikimedia.de>
2013-05-16 21:31:51 +0200

    [DataValues] TimeValue.newFromJSON fix
    Fix for Julian date being initialized with Gregorian iso8601 string in TimeValue.newFromJSON
    
    Change-Id: Ic43e1c56845a39324b020f9f4ef527ff22568ffb

--- view/packages/wikibase-data-values/resources/values/TimeValue.js
+++ view/packages/wikibase-data-values/resources/values/TimeValue.js
@@ -62,14 +62,7 @@
 			if ( !( value instanceof SELF ) ) {
 				return false;
 			}
-
-			var ownTime = this.getValue(),
-				otherTime = value.getValue();
-
-			// no need to check for isValid() since constructor won't allow invalid Time values
-
-			return ownTime.precision() === otherTime.precision()
-				&& ownTime.iso8601() === otherTime.iso8601();
+			return this.getValue().equals( value.getValue() );
 		},
 
 		/**
@@ -96,13 +89,41 @@
 	 * @see dv.DataValue.newFromJSON
 	 */
 	SELF.newFromJSON = function( json ) {
-		// TODO: not good to do it this way, there are some lost information, e.g. the calendar
-		//  model as well as before/after and UTC offset!
-		//  Could simply fix this by creating a second Time object where we use those infos as well
+		// TODO: not good to do it this way, there are some lost information, e.g. before/after and
+		//  UTC offset!
+		//  Could simply fix this by creating a second Time object where we use those info as well
 		//  as the first Time object's year(), month(), day() etc. The Time constructor currently
-		//  only takes a string for parsing though, which is very bad as well.
-		var time = Time.newFromIso8601( json.time, json.precision );
-		return new SELF( time );
+		//  only takes a string for parsing though which is very bad as well.
+		var gregorianTime = Time.newFromIso8601( json.time, json.precision ),
+			finalTime;
+
+		// TODO: The following steps dependent on the calendar model are ugly but consequent with
+		//  how time.Time currently represents all calendar models.
+
+		// NOTE: Having the calendar model as url to Wikidata was a specified data model requirement.
+
+		// Time instance should be defined as Gregorian:
+		if( json.calendarmodel === 'http://wikidata.org/id/Q1985727' ) {
+			finalTime = gregorianTime;
+		}
+		// Time instance should be defined as Julian:
+		else if( json.calendarmodel === 'http://wikidata.org/id/Q1985786' ) {
+			// Since the data value saves the time as Gregorian, we first have to transform that
+			// back into Julian.
+			// NOTE: As of May 15 2013 we decided that this is nonsense and that we should always
+			//  store the time in its native format dependent on the calendar model.
+			finalTime = new Time(
+				$.extend( gregorianTime.julian(), {
+					precision: gregorianTime.precision(),
+					calendarname: Time.CALENDAR.JULIAN
+				} )
+				// todo: consider other fields, e.g. 'before', 'after' and 'utc'
+			);
+		}
+		else {
+			throw new Error( 'Unknown calendar model "' + json.calendarmodel + '"' );
+		}
+		return new SELF( finalTime );
 	};
 
 	/**
