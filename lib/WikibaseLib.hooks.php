d6da4d0b1925d2621a40ceeb3164b1f1a58371ba
jeroendedauw <jeroendedauw@gmail.com>
2013-05-16 15:44:12 +0200

    Put PropertyNotFoundException into its own file and added PropertyId field
    Change-Id: Ie6dd4aa884aaf9be6c5ff7a4c721b9ec5d880e1a

--- lib/WikibaseLib.hooks.php
+++ lib/WikibaseLib.hooks.php
@@ -77,6 +77,7 @@ final class LibHooks {
 			'TemplateRegistry',
 			'LibHooks',
 			'MockRepository',
+			'PropertyNotFoundException',
 			'SettingsArray',
 			'SnakFormatter',
 
---
+++ lib/tests/phpunit/PropertyNotFoundExceptionTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Wikibase\Lib\Test;
+
+use Wikibase\EntityId;
+use Wikibase\Lib\PropertyNotFoundException;
+
+/**
+ * @covers Wikibase\Lib\PropertyNotFoundException class.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @since 0.1
+ *
+ * @ingroup WikibaseDatabaseTest
+ *
+ * @group Wikibase
+ * @group WikibaseLib
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class PropertyNotFoundExceptionTest extends \PHPUnit_Framework_TestCase {
+
+	public function testConstructorWithJustATable() {
+		$propertyId = new EntityId( 'item', 42 );
+
+		$exception = new PropertyNotFoundException( $propertyId );
+
+		$this->assertEquals( $propertyId, $exception->getPropertyId() );
+	}
+
+}
