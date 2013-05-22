b61b4fee698168dd079462fae9781275ffb8b40a
umherirrender <umherirrender_de.wp@web.de>
2013-05-22 21:34:54 +0200

    Use dbKey in query of Special:UnconnectedPage
    page_title has the titles stored with underscored, not spaces.
    
    Change-Id: I88947f6a33bf2f44b83759ed00deed45607778a0

--- client/includes/specials/SpecialUnconnectedPages.php
+++ client/includes/specials/SpecialUnconnectedPages.php
@@ -200,7 +200,7 @@ class SpecialUnconnectedPages extends SpecialWikibaseQueryPage {
 			$checker = $this->getNamespaceChecker();
 		}
 		if ( $title !== null ) {
-			$conds[] = 'page_title >= ' . $dbr->addQuotes( $title->getText() );
+			$conds[] = 'page_title >= ' . $dbr->addQuotes( $title->getDBkey() );
 			$conds[] = 'page_namespace = ' . $title->getNamespace();
 		}
 		$conds[] = 'page_namespace IN (' . implode(',', $checker->getWikibaseNamespaces() ) . ')';
