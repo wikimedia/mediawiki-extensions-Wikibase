b0fb9b090d2562bdf9f22aeecbecb69419e9e9ed
jeroendedauw <jeroendedauw@gmail.com>
2013-05-22 17:17:12 +0200

    Added evilMediaWikiBootstrap.php so the WikibaseDatabase test runner can also run the MW dependent tests
    Change-Id: I6ea07de3d42ba75ccd9a36f865b230f4e0d3efbb

--- Database/tests/bootstrap.php
+++ Database/tests/bootstrap.php
@@ -12,6 +12,10 @@
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
 
+if ( !in_array( '--testsuite=WikibaseDatabaseStandalone', $GLOBALS['argv'] ) ) {
+	require_once( __DIR__ . '/evilMediaWikiBootstrap.php' );
+}
+
 require_once( __DIR__ . '/../Database.php' );
 
 require_once( __DIR__ . '/testLoader.php' );
