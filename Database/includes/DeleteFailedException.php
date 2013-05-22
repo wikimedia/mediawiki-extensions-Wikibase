fa81460efd115673f96f426925aca117bfd240dc
jeroendedauw <jeroendedauw@gmail.com>
2013-05-22 16:08:09 +0200

    Added DeleteFailedException
    Change-Id: Ib70c85b202c1db9c8735baa94f9eb373c4b022f3

---
+++ Database/includes/DeleteFailedException.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Wikibase\Database;
+
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @since 0.1
+ *
+ * @file
+ * @ingroup WikibaseDatabase
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class DeleteFailedException extends QueryInterfaceException {
+
+	protected $tableName;
+	protected $conditions;
+
+	public function __construct( $tableName, array $conditions, $message = '', \Exception $previous = null ) {
+		parent::__construct( $message, 0, $previous );
+
+		$this->tableName = $tableName;
+		$this->conditions = $conditions;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getTableName() {
+		return $this->tableName;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getConditions() {
+		return $this->conditions;
+	}
+
+}
