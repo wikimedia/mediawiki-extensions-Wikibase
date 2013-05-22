d51b1f6061a4eb168c1a86a03cb82ec47ef62d68
jeroendedauw <jeroendedauw@gmail.com>
2013-05-22 17:17:12 +0200

    Y U NO DECENT LOAD MEACHANISM? [DO NOT MERGE]
    Change-Id: I6ea07de3d42ba75ccd9a36f865b230f4e0d3efbb

--- Database/tests/bootstrap.php
+++ Database/tests/bootstrap.php
@@ -12,6 +12,24 @@
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
 
+$IP = getenv( 'MW_INSTALL_PATH' );
+
+if ( $IP === false ) {
+	$IP = dirname( __FILE__ ) . '/../../../..';
+}
+
+require_once( $IP . '/maintenance/Maintenance.php' );
+
+class WhyYouNoHasDecentLoadingMechanism extends Maintenance {
+	public function execute() {}
+}
+
+$maintClass = 'WhyYouNoHasDecentLoadingMechanism';
+require_once( DO_MAINTENANCE );
+
+require_once( $IP . '/includes/AutoLoader.php' );
+
+
 require_once( __DIR__ . '/../Database.php' );
 
 require_once( __DIR__ . '/testLoader.php' );
