586f32c34aee3fde2eb8fd03b3b884796cec324a
Daniel Werner <daniel.werner@wikimedia.de>
2013-05-31 15:59:29 +0200

    (bug 49014) Proper formatting of values in EntityView
    Using the ValueFormatterFactory to build formatters for displaying certain values. This will allow
    for displaying the coordinates value. If there is no formatter implemented, the raw value will be
    displayed if it is represented as a plain string. If not, then an exception will be thrown.
    
    The formatters should probably be injected via the constructor, this will be done in a follow-up.
    Also the function is rather complex now (even more than before), this will also be addressed in
    a follow-up.
    
    Change-Id: Ief836131ac79f489090bd4a5e0f7fee84e13bd3e

--- repo/includes/EntityView.php
+++ repo/includes/EntityView.php
@@ -447,7 +447,7 @@ abstract class EntityView extends \ContextSource {
 	 * @return string
 	 */
 	public function getHtmlForClaims( EntityContent $entity, Language $lang = null, $editable = true ) {
-		global $wgLang;
+		global $wgLang, $wgValueFormatters;
 
 		wfProfileIn( __METHOD__ );
 
@@ -486,17 +486,48 @@ abstract class EntityView extends \ContextSource {
 				);
 			}
 
+			// TODO: inject value formatters into constructor instead
+			$valueFormatters = new \ValueFormatters\ValueFormatterFactory( $wgValueFormatters );
+
+			$entitiesPrefixMap = array();
+			foreach ( Settings::get( 'entityPrefixes' ) as $prefix => $entityType ) {
+				$entitiesPrefixMap[ $entityType ] = $prefix;
+			}
+			$valueFormatterOptions = new \ValueFormatters\FormatterOptions( array(
+				\ValueFormatters\ValueFormatter::OPT_LANG => $languageCode,
+				Lib\EntityIdFormatter::OPT_PREFIX_MAP => $entitiesPrefixMap
+			) );
+
 			$i = 0;
 			foreach( $claims as $claim ) {
 				// TODO: display a "placeholder" message for novalue/somevalue snak
 				$value = '';
 				if ( $claim->getMainSnak()->getType() === 'value' ) {
-					$value = $claim->getMainSnak()->getDataValue()->getValue();
-				}
+					$value = $claim->getMainSnak()->getDataValue();
 
-				// TODO: bad to have a switch for different data types here
-				if( $value instanceof \DataValues\TimeValue ) {
-					$value = $value->getTime() . ' (' . $value->getCalendarModel() . ')';
+					// TODO: Bad to have a switch for different data types here, implement a formatter!
+					if( $value instanceof \DataValues\TimeValue ) {
+						$value = $value->getTime() . ' (' . $value->getCalendarModel() . ')';
+					} else {
+						// Proper way, use value formatter:
+						$valueFormatter = $valueFormatters->newFormatter(
+							$value->getType(), $valueFormatterOptions
+						);
+
+						if( $valueFormatter !== null ) {
+							$value = $valueFormatter->format( $value );
+						} else {
+							// If value representation is a string, just display that one as a
+							// fallback for values not having a formatter implemented yet.
+							$value = $value->getValue();
+
+							if( !is_string( $value ) ) {
+								// TODO: don't fail here, display a message in the UI instead
+								throw new MWException( 'Displaying of values of type "' + $value->getType()
+									+ '" not supported yet' );
+							}
+						}
+					}
 				}
 
 				$additionalCssClasses = '';
