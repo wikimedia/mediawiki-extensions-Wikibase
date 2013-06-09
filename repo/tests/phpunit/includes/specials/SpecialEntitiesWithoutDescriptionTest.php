1b6643d659dda45603bc42502e1ad327576a4315
Bene <benestar.wikimedia@googlemail.com>
2013-06-09 18:24:56 +0200

    Special page for entities without description
    New base class SpecialEntitiesWithoutPage and subpages
    SpecialEntitiesWithoutLabel and SpecialEntitiesWithoutDescription
    
    Patch Set 5: Created tests for special pages
    
    Bug: 42102
    Change-Id: I16570b0844653c9e8a2fb1e75c2efa72b586d5d5

---
+++ repo/tests/phpunit/includes/specials/SpecialEntitiesWithoutDescriptionTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Wikibase\Test;
+
+/**
+ * Tests for the SpecialEntitiesWithoutDescription class.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @since 0.1
+ *
+ * @ingroup WikibaseRepoTest
+ * @ingroup Test
+ *
+ * @group Wikibase
+ * @group SpecialPage
+ * @group WikibaseSpecialPage
+ *
+ * @licence GNU GPL v2+
+ * @author Bene* < benestar.wikimedia@googlemail.com >
+ */
+class SpecialEntitiesWithoutDescriptionTest extends SpecialPageTestBase {
+
+	protected function newSpecialPage() {
+		return new \SpecialEntitiesWithoutDescription();
+	}
+
+	public function testExecute() {
+		//TODO: Actually verify that the output is correct.
+		//      Currently this just tests that there is no fatal error,
+		//      and that the restriction handling is working and doesn't
+		//      block. That is, the default should let the user execute
+		//      the page.
+
+		//TODO: Verify that item creation works via a faux post request
+
+		list( $output, ) = $this->executeSpecialPage( '' );
+		$this->assertTrue( true, 'Calling execute without any subpage value' );
+
+		list( $output, ) = $this->executeSpecialPage( 'en' );
+		$this->assertTrue( true, 'Calling execute with a subpage value' ); //TODO: assert output
+	}
+
+}
