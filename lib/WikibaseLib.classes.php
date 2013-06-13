99438e088ef51666e2f1ee81d23ef2cb395cce05
jeroendedauw <jeroendedauw@gmail.com>
2013-06-13 16:20:35 +0200

    Move SiteLink to lib
    Change-Id: Ie89dbe1c5b0216fc5b747bee967ed1d79e7f924e

--- lib/WikibaseLib.classes.php
+++ lib/WikibaseLib.classes.php
@@ -54,6 +54,7 @@ return call_user_func( function() {
 		'Wikibase\Lib\PropertyNotFoundException' => 'includes/PropertyNotFoundException.php',
 		'Wikibase\Settings' => 'includes/Settings.php',
 		'Wikibase\SettingsArray' => 'includes/SettingsArray.php',
+		'Wikibase\SiteLink' => 'includes/SiteLink.php',
 		'Wikibase\Lib\SnakFormatter' => 'includes/SnakFormatter.php',
 		'Wikibase\Term' => 'includes/Term.php',
 		'Wikibase\Lib\TermsToClaimsTranslator' => 'includes/TermsToClaimsTranslator.php',
