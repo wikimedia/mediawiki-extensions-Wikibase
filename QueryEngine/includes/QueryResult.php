02fbb5d21008c28a3667ac6371d2594c45356a09
jeroendedauw <jeroendedauw@gmail.com>
2013-06-13 11:16:22 +0200

    Move the QueryEngine component to its own git repository
    Moved to git repository https://gerrit.wikimedia.org/r/p/mediawiki/extensions/WikibaseQueryEngine.git
    
    Change-Id: Id673349aa6091de39b2313b781c404e09b9ad867

--- QueryEngine/includes/QueryResult.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Wikibase\QueryEngine;
-
-class QueryResult {
-	// TODO: this is a temporary stub
-	// * some preliminary query result code in other git repo
-	// * still need to figure out where to put it
-}
--- repo/config/Wikibase.experimental.php
+++ repo/config/Wikibase.experimental.php
@@ -33,11 +33,6 @@ if ( !defined( 'WB_VERSION' ) || !defined( 'WB_EXPERIMENTAL_FEATURES' ) ) {
 	die( 'Not an entry point.' );
 }
 
-// Include the Query component if that hasn't been done yet.
-if ( !defined( 'WIKIBASE_QUERY_VERSION' ) && defined( 'WB_EXPERIMENTAL_FEATURES' ) ) {
-	@include_once( __DIR__ . '/../../QueryEngine/QueryEngine.php' );
-}
-
 $dir = __DIR__ . '/../';
 
 $wgAutoloadClasses['Wikibase\Api\SetStatementRank']		= $dir . 'includes/api/SetStatementRank.php';
