60f738b38b4f0736931aec9f0e829483d63d9be6
jeroendedauw <jeroendedauw@gmail.com>
2013-06-14 18:36:06 +0200

    Have Travis run the repo tests
    Change-Id: Ibce2946f045fa4321d2b634ff33a9b52dd3cac85

---
+++ .travis.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+set -x
+
+cd ..
+
+git clone https://gerrit.wikimedia.org/r/p/mediawiki/core.git phase3 --depth 1
+
+cd phase3
+
+mysql -e 'create database its_a_mw;'
+php maintenance/install.php --dbtype $DBTYPE --dbuser root --dbname its_a_mw --dbpath $(pwd) --pass nyan TravisWiki admin
+
+cd extensions
+git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/Diff.git
+git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/DataValues.git
+git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/Ask.git
+git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/WikibaseDataModel.git
+git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/Wikibase.git
+
+cd ..
+echo "define( 'WB_EXPERIMENTAL_FEATURES', true );" >> LocalSettings.php
+echo 'require_once( __DIR__ . "/extensions/Wikibase/repo/Wikibase.php" );' >> LocalSettings.php
+echo 'require_once( __DIR__ . "/extensions/Wikibase/repo/ExampleSettings.php" );' >> LocalSettings.php
+
+php /maintenance/update.php --quick
+
+cd extensions/Wikibase
+phpunit
