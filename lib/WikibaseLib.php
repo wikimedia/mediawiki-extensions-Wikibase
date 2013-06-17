634716b2b2180fd5cf1dd7856d59100e00ba03cc
jeroendedauw <jeroendedauw@gmail.com>
2013-06-17 15:30:39 +0200

    Remove global scope assumptions in lib and exec code in non global scope
    Change-Id: Ie506bafc661111932b15d3d3d4c03b3d3127806d

--- lib/WikibaseLib.php
+++ lib/WikibaseLib.php
@@ -31,7 +31,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	die( 'Not an entry point.' );
 }
 
-if ( version_compare( $wgVersion, '1.20c', '<' ) ) { // Needs to be 1.20c because version_compare() works in confusing ways.
+if ( version_compare( $GLOBALS['wgVersion'], '1.20c', '<' ) ) { // Needs to be 1.20c because version_compare() works in confusing ways.
 	die( '<b>Error:</b> WikibaseLib requires MediaWiki 1.20 or above.' );
 }
 
@@ -74,96 +74,97 @@ $dependencies = array(
 
 foreach ( $dependencies as $constant => $name ) {
 	if ( !defined( $constant ) ) {
-		die(
-			'<b>Error:</b> WikibaseLib depends on the <a href="https://www.mediawiki.org/wiki/Extension:'
-				. $name . '">' . $name . '</a> extension.'
-		);
+		throw new Exception( 'WikibaseLib depends on the ' . $name . ' extension.' );
 	}
 }
 
 unset( $dependencies );
 
+call_user_func( function() {
+	global $wgExtensionCredits, $wgAutoloadClasses, $wgExtensionMessagesFiles, $wgExtensionFunctions;
+	global $wgValueParsers, $wgDataValues, $wgJobClasses, $wgHooks, $wgResourceModules, $wgValueFormatters;
 
-$wgExtensionCredits['wikibase'][] = array(
-	'path' => __DIR__,
-	'name' => 'WikibaseLib',
-	'version' => WBL_VERSION,
-	'author' => array(
-		'The Wikidata team', // TODO: link?
-	),
-	'url' => 'https://www.mediawiki.org/wiki/Extension:WikibaseLib',
-	'descriptionmsg' => 'wikibase-lib-desc'
-);
+	$wgExtensionCredits['wikibase'][] = array(
+		'path' => __DIR__,
+		'name' => 'WikibaseLib',
+		'version' => WBL_VERSION,
+		'author' => array(
+			'The Wikidata team', // TODO: link?
+		),
+		'url' => 'https://www.mediawiki.org/wiki/Extension:WikibaseLib',
+		'descriptionmsg' => 'wikibase-lib-desc'
+	);
 
-foreach ( include( __DIR__ . '/WikibaseLib.classes.php' ) as $class => $file ) {
-	$wgAutoloadClasses[$class] = __DIR__ . '/' . $file;
-}
-
-define( 'SUMMARY_MAX_LENGTH', 250 );
-
-// i18n
-$wgExtensionMessagesFiles['WikibaseLib'] = __DIR__ . '/WikibaseLib.i18n.php';
-
-// TODO: this is not nice, figure out a better design
-$wgExtensionFunctions[] = function() {
-	\Wikibase\TemplateRegistry::singleton()->addTemplates( include( __DIR__ . "/resources/templates.php" ) );
-	return true;
-};
-
-$wgValueParsers['wikibase-entityid'] = 'Wikibase\Lib\EntityIdParser';
-$wgDataValues['wikibase-entityid'] = 'Wikibase\EntityId';
-$wgJobClasses['ChangeNotification'] = 'Wikibase\ChangeNotificationJob';
-
-// Hooks
-$wgHooks['UnitTestsList'][]							= 'Wikibase\LibHooks::registerPhpUnitTests';
-$wgHooks['ResourceLoaderTestModules'][]				= 'Wikibase\LibHooks::registerQUnitTests';
-
-/**
- * Called when generating the extensions credits, use this to change the tables headers.
- * @see https://www.mediawiki.org/wiki/Manual:Hooks/ExtensionTypes
- *
- * @since 0.1
- *
- * @param array &$extensionTypes
- *
- * @return boolean
- */
-$wgHooks['ExtensionTypes'][] = function( array &$extensionTypes ) {
-	// @codeCoverageIgnoreStart
-	$extensionTypes['wikibase'] = wfMessage( 'version-wikibase' )->text();
-
-	return true;
-	// @codeCoverageIgnoreEnd
-};
-
-/**
- * Shorthand function to retrieve a template filled with the specified parameters.
- *
- * @since 0.2
- *
- * @param $key string template key
- * Varargs: normal template parameters
- *
- * @return string
- */
-function wfTemplate( $key /*...*/ ) {
-	$params = func_get_args();
-	array_shift( $params );
-
-	if ( isset( $params[0] ) && is_array( $params[0] ) ) {
-		$params = $params[0];
+	foreach ( include( __DIR__ . '/WikibaseLib.classes.php' ) as $class => $file ) {
+		$wgAutoloadClasses[$class] = __DIR__ . '/' . $file;
 	}
 
-	$template = new \Wikibase\Template( \Wikibase\TemplateRegistry::singleton(), $key, $params );
-	return $template->text();
-}
+	define( 'SUMMARY_MAX_LENGTH', 250 );
 
-// Resource Loader Modules:
-$wgResourceModules = array_merge( $wgResourceModules, include( __DIR__ . "/resources/Resources.php" ) );
+	// i18n
+	$wgExtensionMessagesFiles['WikibaseLib'] = __DIR__ . '/WikibaseLib.i18n.php';
 
-$wgValueFormatters[ \Wikibase\EntityId::getType() ] = 'Wikibase\Lib\EntityIdFormatter';
+	// TODO: this is not nice, figure out a better design
+	$wgExtensionFunctions[] = function() {
+		\Wikibase\TemplateRegistry::singleton()->addTemplates( include( __DIR__ . "/resources/templates.php" ) );
+		return true;
+	};
 
-if ( defined( 'WB_EXPERIMENTAL_FEATURES' ) && WB_EXPERIMENTAL_FEATURES ) {
-	include_once( __DIR__ . '/config/WikibaseLib.experimental.php' );
-}
+	$wgValueParsers['wikibase-entityid'] = 'Wikibase\Lib\EntityIdParser';
+	$wgDataValues['wikibase-entityid'] = 'Wikibase\EntityId';
+	$wgJobClasses['ChangeNotification'] = 'Wikibase\ChangeNotificationJob';
+
+	// Hooks
+	$wgHooks['UnitTestsList'][]							= 'Wikibase\LibHooks::registerPhpUnitTests';
+	$wgHooks['ResourceLoaderTestModules'][]				= 'Wikibase\LibHooks::registerQUnitTests';
+
+	/**
+	 * Called when generating the extensions credits, use this to change the tables headers.
+	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/ExtensionTypes
+	 *
+	 * @since 0.1
+	 *
+	 * @param array &$extensionTypes
+	 *
+	 * @return boolean
+	 */
+	$wgHooks['ExtensionTypes'][] = function( array &$extensionTypes ) {
+		// @codeCoverageIgnoreStart
+		$extensionTypes['wikibase'] = wfMessage( 'version-wikibase' )->text();
+
+		return true;
+		// @codeCoverageIgnoreEnd
+	};
+
+	/**
+	 * Shorthand function to retrieve a template filled with the specified parameters.
+	 *
+	 * @since 0.2
+	 *
+	 * @param $key string template key
+	 * Varargs: normal template parameters
+	 *
+	 * @return string
+	 */
+	function wfTemplate( $key /*...*/ ) {
+		$params = func_get_args();
+		array_shift( $params );
+
+		if ( isset( $params[0] ) && is_array( $params[0] ) ) {
+			$params = $params[0];
+		}
+
+		$template = new \Wikibase\Template( \Wikibase\TemplateRegistry::singleton(), $key, $params );
+		return $template->text();
+	}
+
+	// Resource Loader Modules:
+	$wgResourceModules = array_merge( $wgResourceModules, include( __DIR__ . "/resources/Resources.php" ) );
+
+	$wgValueFormatters[ \Wikibase\EntityId::getType() ] = 'Wikibase\Lib\EntityIdFormatter';
+
+	if ( defined( 'WB_EXPERIMENTAL_FEATURES' ) && WB_EXPERIMENTAL_FEATURES ) {
+		include_once( __DIR__ . '/config/WikibaseLib.experimental.php' );
+	}
+} );
 
