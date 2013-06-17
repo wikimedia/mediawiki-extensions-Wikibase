1baaa2835c044652d449927fd125d6980dcb9e3f
jeroendedauw <jeroendedauw@gmail.com>
2013-06-17 11:44:25 +0200

    Add strict config
    Change-Id: I2c7386df7a3df0d53a083c8b90e7a737556f1c26

--- .travis.sh
+++ .travis.sh
@@ -18,6 +18,11 @@ git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/WikibaseDataMode
 git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/Wikibase.git
 
 cd ..
+echo 'error_reporting(E_ALL| E_STRICT);' >> LocalSettings.php
+echo 'ini_set("display_errors", 1);' >> LocalSettings.php
+echo '$wgShowExceptionDetails = true;' >> LocalSettings.php
+echo '$wgDevelopmentWarnings = true;' >> LocalSettings.php
+
 echo "define( 'WB_EXPERIMENTAL_FEATURES', true );" >> LocalSettings.php
 echo 'require_once( __DIR__ . "/extensions/Wikibase/repo/Wikibase.php" );' >> LocalSettings.php
 echo 'require_once( __DIR__ . "/extensions/Wikibase/repo/ExampleSettings.php" );' >> LocalSettings.php
