d4baa008ee9bf1129ed2242d20ac197563458462
daniel <daniel.kinzler@wikimedia.de>
2013-06-24 15:48:58 +0200

    Remove hacks from jenkins entry point.
    Wikibase.php now activates both the client and the repo extension.
    This allows all tests to be run in a single pass.
    
    Change-Id: I8fc331f91af096e98790ebd5d8e7a0fabb60cd85

--- Wikibase.php
+++ Wikibase.php
@@ -13,60 +13,16 @@
  * @file
  *
  * @licence GNU GPL v2+
- * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ * @author Daniel Kinzler
  */
-define( 'WB_EXPERIMENTAL_FEATURES', true );
 
-require_once __DIR__ . '/lib/WikibaseLib.php';
-
-// Temporary hack that populates the sites table since there are some tests that require this to have happened
-require_once __DIR__ . '/lib/maintenance/populateSitesTable.php';
-$wgExtensionFunctions[] = function() {
-	$evilStuff = new PopulateSitesTable();
-	$evilStuff->execute();
-};
-
-function remove_directory( $dirPath ) {
-	foreach(new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dirPath, FilesystemIterator::SKIP_DOTS), RecursiveIteratorIterator::CHILD_FIRST) as $path) {
-		$path->isFile() ? unlink($path->getPathname()) : rmdir($path->getPathname());
-	}
+if ( !defined( 'WB_EXPERIMENTAL_FEATURES' ) ) {
+	//FIXME: this is for Jenkins! For other users, this might be bad!
+	define( 'WB_EXPERIMENTAL_FEATURES', true );
 }
 
-# Let JenkinsAdapt our test suite when run under Jenkins
-$jenkins_job_name = getenv( 'JOB_NAME' );
-if( PHP_SAPI === 'cli' && $jenkins_job_name !== false ) {
+require_once __DIR__ . '/repo/Wikibase.php';
+require_once __DIR__ . '/client/WikibaseClient.php';
 
-	switch( $jenkins_job_name) {
-		case 'mwext-Wikibase-client-tests':
-//			// Exclude non loaded production code
-//			remove_directory( __DIR__ . '/repo' );
-//
-//			// This test is failing for unknown reason
-//			unlink( __DIR__ . '/client/tests/phpunit/includes/CachedEntityTest.php' );
-//
-//			require_once __DIR__ . '/client/WikibaseClient.php';
-//			require_once __DIR__ . '/client/ExampleSettings.php';
-//
-//			$wgWBSettings['repoDatabase'] = $wgDBname . '-' . $wgDBprefix;
-//			$wgWBSettings['changesDatabase'] = $wgDBname . '-' . $wgDBprefix;
-//
-//			$wgLBFactoryConf['sectionsByDB'] = array(
-//				$wgDBname => 'DEFAULT',
-//				'repowiki' => $wgDBname . '-' . $wgDBprefix,
-//			);
-//		break;
-		case 'mwext-Wikibase-repo-tests':
-			// Exclude non loaded production code
-			remove_directory( __DIR__ . '/client' );
-
-			require_once __DIR__ . '/repo/Wikibase.php';
-			require_once __DIR__ . '/repo/ExampleSettings.php';
-		break;
-		default:
-			require_once __DIR__ . '/client/WikibaseClient.php';
-			require_once __DIR__ . '/repo/Wikibase.php';
-			require_once __DIR__ . '/repo/ExampleSettings.php';
-	}
-}
-// Avoid polluting the global namespace
-unset( $jenkins_job_name, $cmd );
+require_once __DIR__ . '/repo/ExampleSettings.php';
+require_once __DIR__ . '/client/ExampleSettings.php';
