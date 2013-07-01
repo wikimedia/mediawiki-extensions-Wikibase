8e5a0435948ef04eaa43cc2abaa30c4fc4e1d20a
daniel <daniel.kinzler@wikimedia.de>
2013-07-01 15:14:58 +0200

    (bug 49742) Introducing PropertyInfoDataTypeLookup
    Adding an implementation of DataTypeLookup that is based on a
    PropertyInfoStore.
    
    Change-Id: Ibb8f50230be53575bee69b4fb64f6bd27e02ccbf

--- lib/tests/phpunit/PropertyInfoDataTypeLookupTest.php
+++ lib/tests/phpunit/PropertyInfoDataTypeLookupTest.php
@@ -4,11 +4,15 @@ namespace Wikibase\Lib\Test;
 
 use Wikibase\EntityId;
 use Wikibase\Lib\EntityRetrievingDataTypeLookup;
+use Wikibase\Lib\PropertyDataTypeLookup;
+use Wikibase\Lib\PropertyInfoDataTypeLookup;
 use Wikibase\Property;
+use Wikibase\PropertyInfoStore;
+use Wikibase\Test\MockPropertyInfoStore;
 use Wikibase\Test\MockRepository;
 
 /**
- * Tests for the Wikibase\Lib\EntityRetrievingDataTypeLookup class.
+ * Tests for the Wikibase\Lib\PropertyInfoDataTypeLookup class.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -37,8 +41,9 @@ use Wikibase\Test\MockRepository;
  *
  * @licence GNU GPL v2+
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ * @author Daniel Kinzler
  */
-class EntityRetrievingDataTypeLookupTest extends \PHPUnit_Framework_TestCase {
+class PropertyInfoDataTypeLookupTest extends \PHPUnit_Framework_TestCase {
 
 	private $propertiesAndTypes = array(
 		1 => 'NyanData all the way across the sky',
@@ -64,32 +69,87 @@ class EntityRetrievingDataTypeLookupTest extends \PHPUnit_Framework_TestCase {
 	public function getDataTypeForPropertyProvider() {
 		$argLists = array();
 
+		$emptyInfoStore = new MockPropertyInfoStore();
+		$mockInfoStore = new MockPropertyInfoStore();
+
+		$mockRepo = new MockRepository();
+		$mockDataTypeLookup = new EntityRetrievingDataTypeLookup( $mockRepo );
+
 		foreach ( $this->propertiesAndTypes as $propertyId => $dataTypeId ) {
+			$id = new EntityId( Property::ENTITY_TYPE, $propertyId );
+
+			// register property info
+			$mockInfoStore->setPropertyInfo(
+				$id,
+				array( PropertyInfoStore::KEY_DATA_TYPE => $dataTypeId )
+			);
+
+			// register property as an entity, for the fallback
+			$property = Property::newEmpty();
+			$property->setId( $id );
+			$property->setDataTypeId( $dataTypeId );
+			$mockRepo->putEntity( $property );
+
+			// try with a working info store
 			$argLists[] = array(
-				new EntityId( Property::ENTITY_TYPE, $propertyId ),
+				$mockInfoStore,
+				null,
+				$id,
+				$dataTypeId
+			);
+
+			// try with via fallback
+			$argLists[] = array(
+				$emptyInfoStore,
+				$mockDataTypeLookup,
+				$id,
 				$dataTypeId
 			);
 		}
 
+		// try unknown property
+		$id = new EntityId( Property::ENTITY_TYPE, 23 );
+
+		// try with a working info store
+		$argLists[] = array(
+			$mockInfoStore,
+			null,
+			$id,
+			false
+		);
+
+		// try with via fallback
+		$argLists[] = array(
+			$emptyInfoStore,
+			$mockDataTypeLookup,
+			$id,
+			false
+		);
 
 		return $argLists;
 	}
 
 	/**
 	 * @dataProvider getDataTypeForPropertyProvider
-	 *
-	 * @param EntityId $propertyId
-	 * @param string $expectedDataType
 	 */
-	public function testGetDataTypeForProperty( EntityId $propertyId, $expectedDataType ) {
-		$lookup = new EntityRetrievingDataTypeLookup( $this->newEntityLookup() );
+	public function testGetDataTypeForProperty(
+		PropertyInfoStore $infoStore,
+		PropertyDataTypeLookup $fallbackLookup = null,
+		EntityId $propertyId,
+		$expectedDataType
+	) {
+		if ( $expectedDataType === false ) {
+			$this->setExpectedException( 'Wikibase\Lib\PropertyNotFoundException' );
+		}
+
+		$lookup = new PropertyInfoDataTypeLookup( $infoStore, $fallbackLookup );
 
 		$actualDataType = $lookup->getDataTypeIdForProperty( $propertyId );
-		$this->assertInternalType( 'string', $actualDataType );
 
-		$this->assertEquals( $expectedDataType, $actualDataType );
+		if ( $expectedDataType !== false ) {
+			$this->assertInternalType( 'string', $actualDataType );
+			$this->assertEquals( $expectedDataType, $actualDataType );
+		}
 	}
-
-	// TODO: tests for not found
 
 }
