a3b2e60237a67ede0507c628ba2534d58a4e13f4
Liangent <liangent@gmail.com>
2013-07-04 15:52:56 +0000

    Have labels shown in variants for {{#property: }}
    Change-Id: I5a461251c5c4b0ff6c5abd701707c5c04910fadf

---
+++ client/tests/phpunit/includes/parserhooks/PropertyParserFunctionRendererTest.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Wikibase\Test;
+
+use DataValues\StringValue;
+use Wikibase\Claim;
+use Wikibase\Client\WikibaseClient;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\Item;
+use Wikibase\Lib\EntityRetrievingDataTypeLookup;
+use Wikibase\Lib\SnakFormatter;
+use Wikibase\Lib\TypedValueFormatter;
+use Wikibase\ParserErrorMessageFormatter;
+use Wikibase\Property;
+use Wikibase\PropertyParserFunctionRenderer;
+use Wikibase\PropertyValueSnak;
+
+/**
+ * @covers Wikibase\PropertyParserFunctionRenderer
+ *
+ * @file
+ * @since 0.4
+ *
+ * @ingroup WikibaseClient
+ * @ingroup Test
+ *
+ * @group Wikibase
+ * @group WikibaseClient
+ * @group PropertyParserFunctionTest
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class PropertyParserFunctionRendererTest extends \PHPUnit_Framework_TestCase {
+
+	private function getDefaultInstance() {
+		$wikibaseClient = WikibaseClient::getDefaultInstance();
+
+		$targetLanguage = \Language::factory( 'en' );
+		$errorFormatter = new ParserErrorMessageFormatter( $targetLanguage );
+		$dataTypeFactory = $wikibaseClient->getDataTypeFactory();
+		$mockRepo = $this->newMockRepository();
+		$mockResolver = new MockPropertyLabelResolver( $targetLanguage->getCode(), $mockRepo );
+
+		$formatter = new SnakFormatter(
+			new EntityRetrievingDataTypeLookup( $mockRepo ),
+			new TypedValueFormatter(),
+			$dataTypeFactory
+		);
+
+		return new PropertyParserFunctionRenderer(
+			$targetLanguage,
+			$mockRepo,
+			$mockResolver,
+			$errorFormatter,
+			$formatter
+		);
+	}
+
+	private function newMockRepository() {
+		$propertyId = new PropertyId( 'P1337' );
+
+		$entityLookup = new MockRepository();
+
+		$item = Item::newEmpty();
+		$item->setId( 42 );
+		$item->addClaim( new Claim( new PropertyValueSnak(
+			$propertyId,
+			new StringValue( 'Please write tests before merging your code' )
+		) ) );
+		$item->addClaim( new Claim( new PropertyValueSnak(
+			$propertyId,
+			new StringValue( 'or kittens will die' )
+		) ) );
+
+		$property = Property::newEmpty();
+		$property->setId( $propertyId );
+
+		$property->setDataTypeId( 'string' );
+		$property->setLabel( 'en', 'kitten' );
+
+		$entityLookup->putEntity( $item );
+		$entityLookup->putEntity( $property );
+
+		return $entityLookup;
+	}
+
+	public static function provideRenderForEntityId() {
+		return array(
+			array(
+				'p1337',
+				'Please write tests before merging your code, or kittens will die',
+				'Congratulations, you just killed a kitten'
+			),
+			array(
+				'kitten',
+				'Please write tests before merging your code, or kittens will die',
+				'Congratulations, you just killed a kitten'
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider provideRenderForEntityId
+	 */
+	public function testRenderForEntityId( $name, $expected, $info ) {
+		$parserFunction = $this->getDefaultInstance();
+
+		$status = $parserFunction->renderForEntityId(
+			new ItemId( 'Q42' ),
+			$name
+		);
+
+		$this->assertTrue( $status->isOK() );
+
+		$text = $status->getValue();
+		$this->assertInternalType( 'string', $text );
+
+		$this->assertEquals(
+			$expected,
+			$text,
+			$info
+		);
+	}
+
+}
