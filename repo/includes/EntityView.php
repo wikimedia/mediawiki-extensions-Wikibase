0fe7c5161104d490bf4dc5505836fdf5cab4bab2
Liangent <liangent@gmail.com>
2013-07-06 15:09:18 +0000

    Include preferred labels / descriptions for entities in JS on EntityView
    EntityView now initializes a fallback chain for current context, and in
    experimental mode, wbUsedEntities and wbEntity include data with fallback
    using this chain, to have data to be viewed by final user included.
    
    Change-Id: Ic8ad24f49ba7fafe11e3dceb5d36e18d5b9ef19f

--- repo/includes/EntityView.php
+++ repo/includes/EntityView.php
@@ -67,6 +67,11 @@ abstract class EntityView extends \ContextSource {
 	protected $idFormatter;
 
 	/**
+	 * @var LanguageFallbackChain
+	 */
+	protected $languageFallbackChain;
+
+	/**
 	 * Maps entity types to the corresponding entity view.
 	 * FIXME: remove this stuff, big OCP violation
 	 *
@@ -94,6 +99,8 @@ abstract class EntityView extends \ContextSource {
 	 * @param IContextSource|null $context
 	 */
 	public function __construct( ValueFormatterFactory $valueFormatters, IContextSource $context = null ) {
+		global $wgUseSquid;
+
 		$this->valueFormatters = $valueFormatters;
 
 		if ( !$context ) {
@@ -103,6 +110,15 @@ abstract class EntityView extends \ContextSource {
 
 		// TODO: this need to be properly injected
 		$this->idFormatter = WikibaseRepo::getDefaultInstance()->getIdFormatter();
+
+		$factory = WikibaseRepo::getDefaultInstance()->getLanguageFallbackChainFactory();
+		if ( $wgUseSquid && $context->getUser()->isAnon() ) {
+			// Anonymous users share the same Squid cache, which is splitted by URL.
+			// That means we can't do anything except for what completely depends by URL such as &uselang=.
+			$this->languageFallbackChain = $factory->newFromLanguage( $context->getLanguage() );
+		} else {
+			$this->languageFallbackChain = $factory->newFromContext( $context );
+		}
 	}
 
 	/**
@@ -826,6 +842,12 @@ abstract class EntityView extends \ContextSource {
 		// TODO: use injected id formatter
 		$serializationOptions = new EntitySerializationOptions( WikibaseRepo::getDefaultInstance()->getIdFormatter() );
 
+		if ( defined( 'WB_EXPERIMENTAL_FEATURES' ) && WB_EXPERIMENTAL_FEATURES ) {
+			$serializationOptions->setLanguages( Utils::getLanguageCodes() + array(
+				$langCode => $this->languageFallbackChain,
+			) );
+		}
+
 		$serializerFactory = new SerializerFactory();
 		$serializer = $serializerFactory->newSerializerForObject( $entity, $serializationOptions );
 
@@ -839,7 +861,7 @@ abstract class EntityView extends \ContextSource {
 		$refFinder = new ReferencedEntitiesFinder( $entityLoader );
 
 		$usedEntityIds = $refFinder->findClaimLinks( $entity->getClaims() );
-		$basicEntityInfo = static::getBasicEntityInfo( $entityLoader, $usedEntityIds, $langCode );
+		$basicEntityInfo = static::getBasicEntityInfo( $entityLoader, $usedEntityIds, $langCode, $this->languageFallbackChain );
 
 		$out->addJsConfigVars(
 			'wbUsedEntities',
@@ -857,9 +879,12 @@ abstract class EntityView extends \ContextSource {
 	 * @param EntityLookup $entityLoader
 	 * @param EntityId[] $entityIds
 	 * @param string $langCode For the entity labels which will be included in one language only.
+	 * @param LanguageFallbackChain $languageFallbackChain Set it to include labels to display for the given language fallback chain too.
 	 * @return array
 	 */
-	protected static function getBasicEntityInfo( EntityLookup $entityLoader, array $entityIds, $langCode ) {
+	protected static function getBasicEntityInfo(
+		EntityLookup $entityLoader, array $entityIds, $langCode, LanguageFallbackChain $languageFallbackChain = null
+	) {
 		wfProfileIn( __METHOD__ );
 
 		$entityContentFactory = EntityContentFactory::singleton();
@@ -871,7 +896,11 @@ abstract class EntityView extends \ContextSource {
 		$serializationOptions = new EntitySerializationOptions( WikibaseRepo::getDefaultInstance()->getIdFormatter() );
 		$serializationOptions->setProps( array( 'labels', 'descriptions', 'datatype' ) );
 
-		$serializationOptions->setLanguages( array( $langCode ) );
+		$languages = array( $langCode );
+		if ( $languageFallbackChain && defined( 'WB_EXPERIMENTAL_FEATURES' ) && WB_EXPERIMENTAL_FEATURES ) {
+			$languages[$langCode] = $languageFallbackChain;
+		}
+		$serializationOptions->setLanguages( $languages );
 
 		foreach( $entities as $prefixedId => $entity ) {
 			if( $entity === null ) {
