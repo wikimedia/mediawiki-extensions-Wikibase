77281f12b5ae36ada1b66f852e3261aff2182a5c
Liangent <liangent@gmail.com>
2013-07-06 21:39:30 +0000

    LanguageWithConversion: Allow lazy language object construction
    It seems language object construction is too expensive.
    
    Change-Id: I4ae84a8c2dc40874733d64866db4076ff8f43fd8

--- lib/includes/LanguageWithConversion.php
+++ lib/includes/LanguageWithConversion.php
@@ -33,7 +33,9 @@ class LanguageWithConversion {
 	static protected $objectCache = array();
 
 	protected $language;
+	protected $languageCode;
 	protected $sourceLanguage;
+	protected $sourceLanguageCode;
 	protected $parentLanguage;
 
 	protected $translateCache = array();
@@ -42,50 +44,108 @@ class LanguageWithConversion {
 	/**
 	 * Constructor.
 	 *
-	 * @param $language Language
-	 * @param $sourceLanguage null|Language
-	 * @param $parentLanguage null|Language
+	 * @param null|Language $language
+	 * @param string $languageCode
+	 * @param null|Language $sourceLanguage
+	 * @param null|string $sourceLanguageCode
+	 * @param null|Language $parentLanguage
 	 */
-	protected function __construct( Language $language, $sourceLanguage, $parentLanguage ) {
+	protected function __construct( $language, $languageCode, $sourceLanguage, $sourceLanguageCode, $parentLanguage ) {
 		$this->language = $language;
+		$this->languageCode = $languageCode;
 		$this->sourceLanguage = $sourceLanguage;
+		$this->sourceLanguageCode = $sourceLanguageCode;
 		$this->parentLanguage = $parentLanguage;
+	}
+
+	/**
+	 * Validate a language code. Logic taken from class Language.
+	 *
+	 * @param string $code Language code
+	 *
+	 * @return string|null Validated and normalized code, or null on invalid code.
+	 */
+	private static function validateLanguageCode( $code ) {
+		global $wgDummyLanguageCodes;
+
+		if ( isset( $wgDummyLanguageCodes[$code] ) ) {
+			$code = $wgDummyLanguageCodes[$code];
+		}
+
+		if ( !Language::isValidCode( $code )
+			|| strcspn( $code, ":/\\\000" ) !== strlen( $code )
+		) {
+			return null;
+		}
+
+		return $code;
 	}
 
 	/**
 	 * Get a LanguageWithConversion object.
 	 *
-	 * @param $language Language: Language for this object
-	 * @param $sourceLanguage null|Language:
-	 *          Source language if this is a converted language, or null
+	 * @param $language Language|string: Language (code) for this object
+	 * @param $sourceLanguage null|Language|string:
+	 *          Source language (code) if this is a converted language, or null
 	 * @return LanguageWithConversion
 	 */
-	public static function factory( Language $language, $sourceLanguage = null ) {
-		$sourceCode = $sourceLanguage ? $sourceLanguage->getCode() : '';
-		if ( isset( self::$objectCache[$language->getCode()][$sourceCode] ) ) {
-			return self::$objectCache[$language->getCode()][$sourceCode];
+	public static function factory( $language, $sourceLanguage = null ) {
+		if ( is_string( $language ) ) {
+			$languageCode = self::validateLanguageCode( $language );
+			if ( $languageCode === null ) {
+				throw new MWException( __METHOD__ . ': invalid language code ' . $language );
+			}
+			$language = null;
+		} else {
+			$languageCode = $language->getCode();
 		}
 
-		if ( $sourceLanguage ) {
-			$parentLanguage = $language->getParentLanguage();
-			$sourceParentLanguage = $sourceLanguage->getParentLanguage();
-			if ( !$parentLanguage || !$sourceParentLanguage ) {
-				throw new MWException( __METHOD__ .
-					': either $language or $sourceLanguage does not support conversion.'
-				);
+		if ( is_string( $sourceLanguage ) ) {
+			$sourceLanguageCode = self::validateLanguageCode( $sourceLanguage );
+			if ( $sourceLanguageCode === null ) {
+				throw new MWException( __METHOD__ . ': invalid source language code ' . $sourceLanguage );
 			}
-			if ( $parentLanguage->getCode() !== $sourceParentLanguage->getCode() ) {
-				throw new MWException( __METHOD__ .
-					': $language and $sourceLanguage do not share the same parent language'
-				);
+			$sourceLanguage = null;
+		} elseif ( $sourceLanguage === null ) {
+			$sourceLanguageCode = null;
+		} else {
+			$sourceLanguageCode = $sourceLanguage->getCode();
+		}
+
+		$sourceLanguageKey = $sourceLanguageCode === null ? '' : $sourceLanguageCode;
+		if ( isset( self::$objectCache[$languageCode][$sourceLanguageKey] ) ) {
+			return self::$objectCache[$languageCode][$sourceLanguageKey];
+		}
+
+		if ( $sourceLanguageCode !== null ) {
+			if ( !$language ) {
+				$language = Language::factory( $languageCode );
+			}
+			$parentLanguage = $language->getParentLanguage();
+
+			if ( !$parentLanguage ) {
+				throw new MWException( __METHOD__ . ': $language does not support conversion' );
+			}
+
+			if ( !$parentLanguage->hasVariant( $sourceLanguageCode ) ) {
+				throw new MWException( __METHOD__ . ': given languages do not have the same parent language' );
 			}
 		} else {
 			$parentLanguage = null;
 		}
 
-		$object = new self( $language, $sourceLanguage, $parentLanguage );
-		self::$objectCache[$language->getCode()][$sourceCode] = $object;
+		$object = new self( $language, $languageCode, $sourceLanguage, $sourceLanguageCode, $parentLanguage );
+		self::$objectCache[$languageCode][$sourceLanguageKey] = $object;
 		return $object;
+	}
+
+	/**
+	 * Get the code of the language this object wraps.
+	 *
+	 * @return string
+	 */
+	public function getLanguageCode() {
+		return $this->languageCode;
 	}
 
 	/**
@@ -94,20 +154,50 @@ class LanguageWithConversion {
 	 * @return Language
 	 */
 	public function getLanguage() {
+		if ( !$this->language ) {
+			$this->language = Language::factory( $this->languageCode );
+		}
+
 		return $this->language;
 	}
 
 	/**
-	 * Get the source language defined. Semi-private for unit testing etc.
+	 * Get the code of the source language defined.
+	 *
+	 * @return string
+	 */
+	public function getSourceLanguageCode() {
+		return $this->sourceLanguageCode;
+	}
+
+	/**
+	 * Get the source language defined.
 	 *
 	 * @return Language
 	 */
 	public function getSourceLanguage() {
+		if ( $this->sourceLanguageCode !== null && !$this->sourceLanguage ) {
+			$this->sourceLanguage = Language::factory( $this->sourceLanguageCode );
+		}
+
 		return $this->sourceLanguage;
 	}
 
 	/**
-	 * Get the language where data should be fetched.
+	 * Get the code of the language where data should be fetched.
+	 *
+	 * @return string
+	 */
+	public function getFetchLanguageCode() {
+		if ( $this->sourceLanguageCode !== null ) {
+			return $this->sourceLanguageCode;
+		} else {
+			return $this->languageCode;
+		}
+	}
+
+	/**
+	 * Get the language where data should be fetched. Generally this doesn't need to be used.
 	 *
 	 * @return Language
 	 */
