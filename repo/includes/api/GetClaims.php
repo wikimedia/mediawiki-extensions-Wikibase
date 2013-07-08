e01959209c90c2aa6dddf5408c679d2d80e631e0
addshore <addshorewiki@gmail.com>
2013-07-08 18:03:49 +0200

    use EntityIdParser in api/getclaims
    Bug: 50061
    Change-Id: Ic8c389a627d82ac48aa31003b3d8b38aa4340b94

--- repo/includes/api/GetClaims.php
+++ repo/includes/api/GetClaims.php
@@ -129,26 +129,33 @@ class GetClaims extends ApiWikibase {
 		}
 
 		$claims = array();
-		$params = $this->extractRequestParams();
 
-		// TODO: we probably need this elsewhere, so make filter methods in Claim
-		$rank = isset( $params['rank'] ) ? ClaimSerializer::unserializeRank( $params['rank'] ) : false;
-		$propertyId = isset( $params['property'] ) ? $params['property'] : false;
-
-		/**
-		 * @var \Wikibase\Claim $claim
-		 */
+		/** @var \Wikibase\Claim $claim */
 		foreach ( $claimsList as $claim ) {
-			$rankIsOk = $rank === false
-				|| ( $claim instanceof Statement && $claim->getRank() === $rank );
-
-			if ( $rankIsOk
-				&& ( $propertyId === false || $propertyId === $claim->getPropertyId()->getPrefixedId() ) ) {
+			if ( $this->claimMatchesFilters( $claim ) ) {
 				$claims[] = $claim;
 			}
 		}
 
 		return $claims;
+	}
+
+	protected function claimMatchesFilters( Claim $claim ) {
+		return $this->rankMatchesFilter( $claim->getRank() )
+			&& $this->propertyMatchesFilter( $claim->getPropertyId() );
+	}
+
+	protected function rankMatchesFilter( $rank ) {
+		if($rank === null) { return true; }
+		$params = $this->extractRequestParams();
+		return !isset( $params['rank'] )
+			|| $rank === ClaimSerializer::unserializeRank( $params['rank'] );
+	}
+
+	protected function propertyMatchesFilter( EntityId $propertyId ) {
+		$params = $this->extractRequestParams();
+		return !isset( $params['property'] )
+			|| $propertyId->equals( WikibaseRepo::getDefaultInstance()->getEntityIdParser()->parse( $params['property'] ) );
 	}
 
 	/**
