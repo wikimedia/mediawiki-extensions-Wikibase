31bf819e6dcbdb70ddf5c7519a90fad37e41948c
vrandezo <denny.vrandecic@wikimedia.de>
2013-07-12 16:16:40 +0200

    (bug 43238) Add very simple weighting for entity search
    A very simple weighting algorithm (number of sitelinks) has
    been added to do a post-DB-query ranking of search results.
    Also, this limits the search to 5000 items.
    
    Change-Id: Ie09e4932de42676dd5d638d32f76abe0a2200617

--- repo/tests/phpunit/includes/store/sql/TermSqlIndexTest.php
+++ repo/tests/phpunit/includes/store/sql/TermSqlIndexTest.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Wikibase\Test;
+use Wikibase\DataModel\SimpleSiteLink;
 use Wikibase\Item;
 use Wikibase\StringNormalizer;
 use Wikibase\Term;
@@ -99,6 +100,69 @@ class TermSqlIndexTest extends TermIndexTest {
 
 			$this->assertEquals( $termText, $obtainedTerm->getText() );
 		}
+	}
+
+	/**
+	 * @dataProvider termProvider
+	 * @param $languageCode
+	 * @param $termText
+	 * @param $searchText
+	 * @param boolean $matches
+	 */
+	public function testGetMatchingTermsWeights( $languageCode, $termText, $searchText, $matches ) {
+		/**
+		 * @var \Wikibase\TermSqlIndex $termIndex
+		 */
+		$termIndex = $this->getTermIndex();
+
+		if ( !$termIndex->supportsWeight() ) {
+			$this->markTestSkipped( "can't test search weight if withoutTermWeight option is set." );
+		}
+
+		$termIndex->clear();
+
+		$item1 = \Wikibase\Item::newEmpty();
+		$item1->setId( 42 );
+
+		$item1->setLabel( $languageCode, $termText );
+		$item1->addSimpleSiteLink( new SimpleSiteLink( 'enwiki', 'A' ) );
+
+		$termIndex->saveTermsOfEntity( $item1 );
+
+		$item2 = \Wikibase\Item::newEmpty();
+		$item2->setId( 23 );
+
+		$item2->setLabel( $languageCode, $termText );
+		$item2->addSimpleSiteLink( new SimpleSiteLink( 'enwiki', 'B' ) );
+		$item2->addSimpleSiteLink( new SimpleSiteLink( 'dewiki', 'B' ) );
+		$item2->addSimpleSiteLink( new SimpleSiteLink( 'hrwiki', 'B' ) );
+		$item2->addSimpleSiteLink( new SimpleSiteLink( 'uzwiki', 'B' ) );
+
+		$termIndex->saveTermsOfEntity( $item2 );
+
+		$item3 = \Wikibase\Item::newEmpty();
+		$item3->setId( 108 );
+
+		$item3->setLabel( $languageCode, $termText );
+		$item3->addSimpleSiteLink( new SimpleSiteLink( 'hrwiki', 'C' ) );
+		$item3->addSimpleSiteLink( new SimpleSiteLink( 'uzwiki', 'C' ) );
+
+		$termIndex->saveTermsOfEntity( $item3 );
+
+		$term = new Term();
+		$term->setLanguage( $languageCode );
+		$term->setText( $searchText );
+
+		$options = array(
+			'caseSensitive' => false,
+		);
+
+		$obtainedIDs = $termIndex->getMatchingIDs( array( $term ), \Wikibase\Item::ENTITY_TYPE, $options );
+
+		$this->assertEquals( $matches ? 3 : 0, count( $obtainedIDs ) );
+
+		$expectedResult = array( $item2->getId(), $item3->getId(), $item1->getId() );
+		$this->assertArrayEquals( $expectedResult, $obtainedIDs, true );
 	}
 
 	public static function provideGetSearchKey() {
