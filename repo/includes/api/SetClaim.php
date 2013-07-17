d561b4cce1d5baa5859213af2c804a950ebd8059
addshore <addshorewiki@gmail.com>
2013-07-17 17:16:57 +0200

    Fix example in api wbsetclaim
    Change-Id: I4acf6c356256d3350ba493429e00700e8a4071f2

--- repo/includes/api/SetClaim.php
+++ repo/includes/api/SetClaim.php
@@ -232,7 +232,7 @@ class SetClaim extends ApiWikibase {
 	 */
 	protected function getExamples() {
 		return array(
-			'api.php?action=setclaim&claim={"id":"q2$5627445f-43cb-ed6d-3adb-760e85bd17ee","type":"claim","mainsnak":{"snaktype":"value","property":"p1","datavalue":{"value":"City","type":"string"}}}'
+			'api.php?action=wbsetclaim&claim={"id":"q2$5627445f-43cb-ed6d-3adb-760e85bd17ee","type":"claim","mainsnak":{"snaktype":"value","property":"p1","datavalue":{"value":"City","type":"string"}}}'
 			=> 'Set the claim with the given id to property p1 with a string value of "City',
 		);
 	}
