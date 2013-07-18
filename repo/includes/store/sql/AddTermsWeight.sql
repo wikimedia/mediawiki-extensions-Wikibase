f32aa522cdec1bc73e3a72aef059410e6c27ac51
vrandezo <denny.vrandecic@wikimedia.de>
2013-07-18 21:32:23 +0200

    Change DOUBLE to FLOAT for wb_term.term_weight
    Needs less space, and the higher precision is not needed.
    
    Change-Id: I6f9b58fc719edd5f7d9bd474808030616b10e1c4

--- repo/includes/store/sql/AddTermsWeight.sql
+++ repo/includes/store/sql/AddTermsWeight.sql
@@ -2,4 +2,4 @@
 -- Licence: GNU GPL v2+
 
 
-alter table /*_*/wb_terms add column term_weight DOUBLE UNSIGNED NOT NULL DEFAULT 0.0;
+alter table /*_*/wb_terms add column term_weight FLOAT UNSIGNED NOT NULL DEFAULT 0.0;
