8b7c51b312f07dab30d9180100791c33e5f7686d
Tobias Gritschacher <tobias.gritschacher@wikimedia.de>
2013-08-14 13:22:50 +0200

    Extending the label cucumber test
    Change-Id: I35c54506580ce097307f992ebc923f2b5afec259

--- selenium_cuc/features/steps/label_steps.rb
+++ selenium_cuc/features/steps/label_steps.rb
@@ -6,8 +6,7 @@
 #
 # tests for item label
 
-label = generate_random_string(10)
-label_changed = label + " Adding something."
+label = generate_random_string(8)
 
 Given /^I am on an entity page$/ do
   visit(CreateItemPage).create_new_item(label, generate_random_string(20))
@@ -17,15 +16,39 @@ When /^I click the label edit button$/ do
   on(ItemPage).editLabelLink
 end
 
+When /^I press the ESC key$/ do
+  on(ItemPage).labelInputField_element.send_keys :escape
+end
+
+When /^I press the RETURN key$/ do
+  on(ItemPage) do |page|
+    page.labelInputField_element.send_keys :return
+    #page.ajax_wait
+    page.wait_for_api_callback
+  end
+end
+
 When /^I click the label cancel button$/ do
   on(ItemPage).cancelLabelLink
 end
 
-When /^I modify the label$/ do
+When /^I click the label save button$/ do
+  on(ItemPage) do |page|
+    page.saveLabelLink
+    #page.ajax_wait
+    page.wait_for_api_callback
+  end
+end
+
+When /^I enter (.+) as label$/ do |value|
   on(ItemPage) do |page|
     page.labelInputField_element.clear
-    page.labelInputField = label_changed
+    page.labelInputField = value
   end
+end
+
+When /^I reload the page$/ do
+  @browser.refresh
 end
 
 Then /^Label edit button should be there$/ do
@@ -67,4 +90,17 @@ Then /^Original label should be displayed$/ do
     @browser.title.include?(label).should be_true
     page.entityLabelSpan.should == label
   end
+end
+
+Then /^(.+) should be displayed as label$/ do |value|
+  on(ItemPage) do |page|
+    page.firstHeading.should be_true
+    page.entityLabelSpan.should be_true
+    @browser.title.include?(value).should be_true
+    page.entityLabelSpan.should == value
+  end
+end
+
+Then /^An error message should be displayed$/ do
+  on(ItemPage).wbErrorDiv?.should be_true
 end
