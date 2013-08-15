c64ccc949ddb5df93d7c77ba33f0924d3c517bc7
Tobias Gritschacher <tobias.gritschacher@wikimedia.de>
2013-08-15 15:57:21 +0200

    Use WikibaseAPI to create new entities for selenium tests
    Change-Id: Iea0982751c61236408479741c8e2fc08878ca541

--- selenium_cuc/features/support/utils/utils.rb
+++ selenium_cuc/features/support/utils/utils.rb
@@ -6,10 +6,40 @@
 #
 # common used methods
 
+include URL
+
 # creates a random string
 def generate_random_string(length=8)
   chars = 'abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ'
   string = ''
   length.times { string << chars[rand(chars.size)] }
   return string
+end
+
+# creates a new entity via the API
+def create_new_entity(data, type = 'item')
+  uri = URI(URL.repo_api)
+
+  request = Net::HTTP::Post.new(uri.to_s)
+  request.set_form_data(
+    'action' => 'wbeditentity',
+    'token' => '+\\',
+    'new' => type,
+    'data' => data,
+    'format' => 'json',
+    'summary' => 'entity created by selenium test'
+  )
+
+  response = Net::HTTP.start(uri.hostname, uri.port) do |http|
+    http.request(request)
+  end
+  resp = ActiveSupport::JSON.decode(response.body)
+
+  if resp["success"] != 1
+    abort("Failed to create new entity: API error")
+  end
+
+  id = resp["entity"]["id"]
+  url = URL.repo_url(id)
+  return {"id" => id, "url" => url}
 end
