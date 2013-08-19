1df479a09ae4ea6e60302fd768a4ebac506fdb3e
addshore <addshorewiki@gmail.com>
2013-08-19 16:55:30 +0200

    Implement merge api module
    This takes 2 unique item ids and merges the items
    Change-Id: I2a75050bdb3ebd7b97fbd6f236d59464479fa7d8

--- repo/tests/phpunit/includes/api/EntityTestHelper.php
+++ repo/tests/phpunit/includes/api/EntityTestHelper.php
@@ -50,6 +50,10 @@ class EntityTestHelper {
 			"new" => "item",
 			"data" => array(),
 		),
+		'Empty2' => array(
+			"new" => "item",
+			"data" => array(),
+		),
 		'Berlin' => array(
 			"new" => "item",
 			"data" => array(
