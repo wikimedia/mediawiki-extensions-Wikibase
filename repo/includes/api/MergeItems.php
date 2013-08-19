1a9267e34a8e69611df0a38ba78ae8fa1da9dc32
addshore <addshorewiki@gmail.com>
2013-08-19 16:55:30 +0200

    Implement merge api module
    This takes 2 unique item ids and merges the items
    Change-Id: I2a75050bdb3ebd7b97fbd6f236d59464479fa7d8

---
+++ repo/includes/api/MergeItems.php
@@ -0,0 +1,276 @@
+<?php
+
+namespace Wikibase\Api;
+
+use ApiBase, User, Status, SiteList;
+use ValueParsers\ParseException;
+use Wikibase\ChangeOpAliases;
+use Wikibase\ChangeOpClaim;
+use Wikibase\ChangeOpDescription;
+use Wikibase\ChangeOpLabel;
+use Wikibase\ChangeOps;
+use Wikibase\ChangeOpSiteLink;
+use Wikibase\Claim;
+use Wikibase\EntityContentFactory;
+use Wikibase\Entity;
+use Wikibase\EntityContent;
+use Wikibase\EntityId;
+use Wikibase\Item;
+use Wikibase\ItemContent;
+use Wikibase\Property;
+use Wikibase\Repo\WikibaseRepo;
+use Wikibase\Summary;
+use Wikibase\Utils;
+
+/**
+ * @since 0.4
+ *
+ * @ingroup WikibaseRepo
+ * @ingroup API
+ *
+ * @licence GNU GPL v2+
+ * @author Adam Shorland
+ */
+class MergeItems extends ApiWikibase {
+
+	//todo allow merging of specific parts of an item only (eg. sitelinks,aliases,claims)
+	//todo allow optional deletion after merging (for admins)
+
+	/**
+	 * @see \Wikibase\Api\Api::getRequiredPermissions()
+	 */
+	protected function getRequiredPermissions( Entity $entity, array $params ) {
+		$permissions = parent::getRequiredPermissions( $entity, $params );
+		$permissions[] = 'edit';
+		$permissions[] = 'item-merge';
+		return $permissions;
+	}
+
+	/**
+	 * @see \ApiBase::execute()
+	 */
+	public function execute() {
+		wfProfileIn( __METHOD__ );
+
+		$this->getUser();
+		$params = $this->extractRequestParams();
+		if ( empty( $params['fromid'] ) || empty( $params['toid'] ) ){
+			wfProfileOut( __METHOD__ );
+			$this->dieUsage( 'You must provide a fromid and a toid' , 'param-missing' );
+		}
+
+		$entityIdParser = WikibaseRepo::getDefaultInstance()->getEntityIdParser();
+
+		/**
+		 * @var $fromId EntityId
+		 * @var $toId EntityId
+		 **/
+		try{
+			$fromId = $entityIdParser->parse( $params['fromid'] );
+			$toId = $entityIdParser->parse( $params['toid'] );
+		}
+		catch ( ParseException $e ){
+			$this->dieUsage( 'You must provide valid ids' , 'param-invalid' );
+		}
+
+		if( $fromId->getPrefixedId() === $toId->getPrefixedId() ){
+			wfProfileOut( __METHOD__ );
+			$this->dieUsage( 'You must provide unique ids' , 'param-invalid' );
+		}
+
+		$entityContentFactory = WikibaseRepo::getDefaultInstance()->getEntityContentFactory();
+
+		$fromEntityContent = $entityContentFactory->getFromId( $fromId );
+		$toEntityContent = $entityContentFactory->getFromId( $toId );
+
+		if ( !( $fromEntityContent instanceof ItemContent && $toEntityContent instanceof ItemContent ) ) {
+			wfProfileOut( __METHOD__ );
+			$this->dieUsage( "One or more of the entities are not items", "not-item" );
+		}
+
+		$fromItem = $fromEntityContent->getItem();
+		$toItem = $toEntityContent->getItem();
+
+		$fromChangeOps = new ChangeOps();
+		$toChangeOps = new ChangeOps();
+
+		foreach( $fromItem->getLabels() as $langCode => $label ){
+			$toLabel = $toItem->getLabel( $langCode );
+			if( $toLabel === false || $toLabel === $label ){
+				$fromChangeOps->add( new ChangeOpLabel( $langCode, null ) );
+				$toChangeOps->add( new ChangeOpLabel( $langCode, $label ) );
+			} else {
+				//todo add the option to merge conflicting labels into the aliases
+				wfProfileOut( __METHOD__ );
+				$this->dieUsage( "Conflicting labels for language {$langCode}", "merge-conflict");
+			}
+		}
+
+		foreach( $fromItem->getDescriptions() as $langCode => $desc ){
+			$toDescription = $toItem->getDescription( $langCode );
+			if( $toDescription === false || $toDescription === $desc ){
+				$fromChangeOps->add( new ChangeOpDescription( $langCode, null ) );
+				$toChangeOps->add( new ChangeOpDescription( $langCode, $desc ) );
+			} else {
+				//todo add the option to ignore description conflicts, or prioritise one
+				wfProfileOut( __METHOD__ );
+				$this->dieUsage( "Conflicting descriptions for language {$langCode}", "merge-conflict" );
+			}
+		}
+
+		foreach( $fromItem->getAllAliases() as $langCode => $aliases ){
+			$fromChangeOps->add( new ChangeOpAliases( $langCode, $aliases, 'remove' ) );
+			$toChangeOps->add( new ChangeOpAliases( $langCode, $aliases, 'add' ) );
+		}
+
+		foreach( $fromItem->getSimpleSiteLinks() as $simpleSiteLink ){
+			$siteId = $simpleSiteLink->getSiteId();
+			if( !$toItem->hasLinkToSite( $siteId ) ){
+				$fromChangeOps->add( new ChangeOpSiteLink( $siteId, null ) );
+				$toChangeOps->add( new ChangeOpSiteLink( $siteId, $simpleSiteLink->getPageName() ) );
+			} else {
+				wfProfileOut( __METHOD__ );
+				$this->dieUsage( "Conflicting sitelinks for {$siteId}", "merge-conflict" );
+			}
+		}
+
+		foreach( $fromItem->getClaims() as $fromClaim ){
+			$fromChangeOps->add( new ChangeOpClaim( $fromClaim, 'remove' ) );
+			$toChangeOps->add( new ChangeOpClaim( $fromClaim , 'add' ) );
+		}
+
+		$entityIdFormatter = WikibaseRepo::getDefaultInstance()->getEntityIdFormatter();
+
+		$fromSummary = new Summary( $this->getModuleName(), 'from', null, array( $entityIdFormatter->format( $fromId ) ) );
+		if ( !is_null( $params['summary'] ) ) {
+			$fromSummary->setUserSummary( $params['summary'] );
+		}
+
+		//save changes
+		$fromChangeOps->apply( $fromItem );
+		$toChangeOps->apply( $toItem );
+
+		$fromStatus = $this->attemptSaveEntity(
+			$fromEntityContent,
+			$fromSummary->toString()
+		);
+		$this->handleSaveStatus( $fromStatus );
+		$this->addEntityToOutput( $fromEntityContent, $fromStatus, 'from' );
+
+		if( $fromStatus->isGood() ){
+
+			$toSummary = new Summary( $this->getModuleName(), 'to', null, array( $entityIdFormatter->format( $toId ) ) );
+			if ( !is_null( $params['summary'] ) ) {
+				$toSummary->setUserSummary( $params['summary'] );
+			}
+
+			$toStatus = $this->attemptSaveEntity(
+				$toEntityContent,
+				$toSummary->toString()
+			);
+			$this->handleSaveStatus( $toStatus );
+			$this->addEntityToOutput( $toEntityContent, $toStatus, 'to' );
+
+			$this->getResult()->addValue( null,	'success', 1 );
+
+			//todo if the second result is not a success we should probably undo the first change
+		}
+
+		wfProfileOut( __METHOD__ );
+	}
+
+	protected function addEntityToOutput( EntityContent $entityContent, Status $status, $name ) {
+		$formatter = WikibaseRepo::getDefaultInstance()->getEntityIdFormatter();
+
+		$this->getResult()->addValue(
+			$name,
+			'id',
+			$formatter->format( $entityContent->getEntity()->getId() )
+		);
+
+		$this->getResult()->addValue(
+			$name,
+			'type', $entityContent->getEntity()->getType()
+		);
+
+		$this->addRevisionIdFromStatusToResult( $name, 'lastrevid', $status );
+	}
+
+	/**
+	 * @see \ApiBase::getPossibleErrors()
+	 */
+	public function getPossibleErrors() {
+		return array_merge( parent::getPossibleErrors(), array(
+			array( 'code' => 'not-item', 'info' => $this->msg( 'wikibase-api-not-item' )->text() ),
+			array( 'code' => 'merge-conflict', 'info' => 'A conflict occurred when trying to merge the items' ),
+		) );
+	}
+
+	/**
+	 * @see \ApiBase::getAllowedParams()
+	 */
+	public function getAllowedParams() {
+		return array_merge(
+			parent::getAllowedParams(),
+			array(
+				'fromid' => array(
+					ApiBase::PARAM_TYPE => 'string',
+				),
+				'toid' => array(
+				ApiBase::PARAM_TYPE => 'string',
+				),
+				'summary' => array(
+					ApiBase::PARAM_TYPE => 'string',
+				),
+				'token' => null,
+			)
+		);
+	}
+
+	/**
+	 * @see \ApiBase::getParamDescription()
+	 */
+	public function getParamDescription() {
+		return array_merge(
+			parent::getParamDescription(),
+			array(
+				'fromid' => array( 'The id to merge from' ),
+				'toid' => array( 'The id to merge to' ),
+				'token' => 'An "edittoken" token previously obtained through the token module (prop=info).',
+				'summary' => array( 'Summary for the edit.',
+					"Will be prepended by an automatically generated comment. The length limit of the
+					autocomment together with the summary is 260 characters. Be aware that everything above that
+					limit will be cut off."
+				),
+			)
+		);
+	}
+
+	/**
+	 * @see \ApiBase::getDescription()
+	 */
+	public function getDescription() {
+		return array(
+			'API module to merge multiple items.'
+		);
+	}
+
+	/**
+	 * @see \ApiBase::getExamples()
+	 */
+	protected function getExamples() {
+		return array(
+			'api.php?action=wbmergeitems&fromid=q42&toid=q222' => 'Merges data from q42 into q222',
+			'api.php?action=wbmergeitems&fromid=q555&toid=q3' => 'Merges data from q555 into q3',
+		);
+	}
+
+	/**
+	 * @see ApiBase::isWriteMode
+	 * @return bool true
+	 */
+	public function isWriteMode() {
+		return true;
+	}
+
+}
---
+++ repo/tests/phpunit/includes/api/MergeItemsTest.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace Wikibase\Test\Api;
+use Wikibase\Claim;
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\Property;
+use Wikibase\PropertyContent;
+
+/**
+ * Unit tests for the Wikibase\Repo\Api\MergeItems class.
+ *
+ * @since 0.4
+ *
+ * @ingroup WikibaseRepoTest
+ *
+ * @group API
+ * @group Database
+ * @group Wikibase
+ * @group WikibaseAPI
+ * @group WikibaseRepo
+ * @group MergeItemsTest
+ *
+ * @group medium
+ *
+ * @licence GNU GPL v2+
+ * @author Adam Shorland
+ */
+class MergeItemsTest extends WikibaseApiTestCase {
+
+	//todo add check merge conflicts are thrown
+
+	private static $hasSetup;
+
+	public function setUp() {
+		parent::setUp();
+		if( !isset( self::$hasSetup ) ){
+			$this->initTestEntities( array( 'Empty', 'Empty2' ) );
+			$prop42 = PropertyId::newFromNumber( 56 );
+			$prop = PropertyContent::newEmpty();
+			$prop->getEntity()->setId( $prop42 );
+			$prop->getEntity()->setDataTypeId( 'string' );
+			$prop->save( 'testing' );
+		}
+		self::$hasSetup = true;
+	}
+
+	public static function provideData(){
+		return array(
+			array(
+				array( 'labels' => array( 'en' => array( 'language' => 'en', 'value' => 'foo' ) ) ),
+				array(),
+				array(),
+				array( 'labels' => array( 'en' => array( 'language' => 'en', 'value' => 'foo' ) ) ),
+			),
+			array(
+				array( 'descriptions' => array( 'de' => array( 'language' => 'de', 'value' => 'foo' ) ) ),
+				array(),
+				array(),
+				array( 'descriptions' => array( 'de' => array( 'language' => 'de', 'value' => 'foo' ) ) ),
+			),
+			array(
+				array( 'aliases' => array( array( "language" => "nl", "value" => "Dickes B" ) ) ),
+				array(),
+				array(),
+				array( 'aliases' => array( array( "language" => "nl", "value" => "Dickes B" ) ) ),
+			),
+			array(
+				array( 'sitelinks' => array( 'dewiki' => array( 'site' => 'dewiki', 'title' => 'Foo' ) ) ),
+				array(),
+				array(),
+				array( 'sitelinks' => array( 'dewiki' => array( 'site' => 'dewiki', 'title' => 'Foo' ) ) ),
+			),
+			array(
+				array( 'claims' => array( 'P56' => array( 'mainsnak' => array(
+					'snaktype' => 'value', 'property' => 'P56', 'datavalue' => array( 'value' => 'imastring', 'type' => 'string' ) ),
+					'type' => 'statement', 'rank' => 'normal' ) ) ),
+				array(),
+				array(),
+				array( 'claims' => array( 'P56' => array( 'mainsnak' => array(
+					'snaktype' => 'value', 'property' => 'P56', 'datavalue' => array( 'value' => 'imastring', 'type' => 'string' ) ),
+					'type' => 'statement', 'rank' => 'normal' ) ) ),
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider provideData
+	 */
+	function testMergeRequest( $pre1, $pre2, $expected1, $expected2 ){
+		// -- prefill the entities --------------------------------------------
+		$this->doApiRequestWithToken( array( 'action' => 'wbeditentity', 'id' => EntityTestHelper::getId( 'Empty' ) ,'clear' => '', 'data' => json_encode( $pre1 ) ) );
+		$this->doApiRequestWithToken( array( 'action' => 'wbeditentity', 'id' => EntityTestHelper::getId( 'Empty2' ) ,'clear' => '', 'data' => json_encode( $pre2 ) ) );
+
+		// -- do the request --------------------------------------------
+		list( $result,, ) = $this->doApiRequestWithToken( array(
+			'action' => 'wbmergeitems',
+			'fromid' => EntityTestHelper::getId( 'Empty' ),
+			'toid' => EntityTestHelper::getId( 'Empty2' ),
+			'summary' => 'CustomSummary!',
+		) );
+
+		// -- check the result --------------------------------------------
+		$this->assertResultSuccess( $result );
+		$this->assertArrayHasKey( 'from', $result );
+		$this->assertArrayHasKey( 'to', $result );
+		$this->assertArrayHasKey( 'id', $result['from'] );
+		$this->assertArrayHasKey( 'id', $result['to'] );
+		$this->assertArrayHasKey( 'lastrevid', $result['from'] );
+		$this->assertArrayHasKey( 'lastrevid', $result['to'] );
+		$this->assertGreaterThan( 0, $result['from']['lastrevid'] );
+		$this->assertGreaterThan( 0, $result['to']['lastrevid'] );
+
+		// -- check the items --------------------------------------------
+		$this->assertEntityEquals( $expected1, $this->loadEntity( $result['from']['id'] ) );
+		$this->assertEntityEquals( $expected2, $this->loadEntity( $result['to']['id'] ) );
+
+		// -- check the edit summaries --------------------------------------------
+		$this->assertRevisionSummary( array( 'wbmergeitems' ), $result['from']['lastrevid'] );
+		$this->assertRevisionSummary( "/CustomSummary/" , $result['from']['lastrevid'] );
+		$this->assertRevisionSummary( array( 'wbmergeitems' ), $result['to']['lastrevid'] );
+		$this->assertRevisionSummary( "/CustomSummary/" , $result['to']['lastrevid'] );
+	}
+
+	public static function provideExceptionData() {
+		return array(
+			array( //0 no ids given
+				'p' => array( ),
+				'e' => array( 'exception' => array( 'type' => 'UsageException', 'code' => 'param-missing' ) ) ),
+			array( //1 only from id
+				'p' => array( 'fromid' => 'q1' ),
+				'e' => array( 'exception' => array( 'type' => 'UsageException', 'code' => 'param-missing' ) ) ),
+			array( //2 only to id
+				'p' => array( 'toid' => 'q1' ),
+				'e' => array( 'exception' => array( 'type' => 'UsageException', 'code' => 'param-missing' ) ) ),
+			array( //3 toid bad
+				'p' => array( 'fromid' => 'q1', 'toid' => 'ABCDE' ),
+				'e' => array( 'exception' => array( 'type' => 'UsageException', 'code' => 'param-invalid' ) ) ),
+			array( //4 fromid bad
+				'p' => array( 'fromid' => 'ABCDE', 'toid' => 'q1' ),
+				'e' => array( 'exception' => array( 'type' => 'UsageException', 'code' => 'param-invalid' ) ) ),
+			array( //5 bot same id
+				'p' => array( 'fromid' => 'q1', 'toid' => 'q1' ),
+				'e' => array( 'exception' => array( 'type' => 'UsageException', 'code' => 'param-invalid' ) ) ),
+			array( //6 from id is property
+				'p' => array( 'fromid' => 'p56', 'toid' => 'q2' ),
+				'e' => array( 'exception' => array( 'type' => 'UsageException', 'code' => 'not-item' ) ) ),
+			array( //7 to id is property
+				'p' => array( 'fromid' => 'q2', 'toid' => 'p56' ),
+				'e' => array( 'exception' => array( 'type' => 'UsageException', 'code' => 'not-item' ) ) ),
+		);
+	}
+
+	/**
+	 * @dataProvider provideExceptionData
+	 */
+	public function testEditEntityExceptions( $params, $expected ){
+		// -- set any defaults ------------------------------------
+		$params['action'] = 'wbmergeitems';
+		$this->doTestQueryExceptions( $params, $expected['exception'] );
+	}
+
+}
