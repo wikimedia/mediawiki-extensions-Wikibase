b457a0ce417a4debb20e36b4fba2f5dcc3fef4e5
aude <aude.wiki@gmail.com>
2013-08-20 15:14:18 +0000

    Introduce ChangeLineFormatter in the client
    Change-Id: Ib03eb7395b0524cd27052b7e2305a85523170d7c

---
+++ client/includes/recentchanges/ExternalChange.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Wikibase;
+
+use Wikibase\Client\WikibaseClient;
+
+/**
+ * Represents an external change
+ *
+ * @since 0.5
+ *
+ * @file
+ * @ingroup WikibaseClient
+ *
+ * @licence GNU GPL v2+
+ * @author Katie Filbert < aude.wiki@gmail.com >
+ */
+class ExternalChange {
+
+	/**
+	 * @var EntityId
+	 */
+	protected $entityId;
+
+	/**
+	 * @var RevisionObject
+	 */
+	protected $rev;
+
+	/**
+	 * @var string
+	 */
+	protected $changeType;
+
+	/**
+	 * @param EntityId $entityId
+	 * @param RevisionObject $rev
+	 * @param string $changeType
+	 */
+	public function __construct( EntityId $entityId, RevisionObject $rev, $changeType ) {
+		$this->entityId = $entityId;
+		$this->rev = $rev;
+		$this->changeType = $changeType;
+	}
+
+	/**
+	 * return EntityId
+	 */
+	public function getEntityId() {
+		return $this->entityId;
+	}
+
+	/**
+	 * @return RevisionObject
+	 */
+	public function getRev() {
+		return $this->rev;
+	}
+
+	/**
+ 	 * @return string
+	 */
+	public function getChangeType() {
+		return $this->changeType;
+	}
+
+}
