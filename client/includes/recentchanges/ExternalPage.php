d4d050ea7d53b97539f10091153559a75ccbad7a
aude <aude.wiki@gmail.com>
2013-08-20 15:14:18 +0000

    introduce ChangeLineFormatter in client
    Change-Id: I5b126a2a572c961334923d5aac16f2587e135d3c

---
+++ client/includes/recentchanges/ExternalPage.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Wikibase;
+
+/**
+ * Represents an external page
+ *
+ * @since 0.4
+ *
+ * @file
+ * @ingroup WikibaseClient
+ *
+ * @licence GNU GPL v2+
+ * @author Katie Filbert < aude.wiki@gmail.com >
+ */
+class ExternalPage {
+
+	/**
+	 * @var string
+	 */
+	protected $pageTitle;
+
+	/**
+	 * @var string
+	 */
+	protected $namespace;
+
+	/**
+	 * @var int
+	 */
+	protected $pageId;
+
+	/**
+	 * @since 0.4
+	 *
+	 * @param string $pageTitle
+	 * @param int $pageId
+	 * @param string $namespace
+	 */
+	public function __construct( $pageTitle, $pageId, $namespace = '' ) {
+		$this->pageTitle = $pageTitle;
+		$this->pageId = $pageId;
+		$this->namespace = $namespace;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getPageTitle() {
+		return $this->pageTitle;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getPageId() {
+		return $this->pageId;
+	}
+
+	/**
+	 * @todo support i18n
+	 *
+	 * @return string
+	 */
+	public function getNamespace() {
+		return $this->namespace;
+	}
+
+}
