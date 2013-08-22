92930ae025e0163d35e4cabc5f4505536005276f
jeroendedauw <jeroendedauw@gmail.com>
2013-08-23 00:34:18 +0200

    Match EntityId changes in DataModel.
    This commit makes Wikibase use the EntityId rewrite in DataModel
    done in https://gerrit.wikimedia.org/r/#/c/80394/
    
    This commit mainly includes:
    
    * new EntityId to new ItemId or new PropertyId
    * updating references from Wikibase\EntityId to the new location of EntityId
    * constructing EntityIdValue instead of EntityId where applicable
    * removal of id parsing and formatting no longer needed
    * addition of code required to ensure compatibility
    * random cleanup of code already touched by this commit
    
    Change-Id: I8fba1e700fe435c96618c2ab75e3bebe5363561e

--- lib/includes/WikibaseDataTypeBuilders.php
+++ lib/includes/WikibaseDataTypeBuilders.php
@@ -8,33 +8,16 @@ use Wikibase\Client\WikibaseClient;
 use Wikibase\EntityLookup;
 use Wikibase\Item;
 use Wikibase\Repo\WikibaseRepo;
-use Wikibase\SettingsArray;
 use Wikibase\Validators\CompositeValidator;
 use Wikibase\Validators\DataFieldValidator;
 use Wikibase\Validators\DataValueValidator;
 use Wikibase\Validators\EntityExistsValidator;
-use Wikibase\Validators\EntityIdValidator;
 use Wikibase\Validators\RegexValidator;
 use Wikibase\Validators\StringLengthValidator;
 use Wikibase\Validators\TypeValidator;
 
 /**
  * Defines the data types supported by Wikibase.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * http://www.gnu.org/copyleft/gpl.html
  *
  * @since 0.4
  *
@@ -100,7 +83,7 @@ class WikibaseDataTypeBuilders {
 		$validators = array();
 
 		//NOTE: The DataValue in question is going to be an instance of EntityId!
-		$validators[] = new TypeValidator( 'Wikibase\EntityId' );
+		$validators[] = new TypeValidator( 'Wikibase\DataModel\Entity\EntityIdValue' );
 		$validators[] = new EntityExistsValidator( $this->entityLookup );
 
 		return new DataType( $id, 'wikibase-entityid', array(), array(), $validators );
