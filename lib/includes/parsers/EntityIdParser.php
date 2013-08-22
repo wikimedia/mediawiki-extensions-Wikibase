7630365cc932919a2ec895ec8167ae029a992c28
jeroendedauw <jeroendedauw@gmail.com>
2013-08-23 00:34:18 +0200

    Match EntityId changes in DataModel [DO NOT MERGE, DRAFT]
    Change-Id: I8fba1e700fe435c96618c2ab75e3bebe5363561e

--- lib/includes/parsers/EntityIdParser.php
+++ lib/includes/parsers/EntityIdParser.php
@@ -1,28 +1,15 @@
 <?php
 
 namespace Wikibase\Lib;
+
 use ValueParsers\ParseException;
 use ValueParsers\StringValueParser;
-use ValueParsers\ParserOptions;
-use Wikibase\EntityId;
+use Wikibase\DataModel\Entity\BasicEntityIdParser;
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\DataModel\Entity\EntityIdParsingException;
 
 /**
  * Parser that parses entity id strings into EntityId objects.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * http://www.gnu.org/copyleft/gpl.html
  *
  * @since 0.4
  *
@@ -36,61 +23,9 @@ use Wikibase\EntityId;
 class EntityIdParser extends StringValueParser {
 
 	/**
-	 * Option name for the required prefixmap option.
-	 * The value of this option should be an array of
-	 * prefixes (string) pointing to the entity type
-	 * (string) they map to.
-	 *
-	 * @since 0.4
+	 * @deprecated
 	 */
 	const OPT_PREFIX_MAP = 'prefixmap';
-
-	/**
-	 * Cache field that holds the regex used by @see getIdParts
-	 *
-	 * Note that this caching does not get invalidated when changing the options!
-	 *
-	 * @since 0.4
-	 *
-	 * @var string|boolean false
-	 */
-	protected $regex = false;
-
-	private $prefixMap = null;
-
-	/**
-	 * @since 0.4
-	 *
-	 * @param ParserOptions|null $options
-	 *
-	 * The OPT_PREFIX_MAP option MUST be set, and prefixes MUST be lower case.
-	 *
-	 * @todo: make this case insensitive
-	 */
-	public function __construct( ParserOptions $options = null ) {
-		parent::__construct( $options );
-
-		$this->requireOption( self::OPT_PREFIX_MAP );
-	}
-
-	/**
-	 * Returns a prefix map with lower case keys. The map is constructed from
-	 * the OPT_PREFIX_MAP option from the ParserOptions passed to the provider.
-	 *
-	 * @return array A mapping from prefixes to type IDs.
-	 */
-	protected function getPrefixMap() {
-		if ( $this->prefixMap === null ) {
-			$this->prefixMap = array();
-
-			foreach ( $this->getOption( self::OPT_PREFIX_MAP ) as $prefix => $type ) {
-				$prefix = strtolower( $prefix );
-				$this->prefixMap[$prefix] = $type;
-			}
-		}
-
-		return $this->prefixMap;
-	}
 
 	/**
 	 * @see StringValueParser::stringParse
@@ -103,58 +38,19 @@ class EntityIdParser extends StringValueParser {
 	 * @throws ParseException
 	 */
 	protected function stringParse( $value ) {
-		$idParts = $this->getIdParts( $value );
+		$idBuilders = BasicEntityIdParser::getBuilders();
 
-		if ( count( $idParts ) < 3 || !ctype_digit( $idParts[2] ) ) {
-			throw new ParseException( 'Not an EntityId' );
+		// TODO: extensions need to be able to add builders.
+		// The construction of the actual id parser will thus need to be moved out.
+
+		$parser = new \Wikibase\DataModel\Entity\EntityIdParser( $idBuilders );
+
+		try {
+			return $parser->parse( $value );
 		}
-
-		$entityType = $this->getEntityTypeForPrefix( $idParts[1] );
-
-		if ( $entityType === null ) {
-			throw new ParseException( 'EntityId has an invalid prefix' );
+		catch ( EntityIdParsingException $ex ) {
+			throw new ParseException( '', 0, $ex );
 		}
-
-		return new EntityId( $entityType, (int)$idParts[2] );
-	}
-
-	/**
-	 * @since 0.4
-	 *
-	 * @param string $prefix the prefix to look up
-	 *
-	 * @return string|null
-	 */
-	protected function getEntityTypeForPrefix( $prefix ) {
-		$prefix = strtolower( $prefix );
-		$prefixMap = $this->getPrefixMap();
-		return array_key_exists( $prefix, $prefixMap ) ? $prefixMap[$prefix] : null;
-	}
-
-	/**
-	 * Get individual parts of an id. All results are converted to lower case.
-	 *
-	 * @since 0.4
-	 *
-	 * @param string $id
-	 *
-	 * @return array The actual id broken up in prefix, number, hash and fragment and fragment alone
-	 */
-	protected function getIdParts( $id ) {
-		if ( $this->regex === false ) {
-			$prefixes = array();
-			$prefixMap = $this->getPrefixMap();
-
-			foreach ( array_keys( $prefixMap ) as $prefix ) {
-				$prefixes[] = preg_quote( $prefix );
-			}
-
-			$this->regex = '/^(' . implode( '|', $prefixes ) . '|)(\d+)(#.*|)$/';
-		}
-
-		preg_match( $this->regex, strtolower( $id ), $matches );
-
-		return $matches;
 	}
 
 }
--- repo/includes/Validators/SnakValidator.php
+++ repo/includes/Validators/SnakValidator.php
@@ -1,31 +1,6 @@
 <?php
- /**
- * Copyright © 10.06.13 by the authors listed below.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * http://www.gnu.org/copyleft/gpl.html
- *
- * @license GPL 2+
- * @file
- *
- * @author Daniel Kinzler
- */
-
 
 namespace Wikibase\Validators;
-
 
 use DataTypes\DataTypeFactory;
 use DataValues\UnDeserializableValue;
@@ -48,7 +23,10 @@ use Wikibase\Statement;
  *
  * @since 0.4
  *
- * @package Wikibase\Validators
+ * @license GPL 2+
+ * @file
+ *
+ * @author Daniel Kinzler
  */
 class SnakValidator implements ValueValidator {
 
@@ -234,4 +212,5 @@ class SnakValidator implements ValueValidator {
 	public function setOptions( array $options ) {
 		// Do nothing. This method shouldn't even be in the interface.
 	}
+
 }
