5a18eae4ca31cb4fb285447e7ef8789dae7aea0a
daniel <daniel.kinzler@wikimedia.de>
2013-08-27 13:11:19 +0200

    Allow ItemContent to represent a redirect.
    Change-Id: If0678735627240179f14e136304103fc5eaa4145

--- repo/includes/content/ItemHandler.php
+++ repo/includes/content/ItemHandler.php
@@ -3,6 +3,8 @@
 namespace Wikibase;
 
 use RequestContext;
+use Content;
+use MWException;
 use Title;
 use User;
 use Wikibase\Repo\WikibaseRepo;
@@ -52,8 +54,39 @@ class ItemHandler extends EntityHandler {
 	 * @return ItemContent
 	 */
 	public function unserializeContent( $blob, $format = null ) {
-		$entity = EntityFactory::singleton()->newFromBlob( Item::ENTITY_TYPE, $blob, $format );
+		$data = $this->unserializedData( $blob, $format );
+
+		if ( !empty( $data['redirect'] ) ) {
+			$target = new EntityId( $data['redirect']['type'], $data['redirect']['id'] );
+			return ItemContent::newFromRedirect( $target );
+		}
+
+		$entity = EntityFactory::singleton()->newFromArray( $this->getEntityType(), $data );
 		return ItemContent::newFromItem( $entity );
+	}
+
+	/**
+	 * @param \Content $content
+	 * @param null     $format
+	 *
+	 * @return string
+	 */
+	public function serializeContent( \Content $content, $format = null ) {
+		/* @var ItemContent $content */
+		if ( $content->isRedirect() ) {
+			$target = $content->getRedirectTargetId();
+			$data = array(
+				'entity' => $content->getEntity()->getId(),
+				'redirect' => array(
+					'type' => $target->getEntityType(),
+					'id' => $target->getNumericId(),
+				)
+			);
+
+			return $this->serializeData( $data, $format );
+		}
+
+		return parent::serializeContent( $content, $format );
 	}
 
 	/**
@@ -143,6 +176,41 @@ class ItemHandler extends EntityHandler {
 	 */
 	public function getEntityType() {
 		return Item::ENTITY_TYPE;
+	}
+
+	/**
+	 * Creates a new ItemContent object that acts as a redirect to the given page.
+	 *
+	 * @see ContentHandler::makeRedirectContent
+	 *
+	 * @param Title $destination the page to redirect to. Must be an existing wikibase Item.
+	 *
+	 * @throws \MWException
+	 * @return ItemContent
+	 */
+	public function makeRedirectContent( Title $destination, $text = '' ) {
+		$ns = $destination->getNamespace();
+
+		if ( $ns !== $this->getEntityNamespace() ) {
+			throw new MWException( "Items can only redirect to other items." );
+		}
+
+		if ( !$destination->exists() ) {
+			throw new MWException( "Items can only redirect to existing items." );
+		}
+
+		//TODO: mapping from titles to ids should be encapsulated elsewhere, e.g. in EntityTitleLookup
+		$id = EntityId::newFromPrefixedId( $destination->getText() );
+
+		if ( !$id ) {
+			throw new MWException( "Page title is not a valid item ID: " . $destination->getText() );
+		}
+
+		if ( $id->getEntityType() !== Item::ENTITY_TYPE  ) {
+			throw new MWException( "Page title is not a valid item ID: " . $destination->getText() );
+		}
+
+		return ItemContent::newFromRedirect( $id );
 	}
 }
 
