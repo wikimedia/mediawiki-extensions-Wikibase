20413766ab2447cf914b8ff6c6ae63da9cef9e38
addshore <addshorewiki@gmail.com>
2013-08-28 13:36:17 +0200

    Create ChangeOpClaim
    Change-Id: I7d8a56c447031bf1c3debb8c4db4769d59d0e00f

---
+++ repo/includes/changeop/ChangeOpClaim.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Wikibase;
+
+use InvalidArgumentException;
+
+/**
+ * Class for claim change operation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @since 0.4
+ *
+ * @ingroup WikibaseRepo
+ *
+ * @licence GNU GPL v2+
+ * @author Adam Shorland
+ */
+class ChangeOpClaim extends ChangeOp {
+
+	/**
+	 * @since 0.4
+	 *
+	 * @var Claim
+	 */
+	protected $claim;
+
+	/**
+	 * @since 0.4
+	 *
+	 * @var array
+	 */
+	protected $action;
+
+	/**
+	 * @since 0.4
+	 *
+	 * @param Claim $claim
+	 * @param string $action should be add|remove
+	 *
+	 * @throws InvalidArgumentException
+	 */
+	public function __construct( $claim, $action ) {
+		if ( !$claim instanceof Claim ) {
+			throw new InvalidArgumentException( '$claim needs to be an instance of Claim' );
+		}
+
+		if ( !is_string( $action ) ) {
+			throw new InvalidArgumentException( '$action needs to be a string' );
+		}
+
+		$this->claim = $claim;
+		$this->action = $action;
+	}
+
+	/**
+	 * Applies the change to the given entity
+	 *
+	 * @since 0.4
+	 *
+	 * @param Entity $entity
+	 * @param Summary|null $summary
+	 *
+	 * @return bool
+	 *
+	 * @throws ChangeOpException
+	 */
+	public function apply( Entity $entity, Summary $summary = null ) {
+		if ( $this->action === "add" ) {
+			$entity->addClaim( $this->claim );
+			$this->updateSummary( $summary, 'add' );
+		} elseif ( $this->action === "remove" ) {
+			$claims = new Claims ( $entity->getClaims() );
+			$claims->removeClaim( $this->claim );
+			$entity->setClaims( $claims );
+			$this->updateSummary( $summary, 'remove' );
+		} else {
+			throw new ChangeOpException( "Unknown action for change op: $this->action" );
+		}
+		return true;
+	}
+}
