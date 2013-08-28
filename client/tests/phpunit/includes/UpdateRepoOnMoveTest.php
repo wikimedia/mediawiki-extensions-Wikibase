8e91aefa691dca0808878ec9453f28cca4a0cf11
jeroendedauw <jeroendedauw@gmail.com>
2013-08-28 21:26:13 +0200

    Fix incorrect construction of item id
    Change-Id: Iab325674d6c17947be5324cb13706b5fcd7b8b70

--- client/tests/phpunit/includes/UpdateRepoOnMoveTest.php
+++ client/tests/phpunit/includes/UpdateRepoOnMoveTest.php
@@ -1,27 +1,12 @@
 <?php
 namespace Wikibase\Test;
 
+use Wikibase\DataModel\Entity\ItemId;
 use Wikibase\UpdateRepoOnMove;
 use Wikibase\Settings;
-use Wikibase\EntityId;
 
 /**
  * Tests for the UpdateRepoOnMove class.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * http://www.gnu.org/copyleft/gpl.html
  *
  * @file
  * @since 0.4
@@ -43,7 +28,7 @@ class UpdateRepoOnMoveTest extends \MediaWikiTestCase {
 		static $ret = array();
 
 		if ( !$ret ) {
-			$entityId = new EntityId( 'Item', 123 );
+			$entityId = new ItemId( 'Q123' );
 
 			$siteLinkLookupMock = $this->getMockBuilder( '\Wikibase\SiteLinkLookup' )
 				->disableOriginalConstructor()
