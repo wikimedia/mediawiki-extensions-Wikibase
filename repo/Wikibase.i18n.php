2c1f1370e56839f3d3661e89ec6922ed4533033d
Michał Łazowik <mlazowik@gmail.com>
2013-09-04 19:11:58 +0200

    Extend API to support editing badges (DNM)
    Adds ability to edit badges through wbeditentity
    and wbsetsitelinks API modules.
    
    Bug: 40810
    
    Change-Id: I27e9c5e3e86ed04813e60787abe0e0856f2893b3

--- repo/Wikibase.i18n.php
+++ repo/Wikibase.i18n.php
@@ -20,6 +20,7 @@ $messages = array();
  * @author Jens Ohlig
  * @author Anja Jentzsch
  * @author Daniel Werner
+ * @author Michał Łazowik
  */
 $messages['en'] = array(
 	'wikibase-desc' => 'Structured data repository',
@@ -313,6 +314,7 @@ to find out where it went.',
 	'wikibase-item-summary-wbsetsitelink-add' => 'Added link to [$2]',
 	'wikibase-item-summary-wbsetsitelink-set' => 'Changed link to [$2]',
 	'wikibase-item-summary-wbsetsitelink-remove' => 'Removed link to [$2]',
+	'wikibase-item-summary-wbsetsitelink-badges' => 'badges',
 	'wikibase-item-summary-wblinktitles-create' => 'Created this item to connect pages [$2↔$3]',
 	'wikibase-item-summary-wblinktitles-connect' => 'Added a language link to connect pages [$2↔$3]',
 	'wikibase-item-summary-wbcreateclaim-value' => 'Created {{PLURAL:$1|a claim|claims}}', // legacy, used for old log entries!
@@ -1069,6 +1071,7 @@ Parameters:
 	'wikibase-item-summary-wbsetsitelink-set' => '{{doc-important|Translate this as "Changed link which points to <nowiki>[$2]</nowiki>".}}
 {{wikibase summary messages|sitelinks|Automatic edit summary (autocomment) when editing a language link. Such a summary could appear in English as "Changed link to [itwiki]:  Italia".}}',
 	'wikibase-item-summary-wbsetsitelink-remove' => '{{wikibase summary messages|sitelinks|Automatic edit summary (autocomment) when removing a language link from a Wikidata item (page). Such a summary could appear in English as "Removed link to [itwiki]".}}',
+	'wikibase-item-summary-wbsetsitelink-badges' => '{{wikibase summary messages|sitelinks|Part of automatic edit summary (autocomment) indicating that badges were changed. This message preceeds the list of badges.}}',
 	'wikibase-item-summary-wblinktitles-create' => '{{wikibase summary messages|item|Automatic edit summary when connecting page(s) by creating a new data item.}}
 
 This module generates a slightly different summary (autocomment) than the other ones.
@@ -8467,7 +8470,7 @@ tapno birukem ti napananda.',
 * pagsasao – nalawag a pateg para iti pananginagan ti pagsasao para iti testo a paset
 * pateg – nalawag a pateg para iti kuerdas ti naisangsangayan a kita ti pagsasao',
 	'wikibase-listdatatypes-string-head' => 'Kuerdas',
-	'wikibase-listdatatypes-string-body' => 'Literal a pagikabilan ti datos para iti maysa a kuerdas dagiti karakter. Ti kadawyan a panag-usar ket dagiti pananginagan nga adda dagiti naisurat a porma a saan nga agtaltalek iti pagsasao iti agbasbasa. 
+	'wikibase-listdatatypes-string-body' => 'Literal a pagikabilan ti datos para iti maysa a kuerdas dagiti karakter. Ti kadawyan a panag-usar ket dagiti pananginagan nga adda dagiti naisurat a porma a saan nga agtaltalek iti pagsasao iti agbasbasa.
 
 * pateg– nalawag a pateg para iti kuerdas ti naisangsangayan a kita ti kuerdas',
 	'wikibase-listdatatypes-time-head' => 'Oras',
@@ -9702,7 +9705,7 @@ $messages['kk-cyrl'] = array(
 	'wikibase-snakview-snaktypeselector-somevalue' => 'белгісіз мән',
 	'wikibase-snakview-snaktypeselector-novalue' => 'мән жоқ',
 	'wikibase-entityselector-more' => 'толығырақ',
-	'wikibase-anonymouseditwarning' => 'Ескерту: Сіз жүйеге кірмегенсіз. 
+	'wikibase-anonymouseditwarning' => 'Ескерту: Сіз жүйеге кірмегенсіз.
 IP-мекенжайыңыз бұл  $1 түзету тарихында жазылып алынады.',
 	'wikibase-restrictionedit-tooltip-message' => 'Бұл бет құлыпталған. Өңдеуге рұқсатыңыз жоқ.',
 	'wikibase-blockeduser-tooltip-message' => 'Қатысушы атыңыз не IP мекенжайыңыз бұғатталғандықтан өңдеуге рұқсатыңыз жоқ.',
@@ -16126,7 +16129,7 @@ $messages['yi'] = array(
 	'wikibase-shortcopyrightwarning' => 'דורך קליקן "$1", זענט איר מסכים צו די [[$2|ניצבאדינגונגען]], און איר זענט מסכים אָן חרטה צו פארעפנטלעכן אייער ביישטייערונג אונטער דעם $3.',
 	'wikibase-copyrighttooltip-acknowledge' => 'איך נעם אָן די באדינגונגען פֿאר מײַנע קומעדיקע רעדאקטירונגען. ווײַזט נישט מער די מעלדונג.',
 	'wikibase-entityselector-more' => 'נאך',
-	'wikibase-anonymouseditwarning' => 'ווארענונג: איר זענט נישט אריינלאגירט. 
+	'wikibase-anonymouseditwarning' => 'ווארענונג: איר זענט נישט אריינלאגירט.
 אייער IP־אדרעס וועט ווערן דאקומענטירט אין דעם רעדאקטירן היסטאריע פון דעם אביעקט.',
 	'wikibase-restrictionedit-tooltip-message' => 'דער בלאט איז געשיצט. מען טאר נישט רעדאקטירן.',
 	'wikibase-blockeduser-tooltip-message' => 'איר האט נישט קיין דערלויבניש צו רעדאקטירן ווייל אייער באניצער־נאמען אדער IP אדרעס איז געווארן בלאקירט.',
