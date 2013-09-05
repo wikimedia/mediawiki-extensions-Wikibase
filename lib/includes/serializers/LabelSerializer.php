3b67b4899a24d1288a6f6067b462a7b025e95a8a
daniel <daniel.kinzler@wikimedia.de>
2013-09-05 17:28:05 +0200

    (bug 52801) Documenting external JSON format
    Change-Id: If08bd31116589b03aec93c253c31dd666df4267b

--- lib/includes/serializers/LabelSerializer.php
+++ lib/includes/serializers/LabelSerializer.php
@@ -7,24 +7,10 @@ use InvalidArgumentException;
 /**
  * Serializer for labels.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * http://www.gnu.org/copyleft/gpl.html
+ * See docs/json.wiki for details of the format.
  *
  * @since 0.4
  *
- * @file
  * @ingroup WikibaseLib
  *
  * @licence GNU GPL v2+
@@ -81,6 +67,8 @@ class LabelSerializer extends SerializerObject {
 		if ( !is_array( $labels ) ) {
 			throw new InvalidArgumentException( 'LabelSerializer can only serialize an array of labels' );
 		}
+
+		//NOTE: when changing the serialization structure, update docs/json.wiki too!
 
 		$value = $this->multilingualSerializer->serializeMultilingualValues( $labels );
 
--- lib/includes/serializers/ReferenceSerializer.php
+++ lib/includes/serializers/ReferenceSerializer.php
@@ -7,24 +7,10 @@ use Wikibase\Reference;
 /**
  * Serializer for Reference objects.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * http://www.gnu.org/copyleft/gpl.html
+ * See docs/json.wiki for details of the format.
  *
  * @since 0.3
  *
- * @file
  * @ingroup Wikibase
  *
  * @licence GNU GPL v2+
@@ -46,6 +32,8 @@ class ReferenceSerializer extends SerializerObject implements Unserializer {
 		if ( !( $reference instanceof Reference ) ) {
 			throw new MWException( 'ReferenceSerializer can only serialize Reference objects' );
 		}
+
+		//NOTE: when changing the serialization structure, update docs/json.wiki too!
 
 		$serialization = array();
 
