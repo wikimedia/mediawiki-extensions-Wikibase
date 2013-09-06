f8606cf0456c4bd1068ccea7c08479564df04e1d
Tobias Gritschacher <tobias.gritschacher@wikimedia.de>
2013-09-06 15:49:41 +0200

    Use cucumber for aliases tests
    Change-Id: Iaeb4acbe0e6e7575804a48d05999be4a89bbe091

---
+++ selenium_cuc/features/step_definitions/aliases_steps.rb
@@ -0,0 +1,38 @@
+# -*- encoding : utf-8 -*-
+# Wikidata UI tests
+#
+# Author:: Tobias Gritschacher (tobias.gritschacher@wikimedia.de)
+# License:: GNU GPL v2+
+#
+# steps for item aliases
+
+Then /^Aliases UI should be there$/ do
+  on(ItemPage) do |page|
+    page.aliasesDiv?.should be_true
+    page.aliasesTitle?.should be_true
+  end
+end
+
+Then /^Aliases add button should be there$/ do
+  on(ItemPage).addAliases?.should be_true
+end
+
+Then /^Aliases add button should not be there$/ do
+  on(ItemPage).addAliases?.should be_false
+end
+
+Then /^Aliases edit button should be there$/ do
+  on(ItemPage).editAliases?.should be_true
+end
+
+Then /^Aliases edit button should not be there$/ do
+  on(ItemPage).editAliases?.should be_false
+end
+
+Then /^Aliases list should be empty$/ do
+  on(ItemPage).aliasesList?.should be_false
+end
+
+Then /^Aliases list should not be empty$/ do
+  on(ItemPage).aliasesList?.should be_true
+end
