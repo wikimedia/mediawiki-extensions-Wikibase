4434036b8871a5bc3806118e1601f9f7ccce4568
Daniel Werner <daniel.werner@wikimedia.de>
2013-09-10 17:51:58 +0200

    (bug 52387) Introduction of FetchedEntityContentSerializer
    Moved the code for creating special serialization as required by the front-end (deserialized in
    wb.serialization.FetchedContentUnserializer) in its own class. This is based on the current
    serialization system which has some design issues.
    The whole concept of how we get these informations to the front-end should be redesigned at some
    point, for now this is a change for saving some code when requiring the same logic on special
    pages where our front-end widgets requiring this information should be used. This also adds some
    basic test to previously untested functionality.
    
    Change-Id: I6d53bdda60297f79cec192d19f3ca1534e652284

---
+++ lib/includes/serializers/FetchedEntityContentSerializationOptions.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Wikibase\Lib\Serializers;
+
+/**
+ * Serialization options for FetchedEntityContentSerializer.
+ *
+ * @since 0.5
+ * @licence GNU GPL v2+
+ * @author Daniel Werner < daniel.a.r.werner@gmail.com >
+ */
+class FetchedEntityContentSerializationOptions extends SerializationOptions {
+	/**
+	 * @since 0.5
+	 * @var SerializationOptions|null
+	 */
+	protected $contentSerializer;
+
+	/**
+	 * @since 0.5
+	 *
+	 * @param SerializationOptions|null $entitySerializationOptions The serializer options which
+	 *        will be used when serializing the Entity contained by the EntityContent instance.
+	 *
+	 * NOTE: Originally a "contentSerializer" option has been intended without some refactoring
+	 *       since for example EntitySerializer can not be used for serializing all kinds of
+	 *       entities, instead a Property- or ItemSerializer have to be provided.
+	 *       The whole serializer system is quite ugly, this just adds up to this, the whole thing
+	 *       should be refactored in one go.
+	 */
+	public function __construct( SerializationOptions $entitySerializationOptions = null ) {
+		$this->setEntitySerializationOptions( $entitySerializationOptions );
+	}
+
+	/**
+	 * For getting the "entitySerializationOptions" option.
+	 *
+	 * @since 0.5
+	 *
+	 * @return SerializationOptions|null
+	 */
+	public function getEntitySerializationOptions() {
+		return $this->contentSerializer;
+	}
+
+	/**
+	 * For setting the "entitySerializationOptions" option.
+	 *
+	 * @since 0.5
+	 *
+	 * @param SerializationOptions $options|null
+	 */
+	public function setEntitySerializationOptions( SerializationOptions $options = null ) {
+		$this->contentSerializer = $options;
+	}
+}
---
+++ lib/includes/serializers/FetchedEntityContentSerializer.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Wikibase\Lib\Serializers;
+
+use ApiResult;
+use InvalidArgumentException;
+use Content;
+use MWException;
+use Wikibase\Repo\WikibaseRepo;
+use Wikibase\EntityContent;
+
+/**
+ * Serializer for some information related to Content. This is not a full Content serialization,
+ * instead the serialized object will contain information required by the UI to create a
+ * FetchedEntityContent instance in JavaScript.
+ *
+ * @since 0.5
+ * @licence GNU GPL v2+
+ * @author Daniel Werner < daniel.a.r.werner@gmail.com >
+ */
+class FetchedEntityContentSerializer extends SerializerObject {
+	/**
+	 * @see SerializerObject::$options
+	 * @var FetchedEntityContentSerializationOptions
+	 */
+	protected $options;
+
+	/**
+	 * Constructor.
+	 *
+	 * @since 0.5
+	 *
+	 * @param FetchedEntityContentSerializationOptions $options
+	 */
+	public function __construct( FetchedEntityContentSerializationOptions $options = null ) {
+		if( $options === null ) {
+			$options = new FetchedEntityContentSerializationOptions();
+		}
+		parent::__construct( $options );
+	}
+
+	/**
+	 * @see Serializer::getSerialized
+	 *
+	 * @since 0.5
+	 *
+	 * @param EntityContent $entityContent
+	 * @return array
+	 *
+	 * @throws InvalidArgumentException If $entityContent is no instance of Content.
+	 */
+	public function getSerialized( $entityContent ) {
+		if( !( $entityContent instanceof Content ) ) {
+			throw new InvalidArgumentException(
+				'FetchedEntityContentSerializer can only serialize Content objects' );
+		}
+
+		/** @var $entity Entity */
+		$entity = $entityContent->getEntity();
+		$entitySerializationOptions = $this->options->getEntitySerializationOptions();
+
+		$serializerFactory = new SerializerFactory();
+		$entitySerializer = $serializerFactory->newSerializerForObject(
+			$entity,
+			$entitySerializationOptions
+		);
+		$serialization['content'] = $entitySerializer->getSerialized( $entity );
+		$serialization['title'] = $entityContent->getTitle()->getPrefixedText();
+
+		$entityPageRevision = $entityContent->getWikiPage()->getRevision();
+
+		if( !$entityPageRevision ) {
+			$serialization['revision'] = '';
+		} else {
+			$serialization['revision'] = $entityPageRevision->getId();
+		}
+
+		return $serialization;
+	}
+
+	/**
+	 * Creates a new instance suitable for EntityContent serializations in a form as required in the
+	 * frontend's "wikibase.fetchedEntities" global.
+	 *
+	 * @since 0.5
+	 *
+	 * @param string $primaryLanguage
+	 * @return FetchedEntityContentSerializer
+	 */
+	public static function newForFrontendStore( $primaryLanguage ) {
+		$entitySerializationOptions =
+			new EntitySerializationOptions( WikibaseRepo::getDefaultInstance()->getIdFormatter() );
+		$entitySerializationOptions->setProps( array( 'labels', 'descriptions', 'datatype' ) );
+		$entitySerializationOptions->setLanguages( array( $primaryLanguage ) );
+
+		$fetchedEntityContentSerializationOptions =
+			new FetchedEntityContentSerializationOptions( $entitySerializationOptions );
+
+		$serializer = new FetchedEntityContentSerializer( $fetchedEntityContentSerializationOptions );
+		return $serializer;
+	}
+}
