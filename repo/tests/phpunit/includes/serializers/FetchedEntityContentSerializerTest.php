5f7b18dd1ecf2b25d657dd925a0ba833285341b5
Daniel Werner <daniel.werner@wikimedia.de>
2013-09-10 20:26:39 +0200

    (bug 52387) Introduction of FetchedEntityContentSerializer
    Moved the code for creating special serialization as required by the front-end (deserialized in
    wb.serialization.FetchedContentUnserializer) in its own class. This is based on the current
    serialization system which has some design issues.
    
    The whole concept of how we get these informations to the front-end should be redesigned at some
    point, for now this is a change for saving some code when requiring the same logic on special
    pages where our front-end widgets requiring this information should be used. This also adds some
    basic test to previously untested functionality.
    
    Change-Id: I6d53bdda60297f79cec192d19f3ca1534e652284

---
+++ repo/tests/phpunit/includes/serializers/FetchedEntityContentSerializerTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Wikibase\Test;
+
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\Serializers\FetchedEntityContentSerializer;
+use Wikibase\Serializers\FetchedEntityContentSerializationOptions;
+use Wikibase\Lib\Serializers\EntitySerializationOptions;
+use Wikibase\PropertyContent;
+use Wikibase\PropertyHandler;
+use Wikibase\Repo\WikibaseRepo;
+use Title;
+
+/**
+ * @covers Wikibase\Serializers\FetchedEntityContentSerializer
+ *
+ * @group WikibaseLib
+ * @group Wikibase
+ * @group WikibaseSerialization
+ *
+ * @since 0.5
+ * @licence GNU GPL v2+
+ * @author Daniel Werner < daniel.a.r.werner@gmail.com >
+ */
+class FetchedEntityContentSerializerTest extends SerializerBaseTest {
+	/**
+	 * @see SerializerBaseTest::getClass
+	 *
+	 * @since 0.5
+	 *
+	 * @return string
+	 */
+	protected function getClass() {
+		return '\Wikibase\Serializers\FetchedEntityContentSerializer';
+	}
+
+	/**
+	 * @see SerializerBaseTest::validProvider
+	 *
+	 * @since 0.5
+	 *
+	 * @return array
+	 */
+	public function validProvider() {
+		$entityContent = PropertyContent::newEmpty();
+		$entitySerializerOptions = new EntitySerializationOptions(
+			WikibaseRepo::getDefaultInstance()->getIdFormatter() );
+		$entityContentSerializerOptions =
+			new FetchedEntityContentSerializationOptions( $entitySerializerOptions );
+
+		$entity = $entityContent->getEntity();
+		$entity->setId( new PropertyId( 'P20' ) );
+		$entity->setDataTypeId( 'foo' );
+
+		$propertyHandler = new PropertyHandler();
+		$expectedEntityPageTitle =  Title::makeTitle(
+			$propertyHandler->getEntityNamespace(), 'P20' );
+
+		$validArgs[] = array(
+			$entityContent,
+			array(
+				'title' => $expectedEntityPageTitle->getPrefixedText(),
+				'revision' => '',
+				'content' => array(
+					'id' => 'P20',
+					'type' => $propertyHandler->getEntityType(),
+					'datatype' => 'foo'
+				)
+			),
+			$entityContentSerializerOptions
+		);
+
+		return $validArgs;
+	}
+
+	/**
+	 * @since 0.5
+	 */
+	public function testNewForFrontendStore() {
+		$serializer = FetchedEntityContentSerializer::newForFrontendStore( 'en' );
+		$this->assertInstanceOf( $this->getClass(), $serializer );
+	}
+}
