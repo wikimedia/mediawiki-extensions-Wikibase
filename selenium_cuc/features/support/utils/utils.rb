fcfa8be2ae7f519c154c5b90b6c45debeabc0f82
Tobias Gritschacher <tobias.gritschacher@wikimedia.de>
2013-09-10 17:59:20 +0200

    Use cucumber for sitelink tests
    Bug: 53848
    Change-Id: Ic68110ed23755eac872216fc25d9d043c9617bc9

--- selenium_cuc/features/support/utils/utils.rb
+++ selenium_cuc/features/support/utils/utils.rb
@@ -45,3 +45,30 @@ def create_new_entity(data, type = 'item')
   entity = {"id" => id, "url" => url, "label" => entity_data["labels"]["en"]["value"], "description" => entity_data["descriptions"]["en"]["value"]}
   return entity
 end
+
+# removes a sitelink
+def remove_sitelinks(siteid, pagename)
+  uri = URI(URL.repo_api)
+
+  request = Net::HTTP::Post.new(uri.to_s)
+  request.set_form_data(
+      'action' => 'wbsetsitelink',
+      'token' => '+\\',
+      'site' => siteid,
+      'title' => pagename,
+      'linksite' => siteid,
+      'format' => 'json',
+      'summary' => 'sitelink removed by selenium test'
+  )
+
+  response = Net::HTTP.start(uri.hostname, uri.port) do |http|
+    http.request(request)
+  end
+  resp = ActiveSupport::JSON.decode(response.body)
+
+  if resp["success"] != 1 && resp["error"]["code"] != 'no-such-entity-link'
+    abort('Failed to create new entity: API error')
+  end
+
+  return true
+end
