77b79e9b1cba711fcc71df9546c4fac17c2ce399
aude <aude.wiki@gmail.com>
2013-09-11 18:56:34 +0000

    Store serialized entity in parser output (DRAFT)
    The structured data can be passed to OutputPage, where it can be
    rendered and used in other ways, skins (e.g. mobile), etc.
    
    Change-Id: If5a9fb5415552b4e8fc259ebfa1a3f6693d2ac7b

--- lib/includes/serializers/DescriptionSerializer.php
+++ lib/includes/serializers/DescriptionSerializer.php
@@ -7,21 +7,6 @@ use InvalidArgumentException;
 /**
  * Serializer for descriptions.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * http://www.gnu.org/copyleft/gpl.html
- *
  * @since 0.4
  *
  * @file
@@ -29,8 +14,9 @@ use InvalidArgumentException;
  *
  * @licence GNU GPL v2+
  * @author Tobias Gritschacher < tobias.gritschacher@wikimedia.de >
+ * @author Katie Filbert < aude.wiki@gmail.com >
  */
-class DescriptionSerializer extends SerializerObject {
+class DescriptionSerializer extends SerializerObject implements Unserializer {
 
 	/**
 	 * @see ApiSerializerObject::$options
@@ -106,5 +92,27 @@ class DescriptionSerializer extends SerializerObject {
 	public final function getSerializedMultilingualValues( $descriptions ) {
 		$descriptions = $this->multilingualSerializer->filterPreferredMultilingualValues( $descriptions );
 		return $this->getSerialized( $descriptions );
+	}
+
+	/**
+	 * @see Unserializer::newFromSerialization
+	 *
+	 * @since 0.4
+	 *
+	 * @param array $descriptions
+	 *
+	 * @return array
+	 */
+	public function newFromSerialization( array $descriptions ) {
+		$data = array();
+
+		foreach( $descriptions as $description ) {
+			if ( is_array( $description ) ) {
+				$lang = $description['language'];
+				$data[$lang] = $description['value'];
+			}
+		}
+
+		return $data;
 	}
 }
