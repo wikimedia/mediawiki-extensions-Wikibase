f781d72e8a3fb23056c6f79a2904bbbb2ad618ad
Daniel Werner <daniel.werner@wikimedia.de>
2013-09-13 16:28:58 +0200

    Introduction of FetchedEntitiesFinder::findDataValueLinks
    Change-Id: I7e105441533e0060500de3cf214b0eeeb843ac29

--- lib/includes/ReferencedEntitiesFinder.php
+++ lib/includes/ReferencedEntitiesFinder.php
@@ -3,6 +3,7 @@
 namespace Wikibase;
 
 use Wikibase\DataModel\Entity\EntityIdValue;
+use DataValues\DataValue;
 
 /**
  * Finds linked entities given a list of entities or a list of claims.
@@ -14,12 +15,15 @@ use Wikibase\DataModel\Entity\EntityIdValue;
  *
  * @licence GNU GPL v2+
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
- * @author Daniel Werner < daniel.werner@wikimedia.de >
+ * @author Daniel Werner < daniel.a.r.werner@gmail.com >
+ * @author Katie Filbert
  * @author Daniel Kinzler
  */
 class ReferencedEntitiesFinder {
 
 	/**
+	 * Finds linked entities within a set of snaks.
+	 *
 	 * @param Snak[] $snaks
 	 *
 	 * @return EntityId[]
@@ -40,22 +44,34 @@ class ReferencedEntitiesFinder {
 					continue;
 				}
 
-				switch( $snakValue->getType() ) {
-					case 'wikibase-entityid':
-						if( $snakValue instanceof EntityIdValue ) {
-							$foundEntities[] = $snakValue->getEntityId();
-						}
-						break;
-					// TODO: handle values in other formats. E.g. in an earlier version the
-					//  'wikibase-entity' data type has been using 'string' values to store its ID.
-
-					// TODO: we might want to allow extensions to add handling for their custom
-					//  data value types here. Either use a hook or a proper registration for that.
-				}
+				$entitiesInSnakDataValue = $this->findDataValueLinks( $snakValue );
+				$foundEntities = array_merge( $foundEntities, $entitiesInSnakDataValue );
 			}
 		}
 
 		return array_unique( $foundEntities );
 	}
 
+	/**
+	 * Finds linked entities within a given data value.
+	 *
+	 * @since 0.5
+	 *
+	 * @param DataValue $dataValue
+	 * @return EntityId[]
+	 */
+	public function findDataValueLinks( DataValue $dataValue ) {
+		switch( $dataValue->getType() ) {
+			case 'wikibase-entityid':
+				if( $dataValue instanceof EntityIdValue ) {
+					return array(
+						$dataValue->getEntityId() );
+				}
+				break;
+
+			// TODO: we might want to allow extensions to add handling for their custom
+			//  data value types here. Either use a hook or a proper registration for that.
+		}
+		return array();
+	}
 }
