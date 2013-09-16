c234a24889367ac097022c66f47da45aff11df01
addshore <addshorewiki@gmail.com>
2013-09-16 12:57:00 +0200

    Split ClaimModificationHelper
    There have been todos all around the code to
    improve the reuse, some methods were shoved in
    the claim modification helper when really they should
    have their own helper.
    
    This change splits the logic into two helpers and
    deprecates the methods that should no longer be used
    
    Changing the actual use will come in a followup
    
    Change-Id: Ic8822a3d82224556b125ceb0753ac05d703c82f7

--- repo/tests/phpunit/includes/api/ClaimModificationHelperTest.php
+++ repo/tests/phpunit/includes/api/ClaimModificationHelperTest.php
@@ -2,40 +2,20 @@
 
 namespace Wikibase\Test\Api;
 
+use UsageException;
 use Wikibase\Api\CreateClaim;
-use Wikibase\EntityId;
 use ApiMain;
 use Wikibase\Api\ClaimModificationHelper;
 use Wikibase\Api\SnakValidationHelper;
 use Wikibase\Repo\WikibaseRepo;
 use Wikibase\ItemContent;
 use Wikibase\Claims;
-use Wikibase\PropertyContent;
-use Wikibase\Lib\ClaimGuidValidator;
 use Wikibase\Validators\ValidatorErrorLocalizer;
 
 /**
  * @covers Wikibase\Api\ClaimModificationHelper
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
- *
- * @file
+
  * @since 0.4
- *
- * @ingroup Wikibase
- * @ingroup Test
  *
  * @group Wikibase
  * @group WikibaseRepo
