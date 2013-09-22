48cdb903ace2ea936aec1bed2934ba3f3f089be6
jeroendedauw <jeroendedauw@gmail.com>
2013-09-22 17:32:32 +0200

    Adding dataTypeId field to PropertyValueSnak [DNM]
    Change-Id: I90460821c542ecf79a37a20b77be29a0c6da6ead

--- lib/includes/serializers/SnakSerializer.php
+++ lib/includes/serializers/SnakSerializer.php
@@ -1,27 +1,15 @@
 <?php
 
 namespace Wikibase\Lib\Serializers;
+
+use InvalidArgumentException;
 use MWException;
+use Wikibase\PropertyValueSnak;
 use Wikibase\Snak;
 use Wikibase\SnakObject;
 
 /**
  * Serializer for Snak objects.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * http://www.gnu.org/copyleft/gpl.html
  *
  * @since 0.2
  *
@@ -54,16 +42,24 @@ class SnakSerializer extends SerializerObject implements Unserializer {
 
 		$serialization['property'] = $snak->getPropertyId()->getPrefixedId();
 
-		// TODO: we might want to include the data type of the property here as well
-
 		if ( $snak->getType() === 'value' ) {
 			/**
-			 * @var \Wikibase\PropertyValueSnak $snak
+			 * @var PropertyValueSnak $snak
 			 */
 			$serialization['datavalue'] = $snak->getDataValue()->toArray();
+
+			$serialization['datatype'] = $this->getDataTypeOfSnak( $snak );
 		}
 
 		return $serialization;
+	}
+
+	protected function getDataTypeOfSnak( PropertyValueSnak $snak ) {
+		if ( is_string( $snak->getDataTypeId() ) ) {
+			return $snak->getDataTypeId();
+		}
+
+		// TODO: return fetch dataType based on prop id
 	}
 
 	/**
@@ -74,6 +70,7 @@ class SnakSerializer extends SerializerObject implements Unserializer {
 	 * @param array $serialization
 	 *
 	 * @return Snak
+	 * @throws InvalidArgumentException
 	 */
 	public function newFromSerialization( array $serialization ) {
 		// TODO: inject id parser
@@ -83,6 +80,14 @@ class SnakSerializer extends SerializerObject implements Unserializer {
 
 		if ( array_key_exists( 'datavalue', $serialization ) ) {
 			$constructorArguments[] = \DataValues\DataValueFactory::singleton()->newFromArray( $serialization['datavalue'] );
+		}
+
+		if ( array_key_exists( 'datatype', $serialization ) ) {
+			if ( !is_string( $serialization['datatype'] ) ) {
+				throw new InvalidArgumentException( 'The datatype should be a string' );
+			}
+
+			$constructorArguments[] = $serialization['datatype'];
 		}
 
 		return SnakObject::newFromType( $serialization['snaktype'], $constructorArguments );
