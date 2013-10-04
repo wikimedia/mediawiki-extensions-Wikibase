b094e0b0d33562f73bd1ab3b00a5c10a187d9753
addshore <addshorewiki@gmail.com>
2013-10-04 19:51:47 +0200

    Remove deprecated EntityContentFactory::singleton
    All use of this method was removed earlier
    
    Change-Id: I93ef9378dad3f87a6b1004024661122c8808ad43

--- repo/includes/content/EntityContentFactory.php
+++ repo/includes/content/EntityContentFactory.php
@@ -13,46 +13,12 @@ use Wikibase\Repo\WikibaseRepo;
 /**
  * Factory for EntityContent objects.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * http://www.gnu.org/copyleft/gpl.html
- *
  * @since 0.2
- *
- * @file
- * @ingroup WikibaseRepo
  *
  * @licence GNU GPL v2+
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
 class EntityContentFactory implements EntityTitleLookup {
-
-	/**
-	 * @since 0.2
-	 * @deprecated since 0.4
-	 *
-	 * @return EntityContentFactory
-	 */
-	public static function singleton() {
-		static $instance = false;
-
-		if ( $instance === false ) {
-			$instance = WikibaseRepo::getDefaultInstance()->getEntityContentFactory();
-		}
-
-		return $instance;
-	}
 
 	// TODO: inject this map and allow extensions to somehow extend it
 	protected static $typeMap = array(
