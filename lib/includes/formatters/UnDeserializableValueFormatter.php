dadd60bc7b89ce5224abdcebfd32ae8b2a2a5248
aude <aude.wiki@gmail.com>
2013-10-07 09:57:35 +0000

    (bug 55348) Handle formatting of undeserializable values
    this is a super simple solution that "unbreaks" diff display
    
    It would be nice to display the more details about the "bad" value
    
    Change-Id: I0eaa6a2b49b21e9ead1a0c6f3ebdd6616479b458

---
+++ lib/includes/formatters/UnDeserializableValueFormatter.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Wikibase\Lib;
+
+use Message;
+use ValueFormatters\FormatterOptions;
+use ValueFormatters\ValueFormatter;
+use ValueFormatters\ValueFormatterBase;
+
+/**
+ * Formatter for UnDeserializableValue
+ *
+ * @since 0.5
+ *
+ * @licence GNU GPL v2+
+ * @author Katie Filbert < aude.wiki@gmail.com >
+ */
+class UnDeserializableValueFormatter extends ValueFormatterBase {
+
+	const MESSAGE = 'message';
+
+	/**
+	 * @param FormatterOptions $options
+	 */
+	public function __construct( FormatterOptions $options ) {
+		$this->options = $options;
+
+		$this->options->defaultOption(
+			self::OPT_LANG,
+			'en'
+		);
+
+		$this->options->defaultOption(
+			self::MESSAGE,
+			new Message( 'wikibase-undeserializable-value' )
+		);
+	}
+
+	/**
+	 * Formats an UnDeserializableValue
+	 *
+	 * @since 0.5
+	 *
+	 * @param UnDeserializableValue
+	 *
+	 * @return string
+	 */
+	public function format( $dataValue ) {
+		$langCode = $this->options->getOption( self::OPT_LANG );
+
+		$msg = $this->options->getOption( self::MESSAGE );
+		$msg = $msg->inLanguage( $langCode );
+
+		return $msg->text();
+	}
+
+}
---
+++ lib/tests/phpunit/formatters/UnDeserializableValueFormatterTest.php
@@ -0,0 +1,49 @@
+<?php
+namespace Wikibase\Lib\Test;
+
+use Message;
+use DataValues\UnDeserializableValue;
+use ValueFormatters\FormatterOptions;
+use ValueFormatters\ValueFormatter;
+use Wikibase\Lib\UnDeserializableValueFormatter;
+
+/**
+ * @covers Wikibase\Lib\UnDeserializableValueFormatter
+ *
+ * @since 0.5
+ *
+ * @ingroup WikibaseLibTest
+ *
+ * @group WikibaseLib
+ *
+ * @licence GNU GPL v2+
+ * @author Katie Filbert < aude.wiki@gmail.com >
+ */
+class UnDeserializableValueFormatterTest extends \PHPUnit_Framework_TestCase {
+
+	public function testFormat() {
+		$message = $this->getMock( 'Message',
+			array( 'text' ),
+			array( 'wikibase-undeserializable-value' )
+		);
+
+		$message->expects( $this->any() )
+			->method( 'text' )
+			->will( $this->returnValue( 'bad value' ) );
+
+		$options = new FormatterOptions( array(
+			ValueFormatter::OPT_LANG => 'en',
+			UnDeserializableValueFormatter::MESSAGE => $message
+		) );
+
+		$formatter = new UnDeserializableValueFormatter( $options );
+		$value = new UnDeserializableValue(
+			'cookie',
+			'string',
+			'cannot understand!'
+		);
+
+		$this->assertEquals( $message->text(), $formatter->format( $value ) );
+	}
+
+}
