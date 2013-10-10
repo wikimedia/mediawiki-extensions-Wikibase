c237b7e764857b823c189415f7f495752de6d9da
Daniel Werner <daniel.werner@wikimedia.de>
2013-10-10 23:03:51 +0200

    introduces mw.ext.wikibase, wikibase.repo and wikibase.repo.fetchedEntities
    Declares that the global "wikibase" object should ideally be independent of MediaWiki. Therefore
    mw.ext.wikibase got defined to act as bridge between MediaWiki and Wikibase. Code for configuring
    Wikibase to be used in MediaWiki context should go there in the future. The Wikibase object and
    sub modules should increasingly become independent of MediaWiki.
    This requires a lot more work since this is currently violated throughout the entire codebase (not
    just in JavaScript either). The purpose of this change is mostly to provide a place for more future
    proof code.
    
    With this change, the way the wikibase.fetchedEntities object is being constructed in MediaWiki
    context got refactored accordingly. By default, wikibase.fetchedEntities is just an empty object,
    only in MediaWiki context it will be filled based on the 'wbUsedEntities' mediawiki.config variable
    provided by the backend. Also deprecates wikibase.fetchedEntities and moves it into
    wikibase.repo.fetchedEntities since the current structure of the fetched content objects contained
    in the object only makes sense in repo context.
    Increases the reusability of the fetchedEntities code (e.g. on special pages where WB-UI should be
    initialized) and moves the code into its own function.
    
    The whole way we are using the global "fetchedEntities" throughout the code base is still bad and
    we should still implement a proper store abstraction for proper injection.
    
    Change-Id: Id6ddb4941d828e28530a50e2ce3e18e7c279462b

---
+++ repo/resources/mw.ext.wikibase.repo/repo.fetchedEntitiesFromMwConfig.js
@@ -0,0 +1,63 @@
+/**
+ * @licence GNU GPL v2+
+ * @author Daniel Werner < daniel.a.r.werner@gmail.com >
+ *
+ * Takes MediaWiki's mediawiki.config.get( 'wbUsedEntities' ) and builds the global local entity
+ * store from that (wikibase.fetchedEntities).
+ * Will invoked immediately after script inclusion. Exposing this function is for testing purposes
+ * only.
+ *
+ * @private For testing the mechanism only.
+ *
+ * @since 0.5
+ *
+ * TODO: Should vanish together with wikibase.fetchedEntities as bug 54082 gets resolved.
+ */
+mediaWiki.ext.wikibase.repo.fetchedEntitiesFromMwConfig = ( function( $, wb, mwConfig ) {
+	'use strict';
+
+	var Entity = wb.Entity;
+	var FetchedContent = wb.store.FetchedContent;
+	var SerializerFactory = wb.serialization.SerializerFactory;
+
+	function fetchedEntitiesFromMwConfig() {
+		var unserializerFactory = new SerializerFactory();
+		var entityUnserializer = unserializerFactory.newUnserializerFor( Entity );
+
+		// unserializer for fetched content whose content is a wb.Entity:
+		var fetchedEntityUnserializer = unserializerFactory.newUnserializerFor(
+			FetchedContent, {
+				contentUnserializer: entityUnserializer
+			}
+		);
+
+		var serializedFetchedEntities = getSerializedFetchedEntitiesFromConfig( 'wbUsedEntities' );
+
+		$.each( serializedFetchedEntities, function( id, serializedFetchedEntity ) {
+			var fetchedEntity = fetchedEntityUnserializer.unserialize( serializedFetchedEntity );
+			wb.fetchedEntities[ id ] = fetchedEntity;
+		} );
+	}
+
+	/**
+	 * Helper for getting the serialized fetched entities in object form out of mw.config.
+	 *
+	 * @param {string} varName mw.config var name
+	 * @returns {Object}
+	 *
+	 * @throws {Error} In case the given config variable name does not exist.
+	 */
+	function getSerializedFetchedEntitiesFromConfig( varName ) {
+		if( !mwConfig.exists( varName ) ) {
+			throw new Error( 'Can not load data for wikibase.fetchedEntities. mw.config variable "'
+				+ varName + '" is not set.' );
+		}
+		return $.evalJSON( mwConfig.get( varName ) );
+	}
+
+	// Allows to simply add this resource loader module to the output.
+	fetchedEntitiesFromMwConfig();
+
+	return fetchedEntitiesFromMwConfig;
+
+}( jQuery, wikibase, mediaWiki.config ) );
