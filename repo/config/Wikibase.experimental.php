af90401b835427560540be8bc830c2d09d1d1aa5
jeroendedauw <jeroendedauw@gmail.com>
2013-10-12 20:52:46 +0200

    Remove the SetStatementRank API module as we are not going to use it
    Change-Id: I6a5755f59a202b2bf3da8611ef5d6e17309b854c

--- repo/config/Wikibase.experimental.php
+++ repo/config/Wikibase.experimental.php
@@ -6,26 +6,7 @@
  * This file is NOT an entry point the Wikibase extension. Use Wikibase.php.
  * It should furthermore not be included from outside the extension.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * http://www.gnu.org/copyleft/gpl.html
- *
  * @since 0.4
- *
- * @file
- * @ingroup WikibaseRepo
- *
  * @licence GNU GPL v2+
  */
 
@@ -34,13 +15,7 @@ if ( !defined( 'WB_VERSION' ) || !defined( 'WB_EXPERIMENTAL_FEATURES' ) ) {
 }
 
 call_user_func( function() {
-	global $wgAutoloadClasses, $wgAPIModules, $wgHooks;
-
-	$dir = __DIR__ . '/../';
-
-	$wgAutoloadClasses['Wikibase\Api\SetStatementRank']		= $dir . 'includes/api/SetStatementRank.php';
-
-	$wgAPIModules['wbsetstatementrank'] 				= 'Wikibase\Api\SetStatementRank';
+	global $wgHooks;
 
 	/**
 	 * Hook to add PHPUnit test cases.
@@ -55,7 +30,6 @@ call_user_func( function() {
 	$wgHooks['UnitTestsList'][] = function( array &$files ) {
 		// @codeCoverageIgnoreStart
 		$testFiles = array(
-			'api/SetStatementRank',
 		);
 
 		foreach ( $testFiles as $file ) {
