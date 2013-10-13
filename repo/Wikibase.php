52ac6b53cd8f7976a19738808cbe58e4bbf1ecb3
Bene <benestar.wikimedia@googlemail.com>
2013-10-13 22:56:29 +0200

    (bug 47496) Use (hidden) preference for copyright acknowledgment
    Created hidden preference for copyright acknowledgment
    
    Change-Id: I61c5a155ac41c9b799095212d9b8243fc1bfbbca

--- repo/Wikibase.php
+++ repo/Wikibase.php
@@ -168,6 +168,7 @@ call_user_func( function() {
 	$wgHooks['RecentChange_save'][]						= 'Wikibase\RepoHooks::onRecentChangeSave';
 	$wgHooks['ArticleDeleteComplete'][] 				= 'Wikibase\RepoHooks::onArticleDeleteComplete';
 	$wgHooks['ArticleUndelete'][]						= 'Wikibase\RepoHooks::onArticleUndelete';
+	$wgHooks['GetPreferences'][]						= 'Wikibase\RepoHooks::onGetPreferences';
 	$wgHooks['LinkBegin'][] 							= 'Wikibase\RepoHooks::onLinkBegin';
 	$wgHooks['OutputPageBodyAttributes'][] 				= 'Wikibase\RepoHooks::onOutputPageBodyAttributes';
 	//FIXME: handle other types of entities with autocomments too!
