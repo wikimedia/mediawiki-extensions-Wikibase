285fce33c43613c56eb690832776101cddd73cd6
aude <aude.wiki@gmail.com>
2013-10-16 23:08:36 +0000

    (bug 56018) add descriptions to entity search
    This also splits the search text generation into a separate class
    and improves tests.
    
    Change-Id: I08c9f5d10c83506cb540cc4c0c872e8958fa5a85

---
+++ repo/tests/phpunit/includes/EntitySearchTextGeneratorTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Wikibase\Test;
+
+use Wikibase\Entity;
+use Wikibase\Item;
+use Wikibase\Repo\EntitySearchTextGenerator;
+
+/**
+ * @covers Wikibase\Repo\EntitySearchTextGenerator
+ *
+ * @file
+ * @since 0.5
+ *
+ * @ingroup WikibaseRepoTest
+ * @ingroup Test
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ * @author Katie Filbert < aude.wiki@gmail.com >
+ */
+class EntitySearchTextGeneratorTest extends \PHPUnit_Framework_TestCase {
+
+	public function generateProvider() {
+		$item = Item::newEmpty();
+
+		$item->setLabel( 'en', 'Test' );
+		$item->setLabel( 'de', 'Testen' );
+		$item->setDescription( 'en', 'city in Spain' );
+		$item->setAliases( 'en', array( 'abc', 'cde' ) );
+		$item->setAliases( 'de', array( 'xyz', 'uvw' ) );
+
+		$patterns = array(
+			'/^Test$/',
+			'/^Testen$/',
+			'/^city in Spain$/',
+			'/^abc$/',
+			'/^cde$/',
+			'/^uvw$/',
+			'/^xyz$/',
+			'/^(?!abcde).*$/',
+		);
+
+		return array(
+			array( $item, $patterns )
+		);
+	}
+
+	/**
+	 * @dataProvider generateProvider
+	 *
+	 * @param Entity $entity
+	 * @param array $patterns
+	 */
+	public function testGenerate( Entity $entity, array $patterns ) {
+		$generator = new EntitySearchTextGenerator();
+		$text = $generator->generate( $entity );
+
+		foreach ( $patterns as $pattern ) {
+			$this->assertRegExp( $pattern . 'm', $text );
+		}
+	}
+
+}
