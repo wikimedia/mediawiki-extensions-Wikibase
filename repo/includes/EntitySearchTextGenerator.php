3d287a4a54db52fbd9f0a77b88ce8ba649f0c399
aude <aude.wiki@gmail.com>
2013-10-16 23:08:36 +0000

    (bug 56018) add descriptions to entity search
    This also splits the search text generation into a separate class
    and improves tests.
    
    Change-Id: I08c9f5d10c83506cb540cc4c0c872e8958fa5a85

---
+++ repo/includes/EntitySearchTextGenerator.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Wikibase\Repo;
+
+use Wikibase\Entity;
+
+/**
+ * @since 0.5
+ *
+ * @licence GNU GPL v2+
+ * @author Katie Filbert < aude.wiki@gmail.com >
+ */
+class EntitySearchTextGenerator {
+
+	/**
+	 * @param Entity $entity
+	 *
+	 * @return string
+	 */
+	public function generate( Entity $entity ) {
+		$labels = $entity->getLabels();
+		$text = $this->getArrayAsText( $labels );
+
+		$descriptions = $entity->getDescriptions();
+		$text .= "\n" . $this->getArrayAsText( $descriptions );
+
+		$allAliases = $entity->getAllAliases();
+		$text .= $this->getAllAliasesText( $allAliases );
+
+		return $text;
+	}
+
+	/**
+	 * @param string[]
+	 *
+	 * @return string
+	 */
+	protected function getArrayAsText( array $elements ) {
+		return implode( "\n", $elements );
+	}
+
+	/**
+	 * @param array $allAliases
+	 *
+	 * @return string
+	 */
+	protected function getAllAliasesText( array $allAliases ) {
+		$text = '';
+
+		foreach ( $allAliases as $aliases ) {
+			$text .= "\n" . implode( "\n", $aliases );
+		}
+
+		return $text;
+	}
+
+}
