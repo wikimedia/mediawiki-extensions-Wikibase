e1b6b3cb6c88dca578af7f59b2d20edb8357799b
addshore <addshorewiki@gmail.com>
2013-10-20 13:59:17 +0200

    Cleanup lib/util file headers
    Change-Id: I6fb37fc1c1774373f82554bf94caa02834dba591

--- lib/includes/util/HttpAcceptNegotiator.php
+++ lib/includes/util/HttpAcceptNegotiator.php
@@ -1,32 +1,6 @@
 <?php
- /**
- *
- * Copyright © 17.05.13 by the authors listed below.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * http://www.gnu.org/copyleft/gpl.html
- *
- * @license GPL 2+
- * @file
- *
- * @author Daniel Kinzler
- */
-
 
 namespace Wikibase;
-
 
 /**
  * Utility for negotiating a value from a set of supported values using a preference list.
@@ -35,10 +9,9 @@ namespace Wikibase;
  * To use this with a request header, first parse the header value into an array of weights
  * using HttpAcceptParser, then call getBestSupportedKey.
  *
- * Class HttpAcceptNegotiator
- * @package Wikibase
+ * @license GPL 2+
+ * @author Daniel Kinzler
  */
-
 class HttpAcceptNegotiator {
 
 	/**
