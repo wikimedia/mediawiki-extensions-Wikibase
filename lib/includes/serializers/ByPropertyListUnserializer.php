5cdf61d72ac5fb9c247d0a2dba3e4d21e1c77aeb
addshore <addshorewiki@gmail.com>
2013-10-22 01:00:34 +0200

    Parse lowercase ids in ByPropertyListUnserializer
    (cherry picked from commit c1f56396f95e3d42dfc04038f28bf7af1dffa368)
    
    Change-Id: Iac8f4e7cbda907fde186532fea0fbfa4894a322b

--- lib/includes/serializers/ByPropertyListUnserializer.php
+++ lib/includes/serializers/ByPropertyListUnserializer.php
@@ -2,6 +2,8 @@
 
 namespace Wikibase\Lib\Serializers;
 use ApiResult, MWException;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\Lib\EntityIdParser;
 
 /**
  * Serializer for Traversable objects that need to be grouped
@@ -62,19 +64,24 @@ class ByPropertyListUnserializer implements Unserializer {
 	 */
 	public function newFromSerialization( array $serialization ) {
 		$elements = array();
+		$idParser = new EntityIdParser();
 
 		foreach ( $serialization as $propertyId => $byPropId ) {
 			if ( !is_array( $byPropId ) ) {
 				throw new MWException( "Element with key '$propertyId' should be an array, found " . gettype( $byPropId ) );
 			}
 
+			$propertyId = $idParser->parse( $propertyId );
+
 			foreach ( $byPropId as $serializedElement ) {
 				$element = $this->elementUnserializer->newFromSerialization( $serializedElement );
-				// FIXME: usage of deprecated method getPrefixedId
-				$elementPropertyId = $element->getPropertyId()->getPrefixedId();
 
-				if ( $elementPropertyId !== $propertyId ) {
-					throw new MWException( "Element with id '$elementPropertyId' found in list with id '$propertyId'" );
+				/** @var PropertyId $elementPropertyId */
+				$elementPropertyId = $element->getPropertyId();
+
+				if ( !$elementPropertyId->equals( $propertyId ) ) {
+					throw new MWException( "Element with id '" . $elementPropertyId->getSerialization() .
+					"' found in list with id '" . $propertyId->getSerialization() . "'" );
 				}
 
 				$elements[] = $element;
