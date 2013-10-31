d00bb64d995708eadf7d1c201a41bce04b2f33bc
addshore <addshorewiki@gmail.com>
2013-10-31 15:17:20 +0100

    Implement List Serializer and Unserializer
    Needed to allow claims references and qualifiers
    to be optionally serialized by propoerty of just
    in a list!
    
    Change-Id: I07626d07670f6cd6a2ae3d7370dea6ba32c5c366

---
+++ lib/includes/serializers/ListUnserializer.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Wikibase\Lib\Serializers;
+
+/**
+ * Unserializer for Traversable objects
+ *
+ * @since 0.5
+ *
+ * @licence GNU GPL v2+
+ * @author Adam Shorland
+ */
+class ListUnserializer implements Unserializer {
+
+	/**
+	 * @var Unserializer
+	 */
+	protected $elementUnserializer;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param Unserializer $elementUnserializer
+	 */
+	public function __construct( Unserializer $elementUnserializer ) {
+		$this->elementUnserializer = $elementUnserializer;
+	}
+
+	/**
+	 * @see Unserializer::newFromSerialization
+	 *
+	 * @since 0.5
+	 *
+	 * @param array $serialization
+	 *
+	 * @return array
+	 */
+	public function newFromSerialization( array $serialization ) {
+		$elements = array();
+
+		foreach ( $serialization as $serializedElement ) {
+			$element = $this->elementUnserializer->newFromSerialization( $serializedElement );
+			$elements[] = $element;
+		}
+
+		return $elements;
+	}
+
+}
