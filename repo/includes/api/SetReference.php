75c34dd4b2a43cd25436b0850be37643ea9c9054
addshore <addshorewiki@gmail.com>
2013-11-06 12:13:19 +0100

    Add validation for wbsetreference....
    Bug: 56623
    Bug: 56662
    Change-Id: I8b8f8930fc47a530165fc51caaf52ccb9efa1495

--- repo/includes/api/SetReference.php
+++ repo/includes/api/SetReference.php
@@ -5,7 +5,6 @@ namespace Wikibase\Api;
 use ApiBase;
 use InvalidArgumentException;
 use OutOfBoundsException;
-use Wikibase\Repo\WikibaseRepo;
 use Wikibase\ChangeOp\ChangeOpReference;
 use Wikibase\ChangeOp\ChangeOpException;
 use Wikibase\SnakList;
@@ -16,25 +15,7 @@ use Wikibase\Lib\Serializers\SerializerFactory;
 /**
  * API module for creating a reference or setting the value of an existing one.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * http://www.gnu.org/copyleft/gpl.html
- *
  * @since 0.3
- *
- * @ingroup WikibaseRepo
- * @ingroup API
  *
  * @licence GNU GPL v2+
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
@@ -81,9 +62,14 @@ class SetReference extends ModifyClaim {
 			$decodedParams['snaks-order'] = $this->getArrayFromParam( $params['snaks-order'] );
 		}
 
+		/** @var Reference $newReference */
 		$newReference = $unserializer->newFromSerialization(
 			array_merge( $params, $decodedParams )
 		);
+
+		foreach( $newReference->getSnaks() as $snak ){
+			$this->snakValidation->validateSnak( $snak );
+		}
 
 		$changeOp = $this->getChangeOp( $newReference );
 
