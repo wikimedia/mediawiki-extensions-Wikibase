11c7218ea1be786584f4002acbe6f6b5f4f78858
Henning Snater <henning.snater@wikimedia.de>
2013-11-06 08:14:45 +0100

    Adapt changes of ValueView version 0.3 (DO NOT MERGE)
    Change-Id: I8ef9e56ebe69c0451575f498a6f65bcf28a40905

---
+++ lib/resources/experts/resources.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * @licence GNU GPL v2+
+ * @author H. Snater < mediawiki@snater.com >
+ *
+ * @codeCoverageIgnoreStart
+ */
+return call_user_func( function() {
+
+	$remoteExtPathParts = explode(
+		DIRECTORY_SEPARATOR . 'extensions' . DIRECTORY_SEPARATOR, __DIR__, 2
+	);
+	$moduleTemplate = array(
+		'localBasePath' => __DIR__,
+		'remoteExtPath' => $remoteExtPathParts[1],
+	);
+
+	return array(
+
+		'wikibase.experts' => $moduleTemplate + array(
+			'scripts' => array(
+				'wikibase.experts.register.js',
+			),
+			'dependencies' => array(
+				'dataTypes',
+				'jquery.valueview.experts.CommonsMediaType',
+				'jquery.valueview.experts.UrlType',
+				'mw.ext.valueView',
+				'wikibase.dataTypes',
+				'wikibase.datamodel',
+				'wikibase.experts.EntityIdValue',
+			),
+		),
+
+		'wikibase.experts.EntityIdInput' => $moduleTemplate + array(
+			'scripts' => array(
+				'EntityIdInput.js',
+			),
+			'dependencies' => array(
+				'jquery',
+				'jquery.event.special.eachchange',
+				'jquery.valueview.Expert',
+				'jquery.valueview.experts.StringValue',
+				'jquery.wikibase.entityselector',
+				'mediawiki.Title',
+				'mediawiki.util',
+				'wikibase',
+				'wikibase.datamodel',
+				'wikibase.store.FetchedContent',
+			),
+		),
+
+		'wikibase.experts.EntityIdValue' => $moduleTemplate + array(
+			'scripts' => array(
+				'EntityIdValue.js',
+			),
+			'dependencies' => array(
+				'jquery',
+				'jquery.valueview.Expert',
+				'jquery.valueview.BifidExpert',
+				'jquery.valueview.experts.StaticDom',
+				'wikibase',
+				'wikibase.datamodel',
+				'wikibase.experts.EntityIdInput',
+				'wikibase.utilities',
+			),
+		),
+
+	);
+
+} );
