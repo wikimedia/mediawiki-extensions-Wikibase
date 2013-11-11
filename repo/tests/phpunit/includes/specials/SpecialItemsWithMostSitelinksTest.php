1e93a8d3a2ebfe7c4c6a09868b177f1a18ed687d
Bene <benestar.wikimedia@googlemail.com>
2013-11-11 23:13:48 +0100

    (bug 46217) Implement a special page to show items with the most sitelinks
    Change-Id: I1f8a961016be4901a93c9c853ea6f133177baa9d

---
+++ repo/tests/phpunit/includes/specials/SpecialItemsWithMostSitelinksTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Wikibase\Test;
+
+/**
+ * Tests for the SpecialItemsWithMostSitelinks class.
+ *
+ * @file
+ * @since 0.5
+ *
+ * @ingroup WikibaseRepoTest
+ * @ingroup Test
+ *
+ * @group Wikibase
+ * @group SpecialPage
+ * @group WikibaseSpecialPage
+ *
+ * @group Database
+ *        ^---- needed because we rely on Title objects internally
+ *
+ * @licence GNU GPL v2+
+ * @author Bene*
+ */
+class SpecialItemsWithMostSitelinksTest extends SpecialPageTestBase {
+
+	protected function newSpecialPage() {
+		return new \Wikibase\Repo\Specials\SpecialItemsWithMostSitelinks();
+	}
+
+	public function testExecute() {
+		//TODO: Actually verify that the output is correct.
+		//      Currently this just tests that there is no fatal error,
+		//      and that the restriction handling is working and doesn't
+		//      block. That is, the default should let the user execute
+		//      the page.
+
+		list( $output, ) = $this->executeSpecialPage( '' );
+		$this->assertTrue( true, 'Calling execute without any subpage value' );
+	}
+
+}
