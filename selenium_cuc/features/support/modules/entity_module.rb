016a98c89d610d943ff6c205281174380c0643ba
Zeljko Filipin <zeljko.filipin@gmail.com>
2013-11-11 15:33:25 +0100

    Using new page-object API
    It is more similar to HTML element names.
    
    Change-Id: Ie633deb08fd1b0f4459e0758f97f2e6f901de58b

--- selenium_cuc/features/support/modules/entity_module.rb
+++ selenium_cuc/features/support/modules/entity_module.rb
@@ -28,27 +28,27 @@ module EntityPage
   span(:entityLabelSpan, :xpath => "//h1[contains(@class, 'wb-firstHeading')]/span/span")
   span(:entityIdSpan, :xpath => "//h1[contains(@class, 'wb-firstHeading')]/span/span[contains(@class, 'wb-value-supplement')]")
   text_field(:labelInputField, :xpath => "//h1[contains(@class, 'wb-firstHeading')]/span/span/input")
-  link(:editLabelLink, :css => "h1.wb-firstHeading a.wikibase-toolbareditgroup-editbutton:not(.wikibase-toolbarbutton-disabled)")
-  link(:editLabelLinkDisabled, :css => "h1.wb-firstHeading a.wikibase-toolbareditgroup-editbutton.wikibase-toolbarbutton-disabled")
-  link(:saveLabelLink, :css => "h1.wb-firstHeading a.wikibase-toolbareditgroup-savebutton:not(.wikibase-toolbarbutton-disabled)")
-  link(:saveLabelLinkDisabled, :css => "h1.wb-firstHeading a.wikibase-toolbareditgroup-savebutton.wikibase-toolbarbutton-disabled")
-  link(:cancelLabelLink, :css => "h1.wb-firstHeading a.wikibase-toolbareditgroup-cancelbutton:not(.wikibase-toolbarbutton-disabled)")
-  link(:cancelLabelLinkDisabled, :css => "h1.wb-firstHeading a.wikibase-toolbareditgroup-cancelbutton.wikibase-toolbarbutton-disabled")
+  a(:edit_label, :css => "h1.wb-firstHeading a.wikibase-toolbareditgroup-editbutton:not(.wikibase-toolbarbutton-disabled)")
+  a(:editLabelLinkDisabled, :css => "h1.wb-firstHeading a.wikibase-toolbareditgroup-editbutton.wikibase-toolbarbutton-disabled")
+  a(:saveLabelLink, :css => "h1.wb-firstHeading a.wikibase-toolbareditgroup-savebutton:not(.wikibase-toolbarbutton-disabled)")
+  a(:saveLabelLinkDisabled, :css => "h1.wb-firstHeading a.wikibase-toolbareditgroup-savebutton.wikibase-toolbarbutton-disabled")
+  a(:cancelLabelLink, :css => "h1.wb-firstHeading a.wikibase-toolbareditgroup-cancelbutton:not(.wikibase-toolbarbutton-disabled)")
+  a(:cancelLabelLinkDisabled, :css => "h1.wb-firstHeading a.wikibase-toolbareditgroup-cancelbutton.wikibase-toolbarbutton-disabled")
 
   # description UI
   span(:entityDescriptionSpan, :xpath => "//div[contains(@class, 'wb-ui-descriptionedittool')]/span[contains(@class, 'wb-property-container-value')]/span")
   text_field(:descriptionInputField, :xpath => "//div[contains(@class, 'wb-ui-descriptionedittool')]/span[contains(@class, 'wb-property-container-value')]/span/input")
-  link(:editDescriptionLink, :css => "div.wb-ui-descriptionedittool a.wikibase-toolbareditgroup-editbutton:not(.wikibase-toolbarbutton-disabled)")
-  link(:editDescriptionLinkDisabled,  :css => "div.wb-ui-descriptionedittool a.wikibase-toolbareditgroup-editbutton.wikibase-toolbarbutton-disabled")
-  link(:saveDescriptionLink, :css => "div.wb-ui-descriptionedittool a.wikibase-toolbareditgroup-savebutton:not(.wikibase-toolbarbutton-disabled)")
-  link(:saveDescriptionLinkDisabled,  :css => "div.wb-ui-descriptionedittool a.wikibase-toolbareditgroup-savebutton.wikibase-toolbarbutton-disabled")
-  link(:cancelDescriptionLink, :css => "div.wb-ui-descriptionedittool a.wikibase-toolbareditgroup-cancelbutton:not(.wikibase-toolbarbutton-disabled)")
-  link(:cancelDescriptionLinkDisabled,  :css => "div.wb-ui-descriptionedittool a.wikibase-toolbareditgroup-cancelbutton.wikibase-toolbarbutton-disabled")
+  a(:editDescriptionLink, :css => "div.wb-ui-descriptionedittool a.wikibase-toolbareditgroup-editbutton:not(.wikibase-toolbarbutton-disabled)")
+  a(:editDescriptionLinkDisabled,  :css => "div.wb-ui-descriptionedittool a.wikibase-toolbareditgroup-editbutton.wikibase-toolbarbutton-disabled")
+  a(:saveDescriptionLink, :css => "div.wb-ui-descriptionedittool a.wikibase-toolbareditgroup-savebutton:not(.wikibase-toolbarbutton-disabled)")
+  a(:saveDescriptionLinkDisabled,  :css => "div.wb-ui-descriptionedittool a.wikibase-toolbareditgroup-savebutton.wikibase-toolbarbutton-disabled")
+  a(:cancelDescriptionLink, :css => "div.wb-ui-descriptionedittool a.wikibase-toolbareditgroup-cancelbutton:not(.wikibase-toolbarbutton-disabled)")
+  a(:cancelDescriptionLinkDisabled,  :css => "div.wb-ui-descriptionedittool a.wikibase-toolbareditgroup-cancelbutton.wikibase-toolbarbutton-disabled")
 
   span(:apiCallWaitingMessage, :class => "wb-ui-propertyedittool-editablevalue-waitmsg")
 
   # edit-tab
-  list_item(:editTab, :id => "ca-edit")
+  li(:editTab, :id => "ca-edit")
 
   # spinner
   div(:entitySpinner, :xpath => "//div[contains(@class, 'wb-entity-spinner')]")
@@ -57,7 +57,7 @@ module EntityPage
   div(:wbTooltip, :class => "tipsy-inner")
   div(:wbErrorDiv, :class => "wikibase-wbtooltip-error-top-message")
   div(:wbErrorDetailsDiv, :class => "wikibase-wbtooltip-error-details")
-  link(:wbErrorDetailsLink, :class => "wikibase-wbtooltip-error-details-link")
+  a(:wbErrorDetailsLink, :class => "wikibase-wbtooltip-error-details-link")
 
   # mw notifications
   div(:mwNotificationContent, :xpath => "//div[@id='mw-notification-area']/div/div[contains(@class, 'mw-notification-content')]")
