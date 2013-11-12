fcfc2172217698e698640ae01d403f5f54ab5d30
Tobias Gritschacher <tobias.gritschacher@wikimedia.de>
2013-11-12 17:01:55 +0100

    Basic cucumber tests for statements
    - baseline commit for cucumber statement tests
    
    Bug: 53845
    Change-Id: Ie5c47a735b864b0bd28bed37610c09b60ea85ba4

--- selenium_cuc/features/support/modules/entity_selector_module.rb
+++ selenium_cuc/features/support/modules/entity_selector_module.rb
@@ -9,24 +9,47 @@
 module EntitySelectorPage
   include PageObject
   # entity selector widget UI elements
-  ul(:entitySelectorList, :class => "ui-entityselector-list")
-  a(:firstEntitySelectorLink, :xpath => "//ul[contains(@class, 'ui-entityselector-list')]/li/a")
-  span(:firstEntitySelectorLabel, :xpath => "//ul[contains(@class, 'ui-entityselector-list')]/li/a/span/span[contains(@class, 'ui-entityselector-label')]")
-  span(:firstEntitySelectorDescription, :xpath => "//ul[contains(@class, 'ui-entityselector-list')]/li/a/span/span[contains(@class, 'ui-entityselector-description')]")
-  text_field(:entitySelectorInput, :xpath => "//div[contains(@class, 'wb-claimlistview')]//input[contains(@class, 'ui-entityselector-input')]", :index => 0)
-  text_field(:entitySelectorInput2, :xpath => "//div[contains(@class, 'wb-claimlistview')]//input[contains(@class, 'ui-entityselector-input')]", :index => 1)
+  text_field(:entitySelectorInput, xpath: "//div[contains(@class, 'wb-claimlistview')]//input[contains(@class, 'ui-entityselector-input')]", index: 0)
 
+  #ul(:entitySelectorList, class: "ui-entityselector-list")
+  #a(:firstEntitySelectorLink, xpath: "//ul[contains(@class, 'ui-entityselector-list')]/li/a")
+  #span(:firstEntitySelectorLabel, xpath: "//ul[contains(@class, 'ui-entityselector-list')]/li/a/span/span[contains(@class, 'ui-entityselector-label')]")
+  #span(:firstEntitySelectorDescription, xpath: "//ul[contains(@class, 'ui-entityselector-list')]/li/a/span/span[contains(@class, 'ui-entityselector-description')]")
+  #text_field(:entitySelectorInput2, xpath: "//div[contains(@class, 'wb-claimlistview')]//input[contains(@class, 'ui-entityselector-input')]", index: 1)
+  #text_field(:entitySelectorSearchInput, id: "searchInput")
+  #ul(:entitySelectorSearch, xpath: "//body/ul[contains(@class, 'ui-entityselector-list')]")
+
+=begin
   def wait_for_entity_selector_list
     wait_until do
       entitySelectorList?
     end
   end
-end
 
-def select_entity(label)
-  self.entitySelectorInput = label
-  ajax_wait
-  self.wait_for_entity_selector_list
-  firstEntitySelectorLink
-  ajax_wait
+  def wait_for_suggestions_list
+    wait_until do
+      entitySelectorSearch?
+    end
+  end
+
+  def select_entity(label)
+    self.entitySelectorInput = label
+    ajax_wait
+    self.wait_for_entity_selector_list
+    firstEntitySelectorLink
+    ajax_wait
+  end
+
+  def count_search_results
+    entitySelectorSearch_element.items
+  end
+
+  def get_search_results
+    results = []
+    entitySelectorSearch_element.each do |li|
+      results << li
+    end
+    results
+  end
+=end
 end
