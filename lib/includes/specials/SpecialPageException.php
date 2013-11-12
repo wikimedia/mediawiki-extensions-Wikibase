6a4c0ce89239b0e1f007b9ccd50f557da727f2c2
aude <aude.wiki@gmail.com>
2013-11-12 08:38:45 +0000

    Check id entered in SpecialSetSiteLink is an ItemId
    (also make capitalisation of "id" more consistent in these special pages)
    
    Bug: 56933
    Change-Id: I56c35d06039a34473e09225ffc551da5978a169e
    (cherry picked from commit 95a51924c897276c73260c2a76a7eccffc8c520b)

---
+++ lib/includes/specials/SpecialPageException.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Should be moved to core!
+ *
+ * @since 0.5
+ *
+ * @licence GNU GPL v2+
+ * @author Katie Filbert < aude.wiki@gmail.com >
+ */
+class SpecialPageException extends MessageException {
+
+}
