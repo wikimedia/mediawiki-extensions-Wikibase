7bece6db12661f7e5f24ef47d525c47ed74f8609
aude <aude.wiki@gmail.com>
2013-11-14 11:05:50 +0000

    Remove clutter
    Change-Id: Ic34d292b0b361536034bf83cef7b8cdea5ad92f7

--- client/config/WikibaseClient.example.php
+++ client/config/WikibaseClient.example.php
@@ -6,25 +6,7 @@
  * This file is NOT an entry point the Wikibase Client extension. Use WikibaseClient.php.
  * It should furthermore not be included from outside the extension.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * http://www.gnu.org/copyleft/gpl.html
- *
  * @since 0.4
- *
- * @file
- * @ingroup WikibaseClient
  *
  * @licence GNU GPL v2+
  * @author Daniel Kinzler
