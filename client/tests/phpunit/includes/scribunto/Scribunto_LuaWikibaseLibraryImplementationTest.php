e9bc8c6a6ef35b945bdf17cfe99bda371266258f
Jens Ohlig <jens.ohlig@wikimedia.de>
2013-11-19 11:08:29 +0100

    (Bug 54324) [DON'T MERGE] Sequences in Lua should start with 1
    Patch set 3: Added renumber test.
    Patch set 4: "since 0.5"
    Patch set 5: don't rely on repo for testing
    Patch set 6: formatting issues
    Patch set 6: ...
    
    Change-Id: I26c2526cc6258c18ccd83c6e1404eb9269802178

--- client/tests/phpunit/includes/scribunto/Scribunto_LuaWikibaseLibraryImplementationTest.php
+++ client/tests/phpunit/includes/scribunto/Scribunto_LuaWikibaseLibraryImplementationTest.php
@@ -4,12 +4,11 @@ namespace Wikibase\Test;
 
 use Scribunto_LuaWikibaseLibraryImplementation;
 use Wikibase\Client\WikibaseClient;
-use \Language;
 
 /**
  * @covers Wikibase\Scribunto_LuaWikibaseLibraryImplementation
  *
- * @since 0.4
+ * @since 0.5
  *
  * @group Wikibase
  * @group WikibaseClient
@@ -22,14 +21,12 @@ class Scribunto_LuaWikibaseLibraryImplementationTest extends \PHPUnit_Framework_
 
 	public function getWikibaseLibraryImplementation() {
 		$entityLookup = new MockRepository();
-		$language = new Language( "en" );
+		$siteLinkLookup = $this->getMockBuilder( '\Wikibase\SiteLinkLookup' )->disableOriginalConstructor()->getMock();
 		return new Scribunto_LuaWikibaseLibraryImplementation(
 			WikibaseClient::getDefaultInstance()->getEntityIdParser(), // EntityIdParser
 			$entityLookup,
 			WikibaseClient::getDefaultInstance()->getEntityIdFormatter(), // EntityIdFormatter
-			WikibaseClient::getDefaultInstance()->getStore()->getSiteLinkTable(), // SiteLinkLookup
-			$language, // language
-			"enwiki" // siteId
+			$siteLinkLookup // SiteLinkLookup
 		);
 	}
 
@@ -38,10 +35,25 @@ class Scribunto_LuaWikibaseLibraryImplementationTest extends \PHPUnit_Framework_
 	 */
 	public function testGetEntity( $entity ) {
 		$entityArr = $this->getWikibaseLibraryImplementation()->getEntity( $entity );
-		$this->assertInternalType( 'array', $entityArr );
+		$this->assertEquals( is_array( $entityArr ), true );
 	}
 
 	public function provideEntity() {
 		return array( array( 'q42' ), array( 'q23' ) );
+	}
+
+	/**
+	 * @dataProvider provideZeroIndexedArray
+	 */
+	public function testZeroIndexArray ( $array ) {
+		$this->getWikibaseLibraryImplementation()->renumber( $array );
+		$this->assertEquals( is_array( $array ), true );
+		$this->assertEquals( array_key_exists( 0, $array["nyancat"] ), false );
+		$this->assertEquals( $array["nyancat"][1], 'nyan' );
+		$this->assertEquals( $array["nyancat"][2], 'cat' );
+	}
+
+	public function provideZeroIndexedArray() {
+		return array( array( array( "nyancat" => array( "0" => "nyan", "1" => "cat" ) ) ) );
 	}
 }
