e2078bd0fce885a94f0a0f8dfa570e2d8f44f163
mayankmadan <maddiemadan@gmail.com>
2013-11-27 02:51:18 +0530

    Prefer double-quoted strings in Ruby code
    Bug: 57597
    Change-Id: I852460eca49af853e53132b43017e7d18fac8336

--- selenium_cuc/features/support/modules/entity_selector_module.rb
+++ selenium_cuc/features/support/modules/entity_selector_module.rb
@@ -10,14 +10,14 @@ module EntitySelectorPage
   include PageObject
   # entity selector widget UI elements
   ul(:entitySelectorList, class: "ui-entityselector-list")
-  a(:firstEntitySelectorLink, xpath: "//ul[contains(@class, 'ui-entityselector-list')]/li/a")
-  span(:firstEntitySelectorLabel, xpath: "//ul[contains(@class, 'ui-entityselector-list')]/li/a/span/span[contains(@class, 'ui-entityselector-label')]")
-  span(:firstEntitySelectorDescription, xpath: "//ul[contains(@class, 'ui-entityselector-list')]/li/a/span/span[contains(@class, 'ui-entityselector-description')]")
-  text_field(:entitySelectorInput, xpath: "//div[contains(@class, 'wb-claimlistview')]//input[contains(@class, 'ui-entityselector-input')]", index: 0)
-  text_field(:entitySelectorInput2, xpath: "//div[contains(@class, 'wb-claimlistview')]//input[contains(@class, 'ui-entityselector-input')]", index: 1)
+  a(:firstEntitySelectorLink, xpath: "//ul[contains(@class, \"ui-entityselector-list\")]/li/a")
+  span(:firstEntitySelectorLabel, xpath: "//ul[contains(@class, \"ui-entityselector-list\")]/li/a/span/span[contains(@class, \"ui-entityselector-label\")]")
+  span(:firstEntitySelectorDescription, xpath: "//ul[contains(@class, \"ui-entityselector-list\")]/li/a/span/span[contains(@class, \"ui-entityselector-description\")]")
+  text_field(:entitySelectorInput, xpath: "//div[contains(@class, \"wb-claimlistview\")]//input[contains(@class, \"ui-entityselector-input\")]", index: 0)
+  text_field(:entitySelectorInput2, xpath: "//div[contains(@class, \"wb-claimlistview\")]//input[contains(@class, \"ui-entityselector-input\")]", index: 1)
 
   ul(:entitySelectorList, class: "ui-entityselector-list")
-  a(:firstEntitySelectorLink, xpath: "//ul[contains(@class, 'ui-entityselector-list')]/li/a")
+  a(:firstEntitySelectorLink, xpath: "//ul[contains(@class, \"ui-entityselector-list\")]/li/a")
   #span(:firstEntitySelectorLabel, xpath: "//ul[contains(@class, 'ui-entityselector-list')]/li/a/span/span[contains(@class, 'ui-entityselector-label')]")
   #span(:firstEntitySelectorDescription, xpath: "//ul[contains(@class, 'ui-entityselector-list')]/li/a/span/span[contains(@class, 'ui-entityselector-description')]")
   #text_field(:entitySelectorInput2, xpath: "//div[contains(@class, 'wb-claimlistview')]//input[contains(@class, 'ui-entityselector-input')]", index: 1)
