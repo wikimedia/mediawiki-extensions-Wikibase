5969f89af97f82aa312f32c8f91aa943a6612f59
Jared <jaredflores2000@gmail.com>
2013-11-27 14:13:04 -0800

    Retain form input on Special:SetSiteLink in case of an error
    Bug: 57592
    Change-Id: Iafa210c12bbcd0633ddfb0b3edffd83774fc13cd

--- repo/includes/specials/SpecialModifyEntity.php
+++ repo/includes/specials/SpecialModifyEntity.php
@@ -265,7 +265,7 @@ abstract class SpecialModifyEntity extends SpecialWikibasePage {
 		)
 		. Html::input(
 			'id',
-			$id,
+			$this->getRequest()->getVal( 'id' ),
 			'text',
 			array(
 				'class' => 'wb-input',
