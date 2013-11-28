dc123c5012a40c520eedbee93adf60d9573e753c
daniel <daniel.kinzler@wikimedia.de>
2013-11-28 16:03:27 +0100

    (bug #56685) Parsing of localized quantities.
    This introduces MediaWikiNumberUnlocalizer which relies on
    MediaWiki's Language class for converting numbers to canonical form.
    
    IMPORTANT: requires I9acb56d4f7f4 to be merged. (done)
    
    Change-Id: I996e8d032a476d245bbfc2872d7eae4bbad79cb1

---
+++ lib/tests/phpunit/parsers/MediaWikiNumberUnlocalizerTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Wikibase\Lib\Test;
+
+use ValueParsers\ParserOptions;
+use Wikibase\Lib\MediaWikiNumberUnlocalizer;
+
+/**
+ * @covers Wikibase\Lib\MediaWikiNumberUnlocalizer
+ *
+ * @since 0.5
+ *
+ * @group ValueParsers
+ * @group WikibaseLib
+ * @group Wikibase
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ */
+class MediaWikiNumberUnlocalizerTest extends \PHPUnit_Framework_TestCase {
+
+	public function provideUnlocalize() {
+		return array(
+			array( '123,456.789', 'en', '123456.789' ),
+			array( '123.456,789', 'de', '123456.789' ),
+		);
+	}
+
+	/**
+	 * @dataProvider provideUnlocalize
+	 *
+	 * @param $localized
+	 * @param $lang
+	 * @param $expected
+	 */
+	public function testUnlocalize( $localized, $lang, $expected ) {
+		$unlocalizer = new MediaWikiNumberUnlocalizer();
+		$options = new ParserOptions();
+
+		$actual = $unlocalizer->unlocalize( $localized, $lang, $options );
+
+		$this->assertEquals( $expected, $actual );
+	}
+}
