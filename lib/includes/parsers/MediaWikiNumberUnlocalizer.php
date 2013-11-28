46eb1fa2e2a073f5ba7218f7fb801ee01a330ad6
daniel <daniel.kinzler@wikimedia.de>
2013-11-28 16:03:27 +0100

    (bug #56685) Parsing of localized quantities.
    This introduces MediaWikiNumberUnlocalizer which relies on
    MediaWiki's Language class for converting numbers to canonical form.
    
    IMPORTANT: requires I9acb56d4f7f4 to be merged.
    
    Change-Id: I996e8d032a476d245bbfc2872d7eae4bbad79cb1

---
+++ lib/includes/parsers/MediaWikiNumberUnlocalizer.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Wikibase\Lib;
+use Language;
+use ValueParsers\ParserOptions;
+use ValueParsers\Unlocalizer;
+
+/**
+ * MediaWikiNumberUnlocalizer
+ *
+ * @license GPL 2+
+ * @author Daniel Kinzler
+ */
+class MediaWikiNumberUnlocalizer implements Unlocalizer {
+
+	/**
+	 * @see Unlocalizer::unlocalize()
+	 *
+	 * @param string $number string to process
+	 * @param string $language language code
+	 * @param ParserOptions $options
+	 *
+	 * @return string unlocalized string
+	 */
+	public function unlocalize( $number, $language, ParserOptions $options ) {
+		$language = Language::factory( $language );
+
+		$canonicalizedNumber = $language->parseFormattedNumber( $number );
+		return $canonicalizedNumber;
+	}
+}
+ 
