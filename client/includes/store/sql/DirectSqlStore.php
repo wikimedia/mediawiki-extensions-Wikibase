e5121321d52fda6bf8ec115592bcab6e2e649229
aude <aude.wiki@gmail.com>
2013-12-06 15:22:40 +0100

    Use setting for property info table cache key
    This allows the cache key to change with each deployment and whatnot.
    
    Change-Id: I53908d3626d245236f9c82cf4c81993f02cdef5e

--- client/includes/store/sql/DirectSqlStore.php
+++ client/includes/store/sql/DirectSqlStore.php
@@ -327,11 +327,9 @@ class DirectSqlStore implements ClientStore {
 	protected function newPropertyInfoTable() {
 		if ( Settings::get( 'usePropertyInfoTable' ) ) {
 			$table = new PropertyInfoTable( true, $this->repoWiki );
-
-			//TODO: get cache type etc from config
-			//TODO: better version ID from config!
-			$key = $this->repoWiki . '/Wikibase/CachingPropertyInfoStore/' . WBL_VERSION;
-			return new CachingPropertyInfoStore( $table, wfGetMainCache(), 3600, $key );
+			$key = $this->cachePrefix . ':CachingPropertyInfoStore';
+			return new CachingPropertyInfoStore( $table, ObjectCache::getInstance( $this->cacheType ),
+				$this->cacheDuration, $key );
 		} else {
 			// dummy info store
 			return new DummyPropertyInfoStore();
