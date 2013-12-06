1827588d66787b1eb5c5606cc28e754a60a03d2e
daniel <daniel.kinzler@wikimedia.de>
2013-12-06 16:35:28 +0100

    (bug 45529) use composite indexes on wb_terms.
    Change-Id: I3cd44a07666511ff836d7439540c29e824024ec4

--- repo/includes/store/sql/Wikibase.sql
+++ repo/includes/store/sql/Wikibase.sql
@@ -42,12 +42,14 @@ CREATE TABLE IF NOT EXISTS /*_*/wb_terms (
   term_weight                FLOAT UNSIGNED     NOT NULL DEFAULT 0.0 -- weight for ranking
 ) /*$wgDBTableOptions*/;
 
-CREATE INDEX /*i*/wb_terms_entity_id ON /*_*/wb_terms (term_entity_id);
-CREATE INDEX /*i*/wb_terms_entity_type ON /*_*/wb_terms (term_entity_type);
-CREATE INDEX /*i*/wb_terms_language ON /*_*/wb_terms (term_language);
-CREATE INDEX /*i*/wb_terms_type ON /*_*/wb_terms (term_type);
-CREATE INDEX /*i*/wb_terms_text ON /*_*/wb_terms (term_text);
-CREATE INDEX /*i*/wb_terms_search_key ON /*_*/wb_terms (term_search_key);
+-- for TermSqlIndex::getMatchingIDs
+CREATE INDEX /*i*/term_search ON /*_*/wb_terms (term_language, term_search_key(12), term_entity_type, term_type, term_text);
+
+-- for TermSqlIndex::getTermsOfEntity and for the join in TermSqlIndex::getMatchingTermCombination
+CREATE INDEX /*i*/term_entity ON /*_*/wb_terms (term_entity_type, term_entity_id, term_type, term_text);
+
+-- TermSqlIndex::getMatchingTerms with or without given term_text, as well as for TermSqlIndex::getMatchingTermCombination
+CREATE UNIQUE INDEX /*i*/term_identity ON /*_*/wb_terms (term_language, term_type, term_entity_type, term_text, term_entity_id);
 
 -- Links id+type to page ids.
 CREATE TABLE IF NOT EXISTS /*_*/wb_entity_per_page (
---
+++ repo/sql/UpdateTermIndexes.sql
@@ -0,0 +1,17 @@
+
+DROP INDEX /*i*/wb_terms_entity_id ON /*_*/wb_terms;
+DROP INDEX /*i*/wb_terms_entity_type ON /*_*/wb_terms;
+DROP INDEX /*i*/wb_terms_language ON /*_*/wb_terms;
+DROP INDEX /*i*/wb_terms_type ON /*_*/wb_terms;
+DROP INDEX /*i*/wb_terms_text ON /*_*/wb_terms;
+DROP INDEX /*i*/wb_terms_search_key ON /*_*/wb_terms;
+
+
+-- for TermSqlIndex::getMatchingIDs
+CREATE INDEX /*i*/term_search ON /*_*/wb_terms (term_language, term_search_key(12), term_entity_type, term_type);
+
+-- for TermSqlIndex::getTermsOfEntity and for the join in TermSqlIndex::getMatchingTermCombination
+CREATE INDEX /*i*/term_entity ON /*_*/wb_terms (term_entity_type, term_entity_id, term_type);
+
+-- TermSqlIndex::getMatchingTerms with or without given term_text, as well as for TermSqlIndex::getMatchingTermCombination
+CREATE UNIQUE INDEX /*i*/term_identity ON /*_*/wb_terms (term_language, term_type, term_entity_type, term_text, term_entity_id);
