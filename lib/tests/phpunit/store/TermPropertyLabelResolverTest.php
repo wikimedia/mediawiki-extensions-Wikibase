54f0165b8939f0b62327832232c9ada4e7a8b7ee
aude <aude.wiki@gmail.com>
2013-12-07 18:12:19 +0100

    Improve TermPropertyLabelResolver and test, including more cache scenarios
    Change-Id: I0a532af57348bf3f1620240754923205cf95db27

--- lib/tests/phpunit/store/TermPropertyLabelResolverTest.php
+++ lib/tests/phpunit/store/TermPropertyLabelResolverTest.php
@@ -24,8 +24,14 @@ use Wikibase\TermPropertyLabelResolver;
  *
  * @licence GNU GPL v2+
  * @author Daniel Kinzler
+ * @author Katie Filbert < aude.wiki@gmail.com >
  */
-class TermPropertyLabelResolverTest extends \MediaWikiTestCase {
+class TermPropertyLabelResolverTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * @var int
+	 */
+	protected $cacheCounter = 0;
 
 	/**
 	 * @param string $lang
@@ -34,33 +40,141 @@ class TermPropertyLabelResolverTest extends \MediaWikiTestCase {
 	 * @return PropertyLabelResolver
 	 */
 	public function getResolver( $lang, $terms ) {
+		$cache = $this->getMockBuilder( 'HashBagOStuff' )
+			->getMock();
+
+		$cache->expects( $this->any() )
+			->method( 'get' )
+			->will( $this->returnCallback( array( $this, 'cacheGetCallback' ) ) );
+
 		$resolver = new TermPropertyLabelResolver(
 			$lang,
 			new MockTermIndex( $terms ),
-			new HashBagOStuff(),
+			$cache,
 			60 * 60,
-			'testrepo:WBL\0.5alpha'
+			'testrepo:WBL/0.5alpha'
 		);
 
 		return $resolver;
 	}
 
+	public function cacheGetCallback( $cacheKey ) {
+		++$this->cacheCounter;
+
+		if ( $this->cacheCounter === 1 ) {
+			return false;
+		} else {
+			return $this->getCachedLabelMap( $cacheKey );
+		}
+	}
+
 	/**
-	 * @dataProvider provideGetPropertyIdsForLabels
+	 * @dataProvider getPropertyIdsForLabelsProvider
 	 */
-	public function testGetPropertyIdsForLabels( $lang, array $terms, array $labels, array $expected ) {
+	public function testGetPropertyIdsForLabels( $lang, array $terms, array $labels,
+		array $expected, array $expectedCached
+	) {
 		$resolver = $this->getResolver( $lang, $terms );
 
 		// check we are getting back the expected map of labels to IDs
 		$actual = $resolver->getPropertyIdsForLabels( $labels );
-		$this->assertArrayEquals( $expected, $actual, false, true );
+		$this->assertEquals( $expected, $actual, 'not cached' );
 
 		// check again, so we also hit the "stuff it cached" code path
 		$actual = $resolver->getPropertyIdsForLabels( $labels );
-		$this->assertArrayEquals( $expected, $actual, false, true );
+		$this->assertEquals( $expected, $actual, 'in process cache' );
+
+		// memcached
+		$resolver = $this->getResolver( $lang, $terms );
+		$actual = $resolver->getPropertyIdsForLabels( $labels );
+		$this->assertEquals( $expectedCached, $actual, 'memcached' );
+
+		// force recache
+		$actual = $resolver->getPropertyIdsForLabels( $labels, 'recache' );
+		$this->assertEquals( $expected, $actual, 'recache' );
 	}
 
-	public function provideGetPropertyIdsForLabels() {
+	public function getPropertyIdsForLabelsProvider() {
+		$terms = $this->getTerms();
+
+		return array(
+			array( // #0
+				'de',   // lang
+				$terms, // terms
+				array(),  // labels
+				array(),  // expected
+				array(), // cache expected
+			),
+			array( // #1
+				'de',   // lang
+				$terms, // terms
+				array(  // labels
+					'Eins',
+					'Zwei'
+				),
+				array(  // expected
+					'Eins' => new PropertyId( 'P1' ),
+					'Zwei' => new PropertyId( 'P2' ),
+				),
+				array( // cache expected
+					'Eins' => new PropertyId( 'P1' )
+				),
+			),
+			array( // #2
+				'de',   // lang
+				$terms, // terms
+				array(  // labels
+					'Drei',
+					'Vier'
+				),
+				array(  // expected
+					'Drei' => new PropertyId( 'P3' ),
+				),
+				array( // cache expected
+					'Drei' => new PropertyId( 'P3' )
+				)
+			),
+			array( // #3
+				'en',   // lang
+				$terms, // terms
+				array(  // labels
+					'Eins',
+					'Zwei'
+				),
+				array(),  // expected
+				array() // cache expected
+			),
+			array( // #4
+				'en',   // lang
+				$terms, // terms
+				array(  // labels
+					'One',
+					'Two',
+					'Three',
+					'Four'
+				),
+				array(  // expected
+					'One' => new PropertyId( 'P1' ),
+				),
+				array( // cache expected
+					'One' => new PropertyId( 'P1' ),
+					'Two' => new PropertyId( 'P2' )
+				)
+			),
+			array(
+				'es',
+				$terms,
+				array(
+					'Uno',
+					'Dos'
+				),
+				array(),
+				array()
+			)
+		);
+	}
+
+	private function getTerms() {
 		$terms = array(
 			// de
 			new Term( array(
@@ -123,58 +237,28 @@ class TermPropertyLabelResolverTest extends \MediaWikiTestCase {
 			) ),
 		);
 
-		return array(
-			array( // #0
-				'de',   // lang
-				$terms, // terms
-				array(),  // labels
-				array(),  // expected
-			),
-			array( // #1
-				'de',   // lang
-				$terms, // terms
-				array(  // labels
-					'Eins',
-					'Zwei'
-				),
-				array(  // expected
-					'Eins' => new PropertyId( 'P1' ),
-					'Zwei' => new PropertyId( 'P2' ),
-				)
-			),
-			array( // #2
-				'de',   // lang
-				$terms, // terms
-				array(  // labels
-					'Drei',
-					'Vier'
-				),
-				array(  // expected
-					'Drei' => new PropertyId( 'P3' ),
-				)
-			),
-			array( // #3
-				'en',   // lang
-				$terms, // terms
-				array(  // labels
-					'Eins',
-					'Zwei'
-				),
-				array()  // expected
-			),
-			array( // #4
-				'en',   // lang
-				$terms, // terms
-				array(  // labels
-					'One',
-					'Two',
-					'Three',
-					'Four'
-				),
-				array(  // expected
-					'One' => new PropertyId( 'P1' ),
-				)
-			),
-		);
+		return $terms;
 	}
+
+	private function getCachedLabelMap( $cacheKey ) {
+		if ( $cacheKey === 'testrepo:WBL/0.5alpha/en' ) {
+			$labelMap = array(
+				'One' => new PropertyId( 'P1' ),
+				'Two' => new PropertyId( 'P2' ),
+				'Cat' => new PropertyId( 'P3' )
+			);
+		} elseif ( $cacheKey === 'testrepo:WBL/0.5alpha/de' ) {
+			$labelMap = array(
+				'Eins' => new PropertyId( 'P1' ),
+				'Drei' => new PropertyId( 'P3' )
+			);
+		} else {
+			// cache key not found
+			$labelMap = false;
+		}
+
+		return $labelMap;
+	}
+
 }
+
