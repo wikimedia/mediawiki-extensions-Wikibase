b49d679f3eb36f98eb40e0984ac9a67542a15cff
daniel <daniel.kinzler@wikimedia.de>
2013-12-09 10:41:48 +0100

    (bug 56712) use prefixed IDs in wb_entity_per_page.
    INCOMPLETE!
    
    Change-Id: I7c7855a68371f845b1292c77bfe6de1e7ced37f5
    DEPLOYMENT: db schema change, see repo/sql/MigrateEPPEntityId.sql and repo/sql/*EPPIndexes.sql

--- repo/tests/phpunit/includes/store/sql/EntityPerPageTableTest.php
+++ repo/tests/phpunit/includes/store/sql/EntityPerPageTableTest.php
@@ -61,7 +61,7 @@ class EntityPerPageTableTest extends \MediaWikiTestCase {
 	}
 
 	public function testAddEntityContent( /* EntityContent $entityContent */ ) {
-		$this->markTestIncomplete( "test me!" );
+		$this->markTestIncomplete( "test me!" ); //FIXME: !
 	}
 
 	public function testDeleteEntityContent( /* EntityContent $entityContent */ ) {
