bdb21fae0a4d1256c91eae13461bb8b7a11e7b05
Tpt <thomaspt@hotmail.fr>
2013-12-16 21:44:26 +0100

    Implement Snak serialization and unserialization

--- lib/packages/wikibase/data-model-serialization/src/Wikibase/DataModel/Serializers/SnakSerializer.php
+++ lib/packages/wikibase/data-model-serialization/src/Wikibase/DataModel/Serializers/SnakSerializer.php
@@ -2,10 +2,72 @@
 
 namespace Wikibase\DataModel\Serializers;
 
-/**
- * @licence GNU GPL v2+
- * @author Jeroen De Dauw < jeroendedauw@gmail.com >
- */
-class SnakSerializer {
+use Serializers\Exceptions\SerializationException;
+use Serializers\Exceptions\UnsupportedObjectException;
+use Serializers\Serializer;
+use Wikibase\DataModel\Snak\PropertyValueSnak;
+use Wikibase\DataModel\Snak\Snak;
 
+/**
+ * @since 1.0
+ *
+ * @licence GNU GPL v2+
+ * @author Thomas Pellissier Tanon
+ */
+class SnakSerializer implements Serializer {
+
+	/**
+	 * @var Serializer
+	 */
+	protected $dataValueSerializer;
+
+	/**
+	 * @param Serializer $dataValueSerializer
+	 */
+	public function __construct( Serializer $dataValueSerializer ) {
+		$this->dataValueSerializer = $dataValueSerializer;
+	}
+
+	/**
+	 * @see Serializer::isSerializerFor
+	 *
+	 * @param mixed $object
+	 *
+	 * @return boolean
+	 */
+	public function isSerializerFor( $object ) {
+		return is_object( $object ) && $object instanceof Snak;
+	}
+
+	/**
+	 * @see Serializer::serialize
+	 *
+	 * @param mixed $object
+	 *
+	 * @return array
+	 * @throws SerializationException
+	 */
+	public function serialize( $object ) {
+		if ( !$this->isSerializerFor( $object ) ) {
+			throw new UnsupportedObjectException(
+				$object,
+				'SnakSerializer can only serialize Snak objects'
+			);
+		}
+
+		return $this->getSerialized( $object );
+	}
+
+	private function getSerialized( Snak $snak ) {
+		$serialization = array (
+			'snaktype' => $snak->getType(),
+			'property' => $snak->getPropertyId()->getPrefixedId()
+		);
+
+		if ( $snak instanceof PropertyValueSnak ) {
+			$serialization['datavalue'] = $this->dataValueSerializer->serialize( $snak->getDataValue() );
+		}
+
+		return $serialization;
+	}
 }
--- lib/packages/wikibase/data-model-serialization/tests/unit/Deserializers/SnakDeserializerTest.php
+++ lib/packages/wikibase/data-model-serialization/tests/unit/Deserializers/SnakDeserializerTest.php
@@ -2,17 +2,107 @@
 
 namespace Tests\Wikibase\DataModel\Deserializers;
 
+use DataValues\Deserializers\DataValueDeserializer;
+use DataValues\StringValue;
+use Tests\Wikibase\DataModel\Unserializers\DeserializerBaseTest;
+use Wikibase\DataModel\Deserializers\SnakDeserializer;
+use Wikibase\DataModel\Entity\BasicEntityIdParser;
+use Wikibase\DataModel\Snak\PropertyNoValueSnak;
+use Wikibase\DataModel\Snak\PropertySomeValueSnak;
+use Wikibase\DataModel\Snak\PropertyValueSnak;
+
 /**
  * @covers Wikibase\DataModel\Deserializers\SnakDeserializer
  *
  * @licence GNU GPL v2+
- * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ * @author Thomas Pellissier Tanon
  */
-class SnakDeserializerTest extends \PHPUnit_Framework_TestCase {
+class SnakDeserializerTest extends DeserializerBaseTest {
 
-	public function testTrue() {
-		// TODO
-		$this->assertTrue( true );
+	public function buildDeserializer() {
+		return new SnakDeserializer(
+			new DataValueDeserializer( array (
+				'string' => 'DataValues\StringValue',
+			) ),
+			new BasicEntityIdParser()
+		);
 	}
 
+	public function deserializableProvider() {
+		return array(
+			array(
+				array(
+					'snaktype' => 'novalue',
+					'property' => 'P42'
+				)
+			),
+			array(
+				array(
+					'snaktype' => 'somevalue',
+					'property' => 'P42'
+				)
+			),
+			array(
+				array(
+					'snaktype' => 'value',
+					'property' => 'P42',
+					'datavalue' => array(
+						'type' => 'string',
+						'value' => 'hax'
+					)
+				)
+			),
+		);
+	}
+
+	public function nonDeserializableProvider() {
+		return array(
+			array(
+				42
+			),
+			array(
+				array()
+			),
+			array(
+				array(
+					'id' => 'P10'
+				)
+			),
+			array(
+				array(
+					'snaktype' => '42value'
+				)
+			),
+		);
+	}
+
+	public function deserializationProvider() {
+		return array(
+			array(
+				new PropertyNoValueSnak( 42 ),
+				array(
+					'snaktype' => 'novalue',
+					'property' => 'P42'
+				)
+			),
+			array(
+				new PropertySomeValueSnak( 42 ),
+				array(
+					'snaktype' => 'somevalue',
+					'property' => 'P42'
+				)
+			),
+			array(
+				new PropertyValueSnak( 42, new StringValue( 'hax' ) ),
+				array(
+					'snaktype' => 'value',
+					'property' => 'P42',
+					'datavalue' => array(
+						'type' => 'string',
+						'value' => 'hax'
+					)
+				)
+			),
+		);
+	}
 }
