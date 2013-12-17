2b9fdded5e9a9a306289f297241cd1a9b525dea6
Zeljko Filipin <zeljko.filipin@gmail.com>
2013-12-17 12:51:34 +0100

    Use snake case
    Change-Id: I53b72e4fcedb9954a9875a41ccd90f46b108944f

--- tests/browser/features/step_definitions/sitelinks_steps.rb
+++ tests/browser/features/step_definitions/sitelinks_steps.rb
@@ -7,50 +7,50 @@
 # tests for item sitelinks
 
 When /^I click the sitelink add button$/ do
-  on(ItemPage).addSitelinkLink
+  on(ItemPage).add_sitelink_link
 end
 
 When /^I click the sitelink edit button$/ do
-  on(ItemPage).editSitelinkLink
+  on(ItemPage).edit_sitelink_link
 end
 
 When /^I click the sitelink cancel button$/ do
-  on(ItemPage).cancelSitelinkLink
+  on(ItemPage).cancel_sitelink_link
 end
 
 When /^I click the sitelink save button$/ do
   on(ItemPage) do |page|
-    page.saveSitelinkLink
+    page.save_sitelink_link
     page.wait_for_api_callback
   end
 end
 
 When /^I press the ESC key in the siteid input field$/ do
-  on(ItemPage).siteIdInputField_element.send_keys :escape
+  on(ItemPage).site_id_input_field_element.send_keys :escape
 end
 
 When /^I press the ESC key in the pagename input field$/ do
-  on(ItemPage).pageInputField_element.send_keys :escape
+  on(ItemPage).page_input_field_element.send_keys :escape
 end
 
 When /^I press the RETURN key in the pagename input field$/ do
   on(ItemPage) do |page|
-    page.pageInputField_element.send_keys :return
+    page.page_input_field_element.send_keys :return
     page.wait_for_api_callback
   end
 end
 
 When /^I type (.+) into the siteid input field$/ do |value|
   on(ItemPage) do |page|
-    page.siteIdInputField_element.clear
-    page.siteIdInputField = value
+    page.site_id_input_field_element.clear
+    page.site_id_input_field = value
   end
 end
 
 When /^I type (.+) into the page input field$/ do |value|
   on(ItemPage) do |page|
-    page.pageInputField_element.clear
-    page.pageInputField = value
+    page.page_input_field_element.clear
+    page.page_input_field = value
     page.ajax_wait
   end
 end
@@ -64,7 +64,7 @@ When /^I add the following sitelinks:$/ do |table|
 end
 
 When /^I order the sitelinks by languagename$/ do
-  on(ItemPage).sitelinkSortLanguage_element.click
+  on(ItemPage).sitelink_sort_language_element.click
 end
 
 When /^I mock that the list of sitelinks is complete$/ do
@@ -72,64 +72,64 @@ When /^I mock that the list of sitelinks is complete$/ do
 end
 
 Then /^Sitelink table should be there$/ do
-  on(ItemPage).sitelinkTable?.should be_true
+  on(ItemPage).sitelink_table?.should be_true
 end
 
 Then /^Sitelink heading should be there$/ do
-  on(ItemPage).sitelinkHeading?.should be_true
+  on(ItemPage).sitelink_heading?.should be_true
 end
 
 Then /^Sitelink add button should be there$/ do
-  on(ItemPage).addSitelinkLink?.should be_true
+  on(ItemPage).add_sitelink_link?.should be_true
 end
 
 Then /^Sitelink add button should be disabled$/ do
   on(ItemPage) do |page|
-    page.addSitelinkLink?.should be_false
-    page.addSitelinkLinkDisabled?.should be_true
+    page.add_sitelink_link?.should be_false
+    page.add_sitelink_link_disabled?.should be_true
   end
 end
 
 Then /^Sitelink edit button should be there$/ do
-  on(ItemPage).editSitelinkLink?.should be_true
+  on(ItemPage).edit_sitelink_link?.should be_true
 end
 
 Then /^Sitelink edit button should not be there$/ do
-  on(ItemPage).editSitelinkLink?.should be_false
+  on(ItemPage).edit_sitelink_link?.should be_false
 end
 
 Then /^Sitelink edit button should be disabled$/ do
   on(ItemPage) do |page|
-    page.editSitelinkLink?.should be_false
-    page.editSitelinkLinkDisabled?.should be_true
+    page.edit_sitelink_link?.should be_false
+    page.edit_sitelink_link_disabled?.should be_true
   end
 end
 
 Then /^Sitelink save button should be there$/ do
-  on(ItemPage).saveSitelinkLink?.should be_true
+  on(ItemPage).save_sitelink_link?.should be_true
 end
 
 Then /^Sitelink save button should not be there$/ do
-  on(ItemPage).saveSitelinkLink?.should be_false
+  on(ItemPage).save_sitelink_link?.should be_false
 end
 
 Then /^Sitelink save button should be disabled$/ do
   on(ItemPage) do |page|
-    page.saveSitelinkLink?.should be_false
-    page.saveSitelinkLinkDisabled?.should be_true
+    page.save_sitelink_link?.should be_false
+    page.save_sitelink_link_disabled?.should be_true
   end
 end
 
 Then /^Sitelink cancel button should be there$/ do
-  on(ItemPage).cancelSitelinkLink?.should be_true
+  on(ItemPage).cancel_sitelink_link?.should be_true
 end
 
 Then /^Sitelink cancel button should not be there$/ do
-  on(ItemPage).cancelSitelinkLink?.should be_false
+  on(ItemPage).cancel_sitelink_link?.should be_false
 end
 
 Then /^Sitelink counter should be there$/ do
-  on(ItemPage).sitelinkCounter?.should be_true
+  on(ItemPage).sitelink_counter?.should be_true
 end
 
 Then /^Sitelink counter should show (.+)$/ do |value|
@@ -141,65 +141,65 @@ Then /^There should be (\d+) sitelinks in the list$/ do |num|
 end
 
 Then /^Sitelink help field should be there$/ do
-  on(ItemPage).sitelinkHelpField?.should be_true
+  on(ItemPage).sitelink_help_field?.should be_true
 end
 
 Then /^Sitelink siteid input field should be there$/ do
-  on(ItemPage).siteIdInputField?.should be_true
+  on(ItemPage).site_id_input_field?.should be_true
 end
 
 Then /^Sitelink siteid input field should not be there$/ do
-  on(ItemPage).siteIdInputField?.should be_false
+  on(ItemPage).site_id_input_field?.should be_false
 end
 
 Then /^Sitelink pagename input field should be there$/ do
-  on(ItemPage).pageInputField?.should be_true
+  on(ItemPage).page_input_field?.should be_true
 end
 
 Then /^Sitelink pagename input field should be disabled$/ do
-  on(ItemPage).pageInputFieldDisabled?.should be_true
+  on(ItemPage).page_input_field_disabled?.should be_true
 end
 
 Then /^Sitelink siteid dropdown should be there$/ do
-  on(ItemPage).siteIdDropdown_element.visible?.should be_true
+  on(ItemPage).site_id_dropdown_element.visible?.should be_true
 end
 
 Then /^Sitelink siteid dropdown should not be there$/ do
-  on(ItemPage).siteIdDropdown_element.visible?.should be_false
+  on(ItemPage).site_id_dropdown_element.visible?.should be_false
 end
 
 Then /^Sitelink siteid first suggestion should be (.+)$/ do |value|
-  on(ItemPage).siteIdDropdownFirstElement.should == value
+  on(ItemPage).site_id_dropdown_first_element.should == value
 end
 
 Then /^Sitelink pagename dropdown should be there$/ do
-  on(ItemPage).pageNameDropdown_element.visible?.should be_true
+  on(ItemPage).page_name_dropdown_element.visible?.should be_true
 end
 
 Then /^Sitelink pagename dropdown should not be there$/ do
-  on(ItemPage).pageNameDropdown_element.visible?.should be_false
+  on(ItemPage).page_name_dropdown_element.visible?.should be_false
 end
 
 Then /^Sitelink pagename first suggestion should be (.+)$/ do |value|
-  on(ItemPage).pageNameDropdownFirstElement.should == value
+  on(ItemPage).page_name_dropdown_first_element.should == value
 end
 
 Then /^Sitelink language table cell should contain (.+)$/ do |value|
-  on(ItemPage).sitelinkSitename.should == value
+  on(ItemPage).sitelink_sitename.should == value
 end
 
 Then /^Sitelink code table cell should contain (.+)$/ do |value|
-  on(ItemPage).sitelinkSiteid.should == value
+  on(ItemPage).sitelink_siteid.should == value
 end
 
 Then /^Sitelink link text should be (.+)$/ do |value|
-  on(ItemPage).sitelinkLink_element.text.should == value
+  on(ItemPage).sitelink_link_element.text.should == value
 end
 
 Then /^Sitelink link should lead to article (.+)$/ do |value|
   on(ItemPage) do |page|
-    page.sitelinkLink
-    page.articleTitle.should == value
+    page.sitelink_link
+    page.article_title.should == value
   end
 end
 
--- tests/browser/features/support/modules/sitelink_module.rb
+++ tests/browser/features/support/modules/sitelink_module.rb
@@ -9,53 +9,53 @@
 module SitelinkPage
   include PageObject
   # sitelinks UI elements
-  h2(:sitelinkHeading, id: "sitelinks-wikipedia")
-  table(:sitelinkTable, xpath: "//table[contains(@data-wb-sitelinks-group, 'wikipedia')]")
-  element(:sitelinkTableHeader, :tr, xpath: "//table[contains(@data-wb-sitelinks-group, 'wikipedia')]//tr[contains(@class, 'wb-sitelinks-columnheaders')]")
-  element(:sitelinkSitename, :td, xpath: "//table[contains(@data-wb-sitelinks-group, 'wikipedia')]/tbody/tr/td[1]")
-  element(:sitelinkSiteid, :td, xpath: "//table[contains(@data-wb-sitelinks-group, 'wikipedia')]/tbody/tr/td[2]")
-  a(:sitelinkLink, css: "table[data-wb-sitelinks-group='wikipedia'] > tbody > tr > td:nth-child(3) > a")
-  span(:sitelinkCounter, class: "wb-ui-propertyedittool-counter")
-  text_field(:siteIdInputField, xpath: "//table[contains(@data-wb-sitelinks-group, 'wikipedia')]//td[contains(@class, 'wb-sitelinks-sitename')]/input")
-  text_field(:pageInputField, xpath: "//table[contains(@data-wb-sitelinks-group, 'wikipedia')]//td[contains(@class, 'wb-sitelinks-link')]/input[not(@disabled)]")
-  text_field(:pageInputFieldDisabled, xpath: "//table[contains(@data-wb-sitelinks-group, 'wikipedia')]//td[contains(@class, 'wb-sitelinks-link')]/input[contains(@disabled, 'disabled')]")
-  ul(:siteIdDropdown, xpath: "//ul[contains(@class, 'wikibase-siteselector-list')]")
-  li(:siteIdDropdownFirstElement, xpath: "//ul[contains(@class, 'wikibase-siteselector-list')]/li")
-  ul(:pageNameDropdown, xpath: "//ul[@class='ui-autocomplete ui-menu ui-widget ui-widget-content ui-corner-all ui-suggester-list']")
-  li(:pageNameDropdownFirstElement, xpath: "//ul[@class='ui-autocomplete ui-menu ui-widget ui-widget-content ui-corner-all ui-suggester-list']/li")
-  element(:sitelinkSortLanguage, :th, xpath: "//table[contains(@data-wb-sitelinks-group, 'wikipedia')]//th[contains(@class, 'wb-sitelinks-sitename')]")
-  element(:sitelinkSortCode, :th, xpath: "//table[contains(@data-wb-sitelinks-group, 'wikipedia')]//th[contains(@class, 'wb-sitelinks-siteid')]")
-  element(:sitelinkSortLink, :th, xpath: "//table[contains(@data-wb-sitelinks-group, 'wikipedia')]//th[contains(@class, 'wb-sitelinks-link')]")
+  h2(:sitelink_heading, id: "sitelinks-wikipedia")
+  table(:sitelink_table, xpath: "//table[contains(@data-wb-sitelinks-group, 'wikipedia')]")
+  element(:sitelink_table_header, :tr, xpath: "//table[contains(@data-wb-sitelinks-group, 'wikipedia')]//tr[contains(@class, 'wb-sitelinks-columnheaders')]")
+  element(:sitelink_sitename, :td, xpath: "//table[contains(@data-wb-sitelinks-group, 'wikipedia')]/tbody/tr/td[1]")
+  element(:sitelink_siteid, :td, xpath: "//table[contains(@data-wb-sitelinks-group, 'wikipedia')]/tbody/tr/td[2]")
+  a(:sitelink_link, css: "table[data-wb-sitelinks-group='wikipedia'] > tbody > tr > td:nth-child(3) > a")
+  span(:sitelink_counter, class: "wb-ui-propertyedittool-counter")
+  text_field(:site_id_input_field, xpath: "//table[contains(@data-wb-sitelinks-group, 'wikipedia')]//td[contains(@class, 'wb-sitelinks-sitename')]/input")
+  text_field(:page_input_field, xpath: "//table[contains(@data-wb-sitelinks-group, 'wikipedia')]//td[contains(@class, 'wb-sitelinks-link')]/input[not(@disabled)]")
+  text_field(:page_input_field_disabled, xpath: "//table[contains(@data-wb-sitelinks-group, 'wikipedia')]//td[contains(@class, 'wb-sitelinks-link')]/input[contains(@disabled, 'disabled')]")
+  ul(:site_id_dropdown, xpath: "//ul[contains(@class, 'wikibase-siteselector-list')]")
+  li(:site_id_dropdown_first_element, xpath: "//ul[contains(@class, 'wikibase-siteselector-list')]/li")
+  ul(:page_name_dropdown, xpath: "//ul[@class='ui-autocomplete ui-menu ui-widget ui-widget-content ui-corner-all ui-suggester-list']")
+  li(:page_name_dropdown_first_element, xpath: "//ul[@class='ui-autocomplete ui-menu ui-widget ui-widget-content ui-corner-all ui-suggester-list']/li")
+  element(:sitelink_sort_language, :th, xpath: "//table[contains(@data-wb-sitelinks-group, 'wikipedia')]//th[contains(@class, 'wb-sitelinks-sitename')]")
+  element(:sitelink_sort_code, :th, xpath: "//table[contains(@data-wb-sitelinks-group, 'wikipedia')]//th[contains(@class, 'wb-sitelinks-siteid')]")
+  element(:sitelink_sort_link, :th, xpath: "//table[contains(@data-wb-sitelinks-group, 'wikipedia')]//th[contains(@class, 'wb-sitelinks-link')]")
 
-  a(:addSitelinkLink, css: "table[data-wb-sitelinks-group='wikipedia'] a.wb-ui-propertyedittool-toolbarbutton-addbutton:not(.wikibase-toolbarbutton-disabled)")
-  a(:addSitelinkLinkDisabled, css: "table[data-wb-sitelinks-group='wikipedia'] a.wb-ui-propertyedittool-toolbarbutton-addbutton.wikibase-toolbarbutton-disabled")
-  a(:saveSitelinkLink, css: "table[data-wb-sitelinks-group='wikipedia'] a.wikibase-toolbareditgroup-savebutton:not(.wikibase-toolbarbutton-disabled)")
-  a(:saveSitelinkLinkDisabled, css: "table[data-wb-sitelinks-group='wikipedia'] a.wikibase-toolbareditgroup-savebutton.wikibase-toolbarbutton-disabled")
-  a(:cancelSitelinkLink, css: "table[data-wb-sitelinks-group='wikipedia'] a.wikibase-toolbareditgroup-cancelbutton:not(.wikibase-toolbarbutton-disabled)")
-  a(:removeSitelinkLink, css: "table[data-wb-sitelinks-group='wikipedia'] a.wikibase-toolbareditgroup-removebutton:not(.wikibase-toolbarbutton-disabled)")
-  a(:editSitelinkLink, css: "table[data-wb-sitelinks-group='wikipedia'] a.wikibase-toolbareditgroup-editbutton:not(.wikibase-toolbarbutton-disabled)")
-  a(:editSitelinkLinkDisabled, css: "table[data-wb-sitelinks-group='wikipedia'] a.wikibase-toolbareditgroup-editbutton.wikibase-toolbarbutton-disabled")
-  a(:editSitelinkLinkEn, css: "table[data-wb-sitelinks-group='wikipedia'] tr.wb-sitelinks-en a.wikibase-toolbareditgroup-editbutton:not(.wikibase-toolbarbutton-disabled)")
-  a(:editSitelinkLinkEnDisabled, css: "table[data-wb-sitelinks-group='wikipedia'] tr.wb-sitelinks-en a.wikibase-toolbareditgroup-editbutton.wikibase-toolbarbutton-disabled")
-  span(:articleTitle, xpath: "//h1[@id='firstHeading']/span")
-  span(:sitelinkHelpField, css: "table[data-wb-sitelinks-group='wikipedia'] span.mw-help-field-hint")
+  a(:add_sitelink_link, css: "table[data-wb-sitelinks-group='wikipedia'] a.wb-ui-propertyedittool-toolbarbutton-addbutton:not(.wikibase-toolbarbutton-disabled)")
+  a(:add_sitelink_link_disabled, css: "table[data-wb-sitelinks-group='wikipedia'] a.wb-ui-propertyedittool-toolbarbutton-addbutton.wikibase-toolbarbutton-disabled")
+  a(:save_sitelink_link, css: "table[data-wb-sitelinks-group='wikipedia'] a.wikibase-toolbareditgroup-savebutton:not(.wikibase-toolbarbutton-disabled)")
+  a(:save_sitelink_link_disabled, css: "table[data-wb-sitelinks-group='wikipedia'] a.wikibase-toolbareditgroup-savebutton.wikibase-toolbarbutton-disabled")
+  a(:cancel_sitelink_link, css: "table[data-wb-sitelinks-group='wikipedia'] a.wikibase-toolbareditgroup-cancelbutton:not(.wikibase-toolbarbutton-disabled)")
+  a(:remove_sitelink_link, css: "table[data-wb-sitelinks-group='wikipedia'] a.wikibase-toolbareditgroup-removebutton:not(.wikibase-toolbarbutton-disabled)")
+  a(:edit_sitelink_link, css: "table[data-wb-sitelinks-group='wikipedia'] a.wikibase-toolbareditgroup-editbutton:not(.wikibase-toolbarbutton-disabled)")
+  a(:edit_sitelink_link_disabled, css: "table[data-wb-sitelinks-group='wikipedia'] a.wikibase-toolbareditgroup-editbutton.wikibase-toolbarbutton-disabled")
+  a(:edit_sitelink_link_en, css: "table[data-wb-sitelinks-group='wikipedia'] tr.wb-sitelinks-en a.wikibase-toolbareditgroup-editbutton:not(.wikibase-toolbarbutton-disabled)")
+  a(:edit_sitelink_link_en_disabled, css: "table[data-wb-sitelinks-group='wikipedia'] tr.wb-sitelinks-en a.wikibase-toolbareditgroup-editbutton.wikibase-toolbarbutton-disabled")
+  span(:article_title, xpath: "//h1[@id='firstHeading']/span")
+  span(:sitelink_help_field, css: "table[data-wb-sitelinks-group='wikipedia'] span.mw-help-field-hint")
 
   # sitelinks methods
   def count_existing_sitelinks
-    if sitelinkTableHeader? == false
+    if sitelink_table_header? == false
       return 0
     end
     count = 0
-    sitelinkTable_element.each do |tableRow|
+    sitelink_table_element.each do |table_row|
       count = count+1
     end
     return count - 2 # subtracting the table header row and the footer row
   end
 
   def get_number_of_sitelinks_from_counter
-    sitelinkCounter_element.when_visible
+    sitelink_counter_element.when_visible
 
-    scanned = sitelinkCounter.scan(/\(([^)]+)\)/)
+    scanned = sitelink_counter.scan(/\(([^)]+)\)/)
     return scanned[0][0]
   end
 
@@ -63,8 +63,8 @@ module SitelinkPage
     count = 0
     number_of_sitelinks = count_existing_sitelinks
     while count < (number_of_sitelinks)
-      editSitelinkLink
-      removeSitelinkLink
+      edit_sitelink_link
+      remove_sitelink_link
       wait_for_api_callback
       count = count + 1
     end
@@ -72,18 +72,18 @@ module SitelinkPage
 
   def add_sitelinks(sitelinks)
     sitelinks.each do |sitelink|
-      addSitelinkLink
-      self.siteIdInputField = sitelink[0]
-      self.pageInputField_element.when_visible
-      self.pageInputField = sitelink[1]
-      saveSitelinkLink
+      add_sitelink_link
+      self.site_id_input_field = sitelink[0]
+      self.page_input_field_element.when_visible
+      self.page_input_field = sitelink[1]
+      save_sitelink_link
       wait_for_api_callback
     end
   end
 
   def get_sitelinks_order
     siteids = Array.new
-    sitelinkTable_element.each do |row|
+    sitelink_table_element.each do |row|
       siteids.push(row[1].text)
     end
     siteids.delete_at(0)
