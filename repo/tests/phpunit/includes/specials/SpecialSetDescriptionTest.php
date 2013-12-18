7917c994ffa608a769271b5249042b3a0bac47bd
Bene <benestar.wikimedia@googlemail.com>
2013-12-18 21:00:13 +0100

    Create more tests for special pages
    New test case for ModifyTerm special pages which tests the common
    functionalities of those pages. Also moved some code that was used more
    than one times into a TestItemHelper class.
    
    Change-Id: Ie13b24a4da9122bfb7c014221e5e8041d7217b3f

--- repo/tests/phpunit/includes/specials/SpecialSetDescriptionTest.php
+++ repo/tests/phpunit/includes/specials/SpecialSetDescriptionTest.php
@@ -19,56 +19,12 @@ use Wikibase\Repo\Specials\SpecialSetDescription;
  *
  * @licence GNU GPL v2+
  * @author John Erling Blad < jeblad@gmail.com >
+ * @author Bene* < benestar.wikimedia@gmail.com >
  */
-class SpecialSetDescriptionTest extends SpecialPageTestBase {
+class SpecialSetDescriptionTest extends SpecialModifyTermTestCase {
 
 	protected function newSpecialPage() {
 		return new SpecialSetDescription();
-	}
-
-	public function testExecute() {
-		//TODO: Actually verify that the output is correct.
-		//      Currently this just tests that there is no fatal error,
-		//      and that the restriction handling is working and doesn't
-		//      block. That is, the default should let the user execute
-		//      the page.
-
-		//TODO: Verify that item creation works via a faux post request
-
-		$matchers['id'] = array(
-			'tag' => 'input',
-			'attributes' => array(
-				'id' => 'wb-modifyentity-id',
-				'class' => 'wb-input',
-				'name' => 'id',
-			) );
-		$matchers['language'] = array(
-			'tag' => 'input',
-			'attributes' => array(
-				'id' => 'wb-modifyterm-language',
-				'class' => 'wb-input',
-				'name' => 'language',
-			) );
-		$matchers['value'] = array(
-			'tag' => 'input',
-			'attributes' => array(
-				'id' => 'wb-modifyterm-value',
-				'class' => 'wb-input',
-				'name' => 'value',
-			) );
-		$matchers['submit'] = array(
-			'tag' => 'input',
-			'attributes' => array(
-				'id' => 'wb-setdescription-submit',
-				'class' => 'wb-button',
-				'type' => 'submit',
-				'name' => 'wikibase-setdescription-submit',
-			) );
-
-		list( $output, ) = $this->executeSpecialPage( '' );
-		foreach( $matchers as $key => $matcher ){
-			$this->assertTag( $matcher, $output, "Failed to match html output with tag '{$key}''" );
-		}
 	}
 
 }
