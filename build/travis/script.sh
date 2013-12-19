bb80b5a10d8e0df98c0d15e2c811d8cb747eb505
jeroendedauw <jeroendedauw@gmail.com>
2013-12-19 21:17:51 +0100

    Exclude API tests
    Change-Id: I8e90adcb3b9e48ee6c259786ce9e0e6483e518a0

--- build/travis/script.sh
+++ build/travis/script.sh
@@ -6,7 +6,7 @@ cd ../phase3/tests/phpunit
 
 if [ "$TYPE" == "coverage" ]
 then
-	php phpunit.php --group Wikibase --coverage-clover ../../extensions/Wikibase/build/logs/clover.xml
+	php phpunit.php --group Wikibase --exclude-group WikibaseAPI --coverage-clover ../../extensions/Wikibase/build/logs/clover.xml
 else
-	php phpunit.php --group Wikibase
+	php phpunit.php --group Wikibase --exclude-group WikibaseAPI
 fi
