1ec68af7c4f4475097486f460ddd0e5f63ef2925
jeroendedauw <jeroendedauw@gmail.com>
2013-12-19 18:43:20 +0100

    Add missing TravisCI file and improve PHPUnit bootstrap

--- view/packages/wikibase-data-values/php/tests/bootstrap.php
+++ view/packages/wikibase-data-values/php/tests/bootstrap.php
@@ -1,5 +1,12 @@
 <?php
 
-echo exec( 'composer update' ) . "\n";
+if ( php_sapi_name() !== 'cli' ) {
+	die( 'Not an entry point' );
+}
 
-require_once( __DIR__ . '/../vendor/autoload.php' );
+$pwd = exec( 'pwd' );
+chdir( __DIR__ . '/../..' );
+passthru( 'composer update' );
+chdir( $pwd );
+
+require_once( __DIR__ . '/../../vendor/autoload.php' );
