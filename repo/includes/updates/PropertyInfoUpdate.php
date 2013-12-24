4c1ff84663c85d72aef94054e1e80363818da969
Marius Hoch <hoo@online.de>
2013-12-24 05:58:58 +0100

    Debug PropertyInfoUpdate in a saner way

--- repo/includes/updates/PropertyInfoUpdate.php
+++ repo/includes/updates/PropertyInfoUpdate.php
@@ -28,23 +28,30 @@ class PropertyInfoUpdate extends \DataUpdate {
 	 * Perform the actual work
 	 */
 	function doUpdate() {
+		$cry = false;
+		foreach( debug_backtrace() as $step ) {
+			if ( $step['function'] = 'testRebuildPropertyInfo' ) {
+				$cry = true;
+				break;
+			}
+		}
 		//XXX: Where to encode the knowledge about how to extract an info array from a Property object?
 		//     Should we have a PropertyInfo class? Or can we put this into the Property class?
-echo "PropertyInfoUpdate-doUpdate-0\n";
+if ( $cry ) echo "PropertyInfoUpdate-doUpdate-0\n";
 		$info = array(
 			PropertyInfoStore::KEY_DATA_TYPE => $this->property->getDataTypeId()
 		);
-echo "PropertyInfoUpdate-doUpdate-1\n";
-		$id = $this->property->getId();echo "PropertyInfoUpdate-doUpdate-2\n";
+if ( $cry ) echo "PropertyInfoUpdate-doUpdate-1\n";
+		$id = $this->property->getId();if ( $cry ) echo "PropertyInfoUpdate-doUpdate-2\n";
 		$oldInfo = $this->store->getPropertyInfo( $id );
-echo "PropertyInfoUpdate-doUpdate-3\n";
+if ( $cry ) echo "PropertyInfoUpdate-doUpdate-3\n";
 		if ( $oldInfo !== $info ) {
 			wfDebugLog( __CLASS__, __FUNCTION__ . ': property info for ' . $id . ' changed, updating' );
-			echo "PropertyInfoUpdate-doUpdate-setPropertyInfo-0\n";
+			if ( $cry ) echo "PropertyInfoUpdate-doUpdate-setPropertyInfo-0\n";
 			$this->store->setPropertyInfo( $id, $info );
-			echo "PropertyInfoUpdate-doUpdate-setPropertyInfo-1\n";
+			if ( $cry ) echo "PropertyInfoUpdate-doUpdate-setPropertyInfo-1\n";
 		} else {
-			echo "PropertyInfoUpdate-doUpdate-notSetPropertyInfo\n";
+			if ( $cry ) echo "PropertyInfoUpdate-doUpdate-notSetPropertyInfo\n";
 			wfDebugLog( __CLASS__, __FUNCTION__ . ': property info for ' . $id . ' didn\'t change, skipping update' );
 		}
 	}
