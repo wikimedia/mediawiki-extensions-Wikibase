23386c5cf8eec2f4328c9475c770f08e3ca9f0ec
Marius Hoch <hoo@online.de>
2013-12-24 05:25:14 +0100

    Debug PropertyInfoUpdate

--- repo/includes/updates/PropertyInfoUpdate.php
+++ repo/includes/updates/PropertyInfoUpdate.php
@@ -30,18 +30,20 @@ class PropertyInfoUpdate extends \DataUpdate {
 	function doUpdate() {
 		//XXX: Where to encode the knowledge about how to extract an info array from a Property object?
 		//     Should we have a PropertyInfo class? Or can we put this into the Property class?
-
+echo "PropertyInfoUpdate-doUpdate-0\n";
 		$info = array(
 			PropertyInfoStore::KEY_DATA_TYPE => $this->property->getDataTypeId()
 		);
-
-		$id = $this->property->getId();
+echo "PropertyInfoUpdate-doUpdate-1\n";
+		$id = $this->property->getId();echo "PropertyInfoUpdate-doUpdate-2\n";
 		$oldInfo = $this->store->getPropertyInfo( $id );
-
+echo "PropertyInfoUpdate-doUpdate-3\n";
 		if ( $oldInfo !== $info ) {
 			wfDebugLog( __CLASS__, __FUNCTION__ . ': property info for ' . $id . ' changed, updating' );
+			echo "PropertyInfoUpdate-doUpdate-setPropertyInfo\n";
 			$this->store->setPropertyInfo( $id, $info );
 		} else {
+			echo "PropertyInfoUpdate-doUpdate-notSetPropertyInfo\n";
 			wfDebugLog( __CLASS__, __FUNCTION__ . ': property info for ' . $id . ' didn\'t change, skipping update' );
 		}
 	}
