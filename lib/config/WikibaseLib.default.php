8645e96ecac3299f0fa49acec6ff37c0ed7b0c05
aude <aude.wiki@gmail.com>
2014-01-02 12:16:54 +0100

    Allow specifying different license for "data" namespaces
    This patch puts handling of copyright message into a new class,
    with tests.
    
    New settings are added to set license text and url for entity content,
    defaulting to $wgRightsText and $wgRightsUrl, but can be set to
    something different. (e.g. in cases where wikitext and data
    have different licenses)
    
    This also moves wikibase-save and wikibase-shortcopyrightwarning
    messages from WikibaseRepo to WikibaseLib since those are used there.
    
    Bug: 59206
    Change-Id: Ib56092456278822f82afb69612c1b0626212e5c5

--- lib/config/WikibaseLib.default.php
+++ lib/config/WikibaseLib.default.php
@@ -109,7 +109,7 @@ return call_user_func( function() {
 
 		// URL schemes allowed for values of the URL type.
 		// Supported types include 'http', 'https', 'ftp', and 'mailto'.
-		'urlSchemes' => array( 'http', 'https', 'ftp' ),
+		'urlSchemes' => array( 'http', 'https', 'ftp' )
 	);
 
 	// experimental stuff
