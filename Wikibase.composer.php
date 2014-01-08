e4b6fe2cdc410f4c4dadf02fbae2048eba8c8e5b
jeroendedauw <jeroendedauw@gmail.com>
2014-01-08 10:15:31 +0100

    Re-add composer entry point

---
+++ Wikibase.composer.php
@@ -0,0 +1,17 @@
+<?php
+
+// Component loader for Composer.
+// This entry point is not to be used by anything else then Composer.
+// It might well be removed at a future point.
+
+// Config:
+// wgEnableWikibaseRepo - defaults to true
+// wgEnableWikibaseClient - defaults to true
+
+if ( !array_key_exists( 'wgEnableWikibaseRepo', $GLOBALS ) || $GLOBALS['wgEnableWikibaseRepo'] ) {
+	require_once __DIR__ . '/repo/Wikibase.php';
+}
+
+if ( !array_key_exists( 'wgEnableWikibaseClient', $GLOBALS ) || $GLOBALS['wgEnableWikibaseClient'] ) {
+	require_once __DIR__ . '/client/WikibaseClient.php';
+}
