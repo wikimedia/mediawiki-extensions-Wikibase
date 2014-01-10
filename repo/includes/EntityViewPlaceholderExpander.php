5b68650c87c6fa340ff395963c07b37f87885e65
Tobias Gritschacher <tobias.gritschacher@wikimedia.de>
2014-01-10 14:02:24 +0100

    Don't get extra languages for anon users
    Bug: 49079
    Change-Id: I75b95c7a2aaa80273d58ac6ed3a8709d378b5190

--- repo/includes/EntityViewPlaceholderExpander.php
+++ repo/includes/EntityViewPlaceholderExpander.php
@@ -100,8 +100,13 @@ class EntityViewPlaceholderExpander {
 	public function getExtraUserLanguages() {
 		if ( $this->extraLanguages === null ) {
 			// ignore current interface language
-			$skip = array( $this->uiLanguage->getCode() );
-			$this->extraLanguages = $this->userLanguageLookup->getUserLanguages( $this->user, $skip );
+			if ( $this->user->isAnon() ) {
+				// no extra languages for anon user
+				$this->extraLanguages = array();
+			} else {
+				$skip = array( $this->uiLanguage->getCode() );
+				$this->extraLanguages = $this->userLanguageLookup->getUserLanguages( $this->user, $skip );
+			}
 		}
 
 		return $this->extraLanguages;
