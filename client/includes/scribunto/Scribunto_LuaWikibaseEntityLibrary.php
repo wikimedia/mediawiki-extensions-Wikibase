fd3946355909d59709e6bd8ba6138356caffaf47
Marius Hoch <hoo@online.de>
2014-01-14 17:54:06 +0100

    Implement mw.wikibase.entity.formatPropertyValues
    This renders the main Snaks belonging to a Claim (identified
    by a PropertyId).
    
    Change-Id: I6ccf58e6e750ed9cf0637730f02485c7f56779b7

--- client/includes/scribunto/Scribunto_LuaWikibaseEntityLibrary.php
+++ client/includes/scribunto/Scribunto_LuaWikibaseEntityLibrary.php
@@ -1,7 +1,11 @@
 <?php
 
+use Wikibase\Client\WikibaseClient;
+use ValueFormatters\FormatterOptions;
+use Wikibase\Lib\SnakFormatter;
 use Wikibase\Client\Scribunto\WikibaseLuaEntityBindings;
 use Wikibase\Settings;
+use Wikibase\DataModel\Entity\EntityIdParsingException;
 
 /**
  * Registers and defines functions to access Wikibase through the Scribunto extension
@@ -24,8 +28,25 @@ class Scribunto_LuaWikibaseEntityLibrary extends Scribunto_LuaLibraryBase {
 	 * @since 0.5
 	 */
 	public function __construct( $engine ) {
+		// For the language we need $wgContLang, not parser target language or anything else.
+		// See Scribunto_LuaLanguageLibrary::getContLangCode().
+		global $wgContLang;
+
+		$wikibaseClient = WikibaseClient::getDefaultInstance();
+
+		$formatterOptions = new FormatterOptions( array( "language" => $wgContLang ) );
+
+		$snakFormatter = $wikibaseClient->getSnakFormatterFactory()->getSnakFormatter(
+			SnakFormatter::FORMAT_WIKI, $formatterOptions
+		);
+
 		$this->wbLibrary = new WikibaseLuaEntityBindings(
-			Settings::get( 'siteGlobalID' )
+			$snakFormatter,
+			$wikibaseClient->getEntityIdParser(),
+			$wikibaseClient->getStore()->getEntityLookup(),
+			$wikibaseClient->getEntityIdFormatter(),
+			Settings::get( 'siteGlobalID' ),
+			$wgContLang
 		);
 
 		parent::__construct( $engine );
@@ -39,6 +60,7 @@ class Scribunto_LuaWikibaseEntityLibrary extends Scribunto_LuaLibraryBase {
 	public function register() {
 		$lib = array(
 			'getGlobalSiteId' => array( $this, 'getGlobalSiteId' ),
+			'formatPropertyValues' => array( $this, 'formatPropertyValues' ),
 		);
 
 		return $this->getEngine()->registerInterface(
@@ -55,4 +77,29 @@ class Scribunto_LuaWikibaseEntityLibrary extends Scribunto_LuaLibraryBase {
 	public function getGlobalSiteId() {
 		return $this->wbLibrary->getGlobalSiteId();
 	}
+
+	/**
+	 * Render the main Snaks belonging to a Claim (which is identified by a PropertyId).
+	 *
+	 * @since 0.5
+	 *
+	 * @param string $entityId
+	 * @param string $propertyId
+	 *
+	 * @throws ScribuntoException
+	 * @return array
+	 */
+	public function formatPropertyValues( $entityId, $propertyId ) {
+		$this->checkType( 'formatPropertyValues', 0, $entityId, 'string' );
+		// Use 1 as index for the property id, as the first parameter comes from
+		// internals of mw.wikibase.entity (an index of 2 might confuse users
+		// as they only gave one parameter themselves)
+		$this->checkType( 'formatPropertyValues', 1, $propertyId, 'string' );
+		try {
+			return $this->wbLibrary->formatPropertyValues( $entityId, $propertyId );
+		} catch ( EntityIdParsingException $e ) {
+			throw new ScribuntoException( 'wikibase-error-invalid-entity-id' );
+		}
+	}
+
 }
