f7a5ad228b0737e4aa7a36b956cc2458d02e36f2
Marius Hoch <hoo@online.de>
2014-01-14 17:54:06 +0100

    Implement mw.wikibase.entity.renderClaims [WIP]
    Change-Id: I6ccf58e6e750ed9cf0637730f02485c7f56779b7

--- client/tests/phpunit/includes/scribunto/Scribunto_LuaWikibaseEntityLibraryTest.php
+++ client/tests/phpunit/includes/scribunto/Scribunto_LuaWikibaseEntityLibraryTest.php
@@ -58,6 +58,14 @@ class Scribunto_LuaWikibaseEntityLibraryTest extends \MediaWikiTestCase {
 		$this->assertSame( $expected, $luaWikibaseLibrary->getGlobalSiteId() );
 	}
 
+	public function testFormatPropertyValues() {
+		$luaWikibaseLibrary = $this->newScribuntoLuaWikibaseLibrary();
+		$this->assertSame(
+			array( '' ),
+			$luaWikibaseLibrary->formatPropertyValues( 'Q1', 'P65536' )
+		);
+	}
+
 	private function newScribuntoLuaWikibaseLibrary() {
 		$engine = Scribunto::newDefaultEngine( array(
 			'title' => Title::newFromText( 'Whatever' )
