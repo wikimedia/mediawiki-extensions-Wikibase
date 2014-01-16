fb28fd591eefeea6cc1026e77107b6d6ca640480
Marius Hoch <hoo@online.de>
2014-01-16 14:41:34 +0100

    Integration tests for Client's Scribunto integration
    This also removes the broken test mode handling from
    WikibaseClient as that made it impossible to properly
    inject a ClientStore mock.
    
    Change-Id: I026fa93047f9564c997a4005952306c853ff5334

---
+++ client/tests/phpunit/MockClientStore.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Wikibase\Test;
+use Wikibase\Test\MockRepository;
+use Wikibase\Test\MockPropertyInfoStore;
+use Wikibase\ClientStore;
+use Wikibase\SiteLinkLookup;
+use Wikibase\PropertyInfoStore;
+
+/**
+ * (Incomplete) ClientStore mock
+ *
+ * @since 0.5
+ *
+ * @license GNU GPL v2+
+ * @author Marius Hoch < hoo@online.de >
+ */
+class MockClientStore implements ClientStore {
+	public function getItemUsageIndex() {}
+	public function getPropertyLabelResolver() {}
+	public function getTermIndex() {}
+	public function newChangesTable() {}
+	public function clear() {}
+	public function rebuild() {}
+
+	private function getMock() {
+		static $mockRepo = false;
+		if ( !$mockRepo ) {
+			$mockRepo = new MockRepository();
+		}
+
+		return $mockRepo;
+	}
+
+	/*
+	 * @return EntityLookup
+	 */
+	public function getEntityLookup() {
+		return $this->getMock();
+	}
+
+	/**
+	 * @return SiteLinkLookup
+	 */
+	public function getSiteLinkTable() {
+		return $this->getMock();
+	}
+
+	/**
+	 * @return PropertyInfoStore
+	 */
+	public function getPropertyInfoStore() {
+		static $mockPropertyInfoStore = false;
+		if ( !$mockPropertyInfoStore ) {
+			$mockPropertyInfoStore = new MockPropertyInfoStore();
+		}
+		return $mockPropertyInfoStore;
+	}
+}
