13618d090aceebf2d27b238f35663e5d29fcf0f2
Marius Hoch <hoo@online.de>
2014-01-16 14:41:34 +0100

    Integration tests for Client's Scribunto integration
    This also removes the broken test mode handling from
    WikibaseClient as that made it impossible to properly
    inject a ClientStore mock.
    
    Change-Id: I026fa93047f9564c997a4005952306c853ff5334

--- client/tests/phpunit/includes/WikibaseClientTest.php
+++ client/tests/phpunit/includes/WikibaseClientTest.php
@@ -99,7 +99,7 @@ class WikibaseClientTest extends \PHPUnit_Framework_TestCase {
 	 * @dataProvider getLangLinkSiteGroupProvider
 	 */
 	public function testGetLangLinkSiteGroup( $expected, $settings, $siteStore ) {
-		$client = new WikibaseClient( $settings, Language::factory( 'en' ), true, $siteStore );
+		$client = new WikibaseClient( $settings, Language::factory( 'en' ), $siteStore );
 		$this->assertEquals( $expected, $client->getLangLinkSiteGroup() );
 	}
 
@@ -124,7 +124,7 @@ class WikibaseClientTest extends \PHPUnit_Framework_TestCase {
 	 * @dataProvider getSiteGroupProvider
 	 */
 	public function testGetSiteGroup( $expected, SettingsArray $settings, SiteStore $siteStore ) {
-		$client = new WikibaseClient( $settings, Language::factory( 'en' ), true, $siteStore );
+		$client = new WikibaseClient( $settings, Language::factory( 'en' ), $siteStore );
 		$this->assertEquals( $expected, $client->getSiteGroup() );
 	}
 
