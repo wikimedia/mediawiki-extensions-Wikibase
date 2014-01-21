6dd0f8b6a41623f8f4a3847ccc331383d63158b7
jeroendedauw <jeroendedauw@gmail.com>
2014-01-21 23:49:24 +0100

    Wikibase is not currently using Coveralls, so rem from Travis config
    Change-Id: I392fbada52660d0b3d93678c96cc94443c85fe94

--- build/travis/script.sh
+++ build/travis/script.sh
@@ -4,9 +4,4 @@ set -x
 
 cd ../phase3/tests/phpunit
 
-if [ "$TYPE" == "coverage" ]
-then
-	php phpunit.php --group Wikibase --coverage-clover ../../extensions/Wikibase/build/logs/clover.xml
-else
-	php phpunit.php --group Wikibase
-fi
+php phpunit.php --group Wikibase
