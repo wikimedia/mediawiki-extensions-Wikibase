f32cf412c44f5c18d7fb2f9678c90352e1bf997c
jeroendedauw <jeroendedauw@gmail.com>
2014-01-21 23:41:19 +0100

    Add doc to Wikibase Lib to avoid common fallacy
    Change-Id: I7a1a09dc6314414af82d5850864e4e5995b94e3d

--- lib/config/WikibaseLib.default.php
+++ lib/config/WikibaseLib.default.php
@@ -1,6 +1,10 @@
 <?php
 
 /**
+ * Lib is not an application and should not have global config state.
+ * Therefore you should NOT add new settings here. If both client and repo
+ * need them, add them to both.
+ *
  * This file assigns the default values to all Wikibase Lib settings.
  *
  * This file is NOT an entry point the WikibaseLib extension. Use WikibaseLib.php.
