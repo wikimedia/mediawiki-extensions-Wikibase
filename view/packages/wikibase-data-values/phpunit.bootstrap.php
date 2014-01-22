058dd02cfb66785ad99632325eb93853a6450d59
snaterlicious <github@snater.com>
2014-01-22 14:21:59 +0100

    Updated phpunit files

---
+++ view/packages/wikibase-data-values/phpunit.bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+if ( php_sapi_name() !== 'cli' ) {
+	die( 'Not an entry point' );
+}
+
+$pwd = exec( 'pwd' );
+passthru( 'composer update' );
+chdir( $pwd );
+
+require_once( __DIR__ . './vendor/autoload.php' );
