794f775f156daf959dbe0680f39ea6f8c8351180
Bene <benestar.wikimedia@googlemail.com>
2014-02-01 23:54:17 +0100

    Create more tests for Special:SetSiteLink
    Added tests for passed subpage values.
    
    Change-Id: Ie477c51ab77c71c396fc671c6d09fa8f18449a33

--- repo/tests/phpunit/includes/specials/SpecialSetSitelinkTest.php
+++ repo/tests/phpunit/includes/specials/SpecialSetSitelinkTest.php
@@ -3,6 +3,10 @@
 namespace Wikibase\Test;
 
 use Wikibase\Repo\Specials\SpecialSetSiteLink;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\DataModel\SiteLink;
+use Wikibase\ItemContent;
+use Wikibase\Settings;
 
 /**
  * @covers Wikibase\Repo\Specials\SpecialSetSiteLink
@@ -17,37 +21,47 @@ use Wikibase\Repo\Specials\SpecialSetSiteLink;
  *
  * @licence GNU GPL v2+
  * @author Adam Shorland
+ * @author Bene* < benestar.wikimedia@gmail.com >
  */
 class SpecialSetSitelinkTest extends SpecialPageTestBase {
+
+	/**
+	 * @var array
+	 */
+	private $matchers = array();
+
+	/**
+	 * @var string
+	 */
+	private $itemId;
+
+	/**
+	 * @var string
+	 */
+	private $badgeId;
 
 	protected function newSpecialPage() {
 		return new SpecialSetSiteLink();
 	}
 
-	public function testExecute() {
-		//TODO: Actually verify that the output is correct.
-		//      Currently this just tests that there is no fatal error,
-		//      and that the restriction handling is working and doesn't
-		//      block. That is, the default should let the user execute
-		//      the page.
+	public function setUp() {
+		$this->createItems();
 
-		//TODO: Verify that item creation works via a faux post request
-
-		$matchers['id'] = array(
+		$this->matchers['id'] = array(
 			'tag' => 'input',
 			'attributes' => array(
 				'id' => 'wb-modifyentity-id',
 				'class' => 'wb-input',
 				'name' => 'id',
 			) );
-		$matchers['site'] = array(
+		$this->matchers['site'] = array(
 			'tag' => 'input',
 			'attributes' => array(
 				'id' => 'wb-setsitelink-site',
 				'class' => 'wb-input',
 				'name' => 'site',
 			) );
-		$matchers['page'] = array(
+		$this->matchers['page'] = array(
 			'tag' => 'input',
 			'attributes' => array(
 				'id' => 'wb-setsitelink-page',
@@ -58,7 +72,7 @@ class SpecialSetSitelinkTest extends SpecialPageTestBase {
 		// Experimental setting of badges on the special page
 		// @todo remove experimental once JS UI is in place, (also remove the experimental test case)
 		if ( defined( 'WB_EXPERIMENTAL_FEATURES' ) && WB_EXPERIMENTAL_FEATURES ) {
-			$matchers['badges'] = array(
+			$this->matchers['badges'] = array(
 				'tag' => 'input',
 				'attributes' => array(
 					'id' => 'wb-setsitelink-badges',
@@ -67,7 +81,7 @@ class SpecialSetSitelinkTest extends SpecialPageTestBase {
 				) );
 		}
 
-		$matchers['submit'] = array(
+		$this->matchers['submit'] = array(
 			'tag' => 'input',
 			'attributes' => array(
 				'id' => 'wb-setsitelink-submit',
@@ -75,11 +89,77 @@ class SpecialSetSitelinkTest extends SpecialPageTestBase {
 				'type' => 'submit',
 				'name' => 'wikibase-setsitelink-submit',
 			) );
+	}
 
+	private function createItems() {
+		// create empty badge
+		$badge = Item::newEmpty();
+		// save badge
+		ItemContent::newFromItem( $badge )->save( "testing", null, EDIT_NEW );
+		// set the badge id
+		$this->badgeId = $badge->getId()->getSerialization();
+		// add badge to settings
+		Settings::singleton()->setSetting( 'badgeItems', array( $badgeId => '' ) );
+		// create empty item
+		$item = Item::newEmpty();
+		// add data and check if it is shown in the form
+		$item->addSiteLink( new SiteLink( 'dewiki', 'Wikidata', array( $badge->getId() ) ) );
+		// save the item
+		ItemContent::newFromItem( $item )->save( "testing", null, EDIT_NEW );
+		// set the item id
+		$this->itemId = $item->getId()->getSerialization();
+	}
+
+	public function testExecute() {
+		// execute with no subpage value
 		list( $output, ) = $this->executeSpecialPage( '' );
-		foreach( $matchers as $key => $matcher ){
+
+		foreach( $this->matchers as $key => $matcher ){
 			$this->assertTag( $matcher, $output, "Failed to match html output with tag '{$key}''" );
 		}
 	}
 
+	public function testExecuteOneValue() {
+		// execute with one subpage value
+		list( $output, ) = $this->executeSpecialPage( $this->itemId );
+		$this->matchers['id']['attributes']['value'] = $this->itemId;
+
+		foreach( $this->matchers as $key => $matcher ) {
+			$this->assertTag( $matcher, $output, "Failed to match html output with tag '{$key}' passing one subpage value" );
+		}
+	}
+
+	public function testExecuteTwoValues() {
+		// execute with two subpage values
+		list( $output, ) = $this->executeSpecialPage( $this->itemId . '/dewiki' );
+		$this->matchers['id']['attributes'] = array(
+			'type' => 'hidden',
+			'name' => 'id',
+			'value' => $this->itemId,
+		);
+		$this->matchers['site']['attributes'] = array(
+			'type' => 'hidden',
+			'name' => 'language',
+			'value' => 'dewiki',
+		);
+		$this->matchers['remove'] = array(
+			'tag' => 'input',
+			'attributes' => array(
+				'type' => 'hidden',
+				'name' => 'remove',
+				'value' => 'remove',
+			) );
+
+		$this->matchers['value']['attributes']['value'] = 'Wikidata';
+
+		// Experimental setting of badges on the special page
+		// @todo remove experimental once JS UI is in place, (also remove the experimental test case)
+		if ( defined( 'WB_EXPERIMENTAL_FEATURES' ) && WB_EXPERIMENTAL_FEATURES ) {
+			$this->matchers['badges']['attributes']['value'] = $this->badgeId;
+		}
+
+		foreach( $this->matchers as $key => $matcher ) {
+			$this->assertTag( $matcher, $output, "Failed to match html output with tag '{$key}' passing two subpage values" );
+		}
+	}
 }
