9847f5fdd319202794e228a409c0fcbda8753b51
addshore <addshorewiki@gmail.com>
2014-02-04 16:22:33 +0100

    Implement TimeParsers
    Adds Classes:
     - DateTimeParser
     - MonthNameUnlocalizer
     - MWTimeIsoParser
     - TimeParser
     - YearMonthTimeParser
     - YearTimeParser
    
    Change-Id: Ia9080c74f2d72f50fc7d822d153abb37916cfc06

---
+++ lib/includes/parsers/DateTimeParser.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Wikibase\Lib\Parsers;
+
+use DataValues\TimeValue;
+use DateTime;
+use Exception;
+use ValueParsers\CalenderModelParser;
+use ValueParsers\ParseException;
+use ValueParsers\StringValueParser;
+use ValueParsers\ParserOptions;
+
+/**
+ * Time Parser using the DateTime object
+ *
+ * @since 0.5
+ *
+ * @licence GNU GPL v2+
+ * @author Adam Shorland
+ */
+class DateTimeParser extends StringValueParser {
+
+	const FORMAT_STRING = 'Y-m-d\TH:i:s\Z';
+
+	/**
+	 * Parses the provided string and returns the result.
+	 *
+	 * @param string $value
+	 *
+	 * @throws ParseException
+	 * @return TimeValue
+	 */
+	protected function stringParse( $value ) {
+		global $wgLang;
+		$parser = new \ValueParsers\TimeParser( new CalenderModelParser(), $this->getOptions() );
+		$monthUnlocaliser = new MonthNameUnlocalizer();
+		$options = new ParserOptions();
+		try{
+			$value = $monthUnlocaliser->unlocalize( $value, $wgLang->getCode(),$options );
+			$result = new DateTime( $value );
+			$time = $result->format( self::FORMAT_STRING );
+			$time = '+' . str_repeat( '0', 32 - strlen( $time ) ) . $time;
+			return $parser->parse( $time );
+		}
+		catch( Exception $e ) {
+			throw new ParseException( $e->getMessage() );
+		}
+	}
+
+}
---
+++ lib/includes/parsers/MWTimeIsoParser.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Wikibase\Lib\Parsers;
+
+use DataValues\TimeValue;
+use Message;
+use ValueParsers\CalenderModelParser;
+use ValueParsers\ParseException;
+use ValueParsers\StringValueParser;
+
+/**
+ * Class to parse values that can be formatted by MWTimeIsoFormatter
+ * This includes parsing of localized values
+ *
+ * @since 0.5
+ *
+ * @licence GNU GPL v2+
+ * @author Adam Shorland
+ */
+class MWTimeIsoParser extends StringValueParser {
+
+	/**
+	 * Prefix for precision message keys
+	 */
+	const MSG_PREFIX = 'wikibase-time-precision-';
+
+	/**
+	 * Placeholder value used when exploding parsable times
+	 */
+	private static $NUM_PLACEHOLDER = 990990990990990;
+
+	/**
+	 * @var array message key suffixes pointing to possible precisions
+	 * Should be prefixed by MSG_PREFIX to get the message
+	 */
+	private static $precisionMsgKeys = array(
+		'Gannum' => array( TimeValue::PRECISION_Ga ),
+		'Mannum' => array( TimeValue::PRECISION_100Ma, TimeValue::PRECISION_10Ma, TimeValue::PRECISION_Ma ),
+		'annum' => array( TimeValue::PRECISION_100ka, TimeValue::PRECISION_10ka ),
+		'millennium' => array( TimeValue::PRECISION_ka ),
+		'century' => array( TimeValue::PRECISION_100a ),
+		'10annum' => array( TimeValue::PRECISION_10a ),
+	);
+
+	/**
+	 * Parses the provided string and returns the result.
+	 *
+	 * @param string $value
+	 *
+	 * @throws ParseException
+	 * @return TimeValue
+	 */
+	protected function stringParse( $value ) {
+		$reconverted = $this->reconvertOutputString( $value );
+		if( $reconverted !== false ) {
+			return $reconverted;
+		}
+
+		throw new ParseException( 'foo' );
+	}
+
+	/**
+	 * Analyzes a string if it is a time value that has been specified in one of the output
+	 * precision formats specified in the settings. If so, this method re-converts such an output
+	 * string to an object that can be used to instantiate a time.Time object.
+	 *
+	 * @param string $value
+	 * @return TimeValue|bool
+	 */
+	private function reconvertOutputString( $value ) {
+		global $wgLang;
+		foreach( self::$precisionMsgKeys as $msgKeySuffix => $possiblePrecisions ) {
+			$msg = new Message( self::MSG_PREFIX . $msgKeySuffix );
+			$msg->inLanguage( $wgLang ); // todo check other translations?
+			$msg->numParams( array( self::$NUM_PLACEHOLDER ) );
+			$string = $msg->text();
+
+			list( $start, $end ) = explode( self::$NUM_PLACEHOLDER, $string, 2 );
+
+			if( preg_match( '/^\s*' . preg_quote( $start ) . '(.+?)' . preg_quote( $end ) . '\s*$/i', $value, $matches ) ) {
+				list( , $number ) = $matches;
+				$number = $wgLang->parseFormattedNumber( $number );
+				$years = null;
+				switch( $msgKeySuffix ) {
+					case 'Gannum':
+						return $this->getTimeFromYear( $number . str_repeat( '0', 9 ) );
+					case 'Mannum':
+						return $this->getTimeFromYear( $number . str_repeat( '0', 6 ) );
+					case 'millennium':
+						return $this->getTimeFromYear( $number . str_repeat( '0', 3 ) );
+					case 'century':
+						return $this->getTimeFromYear( $number . str_repeat( '0', 2 ) );
+					case 'annum':
+					case '10annum':
+					return $this->getTimeFromYear( $number );
+						break;
+				}
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * @param string $year
+	 * @return TimeValue
+	 */
+	private function getTimeFromYear( $year ) {
+		$timeParser = new \ValueParsers\TimeParser( new CalenderModelParser(), $this->getOptions() );
+		return $timeParser->parse( '+' . $year . '-00-00T00:00:00Z' );
+	}
+
+}
