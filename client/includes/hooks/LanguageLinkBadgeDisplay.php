9c25e40412f048ee4f71056179388095b5f6595b
Bene <benestar.wikimedia@googlemail.com>
2014-02-05 22:59:39 +0100

    Insert badges for language links on client
    Uses the created hook SkinTemplateGetLanguageLink to add a CSS class
    for each badge of a sitelink. The CSS class is built using the format
    "badge-<itemid>". The icons are added by the extension but the clients
    can then override them using their local Common.css page. Also a title
    is added for each badge based on the badge item's description.
    
    Bug: 60717
    Change-Id: I24747a2cfffb48d527882ab4de24567e5199d0b0

---
+++ client/includes/hooks/LanguageLinkBadgeDisplay.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace Wikibase\Client\Hooks;
+
+use Site;
+use Title;
+use SiteStore;
+use Wikibase\SiteLinkLookup;
+use Wikibase\EntityLookup;
+use Wikibase\DataModel\SiteLink;
+use Wikibase\DataModel\Entity\ItemId;
+
+/**
+ * Provides access to the badges of the current page's sitelinks
+ * and adds some properties to the HTML output to display them.
+ *
+ * @since 0.5
+ *
+ * @licence GNU GPL v2+
+ * @author Bene* < benestar.wikimedia@gmail.com >
+ */
+class LanguageLinkBadgeDisplay {
+
+	/**
+	 * @var string
+	 */
+	protected $localSiteId;
+
+	/**
+	 * @var SiteLinkLookup
+	 */
+	protected $siteLinkLookup;
+
+	/**
+	 * @var EntityLookup
+	 */
+	protected $entityLookup;
+
+	/**
+	 * @var SiteStore
+	 */
+	protected $sites;
+
+	/**
+	 * @var array
+	 */
+	protected $displayBadges;
+
+	/**
+	 * @var string
+	 */
+	protected $langCode;
+
+	/**
+	 * @var Site[]
+	 */
+	protected $sitesByNavigationId = null;
+
+	/**
+	 * @param string $localSiteId Global id of the client wiki
+	 * @param SiteLinkLookup $siteLinkLookup
+	 * @param EntityLookup $entityLookup
+	 * @param SiteStore $sites
+	 * @param array $displayBadges
+	 * @param string $langCode
+	 */
+	public function __construct( $localSiteId, SiteLinkLookup $siteLinkLookup,
+			EntityLookup $entityLookup, SiteStore $sites, array $displayBadges, $langCode ) {
+		$this->localSiteId = $localSiteId;
+		$this->siteLinkLookup = $siteLinkLookup;
+		$this->entityLookup = $entityLookup;
+		$this->sites = $sites;
+		$this->displayBadges = $displayBadges;
+		$this->langCode = $langCode;
+	}
+
+	/**
+	 * Looks up the item of the given title and assigns the badges of the sitelink
+	 * associated with the given language link title to the passed array.
+	 *
+	 * @since 0.5
+	 *
+	 * @param Title $title
+	 * @param Title $languageLinkTitle
+	 * @param array &$languageLink
+	 */
+	public function assignBadges( Title $title, Title $languageLinkTitle, array &$languageLink ) {
+		$site = $this->getSiteByNavigationId( $languageLinkTitle->getInterwiki() );
+		if ( !$site ) {
+			return array();
+		}
+
+		$siteLink = $this->getSiteLink( $title, $site->getGlobalId() );
+		if ( !$siteLink ) {
+			return array();
+		}
+
+		$linkBadges = array();
+		foreach ( $siteLink->getBadges() as $badgeObject ) {
+			$badge = $badgeObject->getSerialization();
+			if ( !isset( $languageLink['class'] ) ) {
+				$languageLink['class'] = "badge-$badge";
+			} else {
+				$languageLink['class'] .= " badge-$badge";
+			}
+			$linkBadges[] = $badge;
+		}
+		
+		foreach ( $this->displayBadges as $badge ) {
+			if ( in_array( $badge, $linkBadges ) ) {
+				$title = $this->getTitle( $badge );
+				if ( $title !== null ) {
+					// if a badge comes later in the config,
+					// this will override the title as documented.
+					$languageLink['itemtitle'] = $title;
+				}
+			}
+		}
+	}
+
+	/**
+	 * Finds the corresponding item on the repository and
+	 * returns the item's site link for the given site id.
+	 *
+	 * @since 0.5
+	 *
+	 * @param Title $title
+	 * @param string $languageLinkSiteId
+	 *
+	 * @return SiteLink|null
+	 */
+	protected function getSiteLink( Title $title, $languageLinkSiteId ) {
+		$siteLink = new SiteLink( $this->localSiteId, $title->getText() );
+		$itemId = $this->siteLinkLookup->getEntityIdForSiteLink( $siteLink );
+
+		if ( $itemId === null ) {
+			return null;
+		}
+
+		$item = $this->entityLookup->getEntity( $itemId );
+		if ( !$item->hasLinkToSite( $languageLinkSiteId ) ) {
+			return null;
+		}
+		return $item->getSiteLink( $languageLinkSiteId );
+	}
+
+	/**
+	 * Returns the title for the given badge.
+	 *
+	 * @since 0.5
+	 *
+	 * @param string $badge
+	 *
+	 * @return string|null
+	 */
+	protected function getTitle( $badge ) {
+		$entity = $this->entityLookup->getEntity( new ItemId( $badge ) );
+		if ( !$entity ) {
+			return null;
+		}
+
+		$description = $entity->getDescription( $this->langCode );
+		if ( !$description ) {
+			return null;
+		}
+		return $description;
+	}
+
+	/**
+	 * Returns a Site object for the given navigational ID (alias inter-language prefix).
+	 *
+	 * @todo: move this functionality into Sites/SiteList/SiteArray!
+	 *        This snippet has been copied from LangLinkHandler until we have it in core.
+	 *
+	 * @param string $id The navigation ID to find a site for.
+	 *
+	 * @return bool|Site The site with the given navigational ID, or false if not found.
+	 */
+	protected function getSiteByNavigationId( $id ) {
+		wfProfileIn( __METHOD__ );
+
+		//FIXME: this needs to be moved into core, into SiteList resp. SiteArray!
+		if ( $this->sitesByNavigationId === null ) {
+			$this->sitesByNavigationId = array();
+
+			/* @var Site $site */
+			foreach ( $this->sites->getSites() as $site ) {
+				$ids = $site->getNavigationIds();
+
+				foreach ( $ids as $navId ) {
+					$this->sitesByNavigationId[$navId] = $site;
+				}
+			}
+		}
+
+		wfProfileOut( __METHOD__ );
+		return isset( $this->sitesByNavigationId[$id] ) ? $this->sitesByNavigationId[$id] : false;
+	}
+
+}
