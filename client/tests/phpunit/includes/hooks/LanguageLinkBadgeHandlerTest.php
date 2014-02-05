2a318e6c92c30d60416f088f58ab7728f22ccc2c
Bene <benestar.wikimedia@googlemail.com>
2014-02-05 22:59:39 +0100

    Insert badges for language links on client
    Uses the proposed hook SkinTemplateGetLanguageLink to add a CSS class for
    each badge of a sitelink. The CSS class is built using the format
    "badge-<itemid>". The clients can then add an icon using their local
    Common.css page.
    
    Please note that this patch is not ready and still lacks tests.
    
    Bug: 60717
    Change-Id: I24747a2cfffb48d527882ab4de24567e5199d0b0

---
+++ client/tests/phpunit/includes/hooks/LanguageLinkBadgeHandlerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Wikibase\Test;
+
+
+/**
+ * @covers Wikibase\Client\Hooks\LanguageLinkBadgeHandler
+ *
+ * @since 0.5
+ *
+ * @group WikibaseClient
+ * @group Wikibase
+ * @group Database
+ *
+ * @licence GNU GPL v2+
+ * @author Bene* < benestar.wikimedia@gmail.com >
+ */
+class LanguageLinkBadgeHandlerTest extends \MediaWikiTestCase {
+
+	public function testGetSiteLink() {
+		// TODO
+	}
+
+	public function testGetBadges() {
+		// TODO
+	}
+
+}
