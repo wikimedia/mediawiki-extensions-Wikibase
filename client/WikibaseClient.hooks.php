5c732b415b16096cf45b314534b4f3bea1792ed1
Bene <benestar.wikimedia@googlemail.com>
2014-02-05 22:59:39 +0100

    Insert badges for language links on client
    Uses the created hook SkinTemplateGetLanguageLink to add a CSS class
    for each badge of a sitelink. The CSS class is built using the format
    "badge-<itemid>". The icons are added by the extension but the clients
    can then override them using their local Common.css page. Also a title
    is added for each badge based on the badge item's description.
    
    Bug: 60717
    Change-Id: I24747a2cfffb48d527882ab4de24567e5199d0b0

--- client/WikibaseClient.hooks.php
+++ client/WikibaseClient.hooks.php
@@ -27,10 +27,13 @@ use StripState;
 use Title;
 use UnexpectedValueException;
 use User;
+use Sanitizer;
 use Wikibase\Client\Hooks\InfoActionHookHandler;
 use Wikibase\Client\Hooks\SpecialWatchlistQueryHandler;
 use Wikibase\Client\MovePageNotice;
 use Wikibase\Client\Hooks\OtherProjectsSidebarGenerator;
+use Wikibase\Client\Hooks\LanguageLinkBadgeDisplay;
+use Wikibase\Client\ClientSiteLinkLookup;
 use Wikibase\Client\WikibaseClient;
 
 /**
@@ -46,6 +49,7 @@ use Wikibase\Client\WikibaseClient;
  * @author Tobias Gritschacher
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  * @author Marius Hoch < hoo@online.de >
+ * @author Bene* < benestar.wikimedia@gmail.com >
  */
 final class ClientHooks {
 
@@ -168,6 +172,7 @@ final class ClientHooks {
 	public static function onSpecialMovepageAfterMove( MovePageForm $movePage, Title &$oldTitle,
 		Title &$newTitle ) {
 		$wikibaseClient = WikibaseClient::getDefaultInstance();
+
 		$siteLinkLookup = $wikibaseClient->getStore()->getSiteLinkTable();
 		$repoLinker = $wikibaseClient->newRepoLinker();
 
@@ -398,6 +403,50 @@ final class ClientHooks {
 	}
 
 	/**
+	 * Add badges to the language links.
+	 *
+	 * @since 0.5
+	 *
+	 * @param array &$languageLink
+	 * @param Title $languageLinkTitle
+	 * @param Title $title
+	 *
+	 * @return bool
+	 */
+	public static function onSkinTemplateGetLanguageLink( &$languageLink, Title $languageLinkTitle, Title $title ) {
+		wfProfileIn( __METHOD__ );
+
+		global $wgLang;
+
+		$wikibaseClient = WikibaseClient::getDefaultInstance();
+		$settings = $wikibaseClient->getSettings();
+
+		$localSiteId = $settings->getSetting( 'siteGlobalID' );
+		$siteLinkLookup = $wikibaseClient->getStore()->getSiteLinkTable();
+		$entityLookup = $wikibaseClient->getStore()->getEntityLookup();
+		$clientSiteLinkLookup = new ClientSiteLinkLookup( $localSiteId, $siteLinkLookup, $entityLookup );
+		$sites = SiteSQLStore::newInstance()->getSites();
+		$displayBadges = $settings->getSetting( 'displayBadges' );
+
+		if ( !is_array( $displayBadges ) ) {
+			$displayBadges = array();
+		}
+
+		$languageLinkBadgeDisplay = new LanguageLinkBadgeDisplay(
+			$clientSiteLinkLookup,
+			$entityLookup,
+			$sites,
+			array_keys( $displayBadges ),
+			$wgLang->getCode()
+		);
+
+		$languageLinkBadgeDisplay->assignBadges( $title, $languageLinkTitle, $languageLink );
+
+		wfProfileOut( __METHOD__ );
+		return true;
+	}
+
+	/**
 	 * Add Wikibase item link in toolbox
 	 *
 	 * @since 0.4
@@ -405,7 +454,7 @@ final class ClientHooks {
 	 * @param QuickTemplate &$sk
 	 * @param array &$toolbox
 	 *
-	 * @return boolean
+	 * @return bool
 	 */
 	public static function onBaseTemplateToolbox( QuickTemplate &$sk, &$toolbox ) {
 		$prefixedId = $sk->getSkin()->getOutput()->getProperty( 'wikibase_item' );
@@ -486,6 +535,19 @@ final class ClientHooks {
 				// (needed as jquery.wikibase.linkitem has pretty heavy dependencies)
 				$out->addModules( 'wikibase.client.linkitem.init' );
 			}
+		}
+
+		$displayBadges = $settings->getSetting( 'displayBadges' );
+
+		if ( is_array( $displayBadges ) ) {
+			$badgesCss = '';
+			foreach ( $displayBadges as $badge => $icon ) {
+				$badge = Sanitizer::escapeClass( $badge );
+				$icon = Sanitizer::checkCss( $icon );
+				$icon = Sanitizer::cleanUrl( $icon );
+				$badgesCss .= ".badges-$badge { list-style-image: url( '$icon' ); }\n";
+			}
+			$out->addInlineStyle( $badgesCss );
 		}
 
 		wfProfileOut( __METHOD__ );
