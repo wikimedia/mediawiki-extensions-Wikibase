b2b4fe3e34a76e2023938ce6c1c35f2ef2943513
aude <aude.wiki@gmail.com>
2014-02-10 15:47:45 +0100

    Use BaseTemplateAfterPortlet hook to add "edit/add links" link
    Depends on I099fb2f in core which is merged.
    
    Bug: 57094
    Change-Id: Ib845ca2cc81dd39c18d58b8a9f81a56b00db636b

---
+++ client/includes/hooks/BaseTemplateAfterPortletHandler.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Wikibase\Client\Hooks;
+
+use BaseTemplate;
+use Html;
+
+/**
+ * @since 0.5
+ *
+ * @licence GNU GPL v2+
+ * @author Katie Filbert < aude.wiki@gmail.com >
+ */
+class BaseTemplateAfterPortletHandler {
+
+	/**
+	 * @param BaseTemplate $skinTemplate
+	 * @param string $name
+	 *
+	 * @return string|null
+	 */
+	public function handle( BaseTemplate $baseTemplate, $name ) {
+		if ( $name === 'lang' ) {
+			$link = $baseTemplate->get( 'wbeditlanglinks' );
+
+			if ( $link ) {
+				return $this->formatLink( $link );
+			}
+		}
+
+		return null;
+	}
+
+	/**
+	 * @param array $link
+	 *
+	 * @return string
+	 */
+	private function formatLink( array $link ) {
+		$action = $link['action'];
+		$link = Html::element( 'a', $link, $link['text'] );
+
+		$html = Html::rawElement(
+			'span',
+			array(
+				'class' => "wb-langlinks-$action wb-langlinks-link"
+			),
+			$link
+		);
+
+		return $html;
+	}
+
+}
