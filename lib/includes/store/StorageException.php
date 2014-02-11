668982b3b65990eb80076a110c49132559736f48
daniel <daniel.kinzler@wikimedia.de>
2014-02-11 20:22:33 +0100

    Refactoring EditEntity to use EntityStore
    This attempts to remove any hard coded dependencies
    on EntityContent from EditEntity.
    
    INCOMPLETE, check constuctor calls for EditEntity.
    
    Change-Id: Ieb3be710c6a9e952ce4c2c40e3d89632a51836a2

--- lib/includes/store/StorageException.php
+++ lib/includes/store/StorageException.php
@@ -10,6 +10,8 @@
 
 
 namespace Wikibase;
+use Exception;
+use Status;
 
 
 /**
@@ -18,5 +20,34 @@ namespace Wikibase;
  */
 
 class StorageException extends \MWException {
+
+	/**
+	 * @var Status
+	 */
+	private $status;
+
+	/**
+	 * @param string|Status $status
+	 * @param int $code
+	 * @param Exception $previous
+	 */
+	public function __construct( $status = "", $code = 0, Exception $previous = null ) {
+
+		if ( $status instanceof Status ) {
+			$message = $status->getWikiText();
+			$this->status = $status;
+		} else {
+			$message = $status;
+		}
+
+		parent::__construct( $message, $code, $previous );
+	}
+
+	/**
+	 * @return \Status
+	 */
+	public function getStatus() {
+		return $this->status;
+	}
 
 }
