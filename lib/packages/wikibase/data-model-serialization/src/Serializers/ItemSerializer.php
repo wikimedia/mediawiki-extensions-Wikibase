8ee02a94b674e64da8978153e69ba39989a68fd0
Tpt <thomaspt@hotmail.fr>
2014-02-18 09:53:56 +0100

    Adds ItemSerializer

---
+++ lib/packages/wikibase/data-model-serialization/src/Serializers/ItemSerializer.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Wikibase\DataModel\Serializers;
+
+use Serializers\Serializer;
+use Wikibase\DataModel\Entity\Entity;
+use Wikibase\DataModel\Entity\Item;
+
+/**
+ * @since 0.1
+ *
+ * @licence GNU GPL v2+
+ * @author Thomas Pellissier Tanon
+ */
+class ItemSerializer extends EntitySerializer {
+
+	/**
+	 * @var Serializer
+	 */
+	private $siteLinkSerializer;
+
+	/**
+	 * @param Serializer $claimsSerializer
+	 * @param Serializer $siteLinkSerializer
+	 */
+	public function __construct( Serializer $claimsSerializer, Serializer $siteLinkSerializer ) {
+		parent::__construct( $claimsSerializer );
+
+		$this->siteLinkSerializer = $siteLinkSerializer;
+	}
+
+	/**
+	 * @see Serializer::isSerializerFor
+	 *
+	 * @param mixed $object
+	 *
+	 * @return boolean
+	 */
+	public function isSerializerFor( $object ) {
+		return is_object( $object ) && $object instanceof Item;
+	}
+
+	protected function getSpecificSerialization( Entity $entity ) {
+		$serialization = array();
+
+		$this->addSiteLinksToSerialization( $entity, $serialization );
+
+		return $serialization;
+	}
+
+	private function addSiteLinksToSerialization( Item $item, array &$serialization ) {
+		$siteLinks = $item->getSiteLinks();
+
+		if ( count( $siteLinks ) === 0 ) {
+			return;
+		}
+
+		foreach( $siteLinks as $siteLink ) {
+			$serialization['sitelinks'][$siteLink->getSiteId()] = $this->siteLinkSerializer->serialize( $siteLink );
+		}
+	}
+}
