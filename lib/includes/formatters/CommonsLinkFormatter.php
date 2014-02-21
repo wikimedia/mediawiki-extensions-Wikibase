24984f6f2e13c5e006c4f9ec1c57c0a194845a6a
Adrian Lang <adrian.lang@wikimedia.de>
2014-02-21 16:14:55 +0100

    Correctly format commons links with spaces
    Spaces should be replaced with underscores in URLs, underscores should be
    replaced with spaces for link texts, plus signs should stay what they are.
    
    Using Title here is probably not completely sane since it takes the local
    configuration into account. It works, though.
    
    Bug: 61738
    Bug: 45046
    Change-Id: I4cac81188e5a09bd1a4cbf5ccdf3c579fce9821a

--- lib/includes/formatters/CommonsLinkFormatter.php
+++ lib/includes/formatters/CommonsLinkFormatter.php
@@ -5,6 +5,7 @@ namespace Wikibase\Lib;
 use DataValues\StringValue;
 use Html;
 use InvalidArgumentException;
+use Title;
 use ValueFormatters\FormatterOptions;
 use ValueFormatters\ValueFormatter;
 
@@ -49,9 +50,14 @@ class CommonsLinkFormatter implements ValueFormatter {
 		}
 
 		$fileName = $value->getValue();
+		// We are using NS_MAIN only because makeTitleSafe requires a valid namespace
+		// We cannot use makeTitle because it does not secureAndSplit()
+		$title = Title::makeTitleSafe( NS_MAIN, $fileName );
 
-		$attributes = array_merge( $this->attributes, array( 'href' => '//commons.wikimedia.org/wiki/' . wfUrlencode( 'File:' . $fileName ) ) );
-		$html = Html::element( 'a', $attributes, $fileName );
+		$attributes = array_merge( $this->attributes, array(
+			'href' => '//commons.wikimedia.org/wiki/' . 'File:' . $title->getPartialURL()
+		) );
+		$html = Html::element( 'a', $attributes, $title->getText() );
 
 		return $html;
 	}
