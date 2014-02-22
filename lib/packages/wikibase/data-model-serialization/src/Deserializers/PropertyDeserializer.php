19bae9177a6e5a90c9aec6401cedd9cf34e69a78
Tpt <thomaspt@hotmail.fr>
2014-02-22 07:55:17 +0100

    Adds PropertyDeserializer

---
+++ lib/packages/wikibase/data-model-serialization/src/Deserializers/PropertyDeserializer.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Wikibase\DataModel\Deserializers;
+
+use Deserializers\Deserializer;
+use Deserializers\Exceptions\DeserializationException;
+use Wikibase\DataModel\Entity\Property;
+
+/**
+ * @since 0.1
+ *
+ * @licence GNU GPL v2+
+ * @author Thomas Pellissier Tanon
+ */
+class PropertyDeserializer extends EntityDeserializer {
+
+	/**
+	 * @param Deserializer $entityIdDeserializer
+	 * @param Deserializer $claimsDeserializer
+	 */
+	public function __construct( Deserializer $entityIdDeserializer, Deserializer $claimsDeserializer ) {
+		parent::__construct( 'property', $entityIdDeserializer, $claimsDeserializer );
+	}
+
+	protected function getPartiallyDeserialized( array $serialization ) {
+		$property = Property::newEmpty();
+
+		$this->setDataTypeFromSerialization( $serialization, $property );
+
+		return $property;
+	}
+
+	private function setDataTypeFromSerialization( array $serialization, Property $property ) {
+		$this->requireAttribute( $serialization, 'datatype' );
+		$this->assertAttributeInternalType( $serialization, 'datatype', 'string' );
+
+		$property->setDataTypeId( $serialization['datatype'] );
+	}
+}
