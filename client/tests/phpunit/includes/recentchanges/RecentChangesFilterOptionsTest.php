810906558e9e2d66884cdf63ce94b380706de0df
Marius Hoch <hoo@online.de>
2014-02-23 12:36:00 +0100

    Use WikibaseClient in RecentChangesFilterOptions and add tests
    Follow-up to: I7fd180
    Change-Id: I7fd18030b281d0b0db536146d56259392205f9bb

---
+++ client/tests/phpunit/includes/recentchanges/RecentChangesFilterOptionsTest.php
@@ -0,0 +1,57 @@
+<?php
+namespace Wikibase\Test;
+
+use Wikibase\RecentChangesFilterOptions;
+use Wikibase\Client\WikibaseClient;
+use FormOptions;
+
+/**
+ * @covers Wikibase\RecentChangesFilterOptions
+ *
+ * @since 0.5
+ *
+ * @group WikibaseClient
+ * @group Test
+ * @group Wikibase
+ *
+ * @licence GNU GPL v2+
+ * @author Marius Hoch < hoo@online.de >
+ */
+class RecentChangesFilterOptionsTest extends \MediaWikiTestCase {
+	/**
+	 * @dataProvider provideShowWikibaseEdits
+	 *
+	 * @param bool $expected
+	 * @param bool $showExternalRecentChanges
+	 * @param bool $hideWikibase
+	 */
+	public function testShowWikibaseEdits(
+		$expected,
+		$showExternalRecentChanges,
+		$hideWikibase
+	) {
+		$settings = WikibaseClient::getDefaultInstance()->getSettings();
+		$oldShowExternal = $settings->getSetting( 'showExternalRecentChanges' );
+
+		$settings->setSetting( 'showExternalRecentChanges', $showExternalRecentChanges );
+		$opts = new FormOptions();
+		$opts->add( 'hidewikidata', $hideWikibase );
+
+		$recentChangesFilterOptions = new RecentChangesFilterOptions( $opts );
+		$this->assertSame( $expected, $recentChangesFilterOptions->showWikibaseEdits() );
+
+		$settings->setSetting( 'showExternalRecentChanges', $oldShowExternal );
+	}
+
+	public function provideShowWikibaseEdits() {
+		return array(
+			array( true, true, false ),
+			// hidewikidata set
+			array( false, true, true ),
+			// showExternalRecentChanges is false
+			array( false, false, false ),
+			// hidewikidata set and showExternalRecentChanges is false
+			array( false, false, true ),
+		);
+	}
+}
