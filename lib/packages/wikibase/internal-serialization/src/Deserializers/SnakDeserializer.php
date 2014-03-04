e8c0d8803419be29ae408093a4a7a9aed0e6650d
jeroendedauw <jeroendedauw@gmail.com>
2014-03-04 09:02:20 +0100

    Implemented SnakDeserializer->isDeserializerFor

--- lib/packages/wikibase/internal-serialization/src/Deserializers/SnakDeserializer.php
+++ lib/packages/wikibase/internal-serialization/src/Deserializers/SnakDeserializer.php
@@ -4,6 +4,7 @@ namespace Wikibase\InternalSerialization\Deserializers;
 
 use Deserializers\Deserializer;
 use Deserializers\Exceptions\DeserializationException;
+use Exception;
 use Wikibase\DataModel\Snak\Snak;
 
 /**
@@ -30,7 +31,50 @@ class SnakDeserializer implements Deserializer {
 	 * @return boolean
 	 */
 	public function isDeserializerFor( $serialization ) {
-		return false;
+		try {
+			$this->assertStructureIsValid( $serialization );
+			return true;
+		}
+		catch ( Exception $e ) {
+			return false;
+		}
+	}
+
+	private function assertStructureIsValid( $serialization ) {
+		if ( !is_array( $serialization ) || $serialization === array() ) {
+			throw new DeserializationException( 'Serialization should be a non-empty array' );
+		}
+
+		if ( $serialization[0] === 'value' ) {
+			$this->assertIsValueSnak( $serialization );
+		}
+		else {
+			$this->assertIsNonValueSnak( $serialization );
+		}
+
+		$this->assertIsPropertyId( $serialization[1] );
+	}
+
+	private function assertIsValueSnak( array $serialization ) {
+		if ( count( $serialization ) != 4 ) {
+			throw new DeserializationException( 'Value snaks need to have 4 elements' );
+		}
+	}
+
+	private function assertIsNonValueSnak( array $serialization ) {
+		if ( count( $serialization ) != 2 ) {
+			throw new DeserializationException( 'Non-value snaks need to have 2 elements' );
+		}
+
+		if ( !in_array( $serialization[0], array( 'novalue', 'somevalue' ) ) ) {
+			throw new DeserializationException( 'Unknown snak type' );
+		}
+	}
+
+	private function assertIsPropertyId( $idSerialization ) {
+		if ( !is_int( $idSerialization ) || $idSerialization < 1 ) {
+			throw new DeserializationException( 'Property id needs to be an int bigger than 0' );
+		}
 	}
 
 }
--- lib/packages/wikibase/internal-serialization/tests/unit/Deserializers/SnakDeserializerTest.php
+++ lib/packages/wikibase/internal-serialization/tests/unit/Deserializers/SnakDeserializerTest.php
@@ -22,12 +22,51 @@ class SnakDeserializerTest extends \PHPUnit_Framework_TestCase {
 		$this->deserializer = new SnakDeserializer();
 	}
 
-	public function testGivenInvalidSerialization_isDeserializerForReturnsFalse() {
-		$this->assertCannotDeserialize( null );
+	public function invalidSerializationProvider() {
+		return array(
+			array( null ),
+			array( array() ),
+			array( array( 'novalue' ) ),
+			array( array( 1337, 'novalue' ) ),
+			array( array( 'spam', 1337 ) ),
+			array( array( 'novalue', 'daah' ) ),
+			array( array( 'novalue', 0 ) ),
+			array( array( 'novalue', -1337 ) ),
+			array( array( 'novalue', 1337, 'spam' ) ),
+			array( array( 'value', 1337 ) ),
+			array( array( 'value', 1337, 'data-value-type' ) ),
+			array( array( 'value', 1337, 'data-value-type', 'data-value-value', 'spam' ) ),
+		);
 	}
 
-	private function assertCannotDeserialize( $serialization ) {
+	/**
+	 * @dataProvider invalidSerializationProvider
+	 */
+	public function testGivenInvalidSerialization_isDeserializerForReturnsFalse( $serialization ) {
 		$this->assertFalse( $this->deserializer->isDeserializerFor( $serialization ) );
+	}
+
+	public function testGivenValidSerialization_isDeserializerForReturnsTrue() {
+		$this->assertCanDeserialize( array(
+			'novalue',
+			1337,
+		) );
+
+		$this->assertCanDeserialize( array(
+			'somevalue',
+			1,
+		) );
+
+		$this->assertCanDeserialize( array(
+			'value',
+			42,
+			'data-value-type',
+			'data-value-value'
+		) );
+	}
+
+	private function assertCanDeserialize( $serialization ) {
+		$this->assertTrue( $this->deserializer->isDeserializerFor( $serialization ) );
 	}
 
 }
