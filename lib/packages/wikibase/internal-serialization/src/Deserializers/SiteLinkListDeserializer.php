b23dccb117833746e145d82130ceb79a8d6a04e5
jeroendedauw <jeroendedauw@gmail.com>
2014-03-05 13:06:11 +0100

    Implement most of the structural verification in SiteLinkListDeserializer

--- lib/packages/wikibase/internal-serialization/src/Deserializers/SiteLinkListDeserializer.php
+++ lib/packages/wikibase/internal-serialization/src/Deserializers/SiteLinkListDeserializer.php
@@ -4,8 +4,10 @@ namespace Wikibase\InternalSerialization\Deserializers;
 
 use Deserializers\Deserializer;
 use Deserializers\Exceptions\DeserializationException;
+use Deserializers\Exceptions\MissingAttributeException;
 use Exception;
 use Wikibase\DataModel\SiteLink;
+use Wikibase\DataModel\SiteLinkList;
 
 /**
  * @licence GNU GPL v2+
@@ -21,6 +23,8 @@ class SiteLinkListDeserializer implements Deserializer {
 	 */
 	public function deserialize( $serialization ) {
 		$this->assertStructureIsValid( $serialization );
+
+		return new SiteLinkList( array() );
 	}
 
 	/**
@@ -43,6 +47,37 @@ class SiteLinkListDeserializer implements Deserializer {
 	private function assertStructureIsValid( $serialization ) {
 		if ( !is_array( $serialization ) ) {
 			throw new DeserializationException( 'SiteLink list serializations should be arrays' );
+		}
+
+		foreach ( $serialization as $key => $arrayElement ) {
+			$this->assertKeyIsValid( $key );
+			$this->assertElementIsValid( $arrayElement );
+		}
+	}
+
+	private function assertKeyIsValid( $key ) {
+		if ( !is_string( $key ) ) {
+			throw new DeserializationException( 'All array keys should be strings' );
+		}
+	}
+
+	private function assertElementIsValid( $arrayElement ) {
+		if ( !is_string( $arrayElement ) && !is_array( $arrayElement ) ) {
+			throw new DeserializationException( 'All array elements should be of type string or array' );
+		}
+
+		if ( is_array( $arrayElement ) ) {
+			$this->assertElementIsValidArray( $arrayElement );
+		}
+	}
+
+	private function assertElementIsValidArray( array $arrayElement ) {
+		if ( !array_key_exists( 'name', $arrayElement ) ) {
+			throw new MissingAttributeException( 'name' );
+		}
+
+		if ( !array_key_exists( 'badges', $arrayElement ) ) {
+			throw new MissingAttributeException( 'badges' );
 		}
 	}
 
--- lib/packages/wikibase/internal-serialization/tests/unit/Deserializers/SiteLinkListDeserializerTest.php
+++ lib/packages/wikibase/internal-serialization/tests/unit/Deserializers/SiteLinkListDeserializerTest.php
@@ -6,7 +6,7 @@ use Deserializers\Deserializer;
 use Wikibase\InternalSerialization\Deserializers\SiteLinkListDeserializer;
 
 /**
- * @covers Wikibase\InternalSerialization\Deserializers\SiteLinkDeserializer
+ * @covers Wikibase\InternalSerialization\Deserializers\SiteLinkListDeserializer
  *
  * @licence GNU GPL v2+
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
@@ -22,12 +22,6 @@ class SiteLinkListDeserializerTest extends \PHPUnit_Framework_TestCase {
 		$this->deserializer = new SiteLinkListDeserializer();
 	}
 
-	public function invalidSerializationProvider() {
-		return array(
-			array( null ),
-		);
-	}
-
 	/**
 	 * @dataProvider invalidSerializationProvider
 	 */
@@ -38,6 +32,24 @@ class SiteLinkListDeserializerTest extends \PHPUnit_Framework_TestCase {
 		);
 	}
 
+	public function invalidSerializationProvider() {
+		return array(
+			array( null ),
+			array( 42 ),
+			array( 'foo' ),
+
+			array( array( 'foo' ) ),
+			array( array( 'foo' => 42 ) ),
+			array( array( 'foo' => array() ) ),
+
+			array( array( 'foo' => array( 'bar' => 'baz' ) ) ),
+			array( array( 'foo' => array( 'name' => 'baz' ) ) ),
+			array( array( 'foo' => array( 'badges' => array() ) ) ),
+
+			// TODO: invalid badges
+		);
+	}
+
 	/**
 	 * @dataProvider invalidSerializationProvider
 	 */
@@ -45,5 +57,53 @@ class SiteLinkListDeserializerTest extends \PHPUnit_Framework_TestCase {
 		$this->setExpectedException( 'Deserializers\Exceptions\DeserializationException' );
 		$this->deserializer->deserialize( $serialization );
 	}
+
+	public function testEmptyListDeserialization() {
+		$list = $this->deserializer->deserialize( array() );
+		$this->assertInstanceOf( 'Wikibase\DataModel\SiteLinkList', $list );
+	}
+
+	/**
+	 * @dataProvider serializationProvider
+	 */
+	public function testGivenValidSerialization_isDeserializerForReturnsTrue( array $serialization ) {
+		$this->assertTrue( $this->deserializer->isDeserializerFor( $serialization ) );
+	}
+
+	public function serializationProvider() {
+		return array(
+			array( array(
+			) ),
+
+			array( array(
+				'foo' => array(
+					'name' => 'bar',
+					'badges' => array(),
+				),
+				'baz' => array(
+					'name' => 'bah',
+					'badges' => array(),
+				)
+			) ),
+
+			array( array(
+				'foo' => array(
+					'name' => 'bar',
+					'badges' => array( 'Q42', 'Q1337' ),
+				)
+			) ),
+
+			array( array(
+				'foo' => 'bar',
+			) ),
+
+			array( array(
+				'foo' => 'bar',
+				'baz' => 'bah',
+			) ),
+		);
+	}
+
+	// TODO: test deserialize
 
 }
