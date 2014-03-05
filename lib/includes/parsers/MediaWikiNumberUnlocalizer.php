8c5e07d7816555c899387bebc909d9497b4d20ab
daniel <daniel.kinzler@wikimedia.de>
2014-03-05 17:44:58 +0100

    (bug 61911) use localized number patterns.
    When splitting quantitiy strings, use regex patterns derived from
    MediaWiki language objects.
    
    IMPORTANT: needs https://github.com/DataValues/Number/pull/2 (that is,
    it needs the yet-to-be-released version 0.3 of data-values/numbers).
    
    Change-Id: If2345c45b5da77ca83437aa925e4b36631fafd15

--- lib/includes/parsers/MediaWikiNumberUnlocalizer.php
+++ lib/includes/parsers/MediaWikiNumberUnlocalizer.php
@@ -2,8 +2,7 @@
 
 namespace Wikibase\Lib;
 use Language;
-use ValueParsers\ParserOptions;
-use ValueParsers\Unlocalizer;
+use ValueParsers\BasicUnlocalizer;
 
 /**
  * MediaWikiNumberUnlocalizer
@@ -13,21 +12,68 @@ use ValueParsers\Unlocalizer;
  * @license GPL 2+
  * @author Daniel Kinzler
  */
-class MediaWikiNumberUnlocalizer implements Unlocalizer {
+class MediaWikiNumberUnlocalizer extends BasicUnlocalizer {
+
+	/**
+	 * @var Language
+	 */
+	protected $language;
+
+	/**
+	 * @param Language $language
+	 */
+	public function __construct( Language $language ) {
+		$this->language = $language;
+	}
 
 	/**
 	 * @see Unlocalizer::unlocalize()
 	 *
 	 * @param string $number string to process
-	 * @param string $language language code
-	 * @param ParserOptions $options
 	 *
 	 * @return string unlocalized string
 	 */
-	public function unlocalize( $number, $language, ParserOptions $options ) {
-		$language = Language::factory( $language );
-
-		$canonicalizedNumber = $language->parseFormattedNumber( $number );
+	public function unlocalizeNumber( $number ) {
+		$canonicalizedNumber = $this->language->parseFormattedNumber( $number );
 		return $canonicalizedNumber;
 	}
+
+	/**
+	 * @see Unlocalizer::getNumberRegex()
+	 *
+	 * Constructs a regular expression based on Language::digitTransformTable()
+	 * and Language::separatorTransformTable().
+	 *
+	 * @param string $delim the regex delimiter, used for escaping.
+	 *
+	 * @return string regular expression
+	 */
+	public function getNumberRegex( $delim = '/' ) {
+		$digitMap = $this->language->digitTransformTable();
+		$separatorMap = $this->language->separatorTransformTable();
+
+		if ( empty( $digitMap ) ) {
+			$numerals = '0123456789';
+		} else {
+			$numerals = implode( '', array_keys( $digitMap ) ) // accept canonical numerals
+				. implode( '', array_values( $digitMap ) ); // ...and localized numerals
+		}
+
+		if ( empty( $separatorMap ) ) {
+			$separators = '.,';
+		} else {
+			$separators = implode( '', array_keys( $separatorMap ) ) // accept canonical separators
+				. implode( '', array_values( $separatorMap ) ); // ...and localized separators
+		}
+
+		$characters = $numerals . $separators;
+
+		// if any whitespace characters are acceptable, also accept a regular blank.
+		if ( preg_match( '/\s/u', $characters ) ) {
+			$characters = $characters . ' ';
+		}
+
+		return '[-+]?[' . preg_quote( $characters, $delim ) . ']+';
+	}
+
 }
