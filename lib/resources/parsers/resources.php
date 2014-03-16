c2a482e3b6184480bcd2f8a196b6d5e23954fc31
Henning Snater <henning.snater@wikimedia.de>
2014-03-16 12:03:15 +0100

    Considering removed mw.ext.* files sub-components
    Change-Id: Ifc781f83c5f57f1f206acb7a28962e7ba2210b62

---
+++ lib/resources/parsers/resources.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * @licence GNU GPL v2+
+ * @author H. Snater < mediawiki@snater.com >
+ *
+ * @codeCoverageIgnoreStart
+ */
+return call_user_func( function() {
+
+	$remoteExtPathParts = explode(
+		DIRECTORY_SEPARATOR . 'extensions' . DIRECTORY_SEPARATOR, __DIR__, 2
+	);
+	$moduleTemplate = array(
+		'localBasePath' => __DIR__,
+		'remoteExtPath' => $remoteExtPathParts[1],
+	);
+
+	return array(
+
+		'wikibase.parsers.api' => $moduleTemplate + array(
+			'scripts' => array(
+				'wikibase.parsers.api.js',
+			),
+			'dependencies' => array(
+				'wikibase',
+			),
+		),
+
+		'wikibase.ApiBasedValueParser' => $moduleTemplate + array(
+			'scripts' => array(
+				'ApiBasedValueParser.js',
+			),
+			'dependencies' => array(
+				'util.inherit',
+				'valueParsers.ValueParser',
+				'wikibase.parsers.api',
+			),
+		),
+
+		'wikibase.EntityIdParser' => $moduleTemplate + array(
+			'scripts' => array(
+				'EntityIdParser.js',
+			),
+			'dependencies' => array(
+				'util.inherit',
+				'valueParsers.ValueParser',
+				'wikibase',
+				'wikibase.datamodel',
+			),
+		),
+
+		'wikibase.GlobeCoordinateParser' => $moduleTemplate + array(
+			'scripts' => array(
+				'GlobeCoordinateParser.js',
+			),
+			'dependencies' => array(
+				'util.inherit',
+				'wikibase.ApiBasedValueParser',
+			),
+		),
+
+		'wikibase.QuantityParser' => $moduleTemplate + array(
+			'scripts' => array(
+				'QuantityParser.js',
+			),
+			'dependencies' => array(
+				'util.inherit',
+				'wikibase.ApiBasedValueParser',
+			),
+		),
+
+		'wikibase.TimeParser' => $moduleTemplate + array(
+			'scripts' => array(
+				'TimeParser.js',
+			),
+			'dependencies' => array(
+				'util.inherit',
+				'wikibase.ApiBasedValueParser',
+			),
+		),
+
+		'wikibase.parsers' => $moduleTemplate + array(
+			'scripts' => array(
+				'parsers/wikibase.parsers.register.js',
+			),
+			'dependencies' => array(
+				'dataValues.values',
+				'valueParsers.parsers',
+				'valueParsers.ValueParserFactory',
+				'wikibase.datamodel',
+				'wikibase.EntityIdParser',
+				'wikibase.GlobeCoordinateParser',
+				'wikibase.QuantityParser',
+				'wikibase.TimeParser',
+			),
+		),
+
+	);
+
+} );
