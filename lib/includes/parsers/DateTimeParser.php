c9c3716829be345b1f9c1c4b2748705782e9f209
addshore <addshorewiki@gmail.com>
2014-03-17 12:49:43 +0000

    Support 1 2 and 3 digit years in DateTimeParser
    Change-Id: I304a0b8a254dc98703a9c9edab8f18c6a729d898

--- lib/includes/parsers/DateTimeParser.php
+++ lib/includes/parsers/DateTimeParser.php
@@ -45,16 +45,15 @@ class DateTimeParser extends StringValueParser {
 		$calendarModelParser = new CalendarModelParser();
 		$options = $this->getOptions();
 		try{
-			$value = $this->monthUnlocaliser->unlocalize(
-				$value,
-				$options->getOption( ValueParser::OPT_LANG ),
-				new ParserOptions()
+			$value = $this->getValueWithFixedYearLengths(
+				$this->getValueWithFixedSeperators(
+					$this->monthUnlocaliser->unlocalize(
+						trim( $value ),
+						$options->getOption( ValueParser::OPT_LANG ),
+						new ParserOptions()
+					)
+				)
 			);
-
-			//PHP's DateTime object does not accept spaces as separators between year, month and day,
-			//e.g. dates like 20 12 2012, but we want to support them.
-			//See http://de1.php.net/manual/en/datetime.formats.date.php
-			$value = preg_replace( '/\s+/', '.', trim( $value ) );
 
 			//Parse using the DateTime object (this will allow us to format the date in a nicer way)
 			//TODO try to match and remove BCE etc. before putting the value into the DateTime object to get - dates!
@@ -69,6 +68,48 @@ class DateTimeParser extends StringValueParser {
 		catch( Exception $exception ) {
 			throw new ParseException( $exception->getMessage() );
 		}
+	}
+
+	/**
+	 * PHP's DateTime object does not accept spaces as separators between year, month and day,
+	 * e.g. dates like 20 12 2012, but we want to support them.
+	 * See http://de1.php.net/manual/en/datetime.formats.date.php
+	 *
+	 * @param string $value
+	 *
+	 * @return mixed
+	 */
+	private function getValueWithFixedSeperators( $value ) {
+		return preg_replace( '/\s+/', '.', $value );
+	}
+
+	/**
+	 * PHP's DateTime object also cant handel smaller than 4 digit years
+	 * e.g. instead of 12 it needs 0012 etc.
+	 *
+	 * @param string $value
+	 *
+	 * @return string
+	 */
+	private function getValueWithFixedYearLengths( $value ) {
+		if( preg_match( '/^(\d+)[^\d]?(\d+)[^\d]?(\d+)$/', $value, $dateParts ) ) {
+			if( $dateParts[1] > 31 && $dateParts[3] <= 31 ) {
+				// the year looks like it is at the front
+				if( preg_match( '/^(\d{1,3})([^\d].*)$/', $value, $matches ) ) {
+					$value = str_pad( $matches[1], 4, '0', STR_PAD_LEFT ) . $matches[2];
+				}
+			} else {
+				// presume the year is at the back
+				if( preg_match( '/^(.*[^\d])(\d{1,3})$/', $value, $matches ) ) {
+					$value = $matches[1] . str_pad( $matches[2], 4, '0', STR_PAD_LEFT );
+				}
+			}
+		} else {
+			if( preg_match( '/^(.*[^\d])(\d{1,3})$/', $value, $matches ) ) {
+				$value = $matches[1] . str_pad( $matches[2], 4, '0', STR_PAD_LEFT );
+			}
+		}
+		return $value;
 	}
 
 }
