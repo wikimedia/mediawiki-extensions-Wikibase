14d81e4324897c32b7f2ff50269a32280a65b775
jeroendedauw <jeroendedauw@gmail.com>
2014-03-18 21:15:04 +0100

    Add SerializerFactory which just delegates to the DataModel Serialization component

---
+++ lib/packages/wikibase/internal-serialization/src/SerializerFactory.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Wikibase\InternalSerialization;
+
+/**
+ * @since 1.0
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class SerializerFactory extends \Wikibase\DataModel\SerializerFactory {}
---
+++ lib/packages/wikibase/internal-serialization/tests/integration/SerializerFactoryTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Integration\Wikibase\InternalSerialization;
+
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\InternalSerialization\SerializerFactory;
+
+/**
+ * @covers Wikibase\InternalSerialization\SerializerFactory
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class SerializerFactoryTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * @var SerializerFactory
+	 */
+	private $factory;
+
+	public function setUp() {
+		$this->factory = new SerializerFactory( $this->getMock( 'Serializers\Serializer' ) );
+	}
+
+	public function testEntitySerializerConstruction() {
+		$this->factory->newEntitySerializer()->serialize( Item::newEmpty() );
+
+		$this->assertTrue(
+			true,
+			'The serializer returned by newEntitySerializer can serialize an Item'
+		);
+	}
+
+}
