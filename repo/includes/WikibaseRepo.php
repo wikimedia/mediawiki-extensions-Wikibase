df03e05e15105beae17539fc4dddae0545faf6d1
aude <aude.wiki@gmail.com>
2014-03-19 17:51:27 +0100

    Add / allow injecting experimental mode param in LanguageFallbackChainFactory
    also adds simple test. more/better tests could be done as follow-up.
    
    and fixes codesniffer issue "Must use "self::" for local static member reference"
    
    https://scrutinizer-ci.com/g/wikimedia/mediawiki-extensions-Wikibase/inspections/4cc150eb-7315-4b95-b2cf-f2687f6ac816/issues/
    
    Change-Id: Id20d4512254249aaccae54f0de6c76ab5139e708

--- repo/includes/WikibaseRepo.php
+++ repo/includes/WikibaseRepo.php
@@ -319,9 +319,11 @@ class WikibaseRepo {
 	public function getLanguageFallbackChainFactory() {
 		if ( $this->languageFallbackChainFactory === null ) {
 			global $wgUseSquid;
+
 			// The argument is about whether full page output (OutputPage, specifically JS vars in it currently)
 			// is cached for anons, where the only caching mechanism in use now is Squid.
 			$this->languageFallbackChainFactory = new LanguageFallbackChainFactory(
+				defined( 'WB_EXPERIMENTAL_FEATURES' ) && WB_EXPERIMENTAL_FEATURES,
 				/* $anonymousPageViewCached = */ $wgUseSquid
 			);
 		}
