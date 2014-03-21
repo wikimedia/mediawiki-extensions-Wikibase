56dd08314ae0f33ddb547ec52fe5359307c822d2
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2014-03-21 12:30:06 +0100

    [WIP] Partial support for thousands separators in DateTimeParsers
    * Added support for thousands separators, but only for negative
      years. Reason: "1.10.100" is most probably a "day.month.year"
      after Christ but "-1.10.100" is more probably the year "110100"
      before Christ with decimal grouping (Indian decimal grouping in
      my example).
    * Added support for "B.C." with dot at the end.
    * Optimized regular expressions.
    * Replaced '+' and '-' with constants.
    
    Change-Id: I04cc07674b7f675652a9ed94e17dbfe2acb710a7

--- lib/tests/phpunit/parsers/TimeParserTest.php
+++ lib/tests/phpunit/parsers/TimeParserTest.php
@@ -58,6 +58,8 @@ class TimeParserTest extends StringValueParserTest {
 				array( '-0000000000000001-00-00T00:00:00Z', 0 , 0 , 0 , TimeValue::PRECISION_YEAR , TimeFormatter::CALENDAR_GREGORIAN ),
 			'1CE' =>
 				array( '+0000000000000001-00-00T00:00:00Z', 0 , 0 , 0 , TimeValue::PRECISION_YEAR , TimeFormatter::CALENDAR_GREGORIAN ),
+			'1,000,000 BC' =>
+				array( '-0000000001000000-00-00T00:00:00Z', 0 , 0 , 0 , TimeValue::PRECISION_Ma , TimeFormatter::CALENDAR_GREGORIAN ),
 
 			//Wikibase\Lib\YearMonthTimeParser
 			'1 1999' =>
