3ea8535a54f8a86667f785b67448648ad890f585
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2014-03-21 12:30:06 +0100

    [WIP] Optimize DateTimeParser regular expressions
    Change-Id: I04cc07674b7f675652a9ed94e17dbfe2acb710a7

--- lib/includes/parsers/DateTimeParser.php
+++ lib/includes/parsers/DateTimeParser.php
@@ -66,9 +66,9 @@ class DateTimeParser extends StringValueParser {
 				)
 			);
 
-			//PHP's DateTime object also cant handel larger than 4 digit years
+			//PHP's DateTime object also can't handle larger than 4 digit years
 			//e.g. 1 June 202020
-			if( preg_match( '/^(.*[^\d]|)(\d{5,})(.*|)$/', $value, $matches ) ) {
+			if( preg_match( '/^(.*\D)?(\d{5,})(.*)$/', $value, $matches ) ) {
 				$value = $matches[1] . substr( $matches[2], -4 ) . $matches[3];
 				$largeYear = $matches[2];
 			}
@@ -107,7 +107,7 @@ class DateTimeParser extends StringValueParser {
 	}
 
 	/**
-	 * PHP's DateTime object also cant handel smaller than 4 digit years
+	 * PHP's DateTime object also can't handle smaller than 4 digit years
 	 * e.g. instead of 12 it needs 0012 etc.
 	 *
 	 * @param string $value
@@ -115,7 +115,7 @@ class DateTimeParser extends StringValueParser {
 	 * @return string
 	 */
 	private function getValueWithFixedYearLengths( $value ) {
-		if( preg_match( '/^(\d+)([^\d])(\d+)([^\d])(\d+)$/', $value, $dateParts ) ) {
+		if( preg_match( '/^(\d+)(\D)(\d+)(\D)(\d+)$/', $value, $dateParts ) ) {
 			if( $dateParts[1] > 31 && $dateParts[5] <= 31 ) {
 				// the year looks like it is at the front
 				if( strlen( $dateParts[1] ) < 4 ) {
@@ -130,7 +130,7 @@ class DateTimeParser extends StringValueParser {
 				}
 			}
 		} else {
-			if( preg_match( '/^(.*[^\d])(\d{1,3})$/', $value, $matches ) ) {
+			if( preg_match( '/^(.*\D)(\d{1,3})$/', $value, $matches ) ) {
 				$value = $matches[1] . str_pad( $matches[2], 4, '0', STR_PAD_LEFT );
 			}
 		}
