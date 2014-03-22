f89c8a11fcc45f5367da5477bb85910cf7e2b096
addshore <addshorewiki@gmail.com>
2014-03-22 18:07:48 +0100

    Fix ApiSandbox continue param default
    This is how the continue param is defined for things
    in core. This stops the ApiSandbox extension from
    giving continue a default of 10.
    
    Bug: 62959
    Change-Id: I8eca2a90a9628ee835156fb767fda11019d42af8

--- repo/includes/api/SearchEntities.php
+++ repo/includes/api/SearchEntities.php
@@ -323,14 +323,7 @@ class SearchEntities extends ApiBase {
 				ApiBase::PARAM_MIN => 0,
 				ApiBase::PARAM_RANGE_ENFORCE => true,
 			),
-			'continue' => array(
-				ApiBase::PARAM_TYPE => 'limit',
-				ApiBase::PARAM_DFLT => 0,
-				ApiBase::PARAM_MAX => ApiBase::LIMIT_SML1,
-				ApiBase::PARAM_MAX2 => ApiBase::LIMIT_SML2,
-				ApiBase::PARAM_MIN => 0,
-				ApiBase::PARAM_RANGE_ENFORCE => true,
-			),
+			'continue' => null,
 		);
 	}
 
