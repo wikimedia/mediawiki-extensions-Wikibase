43aea5961ac5e2f0d389872dc7ad9685ed6516d1
aude <aude.wiki@gmail.com>
2014-03-24 17:20:15 +0100

    Add check for EditFilterMergedContent handler registered in EditEntity
    Change-Id: Ib9cda3ed1adae87f05e1c0ae8a5ba5ea1161db5e

--- repo/includes/EditEntity.php
+++ repo/includes/EditEntity.php
@@ -2,6 +2,7 @@
 
 namespace Wikibase;
 
+use Hooks;
 use IContextSource;
 use Status;
 use User;
@@ -741,14 +742,16 @@ class EditEntity {
 	}
 
 	/**
-	 * Call EditFilterMergedContent hook
+	 * Call EditFilterMergedContent hook, if registered.
 	 *
 	 * @param string $summary
 	 *
 	 * @todo: move the implementation elsewhere, it depends on WikiPage.
 	 */
 	protected function runEditFilterHooks( $summary ) {
-		//TODO: short out if no handler is registered for EditFilterMergedContent?
+		if ( !Hooks::isRegistered( 'EditFilterMergedContent' ) ) {
+			return;
+		}
 
 		if ( !$this->isNew() ) {
 			$context = clone $this->context;
