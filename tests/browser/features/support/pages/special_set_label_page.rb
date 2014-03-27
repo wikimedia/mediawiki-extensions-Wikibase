6ef0b692dca6bc7029206bd9589a994e28845f0e
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2014-03-27 16:40:26 +0100

    [WIP] Add Cucumber test for Special:SetLabel
    Change-Id: I544e453372ab8441f2df6226665e762677e7d1e8

---
+++ tests/browser/features/support/pages/special_set_label_page.rb
@@ -0,0 +1,20 @@
+# -*- encoding : utf-8 -*-
+# Wikidata UI tests
+#
+# Author:: Thiemo Mättig (thiemo.maettig@wikimedia.de)
+# License:: GNU GPL v2+
+#
+# page object for set label special page
+
+class SpecialSetLabelPage
+  include PageObject
+
+  page_url URL.repo_url("Special:SetLabel")
+
+  p(:anonymous_edit_warning, :class => 'warning')
+  text_field(:id_input_field, :id => 'wb-modifyentity-id')
+  text_field(:language_input_field, :id => 'wb-modifyterm-language')
+  text_field(:label_input_field, :id => 'wb-modifyterm-value')
+  button(:set_label_button, :id => 'wb-setlabel-submit')
+
+end
