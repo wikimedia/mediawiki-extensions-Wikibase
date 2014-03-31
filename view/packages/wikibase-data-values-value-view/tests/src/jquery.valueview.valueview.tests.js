567c3213af0a08cb313ecac7c048abcb6ff2f622
Adrian Lang <adrian.lang@wikimedia.de>
2014-03-31 12:20:52 +0200

    Add CompletenessTest to ValueView test, add ValueView tests

--- view/packages/wikibase-data-values-value-view/tests/src/jquery.valueview.valueview.tests.js
+++ view/packages/wikibase-data-values-value-view/tests/src/jquery.valueview.valueview.tests.js
@@ -3,29 +3,41 @@
  * @author Adrian Lang < adrian.lang@wikimedia.de >
  */
 
-( function( vv, vf, vp, QUnit ) {
+( function( $, vv, dv, vf, vp, sinon, QUnit, CompletenessTest ) {
 	'use strict';
 
 	var $vvElem;
 	var vvInst;
+	var stringValue = new dv.StringValue( 'STRING VALUE' );
 
 	QUnit.module( 'jquery.valueview.valueview' );
 
-	function initVv( opts ) {
-		opts = opts || {};
+	if( QUnit.urlParams.completenesstest ) {
+		new CompletenessTest( vv.prototype, function( cur, tester, path ) {
+			// Don't check code coverage for options
+			return path[path.length - 1] === 'options';
+		} );
+	}
 
-		$vvElem = jQuery( '<div/>' );
-		vvInst = $vvElem.valueview( {
-			expertStore: new vv.ExpertStore(),
-			formatterStore: new vf.ValueFormatterStore( vf.NullFormatter ),
-			parserStore: new vp.ValueParserStore( vp.NullParser )
-		} ).data( 'valueview' );
+	function initVv( opts ) {
+		opts = $.extend( {}, initVv.defaultOpts, opts );
+
+		$vvElem = opts.generateDom();
+		vvInst = $vvElem.valueview( opts.vvArgs ).data( 'valueview' );
 
 		if( opts.withExpert ) {
 			vvInst.startEditing();
-			vvInst.draw();
 		}
 	}
+	initVv.defaultOpts = {
+		withExpert: false,
+		generateDom: function() { return $( '<div/>' ); },
+		vvArgs: {
+			expertStore: new vv.ExpertStore(),
+			formatterStore: new vf.ValueFormatterStore( vf.NullFormatter ),
+			parserStore: new vp.ValueParserStore( vp.NullParser )
+		}
+	};
 
 	QUnit.test( 'Constructor', function( assert ) {
 		initVv();
@@ -54,4 +66,119 @@
 		assert.ok( !vvInst.expert() );
 	} );
 
-} )( jQuery.valueview, valueFormatters, valueParsers, QUnit );
+	QUnit.test( 'startEditing', function( assert ) {
+		initVv();
+
+		vvInst.startEditing();
+
+		assert.ok( vvInst.isInEditMode() );
+	} );
+
+	QUnit.test( 'stopEditing without startEditing', function( assert ) {
+		initVv();
+		assert.ok( !vvInst.isInEditMode() );
+
+		vvInst.stopEditing();
+
+		assert.ok( !vvInst.isInEditMode() );
+		assert.ok( !vvInst.expert() );
+	} );
+
+	QUnit.test( 'stopEditing after startEditing', function( assert ) {
+		initVv();
+
+		vvInst.startEditing();
+		vvInst.value( stringValue );
+		vvInst.stopEditing();
+
+		assert.ok( !vvInst.isInEditMode() );
+		assert.ok( !vvInst.expert() );
+		assert.equal( vvInst.value(), stringValue );
+		assert.ok( !vvInst.isEmpty() );
+	} );
+
+	QUnit.test( 'cancelEditing without startEditing', function( assert ) {
+		initVv();
+		assert.ok( !vvInst.isInEditMode() );
+
+		vvInst.cancelEditing();
+
+		assert.ok( !vvInst.isInEditMode() );
+		assert.ok( !vvInst.expert() );
+	} );
+
+	QUnit.test( 'cancelEditing after startEditing', function( assert ) {
+		initVv();
+
+		vvInst.startEditing();
+		vvInst.value( stringValue );
+		vvInst.cancelEditing();
+
+		assert.ok( !vvInst.isInEditMode() );
+		assert.ok( !vvInst.expert() );
+		assert.notEqual( vvInst.value(), stringValue );
+		assert.ok( vvInst.isEmpty() );
+	} );
+
+	QUnit.test( 'getFormattedValue with DOM', function( assert ) {
+		var vvArgs = $.extend( {
+			value: stringValue
+		}, initVv.defaultOpts.vvArgs );
+		sinon.spy( vvArgs.formatterStore, 'getFormatter' );
+		sinon.spy( vvArgs.parserStore, 'getParser' );
+		initVv( {
+			generateDom: function() {
+				return jQuery( '<div/>' ).append( 'FORMATTED VALUE' );
+			},
+			vvArgs: vvArgs
+		} );
+
+		vvInst.draw();
+
+		assert.equal( vvInst.getFormattedValue(), 'FORMATTED VALUE' );
+		sinon.assert.notCalled( vvArgs.formatterStore.getFormatter );
+		sinon.assert.notCalled( vvArgs.parserStore.getParser );
+
+		vvArgs.formatterStore.getFormatter.restore();
+		vvArgs.parserStore.getParser.restore();
+	} );
+
+	QUnit.test( 'getFormattedValue without DOM', function( assert ) {
+		var vvArgs = $.extend( {
+			value: stringValue
+		}, initVv.defaultOpts.vvArgs );
+		sinon.spy( vvArgs.formatterStore, 'getFormatter' );
+		sinon.spy( vvArgs.parserStore, 'getParser' );
+		initVv( {
+			vvArgs: vvArgs
+		} );
+
+		vvInst.draw();
+
+		assert.equal( vvInst.getFormattedValue(), 'STRING VALUE' );
+		sinon.assert.calledOnce( vvArgs.formatterStore.getFormatter );
+		sinon.assert.notCalled( vvArgs.parserStore.getParser );
+
+		vvArgs.formatterStore.getFormatter.restore();
+		vvArgs.parserStore.getParser.restore();
+	} );
+
+	QUnit.test( 'disable', function( assert ) {
+		initVv();
+
+		vvInst.disable();
+
+		assert.ok( vvInst.isDisabled() );
+		assert.ok( vvInst.option( 'disabled') );
+	} );
+
+	QUnit.test( 'enable', function( assert ) {
+		initVv();
+
+		vvInst.enable();
+
+		assert.ok( !vvInst.isDisabled() );
+		assert.ok( !vvInst.option( 'disabled') );
+	} );
+
+} )( jQuery, jQuery.valueview, dataValues, valueFormatters, valueParsers, sinon, QUnit, CompletenessTest );
