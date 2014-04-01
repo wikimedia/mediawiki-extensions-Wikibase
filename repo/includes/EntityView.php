183c1fdb9ee2a4d61900b9ac60a6ef5e5d6a5b48
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2014-04-01 23:09:44 +0200

    Don't use unchecked global jQuery variable in non-critical JavaScript
    This code simply relies on jQuery being loaded, but there is no
    guarantee. I had this error in my developement environment while
    testing and found it really strange that such a purely cosmetic,
    completely optional piece of code stops all other scripts on the page.
    
    Change-Id: I4b3ea6658c5fdf73da72dd9838d3952b0098c2c7

--- repo/includes/EntityView.php
+++ repo/includes/EntityView.php
@@ -185,25 +185,27 @@ abstract class EntityView extends ContextSource {
 		// the fastest way to show the loading spinner is placing the script right after the
 		// corresponsing html
 		$html .= Html::inlineScript( '
-$( ".wb-entity" ).addClass( "loading" ).after( function() {
-	var $div = $( "<div/>" ).addClass( "wb-entity-spinner mw-small-spinner" );
-	$div.css( "top", $div.height() + "px" );
-	$div.css(
-		"' . ( $lang->isRTL() ? 'right' : 'left' ) . '",
-		( ( $( this ).width() - $div.width() ) / 2 | 0 ) + "px"
-	);
-	return $div;
-} );
+if ( $ ) {
+	$( ".wb-entity" ).addClass( "loading" ).after( function() {
+		var $div = $( "<div/>" ).addClass( "wb-entity-spinner mw-small-spinner" );
+		$div.css( "top", $div.height() + "px" );
+		$div.css(
+			"' . ( $lang->isRTL() ? 'right' : 'left' ) . '",
+			( ( $( this ).width() - $div.width() ) / 2 | 0 ) + "px"
+		);
+		return $div;
+	} );
 
-// Remove loading spinner after a couple of seconds in any case. (e.g. some resource
-// might not have been loaded silently, so JavaScript is not initialising)
-// Additionally attaching to window.error would only make sense before any other
-// JavaScript is parsed. Since the JavaScript is loaded in the header, it does not make
-// any sense to attach to window.error here.
-window.setTimeout( function() {
-	$( ".wb-entity" ).removeClass( "loading" );
-	$( ".wb-entity-spinner" ).remove();
-}, 7000 );
+	// Remove loading spinner after a couple of seconds in any case. (e.g. some resource
+	// might not have been loaded silently, so JavaScript is not initialising)
+	// Additionally attaching to window.error would only make sense before any other
+	// JavaScript is parsed. Since the JavaScript is loaded in the header, it does not make
+	// any sense to attach to window.error here.
+	window.setTimeout( function() {
+		$( ".wb-entity" ).removeClass( "loading" );
+		$( ".wb-entity-spinner" ).remove();
+	}, 7000 );
+}
 ' );
 
 		wfProfileOut( __METHOD__ );
