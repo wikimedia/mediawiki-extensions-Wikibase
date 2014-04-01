a138ee4b7f8608d7fa98a9206708ad7e5f573906
aude <aude.wiki@gmail.com>
2014-04-01 17:46:20 +0200

    Split Snak html formatting code from ClaimHtmlGenerator
    Bug: 63364
    Change-Id: Ifd89b103c74e49f299bd7ee95e29822195a44520

---
+++ repo/includes/view/SnakHtmlGenerator.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace Wikibase\View;
+
+use InvalidArgumentException;
+use Wikibase\DataModel\Snak\PropertySomeValueSnak;
+use Wikibase\DataModel\Snak\PropertyValueSnak;
+use Wikibase\DataModel\Snak\Snak;
+use Wikibase\EntityTitleLookup;
+use Wikibase\Lib\FormattingException;
+use Wikibase\Lib\PropertyNotFoundException;
+use Wikibase\Lib\SnakFormatter;
+
+/**
+ * Base class for generating Snak html.
+ *
+ * @since 0.5
+ * @licence GNU GPL v2+
+ *
+ * @author H. Snater < mediawiki@snater.com >
+ * @author Pragunbhutani
+ * @author Katie Filbert < aude.wiki@gmail.com>
+ */
+class SnakHtmlGenerator {
+
+	/**
+	 * @since 0.4
+	 *
+	 * @var SnakFormatter
+	 */
+	protected $snakFormatter;
+
+	/**
+	 * @since 0.5
+	 *
+	 * @var EntityTitleLookup
+	 */
+	protected $entityTitleLookup;
+
+	/**
+	 * @param SnakFormatter $snakFormatter
+	 * @param EntityTitleLookup $entityTitleLookup
+	 */
+	public function __construct(
+		SnakFormatter $snakFormatter,
+		EntityTitleLookup $entityTitleLookup
+	) {
+		$this->snakFormatter = $snakFormatter;
+		$this->entityTitleLookup = $entityTitleLookup;
+	}
+
+	/**
+	 * Generates the HTML for a single snak.
+	 *
+	 * @param Snak $snak
+	 * @param string[] $propertyLabels
+	 * @param boolean $showPropertyLink
+	 * @return string
+	 */
+	public function getSnakHtml( Snak $snak, array $propertyLabels, $showPropertyLink = false ) {
+		$propertyLink = '';
+
+		if( $showPropertyLink ) {
+			$propertyId = $snak->getPropertyId();
+			$propertyKey = $propertyId->getSerialization();
+			$propertyLabel = isset( $propertyLabels[$propertyKey] )
+				? $this->propertyLabels[$propertyKey]
+				: $propertyKey;
+			$propertyLink = \Linker::link(
+				$this->entityTitleLookup->getTitleForId( $propertyId ),
+				htmlspecialchars( $propertyLabel )
+			);
+		}
+
+		$snakViewVariation = $this->getSnakViewVariation( $snak );
+		$snakViewCssClass = 'wb-snakview-variation-' . $snakViewVariation;
+
+		$formattedValue = $this->getFormattedSnakValue( $snak );
+
+		if ( $formattedValue === '' ) {
+			$formattedValue = '&nbsp;';
+		}
+
+		$html = wfTemplate( 'wb-snak',
+			// Display property link only once for snaks featuring the same property:
+			$propertyLink,
+			$snakViewCssClass,
+			$formattedValue
+		);
+
+		return $html;
+	}
+
+	/**
+	 * @param Snak $snak
+	 *
+	 * @return string
+	 */
+	private function getSnakViewVariation( Snak $snak ) {
+		if ( $snak instanceof PropertyValueSnak ) {
+			$variation = 'valuesnak';
+		} elseif ( $snak instanceof PropertySomeValueSnak ) {
+			$variation = 'somevaluesnak';
+		} else {
+			$variation = 'novaluesnak';
+		}
+
+		return $variation;
+	}
+
+	/**
+	 * @fixme handle errors more consistently as done in JS UI, and perhaps add
+	 * localised exception messages.
+	 *
+	 * @param Snak $snak
+	 * @return string
+	 */
+	protected function getFormattedSnakValue( $snak ) {
+		try {
+			$formattedSnak = $this->snakFormatter->formatSnak( $snak );
+		} catch ( FormattingException $ex ) {
+			return $this->getInvalidSnakMessage();
+		} catch ( PropertyNotFoundException $ex ) {
+			return $this->getPropertyNotFoundMessage();
+		} catch ( InvalidArgumentException $ex ) {
+			return $this->getInvalidSnakMessage();
+		}
+
+		return $formattedSnak;
+	}
+
+	/**
+	 * @return string
+	 */
+	private function getInvalidSnakMessage() {
+		return wfMessage( 'wikibase-snakformat-invalid-value' )->parse();
+	}
+
+	/**
+	 * @return string
+	 */
+	private function getPropertyNotFoundMessage() {
+		return wfMessage ( 'wikibase-snakformat-propertynotfound' )->parse();
+	}
+}
