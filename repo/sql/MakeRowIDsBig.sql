496dcc619923afa18a158e82100fb8413b6c06cb
daniel <daniel.kinzler@wikimedia.de>
2014-04-03 15:38:43 +0200

    (bug 63482) Fix table prefix in MakeRowIDsBig.sql
    Change-Id: I0c59def5a224722bb61be662d5b627336f19689d

--- repo/sql/MakeRowIDsBig.sql
+++ repo/sql/MakeRowIDsBig.sql
@@ -1,7 +1,7 @@
 -- Update row IDs to BIGINT, so we don't run out of bits.
 
-ALTER TABLE /*:*/wb_terms
+ALTER TABLE /*_*/wb_terms
 MODIFY term_row_id BIGINT unsigned NOT NULL auto_increment;
 
-ALTER TABLE /*:*/wb_items_per_site
+ALTER TABLE /*_*/wb_items_per_site
 MODIFY ips_row_id BIGINT unsigned NOT NULL auto_increment;
