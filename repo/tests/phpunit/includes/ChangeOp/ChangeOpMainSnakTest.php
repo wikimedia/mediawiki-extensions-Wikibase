e4c11612e2eb36cf090d5ed6fb96dd54a19d8a9b
daniel <daniel.kinzler@wikimedia.de>
2014-04-04 17:25:06 +0200

    (bug 62644) snak validation in more change ops
    This adds snak validation to ChangeOpQualifier
    and ChangeOpReference
    
    Change-Id: I181379984027f081d7e40c0f792d58cece656dd7

--- repo/tests/phpunit/includes/ChangeOp/ChangeOpMainSnakTest.php
+++ repo/tests/phpunit/includes/ChangeOp/ChangeOpMainSnakTest.php
@@ -15,8 +15,6 @@ use Wikibase\DataModel\Entity\PropertyId;
 use Wikibase\DataModel\Snak\PropertyNoValueSnak;
 use Wikibase\DataModel\Snak\PropertyValueSnak;
 use Wikibase\DataModel\Snak\Snak;
-use Wikibase\ItemContent;
-use Wikibase\Lib\ClaimGuidGenerator;
 use InvalidArgumentException;
 
 /**
@@ -50,16 +48,11 @@ class ChangeOpMainSnakTest extends \PHPUnit_Framework_TestCase {
 	}
 
 	public function invalidArgumentProvider() {
-		$item = ItemContent::newFromArray( array( 'entity' => 'q42' ) )->getEntity();
-		$validClaimGuid = $this->helper->getGuidGenerator()->newGuid( $item->getId() );
 		$validSnak = new PropertyValueSnak( 7201010, new StringValue( 'o_O' ) );
 
 		$args = array();
 		$args[] = array( 123, $validSnak );
-		$args[] = array( 123, null );
-		$args[] = array( $validClaimGuid, 'notASnak' );
-		$args[] = array( '', 'notASnak' );
-		$args[] = array( '', null );
+		$args[] = array( false, $validSnak );
 
 		return $args;
 	}
