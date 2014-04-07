5387dc1f02496b74e1762a9ba869d01ff53596f7
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2014-04-07 21:28:25 +0200

    Avoid code smell warnings by adding LogicExceptions
    "Code smell" may sound harsh. I'm just quoting the term. ;-)
    
    I'm aware this doesn't change anything. But since PHPStorm and other
    static code ananlyis tools are complaining about "code smell" this
    hopefully helps making the code more maintainable.

--- lib/packages/wikibase/data-model/src/Entity/DispatchingEntityIdParser.php
+++ lib/packages/wikibase/data-model/src/Entity/DispatchingEntityIdParser.php
@@ -3,15 +3,19 @@
 namespace Wikibase\DataModel\Entity;
 
 use InvalidArgumentException;
+use LogicException;
 
 /**
  * @since 0.5
  *
  * @licence GNU GPL v2+
- * @author Jeroen De Dauw < jeroendedauw@gmail.com
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
 class DispatchingEntityIdParser implements EntityIdParser {
 
+	/**
+	 * @var callback[]
+	 */
 	protected $idBuilders;
 
 	/**
@@ -21,7 +25,7 @@ class DispatchingEntityIdParser implements EntityIdParser {
 	 * that takes as only required argument the id serialization
 	 * (string) and returns an EntityId instance.
 	 *
-	 * @param array $idBuilders
+	 * @param callback[] $idBuilders
 	 */
 	public function __construct( array $idBuilders ) {
 		$this->idBuilders = $idBuilders;
@@ -30,8 +34,9 @@ class DispatchingEntityIdParser implements EntityIdParser {
 	/**
 	 * @param string $idSerialization
 	 *
-	 * @return EntityId
 	 * @throws EntityIdParsingException
+	 * @throws LogicException
+	 * @return EntityId
 	 */
 	public function parse( $idSerialization ) {
 		$this->assertIdIsString( $idSerialization );
@@ -43,14 +48,31 @@ class DispatchingEntityIdParser implements EntityIdParser {
 		}
 
 		$this->throwInvalidId( $idSerialization );
+
+		throw new LogicException(
+			'DispatchingEntityIdParser::throwInvalidId did not throw an EntityIdParsingException'
+		);
 	}
 
+	/**
+	 * @param string $idSerialization
+	 *
+	 * @throws EntityIdParsingException
+	 */
 	protected function assertIdIsString( $idSerialization ) {
 		if ( !is_string( $idSerialization ) ) {
 			throw new EntityIdParsingException( 'Entity id serializations need to be strings' );
 		}
 	}
 
+	/**
+	 * @param callback $idBuilder
+	 * @param string $idSerialization
+	 *
+	 * @throws EntityIdParsingException
+	 * @throws LogicException
+	 * @return EntityId
+	 */
 	protected function buildId( $idBuilder, $idSerialization ) {
 		try {
 			return call_user_func( $idBuilder, $idSerialization );
@@ -58,8 +80,17 @@ class DispatchingEntityIdParser implements EntityIdParser {
 		catch ( InvalidArgumentException $ex ) {
 			$this->throwInvalidId( $idSerialization );
 		}
+
+		throw new LogicException(
+			'DispatchingEntityIdParser::throwInvalidId did not throw an EntityIdParsingException'
+		);
 	}
 
+	/**
+	 * @param string $idSerialization
+	 *
+	 * @throws EntityIdParsingException
+	 */
 	protected function throwInvalidId( $idSerialization ) {
 		throw new EntityIdParsingException(
 			'The provided id serialization "' . $idSerialization . '" is not valid'
