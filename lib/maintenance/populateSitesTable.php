b619ee41d3ad8cd40b2b610de4369a15f42ba037
aude <aude.wiki@gmail.com>
2014-04-08 07:34:10 +0200

    Allow populate sites table to work without wikibase enabled
    @note: this really needs to be moved to core or somewhere
    outside of Wikibase!
    
    Change-Id: Ie3116f5d0784c8b49674eb14c98c34c3135404af

--- lib/maintenance/populateSitesTable.php
+++ lib/maintenance/populateSitesTable.php
@@ -4,6 +4,21 @@ $basePath = getenv( 'MW_INSTALL_PATH' ) !== false ? getenv( 'MW_INSTALL_PATH' ) 
 require_once $basePath . '/maintenance/Maintenance.php';
 
 /**
+ * For production (e.g. Wikiquote) use, we need the sites table populated
+ * before enabling Wikibase. This script really has no dependency on
+ * Wikibase, other than these two classes.
+ *
+ * @todo move this to core!
+ */
+if ( !class_exists( 'SitesBuilder' ) ) {
+	require_once __DIR__ . '/../includes/sites/SitesBuilder.php';
+}
+
+if ( !class_exists( 'SiteMatrixParser' ) ) {
+	require_once __DIR__ . '/../includes/sites/SiteMatrixParser.php';
+}
+
+/**
  * Maintenance script for populating the Sites table from another wiki that runs the
  * SiteMatrix extension.
  *
