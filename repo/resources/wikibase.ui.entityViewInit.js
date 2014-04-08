011e9ea218b6bb2990d1b4e6b007730cb1b67a48
Aude <aude.wiki@gmail.com>
2014-04-08 22:57:13 +0000

    per lydia etc., this can wait until next regular deployment
    Revert "Fix edit restriction handling"
    
    This reverts commit 546fab73a5b51542ca007c09bb9c910e8560c7bf.
    
    Change-Id: I54aaba06634d61e1ec30f5ca3ae2be51a4a91943

--- repo/resources/wikibase.ui.entityViewInit.js
+++ repo/resources/wikibase.ui.entityViewInit.js
@@ -149,6 +149,20 @@
 			} );
 		} );
 
+		// Restrict entity page actions if editing is restricted:
+		var editRestriction = mw.config.get( 'wgRestrictionEdit' );
+		if( editRestriction !== null && editRestriction.length === 1 ) {
+			var hasGroup = $.inArray( editRestriction[0], mw.config.get( 'wgUserGroups' ) );
+			if( !hasGroup ) {
+				mw.user.getRights( function( rights ) {
+					var hasRight = $.inArray( editRestriction, rights ) !== -1;
+					if( !hasRight ) {
+						$( wb ).triggerHandler( 'restrictEntityPageActions' );
+					}
+				} );
+			}
+		}
+
 		if ( !mw.config.get( 'wbUserCanEdit' ) ) {
 			$( wb ).triggerHandler( 'restrictEntityPageActions' );
 		} else if ( mw.config.get( 'wbUserIsBlocked' ) ) {
