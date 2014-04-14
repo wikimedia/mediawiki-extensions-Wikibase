0e609cf7f9fc8a287527761b61b378b7ba170f1b
Bene <benestar.wikimedia@googlemail.com>
2014-04-14 23:43:03 +0200

    Create api module 'wbavailablebadges' to query all available badge items
    The module expects no params and just returns all available badge items
    that are specified in the config.
    
    Bug: 63486
    Change-Id: Iae97c4cf8a1c174b1466a80dea6bf5051fd590ab

---
+++ repo/tests/phpunit/includes/api/AvailableBadgesTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Wikibase\Test\Api;
+
+use ApiTestCase;
+use Wikibase\Repo\WikibaseRepo;
+
+/**
+ * Tests for the AvailableBadges class.
+ *
+ * @group API
+ * @group Wikibase
+ * @group WikibaseAPI
+ * @gorup WikibaseRepo
+ * @group medium
+ *
+ * @licence GNU GPL v2+
+ *
+ * @author Bene* < benestar.wikimedia@gmail.com >
+ */
+class AvailabeBadgesTest extends ApiTestCase {
+
+	static $badgeItems = array(
+		'Q123' => '',
+		'Q999' => ''
+	);
+
+	static $oldBadgeItems;
+
+	public function setUp() {
+		parent::setUp();
+		// Allow some badges for testing
+		self::$oldBadgeItems = WikibaseRepo::getDefaultInstance()->getSettings()->getSetting( 'badgeItems' );
+		WikibaseRepo::getDefaultInstance()->getSettings()->setSetting( 'badgeItems', self::$badgeItems );
+	}
+
+	public function tearDown() {
+		parent::tearDown();
+		WikibaseRepo::getDefaultInstance()->getSettings()->setSetting( 'badgeItems', self::$oldBadgeItems );
+	}
+
+	public function testExecute() {
+		list( $result,, ) = $this->doApiRequest( array(
+			'action' => 'wbavailablebadges'
+		) );
+
+		$this->assertEquals( array_keys( self::$badgeItems ), $result['badges'] );
+	}
+
+}
