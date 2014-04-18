4e2117a6b297bc525f3f77784e1fa58d19602a50
addshore <addshorewiki@gmail.com>
2014-04-18 13:12:58 +0100

    Introduce IntegrationApiTest test class
    Change-Id: Ia5cbb25921fb5f78bd4d99462782e8d9c2524016

---
+++ repo/tests/phpunit/includes/api/IntegrationApiTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Wikibase\Test\Api;
+
+use ApiTestCase;
+use TestSites;
+
+/**
+ * This class holds simple integration tests for Wikibase API modules
+ *
+ * @group API
+ * @group Wikibase
+ * @group WikibaseAPI
+ * @gorup WikibaseRepo
+ * @group BreakingTheSlownessBarrier
+ * @group Database
+ * @group medium
+ *
+ * @author Adam Shorland
+ */
+class IntegrationApiTest extends ApiTestCase {
+
+	public function setUp() {
+		parent::setUp();
+		TestSites::insertIntoDb();
+	}
+
+	public function provideApiRequests() {
+		return array(
+			'wbgetentities-id' => array(
+				array(
+					'action' => 'wbgetentities',
+					'ids' => 'Q919191919191',
+				),
+				array(
+					'entities' => array(
+						'-1' => array(
+							'id' => 'Q919191919191',
+							'missing' => '',
+						),
+					),
+					'success' => 1,
+				),
+			),
+			'wbgetentities-sitetitle' => array(
+				array(
+					'action' => 'wbgetentities',
+					'sites' => 'enwiki',
+					'titles' => 'FooBarBazBazBaz1111211',
+				),
+				array(
+					'entities' => array(
+						'-1' => array(
+							'site' => 'enwiki',
+							'title' => 'FooBarBazBazBaz1111211',
+							'missing' => '',
+						),
+					),
+					'success' => 1,
+				),
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider provideApiRequests
+	 */
+	public function testApiModuleResult( $params, $expected ) {
+		list( $result ) = $this->doApiRequest( $params );
+		$this->assertEquals( $expected, $result );
+	}
+
+}
