c0a122b9146498d9c6bd8a39a42c4cf69813b9d2
Adrian Lang <adrian.lang@wikimedia.de>
2014-04-23 09:12:34 +0200

    Factor out generation of site links HTML into own class
    This has the nice side effect of reducing the number of created SiteSQLStore
    instances. Since every SiteSQLStore instance maintains its own cached version
    of the sites list, which it first has to fetch from the DB or cache, this
    reduces both rendering time and memory consumption.
    
    Change-Id: Id2e1fba574b737c15fa2dc5ee4fb6152ca240c96

--- repo/includes/ItemView.php
+++ repo/includes/ItemView.php
@@ -4,6 +4,7 @@ namespace Wikibase;
 
 use Wikibase\DataModel\SimpleSiteLink;
 use Wikibase\Repo\WikibaseRepo;
+use Wikibase\View\SiteLinksView;
 
 /**
  * Class for creating views for Wikibase\Item instances.
@@ -75,156 +76,14 @@ class ItemView extends EntityView {
 	 */
 	public function getHtmlForSiteLinks( Item $item, $editable = true ) {
 		$groups = WikibaseRepo::getDefaultInstance()->getSettings()->getSetting( 'siteLinkGroups' );
-		$html = '';
 
-		foreach ( $groups as $group ) {
-			$html .= $this->getHtmlForSiteLinkGroup( $item, $group, $editable );
-		}
-
-		return $html;
-	}
-
-	/**
-	 * Builds and returns the HTML representing a group of a WikibaseEntity's site-links.
-	 *
-	 * @todo: code in this function belongs in its own class!
-	 *
-	 * @since 0.4
-	 *
-	 * @param Item $item the entity to render
-	 * @param string $group a site group ID
-	 * @param bool $editable whether editing is allowed (enabled edit links)
-	 * @return string
-	 */
-	public function getHtmlForSiteLinkGroup( Item $item, $group, $editable = true ) {
-		$wikibaseRepo = WikibaseRepo::getDefaultInstance();
-
-		// @todo inject into constructor
-		$sites = $wikibaseRepo->getSiteStore()->getSites();
-
-		$specialGroups = $wikibaseRepo->getSettings()->getSetting( "specialSiteLinkGroups" );
-
-		$allSiteLinks = $item->getSiteLinks();
-		$siteLinks = array(); // site links of the currently handled site group
-
-		foreach( $allSiteLinks as $siteLink ) {
-			$site = $sites->getSite( $siteLink->getSiteId() );
-
-			if ( $site === null ) {
-				continue;
-			}
-
-			if ( $site->getGroup() === $group ) {
-				$siteLinks[] = $siteLink;
-			}
-		}
-
-		$html = $thead = $tbody = $tfoot = '';
-
-		$html .= wfTemplate(
-			'wb-section-heading-sitelinks',
-			wfMessage( 'wikibase-sitelinks-' . $group )->parse(), // heading
-			htmlspecialchars( 'sitelinks-' . $group, ENT_QUOTES ) // ID
-			// TODO: support entity-id as prefix for element IDs.
+		// FIXME: Inject this
+		$siteLinksView = new SiteLinksView(
+			WikibaseRepo::getDefaultInstance()->getSiteStore(),
+			$this->sectionEditLinkGenerator
 		);
 
-		// FIXME: quickfix to allow a custom site-name / handling for groups defined in $wgSpecialSiteLinkGroups
-		$siteNameMessageKey = 'wikibase-sitelinks-sitename-columnheading';
-		if ( in_array( $group, $specialGroups ) ) {
-			$siteNameMessageKey .= '-special';
-		}
-
-		if( !empty( $siteLinks ) ) {
-			$thead = wfTemplate( 'wb-sitelinks-thead',
-				wfMessage( $siteNameMessageKey )->parse(),
-				wfMessage( 'wikibase-sitelinks-siteid-columnheading' )->parse(),
-				wfMessage( 'wikibase-sitelinks-link-columnheading' )->parse()
-			);
-		}
-
-		$i = 0;
-
-		// Sort the sitelinks according to their global id
-		$safetyCopy = $siteLinks; // keep a shallow copy;
-		$sortOk = usort(
-			$siteLinks,
-			function( SimpleSiteLink $a, SimpleSiteLink $b ) {
-				return strcmp( $a->getSiteId(), $b->getSiteId() );
-			}
-		);
-
-		if ( !$sortOk ) {
-			$siteLinks = $safetyCopy;
-		}
-
-		// Link to SpecialPage
-		$editLink = $this->getEditUrl( 'SetSiteLink', $item, null );
-
-		/* @var SimpleSiteLink $link */
-		foreach( $siteLinks as $siteLink ) {
-			$alternatingClass = ( $i++ % 2 ) ? 'even' : 'uneven';
-
-			$siteId = $siteLink->getSiteId();
-			$pageName = $siteLink->getPageName();
-
-			$site = $sites->hasSite( $siteId ) ? $sites->getSite( $siteId ) : null;
-
-			if ( !$site || $site->getDomain() === '' ) {
-				// the link is pointing to an unknown site.
-				// XXX: hide it? make it red? strike it out?
-
-				$tbody .= wfTemplate( 'wb-sitelink-unknown',
-					$alternatingClass,
-					htmlspecialchars( $siteId ),
-					htmlspecialchars( $siteLink->getPageName() ),
-					$this->getHtmlForEditSection( $editLink, 'td' )
-				);
-
-			} else {
-				$languageCode = $site->getLanguageCode();
-				$escapedSiteId = htmlspecialchars( $siteId );
-				// FIXME: this is a quickfix to allow a custom site-name for groups defined in $wgSpecialSiteLinkGroups instead of showing the language-name
-				if ( in_array( $group, $specialGroups ) ) {
-					$siteNameMsg = wfMessage( 'wikibase-sitelinks-sitename-' . $siteId );
-					$siteName = $siteNameMsg->exists() ? $siteNameMsg->parse() : $siteId;
-				} else {
-					// TODO: get an actual site name rather then just the language
-					$siteName = htmlspecialchars( Utils::fetchLanguageName( $languageCode ) );
-				}
-
-				// TODO: for non-JS, also set the dir attribute on the link cell;
-				// but do not build language objects for each site since it causes too much load
-				// and will fail when having too much site links
-				$tbody .= wfTemplate( 'wb-sitelink',
-					$languageCode,
-					$alternatingClass,
-					$siteName,
-					$escapedSiteId, // displayed site ID
-					htmlspecialchars( $site->getPageUrl( $pageName ) ),
-					htmlspecialchars( $pageName ),
-					$this->getHtmlForEditSection( $editLink . '/' . $escapedSiteId, 'td' ),
-					$escapedSiteId // ID used in classes
-				);
-			}
-		}
-
-		// built table footer with button to add site-links, consider list could be complete!
-		$isFull = count( $siteLinks ) >= count( $sites );
-
-		$tfoot = wfTemplate( 'wb-sitelinks-tfoot',
-			$isFull ? wfMessage( 'wikibase-sitelinksedittool-full' )->parse() : '',
-			$this->getHtmlForEditSection( $editLink, 'td', 'add', !$isFull )
-		);
-
-		$groupName = in_array( $group, $specialGroups ) ? 'special' : $group;
-
-		return $html . wfTemplate(
-			'wb-sitelinks-table',
-			$thead,
-			$tbody,
-			$tfoot,
-			htmlspecialchars( $groupName )
-		);
+		return $siteLinksView->getHtml( $item, $groups, $editable );
 	}
 
 }
