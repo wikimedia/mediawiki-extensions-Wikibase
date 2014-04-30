ec40ed93d91d19fb11bc58d2e7cb965c339d3108
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2014-04-30 21:04:12 +0200

    Refacatoring MwTimeIsoFormatter
    ... again. ;-) I hope it makes sense.
    
    This also adds a bunch of additional test cases.
    
    In case you disagree with my relaxed aproach (e.g. the year +0 being
    rendered), my answer is:
    
    This is a formatter. A formatter should not do much validation. If
    a value can be rendered without loosing digits, it should be rendered.
    Validation must be done somewhere else.
    
    Change-Id: I2490c9ca26824b5b0a312044f19efb93616f0597

--- lib/tests/phpunit/formatters/MwTimeIsoFormatterTest.php
+++ lib/tests/phpunit/formatters/MwTimeIsoFormatterTest.php
@@ -22,6 +22,7 @@ use Wikibase\Lib\Parsers\TimeParser;
  * @licence GNU GPL v2+
  * @author H. Snater < mediawiki@snater.com >
  * @author Adam Shorland
+ * @author Thiemo Mättig
  */
 class MwTimeIsoFormatterTest extends \MediaWikiTestCase {
 
@@ -42,7 +43,7 @@ class MwTimeIsoFormatterTest extends \MediaWikiTestCase {
 	 */
 	public function formatProvider() {
 		$tests = array(
-			//+ dates
+			// Positive dates
 			array(
 				'+2013-08-16T00:00:00Z', TimeValue::PRECISION_DAY,
 				'16 August 2013',
@@ -72,22 +73,6 @@ class MwTimeIsoFormatterTest extends \MediaWikiTestCase {
 				'2013',
 			),
 			array(
-				'+00000001995-00-00T00:00:00Z', TimeValue::PRECISION_YEAR,
-				'1995',
-			),
-			array(
-				'+00000001996-01-00T00:00:00Z', TimeValue::PRECISION_YEAR,
-				'1996',
-			),
-			array(
-				'+00000001996-01-00T00:00:00Z', TimeValue::PRECISION_MONTH,
-				'January 1996',
-			),
-			array(
-				'+00000001997-00-01T00:00:00Z', TimeValue::PRECISION_YEAR,
-				'1997',
-			),
-			array(
 				'+00000000013-07-16T00:00:00Z', TimeValue::PRECISION_YEAR,
 				'13',
 			),
@@ -99,7 +84,8 @@ class MwTimeIsoFormatterTest extends \MediaWikiTestCase {
 				'+12342222013-07-16T00:10:00Z', TimeValue::PRECISION_YEAR,
 				'12342222013',
 			),
-			//stepping through precisions
+
+			// Positive dates, stepping through precisions
 			array(
 				'+12345678912-01-01T01:01:01Z', TimeValue::PRECISION_10a,
 				'12345678910s',
@@ -173,7 +159,7 @@ class MwTimeIsoFormatterTest extends \MediaWikiTestCase {
 				'in 13 billion years',
 			),
 
-			//- dates
+			// Negative dates
 			array(
 				'-2013-08-16T00:00:00Z', TimeValue::PRECISION_DAY,
 				'16 August 2013 BCE',
@@ -214,7 +200,8 @@ class MwTimeIsoFormatterTest extends \MediaWikiTestCase {
 				'-12342222013-07-16T00:10:00Z', TimeValue::PRECISION_YEAR,
 				'12342222013 BCE',
 			),
-			//stepping through precisions
+
+			// Negative dates, stepping through precisions
 			array(
 				'-12345678912-01-01T01:01:01Z', TimeValue::PRECISION_10a,
 				'12345678910s BCE',
@@ -288,20 +275,97 @@ class MwTimeIsoFormatterTest extends \MediaWikiTestCase {
 				'13 billion years ago',
 			),
 
-			// Stuff we dont want to format so must return it :<
+			// Valid values with day, month and/or year zero
+			array(
+				'+00000001995-00-00T00:00:00Z', TimeValue::PRECISION_YEAR,
+				'1995',
+			),
+			array(
+				'+00000001996-01-00T00:00:00Z', TimeValue::PRECISION_YEAR,
+				'1996',
+			),
+			array(
+				'+00000001996-01-00T00:00:00Z', TimeValue::PRECISION_MONTH,
+				'January 1996',
+			),
+			array(
+				'+00000001997-00-01T00:00:00Z', TimeValue::PRECISION_YEAR,
+				'1997',
+			),
+			array(
+				'+0-00-00T00:00:42Z', TimeValue::PRECISION_YEAR,
+				'0',
+			),
+
+			// Integer overflows should not happen
+			array(
+				'+2147483648-00-00T00:00:00Z', TimeValue::PRECISION_YEAR,
+				'2147483648',
+			),
+			array(
+				'+9999999999999999-00-00T00:00:00Z', TimeValue::PRECISION_YEAR,
+				'9999999999999999',
+			),
+
+			// Stuff we do not want to format so must return it :<
 			array(
 				'-00000000000-01-01T01:01:01Z', TimeValue::PRECISION_Ga,
 			),
 			array(
 				'-0-01-01T01:01:01Z', TimeValue::PRECISION_Ga,
 			),
+			array(
+				'+100000000-00-00T00:00:00Z', TimeValue::PRECISION_Ga,
+			),
+			array(
+				'+10000000-00-00T00:00:01Z', TimeValue::PRECISION_100Ma,
+			),
+			array(
+				'+1000000-00-00T00:00:02Z', TimeValue::PRECISION_10Ma,
+			),
+			array(
+				'+100000-00-00T00:00:03Z', TimeValue::PRECISION_Ma,
+			),
+			array(
+				'+10000-00-00T00:00:04Z', TimeValue::PRECISION_100ka,
+			),
+			array(
+				'+1000-00-00T00:00:05Z', TimeValue::PRECISION_10ka,
+			),
+			array(
+				'+100-00-00T00:00:06Z', TimeValue::PRECISION_ka,
+			),
+			array(
+				'+10-00-00T00:00:07Z', TimeValue::PRECISION_100a,
+			),
+			array(
+				'+1-00-00T00:00:08Z', TimeValue::PRECISION_10a,
+			),
+			array(
+				'-0-00-00T00:00:42Z', TimeValue::PRECISION_YEAR,
+			),
+			array(
+				'+00000002013-07-00T00:00:00Z', TimeValue::PRECISION_DAY,
+			),
+			array(
+				'+10000000000-00-00T00:00:00Z', TimeValue::PRECISION_DAY,
+			),
 		);
 
 		$argLists = array();
 
 		foreach ( $tests as $args ) {
-			$timeValue = new TimeValue( $args[0], 0, 0, 0, $args[1], TimeFormatter::CALENDAR_GREGORIAN );
-			$argLists[] = array( empty( $args[2] ) ? $args[0] : $args[2], $timeValue, !empty( $args[3] ) );
+			$timeValue = new TimeValue(
+				$args[0],
+				0, 0, 0,
+				$args[1],
+				TimeFormatter::CALENDAR_GREGORIAN
+			);
+			$argLists[] = array(
+				isset( $args[2] ) ? $args[2] : $args[0],
+				$timeValue,
+				isset( $args[3] )
+			);
 		}
 
 		// Different languages at year precision
@@ -311,7 +375,7 @@ class MwTimeIsoFormatterTest extends \MediaWikiTestCase {
 			'de', //switches separators
 			'or', //replaces all numbers and separators
 		);
-		foreach( $languageCodes as $languageCode ) {
+		foreach ( $languageCodes as $languageCode ) {
 			$argLists[] = array(
 				'3333',
 				new TimeValue(
@@ -334,25 +398,29 @@ class MwTimeIsoFormatterTest extends \MediaWikiTestCase {
 	 * @param string $expected
 	 * @param TimeValue $timeValue
 	 * @param bool $roundtrip
-	 * @param string $langCode
+	 * @param string $languageCode
 	 */
-	public function testFormat( $expected, TimeValue $timeValue, $roundtrip = false, $langCode = 'en' ) {
+	public function testFormat(
+		$expected,
+		TimeValue $timeValue,
+		$roundtrip = false,
+		$languageCode = 'en'
+	) {
 		$options = new FormatterOptions( array(
-			ValueFormatter::OPT_LANG => $langCode
+			ValueFormatter::OPT_LANG => $languageCode
 		) );
+		$formatter = new MwTimeIsoFormatter( $options );
+		$actual = $formatter->format( $timeValue );
 
-		$isoFormatter = new MwTimeIsoFormatter( $options );
-
-		$formattedTime = $isoFormatter->format( $timeValue );
-		$this->assertEquals( $expected, $formattedTime );
-		if( $roundtrip ) {
-			$this->assertCanRoundTrip( $formattedTime, $timeValue, $langCode );
+		$this->assertEquals( $expected, $actual );
+		if ( $roundtrip ) {
+			$this->assertCanRoundTrip( $actual, $timeValue, $languageCode );
 		}
 	}
 
-	private function assertCanRoundTrip( $formattedTime, TimeValue $timeValue, $langCode ) {
+	private function assertCanRoundTrip( $formattedTime, TimeValue $timeValue, $languageCode ) {
 		$options = new ParserOptions( array(
-			ValueParser::OPT_LANG => $langCode,
+			ValueParser::OPT_LANG => $languageCode,
 			\ValueParsers\TimeParser::OPT_PRECISION => $timeValue->getPrecision(),
 			\ValueParsers\TimeParser::OPT_CALENDAR => $timeValue->getCalendarModel(),
 		) );
