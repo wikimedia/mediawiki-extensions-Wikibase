a7268afc8fdfef275836207c923803b6d946da8c
daniel <daniel.kinzler@wikimedia.de>
2014-05-07 14:57:39 +0200

    Refactoring ItemDisambiguation logic.
    Removes all uses of EntityContent from ItemDisambiguation
    and refactors the logic.
    
    This also adds a load of test cases.
    
    Change-Id: I340adafdf678796cd5862b85858205a76b0a8017

--- repo/includes/ItemDisambiguation.php
+++ repo/includes/ItemDisambiguation.php
@@ -2,122 +2,159 @@
 
 namespace Wikibase;
 
-use IContextSource;
+use Language;
+use Wikibase\Lib\EntityIdFormatter;
 
 /**
  * Class representing the disambiguation of a list of WikibaseItems.
  *
- * @since 0.1
+ * @since 0.5
  *
  * @licence GNU GPL v2+
  * @author aude
  * @author jeblad
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ * @author Daniel Kinzler
  */
-class ItemDisambiguation extends \ContextSource {
+class ItemDisambiguation {
 
 	/**
-	 * @since 0.1
-	 * @var Item
+	 * @var string
 	 */
-	protected $items;
+	protected $searchLangCode;
 
-	protected $langCode;
+	/**
+	 * @var string
+	 */
+	protected $userLangCode;
+
+	/**
+	 * @var EntityIdFormatter
+	 */
+	private $linkFormatter;
 
 	/**
 	 * Constructor.
 	 *
-	 * @since 0.1
+	 * @since 0.5
 	 *
-	 * @param ItemContent[] $items
-	 * @param string $langCode
-	 * @param IContextSource|null $context
-	 *
-	 * @thorws MWException
+	 * @param string $searchLangCode The language the search was performed for.
+	 * @param string $userLangCode The user's interface language.
+	 * @param EntityIdFormatter $linkFormatter A formatter for generating HTML links for a given EntityId.
 	 */
-	public function __construct( array $items, $langCode, IContextSource $context = null ) {
-		$this->items = $items;
-		$this->langCode = $langCode;
+	public function __construct( $searchLangCode, $userLangCode, EntityIdFormatter $linkFormatter ) {
+		$this->searchLangCode = $searchLangCode;
+		$this->userLangCode = $userLangCode;
 
-		if ( !is_null( $context ) ) {
-			$this->setContext( $context );
-		}
+		$this->linkFormatter = $linkFormatter;
 	}
 
 	/**
 	 * Builds and returns the HTML to represent the WikibaseItem.
 	 *
-	 * @since 0.1
+	 * @since 0.5
 	 *
-	 * @return string
+	 * @param Item[] $items
+	 *
+	 * @return string HTML
 	 */
-	public function getHTML() {
-		$userLang = $this->getLanguage()->getCode();
-		$searchLang = $this->langCode;
-
+	public function getHTML( array $items ) {
 		return
 			'<ul class="wikibase-disambiguation">' .
 				implode( '', array_map(
-					function( ItemContent $itemContent ) use ( $userLang, $searchLang ) {
-
-						$userLabel = $itemContent->getItem()->getLabel( $userLang );
-						$searchLabel = $itemContent->getItem()->getLabel( $searchLang );
-
-						// link to the item. The text is the label in the user's language, or the ID if no label exists
-						$idLabel = \Html::openElement( 'span', array( 'class' => 'wb-itemlink-id' ) )
-							. wfMessage( 'wikibase-itemlink-id-wrapper' )->params(
-								$itemContent->getEntity()->getId()->getSerialization() )->escaped()
-							. \Html::closeElement( 'span' );
-						$result =
-							\Linker::link(
-								$itemContent->getTitle(),
-								$userLabel ? htmlspecialchars( $userLabel ) : $idLabel
-							);
-
-						// display the label in the searched language in case it is different than in the user language
-						if ( ( $userLang !== $searchLang ) && ( $userLabel !== $searchLabel ) ) {
-							$result = $result
-								. wfMessage( 'wikibase-itemlink-userlang-wrapper' )
-									->rawParams(
-										\Language::fetchLanguageName( $searchLang, $userLang ),
-										\Html::element(
-											'span',
-											array( 'class' => 'wb-itemlink-query-lang', 'lang' => $searchLang ),
-											$searchLabel
-										)
-									)
-									->parse();
-						};
-
-						// display the description in the user's language
-						$description = htmlspecialchars( $itemContent->getItem()->getDescription( $userLang ) );
-						if ( $description === "" ) {
-							// Display the ID if no description is available
-							// do not display it if the ID was already displayed, i.e. if it was used instead of the label previously
-							$result .= $userLabel ? ' ' . $idLabel : '';
-						} else {
-							$result .=
-								wfMessage( 'colon-separator' )->escaped()
-								. \Html::openElement( 'span', array( 'class' => 'wb-itemlink-description' ) )
-								. $description
-								. \Html::closeElement( 'span' );
-						}
-
-						$result = \Html::rawElement( 'li', array( 'class' => 'wikibase-disambiguation' ), $result );
-						return $result;
-					},
-					$this->items
+					array( $this, 'getItemHtml' ),
+					$items
 				) ).
 			'</ul>';
 	}
 
 	/**
-	 * Display the item using the set context.
+	 * @param Item $item
 	 *
-	 * @since 0.1
+	 * @return string HTML
 	 */
-	public function display() {
-		$this->getOutput()->addHTML( $this->getHTML() );
+	public function getItemHtml( Item $item ) {
+		$userLang = $this->userLangCode;
+		$searchLang = $this->searchLangCode;
+
+		$result = $this->linkFormatter->format( $item->getId() );
+
+		// Display the label in the searched language in case it is different than in
+		// the user language.
+		if ( $userLang !== $searchLang
+			&& $item->getLabel( $userLang ) !== $item->getLabel( $searchLang )  ) {
+			$result .= $this->getLabelHtml( $item, $searchLang );
+		};
+
+		$result .= $this->getDescriptionHtml( $item, $userLang );
+
+		$result = \Html::rawElement( 'li', array( 'class' => 'wikibase-disambiguation' ), $result );
+
+		return $result;
+	}
+
+	/**
+	 * Returns HTML representing the label in the given language.
+	 * The result will include the language's name in the user language.
+	 *
+	 * @param Item $item
+	 * @param string $language
+	 *
+	 * @return string HTML
+	 */
+	private function getLabelHtml( Item $item, $language ) {
+		$label = $item->getLabel( $language );
+
+		$labelElement = \Html::element(
+			'span',
+			array( 'class' => 'wb-itemlink-query-lang', 'lang' => $language ),
+			$label
+		);
+
+		$msg = wfMessage( 'wikibase-itemlink-userlang-wrapper' )
+			->rawParams(
+				\Language::fetchLanguageName( $language, $this->userLangCode ),
+				$labelElement
+			);
+
+		return $msg->parse();
+	}
+
+	/**
+	 * Returns HTML representing the description in the given language.
+	 * If no description is defined in that language, return the item's ID,
+	 * unless the label is not defined either. In that case, this method
+	 * returns an empty string, because the entity ID was already used as
+	 * a label.
+	 *
+	 * @param Item $item
+	 * @param string $language
+	 *
+	 * @return string HTML
+	 */
+	private function getDescriptionHtml( Item $item, $language ) {
+
+		// display the description in the user's language
+		$description = $item->getDescription( $language );
+		if ( $description === false || $description === '' ) {
+			// Display the ID if no description is available
+			// do not display it if the ID was already displayed, i.e. if it was used instead of the label previously
+			$userLabel = $item->getLabel( $language );
+			$idLabel = $item->getId()->getSerialization();
+
+			$html = $userLabel ? ' ' . $idLabel : '';
+		} else {
+			$descriptionElement = \Html::element(
+				'span',
+				array( 'class' => 'wb-itemlink-description' ),
+				$description
+			);
+
+			$html = htmlspecialchars( wfMessage( 'colon-separator' )->plain() )
+				. $descriptionElement;
+		}
+
+		return $html;
 	}
 
 }
