430a02e80d8d716d04523084ef38d9a89476deee
daniel <daniel.kinzler@wikimedia.de>
2014-05-14 18:14:14 +0200

    Handle DVs that mismatch a prop's expected type
    If a Snak contains a DataValue of a type different from
    the value type expected by the peroperty's data type,
    don't fail, but handle gracefully.
    
    Bug: 63299
    Change-Id: I41b489ff5b5ea474861922a8c50b214e67851319

--- lib/tests/phpunit/formatters/PropertyValueSnakFormatterTest.php
+++ lib/tests/phpunit/formatters/PropertyValueSnakFormatterTest.php
@@ -2,13 +2,18 @@
 
 namespace Wikibase\Lib\Test;
 
+use DataTypes\DataType;
 use DataValues\StringValue;
+use DataValues\UnDeserializableValue;
+use ValueFormatters\FormatterOptions;
 use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\DataModel\Snak\PropertySomeValueSnak;
+use Wikibase\DataModel\Snak\PropertyValueSnak;
 use Wikibase\Lib\DispatchingValueFormatter;
+use Wikibase\Lib\PropertyNotFoundException;
 use Wikibase\Lib\PropertyValueSnakFormatter;
 use Wikibase\Lib\SnakFormatter;
-use Wikibase\PropertySomeValueSnak;
-use Wikibase\PropertyValueSnak;
+use Wikibase\Lib\UnDeserializableValueFormatter;
 
 /**
  * @covers Wikibase\Lib\PropertyValueSnakFormatter
@@ -32,12 +37,7 @@ class PropertyValueSnakFormatterTest extends \PHPUnit_Framework_TestCase {
 	public function testConstructorErrors( $format, $error ) {
 		$this->setExpectedException( $error );
 
-		$typeLookup = $this->getMock( 'Wikibase\Lib\PropertyDataTypeLookup' );
-		$typeLookup->expects( $this->never() )->method( 'getDataTypeIdForProperty' );
-
-		$valueFormatter = new DispatchingValueFormatter( array() );
-
-		new PropertyValueSnakFormatter( $format, $valueFormatter, $typeLookup );
+		$this->getDummyPropertyValueSnakFormatter( $format );
 	}
 
 	public function constructorErrorsProvider() {
@@ -53,74 +53,138 @@ class PropertyValueSnakFormatterTest extends \PHPUnit_Framework_TestCase {
 	 * @dataProvider formatSnakProvider
 	 * @covers PropertyValueSnakFormatter::formatSnak()
 	 */
-	public function testFormatSnak( $snak, $type, $formatters, $expected ) {
+	public function testFormatSnak( $snak, $dataType, $valueType, $targetFormat, $formatters, $expected ) {
+		if ( $dataType !== '' ) {
+			$getDataTypeIdForPropertyResult = $this->returnValue( $dataType );
+		} else {
+			$getDataTypeIdForPropertyResult = $this->throwException(
+				new PropertyNotFoundException( new PropertyId( 'P666' ) ) );
+		}
+
 		$typeLookup = $this->getMock( 'Wikibase\Lib\PropertyDataTypeLookup' );
 		$typeLookup->expects( $this->atLeastOnce() )
 			->method( 'getDataTypeIdForProperty' )
-			->will( $this->returnValue( $type ) );
+			->will( $getDataTypeIdForPropertyResult );
+
+		$typeFactory = $this->getMock( 'DataTypes\DataTypeFactory' );
+		$typeFactory->expects( $this->any() )
+			->method( 'getType' )
+			->will( $this->returnValue( new DataType( $dataType, $valueType, array() ) ) );
 
 		$formatter = new PropertyValueSnakFormatter(
-			SnakFormatter::FORMAT_PLAIN,
+			$targetFormat,
 			new DispatchingValueFormatter( $formatters ),
-			$typeLookup
+			$typeLookup,
+			$typeFactory
 		);
 
-		$this->assertEquals( $expected, $formatter->formatSnak( $snak ) );
+		// NOTE: we want to suppress warnings here, so we can test that mismatching DataValues
+		// are still processed correctly after causing a warning.
+		wfSuppressWarnings();
+		$actual = $formatter->formatSnak( $snak );
+		wfRestoreWarnings();
+
+		$this->assertRegExp( $expected, $actual );
+	}
+
+	private function getMockFormatter( $value ) {
+		$formatter = $this->getMock( 'ValueFormatters\ValueFormatter' );
+		$formatter->expects( $this->any() )
+			->method( 'format' )
+			->will( $this->returnValue( $value ) );
+
+		return $formatter;
 	}
 
 	public function formatSnakProvider() {
-		$stringFormatter = $this->getMock( 'ValueFormatters\ValueFormatter' );
-		$stringFormatter->expects( $this->any() )
-			->method( 'format' )
-			->will( $this->returnValue( 'VT:string' ) );
-
-		$mediaFormatter = $this->getMock( 'ValueFormatters\ValueFormatter' );
-		$mediaFormatter->expects( $this->any() )
-			->method( 'format' )
-			->will( $this->returnValue( 'PT:commonsMedia' ) );
-
 		$formatters = array(
-			'VT:string' => $stringFormatter,
-			'PT:commonsMedia' => $mediaFormatter,
+			'VT:bad' => new UnDeserializableValueFormatter( new FormatterOptions() ),
+			'VT:string' => $this->getMockFormatter( 'VT:string' ),
+			'PT:commonsMedia' => $this->getMockFormatter( 'PT:commonsMedia' )
 		);
 
 		return array(
 			'match PT' => array(
 				new PropertyValueSnak( 17, new StringValue( 'Foo.jpg' ) ),
 				'commonsMedia',
+				'string',
+				SnakFormatter::FORMAT_PLAIN,
 				$formatters,
-				'PT:commonsMedia'
+				'/^PT:commonsMedia$/'
 			),
 
 			'match VT' => array(
 				new PropertyValueSnak( 33, new StringValue( 'something' ) ),
 				'someStuff',
+				'string',
+				SnakFormatter::FORMAT_WIKI,
 				$formatters,
-				'VT:string'
+				'/^VT:string$/'
+			),
+
+			//NOTE: will fail unless warnings are suppressed
+			'UnDeserializableValue' => array(
+				new PropertyValueSnak( 7,
+					new UnDeserializableValue( 'cookie', 'globecoordinate', 'cannot understand!' )
+				),
+				'globe-coordinate',
+				'bad',
+				SnakFormatter::FORMAT_HTML,
+				$formatters,
+				// message key: wikibase-undeserializable-value
+				'/value is invalid/'
+			),
+
+			//NOTE: will fail unless warnings are suppressed
+			'VT mismatching PT' => array(
+				new PropertyValueSnak( 7, new StringValue( 'dummy' ) ),
+				'url',
+				'iri', // url expects an iri, but will get a string
+				SnakFormatter::FORMAT_WIKI,
+				$formatters,
+				// message key: wikibase-snakview-variation-datavaluetypemismatch-details
+				'@^VT:string <span class="error">\(.*does not match.*\)</span>$@'
+			),
+
+			//NOTE: will fail unless warnings are suppressed
+			'property not found' => array(
+				new PropertyValueSnak( 7, new StringValue( 'dummy' ) ),
+				'', // triggers an exception from the mock PropertyDataTypeLookup
+				'xxx', // should not be used
+				SnakFormatter::FORMAT_HTML,
+				$formatters,
+				// message key: wikibase-snakformat-propertynotfound
+				'@^VT:string <span class="error">\(.*not found.*\)</span>$@'
 			),
 		);
+	}
+
+	private function getDummyPropertyValueSnakFormatter( $format = 'test' ) {
+		$typeLookup = $this->getMock( 'Wikibase\Lib\PropertyDataTypeLookup' );
+		$typeLookup->expects( $this->never() )->method( 'getDataTypeIdForProperty' );
+
+		$typeFactory = $this->getMock( 'DataTypes\DataTypeFactory' );
+		$typeFactory->expects( $this->never() )->method( 'getType' );
+
+		$valueFormatter = new DispatchingValueFormatter( array() );
+
+		$formatter = new PropertyValueSnakFormatter( $format, $valueFormatter, $typeLookup, $typeFactory );
+		return $formatter;
 	}
 
 	/**
 	 * @covers PropertyValueSnakFormatter::getFormat()
 	 */
 	public function testGetFormat() {
-		$typeLookup = $this->getMock( 'Wikibase\Lib\PropertyDataTypeLookup' );
-		$typeLookup->expects( $this->never() )->method( 'getDataTypeIdForProperty' );
-
-		$formatter = new PropertyValueSnakFormatter( 'test', new DispatchingValueFormatter( array() ), $typeLookup );
+		$formatter = $this->getDummyPropertyValueSnakFormatter();
 		$this->assertEquals( 'test', $formatter->getFormat() );
 	}
-
 
 	/**
 	 * @covers MessageSnakFormatter::canFormatSnak()
 	 */
 	public function testCanFormatSnak() {
-		$typeLookup = $this->getMock( 'Wikibase\Lib\PropertyDataTypeLookup' );
-		$typeLookup->expects( $this->never() )->method( 'getDataTypeIdForProperty' );
-
-		$formatter = new PropertyValueSnakFormatter( 'test', new DispatchingValueFormatter( array() ), $typeLookup );
+		$formatter = $this->getDummyPropertyValueSnakFormatter();
 
 		$snak = new PropertyValueSnak( new PropertyId( "P23" ), new StringValue( 'test' ) );
 		$this->assertTrue( $formatter->canFormatSnak( $snak ), $snak->getType() );
