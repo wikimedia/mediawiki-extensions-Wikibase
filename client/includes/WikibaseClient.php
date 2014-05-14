9881be23d8aeffc566b10291f700487367a3ca01
daniel <daniel.kinzler@wikimedia.de>
2014-05-14 18:14:14 +0200

    Handle DVs that mismatch a prop's expected type
    If a Snak contains a DataValue of a type different from
    the value type expected by the property's data type,
    don't fail, but handle gracefully.
    
    Bug: 63299
    Change-Id: I41b489ff5b5ea474861922a8c50b214e67851319

--- client/includes/WikibaseClient.php
+++ client/includes/WikibaseClient.php
@@ -487,7 +487,8 @@ final class WikibaseClient {
 
 		$builders = new WikibaseSnakFormatterBuilders(
 			$valueFormatterBuilders,
-			$this->getPropertyDataTypeLookup()
+			$this->getPropertyDataTypeLookup(),
+			$this->getDataTypeFactory()
 		);
 
 		$factory = new OutputFormatSnakFormatterFactory( $builders->getSnakFormatterBuildersForFormats() );
