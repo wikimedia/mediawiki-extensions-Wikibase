a0dd85522df55c86d659d96bad12ae1625d77bc6
daniel <daniel.kinzler@wikimedia.de>
2014-05-14 15:07:16 +0200

    Prevent import of entities to avoid ID conflicts
    Bug: 63228
    Change-Id: Ie03b10dcb37feb5d038de1730e58daf00d2b49ea

---
+++ repo/tests/phpunit/includes/RepoHooksTest.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace Wikibase\Tests;
+
+use ImportStreamSource;
+use Wikibase\RepoHooks;
+use WikiImporter;
+
+/**
+ * @covers Wikibase\RepoHooks
+ *
+ * @group Wikibase
+ * @group WikibaseRepo
+ *
+ * @group Database
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ */
+class RepoHooksTest extends \MediaWikiTestCase {
+
+	public function revisionInfoProvider() {
+		return array(
+			'empty' => array( array() ),
+			'wikitext' => array( array( 'model' => CONTENT_MODEL_WIKITEXT ) ),
+			'item' => array( array( 'model' => CONTENT_MODEL_WIKIBASE_ITEM ), 'MWException' ),
+		);
+	}
+
+	/**
+	 * @dataProvider revisionInfoProvider
+	 * @param $revisionInfo
+	 * @param null $expectedException
+	 */
+	public function testOnImportHandleRevisionXMLTag( $revisionInfo, $expectedException = null ) {
+		//NOTE: class is unclear, see Bug 64657. But we don't use that object anyway.
+		$importer = $this->getMockBuilder( 'Import' )
+			->disableOriginalConstructor()
+			->getMock();
+
+		if ( $expectedException !== null ) {
+			$this->setExpectedException( $expectedException );
+		}
+
+		RepoHooks::onImportHandleRevisionXMLTag( $importer, array(), $revisionInfo );
+		$this->assertTrue( true ); // make PHPUnit happy
+	}
+
+	private function getMockImportStream( $xml ) {
+		$source = $this->getMockBuilder( 'ImportStreamSource' )
+			->disableOriginalConstructor()
+			->getMock();
+
+		$atEnd = new \stdClass();
+		$atEnd->atEnd = false;
+
+		$source->expects( $this->any() )
+			->method( 'atEnd' )
+			->will( $this->returnCallback( function () use ( $atEnd ) {
+				return $atEnd->atEnd;
+			} ) );
+
+		$source->expects( $this->any() )
+			->method( 'readChunk' )
+			->will( $this->returnCallback( function () use ( $atEnd, $xml ) {
+				$atEnd->atEnd = true;
+				return $xml;
+			} ) );
+
+		return $source;
+	}
+
+	public function importProvider() {
+		return array(
+			'wikitext' => array( <<<XML
+<mediawiki>
+  <siteinfo>
+    <sitename>TestWiki</sitename>
+    <case>first-letter</case>
+  </siteinfo>
+  <page>
+    <title>Bla</title><ns>0</ns>
+    <revision>
+      <contributor><username>Tester</username><id>0</id></contributor>
+      <comment>Test</comment>
+      <text>Hallo Welt</text>
+      <model>wikitext</model>
+      <format>text/x-wiki</format>
+    </revision>
+  </page>
+ </mediawiki>
+XML
+			),
+			'item' => array( <<<XML
+<mediawiki>
+  <siteinfo>
+    <sitename>TestWiki</sitename>
+    <case>first-letter</case>
+  </siteinfo>
+  <page>
+    <title>Q123</title><ns>1234</ns>
+    <revision>
+      <contributor><username>Tester</username><id>0</id></contributor>
+      <comment>Test</comment>
+      <text>{ "id":"Q123" }</text>
+      <model>wikibase-item</model>
+      <format>application/json</format>
+    </revision>
+  </page>
+ </mediawiki>
+XML
+				,
+				'MWException'
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider importProvider
+	 * @param $xml
+	 * @param null $expectedException
+	 */
+	public function testImportHandleRevisionXMLTag_hook( $xml, $expectedException = null ) {
+		// WikiImporter tried to register this protocol every time, so unregister first to avoid errors.
+		wfSuppressWarnings();
+		stream_wrapper_unregister( 'uploadsource' );
+		wfRestoreWarnings();
+
+		$source = $this->getMockImportStream( $xml );
+		$importer = new WikiImporter( $source );
+
+		$importer->setNoticeCallback( function () {
+			// Do nothing for now. Could collect and compare notices.
+		} );
+
+		if ( $expectedException !== null ) {
+			$this->setExpectedException( $expectedException );
+		}
+
+		$importer->doImport();
+		$this->assertTrue( true ); // make PHPUnit happy
+	}
+
+}
