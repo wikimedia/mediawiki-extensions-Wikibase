bf7d30e92b8c08f3bbd1c6075c03970aad7df28b
daniel <daniel.kinzler@wikimedia.de>
2014-05-16 10:08:01 +0200

    Implement redirects in ItemContent
    This allows ItemContent to be instantiated based on an
    EntityRedirect, and allows WikiPageEntityLookup to load
    such ItemContent objects.
    
    This does not yet provide a way to create or resolve
    redirects in the the database.
    
    Bug: 57744
    Change-Id: If091211c1d1d9ab62dd04f23ec6df6cc5f3ffebb

--- repo/tests/phpunit/includes/content/EntityHandlerTest.php
+++ repo/tests/phpunit/includes/content/EntityHandlerTest.php
@@ -6,10 +6,11 @@ use ContentHandler;
 use Language;
 use Revision;
 use Title;
+use Wikibase\DataModel\Entity\EntityId;
 use Wikibase\Entity;
 use Wikibase\EntityContent;
-use Wikibase\EntityFactory;
 use Wikibase\EntityHandler;
+use Wikibase\Lib\Store\EntityRedirect;
 use Wikibase\Repo\WikibaseRepo;
 
 /**
@@ -48,11 +49,28 @@ abstract class EntityHandlerTest extends \MediaWikiTestCase {
 	}
 
 	/**
+	 * @param Entity $entity
+	 *
+	 * @return EntityContent
+	 */
+	protected abstract function newEntityContent( Entity $entity = null );
+
+	/**
+	 * @param EntityId $id
+	 * @param EntityId $target
+	 *
+	 * @return EntityContent
+	 */
+	protected function newRedirectContent( EntityId $id, EntityId $target ) {
+		$handler = $this->getHandler();
+		return $handler->makeEntityRedirectContent( new EntityRedirect( $id, $target ) );
+	}
+
+	/**
 	 * @return Entity
 	 */
 	protected function newEntity() {
-		$handler = $this->getHandler();
-		return EntityFactory::singleton()->newEmpty( $handler->getEntityType() );
+		return $this->newEntityContent()->getEntity();
 	}
 
 	/**
@@ -63,14 +81,14 @@ abstract class EntityHandlerTest extends \MediaWikiTestCase {
 		/**
 		 * @var EntityContent $content
 		 */
-		$content = $this->getHandler()->makeEmptyContent();
+		$content = $this->newEntityContent();
 		$content->getEntity()->addAliases( 'en', array( 'foo' ) );
 		$content->getEntity()->setDescription( 'de', 'foobar' );
 		$content->getEntity()->setDescription( 'en', 'baz' );
 		$content->getEntity()->setLabel( 'nl', 'o_O' );
 
 		return array(
-			array( $this->getHandler()->makeEmptyContent() ),
+			array( $this->newEntityContent() ),
 			array( $content ),
 		);
 	}
@@ -175,9 +193,7 @@ abstract class EntityHandlerTest extends \MediaWikiTestCase {
 		$this->assertNotEquals( $this->getModelId(), $name, "localization of model name" );
 	}
 
-	protected function fakeRevision( Entity $entity, $id = 0 ) {
-		$content = WikibaseRepo::getDefaultInstance()->getEntityContentFactory()->newFromEntity( $entity );
-
+	protected function fakeRevision( EntityContent $content, $id = 0 ) {
 		$revision = new Revision( array(
 			'id' => $id,
 			'page' => $id,
@@ -189,23 +205,23 @@ abstract class EntityHandlerTest extends \MediaWikiTestCase {
 
 	public function provideGetUndoContent() {
 		$e1 = $this->newEntity();
-		$r1 = $this->fakeRevision( $e1, 1 );
+		$r1 = $this->fakeRevision( $this->newEntityContent( $e1 ), 1 );
 
 		$e2 = $e1->copy();
 		$e2->setLabel( 'en', 'Foo' );
-		$r2 = $this->fakeRevision( $e2, 2 );
+		$r2 = $this->fakeRevision( $this->newEntityContent( $e2 ), 2 );
 
 		$e3 = $e2->copy();
 		$e3->setLabel( 'de', 'Fuh' );
-		$r3 = $this->fakeRevision( $e3, 3 );
+		$r3 = $this->fakeRevision( $this->newEntityContent( $e3 ), 3 );
 
 		$e4 = $e3->copy();
 		$e4->setLabel( 'fr', 'Fu' );
-		$r4 = $this->fakeRevision( $e4, 4 );
+		$r4 = $this->fakeRevision( $this->newEntityContent( $e4 ), 4 );
 
 		$e5 = $e4->copy();
 		$e5->setLabel( 'en', 'F00' );
-		$r5 = $this->fakeRevision( $e5, 5 );
+		$r5 = $this->fakeRevision( $this->newEntityContent( $e5 ), 5 );
 
 		$e5u4 = $e5->copy();
 		$e5u4->removeLabel( 'fr' );
@@ -214,11 +230,11 @@ abstract class EntityHandlerTest extends \MediaWikiTestCase {
 		$e5u4u3->removeLabel( 'de' );
 
 		return array(
-			array( $r5, $r5, $r4, $e4, "undo last edit" ),
-			array( $r5, $r4, $r3, $e5u4, "undo previous edit" ),
+			array( $r5, $r5, $r4, $this->newEntityContent( $e4 ), "undo last edit" ),
+			array( $r5, $r4, $r3, $this->newEntityContent( $e5u4 ), "undo previous edit" ),
 
-			array( $r5, $r5, $r3, $e3, "undo last two edits" ),
-			array( $r5, $r4, $r2, $e5u4u3, "undo past two edits" ),
+			array( $r5, $r5, $r3, $this->newEntityContent( $e3 ), "undo last two edits" ),
+			array( $r5, $r4, $r2, $this->newEntityContent( $e5u4u3 ), "undo past two edits" ),
 
 			array( $r5, $r2, $r1, null, "undo conflicting edit" ),
 			array( $r5, $r3, $r1, null, "undo two edits with conflict" ),
@@ -231,22 +247,23 @@ abstract class EntityHandlerTest extends \MediaWikiTestCase {
 	 * @param Revision $latestRevision
 	 * @param Revision $newerRevision
 	 * @param Revision $olderRevision
-	 * @param Entity $expected
+	 * @param EntityContent $expected
 	 * @param string $message
 	 */
 	public function testGetUndoContent(
 		Revision $latestRevision,
 		Revision $newerRevision,
 		Revision $olderRevision,
-		Entity $expected = null,
-		$message ) {
+		EntityContent $expected = null,
+		$message
+	) {
 
 		$handler = $this->getHandler();
 		$undo = $handler->getUndoContent( $latestRevision, $newerRevision, $olderRevision );
 
 		if ( $expected ) {
 			$this->assertInstanceOf( 'Wikibase\EntityContent', $undo, $message );
-			$this->assertEquals( $expected->toArray(), $undo->getEntity()->toArray(), $message );
+			$this->assertEquals( $expected->getNativeData(), $undo->getNativeData(), $message );
 		} else {
 			$this->assertFalse( $undo, $message );
 		}
@@ -254,7 +271,7 @@ abstract class EntityHandlerTest extends \MediaWikiTestCase {
 
 	public function testGetEntityType() {
 		$handler = $this->getHandler();
-		$content = $handler->makeEmptyContent();
+		$content = $this->newEntityContent();
 		$entity = $content->getEntity();
 
 		$this->assertEquals( $entity->getType(), $handler->getEntityType() );
@@ -271,10 +288,39 @@ abstract class EntityHandlerTest extends \MediaWikiTestCase {
 	}
 
 	public function testMakeEmptyContent() {
-		$handler = $this->getHandler();
-		$content = $handler->makeEmptyContent();
+		// We don't support empty content.
+		$this->setExpectedException( 'MWException' );
 
-		$this->assertEquals( $this->getModelId(), $content->getModel() );
+		$handler = $this->getHandler();
+		$handler->makeEmptyContent();
+	}
+
+	public function testMakeRedirectContent() {
+		// We don't support title based redirects.
+		$this->setExpectedException( 'MWException' );
+
+		$handler = $this->getHandler();
+		$handler->makeRedirectContent( Title::newFromText( 'X11', $handler->getEntityNamespace() ) );
+	}
+
+	public function testMakeEmptyEntity() {
+		$handler = $this->getHandler();
+		$entity = $handler->makeEmptyEntity();
+
+		$this->assertEquals( $handler->getEntityType(), $entity->getType(), 'entity type' );
+	}
+
+	public abstract function entityIdProvider();
+
+	/**
+	 * @dataProvider entityIdProvider
+	 */
+	public function testMakeEntityId( $idString ) {
+		$handler = $this->getHandler();
+		$id = $handler->makeEntityId( $idString );
+
+		$this->assertEquals( $handler->getEntityType(), $id->getEntityType() );
+		$this->assertEquals( $idString, $id->getSerialization() );
 	}
 
 }
