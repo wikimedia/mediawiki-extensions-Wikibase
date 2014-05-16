f16fa82603d47f24d1e51e7ea59119c05f928ba4
daniel <daniel.kinzler@wikimedia.de>
2014-05-16 10:08:01 +0200

    Implement redirects in ItemContent
    This allows ItemContent to be instantiated based on an
    EntityRedirect, and allows WikiPageEntityLookup to load
    such ItemContent objects.
    
    This does not yet provide a way to create or resolve
    redirects in the database.
    
    Bug: 57744
    Change-Id: If091211c1d1d9ab62dd04f23ec6df6cc5f3ffebb

--- repo/tests/phpunit/includes/content/EntityContentTest.php
+++ repo/tests/phpunit/includes/content/EntityContentTest.php
@@ -10,8 +10,10 @@ use RequestContext;
 use Title;
 use Wikibase\DataModel\Claim\Statement;
 use Wikibase\DataModel\Entity\Entity;
+use Wikibase\Lib\Store\EntityRedirect;
 use Wikibase\DataModel\Entity\EntityDiff;
 use Wikibase\DataModel\Snak\PropertyNoValueSnak;
+use Wikibase\DataModel\Entity\EntityId;
 use Wikibase\EntityContent;
 use Wikibase\LanguageFallbackChain;
 use Wikibase\LanguageWithConversion;
@@ -77,6 +79,11 @@ abstract class EntityContentTest extends \MediaWikiTestCase {
 	protected abstract function getContentClass();
 
 	/**
+	 * @return EntityId
+	 */
+	protected abstract function getDummyId();
+
+	/**
 	 * @param array $data
 	 *
 	 * @return EntityContent
@@ -89,11 +96,34 @@ abstract class EntityContentTest extends \MediaWikiTestCase {
 	}
 
 	/**
+	 * @param EntityId $id
+	 *
 	 * @return EntityContent
 	 */
-	protected function newEmpty() {
+	protected function newEmpty( EntityId $id = null ) {
 		$class = $this->getContentClass();
-		return $class::newEmpty();
+		$empty = $class::newEmpty();
+
+		if ( $id ) {
+			$empty->getEntity()->setId( $id );
+		}
+
+		return $empty;
+	}
+
+	/**
+	 * @param EntityId $id
+	 * @param EntityId $target
+	 *
+	 * @return EntityContent
+	 */
+	protected function newRedirect( EntityId $id, EntityId $target ) {
+		//FIXME: use the respective EntityHandler instead of going via the global title lookup!
+		$titleLookup = WikibaseRepo::getDefaultInstance()->getEntityTitleLookup();
+		$title = $titleLookup->getTitleForId( $target );
+
+		$class = $this->getContentClass();
+		return $class::newFromRedirect( new EntityRedirect( $id, $target ), $title );
 	}
 
 	/**
@@ -220,6 +250,17 @@ abstract class EntityContentTest extends \MediaWikiTestCase {
 		$this->assertEquals( $status, $actual );
 	}
 
+	public abstract function provideGetEntityId();
+
+	/**
+	 * @dataProvider provideGetEntityId
+	 */
+	public function testGetEntityId( EntityContent $content, $expected ) {
+		$actual = $content->getEntityId();
+
+		$this->assertEquals( $expected, $actual );
+	}
+
 	public function provideGetEntityPageProperties() {
 		return array(
 			'empty' => array(
@@ -303,12 +344,12 @@ abstract class EntityContentTest extends \MediaWikiTestCase {
 	}
 
 	public function diffProvider() {
-		$empty = $this->newEmpty();
+		$empty = $this->newEmpty( $this->getDummyId() );
 
-		$spam = $this->newEmpty();
+		$spam = $this->newEmpty( $this->getDummyId() );
 		$spam->getEntity()->setLabel( 'en', 'Spam' );
 
-		$ham = $this->newEmpty();
+		$ham = $this->newEmpty( $this->getDummyId() );
 		$ham->getEntity()->setLabel( 'en', 'Ham' );
 
 		$spamToHam = new DiffOpChange( 'Spam', 'Ham' );
@@ -333,22 +374,29 @@ abstract class EntityContentTest extends \MediaWikiTestCase {
 	public function testGetDiff( EntityContent $a, EntityContent $b, EntityContentDiff $expected ) {
 		$actual = $a->getDiff( $b );
 
-		$expectedOps = $expected->getOperations();
-		$actualOps = $actual->getOperations();
+		$this->assertInstanceOf( 'Wikibase\Repo\Content\EntityContentDiff', $actual );
+
+		$expectedEntityOps = $expected->getEntityDiff()->getOperations();
+		$actualEntityOps = $actual->getEntityDiff()->getOperations();
 
 		// HACK: ItemDiff always sets this, even if it's empty. Ignore.
-		if ( isset( $actualOps['claim'] ) && $actualOps['claim']->isEmpty() ) {
-			unset( $actualOps['claim'] );
+		if ( isset( $actualEntityOps['claim'] ) && $actualEntityOps['claim']->isEmpty() ) {
+			unset( $actualEntityOps['claim'] );
 		}
 
-		$this->assertArrayEquals( $expectedOps, $actualOps, true );
+		$this->assertArrayEquals( $expectedEntityOps, $actualEntityOps, false, true );
+
+		$expectedRedirectOps = $expected->getRedirectDiff()->getOperations();
+		$actualRedirectOps = $actual->getRedirectDiff()->getOperations();
+
+		$this->assertArrayEquals( $expectedRedirectOps, $actualRedirectOps, false, true );
 	}
 
 	public function patchedCopyProvider() {
-		$spam = $this->newEmpty();
+		$spam = $this->newEmpty( $this->getDummyId() );
 		$spam->getEntity()->setLabel( 'en', 'Spam' );
 
-		$ham = $this->newEmpty();
+		$ham = $this->newEmpty( $this->getDummyId() );
 		$ham->getEntity()->setLabel( 'en', 'Ham' );
 
 		$spamToHam = new DiffOpChange( 'Spam', 'Ham' );
@@ -369,10 +417,69 @@ abstract class EntityContentTest extends \MediaWikiTestCase {
 	 * @param EntityContentDiff $patch
 	 * @param EntityContent $expected
 	 */
-	public function testGetPatchedCopy( EntityContent $base, EntityContentDiff $patch, EntityContent $expected ) {
+	public function testGetPatchedCopy( EntityContent $base, EntityContentDiff $patch, EntityContent $expected = null ) {
+		if ( $expected === null ) {
+			$this->setExpectedException( 'Diff\Patcher\PatcherException' );
+		}
+
 		$actual = $base->getPatchedCopy( $patch );
 
-		$this->assertTrue( $expected->equals( $actual ), 'equals()' );
+		if ( $expected !== null ) {
+			$this->assertTrue( $expected->equals( $actual ), 'equals()' );
+		}
+	}
+
+	public function copyProvider() {
+		$empty = $this->newEmpty();
+		$labels = $this->newEmpty();
+
+		$labels->getEntity()->setLabel( 'en', 'Foo' );
+
+		return array(
+			'empty' => array( $empty ),
+			'labels' => array( $labels ),
+		);
+	}
+
+	/**
+	 * @dataProvider copyProvider
+	 * @param EntityContent $content
+	 */
+	public function testCopy( EntityContent $content ) {
+		$copy = $content->copy();
+		$this->assertNotSame( $content, $copy, 'Copy must not be the same instance.' );
+		$this->assertTrue( $content->equals( $copy ), 'Copy must be equal to the original.' );
+		$this->assertSame( get_class( $content ), get_class( $copy ), 'Copy must have the same type.' );
+		$this->assertEquals( $content->getNativeData(), $copy->getNativeData(), 'Copy must have the same data.' );
+	}
+
+
+	public function equalsProvider() {
+		$empty = $this->newEmpty();
+
+		$labels1 = $this->newEmpty();
+		$labels1->getEntity()->setLabel( 'en', 'Foo' );
+
+		$labels2 = $this->newEmpty();
+		$labels2->getEntity()->setLabel( 'de', 'Foo' );
+
+		return array(
+			'empty' => array( $empty, $empty, true ),
+			'same labels' => array( $labels1, $labels1, true ),
+			'different labels' => array( $labels1, $labels2, false ),
+		);
+	}
+
+	/**
+	 * @dataProvider equalsProvider
+	 */
+	public function testEquals( EntityContent $a, EntityContent $b, $equals ) {
+
+		$actual = $a->equals( $b );
+		$this->assertEquals( $equals, $actual );
+
+		$actual = $b->equals( $a );
+		$this->assertEquals( $equals, $actual );
 	}
 
 	private function createTitleForEntity( Entity $entity ) {
@@ -420,4 +527,22 @@ abstract class EntityContentTest extends \MediaWikiTestCase {
 		$this->assertContainsOnlyInstancesOf( 'DataUpdate', $updates );
 	}
 
+	public function entityRedirectProvider() {
+		return array(
+			'empty' => array( $this->newEmpty(), null ),
+		);
+	}
+
+	/**
+	 * @dataProvider entityRedirectProvider
+	 */
+	public function testGetEntityRedirect( EntityContent $content, EntityRedirect $redirect = null ) {
+		$this->assertEquals( $content->getEntityRedirect(), $redirect );
+
+		if ( $redirect === null ) {
+			$this->assertNull( $content->getRedirectTarget() );
+		} else {
+			$this->assertNotNull( $content->getRedirectTarget() );
+		}
+	}
 }
