6363afa04218b9f1fecf5b0e6d9ed4e530d955e3
daniel <daniel.kinzler@wikimedia.de>
2014-05-16 10:08:01 +0200

    Implement redirects in ItemContent
    This allows ItemContent to be instantiated based on an
    EntityRedirect, and allows WikiPageEntityLookup to load
    such ItemContent objects.
    
    This does not yet provide a way to create or resolve
    redirects in the database.
    
    Bug: 57744
    Change-Id: If091211c1d1d9ab62dd04f23ec6df6cc5f3ffebb

--- lib/includes/store/EntityContentDataCodec.php
+++ lib/includes/store/EntityContentDataCodec.php
@@ -3,6 +3,7 @@
 namespace Wikibase\Lib\Store;
 
 use Deserializers\Deserializer;
+use Deserializers\Exceptions\DeserializationException;
 use InvalidArgumentException;
 use MWContentSerializationException;
 use Serializers\Serializer;
@@ -139,6 +140,28 @@ class EntityContentDataCodec {
 	}
 
 	/**
+	 * Encodes an EntityRedirect into a blob for storage.
+	 *
+	 * @see Entity::toArray()
+	 * @see EntityHandler::serializeContent()
+	 *
+	 * @param EntityRedirect $redirect
+	 * @param string|null $format The desired serialization format.
+	 *
+	 * @throws InvalidArgumentException If the format is not supported.
+	 * @throws MWContentSerializationException If the array could not be encoded.
+	 * @return string A blob representing the given Entity.
+	 */
+	public function encodeRedirect( EntityRedirect $redirect, $format ) {
+		$data = array(
+			'entity' => $redirect->getEntityId()->getSerialization(),
+			'redirect' => $redirect->getTargetId()->getSerialization(),
+		);
+
+		return $this->encodeEntityContentData( $data, $format );
+	}
+
+	/**
 	 * Decodes the given blob into an array structure representing an EntityContent
 	 * object.
 	 *
@@ -186,7 +209,8 @@ class EntityContentDataCodec {
 	 * @param string|null $format The serialization format of $blob.
 	 *
 	 * @throws InvalidArgumentException If the format is not supported.
-	 * @throws MWContentSerializationException If the array could not be decoded.
+	 * @throws MWContentSerializationException If the array could not be decoded
+	 *
 	 * @return Entity|null The Entity represented by $blob, or null if $blob represents a redirect.
 	 */
 	public function decodeEntity( $blob, $format ) {
@@ -197,8 +221,51 @@ class EntityContentDataCodec {
 			return null;
 		}
 
-		$entity = $this->entityDeserializer->deserialize( $data );
-		return $entity;
+		try {
+			$entity = $this->entityDeserializer->deserialize( $data );
+			return $entity;
+		}
+		catch ( DeserializationException $ex ) {
+			throw new MWContentSerializationException( $ex->getMessage(), 0, $ex );
+		}
+	}
+
+	/**
+	 * Decodes a blob loaded from storage into an EntityRedirect.
+	 *
+	 * @see Entity::toArray()
+	 * @see EntityHandler::serializeContent()
+	 *
+	 * @param string $blob
+	 * @param string|null $format The serialization format of $blob.
+	 *
+	 * @throws InvalidArgumentException If the format is not supported.
+	 * @throws MWContentSerializationException If the array could not be decoded.
+	 * @return EntityRedirect|null The EntityRedirect represented by $blob,
+	 *         or null if $blob does not represent a redirect.
+	 */
+	public function decodeRedirect( $blob, $format ) {
+		$data = $this->decodeEntityContentData( $blob, $format );
+
+		$targetId = $this->extractEntityId( $data, 'redirect' );
+
+		if ( !$targetId ) {
+			// If it's not a redirect, return null.
+			return null;
+		}
+
+		$entityId = $this->extractEntityId( $data, 'entity' );
+
+		if ( !$entityId ) {
+			throw new MWContentSerializationException( 'No entity ID found in serialization data!' );
+		}
+
+		try {
+			$redirect = new EntityRedirect( $entityId, $targetId );
+			return $redirect;
+		} catch ( InvalidArgumentException $ex ) {
+			throw new MWContentSerializationException( $ex->getMessage(), 0, $ex );
+		}
 	}
 
 	/**
---
+++ lib/tests/phpunit/store/EntityRedirectTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Wikibase\Lib\Test\Store;
+
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\Lib\Store\EntityRedirect;
+
+/**
+ * @covers Wikibase\Lib\Store\EntityRedirect
+ *
+ * @group WikibaseRepo
+ * @group Wikibase
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ */
+class EntityRedirectTest extends \PHPUnit_Framework_TestCase {
+
+	public function testConstruction() {
+		$entityId = new ItemId( 'Q123' );
+		$targetId = new ItemId( 'Q345' );
+
+		$redirect = new EntityRedirect( $entityId, $targetId );
+
+		$this->assertEquals( $entityId, $redirect->getEntityId(), '$redirect->getEntityId()' );
+		$this->assertEquals( $targetId, $redirect->getTargetId(), '$redirect->getTargetId()' );
+	}
+
+	public function testConstruction_baseType() {
+		$this->setExpectedException( 'InvalidArgumentException' );
+
+		$entityId = new ItemId( 'Q123' );
+		$targetId = new PropertyId( 'P345' );
+
+		new EntityRedirect( $entityId, $targetId );
+	}
+
+	public function equalsProvider() {
+		$q123 = new ItemId( 'Q123' );
+		$q345 = new ItemId( 'Q345' );
+		$q567 = new ItemId( 'Q567' );
+		$q123_345 = new EntityRedirect( $q123, $q345 );
+
+		$p123 = new PropertyId( 'P123' );
+		$p345 = new PropertyId( 'P345' );
+		$p123_345 = new EntityRedirect( $p123, $p345 );
+
+		return array(
+			'same' => array( $q123_345, $q123_345, true ),
+			'equal' => array( $q123_345, new EntityRedirect( $q123, $q345 ), true ),
+
+			'different base' => array( $q123_345, new EntityRedirect( $q567, $q345 ), false ),
+			'different target' => array( $q123_345, new EntityRedirect( $q123, $q567 ), false ),
+
+			'different entity type' => array( $q123_345, $p123_345, false ),
+			'different number' => array( $q123_345, new EntityRedirect( $q345, $q123 ), false ),
+
+			'null' => array( $q123_345, null, false ),
+			'string' => array( $q123_345, 'foo', false ),
+			'id' => array( $q123_345, $q123, false ),
+		);
+	}
+
+	/**
+	 * @dataProvider equalsProvider
+	 *
+	 * @param EntityRedirect $a
+	 * @param mixed $b
+	 * @param bool $expected
+	 */
+	public function testEquals( $a, $b, $expected ) {
+		$this->assertEquals( $expected, $a->equals( $b ), '$a->equals( $b )' );
+
+		if ( $b instanceof EntityRedirect ) {
+			$this->assertEquals( $expected, $b->equals( $a ), '$b->equals( $a )' );
+		}
+	}
+
+}
