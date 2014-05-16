05cef6d42afac15008bd1106c9b6c894e03367e8
daniel <daniel.kinzler@wikimedia.de>
2014-05-16 10:08:01 +0200

    Implement redirects in ItemContent
    *** INCOMPLTE ***
    - everythign that calls getEntity or getItem
    - test EntityChange notification on transition from/to redirect this!
    - FIX undo (-> EditEntity)
    
    Bug: 57744
    Change-Id: If091211c1d1d9ab62dd04f23ec6df6cc5f3ffebb

--- lib/includes/store/EntityContentDataCodec.php
+++ lib/includes/store/EntityContentDataCodec.php
@@ -4,6 +4,11 @@ namespace Wikibase\Lib\Store;
 
 use InvalidArgumentException;
 use MWContentSerializationException;
+use Wikibase\DataModel\Entity\Entity;
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\DataModel\Entity\EntityIdParser;
+use Wikibase\DataModel\Internal\LegacyIdInterpreter;
+use Wikibase\EntityFactory;
 
 /**
  * A codec for use by EntityContent resp EntityHandler subclasses for the
@@ -21,6 +26,25 @@ use MWContentSerializationException;
  * @author Daniel Kinzler
  */
 class EntityContentDataCodec {
+
+	/**
+	 * @var EntityIdParser
+	 */
+	private $entityIdParser;
+
+	/**
+	 * @var EntityFactory
+	 */
+	private $entityFactory;
+
+	/**
+	 * @param EntityIdParser $entityIdParser
+	 * @param EntityFactory $entityFactory
+	 */
+	function __construct( EntityIdParser $entityIdParser, EntityFactory $entityFactory ) {
+		$this->entityIdParser = $entityIdParser;
+		$this->entityFactory = $entityFactory;
+	}
 
 	/**
 	 * Returns the supported serialization formats as a list of strings.
@@ -58,17 +82,14 @@ class EntityContentDataCodec {
 	/**
 	 * Encodes the given array structure as a blob using the given serialization format.
 	 *
-	 * @see EntityContent::toArray()
-	 * @see EntityHandler::serializeContent()
-	 *
-	 * @param array $data A nested data array representing an EntityContent object.
+	 * @param array $data A nested data array representing (part of) an EntityContent object.
 	 * @param string|null $format The desired serialization format.
 	 *
 	 * @throws InvalidArgumentException If the format is not supported.
 	 * @throws MWContentSerializationException If the array could not be encoded.
 	 * @return string the blob
 	 */
-	public function encodeEntityContentData( array $data, $format ) {
+	private function encodeEntityContentData( array $data, $format ) {
 		switch ( $this->sanitizeFormat( $format ) ) {
 			case CONTENT_FORMAT_JSON:
 				$blob = json_encode( $data );
@@ -88,6 +109,46 @@ class EntityContentDataCodec {
 	}
 
 	/**
+	 * Encodes an Entity into a blob for storage.
+	 *
+	 * @see Entity::toArray()
+	 * @see EntityHandler::serializeContent()
+	 *
+	 * @param Entity $entity
+	 * @param string|null $format The desired serialization format.
+	 *
+	 * @throws InvalidArgumentException If the format is not supported.
+	 * @throws MWContentSerializationException If the array could not be encoded.
+	 * @return string A blob representing the given Entity.
+	 */
+	public function encodeEntity( Entity $entity, $format ) {
+		$data = $entity->toArray();
+		return $this->encodeEntityContentData( $data, $format );
+	}
+
+	/**
+	 * Encodes an EntityRedirect into a blob for storage.
+	 *
+	 * @see Entity::toArray()
+	 * @see EntityHandler::serializeContent()
+	 *
+	 * @param EntityRedirect $redirect
+	 * @param string|null $format The desired serialization format.
+	 *
+	 * @throws InvalidArgumentException If the format is not supported.
+	 * @throws MWContentSerializationException If the array could not be encoded.
+	 * @return string A blob representing the given Entity.
+	 */
+	public function encodeRedirect( EntityRedirect $redirect, $format ) {
+		$data = array(
+			'entity' => $redirect->getEntityId()->getSerialization(),
+			'redirect' => $redirect->getTargetId()->getSerialization(),
+		);
+
+		return $this->encodeEntityContentData( $data, $format );
+	}
+
+	/**
 	 * Decodes the given blob into an array structure representing an EntityContent
 	 * object.
 	 *
@@ -100,7 +161,7 @@ class EntityContentDataCodec {
 	 * @throws MWContentSerializationException If the blob could not be decoded.
 	 * @return array An array representation of an EntityContent object
 	 */
-	public function decodeEntityContentData( $blob, $format ) {
+	private function decodeEntityContentData( $blob, $format ) {
 		if ( !is_string( $blob ) ) {
 			throw new InvalidArgumentException( '$blob must be a string' );
 		}
@@ -123,6 +184,96 @@ class EntityContentDataCodec {
 		}
 
 		return $data;
+	}
+
+	/**
+	 * Decodes a blob loaded from storage into an Entity.
+	 *
+	 * @see Entity::toArray()
+	 * @see EntityHandler::serializeContent()
+	 *
+	 * @param string $blob
+	 * @param string|null $format The serialization format of $blob.
+	 *
+	 * @throws InvalidArgumentException If the format is not supported.
+	 * @throws MWContentSerializationException If the array could not be decoded.
+	 * @return Entity|null The Entity represented by $blob, or null if $blob represents a redirect.
+	 */
+	public function decodeEntity( $blob, $format ) {
+		$data = $this->decodeEntityContentData( $blob, $format );
+
+		if ( $this->extractEntityId( $data, 'redirect' ) ) {
+			// If it's a redirect, return null.
+			return null;
+		}
+
+		$entityId = $this->extractEntityId( $data, 'entity' );
+
+		if ( !$entityId ) {
+			throw new MWContentSerializationException( 'No entity ID found in serialization data!' );
+		}
+
+		$entity = $this->entityFactory->newFromArray( $entityId->getEntityType(), $data );
+		return $entity;
+	}
+
+	/**
+	 * Decodes a blob loaded from storage into an EntityRedirect.
+	 *
+	 * @see Entity::toArray()
+	 * @see EntityHandler::serializeContent()
+	 *
+	 * @param string $blob
+	 * @param string|null $format The serialization format of $blob.
+	 *
+	 * @throws InvalidArgumentException If the format is not supported.
+	 * @throws MWContentSerializationException If the array could not be decoded.
+	 * @return EntityRedirect|null The EntityRedirect represented by $blob,
+	 *         or null if $blob does not represent a redirect.
+	 */
+	public function decodeRedirect( $blob, $format ) {
+		$data = $this->decodeEntityContentData( $blob, $format );
+
+		$targetId = $this->extractEntityId( $data, 'redirect' );
+
+		if ( !$targetId ) {
+			// If it's not a redirect, return null.
+			return null;
+		}
+
+		$entityId = $this->extractEntityId( $data, 'entity' );
+
+		if ( !$entityId ) {
+			throw new MWContentSerializationException( 'No entity ID found in serialization data!' );
+		}
+
+		try {
+			$redirect = new EntityRedirect( $entityId, $targetId );
+			return $redirect;
+		} catch ( InvalidArgumentException $ex ) {
+			throw new MWContentSerializationException( $ex->getMessage(), 0, $ex );
+		}
+	}
+
+	/**
+	 * @param array $data An array representation of an EntityContent object.
+	 * @param string $key The key in $data that contains the serialized ID.
+	 *
+	 * @return EntityId|null The ID of the entity (resp. redirect), or null if
+	 *         $key is not set in $data.
+	 */
+	private function extractEntityId( array $data, $key ) {
+		if ( !isset( $data[$key] ) ) {
+			return null;
+		}
+
+		if ( is_array( $data[$key] ) ) {
+			// Handle the old-style representation of IDs as a two element array.
+			$stubbedId = $data[$key];
+			return LegacyIdInterpreter::newIdFromTypeAndNumber( $stubbedId[0], $stubbedId[1] );
+		}
+
+		return $this->entityIdParser->parse( $data[$key] );
 	}
 
 }
---
+++ repo/includes/content/UnresolvedRedirectException.php
@@ -0,0 +1,45 @@
+<?php
+namespace Wikibase;
+
+use Exception;
+
+/**
+ * Exception indicating that an attempt was made to access a redirected EntityId
+ * without resolving the redirect first.
+ *
+ * @license GPL 2+
+ * @author Daniel Kinzler
+ */
+class UnresolvedRedirectException extends StorageException {
+
+	/**
+	 * @var EntityId
+	 */
+	protected $redirectTargetId;
+
+	/**
+	 * @param EntityId $redirectTargetId The ID of the target Entity of the redirect
+	 * @param string|null $message
+	 * @param int $code
+	 * @param Exception $previous
+	 */
+	public function __construct( EntityId $redirectTargetId, $message = null, $code = 0, Exception $previous = null ) {
+		if ( $message === null ) {
+			$message = "Unresolved redirect to " . $redirectTargetId->getSerialization();
+		}
+
+		parent::__construct( $message, $code, $previous );
+
+		$this->redirectTargetId = $redirectTargetId;
+	}
+
+	/**
+	 * Returns the ID of the entity referenced by the redirect.
+	 *
+	 * @return EntityId
+	 */
+	public function getRedirectTargetId() {
+		return $this->redirectTargetId;
+	}
+}
+ 
