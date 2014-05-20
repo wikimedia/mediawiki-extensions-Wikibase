42f06498e971e7712b07e426fa9aa467620f47f4
daniel <daniel.kinzler@wikimedia.de>
2014-05-20 13:17:42 +0200

    Remove StoreFactory and related logic
    The factory/registry logic for different kinds of storage backends
    was overly complex and disfunctional.
    As we don't need this right now, I removed it,
    for now hardcoding which store is used by client and repo.
    
    Change-Id: I0319cb28e8d04d6ee589aadaccd3c90d886d658f

--- client/WikibaseClient.hooks.php
+++ client/WikibaseClient.hooks.php
@@ -19,10 +19,10 @@ use RecentChange;
 use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
 use RuntimeException;
-use SpecialWatchlist;
-use SplFileInfo;
 use Skin;
 use SpecialRecentChanges;
+use SpecialWatchlist;
+use SplFileInfo;
 use StripState;
 use Title;
 use UnexpectedValueException;
@@ -30,11 +30,10 @@ use User;
 use Wikibase\Client\Hooks\BaseTemplateAfterPortletHandler;
 use Wikibase\Client\Hooks\BeforePageDisplayHandler;
 use Wikibase\Client\Hooks\InfoActionHookHandler;
+use Wikibase\Client\Hooks\LanguageLinkBadgeDisplay;
+use Wikibase\Client\Hooks\OtherProjectsSidebarGenerator;
 use Wikibase\Client\Hooks\SpecialWatchlistQueryHandler;
 use Wikibase\Client\MovePageNotice;
-use Wikibase\Client\Hooks\OtherProjectsSidebarGenerator;
-use Wikibase\Client\Hooks\LanguageLinkBadgeDisplay;
-use Wikibase\Client\ClientSiteLinkLookup;
 use Wikibase\Client\WikibaseClient;
 
 /**
@@ -105,9 +104,8 @@ final class ClientHooks {
 		wfProfileIn( __METHOD__ );
 
 		$store = WikibaseClient::getDefaultInstance()->getStore();
-		$stores = array_flip( $GLOBALS['wgWBClientStores'] );
 
-		$reportMessage( "Deleting data from the " . $stores[get_class( $store )] . " store..." );
+		$reportMessage( "Deleting data from the " . get_class( $store ) . " store..." );
 
 		$store->clear();
 
@@ -140,8 +138,7 @@ final class ClientHooks {
 		wfProfileIn( __METHOD__ );
 
 		$store = WikibaseClient::getDefaultInstance()->getStore();
-		$stores = array_flip( $GLOBALS['wgWBClientStores'] );
-		$reportMessage( "Rebuilding all data in the " . $stores[get_class( $store )]
+		$reportMessage( "Rebuilding all data in the " . get_class( $store )
 			. " store on the client..." );
 		$store->rebuild();
 		$changes = ChangesTable::singleton();
