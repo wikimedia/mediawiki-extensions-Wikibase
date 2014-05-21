3b454ef6618a87c8ccd2b377efc0edfc35f653ec
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2014-05-21 11:15:13 +0200

    [DNM] Documentation for Data Type to Data Value Type mapping
    Merge all the other patches first! I will rebase this if needed.
    
    Change-Id: I8caed8af113b3c6a748548f974ebd7cee2edd951

--- lib/includes/WikibaseDataTypeBuilders.php
+++ lib/includes/WikibaseDataTypeBuilders.php
@@ -71,14 +71,27 @@ class WikibaseDataTypeBuilders {
 		//     the dataTypes setting. On the other hand, perhaps that setting should only
 		//     be used for the UI, and the factory should simply know all data types always.
 
+		/**
+		 * Data types to data value types mapping (also see wgDataTypes in DataTypes.php):
+		 * commonsMedia     => string (camel case, FIXME maybe?)
+		 * globe-coordinate => globecoordinate (FIXME!)
+		 * quantity         => quantity
+		 * string           => string
+		 * time             => time
+		 * url              => string
+		 * wikibase-item    => wikibase-entityid
+		 * monolingualtext  => monolingualtext
+		 * multilingualtext => multilingualtext
+		 */
+
 		$types = array(
-			'commonsMedia' => array( $this, 'buildMediaType' ),
+			'commonsMedia'     => array( $this, 'buildMediaType' ),
 			'globe-coordinate' => array( $this, 'buildCoordinateType' ),
-			'quantity'=> array( $this, 'buildQuantityType' ),
-			'string' => array( $this, 'buildStringType' ),
-			'time' => array( $this, 'buildTimeType' ),
-			'url' => array( $this, 'buildUrlType' ),
-			'wikibase-item' => array( $this, 'buildItemType' ),
+			'quantity'         => array( $this, 'buildQuantityType' ),
+			'string'           => array( $this, 'buildStringType' ),
+			'time'             => array( $this, 'buildTimeType' ),
+			'url'              => array( $this, 'buildUrlType' ),
+			'wikibase-item'    => array( $this, 'buildItemType' ),
 		);
 
 		$experimental = array(
