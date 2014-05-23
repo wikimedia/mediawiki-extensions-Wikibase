1f17a9536ea143abd3b3f727f7098a711ac99f5f
daniel <daniel.kinzler@wikimedia.de>
2014-05-23 17:09:54 +0200

    Introducing EntityContentDiff
    EntityContentDiff handles redirects if present,
    and uses EntityDiff otherwise.
    
    Change-Id: I0d8798cf18bf8375af9813d446862f5d56fb1b66

---
+++ repo/tests/phpunit/includes/content/EntityContentDiffTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Wikibase\Test;
+
+use Diff\DiffOp\Diff\Diff;
+use Diff\DiffOp\DiffOpAdd;
+use Diff\DiffOp\DiffOpRemove;
+use MediaWikiTestCase;
+use Wikibase\Repo\Content\EntityContentDiff;
+use Wikibase\DataModel\Entity\EntityDiff;
+
+/**
+ * @covers Wikibase\EntityContentDiff
+ *
+ * @group WikibaseRepo
+ * @group Wikibase
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ */
+class EntityContentDiffTest extends MediaWikiTestCase {
+
+	public function provideConstruction() {
+		return array(
+			'empty' => array(
+				new EntityDiff(),
+				new Diff()
+			),
+			'entity diff' => array(
+				new EntityDiff( array(
+					'label' => new Diff( array(
+						'en' => new DiffOpAdd( 'Spam' ),
+					) )
+				) ),
+				new Diff()
+			),
+			'redirect diff' => array(
+				new EntityDiff(),
+				new Diff( array(
+					'redirect' => new DiffOpAdd( 'Spam' ),
+				) )
+			),
+			'entity and redirect diff' => array(
+				new EntityDiff( array(
+					'label' => new Diff( array(
+							'en' => new DiffOpAdd( 'Spam' ),
+						) )
+				) ),
+				new Diff( array(
+					'redirect' => new DiffOpRemove( 'Spam' ),
+				) )
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider provideConstruction
+	 *
+	 * @param EntityDiff $entityDiff
+	 * @param Diff $redirectDiff
+	 */
+	public function testConstruction( EntityDiff $entityDiff, Diff $redirectDiff ) {
+		$diff = new EntityContentDiff( $entityDiff, $redirectDiff );
+
+		$this->assertArrayEquals(
+			$entityDiff->getOperations(),
+			$diff->getEntityDiff()->getOperations()
+		);
+		$this->assertEmpty( array_diff(
+			array_keys( $entityDiff->getOperations() ),
+			array_keys( $diff->getEntityDiff()->getOperations() )
+		) );
+
+		$this->assertArrayEquals(
+			$redirectDiff->getOperations(),
+			$diff->getRedirectDiff()->getOperations()
+		);
+		$this->assertEmpty( array_diff(
+			array_keys( $redirectDiff->getOperations() ),
+			array_keys( $diff->getRedirectDiff()->getOperations() )
+		) );
+	}
+
+}
