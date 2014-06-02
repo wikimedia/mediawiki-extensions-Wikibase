9dcd17c3ce49e29bf6d81399bca3ae7a53f899e2
jeroendedauw <jeroendedauw@gmail.com>
2014-06-02 19:31:59 +0200

    Update refs to Diff classes
    Change-Id: I97249519a936233a547dcc8f2ea3d2af3f702d93

--- client/includes/ReferencedPagesFinder.php
+++ client/includes/ReferencedPagesFinder.php
@@ -2,10 +2,10 @@
 
 namespace Wikibase;
 
-use Diff\Diff;
-use Diff\DiffOpAdd;
-use Diff\DiffOpChange;
-use Diff\DiffOpRemove;
+use Diff\DiffOp\Diff\Diff;
+use Diff\DiffOp\DiffOpAdd;
+use Diff\DiffOp\DiffOpChange;
+use Diff\DiffOp\DiffOpRemove;
 use Title;
 use UnexpectedValueException;
 
