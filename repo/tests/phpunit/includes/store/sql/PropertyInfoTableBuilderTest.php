a66dd168b6bab91f8dffb4db797f459174e66a94
daniel <daniel.kinzler@wikimedia.de>
2014-06-11 11:01:42 +0200

    Use uncached EntityLookup during testin
    Change-Id: I4db73ab031d959d24ba138f4b80f1b5bbe2fa17f

--- repo/tests/phpunit/includes/store/sql/PropertyInfoTableBuilderTest.php
+++ repo/tests/phpunit/includes/store/sql/PropertyInfoTableBuilderTest.php
@@ -60,7 +60,7 @@ class PropertyInfoTableBuilderTest extends \MediaWikiTestCase {
 
 		// NOTE: We use the EntityStore from WikibaseRepo in initProperties,
 		//       so we should also use the EntityLookup from WikibaseRepo.
-		$entityLookup = WikibaseRepo::getDefaultInstance()->getEntityLookup();
+		$entityLookup = WikibaseRepo::getDefaultInstance()->getEntityLookup( 'uncached' );
 
 		$table = new PropertyInfoTable( false );
 		$builder = new PropertyInfoTableBuilder( $table, $entityLookup );
