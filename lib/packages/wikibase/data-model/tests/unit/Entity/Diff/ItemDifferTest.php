ba2bb9bff6dcb3f9455c84615d6d817be694d76f
jeroendedauw <jeroendedauw@gmail.com>
2014-06-12 16:21:22 +0200

    Add dedicated tests for ItemDiffer

---
+++ lib/packages/wikibase/data-model/tests/unit/Entity/Diff/ItemDifferTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Wikibase\Test;
+
+use Diff\DiffOp\Diff\Diff;
+use Diff\DiffOp\DiffOpAdd;
+use Diff\DiffOp\DiffOpChange;
+use Diff\DiffOp\DiffOpRemove;
+use Wikibase\DataModel\Claim\Statement;
+use Wikibase\DataModel\Entity\Diff\ItemDiffer;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\DataModel\Snak\PropertySomeValueSnak;
+
+/**
+ * @covers Wikibase\DataModel\Entity\Diff\ItemDiffer
+ * @covers Wikibase\DataModel\Entity\Diff\ItemDiff
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class ItemDifferTest extends \PHPUnit_Framework_TestCase {
+
+	public function testGivenTwoEmptyItems_emptyItemDiffIsReturned() {
+		$differ = new ItemDiffer();
+
+		$diff = $differ->diffEntities( Item::newEmpty(), Item::newEmpty() );
+
+		$this->assertInstanceOf( 'Wikibase\DataModel\Entity\Diff\ItemDiff', $diff );
+		$this->assertTrue( $diff->isEmpty() );
+	}
+
+	public function testFingerprintIsDiffed() {
+		$firstItem = Item::newEmpty();
+		$firstItem->getFingerprint()->setLabel( 'en', 'kittens' );
+		$firstItem->getFingerprint()->setAliasGroup( 'en', array( 'cats' ) );
+
+		$secondItem = Item::newEmpty();
+		$secondItem->getFingerprint()->setLabel( 'en', 'nyan' );
+		$secondItem->getFingerprint()->setDescription( 'en', 'foo bar baz' );
+
+		$differ = new ItemDiffer();
+		$diff = $differ->diffItems( $firstItem, $secondItem );
+
+		$this->assertEquals(
+			new Diff( array( 'en' => new DiffOpChange( 'kittens', 'nyan' ) ) ),
+			$diff->getLabelsDiff()
+		);
+
+		$this->assertEquals(
+			new Diff( array( 'en' => new DiffOpAdd( 'foo bar baz' ) ) ),
+			$diff->getDescriptionsDiff()
+		);
+
+		$this->assertEquals(
+			new Diff( array( 'en' => new Diff( array( new DiffOpRemove( 'cats' ) ) ) ) ),
+			$diff->getAliasesDiff()
+		);
+	}
+
+	public function testClaimsAreDiffed() {
+		$firstItem = Item::newEmpty();
+
+		$secondItem = Item::newEmpty();
+		$statement = new Statement( new PropertySomeValueSnak( 42 ) );
+		$statement->setGuid( 'kittens' );
+		$secondItem->addClaim( $statement );
+
+		$differ = new ItemDiffer();
+		$diff = $differ->diffItems( $firstItem, $secondItem );
+
+		$this->assertCount( 1, $diff->getClaimsDiff()->getAdditions() );
+	}
+
+}
+
