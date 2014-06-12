1f2023fcf083f2bd1a1716f944da24cb9d2ce50a
Jeroen De Dauw <jeroendedauw@gmail.com>
2014-06-12 13:27:17 +0200

    Update bootstrap.php

--- lib/packages/wikibase/internal-serialization/tests/bootstrap.php
+++ lib/packages/wikibase/internal-serialization/tests/bootstrap.php
@@ -4,11 +4,6 @@ if ( php_sapi_name() !== 'cli' ) {
 	die( 'Not an entry point' );
 }
 
-$pwd = getcwd();
-chdir( __DIR__ . '/..' );
-passthru( 'composer update' );
-chdir( $pwd );
-
 if ( !is_readable( __DIR__ . '/../vendor/autoload.php' ) ) {
 	die( 'You need to install this package with Composer before you can run the tests' );
 }
