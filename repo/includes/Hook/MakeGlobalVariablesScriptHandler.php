78d758504faa211d8898eec6a2b049dfbd6e4c53
daniel <daniel.kinzler@wikimedia.de>
2014-06-13 17:12:14 +0200

    Check for redirect before calling getEntity().
    EntityContent::getEntity() will fail if the content is a redirect,
    so calls to getEntity() need to be guarded against this case.
    
    Bug: 66030
    Change-Id: I00dd55c02dccf9467f8ff3895392a1b287355c1d

--- repo/includes/Hook/MakeGlobalVariablesScriptHandler.php
+++ repo/includes/Hook/MakeGlobalVariablesScriptHandler.php
@@ -3,6 +3,7 @@
 namespace Wikibase\Hook;
 
 use OutputPage;
+use Wikibase\DataModel\Entity\Entity;
 use Wikibase\EntityContent;
 use Wikibase\EntityContentFactory;
 use Wikibase\Lib\Serializers\SerializationOptions;
@@ -88,24 +89,27 @@ class MakeGlobalVariablesScriptHandler {
 	private function getParserConfigVars( $revisionId ) {
 		$entityContent = $this->entityContentFactory->getFromRevision( $revisionId );
 
-		if ( $entityContent === null || ! $entityContent instanceof \Wikibase\EntityContent ) {
+		if ( $entityContent === null || ! $entityContent instanceof EntityContent ) {
 			// entity or revision deleted, or non-entity content in entity namespace
 			wfDebugLog( __CLASS__, "No entity content found for revision $revisionId" );
 			return array();
 		}
 
-		return $this->buildParserConfigVars( $entityContent );
+		if ( $entityContent->isRedirect() ) {
+			return array();
+		} else {
+			$entity = $entityContent->getEntity();
+			return $this->buildParserConfigVars( $entity );
+		}
 	}
 
 	/**
-	 * @param EntityContent $entityContent
+	 * @param Entity $entity
 	 *
 	 * @return array
 	 */
-	private function buildParserConfigVars( EntityContent $entityContent ) {
+	private function buildParserConfigVars( Entity $entity ) {
 		$options = $this->makeSerializationOptions();
-
-		$entity = $entityContent->getEntity();
 
 		$parserConfigVars = $this->parserOutputConfigBuilder->build(
 			$entity,
