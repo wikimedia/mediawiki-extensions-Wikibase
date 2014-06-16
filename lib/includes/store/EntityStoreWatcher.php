e19d568ffbbcce20f3dbf992599e2f4fb397f889
daniel <daniel.kinzler@wikimedia.de>
2014-06-16 16:08:50 +0200

    Allow creation of redirects via EntityStore.
    Bug: 66073
    Change-Id: Ie07228b77c49922fc331ce7985d31a830d44a147

--- lib/includes/store/EntityStoreWatcher.php
+++ lib/includes/store/EntityStoreWatcher.php
@@ -15,7 +15,29 @@ use Wikibase\EntityRevision;
  */
 interface EntityStoreWatcher {
 
+	/**
+	 * Called when an entity is updated, created, or replaces a redirect.
+	 * This is not called when an entity is deleted or replaced by a redirect.
+	 *
+	 * @param EntityRevision $entityRevision
+	 */
 	public function entityUpdated( EntityRevision $entityRevision );
+
+	/**
+	 * Called when an entity or redirect is deleted.
+	 * This is not called when an entity is replaced by a redirect or vice versa.
+	 *
+	 * @param EntityId $entityId
+	 */
 	public function entityDeleted( EntityId $entityId );
+
+	/**
+	 * Called when a redirect is updated, created, or replaces an entity.
+	 * Not called when a redirect is deleted or replaced by an entity.
+	 *
+	 * @param EntityRedirect $entityRedirect
+	 * @param int $revisionId
+	 */
+	public function redirectUpdated( EntityRedirect $entityRedirect, $revisionId );
 
 }
