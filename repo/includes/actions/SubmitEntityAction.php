d025643abed0de8466336c293e907442cc0ebaf6
daniel <daniel.kinzler@wikimedia.de>
2014-06-16 14:49:54 +0200

    Re-implement undo action based on EntityContent.
    Previously, SubmitEntityAction implemented the undo action based
    on the EditEntity class, which is not applicable to non-entites
    like redirects.
    
    This patch re-implements the basic saving logic for use with undo
    operations. The logic for doing token and permissions checks and
    then saving the new content is pretty generic and could be re-used
    for other tasks, but it's unclear where it should reside. A full
    refactoring of core's EditPage would give us the relevant tools
    for free.
    
    Change-Id: I832bef721f481bf91375174ddecdd3c268ac8dd5

--- repo/includes/actions/SubmitEntityAction.php
+++ repo/includes/actions/SubmitEntityAction.php
@@ -2,8 +2,13 @@
 
 namespace Wikibase;
 
+use Content;
 use Status;
+use Title;
+use User;
+use WatchAction;
 use Wikibase\Repo\WikibaseRepo;
+use WikiPage;
 
 /**
  * Handles the submit action for Wikibase entities.
@@ -90,14 +95,11 @@ class SubmitEntityAction extends EditEntityAction {
 		$latestContent = $latestRevision->getContent();
 
 		$diff = $newerContent->getDiff( $olderContent );
-		$edit = false;
-		$token = $this->getRequest()->getText( 'wpEditToken' );
 
-		//TODO: allow injection/override!
-		$entityTitleLookup = WikibaseRepo::getDefaultInstance()->getEntityTitleLookup();
-		$entityRevisionLookup = WikibaseRepo::getDefaultInstance()->getEntityRevisionLookup( 'uncached' );
-		$entityStore = WikibaseRepo::getDefaultInstance()->getEntityStore();
-		$entityPermissionChecker = WikibaseRepo::getDefaultInstance()->getEntityPermissionChecker();
+		$title = $this->getTitle();
+		$user = $this->getUser();
+		$token = $req->getText( 'wpEditToken' );
+		$watch = $user->getOption( 'watchdefault' );
 
 		if ( $newerRevision->getId() == $latestRevision->getId() ) { // restore
 			$summary = $req->getText( 'wpSummary' );
@@ -110,25 +112,12 @@ class SubmitEntityAction extends EditEntityAction {
 				$status = Status::newGood();
 				$status->warning( 'wikibase-empty-undo' );
 			} else {
-				// make the old content the new content.
-				// NOTE: conflict detection is not needed for a plain restore, it's not based on anything.
-				$edit = new EditEntity(
-					$entityTitleLookup,
-					$entityRevisionLookup,
-					$entityStore,
-					$entityPermissionChecker,
-					$olderContent->getEntity(),
-					$this->getUser(),
-					false,
-					$this->getContext() );
-
-				$status = $edit->attemptSave( $summary, 0, $token );
+				$status = $this->attemptSave( $title, $olderContent, $summary, $user, $token, $watch );
 			}
 		} else { // undo
-			$entity = $latestContent->getEntity()->copy();
-			$latestContent = $latestContent->getPatchedCopy( $diff );
+			$patchedContent = $latestContent->getPatchedCopy( $diff );
 
-			if ( $latestContent->getEntity()->getDiff( $entity )->isEmpty() ) {
+			if ( $patchedContent->equals( $latestContent ) ) {
 				$status = Status::newGood();
 				$status->warning( 'wikibase-empty-undo' );
 			} else {
@@ -138,30 +127,111 @@ class SubmitEntityAction extends EditEntityAction {
 					$summary = $this->makeUndoSummary( $olderRevision, $newerRevision, $latestRevision );
 				}
 
-				//NOTE: use latest revision as base revision - we are saving patched content
-				//      based on the latest revision.
-				$edit = new EditEntity(
-					$entityTitleLookup,
-					$entityRevisionLookup,
-					$entityStore,
-					$entityPermissionChecker,
-					$latestContent->getEntity(),
-					$this->getUser(),
-					$latestRevision->getId(),
-					$this->getContext() );
-
-				$status = $edit->attemptSave( $summary, 0, $token );
+				$status = $this->attemptSave( $title, $patchedContent, $summary, $user, $token, $watch );
 			}
 		}
 
 		if ( $status->isOK() ) {
 			$this->getOutput()->redirect( $this->getTitle()->getFullUrl() );
 		} else {
-			$edit->showErrorPage();
+			$this->showStatusErrorsPage( 'wikibase-undo-title', $status );
 		}
 	}
 
 	public function execute() {
 		throw new \MWException( "not applicable" );
+	}
+
+	/**
+	 * @param Title $title
+	 * @param Content $content
+	 * @param string $summary
+	 * @param User $user
+	 * @param string $token
+	 * @param bool $watch
+	 *
+	 * @return Status
+	 */
+	private function attemptSave( Title $title, Content $content, $summary, User $user, $token, $watch ) {
+
+		// check token
+		$status = $this->getTokenStatus( $user, $token );
+
+		if ( !$status->isOK() ) {
+			return $status;
+		}
+
+		// check edit permission
+		$status = $this->getPermissionStatus( $user, 'edit', $title );
+
+		if ( !$status->isOK() ) {
+			return $status;
+		}
+
+		// save edit
+		$page = new WikiPage( $title );
+
+		// NOTE: Constraint checks are performed automatically via EntityContent::prepareSave.
+		$status = $page->doEditContent( $content, $summary );
+
+		if ( !$status->isOK() ) {
+			return $status;
+		}
+
+		// update watchlist
+		if ( $watch && $user->isLoggedIn() && !$user->isWatched( $title ) ) {
+			WatchAction::doWatch( $title, $user );
+		}
+
+		return $status;
+	}
+
+	/**
+	 * Checks the given permission.
+	 *
+	 * @param User $user
+	 * @param string $permission
+	 * @param Title $title
+	 * @param string $quick
+	 *
+	 * @return Status a status object representing the check's result.
+	 */
+	private function getPermissionStatus( User $user, $permission, Title $title, $quick = '' ) {
+		wfProfileIn( __METHOD__ );
+
+		//XXX: would be nice to be able to pass the $short flag too,
+		//     as used by getUserPermissionsErrorsInternal. But Title doesn't expose that.
+		$errors = $title->getUserPermissionsErrors( $permission, $user, $quick !== 'quick' );
+		$status = Status::newGood();
+
+		foreach ( $errors as $error ) {
+			call_user_func_array( array( $status, 'fatal'), $error );
+			$status->setResult( false );
+		}
+
+		wfProfileOut( __METHOD__ );
+		return $status;
+	}
+
+	/**
+	 * Checks that the given token is valid.
+	 *
+	 * @param User $user
+	 * @param string $token
+	 *
+	 * @return Status
+	 */
+	private function getTokenStatus( User $user, $token ) {
+		$status = Status::newGood();
+
+		if ( !$user->matchEditToken( $token ) ) {
+			if ( $user->matchEditTokenNoSuffix( $token ) ) {
+				$status = Status::newFatal( 'token_suffix_mismatch' );
+			} else {
+				$status = Status::newFatal( 'session_fail_preview' );
+			}
+		}
+
+		return $status;
 	}
 }
