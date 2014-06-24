efc66286fcac0b1560e0179d1f5f0bb2f9689602
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2014-06-24 11:39:53 +0200

    Make ChangeNotifier an interface and introduce NullChangeNotifier
    See the discussion at If2466a07f6ab6a15e077bba4ff147e3c3ba66953.
    
    Basic idea: It's pointless to even create change objects in the
    first place if they get dumped to dev/null anyway.
    
    Please note that this is only the first step in a planned series.
    This patch adds more code but in the end a lote more code will go
    away.
    
    Change-Id: Ic84dc0f32564f61ce37cf6a10fa9118c58816957

--- repo/includes/Notifications/ChangeNotifier.php
+++ repo/includes/Notifications/ChangeNotifier.php
@@ -7,7 +7,6 @@ use Revision;
 use User;
 use Wikibase\EntityChange;
 use Wikibase\EntityContent;
-use Wikibase\Lib\Changes\EntityChangeFactory;
 
 /**
  * Class for generating and submitting change notifications in different situations.
@@ -16,22 +15,7 @@ use Wikibase\Lib\Changes\EntityChangeFactory;
  * @license GPL 2+
  * @author Daniel Kinzler
  */
-class ChangeNotifier {
-
-	/**
-	 * @var EntityChangeFactory
-	 */
-	private $changeFactory;
-
-	/**
-	 * @var ChangeTransmitter
-	 */
-	private $changeTransmitter;
-
-	public function __construct( EntityChangeFactory $changeFactory, ChangeTransmitter $changeTransmitter ) {
-		$this->changeFactory = $changeFactory;
-		$this->changeTransmitter = $changeTransmitter;
-	}
+interface ChangeNotifier {
 
 	/**
 	 * This method constructs and sends the appropriate notifications (if any)
@@ -46,25 +30,7 @@ class ChangeNotifier {
 	 *
 	 * @return EntityChange|null
 	 */
-	public function notifyOnPageDeleted( EntityContent $content, User $user, $timestamp ) {
-		wfProfileIn( __METHOD__ );
-
-		if ( $content->isRedirect() ) {
-			// TODO: notify the client about changes to redirects!
-			return null;
-		}
-
-		$change = $this->changeFactory->newFromUpdate( EntityChange::REMOVE, $content->getEntity() );
-
-		$change->setTimestamp( $timestamp );
-		$change->setUserId( $user->getId() );
-		$change->setMetadataFromUser( $user );
-
-		$this->changeTransmitter->transmitChange( $change );
-
-		wfProfileOut( __METHOD__ );
-		return $change;
-	}
+	public function notifyOnPageDeleted( EntityContent $content, User $user, $timestamp );
 
 	/**
 	 * This method constructs and sends the appropriate notifications (if any)
@@ -74,29 +40,7 @@ class ChangeNotifier {
 	 *
 	 * @return EntityChange|null
 	 */
-	public function notifyOnPageUndeleted( Revision $revision ) {
-		wfProfileIn( __METHOD__ );
-
-		/** @var EntityContent $content */
-		$content = $revision->getContent();
-
-		if ( $content->isRedirect() ) {
-			// TODO: notify the client about changes to redirects!
-			return null;
-		}
-
-		$change = $this->changeFactory->newFromUpdate( EntityChange::RESTORE, null, $content->getEntity() );
-
-		$change->setRevisionInfo( $revision );
-
-		$user = User::newFromId( $revision->getUser() );
-		$change->setMetadataFromUser( $user );
-
-		$this->changeTransmitter->transmitChange( $change );
-
-		wfProfileOut( __METHOD__ );
-		return $change;
-	}
+	public function notifyOnPageUndeleted( Revision $revision );
 
 	/**
 	 * This method constructs and sends the appropriate notifications (if any)
@@ -109,28 +53,7 @@ class ChangeNotifier {
 	 *
 	 * @return EntityChange|null
 	 */
-	public function notifyOnPageCreated( Revision $revision ) {
-		wfProfileIn( __METHOD__ );
-
-		/** @var EntityContent $content */
-		$content = $revision->getContent();
-
-		if ( $content->isRedirect() ) {
-			// Clients currently don't care about redirected being created.
-			// TODO: notify the client about changes to redirects!
-			return null;
-		}
-
-		$change = $this->changeFactory->newFromUpdate( EntityChange::ADD, null, $content->getEntity() );
-
-		$change->setRevisionInfo( $revision );
-
-		// FIXME: RepoHooks::onRecentChangeSave currently adds to the change later!
-		$this->changeTransmitter->transmitChange( $change );
-
-		wfProfileOut( __METHOD__ );
-		return $change;
-	}
+	public function notifyOnPageCreated( Revision $revision );
 
 	/**
 	 * This method constructs and sends the appropriate notifications (if any)
@@ -144,60 +67,6 @@ class ChangeNotifier {
 	 *
 	 * @return EntityChange|null
 	 */
-	public function notifyOnPageModified( Revision $current, Revision $parent ) {
-		wfProfileIn( __METHOD__ );
-
-		if ( $current->getParentId() !== $parent->getId() ) {
-			throw new InvalidArgumentException( '$parent->getId() must be the same as $current->getParentId()!' );
-		}
-
-		$change = $this->getChangeForModification( $parent->getContent(), $current->getContent() );
-
-		if ( !$change ) {
-			// nothing to do
-			return null;
-		}
-
-		$change->setRevisionInfo( $current );
-
-		// FIXME: RepoHooks::onRecentChangeSave currently adds to the change later!
-		$this->changeTransmitter->transmitChange( $change );
-
-		wfProfileOut( __METHOD__ );
-		return $change;
-	}
-
-	/**
-	 * Returns a EntityChange based on the old and new content object, taking
-	 * redirects into consideration.
-	 *
-	 * @todo: Notify the client about changes to redirects explicitly.
-	 *
-	 * @param EntityContent $oldContent
-	 * @param EntityContent $newContent
-	 *
-	 * @return EntityChange|null
-	 */
-	private function getChangeForModification( EntityContent $oldContent, EntityContent $newContent ) {
-		$oldEntity = $oldContent->isRedirect() ? null : $oldContent->getEntity();
-		$newEntity = $newContent->isRedirect() ? null : $newContent->getEntity();
-
-		if ( $oldEntity === null && $newEntity === null ) {
-			// Old and new versions are redirects. Nothing to do.
-			return null;
-		} elseif ( $newEntity === null ) {
-			// The new version is a redirect. For now, treat that as a deletion.
-			$action = EntityChange::REMOVE;
-		} elseif ( $oldEntity === null ) {
-			// The old version is a redirect. For now, treat that like restoring the entity.
-			$action = EntityChange::RESTORE;
-		} else {
-			// No redirects involved
-			$action = EntityChange::UPDATE;
-		}
-
-		$change = $this->changeFactory->newFromUpdate( $action, $oldEntity, $newEntity );
-		return $change;
-	}
+	public function notifyOnPageModified( Revision $current, Revision $parent );
 
 }
