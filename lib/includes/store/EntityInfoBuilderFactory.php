f87ab8559daa8590a00dbdaf63c400de8fc09426
daniel <daniel.kinzler@wikimedia.de>
2014-07-04 13:55:00 +0200

    Refactor EntityInfoBuilder
    EntityInfoBuilder is now a stateful builder that wraps the entity
    info array.
    
    This change also introduces EntityInfoBuilderFactory and moves
    related things to the Wikibase\Lib\Store namespace.
    
    Change-Id: If2576071871705e7fc03cbc61cd26e4c3f7b1ae4

---
+++ lib/includes/store/EntityInfoBuilderFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Wikibase\Lib\Store;
+
+use Wikibase\DataModel\Entity\EntityId;
+
+/**
+ * Interface for a factory service for EntityInfoBuilder instances.
+ *
+ * @see EntityInfoBuilder
+ *
+ * @since 0.5
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ */
+interface EntityInfoBuilderFactory {
+
+	/**
+	 * Returns a new EntityInfoBuilder for gathering information about the
+	 * Entities specified by the given IDs.
+	 *
+	 * @param EntityId[] $ids
+	 *
+	 * @return EntityInfoBuilder
+	 */
+	public function newEntityInfoBuilder( array $ids );
+}
