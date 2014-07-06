946484804f4ec4d5e3748452e41258658f0794d2
daniel <daniel.kinzler@wikimedia.de>
2014-07-06 21:54:55 +0200

    Resolve redirects in EntityInfoBuilder
    Bug: 66972
    Change-Id: Id88b9bc824ab30cfb02f7628508f9678457d92f4

--- lib/includes/store/EntityInfoBuilder.php
+++ lib/includes/store/EntityInfoBuilder.php
@@ -33,9 +33,24 @@ interface EntityInfoBuilder {
 	 * EntitySerializer and related classes. It should be suitable for serialization,
 	 * and must thus not contain any objects.
 	 *
+	 * @note: after resolveRedirects() is called, entity records will be available under
+	 * their actual ID as well as any relevant redirect ID.
+	 *
 	 * @return array[]
 	 */
 	public function getEntityInfo();
+
+	/**
+	 * Resolves any redirects.
+	 *
+	 * This updates the 'id' field of the records in the data structure
+	 * returned by getEntityInfo() to the id of the target redirect, if the
+	 * original ID referred to a redirect.
+	 *
+	 * Thus, the keys in the data structure returned by getEntityInfo()
+	 * may come to be different from the respective record's id field.
+	 */
+	public function resolveRedirects();
 
 	/**
 	 * Adds terms (like labels and/or descriptions) to the entity info.
@@ -44,6 +59,9 @@ interface EntityInfoBuilder {
 	 *
 	 * @note: For historical reasons, the types expected by $termTypes are different from the
 	 * keys used in entity info!
+	 *
+	 * @note: If resolveRedirects() was previously called, terms from any redirect's target
+	 * entity are used.
 	 *
 	 * @param string[]|null $termTypes Which types of terms to include (e.g. "label", "description",
 	 * "alias"). Note that the corresponding fields that will be set set in the entity records
@@ -66,6 +84,9 @@ interface EntityInfoBuilder {
 	 * Removes entries for non-existent Entities from this builder. The data structure
 	 * returned by getEntityInfo will then only contain records for entities that could
 	 * actually be found.
+	 *
+	 * @note: If resolveRedirects() was previously called, any redirects are kept.
+	 * If resolveRedirects() was not called, redirects will be removed as missing.
 	 */
 	public function removeMissing();
 }
