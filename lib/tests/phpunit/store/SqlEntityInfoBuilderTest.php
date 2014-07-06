b187bcc1e42b351c89b40702d06feec4e99f4bdf
daniel <daniel.kinzler@wikimedia.de>
2014-07-06 21:54:55 +0200

    Resolve redirects in EntityInfoBuilder
    Change-Id: Id88b9bc824ab30cfb02f7628508f9678457d92f4

--- lib/tests/phpunit/store/SqlEntityInfoBuilderTest.php
+++ lib/tests/phpunit/store/SqlEntityInfoBuilderTest.php
@@ -2,13 +2,13 @@
 
 namespace Wikibase\Test;
 
-use Wikibase\DataModel\Entity\BasicEntityIdParser;
+use Wikibase\Content\UnresolvedRedirectException;
 use Wikibase\DataModel\Entity\EntityId;
 use Wikibase\DataModel\Entity\ItemId;
 use Wikibase\DataModel\Entity\PropertyId;
 use Wikibase\Item;
-use Wikibase\Property;
 use Wikibase\Lib\Store\Sql\SqlEntityInfoBuilder;
+use Wikibase\Property;
 
 /**
  * @covers Wikibase\SqlEntityInfoBuilder
@@ -109,11 +109,24 @@ class SqlEntityInfoBuilderTest extends \MediaWikiTestCase {
 
 	/**
 	 * @param EntityId[] $ids
+	 * @param EntityId[] $redirects
 	 *
 	 * @return SqlEntityInfoBuilder
 	 */
-	public function newEntityInfoBuilder( array $ids ) {
-		return new SqlEntityInfoBuilder( $ids, new BasicEntityIdParser() );
+	private function newEntityInfoBuilder( array $ids, array $redirects = array() ) {
+		$repo = $this->getMock( 'Wikibase\Lib\Store\EntityRevisionLookup' );
+		$repo->expects( $this->any() )
+			->method( 'getEntityRevision' )
+			->will( $this->returnCallback(
+				function ( EntityId $id ) use ( $redirects ) {
+					$key = $id->getSerialization();
+					if ( isset( $redirects[$key] ) ) {
+						throw new UnresolvedRedirectException( $redirects[$key] );
+					}
+				}
+			) );
+
+		return new SqlEntityInfoBuilder( $ids, $repo );
 	}
 
 	public function provideBuildEntityInfo() {
@@ -260,6 +273,38 @@ class SqlEntityInfoBuilderTest extends \MediaWikiTestCase {
 		}
 	}
 
+	public function testCollectTerms_redirect() {
+		$ids = array( new ItemId( 'Q7' ), new ItemId( 'Q1' ) );
+		$redirects = array( 'Q7' => new ItemId( 'Q2' ) );
+
+		$expected = array(
+			'Q1' => array( 'id' => 'Q1', 'type' => Item::ENTITY_TYPE,
+				'labels' => $this->makeLanguageValueRecords( array( 'de' => 'label:Q1/de' ) ),
+			),
+			'Q2' => array( 'id' => 'Q2', 'type' => Item::ENTITY_TYPE,
+				'labels' => $this->makeLanguageValueRecords( array( 'de' => 'label:Q2/de' ) ),
+			),
+			'Q7' => array( 'id' => 'Q2', 'type' => Item::ENTITY_TYPE,
+				'labels' => $this->makeLanguageValueRecords( array( 'de' => 'label:Q2/de' ) ),
+		)
+		);
+
+		$builder = $this->newEntityInfoBuilder( $ids, $redirects );
+
+		$builder->resolveRedirects();
+		$builder->collectTerms( array( 'label' ), array( 'de' ) );
+		$entityInfo = $builder->getEntityInfo();
+
+		$this->assertEquals( array_keys( $expected ), array_keys( $entityInfo ) );
+
+		foreach ( $expected as $id => $expectedRecord ) {
+			$this->assertArrayHasKey( $id, $entityInfo );
+			$actualRecord = $entityInfo[$id];
+
+			$this->assertArrayEquals( $expectedRecord, $actualRecord, false, true );
+		}
+	}
+
 	public function provideCollectDataTypes() {
 		return array(
 			array(
@@ -305,38 +350,52 @@ class SqlEntityInfoBuilderTest extends \MediaWikiTestCase {
 
 	public function provideRemoveMissing() {
 		return array(
-			array(
+			'empty' => array(
+				array(),
 				array(),
 				array()
 			),
 
-			array(
+			'found' => array(
 				array(
 					new ItemId( 'Q2' ),
 				),
+				array(),
 				array(
 					'Q2' => array( 'id' => 'Q2', 'type' => Item::ENTITY_TYPE ),
 				),
 			),
 
-			array(
+			'missing' => array(
 				array(
 					new ItemId( 'Q7' ),
 				),
+				array(),
 				array()
 			),
 
-			array(
+			'some found' => array(
 				array(
 					new ItemId( 'Q2' ),
 					new PropertyId( 'P7' ),
 					new ItemId( 'Q7' ),
 					new PropertyId( 'P2' ),
 				),
+				array(),
 				array(
 					'P2' => array( 'id' => 'P2', 'type' => Property::ENTITY_TYPE ),
 					'Q2' => array( 'id' => 'Q2', 'type' => Item::ENTITY_TYPE ),
 				)
+			),
+
+			'redirect not resolved' => array(
+				array(
+					new ItemId( 'Q7' ),
+				),
+				array(
+					'Q7' => new ItemId( 'Q2' )
+				),
+				array()
 			),
 		);
 	}
@@ -344,12 +403,123 @@ class SqlEntityInfoBuilderTest extends \MediaWikiTestCase {
 	/**
 	 * @dataProvider provideRemoveMissing
 	 */
-	public function testRemoveMissing( array $ids, array $expected = null ) {
-		$builder = $this->newEntityInfoBuilder( $ids );
+	public function testRemoveMissing( array $ids, array $redirects, array $expected = null ) {
+		$builder = $this->newEntityInfoBuilder( $ids, $redirects );
 
 		$builder->removeMissing();
 		$entityInfo = $builder->getEntityInfo();
 
 		$this->assertArrayEquals( array_keys( $expected ), array_keys( $entityInfo ) );
+	}
+
+	public function provideRemoveMissing_keepRedirects() {
+		return array(
+			'empty' => array(
+				array(),
+				array(),
+				array()
+			),
+
+			'unrelated redirect' => array(
+				array(
+					new ItemId( 'Q2' ),
+				),
+				array(
+					'Q7' => new ItemId( 'Q2' )
+				),
+				array(
+					'Q2' => array( 'id' => 'Q2', 'type' => Item::ENTITY_TYPE ),
+				),
+			),
+
+			'redirect resolved' => array(
+				array(
+					new ItemId( 'Q7' ),
+				),
+				array(
+					'Q7' => new ItemId( 'Q2' )
+				),
+				array(
+					'Q2' => array( 'id' => 'Q2', 'type' => Item::ENTITY_TYPE ),
+					'Q7' => array( 'id' => 'Q2', 'type' => Item::ENTITY_TYPE ),
+				),
+			),
+
+			'some found, some resolved' => array(
+				array(
+					new ItemId( 'Q2' ),
+					new PropertyId( 'P7' ),
+					new ItemId( 'Q7' ),
+					new PropertyId( 'P2' ),
+				),
+				array(
+					'Q7' => new ItemId( 'Q2' )
+				),
+				array(
+					'P2' => array( 'id' => 'P2', 'type' => Property::ENTITY_TYPE ),
+					'Q2' => array( 'id' => 'Q2', 'type' => Item::ENTITY_TYPE ),
+					'Q7' => array( 'id' => 'Q2', 'type' => Item::ENTITY_TYPE ),
+				)
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider provideRemoveMissing_keepRedirects
+	 */
+	public function testRemoveMissing_keepRedirects( array $ids, array $redirects, array $expected = null ) {
+		$builder = $this->newEntityInfoBuilder( $ids, $redirects );
+
+		$builder->resolveRedirects();
+		$builder->removeMissing();
+		$entityInfo = $builder->getEntityInfo();
+
+		$this->assertArrayEquals( array_keys( $expected ), array_keys( $entityInfo ) );
+	}
+
+	public function provideResolveredirects() {
+		return array(
+			'empty' => array(
+				array(),
+				array(),
+				array()
+			),
+
+			'some redirects' => array(
+				array(
+					new ItemId( 'Q2' ),
+					new ItemId( 'Q12' ),
+					new ItemId( 'Q22' ),
+				),
+				array(
+					'Q12' => new ItemId( 'Q2' ),
+					'Q22' => new ItemId( 'Q2' ),
+				),
+				array(
+					'Q2' => 'Q2',
+					'Q12' => 'Q2',
+					'Q22' => 'Q2',
+				),
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider provideResolveRedirects
+	 */
+	public function testResolveRedirects( array $ids, array $redirects, array $expected = null ) {
+		$builder = $this->newEntityInfoBuilder( $ids, $redirects );
+
+		$builder->resolveRedirects();
+		$entityInfo = $builder->getEntityInfo();
+
+		$resolvedIds = array_map(
+			function( $record ) {
+				return $record['id'];
+			} ,
+			$entityInfo
+		);
+
+		$this->assertArrayEquals( $expected, $resolvedIds );
 	}
 }
