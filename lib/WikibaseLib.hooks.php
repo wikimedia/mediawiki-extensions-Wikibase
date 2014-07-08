80790fb5b6bf1e1e41fb47be8f83f7af0926b001
Adrian Lang <adrian.lang@wikimedia.de>
2014-07-08 13:14:07 +0200

    Inject AbstractedRepoApi into EntityStore
    Change-Id: Ib057aeea1c7d71362010944f9f56eb4eb1c0cc0a

--- lib/WikibaseLib.hooks.php
+++ lib/WikibaseLib.hooks.php
@@ -190,11 +190,13 @@ final class LibHooks {
 				'tests/qunit/jquery.wikibase/jquery.wikibase.referenceview.tests.js',
 			),
 			'dependencies' => array(
+				'jquery.valueview.ExpertStore',
 				'jquery.wikibase.referenceview',
+				'mediawiki.Title',
 				'wikibase.datamodel',
+				'wikibase.store.FetchedContent',
 				'wikibase.store.EntityStore',
 				'wikibase.ValueViewBuilder',
-				'jquery.valueview',
 				'valueFormatters'
 			),
 		);
@@ -259,6 +261,7 @@ final class LibHooks {
 				'tests/qunit/wikibase.store/store.EntityStore.tests.js',
 			),
 			'dependencies' => array(
+				'jquery',
 				'wikibase.store.EntityStore',
 				'wikibase.tests.qunit.testrunner'
 			),
