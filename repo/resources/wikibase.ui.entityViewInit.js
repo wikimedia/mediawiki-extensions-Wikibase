fc642f3fcbca242e8a7e321bc2eb218b06b4319f
Adrian Lang <adrian.lang@wikimedia.de>
2014-07-08 13:54:05 +0200

    Inject RepoApi into old UI
    Change-Id: I96bfb91d93002568ebb2535a27d6991209591277

--- repo/resources/wikibase.ui.entityViewInit.js
+++ repo/resources/wikibase.ui.entityViewInit.js
@@ -32,10 +32,12 @@
 		// adjustments are necessary.
 		mw.hook( 'wikibase.domready' ).fire();
 
+		var repoApi = new wb.RepoApi();
+
 		// add an edit tool for the main label. This will be integrated into the heading nicely:
 		var $firstHeading = $( '.wb-firstHeading' );
 		if ( $firstHeading.length ) { // Special pages do not have a custom wb heading
-			var labelEditTool = new wb.ui.LabelEditTool( $firstHeading[0] ),
+			var labelEditTool = new wb.ui.LabelEditTool( $firstHeading[0], { api: repoApi } ),
 				editableLabel = labelEditTool.getValues( true )[0], // [0] will always be set
 				fn = function( event, origin ) {
 					// Limit the global stopItemPageEditMode event to that element
@@ -58,7 +60,7 @@
 		// add an edit tool for all properties in the data view:
 		$( '.wb-property-container:has( > .wb-property-container-key[title=description] )' ).each( function() {
 			// TODO: Make this nicer when we have implemented the data model
-			new wb.ui.DescriptionEditTool( this );
+			new wb.ui.DescriptionEditTool( this, { api: repoApi } );
 		} );
 
 		if( mw.config.get( 'wbEntity' ) !== null ) {
@@ -71,7 +73,7 @@
 
 			// edit tool for aliases:
 			$( '.wb-aliases' ).each( function() {
-				new wb.ui.AliasesEditTool( this );
+				new wb.ui.AliasesEditTool( this, { api: repoApi } );
 			} );
 
 			// BUILD CLAIMS VIEW:
@@ -108,7 +110,6 @@
 			entityInitializer.getEntity().done( function( entity ) {
 				// FIXME: Initializing entityview on $claims leads to the claim section inserted as
 				// child of $claims. It should be direct child of ".wb-entity".
-				var repoApi = new wb.RepoApi();
 				var abstractedRepoApi = new wb.AbstractedRepoApi();
 				var entityStore = new wb.store.EntityStore( abstractedRepoApi );
 				wb.compileEntityStoreFromMwConfig( entityStore );
@@ -142,7 +143,8 @@
 					// actual initialization
 					new wb.ui.SiteLinksEditTool( $( this ), {
 						allowedSites: wb.getSitesOfGroup( group ),
-						counterContainers: $sitesCounterContainer
+						counterContainers: $sitesCounterContainer,
+						api: repoApi
 					} );
 				} );
 
