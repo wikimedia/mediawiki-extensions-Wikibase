6ab71c8865907b12167ee8f0fa15cc1162e76e00
jeroendedauw <jeroendedauw@gmail.com>
2014-07-21 00:22:22 +0200

    Do not run composer update in test bootstrap

--- lib/packages/wikibase/data-model-serialization/tests/bootstrap.php
+++ lib/packages/wikibase/data-model-serialization/tests/bootstrap.php
@@ -4,10 +4,8 @@ if ( php_sapi_name() !== 'cli' ) {
 	die( 'Not an entry point' );
 }
 
-$pwd = getcwd();
-chdir( __DIR__ . '/..' );
-passthru( 'composer update' );
-chdir( $pwd );
+error_reporting( E_ALL | E_STRICT );
+ini_set( 'display_errors', 1 );
 
 if ( !is_readable( __DIR__ . '/../vendor/autoload.php' ) ) {
 	die( 'You need to install this package with Composer before you can run the tests' );
