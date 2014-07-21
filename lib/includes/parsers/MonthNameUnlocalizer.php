e858791c14f8c27a393673c2baf739af3332330a
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2014-07-21 19:19:56 +0200

    Make MonthNameUnlocalizer aware of genitive month names
    See Ic7a5797.
    
    This includes a major rewrite of the method. Now it tries to
    unlocalize longer strings first. This should avoid all possible
    conflicts.
    
    Change-Id: I232fd3b5433f04396c53eecf3ad49d025a84ee64

--- lib/includes/parsers/MonthNameUnlocalizer.php
+++ lib/includes/parsers/MonthNameUnlocalizer.php
@@ -6,7 +6,7 @@ use Language;
 use ValueParsers\ParserOptions;
 
 /**
- * Class to unlocalise month names using Mediawiki's Language object
+ * Class to unlocalize a month name in a date string using MediaWiki's Language object.
  *
  * @since 0.5
  *
@@ -18,51 +18,77 @@ use ValueParsers\ParserOptions;
 class MonthNameUnlocalizer {
 
 	/**
-	 * @see Unlocalizer::unlocalize()
+	 * @see NumberUnlocalizer::unlocalizeNumber
 	 *
-	 * @param string $string string to process
-	 * @param string $langCode
+	 * @param string $date Localized date string.
+	 * @param string $languageCode
 	 * @param ParserOptions $options
-	 *
-	 * @return string unlocalized string
-	 */
-	public function unlocalize( $string, $langCode, ParserOptions $options ) {
-		if( $langCode === 'en' ) {
-			return $string;
-		}
-
-		$lang = Language::factory( $langCode );
-		$en = Language::factory( 'en' );
-
-		$string = $this->unlocalizeMonthNames( $lang, $en, $string );
-
-		return $string;
-	}
-
-	/**
-	 * Unlocalizes month names in a string, checking both full month names and abbreviations
-	 * @param Language $from
-	 * @param Language $to
-	 * @param string $string
 	 *
 	 * @return string
 	 */
-	private function unlocalizeMonthNames( Language $from, Language $to, $string ) {
-		$initialString = $string;
+	public function unlocalize( $date, $languageCode, ParserOptions $options ) {
+		if ( $languageCode === 'en' ) {
+			return $date;
+		}
+
+		$language = Language::factory( $languageCode );
+		$en = Language::factory( 'en' );
+
+		$date = $this->unlocalizeMonthName( $language, $en, $date );
+
+		return $date;
+	}
+
+	/**
+	 * Unlocalizes the longest month name in a date string that could be found first.
+	 * Tries to avoid doing multiple replacements and returns the localized original if in doubt.
+	 * Takes full month names, genitive names and abbreviations into account.
+	 *
+	 * @param Language $from
+	 * @param Language $to
+	 * @param string $date Localized date string.
+	 *
+	 * @return string
+	 */
+	private function unlocalizeMonthName( Language $from, Language $to, $date ) {
+		$replacements = array();
 
 		for ( $i = 1; $i <= 12; $i++ ) {
-			$string = str_replace( $from->getMonthName( $i ), $to->getMonthName( $i ), $string );
+			$replace = $to->getMonthName( $i );
+
+			$replacements[$from->getMonthName( $i )] = $replace;
+			$replacements[$from->getMonthNameGen( $i )] = $replace;
+			$replacements[$from->getMonthAbbreviation( $i )] = $replace;
 		}
 
-		if( $string !== $initialString ) {
-			return $string;
+		// Order search strings from longest to shortest
+		uksort( $replacements, function( $a, $b ) {
+			return strlen( $b ) - strlen( $a );
+		} );
+
+		foreach ( $replacements as $search => $replace ) {
+			$unlocalized = str_replace( $search, $replace, $date, $count );
+
+			// Nothing happened, try the next.
+			if ( $count <= 0 ) {
+				continue;
+			}
+
+			// Do not mess with strings that are clearly not a valid date.
+			if ( $count > 1 ) {
+				break;
+			}
+
+			// Do not mess with already unlocalized month names, e.g. "Juli" should not become
+			// "Julyi" when replacing "Jul" with "July". But shortening "Julyus" to "July" is ok.
+			if ( strpos( $date, $replace ) !== false && strlen( $replace ) >= strlen( $search ) ) {
+				break;
+			}
+
+			return $unlocalized;
 		}
 
-		for ( $i = 1; $i <= 12; $i++ ) {
-			$string = str_replace( $from->getMonthAbbreviation( $i ), $to->getMonthName( $i ), $string );
-		}
-
-		return $string;
+		return $date;
 	}
 
 }
