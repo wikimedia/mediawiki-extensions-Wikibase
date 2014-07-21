6173167f0e01349acbff66d7f1a15303313c5115
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2014-07-21 19:19:56 +0200

    Make MonthNameUnlocalizer aware of genitive month names
    See Ic7a5797.
    
    This includes a major rewrite of the method. Now it tries to
    unlocalize longer strings first. This should avoid all possible
    conflicts.
    
    Change-Id: I232fd3b5433f04396c53eecf3ad49d025a84ee64

--- lib/tests/phpunit/parsers/MonthNameUnlocalizerTest.php
+++ lib/tests/phpunit/parsers/MonthNameUnlocalizerTest.php
@@ -20,28 +20,60 @@ class MonthNameUnlocalizerTest extends \PHPUnit_Framework_TestCase {
 
 	public function provideUnlocalize() {
 		$testCases = array(
-
-			//Should unlocalize dates
+			// Nominative month names.
 			array( '1 Juli 2013', 'de', '1 July 2013' ),
 			array( '1 Januarie 1999', 'af', '1 January 1999' ),
-			array( '1 Jan 1999', 'af', '1 January 1999' ),
 			array( '16 Jenna 1999', 'bar', '16 January 1999' ),
 
-			//Shouldn#t do anything if we cant / don#t need to
+			// Genitive month names.
+			array( '1 Julis 2013', 'de', '1 July 2013' ),
+			array( '31 Decembris 2013', 'la', '31 December 2013' ),
+
+			// Abbreviations.
+			array( '1 Jan 1999', 'af', '1 January 1999' ),
+			array( '1 Mär. 1999', 'de', '1 March 1999' ),
+
+			// Nothing to do in English.
 			array( '1 June 2013', 'en', '1 June 2013' ),
 			array( '1 Jan 2013', 'en', '1 Jan 2013' ),
-			array( '16 FooBarBarxxx 1999', 'bar', '16 FooBarBarxxx 1999' ),
+			array( '1 January 1999', 'en', '1 January 1999' ),
 
+			// No localized month name found.
+			array( '16 FooBarBarxxx 1999', 'bar', '16 FooBarBarxxx 1999' ),
+			array( '16 Martii 1999', 'de', '16 Martii 1999' ),
+			array( '16 May 1999', 'de', '16 May 1999' ),
+			array( '16 Dezember 1999', 'la', '16 Dezember 1999' ),
+
+			// Replace the longest unlocalized substring first.
+			array( 'Juli Januar', 'de', 'Juli January' ),
+			array( 'Juli Mai', 'de', 'July Mai' ),
+			array( 'Juli December', 'de', 'July December' ),
+			array( 'July Dezember', 'de', 'July December' ),
+
+			// Do not mess with already unlocalized month names.
+			array( 'January', 'de', 'January' ),
+			array( 'April', 'la', 'April' ),
+
+			// Do not mess with strings that are clearly not a valid date.
+			array( 'Juli Juli', 'de', 'Juli Juli' ),
+
+			// Word boundaries currently do not prevent unlocalization on purpose.
+			array( 'Mai2013', 'de', 'May2013' ),
+			array( 'Februarii', 'de', 'Februaryii' ),
+
+			// Capitalization is currently significant. This may need to depend on the languages.
+			array( '1 juli 2013', 'de', '1 juli 2013' ),
 		);
 
-		//Loop through some other languages
-		$someLangs = array( 'war', 'ceb', 'uk', 'ru', 'de' );
+		// Loop through some other languages
+		$languageCodes = array( 'war', 'ceb', 'uk', 'ru', 'de' );
 		$en = Language::factory( 'en' );
 
-		foreach( $someLangs as $from ) {
+		foreach ( $languageCodes as $from ) {
 			$fromLang = Language::factory( $from );
 			for ( $i = 1; $i <= 12; $i++ ) {
 				$testCases[] = array( $fromLang->getMonthName( $i ), $from, $en->getMonthName( $i ) );
+				$testCases[] = array( $fromLang->getMonthNameGen( $i ), $from, $en->getMonthName( $i ) );
 				$testCases[] = array( $fromLang->getMonthAbbreviation( $i ), $from, $en->getMonthName( $i ) );
 			}
 		}
@@ -53,14 +85,14 @@ class MonthNameUnlocalizerTest extends \PHPUnit_Framework_TestCase {
 	 * @dataProvider provideUnlocalize
 	 *
 	 * @param $localized
-	 * @param $lang
+	 * @param $languageCode
 	 * @param $expected
 	 */
-	public function testUnlocalize( $localized, $lang, $expected ) {
+	public function testUnlocalize( $localized, $languageCode, $expected ) {
 		$monthUnlocalizer = new MonthNameUnlocalizer();
 		$options = new ParserOptions();
 
-		$actual = $monthUnlocalizer->unlocalize( $localized, $lang, $options );
+		$actual = $monthUnlocalizer->unlocalize( $localized, $languageCode, $options );
 
 		$this->assertEquals( $expected, $actual );
 	}
