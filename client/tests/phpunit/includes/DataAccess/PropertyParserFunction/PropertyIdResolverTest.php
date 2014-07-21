6ff25f548aba08976ba61f523342517607d13ee5
aude <aude.wiki@gmail.com>
2014-07-21 17:10:35 +0200

    Move property id resolving code for parser function into own class
    the code for resolving PropertyId from the users input does
    not belong in SnaksFinder!
    
    resolving property id from a label *is* language-dependent, though,
    so it should be a service provided to the LanguageAwareRenderer.
    
    Change-Id: I52b69565e9b91c838a421cb4b6f6dadbd0967511

---
+++ client/tests/phpunit/includes/DataAccess/PropertyParserFunction/PropertyIdResolverTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Wikibase\DataAccess\Tests\PropertyParserFunction;
+
+use DataValues\StringValue;
+use Language;
+use Wikibase\Claim;
+use Wikibase\DataAccess\PropertyParserFunction\PropertyIdResolver;
+use Wikibase\DataModel\Claim\Statement;
+use Wikibase\DataModel\Entity\BasicEntityIdParser;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\Item;
+use Wikibase\Property;
+use Wikibase\PropertyValueSnak;
+use Wikibase\Test\MockPropertyLabelResolver;
+use Wikibase\Test\MockRepository;
+
+/**
+ * @covers Wikibase\DataAccess\PropertyParserFunction\PropertyIdResolver
+ *
+ * @group Wikibase
+ * @group WikibaseClient
+ * @group WikibaseDataAccess
+ * @group PropertyParserFunctionTest
+ *
+ * @licence GNU GPL v2+
+ * @author Katie Filbert < aude.wiki@gmail.com >
+ */
+class PropertyIdResolverTest extends \PHPUnit_Framework_TestCase {
+
+	private function getDefaultInstance() {
+		$repo = $this->newMockRepository();
+		$propertyLabelResolver = new MockPropertyLabelResolver( 'en', $repo );
+
+		return new PropertyIdResolver( $propertyLabelResolver );
+	}
+
+	private function newMockRepository() {
+		$propertyId = new PropertyId( 'P1337' );
+
+		$property = Property::newFromType( 'string' );
+		$property->setId( $propertyId );
+		$property->setLabel( 'en', 'a kitten!' );
+
+		$entityLookup = new MockRepository();
+		$entityLookup->putEntity( $property );
+
+		return $entityLookup;
+	}
+
+	/**
+	 * @dataProvider resolvePropertyIdProvider
+	 */
+	public function testResolvePropertyId( $expected, $propertyLabelOrId ) {
+		$propertyIdResolver = $this->getDefaultInstance();
+
+		$propertyId = $propertyIdResolver->resolvePropertyId( $propertyLabelOrId, 'en' );
+		$this->assertEquals( $expected, $propertyId );
+	}
+
+	public function resolvePropertyIdProvider() {
+		return array(
+			array( new PropertyId( 'P1337' ), 'a kitten!' ),
+			array( new PropertyId( 'P1337' ), 'p1337' ),
+			array( new PropertyId( 'P1337' ), 'P1337' ),
+			array( new PropertyId( 'P1444' ), 'P1444' )
+		);
+	}
+
+	/**
+	 * @dataProvider resolvePropertyIdWithInvalidInput_throwsExceptionProvider
+	 */
+	public function testResolvePropertyIdWithInvalidInput_throwsException( $propertyIdOrLabel ) {
+		$propertyIdResolver = $this->getDefaultInstance();
+
+		$this->setExpectedException( 'Wikibase\Lib\PropertyLabelNotResolvedException' );
+
+		$propertyIdResolver->resolvePropertyId( $propertyIdOrLabel, 'en' );
+	}
+
+	public function resolvePropertyIdWithInvalidInput_throwsExceptionProvider() {
+		return array(
+			array( 'hedgehog' ),
+			array( 'Q100' )
+		);
+	}
+}
