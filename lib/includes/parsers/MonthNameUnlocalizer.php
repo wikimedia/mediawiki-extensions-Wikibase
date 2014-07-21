d86c571ac985535923b3fc1a6f0d9b115810c8d2
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2014-07-21 20:02:59 +0200

    Refactor MonthNameUnlocalizer
    Move the source language to the constructor and split the code.
    
    Change-Id: I7f1ce71fc06e7eea7eee820726ba5891738e5128

--- lib/includes/parsers/MonthNameUnlocalizer.php
+++ lib/includes/parsers/MonthNameUnlocalizer.php
@@ -11,30 +11,29 @@ use Language;
  *
  * @licence GNU GPL v2+
  * @author Adam Shorland
+ * @author Thiemo Mättig
  *
  * @todo move me to DataValues-time
  */
 class MonthNameUnlocalizer {
 
+	const BASE_LANGUAGE_CODE = 'en';
+
 	/**
-	 * @see NumberUnlocalizer::unlocalizeNumber
-	 *
-	 * @param string $date Localized date string.
-	 * @param string $languageCode
-	 *
-	 * @return string
+	 * @var string|null Language code of the source strings or null if it's the base language.
 	 */
-	public function unlocalize( $date, $languageCode ) {
-		if ( $languageCode === 'en' ) {
-			return $date;
+	private $languageCode = null;
+
+	private $baseLanguage = null;
+	private $language = null;
+
+	/**
+	 * @param string $languageCode
+	 */
+	public function __construct( $languageCode ) {
+		if ( $languageCode !== self::BASE_LANGUAGE_CODE ) {
+			$this->languageCode = $languageCode;
 		}
-
-		$language = Language::factory( $languageCode );
-		$en = Language::factory( 'en' );
-
-		$date = $this->unlocalizeMonthName( $language, $en, $date );
-
-		return $date;
 	}
 
 	/**
@@ -42,29 +41,14 @@ class MonthNameUnlocalizer {
 	 * Tries to avoid doing multiple replacements and returns the localized original if in doubt.
 	 * Takes full month names, genitive names and abbreviations into account.
 	 *
-	 * @param Language $from
-	 * @param Language $to
+	 * @see NumberUnlocalizer::unlocalizeNumber
+	 *
 	 * @param string $date Localized date string.
 	 *
-	 * @return string
+	 * @return string Unlocalized date string.
 	 */
-	private function unlocalizeMonthName( Language $from, Language $to, $date ) {
-		$replacements = array();
-
-		for ( $i = 1; $i <= 12; $i++ ) {
-			$replace = $to->getMonthName( $i );
-
-			$replacements[$from->getMonthName( $i )] = $replace;
-			$replacements[$from->getMonthNameGen( $i )] = $replace;
-			$replacements[$from->getMonthAbbreviation( $i )] = $replace;
-		}
-
-		// Order search strings from longest to shortest
-		uksort( $replacements, function( $a, $b ) {
-			return strlen( $b ) - strlen( $a );
-		} );
-
-		foreach ( $replacements as $search => $replace ) {
+	public function unlocalize( $date ) {
+		foreach ( $this->getReplacements() as $search => $replace ) {
 			$unlocalized = str_replace( $search, $replace, $date, $count );
 
 			// Nothing happened, try the next.
@@ -77,8 +61,8 @@ class MonthNameUnlocalizer {
 				break;
 			}
 
-			// Do not mess with already unlocalized month names, e.g. "Juli" should not become
-			// "Julyi" when replacing "Jul" with "July". But shortening "Julyus" to "July" is ok.
+			// Do not mess with already unlocalized month names, e.g. "July" should not become
+			// "Julyy" when replacing "Jul" with "July". But shortening "Julyus" to "July" is ok.
 			if ( strpos( $date, $replace ) !== false && strlen( $replace ) >= strlen( $search ) ) {
 				break;
 			}
@@ -87,6 +71,49 @@ class MonthNameUnlocalizer {
 		}
 
 		return $date;
+	}
+
+	private function getReplacements() {
+		$replacements = array();
+		$language = $this->getLanguage();
+
+		if ( $language !== null ) {
+			$baseLanguage = $this->getBaseLanguage();
+
+			for ( $i = 1; $i <= 12; $i++ ) {
+				$replace = $baseLanguage->getMonthName( $i );
+
+				$replacements[$language->getMonthName( $i )] = $replace;
+				$replacements[$language->getMonthNameGen( $i )] = $replace;
+				$replacements[$language->getMonthAbbreviation( $i )] = $replace;
+			}
+
+			// Order search strings from longest to shortest
+			uksort( $replacements, function( $a, $b ) {
+				return strlen( $b ) - strlen( $a );
+			} );
+		}
+
+		return $replacements;
+	}
+
+	private function getBaseLanguage() {
+		if ( $this->baseLanguage === null ) {
+			$this->baseLanguage = Language::factory( self::BASE_LANGUAGE_CODE );
+		}
+
+		return $this->baseLanguage;
+	}
+
+	/**
+	 * @return Language|null
+	 */
+	private function getLanguage() {
+		if ( $this->language === null && $this->languageCode !== null ) {
+			$this->language = Language::factory( $this->languageCode );
+		}
+
+		return $this->language;
 	}
 
 }
