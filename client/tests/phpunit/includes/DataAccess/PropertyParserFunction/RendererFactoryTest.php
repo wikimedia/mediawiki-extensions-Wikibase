c94d7ed945b6399f34069311065e12bb10f0255a
aude <aude.wiki@gmail.com>
2014-07-23 14:31:39 +0200

    Move more renderer construction code into RendererFactory
    checking if variants are used and then which renderer to use
    is more the responsibility of the RendererFactory.
    
    Change-Id: I1857241f3b71308c32ba75ca6af49623c77e53cb

--- client/tests/phpunit/includes/DataAccess/PropertyParserFunction/RendererFactoryTest.php
+++ client/tests/phpunit/includes/DataAccess/PropertyParserFunction/RendererFactoryTest.php
@@ -23,7 +23,48 @@ use Wikibase\Lib\SnakFormatter;
  * @licence GNU GPL v2+
  * @author Katie Filbert < aude.wiki@gmail.com >
  */
-class PropertyParserFunctionRendererFactoryTest extends \PHPUnit_Framework_TestCase {
+class RendererFactoryTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * @dataProvider newRendererFromParserProvider
+	 */
+	public function testNewRendererFromParser( $expected, $languageCode, $interfaceMessage,
+		$disableContentConversion, $disableTitleConversion, $outputType
+	) {
+		$parser = $this->getParser(
+			$languageCode,
+			$interfaceMessage,
+			$disableContentConversion,
+			$disableTitleConversion,
+			$outputType
+		);
+
+		$rendererFactory = new RendererFactory(
+			$this->getSnaksFinder(),
+			$this->getLanguageFallbackChainFactory(),
+			$this->getSnakFormatterFactory()
+		);
+
+		$renderer = $rendererFactory->newRendererFromParser( $parser );
+
+		$this->assertInstanceOf( $expected, $renderer );
+	}
+
+	public function newRendererFromParserProvider() {
+		$languageRendererClass = 'Wikibase\DataAccess\PropertyParserFunction\LanguageAwareRenderer';
+		$variantsRendererClass = 'Wikibase\DataAccess\PropertyParserFunction\VariantsAwareRenderer';
+
+		return array(
+			array( $languageRendererClass, 'en', false, false, false, Parser::OT_HTML ),
+			array( $languageRendererClass, 'ku', false, false, false, Parser::OT_PLAIN ),
+			array( $languageRendererClass, 'zh', false, false, false, Parser::OT_WIKI ),
+			array( $languageRendererClass, 'zh', false, true, false, Parser::OT_HTML ),
+			array( $languageRendererClass, 'zh', true, false, false, Parser::OT_HTML ),
+			array( $languageRendererClass, 'zh', false, false, false, Parser::OT_PREPROCESS ),
+			array( $variantsRendererClass, 'zh', false, false, true, Parser::OT_HTML ),
+			array( $variantsRendererClass, 'ku', false, false, false, Parser::OT_HTML ),
+		);
+	}
 
 	public function testNewLanguageAwareRenderer() {
 		$rendererFactory = new RendererFactory(
@@ -35,8 +76,8 @@ class PropertyParserFunctionRendererFactoryTest extends \PHPUnit_Framework_TestC
 		$language = Language::factory( 'he' );
 		$renderer = $rendererFactory->newLanguageAwareRenderer( $language );
 
-		$expectedClass = 'Wikibase\DataAccess\PropertyParserFunction\LanguageAwareRenderer';
-		$this->assertInstanceOf( $expectedClass, $renderer );
+		$languageRendererClass = 'Wikibase\DataAccess\PropertyParserFunction\LanguageAwareRenderer';
+		$this->assertInstanceOf( $languageRendererClass, $renderer );
 	}
 
 	private function getSnaksFinder() {
@@ -75,6 +116,40 @@ class PropertyParserFunctionRendererFactoryTest extends \PHPUnit_Framework_TestC
 			->will( $this->returnValue( $snakFormatter ) );
 
 		return $snakFormatterFactory;
+	}
+
+	private function getParser( $languageCode, $interfaceMessage, $disableContentConversion,
+		$disableTitleConversion, $outputType
+	) {
+		$parserConfig = array( 'class' => 'Parser' );
+
+		$parserOptions = $this->getParserOptions(
+			$languageCode,
+			$interfaceMessage,
+			$disableContentConversion,
+			$disableTitleConversion
+		);
+
+		$parser = new Parser( $parserConfig );
+
+		$parser->setTitle( Title::newFromText( 'Cat' ) );
+		$parser->startExternalParse( null, $parserOptions, $outputType );
+
+		return $parser;
+	}
+
+	private function getParserOptions( $languageCode, $interfaceMessage, $disableContentConversion,
+		$disableTitleConversion
+	) {
+		$language = Language::factory( $languageCode );
+
+		$parserOptions = new ParserOptions( User::newFromId( 0 ), $languageCode );
+		$parserOptions->setTargetLanguage( $language );
+		$parserOptions->setInterfaceMessage( $interfaceMessage );
+		$parserOptions->disableContentConversion( $disableContentConversion );
+		$parserOptions->disableTitleConversion( $disableTitleConversion );
+
+		return $parserOptions;
 	}
 
 }
