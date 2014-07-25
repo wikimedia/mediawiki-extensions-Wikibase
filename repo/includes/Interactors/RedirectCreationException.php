950196dbe106dfdf0c5df84fd1f8bbaf4fffb388
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2014-07-25 12:16:12 +0200

    Code style cleanup in wbcreateredirect API module
    This is a follow-up to all my comments in I4877736. All about style
    and wording, none of them is a reason to block the original patch.
    
    Change-Id: Ia3ee4ccbdb4184547fff94a01ba43723156adc2c

--- repo/includes/Interactors/RedirectCreationException.php
+++ repo/includes/Interactors/RedirectCreationException.php
@@ -22,9 +22,9 @@ class RedirectCreationException extends Exception {
 	/**
 	 * @param string $message A free form message, for logging and debugging
 	 * @param string $errorCode An error code, for use in the API
-	 * @param Exception $previous The Exception that caused this exception.
+	 * @param Exception $previous The previous exception that caused this exception.
 	 */
-	public function __construct( $message, $errorCode = "", Exception $previous = null ) {
+	public function __construct( $message, $errorCode = '', Exception $previous = null ) {
 		parent::__construct( $message, 0, $previous );
 		$this->errorCode = $errorCode;
 	}
