df3afc24e805c2d46a9d240c659ecc3ed91e7752
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2014-07-25 14:00:08 +0200

    Only use date formats that can roundtrip
    This partially reverts Id09e26e. Now the formatter checks for a few
    specific aspects of the format given in the respective MessagesXx.php:
    * Day can only be "d" (with leading zeros) or "j" (without).
    * Month can only be "F" (normal), "M" (abbreviation) or "xg" (genitive).
    * Both support optional dots or commas.
    * Spaces between the parts are enforced.
    * "Y" (full year) is enforced.
    
    Main change still is the added support for genitive month names which
    requires I232fd3b to make full roundtrips possible.
    
    See the comments in Id09e26e for more examples of strange date formats.
    
    Change-Id: I9aefa02a8355c9cca6ac750b2bf82a7734338640

--- lib/includes/formatters/MwTimeIsoFormatter.php
+++ lib/includes/formatters/MwTimeIsoFormatter.php
@@ -120,15 +120,43 @@ class MwTimeIsoFormatter extends ValueFormatterBase {
 	private function getDateFormat( $precision ) {
 		if ( $precision <= TimeValue::PRECISION_YEAR ) {
 			return 'Y';
-		}
-
-		if ( $precision === TimeValue::PRECISION_MONTH ) {
+		} elseif ( $precision === TimeValue::PRECISION_MONTH ) {
 			$format = $this->language->getDateFormatString( 'monthonly', 'dmy' );
-			return $format ?: 'F Y';
+			return sprintf( '%s Y', $this->getMonthFormat( $format ) );
+		} else {
+			$format = $this->language->getDateFormatString( 'date', 'dmy' );
+			return sprintf( '%s %s Y', $this->getDayFormat( $format ), $this->getMonthFormat( $format ) );
+		}
+	}
+
+	/**
+	 * @see Language::sprintfDate
+	 *
+	 * @param string $dateFormat
+	 *
+	 * @return string A date format for the day that roundtrips the Wikibase TimeParsers.
+	 */
+	private function getDayFormat( $dateFormat ) {
+		if ( preg_match( '/[dj][.,]?/', $dateFormat, $matches ) ) {
+			return $matches[0];
 		}
 
-		$format = $this->language->getDateFormatString( 'date', 'dmy' );
-		return $format ?: 'j F Y';
+		return 'j';
+	}
+
+	/**
+	 * @see Language::sprintfDate
+	 *
+	 * @param string $dateFormat
+	 *
+	 * @return string A date format for the month that roundtrips the Wikibase TimeParsers.
+	 */
+	private function getMonthFormat( $dateFormat ) {
+		if ( preg_match( '/(?:[FM]|xg)[.,]?/', $dateFormat, $matches ) ) {
+			return $matches[0];
+		}
+
+		return 'F';
 	}
 
 	/**
