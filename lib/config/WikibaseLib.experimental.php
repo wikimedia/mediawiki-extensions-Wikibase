68c992cf557d9f98f949dcf1a86fd59d71457897
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2014-07-25 11:32:57 +0200

    Drop dead experimental code
    Warning: I honestly don't know what the purpose of this
    UnitTestsList hook code was. But it's obvious that it's dead code
    now, right? The array was emptied in previous commits, e.g. Iece4a23.
    Should be easy to recover these snippets from the history if they
    are needed again.
    
    Change-Id: Iab6bf24c9602c63310c94d7e77d7975be07c7058

--- lib/config/WikibaseLib.experimental.php
+++ lib/config/WikibaseLib.experimental.php
@@ -7,7 +7,6 @@
  * It should furthermore not be included from outside the extension.
  *
  * @since 0.4
- *
  * @licence GNU GPL v2+
  */
 
@@ -15,23 +14,5 @@ if ( !defined( 'WBL_VERSION' ) || !defined( 'WB_EXPERIMENTAL_FEATURES' ) ) {
 	die( 'Not an entry point.' );
 }
 
-global $wgHooks;
-
-$wgHooks['UnitTestsList'][]	= function( array &$files ) {
-	// @codeCoverageIgnoreStart
-	$testFiles = array(
-
-	);
-
-	foreach ( $testFiles as $file ) {
-		$files[] = __DIR__ . '/../tests/phpunit/' . $file . 'Test.php';
-	}
-
-	return true;
-	// @codeCoverageIgnoreEnd
-};
-
 //NOTE: any experimental default settings need to go into the
 //      appropriate section of WikibaseLib.defaults.php.
-
-
