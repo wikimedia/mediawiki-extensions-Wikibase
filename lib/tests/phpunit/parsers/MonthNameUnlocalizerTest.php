5737ea7d1db80c0e207ae8ba606290a185e0d34e
daniel <daniel.kinzler@wikimedia.de>
2014-07-25 19:42:25 +0200

    Simpified MonthNameUnlocalizer
    This changes the behavior for some edge cases, but should not affect
    any valid dates. The intention is to reduce the code complexity, and
    make the behavior more predictable.
    
    Change-Id: I09d71b29db3c733fcc3abe809ab5db82782f34b6

--- lib/tests/phpunit/parsers/MonthNameUnlocalizerTest.php
+++ lib/tests/phpunit/parsers/MonthNameUnlocalizerTest.php
@@ -24,6 +24,7 @@ class MonthNameUnlocalizerTest extends \PHPUnit_Framework_TestCase {
 			array( '1 Juli 2013', 'de', '1 July 2013' ),
 			array( '1 Januarie 1999', 'af', '1 January 1999' ),
 			array( '16 Jenna 1999', 'bar', '16 January 1999' ),
+			array( '12 Jänner 2013', 'de-at', '12 January 2013' ),
 
 			// Genitive month names.
 			array( '1 Julis 2013', 'de', '1 July 2013' ),
@@ -33,27 +34,28 @@ class MonthNameUnlocalizerTest extends \PHPUnit_Framework_TestCase {
 			array( '1 Jan 1999', 'af', '1 January 1999' ),
 			array( '1 Mär. 1999', 'de', '1 March 1999' ),
 
-			// Nothing to do in English.
+			// Also normalize in English.
 			array( '1 June 2013', 'en', '1 June 2013' ),
-			array( '1 Jan 2013', 'en', '1 Jan 2013' ),
+			array( '1 Jan 2013', 'en', '1 January 2013' ),
 			array( '1 January 1999', 'en', '1 January 1999' ),
 
 			// No localized month name found.
 			array( '16 FooBarBarxxx 1999', 'bar', '16 FooBarBarxxx 1999' ),
-			array( '16 Martii 1999', 'de', '16 Martii 1999' ),
 			array( '16 May 1999', 'de', '16 May 1999' ),
 			array( '16 Dezember 1999', 'la', '16 Dezember 1999' ),
 
 			// Replace the longest unlocalized substring first.
 			array( 'Juli Januar', 'de', 'Juli January' ),
 			array( 'Juli Mai', 'de', 'July Mai' ),
-			array( 'Juli December', 'de', 'July December' ),
 			array( 'July Dezember', 'de', 'July December' ),
 			array( 'Januar Mär Dez', 'de', 'January Mär Dez' ),
+			array( 'Juli Juli', 'de', 'July July' ),
 
-			// Do not mess with already unlocalized month names.
+			// Allow English names even if another language is selected
+			array( 'Juli December', 'de', 'Juli December' ),
 			array( 'January', 'de', 'January' ),
 			array( 'April', 'la', 'April' ),
+			array( 'Dec', 'de', 'December' ),
 			array( 'Juni June', 'de', 'Juni June' ),
 			array( 'July Jul', 'de', 'July Jul' ),
 
@@ -61,12 +63,11 @@ class MonthNameUnlocalizerTest extends \PHPUnit_Framework_TestCase {
 			array( 'Mayo', 'war', 'May' ),
 			array( 'July Julis', 'de', 'July July' ),
 
-			// Do not mess with strings that are clearly not a valid date.
-			array( 'Juli Juli', 'de', 'Juli Juli' ),
-
-			// Word boundaries currently do not prevent unlocalization on purpose.
+			// Word boundaries currently do not prevent unlocalization,
+			// since they may not be present in some languages
 			array( 'Mai2013', 'de', 'May2013' ),
 			array( 'Februarii', 'de', 'Februaryii' ),
+			array( '16 Martii 1999', 'de', '16 Marchtii 1999' ),
 
 			// Capitalization is currently significant. This may need to depend on the languages.
 			array( '1 juli 2013', 'de', '1 juli 2013' ),
