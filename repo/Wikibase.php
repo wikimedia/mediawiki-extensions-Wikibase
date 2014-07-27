2a5a4a22b2002b786da2482a24e3af70776e10dc
Jan Zerebecki <jan.wikimedia@zerebecki.de>
2014-07-27 18:37:01 +0200

    Add go to linked page special page.
    It redirects to a client page based on the ItemId and the global SiteId of the client.
    
    Bug: 64776
    Change-Id: I214822688248a2dacd80143e5a138eaa2e968c12

--- repo/Wikibase.php
+++ repo/Wikibase.php
@@ -142,6 +142,7 @@ call_user_func( function() {
 	$wgSpecialPages['NewItem'] 							= 'Wikibase\Repo\Specials\SpecialNewItem';
 	$wgSpecialPages['NewProperty'] 						= 'Wikibase\Repo\Specials\SpecialNewProperty';
 	$wgSpecialPages['ItemByTitle'] 						= 'Wikibase\Repo\Specials\SpecialItemByTitle';
+	$wgSpecialPages['GoToLinkedPage']					= 'Wikibase\Repo\Specials\SpecialGoToLinkedPage';
 	$wgSpecialPages['ItemDisambiguation'] 				= 'Wikibase\Repo\Specials\SpecialItemDisambiguation';
 	$wgSpecialPages['ItemsWithoutSitelinks']			= 'Wikibase\Repo\Specials\SpecialItemsWithoutSitelinks';
 	$wgSpecialPages['SetLabel'] 						= 'Wikibase\Repo\Specials\SpecialSetLabel';
@@ -160,6 +161,7 @@ call_user_func( function() {
 	$wgSpecialPageGroups['NewItem']						= 'wikibaserepo';
 	$wgSpecialPageGroups['NewProperty']					= 'wikibaserepo';
 	$wgSpecialPageGroups['ItemByTitle']					= 'wikibaserepo';
+	$wgSpecialPageGroups['GoToLinkedPage']					= 'wikibaserepo';
 	$wgSpecialPageGroups['ItemDisambiguation']			= 'wikibaserepo';
 	$wgSpecialPageGroups['ItemsWithoutSitelinks']		= 'wikibaserepo';
 	$wgSpecialPageGroups['SetLabel']					= 'wikibaserepo';
