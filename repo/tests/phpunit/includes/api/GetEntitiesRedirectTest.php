84ec72bbe1922bf13ab19e619c933f5e4ca6d236
daniel <daniel.kinzler@wikimedia.de>
2014-07-30 19:04:00 +0200

    Resolve redirects in wbgetentities.
    This adds a new parameter, noredirects, to disable automatic
    redirect resolution.
    
    Bug: 67031
    Change-Id: Ic5c4d211df29bafc64da3afa90b5260f20969356

---
+++ repo/tests/phpunit/includes/api/GetEntitiesRedirectTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Wikibase\Test\Api;
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\Lib\Store\EntityRedirect;
+use Wikibase\Repo\WikibaseRepo;
+
+/**
+ * @covers Wikibase\Api\GetEntities
+ *
+ * Test for redirect resolution in the wbgetentities API module
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ *
+ * @group API
+ * @group Wikibase
+ * @group WikibaseAPI
+ * @group WikibaseRepo
+ * @group Database
+ * @group medium
+ */
+class GetEntitiesRedirectTest extends WikibaseApiTestCase {
+
+	private function createEntity( $label ) {
+		$entity = Item::newEmpty();
+		$entity->setLabel( 'en', $label );
+
+		$store = WikibaseRepo::getDefaultInstance()->getEntityStore();
+		$rev = $store->saveEntity( $entity, 'GetEntitiesRedirectTest', $GLOBALS['wgUser'], EDIT_NEW );
+		$id =  $rev->getEntity()->getId();
+
+		return $id;
+	}
+
+	private function createEntityRedirect( EntityId $target ) {
+		$id = $this->createEntity( 'Dummy' );
+
+		$redirect = new EntityRedirect( $id, $target );
+
+		$store = WikibaseRepo::getDefaultInstance()->getEntityStore();
+		$store->saveRedirect( $redirect, 'GetEntitiesRedirectTest', $GLOBALS['wgUser'], EDIT_UPDATE );
+
+		return $id;
+	}
+
+	public function testResolveRedirect() {
+		$user =  $GLOBALS['wgUser'];
+
+		$targetId = $this->createEntity( 'GetEntitiesRedirectTest' );
+		$redirectId = $this->createEntityRedirect( $targetId );
+
+		$redirectKey = $redirectId->getSerialization();
+		$targetKey = $targetId->getSerialization();
+
+		// if redirect resolution is enabled, the redirect should be resolved
+		$params = array();
+		$params['action'] = 'wbgetentities';
+		$params['token'] = $user->getToken();
+		$params['ids'] = $redirectId->getSerialization();
+		list( $result,, ) = $this->doApiRequest( $params, null, false, $user );
+
+		$this->assertArrayHasKey( 'entities', $result );
+		$this->assertArrayHasKey( $redirectKey, $result['entities'], 'the id from the request should be used as a key in the result' );
+		$this->assertArrayNotHasKey( $targetKey, $result['entities'], 'the id from the request should be used as a key in the result' );
+		$this->assertArrayHasKey( 'labels', $result['entities'][$redirectKey], 'the redirect should be resolve to a full entity' );
+		$this->assertEquals( $targetKey, $result['entities'][$redirectKey]['id'], 'the entity id should be the id of the redirect target' );
+
+		// if redirect resolution is disabled, the redirect should be treated like a missing entity
+		$params['noredirects'] = '';
+		list( $result,, ) = $this->doApiRequest( $params, null, false, $user );
+
+		$this->assertArrayHasKey( 'entities', $result );
+		$this->assertArrayHasKey( $redirectKey, $result['entities'], 'the id from the request should be used as a key in the result' );
+		$this->assertArrayNotHasKey( $targetKey, $result['entities'], 'the id from the request should be used as a key in the result' );
+		$this->assertEquals( $redirectKey, $result['entities'][$redirectKey]['id'], 'the reported entitiy id should be the id of the redirect' );
+		$this->assertArrayHasKey( 'missing', $result['entities'][$redirectKey], 'the entity should be labeled as missing' );
+		$this->assertArrayNotHasKey( 'labels', $result['entities'][$redirectKey], 'the unresolved redirect should not have labels' );
+	}
+
+}
