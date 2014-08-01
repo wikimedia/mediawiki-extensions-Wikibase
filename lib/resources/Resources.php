9c2a188bf8569cf238666217eb065e8208860c97
Henning Snater <henning.snater@wikimedia.de>
2014-08-01 17:50:34 +0200

    Implemented pagesuggester widget
    (bug 68905)
    For now, $.wikibase.pagesuggester features the functionality extracted from SitePageInterface.
    
    Change-Id: I44a22407c6c31c4b6b1b5f91700c1b7381d0fdec

--- lib/resources/Resources.php
+++ lib/resources/Resources.php
@@ -324,6 +324,7 @@ return call_user_func( function() {
 				'jquery.ui.suggester',
 				'util.inherit',
 				'jquery.wikibase.entityselector',
+				'jquery.wikibase.pagesuggester',
 				'jquery.wikibase.toolbareditgroup',
 				'jquery.wikibase.siteselector',
 				'mediawiki.api',
@@ -500,6 +501,16 @@ return call_user_func( function() {
 				'jquery.ui.ooMenu',
 				'jquery.ui.suggester',
 			)
+		),
+
+		'jquery.wikibase.pagesuggester' => $moduleTemplate + array(
+			'scripts' => array(
+				'jquery.wikibase/jquery.wikibase.pagesuggester.js',
+			),
+			'dependencies' => array(
+				'jquery',
+				'jquery.ui.suggester',
+			),
 		),
 
 		'jquery.wikibase.listview' => $moduleTemplate + array(
