261f732320911f32c09693cbd85c827866ea0362
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2014-08-09 20:23:28 +0100

    Add DateFormatParser and MwDateFormatParserFactory
    This implements the reverse operation of the date formatter in the
    Language class.
    
    Bug: T63958
    Bug: T67722
    Bug: T95532
    Change-Id: Ic05c31051375d998c1691efcc2b48618b6352b36

---
+++ repo/includes/Parsers/DateFormatParser.php
@@ -0,0 +1,400 @@
+<?php
+
+namespace Wikibase\Repo\Parsers;
+
+use DataValues\IllegalValueException;
+use DataValues\TimeValue;
+use ValueParsers\ParseException;
+use ValueParsers\ParserOptions;
+use ValueParsers\StringValueParser;
+
+/**
+ * This parser is in essence the inverse operation of MediaWiki's Language::sprintfDate.
+ *
+ * @see Language::sprintfDate
+ *
+ * @since 0.5
+ *
+ * @licence GNU GPL v2+
+ * @author Thiemo Mättig
+ */
+class DateFormatParser extends StringValueParser {
+
+	const FORMAT_NAME = 'date-format';
+
+	const OPT_DATE_FORMAT = 'dateFormat';
+
+	/**
+	 * Option for unlocalizing non-canonical digits. Must be an array of strings, mapping canonical
+	 * digit characters ("1", "2" and so on, possibly including "." and ",") to localized
+	 * characters.
+	 */
+	const OPT_DIGIT_TRANSFORM_TABLE = 'digitTransformTable';
+
+	/**
+	 * Option for localized month names. Should be a two-dimensional array, the first dimension
+	 * mapping the month's numbers 1 to 12 to arrays of localized month names, possibly including
+	 * full month names, genitive names and abbreviations. Can also be a one-dimensional array of
+	 * strings.
+	 */
+	const OPT_MONTH_NAMES = 'monthNames';
+
+	public function __construct( ParserOptions $options = null ) {
+		parent::__construct( $options );
+
+		$this->defaultOption( self::OPT_DATE_FORMAT, 'j F Y' );
+		// FIXME: Should not be an option. Options should be trivial, never arrays or objects!
+		$this->defaultOption( self::OPT_DIGIT_TRANSFORM_TABLE, null );
+		$this->defaultOption( self::OPT_MONTH_NAMES, null );
+	}
+
+	/**
+	 * @see StringValueParser::stringParse
+	 *
+	 * @param string $value
+	 *
+	 * @throws ParseException
+	 * @return TimeValue
+	 */
+	protected function stringParse( $value ) {
+		$date = $this->parseDate( $value );
+		$precision = TimeValue::PRECISION_YEAR;
+		$time = [ $this->parseFormattedNumber( $date['year'] ), 0, 0, 0, 0, 0 ];
+
+		if ( isset( $date['month'] ) ) {
+			$precision = TimeValue::PRECISION_MONTH;
+			$time[1] = $this->findMonthMatch( $date );
+
+			if ( isset( $date['day'] ) ) {
+				$precision = TimeValue::PRECISION_DAY;
+				$time[2] = $this->parseFormattedNumber( $date['day'] );
+
+				if ( isset( $date['hour'] ) ) {
+					$precision = TimeValue::PRECISION_HOUR;
+					$time[3] = $this->parseFormattedNumber( $date['hour'] );
+
+					if ( isset( $date['minute'] ) ) {
+						$precision = TimeValue::PRECISION_MINUTE;
+						$time[4] = $this->parseFormattedNumber( $date['minute'] );
+
+						if ( isset( $date['second'] ) ) {
+							$precision = TimeValue::PRECISION_SECOND;
+							$time[5] = $this->parseFormattedNumber( $date['second'] );
+						}
+					}
+				}
+			}
+		}
+
+		$timestamp = vsprintf( '+%04s-%02s-%02sT%02s:%02s:%02sZ', $time );
+
+		try {
+			return new TimeValue( $timestamp, 0, 0, 0, $precision, TimeValue::CALENDAR_GREGORIAN );
+		} catch ( IllegalValueException $ex ) {
+			throw new ParseException( $ex->getMessage(), $value, self::FORMAT_NAME );
+		}
+	}
+
+	// @codingStandardsIgnoreStart
+	/**
+	 * @see Language::sprintfDate
+	 *
+	 * @param string $format A date format, as described in Language::sprintfDate.
+	 *
+	 * @return string Regular expression
+	 */
+	private function parseDateFormat( $format ) {
+		$length = strlen( $format );
+
+		$number = $this->getNumberPattern();
+		$notFollowedByNumber = '(?!' . $number . ')';
+		$optionalPunctuation = '\p{P}*';
+		$optionalWhitespace = '\p{Z}*';
+		$separation = $notFollowedByNumber . $optionalWhitespace;
+		$pattern = '<^' . $optionalWhitespace;
+
+		for ( $p = 0; $p < $length; $p++ ) {
+			$code = $format[$p];
+
+			if ( $code === 'x' && $p < $length - 1 ) {
+				$code .= $format[++$p];
+
+				if ( preg_match( '<^x[ijkmot]$>', $code ) && $p < $length - 1 ) {
+					$code .= $format[++$p];
+				}
+			}
+
+			switch ( $code ) {
+				// Year
+				case 'o':
+				case 'xiY':
+				case 'xjY':
+				case 'xkY':
+				case 'xmY':
+				case 'xoY':
+				case 'xtY':
+				case 'Y':
+					$pattern .= '(?P<year>' . $number . '+)' . $separation;
+					break;
+
+				// Month
+				case 'F':
+				case 'M':
+				case 'm':
+				case 'n':
+				case 'xg':
+				case 'xiF':
+				case 'xin':
+				case 'xjF':
+				case 'xjn':
+				case 'xjx':
+				case 'xmF':
+				case 'xmn':
+					$pattern .= '(?P<month>' . $number . '{1,2}' . $notFollowedByNumber
+						. $this->getMonthNamesPattern() . ')' . $optionalPunctuation
+						. $optionalWhitespace;
+					break;
+
+				// Day
+				case 'd':
+				case 'j':
+				case 'xij':
+				case 'xjj':
+				case 'xjt':
+				case 'xmj':
+					$pattern .= '(?P<day>' . $number . '{1,2})' . $optionalPunctuation
+						. $separation;
+					break;
+
+				// Hour
+				case 'G':
+				case 'H':
+					$pattern .= '(?P<hour>' . $number . '{1,2})' . $separation;
+					break;
+
+				// Minute
+				case 'i':
+					$pattern .= '(?P<minute>' . $number . '{1,2})' . $separation;
+					break;
+
+				// Second
+				case 's':
+					$pattern .= '(?P<second>' . $number . '{1,2})' . $separation;
+					break;
+
+				// Escaped "x"
+				case 'xx':
+					$pattern .= 'x';
+					break;
+
+				// Escaped character or backslash at the end of the sequence
+				case '\\':
+					$pattern .= preg_quote( $p < $length - 1 ? $format[++$p] : '\\' );
+					break;
+
+				// Quoted sequence
+				case '"':
+					$endQuote = strpos( $format, '"', $p + 1 );
+					if ( $endQuote !== false ) {
+						$pattern .= preg_quote( substr( $format, $p + 1, $endQuote - $p - 1 ) );
+						$p = $endQuote;
+					} else {
+						$pattern .= '"';
+					}
+					break;
+
+				// We can ignore "raw" and "raw toggle" when parsing, because we always accept
+				// canonical digits.
+				case 'xN':
+				case 'xn':
+					break;
+
+				// 12-hour format
+				case 'A':
+				case 'a':
+				case 'g':
+				case 'h':
+
+				// Full, formatted dates
+				case 'c':
+				case 'r':
+				case 'U':
+
+				// Day of the week
+				case 'D':
+				case 'l':
+				case 'N':
+				case 'w':
+
+				// Timezone
+				case 'e':
+				case 'O':
+				case 'P':
+				case 'T':
+				case 'Z':
+
+				// Daylight saving time ("1" if true)
+				case 'I':
+
+				// Leap year ("1" if true)
+				case 'L':
+
+				// Number of days in the current month
+				case 't':
+				case 'xit':
+
+				// Week number
+				case 'W':
+
+				// "Roman" and "Hebrew" modifiers
+				case 'xh':
+				case 'xr':
+
+				// 2-digit year
+				case 'xiy':
+				case 'y':
+
+				// Day of the year
+				case 'xiz':
+				case 'z':
+					throw new ParseException( 'Unsupported date format "' . $code . '"' );
+					break;
+
+				// Character with no meaning
+				default:
+					if ( preg_match( '<^\p{P}+$>u', $format[$p] ) ) {
+						// Unicode character class "P" is for "Punctuation".
+						$pattern .= $optionalPunctuation;
+					} elseif ( preg_match( '<^\p{Z}+$>u', $format[$p] ) ) {
+						// Unicode character class "Z" or "Separator" is for whitespace.
+						$pattern .= $optionalWhitespace;
+					} else {
+						$pattern .= preg_quote( $format[$p] );
+					}
+			}
+		}
+
+		return $pattern . '$>iu';
+	}
+	// @codingStandardsIgnoreEnd
+
+	/**
+	 * @return string Partial regular expression
+	 */
+	private function getNumberPattern() {
+		$pattern = '[\d';
+
+		$transformTable = $this->getDigitTransformTable();
+		if ( is_array( $transformTable ) ) {
+			$pattern .= preg_quote( implode( '', $transformTable ) );
+		}
+
+		return $pattern . ']';
+	}
+
+	/**
+	 * @return string Partial regular expression
+	 */
+	private function getMonthNamesPattern() {
+		$pattern = '';
+
+		foreach ( $this->getMonthNames() as $i => $monthNames ) {
+			$pattern .= '|(?P<month' . $i . '>'
+				. implode( '|', array_map( 'preg_quote', (array)$monthNames ) )
+				. ')';
+		}
+
+		return $pattern;
+	}
+
+	/**
+	 * @param string $input
+	 *
+	 * @throws ParseException
+	 * @return string[] Guaranteed to have the "year" key, optionally followed by more elements.
+	 *  Guaranteed to be continuous, e.g. "year" and "day" with no "month" is illegal.
+	 */
+	private function parseDate( $input ) {
+		$pattern = $this->parseDateFormat( $this->getDateFormat() );
+
+		$reportingLevel = error_reporting();
+		error_reporting( $reportingLevel & ~E_WARNING );
+		$success = preg_match( $pattern, $input, $matches );
+		error_reporting( $reportingLevel );
+
+		if ( !$success ) {
+			throw new ParseException(
+				$success === false
+					? 'Illegal date format "' . $this->getDateFormat() . '"'
+					: 'Failed to parse "' . $input . '"',
+				$input,
+				self::FORMAT_NAME
+			);
+		}
+
+		if ( !isset( $matches['year'] )
+			|| isset( $matches['day'] ) && !isset( $matches['month'] )
+			|| isset( $matches['hour'] ) && !isset( $matches['day'] )
+			|| isset( $matches['minute'] ) && !isset( $matches['hour'] )
+			|| isset( $matches['second'] ) && !isset( $matches['minute'] )
+		) {
+			throw new ParseException( 'Non-continuous date format', $input, self::FORMAT_NAME );
+		}
+
+		return $matches;
+	}
+
+	/**
+	 * @param string[] $matches
+	 *
+	 * @return int|string
+	 */
+	private function findMonthMatch( $matches ) {
+		for ( $i = 1; $i <= 12; $i++ ) {
+			if ( !empty( $matches['month' . $i] ) ) {
+				return $i;
+			}
+		}
+
+		return $this->parseFormattedNumber( $matches['month'] );
+	}
+
+	/**
+	 * @param string $number
+	 *
+	 * @return string Canonical number
+	 */
+	private function parseFormattedNumber( $number ) {
+		$transformTable = $this->getDigitTransformTable();
+
+		if ( is_array( $transformTable ) ) {
+			// Eliminate empty array values (bug T66347).
+			$transformTable = array_filter( $transformTable );
+			$number = strtr( $number, array_flip( $transformTable ) );
+		}
+
+		return $number;
+	}
+
+	/**
+	 * @return string
+	 */
+	private function getDateFormat() {
+		return $this->getOption( self::OPT_DATE_FORMAT );
+	}
+
+	/**
+	 * @return string[]|null
+	 */
+	private function getDigitTransformTable() {
+		return $this->getOption( self::OPT_DIGIT_TRANSFORM_TABLE );
+	}
+
+	/**
+	 * @return array[]|string[]
+	 */
+	private function getMonthNames() {
+		return $this->getOption( self::OPT_MONTH_NAMES ) ?: [];
+	}
+
+}
---
+++ repo/tests/phpunit/includes/Parsers/MwDateFormatParserFactoryTest.php
@@ -0,0 +1,306 @@
+<?php
+
+namespace Wikibase\Repo\Tests\Parsers;
+
+use DataValues\TimeValue;
+use InvalidArgumentException;
+use Language;
+use PHPUnit_Framework_TestCase;
+use ValueParsers\ParseException;
+use ValueParsers\ParserOptions;
+use ValueParsers\ValueParser;
+use Wikibase\Repo\Parsers\DateFormatParser;
+use Wikibase\Repo\Parsers\MwDateFormatParserFactory;
+
+/**
+ * @covers Wikibase\Repo\Parsers\MwDateFormatParserFactory
+ *
+ * @group ValueParsers
+ * @group WikibaseRepo
+ * @group Wikibase
+ * @group TimeParsers
+ *
+ * @licence GNU GPL v2+
+ * @author Thiemo Mättig
+ */
+class MwDateFormatParserFactoryTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * @var MwDateFormatParserFactory
+	 */
+	private $factory;
+
+	protected function setUp() {
+		$this->factory = new MwDateFormatParserFactory();
+	}
+
+	public function testGetMwDateFormatParser() {
+		$parser = $this->factory->getMwDateFormatParser();
+		$this->assertInstanceOf( ValueParser::class, $parser );
+	}
+
+	public function testGetMwDateFormatParserWithParameters() {
+		$parser = $this->factory->getMwDateFormatParser( 'en', 'dmy', 'date' );
+		$this->assertInstanceOf( ValueParser::class, $parser );
+	}
+
+	public function testGetMwDateFormatParserWithInvalidLanguageCode() {
+		$this->setExpectedException( InvalidArgumentException::class );
+		$this->factory->getMwDateFormatParser( null );
+	}
+
+	public function testGetMwDateFormatParserWithInvalidFormat() {
+		$this->setExpectedException( InvalidArgumentException::class );
+		$this->factory->getMwDateFormatParser( 'en', null );
+	}
+
+	public function testGetMwDateFormatParserWithInvalidFormatType() {
+		$this->setExpectedException( InvalidArgumentException::class );
+		$this->factory->getMwDateFormatParser( 'en', 'dmy', null );
+	}
+
+	private function getLanguageCodes() {
+		// Focus on a critical subset of languages. Enable the following MediaWiki dependency to
+		// test the full set of all 400+ supported languages. This may take several minutes.
+		// return array_keys( Language::fetchLanguageNames() );
+		return [
+			'ace',
+			'anp',
+			'bo',
+			'de',
+			'en',
+			'fa', // right-to-left
+			'gan',
+			'haw',
+			'krj',
+			'ln',
+			'lzh', // Chinese
+			'lzz',
+			'nn',
+			'pt',
+			'sma',
+			'sv',
+			'ty',
+			'udm',
+			'vi',
+			'zh-hans', // Chinese
+			'zh-hant', // Chinese
+		];
+	}
+
+	public function validInputProvider() {
+		$dateFormatPreferences = [
+			'mdy' => TimeValue::PRECISION_MINUTE,
+			'dmy' => TimeValue::PRECISION_MINUTE,
+			'ymd' => TimeValue::PRECISION_MINUTE,
+			'ISO 8601' => TimeValue::PRECISION_SECOND,
+		];
+		$dateFormatTypes = [
+			'date' => TimeValue::PRECISION_DAY,
+			'monthonly' => TimeValue::PRECISION_MONTH,
+			'both' => null,
+		];
+
+		$cases = [];
+
+		foreach ( $this->getLanguageCodes() as $languageCode ) {
+			$language = Language::factory( $languageCode );
+
+			foreach ( $dateFormatPreferences as $dateFormatPreference => $maximumPrecision ) {
+				foreach ( $dateFormatTypes as $dateFormatType => $precision ) {
+					$mwTimestamp = $this->generateMwTimestamp();
+
+					$dateFormat = $language->getDateFormatString( $dateFormatType, $dateFormatPreference );
+					$input = $language->sprintfDate( $dateFormat, $mwTimestamp );
+
+					if ( $precision === null ) {
+						$precision = $maximumPrecision;
+					}
+					$expected = new TimeValue(
+						$this->getIsoTimestamp( $mwTimestamp, $precision ),
+						0, 0, 0,
+						$precision,
+						TimeValue::CALENDAR_GREGORIAN
+					);
+
+					$cases[] = [
+						$input,
+						$expected,
+						$languageCode,
+						$dateFormatPreference,
+						$dateFormatType
+					];
+				}
+			}
+		}
+
+		return $cases;
+	}
+
+	/**
+	 * @dataProvider validInputProvider
+	 */
+	public function testParseWithValidInputs(
+		$input,
+		TimeValue $expected,
+		$languageCode,
+		$dateFormatPreference,
+		$dateFormatType
+	) {
+		$parser = $this->factory->getMwDateFormatParser(
+			$languageCode,
+			$dateFormatPreference,
+			$dateFormatType
+		);
+		$parsed = $parser->parse( $input );
+		$this->assertTrue( $expected->equals( $parsed ), $input . ' became ' . $parsed );
+	}
+
+	/**
+	 * @return string
+	 */
+	private function generateMwTimestamp() {
+		static $mwTimestamps;
+
+		if ( $mwTimestamps === null ) {
+			$mwTimestamps = [
+				'12010304054201',
+				'19701110064300',
+				'20301231234500',
+			];
+
+			for ( $i = 1; $i <= 12; $i++ ) {
+				$mwTimestamps[] = sprintf( '2014%02d01224400', $i );
+			}
+		}
+
+		$mwTimestamp = next( $mwTimestamps );
+
+		if ( !$mwTimestamp ) {
+			$mwTimestamp = reset( $mwTimestamps );
+		}
+
+		return $mwTimestamp;
+	}
+
+	/**
+	 * @param string $mwTimestamp
+	 * @param int $precision
+	 *
+	 * @return string
+	 */
+	private function getIsoTimestamp( $mwTimestamp, $precision ) {
+		if ( $precision <= TimeValue::PRECISION_YEAR ) {
+			$mwTimestamp = substr( $mwTimestamp, 0, 4 ) . '0000000000';
+		} elseif ( $precision === TimeValue::PRECISION_MONTH ) {
+			$mwTimestamp = substr( $mwTimestamp, 0, 6 ) . '00000000';
+		} elseif ( $precision === TimeValue::PRECISION_DAY ) {
+			$mwTimestamp = substr( $mwTimestamp, 0, 8 ) . '000000';
+		} elseif ( $precision === TimeValue::PRECISION_HOUR ) {
+			$mwTimestamp = substr( $mwTimestamp, 0, 10 ) . '0000';
+		} elseif ( $precision === TimeValue::PRECISION_MINUTE ) {
+			$mwTimestamp = substr( $mwTimestamp, 0, 12 ) . '00';
+		}
+
+		return preg_replace( '/(....)(..)(..)(..)(..)(..)/s', '+$1-$2-$3T$4:$5:$6Z', $mwTimestamp );
+	}
+
+	public function invalidInputProvider() {
+		return [
+			[ null ],
+			[ true ],
+			[ 2015 ],
+		];
+	}
+
+	/**
+	 * @dataProvider invalidInputProvider
+	 */
+	public function testParseWithInvalidInputs( $input ) {
+		$parser = $this->factory->getMwDateFormatParser();
+		$this->setExpectedException( ParseException::class );
+		$parser->parse( $input );
+	}
+
+	/**
+	 * This test is supposed to break the moment Language::sprintfDate starts supporting one of the
+	 * currently unused character sequences listed in the provider. When this happens, remove the
+	 * sequence here and add it to the DateFormatParser base class.
+	 *
+	 * @dataProvider nonSpecialFormatStringProvider
+	 */
+	public function testNonSpecialFormatStringFeatureParity( $format, $expected ) {
+		$en = Language::factory( 'en' );
+		$formatted = $en->sprintfDate( $format, '20140901123040' );
+		$this->assertSame( $expected, $formatted );
+	}
+
+	/**
+	 * This test is supposed to break the moment the DateFormatParser base class starts supporting
+	 * one of the character sequences listed in the provider. When this happens, make sure this is
+	 * intended and in parity with Language::sprintfDate and remove the sequence here.
+	 *
+	 * @dataProvider nonSpecialFormatStringProvider
+	 */
+	public function testNonSpecialFormatStringRoundtrip( $format, $expected ) {
+		$parser = new DateFormatParser( new ParserOptions( [
+			DateFormatParser::OPT_DATE_FORMAT => 'Y ' . $format,
+		] ) );
+		$timeValue = $parser->parse( '2014 ' . $expected );
+		$this->assertInstanceOf( TimeValue::class, $timeValue );
+	}
+
+	public function nonSpecialFormatStringProvider() {
+		return [
+			[
+				'B C E J K Q R S V X',
+				'B C E J K Q R S V X'
+			],
+			[
+				'b f k p q u v',
+				'b f k p q u v'
+			],
+			[
+				'XB XC XE XJ XK XQ XR XS XV XX',
+				'XB XC XE XJ XK XQ XR XS XV XX'
+			],
+			[
+				'xa xb xc xd xe xf xp xl xq xs xu xv xw xy xz',
+				'a b c d e f p l q s u v w y z'
+			],
+			[
+				'xiA xiB xiC xiD xiE xiG xiH xiI xiJ xiK xiL xiM xiN xiO xiQ xiR xiS xiT xiU xiV xiW xiX',
+				'A B C D E G H I J K L M N O Q R S T U V W X',
+			],
+			[
+				'xia xib xic xid xie xif xig xih xii xik xil xim xio xip xiq xir xis xiu xiv xiw xix',
+				'a b c d e f g h i k l m o p q r s u v w x',
+			],
+			[
+				'xjA xjB xjC xjD xjE xjG xjH xjI xjJ xjK xjL xjM xjN xjO xjP xjQ xjR xjS xjT xjU xjV xjW xjX',
+				'A B C D E G H I J K L M N O P Q R S T U V W X',
+			],
+			[
+				'xja xjb xjc xjd xje xjf xjg xjh xji xjk xjl xjm xjo xjp xjq xjr xjs xju xjv xjw xjy xjz',
+				'a b c d e f g h i k l m o p q r s u v w y z',
+			],
+			[
+				'xkA xkB xkC xkD xkE xkF xkG xkH xkI xkJ xkK xkL xkM xkN xkO xkP xkQ xkR xkS xkT xkU xkV xkW xkX xkZ',
+				'A B C D E F G H I J K L M N O P Q R S T U V W X Z',
+			],
+			[
+				'xka xkb xkc xkd xke xkf xkg xkh xki xkj xkk xkl xkm xkn xko xkp xkq xkr xks xkt xku xkv xkw xkx xky xkz',
+				'a b c d e f g h i j k l m n o p q r s t u v w x y z',
+			],
+			[
+				'xmA xmB xmC xmD xmE xmG xmH xmI xmJ xmK xmL xmM xmN xmO xmP xmQ xmR xmS xmT xmU xmV xmW xmX xmZ',
+				'A B C D E G H I J K L M N O P Q R S T U V W X Z',
+			],
+			[
+				'xma xmb xmc xmd xme xmf xmg xmh xmi xmk xml xmm xmo xmp xmq xmr xms xmt xmu xmv xmw xmx xmy xmz',
+				'a b c d e f g h i k l m o p q r s t u v w x y z',
+			],
+		];
+	}
+
+}
