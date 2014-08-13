0107ce96d25ca0412acfe55bf8fb9f162f98fbb2
bene <benestar.wikimedia@gmail.com>
2014-08-14 00:10:27 +0200

    Refactor Fingerprint code out of EntityView
    All code related to fingerprint is moved into a separate FingerprintView
    which only knows about a fingerprint and an entity id. This helps us
    getting rid of EntityView at one point.
    
    Change-Id: Ib932282be071c20455045f32e60f0244c4623980

---
+++ repo/tests/phpunit/includes/View/FingerprintViewTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Wikibase\Test;
+
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Term\Fingerprint;
+use Wikibase\Repo\View\FingerprintView;
+use Wikibase\Repo\View\SectionEditLinkGenerator;
+
+/**
+ * @covers Wikibase\Repo\View\FingerprintView
+ *
+ * @group Wikibase
+ * @group WikibaseRepo
+ *
+ * @licence GNU GPL v2+
+ * @author Bene* < benestar.wikimedia@gmail.com >
+ */
+class FingerprintViewTest extends \PHPUnit_Framework_TestCase {
+
+	public function provideTestGetHtml() {
+		$cases = array();
+
+		$fingerprint = Fingerprint::newEmpty();
+
+		$cases['empty fingerprint'] = array(
+			$fingerprint,
+			new ItemId( 'Q42' ),
+			'en'
+		);
+
+		$fingerprint->setLabel( 'en', 'Foobar' );
+		$fingerprint->setDescription( 'en', 'This is a foo bar.' );
+		$fingerprint->setAliasGroup( 'en', array( 'foo', 'bar' ) );
+
+		$cases['empty fingerprint'] = array(
+			$fingerprint,
+			new ItemId( 'Q42' ),
+			'en'
+		);
+
+		$cases['other language'] = array(
+			$fingerprint,
+			new ItemId( 'Q42' ),
+			'de'
+		);
+
+		$cases['other item id'] = array(
+			$fingerprint,
+			new ItemId( 'Q12' ),
+			'en'
+		);
+
+		return $cases;
+	}
+
+	/**
+	 * @dataProvider provideTestGetHtml
+	 */
+	public function testGetHtmlEditable( Fingerprint $fingerprint, EntityId $entityId, $languageCode ) {
+		$fingerprintView = new FingerprintView( new SectionEditLinkGenerator(), $languageCode );
+		$html = $fingerprintView->getHtml( $fingerprint, $entityId, true );
+		$serializedId = $entityId->getSerialization();
+
+		$this->assertRegExp( '@<a href="[^"]*\bSpecial:SetLabel/' . $serializedId . '/' . $languageCode . '"[^>]*>\S+</a>@', $html );
+		$this->assertRegExp( '@<a href="[^"]*\bSpecial:SetDescription/' . $serializedId . '/' . $languageCode . '"[^>]*>\S+</a>@', $html );
+		$this->assertRegExp( '@<a href="[^"]*\bSpecial:SetAliases/' . $serializedId . '/' . $languageCode . '"[^>]*>\S+</a>@', $html );
+	}
+
+	/**
+	 * @dataProvider provideTestGetHtml
+	 */
+	public function testGetHtmlNotEditable( Fingerprint $fingerprint, EntityId $entityId, $languageCode ) {
+		$fingerprintView = new FingerprintView( new SectionEditLinkGenerator(), $languageCode );
+		$html = $fingerprintView->getHtml( $fingerprint, $entityId, false );
+
+		$this->assertNotContains( '<a ', $html );
+	}
+
+	public function testGetHtmlNoEntityId() {
+		$fingerprintView = new FingerprintView( new SectionEditLinkGenerator(), 'en' );
+		$html = $fingerprintView->getHtml( Fingerprint::newEmpty(), null, true );
+
+		$this->assertNotContains( '<a ', $html );
+	}
+
+}
