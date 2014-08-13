aa6a3f584f08e1913ef594c07b57b74733fdfd2e
bene <benestar.wikimedia@gmail.com>
2014-08-13 21:10:03 +0200

    Make PropertyContentTest work with DataModel 1.0
    In DataModel 1.0 Properties do not contain claims any more so we have to
    move all claim related stuff into the ItemHandlerTest.
    
    Change-Id: Ib3d761912e5721e63d14e4565c8a272ab4164f50

--- repo/tests/phpunit/includes/content/EntityContentTest.php
+++ repo/tests/phpunit/includes/content/EntityContentTest.php
@@ -167,16 +167,6 @@ abstract class EntityContentTest extends \MediaWikiTestCase {
 			array( 'wb-status' => EntityContent::STATUS_STUB, 'wb-claims' => 0 )
 		);
 
-		$contentWithClaim = $this->newEmpty();
-		$claim = new Statement( new PropertyNoValueSnak( 83 ) );
-		$claim->setGuid( '$testing$' );
-		$contentWithClaim->getEntity()->addClaim( $claim );
-
-		$cases['claims'] = array(
-			$contentWithClaim,
-			array( 'wb-claims' => 1 )
-		);
-
 		return $cases;
 	}
 
@@ -197,11 +187,6 @@ abstract class EntityContentTest extends \MediaWikiTestCase {
 		$contentWithLabel = $this->newEmpty();
 		$contentWithLabel->getEntity()->setLabel( 'de', 'xyz' );
 
-		$contentWithClaim = $this->newEmpty();
-		$claim = new Statement( new PropertyNoValueSnak( 83 ) );
-		$claim->setGuid( '$testing$' );
-		$contentWithClaim->getEntity()->addClaim( $claim );
-
 		return array(
 			'empty' => array(
 				$this->newEmpty(),
@@ -210,10 +195,6 @@ abstract class EntityContentTest extends \MediaWikiTestCase {
 			'labels' => array(
 				$contentWithLabel,
 				EntityContent::STATUS_STUB
-			),
-			'claims' => array(
-				$contentWithClaim,
-				EntityContent::STATUS_NONE
 			),
 		);
 	}
