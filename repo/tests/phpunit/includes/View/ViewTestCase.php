b5f4d4f6c224ba2e1c64a448bf1b4e2f9c569351
bene <benestar.wikimedia@gmail.com>
2014-08-15 22:19:29 +0200

    Refactor claims code out of EntityView
    All code related to claims is moved into a separate ClaimsView which only
    knows about a list of claims. This helps us getting rid of EntityView.
    
    Note that lots of test code is copied from EntityViewTest which however
    will be deleted once we remove EntityView so that there is no duplicate
    code in the long term. Furthermore, the tests do not actually test
    anything further than that the output is actually HTML. The ClaimsViewTest
    can also be improved in another patch as this one would grow too big.
    
    A problem at the moment is the ClaimHtmlGenerator which also accepts
    statements which extend claims at the moment. However, when this will
    change we may think of renaming this to StatementsView and only accept
    statements. Or we find another way of using this to display both claims
    and statements.
    
    In future ClaimsView should perhaps only be created in ItemView and be
    injected via the constructor. However, this can be done in another patch
    as this one would get too big. To actually kill EntityView we have to
    modify the Content classes (EntityContent, ItemContent and
    PropertyContent) atm.
    
    Change-Id: Iee15ec58f69abac9fb6a85c23d6196a309d1a651

---
+++ repo/tests/phpunit/includes/View/ViewTestCase.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Description of ViewTestCase
+ *
+ * @author Bene* < benestar.wikimedia@gmail.com >
+ */
+abstract class ViewTestCase extends \MediaWikiLangTestCase {
+
+	protected function assertIsValidHtml( $html, $message = '' ) {
+		// Using a DOM document to parse HTML output:
+		$doc = new \DOMDocument();
+
+		// Disable default error handling in order to catch warnings caused by malformed markup:
+		libxml_use_internal_errors( true );
+
+		// Try loading the HTML:
+		$this->assertTrue( $doc->loadHTML( $html ), $message );
+
+		// Check if no warnings have been thrown:
+		$errorString = '';
+		foreach( libxml_get_errors() as $error ) {
+			$errorString .= "\r\n" . $error->message;
+		}
+
+		$this->assertEmpty( $errorString, 'Malformed markup:' . $errorString . ' ' . $message );
+
+		// Clear error cache and re-enable default error handling:
+		libxml_clear_errors();
+		libxml_use_internal_errors();
+	}
+	
+}
