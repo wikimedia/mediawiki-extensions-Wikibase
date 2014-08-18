da5ecedeb3a7a44b15831ea6b6d75323d2c8cc53
Henning Snater <henning.snater@wikimedia.de>
2014-08-18 11:53:27 +0200

    Refactored ResourceLoader QUnit test modules
    - Split up modules containing more than one test file.
    - Split up list of module definitions into separate files.
    - Evaluated use of newWbEnvironment/newMwEnvironment
    
    Change-Id: Id019051db23cb0f2c8b4d3c605dd1a94a9b27885

---
+++ lib/tests/qunit/jquery.wikibase/resources.php
@@ -0,0 +1,208 @@
+<?php
+
+/**
+ * @licence GNU GPL v2+
+ * @author H. Snater < mediawiki@snater.com >
+ *
+ * @codeCoverageIgnoreStart
+ */
+return call_user_func( function() {
+	$remoteExtPathParts = explode(
+		DIRECTORY_SEPARATOR . 'extensions' . DIRECTORY_SEPARATOR, __DIR__, 2
+	);
+	$moduleBase = array(
+		'localBasePath' => __DIR__,
+		'remoteExtPath' => $remoteExtPathParts[1],
+	);
+
+	$modules = array(
+
+		'jquery.wikibase.aliasesview.tests' => $moduleBase + array(
+			'scripts' => array(
+				'jquery.wikibase.aliasesview.tests.js',
+			),
+			'dependencies' => array(
+				'jquery.wikibase.aliasesview'
+			),
+		),
+
+		'jquery.wikibase.claimgrouplabelscroll.tests' => $moduleBase + array(
+			'scripts' => array(
+				'jquery.wikibase.claimgrouplabelscroll.tests.js',
+			),
+			'dependencies' => array(
+				'jquery.wikibase.claimgrouplabelscroll'
+			),
+		),
+
+		'jquery.wikibase.claimview.tests' => $moduleBase + array(
+			'scripts' => array(
+				'jquery.wikibase.claimview.tests.js',
+			),
+			'dependencies' => array(
+				'dataValues.values',
+				'jquery.valueview',
+				'jquery.wikibase.claimview',
+				'mediawiki.Title',
+				'valueFormatters',
+				'wikibase.AbstractedRepoApi',
+				'wikibase.datamodel',
+				'wikibase.RepoApi',
+				'wikibase.store.EntityStore',
+				'wikibase.store.FetchedContent',
+				'wikibase.ValueViewBuilder',
+			),
+		),
+
+		'jquery.wikibase.descriptionview.tests' => $moduleBase + array(
+			'scripts' => array(
+				'jquery.wikibase.descriptionview.tests.js',
+			),
+			'dependencies' => array(
+				'jquery.wikibase.descriptionview',
+			),
+		),
+
+		'wikibase.utilities.entityselector.tests' => $moduleBase + array(
+			'scripts' => array(
+				'jquery.wikibase.entityselector.tests.js',
+			),
+			'dependencies' => array(
+				'jquery.wikibase.entityselector',
+			),
+		),
+
+		'jquery.wikibase.labelview.tests' => $moduleBase + array(
+			'scripts' => array(
+				'jquery.wikibase.labelview.tests.js',
+			),
+			'dependencies' => array(
+				'jquery.wikibase.labelview',
+			),
+		),
+
+		'jquery.wikibase.listview.tests' => $moduleBase + array(
+			'scripts' => array(
+				'jquery.wikibase.listview.tests.js',
+			),
+			'dependencies' => array(
+				'jquery.wikibase.listview',
+			),
+		),
+
+		'jquery.wikibase.pagesuggester.tests' => $moduleBase + array(
+			'scripts' => array(
+				'jquery.wikibase.pagesuggester.tests.js'
+			),
+			'dependencies' => array(
+				'jquery.wikibase.pagesuggester',
+			),
+		),
+
+		'jquery.wikibase.referenceview.tests' => $moduleBase + array(
+			'scripts' => array(
+				'jquery.wikibase.referenceview.tests.js',
+			),
+			'dependencies' => array(
+				'jquery.valueview.ExpertStore',
+				'jquery.wikibase.referenceview',
+				'mediawiki.Title',
+				'wikibase.AbstractedRepoApi',
+				'wikibase.datamodel',
+				'wikibase.RepoApi',
+				'wikibase.store.FetchedContent',
+				'wikibase.store.EntityStore',
+				'wikibase.ValueViewBuilder',
+				'valueFormatters'
+			),
+		),
+
+		'jquery.wikibase.sitelinkgroupview.tests' => $moduleBase + array(
+			'scripts' => array(
+				'jquery.wikibase.sitelinkgroupview.tests.js',
+			),
+			'dependencies' => array(
+				'jquery.wikibase.sitelinkgroupview',
+				'wikibase.datamodel',
+				'wikibase.store.EntityStore',
+				'wikibase.tests.qunit.testrunner',
+			),
+		),
+
+		'jquery.wikibase.sitelinklistview.tests' => $moduleBase + array(
+			'scripts' => array(
+				'jquery.wikibase.sitelinklistview.tests.js',
+			),
+			'dependencies' => array(
+				'jquery.wikibase.sitelinklistview',
+				'wikibase.datamodel',
+				'wikibase.store.EntityStore',
+				'wikibase.tests.qunit.testrunner',
+			),
+		),
+
+		'jquery.wikibase.sitelinkview.tests' => $moduleBase + array(
+			'scripts' => array(
+				'jquery.wikibase.sitelinkview.tests.js',
+			),
+			'dependencies' => array(
+				'jquery.wikibase.sitelinkview',
+				'wikibase.datamodel',
+				'wikibase.store.EntityStore',
+				'wikibase.tests.qunit.testrunner',
+			),
+		),
+
+		'jquery.wikibase.siteselector.tests' => $moduleBase + array(
+			'scripts' => array(
+				'jquery.wikibase.siteselector.tests.js',
+			),
+			'dependencies' => array(
+				'jquery.wikibase.siteselector',
+				'wikibase.Site',
+			),
+		),
+
+		'jquery.wikibase.snaklistview.tests' => $moduleBase + array(
+			'scripts' => array(
+				'jquery.wikibase.snaklistview.tests.js',
+			),
+			'dependencies' => array(
+				'jquery.wikibase.snaklistview',
+				'wikibase.store.EntityStore',
+				'wikibase.store.FetchedContent',
+				'wikibase.ValueViewBuilder',
+				'wikibase.datamodel',
+				'mediawiki.Title',
+				'jquery.valueview',
+				'valueFormatters'
+			),
+		),
+
+		'jquery.wikibase.statementview.RankSelector.tests' => $moduleBase + array(
+			'scripts' => array(
+				'jquery.wikibase.statementview.RankSelector.tests.js',
+			),
+			'dependencies' => array(
+				'jquery.wikibase.statementview',
+				'wikibase.datamodel',
+			),
+		),
+
+		'jquery.wikibase.wbtooltip.tests' => $moduleBase + array(
+			'scripts' => array(
+				'jquery.wikibase.wbtooltip.tests.js',
+			),
+			'dependencies' => array(
+				'jquery.wikibase.wbtooltip',
+			),
+		),
+
+	);
+
+	return array_merge(
+		$modules,
+		include( __DIR__ . '/toolbar/resources.php' )
+	);
+
+} );
