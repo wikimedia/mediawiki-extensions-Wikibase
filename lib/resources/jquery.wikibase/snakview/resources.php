a1b877f66ef7087c781731d3aef572fddf386339
Henning Snater <henning.snater@wikimedia.de>
2014-08-18 14:46:46 +0200

    Refactored ResourceLoader module definitions
    Split up module definitions into separate files per directory.
    
    Change-Id: Ia712356c1904056ec8835128c907f11d2fd6cbd9

---
+++ lib/resources/jquery.wikibase/snakview/resources.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * @licence GNU GPL v2+
+ * @author H. Snater < mediawiki@snater.com >
+ *
+ * @codeCoverageIgnoreStart
+ */
+return call_user_func( function() {
+	$remoteExtPathParts = explode(
+		DIRECTORY_SEPARATOR . 'extensions' . DIRECTORY_SEPARATOR, __DIR__, 2
+	);
+	$moduleTemplate = array(
+		'localBasePath' => __DIR__,
+		'remoteExtPath' => $remoteExtPathParts[1],
+	);
+
+	$modules = array(
+
+		'jquery.wikibase.snakview' => $moduleTemplate + array(
+			'scripts' => array(
+				'snakview.js',
+				'snakview.SnakTypeSelector.js',
+			),
+			'styles' => array(
+				'themes/default/snakview.SnakTypeSelector.css',
+			),
+			'dependencies' => array(
+				'jquery.event.special.eachchange',
+				'jquery.NativeEventHandler',
+				'jquery.ui.position',
+				'jquery.ui.TemplatedWidget',
+				'jquery.wikibase.entityselector',
+				'jquery.wikibase.snakview.variations',
+				'jquery.wikibase.snakview.variations.NoValue',
+				'jquery.wikibase.snakview.variations.SomeValue',
+				'jquery.wikibase.snakview.variations.Value',
+				'jquery.wikibase.snakview.ViewState',
+				'mediawiki.legacy.shared',
+				'mw.config.values.wbRepo',
+				'wikibase.datamodel',
+				'wikibase.utilities',
+			),
+			'messages' => array(
+				'wikibase-snakview-property-input-placeholder',
+				'wikibase-snakview-choosesnaktype',
+				'wikibase-snakview-snaktypeselector-value',
+				'wikibase-snakview-snaktypeselector-somevalue',
+				'wikibase-snakview-snaktypeselector-novalue'
+			),
+		),
+
+		'jquery.wikibase.snakview.variations' => $moduleTemplate + array(
+			'scripts' => array(
+				'snakview.variations.js',
+			),
+			'dependencies' => array(
+				'jquery.wikibase.snakview.variations.Variation',
+				'util.inherit',
+			),
+		),
+
+		'jquery.wikibase.snakview.variations.Variation' => $moduleTemplate + array(
+			'scripts' => array(
+				'snakview.variations.Variation.js',
+			),
+			'dependencies' => array(
+				'util.inherit',
+			),
+		),
+
+		'jquery.wikibase.snakview.variations.NoValue' => $moduleTemplate + array(
+			'scripts' => array(
+				'snakview.variations.NoValue.js',
+			),
+			'dependencies' => array(
+				'jquery.wikibase.snakview.variations',
+				'jquery.wikibase.snakview.variations.Variation',
+				'wikibase.datamodel',
+			),
+			'messages' => array(
+				'wikibase-snakview-variations-novalue-label',
+			),
+		),
+
+		'jquery.wikibase.snakview.variations.SomeValue' => $moduleTemplate + array(
+			'scripts' => array(
+				'snakview.variations.SomeValue.js',
+			),
+			'dependencies' => array(
+				'jquery.wikibase.snakview.variations',
+				'jquery.wikibase.snakview.variations.Variation',
+				'wikibase.datamodel',
+			),
+			'messages' => array(
+				'wikibase-snakview-variations-somevalue-label',
+			),
+		),
+
+		'jquery.wikibase.snakview.variations.Value' => $moduleTemplate + array(
+			'scripts' => array(
+				'snakview.variations.Value.js',
+			),
+			'dependencies' => array(
+				'jquery.wikibase.snakview.variations',
+				'jquery.wikibase.snakview.variations.Variation',
+				'wikibase.datamodel',
+				'wikibase.dataTypes',
+			),
+			'messages' => array(
+				'wikibase-snakview-variation-datavaluetypemismatch',
+				'wikibase-snakview-variation-datavaluetypemismatch-details',
+				'wikibase-snakview-variation-nonewvaluefordeletedproperty',
+			),
+		),
+
+		'jquery.wikibase.snakview.ViewState' => $moduleTemplate + array(
+			'scripts' => array(
+				'snakview.ViewState.js',
+			),
+		),
+
+	);
+
+	return $modules;
+} );
