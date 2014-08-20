1ba53c3cea332bda9e02687ad52b7b8869d88a24
Bene <benestar.wikimedia@gmail.com>
2014-08-20 17:35:42 +0200

    Record usage of badges
    This only a temporary hack and will be done in a saner way after we have
    an EntityParserOutput class [1]. Therefor, this also will get tested in
    the other patch.
    
    [1] https://gerrit.wikimedia.org/r/#/c/155287/
    
    Change-Id: I767ec181522272d9ad7e9e15b1f13036bcf8a707

--- repo/includes/EntityView.php
+++ repo/includes/EntityView.php
@@ -378,6 +378,10 @@ if ( $ ) {
 			$pout->setExtensionData( 'wikibase-view-chunks', $this->getPlaceholders() );
 		}
 
+		if ( $entity instanceof Item ) {
+			$this->addBadgesToParserOutput( $pout, $entity->getSiteLinkList() );
+		}
+
 		//@todo: record sitelinks as iwlinks
 		//@todo: record CommonsMedia values as imagelinks
 
@@ -401,6 +405,14 @@ if ( $ ) {
 
 		wfProfileOut( __METHOD__ );
 		return $pout;
+	}
+
+	private function addBadgesToParserOutput( ParserOutput $pout, SiteLinkList $siteLinkList ) {
+		foreach ( $siteLinkList as $siteLink ) {
+			foreach ( $siteLink->getBadges() as $badge ) {
+				$pout->addLink( $this->entityTitleLookup->getTitleForID( $badge ) );
+			}
+		}
 	}
 
 	/**
