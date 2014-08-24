8def017762d0528198d85a305ad38b08f9e7b550
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2014-08-24 16:47:48 +0200

    Proposed refactoring for ReferencedEntitiesFinder
    Change-Id: Ibb6e011b6de9cce1629619c810b94f13a1ff33de

--- lib/includes/ReferencedEntitiesFinder.php
+++ lib/includes/ReferencedEntitiesFinder.php
@@ -20,45 +20,44 @@ use Wikibase\DataModel\Entity\EntityIdValue;
 class ReferencedEntitiesFinder {
 
 	/**
-	 * @var EntityId[] serialized entity ids pointing to entity id objects
-	 */
-	private $foundEntityIds;
-
-	/**
-	 * Finds linked entities within a set of snaks.
+	 * Finds linked Entities within a set of Snaks.
 	 *
 	 * @param Snak[] $snaks
 	 *
-	 * @return EntityId[]
+	 * @return EntityId[] Entity ID strings pointing to Entity ID objects.
 	 */
 	public function findSnakLinks( array $snaks ) {
-		$this->foundEntityIds = array();
+		$entityIds = array();
 
 		foreach ( $snaks as $snak ) {
-			$this->handleSnak( $snak );
+			$this->addEntityIds( $entityIds, $snak );
 		}
 
-		return $this->foundEntityIds;
+		return $entityIds;
 	}
 
-	private function handleSnak( Snak $snak ) {
-		// all of the Snak's properties are referenced entities
-		$this->addEntityId( $snak->getPropertyId() );
+	/**
+	 * @param EntityId[] $entityIds
+	 * @param Snak $snak
+	 */
+	private function addEntityIds( array &$entityIds, Snak $snak ) {
+		$this->addEntityId( $entityIds, $snak->getPropertyId() );
 
-		// PropertyValueSnaks might have a value referencing an Entity
-		if( $snak instanceof PropertyValueSnak ) {
-			$this->handleDataValue( $snak->getDataValue() );
+		if ( $snak instanceof PropertyValueSnak ) {
+			$dataValue = $snak->getDataValue();
+
+			if ( $dataValue instanceof EntityIdValue ) {
+				$this->addEntityId( $entityIds, $dataValue->getEntityId() );
+			}
 		}
 	}
 
-	private function handleDataValue( DataValue $dataValue ) {
-		if ( $dataValue instanceof EntityIdValue ) {
-			$this->addEntityId( $dataValue->getEntityId() );
-		}
-	}
-
-	private function addEntityId( EntityId $entityId ) {
-		$this->foundEntityIds[$entityId->getSerialization()] = $entityId;
+	/**
+	 * @param EntityId[] $entityIds
+	 * @param EntityId $entityId
+	 */
+	private function addEntityId( array &$entityIds, EntityId $entityId ) {
+		$entityIds[$entityId->getSerialization()] = $entityId;
 	}
 
 }
