26a57919d53a64d9ca383cd0948bb0897ae8c31c
Bene <benestar.wikimedia@gmail.com>
2014-08-24 19:38:05 +0200

    Refactor ReferencedEntitiesFinder
    This patch improves the readability and structure in ReferencedEntitiesFinder.
    It was split from https://gerrit.wikimedia.org/r/#/c/155896/
    
    Change-Id: Ia8ccfe2db6fc857b3d10147326adaefd3055e255

--- lib/includes/ReferencedEntitiesFinder.php
+++ lib/includes/ReferencedEntitiesFinder.php
@@ -15,8 +15,14 @@ use Wikibase\DataModel\Entity\EntityIdValue;
  * @author Daniel Werner < daniel.a.r.werner@gmail.com >
  * @author Katie Filbert
  * @author Daniel Kinzler
+ * @author Bene* < benestar.wikimedia@gmail.com >
  */
 class ReferencedEntitiesFinder {
+
+	/**
+	 * @var EntityId[] serialized entity ids pointing to entity id objects
+	 */
+	private $foundEntityIds;
 
 	/**
 	 * Finds linked entities within a set of snaks.
@@ -26,51 +32,33 @@ class ReferencedEntitiesFinder {
 	 * @return EntityId[]
 	 */
 	public function findSnakLinks( array $snaks ) {
-		$foundEntities = array();
+		$this->foundEntityIds = array();
 
 		foreach ( $snaks as $snak ) {
-			// all of the Snak's properties are referenced entities, add them:
-			$propertyId = $snak->getPropertyId();
-			$foundEntities[ $propertyId->getSerialization() ] = $propertyId;
-
-			// PropertyValueSnaks might have a value referencing an Entity, find those as well:
-			if( $snak instanceof PropertyValueSnak ) {
-				$snakValue = $snak->getDataValue();
-
-				if( $snakValue === null ) {
-					// shouldn't ever run into this, but make sure!
-					continue;
-				}
-
-				$entitiesInSnakDataValue = $this->findDataValueLinks( $snakValue );
-				$foundEntities = array_merge( $foundEntities, $entitiesInSnakDataValue );
-			}
+			$this->handleSnak( $snak );
 		}
 
-		return $foundEntities;
+		return $this->foundEntityIds;
 	}
 
-	/**
-	 * Finds linked entities within a given data value.
-	 *
-	 * @since 0.5
-	 *
-	 * @param DataValue $dataValue
-	 * @return EntityId[]
-	 */
-	public function findDataValueLinks( DataValue $dataValue ) {
-		switch( $dataValue->getType() ) {
-			case 'wikibase-entityid':
-				if( $dataValue instanceof EntityIdValue ) {
-					$entityId = $dataValue->getEntityId();
-					return array(
-						$entityId->getSerialization() => $entityId );
-				}
-				break;
+	private function handleSnak( Snak $snak ) {
+		// all of the Snak's properties are referenced entities
+		$this->addEntityId( $snak->getPropertyId() );
 
-			// TODO: we might want to allow extensions to add handling for their custom
-			//  data value types here. Either use a hook or a proper registration for that.
+		// PropertyValueSnaks might have a value referencing an Entity
+		if( $snak instanceof PropertyValueSnak ) {
+			$this->handleDataValue( $snak->getDataValue() );
 		}
-		return array();
 	}
+
+	private function handleDataValue( DataValue $dataValue ) {
+		if ( $dataValue instanceof EntityIdValue ) {
+			$this->addEntityId( $dataValue->getEntityId() );
+		}
+	}
+
+	private function addEntityId( EntityId $entityId ) {
+		$this->foundEntityIds[$entityId->getSerialization()] = $entityId;
+	}
+
 }
