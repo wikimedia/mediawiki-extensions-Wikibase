3b5591b3b8974029177b847c5e5d4192386baf3c
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2014-08-24 16:47:48 +0200

    Small refactoring for ReferencedEntitiesFinder
    There still was a warning shown in PHPStorm. The introduction of the
    $dataValue variable makes it go away.
    
    Also the array of EntityId objects had a strange name. $links sounds
    like a list of SiteLink objects.
    
    Change-Id: Ibb6e011b6de9cce1629619c810b94f13a1ff33de

--- lib/includes/ReferencedEntitiesFinder.php
+++ lib/includes/ReferencedEntitiesFinder.php
@@ -24,22 +24,27 @@ class ReferencedEntitiesFinder {
 	 * @since 0.4
 	 *
 	 * @param Snak[] $snaks
-	 * @return EntityId[]
+	 *
+	 * @return EntityId[] Entity id strings pointing to EntityId objects.
 	 */
 	public function findSnakLinks( array $snaks ) {
-		$links = array();
+		$entityIds = array();
 
 		foreach ( $snaks as $snak ) {
 			$propertyId = $snak->getPropertyId();
-			$links[$propertyId->getSerialization()] = $propertyId;
+			$entityIds[$propertyId->getSerialization()] = $propertyId;
 
-			if( $snak instanceof PropertyValueSnak && $snak->getDataValue() instanceof EntityIdValue ) {
-				$entityId = $snak->getDataValue()->getEntityId();
-				$links[$entityId->getSerialization()] = $entityId;
+			if ( $snak instanceof PropertyValueSnak ) {
+				$dataValue = $snak->getDataValue();
+
+				if ( $dataValue instanceof EntityIdValue ) {
+					$entityId = $dataValue->getEntityId();
+					$entityIds[$entityId->getSerialization()] = $entityId;
+				}
 			}
 		}
 
-		return $links;
+		return $entityIds;
 	}
 
 }
