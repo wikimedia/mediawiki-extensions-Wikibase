63a2934161341c63b3dd12b0bc40b2dc5dbb0722
Henning Snater <henning.snater@wikimedia.de>
2014-08-27 18:04:37 +0200

    Added aliasesview to fingerprintview
    Change-Id: I12309c6bdebbe7461c7922f1e8c78e8c5aeeab31

--- repo/includes/View/TermBoxView.php
+++ repo/includes/View/TermBoxView.php
@@ -6,6 +6,7 @@ use Language;
 use Message;
 use Title;
 use Wikibase\DataModel\Entity\Entity;
+use Wikibase\DataModel\Term\AliasGroupList;
 use Wikibase\Utils;
 
 /**
@@ -63,17 +64,17 @@ class TermBoxView {
 
 		wfProfileIn( __METHOD__ );
 
-		$thead = $tbody = '';
-
 		$entityId = $entity->getId()->getSerialization();
-		$labels = $entity->getLabels();
-		$descriptions = $entity->getDescriptions();
+		$fingerprint = $entity->getFingerprint();
+		$labels = $fingerprint->getLabels();
+		$descriptions = $fingerprint->getDescriptions();
+		$aliasGroups = $fingerprint->getAliasGroups();
+
+		$tbody = '';
 
 		foreach ( $languageCodes as $languageCode ) {
-			$label = array_key_exists( $languageCode, $labels ) ? $labels[$languageCode] : false;
-			$description = array_key_exists( $languageCode, $descriptions )
-				? $descriptions[$languageCode]
-				: false;
+			$hasLabel = $labels->hasTermForLanguage( $languageCode );
+			$hasDescription = $descriptions->hasTermForLanguage( $languageCode );
 
 			$editLabelSection = $this->sectionEditLinkGenerator->getHtmlForEditSection(
 				'SetLabel',
@@ -87,23 +88,39 @@ class TermBoxView {
 				$this->msg( 'wikibase-edit' ),
 				$editable
 			);
+			$editAliasesSection = $this->sectionEditLinkGenerator->getHtmlForEditSection(
+				'SetAliases',
+				array( $entityId, $languageCode ),
+				$this->msg( 'wikibase-edit' ),
+				$editable
+			);
 
 			$tbody .= wfTemplate( 'wikibase-fingerprintview',
 				$languageCode,
+				$title->getLocalURL( array( 'setlang' => $languageCode ) ),
 				htmlspecialchars( Utils::fetchLanguageName( $languageCode ) ),
-				htmlspecialchars( $label !== false
-					? $label
-					: $this->msg( 'wikibase-label-empty' )->text()
-				),
-				htmlspecialchars( $description !== false
-					? $description
-					: $this->msg( 'wikibase-description-empty' )->text()
+				wfTemplate( 'wikibase-labelview',
+					$hasLabel ? '' : 'wb-empty',
+					htmlspecialchars( $hasLabel
+						? $labels->getByLanguage( $languageCode )->getText()
+						: $this->msg( 'wikibase-label-empty' )->text()
+					),
+					'',
+					''
 				),
 				'<td>' . $editLabelSection . '</td>',
+				wfTemplate( 'wikibase-descriptionview',
+					$hasDescription ? '' : 'wb-empty',
+					htmlspecialchars( $hasDescription
+						? $descriptions->getByLanguage( $languageCode )->getText()
+						: $this->msg( 'wikibase-description-empty' )->text()
+					),
+					'',
+					''
+				),
 				'<td>' . $editDescriptionSection . '</td>',
-				$label !== false ? '' : 'wb-value-empty',
-				$description !== false ? '' : 'wb-value-empty',
-				$title->getLocalURL( array( 'setlang' => $languageCode ) )
+				$this->getHtmlForAliases( $aliasGroups, $languageCode ),
+				'<td>' . $editAliasesSection . '</td>'
 			);
 		}
 
@@ -116,4 +133,36 @@ class TermBoxView {
 		return $html;
 	}
 
+	/**
+	 * @param AliasGroupList $aliasGroups
+	 * @param string $languageCode
+	 *
+	 * @return string
+	 */
+	private function getHtmlForAliases( AliasGroupList $aliasGroups, $languageCode ) {
+		if ( !$aliasGroups->hasGroupForLanguage( $languageCode ) ) {
+			return wfTemplate( 'wikibase-aliasesview',
+				'wb-empty',
+				wfMessage( 'wikibase-aliases-empty' )->escaped(),
+				'',
+				''
+			);
+		} else {
+			$aliasesHtml = '';
+			$aliases = $aliasGroups->getByLanguage( $languageCode )->getAliases();
+			foreach ( $aliases as $alias ) {
+				$aliasesHtml .= wfTemplate(
+					'wikibase-aliasesview-list-item',
+					htmlspecialchars( $alias )
+				);
+			}
+
+			return wfTemplate( 'wikibase-aliasesview',
+				'',
+				wfMessage( 'wikibase-aliases-label' )->escaped(),
+				$aliasesHtml,
+				''
+			);
+		}
+	}
 }
