13f3712b14385c908d1db9ea021e8b160ca86f3b
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2014-09-01 12:48:56 +0200

    Data providers for Item::equals tests

--- lib/packages/wikibase/data-model/tests/unit/Entity/ItemTest.php
+++ lib/packages/wikibase/data-model/tests/unit/Entity/ItemTest.php
@@ -9,7 +9,6 @@ use Diff\DiffOp\DiffOpChange;
 use Diff\DiffOp\DiffOpRemove;
 use Wikibase\DataModel\Claim\Claim;
 use Wikibase\DataModel\Claim\Claims;
-use Wikibase\DataModel\Statement\Statement;
 use Wikibase\DataModel\Entity\Diff\ItemDiff;
 use Wikibase\DataModel\Entity\EntityIdValue;
 use Wikibase\DataModel\Entity\Item;
@@ -25,6 +24,7 @@ use Wikibase\DataModel\Snak\PropertySomeValueSnak;
 use Wikibase\DataModel\Snak\PropertyValueSnak;
 use Wikibase\DataModel\Snak\Snak;
 use Wikibase\DataModel\Snak\SnakList;
+use Wikibase\DataModel\Statement\Statement;
 use Wikibase\DataModel\Statement\StatementList;
 use Wikibase\DataModel\Term\Fingerprint;
 
@@ -820,23 +820,66 @@ class ItemTest extends EntityTest {
 		$this->assertEquals( new StatementList(), $item->getStatements() );
 	}
 
-	public function testItemsWithDifferentStatementsAreNotEqual() {
+	public function equalsProvider() {
 		$firstItem = Item::newEmpty();
-		$secondItem = Item::newEmpty();
-
 		$firstItem->getStatements()->addNewStatement( new PropertyNoValueSnak( 42 ) );
 
-		$this->assertFalse( $firstItem->equals( $secondItem ) );
-	}
-
-	public function testItemsWithTheSameStatementsAreEqual() {
-		$firstItem = Item::newEmpty();
 		$secondItem = Item::newEmpty();
-
-		$firstItem->getStatements()->addNewStatement( new PropertyNoValueSnak( 42 ) );
 		$secondItem->getStatements()->addNewStatement( new PropertyNoValueSnak( 42 ) );
 
+		return array(
+			array( Item::newEmpty(), Item::newEmpty() ),
+			array( $firstItem, $secondItem ),
+		);
+	}
+
+	/**
+	 * @dataProvider equalsProvider
+	 */
+	public function testEquals( Item $firstItem, Item $secondItem ) {
 		$this->assertTrue( $firstItem->equals( $secondItem ) );
+	}
+
+	public function notEqualsProvider() {
+		$item = Item::newEmpty();
+		$item->getFingerprint()->setLabel( 'en', 'Same' );
+		$item->getFingerprint()->setDescription( 'en', 'Same' );
+		$item->getFingerprint()->setAliasGroup( 'en', array( 'Same' ) );
+		$item->getSiteLinkList()->addNewSiteLink( 'enwiki', 'Same' );
+		$item->getStatements()->addNewStatement( new PropertyNoValueSnak( 42 ) );
+
+		$differentLabel = $item->copy();
+		$differentLabel->getFingerprint()->setLabel( 'en', 'Different' );
+
+		$differentDescription = $item->copy();
+		$differentDescription->getFingerprint()->setDescription( 'en', 'Different' );
+
+		$differentAlias = $item->copy();
+		$differentAlias->getFingerprint()->setAliasGroup( 'en', array( 'Different' ) );
+
+		$differentSiteLink = $item->copy();
+		$differentSiteLink->getSiteLinkList()->removeLinkWithSiteId( 'enwiki' );
+		$differentSiteLink->getSiteLinkList()->addNewSiteLink( 'enwiki', 'Different' );
+
+		$differentStatement = $item->copy();
+		$differentStatement->setStatements( new StatementList() );
+		$differentStatement->getStatements()->addNewStatement( new PropertyNoValueSnak( 24 ) );
+
+		return array(
+			array( $item, Item::newEmpty() ),
+			array( $item, $differentLabel ),
+			array( $item, $differentDescription ),
+			array( $item, $differentAlias ),
+			array( $item, $differentSiteLink ),
+			array( $item, $differentStatement ),
+		);
+	}
+
+	/**
+	 * @dataProvider notEqualsProvider
+	 */
+	public function testNotEquals( Item $firstItem, Item $secondItem ) {
+		$this->assertFalse( $firstItem->equals( $secondItem ) );
 	}
 
 }
