da450bdaa5eaea80b9193dafa03c491a2e6428d9
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2014-09-01 16:51:01 +0200

    Make ItemSerializerTest compatible to DataModel 1.0
    Change-Id: I17a6cdd8d267487cb2642dfff976c2f96667ea9c

--- lib/tests/phpunit/serializers/ItemSerializerTest.php
+++ lib/tests/phpunit/serializers/ItemSerializerTest.php
@@ -2,8 +2,13 @@
 
 namespace Wikibase\Test;
 
+use DataValues\StringValue;
 use SiteSQLStore;
+use Wikibase\DataModel\Claim\Statement;
 use Wikibase\DataModel\Entity\Item;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\DataModel\Snak\PropertyValueSnak;
+use Wikibase\Lib\ClaimGuidGenerator;
 use Wikibase\Lib\Serializers\ClaimSerializer;
 use Wikibase\Lib\Serializers\EntitySerializer;
 use Wikibase\Lib\Serializers\ItemSerializer;
@@ -28,7 +33,7 @@ class ItemSerializerTest extends EntitySerializerBaseTest {
 	 * @return string
 	 */
 	protected function getClass() {
-		return '\Wikibase\Lib\Serializers\ItemSerializer';
+		return 'Wikibase\Lib\Serializers\ItemSerializer';
 	}
 
 	/**
@@ -53,7 +58,7 @@ class ItemSerializerTest extends EntitySerializerBaseTest {
 	/**
 	 * @see SerializerBaseTest::validProvider
 	 *
-	 * @return array
+	 * @return array[]
 	 */
 	public function validProvider() {
 		$validArgs = array();
@@ -85,6 +90,149 @@ class ItemSerializerTest extends EntitySerializerBaseTest {
 		foreach ( $this->semiValidProvider() as $argList ) {
 			$validArgs[] = $argList;
 		}
+
+		return $validArgs;
+	}
+
+	/**
+	 * Returns arguments for entity agnostic arguments that can be returned
+	 * by validProvider after making sure the provided serialization contains
+	 * anything the entity implementing class requires.
+	 *
+	 * @return array[]
+	 */
+	private function semiValidProvider() {
+		$item = $this->getEntityInstance();
+
+		$validArgs = array();
+
+		$options = new SerializationOptions();
+		$options->setOption( EntitySerializer::OPT_PARTS, array( 'aliases' ) );
+
+		$item0 = $item->copy();
+		$item0->setAliases( 'en', array( 'foo', 'bar' ) );
+		$item0->setAliases( 'de', array( 'baz', 'bah' ) );
+
+		$validArgs[] = array(
+			$item0,
+			array(
+				'id' => $item0->getId()->getSerialization(),
+				'type' => $item0->getType(),
+				'aliases' => array(
+					'en' => array(
+						array(
+							'value' => 'foo',
+							'language' => 'en',
+						),
+						array(
+							'value' => 'bar',
+							'language' => 'en',
+						),
+					),
+					'de' => array(
+						array(
+							'value' => 'baz',
+							'language' => 'de',
+						),
+						array(
+							'value' => 'bah',
+							'language' => 'de',
+						),
+					),
+				)
+			),
+			$options
+		);
+
+		$options = new SerializationOptions();
+		$options->setOption( EntitySerializer::OPT_PARTS, array( 'descriptions', 'labels' ) );
+
+		$item1 = $item->copy();
+		$item1->setLabel( 'en', 'foo' );
+		$item1->setLabel( 'de', 'bar' );
+		$item1->setDescription( 'en', 'baz' );
+		$item1->setDescription( 'de', 'bah' );
+
+		$validArgs[] = array(
+			$item1,
+			array(
+				'id' => $item1->getId()->getSerialization(),
+				'type' => $item1->getType(),
+				'labels' => array(
+					'en' => array(
+						'value' => 'foo',
+						'language' => 'en',
+					),
+					'de' => array(
+						'value' => 'bar',
+						'language' => 'de',
+					),
+				),
+				'descriptions' => array(
+					'en' => array(
+						'value' => 'baz',
+						'language' => 'en',
+					),
+					'de' => array(
+						'value' => 'bah',
+						'language' => 'de',
+					),
+				),
+			),
+			$options
+		);
+
+		$item2 = $this->getEntityInstance();
+
+		$options->setOption(
+			EntitySerializer::OPT_PARTS,
+			array( 'descriptions', 'labels', 'claims', 'aliases' )
+		);
+
+		$statement = new Statement(
+			new PropertyValueSnak(
+				new PropertyId( 'P42' ),
+				new StringValue( 'foobar!' )
+			)
+		);
+
+		$guidGenerator = new ClaimGuidGenerator();
+		$statement->setGuid( $guidGenerator->newGuid( $item2->getId() ) );
+
+		$item2->setLabel( 'en', 'foo' );
+		$item2->addClaim( $statement );
+
+		$validArgs[] = array(
+			$item2,
+			array(
+				'id' => $item2->getId()->getSerialization(),
+				'type' => $item2->getType(),
+				'labels' => array(
+					'en' => array(
+						'language' => 'en',
+						'value' => 'foo',
+					)
+				),
+				'claims' => array(
+					'P42' => array(
+						array(
+							'id' => $statement->getGuid(),
+							'mainsnak' => array(
+								'snaktype' => 'value',
+								'property' => 'P42',
+								'datavalue' => array(
+									'value' => 'foobar!',
+									'type' => 'string'
+								)
+							),
+							'type' => 'statement',
+							'rank' => 'normal',
+						)
+					)
+				)
+			),
+			$options
+		);
 
 		return $validArgs;
 	}
