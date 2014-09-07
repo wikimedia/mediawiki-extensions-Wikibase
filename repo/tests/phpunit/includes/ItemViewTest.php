c5a66fe74ca3e6d2e204dd24e3d2567f83ba2cc8
Bene <benestar.wikimedia@googlemail.com>
2014-09-07 17:46:59 +0200

    Refactor EntityView
    This removes all dependencies to Fingerprints and Claims from EntityView
    and gives its subclasses full control about what should be displayed.
    
    Change-Id: Ib27b485c44b0e933c21a034403ceac25bc408f10

--- repo/tests/phpunit/includes/ItemViewTest.php
+++ repo/tests/phpunit/includes/ItemViewTest.php
@@ -2,10 +2,12 @@
 
 namespace Wikibase\Test;
 
-use Wikibase\DataModel\Entity\Entity;
-use Wikibase\DataModel\Entity\EntityId;
-use Wikibase\DataModel\Entity\ItemId;
-use Wikibase\DataModel\Statement\Statement;
+use InvalidArgumentException;
+use Language;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\DataModel\Entity\Property;
+use Wikibase\EntityRevision;
+use Wikibase\ItemView;
 
 /**
  * @covers Wikibase\ItemView
@@ -14,36 +16,96 @@ use Wikibase\DataModel\Statement\Statement;
  * @group WikibaseItemView
  *
  * @licence GNU GPL v2+
- * @author Daniel Kinzler
- *
- * @group Database
- * @group medium
+ * @author Bene* < benestar.wikimedia@gmail.com >
  */
-class ItemViewTest extends EntityViewTest {
+class ItemViewTest extends \PHPUnit_Framework_TestCase {
 
-	protected function getEntityViewClass() {
-		return 'Wikibase\ItemView';
+	public function provideEntityRevisions() {
+		$revisions = array();
+		$revId = 1234;
+		$timestamp = wfTimestamp( TS_MW );
+
+		$item = Item::newEmpty();
+		$revisions['new item'] = array( new EntityRevision( $item, $revId++, $timestamp ) );
+
+		$item = $item->copy();
+		$item->setId( 123 );
+		$revisions['existing item'] = array( new EntityRevision( $item, $revId++, $timestamp ) );
+
+		return $revisions;
 	}
 
 	/**
-	 * @param EntityId $id
-	 * @param Statement[] $statements
+	 * @dataProvider provideEntityRevisions
 	 *
-	 * @return Entity
+	 * @param EntityRevision $entityRevision
 	 */
-	protected function makeEntity( EntityId $id, array $statements = array() ) {
-		return $this->makeItem( $id, $statements );
+	public function testGetHtml( EntityRevision $entityRevision ) {
+		$itemView = $this->getItemView();
+		$html = $itemView->getHtml( $entityRevision );
+		$id = $entityRevision->getEntity()->getId();
+
+		if ( $id === null ) {
+			$this->assertContains( 'new', $html, "Html for new items should cotain keyword 'new'" );
+		} else {
+			$this->assertContains( $id->getSerialization(), $html, "Html for existing items should contain the serialized id" );
+		}
+
+		$this->assertContains( Item::ENTITY_TYPE, $html, "Html should contain item type" );
+
+		foreach ( array( '<fingerprint>', '<claims>', '<sitelinks>' ) as $htmlSnippet ) {
+			$this->assertContains( $htmlSnippet, $html, "Generated html should contain '$htmlSnippet'" );
+		}
+
+		foreach ( array( 'wikipedia', 'special' ) as $siteLinkGroup ) {
+			$this->assertContains( $siteLinkGroup, $html, "Generated html should contain $siteLinkGroup site link group" );
+		}
+
+		$placeholders = $itemView->getPlaceholders();
+		$placeholderNames = array();
+		foreach ( $placeholders as $placeholder => $args ) {
+			$placeholderNames[] = $args[0];
+			$this->assertContains( $placeholder, $html, "Generated html should contain placeholder for key '{$args[0]}'" );
+		}
+
+		if ( $id !== null ) {
+			foreach ( array( 'termbox', 'termbox-toc' ) as $placeholderName ) {
+				$this->assertContains( $placeholderName, $placeholderNames, "Placeholder '$placeholderName' should be used" );
+			}
+		}
 	}
 
 	/**
-	 * Generates a suitable entity ID based on $n.
-	 *
-	 * @param int|string $n
-	 *
-	 * @return EntityId
+	 * @expectedException InvalidArgumentException
+	 * @expectedExceptionMessage $entityRevision must contain an Item.
 	 */
-	protected function makeEntityId( $n ) {
-		return new ItemId( "Q$n");
+	public function testGetHtmlFailsForWrongEntityType() {
+		$itemView = $this->getItemView();
+		$revision = new EntityRevision( Property::newEmpty(), 1234, wfTimestamp( TS_MW ) );
+
+		$itemView->getHtml( $revision );
+	}
+
+	private function getItemView() {
+		return new ItemView(
+			$this->getViewMock( 'Wikibase\Repo\View\FingerprintView', '<fingerprint>' ),
+			$this->getViewMock( 'Wikibase\Repo\View\ClaimsView', '<claims>' ),
+			$this->getViewMock( 'Wikibase\Repo\View\SiteLinksView', '<sitelinks>' ),
+			array( 'wikipedia', 'special' ),
+			Language::factory( 'en' )
+		);
+	}
+
+	private function getViewMock( $className, $returnValue ) {
+		$view = $this->getMockBuilder( $className )
+			->disableOriginalConstructor()
+			->getMock();
+
+		$view->expects( $this->any() )
+			->method( 'getHtml' )
+			->will( $this->returnValue( $returnValue ) );
+
+		return $view;
 	}
 
 }
