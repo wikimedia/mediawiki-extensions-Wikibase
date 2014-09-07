f8805e4dad9456c15af70cffc01d3f559ce5ed84
Bene <benestar.wikimedia@googlemail.com>
2014-09-07 17:46:59 +0200

    Refactor EntityView
    This removes all dependencies to Fingerprints and Claims from EntityView
    and gives its subclasses full control about what should be displayed.
    
    Change-Id: Ib27b485c44b0e933c21a034403ceac25bc408f10

--- repo/includes/ItemView.php
+++ repo/includes/ItemView.php
@@ -3,9 +3,11 @@
 namespace Wikibase;
 
 use InvalidArgumentException;
-use Wikibase\Repo\View\SectionEditLinkGenerator;
+use Language;
+use Wikibase\Repo\View\ClaimsView;
+use Wikibase\Repo\View\FingerprintView;
 use Wikibase\Repo\View\SiteLinksView;
-use Wikibase\Repo\WikibaseRepo;
+use Wikibase\Repo\View\TocGenerator;
 
 /**
  * Class for creating views for Item instances.
@@ -16,8 +18,44 @@ use Wikibase\Repo\WikibaseRepo;
  * @licence GNU GPL v2+
  * @author H. Snater < mediawiki@snater.com >
  * @author Daniel Werner
+ * @author Bene* < benestar.wikimedia@gmail.com >
  */
 class ItemView extends EntityView {
+
+	/**
+	 * @var FingerprintView
+	 */
+	private $fingerprintView;
+
+	/**
+	 * @var ClaimsView
+	 */
+	private $claimsView;
+
+	/**
+	 * @var SiteLinksView
+	 */
+	private $siteLinksView;
+
+	/**
+	 * @var string[]
+	 */
+	private $siteLinkGroups;
+
+	public function __construct(
+		FingerprintView $fingerprintView,
+		ClaimsView $claimsView,
+		SiteLinksView $siteLinksView,
+		array $siteLinkGroups,
+		Language $language
+	) {
+		parent::__construct( $language );
+
+		$this->fingerprintView = $fingerprintView;
+		$this->claimsView = $claimsView;
+		$this->siteLinksView = $siteLinksView;
+		$this->siteLinkGroups = $siteLinkGroups;
+	}
 
 	/**
 	 * @see EntityView::getInnerHtml
@@ -30,25 +68,46 @@ class ItemView extends EntityView {
 		}
 
 		$html = '';
-		$html = parent::getInnerHtml( $entityRevision, $editable );
+		$html .= $this->fingerprintView->getHtml( $item->getFingerprint(), $item->getId(), $editable );
+		$html .= $this->getHtmlForToc();
+		$html .= $this->getHtmlForTermBox( $entityRevision );
 		$html .= $this->claimsView->getHtml( $item->getClaims(), 'wikibase-statements' );
-		$html .= $this->getHtmlForSiteLinks( $item, $editable );
+		$html .= $this->siteLinksView->getHtml( $item->getSiteLinkList(), $item->getId(), $this->siteLinkGroups, $editable );
 
 		return $html;
 	}
 
-	/**
-	 * @see EntityView::getTocSections
-	 */
-	protected function getTocSections() {
-		$array = parent::getTocSections();
-		$array['claims'] = 'wikibase-statements';
-		$groups = WikibaseRepo::getDefaultInstance()->getSettings()->getSetting( 'siteLinkGroups' );
-		foreach ( $groups as $group ) {
+	private function getHtmlForToc() {
+		$tocSections = array(
+			// Placeholder for the TOC entry for the term box (which may or may not be used for a given user).
+			// EntityViewPlaceholderExpander must know about the 'termbox-toc' name.
+			// This is a hack because the marker does not exist as a system message it will be added as is.
+			'termbox' => $this->textInjector->newMarker( 'termbox-toc' ),
+			'claims' => 'wikibase-statements'
+		);
+
+		foreach ( $this->siteLinkGroups as $group ) {
 			$id = htmlspecialchars( 'sitelinks-' . $group, ENT_QUOTES );
-			$array[$id] = 'wikibase-sitelinks-' . $group;
+			$tocSections[$id] = 'wikibase-sitelinks-' . $group;
 		}
-		return $array;
+
+		$tocGenerator = new TocGenerator();
+
+		return $tocGenerator->getHtmlForToc( $tocSections );
+	}
+
+	private function getHtmlForTermBox( EntityRevision $entityRevision ) {
+		if ( $entityRevision->getEntity()->getId() ) {
+			// Placeholder for a termbox for the present item.
+			// EntityViewPlaceholderExpander must know about the parameters used here.
+			return $this->textInjector->newMarker(
+				'termbox',
+				$entityRevision->getEntity()->getId()->getSerialization(),
+				$entityRevision->getRevision()
+			);
+		}
+
+		return '';
 	}
 
 	/**
@@ -60,8 +119,8 @@ class ItemView extends EntityView {
 	 * @param bool $editable whether editing is allowed (enabled edit links)
 	 *
 	 * @return string
-	 */
-	protected function getHtmlForSiteLinks( Item $item, $editable = true ) {
+	 *
+	private function getHtmlForSiteLinks( Item $item, $editable = true ) {
 		$wikibaseRepo = WikibaseRepo::getDefaultInstance();
 		$groups = $wikibaseRepo->getSettings()->getSetting( 'siteLinkGroups' );
 
@@ -77,5 +136,6 @@ class ItemView extends EntityView {
 
 		return $siteLinksView->getHtml( $item->getSiteLinks(), $itemId, $groups, $editable );
 	}
+	*/
 
 }
