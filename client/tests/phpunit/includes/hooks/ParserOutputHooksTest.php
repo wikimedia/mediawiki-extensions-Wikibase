6b2c287b5cce4d5bca1a7a0c8d11151f6754dfd7
daniel <daniel.kinzler@wikimedia.de>
2014-09-08 18:03:43 +0200

    Extract handlers for ParserOutput hooks
    This factors the handlers for ParserOutput related hooks
    out of the general WikibaseCLientHooks class into a separate class
    and makes them testable.
    
    Change-Id: I5a76107fb9e02c94d75205738858a57b1175ac56

---
+++ client/tests/phpunit/includes/hooks/ParserOutputHooksTest.php
@@ -0,0 +1,365 @@
+<?php
+
+namespace Wikibase\Test;
+
+use FauxRequest;
+use MediaWikiSite;
+use OutputPage;
+use Parser;
+use ParserOptions;
+use ParserOutput;
+use RequestContext;
+use Site;
+use SiteStore;
+use StripState;
+use Title;
+use Wikibase\Client\Hooks\OtherProjectsSidebarGenerator;
+use Wikibase\Client\Hooks\ParserOutputHooks;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\SiteLink;
+use Wikibase\InterwikiSorter;
+use Wikibase\LangLinkHandler;
+use Wikibase\Lib\Store\SiteLinkLookup;
+use Wikibase\NamespaceChecker;
+use Wikibase\Settings;
+use Wikibase\SettingsArray;
+
+/**
+ * @covers Wikibase\Client\Hooks\ParserOutputHooks
+ *
+ * @group WikibaseClient
+ * @group Wikibase
+ * @group WikibaseHooks
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ */
+class ParserOutputHooksTest extends \MediaWikiTestCase {
+
+	/**
+	 * @param string $globalId
+	 * @param string $group
+	 * @param $language
+	 *
+	 * @return Site
+	 */
+	private function newSite( $globalId, $group, $language ) {
+		$site = new MediaWikiSite();
+		$site->setGlobalId( $globalId );
+		$site->setGroup( $group );
+		$site->setLanguageCode( $language );
+		$site->addNavigationId( $language );
+		$site->setPagePath( 'wiki/' );
+		$site->setFilePath( 'w/' );
+		$site->setLinkPath( 'http://' . $globalId . '.test.com/wiki/$1' );
+
+		return $site;
+	}
+
+	/**
+	 * @return SiteStore
+	 */
+	private function getSiteStore() {
+		$siteStore = new MockSiteStore( array(
+			$this->newSite( 'wikidatawiki', 'wikidata', 'en' ),
+			$this->newSite( 'commonswiki', 'commons', 'en' ),
+			$this->newSite( 'enwiki', 'wikipedia', 'en' ),
+			$this->newSite( 'dewiki', 'wikipedia', 'de' ),
+		) );
+
+		return $siteStore;
+	}
+
+	/**
+	 * @return SiteLinkLookup
+	 */
+	private function getSiteLinkLookup() {
+		$lookup = $this->getMock( 'Wikibase\Lib\Store\SiteLinkLookup' );
+
+		$badgeQ7 = new ItemId( 'Q17' );
+
+		$links = array(
+			'Q1' => array(
+				new SiteLink( 'dewiki', 'Sauerstoff', array( $badgeQ7 ) ),
+				new SiteLink( 'enwiki', 'Oxygen' ),
+				new SiteLink( 'commonswiki', 'Oxygen' ),
+			),
+			'Q7' => array(
+				new SiteLink( 'dewiki', 'User:Foo' ),
+				new SiteLink( 'enwiki', 'User:Foo' ),
+				new SiteLink( 'commonswiki', 'User:Foo' ),
+			),
+		);
+
+		$q1 = new ItemId( 'Q1' );
+
+		$items = array(
+			'dewiki:Sauerstoff' => $q1,
+			'enwiki:Oxygen' => $q1,
+		);
+
+		$lookup->expects( $this->any() )
+			->method( 'getSiteLinksForItem' )
+			->will( $this->returnCallback( function( ItemId $itemId ) use ( $links ) {
+				$key = $itemId->getSerialization();
+				return isset( $links[$key] ) ? $links[$key] : array();
+			} ) );
+
+		$lookup->expects( $this->any() )
+			->method( 'getEntityIdForSiteLink' )
+			->will( $this->returnCallback( function( SiteLink $link ) use ( $items ) {
+				$key = $link->getSiteId() . ':' . $link->getPageName();
+				return isset( $items[$key] ) ? $items[$key] : null;
+			} ) );
+
+		return $lookup;
+	}
+
+	/**
+	 * @param array $settings
+	 *
+	 * @return Settings
+	 */
+	private function newSettings( array $settings ) {
+		$defaults = array(
+			'sort' => 'code',
+			'sortPrepend' => array(),
+			'interwikiSortOrders' => array( 'alphabetic' => array(
+				'ar', 'de', 'en', 'sv', 'zh'
+			) ),
+			'siteGlobalid' => 'enwiki',
+			'languageLinkSiteGroup' => 'wikipedia',
+			'namespaces' => array( NS_MAIN, NS_CATEGORY ),
+			'alwaysSort' => false,
+			'otherProjectsLinks' => array( 'commonswiki' ),
+		);
+
+		return new SettingsArray( array_merge( $defaults, $settings ) );
+	}
+
+	private function newParserOutputHooks( array $settings = array() ) {
+		$settings = $this->newSettings( $settings );
+
+		$siteId = $settings->getSetting( 'siteGlobalid' );
+		$siteGroup = $settings->getSetting( 'languageLinkSiteGroup' );
+		$namespaces = $settings->getSetting( 'namespaces' );
+		$otherProjectIds = $settings->getSetting( 'otherProjectsLinks' );
+
+		$namespaceChecker = new NamespaceChecker( array(), $namespaces );
+		$siteLinkLookup = $this->getSiteLinkLookup();
+		$siteStore = $this->getSiteStore();
+
+		$otherProjectsSidebarGenerator = new OtherProjectsSidebarGenerator(
+			$siteId,
+			$siteLinkLookup,
+			$siteStore,
+			$otherProjectIds
+		);
+
+		$langLinkHandler = new LangLinkHandler(
+			$otherProjectsSidebarGenerator,
+			$siteId,
+			$namespaceChecker,
+			$siteLinkLookup,
+			$siteStore,
+			$siteGroup
+		);
+
+		$interwikiSorter = new InterwikiSorter(
+			$settings->getSetting( 'sort' ),
+			$settings->getSetting( 'interwikiSortOrders' ),
+			$settings->getSetting( 'sortPrepend' )
+		);
+
+		return new ParserOutputHooks(
+			$namespaceChecker,
+			$langLinkHandler,
+			$interwikiSorter,
+			$settings->getSetting( 'alwaysSort' )
+		);
+
+	}
+
+	private function newParser( Title $title, array $pageProps, array $extensionData ) {
+		$popt = new ParserOptions();
+		$parser = new Parser();
+
+		$parser->startExternalParse( $title, $popt, Parser::OT_HTML );
+
+		$parserOutput = $parser->getOutput();
+		$this->primeParserOutput( $parserOutput, $pageProps, $extensionData );
+
+		return $parser;
+	}
+
+	private function primeParserOutput( ParserOutput $parserOutput, array $pageProps, array $extensionData ) {
+		foreach ( $pageProps as $name => $value ) {
+			$parserOutput->setProperty( $name, $value );
+		}
+
+		foreach ( $extensionData as $key => $value ) {
+			$parserOutput->setExtensionData( $key, $value );
+		}
+	}
+
+	public function parserAfterParseProvider() {
+		$commonsOxygen = array(
+			'msg' => 'wikibase-otherprojects-commons',
+			'class' => 'wb-otherproject-link wb-otherproject-commons',
+			'href' => 'http://commonswiki.test.com/wiki/Oxygen',
+			'hreflang' => 'en',
+		);
+
+		return array(
+			'repo-links' => array(
+				Title::makeTitle( NS_MAIN, 'Oxygen' ),
+				'Q1',
+				array(),
+				array( 'de:Sauerstoff' ),
+				array( $commonsOxygen ),
+			),
+
+			'noexternallanglinks=*' => array(
+				Title::makeTitle( NS_MAIN, 'Oxygen' ),
+				'Q1',
+				array( 'noexternallanglinks' => serialize( array( '*' ) ) ),
+				array(),
+				array( $commonsOxygen ),
+			),
+
+			'noexternallanglinks=de' => array(
+				Title::makeTitle( NS_MAIN, 'Oxygen' ),
+				'Q1',
+				array( 'noexternallanglinks' => serialize( array( 'de' ) ) ),
+				array(),
+				array( $commonsOxygen ),
+			),
+
+			'noexternallanglinks=ja' => array(
+				Title::makeTitle( NS_MAIN, 'Oxygen' ),
+				'Q1',
+				array( 'noexternallanglinks' => serialize( array( 'ja' ) ) ),
+				array( 'de:Sauerstoff' ),
+				array( $commonsOxygen ),
+			),
+
+			'no-item' => array(
+				Title::makeTitle( NS_MAIN, 'Plutonium' ),
+				null,
+				array(),
+				array(),
+				array(),
+			),
+
+			'ignored-namespace' => array(
+				Title::makeTitle( NS_USER, 'Foo' ),
+				null,
+				array(),
+				array(),
+				null,
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider parserAfterParseProvider
+	 */
+	public function testDoParserAfterParse(
+		Title $title,
+		$expectedItem,
+		array $pagePropsBefore,
+		array $expectedLanguageLinks = null,
+		array $expectedSisterLinks = null
+	) {
+		$parser = $this->newParser( $title, $pagePropsBefore, array() );
+		$handler = $this->newParserOutputHooks();
+
+		$text = '';
+		$stripState = new StripState( 'x' );
+
+		$handler->doParserAfterParse( $parser, $text, $stripState );
+
+		$parserOutput = $parser->getOutput();
+		$this->assertEquals( $expectedItem, $parserOutput->getProperty( 'wikibase_item' ) );
+		$this->assertLanguageLinks( $expectedLanguageLinks, $parserOutput );
+		$this->assertSisterLinks( $expectedSisterLinks, $parserOutput->getExtensionData( 'wikibase-otherprojects-sidebar' ) );
+	}
+
+	public function testDoOutputPageParserOutput() {
+		$title = Title::makeTitle( NS_MAIN, 'Oxygen' );
+
+		$sisterLinks = array(
+			array(
+				'msg' => 'wikibase-otherprojects-test',
+				'class' => 'wb-otherproject-link wb-otherproject-test',
+				'href' => 'http://acme.tests.com/wiki/Foo'
+			),
+		);
+
+		$pageProps = array(
+			'noexternallanglinks' => serialize( array( '*' ) ),
+			'wikibase_item' => 'Q1',
+		);
+
+		$extData = array(
+			'wikibase-otherprojects-sidebar' => $sisterLinks,
+		);
+
+		$outputProps = array(
+			'noexternallanglinks' => array( '*' ),
+			'wikibase_item' => 'Q1',
+			'wikibase-otherprojects-sidebar' => $sisterLinks,
+		);
+
+		$handler = $this->newParserOutputHooks();
+
+		$parserOutput = new ParserOutput();
+
+		$context = new RequestContext( new FauxRequest() );
+		$outputPage = new OutputPage( $context );
+		$outputPage->setTitle( $title );
+
+		$this->primeParserOutput( $parserOutput, $pageProps, $extData );
+
+		$handler->doOutputPageParserOutput( $outputPage, $parserOutput );
+
+		$this->assertOutputPageProperties( $outputProps, $outputPage );
+	}
+
+	private function assertOutputPageProperties( $props, OutputPage $outputPage ) {
+		$this->assertInternalType( 'array', $props );
+
+		foreach ( $props as $key => $value ) {
+			$this->assertEquals( $value, $outputPage->getProperty( $key ), 'OutputProperty: ' . $key );
+		}
+	}
+
+	private function assertLanguageLinks( $links, ParserOutput $parserOutput ) {
+		$this->assertInternalType( 'array', $links );
+
+		$actualLinks = $parserOutput->getLanguageLinks();
+
+		foreach ( $links as $link ) {
+			$this->assertContains( $link, $actualLinks, 'LanguageLink: ' );
+		}
+
+		$this->assertSameSize( $links, $actualLinks, 'Unmatched languageLinks!' );
+	}
+
+
+	private function assertSisterLinks( $expectedLinks, $actualLinks ) {
+		if ( !is_array( $expectedLinks ) ) {
+			$this->assertEquals( $expectedLinks, $actualLinks );
+			return;
+		}
+
+		$this->assertSameSize( $expectedLinks, $actualLinks, 'SisterLinks' );
+
+		$actual = reset( $actualLinks );
+		foreach ( $expectedLinks as $expected ) {
+			$this->assertEquals( $expected, $actual, 'SisterLink: ' );
+			$actual = next( $actualLinks );
+		}
+	}
+
+}
