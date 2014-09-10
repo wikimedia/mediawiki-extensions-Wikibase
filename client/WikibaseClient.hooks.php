73feea8795b4d220d91a4f3f8d65c5246e7d78b2
aude <aude.wiki@gmail.com>
2014-09-10 12:05:01 +0200

    Do not invalidate Title in other projects sidebar feature
    Fallback to loading this on-the-fly, as done now. This is only
    done for users with the beta feature enabled, and only when
    a page has old ParserOutput from cache.
    
    In time, the fallback will be used/needed less and less,
    and if we update $wgCacheEpoch in a gradual way, then this
    fallback situation will no longer occur.
    
    Bug: 70482
    Change-Id: I92fbe6202e41b0d061ec52d9b2be29f8e90446ca

--- client/WikibaseClient.hooks.php
+++ client/WikibaseClient.hooks.php
@@ -638,11 +638,14 @@ final class ClientHooks {
 
 		if ( $settings->getSetting( 'otherProjectsLinksByDefault' ) || $betaFeatureEnabled ) {
 			$otherProjectsSidebar = $outputPage->getProperty( 'wikibase-otherprojects-sidebar' );
+			$otherProjectsSidebar = null;
 
 			// in case of stuff in cache without the other projects
 			if ( $otherProjectsSidebar === null ) {
-				$title->invalidateCache();
-				return true;
+				// @todo remove this fallback before this graduates from
+				// a beta feature, if not sooner.
+				$otherProjectsSidebarGenerator = $wikibaseClient->getOtherProjectsSidebarGenerator();
+				$otherProjectsSidebar = $otherProjectsSidebarGenerator->buildProjectLinkSidebar( $title );
 			}
 
 			if ( !empty( $otherProjectsSidebar ) ) {
