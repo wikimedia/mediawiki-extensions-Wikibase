88f0df1e1730de696c7ce93b8f83adc36a728ca8
daniel <daniel.kinzler@wikimedia.de>
2014-09-19 19:07:28 +0200

    Introduce usage tracking service.
    This introduces the UsageTracker and UsageLookup service interfaces
    along with a suitable implementation, database setup/update code,
    and bidings for the top level storage factory.
    
    The usage tracking service is however not used anywhere yet.
    
    Bug: 64586
    Bug: 64587
    Change-Id: Ib3a970179eb47d19e449baba5ed89e46723b5559

---
+++ client/includes/Usage/EntityUsage.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Wikibase\Client\Usage;
+
+use Wikibase\DataModel\Entity\EntityId;
+
+/**
+ * Value object representing the usage of an entity. This includes information about
+ * how the entity is used, but not where.
+ *
+ * @see docs/usagetracking.wiki
+ *
+ * @license GPL 2+
+ * @author Daniel Kinzler
+ */
+class EntityUsage {
+
+	/**
+	 * Usage flag indicating that the entity's sitelinks were used.
+	 * This would be the case when generating language links or sister links from
+	 * an entity's sitelinks.
+	 */
+	const SITELINK_USAGE = 'I';
+
+	/**
+	 * Usage flag indicating that the entity's label in the local content language was used.
+	 * This would be the case when showing the label of a referenced entity.
+	 */
+	const LABEL_USAGE = 'L';
+
+	/**
+	 * Usage flag indicating that the entity's local page name was used.
+	 * This would be the case when linking a referenced entity to the
+	 * corresponding local wiki page.
+	 */
+	const PAGE_USAGE = 'P';
+
+	/**
+	 * Usage flag indicating that any and all aspects of the entity
+	 * were (or may have been) used.
+	 */
+	const ALL_USAGE = 'X';
+
+	/**
+	 * @var EntityId
+	 */
+	private $entityId;
+
+	/**
+	 * @var string
+	 */
+	private $aspect;
+
+	/**
+	 * @param EntityId $entityId
+	 * @param string $aspect use the XXX_USAGE constants
+	 */
+	public function __construct( EntityId $entityId, $aspect ) {
+		$this->entityId = $entityId;
+		$this->aspect = $aspect;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getAspect() {
+		return $this->aspect;
+	}
+
+	/**
+	 * @return EntityId
+	 */
+	public function getEntityId() {
+		return $this->entityId;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function toString() {
+		return $this->getEntityId()->getSerialization() . '#' . $this->getAspect();
+	}
+
+	/**
+	 * @return string
+	 */
+	public function __toString() {
+		return $this->toString();
+	}
+
+}
+ 
