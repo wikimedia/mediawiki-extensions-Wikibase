949e0c10edf44b2190de5eba699fd046a74f8382
daniel <daniel.kinzler@wikimedia.de>
2014-09-19 19:07:28 +0200

    Introduce usage tracking service.
    This introduces the UsageTracker and UsageLookup service interfaces
    along with a suitable implementation, database setup/update code,
    and bidings for the top level storage factory.
    
    The usage tracking service is however not used anywhere yet.
    
    Bug: 64586
    Bug: 64587
    Change-Id: Ib3a970179eb47d19e449baba5ed89e46723b5559

---
+++ client/includes/Usage/Sql/entity_usage.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS /*_*/wbc_entity_usage (
+  eu_row_id         BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  eu_entity_type    VARBINARY(32) NOT NULL,  -- the type of the entity being used
+  eu_entity_id      VARBINARY(255) NOT NULL, -- the ID of the entity being used
+  eu_aspect         BINARY(1) NOT NULL,      -- the aspect of the entity: L, I, P or X.
+  eu_page_id        INT NOT NULL
+) /*$wgDBTableOptions*/;
+
+-- record one usage per page per aspect of an entity
+CREATE UNIQUE INDEX /*i*/eu_entity_id ON /*_*/wbc_entity_usage ( eu_entity_id, eu_aspect, eu_page_id );
+
+-- look up (and especially, delete) usage entris by page id
+CREATE INDEX /*i*/eu_page_id ON /*_*/wbc_entity_usage ( eu_page_id, eu_entity_id ) ;
+
+-- look up usage by entity type (filter by namespace)
+CREATE INDEX /*i*/eu_entity_type ON /*_*/wbc_entity_usage ( eu_entity_type ) ;
