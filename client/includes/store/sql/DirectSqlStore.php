0ff58f0a9b6d40be79607e13364802ebdfe5cb05
daniel <daniel.kinzler@wikimedia.de>
2014-09-19 19:07:28 +0200

    Introduce usage tracking service.
    This introduces the UsageTracker and UsageLookup service interfaces
    along with a suitable implementation, database setup/update code,
    and bidings for the top level storage factory.
    
    The usage tracking service is however not used anywhere yet.
    
    Bug: 64586
    Bug: 64587
    Change-Id: Ib3a970179eb47d19e449baba5ed89e46723b5559

--- client/includes/store/sql/DirectSqlStore.php
+++ client/includes/store/sql/DirectSqlStore.php
@@ -3,9 +3,12 @@
 namespace Wikibase;
 
 use Language;
+use LoadBalancer;
 use ObjectCache;
 use Site;
+use Wikibase\Client\Usage\SubscriptionManager;
 use Wikibase\Client\WikibaseClient;
+use Wikibase\DataModel\Entity\EntityIdParser;
 use Wikibase\Lib\Store\CachingEntityRevisionLookup;
 use Wikibase\Lib\Store\EntityContentDataCodec;
 use Wikibase\Lib\Store\EntityLookup;
@@ -15,6 +18,9 @@ use Wikibase\Lib\Store\RevisionBasedEntityLookup;
 use Wikibase\Lib\Store\SiteLinkLookup;
 use Wikibase\Lib\Store\SiteLinkTable;
 use Wikibase\Lib\Store\WikiPageEntityRevisionLookup;
+use Wikibase\Client\Usage\Sql\SqlUsageTracker;
+use Wikibase\Client\Usage\UsageLookup;
+use Wikibase\Client\Usage\UsageTracker;
 
 /**
  * Implementation of the client store interface using direct access to the repository's
@@ -49,6 +55,11 @@ class DirectSqlStore implements ClientStore {
 	private $propertyInfoTable = null;
 
 	/**
+	 * @var EntityIdParser
+	 */
+	private $idParser;
+
+	/**
 	 * @var String|bool $repoWiki
 	 */
 	protected $repoWiki;
@@ -64,9 +75,9 @@ class DirectSqlStore implements ClientStore {
 	private $siteLinkTable = null;
 
 	/**
-	 * @var ItemUsageIndex
+	 * @var SqlUsageTracker
 	 */
-	private $entityUsageIndex = null;
+	private $usageTracker = null;
 
 	/**
 	 * @var Site|null
@@ -96,11 +107,13 @@ class DirectSqlStore implements ClientStore {
 	/**
 	 * @param EntityContentDataCodec $contentCodec
 	 * @param Language $wikiLanguage
-	 * @param string    $repoWiki the symbolic database name of the repo wiki
+	 * @param EntityIdParser $idParser
+	 * @param string $repoWiki the symbolic database name of the repo wiki
 	 */
 	public function __construct(
 		EntityContentDataCodec $contentCodec,
 		Language $wikiLanguage,
+		EntityIdParser $idParser,
 		$repoWiki
 	) {
 		$this->repoWiki = $repoWiki;
@@ -117,30 +130,54 @@ class DirectSqlStore implements ClientStore {
 		$this->cachePrefix = $cachePrefix;
 		$this->cacheDuration = $cacheDuration;
 		$this->cacheType = $cacheType;
+		$this->idParser = $idParser;
 	}
 
 	/**
-	 * @see Store::getEntityUsageIndex
+	 * @see Store::getSubscriptionManager
 	 *
-	 * @since 0.4
+	 * @since 0.5
 	 *
-	 * @return ItemUsageIndex
+	 * @return SubscriptionManager
 	 */
-	public function getItemUsageIndex() {
-		if ( !$this->entityUsageIndex ) {
-			$this->entityUsageIndex = $this->newEntityUsageIndex();
+	public function getSubscriptionManager() {
+		return new SubscriptionManager();
+	}
+
+	/**
+	 * @see Store::getUsageLookup
+	 *
+	 * @since 0.5
+	 *
+	 * @return UsageLookup
+	 */
+	public function getUsageLookup() {
+		return $this->getUsageTracker();
+	}
+
+	/**
+	 * Returns a LoadBalancer that acts as a factory for connections to the local (client) wiki's
+	 * database.
+	 *
+	 * @return LoadBalancer
+	 */
+	private function getLocalLoadBalancer() {
+		return wfGetLB();
+	}
+
+	/**
+	 * @see Store::getUsageTracker
+	 *
+	 * @since 0.5
+	 *
+	 * @return UsageTracker
+	 */
+	public function getUsageTracker() {
+		if ( !$this->usageTracker ) {
+			$this->usageTracker = new SqlUsageTracker( $this->idParser, $this->getLocalLoadBalancer() );
 		}
 
-		return $this->entityUsageIndex;
-	}
-
-	/**
-	 * @since 0.4
-	 *
-	 * @return ItemUsageIndex
-	 */
-	protected function newEntityUsageIndex() {
-		return new ItemUsageIndex( $this->getSite(), $this->getSiteLinkTable() );
+		return $this->usageTracker;
 	}
 
 	/**
