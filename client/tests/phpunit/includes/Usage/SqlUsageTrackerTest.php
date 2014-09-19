fa87ae0c8c33cf629fd453d35b12553a12b294fe
daniel <daniel.kinzler@wikimedia.de>
2014-09-19 19:07:28 +0200

    Introduce usage tracking service.
    This introduces the UsageTracker and UsageLookup service interfaces
    along with a suitable implementation, database setup/update code,
    and bidings for the top level storage factory.
    
    The usage tracking service is however not used anywhere yet.
    
    Change-Id: Ib3a970179eb47d19e449baba5ed89e46723b5559

---
+++ client/tests/phpunit/includes/Usage/SqlUsageTrackerTest.php
@@ -0,0 +1,67 @@
+<?php
+namespace Wikibase\Client\Tests\Usage;
+
+use PHPUnit_Framework_TestCase;
+use PHPUnit_Framework_Assert as Assert;
+use Wikibase\DataModel\Entity\BasicEntityIdParser;
+use Wikibase\Client\Usage\Sql\SqlUsageTracker;
+
+/**
+ * @covers Wikibase\Client\Usage\Sql\SqlUsageTracker
+ *
+ * @group Wikibase
+ * @group WikibaseClient
+ * @group WikibaseUsageTracking
+ * @group database
+ *
+ * @license GPL 2+
+ * @author Daniel Kinzler
+ */
+class SqlUsageTrackerTest extends \MediaWikiTestCase {
+
+	/**
+	 * @var UsageTrackerContractTester
+	 */
+	private $trackerTester;
+
+	/**
+	 * @var UsageLookupContractTester
+	 */
+	private $lookupTester;
+
+	public function __construct( $name = null, array $data = array(), $dataName = '' ) {
+		parent::__construct( $name, $data, $dataName );
+
+		$this->tablesUsed[] = 'wbc_entity_usage';
+	}
+
+	public function setUp() {
+		parent::setUp();
+
+		$sqlUsageTracker = new SqlUsageTracker( new BasicEntityIdParser() );
+
+		$this->trackerTester = new UsageTrackerContractTester( $sqlUsageTracker );
+		$this->lookupTester = new UsageLookupContractTester( $sqlUsageTracker, $sqlUsageTracker );
+	}
+
+	public function testTrackUsedEntities() {
+		$this->trackerTester->testTrackUsedEntities();
+	}
+
+	public function testRemoverEntities() {
+		$this->trackerTester->testRemoveEntities();
+	}
+
+	public function testGetUsageForPage() {
+		$this->lookupTester->testGetUsageForPage();
+	}
+
+	public function testGetPagesUsing() {
+		$this->lookupTester->testGetPagesUsing();
+	}
+
+	public function testGetUnusedEntities() {
+		$this->lookupTester->testGetUnusedEntities();
+	}
+
+}
