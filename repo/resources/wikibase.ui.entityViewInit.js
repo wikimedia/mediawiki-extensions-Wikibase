9d31d04e0d4f0062b80adb67f29ccc57c63050f6
Adrian Lang <adrian.lang@wikimedia.de>
2014-09-24 09:00:13 +0200

    Introduce entityChangers (incomplete)
    Change-Id: Iab57c239f2477cf71198b7a94d05d231fdf2dfbf

--- repo/resources/wikibase.ui.entityViewInit.js
+++ repo/resources/wikibase.ui.entityViewInit.js
@@ -96,6 +96,7 @@
 		$entityview
 		.entityview( {
 			value: entity,
+			entityChangersFactory: new wb.entityChangers.EntityChangersFactory( abstractedRepoApi, wb.getRevisionStore(), entity ),
 			entityStore: entityStore,
 			valueViewBuilder: new wb.ValueViewBuilder(
 				experts,
