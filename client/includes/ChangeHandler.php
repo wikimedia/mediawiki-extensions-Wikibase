972aa8ad440c5fdea1105bb68f5d06fd75850cd9
daniel <daniel.kinzler@wikimedia.de>
2014-09-24 19:43:49 +0200

    Record usage info from ParserOutput to the database.
    Note: this is a naive baseline implementation of what bug 64589
    calls for. To resolve that bug fully, updates should depend on
    *how* the entity is used on a given page (i.e. on the "aspect"
    being used).
    
    Bug: 64589
    Change-Id: I674915557397a827f250261c7d11afc71f8f646f

--- client/includes/ChangeHandler.php
+++ client/includes/ChangeHandler.php
@@ -6,6 +6,7 @@ use MWException;
 use Site;
 use SiteList;
 use Title;
+use Wikibase\Client\Usage\UsageLookup;
 use Wikibase\Client\WikibaseClient;
 use Wikibase\Lib\Changes\EntityChangeFactory;
 use Wikibase\Lib\Store\EntityRevisionLookup;
@@ -108,7 +109,7 @@ class ChangeHandler {
 		EntityChangeFactory $changeFactory = null,
 		PageUpdater $updater = null,
 		EntityRevisionLookup $entityRevisionLookup = null,
-		ItemUsageIndex $entityUsageIndex = null,
+		UsageLookup $entityUsage = null,
 		Site $localSite = null,
 		SiteList $sites = null
 	) {
@@ -130,8 +131,8 @@ class ChangeHandler {
 			$entityRevisionLookup = $wikibaseClient->getStore()->getEntityRevisionLookup();
 		}
 
-		if ( !$entityUsageIndex ) {
-			$entityUsageIndex = $wikibaseClient->getStore()->getItemUsageIndex();
+		if ( !$entityUsage ) {
+			$entityUsage = $wikibaseClient->getStore()->getUsageLookup();
 		}
 
 		if ( $sites === null ) {
@@ -154,7 +155,7 @@ class ChangeHandler {
 
 		$this->updater = $updater;
 		$this->entityRevisionLookup = $entityRevisionLookup;
-		$this->entityUsageIndex = $entityUsageIndex;
+		$this->usageLookup = $entityUsage;
 
 		$this->site = $localSite;
 		$this->siteId = $localSite->getGlobalId();
@@ -563,7 +564,7 @@ class ChangeHandler {
 
 		// todo inject!
 		$referencedPagesFinder = new ReferencedPagesFinder(
-			$this->entityUsageIndex,
+			$this->usageLookup,
 			$this->namespaceChecker,
 			$this->siteId,
 			$this->checkPageExistence
