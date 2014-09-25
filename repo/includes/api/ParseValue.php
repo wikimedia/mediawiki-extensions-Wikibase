4a9b924aec89fdd25cf38aed590ca520a704d440
lucie <lucie.kaffee@wikimedia.de>
2014-09-25 13:05:57 +0200

    [WIP] Add examples to ParseValue
    First Version of the examples. Want to add an example with options, too.
    Would be lovely, if someone could correct my English and make the message clearer.
    
    Bug: 69739
    Change-Id: Ic8e0e4b91a190134b2364a48f766e9661a856bfd

--- repo/includes/api/ParseValue.php
+++ repo/includes/api/ParseValue.php
@@ -209,7 +209,236 @@ class ParseValue extends ApiWikibase {
 	 */
 	protected function getExamples() {
 		return array(
-			// 'ex' => 'desc' // TODO
+			'https://www.wikidata.org/w/api.php?action=wbparsevalue&parser=time&values=1994-02-08' => 'Parse 1994-02-08 to a date format.', //there should be an example with options here as well
+			'https://www.wikidata.org/w/api.php?action=wbparsevalue&parser=null&values=foo|bar' => 'No change to the format of the string.',
+		);
+	}
+
+	/**
+	 * @see ApiBase::getVersion
+	 *
+	 * @since 0.1
+	 *
+	 * @return string
+	 */
+	public function getVersion() {
+		return __CLASS__ . '-0.2';
+	}
+
+}
+<?php
+
+namespace Wikibase\Api;
+
+use ApiBase;
+use DataValues\DataValue;
+use LogicException;
+use OutOfBoundsException;
+use ValueParsers\ParseException;
+use ValueParsers\ParserOptions;
+use ValueParsers\ValueParser;
+use Wikibase\Repo\ValueParserFactory;
+
+/**
+ * API module for using value parsers.
+ *
+ * @since 0.1
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ * @author Daniel Kinzler
+ */
+class ParseValue extends ApiWikibase {
+
+	/**
+	 * @var null|ValueParserFactory
+	 */
+	protected $factory = null;
+
+	/**
+	 * @since 0.1
+	 *
+	 * @return ValueParserFactory
+	 */
+	protected function getFactory() {
+		if ( $this->factory === null ) {
+			$this->factory = new ValueParserFactory( $GLOBALS['wgValueParsers'] );
+		}
+
+		return $this->factory;
+	}
+
+	/**
+	 * @see ApiBase::execute
+	 *
+	 * @since 0.1
+	 */
+	public function execute() {
+		$parser = $this->getParser();
+
+		$results = array();
+
+		$params = $this->extractRequestParams();
+
+		foreach ( $params['values'] as $value ) {
+			$results[] = $this->parseValue( $parser, $value );
+		}
+
+		$this->outputResults( $results );
+	}
+
+	/**
+	 * @return ValueParser
+	 * @throws LogicException
+	 */
+	private function getParser() {
+		$params = $this->extractRequestParams();
+
+		$options = $this->getOptionsObject( $params['options'] );
+
+		try {
+			$parser = $this->getFactory()->newParser( $params['parser'], $options );
+		} catch ( OutOfBoundsException $ex ) {
+			throw new LogicException( 'Could not obtain a ValueParser instance' );
+		}
+
+		return $parser;
+	}
+
+	private function parseValue( ValueParser $parser, $value ) {
+		$result = array(
+			'raw' => $value
+		);
+
+		try {
+			$parseResult = $parser->parse( $value );
+		}
+		catch ( ParseException $parseError ) {
+			$this->addParseErrorToResult( $result, $parseError );
+			return $result;
+		}
+
+		if ( $parseResult instanceof DataValue ) {
+			$result['value'] = $parseResult->getArrayValue();
+			$result['type'] = $parseResult->getType();
+		}
+		else {
+			$result['value'] = $parseResult;
+		}
+
+		return $result;
+	}
+
+	protected function addParseErrorToResult( &$result, ParseException $parseError ) {
+		$result['error'] = get_class( $parseError );
+
+		$result['error-info'] = $parseError->getMessage();
+		$result['expected-format'] = $parseError->getExpectedFormat();
+
+		$status = $this->getExceptionStatus( $parseError );
+		$this->getErrorReporter()->addStatusToResult( $status, $result );
+	}
+
+	private function outputResults( array $results ) {
+		$this->getResult()->setIndexedTagName( $results, 'result' );
+
+		$this->getResult()->addValue(
+			null,
+			'results',
+			$results
+		);
+	}
+
+	/**
+	 * @since 0.1
+	 *
+	 * @param string $optionsParam
+	 *
+	 * @return ParserOptions
+	 */
+	protected function getOptionsObject( $optionsParam ) {
+		$parserOptions = new ParserOptions();
+		$parserOptions->setOption( ValueParser::OPT_LANG, $this->getLanguage()->getCode() );
+
+		if ( $optionsParam !== null && $optionsParam !== '' ) {
+			$options = \FormatJson::decode( $optionsParam, true );
+
+			if ( !is_array( $options ) ) {
+				$this->dieError( 'Malformed options parameter', 'malformed-options' );
+			}
+
+			foreach ( $options as $name => $value ) {
+				$parserOptions->setOption( $name, $value );
+			}
+		}
+
+		return $parserOptions;
+	}
+
+	/**
+	 * @see ApiBase::getAllowedParams
+	 *
+	 * @since 0.1
+	 *
+	 * @return array
+	 */
+	public function getAllowedParams() {
+		return array(
+			'parser' => array(
+				ApiBase::PARAM_TYPE => $this->getFactory()->getParserIds(),
+				ApiBase::PARAM_REQUIRED => true,
+			),
+			'values' => array(
+				ApiBase::PARAM_TYPE => 'string',
+				ApiBase::PARAM_REQUIRED => true,
+				ApiBase::PARAM_ISMULTI => true,
+			),
+			'options' => array(
+				ApiBase::PARAM_TYPE => 'string',
+				ApiBase::PARAM_REQUIRED => false,
+			),
+		);
+	}
+
+	/**
+	 * @see ApiBase::getParamDescription
+	 *
+	 * @since 0.1
+	 *
+	 * @return array
+	 */
+	public function getParamDescription() {
+		return array(
+			'parser' => 'Id of the ValueParser to use',
+			'values' => 'The values to parse',
+			'options' => 'The options the parser should use. Provided as a JSON object.',
+		);
+	}
+
+	/**
+	 * @see ApiBase::getDescription
+	 *
+	 * @since 0.1
+	 *
+	 * @return string
+	 */
+	public function getDescription() {
+		return array(
+			'API module for parsing values using a ValueParser.'
+		);
+	}
+
+	/**
+	 * @see ApiBase::getExamples
+	 *
+	 * @since 0.1
+	 *
+	 * @return array
+	 */
+	protected function getExamples() {
+		return array(
+			'https://www.wikidata.org/w/api.php?action=wbparsevalue&parser=time&values=1994-02-08' => 'Parse 1994-02-08 to a date format.', //there should be an example with options here as well
+			'https://www.wikidata.org/w/api.php?action=wbparsevalue&parser=null&values=foo|bar' => 'No change to the format of the string.',
 		);
 	}
 
