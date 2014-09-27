84840c67275fa05e3bca3cfd71c4052aca32e82f
Bene <benestar.wikimedia@gmail.com>
2014-09-27 15:56:41 +0200

    Show labels instead of item ids on badge diffs
    Bug: 698322
    Change-Id: Ie94ea8c6a3612e14996641812f95a1e0a2e9ee98

--- repo/tests/phpunit/includes/Diff/DiffViewTest.php
+++ repo/tests/phpunit/includes/Diff/DiffViewTest.php
@@ -6,9 +6,10 @@ use Diff\DiffOp\Diff\Diff;
 use Diff\DiffOp\DiffOpAdd;
 use Diff\DiffOp\DiffOpChange;
 use Diff\DiffOp\DiffOpRemove;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\DataModel\Entity\ItemId;
 use Wikibase\Repo\Diff\DiffView;
 use Wikibase\Repo\WikibaseRepo;
-use Wikibase\DataModel\Entity\ItemId;
 
 /**
  * @covers Wikibase\Repo\Diff\DiffView
@@ -19,7 +20,7 @@ use Wikibase\DataModel\Entity\ItemId;
  * @licence GNU GPL v2+
  * @author Thiemo Mättig
  */
-class DiffViewTest extends \PHPUnit_Framework_TestCase {
+class DiffViewTest extends \MediaWikiLangTestCase {
 
 	public function diffOpProvider() {
 		$linkPath = wfMessage( 'wikibase-diffview-link' )->text();
@@ -69,6 +70,12 @@ class DiffViewTest extends \PHPUnit_Framework_TestCase {
 				'Q123',
 				$linkPath . '/enwiki/badges'
 			),
+			'Badge has label' => array(
+				'@<a\b[^>]*>nyan article</a>@',
+				null,
+				'Q123',
+				$linkPath . '/enwiki/badges'
+			),
 		);
 	}
 
@@ -82,6 +89,13 @@ class DiffViewTest extends \PHPUnit_Framework_TestCase {
 			$diffOps['add'] = new DiffOpAdd( $newValue );
 		}
 		return $diffOps;
+	}
+
+	private function getBadgeItem() {
+		$item = Item::newEmpty();
+		$item->setId( new ItemId( 'Q123' ) );
+		$item->getFingerprint()->setLabel( 'en', 'nyan article' );
+		return $item;
 	}
 
 	/**
@@ -99,7 +113,9 @@ class DiffViewTest extends \PHPUnit_Framework_TestCase {
 		$diff = new Diff( $this->getDiffOps( $oldValue, $newValue ) );
 		$siteStore = MockSiteStore::newFromTestSites();
 		$entityTitleLookup = WikibaseRepo::getDefaultInstance()->getEntityTitleLookup();
-		$diffView = new DiffView( $path, $diff, $siteStore, $entityTitleLookup );
+		$mockRepository = new MockRepository();
+		$mockRepository->putEntity( $this->getBadgeItem() );
+		$diffView = new DiffView( $path, $diff, $siteStore, $entityTitleLookup, $mockRepository );
 
 		$html = $diffView->getHtml();
 
