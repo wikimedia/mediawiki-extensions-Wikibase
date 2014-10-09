c976215828a088cbbda8132f550b9cfe8b3029be
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2014-10-09 16:11:39 +0200

    Drop aliases in Wikibase main namespace

--- lib/packages/wikibase/data-model/tests/component/AutoloadingAliasesTest.php
+++ lib/packages/wikibase/data-model/tests/component/AutoloadingAliasesTest.php
@@ -24,33 +24,6 @@ class AutoloadingAliasesTest extends \PHPUnit_Framework_TestCase {
 				return array( $className );
 			},
 			array(
-				'Wikibase\EntityId',
-
-				'Wikibase\Reference',
-				'Wikibase\ReferenceList',
-				'Wikibase\References',
-				'Wikibase\HashableObjectStorage',
-				'Wikibase\HashArray',
-				'Wikibase\MapHasher',
-				'Wikibase\ByPropertyIdArray',
-				'Wikibase\Claim',
-				'Wikibase\ClaimListAccess',
-				'Wikibase\Claims',
-				'Wikibase\Statement',
-				'Wikibase\Entity',
-				'Wikibase\Item',
-				'Wikibase\Property',
-				'Wikibase\PropertyNoValueSnak',
-				'Wikibase\PropertySomeValueSnak',
-				'Wikibase\PropertyValueSnak',
-				'Wikibase\Snak',
-				'Wikibase\SnakList',
-				'Wikibase\SnakObject',
-				'Wikibase\SnakRole',
-				'Wikibase\Snaks',
-				'Wikibase\ItemDiff',
-				'Wikibase\EntityDiff',
-
 				'Wikibase\DataModel\SimpleSiteLink',
 			)
 		);
