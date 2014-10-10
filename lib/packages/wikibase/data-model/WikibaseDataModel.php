4950232db4f3370981231ca261e35458e8d618b2
jeroendedauw <jeroendedauw@gmail.com>
2014-10-10 14:53:09 +0200

    Fix branch alias and version number

--- lib/packages/wikibase/data-model/WikibaseDataModel.php
+++ lib/packages/wikibase/data-model/WikibaseDataModel.php
@@ -12,7 +12,7 @@ if ( defined( 'WIKIBASE_DATAMODEL_VERSION' ) ) {
 	return 1;
 }
 
-define( 'WIKIBASE_DATAMODEL_VERSION', '1.1' );
+define( 'WIKIBASE_DATAMODEL_VERSION', '2.0 alpha' );
 
 if ( defined( 'MEDIAWIKI' ) ) {
 	call_user_func( function() {
