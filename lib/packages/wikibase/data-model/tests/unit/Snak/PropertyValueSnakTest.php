00cf84a027aa6d143f9a91fc03fdfc70414ad761
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2014-10-14 11:27:13 +0200

    Test if getHash is stable

--- lib/packages/wikibase/data-model/tests/unit/Snak/PropertyValueSnakTest.php
+++ lib/packages/wikibase/data-model/tests/unit/Snak/PropertyValueSnakTest.php
@@ -22,6 +22,7 @@ use Wikibase\DataModel\Snak\PropertyValueSnak;
  *
  * @licence GNU GPL v2+
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ * @author Thiemo Mättig
  */
 class PropertyValueSnakTest extends SnakObjectTest {
 
@@ -34,6 +35,18 @@ class PropertyValueSnakTest extends SnakObjectTest {
 
 	public function getClass() {
 		return 'Wikibase\DataModel\Snak\PropertyValueSnak';
+	}
+
+	/**
+	 * This test is a safeguard to make sure hashes are not changed unintentionally.
+	 * @see EntityIdTest::testSerializationStability
+	 */
+	public function testHashStability() {
+		$snak = new PropertyValueSnak( new PropertyId( 'P1' ), new StringValue( 'a' ) );
+		$hash = $snak->getHash();
+
+		$expected = sha1( 'C:41:"Wikibase\DataModel\Snak\PropertyValueSnak":53:{a:2:{i:0;i:1;i:1;C:22:"DataValues\StringValue":1:{a}}}' );
+		$this->assertSame( $expected, $hash );
 	}
 
 	/**
