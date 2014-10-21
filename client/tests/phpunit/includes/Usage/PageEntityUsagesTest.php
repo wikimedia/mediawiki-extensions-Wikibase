afd4e4401c16755002d95b650c5804fc6cba0c01
daniel <daniel.kinzler@wikimedia.de>
2014-10-21 20:33:54 +0200

    Filter page purged by usage aspect.
    Bug: 71352
    Change-Id: I9ce6a2e90347f6ce8a9ce9ef4d0a083592211d23

---
+++ client/tests/phpunit/includes/Usage/PageEntityUsagesTest.php
@@ -0,0 +1,48 @@
+<?php
+namespace Wikibase\Client\Tests\Usage;
+
+use PHPUnit_Framework_TestCase;
+use Wikibase\Client\Usage\EntityUsage;
+use Wikibase\Client\Usage\PageEntityUsages;
+use Wikibase\DataModel\Entity\ItemId;
+
+/**
+ * @covers Wikibase\Client\Usage\PageEntityUsages
+ *
+ * @group Wikibase
+ * @group WikibaseClient
+ * @group WikibaseUsageTracking
+ *
+ * @license GPL 2+
+ * @author Daniel Kinzler
+ */
+class PageEntityUsagesTest extends PHPUnit_Framework_TestCase {
+
+	public function testGetters() {
+		$q7 = new ItemId( 'Q7' );
+		$q11 = new ItemId( 'Q11' );
+
+		$usages = array(
+			new EntityUsage( $q7, EntityUsage::ALL_USAGE ),
+			new EntityUsage( $q11, EntityUsage::LABEL_USAGE ),
+			new EntityUsage( $q11, EntityUsage::TITLE_USAGE ),
+		);
+
+		$pageUsages = new PageEntityUsages( 6, $usages );
+
+		$this->assertEquals( 6, $pageUsages->getPageId() );
+
+		$expectedAspects = array(
+			EntityUsage::LABEL_USAGE,
+			EntityUsage::TITLE_USAGE,
+			EntityUsage::ALL_USAGE,
+		);
+
+		$this->assertEquals( $expectedAspects, $pageUsages->getAspects() );
+		$this->assertEquals( array( 'Q11' => $q11, 'Q7' => $q7 ), $pageUsages->getEntityIds() );
+		$this->assertEquals( array( 'Q11#L', 'Q11#T', 'Q7#X' ), array_keys( $pageUsages->getUsages() ) );
+
+		$this->assertEquals( array( EntityUsage::ALL_USAGE ), $pageUsages->getUsageAspects( $q7 ) );
+	}
+
+}
