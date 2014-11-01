4662cdefd15e69f820486420fbec88fe08c0a27d
Marius Hoch <hoo@online.de>
2014-11-01 20:49:53 +0100

    Add UpdateRepoOnDeleteJob
    For updating the repo after a page on the client has been deleted.
    
    Bug: 49100
    Change-Id: Idc382607b4c29ae7ca7e52c50e8f727e5ff8d619

---
+++ repo/tests/phpunit/includes/UpdateRepo/UpdateRepoOnDeleteJobTest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Wikibase\Repo\Tests\UpdateRepo;
+
+use Title;
+use User;
+use Wikibase\DataModel\Entity\BasicEntityIdParser;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\Repo\Store\SQL\EntityPerPageTable;
+use Wikibase\Repo\Store\WikiPageEntityStore;
+use Wikibase\Repo\WikibaseRepo;
+use Wikibase\Repo\UpdateRepo\UpdateRepoOnDeleteJob;
+
+/**
+ * @covers Wikibase\Repo\UpdateRepo\UpdateRepoOnDeleteJob
+ *
+ * @group Wikibase
+ * @group WikibaseRepo
+ * @group WikibaseIntegration
+ * @group Database
+ *
+ * @licence GNU GPL v2+
+ * @author Marius Hoch < hoo@online.de >
+ */
+class UpdateRepoOnDeleteJobTest extends \MediaWikiTestCase {
+
+	public function testGetSummary() {
+		$job = new UpdateRepoOnDeleteJob( Title::newMainPage() );
+
+		$summary = $job->getSummary( 'SiteID', 'Test' );
+
+		$this->assertEquals( 'clientsitelink-remove', $summary->getMessageKey() );
+		$this->assertEquals( 'SiteID', $summary->getLanguageCode() );
+		$this->assertEquals(
+			array( 'SiteID:Test' ),
+			$summary->getCommentArgs()
+		);
+	}
+
+	/**
+	 * Simple generic integration test testing the whole class.
+	 */
+	public function testJob() {
+		$wikibaseRepo = WikibaseRepo::getDefaultInstance();
+
+		$enwiki = $this->getMock( 'Site' );
+		$enwiki->expects( $this->once() )
+			->method( 'normalizePageName' )
+			->will( $this->returnValue( false ) );
+
+		$siteStore = $this->getMock( 'SiteStore' );
+		$siteStore->expects( $this->once() )
+			->method( 'getSite' )
+			->with( 'enwiki' )
+			->will( $this->returnValue( $enwiki ) );
+
+		$user = User::newFromName( 'UpdateRepoOnDeleteJobTest' );
+		$user->addToDatabase();
+
+		$item = Item::newEmpty();
+		$item->getSiteLinkList()->addNewSiteLink( 'enwiki', 'Delete me', array( new ItemId( 'Q42' ) ) );
+
+		$store = new WikiPageEntityStore(
+			$wikibaseRepo->getEntityContentFactory(),
+			$wikibaseRepo->getStore()->newIdGenerator(),
+			new EntityPerPageTable( new BasicEntityIdParser() )
+		);
+
+		$store->saveEntity( $item, 'UpdateRepoOnDeleteJobTest', $user, EDIT_NEW );
+
+		$params = array(
+			'siteId' => 'enwiki',
+			'entityId' => $item->getId()->getSerialization(),
+			'title' => 'Delete me',
+			'user' => $user->getName()
+		);
+
+		$job = new UpdateRepoOnDeleteJob( Title::newMainPage(), $params );
+		$job->initServices(
+			$wikibaseRepo->getEntityTitleLookup(),
+			$wikibaseRepo->getEntityRevisionLookup( 'uncached' ),
+			$wikibaseRepo->getEntityStore(),
+			$wikibaseRepo->getSummaryFormatter(),
+			$wikibaseRepo->getEntityPermissionChecker(),
+			$siteStore
+		);
+
+		$job->run();
+
+		$item = $wikibaseRepo->getEntityLookup()->getEntity( $item->getId() );
+
+		$this->assertFalse(
+			$item->getSiteLinkList()->hasLinkWithSiteId( 'enwiki' ),
+			'Sitelink has been removed.'
+		);
+	}
+}
