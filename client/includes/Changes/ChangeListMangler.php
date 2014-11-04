5e3b3baf2d1df6f4ae1255322dc34ff265170adf
daniel <daniel.kinzler@wikimedia.de>
2014-11-04 20:12:10 +0100

    Extract change coalescing logic from ChangeHandler.
    This introduces the ChangeRunCoalescer class.
    
    Change-Id: Ib0085abc4c1cbf8ca162a031a96008840690907b

---
+++ client/includes/Changes/ChangeListMangler.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Wikibase\Client\Changes;
+
+use Wikibase\Change;
+
+/**
+ * Interface for service objects implementing some operation on a list of Change objects.
+ *
+ * @since 0.5
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ *
+ */
+interface ChangeListMangler {
+
+	/**
+	 * Processes the given list of changes, possibly merging, filtering or otherwise modifying
+	 * changes and/or the list of changes.
+	 *
+	 * @param Change[] $changes
+	 *
+	 * @return Change[]
+	 */
+	public function mangleChanges( array $changes );
+
+}
