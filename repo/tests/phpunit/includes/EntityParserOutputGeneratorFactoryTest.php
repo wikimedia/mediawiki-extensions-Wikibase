25d068a24c75389b94ea4ef5ffbdbface2c7489d
aude <aude.wiki@gmail.com>
2014-11-05 13:48:28 +0100

    Implement EntityParserOutputGeneratorFactory
    allows removing a lot of cruft from EntityContent
    
    Change-Id: I9453fd95eb7af8f34e7dbe18bd159b8bcb1927f7

---
+++ repo/tests/phpunit/includes/EntityParserOutputGeneratorFactoryTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Wikibase;
+
+use Language;
+use ParserOptions;
+use TestUser;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\Repo\WikibaseRepo;
+
+/**
+ * @group Database
+ *
+ * @licence GNU GPL v2+
+ * @author Katie Filbert < aude.wiki@gmail.com >
+ */
+class EntityParserOutputGeneratorFactoryTest extends \MediaWikiTestCase {
+
+	/**
+	 * @dataProvider getEntityParserOutputGeneratorProvider
+	 */
+	public function testGetEntityParserOutputGenerator( $entityType ) {
+		$wikibaseRepo = WikibaseRepo::getDefaultInstance();
+		$parserOutputGeneratorFactory = $wikibaseRepo->getEntityParserOutputGeneratorFactory();
+
+		$parserOutputGenerator = $parserOutputGeneratorFactory->getEntityParserOutputGenerator(
+			$entityType,
+			$this->getParserOptions()
+		);
+
+		$this->assertInstanceOf(
+			'Wikibase\EntityParserOutputGenerator',
+			$parserOutputGenerator
+		);
+	}
+
+	public function getEntityParserOutputGeneratorProvider() {
+		return array(
+			array( 'item' ),
+			array( 'property' )
+		);
+	}
+
+	public function testGetEntityParserOutputGenerator_invalidType() {
+		$wikibaseRepo = WikibaseRepo::getDefaultInstance();
+		$parserOutputGeneratorFactory = $wikibaseRepo->getEntityParserOutputGeneratorFactory();
+
+		$this->setExpectedException( 'InvalidArgumentException' );
+
+		$parserOutputGeneratorFactory->getEntityParserOutputGenerator(
+			'kittens',
+			$this->getParserOptions()
+		);
+	}
+
+	private function getParserOptions() {
+		$testUser = new TestUser( 'Wikibase User' );
+		$language = Language::factory( 'en' );
+
+		return new ParserOptions( $testUser->getUser(), $language );
+	}
+}
