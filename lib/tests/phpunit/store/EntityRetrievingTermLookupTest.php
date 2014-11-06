560c99645f54ff3e7580839cb15a767b68b83776
aude <aude.wiki@gmail.com>
2014-11-06 12:56:07 +0100

    Use in LabelLookup (w/ EntityRetrievingTermLookup) in EntityIdLabelFormatter
    this moves the label lookup code out of the formatter,
    and into the EntityRetrievingTermLookup and then injects
    a LabelLookup (albeit still using EntityLookup) into
    the EntityIdLabelFormatter.
    
    in comparing the TermIndex (database) TermLookup vs this,
    it seems this is better option (loading the entire entity from
    memcached) of the two when doing many lookups.
    
    and makes it easier to swap in a batching lookup or other
    implementation at a later time, depending on the use case.
    
    Change-Id: Ia6a968d289cefc2e4df917b60dc761652d8354b6

---
+++ lib/tests/phpunit/store/EntityRetrievingTermLookupTest.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Wikibase\Test;
+
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\Lib\Store\EntityRetrievingTermLookup;
+
+class EntityRetrievingTermLookupTest extends \PHPUnit_Framework_TestCase {
+
+	public function testGetLabel() {
+		$termLookup = $this->getEntityTermLookup();
+
+		$label = $termLookup->getLabel( new ItemId( 'Q116' ), 'en' );
+		$this->assertEquals( 'New York City', $label );
+	}
+
+	public function testGetLabel_notFoundThrowsException() {
+		$termLookup = $this->getEntityTermLookup();
+
+		$this->setExpectedException( 'OutOfBoundsException' );
+		$termLookup->getLabel( new ItemId( 'Q116' ), 'fa' );
+	}
+
+	public function testGetLabel_entityNotFound() {
+		$termLookup = $this->getEntityTermLookup();
+
+		$this->setExpectedException( '\Wikibase\Lib\Store\StorageException' );
+		$termLookup->getLabel( new ItemId( 'Q120' ), 'en' );
+	}
+
+	public function testGetLabels() {
+		$termLookup = $this->getEntityTermLookup();
+
+		$labels = $termLookup->getLabels( new ItemId( 'Q116' ) );
+
+		$expected = array(
+			'en' => 'New York City',
+			'es' => 'Nueva York'
+		);
+
+		$this->assertEquals( $expected, $labels );
+	}
+
+	public function testGetDescription() {
+		$termLookup = $this->getEntityTermLookup();
+
+		$description = $termLookup->getDescription( new ItemId( 'Q116' ), 'de' );
+		$expected = 'Metropole an der Ostküste der Vereinigten Staaten';
+
+		$this->assertEquals( $expected, $description );
+	}
+
+	public function testGetDescription_notFoundThrowsException() {
+		$termLookup = $this->getEntityTermLookup();
+
+		$this->setExpectedException( 'OutOfBoundsException' );
+		$termLookup->getDescription( new ItemId( 'Q116' ), 'fr' );
+	}
+
+	public function getDescriptions() {
+		$termLookup = $this->getEntityTermLookup();
+
+		$descriptions = $termLookup->getDescriptions( new ItemId( 'Q116' ) );
+
+		$expected = array(
+			'de' => 'Metropole an der Ostküste der Vereinigten Staaten',
+			'en' => 'largest city in New York and the United States of America',
+		);
+
+		$this->assertEquals( $expected, $descriptions );
+	}
+
+	private function getEntityTermLookup() {
+		return new EntityRetrievingTermLookup( $this->getEntityLookup() );
+	}
+
+	private function getEntityLookup() {
+		$mockRepo = new MockRepository();
+
+		$item = Item::newEmpty();
+		$item->setId( new ItemId( 'Q116' ) );
+
+		$item->setLabel( 'en', 'New York City' );
+		$item->setLabel( 'es', 'Nueva York' );
+
+		$item->setDescription( 'de', 'Metropole an der Ostküste der Vereinigten Staaten' );
+		$item->setDescription( 'en', 'largest city in New York and the United States of America' );
+
+		$mockRepo->putEntity( $item );
+
+		return $mockRepo;
+	}
+
+}
