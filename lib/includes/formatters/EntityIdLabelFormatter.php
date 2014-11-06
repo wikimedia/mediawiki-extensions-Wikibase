6d0e73b478176f5c6d45c81b4d663d270c471270
aude <aude.wiki@gmail.com>
2014-11-06 12:56:07 +0100

    Implement CachingLabelLookup with EntityLookup [WIP]
    this moves the label lookup code out of the formatter,
    and into implementations of the LabelLookup, using
    the existing caching EntityLookup service which uses
    memcached.  this is better performance-wise, at the
    moment than using lookup via the TermSqlIndex.
    
    Change-Id: Ia6a968d289cefc2e4df917b60dc761652d8354b6

--- lib/includes/formatters/EntityIdLabelFormatter.php
+++ lib/includes/formatters/EntityIdLabelFormatter.php
@@ -8,6 +8,8 @@ use ValueFormatters\FormatterOptions;
 use ValueFormatters\FormattingException;
 use Wikibase\DataModel\Entity\EntityId;
 use Wikibase\LanguageFallbackChain;
+use Wikibase\Lib\Store\CachingLanguageFallbackLabelLookup;
+use Wikibase\Lib\Store\CachingLanguageLabelLookup;
 use Wikibase\Lib\Store\EntityLookup;
 use Wikibase\Lib\Store\StorageException;
 use Wikibase\Lib\Store\UnresolvedRedirectException;
@@ -19,8 +21,6 @@ use Wikibase\Lib\Store\UnresolvedRedirectException;
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  * @author Katie Filbert < aude.wiki@gmail.com >
  * @author Daniel Kinzler
- *
- * @todo: add support for language fallback chains
  */
 class EntityIdLabelFormatter extends EntityIdFormatter {
 
@@ -39,9 +39,9 @@ class EntityIdLabelFormatter extends EntityIdFormatter {
 	const FALLBACK_NONE = 2;
 
 	/**
-	 * @var EntityLookup
+	 * @var LabelLookup
 	 */
-	protected $entityLookup;
+	private $labelLookup;
 
 	/**
 	 * @since 0.4
@@ -54,8 +54,6 @@ class EntityIdLabelFormatter extends EntityIdFormatter {
 	 */
 	public function __construct( FormatterOptions $options, EntityLookup $entityLookup ) {
 		parent::__construct( $options );
-
-		$this->entityLookup = $entityLookup;
 
 		$this->defaultOption( self::OPT_LOOKUP_LABEL, true );
 		$this->defaultOption( self::OPT_LABEL_FALLBACK, self::FALLBACK_PREFIXED_ID );
@@ -75,6 +73,18 @@ class EntityIdLabelFormatter extends EntityIdFormatter {
 
 		if ( !is_bool( $this->getOption( self::OPT_LOOKUP_LABEL ) ) ) {
 			throw new InvalidArgumentException( 'Bad value for OPT_LOOKUP_LABEL option: must be a boolean' );
+		}
+
+		if ( $this->options->hasOption( 'languages' ) ) {
+			$this->labelLookup = new CachingLanguageFallbackLabelLookup(
+				$entityLookup,
+				$this->getOption( 'languages' )
+			);
+		} else {
+			$this->labelLookup = new CachingLanguageLabelLookup(
+				$entityLookup,
+				$this->getOption( self::OPT_LANG )
+			);
 		}
 	}
 
@@ -124,37 +134,7 @@ class EntityIdLabelFormatter extends EntityIdFormatter {
 	 * @return string|bool False if no label was found in the language or language fallback chain.
 	 */
 	protected function lookupEntityLabel( EntityId $entityId ) {
-		try {
-			$entity = $this->entityLookup->getEntity( $entityId );
-		} catch ( UnresolvedRedirectException $ex )  {
-			$entity = null;
-		} catch ( StorageException $ex )  {
-			$entity = null;
-			wfLogWarning( 'Failed to load entity: '
-				. $entityId->getSerialization() . ': '
-				. $ex->getMessage() );
-		}
-
-		if ( $entity === null ) {
-			// double redirect, deleted entity, etc
-			throw new OutOfBoundsException( "An Entity with the id $entityId could not be loaded" );
-		}
-
-		/* @var LanguageFallbackChain $languageFallbackChain */
-		if ( $this->options->hasOption( 'languages' ) ) {
-			$languageFallbackChain = $this->getOption( 'languages' );
-
-			$extractedData = $languageFallbackChain->extractPreferredValue( $entity->getLabels() );
-
-			if ( $extractedData === null ) {
-				return false;
-			} else {
-				return $extractedData['value'];
-			}
-		} else {
-			$lang = $this->getOption( self::OPT_LANG );
-			return $entity->getLabel( $lang );
-		}
+		return $this->labelLookup->getLabel( $entityId );
 	}
 
 }
