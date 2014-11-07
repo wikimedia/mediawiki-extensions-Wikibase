b2d77850836d1f2ed31c52098a6a808a4407a18e
Marius Hoch <hoo@online.de>
2014-11-07 17:22:32 +0100

    Introduce the StatementListProvider interface
    So that it's possible to use type hints to recognize whether
    something can hold Statements.

---
+++ lib/packages/wikibase/data-model/src/StatementListProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Wikibase\DataModel;
+
+use Wikibase\DataModel\Statement\StatementList;
+
+/**
+ * Interface for classes that can contain Statements.
+ *
+ * @since 2.2.0
+ *
+ * @license GNU GPL v2+
+ * @author Marius Hoch < hoo@online.de >
+ */
+interface StatementListProvider {
+
+	/**
+	 * @return StatementList
+	 */
+	public function getStatements();
+
+}
