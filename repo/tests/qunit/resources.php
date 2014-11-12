fa1d6be2c519b4c695882b10c4f83976d6c01312
Adrian Lang <adrian.lang@wikimedia.de>
2014-11-12 14:11:37 +0100

    Move wikibase.dataTypes from lib to repo
    It's only used in repo.
    
    Change-Id: If4e31db707ce582af10ed42a714fe95e55f1ea34

--- repo/tests/qunit/resources.php
+++ repo/tests/qunit/resources.php
@@ -82,6 +82,16 @@ return call_user_func( function() {
 			),
 		),
 
+		'wikibase.dataTypes.tests' => $moduleBase + array(
+			'scripts' => array(
+				'dataTypes/wikibase.dataTypes.tests.js',
+			),
+			'dependencies' => array(
+				'dataTypes.DataTypeStore',
+				'wikibase.dataTypes',
+			),
+		),
+
 		'wikibase.ValueViewBuilder.tests' => $moduleBase + array(
 			'scripts' => array(
 				'wikibase.ValueViewBuilder.tests.js'
