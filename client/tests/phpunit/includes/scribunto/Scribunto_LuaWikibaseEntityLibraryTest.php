c8cf3b3a922d191e192a2268eacf322c49ce2a3f
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2014-11-12 14:18:18 +0100

    Fix visibility issues in Scribunto tests
    This needs If6d2705 to be merged first.
    
    Change-Id: I1f71d9d4b42518bdc2ab4bd35b39793c36438dd3

--- client/tests/phpunit/includes/scribunto/Scribunto_LuaWikibaseEntityLibraryTest.php
+++ client/tests/phpunit/includes/scribunto/Scribunto_LuaWikibaseEntityLibraryTest.php
@@ -22,9 +22,10 @@ use Wikibase\Client\WikibaseClient;
  * @author Marius Hoch < hoo@online.de >
  */
 class Scribunto_LuaWikibaseEntityLibraryTest extends Scribunto_LuaWikibaseLibraryTestCase {
+
 	protected static $moduleName = 'LuaWikibaseEntityLibraryTests';
 
-	function getTestModules() {
+	protected function getTestModules() {
 		return parent::getTestModules() + array(
 			'LuaWikibaseEntityLibraryTests' => __DIR__ . '/LuaWikibaseEntityLibraryTests.lua',
 		);
