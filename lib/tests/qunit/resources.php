26aa6d4dccf2a8c7ae0be3aa48fb383b0538b92c
Adrian Lang <adrian.lang@wikimedia.de>
2014-11-12 13:15:26 +0100

    Move RepoApi files
    Change-Id: I25c5ad11352c0f869b99096894f2b0d589dc1490

--- lib/tests/qunit/resources.php
+++ lib/tests/qunit/resources.php
@@ -47,7 +47,7 @@ return call_user_func( function() {
 
 		'wikibase.api.RepoApi.tests' => $moduleBase + array(
 			'scripts' => array(
-				'wikibase.RepoApi/wikibase.RepoApi.tests.js',
+				'api/RepoApi.tests.js',
 			),
 			'dependencies' => array(
 				'wikibase',
@@ -58,7 +58,7 @@ return call_user_func( function() {
 
 		'wikibase.api.RepoApiError.tests' => $moduleBase + array(
 			'scripts' => array(
-				'wikibase.RepoApi/wikibase.RepoApiError.tests.js',
+				'api/RepoApiError.tests.js',
 			),
 			'dependencies' => array(
 				'wikibase',
