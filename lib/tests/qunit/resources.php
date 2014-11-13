a1a4f989e0bf59bd3f1499830f0555412dd6767b
Adrian Lang <adrian.lang@wikimedia.de>
2014-11-13 14:40:13 +0100

    Move $.removeClassByRegex and $.sticknode to Repo
    They are both only used by jquery.wikibase widgets due to be moved
    to Repo.
    
    Change-Id: I6b0865ace791d9b3991f83c724f2d7caa3592282

--- lib/tests/qunit/resources.php
+++ lib/tests/qunit/resources.php
@@ -35,33 +35,6 @@ return call_user_func( function() {
 			),
 		),
 
-		'jquery.removeClassByRegex.tests' => $moduleBase + array(
-			'scripts' => array(
-				'jquery/jquery.removeClassByRegex.tests.js',
-			),
-			'dependencies' => array(
-				'jquery.removeClassByRegex',
-			),
-		),
-
-		'jquery.sticknode.tests' => $moduleBase + array(
-			'scripts' => array(
-				'jquery/jquery.sticknode.tests.js',
-			),
-			'dependencies' => array(
-				'jquery.sticknode',
-			),
-		),
-
-		'jquery.util.EventSingletonManager.tests' => $moduleBase + array(
-			'scripts' => array(
-				'jquery/jquery.util.EventSingletonManager.tests.js',
-			),
-			'dependencies' => array(
-				'jquery.util.EventSingletonManager',
-			),
-		),
-
 		'wikibase.dataTypes.tests' => $moduleBase + array(
 			'scripts' => array(
 				'wikibase.dataTypes/wikibase.dataTypes.tests.js',
