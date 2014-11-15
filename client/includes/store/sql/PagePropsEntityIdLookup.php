785c8705db844e75939f9ed2f55449a6ab2e43e7
daniel <daniel.kinzler@wikimedia.de>
2014-11-15 22:03:25 +0100

    Introduce PageTerms API module
    Allows labels and descriptions from the wikibase repo to be included in
    the response from client wiki APIs, e.g. to augment search results.
    
    Bug: 72729
    Change-Id: I9b6b52f6b75e4d6aef51141609dc744e5240e778

---
+++ client/includes/store/sql/PagePropsEntityIdLookup.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Wikibase\Client\Store\Sql;
+
+use LoadBalancer;
+use Title;
+use Wikibase\Client\Store\EntityIdLookup;
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\DataModel\Entity\EntityIdParser;
+
+/**
+ * Lookup of EntityIds based on wikibase_item entries in the page_props table.
+ *
+ * @since 0.5
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ *
+ */
+class PagePropsEntityIdLookup implements EntityIdLookup {
+
+	/**
+	 * @var LoadBalancer
+	 */
+	private $loadBalancer;
+
+	/**
+	 * @var EntityIdParser
+	 */
+	private $idParser;
+
+	/**
+	 * @param LoadBalancer $loadBalancer
+	 * @param EntityIdParser $idParser
+	 */
+	public function __construct(
+		LoadBalancer $loadBalancer,
+		EntityIdParser $idParser
+	) {
+		$this->loadBalancer = $loadBalancer;
+		$this->idParser = $idParser;
+	}
+
+	/**
+	 * @param Title[] $titles
+	 *
+	 * @return EntityId[]
+	 */
+	public function getEntityIds( array $titles ) {
+		$db = $this->loadBalancer->getConnection( DB_READ );
+
+		$pageIds = array_map(
+			function ( Title $title ) {
+				return $title->getArticleID();
+			},
+			$titles
+		);
+
+		$res = $db->select(
+			'page_props',
+			array( 'pp_page', 'pp_value' ),
+			array(
+				'pp_page' => $pageIds,
+				'pp_propname' => 'wikibase_item',
+			),
+			__METHOD__
+		);
+
+		$entityIds = array();
+
+		foreach ( $res as $row ) {
+			$entityIds[$row->pp_page] = $this->idParser->parse( $row->pp_value );
+		}
+
+		$this->loadBalancer->reuseConnection( $db );
+		return $entityIds;
+	}
+
+}
---
+++ client/tests/phpunit/includes/api/PageTermsTest.php
@@ -0,0 +1,386 @@
+<?php
+
+namespace Wikibase\Test;
+
+use ApiMain;
+use ApiQuery;
+use FauxRequest;
+use Language;
+use RequestContext;
+use Title;
+use Wikibase\Client\Store\EntityIdLookup;
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\PageTerms;
+use Wikibase\Term;
+use Wikibase\TermIndex;
+
+/**
+ * @covers Wikibase\PageTerms
+ *
+ * @group API
+ * @group Wikibase
+ * @group WikibaseAPI
+ * @group WikibaseClient
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ */
+class PageTermsTest extends \MediaWikiTestCase {
+
+	protected function setUp() {
+		parent::setUp();
+
+		$this->setMwGlobals( 'wgLang', Language::factory( 'en' ) );
+		$this->setMwGlobals( 'wgContLang', Language::factory( 'en' ) );
+	}
+
+	/**
+	 * @param array $params
+	 * @param Title[] $titles
+	 *
+	 * @return ApiQuery
+	 */
+	private function getQueryModule( array $params, array $titles ) {
+		$context = new RequestContext();
+		$context->setRequest( new FauxRequest( $params, true ) );
+
+		$main = new ApiMain( $context );
+
+		$pageSet = $this->getMockBuilder( 'ApiPageSet' )
+			->setConstructorArgs( array( $main ) )
+			->getMock();
+
+		$pageSet->expects( $this->any() )
+			->method( 'getGoodTitles' )
+			->will( $this->returnValue( $titles ) );
+
+		$query = $this->getMockBuilder( 'ApiQuery' )
+			->setConstructorArgs( array( $main, $params['action'] ) )
+			->setMethods( array( 'getPageSet' ) )
+			->getMock();
+
+		$query->expects( $this->any() )
+			->method( 'getPageSet' )
+			->will( $this->returnValue( $pageSet ) );
+
+		return $query;
+	}
+
+	/**
+	 * @param string[] $names
+	 *
+	 * @return Title[]
+	 */
+	private function makeTitles( array $names ) {
+		$titles = array();
+
+		foreach ( $names as $name ) {
+			$title = Title::makeTitle( NS_MAIN, $name );
+
+			$pid = (int)preg_replace( '/^[^\d]+/', '', $name );
+			$title->resetArticleID( $pid );
+
+			$titles[$pid] = $title;
+		}
+
+		return $titles;
+	}
+
+	/**
+	 * @param int[] $pageIds
+	 *
+	 * @return EntityId[]
+	 */
+	private function makeEntityIds( $pageIds ) {
+		$entityIds = array();
+
+		foreach ( $pageIds as $pid ) {
+			$entityIds[$pid] = $this->newEntityId( $pid );
+		}
+
+		return $entityIds;
+	}
+
+	/**
+	 * @param array $terms
+	 *
+	 * @return TermIndex
+	 */
+	private function getTermIndex( array $terms ) {
+		$termObjectsByEntityId = array();
+
+		foreach ( $terms as $pid => $termGroups ) {
+			$entityId = $this->newEntityId( $pid );
+			$key = $entityId->getSerialization();
+			$termObjectsByEntityId[$key] = $this->makeTermsFromGroups( $entityId, $termGroups );
+		}
+
+		$this_ = $this;
+
+		$termIndex = $this->getMock( 'Wikibase\TermIndex' );
+		$termIndex->expects( $this->any() )
+			->method( 'getTermsOfEntities' )
+			->will( $this->returnCallback(
+				function ( array $entityIds, $entityType, $language = null ) use( $termObjectsByEntityId, $this_ ) {
+					return $this_->getTermsOfEntities( $termObjectsByEntityId, $entityIds, $entityType, $language );
+				}
+			) );
+
+		return $termIndex;
+	}
+
+	/**
+	 * @note Public only because older PHP versions don't allow it to be called
+	 *       from a closure otherwise.
+	 *
+	 * @param array $termObjectsByEntityId
+	 * @param EntityId[] $entityIds
+	 * @param string $entityType
+	 * @param string|null $language
+	 *
+	 * @return Term[]
+	 */
+	public function getTermsOfEntities( $termObjectsByEntityId, $entityIds, $entityType, $language = null ) {
+		$result = array();
+
+		foreach ( $entityIds as $id ) {
+			$key = $id->getSerialization();
+
+			if ( !isset( $termObjectsByEntityId[$key] ) ) {
+				continue;
+			}
+
+			/** @var Term $term */
+			foreach ( $termObjectsByEntityId[$key] as $term ) {
+				if ( $term->getLanguage() === $language ) {
+					$result[] = $term;
+				}
+			}
+		}
+
+		return $result;
+	}
+
+	/**
+	 * @param EntityId $entityId
+	 * @param array $termGroups
+	 *
+	 * @return array
+	 */
+	private function makeTermsFromGroups( EntityId $entityId, $termGroups ) {
+		$terms = array();
+
+		foreach ( $termGroups as $type => $group ) {
+			foreach ( $group as $lang => $text ) {
+				$terms[] = new Term( array(
+					'termType' => $type,
+					'termLanguage' => $lang,
+					'termText' => $text,
+					'entityType' => $entityId->getEntityType(),
+					'entityId' => $entityId->getNumericId()
+				) );
+			}
+		}
+
+		return $terms;
+	}
+
+	private function newEntityId( $pageId ) {
+		if ( $pageId > 1000 ) {
+			return new PropertyId( "P$pageId" );
+		} else {
+			return new ItemId( "Q$pageId" );
+		}
+	}
+
+	/**
+	 * @return EntityIdLookup
+	 */
+	private function getEntityIdLookup() {
+		$idLookup = $this->getMock( 'Wikibase\Client\Store\EntityIdLookup' );
+		$idLookup->expects( $this->any() )
+			->method( 'getEntityIds' )
+			->will( $this->returnCallback( function( array $titles ) {
+				$entityIds = array();
+
+				/** @var Title $title */
+				foreach ( $titles as $title ) {
+					$pid = $title->getArticleId();
+
+					$entityIds[$pid] = $this->newEntityId( $pid );
+				}
+
+				return $entityIds;
+			} ) );
+
+		return $idLookup;
+	}
+
+	/**
+	 * @param array $params
+	 * @param array $terms
+	 *
+	 * @return array
+	 */
+	private function callApiModule( array $params, array $terms ) {
+		$titles = $this->makeTitles( explode( '|', $params['titles'] ) );
+		$entityIds = $this->makeEntityIds( array_keys( $terms ) );
+
+		$module = new PageTerms(
+			$this->getTermIndex( $terms ),
+			$this->getEntityIdLookup( $entityIds ),
+			$this->getQueryModule( $params, $titles ),
+			'pageterms'
+		);
+
+		$module->execute();
+
+		$result = $module->getResult();
+		return $result->getData();
+	}
+
+	public function pageTermsProvider() {
+		$terms = array(
+			11 => array(
+				'label' => array(
+					'en' => 'Vienna',
+					'de' => 'Wien',
+				),
+				'description' => array(
+					'en' => 'Capitol city of Austria',
+					'de' => 'Hauptstadt Österreichs',
+				),
+			),
+			22 => array(
+				'label' => array(
+					'en' => 'Moscow',
+					'de' => 'Moskau',
+				),
+				'description' => array(
+					'en' => 'Capitol city of Russia',
+					'de' => 'Hauptstadt Russlands',
+				),
+			),
+			3333 => array(
+				'label' => array(
+					'en' => 'Population',
+					'de' => 'Einwohnerzahl',
+				),
+				'description' => array(
+					'en' => 'number of inhabitants',
+					'de' => 'Anzahl der Bewohner',
+				),
+			),
+		);
+
+		return array(
+			'by title' => array(
+				array(
+					'action' => 'query',
+					'query' => 'pageterms',
+					'titles' => 'No11|No22|No3333',
+				),
+				$terms,
+				array(
+					11 => array(
+						'terms' => array(
+							'label' => array( 'Vienna' ),
+							'description' => array( 'Capitol city of Austria' ),
+						)
+					),
+					22 => array(
+						'terms' => array(
+							'label' => array( 'Moscow' ),
+							'description' => array( 'Capitol city of Russia' ),
+						)
+					),
+					3333 => array(
+						'terms' => array(
+							'label' => array( 'Population' ),
+							'description' => array( 'number of inhabitants' ),
+						)
+					),
+				)
+			),
+			'descriptions only' => array(
+				array(
+					'action' => 'query',
+					'query' => 'pageterms',
+					'titles' => 'No11|No22',
+					'ptterms' => 'description',
+				),
+				$terms,
+				array(
+					11 => array(
+						'terms' => array(
+							'description' => array( 'Capitol city of Austria' ),
+						)
+					),
+					22 => array(
+						'terms' => array(
+							'description' => array( 'Capitol city of Russia' ),
+						)
+					),
+				)
+			),
+			'with uselang' => array(
+				array(
+					'action' => 'query',
+					'query' => 'pageterms',
+					'titles' => 'No11|No22',
+					'uselang' => 'de',
+				),
+				$terms,
+				array(
+					11 => array(
+						'terms' => array(
+							'label' => array( 'Wien' ),
+							'description' => array( 'Hauptstadt Österreichs' ),
+						)
+					),
+					22 => array(
+						'terms' => array(
+							'label' => array( 'Moskau' ),
+							'description' => array( 'Hauptstadt Russlands' ),
+						)
+					),
+				)
+			),
+			'continue' => array(
+				array(
+					'action' => 'query',
+					'query' => 'pageterms',
+					'titles' => 'No11|No22',
+					'ptcontinue' => '20',
+				),
+				$terms,
+				array(
+					22 => array(
+						'terms' => array(
+							'label' => array( 'Moscow' ),
+							'description' => array( 'Capitol city of Russia' ),
+						)
+					),
+				)
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider pageTermsProvider
+	 */
+	public function testPageTerms( $params, $terms, $expected ) {
+		$result = $this->callApiModule( $params, $terms );
+
+		if ( isset( $result['error'] ) ) {
+			$this->fail( 'API error: ' . print_r( $result['error'], true ) );
+		}
+
+		$this->assertArrayHasKey( 'query', $result );
+		$this->assertArrayHasKey( 'pages', $result['query'] );
+		$this->assertEquals( $expected, $result['query']['pages'] );
+	}
+
+}
