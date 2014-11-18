3a3c8c5af88d1d2102c35c068a027f4397322ca9
jeroendedauw <jeroendedauw@gmail.com>
2014-11-18 17:05:24 +0100

    2.3.0 release

--- lib/packages/wikibase/data-model/WikibaseDataModel.php
+++ lib/packages/wikibase/data-model/WikibaseDataModel.php
@@ -12,7 +12,7 @@ if ( defined( 'WIKIBASE_DATAMODEL_VERSION' ) ) {
 	return 1;
 }
 
-define( 'WIKIBASE_DATAMODEL_VERSION', '2.2.0' );
+define( 'WIKIBASE_DATAMODEL_VERSION', '2.3.0' );
 
 if ( defined( 'MEDIAWIKI' ) ) {
 	call_user_func( function() {
