7aa1013e04154e852cb120b8c6595062903166eb
Adrian Lang <adrian.lang@wikimedia.de>
2014-11-20 13:04:08 +0100

    Inject EntityInfoTermLookup into WikibaseValueFormattersBuilders
    Change-Id: Iea8191e88a19113eba929e6bb9b74081384ba46d

--- repo/includes/EntityParserOutputGenerator.php
+++ repo/includes/EntityParserOutputGenerator.php
@@ -15,6 +15,7 @@ use Wikibase\DataModel\StatementListProvider;
 use Wikibase\LanguageFallbackChain;
 use Wikibase\Lib\Serializers\SerializationOptions;
 use Wikibase\Lib\Store\EntityInfoBuilderFactory;
+use Wikibase\Lib\Store\EntityInfoTermLookup;
 use Wikibase\Lib\Store\EntityTitleLookup;
 use Wikibase\Repo\View\EntityViewFactory;
 
@@ -287,13 +288,14 @@ class EntityParserOutputGenerator {
 		array $entityIds,
 		$editable
 	) {
+		$entityInfo = $this->getEntityInfoForView( $entityIds );
+
 		$entityView = $this->entityViewFactory->newEntityView(
 			$this->languageFallbackChain,
 			$this->languageCode,
-			$entityRevision->getEntity()->getType()
+			$entityRevision->getEntity()->getType(),
+			new EntityInfoTermLookup( $entityInfo )
 		);
-
-		$entityInfo = $this->getEntityInfoForView( $entityIds );
 
 		$html = $entityView->getHtml( $entityRevision, $entityInfo, $editable );
 		$parserOutput->setText( $html );
