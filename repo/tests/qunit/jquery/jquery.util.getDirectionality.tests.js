dc0a079051ebf3713746d1d444a0c1bcc551ae79
Henning Snater <henning.snater@wikimedia.de>
2014-12-03 15:29:30 +0100

    Implemented $.util.getDirectionality
    By acting as an interface to ULS, the function allows move ULS specifics out of the *view widgets.
    
    Change-Id: I987843e00c06e1c5d5b7b9c7b2f25d9801441017

---
+++ repo/tests/qunit/jquery/jquery.util.getDirectionality.tests.js
@@ -0,0 +1,59 @@
+/**
+ * @licence GNU GPL v2+
+ * @author H. Snater < mediawiki@snater.com >
+ */
+( function( $, QUnit ) {
+	'use strict';
+
+var htmlDir = null;
+
+QUnit.module( 'jquery.util.getDirectionality', {
+	setup: function() {
+		htmlDir = $( 'html' ).prop( 'dir' );
+	},
+	teardown: function() {
+		$( 'html' ).prop( 'dir', htmlDir );
+	}
+} );
+
+QUnit.test( 'Basic tests', function( assert ) {
+	var $html = $( 'html' );
+
+	if( $.uls && $.uls.data ) {
+		assert.equal(
+			$.util.getDirectionality( 'fa' ),
+			'rtl',
+			'Retrieved language code from ULS.'
+		);
+
+		// There is no reason to further test ULS behaviour as ULS is supposed to always return a
+		// sensible directionality string.
+		return;
+	}
+
+	$html.prop( 'dir', 'ltr' );
+
+	assert.equal(
+		$.util.getDirectionality( 'doesNotExist' ),
+		'ltr',
+		'Falling back to HTML "dir" attribute.'
+	);
+
+	$html.prop( 'dir', 'rtl' );
+
+	assert.equal(
+		$.util.getDirectionality( 'doesNotExist' ),
+		'rtl',
+		'Verified falling back to HTML "dir" attribute after changing "dir" attribute.'
+	);
+
+	$html.removeAttr( 'dir' );
+
+	assert.equal(
+		$.util.getDirectionality( 'doesNotExist' ),
+		'auto',
+		'Falling back to HTML "auto" attribute if no "dir" attribute is set on the HTML element.'
+	);
+} );
+
+}( jQuery, QUnit ) );
