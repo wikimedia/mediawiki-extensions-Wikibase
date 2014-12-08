e2ac8f13da2a24ca599064de691f5876b9a90e57
Marius Hoch <hoo@online.de>
2014-12-08 04:33:31 +0100

    Adopt ApiConventionsTest to MediaWiki 1.25 API module changes
    Also minor style enhancements
    
    Change-Id: I69008ed8aebb819bd536d3e9e79d41cc64d3495d

--- repo/tests/phpunit/includes/api/ApiConventionsTest.php
+++ repo/tests/phpunit/includes/api/ApiConventionsTest.php
@@ -3,10 +3,15 @@
 namespace Wikibase\Test\Api;
 
 use PHPUnit_Framework_TestCase;
+use ApiBase;
+use ApiMain;
+use FauxRequest;
+use ReflectionMethod;
 
 /**
  *
  * @group Wikibase
+ * @group WikibaseRepo
  * @group WikibaseAPI
  *
  * @group medium
@@ -20,7 +25,7 @@ class ApiConventionsTest extends PHPUnit_Framework_TestCase {
 		$argList = array();
 
 		foreach ( $GLOBALS['wgAPIModules'] as $moduleClass ) {
-			//make sure to only test Wikibase Api modules
+			// Make sure to only test Wikibase Api modules
 			if ( strpos( $moduleClass, 'Wikibase' ) !== false ) {
 				$argList[] = array( $moduleClass );
 			}
@@ -30,33 +35,35 @@ class ApiConventionsTest extends PHPUnit_Framework_TestCase {
 	}
 
 	/**
-	* Connects the assertions for the different methods and iterates through the api modules
-	* @dataProvider wikibaseApiModuleProvider
+	 * Connects the assertions for the different methods and iterates through the api modules
+	 *
+	 * @dataProvider wikibaseApiModuleProvider
 	*/
 	public function testApiConventions( $moduleClass ) {
 		$params = array();
 		$user =  $GLOBALS['wgUser'];
 
-		$request = new \FauxRequest( $params, true );
-		$main = new \ApiMain( $request );
+		$request = new FauxRequest( $params, true );
+		$main = new ApiMain( $request );
 		$main->getContext()->setUser( $user );
 		$module = new $moduleClass( $main, 'moduleClass' );
 
 		$this->assertGetFinalParamDescription( $moduleClass, $module );
-		$this->assertGetExamples( $moduleClass, $module );
 		$this->assertGetFinalDescription( $moduleClass, $module );
+		$this->assertGetExamplesMessages( $moduleClass, $module );
 	}
 
 	/**
 	 * This method is for the assertions in particular for getFinalDescription as defined in ApiBase
+	 *
 	 * @param string $moduleClass one of the modules in $GLOBALS['wgAPIModules'], only in this function for the error messages
-	 * @param Module $module is an instance of $moduleClass
+	 * @param ApiBase $module is an instance of $moduleClass
 	 **/
-	private function assertGetFinalDescription ( $moduleClass, $module ) {
+	private function assertGetFinalDescription ( $moduleClass, ApiBase $module ) {
 		$method = 'getFinalDescription';
 		$descArray = $module->$method();
 
-		$rMethod = new \ReflectionMethod( $module, $method );
+		$rMethod = new ReflectionMethod( $module, $method );
 		$this->assertTrue( $rMethod->isPublic(), 'the method ' . $method . ' of module ' . $moduleClass . ' is not public' );
 
 		$this->assertNotEmpty( $module->$method(), 'the Module ' . $moduleClass . ' does not have the method ' . $method );
@@ -68,39 +75,45 @@ class ApiConventionsTest extends PHPUnit_Framework_TestCase {
 
 	/**
 	 * This method is for the assertions for getFinalParamDescription as defined in ApiBase, depending on getFinalParams
+	 *
 	 * @param string $moduleClass one of the modules in $GLOBALS['wgAPIModules'], only in this function for the error messages
-	 * @param Module $module is an instance of $moduleClass
+	 * @param ApiBase $module is an instance of $moduleClass
 	 **/
-	private function assertGetFinalParamDescription ( $moduleClass, $module ) {
+	private function assertGetFinalParamDescription ( $moduleClass, ApiBase $module ) {
 		$method = 'getFinalParamDescription';
-		$paramsMethod = 'getFinalParams';
-		$paramsArray = $module->$paramsMethod();
+		$paramsArray = $module->getFinalParams();
+
 		if ( !empty( $paramsArray ) ) {
 			$paramDescArray = $module->$method();
 			$this->assertNotEmpty( $paramDescArray, 'the array returned by the method ' . $method . ' of module ' . $moduleClass . ' is empty' );
 
-			//comparing the keys of the arrays of getParamDescription and getParams
+			// Comparing the keys of the arrays of getParamDescription and getParams
 			$arrayKeysMatch = !array_diff_key( $paramDescArray, $paramsArray ) && !array_diff_key( $paramsArray, $paramDescArray );
 			$this->assertTrue( $arrayKeysMatch, 'keys different at ' . $moduleClass );
 		}
 	}
 
 	/**
-	 * This method is for the assertions of getExamples as defined in ApiBase
+	 * This method is for the assertions of getExamplesMessages/ getExamples as defined in ApiBase
+	 *
 	 * @param string $moduleClass one of the modules in $GLOBALS['wgAPIModules'], only in this function for the error messages
-	 * @param Module $module is an instance of $moduleClass
+	 * @param ApiBase $module is an instance of $moduleClass
 	 **/
-	private function assertGetExamples( $moduleClass, $module ) {
-		$method = 'getExamples';
-		$rMethod = new \ReflectionMethod( $moduleClass,  $method );
+	private function assertGetExamplesMessages( $moduleClass, ApiBase $module ) {
+		$method = 'getExamplesMessages';
+		$rMethod = new ReflectionMethod( $moduleClass,  $method );
 		$rMethod->setAccessible( true );
 		$exArray = $rMethod->invoke( $module );
 
 		$this->assertNotEmpty( $exArray, 'there are no examples for ' . $moduleClass );
 
 		foreach ( $exArray as $key => $value ) {
-			$this->assertContains('api.php?action=', $key, 'the key ' . $key . ' is not an url at ' . $moduleClass );
-			$this->assertInternalType( 'string', $value, 'the value of the example for ' . $key . ' in ' . $moduleClass . ' is not a string' );
+			$this->assertRegExp( '/^action=\w/', $key, 'the key ' . $key . ' is not an url at ' . $moduleClass );
+			if ( is_string( $value ) ) {
+				$this->assertTrue( wfMessage( $value )->exists(), "message ($value) for $key doesn't exist" );
+			} else {
+				$this->assertInstanceOf( 'Message', $value, 'the value of the example for ' . $key . ' in ' . $moduleClass . ' is not a Message' );
+			}
 		}
 
 	}
