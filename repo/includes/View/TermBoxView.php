c1c5097e0f9b3b3db9ba9a0fa0f8fba07b1b87fe
Henning Snater <henning.snater@wikimedia.de>
2014-12-17 11:45:50 +0100

    Renamed fingerprintview widgets
    fingerprintview widgets were named incorrectly. As "Fingerprint" is the
    aggregation of labels, descriptions and aliases in all languages,
    fingerprintview should have been what was labeled fingerprintgroupview.
    While there is no fictitious term for the aggregation of label,
    description and aliases of a single language and the term "Fingerprint"
    does not pursue a solid concept, the UI components have been renamed in
    a more obvious fashion.
    "entitytermsview" aggregates all term related objects of an entity.
    (Fingerprint is actually an aggregation of wikibase.datamodel.Term(Map)
    and wikibase.datamodel.MultiTerm(Map) objects.)
    This change advances the effort of renaming/removing the term
    "Fingerprint".
    
    Bug: T75655
    
    Change-Id: I084b6a11f8ddb7930b145e7852b7a5e66daa0a32

--- repo/includes/View/TermBoxView.php
+++ repo/includes/View/TermBoxView.php
@@ -74,7 +74,7 @@ class TermBoxView {
 			$hasLabel = $labels->hasTermForLanguage( $languageCode );
 			$hasDescription = $descriptions->hasTermForLanguage( $languageCode );
 
-			$tbody .= wfTemplate( 'wikibase-fingerprintview',
+			$tbody .= wfTemplate( 'wikibase-entitytermsforlanguage',
 				$languageCode,
 				$title->getLocalURL( array( 'setlang' => $languageCode ) ),
 				htmlspecialchars( Utils::fetchLanguageName( $languageCode ) ),
@@ -100,9 +100,9 @@ class TermBoxView {
 			);
 		}
 
-		$html = wfTemplate( 'wikibase-fingerprintgroupview',
+		$html = wfTemplate( 'wikibase-entitytermsview',
 			$this->msg( 'wikibase-terms' )->text(),
-			wfTemplate( 'wikibase-fingerprintlistview', $tbody ),
+			wfTemplate( 'wikibase-entitytermsforlanguagelistview', $tbody ),
 			$this->sectionEditLinkGenerator->getHtmlForEditSection(
 				'SpecialPages',
 				array(),
