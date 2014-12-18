fe5a6bce375af3d9208ad4b535843b910c5677df
Adrian Lang <adrian.lang@wikimedia.de>
2014-12-18 10:23:47 +0100

    Factor out EntityTermLookupBase
    Change-Id: Icd364395285f3034b87306fd821968feaeaeb678

---
+++ lib/includes/store/EntityTermLookupBase.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Wikibase\Lib\Store;
+
+use OutOfBoundsException;
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\Term;
+
+/**
+ * @since 0.5
+ *
+ * @licence GNU GPL v2+
+ * @author Katie Filbert < aude.wiki@gmail.com >
+ * @author Daniel Kinzler
+ */
+abstract class EntityTermLookupBase implements TermLookup {
+
+
+	/**
+	 * @see TermLookup::getLabel
+	 *
+	 * @param EntityId $entityId
+	 * @param string $languageCode
+	 *
+	 * @throws OutOfBoundsException if no label in that language is known
+	 * @return string
+	 */
+	public function getLabel( EntityId $entityId, $languageCode ) {
+		$labels = $this->getLabels( $entityId, array( $languageCode ) );
+
+		if ( !isset( $labels[$languageCode] ) ) {
+			throw new OutOfBoundsException( 'No label found for language ' . $languageCode );
+		}
+
+		return $labels[$languageCode];
+	}
+
+	/**
+	 * @see TermLookup::getLabels
+	 *
+	 * @param EntityId $entityId
+	 * @param string[]|null $languageCodes The languages to get terms for; null means all languages.
+	 *
+	 * @return string[]
+	 */
+	public function getLabels( EntityId $entityId, array $languageCodes = null ) {
+		return $this->getTermsOfType( $entityId, 'label', $languageCodes );
+	}
+
+	/**
+	 * @see TermLookup::getDescription
+	 *
+	 * @param EntityId $entityId
+	 * @param string $languageCode
+	 *
+	 * @throws OutOfBoundsException if no description in that language is known
+	 * @return string
+	 */
+	public function getDescription( EntityId $entityId, $languageCode ) {
+		$descriptions = $this->getDescriptions( $entityId, array( $languageCode ) );
+
+		if ( !isset( $descriptions[$languageCode] ) ) {
+			throw new OutOfBoundsException( 'No description found for language ' . $languageCode );
+		}
+
+		return $descriptions[$languageCode];
+	}
+
+	/**
+	 * @see TermLookup::getDescriptions
+	 *
+	 * @param EntityId $entityId
+	 * @param string[]|null $languageCodes The languages to get terms for; null means all languages.
+	 *
+	 * @return string[]
+	 */
+	public function getDescriptions( EntityId $entityId, array $languageCodes = null ) {
+		return $this->getTermsOfType( $entityId, 'description', $languageCodes );
+	}
+
+	/**
+	 * @param EntityId $entityId
+	 * @param string $termType
+	 * @param string[]|null $languageCodes The languages to get terms for; null means all languages.
+	 *
+	 * @return string[]
+	 */
+	abstract protected function getTermsOfType( EntityId $entityId, $termType, array $languageCodes = null );
+
+	/**
+	 * @param Term[] $wikibaseTerms
+	 *
+	 * @return string[] strings keyed by language code
+	 */
+	protected function convertTermsToMap( array $wikibaseTerms ) {
+		$terms = array();
+
+		foreach( $wikibaseTerms as $wikibaseTerm ) {
+			$languageCode = $wikibaseTerm->getLanguage();
+			$terms[$languageCode] = $wikibaseTerm->getText();
+		}
+
+		return $terms;
+	}
+
+}
