406a06d634ec364a524fa2cd24b72dcca48d056e
Marius Hoch <hoo@online.de>
2014-12-24 05:45:55 +0100

    Add EntityRevisionLookup::getEntityRevisions/getLatestRevisionIds
    To reduce database load when we need to load multiple entities.
    
    That is significant for wbgetentities requests with many ids,
    json dump generation and possibly other things where we need to
    access multiple entities at once.
    
    Change-Id: I90380428fe9f31677458bb43ceb5cdac24a81ef5

--- lib/includes/store/sql/WikiPageEntityRevisionLookup.php
+++ lib/includes/store/sql/WikiPageEntityRevisionLookup.php
@@ -3,12 +3,14 @@
 namespace Wikibase\Lib\Store;
 
 use DBAccessBase;
+use DatabaseBase;
 use DBQueryError;
 use MWContentSerializationException;
 use Revision;
 use Wikibase\DataModel\Entity\EntityId;
 use Wikibase\DataModel\Entity\EntityIdParser;
 use Wikibase\EntityRevision;
+use Wikibase\Lib\Store\EntityRedirect;
 
 /**
  * Implements an entity repo based on blobs stored in wiki pages on a locally reachable
@@ -19,6 +21,7 @@ use Wikibase\EntityRevision;
  *
  * @licence GNU GPL v2+
  * @author Daniel Kinzler
+ * @author Marius Hoch < hoo@online.de >
  */
 class WikiPageEntityRevisionLookup extends DBAccessBase implements EntityRevisionLookup {
 
@@ -62,6 +65,8 @@ class WikiPageEntityRevisionLookup extends DBAccessBase implements EntityRevisio
 	 */
 	public function getEntityRevision( EntityId $entityId, $revisionId = self::LATEST_FROM_SLAVE ) {
 		wfProfileIn( __METHOD__ );
+		$entityId = $this->getProperEntityId( $entityId );
+
 		wfDebugLog( __CLASS__, __FUNCTION__ . ': Looking up entity ' . $entityId
 			. " (revision $revisionId)." );
 
@@ -75,7 +80,12 @@ class WikiPageEntityRevisionLookup extends DBAccessBase implements EntityRevisio
 		/** @var EntityRevision $entityRevision */
 		$entityRevision = null;
 
-		$row = $this->loadRevisionRow( $entityId, $revisionId );
+		if ( is_int( $revisionId ) ) {
+			$row = $this->loadRevisionInformationById( $entityId, $revisionId );
+		} else {
+			$rows = $this->loadRevisionInformation( array( $entityId ), $revisionId );
+			$row = $rows[$entityId->getSerialization()];
+		}
 
 		if ( $row ) {
 			/** @var EntityRedirect $redirect */
@@ -111,6 +121,53 @@ class WikiPageEntityRevisionLookup extends DBAccessBase implements EntityRevisio
 	}
 
 	/**
+	 * Returns an array like entityid -> EntityRevision, EntityRedirect or null (if not found)
+	 * Please note that this doesn't throw UnresolvedRedirectExceptions but rather returns
+	 * EntityRedirect objects for entities that are infact redirects.
+	 *
+	 * @since 0.5
+	 * @see EntityRevisionLookup::getEntityRevisions
+	 *
+	 * @param EntityId[] $entityIds
+	 * @param string $mode LATEST_FROM_SLAVE or LATEST_FROM_MASTER. LATEST_FROM_MASTER would
+	 *        force the revision to be determined from the canonical master database.
+	 *
+	 * @throws InvalidArgumentException
+	 * @throws StorageException
+	 * @return array entityid -> EntityRevision, EntityRedirect or null (if not found)
+	 */
+	public function getEntityRevisions( array $entityIds, $mode = self::LATEST_FROM_SLAVE ) {
+		if ( empty( $entityIds ) ) {
+			return array();
+		}
+
+		$entityIds = $this->getProperEntityIds( $entityIds );
+
+		$rows = $this->loadRevisionInformation( $entityIds, $mode );
+
+		$entityRevisions = array();
+
+		foreach ( $rows as $id => $row ) {
+			if ( !$row ) {
+				$entityRevisions[$id] = null;
+				continue;
+			}
+
+			list( $entityRevision, $redirect ) = $this->loadEntity( $row );
+
+			if ( $redirect !== null ) {
+				$entityRevisions[$id] = $redirect;
+			} elseif( $entityRevision ) {
+				$entityRevisions[$id] = $entityRevision;
+			} else {
+				$entityRevisions[$id] = null;
+			}
+		}
+
+		return $entityRevisions;
+	}
+
+	/**
 	 * @see EntityRevisionLookup::getLatestRevisionId
 	 *
 	 * @since 0.5
@@ -121,43 +178,98 @@ class WikiPageEntityRevisionLookup extends DBAccessBase implements EntityRevisio
 	 * @return int|false
 	 */
 	public function getLatestRevisionId( EntityId $entityId, $mode = self::LATEST_FROM_SLAVE ) {
-		$row = null;
+		$revisions = $this->getLatestRevisionIds( array( $entityId ), $mode );
+		return $revisions[$entityId->getSerialization()];
+	}
+
+	/**
+	 * @since 0.5
+	 * @see EntityRevisionLookup::getLatestRevisionIds
+	 *
+	 * @param EntityId[] $entityIds
+	 * @param string $mode LATEST_FROM_SLAVE or LATEST_FROM_MASTER. LATEST_FROM_MASTER would force the
+	 *        revision to be determined from the canonical master database.
+	 *
+	 * @throws StorageException
+	 * @return array entityid -> page_latest or false (if not found)
+	 */
+	public function getLatestRevisionIds( array $entityIds, $mode = self::LATEST_FROM_SLAVE ) {
+		if ( empty( $entityIds ) ) {
+			return array();
+		}
+
+		$entityIds = $this->getProperEntityIds( $entityIds );
+
+		$revisions = array();
 
 		if ( $mode !== self::LATEST_FROM_MASTER ) {
-			$row = $this->selectPageLatest( $entityId, DB_SLAVE );
+			$revisions = $this->selectPageLatest( $entityIds, DB_SLAVE );
 		}
 
-		if ( !$row ) {
-			$row = $this->selectPageLatest( $entityId, DB_MASTER );
+		$loadFromMaster = array();
+		foreach ( $entityIds as $entityId ) {
+			if ( !isset( $revisions[$entityId->getSerialization()] ) || !is_int( $revisions[$entityId->getSerialization()] ) ) {
+				$loadFromMaster[] = $entityId;
+			}
 		}
 
-		if ( $row ) {
-			return (int)$row->page_latest;
+		if ( $loadFromMaster ) {
+			$revisions = array_merge(
+				$revisions,
+				$this->selectPageLatest( $loadFromMaster, DB_MASTER )
+			);
 		}
 
-		return false;
+		return $revisions;
+	}
+
+	/**
+	 * @param EntityId[] $entityIds
+	 * @param string $mode
+	 *
+	 * @throws DBQueryError
+	 * @return array entityid -> object or false (if not found)
+	 */
+	private function loadRevisionInformation( array $entityIds, $mode ) {
+		$rows = array();
+
+		if ( $mode !== self::LATEST_FROM_MASTER ) {
+			$rows = $this->selectRevisionInformation( $entityIds, DB_SLAVE );
+		}
+
+		$loadFromMaster = array();
+		foreach ( $entityIds as $entityId ) {
+			if ( !isset( $rows[$entityId->getSerialization()] ) || !$rows[$entityId->getSerialization()] ) {
+				$loadFromMaster[] = $entityId;
+			}
+		}
+
+		if ( $loadFromMaster ) {
+			$rows = array_merge(
+				$rows,
+				$this->selectRevisionInformation( $loadFromMaster, DB_MASTER )
+			);
+		}
+
+		return $rows;
 	}
 
 	/**
 	 * @param EntityId $entityId
-	 * @param int|string $revisionId
+	 * @param int $revisionId
 	 *
 	 * @throws DBQueryError
-	 * @return object|null
+	 * @return object|bool
 	 */
-	private function loadRevisionRow( EntityId $entityId, $revisionId ) {
-		$row = null;
-
-		if ( $revisionId !== self::LATEST_FROM_MASTER ) {
-			$row = $this->selectRevisionRow( $entityId, $revisionId, DB_SLAVE );
-		}
+	private function loadRevisionInformationById( EntityId $entityId, $revisionId ) {
+		$row = $this->selectRevisionInformationById( $entityId, $revisionId, DB_SLAVE );
 
 		if ( !$row ) {
-			// try loading from master
+			// Try loading from master
 			wfDebugLog(  __CLASS__, __FUNCTION__ . ': try to load ' . $entityId
 				. " with $revisionId from DB_MASTER." );
 
-			$row = $this->selectRevisionRow( $entityId, $revisionId, DB_MASTER );
+			$row = $this->selectRevisionInformationById( $entityId, $revisionId, DB_MASTER );
 		}
 
 		return $row;
@@ -183,51 +295,68 @@ class WikiPageEntityRevisionLookup extends DBAccessBase implements EntityRevisio
 	 * Selects revision information from the page, revision, and text tables.
 	 *
 	 * @param EntityId $entityId The entity to query the DB for.
-	 * @param int|string $revisionId The desired revision id, or LATEST_FROM_SLAVE or LATEST_FROM_MASTER.
+	 * @param int $revisionId The desired revision id
 	 * @param int $connType DB_SLAVE or DB_MASTER
 	 *
 	 * @throws DBQueryError If the query fails.
-	 * @return object|null a raw database row object, or null if no such entity revision exists.
+	 * @return object|bool a raw database row object, or false if no such entity revision exists.
 	 */
-	private function selectRevisionRow( EntityId $entityId, $revisionId, $connType ) {
+	private function selectRevisionInformationById( EntityId $entityId, $revisionId, $connType ) {
 		wfProfileIn( __METHOD__ );
 		$db = $this->getConnection( $connType );
 
-		$where = array();
+		// pick text via rev_text_id
+		$join = array( 'text' => array( 'INNER JOIN', 'old_id=rev_text_id' ) );
+
+		wfDebugLog( __CLASS__, __FUNCTION__ . ": Looking up revision $revisionId of " . $entityId );
+
+		$row = $db->selectRow(
+			array( 'revision', 'text' ),
+			$this->selectFields(),
+			array( 'rev_id' => $revisionId ),
+			__METHOD__,
+			array(),
+			$join
+		);
+
+		$this->releaseConnection( $db );
+
+		wfProfileOut( __METHOD__ );
+		return $row;
+	}
+
+	/**
+	 * Selects revision information from the page, revision, and text tables.
+	 * Returns an array like entityid -> object or false (if not found)
+	 *
+	 * @param EntityId $entityId The entity to query the DB for.
+	 * @param int $connType DB_SLAVE or DB_MASTER
+	 *
+	 * @throws DBQueryError If the query fails.
+	 * @return array
+	 */
+	private function selectRevisionInformation( array $entityIds, $connType ) {
+		wfProfileIn( __METHOD__ );
+		$db = $this->getConnection( $connType );
+
 		$join = array();
+		$fields = $this->selectFields();
+		// To be able to link rows with entity ids
+		$fields[] = 'epp_entity_id';
+		$fields[] = 'epp_entity_type';
 
-		if ( is_int( $revisionId ) ) {
-			$tables = array( 'revision', 'text' );
+		$tables = array( 'page', 'revision', 'text', 'wb_entity_per_page' );
 
-			// pick revision by id
-			$where['rev_id'] = $revisionId;
+		// pick page via epp_page_id
+		$join['page'] = array( 'INNER JOIN', 'epp_page_id=page_id' );
 
-			// pick text via rev_text_id
-			$join['text'] = array( 'INNER JOIN', 'old_id=rev_text_id' );
+		// pick latest revision via page_latest
+		$join['revision'] = array( 'INNER JOIN', 'page_latest=rev_id' );
 
-			wfDebugLog( __CLASS__, __FUNCTION__ . ": Looking up revision $revisionId of " . $entityId );
-		} else {
-			$tables = array( 'page', 'revision', 'text', 'wb_entity_per_page' );
+		// pick text via rev_text_id
+		$join['text'] = array( 'INNER JOIN', 'old_id=rev_text_id' );
 
-			$entityId = $this->getProperEntityId( $entityId );
-
-			// pick entity by id
-			$where['epp_entity_id'] = $entityId->getNumericId();
-			$where['epp_entity_type'] = $entityId->getEntityType();
-
-			// pick page via epp_page_id
-			$join['page'] = array( 'INNER JOIN', 'epp_page_id=page_id' );
-
-			// pick latest revision via page_latest
-			$join['revision'] = array( 'INNER JOIN', 'page_latest=rev_id' );
-
-			// pick text via rev_text_id
-			$join['text'] = array( 'INNER JOIN', 'old_id=rev_text_id' );
-
-			wfDebugLog( __CLASS__, __FUNCTION__ . ': Looking up latest revision of ' . $entityId );
-		}
-
-		$res = $db->select( $tables, $this->selectFields(), $where, __METHOD__, array(), $join );
+		$res = $db->select( $tables, $fields, $this->getEppWhere( $entityIds, $db ), __METHOD__, array(), $join );
 
 		if ( !$res ) {
 			// this can only happen if the DB is set to ignore errors, which shouldn't be the case...
@@ -239,26 +368,39 @@ class WikiPageEntityRevisionLookup extends DBAccessBase implements EntityRevisio
 
 		$this->releaseConnection( $db );
 
-		if ( !( $row = $res->fetchObject() ) ) {
-			$row = null;
+		$rows = array();
+		// This sucks, but is the sanest option...
+		foreach ( $res as $row ) {
+			$rows[$row->epp_entity_type . $row->epp_entity_id] = $row;
+		}
+
+		$result = array();
+		foreach ( $entityIds as $entityId ) {
+			$result[$entityId->getSerialization()] = false;
+
+			$key = $entityId->getEntityType() . $entityId->getNumericId();
+			if ( isset( $rows[$key] ) ) {
+				$result[$entityId->getSerialization()] = $rows[$key];
+			}
 		}
 
 		wfProfileOut( __METHOD__ );
-		return $row;
+		return $result;
 	}
 
 	/**
-	 * Selects page information from the page table.
+	 * Selects page_latest from the page table for the given entity ids.
+	 * Returns an array like entityid -> page_latest or false (if not found)
 	 *
 	 * @since 0.4
 	 *
-	 * @param EntityId $entityId The entity to query the DB for.
+	 * @param EntityId[] $entityIds
 	 * @param int $connType DB_SLAVE or DB_MASTER
 	 *
 	 * @throws DBQueryError If the query fails.
-	 * @return object|null a raw database row object, or null if no such entity revision exists.
+	 * @return array
 	 */
-	protected function selectPageLatest( EntityId $entityId, $connType = DB_SLAVE ) {
+	private function selectPageLatest( array $entityIds, $connType = DB_SLAVE ) {
 		wfProfileIn( __METHOD__ );
 		$db = $this->getConnection( $connType );
 
@@ -267,20 +409,13 @@ class WikiPageEntityRevisionLookup extends DBAccessBase implements EntityRevisio
 			'wb_entity_per_page',
 		);
 
-		$where = array();
 		$join = array();
-
-		$entityId = $this->getProperEntityId( $entityId );
-
-		// pick entity by id
-		$where['epp_entity_id'] = $entityId->getNumericId();
-		$where['epp_entity_type'] = $entityId->getEntityType();
 
 		// pick page via epp_page_id
 		$join['page'] = array( 'INNER JOIN', 'epp_page_id=page_id' );
+		$fields = array( 'page_latest', 'epp_entity_id', 'epp_entity_type' );
 
-		$res = $db->select( $tables, 'page_latest', $where, __METHOD__, array(), $join );
-
+		$res = $db->select( $tables, $fields, $this->getEppWhere( $entityIds, $db ), __METHOD__, array(), $join );
 		if ( !$res ) {
 			// this can only happen if the DB is set to ignore errors, which shouldn't be the case...
 			$error = $db->lastError();
@@ -292,12 +427,40 @@ class WikiPageEntityRevisionLookup extends DBAccessBase implements EntityRevisio
 
 		$this->releaseConnection( $db );
 
-		if ( !( $row = $res->fetchObject() ) ) {
-			$row = null;
+		$rows = array();
+		// This sucks, but is the sanest option...
+		foreach ( $res as $row ) {
+			$rows[$row->epp_entity_type . $row->epp_entity_id] = (int)$row->page_latest;
+		}
+
+		$pageLatest = array();
+		foreach ( $entityIds as $entityId ) {
+			$pageLatest[$entityId->getSerialization()] = false;
+
+			$key = $entityId->getEntityType() . $entityId->getNumericId();
+			if ( isset( $rows[$key] ) ) {
+				$pageLatest[$entityId->getSerialization()] = $rows[$key];
+			}
 		}
 
 		wfProfileOut( __METHOD__ );
-		return $row;
+		return $pageLatest;
+	}
+
+	/**
+	 * @param EntityId[] $entityIds
+	 * @param DatabaseBase $db
+	 * @return string
+	 */
+	private function getEppWhere( array $entityIds, DatabaseBase $db ) {
+		foreach ( $entityIds as &$entityId ) {
+			$where[] = $db->makeList( array(
+				'epp_entity_id' => $entityId->getNumericId(),
+				'epp_entity_type' => $entityId->getEntityType()
+			), LIST_AND );
+		}
+
+		return $db->makeList( $where, LIST_OR );
 	}
 
 	/**
@@ -307,8 +470,21 @@ class WikiPageEntityRevisionLookup extends DBAccessBase implements EntityRevisio
 	 *
 	 * @return mixed
 	 */
-	protected function getProperEntityId( EntityId $id ) {
+	private function getProperEntityId( EntityId $id ) {
 		return $this->entityIdParser->parse( $id->getSerialization() );
+	}
+
+	/**
+	 * @param EntityId[] $entityIds
+	 *
+	 * @return EntityId[]
+	 */
+	private function getProperEntityIds( array $entityIds ) {
+		foreach ( $entityIds as &$entityId ) {
+			$entityId = $this->getProperEntityId( $entityId );
+		}
+
+		return $entityIds;
 	}
 
 	/**
