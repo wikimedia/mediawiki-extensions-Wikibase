873e18cfe32e38199056250cd32f8d6e16e98048
multichill <maarten@mdammers.nl>
2014-12-27 17:42:43 +0100

    Implement a special page to show items with the most sitelinks
    Bug: T48217
    Change-Id: I2b11256f1d88e69d04bb8631fdfb4c36295c2978

---
+++ repo/includes/specials/SpecialItemsMostSitelinks.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Wikibase\Repo\Specials;
+
+use ContentHandler;
+use Linker;
+use Title;
+use QueryPage;
+use Wikibase\DataModel\Entity\ItemId;
+
+/**
+ * Page for listing items with most sitelinks.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @since 0.5
+ *
+ * @file
+ * @ingroup WikibaseRepo
+ *
+ * @licence GNU GPL v2+
+ * @author Maarten Dammers
+ */
+class SpecialItemsMostSitelinks extends QueryPage {
+	public function __construct() {
+		parent::__construct( 'ItemsMostSitelinks' );
+	}
+
+	function isSyndicated() {
+		return false;
+	}
+
+	function sortDescending() {
+		return true;
+	}
+
+	function isExpensive() {
+                return true;
+        }
+
+	public function getGroupName() {
+		return 'wikibaserepo';
+        }
+
+	function getQueryInfo() {
+		$itemNs = ContentHandler::getForModelID( CONTENT_MODEL_WIKIBASE_ITEM )->getEntityNamespace();
+		return array (
+			'tables' => array ( 'wb_items_per_site' ),
+			'fields' => array (
+				'namespace' => $itemNs,
+				'title' => 'ips_item_id',
+				'value' => 'COUNT(ips_item_id)' ),
+                                'options' => array ( 'GROUP BY' => array( 'ips_item_id' ),
+			),
+		);
+	}
+
+	/**
+	 * @param Skin $skin
+	 * @param object $result Result row
+	 * @return string
+	 */
+	function formatResult( $skin, $result ) {
+		$itemId = ItemId::newFromNumber( (int)$result->title );
+		$title = Title::makeTitleSafe( $result->namespace, $itemId );
+		$titleSection = Title::makeTitleSafe( $result->namespace, $itemId, $fragment = 'sitelinks-wikipedia' );
+		$label = $this->msg( 'wikibase-nsitelinks' )->numParams( $result->value )->escaped();
+		return $this->getLanguage()->specialList(
+			Linker::link( $title ),
+			Linker::link( $titleSection, $label )
+		);
+	}
+}
---
+++ repo/tests/phpunit/includes/specials/SpecialItemsMostSitelinksTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Wikibase\Test;
+
+use Language;
+use Wikibase\Repo\Specials\SpecialItemsMostSitelinks;
+
+/**
+ * @covers Wikibase\Repo\Specials\SpecialItemsMostSitelinks
+ *
+ * @group Wikibase
+ * @group WikibaseRepo
+ * @group SpecialPage
+ * @group WikibaseSpecialPage
+ *
+ * @group Database
+ *        ^---- needed because we rely on Title objects internally
+ *
+ * @licence GNU GPL v2+
+ * @author Adam Shorland
+ * @author Maarten Dammers
+ */
+class SpecialItemsMostSitelinksTest extends SpecialPageTestBase {
+
+	protected function setUp() {
+		parent::setUp();
+
+		$this->setMwGlobals( array(
+			'wgContLang' => Language::factory( 'qqx' )
+		) );
+	}
+
+	protected function newSpecialPage() {
+		return new SpecialItemsMostSitelinks();
+	}
+
+	public function testExecute() {
+		// This also tests that there is no fatal error, that the restriction handling is working
+		// and doesn't block. That is, the default should let the user execute the page.
+		list( $output, ) = $this->executeSpecialPage( '' );
+
+		$this->assertInternalType( 'string', $output );
+		$this->assertContains( 'itemsmostsitelinks-summary', $output );
+		$this->assertContains( '<div class="mw-spcontent">', $output );
+		$this->assertContains( 'wikibase-nsitelinks', $output );
+	}
+
+}
