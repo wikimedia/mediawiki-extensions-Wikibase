b192f40f72bce567332fdefa03ade8f6630219ee
Adrian Heine <adrian.heine@wikimedia.de>
2015-01-06 15:51:14 +0100

    Include the composer autoloader if it is present
    Change-Id: I82cadc5ba224145d92200d301faedd09858d096b

--- lib/resources/packages/wikibase-api/WikibaseJavaScriptApi.php
+++ lib/resources/packages/wikibase-api/WikibaseJavaScriptApi.php
@@ -2,6 +2,11 @@
 
 define( 'WIKIBASE_JAVASCRIPT_API_VERSION', '1.0.2-dev' );
 
+// Include the composer autoloader if it is present.
+if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
+	include_once __DIR__ . '/vendor/autoload.php';
+}
+
 if ( defined( 'MEDIAWIKI' ) ) {
 	call_user_func( function() {
 		require_once __DIR__ . '/init.mw.php';
