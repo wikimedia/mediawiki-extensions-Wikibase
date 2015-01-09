9afbcb9a692424bdc43b8619c25cfdc2599aff55
daniel <daniel.kinzler@wikimedia.de>
2015-01-09 12:18:08 +0000

    Introducing changes subscription management.
    This introduces a service for tracking which clients (subscribers)
    are interested in changes to which entities.
    
    Bug: T86184
    Change-Id: I1b3531fd0cb7b840161599c683633069ff71a3f3

---
+++ client/includes/Usage/NullSubscriptionManager.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Wikibase\Client\Usage;
+
+use Wikibase\DataModel\Entity\EntityId;
+
+/**
+ * No-op implementation of the SubscriptionManager and UsageLookup interfaces.
+ *
+ * @license GPL 2+
+ * @author Daniel Kinzler
+ */
+class NullSubscriptionManager implements SubscriptionManager {
+
+	/**
+	 * Subscribes the given subscriber to notificatiosn about changes on the specified entities.
+	 *
+	 * @param string $subscriber Global site ID of the subscriber
+	 * @param EntityId[] $entityIds The entities to subscribe to.
+	 *
+	 * @throws UsageTrackerException
+	 */
+	public function subscribe( $subscriber, array $entityIds ) {
+		// NO-OP
+	}
+
+	/**
+	 * Unsubscribes the given subscriber from notificatiosn about changes on the specified entities.
+	 *
+	 * @param string $subscriber Global site ID of the subscriber
+	 * @param EntityId[] $entityIds The entities to subscribe to.
+	 *
+	 * @throws UsageTrackerException
+	 */
+	public function unsubscribe( $subscriber, array $entityIds ) {
+		// NO-OP
+	}
+}
