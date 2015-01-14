d93943a27552bbb24621d907bb6857dc4f1b6db4
Henning Snater <henning.snater@wikimedia.de>
2015-01-14 10:12:23 +0100

    Renamed FingerprintView to EntityTermsView
    Reflecting changes of the JS code in the PHP code generating the static HTML.
    As the term "fingerprintview" is supposed to disappear, message keys are adapted as well.
    
    Change-Id: I1821c583c61d3cf5f0ca6f15bb5f151397da8f7b

--- lib/resources/jquery.wikibase/resources.php
+++ lib/resources/jquery.wikibase/resources.php
@@ -153,7 +153,7 @@ return call_user_func( function() {
 				'wikibase.templates',
 			),
 			'messages' => array(
-				'wikibase-fingerprintgroupview-input-help-message',
+				'wikibase-entitytermsview-input-help-message',
 				'wikibase-terms',
 			),
 		),
@@ -197,7 +197,7 @@ return call_user_func( function() {
 				'wikibase-entitytermsforlanguagelistview-description',
 				'wikibase-entitytermsforlanguagelistview-label',
 				'wikibase-entitytermsforlanguagelistview-language',
-				'wikibase-fingerprintview-input-help-message',
+				'wikibase-entitytermsforlanguageview-input-help-message',
 			),
 		),
 
@@ -217,7 +217,7 @@ return call_user_func( function() {
 				'wikibase.templates',
 			),
 			'messages' => array(
-				'wikibase-fingerprintview-input-help-message',
+				'wikibase-entitytermsforlanguageview-input-help-message',
 			),
 		),
 
