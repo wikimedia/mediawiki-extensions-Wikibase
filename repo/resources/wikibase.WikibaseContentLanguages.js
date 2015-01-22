7d755e6d35bebac95834627e3f5345b1faa134c9
Adrian Heine <adrian.heine@wikimedia.de>
2015-01-22 13:48:54 +0100

    Pass ContentLanguages instance using ULS to ValueView
    Bug: T86653
    Change-Id: I4e603bde39bf6634320b562df44149fdc9a2dd10

---
+++ repo/resources/wikibase.WikibaseContentLanguages.js
@@ -0,0 +1,44 @@
+
+/**
+ * @licence GNU GPL v2+
+ * @author Adrian Heine <adrian.heine@wikimedia.de>
+ */
+( function( wb ) {
+	'use strict';
+
+var MODULE = wb;
+var PARENT = util.ContentLanguages;
+
+/**
+ * @constructor
+ */
+var SELF = MODULE.WikibaseContentLanguages = util.inherit(
+	'WbContentLanguages',
+	PARENT,
+	function() {
+		this._languageMap = mw.config.get( 'wgULSLanguages' );
+	}
+);
+
+$.extend( SELF.prototype, {
+	/**
+	 * @type {Object|null}
+	 */
+	_languageMap: null,
+
+	/**
+	 * @inheritdoc
+	 */
+	getAll: function() {
+		return this._languageMap ? Object.keys( this._languageMap ) : null;
+	},
+
+	/**
+	 * @inheritdoc
+	 */
+	getName: function( code ) {
+		return this._languageMap ? this._languageMap[ code ] : null;
+	}
+} );
+
+}( wikibase, jQuery ) );
