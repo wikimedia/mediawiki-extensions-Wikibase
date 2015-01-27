4476298e166fdfcede8038d9cf5f48b5571f50ac
Henning Snater <henning.snater@wikimedia.de>
2015-01-27 16:12:36 +0100

    Added basic snakview QUnit tests
    Change-Id: Ib1a9c7e350aba2d9ca025352ef1e65ad11fa0539

---
+++ lib/tests/qunit/jquery.wikibase/snakview/resources.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @licence GNU GPL v2+
+ * @author H. Snater < mediawiki@snater.com >
+ *
+ * @codeCoverageIgnoreStart
+ */
+return call_user_func( function() {
+
+	$remoteExtPathParts = explode(
+		DIRECTORY_SEPARATOR . 'extensions' . DIRECTORY_SEPARATOR, __DIR__, 2
+	);
+	$moduleTemplate = array(
+		'localBasePath' => __DIR__,
+		'remoteExtPath' => $remoteExtPathParts[1],
+	);
+
+	$resources = array(
+
+		'jquery.wikibase.snakview.tests' => $moduleTemplate + array(
+			'scripts' => array(
+				'snakview.tests.js',
+			),
+			'dependencies' => array(
+				'jquery.wikibase.snakview',
+				'mediawiki.Title',
+				'wikibase.datamodel.Fingerprint',
+				'wikibase.datamodel.Property',
+				'wikibase.datamodel.Term',
+				'wikibase.datamodel.TermMap',
+				'wikibase.serialization.SnakSerializer',
+				'wikibase.store.FetchedContent',
+			),
+		),
+
+	);
+
+	return $resources;
+} );
