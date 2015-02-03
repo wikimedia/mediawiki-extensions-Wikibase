d6d059cb7a2c32857ce4175ffccd883746a956de
Adrian Heine <adrian.heine@wikimedia.de>
2015-02-03 13:25:56 +0100

    Add WikibaseView skeleton
    Change-Id: Ibbeed0a72d593d4a8cb583badc38a95d8c320e80

---
+++ view/WikibaseView.php
@@ -0,0 +1,19 @@
+<?php
+
+if ( defined( 'WIKIBASE_VIEW_VERSION' ) ) {
+	// Do not initialize more than once.
+	return 1;
+}
+
+define( 'WIKIBASE_VIEW_VERSION', '0.1-dev' );
+
+// Include the composer autoloader if it is present.
+if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
+	include_once __DIR__ . '/vendor/autoload.php';
+}
+
+if ( defined( 'MEDIAWIKI' ) ) {
+	call_user_func( function() {
+		require_once __DIR__ . '/init.mw.php';
+	} );
+}
