42e0a0b21c0d829c4cf7b971ebc217f4337bd7b4
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2015-02-12 11:32:21 +0100

    Update release notes and increment version to 3.0.x alpha

--- lib/packages/wikibase/data-model/WikibaseDataModel.php
+++ lib/packages/wikibase/data-model/WikibaseDataModel.php
@@ -12,7 +12,7 @@ if ( defined( 'WIKIBASE_DATAMODEL_VERSION' ) ) {
 	return 1;
 }
 
-define( 'WIKIBASE_DATAMODEL_VERSION', '2.6.0 alpha' );
+define( 'WIKIBASE_DATAMODEL_VERSION', '3.0.0 alpha' );
 
 if ( defined( 'MEDIAWIKI' ) ) {
 	call_user_func( function() {
