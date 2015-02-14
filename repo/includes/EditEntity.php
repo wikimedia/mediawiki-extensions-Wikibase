c9aa4f3a193b4105855c24f76f722f7fc99d4b10
Marius Hoch <hoo@online.de>
2015-02-14 17:20:57 +0100

    Fix EditEntity::isNew for new entities with an Id set
    In some places (eg. the EditEntity api) we set an id on new
    entities before calling out to EditEntity (because several
    things require that).
    
    This broke watching new entities that were created via the api,
    as we always assumed that they already existed.
    
    Bug: T85454
    Change-Id: Ib915e928ee65a9db027326fb8e4aec62ea88b362

--- repo/includes/EditEntity.php
+++ repo/includes/EditEntity.php
@@ -281,18 +281,21 @@ class EditEntity {
 	/**
 	 * Returns the latest revision ID.
 	 *
-	 * @return int
+	 * @return int 0 if the entity doesn't exist
 	 */
 	public function getLatestRevisionId() {
-		if ( $this->isNew() ) {
+		if ( $this->newEntity->getId() === null ) {
 			return 0;
 		}
 
-		if ( $this->latestRevId === null ) {
+		wfProfileIn( __METHOD__ );
+		// Don't do negative caching: We call this to see whether the entity yet exists
+		// before creating.
+		if ( $this->latestRevId === null || $this->latestRevId === 0 ) {
 			if ( $this->latestRev !== null ) {
 				$this->latestRevId = $this->latestRev->getRevisionId();
 			} else {
-				$this->latestRevId = $this->entityRevisionLookup->getLatestRevisionId(
+				$this->latestRevId = (int)$this->entityRevisionLookup->getLatestRevisionId(
 					$this->getEntityId(),
 					EntityRevisionLookup::LATEST_FROM_MASTER
 				);
@@ -312,10 +315,10 @@ class EditEntity {
 	}
 
 	/**
-	 * Returns whether the new content is new, that is, does not have an ID yet and thus no title, page or revisions.
+	 * Returns whether the new content is new: No title, page or revisions (but maybe an Id).
 	 */
 	public function isNew() {
-		return $this->newEntity->getId() === null;
+		return $this->newEntity->getId() === null || $this->getLatestRevisionId() === 0;
 	}
 
 	/**
