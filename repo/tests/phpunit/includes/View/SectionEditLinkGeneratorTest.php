8bb872b1bcb66b1326a6fd01ae19e4ff0628d184
Adrian Heine <adrian.heine@wikimedia.de>
2015-02-17 16:03:10 +0100

    Add @uses annotations for View tests
    Change-Id: I03a759e6d40f157d2ef96b7212771b79db796fcb

--- repo/tests/phpunit/includes/View/SectionEditLinkGeneratorTest.php
+++ repo/tests/phpunit/includes/View/SectionEditLinkGeneratorTest.php
@@ -11,6 +11,10 @@ use Wikibase\Template\TemplateRegistry;
 /**
  * @covers Wikibase\Repo\View\SectionEditLinkGenerator
  *
+ * @uses Wikibase\Template\Template
+ * @uses Wikibase\Template\TemplateFactory
+ * @uses Wikibase\Template\TemplateRegistry
+ *
  * @group Wikibase
  * @group WikibaseRepo
  * @group EntityView
