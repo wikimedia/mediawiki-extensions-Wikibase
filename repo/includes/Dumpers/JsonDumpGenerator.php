88dd49d690e3c68693b3b31414e379d4d0de66c4
Stanislav Malyshev <smalyshev@gmail.com>
2015-02-18 15:34:47 +0100

    Implement RDF export for items and RDF dumps (Turtle only for now)
    + Add compound values support
    
    Use the data with https://www.wikidata.org/wiki/Special:EntityData/Q30.ttl?flavor=full
    Flavors: simple, long, full, dump
    By default displays the same data as before
    
    Issues: T53795, T50141, T50143, T46580
    
    Change-Id: I51334df5b09bb46fc6e46cbfb56b4b5248335f7e

--- repo/includes/Dumpers/JsonDumpGenerator.php
+++ repo/includes/Dumpers/JsonDumpGenerator.php
@@ -6,16 +6,11 @@ use InvalidArgumentException;
 use MWContentSerializationException;
 use MWException;
 use Wikibase\DataModel\Entity\EntityId;
-use Wikibase\Lib\Reporting\ExceptionHandler;
-use Wikibase\Lib\Reporting\MessageReporter;
-use Wikibase\Lib\Reporting\NullMessageReporter;
-use Wikibase\Lib\Reporting\RethrowingExceptionHandler;
 use Wikibase\Lib\Serializers\Serializer;
 use Wikibase\Lib\Store\EntityLookup;
 use Wikibase\Lib\Store\RedirectResolvingEntityLookup;
 use Wikibase\Lib\Store\StorageException;
 use Wikibase\Lib\Store\UnresolvedRedirectException;
-use Wikibase\Repo\Store\EntityIdPager;
 
 /**
  * JsonDumpGenerator generates an JSON dump of a given set of entities, excluding
@@ -26,23 +21,12 @@ use Wikibase\Repo\Store\EntityIdPager;
  * @license GPL 2+
  * @author Daniel Kinzler
  */
-class JsonDumpGenerator {
+class JsonDumpGenerator extends DumpGenerator {
 
 	/**
 	 * @var int flags to use with json_encode as a bit field, see PHP's JSON_XXX constants.
 	 */
 	public $jsonFlags = 0;
-
-	/**
-	 * @var int The max number of entities to process in a single batch.
-	 * Also controls the interval for progress reports.
-	 */
-	public $batchSize = 100;
-
-	/**
-	 * @var resource File handle for output
-	 */
-	private $out;
 
 	/**
 	 * @var Serializer
@@ -55,34 +39,9 @@ class JsonDumpGenerator {
 	private $entityLookup;
 
 	/**
-	 * @var int Total number of shards a request should be split into
-	 */
-	private $shardingFactor = 1;
-
-	/**
-	 * @var int Number of the requested shard
-	 */
-	private $shard = 0;
-
-	/**
 	 * @var bool
 	 */
 	private $useSnippets = false;
-
-	/**
-	 * @var string|null
-	 */
-	private $entityType = null;
-
-	/**
-	 * @var MessageReporter
-	 */
-	private $progressReporter;
-
-	/**
-	 * @var ExceptionHandler
-	 */
-	private $exceptionHandler;
 
 	/**
 	 * @param resource $out
@@ -92,170 +51,41 @@ class JsonDumpGenerator {
 	 * @throws InvalidArgumentException
 	 */
 	public function __construct( $out, EntityLookup $lookup, Serializer $entitySerializer ) {
-		if ( !is_resource( $out ) ) {
-			throw new InvalidArgumentException( '$out must be a file handle!' );
-		}
+		parent::__construct( $out );
 		if ( $lookup instanceof RedirectResolvingEntityLookup ) {
 			throw new InvalidArgumentException( '$lookup must not resolve redirects!' );
 		}
 
-		$this->out = $out;
 		$this->entitySerializer = $entitySerializer;
 		$this->entityLookup = $lookup;
-
-		$this->progressReporter = new NullMessageReporter();
-		$this->exceptionHandler = new RethrowingExceptionHandler();
 	}
 
 	/**
-	 * Sets the batch size for processing. The batch size is used as the limit
-	 * when listing IDs via the EntityIdPager::getNextBatchOfIds() method, and
-	 * also controls the interval of progress reports.
-	 *
-	 * @param int $batchSize
-	 *
-	 * @throws InvalidArgumentException
+	 * Do something before dumping data
 	 */
-	public function setBatchSize( $batchSize ) {
-		if ( !is_int( $batchSize ) || $batchSize < 1 ) {
-			throw new InvalidArgumentException( '$batchSize must be an integer >= 1' );
-		}
-
-		$this->batchSize = $batchSize;
-	}
-
-	/**
-	 * @param bool $useSnippets Whether to output valid json (false) or only comma separated entities
-	 */
-	public function setUseSnippets( $useSnippets ) {
-		$this->useSnippets = (bool)$useSnippets;
-	}
-
-	/**
-	 * @see setBatchSize()
-	 *
-	 * @return int
-	 */
-	public function getBatchSize() {
-		return $this->batchSize;
-	}
-
-	/**
-	 * @param MessageReporter $progressReporter
-	 */
-	public function setProgressReporter( MessageReporter $progressReporter ) {
-		$this->progressReporter = $progressReporter;
-	}
-
-	/**
-	 * @return MessageReporter
-	 */
-	public function getProgressReporter() {
-		return $this->progressReporter;
-	}
-
-	/**
-	 * @param ExceptionHandler $exceptionHandler
-	 */
-	public function setExceptionHandler( ExceptionHandler $exceptionHandler ) {
-		$this->exceptionHandler = $exceptionHandler;
-	}
-
-	/**
-	 * @return ExceptionHandler
-	 */
-	public function getExceptionHandler() {
-		return $this->exceptionHandler;
-	}
-
-	/**
-	 * Set the sharding factor and desired shard.
-	 * For instance, to generate four dumps in parallel, use setShardingFilter( 4, 0 )
-	 * for the first dump, setShardingFilter( 4, 1 ) for the second dump, etc.
-	 *
-	 * @param int $shardingFactor
-	 * @param int $shard
-	 *
-	 * @throws InvalidArgumentException
-	 */
-	public function setShardingFilter( $shardingFactor, $shard ) {
-		if ( !is_int( $shardingFactor ) || $shardingFactor < 1 ) {
-			throw new InvalidArgumentException( '$shardingFactor must be a positive integer.' );
-		}
-
-		if ( !is_int( $shard ) || $shard < 0 ) {
-			throw new InvalidArgumentException( '$shard must be a non-negative integer.' );
-		}
-
-		if ( $shard >= $shardingFactor ) {
-			throw new InvalidArgumentException( '$shard must be smaller than $shardingFactor.' );
-		}
-
-		$this->shardingFactor = $shardingFactor;
-		$this->shard = $shard;
-	}
-
-	/**
-	 * Set the entity type to be included in the output.
-	 *
-	 * @param string|null $type The desired type (use null for any type).
-	 *
-	 * @throws InvalidArgumentException
-	 */
-	public function setEntityTypeFilter( $type ) {
-		$this->entityType = $type;
-	}
-
-	/**
-	 * Generates a JSON dump, writing to the file handle provided to the constructor.
-	 *
-	 * @param EntityIdPager $idPager an Iterator that returns EntityId instances
-	 */
-	public function generateDump( EntityIdPager $idPager ) {
-
+	protected function preDump() {
 		if ( !$this->useSnippets ) {
 			$this->writeToDump( "[\n" );
 		}
+	}
 
-		$dumpCount = 0;
-
-		// Iterate over batches of IDs, maintaining the current position of the pager in the $position variable.
-		while ( $ids = $idPager->fetchIds( $this->batchSize ) ) {
-			$this->dumpEntities( $ids, $dumpCount );
-
-			$this->progressReporter->reportMessage( 'Processed ' . $dumpCount . ' entities.' );
-		};
-
+	/**
+	 * Do something after dumping data
+	 */
+	protected function postDump() {
 		if ( !$this->useSnippets ) {
 			$this->writeToDump( "\n]\n" );
 		}
 	}
 
 	/**
-	 * @param EntityId[] $entityIds
-	 * @param int &$dumpCount The number of entities already dumped (will be updated).
+	 * Do something before dumping entity
+	 *
+	 * @param int $dumpCount
 	 */
-	private function dumpEntities( array $entityIds, &$dumpCount ) {
-		foreach ( $entityIds as $entityId ) {
-			if ( !$this->idMatchesFilters( $entityId ) ) {
-				continue;
-			}
-
-			try {
-				$json = $this->generateJsonForEntityId( $entityId );
-				if ( $json === null ) {
-					continue;
-				}
-
-				if ( $dumpCount > 0 ) {
-					$this->writeToDump( ",\n" );
-				}
-
-				$this->writeToDump( $json );
-				$dumpCount++;
-			} catch ( StorageException $ex ) {
-				$this->exceptionHandler->handleException( $ex, 'failed-to-dump', 'Failed to dump '. $entityId );
-			}
+	protected function preEntityDump( $dumpCount ) {
+		if ( $dumpCount > 0 ) {
+			$this->writeToDump( ",\n" );
 		}
 	}
 
@@ -266,17 +96,16 @@ class JsonDumpGenerator {
 	 *
 	 * @return string|null
 	 */
-	private function generateJsonForEntityId( EntityId $entityId ) {
+	protected function generateDumpForEntityId( EntityId $entityId ) {
 		try {
 			$entity = $this->entityLookup->getEntity( $entityId );
 
 			if ( !$entity ) {
 				throw new StorageException( 'Entity not found: ' . $entityId->getSerialization() );
 			}
-		} catch( MWContentSerializationException $ex ) {
-			throw new StorageException( 'Deserialization error for '
-				. $entityId->getSerialization() );
-		} catch( UnresolvedRedirectException $e ) {
+		} catch ( MWContentSerializationException $ex ) {
+			throw new StorageException( 'Deserialization error for ' . $entityId->getSerialization() );
+		} catch ( UnresolvedRedirectException $e ) {
 			return null;
 		}
 
@@ -286,34 +115,10 @@ class JsonDumpGenerator {
 		return $json;
 	}
 
-	private function idMatchesFilters( EntityId $entityId ) {
-		return $this->idMatchesShard( $entityId )
-			&& $this->idMatchesType( $entityId );
-	}
-
-	private function idMatchesShard( EntityId $entityId ) {
-		// Shorten out
-		if ( $this->shardingFactor === 1 ) {
-			return true;
-		}
-
-		$hash = sha1( $entityId->getSerialization() );
-		$shard = (int)hexdec( substr( $hash, 0, 8 ) ); // 4 bytes of the hash
-		$shard = abs( $shard ); // avoid negative numbers on 32 bit systems
-		$shard %= $this->shardingFactor; // modulo number of shards
-
-		return $shard === $this->shard;
-	}
-
-	private function idMatchesType( EntityId $entityId ) {
-		return $this->entityType === null
-			|| ( $entityId->getEntityType() === $this->entityType );
-	}
-
 	/**
 	 * Encodes the given data as JSON
 	 *
-	 * @param $data
+	 * @param string $data
 	 *
 	 * @return string
 	 * @throws MWException
@@ -329,13 +134,10 @@ class JsonDumpGenerator {
 	}
 
 	/**
-	 * Writers the given string to the output provided to the constructor.
-	 *
-	 * @param $json
+	 * @param bool $useSnippets Whether to output valid json (false) or only comma separated entities
 	 */
-	private function writeToDump( $json ) {
-		//TODO: use output stream object
-		fwrite( $this->out, $json );
+	public function setUseSnippets( $useSnippets ) {
+		$this->useSnippets = (bool)$useSnippets;
 	}
 
 	/**
@@ -355,5 +157,4 @@ class JsonDumpGenerator {
 	public function getJsonFlags() {
 		return $this->jsonFlags;
 	}
-
 }
