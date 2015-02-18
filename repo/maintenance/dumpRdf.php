e6f98355eaaab9177c51a82743e4bc7854086657
Stanislav Malyshev <smalyshev@gmail.com>
2015-02-18 15:34:47 +0100

    Add dump implementation
    Change-Id: I93e5135ebdeee236e61c85f4ee818380cd803a6c
    Issues: T53795, T50141, T50143, T46580

---
+++ repo/maintenance/dumpRdf.php
@@ -0,0 +1,33 @@
+<?php
+namespace Wikibase;
+use Wikibase\Dumpers\RdfDumpGenerator;
+
+require_once __DIR__ . '/dumpEntities.php';
+
+class DumpRdf extends DumpScript {
+
+	public function __construct() {
+		parent::__construct();
+		$this->addOption( 'format', "Set the dump format.", false, true );
+	}
+
+	private function createDumper( $output ) {
+		$format = $dumpFormat = $this->getOption( 'format', 'ttl' );
+		$rdfFormat = RdfSerializer::getFormat( $format );
+		if( !$rdfFormat ) {
+			throw new \MWException( "Unknown format: $format" );
+		}
+		$entitySerializer = new RdfSerializer( $rdfFormat,
+				$GLOBALS['wgCanonicalServer']."/entity/",
+				$GLOBALS['wgCanonicalServer']."/Special:EntityData/",
+				$this->wikibaseRepo->getSiteStore()->getSites(), $this->entityLookup,
+				RdfProducer::PRODUCE_ALL_STATEMENTS | RdfProducer::PRODUCE_TRUTHY_STATEMENTS |
+				RdfProducer::PRODUCE_QUALIFIERS | RdfProducer::PRODUCE_REFERENCES |
+				RdfProducer::PRODUCE_SITELINKS
+		);
+		return new RdfDumpGenerator( $output, $this->revisionLookup, $entitySerializer );
+	}
+}
+
+$maintClass = 'Wikibase\DumpRdf';
+require_once( RUN_MAINTENANCE_IF_MAIN );
