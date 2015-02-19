371229da22ce5128a277df17727d2908c82ba8a8
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2015-02-19 18:01:18 +0100

    Always pad year to 4 digits in DateTime diffs
    Change-Id: I6f017772fb30324d3039cf89ead3d840e3a4bf0a

--- lib/includes/formatters/TimeDetailsFormatter.php
+++ lib/includes/formatters/TimeDetailsFormatter.php
@@ -101,7 +101,15 @@ class TimeDetailsFormatter extends ValueFormatterBase {
 	 * @return string HTML
 	 */
 	private function getTimeHtml( $time ) {
-		return preg_replace( '/(?<=\d)Z$/i', '', $time );
+		// Loose check if the ISO-like string contains at least year, month, day and hour.
+		if ( !preg_match( '/^([-+])(\d+)(-\d+-\d+T\d+(?::\d+)*)Z?$/i', $time, $matches ) ) {
+			return htmlspecialchars( $time );
+		}
+
+		// Warning, never cast the year to integer to not run into 32-bit integer overflows!
+		$year = ltrim( $matches[2], '0' );
+		// Keep the sign. Pad the year. Keep month, day, and time. Drop the trailing "Z".
+		return htmlspecialchars( $matches[1] . str_pad( $year, 4, '0', STR_PAD_LEFT ) . $matches[3] );
 	}
 
 	/**
