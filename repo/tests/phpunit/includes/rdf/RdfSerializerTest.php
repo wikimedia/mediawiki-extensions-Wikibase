fab7dbfb9093fe59077762e4b1af9844b3326066
Stanislav Malyshev <smalyshev@wikimedia.org>
2015-03-05 15:42:02 -0800

    Refactor tests to be data-driven
    Change-Id: I737de39eed89ee38a504e765c3e80ae331b0471e

--- repo/tests/phpunit/includes/rdf/RdfSerializerTest.php
+++ repo/tests/phpunit/includes/rdf/RdfSerializerTest.php
@@ -3,9 +3,19 @@
 namespace Wikibase\Test;
 
 use EasyRdf_Graph;
+use EasyRdf_Namespace;
+use EasyRdf_Literal;
+use EasyRdf_Resource;
 use SiteList;
+use DateTime;
 use Wikibase\EntityRevision;
 use Wikibase\RdfSerializer;
+use Wikibase\RdfBuilder;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\RdfProducer;
+use Wikibase\DataModel\Term\Fingerprint;
 
 /**
  * @covers Wikibase\RdfSerializer
@@ -29,14 +39,8 @@ class RdfSerializerTest extends \MediaWikiTestCase {
 		'turtle',
 	);
 
-	/**
-	 * @var RdfBuilderTest
-	 */
-	private $rdfTest;
-
 	public function setUp() {
 		parent::setUp();
-		$this->rdfTest = new RdfBuilderTest();
 	}
 
 	/**
@@ -57,14 +61,179 @@ class RdfSerializerTest extends \MediaWikiTestCase {
 	 * @return Entity[]
 	 */
 	private function getTestEntities() {
-		return $this->rdfTest->getTestEntities();
+		static $entities = array ();
+
+		if ( !empty( $entities ) ) {
+			return $entities;
+		}
+
+		$entity = new Item();
+		$entities['empty'] = $entity;
+
+		$entity = new Item();
+		$entities['terms'] = $entity;
+		$entity->setFingerprint( self::newTestFingerprint() );
+
+		// TODO: test links
+		// TODO: test data values
+
+
+		$i = 1;
+
+		/**
+		 *
+		 * @var Entity $entity
+		 */
+		foreach ( $entities as $entity ) {
+			$entity->setId( ItemId::newFromNumber( $i ++ ) );
+		}
+
+		return $entities;
+
+	}
+
+	private static function newTestFingerprint() {
+		$fingerprint = Fingerprint::newEmpty();
+
+		$fingerprint->setLabel( 'en', 'Berlin' );
+		$fingerprint->setLabel( 'ru', 'Берлин' );
+
+		$fingerprint->setDescription( 'en', 'German city' );
+		$fingerprint->setDescription( 'ru', 'столица и одновременно земля Германии' );
+
+		$fingerprint->setAliasGroup( 'en', array( 'Berlin, Germany', 'Land Berlin' ) );
+		$fingerprint->setAliasGroup( 'ru', array( 'Berlin' ) );
+
+		return $fingerprint;
+	}
+
+	/**
+	 * @return RdfBuilder
+	 */
+	private static function newRdfBuilder() {
+		return new RdfBuilder(
+				new SiteList(),
+				RdfBuilderTest::URI_BASE,
+				RdfBuilderTest::URI_DATA,
+				RdfBuilderTest::getMockRepository(),
+				RdfProducer::PRODUCE_ALL
+		);
+	}
+
+	/**
+	 * @param EntityId $entityId
+	 * @param array $entityProps
+	 * @param array $dataProps
+	 *
+	 * @return EasyRdf_Graph
+	 */
+	private static function makeEntityGraph( EntityId $entityId, $entityProps, $dataProps ) {
+		$graph = new EasyRdf_Graph();
+
+		$builder = self::newRdfBuilder( 'rdf' ); //XXX: ugh, dummy object
+
+		$entityUri = $builder->getEntityQName( RdfBuilder::NS_ENTITY, $entityId );
+		$dataUri = $builder->getDataURL( $entityId );
+		$entityResource = $graph->resource( $entityUri );
+		$dataResource = $graph->resource( $dataUri );
+
+		self::addProperties( $graph, $entityResource, $entityProps );
+		self::addProperties( $graph, $dataResource, $dataProps );
+
+		return $graph;
+	}
+
+	/**
+	 * @param EasyRdf_Graph $graph
+	 * @param EasyRdf_Resource $resource
+	 * @param array $properties
+	 */
+	private static function addProperties( EasyRdf_Graph $graph, EasyRdf_Resource $resource, $properties ) {
+		foreach ( $properties as $prop => $values ) {
+			if ( !is_array( $values ) ) {
+				$values = array( $values );
+			}
+
+			foreach ( $values as $val ) {
+				if ( is_string( $val ) ) {
+					$val = $graph->resource( $val );
+				}
+
+				$resource->add( $prop, $val );
+			}
+		}
 	}
 
 	/**
 	 * @return EasyRdf_Graph[]
 	 */
 	private function getTestGraphs() {
-		return $this->rdfTest->getTestGraphs();
+		static $graphs = array();
+
+		if ( !empty( $graphs ) ) {
+			return $graphs;
+		}
+
+		$builder = self::newRdfBuilder( 'rdf' ); //XXX: ugh, dummy object
+
+		foreach ( $builder->getNamespaces() as $gname => $uri ) {
+			EasyRdf_Namespace::set( $gname, $uri );
+		}
+
+		$entities = self::getTestEntities();
+
+		$graphs['empty'] = self::makeEntityGraph(
+			$entities['empty']->getId(),
+			array(
+				'rdf:type' => RdfBuilder::NS_ONTOLOGY . ':Item',
+			),
+			array(
+				'rdf:type' => RdfBuilder::NS_SCHEMA_ORG . ':Dataset',
+				'schema:about' => $builder->getEntityQName( RdfBuilder::NS_ENTITY, $entities['empty']->getId() ),
+				'schema:version' => new EasyRdf_Literal( 23, null, 'xsd:integer' ),
+				'schema:dateModified' => new EasyRdf_Literal( '2013-01-01T00:00:00Z', null, 'xsd:dateTime' ),
+			)
+		);
+
+		$graphs['terms'] = self::makeEntityGraph(
+			$entities['terms']->getId(),
+			array(
+				'rdf:type' => RdfBuilder::NS_ONTOLOGY . ':Item',
+				'rdfs:label' => array(
+					new EasyRdf_Literal( 'Berlin', 'en' ),
+					new EasyRdf_Literal( 'Берлин', 'ru' )
+				),
+				'skos:prefLabel' => array(
+					new EasyRdf_Literal( 'Berlin', 'en' ),
+					new EasyRdf_Literal( 'Берлин', 'ru' )
+				),
+				'schema:name' => array(
+					new EasyRdf_Literal( 'Berlin', 'en' ),
+					new EasyRdf_Literal( 'Берлин', 'ru' )
+				),
+				'schema:description' => array(
+					new EasyRdf_Literal( 'German city', 'en' ),
+					new EasyRdf_Literal( 'столица и одновременно земля Германии', 'ru' )
+				),
+				'skos:altLabel' => array(
+					new EasyRdf_Literal( 'Berlin, Germany', 'en' ),
+					new EasyRdf_Literal( 'Land Berlin', 'en' ),
+					new EasyRdf_Literal( 'Berlin', 'ru' )
+				),
+			),
+
+			array(
+				'rdf:type' => RdfBuilder::NS_SCHEMA_ORG . ':Dataset',
+				'schema:about' => $builder->getEntityQName( RdfBuilder::NS_ENTITY, $entities['terms']->getId() ),
+				'schema:version' => new EasyRdf_Literal( 23, null, 'xsd:integer' ),
+				'schema:dateModified' => new EasyRdf_Literal( '2013-01-01T00:00:00Z', null, 'xsd:dateTime' ),
+			)
+		);
+
+		// TODO: test links
+		// TODO: test data values
+
+		return $graphs;
 	}
 
 	private function getTestDataPatterns() {
@@ -181,8 +350,8 @@ class RdfSerializerTest extends \MediaWikiTestCase {
 				$expectedValues = $expectedGraph->all( $rc, $prop );
 				$actualValues = $graph->all( $rc, $prop );
 
-				$expectedStrings = RdfBuilderTest::rdf2strings( $expectedValues );
-				$actualStrings = RdfBuilderTest::rdf2strings( $actualValues );
+				$expectedStrings = self::rdf2strings( $expectedValues );
+				$actualStrings = self::rdf2strings( $actualValues );
 
 				$this->assertArrayEquals( $expectedStrings, $actualStrings );
 			}
@@ -264,4 +433,43 @@ class RdfSerializerTest extends \MediaWikiTestCase {
 		}
 	}
 
+	public static function rdf2strings( array $data ) {
+		$strings = array();
+
+		foreach ( $data as $obj ) {
+			$strings[] = self::rdf2string( $obj );
+		}
+
+		return $strings;
+	}
+
+	public static function rdf2string( $obj ) {
+		if ( $obj instanceof EasyRdf_Resource ) {
+			return '<' . $obj->getUri() . '>';
+		} elseif ( $obj instanceof EasyRdf_Literal ) {
+			$value = $obj->getValue();
+
+			if ( $value instanceof DateTime ) {
+				$value = wfTimestamp( TS_ISO_8601, $value->getTimestamp() );
+			}
+
+			if ( is_int( $value ) ) {
+				$s = strval( $value );
+			} elseif ( is_bool( $value ) ) {
+				$s = $value ? 'true' : 'false';
+			} else {
+				$s = '"' . strval( $value ) . '"';
+
+				if ( $obj->getDatatype() ) {
+					$s .= '^^' . $obj->getDatatype();
+				} elseif ( $obj->getLang() ) {
+					$s .= '@' . $obj->getLang();
+				}
+			}
+
+			return $s;
+		} else {
+			return strval( $obj );
+		}
+	}
 }
