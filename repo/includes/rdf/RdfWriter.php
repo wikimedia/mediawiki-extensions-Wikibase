b359c68b2893b2150e508a141fdf975ccd23b408
daniel <daniel.kinzler@wikimedia.de>
2015-03-08 22:38:07 +0100

    Introduce fast RDF writer
    TODO: documentation
    TODO: investigate: replace baseclass with composition
    TODO: Test Quoter, etc
    
    LATER: sub-documents (share bnode ids, prefixes!)
    LATER: inline bnodes (via sub-documents)
    LATER: inline rdr statements (via sub-documents)
    LATER: inline collections (via sub-documents)
    
    Change-Id: Ibf75485b0ffb075f1efed3d76adafb2da1cec48d

---
+++ repo/includes/rdf/RdfWriter.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace Wikibase\RDF;
+
+/**
+ * Writer interface for RDF output. RdfWriter instances generally stateful, but should
+ * be implemented to operate in a stream-like manner with a minimum of state.
+ *
+ * Caveats:
+ * - no relative uris
+ * - predicates must be qnames
+ * - no inline/nested blank nodes
+ * - no comments
+ * - no collections
+ * - no automatic xsd types
+ * - no automatic conversion of uris to qnames
+ *
+ * @license GPL 2+
+ * @author Daniel Kinzler
+ */
+interface RdfWriter {
+
+	//TODO: split: generic RdfWriter class with shorthands, use RdfFormatters for output
+	//TODO: magic DSL class on top.
+	//TODO: dummy ->and()  does nothing, returns this.
+
+	/**
+	 * @param string|null $label node label, will be generated if not given.
+	 *
+	 * @return string A qname for the blank node.
+	 */
+	public function blank( $label = null );
+
+	/**
+	 * Write a document header.
+	 */
+	public function start();
+
+	/**
+	 * Generates an RDF string from the current buffers state and returns it.
+	 * The buffer is reset to the empty state.
+	 * Before the result string is generated, implementations should close any
+	 * open syntactical structures (close tags, generate footers, etc).
+	 *
+	 * @return string The RDF output
+	 */
+	public function drain();
+
+	/**
+	 * Write a prefix declaration.
+	 * May remember the prefix and URI for later use.
+	 * Implementations are free to fail if prefix() is called after the first call to about().
+	 *
+	 * @note Depending on implementation, re-definitions of prefixes may fail silently.
+	 *
+	 * @param string $prefix
+	 * @param string $uri a reference container as returned by uri()
+	 */
+	public function prefix( $prefix, $uri );
+
+	/**
+	 * Start an about clause. May or may not immediately write anything.
+	 * Must be preceded by a call to start().
+	 * May remember the subject reference for later use.
+	 *
+	 * @param string $base A QName prefix if $local is given, or an IRI if $local is null.
+	 * @param string|null $local A QName suffix, or null if $base is an IRI.
+	 *
+	 * @return RdfWriter $this
+	 */
+	public function about( $base, $local = null );
+
+	/**
+	 * Start a predicate clause. May or may not immediately write anything.
+	 * Must be preceded by a call to about().
+	 * May remember the verb reference for later use.
+	 *
+	 * @param string $base A QName prefix if $local is given, or an IRI or shorthand if $local is null.
+	 * @param string|null $local A QName suffix, or null if $base is an IRI or shorthand.
+	 *
+	 * @return RdfWriter $this
+	 */
+	public function say( $base, $local = null );
+
+	/**
+	 * Emits a resource object.
+	 * Must be preceded by a call to predicate().
+	 *
+	 * @param string $base A QName prefix if $local is given, or an IRI or shorthand if $local is null.
+	 * @param string|null $local A QName suffix, or null if $base is an IRI or shorthand.
+	 *
+	 * @return RdfWriter $this
+	 */
+	public function is( $base, $local = null );
+
+	/**
+	 * Emits a text object.
+	 * Must be preceded by a call to say().
+	 *
+	 * @param string $text the text to be writeted
+	 * @param string|null $language the language the text is in
+	 *
+	 * @return RdfWriter $this
+	 */
+	public function text( $text, $language = null );
+
+
+	/**
+	 * Emits a value object.
+	 * Must be preceded by a call to say().
+	 *
+	 * @param string $literal the value encoded as a string
+	 * @param string $typeBase The data type's QName prefix if $typeLocal is given,
+	 *        or an IRI or shorthand if $typeLocal is null.
+	 * @param string|null $typeLocal The data type's  QName suffix,
+	 *        or null if $typeBase is an IRI or shorthand.
+	 *
+	 * @return RdfWriter $this
+	 */
+	public function value( $literal, $typeBase = null, $typeLocal = null );
+
+	/**
+	 * Shorthand for say( 'a' )->is( $type ).
+	 *
+	 * @param string $typeBase The data type's QName prefix if $typeLocal is given,
+	 *        or an IRI or shorthand if $typeLocal is null.
+	 * @param string|null $typeLocal The data type's  QName suffix,
+	 *        or null if $typeBase is an IRI or shorthand.
+	 *
+	 * @return RdfWriter $this
+	 */
+	public function a( $typeBase, $typeLocal = null );
+
+	/**
+	 * Returns a document-level sub-writer.
+	 *
+	 * @note: do not call drain() on sub-writers!
+	 *
+	 * @return RdfWriter
+	 */
+	public function sub();
+
+	/**
+	 * Resets any state the writer may be holding.
+	 */
+	public function reset();
+
+	/**
+	 * @return string a MIME type
+	 */
+	public function getMimeType();
+}
---
+++ repo/includes/rdf/XmlRdfWriter.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace Wikibase\RDF;
+
+use InvalidArgumentException;
+
+/**
+ * XML/RDF implementation of RdfWriter
+ *
+ * @license GPL 2+
+ * @author Daniel Kinzler
+ */
+class XmlRdfWriter extends RdfWriterBase {
+
+	public function __construct( $role = parent::DOCUMENT_ROLE, BNodeLabeler $labeler = null ) {
+		parent::__construct( $role, $labeler );
+	}
+
+	private function escape( $text ) {
+		return htmlspecialchars( $text, ENT_QUOTES );
+	}
+
+	protected function expandSubject( &$base, &$local ) {
+		$this->expandQName( $base, $local );
+	}
+
+	protected function expandPredicate( &$base, &$local ) {
+		$this->expandShorthand( $base, $local );
+	}
+
+	protected function expandResource( &$base, &$local ) {
+		$this->expandQName( $base, $local );
+	}
+
+	protected function expandType( &$base, &$local ) {
+		$this->expandQName( $base, $local );
+	}
+
+	private function tag( $ns, $name, $attributes = array(), $content = null ) {
+		$sep = $ns === '' ? '' : ':';
+		$this->write( '<', $ns, $sep, $name );
+
+		foreach ( $attributes as $attr => $value ) {
+			if ( is_int( $attr ) ) {
+				// positional array entries are passed verbatim, may be callbacks.
+				$this->write( $value );
+				continue;
+			}
+
+			$this->write( ' ', $attr, '=', '"', $this->escape( $value ), '"' );
+		}
+
+		if ( $content === null ) {
+			$this->write( '>' );
+		} elseif ( $content === '' ) {
+			$this->write( '/>' );
+		} else {
+			$this->write( '>' );
+			$this->write( $content );
+			$this->close( $ns, $name );
+		}
+	}
+
+	private function close( $ns, $name ) {
+		$sep = $ns === '' ? '' : ':';
+		$this->write( '</', $ns, $sep, $name, '>' );
+	}
+
+	/**
+	 * Generates an attribute list, containing the attribute given by $name, or rdf:nodeID
+	 * if $target is a blank node id (starting with "_:"). If $target is a qname, an attempt
+	 * is made to resolve it into a full IRI based on the namespaces registered by calling
+	 * prefix().
+	 *
+	 * @param string $name the attribute name (without the 'rdf:' prefix)
+	 * @param string $base
+	 * @param string $local
+	 *
+	 * @return string[]
+	 */
+	private function getTargetAttributes( $name, $base, $local ) {
+		if ( $base === null && $local === null ) {
+			return array();
+		}
+
+		// handle blank
+		if ( $base === '_' ) {
+			$name = 'nodeID';
+			$value = $local;
+		} elseif ( $local !== null ) {
+			throw new InvalidArgumentException( "Expected IRI, got QName: $base:$local" );
+		} else {
+			$value = $base;
+		}
+
+		return array(
+			"rdf:$name" => $value
+		);
+	}
+
+	/**
+	 * Emit a document header.
+	 */
+	protected function beginDocument() {
+		$this->write( '<?xml version="1.0"?>', "\n" );
+
+		// define a callback for generating namespace attributes
+		$self = $this;
+		$namespaceAttrCallback = function() use ( $self ) {
+			$attr = '';
+
+			$namespaces = $self->getPrefixes();
+			foreach ( $namespaces as $ns => $uri ) {
+				$escapedUri = htmlspecialchars( $uri, ENT_QUOTES );
+				$nss = $ns === '' ? '' : ":$ns";
+				$attr .= " xmlns$nss=\"$escapedUri\"";
+			}
+
+			return $attr;
+		};
+
+		$this->tag( 'rdf', 'RDF', array( $namespaceAttrCallback ) );
+		$this->write( "\n" );
+	}
+
+	protected function writePrefix( $prefix, $uri ) {
+		// noop
+	}
+
+	protected function writeSubject( $base, $local = null ) {
+		$attr = $this->getTargetAttributes( 'about', $base, $local );
+
+		$this->write( "\t" );
+		$this->tag( 'rdf', 'Description', $attr );
+		$this->write( "\n" );
+	}
+
+	/**
+	 * Emit the root element
+	 */
+	protected function finishSubject() {
+		$this->write( "\t" );
+		$this->close( 'rdf', 'Description' );
+		$this->write( "\n" );
+	}
+
+	protected function finishDocument() {
+		// close document element
+		$this->close( 'rdf', 'RDF' );
+		$this->write( "\n" );
+	}
+
+	protected function writePredicate( $base, $local = null ) {
+		// noop
+	}
+
+	protected function writeResource( $base, $local = null ) {
+		$attr = $this->getTargetAttributes( 'resource', $base, $local );
+
+		$this->write( "\t\t" );
+		$this->tag( $this->currentPredicate[0], $this->currentPredicate[1], $attr, '' );
+		$this->write( "\n" );
+	}
+
+	protected function writeText( $text, $language = null ) {
+		$attr = empty( $language ) ? array() : array( 'xml:lang' => $language );
+
+		$this->write( "\t\t" );
+		$this->tag( $this->currentPredicate[0], $this->currentPredicate[1], $attr, $this->escape( $text ) );
+		$this->write( "\n" );
+	}
+
+	public function writeValue( $literal, $typeBase, $typeLocal = null ) {
+		$attr = $this->getTargetAttributes( 'datatype', $typeBase, $typeLocal );
+
+		$this->write( "\t\t" );
+		$this->tag( $this->currentPredicate[0], $this->currentPredicate[1], $attr, $this->escape( $literal ) );
+		$this->write( "\n" );
+	}
+
+	/**
+	 * @param string $role
+	 * @param BNodeLabeler $labeler
+	 *
+	 * @return RdfWriterBase
+	 */
+	protected function newSubWriter( $role, BNodeLabeler $labeler ) {
+		$writer = new self( $role, $labeler );
+
+		return $writer;
+	}
+
+	/**
+	 * @return string a MIME type
+	 */
+	public function getMimeType() {
+		return 'application/rdf+xml; charset=UTF-8';
+	}
+
+}
