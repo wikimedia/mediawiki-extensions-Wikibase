d0e7387a5854810b1b920e160d9c8d20bab3722f
daniel <daniel.kinzler@wikimedia.de>
2015-03-08 22:38:07 +0100

    Introduce fast RDF writer
    TODO: documentation
    TODO: investigate: replace baseclass with composition
    TODO: investigate: avoid qname parsing (two-param mode)
    TODO: Test Quoter, etc
    
    LATER: sub-documents (share bnode ids, prefixes!)
    LATER: inline bnodes (via sub-documents)
    LATER: inline rdr statements (via sub-documents)
    LATER: inline collections (via sub-documents)
    
    Change-Id: Ibf75485b0ffb075f1efed3d76adafb2da1cec48d

---
+++ repo/includes/rdf/NTriplesRdfWriter.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Wikibase\RDF;
+use InvalidArgumentException;
+
+/**
+ * NTriplesRdfWriter
+ *
+ * @license GPL 2+
+ * @author Daniel Kinzler
+ */
+class NTriplesRdfWriter extends RdfWriterBase {
+
+	/**
+	 * @var N3Quoter
+	 */
+	private $quoter;
+
+	private $currentSubject;
+
+	private $currentPredicate;
+
+	public function __construct( $role = parent::DOCUMENT_ROLE, BNodeLabeler $labeler = null, N3Quoter $quoter = null ) {
+		parent::__construct( $role, $labeler );
+
+		$this->quoter = $quoter ?: new N3Quoter();
+		$this->quoter->setAllowQNames( false );
+
+		//NOTE: The RDF 1.1 spec of N-Triples allows full UTF-8, so escaping would not be required.
+		//      However, as of 2015, many consumers of N-Triples still expect non-ASCII characters
+		//      to be escaped.
+		//NOTE: if this is changed, getMimeType must be changed accordingly.
+		$this->quoter->setEscapeUnicode( true );
+
+		$this->quoter->registerShorthand( 'a', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type' );
+		$this->quoter->registerPrefix( 'rdf', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#' );
+		$this->quoter->registerPrefix('xsd', 'http://www.w3.org/2001/XMLSchema#' );
+	}
+
+	public function reset() {
+		parent::reset();
+
+		$this->quoter = new N3Quoter(); // FIXME: if the quoter was shared, this may cause inconsistencies
+		$this->currentPredicate = null;
+		$this->currentSubject = null;
+	}
+
+	private function quoteResource( $s ) {
+		if ( $s instanceof RdfWriter ) {
+			return $s;
+		}
+
+		//FIXME: nasty hack for little benefit
+		return call_user_func_array( array( $this->quoter, 'quoteResource' ), func_get_args() );
+	}
+
+	protected function writePrefix( $prefix, $uri ) {
+		$this->quoter->registerPrefix( $prefix, $uri );
+	}
+
+	protected function writeSubject( $subject ) {
+		$subject = $this->quoteResource( $subject );
+		$this->currentSubject = $subject;
+	}
+
+	protected function writePredicate( $verb ) {
+		$verb = $this->quoteResource( $verb, 'a' );
+		$this->currentPredicate = $verb;
+	}
+
+	private function writeTriple( $object ) {
+		$this->write( $this->currentSubject, ' ' );
+		$this->write( $this->currentPredicate, ' ' );
+		$this->write( $object );
+	}
+
+	protected function writeResource( $object ) {
+		$object = $this->quoteResource( $object );
+		$this->writeTriple( $object );
+	}
+
+	protected function writeText( $text, $language = null ) {
+		$literal = $this->quoter->getLiteral( $text, '@', $language );
+		$this->writeTriple( $literal );
+	}
+
+	protected function writeValue( $text, $type = null ) {
+		$type = $type === null ? null : $this->quoteResource( $type );
+		$literal = $this->quoter->getLiteral( $text, '^^', $type );
+		$this->writeTriple( $literal );
+	}
+
+	protected function finishObject( $last = false ) {
+		$this->write( ' .', "\n" );
+	}
+
+	/**
+	 * @param string $role
+	 * @param BNodeLabeler $labeler
+	 *
+	 * @return RdfWriterBase
+	 */
+	protected function newSubWriter( $role, BNodeLabeler $labeler ) {
+		$writer = new self( $role, $labeler, $this->quoter );
+
+		return $writer;
+	}
+
+	/**
+	 * @return string a MIME type
+	 */
+	public function getMimeType() {
+		//NOTE: Add charset=UTF-8 if and when the constructor configures $this->quoter
+		//      to write utf-8.
+		return 'application/n-triples';
+	}
+
+}
---
+++ repo/tests/phpunit/includes/rdf/RdfWriterTestBase.php
@@ -0,0 +1,245 @@
+<?php
+
+namespace Wikibase\Test;
+use Wikibase\RDF\RdfWriter;
+
+/**
+ * Base class for tests for RdfWriter implementations.
+ * Provides a common test suite for all implementations.
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ */
+abstract class RdfWriterTestBase extends \PHPUnit_Framework_TestCase{
+
+	abstract protected function getFileSuffix();
+
+	protected function getExpectedOutputFile( $datasetName ) {
+		$path = __DIR__ . '/../../data/rdf/' . $datasetName . '.' . $this->getFileSuffix();
+		return $path;
+	}
+
+	private function normalizeLines( array $lines ) {
+		$normalized = array();
+
+		foreach ( $lines as $s ) {
+			$s = trim( $s, "\r\n" );
+			$normalized[] = $s;
+		}
+
+		return $normalized;
+	}
+
+	protected function assertOutputLines( $datasetName, $actual ) {
+		if ( is_string( $actual ) ) {
+			$actual = trim( $actual, "\r\n" );
+			$actual = explode( "\n", $actual );
+		}
+
+		$path = $this->getExpectedOutputFile( $datasetName );
+
+		// Create test data file if it doesn't exist.
+		if ( !file_exists( $path ) ) {
+			file_put_contents( $path . '.actual', join( "\n", $actual ) );
+		}
+
+		$expected = file( $path );
+
+		$expected = $this->normalizeLines( $expected );
+		$actual = $this->normalizeLines( $actual );
+
+		$this->assertEquals( $expected, $actual, 'Result mismatches data in ' . $path );
+	}
+
+	/**
+	 * @return RdfWriter
+	 */
+	protected abstract function newWriter();
+
+	public function testTriples() {
+		$writer = $this->newWriter();
+
+		$writer->start();
+		$writer->prefix( 'acme', 'http://acme.test/' );
+
+		$writer->about( 'http://foobar.test/Bananas' )
+			->say( 'a' )->is( 'http://foobar.test/Fruit' ); // shorthand name "a"
+
+		// interspersed prefix definition
+		$writer->prefix( 'xsd', 'http://www.w3.org/2001/XMLSchema#' );
+
+		$writer->about( 'acme:Nuts' )
+			->say( 'acme:weight' )->value( '5.5', 'xsd:decimal' );
+
+		$rdf = $writer->drain();
+		$this->assertOutputLines( 'Triples', $rdf );
+	}
+
+	public function testPredicates() {
+		$writer = $this->newWriter();
+
+		$writer->start();
+		$writer->prefix( '', 'http://acme.test/' ); // empty prefix
+		$writer->prefix( 'xsd', 'http://www.w3.org/2001/XMLSchema#' );
+
+		$writer->about( 'http://foobar.test/Bananas' )
+			->a( 'http://foobar.test/Fruit' ) // shorthand function a()
+			->say( ':weight' ) // empty prefix
+				->value( 5.5, 'xsd:double' );
+
+		$rdf = $writer->drain();
+		$this->assertOutputLines( 'Predicates', $rdf );
+	}
+
+	public function testValues() {
+		$writer = $this->newWriter();
+
+		$writer->start();
+		$writer->prefix( 'acme', 'http://acme.test/' );
+
+		$writer->about( 'http://foobar.test/Bananas' )
+			->say( 'acme:multi' )
+				->value( 'A' )
+				->value( 'B' )
+				->value( 'C' )
+			->say( 'acme:type' )
+				->value( 'foo', 'acme:thing' )
+				->value( '-5', 'xsd:integer' )
+				->value( '-5', 'xsd:decimal' )
+				->value( '-5', 'xsd:double' )
+				->value( 'true', 'xsd:boolean' )
+				->value( 'false', 'xsd:boolean' )
+			->say( 'acme:autotype' )
+				->value( -5 )
+				->value( 3.14 )
+				->value( true )
+				->value( false )
+			->say( 'acme:no-autotype' )
+				->value( -5, 'xsd:decimal' )
+				->value( 3.14, 'xsd:string' )
+				->value( true, 'xsd:string' )
+				->value( false, 'xsd:string' )
+			->hasv( 'acme:shorthand', 'foo' )
+			->hasv( 'acme:typed-shorthand', 'foo', 'acme:thing' );
+
+		$rdf = $writer->drain();
+		$this->assertOutputLines( 'Values', $rdf );
+	}
+
+	public function testResources() {
+		$writer = $this->newWriter();
+
+		$writer->start();
+		$writer->prefix( 'acme', 'http://acme.test/' );
+
+		$writer->about( 'acme:Bongos' )
+			->say( 'acme:sounds' )
+				->is( 'acme:Bing' )
+				->is( 'http://foobar.test/sound/Bang' );
+
+		$rdf = $writer->drain();
+		$this->assertOutputLines( 'Resources', $rdf );
+
+	}
+
+	public function testTexts() {
+		$writer = $this->newWriter();
+
+		$writer->start();
+		$writer->prefix( 'acme', 'http://acme.test/' );
+
+		$writer->about( 'acme:Bongos' )
+			->say( 'acme:sounds' )
+				->text( 'Bom', 'de' )
+				->text( 'Bam', 'en' );
+
+		$rdf = $writer->drain();
+		$this->assertOutputLines( 'Texts', $rdf );
+	}
+
+	public function testNumbers() {
+		$writer = $this->newWriter();
+
+		$writer->start();
+		$writer->prefix( 'acme', 'http://acme.test/' );
+		$writer->prefix( 'xsd', 'http://www.w3.org/2001/XMLSchema#' );
+
+		$writer->about( 'acme:Bongos' )
+			->hasv( 'acme:stock', 5, 'xsd:integer' )
+				->value( 7 )
+		->about( 'acme:Tablas' )
+			->hasv( 'acme:stock', 6 );
+
+		$rdf = $writer->drain();
+		$this->assertOutputLines( 'Numbers', $rdf );
+	}
+
+	public function testEricMiller() {
+		// exampel taken from http://www.w3.org/2007/02/turtle/primer/
+
+		$writer = $this->newWriter();
+
+		$writer->start();
+		$writer->prefix( 'rdf', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#' );
+		$writer->prefix( 'contact', 'http://www.w3.org/2000/10/swap/pim/contact#' );
+
+		$writer->about( 'http://www.w3.org/People/EM/contact#me' )
+			->hasr( 'rdf:type', 'contact:Person' )
+			->hast( 'contact:fullName', 'Eric Miller' )
+			->hasr( 'contact:mailbox','mailto:em@w3.org' )
+			->hast( 'contact:personalTitle', 'Dr.' );
+
+		$rdf = $writer->drain();
+		$this->assertOutputLines( 'EricMiller', $rdf );
+	}
+
+	public function testLabeledBlankNode() {
+		// exampel taken from http://www.w3.org/2007/02/turtle/primer/
+
+		$writer = $this->newWriter();
+
+		$writer->start();
+		$writer->prefix( 'exterms', 'http://www.example.org/terms/' );
+		$writer->prefix( 'exstaff', 'http://www.example.org/staffid/' );
+
+		$writer->about( 'exstaff:85740' )
+			->say( 'exterms:address' )->is( $label = $writer->blank( 'johnaddress' ) )
+		->about( $label )
+			->say( 'exterms:street' )->text( "1501 Grant Avenue" )
+			->say( 'exterms:city' )->text( "Bedfort" )
+			->say( 'exterms:state' )->text( "Massachusetts" )
+			->say( 'exterms:postalCode' )->text( "01730" );
+
+		$rdf = $writer->drain();
+		$this->assertOutputLines( 'LabeledBlankNode', $rdf );
+	}
+
+	public function testNumberedBlankNodes() {
+		// exampel taken from http://www.w3.org/2007/02/turtle/primer/
+
+		$writer = $this->newWriter();
+
+		$writer->start();
+		$writer->prefix( 'exterms', 'http://www.example.org/terms/' );
+		$writer->prefix( 'exstaff', 'http://www.example.org/staffid/' );
+		$writer->prefix( 'ex', 'http://example.org/packages/vocab#' );
+
+		$writer->about( 'exstaff:Sue' )
+			->say( 'exterms:publication' )->is( $label1 = $writer->blank() );
+		$writer->about( $label1 )
+			->say( 'exterms:title' )->text( 'Antology of Time' );
+
+		$writer->about( 'exstaff:Jack' )
+			->say( 'exterms:publication' )->is( $label2 = $writer->blank() );
+		$writer->about( $label2 )
+			->say( 'exterms:title' )->text( 'Anthony of Time' );
+
+		$rdf = $writer->drain();
+		$this->assertOutputLines( 'NumberedBlankNode', $rdf );
+	}
+
+	//FIXME: test quoting/escapes!
+	//FIXME: test non-ascii literals!
+	//FIXME: test uerl-encoding
+	//FIXME: test IRIs!
+}
