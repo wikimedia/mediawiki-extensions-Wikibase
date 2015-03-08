311f5e71e4b40e2f0039a2b84d4696dd45b446b5
daniel <daniel.kinzler@wikimedia.de>
2015-03-08 22:38:07 +0100

    Introduce fast RDF writer
    TODO: documentation
    TODO: investigate: replace baseclass with composition
    TODO: investigate: avoid qname parsing (two-param mode)
    TODO: Test Quoter, etc
    
    LATER: sub-documents (share bnode ids, prefixes!)
    LATER: inline bnodes (via sub-documents)
    LATER: inline rdr statements (via sub-documents)
    LATER: inline collections (via sub-documents)
    
    Change-Id: Ibf75485b0ffb075f1efed3d76adafb2da1cec48d

---
+++ repo/tests/phpunit/includes/rdf/NTriplesRdfWriterTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Wikibase\Test;
+
+use Wikibase\RDF\NTriplesRdfWriter;
+use Wikibase\RDF\RdfWriter;
+
+/**
+ * @covers Wikibase\RDF\NTriplesRdfWriter
+ *
+ * @group Wikibase
+ * @group WikibaseRepo
+ * @group WikibaseRdf
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ */
+class NTriplesRdfWriterTest extends RdfWriterTestBase {
+
+	protected function getFileSuffix() {
+		return 'nt';
+	}
+
+	/**
+	 * @return RdfWriter
+	 */
+	protected function newWriter() {
+		return new NTriplesRdfWriter();
+	}
+}
