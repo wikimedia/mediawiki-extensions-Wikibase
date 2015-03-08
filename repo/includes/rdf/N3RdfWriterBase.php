39c79610a229cc1eb72723d0d9bdaae797a48881
daniel <daniel.kinzler@wikimedia.de>
2015-03-08 22:38:07 +0100

    Introduce fast RDF writer.
    RdfWriter is intended to allow a turtle-like syntax when generating
    RDF output:
    
     $writer->prefix( 'acme', 'http://acme.test/terms/' );
     $writer->about( 'http://quux.test/Something' )
       ->say( 'acme', 'name' )->text( 'Thingy' )->text( 'Dingsda', 'de' )
       ->say( 'acme', 'owner' )->is( 'http://quux.test/' );
    
    Output is generated on-the-fly, without constructing an in-memory model.
    
    RdfWriter is optimized for speed, and omits most sanity checks
    and similar precautions in favor of performance. Further optimization
    is however expected.
    
    Change-Id: Ibf75485b0ffb075f1efed3d76adafb2da1cec48d

---
+++ repo/includes/rdf/N3RdfWriterBase.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Wikibase\RDF;
+use InvalidArgumentException;
+
+/**
+ * Base class for RdfWriter implementations that output an N3 dialect.
+ *
+ * @license GPL 2+
+ * @author Daniel Kinzler
+ */
+abstract class N3RdfWriterBase extends RdfWriterBase {
+
+	/**
+	 * @var N3Quoter
+	 */
+	protected $quoter;
+
+	public function __construct( $role = parent::DOCUMENT_ROLE, BNodeLabeler $labeler = null, N3Quoter $quoter = null ) {
+		parent::__construct( $role, $labeler );
+
+		$this->quoter = $quoter ?: new N3Quoter();
+	}
+
+	protected function writeRef( $base, $local = null ) {
+		if ( $local === null ) {
+			if( $this->isShorthand( $base) ) {
+				$this->writeShorthand( $base );
+			} else {
+				$this->writeIRI( $base );
+			}
+		} else {
+			$this->writeQName( $base, $local );
+		}
+	}
+
+	protected function writeShorthand( $shorthand ) {
+		if ( $shorthand === null || $shorthand === '' ) {
+			throw new InvalidArgumentException( '$shorthand must not be empty' );
+		}
+
+		$this->write( $shorthand );
+	}
+
+	protected function writeIRI( $iri ) {
+		if ( $iri === null || $iri === '' ) {
+			throw new InvalidArgumentException( '$iri must not be empty' );
+		}
+
+		if ( $iri[0] === '_' || $iri[0] === ':' || $iri[0] === '/' || $iri[0] === '#' ) {
+			throw new InvalidArgumentException( '$iri must be an absolute iri: ' . $iri );
+		}
+
+		$iri = $this->quoter->escapeIRI( $iri );
+		$this->write( '<', $iri, '>' );
+	}
+
+	protected function writeQName( $base, $local ) {
+		if ( $base === null ) {
+			throw new InvalidArgumentException( '$base must not be null' );
+		}
+
+		if ( $local === null || $local === '' ) {
+			throw new InvalidArgumentException( '$local must not be empty' );
+		}
+
+		$this->write( $base, ':', $local );
+	}
+
+
+	protected function writeText( $text, $language = null ) {
+		$value = $this->quoter->escapeLiteral( $text );
+		$this->write( '"', $value, '"' );
+
+		if ( $language !== null ) {
+			$this->write( '@', $language );
+		}
+	}
+
+	protected function writeValue( $value, $typeBase = null, $typeLocal = null  ) {
+		$value = $this->quoter->escapeLiteral( $value );
+		$this->write( '"', $value, '"' );
+
+		if ( $typeBase !== null ) {
+			$this->write( '^^' );
+			$this->writeRef( $typeBase, $typeLocal );
+		}
+	}
+
+}
