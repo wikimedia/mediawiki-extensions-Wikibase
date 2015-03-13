c79c7488f73a540d4c13d04a5f95d9e64b9c306d
Stanislav Malyshev <smalyshev@wikimedia.org>
2015-03-13 13:23:05 -0700

    Addressed several FIXMEs in RDF related code
    - Add date cleanup
    - Improve handling of non-literal values
    - Add comments to producer
    - Made adding used properties and mentioned entities configurable. I still think it's two separate use cases.
    
    Change-Id: I51d6377511b42eb9cf638af975e3a940550643e6

--- repo/includes/rdf/RdfProducer.php
+++ repo/includes/rdf/RdfProducer.php
@@ -7,15 +7,28 @@ namespace Wikibase;
  */
 interface RdfProducer {
 
-	//FIXME: documentation needed
+	// Produce "truthy" statements, i.e. best ranked
+	// entity-property-value without qualifiers or expanded values
 	const PRODUCE_TRUTHY_STATEMENTS = 1;
+	// Produce all statements
 	const PRODUCE_ALL_STATEMENTS    = 2;
+	// Produce qualifiers for statements
+	// Should be used together with PRODUCE_ALL_STATEMENTS.
 	const PRODUCE_QUALIFIERS        = 4;
+	// Produce references for statements
+	// Should be used together with PRODUCE_ALL_STATEMENTS.
 	const PRODUCE_REFERENCES        = 8;
+	// Produce links and badges
 	const PRODUCE_SITELINKS         = 16;
+	// Add entity definitions for properties used in the dump.
 	const PRODUCE_PROPERTIES        = 32;
+	// Produce full expanded values as nodes.
+	// Should be used together with PRODUCE_ALL_STATEMENTS.
 	const PRODUCE_FULL_VALUES       = 64;
+	// Produce metadata header containing software version info and copyright
 	const PRODUCE_VERSION_INFO      = 128;
+	// Produce definitions for all entities used in the dump
+	const PRODUCE_RESOLVED_ENTITIES = 256;
 
-	const PRODUCE_ALL = 0xFF;
+	const PRODUCE_ALL = 0xFFFF;
 }
