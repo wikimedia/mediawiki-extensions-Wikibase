6bf44bab4fad174d5182a206f1b76fd8f56fed36
Adrian Heine <adrian.heine@wikimedia.de>
2015-03-16 12:30:03 +0100

    Introduce SpecialPageLinker
    Change-Id: I937a17873221a614bddeb4a81ca12eea610f1e43

---
+++ repo/tests/phpunit/includes/View/RepoSpecialPageLinkerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Wikibase\Test;
+
+use MediaWikiLangTestCase;
+use Wikibase\Repo\View\RepoSpecialPageLinker;
+
+/**
+ * @covers Wikibase\Repo\View\RepoSpecialPageLinker
+ *
+ * @author Adrian Heine < adrian.heine@wikimedia.de >
+ */
+class RepoSpecialPageLinkerTest extends MediaWikiLangTestCase {
+
+	/**
+	 * @dataProvider getLinkProvider
+	 *
+	 * @param string $specialPageName
+	 * @param string[] $subPageParams
+	 * @param string $expectedMatch
+	 */
+	public function testGetLink( $specialPageName, $subPageParams, $expectedMatch) {
+		$linker = new RepoSpecialPageLinker();
+
+		$link = $linker->getLink( $specialPageName, $subPageParams );
+
+		$this->assertRegExp( $expectedMatch, $link );
+	}
+
+	public function getLinkProvider() {
+		return array(
+			array( 'SetLabel', array(), '/Special:SetLabel\/?$/' ),
+			array( 'SetLabel', array( 'en' ), '/Special:SetLabel\/en\/?$/' ),
+			array( 'SetLabel', array( 'en', 'Q5' ), '/Special:SetLabel\/en\/Q5\/?$/' )
+		);
+	}
+
+
+}
