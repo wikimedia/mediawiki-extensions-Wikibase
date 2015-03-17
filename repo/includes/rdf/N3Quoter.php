9a1cf3daee7f118521eb1ab100fc2652407d4e45
daniel <daniel.kinzler@wikimedia.de>
2015-03-17 21:31:27 +0100

    Performance improvements for RdfWriter
    Change-Id: I20df20124a3db2404a3a99421373128332d9fd4b

--- repo/includes/rdf/N3Quoter.php
+++ repo/includes/rdf/N3Quoter.php
@@ -2,8 +2,6 @@
 
 namespace Wikibase\RDF;
 
-use InvalidArgumentException;
-
 /**
  * Helper class for quoting literals and URIs in N3 output.
  * Optionally supports shorthand and prefix resolution.
@@ -13,67 +11,31 @@ use InvalidArgumentException;
  */
 class N3Quoter {
 
-	private $badChars = array(
-		"\"",
-		"\\",
-		"\0",
-		"\n",
-		"\r",
-		"\t",
-	);
-
-	private $badCharEscapes = array(
-		'\"',
-		'\\\\',
-		'\0',
-		'\n',
-		'\r',
-		'\t',
-	);
-
-	private $badUriChars = array(
-		"<",
-		">",
-		"\"",
-		" ",
-		"\n",
-		"\r",
-		"\t",
-	);
-
-	private $badUriCharEscapes = array(
-		'%3C',
-		'%3E',
-		'%22',
-		'%20',
-		'%0D',
-		'%0A',
-		'%09',
-	);
-
 	/**
 	 * @var UnicodeEscaper
 	 */
 	private $escaper = null;
 
 	/**
-	 * @param $escapeUnicode
+	 * @param bool $escapeUnicode
 	 */
 	public function setEscapeUnicode( $escapeUnicode ) {
 		$this->escaper = $escapeUnicode ? new UnicodeEscaper() : null;
 	}
 
-	public function escapeIRI( $uri ) {
-		//FIXME: more robust escaping;
-		//FIXME: apply unicode escaping?!
-		$quoted = str_replace( $this->badUriChars, $this->badUriCharEscapes, $uri );
+	public function escapeIRI( $iri ) {
+		$quoted = strtr( $iri, array(
+			' ' => '%20',
+			'"' => '%22',
+			'<' => '%3C',
+			'>' => '%3E',
+		) );
 
 		return $quoted;
 	}
 
 	public function escapeLiteral( $s ) {
-		//FIXME: more robust escaping
-		$escaped = str_replace( $this->badChars, $this->badCharEscapes, $s );
+		$escaped = addcslashes( $s, "\r\n\t\0\\\"" );
 
 		if ( $this->escaper !== null ) {
 			$escaped = $this->escaper->escapeString( $escaped );
