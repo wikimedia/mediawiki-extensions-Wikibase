917b4fa5ae4244be798e52c6c905f9b6a66f32ef
Stanislav Malyshev <smalyshev@wikimedia.org>
2015-03-19 14:46:17 -0700

    Performance tweaks
    Tweaks:
    - Simplified state transitions to have less call depths
    - Refactored write() to take one argument
    - Use Snaks::getHash for refs since it doesn't duplicate the list
    - Simplify URL encoding for pages for performance
    
    Change-Id: I5eebfbce570d2b5a6d9321788e89052ef2910226

--- repo/includes/rdf/N3RdfWriterBase.php
+++ repo/includes/rdf/N3RdfWriterBase.php
@@ -43,65 +43,63 @@ abstract class N3RdfWriterBase extends RdfWriterBase {
 
 	protected function writeRef( $base, $local = null ) {
 		if ( $local === null ) {
-			if ( $this->isShorthand( $base ) ) {
-				$this->writeShorthand( $base );
+			if( $base === 'a' ) {
+				$this->write( 'a' );
 			} else {
 				$this->writeIRI( $base );
 			}
 		} else {
-			$this->writeQName( $base, $local );
+			$this->write( "$base:$local" );
 		}
 	}
 
 	protected function writeShorthand( $shorthand ) {
-		if ( $shorthand === null || $shorthand === '' ) {
-			throw new InvalidArgumentException( '$shorthand must not be empty' );
-		}
+// 		if ( $shorthand === null || $shorthand === '' ) {
+// 			throw new InvalidArgumentException( '$shorthand must not be empty' );
+// 		}
 
 		$this->write( $shorthand );
 	}
 
 	protected function writeIRI( $iri ) {
-		if ( $iri === null || $iri === '' ) {
-			throw new InvalidArgumentException( '$iri must not be empty' );
-		}
+// 		if ( $iri === null || $iri === '' ) {
+// 			throw new InvalidArgumentException( '$iri must not be empty' );
+// 		}
 
-		if ( $iri[0] === '_' || $iri[0] === ':' || $iri[0] === '/' || $iri[0] === '#' ) {
-			throw new InvalidArgumentException( '$iri must be an absolute iri: ' . $iri );
-		}
-
+// 		if ( $iri[0] === '_' || $iri[0] === ':' || $iri[0] === '/' || $iri[0] === '#' ) {
+// 			throw new InvalidArgumentException( '$iri must be an absolute iri: ' . $iri );
+// 		}
 		if ( !$this->trustIRIs ) {
 			$iri = $this->quoter->escapeIRI( $iri );
 		}
-
-		$this->write( '<', $iri, '>' );
+		$this->write( "<$iri>" );
 	}
 
 	protected function writeQName( $base, $local ) {
-		if ( $base === null ) {
-			throw new InvalidArgumentException( '$base must not be null' );
-		}
+// 		if ( $base === null ) {
+// 			throw new InvalidArgumentException( '$base must not be null' );
+// 		}
 
-		if ( $local === null || $local === '' ) {
-			throw new InvalidArgumentException( '$local must not be empty' );
-		}
+// 		if ( $local === null || $local === '' ) {
+// 			throw new InvalidArgumentException( '$local must not be empty' );
+// 		}
 
-		$this->write( $base, ':', $local );
+		$this->write( "$base:$local" );
 	}
 
 
 	protected function writeText( $text, $language = null ) {
 		$value = $this->quoter->escapeLiteral( $text );
-		$this->write( '"', $value, '"' );
+		$this->write( '"' . $value . '"' );
 
 		if ( $language !== null ) {
-			$this->write( '@', $language );
+			$this->write( '@' . $language );
 		}
 	}
 
 	protected function writeValue( $value, $typeBase = null, $typeLocal = null ) {
 		$value = $this->quoter->escapeLiteral( $value );
-		$this->write( '"', $value, '"' );
+		$this->write( '"' . $value. '"' );
 
 		if ( $typeBase !== null ) {
 			$this->write( '^^' );
