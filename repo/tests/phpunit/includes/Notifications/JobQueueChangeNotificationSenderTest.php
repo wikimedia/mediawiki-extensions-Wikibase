bce1aa8aab4dbd365ba4e65c2063137bfc4e31e5
daniel <daniel.kinzler@wikimedia.de>
2015-03-21 00:19:18 +0100

    [WIP] Refactoring dispatchChanges.php
    Change-Id: If5ea19e29a1508341f64714ab577612c93418f1c
    TODO: tests

---
+++ repo/tests/phpunit/includes/Notifications/JobQueueChangeNotificationSenderTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Wikibase\Test;
+
+/**
+ * @covers Wikibase\Rep\Notifications\JobQueueChangeNotificationSender
+ *
+ * @group Wikibase
+ * @group WikibaseStore
+ * @group WikibaseChange
+ * @group WikibaseRepo
+ * @group Database
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ */
+class JobQueueChangeNotificationSenderTest extends \MediaWikiTestCase {
+
+	public function testSendNotification() {
+		$this->markTestIncomplete( 'Need a good way to test job queue interaction!' );
+	}
+
+}
---
+++ repo/tests/phpunit/includes/store/sql/SqlChangeDispatchCoordinatorTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Wikibase\Test;
+
+/**
+ * @covers Wikibase\Store\Sql\SqlChangeDispatchCoordinator
+ *
+ * @group Wikibase
+ * @group WikibaseStore
+ * @group WikibaseChange
+ * @group WikibaseRepo
+ * @group Database
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ */
+class SqlChangeDispatchCoordinatorTest extends \MediaWikiTestCase {
+
+	public function testSelectClient() {
+		$this->fail( 'test me' );
+	}
+
+	public function testInitState() {
+		$this->fail( 'test me' );
+	}
+
+	public function testLockClient() {
+		$this->fail( 'test me' );
+	}
+
+	public function testReleaseClient() {
+		$this->fail( 'test me' );
+	}
+
+}
