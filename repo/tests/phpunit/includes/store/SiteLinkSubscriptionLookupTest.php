9cd19aba95016849ffbf749b1062892613919ddd
daniel <daniel.kinzler@wikimedia.de>
2015-03-21 00:19:18 +0100

    [WIP] Refactoring dispatchChanges.php
    Change-Id: If5ea19e29a1508341f64714ab577612c93418f1c
    TODO: tests

---
+++ repo/tests/phpunit/includes/store/SiteLinkSubscriptionLookupTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Wikibase\Test;
+
+/**
+ * @covers Wikibase\Store\SiteLinkSubscriptionLookup
+ *
+ * @group Wikibase
+ * @group WikibaseStore
+ * @group WikibaseChange
+ * @group WikibaseRepo
+ * @group Database
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ */
+class SiteLinkSubscriptionLookupTest extends \MediaWikiTestCase {
+
+	public function testGetSubscriptions() {
+		$this->fail( 'test me' );
+	}
+
+}
