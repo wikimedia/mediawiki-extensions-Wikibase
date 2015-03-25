7885fb008c5badcd1f8465b5a0016fcb194fe206
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2015-03-25 10:18:14 +0100

    Split individual tests in UsageAccumulatorContractTester
    This doesn't change semantics, all it does is splitting the code in
    individual private methods. And I'm adding a contains check for each.
    
    Change-Id: Ib957f55231572b328c187d74c177d8388ec64fb8

--- client/tests/phpunit/includes/Usage/UsageAccumulatorContractTester.php
+++ client/tests/phpunit/includes/Usage/UsageAccumulatorContractTester.php
@@ -2,6 +2,7 @@
 
 namespace Wikibase\Client\Usage\Tests;
 
+use DataValues\StringValue;
 use PHPUnit_Framework_Assert as Assert;
 use Wikibase\Client\Usage\EntityUsage;
 use Wikibase\Client\Usage\UsageAccumulator;
@@ -11,7 +12,10 @@ use Wikibase\DataModel\Entity\PropertyId;
 use Wikibase\DataModel\Snak\PropertyValueSnak;
 
 /**
- * Contract tester for implementations of the UsageAccumulator interface
+ * Contract tester for implementations of the UsageAccumulator interface.
+ *
+ * @covers Wikibase\Client\Usage\UsageAccumulator
+ * @uses Wikibase\Client\Usage\EntityUsage
  *
  * @group Wikibase
  * @group WikibaseClient
@@ -32,33 +36,102 @@ class UsageAccumulatorContractTester  {
 	}
 
 	public function testAddGetUsage() {
+		$this->testAddAndGetLabelUsageForSnaks();
+		$this->testAddAndGetLabelUsage();
+		$this->testAddAndGetTitleUsage();
+		$this->testAddAndGetSiteLinksUsage();
+		$this->testAddAndGetOtherUsage();
+		$this->testAddAndGetAllUsage();
+
 		$q2 = new ItemId( 'Q2' );
 		$q3 = new ItemId( 'Q3' );
 		$q4 = new ItemId( 'Q4' );
-
-		// FIXME: Split into separate tests.
-		// FIXME: Why is OTHER_USAGE missing?
-		$this->usageAccumulator->addSiteLinksUsage( $q2 );
-		$this->usageAccumulator->addLabelUsage( $q2 );
-		$this->usageAccumulator->addTitleUsage( $q2 );
-		$this->usageAccumulator->addAllUsage( $q3 );
-
-		// Testing convenience methods
-		$this->usageAccumulator->addLabelUsageForSnaks( array(
-			new PropertyValueSnak( new PropertyId( 'P4' ), new EntityIdValue( $q4 ) ),
-		) );
-
-		$usage = $this->usageAccumulator->getUsages();
-
 		$expected = array(
-			new EntityUsage( $q2, EntityUsage::SITELINK_USAGE ),
+			new EntityUsage( $q4, EntityUsage::LABEL_USAGE ),
 			new EntityUsage( $q2, EntityUsage::LABEL_USAGE ),
 			new EntityUsage( $q2, EntityUsage::TITLE_USAGE ),
+			new EntityUsage( $q2, EntityUsage::SITELINK_USAGE ),
+			new EntityUsage( $q2, EntityUsage::OTHER_USAGE ),
 			new EntityUsage( $q3, EntityUsage::ALL_USAGE ),
-			new EntityUsage( $q4, EntityUsage::LABEL_USAGE ),
 		);
 
-		$this->assertSameUsages( $expected, $usage );
+		$usages = $this->usageAccumulator->getUsages();
+		$this->assertSameUsages( $expected, $usages );
+	}
+
+	private function testAddAndGetLabelUsageForSnaks() {
+		$q4 = new ItemId( 'Q4' );
+		$this->usageAccumulator->addLabelUsageForSnaks( array(
+			new PropertyValueSnak( new PropertyId( 'P4' ), new EntityIdValue( $q4 ) ),
+			new PropertyValueSnak( new PropertyId( 'P5' ), new StringValue( 'string' ) ),
+		) );
+
+		$expected = new EntityUsage( $q4, EntityUsage::LABEL_USAGE );
+
+		$usages = $this->usageAccumulator->getUsages();
+		$this->assertContainsUsage( $expected, $usages );
+	}
+
+	private function testAddAndGetLabelUsage() {
+		$q2 = new ItemId( 'Q2' );
+		$this->usageAccumulator->addLabelUsage( $q2 );
+
+		$expected = new EntityUsage( $q2, EntityUsage::LABEL_USAGE );
+
+		$usages = $this->usageAccumulator->getUsages();
+		$this->assertContainsUsage( $expected, $usages );
+	}
+
+	private function testAddAndGetTitleUsage() {
+		$q2 = new ItemId( 'Q2' );
+		$this->usageAccumulator->addTitleUsage( $q2 );
+
+		$expected = new EntityUsage( $q2, EntityUsage::TITLE_USAGE );
+
+		$entityUsages = $this->usageAccumulator->getUsages();
+		$this->assertContainsUsage( $expected, $entityUsages );
+	}
+
+	private function testAddAndGetSiteLinksUsage() {
+		$q2 = new ItemId( 'Q2' );
+		$this->usageAccumulator->addSiteLinksUsage( $q2 );
+
+		$expected = new EntityUsage( $q2, EntityUsage::SITELINK_USAGE );
+
+		$entityUsages = $this->usageAccumulator->getUsages();
+		$this->assertContainsUsage( $expected, $entityUsages );
+	}
+
+	private function testAddAndGetOtherUsage() {
+		$q2 = new ItemId( 'Q2' );
+		$this->usageAccumulator->addOtherUsage( $q2 );
+
+		$expected = new EntityUsage( $q2, EntityUsage::OTHER_USAGE );
+
+		$usages = $this->usageAccumulator->getUsages();
+		$this->assertContainsUsage( $expected, $usages );
+	}
+
+	private function testAddAndGetAllUsage() {
+		$q3 = new ItemId( 'Q3' );
+		$this->usageAccumulator->addAllUsage( $q3 );
+
+		$expected = new EntityUsage( $q3, EntityUsage::ALL_USAGE );
+
+		$usages = $this->usageAccumulator->getUsages();
+		$this->assertContainsUsage( $expected, $usages );
+	}
+
+	/**
+	 * @param EntityUsage $expected
+	 * @param EntityUsage[] $actual
+	 * @param string $message
+	 */
+	private function assertContainsUsage( EntityUsage $expected, array $actual, $message = '' ) {
+		$expected = $expected->getIdentityString();
+		$actual = $this->getIdentityStrings( $actual );
+
+		Assert::assertContains( $expected, $actual, $message );
 	}
 
 	/**
@@ -67,8 +140,8 @@ class UsageAccumulatorContractTester  {
 	 * @param string $message
 	 */
 	private function assertSameUsages( array $expected, array $actual, $message = '' ) {
-		$expected = $this->getUsageStrings( $expected );
-		$actual = $this->getUsageStrings( $actual );
+		$expected = $this->getIdentityStrings( $expected );
+		$actual = $this->getIdentityStrings( $actual );
 
 		Assert::assertEquals( $expected, $actual, $message );
 	}
@@ -78,7 +151,7 @@ class UsageAccumulatorContractTester  {
 	 *
 	 * @return string[]
 	 */
-	private function getUsageStrings( array $usages ) {
+	private function getIdentityStrings( array $usages ) {
 		return array_values(
 			array_map( function( EntityUsage $usage ) {
 				return $usage->getIdentityString();
