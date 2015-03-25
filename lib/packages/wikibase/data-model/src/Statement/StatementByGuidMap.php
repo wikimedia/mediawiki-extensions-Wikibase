6a4278e2051a1f4d31d51ed26809ee741f332d7e
jeroendedauw <jeroendedauw@gmail.com>
2015-03-25 19:11:21 +0100

    Added minimal StatementByGuidMap
    Pair programmed with Kai
    
    Tackles https://github.com/wmde/WikibaseDataModel/issues/304

---
+++ lib/packages/wikibase/data-model/src/Statement/StatementByGuidMap.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Wikibase\DataModel\Statement;
+
+use InvalidArgumentException;
+
+/**
+ * Ordered and unique collection of Statement objects.
+ * Provides indexed access by Statement GUID. Can only contain Statements that have a non-null GUID.
+ *
+ * @since 3.0
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ * @author Kai Nissen < kai.nissen@wikimedia.de >
+ */
+class StatementByGuidMap {
+
+	private $statements = array();
+
+	/**
+	 * @param Statement[] $statements
+	 */
+	public function __construct( array $statements = array() ) {
+		foreach ( $statements as $statement ) {
+			$this->addStatement( $statement );
+		}
+	}
+
+	private function addStatement( Statement $statement ) {
+		if ( $statement->getGuid() === null ) {
+			throw new InvalidArgumentException( 'Can only add statements that have a non-null GUID' );
+		}
+
+		$this->statements[$statement->getGuid()] = $statement;
+	}
+
+	/**
+	 * @param string $statementGuid
+	 *
+	 * @return bool
+	 * @throws InvalidArgumentException
+	 */
+	public function hasStatementWithGuid( $statementGuid ) {
+		$this->assertIsStatementGuid( $statementGuid );
+
+		return array_key_exists( $statementGuid, $this->statements );
+	}
+
+	private function assertIsStatementGuid( $statementGuid ) {
+		if ( !is_string( $statementGuid ) ) {
+			throw new InvalidArgumentException( '$statementGuid needs to be a string' );
+		}
+	}
+
+	/**
+	 * @param string $statementGuid
+	 *
+	 * @return Statement|null
+	 */
+	public function getStatementByGuid( $statementGuid ) {
+		$this->assertIsStatementGuid( $statementGuid );
+
+		if ( array_key_exists( $statementGuid, $this->statements ) ) {
+			return $this->statements[$statementGuid];
+		}
+
+		return null;
+	}
+
+	/**
+	 * Removes the statement with the specified GUID if it exists.
+	 *
+	 * @param string $statementGuid
+	 */
+	public function removeStatementWithGuid( $statementGuid ) {
+		$this->assertIsStatementGuid( $statementGuid );
+		unset( $this->statements[$statementGuid] );
+	}
+
+}
---
+++ lib/packages/wikibase/data-model/tests/unit/Statement/StatementByGuidMapTest.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Wikibase\DataModel\Tests\Statement;
+
+use Wikibase\DataModel\Snak\PropertyNoValueSnak;
+use Wikibase\DataModel\Statement\Statement;
+use Wikibase\DataModel\Statement\StatementByGuidMap;
+
+/**
+ * @covers Wikibase\DataModel\Statement\StatementByGuidMap
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ * @author Kai Nissen < kai.nissen@wikimedia.de >
+ */
+class StatementByGuidMapTest extends \PHPUnit_Framework_TestCase {
+
+	public function testGivenNotPresentGuid_hasClaimWithGuidReturnsFalse() {
+		$statements = new StatementByGuidMap();
+
+		$this->assertFalse( $statements->hasStatementWithGuid( 'some guid' ) );
+	}
+
+	public function testGivenPresentGuid_hasStatementWithGuidReturnsTrue() {
+
+		$statements = new StatementByGuidMap( array(
+			$this->newStatement( 1, 'some guid' )
+		) );
+
+		$this->assertTrue( $statements->hasStatementWithGuid( 'some guid' ) );
+	}
+
+	private function newStatement( $propertyId, $guid ) {
+		$statement = new Statement( new PropertyNoValueSnak( $propertyId ) );
+		$statement->setGuid( $guid );
+		return $statement;
+	}
+
+	/**
+	 * @dataProvider nonStringProvider
+	 */
+	public function testGivenNonStringGuid_hasClaimWithGuidThrowsException( $nonString ) {
+		$statements = new StatementByGuidMap();
+
+		$this->setExpectedException( 'InvalidArgumentException' );
+		$statements->hasStatementWithGuid( $nonString );
+	}
+
+	public function nonStringProvider() {
+		return array(
+			array( null ),
+			array( 42 ),
+			array( 4.2 ),
+			array( array() ),
+			array( (object)array() ),
+		);
+	}
+
+	public function testGivenGuidOfPresentStatement_getStatementByGuidReturnsStatement() {
+		$statement = $this->newStatement( 1, 'some guid' );
+
+		$statements = new StatementByGuidMap( array( $statement ) );
+
+		$this->assertEquals( $statement, $statements->getStatementByGuid( 'some guid' ) );
+	}
+
+	public function testGivenGuidOfNotPresentStatement_getStatementByGuidReturnsNull() {
+		$statements = new StatementByGuidMap();
+
+		$this->assertNull( $statements->getStatementByGuid( 'some guid' ) );
+	}
+
+	/**
+	 * @dataProvider nonStringProvider
+	 */
+	public function testGivenNonStringGuid_getStatementByGuidThrowsException( $nonString ) {
+		$statements = new StatementByGuidMap();
+
+		$this->setExpectedException( 'InvalidArgumentException' );
+		$statements->getStatementByGuid( $nonString );
+	}
+
+	public function testGivenGuidOfPresentStatement_removeStatementWithGuidRemovesTheStatement() {
+		$statement = $this->newStatement( 1, 'some guid' );
+		$statements = new StatementByGuidMap( array( $statement ) );
+
+		$statements->removeStatementWithGuid( 'some guid' );
+
+		$this->assertFalse( $statements->hasStatementWithGuid( 'some guid' ) );
+	}
+
+	public function testGivenGuidOfNonPresentStatement_removeStatementWithGuidDoesNoOp() {
+		$statement = $this->newStatement( 1, 'some guid' );
+		$statements = new StatementByGuidMap( array( $statement ) );
+
+		$statements->removeStatementWithGuid( '-- different guid --' );
+
+		$this->assertTrue( $statements->hasStatementWithGuid( 'some guid' ) );
+	}
+
+	/**
+	 * @dataProvider nonStringProvider
+	 */
+	public function testGivenNonStringGuid_removeStatementWithGuidThrowsException( $nonString ) {
+		$statements = new StatementByGuidMap();
+
+		$this->setExpectedException( 'InvalidArgumentException' );
+		$statements->removeStatementWithGuid( $nonString );
+	}
+
+	public function testGivenStatementWithNoGuid_constructorThrowsException() {
+		$this->setExpectedException( 'InvalidArgumentException' );
+
+		new StatementByGuidMap( array(
+			$this->newStatement( 1, null )
+		) );
+	}
+
+}
