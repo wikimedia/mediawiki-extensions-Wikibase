9e8bd50b193a9a832e33d71680570febd90e15b6
daniel <daniel.kinzler@wikimedia.de>
2015-03-30 01:07:44 +0200

    Split RdfBuilder
    This refactors RdfBuilder to place the generation of
    different aspects of the RDF (such as terms, sitelinks,
    statements, and values) in separate classes.
    
    Note that most of the lines added are in test code and data.
    
    Bug: T92515
    Change-Id: Ib158eca0ec5669fedf12c948fab20056453d0c5b

---
+++ repo/tests/phpunit/includes/rdf/FullStatementsRdfBuilderTest.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace Wikibase\Test\Rdf;
+
+use Wikibase\Rdf\ComplexValueRdfBuilder;
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\Rdf\Test\RdfBuilderTestData;
+use Wikibase\RdfProducer;
+use Wikibase\Rdf\SimpleValueRdfBuilder;
+use Wikibase\Rdf\FullStatementRdfBuilder;
+
+/**
+ * @covers Wikibase\FullStatementRdfBuilder
+ *
+ * @group Wikibase
+ * @group WikibaseRepo
+ * @group WikibaseRdf
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ * @author Stas Malyshev
+ */
+class FullStatementRdfBuilderTest extends \PHPUnit_Framework_TestCase {
+
+	private $testData;
+
+	/**
+	 * Initialize repository data
+	 *
+	 * @return RdfBuilderTestData
+	 */
+	private function getTestData()
+	{
+		if( empty( $this->testData ) ) {
+			$this->testData = new RdfBuilderTestData(
+				__DIR__ . "/../../data/rdf",
+				__DIR__ . "/../../data/rdf/FullStatementRdfBuilder"
+			);
+		}
+		return $this->testData;
+	}
+
+	/**
+	 * @param int $flavor Bitmap for the output flavor, use RdfProducer::PRODUCE_XXX constants.
+	 * @param EntityId[] &$mentioned Receives any entity IDs being mentioned.
+	 * @param string[] $referencesSeen A list of reference hashes that should be considered "already seen".
+	 *
+	 * @return FullStatementRdfBuilder
+	 */
+	private function newBuilder( $flavor, array &$mentioned = array(), array $referencesSeen = array() ) {
+		$vocabulary = $this->getTestData()->getVocabulary();
+
+		$writer = $this->getTestData()->getNTriplesWriter();
+
+		$entityMentioned = function( EntityId $id ) use ( &$mentioned ) {
+			$key = $id->getSerialization();
+			$mentioned[$key] = $id;
+		};
+
+		if ( $flavor & RdfProducer::PRODUCE_FULL_VALUES ) {
+			$valueWriter = $writer->sub();
+
+			$statementValueBuilder = new ComplexValueRdfBuilder( $vocabulary, $valueWriter, $this->getTestData()->getMockRepository() );
+		} else {
+			$statementValueBuilder = new SimpleValueRdfBuilder( $vocabulary, $this->getTestData()->getMockRepository() );
+		}
+
+		$referenceSeenCallback = function( $hash ) use ( $referencesSeen ) {
+			return in_array( $hash, $referencesSeen );
+		};
+
+		$statementBuilder = new FullStatementRdfBuilder( $vocabulary, $writer, $statementValueBuilder );
+		$statementBuilder->setReferenceSeenCallback( $referenceSeenCallback );
+
+		if ( $flavor & RdfProducer::PRODUCE_PROPERTIES  ) {
+			$statementBuilder->setPropertyMentionCallback( $entityMentioned );
+		}
+
+		$statementBuilder->setProduceQualifiers( $flavor & RdfProducer::PRODUCE_QUALIFIERS );
+		$statementBuilder->setProduceReferences( $flavor & RdfProducer::PRODUCE_REFERENCES );
+
+		// HACK: stick the writer into a public field, for use by getDataFromBuilder()
+		$statementBuilder->test_writer = $writer;
+
+		return $statementBuilder;
+	}
+
+	/**
+	 * Extract text test data from RDF builder
+	 * @param FullStatementRdfBuilder $builder
+	 * @return string[] ntriples lines, sorted
+	 */
+	private function getDataFromBuilder( FullStatementRdfBuilder $builder ) {
+		// HACK: $builder->test_writer is glued on by newBuilder().
+		$ntriples = $builder->test_writer->drain();
+
+		$lines = explode( "\n", trim( $ntriples ) );
+		sort( $lines );
+		return $lines;
+	}
+
+	private function assertOrCreateNTriples( $dataSetName, FullStatementRdfBuilder $builder ) {
+		$actualData = $this->getDataFromBuilder( $builder );
+		$correctData = $this->getTestData()->getNTriples( $dataSetName );
+
+		if ( $correctData === null ) {
+			$this->getTestData()->putTestData( $dataSetName, $actualData, '.actual' );
+			$this->fail( 'Data set `' . $dataSetName . '` not found! Created file with the current data using the suffix .actual' );
+		}
+
+		$this->assertEquals( $correctData, $actualData, "Data set $dataSetName" );
+	}
+
+	public function provideAddEntity() {
+		$props = array( 'P2', 'P3', 'P4', 'P5', 'P6', 'P7', 'P8', 'P9' );
+
+		return array(
+			array( 'Q4', 0, 'Q4_minimal', array() ),
+			array( 'Q4', RdfProducer::PRODUCE_ALL, 'Q4_all', $props ),
+			array( 'Q4', RdfProducer::PRODUCE_ALL_STATEMENTS, 'Q4_statements', array() ),
+			array( 'Q6', RdfProducer::PRODUCE_ALL_STATEMENTS, 'Q6_no_qualifiers', array() ),
+			array( 'Q6', RdfProducer::PRODUCE_ALL_STATEMENTS | RdfProducer::PRODUCE_QUALIFIERS, 'Q6_with_qualifiers', array() ),
+			array( 'Q7', RdfProducer::PRODUCE_ALL_STATEMENTS , 'Q7_no_refs', array() ),
+			array( 'Q7', RdfProducer::PRODUCE_ALL_STATEMENTS | RdfProducer::PRODUCE_REFERENCES, 'Q7_refs', array() ),
+			array( 'Q4', RdfProducer::PRODUCE_ALL_STATEMENTS | RdfProducer::PRODUCE_PROPERTIES, 'Q4_minimal', $props ),
+			array( 'Q4', RdfProducer::PRODUCE_ALL_STATEMENTS | RdfProducer::PRODUCE_FULL_VALUES, 'Q4_values', array() ),
+		);
+	}
+
+	/**
+	 * @dataProvider provideAddEntity
+	 */
+	public function testAddEntity( $entityName, $flavor, $dataSetName, array $expectedMentions ) {
+		$entity = $this->getTestData()->getEntity( $entityName );
+
+		$mentioned = array();
+		$builder = $this->newBuilder( $flavor, $mentioned );
+		$builder->addEntity( $entity );
+
+		$this->assertOrCreateNTriples( $dataSetName, $builder );
+		$this->assertEquals( $expectedMentions, array_keys( $mentioned ), 'Entities mentioned' );
+	}
+
+	public function provideAddEntity_seen() {
+		return array(
+			array( 'Q7', 'Q7_all_refs_seen', array( '569a639de6dc67beb02644abfcf55534cb2f51ce' ) ),
+		);
+	}
+
+	/**
+	 * @dataProvider provideAddEntity_seen
+	 */
+	public function testAddEntity_seen( $entityName, $dataSetName, array $referencesSeen ) {
+		$entity = $this->getTestData()->getEntity( $entityName );
+
+		$mentioned = array();
+		$builder = $this->newBuilder( RdfProducer::PRODUCE_ALL, $mentioned, $referencesSeen );
+		$builder->addEntity( $entity );
+
+		$this->assertOrCreateNTriples( $dataSetName, $builder );
+	}
+
+	public function provideAddStatements() {
+		return array(
+			array( 'Q4', 'Q4_all' ),
+		);
+	}
+
+	/**
+	 * @dataProvider provideAddStatements
+	 */
+	public function testAddStatements( $entityName, $dataSetName ) {
+		$entity = $this->getTestData()->getEntity( $entityName );
+
+		$builder = $this->newBuilder( RdfProducer::PRODUCE_ALL );
+		$builder->addStatements( $entity->getId(), $entity->getStatements() );
+
+		$this->assertOrCreateNTriples( $dataSetName, $builder );
+	}
+
+}
---
+++ repo/tests/phpunit/includes/rdf/RdfBuilderTestData.php
@@ -0,0 +1,214 @@
+<?php
+
+namespace Wikibase\Rdf\Test;
+
+use SiteList;
+use Wikibase\DataModel\Entity\Entity;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Entity\Property;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\DataModel\Term\Fingerprint;
+use Wikibase\Rdf\RdfVocabulary;
+use Wikimedia\Purtle\NTriplesRdfWriter;
+use Wikibase\Repo\WikibaseRepo;
+use Wikibase\Test\MockRepository;
+
+/**
+ * Helper class for accessing data files for RdfBuilder related tests.
+ *
+ * @group Wikibase
+ * @group WikibaseRepo
+ * @group WikibaseRdf
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ * @author Stas Malyshev
+ */
+class RdfBuilderTestData {
+
+	const URI_BASE = 'http://acme.test/';
+	const URI_DATA = 'http://data.acme.test/';
+
+	private $codec;
+	private $dataDir;
+	private $entityDir;
+
+	/**
+	 * @param string $entityDir directory containing entity data (JSON files)
+	 * @param string $dataDir directory containing RDF data (n-triples files)
+	 */
+	public function __construct( $entityDir, $dataDir ) {
+		$this->entityDir = $entityDir;
+		$this->dataDir = $dataDir;
+	}
+
+	/**
+	 * Initialize repository data
+	 */
+	private function getCodec()
+	{
+		if( empty($this->codec) ) {
+			$wikibaseRepo = WikibaseRepo::getDefaultInstance();
+			$wikibaseRepo->getSettings()->setSetting( 'internalEntitySerializerClass', null );
+			$wikibaseRepo->getSettings()->setSetting( 'useRedirectTargetColumn', true );
+			$this->codec = $wikibaseRepo->getEntityContentDataCodec();
+		}
+		return $this->codec;
+	}
+
+	/**
+	 * Load entity from JSON
+	 * @param string $entityName
+	 * @return Entity
+	 */
+	public function getEntity( $entityName )
+	{
+		return $this->getCodec()->decodeEntity(
+			file_get_contents( "{$this->entityDir}/$entityName.json" ), CONTENT_FORMAT_JSON );
+	}
+
+	/**
+	 * Load serialized ntriples.
+	 *
+	 * @param string $dataSetName
+	 * @return string[]|null ntriples lines, sorted, or null if
+	 *         no data file was found with the given name.
+	 */
+	public function getNTriples( $dataSetName )
+	{
+		$filename = "{$this->dataDir}/$dataSetName.nt";
+		if ( !file_exists( $filename ) )
+		{
+			return null;
+		}
+		$data = trim( file_get_contents( $filename ) );
+		$data = explode( "\n", $data );
+		sort( $data );
+		return $data;
+	}
+
+	/**
+	 * Writes the given ntriples lines to the test data file with the given name.
+	 * Existing files will not be overwritten.
+	 *
+	 * @note This is intended as a helper function for building test cases, it should
+	 *       not be used while testing.
+	 *
+	 * @param string $dataSetName
+	 * @param string[] $lines
+	 * @param string $suffix File name suffix
+	 *
+	 * @return bool|int the number of bytes that were written to the file, or
+	 *         false on failure.
+	 */
+	public function putTestData( $dataSetName, $lines, $suffix = '' ) {
+		$filename = "{$this->dataDir}/$dataSetName.nt$suffix";
+		if ( file_exists( $filename ) )
+		{
+			return false;
+		}
+
+		$data = join( "\n", (array)$lines );
+		return file_put_contents( $filename, $data );
+	}
+
+	/**
+	 * Returns the vocabulary to use with the test data.
+	 *
+	 * @return RdfVocabulary
+	 */
+	public function getVocabulary() {
+		return new RdfVocabulary( self::URI_BASE, self::URI_DATA );
+	}
+
+	/**
+	 * Returns a new NTriplesRdfWriter, with vocabulary namespaces registered.
+	 *
+	 * @param bool $start whether to call start() on the writer.
+	 *
+	 * @return NTriplesRdfWriter
+	 */
+	public function getNTriplesWriter( $start = true ) {
+		$writer = new NTriplesRdfWriter();
+
+		foreach ( $this->getVocabulary()->getNamespaces() as $ns => $uri ) {
+			$writer->prefix( $ns, $uri );
+		}
+
+		if ( $start ) {
+			$writer->start();
+		}
+
+		return $writer;
+	}
+
+	/**
+	 * Get site definitions matching the test data.
+	 *
+	 * @return \SiteList
+	 */
+	public function getSiteList() {
+		$list = new SiteList();
+
+		$wiki = new \Site();
+		$wiki->setGlobalId( 'enwiki' );
+		$wiki->setLanguageCode( 'en' );
+		$wiki->setLinkPath( 'http://enwiki.acme.test/$1' );
+		$list['enwiki'] = $wiki;
+
+		$wiki = new \Site();
+		$wiki->setGlobalId( 'ruwiki' );
+		$wiki->setLanguageCode( 'ru' );
+		$wiki->setLinkPath( 'http://ruwiki.acme.test/$1' );
+		$list['ruwiki'] = $wiki;
+
+		return $list;
+	}
+
+	/**
+	 * Define a set of fake properties
+	 * @return array
+	 */
+	private static function getTestProperties() {
+		return array(
+			array(2, 'wikibase-entityid'),
+			array(3, 'commonsMedia'),
+			array(4, 'globecoordinate'),
+			array(5, 'monolingualtext'),
+			array(6, 'quantity'),
+			array(7, 'string'),
+			array(8, 'time'),
+			array(9, 'url'),
+		);
+	}
+
+	/**
+	 * Construct mock repository matching the test data.
+	 *
+	 * @return MockRepository
+	 */
+	public function getMockRepository() {
+		static $repo;
+
+		if ( !empty($repo) ) {
+			return $repo;
+		}
+
+		$repo = new MockRepository();
+
+		foreach( self::getTestProperties() as $prop ) {
+			list($id, $type) = $prop;
+			$fingerprint = Fingerprint::newEmpty();
+			$fingerprint->setLabel( 'en', "Property$id" );
+			$entity = new Property( PropertyId::newFromNumber($id), $fingerprint, $type );
+			$repo->putEntity( $entity );
+		}
+		$fingerprint = Fingerprint::newEmpty();
+		$fingerprint->setLabel( 'en', "Item42" );
+		$entity = new Item( ItemId::newFromNumber(42), $fingerprint );
+		$repo->putEntity( $entity );
+		return $repo;
+	}
+
+}
