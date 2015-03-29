8b274b6582764bcf5e3beb800c3457f8158ffbf1
daniel <daniel.kinzler@wikimedia.de>
2015-03-30 01:07:44 +0200

    Split RdfBuilder
    This refactors RdfBuilder to place the generation of
    different aspects of the RDF (such as terms, sitelinks,
    statements, and values) in separate classes.
    
    Note that most of the lines added are in test code and data.
    
    Change-Id: Ib158eca0ec5669fedf12c948fab20056453d0c5b

---
+++ repo/includes/rdf/TruthyStatementRdfBuilder.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Wikibase;
+
+use Wikibase\DataModel\Entity\EntityDocument;
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\DataModel\Snak\PropertyValueSnak;
+use Wikibase\DataModel\Statement\Statement;
+use Wikibase\DataModel\Statement\StatementList;
+use Wikibase\DataModel\StatementListProvider;
+use Wikibase\RDF\RdfWriter;
+
+/**
+ * "Truthy" RDF mapping for wikibase statements.
+ *
+ * @since 0.5
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ * @author Stas Malyshev
+ */
+class TruthyStatementRdfBuilder implements EntityRdfBuilder {
+
+	/**
+	 * @var RdfVocabulary
+	 */
+	private $vocabulary;
+
+	/**
+	 * @var RdfWriter
+	 */
+	private $writer;
+
+	/**
+	 * @var SnakValueRdfBuilder
+	 */
+	private $valueBuilder;
+
+	/**
+	 * @param RdfVocabulary $vocabulary
+	 * @param RdfWriter $writer
+	 * @param SnakValueRdfBuilder $valueBuilder
+	 */
+	public function __construct( RdfVocabulary $vocabulary, RdfWriter $writer, SnakValueRdfBuilder $valueBuilder ) {
+		$this->vocabulary = $vocabulary;
+		$this->writer = $writer;
+		$this->valueBuilder = $valueBuilder;
+	}
+
+	/**
+	 * Adds Statements to the RDF graph.
+	 *
+	 * @param EntityId $entityId
+	 * @param StatementList $statementList
+	 */
+	public function addStatements( EntityId $entityId, StatementList $statementList ) {
+		foreach ( $statementList->getBestStatementPerProperty() as $statement ) {
+			$this->addMainSnak( $entityId, $statement, true );
+		}
+	}
+
+	/**
+	 * Adds the given Statement's main Snak to the RDF graph.
+	 *
+	 * @param EntityId $entityId
+	 * @param Statement $statement
+	 */
+	private function addMainSnak( EntityId $entityId, Statement $statement ) {
+		$snak = $statement->getMainSnak();
+
+		$entityLName = $this->vocabulary->getEntityLName( $entityId );
+
+		$this->writer->about( RdfVocabulary::NS_ENTITY, $entityLName );
+
+		$propertyId = $snak->getPropertyId();
+		switch ( $snak->getType() ) {
+			case 'value':
+				/** @var PropertyValueSnak $snak */
+				$this->valueBuilder->addSnakValue( $this->writer, $propertyId, $snak->getDataValue(), RdfVocabulary::NS_DIRECT_CLAIM );
+				break;
+			case 'somevalue':
+				$propertyValueLName = $this->vocabulary->getEntityLName( $propertyId );
+
+				$this->writer->say( RdfVocabulary::NS_DIRECT_CLAIM, $propertyValueLName )->is( RdfVocabulary::NS_ONTOLOGY, 'Somevalue' );
+				break;
+			case 'novalue':
+				$propertyValueLName = $this->vocabulary->getEntityLName( $propertyId );
+
+				$this->writer->say( RdfVocabulary::NS_DIRECT_CLAIM, $propertyValueLName )->is( RdfVocabulary::NS_ONTOLOGY, 'Novalue' );
+				break;
+			default:
+				throw new \InvalidArgumentException( 'Unknown snak type: ' . $snak->getType() );
+		}
+	}
+
+	/**
+	 * Add truthy statements for the given entity to the RDF graph.
+	 *
+	 * @param EntityDocument $entity the entity to output.
+	 */
+	public function addEntity( EntityDocument $entity ) {
+		$entityId = $entity->getId();
+
+		if ( $entity instanceof StatementListProvider ) {
+			$this->addStatements( $entityId, $entity->getStatements() );
+		}
+	}
+
+}
