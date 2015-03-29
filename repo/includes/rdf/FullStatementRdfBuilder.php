da232e986a9436f28220698d2a6a23ce058081ce
daniel <daniel.kinzler@wikimedia.de>
2015-03-30 01:07:44 +0200

    Split RdfBuilder
    This refactors RdfBuilder to place the generation of
    different aspects of the RDF (such as terms, sitelinks,
    statements, and values) in separate classes.
    
    Note that most of the lines added are in test code and data.
    
    Bug: T92515
    Change-Id: Ib158eca0ec5669fedf12c948fab20056453d0c5b

---
+++ repo/includes/rdf/FullStatementRdfBuilder.php
@@ -0,0 +1,307 @@
+<?php
+
+namespace Wikibase\Rdf;
+
+use Wikibase\DataModel\Entity\EntityDocument;
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\DataModel\Snak\PropertyValueSnak;
+use Wikibase\DataModel\Snak\Snak;
+use Wikibase\DataModel\Statement\Statement;
+use Wikibase\DataModel\Statement\StatementList;
+use Wikibase\DataModel\StatementListProvider;
+use Wikimedia\Purtle\RdfWriter;
+
+/**
+ * Fully reified RDF mapping for wikibase statements.
+ * This does not output simple statements. If both forms (simple and full) are desired,
+ * use SimpleStatementRdfBuilder in addition to FullStatementRdfBuilder.
+ *
+ * @since 0.5
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ * @author Stas Malyshev
+ */
+class FullStatementRdfBuilder implements EntityRdfBuilder {
+
+	/**
+	 * @var callable
+	 */
+	private $propertyMentionCallback = null;
+
+	/**
+	 * @var callable
+	 */
+	private $referenceSeenCallback = null;
+
+	/**
+	 * @var bool
+	 */
+	private $produceQualifiers = true;
+
+	/**
+	 * @var bool
+	 */
+	private $produceReferences = true;
+
+	/**
+	 * @var RdfVocabulary
+	 */
+	private $vocabulary;
+
+	/**
+	 * @var RdfWriter
+	 */
+	private $statementWriter;
+
+	/**
+	 * @var RdfWriter
+	 */
+	private $referenceWriter;
+
+	/**
+	 * @var SnakValueRdfBuilder
+	 */
+	private $valueBuilder;
+
+	/**
+	 * @param RdfVocabulary $vocabulary
+	 * @param RdfWriter $writer
+	 * @param SnakValueRdfBuilder $valueBuilder
+	 */
+	public function __construct( RdfVocabulary $vocabulary, RdfWriter $writer, SnakValueRdfBuilder $valueBuilder ) {
+		$this->vocabulary = $vocabulary;
+
+		// Note: since we process references as nested structures, they need a separate
+		// rdf writer, so outputting references doesn't destroy the state of the statement writer.
+		$this->statementWriter = $writer;
+		$this->referenceWriter = $writer->sub();
+
+		$this->valueBuilder = $valueBuilder;
+	}
+
+	/**
+	 * @return callable
+	 */
+	public function getPropertyMentionCallback() {
+		return $this->propertyMentionCallback;
+	}
+
+	/**
+	 * @param callable $propertyMentionCallback
+	 */
+	public function setPropertyMentionCallback( $propertyMentionCallback ) {
+		$this->propertyMentionCallback = $propertyMentionCallback;
+	}
+
+	/**
+	 * @return callable
+	 */
+	public function getReferenceSeenCallback() {
+		return $this->referenceSeenCallback;
+	}
+
+	/**
+	 * @param callable $referenceSeenCallback
+	 */
+	public function setReferenceSeenCallback( $referenceSeenCallback ) {
+		$this->referenceSeenCallback = $referenceSeenCallback;
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function getProduceQualifiers() {
+		return $this->produceQualifiers;
+	}
+
+	/**
+	 * @param boolean $produceQualifiers
+	 */
+	public function setProduceQualifiers( $produceQualifiers ) {
+		$this->produceQualifiers = $produceQualifiers;
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function getProduceReferences() {
+		return $this->produceReferences;
+	}
+
+	/**
+	 * @param boolean $produceReferences
+	 */
+	public function setProduceReferences( $produceReferences ) {
+		$this->produceReferences = $produceReferences;
+	}
+
+	/**
+	 * Adds Statements to the RDF graph.
+	 *
+	 * @param EntityId $entityId
+	 * @param StatementList $statementList
+	 */
+	public function addStatements( EntityId $entityId, StatementList $statementList ) {
+		$bestList = array();
+
+		// FIXME: getBestStatementPerProperty() is expensive, share the result with TruthyStatementRdfBuilder!
+		foreach ( $statementList->getBestStatementPerProperty() as $statement ) {
+			$bestList[$statement->getGuid()] = true;
+		}
+
+		foreach ( $statementList as $statement ) {
+			$this->addStatement( $entityId, $statement, isset( $bestList[$statement->getGuid()] ) );
+		}
+	}
+
+	/**
+	 * Adds the given Statement from the given Entity to the RDF graph.
+	 *
+	 * @param EntityId $entityId
+	 * @param Statement $statement
+	 * @param bool $isBest Is this best ranked statement?
+	 */
+	private function addStatement( EntityId $entityId, Statement $statement, $isBest ) {
+		$statementLName = $this->vocabulary->getStatementLName( $statement );
+		$this->statementWriter->about( RdfVocabulary::NS_STATEMENT, $statementLName )
+			->a( RdfVocabulary::NS_ONTOLOGY, 'Statement' );
+
+		$this->addMainSnak( $entityId, $statementLName, $statement, $isBest );
+
+		// XXX: separate builder for qualifiers?
+		if ( $this->produceQualifiers ) {
+			// this assumes statement was added by addMainSnak
+			foreach ( $statement->getQualifiers() as $q ) {
+				$this->addSnak( $this->statementWriter, $q, RdfVocabulary::NS_QUALIFIER );
+			}
+		}
+
+		// XXX: separate builder for references?
+		if ( $this->produceReferences ) {
+			foreach ( $statement->getReferences() as $reference ) { //FIXME: split body into separate method
+				$hash = $reference->getSnaks()->getHash();
+				$refLName = $hash;
+
+				$this->statementWriter->about( RdfVocabulary::NS_STATEMENT, $statementLName )
+					->say( RdfVocabulary::NS_PROV, 'wasDerivedFrom' )->is( RdfVocabulary::NS_REFERENCE, $refLName );
+				if ( $this->referenceSeen( $hash ) !== false ) {
+					continue;
+				}
+
+				$this->referenceWriter->about( RdfVocabulary::NS_REFERENCE, $refLName )
+					->a( RdfVocabulary::NS_ONTOLOGY, 'Reference' );
+
+				foreach ( $reference->getSnaks() as $refSnak ) {
+					$this->addSnak( $this->referenceWriter, $refSnak, RdfVocabulary::NS_VALUE );
+				}
+			}
+		}
+	}
+
+	/**
+	 * Adds the given Statement's main Snak to the RDF graph.
+	 *
+	 * @param EntityId $entityId
+	 * @param string $statementLName
+	 * @param Statement $statement
+	 * @param bool $isBest Is this best ranked statement?
+	 */
+	private function addMainSnak( EntityId $entityId, $statementLName, Statement $statement, $isBest ) {
+		$snak = $statement->getMainSnak();
+
+		$entityLName = $this->vocabulary->getEntityLName( $entityId );
+		$propertyLName = $this->vocabulary->getEntityLName( $snak->getPropertyId() );
+
+		$this->statementWriter->about( RdfVocabulary::NS_ENTITY,  $entityLName )
+			->say( RdfVocabulary::NS_ENTITY, $propertyLName )->is( RdfVocabulary::NS_STATEMENT, $statementLName );
+
+		$this->statementWriter->about( RdfVocabulary::NS_STATEMENT, $statementLName );
+		$this->addSnak( $this->statementWriter, $snak, RdfVocabulary::NS_VALUE );
+
+		$this->propertyMentioned( $snak->getPropertyId() );
+
+		$rank = $statement->getRank();
+		if ( isset( RdfVocabulary::$rankMap[$rank] ) ) {
+			$this->statementWriter->about( RdfVocabulary::NS_STATEMENT, $statementLName )
+				->say( RdfVocabulary::NS_ONTOLOGY, 'rank' )->is( RdfVocabulary::NS_ONTOLOGY, RdfVocabulary::$rankMap[$rank] );
+			if( $isBest ) {
+				$this->statementWriter->say( RdfVocabulary::NS_ONTOLOGY, 'rank' )->is( RdfVocabulary::NS_ONTOLOGY, RdfVocabulary::WIKIBASE_RANK_BEST );
+			}
+		} else {
+			wfLogWarning( "Unknown rank $rank encountered for $entityId:{$statement->getGuid()}" );
+		}
+
+	}
+
+	/**
+	 * Adds the given Statement's main Snak to the RDF graph.
+	 *
+	 * @todo share more of this code with TruthyStatementRdfBuilder
+	 *
+	 * @param RdfWriter $writer
+	 * @param Snak $snak
+	 * @param $propertyNamespace
+	 */
+	private function addSnak( RdfWriter $writer, Snak $snak, $propertyNamespace ) {
+
+		$propertyId = $snak->getPropertyId();
+		switch ( $snak->getType() ) {
+			case 'value':
+				/** @var PropertyValueSnak $snak */
+				$this->valueBuilder->addSnakValue( $writer, $propertyId, $snak->getDataValue(), $propertyNamespace );
+				break;
+			case 'somevalue':
+				$propertyValueLName = $this->vocabulary->getEntityLName( $propertyId );
+
+				$writer->say( $propertyNamespace, $propertyValueLName )->is( RdfVocabulary::NS_ONTOLOGY, 'Somevalue' );
+				break;
+			case 'novalue':
+				$propertyValueLName = $this->vocabulary->getEntityLName( $propertyId );
+
+				$writer->say( $propertyNamespace, $propertyValueLName )->is( RdfVocabulary::NS_ONTOLOGY, 'Novalue' );
+				break;
+			default:
+				throw new \InvalidArgumentException( 'Unknown snak type: ' . $snak->getType() );
+		}
+	}
+
+	/**
+	 * @param EntityId $propertyId
+	 */
+	private function propertyMentioned( EntityId $propertyId ) {
+		if ( $this->propertyMentionCallback ) {
+			call_user_func( $this->propertyMentionCallback, $propertyId );
+		}
+	}
+
+	/**
+	 * @param string $hash
+	 *
+	 * @return bool
+	 */
+	private function referenceSeen( $hash ) {
+		if ( $this->referenceSeenCallback ) {
+			if ( call_user_func( $this->referenceSeenCallback, $hash ) ) {
+				return $hash;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * Add fully reified statements for the given entity to the RDF graph.
+	 * This may include qualifiers and references, depending on calls to
+	 * setProduceQualifiers() resp. setProduceReferences().
+	 *
+	 * @param EntityDocument $entity the entity to output.
+	 */
+	public function addEntity( EntityDocument $entity ) {
+		if ( $entity instanceof StatementListProvider ) {
+			$entityId = $entity->getId();
+			$this->addStatements( $entityId, $entity->getStatements() );
+		}
+	}
+
+}
---
+++ repo/tests/phpunit/includes/rdf/SimpleValueRdfBuilderTest.php
@@ -0,0 +1,248 @@
+<?php
+
+namespace Wikibase\Test\Rdf;
+
+use DataValues\DataValue;
+use DataValues\DecimalValue;
+use DataValues\Geo\Values\GlobeCoordinateValue;
+use DataValues\LatLongValue;
+use DataValues\MonolingualTextValue;
+use DataValues\QuantityValue;
+use DataValues\StringValue;
+use DataValues\TimeValue;
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\DataModel\Entity\EntityIdValue;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikimedia\Purtle\RdfWriter;
+use Wikibase\Rdf\Test\RdfBuilderTestData;
+use Wikibase\Rdf\RdfVocabulary;
+use Wikibase\Rdf\SimpleValueRdfBuilder;
+
+/**
+ * @covers Wikibase\SimpleValueRdfBuilder
+ *
+ * @group Wikibase
+ * @group WikibaseRepo
+ * @group WikibaseRdf
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ * @author Stas Malyshev
+ */
+class SimpleValueRdfBuilderTest extends \PHPUnit_Framework_TestCase {
+
+	private $testData;
+
+	/**
+	 * Initialize repository data
+	 *
+	 * @return RdfBuilderTestData
+	 */
+	private function getTestData()
+	{
+		if( empty( $this->testData ) ) {
+			$this->testData = new RdfBuilderTestData(
+				__DIR__ . "/../../data/rdf",
+				__DIR__ . "/../../data/rdf/SimpleValueRdfBuilder"
+			);
+		}
+		return $this->testData;
+	}
+
+	/**
+	 * @param EntityId[] &$mentioned receives the IDs of any mentioned entities.
+	 *
+	 * @return SimpleValueRdfBuilder
+	 */
+	private function newBuilder( array &$mentioned = array() ) {
+		$entityMentioned = function( EntityId $id ) use ( &$mentioned ) {
+			$key = $id->getSerialization();
+			$mentioned[$key] = $id;
+		};
+
+		$vocabulary = $this->getTestData()->getVocabulary();
+
+		$builder = new SimpleValueRdfBuilder( $vocabulary, $this->getTestData()->getMockRepository() );
+		$builder->setEntityMentionCallback( $entityMentioned );
+
+		return $builder;
+	}
+
+	/**
+	 * Extract text test data from RDF builder
+	 *
+	 * @param RdfWriter $writer
+	 *
+	 * @return string[] ntriples lines, sorted
+	 */
+	private function getDataFromWriter( RdfWriter $writer ) {
+		$ntriples = $writer->drain();
+
+		$lines = explode( "\n", trim( $ntriples ) );
+		sort( $lines );
+		return $lines;
+	}
+
+	private function assertTriplesEqual( array $expectedTriples, RdfWriter $writer ) {
+		$actualTripels = $this->getDataFromWriter( $writer );
+		sort( $expectedTriples );
+
+		$this->assertEquals( $expectedTriples, $actualTripels );
+	}
+
+	public function provideAddSnakValue() {
+		// NOTE: data types must match $this->getTestData()->getMockRepository();
+
+		return array(
+			'wikibase-entityid' => array(
+				new PropertyId( 'P2' ),
+				new EntityIdValue( new ItemId( 'Q42' ) ),
+				array(
+					'<http://acme.test/Q11> <http://acme.test/assert/P2> <http://acme.test/Q42> .',
+				)
+			),
+			'commonsMedia' => array(
+				new PropertyId( 'P3' ),
+				new StringValue( 'Test.jpg' ),
+				array(
+					'<http://acme.test/Q11> <http://acme.test/assert/P3> <http://commons.wikimedia.org/wiki/Special:FilePath/Test.jpg> .',
+				)
+			),
+			'globecoordinate' => array(
+				new PropertyId( 'P4' ),
+				new GlobeCoordinateValue(
+					new LatLongValue( 12.25, -45.5 ),
+					0.025,
+					'https://www.wikidata.org/entity/Q2'
+				),
+				array(
+					'<http://acme.test/Q11> <http://acme.test/assert/P4> "Point(12.25 -45.5)"^^<http://www.opengis.net/ont/geosparql#wktLiteral> .',
+				)
+			),
+			'monolingualtext' => array(
+				new PropertyId( 'P5' ),
+				new MonolingualTextValue( 'ru', 'Берлин' ),
+				array(
+					'<http://acme.test/Q11> <http://acme.test/assert/P5> "\u0411\u0435\u0440\u043B\u0438\u043D"@ru .',
+				)
+			),
+			'quantity' => array(
+				new PropertyId( 'P6' ),
+				new QuantityValue(
+					new DecimalValue( '+0.00011' ),
+					'1',
+					new DecimalValue( '+0.00013' ),
+					new DecimalValue( '+0.00010' ) ),
+				array(
+					'<http://acme.test/Q11> <http://acme.test/assert/P6> "+0.00011"^^<http://www.w3.org/2001/XMLSchema#decimal> .',
+				)
+			),
+			'quantity-unit' => array(
+				new PropertyId( 'P6' ),
+				new QuantityValue(
+					new DecimalValue( '-2.3' ),
+					'https://www.wikidata.org/entity/Q11573',
+					new DecimalValue( '-2.3' ),
+					new DecimalValue( '-2.3' ) ),
+				array(
+					'<http://acme.test/Q11> <http://acme.test/assert/P6> "-2.3"^^<http://www.w3.org/2001/XMLSchema#decimal> .',
+				)
+			),
+			'string' => array(
+				new PropertyId( 'P7' ),
+				new StringValue( 'Kittens' ),
+				array(
+					'<http://acme.test/Q11> <http://acme.test/assert/P7> "Kittens" .',
+				)
+			),
+			'time' => array(
+				new PropertyId( 'P8' ),
+				new TimeValue( '+2015-03-03T00:00:00Z', 0, 0, 0, TimeValue::PRECISION_DAY, RdfVocabulary::GREGORIAN_CALENDAR ),
+				array(
+					'<http://acme.test/Q11> <http://acme.test/assert/P8> "2015-03-03T00:00:00Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .',
+				)
+			),
+			'time-year' => array( // NOTE: may changed to use xsd:gYear
+				new PropertyId( 'P8' ),
+				new TimeValue( '+2015-00-00T00:00:00Z', 0, 0, 0, TimeValue::PRECISION_YEAR, RdfVocabulary::GREGORIAN_CALENDAR ),
+				array(
+					'<http://acme.test/Q11> <http://acme.test/assert/P8> "2015-01-01T00:00:00Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .',
+				)
+			),
+			'time-margin' => array(
+				new PropertyId( 'P8' ),
+				new TimeValue( '+2015-03-03T00:00:00Z', 0, 3, 3, TimeValue::PRECISION_DAY, RdfVocabulary::GREGORIAN_CALENDAR ),
+				array(
+					'<http://acme.test/Q11> <http://acme.test/assert/P8> "2015-03-03T00:00:00Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .',
+				)
+			),
+			'time-bce' => array( // NOTE: This assumes that internal data and the RDF data use the
+				                 //       same notion of negative years. If one uses traditional
+				                 //       numbering (-44 means 44 BCE, XSD 1.0) and the other
+				                 //       astronomical numbering (-44 means 43 BCE, XSD 1.1),
+				                 //       conversion would apply.
+				new PropertyId( 'P8' ),
+				new TimeValue( '-0044-03-15T00:00:00Z', 0, 3, 3, TimeValue::PRECISION_DAY, RdfVocabulary::GREGORIAN_CALENDAR ),
+				array(
+					'<http://acme.test/Q11> <http://acme.test/assert/P8> "-0044-03-15T00:00:00Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .',
+				)
+			),
+			'time-julian' => array( // NOTE: Currently, giving a calendar other than gregorian
+				                    //       causes the timestamp to be output as an untyped literal.
+				                    //       If the calendar and interpretation of the timestamp
+				                    //       are known, they could be converted to (proleptic)
+				                    //       gregorian an output as an XSD date.
+				new PropertyId( 'P8' ),
+				new TimeValue( '+1492-10-12T00:00:00Z', 0, 0, 0, TimeValue::PRECISION_DAY, RdfVocabulary::JULIAN_CALENDAR ),
+				array(
+					'<http://acme.test/Q11> <http://acme.test/assert/P8> "1492-10-21T00:00:00Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .',
+				)
+			),
+			'url' => array(
+				new PropertyId( 'P9' ),
+				new StringValue( 'http://quux.test/xyzzy' ),
+				array(
+					'<http://acme.test/Q11> <http://acme.test/assert/P9> <http://quux.test/xyzzy> .',
+				)
+			),
+			'url-mailto' => array(
+				new PropertyId( 'P9' ),
+				new StringValue( 'mailto:xyzzy@quux.test' ),
+				array(
+					'<http://acme.test/Q11> <http://acme.test/assert/P9> <mailto:xyzzy@quux.test> .',
+				)
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider provideAddSnakValue
+	 */
+	public function testAddSnakValue( PropertyId $propertyId, DataValue $value, $expectedTriples ) {
+		$writer = $this->getTestData()->getNTriplesWriter();
+
+		$writer->about( RdfVocabulary::NS_ENTITY, 'Q11' );
+
+		$builder = $this->newBuilder();
+		$builder->addSnakValue( $writer, $propertyId, $value, RdfVocabulary::NS_DIRECT_CLAIM );
+
+		$this->assertTriplesEqual( $expectedTriples, $writer );
+	}
+
+	public function testAddSnakValue_mention() {
+		$propertyId = new PropertyId( 'P2' );
+		$value = new EntityIdValue( new ItemId( 'Q42' ) );
+
+		$writer = $this->getTestData()->getNTriplesWriter();
+
+		$writer->about( RdfVocabulary::NS_ENTITY, 'Q11' );
+
+		$mentioned = array();
+		$builder = $this->newBuilder( $mentioned );
+		$builder->addSnakValue( $writer, $propertyId, $value, RdfVocabulary::NS_DIRECT_CLAIM );
+
+		$this->assertEquals( array( 'Q42' ), array_keys( $mentioned ) );
+	}
+
+}
