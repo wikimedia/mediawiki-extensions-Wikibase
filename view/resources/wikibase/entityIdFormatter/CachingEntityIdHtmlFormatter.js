e868e30253edea551a6fbcc74006fce1b7f7b9bc
Adrian Heine <adrian.heine@wikimedia.de>
2015-03-31 15:04:13 +0200

    Use API for formatting in JS
    Bug: T87754
    Bug: T88275
    Bug: T88426
    Bug: T98208
    Change-Id: I641d8c46009f59e2b11284cb949322ae4d186b60

---
+++ view/resources/wikibase/entityIdFormatter/CachingEntityIdHtmlFormatter.js
@@ -0,0 +1,28 @@
+( function( wb, util ) {
+	'use strict';
+
+	/**
+	 * @param {wikibase.entityIdFormatter.EntityIdHtmlFormatter} entityIdFormatter
+	 */
+	wb.entityIdFormatter.CachingEntityIdHtmlFormatter = util.inherit(
+		'CachingEntityIdHtmlFormatter',
+		wb.entityIdFormatter.EntityIdHtmlFormatter,
+		function( entityIdFormatter ) {
+			this._entityIdFormatter = entityIdFormatter;
+			this._cache = {};
+		},
+		{
+			_entityIdFormatter: null,
+
+			_cache: null,
+
+			format: function( entityId ) {
+				if( !this._cache.hasOwnProperty( entityId ) ) {
+					this._cache[ entityId ] = this._entityIdFormatter.format( entityId );
+				}
+				return this._cache[ entityId ];
+			}
+
+		}
+	);
+}( wikibase, util ) );
