a787fe02ea6441c3c665ffebdb6fb7bd0d398b6d
Adrian Heine <adrian.heine@wikimedia.de>
2015-04-01 11:41:48 +0200

    Move jquery.ui from repo/ to view/
    jQuery.ui.TemplatedWidget depends on wikibase.templates, which currently is in
    repo but will be moved, too.
    
    Change-Id: Iafe73684975a9641692bc09ac169e9b40900df29

---
+++ view/tests/qunit/jquery/ui/resources.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * @licence GNU GPL v2+
+ * @author H. Snater < mediawiki@snater.com >
+ */
+return call_user_func( function() {
+	$remoteExtPathParts = explode(
+		DIRECTORY_SEPARATOR . 'extensions' . DIRECTORY_SEPARATOR, __DIR__, 2
+	);
+	$moduleBase = array(
+		'localBasePath' => __DIR__,
+		'remoteExtPath' => $remoteExtPathParts[1],
+	);
+
+	$modules = array(
+
+		'jquery.ui.closeable.tests' => $moduleBase + array(
+			'scripts' => array(
+				'jquery.ui.closeable.tests.js',
+			),
+			'dependencies' => array(
+				'jquery.ui.closeable',
+			),
+		),
+
+		'jquery.ui.tagadata.tests' => $moduleBase + array(
+			'scripts' => array(
+				'jquery.ui.tagadata.tests.js',
+			),
+			'dependencies' => array(
+				'jquery.ui.tagadata',
+			),
+		),
+
+		'jquery.ui.EditableTemplatedWidget.tests' => $moduleBase + array(
+			'scripts' => array(
+				'jquery.ui.EditableTemplatedWidget.tests.js',
+			),
+			'dependencies' => array(
+				'jquery.ui.EditableTemplatedWidget',
+				'wikibase.templates',
+			),
+		),
+
+		'jquery.ui.TemplatedWidget.tests' => $moduleBase + array(
+			'scripts' => array(
+				'jquery.ui.TemplatedWidget.tests.js',
+			),
+			'dependencies' => array(
+				'jquery.ui.TemplatedWidget',
+				'wikibase.templates',
+			),
+		),
+
+	);
+
+	return $modules;
+
+} );
