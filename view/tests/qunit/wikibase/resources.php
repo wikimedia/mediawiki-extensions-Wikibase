85314fc3732b9493b7402ba18f01cd069d7d27de
Adrian Heine <adrian.heine@wikimedia.de>
2015-04-01 12:18:57 +0200

    Move wikibase.ValueViewBuilder from repo/ to view/
    Change-Id: Ie2f657c5be266f9d3c06bf5fdc91217286ac6304

---
+++ view/tests/qunit/wikibase/resources.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @licence GNU GPL v2+
+ * @author H. Snater < mediawiki@snater.com >
+ */
+return call_user_func( function() {
+	$remoteExtPathParts = explode(
+		DIRECTORY_SEPARATOR . 'extensions' . DIRECTORY_SEPARATOR, __DIR__, 2
+	);
+	$moduleBase = array(
+		'localBasePath' => __DIR__,
+		'remoteExtPath' => $remoteExtPathParts[1],
+	);
+
+	$modules = array(
+
+		'wikibase.ValueViewBuilder.tests' => $moduleBase + array(
+			'scripts' => array(
+				'wikibase.ValueViewBuilder.tests.js'
+			),
+			'dependencies' => array(
+				'test.sinonjs',
+				'wikibase.ValueViewBuilder'
+			)
+		),
+
+	);
+
+	return $modules;
+
+} );
