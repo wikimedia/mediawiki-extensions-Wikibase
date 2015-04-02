23510aa468d7d8e883d6b929038c843a3e4307e9
aude <aude.wiki@gmail.com>
2015-04-02 16:45:24 +0200

    Add is_string check for $moduleClass in ApiConventionsTest
    Bug: T94837
    Change-Id: I46939c95d1e3f7fe99fb9fe5eb8642b571f4f26d

--- repo/tests/phpunit/includes/api/ApiConventionsTest.php
+++ repo/tests/phpunit/includes/api/ApiConventionsTest.php
@@ -26,7 +26,9 @@ class ApiConventionsTest extends \MediaWikiTestCase {
 
 		foreach ( $GLOBALS['wgAPIModules'] as $moduleClass ) {
 			// Make sure to only test Wikibase Api modules
-			if ( strpos( $moduleClass, 'Wikibase' ) !== false ) {
+			// This works as long as Wikibase modules are always defined as a class name string.
+			// @todo adjust this if we ever define our api modules differently.
+			if ( is_string( $moduleClass ) &&  strpos( $moduleClass, 'Wikibase' ) !== false ) {
 				$argList[] = array( $moduleClass );
 			}
 		}
