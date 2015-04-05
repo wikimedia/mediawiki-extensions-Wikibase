344dcf6481e3fca1028c3010aa8e45e0344da109
Marius Hoch <hoo@online.de>
2015-04-05 02:37:58 +0200

    Implement CachingSiteLinkLookup and make use in client
    Bug: T94168
    Change-Id: I632b25d1bca29c13dd07da7ef5ec1951a2a36aab

---
+++ lib/includes/store/CachingSiteLinkLookup.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace Wikibase\Lib\Store;
+
+use BagOStuff;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\SiteLink;
+use Wikibase\Lib\Store\SiteLinkLookup;
+
+/**
+ * SiteLinkLookup implementation that caches the obtained data (except for data obtained
+ * via "getLinks").
+ * Note: This doesn't implement any means of purging or data invalidation beyond the cache
+ * timeout.
+ *
+ * @since 0.5
+ *
+ * @license GNU GPL v2+
+ * @author Marius Hoch < hoo@online.de >
+ */
+class CachingSiteLinkLookup implements SiteLinkLookup {
+
+	/**
+	 * @var SiteLinkLookup
+	 */
+	private $lookup;
+
+	/**
+	 * The cache to use for caching entities.
+	 *
+	 * @var BagOStuff
+	 */
+	private $cache;
+
+	/**
+	 * @var int
+	 */
+	private $cacheDuration;
+
+	/**
+	 * @var string
+	 */
+	private $cacheKeyPrefix;
+
+	/**
+	 * @param SiteLinkLookup $siteLinkLookup The lookup to use
+	 * @param BagOStuff $cache The cache to use
+	 * @param int $cacheDuration Cache duration in seconds. Defaults to 3600 (1 hour).
+	 * @param string $cacheKeyPrefix Cache key prefix to use.
+	 *     Important in case we're not in-process caching. Defaults to "wikibase"
+	 */
+	public function __construct(
+		SiteLinkLookup $siteLinkLookup,
+		BagOStuff $cache,
+		$cacheDuration = 3600,
+		$cacheKeyPrefix = 'wikibase'
+	) {
+		$this->lookup = $siteLinkLookup;
+		$this->cache = $cache;
+		$this->cacheDuration = $cacheDuration;
+		$this->cacheKeyPrefix = $cacheKeyPrefix;
+	}
+
+	/**
+	 * @see SiteLinkLookup::getItemIdForLink
+	 *
+	 * @param string $globalSiteId
+	 * @param string $pageTitle
+	 *
+	 * @return ItemId|null
+	 */
+	public function getItemIdForLink( $globalSiteId, $pageTitle ) {
+		$itemIdSerialization = $this->cache->get( $this->getByPageCacheKey( $globalSiteId, $pageTitle ) );
+
+		if ( is_string( $itemIdSerialization ) ) {
+			return new ItemId( $itemIdSerialization );
+		} elseif ( $itemIdSerialization === false ) {
+			return $this->getAndCacheItemIdForLink( $globalSiteId, $pageTitle );
+		}
+
+		return null;
+	}
+
+	/**
+	 * @see SiteLinkLookup::getLinks
+	 * This is uncached!
+	 *
+	 * @param int[] $numericIds Numeric (unprefixed) item ids
+	 * @param string[] $siteIds
+	 * @param string[] $pageNames
+	 *
+	 * @return array[]
+	 */
+	public function getLinks( array $numericIds = array(), array $siteIds = array(), array $pageNames = array() ) {
+		// Caching this would be rather complicated for little to no benefit.
+		return $this->lookup->getLinks( $numericIds, $siteIds, $pageNames );
+	}
+
+	/**
+	 * Returns an array of SiteLink objects for an item. If the item isn't known or not an Item,
+	 * an empty array is returned.
+	 *
+	 * @since 0.4
+	 *
+	 * @param ItemId $itemId
+	 *
+	 * @return SiteLink[]
+	 */
+	public function getSiteLinksForItem( ItemId $itemId ) {
+		$cacheKey = $this->cacheKeyPrefix . ':sitelinks:' . $itemId->getSerialization();
+		$siteLinks = $this->cache->get( $cacheKey );
+
+		if ( !is_array( $siteLinks ) ) {
+			$siteLinks = $this->lookup->getSiteLinksForItem( $itemId );
+			$this->cache->set( $cacheKey, $siteLinks, $this->cacheDuration );
+		}
+
+		return $siteLinks;
+	}
+
+	/**
+	 * @see SiteLinkLookup::getEntityIdForSiteLink
+	 *
+	 * @param SiteLink $siteLink
+	 *
+	 * @return ItemId|null
+	 */
+	public function getEntityIdForSiteLink( SiteLink $siteLink ) {
+		return $this->getItemIdForLink(
+			$siteLink->getSiteId(),
+			$siteLink->getPageName()
+		);
+	}
+
+	/**
+	 * @param string $globalSiteId
+	 * @param string $pageTitle
+	 *
+	 * @return string
+	 */
+	private function getByPageCacheKey( $globalSiteId, $pageTitle ) {
+		return $this->cacheKeyPrefix . ':sitelinks-by-page:' . $globalSiteId . ':' . $pageTitle;
+	}
+
+	/**
+	 * @param string $globalSiteId
+	 * @param string $pageTitle
+	 *
+	 * @return ItemId|null
+	 */
+	private function getAndCacheItemIdForLink( $globalSiteId, $pageTitle ) {
+		$itemId = $this->lookup->getItemIdForLink( $globalSiteId, $pageTitle );
+
+		$this->cache->set(
+			$this->getByPageCacheKey( $globalSiteId, $pageTitle ),
+			$itemId instanceof ItemId ? $itemId->getSerialization() : null,
+			$this->cacheDuration
+		);
+
+		return $itemId;
+	}
+}
---
+++ lib/tests/phpunit/store/CachingSiteLinkLookupTest.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace Wikibase\Test;
+
+use HashBagOStuff;
+use PHPUnit_Framework_TestCase;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\SiteLink;
+use Wikibase\Lib\Store\CachingSiteLinkLookup;
+
+/**
+ * @covers Wikibase\Lib\Store\CachingSiteLinkLookup
+ *
+ * @group WikibaseLib
+ * @group WikibaseStore
+ * @group Wikibase
+ *
+ * @license GNU GPL v2+
+ * @author Marius Hoch < hoo@online.de >
+ */
+class CachingSiteLinkLookupTest extends PHPUnit_Framework_TestCase {
+
+	public function testGetItemIdForLink_cacheHit() {
+		$cache = new HashBagOStuff();
+		$cache->set( 'wikibase:sitelinks-by-page:foowiki:bar', 'Q42' );
+
+		$cachingSiteLinkLookup = new CachingSiteLinkLookup(
+			$this->getMock( 'Wikibase\Lib\Store\SiteLinkLookup' ),
+			$cache
+		);
+
+		$this->assertSame(
+			'Q42',
+			$cachingSiteLinkLookup->getItemIdForLink( 'foowiki', 'bar' )->getSerialization()
+		);
+	}
+
+	public function testGetItemIdForLink_cacheMiss() {
+		$cache = new HashBagOStuff();
+		$lookup = $this->getMock( 'Wikibase\Lib\Store\SiteLinkLookup' );
+		$lookup->expects( $this->once() )
+			->method( 'getItemIdForLink' )
+			->with( 'foowiki', 'bar' )
+			->will( $this->returnValue( new ItemId( 'Q42' ) ) );
+
+		$cachingSiteLinkLookup = new CachingSiteLinkLookup(
+			$lookup,
+			$cache
+		);
+
+		$this->assertSame(
+			'Q42',
+			$cachingSiteLinkLookup->getItemIdForLink( 'foowiki', 'bar' )->getSerialization()
+		);
+
+		// Make sure the new value also made it into the cache
+		$this->assertSame(
+			'Q42',
+			$cache->get( 'wikibase:sitelinks-by-page:foowiki:bar' )
+		);
+	}
+
+	public function testGetEntityIdForSiteLink_cacheHit() {
+		$siteLink = new SiteLink( 'foowiki', 'bar' );
+		$cache = new HashBagOStuff();
+		$cache->set( 'wikibase:sitelinks-by-page:foowiki:bar', 'Q42' );
+
+		$cachingSiteLinkLookup = new CachingSiteLinkLookup(
+			$this->getMock( 'Wikibase\Lib\Store\SiteLinkLookup' ),
+			$cache
+		);
+
+		$this->assertSame(
+			'Q42',
+			$cachingSiteLinkLookup->getEntityIdForSiteLink( $siteLink )->getSerialization()
+		);
+	}
+
+	public function testGetEntityIdForSiteLink_cacheMiss() {
+		$siteLink = new SiteLink( 'foowiki', 'bar' );
+		$cache = new HashBagOStuff();
+		$lookup = $this->getMock( 'Wikibase\Lib\Store\SiteLinkLookup' );
+		$lookup->expects( $this->once() )
+			->method( 'getItemIdForLink' )
+			->with( 'foowiki', 'bar' )
+			->will( $this->returnValue( new ItemId( 'Q42' ) ) );
+
+		$cachingSiteLinkLookup = new CachingSiteLinkLookup(
+			$lookup,
+			$cache
+		);
+
+		$this->assertSame(
+			'Q42',
+			$cachingSiteLinkLookup->getEntityIdForSiteLink( $siteLink )->getSerialization()
+		);
+
+		// Make sure the new value also made it into the cache
+		$this->assertSame(
+			'Q42',
+			$cache->get( 'wikibase:sitelinks-by-page:foowiki:bar' )
+		);
+	}
+
+	public function testGetSiteLinksForItem_cacheHit() {
+		$siteLinks = array( new SiteLink( 'foowiki', 'bar' ) );
+
+		$cache = new HashBagOStuff();
+		$cache->set( 'wikibase:sitelinks:Q42', $siteLinks );
+
+		$cachingSiteLinkLookup = new CachingSiteLinkLookup(
+			$this->getMock( 'Wikibase\Lib\Store\SiteLinkLookup' ),
+			$cache
+		);
+
+		$this->assertSame(
+			$siteLinks,
+			$cachingSiteLinkLookup->getSiteLinksForItem( new ItemId( 'Q42' ) )
+		);
+	}
+
+	public function testGetSiteLinksForItem_cacheMiss() {
+		$siteLinks = array( new SiteLink( 'foowiki', 'bar' ) );
+		$q42 = new ItemId( 'Q42' );
+
+		$cache = new HashBagOStuff();
+		$lookup = $this->getMock( 'Wikibase\Lib\Store\SiteLinkLookup' );
+		$lookup->expects( $this->once() )
+			->method( 'getSiteLinksForItem' )
+			->with( $q42 )
+			->will( $this->returnValue( $siteLinks ) );
+
+		$cachingSiteLinkLookup = new CachingSiteLinkLookup( $lookup, $cache );
+
+		$this->assertSame(
+			$siteLinks,
+			$cachingSiteLinkLookup->getSiteLinksForItem( new ItemId( 'Q42' ) )
+		);
+
+		// Make sure the new value also made it into the cache
+		$this->assertSame( $siteLinks, $cache->get( 'wikibase:sitelinks:Q42' ) );
+	}
+
+	public function testGetLinks() {
+		// getLinks is a simple pass through
+		$lookup = $this->getMock( 'Wikibase\Lib\Store\SiteLinkLookup' );
+		$lookup->expects( $this->once() )
+			->method( 'getLinks' )
+			->with( array( 1 ), array( 'a' ), array( 'b' ) )
+			->will( $this->returnValue( 'bar' ) );
+
+		$cachingSiteLinkLookup = new CachingSiteLinkLookup( $lookup, new HashBagOStuff() );
+
+		$this->assertSame(
+			'bar',
+			$cachingSiteLinkLookup->getLinks( array( 1 ), array( 'a' ), array( 'b' ) )
+		);
+	}
+}
