7816d3a1b4e9077957b3f272228820646548d7cf
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2015-04-09 17:44:15 +0200

    Make constructor parameters in EntityViewFactory optional
    Change-Id: I35e2d2985c0fe32f36038e523b5335f70919076a

--- repo/includes/WikibaseRepo.php
+++ repo/includes/WikibaseRepo.php
@@ -1033,9 +1033,9 @@ class WikibaseRepo {
 			$this->getDataTypeFactory(),
 			$templateFactory,
 			new LanguageNameLookup(),
-			$this->getSettings()->getSetting( 'siteLinkGroups' ),
-			$this->getSettings()->getSetting( 'specialSiteLinkGroups' ),
-			$this->getSettings()->getSetting( 'badgeItems' )
+			$this->settings->getSetting( 'siteLinkGroups' ),
+			$this->settings->getSetting( 'specialSiteLinkGroups' ),
+			$this->settings->getSetting( 'badgeItems' )
 		);
 
 		return new EntityParserOutputGeneratorFactory(
