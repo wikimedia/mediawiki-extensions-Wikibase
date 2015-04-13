34ac6926db3cf177ca97fe4cb8b1e5a56c3f6c92
Bene <benestar.wikimedia@gmail.com>
2015-04-13 20:26:35 +0200

    Add heading before sitelinks section
    This patch adds a <h2> heading before the sitelinks section
    and makes all site link group <h2> headings <h3> headings.
    The styling of these headings looks a bit different but
    the semantics are correct this way.
    
    The <h2> section is required for semantic reasons and also
    to display a collapsible "Site links" section on mobile wikidata.
    
    Furthermore, the <h2> tag got the class section-heading so that
    MobileFrontend will recognize it should be collapsed although
    it is not a direct descendant of #content.
    
    Because MobileFrontend won't recognize the heading otherwise,
    a <span> is inserted with the id of the heading containing the
    actual text.
    
    Bug: T95889
    Change-Id: I64d91e2f3b24b6fe03feae51d54ddd02a9c62fa3

--- view/resources/templates.php
+++ view/resources/templates.php
@@ -34,7 +34,7 @@ HTML;
 // $2: Optional ID for the heading.
 	$templates['wb-section-heading'] =
 <<<HTML
-<h2 class="wb-section-heading" dir="auto" id="$2">$1</h2>
+<h2 class="wb-section-heading section-heading" dir="auto"><span id="$2">$1</span></h2>
 HTML;
 
 	$templates['wikibase-statementgrouplistview'] =
@@ -222,7 +222,7 @@ HTML;
 <div class="wikibase-sitelinkgroupview" data-wb-sitelinks-group="$5">
 	<div class="wikibase-sitelinkgroupview-heading-section">
 		<div class="wikibase-sitelinkgroupview-heading-container">
-			<h2 class="wb-section-heading" dir="auto" id="$1">$2<span class="wikibase-sitelinkgroupview-counter">$3</span></h2>
+			<h3 class="wb-sitelinks-heading" dir="auto" id="$1">$2<span class="wikibase-sitelinkgroupview-counter">$3</span></h3>
 			<!-- wikibase-toolbar -->$6
 		</div>
 	</div>
