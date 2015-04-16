d7890dde27b0d6d74963df15c1864bcddebe571b
Bene <benestar.wikimedia@gmail.com>
2015-04-16 17:09:49 +0200

    Allow filtering by site on Special:ItemsWithoutSitelinks
    This patch adds a form on the top of Special:ItemsWithoutSitelinks
    to allow searching only for items which have no sitelink for a
    given site.
    
    Furthermore, a test is created for the getItemsWithoutSiteLinks
    method to verify that the result is ordered by the number of
    sitelinks.
    
    Things to be considered:
    * Show a message when an invalid site id is entered?
    * How to make clear that when no site id is entered items
      without any site link will be shown?
    * <s>Order the results by the count of sitelinks the items have
      (only relevant when a site is entered obviously).</s>
    
    Change-Id: Id4637eb701de32b2ee9ffb75847e8376258bda19

--- repo/includes/specials/SpecialItemsWithoutSitelinks.php
+++ repo/includes/specials/SpecialItemsWithoutSitelinks.php
@@ -2,19 +2,46 @@
 
 namespace Wikibase\Repo\Specials;
 
+use Html;
+use Wikibase\Repo\Store\EntityPerPage;
 use Wikibase\Repo\WikibaseRepo;
 
 /**
- * Page for listing entities without label.
+ * Page for listing entities without site links.
  *
  * @since 0.4
  * @licence GNU GPL v2+
  * @author Thomas Pellissier Tanon
+ * @author Bene* < benestar.wikimedia@gmail.com >
  */
 class SpecialItemsWithoutSitelinks extends SpecialWikibaseQueryPage {
 
+	/**
+	 * @var string|null
+	 */
+	private $siteId;
+
+	/**
+	 * @var EntityPerPage
+	 */
+	private $entityPerPage;
+
 	public function __construct() {
 		parent::__construct( 'ItemsWithoutSitelinks' );
+
+		$this->initServices(
+			WikibaseRepo::getDefaultInstance()->getStore()->newEntityPerPage()
+		);
+	}
+
+	/**
+	 * Initialize the services used be this special page.
+	 * May be used to inject mock services for testing.
+	 *
+	 * @param EntityPerPage $entityPerPage
+	 */
+	public function initServices( EntityPerPage $entityPerPage ) {
+		$this->entityPerPage = $entityPerPage;
 	}
 
 	/**
@@ -27,7 +54,66 @@ class SpecialItemsWithoutSitelinks extends SpecialWikibaseQueryPage {
 	public function execute( $subPage ) {
 		parent::execute( $subPage );
 
+		$this->prepareArguments( $subPage );
+		$this->showForm();
 		$this->showQuery();
+	}
+
+	private function prepareArguments( $subPage ) {
+		$request = $this->getRequest();
+
+		$this->siteId = $request->getText( 'site', $subPage );
+		if ( $this->siteId === '' ) {
+			$this->siteId = null;
+		}
+	}
+
+	private function showForm() {
+		$this->getOutput()->addHTML(
+			Html::openElement(
+				'form',
+				array(
+					'action' => $this->getPageTitle()->getLocalURL(),
+					'name' => 'itemswithoutsitelinks',
+					'id' => 'wb-itemswithoutsitelinks-form'
+				)
+			) .
+			Html::openElement( 'fieldset' ) .
+			Html::element(
+				'legend',
+				array(),
+				$this->msg( 'wikibase-itemswithoutsitelinks-legend' )->text()
+			) .
+			Html::openElement( 'p' ) .
+			Html::element(
+				'label',
+				array(
+					'for' => 'wb-itemswithoutsitelinks-site'
+				),
+				$this->msg( 'wikibase-itemswithoutsitelinks-label-site' )->text()
+			) . ' ' .
+			Html::input(
+				'site',
+				$this->siteId !== null ? htmlspecialchars( $this->siteId ) : '',
+				'text',
+				array(
+					'id' => 'wb-itemswithoutsitelinks-site',
+					'class' => 'wb-input-text'
+				)
+			) . ' ' .
+			Html::input(
+				'',
+				$this->msg( 'wikibase-entitieswithoutlabel-submit' )->text(),
+				'submit',
+				array(
+					'id' => 'wikibase-listproperties-submit',
+					'class' => 'wb-input-button'
+				)
+			) .
+			Html::closeElement( 'p' ) .
+			Html::closeElement( 'fieldset' ) .
+			Html::closeElement( 'form' )
+		);
 	}
 
 	/**
@@ -36,8 +122,7 @@ class SpecialItemsWithoutSitelinks extends SpecialWikibaseQueryPage {
 	 * @since 0.4
 	 */
 	protected function getResult( $offset = 0, $limit = 0 ) {
-		$entityPerPage = WikibaseRepo::getDefaultInstance()->getStore()->newEntityPerPage();
-		return $entityPerPage->getItemsWithoutSitelinks( null, $limit, $offset );
+		return $this->entityPerPage->getItemsWithoutSitelinks( $this->siteId, $limit, $offset );
 	}
 
 }
