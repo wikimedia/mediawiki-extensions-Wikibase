478d50728674f1f28cef803ae6885ae5bdc33a2c
jeroendedauw <jeroendedauw@gmail.com>
2015-04-27 01:39:17 +0200

    Show component version on Special:Version when used with MediaWiki

---
+++ lib/packages/wikibase/internal-serialization/mediawiki.php
@@ -0,0 +1,15 @@
+<?php
+
+if ( defined( 'MEDIAWIKI' ) ) {
+	$GLOBALS['wgExtensionCredits']['wikibase']['WikibaseInternalSerialization'] = array(
+		'path' => __DIR__,
+		'name' => 'Wikibase Internal Serialization',
+		'version' => '1.4',
+		'author' => array(
+			'[https://www.mediawiki.org/wiki/User:Jeroen_De_Dauw Jeroen De Dauw]',
+		),
+		'url' => 'https://github.com/wmde/WikibaseInternalSerialization',
+		'description' => 'Serializers and deserializers for the data access layer of Wikibase Repository',
+		'license-name' => 'GPL-2.0+'
+	);
+}
