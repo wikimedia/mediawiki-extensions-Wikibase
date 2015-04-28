bd9f81cca234ff36eabda1a18da36fd0b6e50dec
aude <aude.wiki@gmail.com>
2015-04-28 14:42:52 -0400

    Add some (optional) verbosity to ChangesSubscriptionTableBuilder
    Some additional verbosity can be helpful, plus timestamps on the
    script output in case of problems with running the script.
    
    Change-Id: I7f2e160f3c833c6e268c707b3bc05c373d237301

--- repo/includes/store/sql/ChangesSubscriptionTableBuilder.php
+++ repo/includes/store/sql/ChangesSubscriptionTableBuilder.php
@@ -43,13 +43,24 @@ class ChangesSubscriptionTableBuilder {
 	private $progressReporter;
 
 	/**
+	 * @var string 'verbose' or 'standard'
+	 */
+	private $verbosity;
+
+	/**
 	 * @param LoadBalancer $loadBalancer
 	 * @param string $tableName
 	 * @param int $batchSize
+	 * @param string $verbosity
 	 *
 	 * @throws InvalidArgumentException
 	 */
-	public function __construct( LoadBalancer $loadBalancer, $tableName, $batchSize = 1000 ) {
+	public function __construct(
+		LoadBalancer $loadBalancer,
+		$tableName,
+		$batchSize,
+		$verbosity = 'standard'
+	) {
 		if ( !is_string( $tableName ) ) {
 			throw new InvalidArgumentException( '$tableName must be a string' );
 		}
@@ -58,9 +69,14 @@ class ChangesSubscriptionTableBuilder {
 			throw new InvalidArgumentException( '$batchSize must be an integer >= 1' );
 		}
 
-		$this->loadBalancer = $loadBalancer;
+		if ( $verbosity !== 'standard' && $verbosity !== 'verbose' ) {
+			throw new InvalidArgumentException( '$verbosity must be either "verbose"'
+				. ' or "standard".' );
+		}
+
 		$this->tableName = $tableName;
 		$this->batchSize = $batchSize;
+		$this->verbosity = $verbosity;
 
 		$this->exceptionHandler = new LogWarningExceptionHandler();
 		$this->progressReporter = new NullMessageReporter();
@@ -157,6 +173,11 @@ class ChangesSubscriptionTableBuilder {
 				)
 			);
 
+			if ( $this->verbosity === 'verbose' ) {
+				$this->progressReporter->reportMessage( 'Inserted ' . $db->affectedRows()
+					. ' into wb_changes_subscription' );
+			}
+
 			$c+= count( $rows );
 		}
 
@@ -171,7 +192,6 @@ class ChangesSubscriptionTableBuilder {
 	 * @return array[] An associative array mapping item IDs to lists of site IDs.
 	 */
 	private function getSubscriptionsPerItemBatch( DatabaseBase $db, &$continuation = array() ) {
-
 		if ( empty( $continuation ) ) {
 			$continuationCondition = '1';
 		} else {
@@ -194,6 +214,11 @@ class ChangesSubscriptionTableBuilder {
 				'ORDER BY' => 'ips_item_id, ips_site_id'
 			)
 		);
+
+		if ( $this->verbosity === 'verbose' ) {
+			$this->progressReporter->reportMessage( 'Selected ' . $res->numRows() . ' wb_item_per_site records'
+				. ' with continuation: ' . $continuationCondition );
+		}
 
 		return $this->getSubscriptionsPerItemFromRows( $res, $continuation );
 	}
