9fe4e74feeb53f2a349ca5ef22c8a3285453cde3
daniel <daniel.kinzler@wikimedia.de>
2015-04-29 14:26:30 +0200

    Add modifier field to EntityUsage
    Bug: 93055
    Change-Id: I1b4918dc023389eeb034ec87b7309c2c871c1619

--- client/tests/phpunit/includes/Usage/EntityUsageTest.php
+++ client/tests/phpunit/includes/Usage/EntityUsageTest.php
@@ -18,16 +18,73 @@ use Wikibase\DataModel\Entity\ItemId;
  */
 class EntityUsageTest extends PHPUnit_Framework_TestCase {
 
-	public function testGetters() {
+
+	public function testGetEntityId() {
 		$id = new ItemId( 'Q7' );
 		$aspect = EntityUsage::ALL_USAGE;
 
 		$usage = new EntityUsage( $id, $aspect );
 
 		$this->assertEquals( $id, $usage->getEntityId() );
-		$this->assertEquals( $aspect, $usage->getAspect() );
+	}
 
+	public function testGetAspect() {
+		$id = new ItemId( 'Q7' );
+		$aspect = EntityUsage::ALL_USAGE;
+
+		$usage = new EntityUsage( $id, $aspect );
+		$this->assertEquals( $aspect, $usage->getAspect() );
+	}
+
+	public function testGetIdentityString() {
+		$id = new ItemId( 'Q7' );
+		$aspect = EntityUsage::ALL_USAGE;
+
+		$usage = new EntityUsage( $id, $aspect );
 		$this->assertInternalType( 'string', $usage->getIdentityString() );
+	}
+
+	public function testGetAspectKey() {
+		$id = new ItemId( 'Q7' );
+		$aspect = EntityUsage::LABEL_USAGE;
+		$modifier = 'ru';
+
+		$usage = new EntityUsage( $id, $aspect );
+		$this->assertEquals( $aspect, $usage->getAspectKey() );
+
+		$usage = new EntityUsage( $id, $aspect, $modifier );
+		$this->assertEquals( "$aspect.$modifier", $usage->getAspectKey() );
+	}
+
+	public function provideSplitAspectKey() {
+		return array(
+			array( 'L', array( 'L', null ) ),
+			array( 'L.x', array( 'L', 'x' ) ),
+			array( 'L.x.y', array( 'L', 'x.y' ) ),
+		);
+	}
+
+	/**
+	 * @dataProvider provideSplitAspectKey
+	 */
+	public function testSplitAspectKey( $aspectKey, $expectedParts ) {
+		$parts = EntityUsage::splitAspectKey( $aspectKey );
+		$this->assertEquals( $expectedParts, $parts );
+	}
+
+	public function provideMakeAspectKey() {
+		return array(
+			array( 'L', null, 'L' ),
+			array( 'L', 'x', 'L.x' ),
+		);
+	}
+
+	/**
+	 * @dataProvider provideMakeAspectKey
+	 */
+	public function testMakeAspectKey( $aspect, $modifier, $expectedKey ) {
+		$key = EntityUsage::makeAspectKey( $aspect, $modifier );
+		$this->assertEquals( $expectedKey, $key );
 	}
 
 }
