f47136229941499e91d8bd4758062292e9f911d9
Bene <benestar.wikimedia@gmail.com>
2015-04-30 22:38:29 +0200

    Created StatementListSerializer and StatementListDeserializer

---
+++ lib/packages/wikibase/data-model-serialization/src/Deserializers/StatementListDeserializer.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Wikibase\DataModel\Deserializers;
+
+use Deserializers\Deserializer;
+use Deserializers\Exceptions\DeserializationException;
+use Wikibase\DataModel\Statement\StatementList;
+
+/**
+ * Package private
+ *
+ * @licence GNU GPL v2+
+ * @author Bene* < benestar.wikimedia@gmail.com >
+ */
+class StatementListDeserializer implements Deserializer {
+
+	/**
+	 * @var Deserializer
+	 */
+	private $statementDeserializer;
+
+	/**
+	 * @param Deserializer $statementDeserializer
+	 */
+	public function __construct( Deserializer $statementDeserializer ) {
+		$this->statementDeserializer = $statementDeserializer;
+	}
+
+	/**
+	 * @see Deserializer::deserialize
+	 *
+	 * @param mixed $serialization
+	 *
+	 * @return object
+	 * @throws DeserializationException
+	 */
+	public function deserialize( $serialization ) {
+		$this->assertHasGoodFormat( $serialization );
+
+		return $this->getDeserialized( $serialization );
+	}
+
+	private function getDeserialized( array $serialization ) {
+		$statementList = new StatementList();
+
+		foreach( $serialization as $statementArray ) {
+			foreach( $statementArray as $statementSerialization ) {
+				$statementList->addStatement( $this->statementDeserializer->deserialize( $statementSerialization ) );
+			}
+		}
+
+		return $statementList;
+	}
+
+	private function assertHasGoodFormat( $serialization ) {
+		if( !is_array( $serialization ) ) {
+			throw new DeserializationException( 'The StatementList serialization should be an array' );
+		}
+
+		foreach( $serialization as $statementArray ) {
+			if( !is_array( $statementArray ) ) {
+				throw new DeserializationException( 'The statements per property should be an array' );
+			}
+		}
+	}
+
+}
---
+++ lib/packages/wikibase/data-model-serialization/src/Serializers/StatementListSerializer.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Wikibase\DataModel\Serializers;
+
+use Serializers\DispatchableSerializer;
+use Serializers\Exceptions\SerializationException;
+use Serializers\Exceptions\UnsupportedObjectException;
+use Wikibase\DataModel\ByPropertyIdGrouper;
+use Wikibase\DataModel\Statement\StatementList;
+
+/**
+ * Package private
+ *
+ * @licence GNU GPL v2+
+ * @author Bene* < benestar.wikimedia@gmail.com >
+ */
+class StatementListSerializer implements DispatchableSerializer {
+
+	/**
+	 * @var Serializer
+	 */
+	protected $statementSerializer;
+
+	/**
+	 * @var bool
+	 */
+	protected $useObjectsForMaps;
+
+	/**
+	 * @param Serializer $statementSerializer
+	 * @param bool $useObjectsForMaps
+	 */
+	public function __construct( Serializer $statementSerializer, $useObjectsForMaps ) {
+		$this->statementSerializer = $statementSerializer;
+		$this->useObjectsForMaps = $useObjectsForMaps;
+	}
+
+	/**
+	 * @see Serializer::isSerializerFor
+	 *
+	 * @param mixed $object
+	 *
+	 * @return bool
+	 */
+	public function isSerializerFor( $object ) {
+		return $object instanceof StatementList;
+	}
+
+	/**
+	 * @see Serializer::serialize
+	 *
+	 * @param mixed $object
+	 *
+	 * @return array
+	 * @throws SerializationException
+	 */
+	public function serialize( $object ) {
+		if ( !$this->isSerializerFor( $object ) ) {
+			throw new UnsupportedObjectException(
+				$object,
+				'StatementListSerializer can only serialize StatementList objects'
+			);
+		}
+
+		return $this->getSerialized( $object );
+	}
+
+	private function getSerialized( StatementList $statementList ) {
+		$serialization = array();
+
+		$byPropertyIdGrouper = new ByPropertyIdGrouper( $statementList );
+
+		foreach( $byPropertyIdGrouper->getPropertyIds() as $propertyId ) {
+			$serialization[$propertyId->getSerialization()] = $this->getSerializedStatements(
+				$byPropertyIdGrouper->getByPropertyId( $propertyId )
+			);
+		}
+
+		if ( $this->useObjectsForMaps ) {
+			$serialization = (object)$serialization;
+		}
+
+		return $serialization;
+	}
+
+	private function getSerializedStatements( array $statements ) {
+		$serialization = array();
+
+		foreach ( $statements as $statement ) {
+			$serialization[] = $this->statementSerializer->serialize( $statement );
+		}
+
+		return $serialization;
+	}
+
+}
