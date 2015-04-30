9d71b1da56b78a70808bb83e31717b697a35a022
Bene <benestar.wikimedia@gmail.com>
2015-04-30 23:08:35 +0200

    Create tests for StatementListSerializer

---
+++ lib/packages/wikibase/data-model-serialization/tests/unit/Deserializers/StatementListDeserializerTest.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace Tests\Wikibase\DataModel\Deserializers;
+
+use Wikibase\DataModel\Claim\Claim;
+use Wikibase\DataModel\Deserializers\StatementListDeserializer;
+use Wikibase\DataModel\Snak\PropertyNoValueSnak;
+use Wikibase\DataModel\Statement\Statement;
+use Wikibase\DataModel\Statement\StatementList;
+
+/**
+ * @covers Wikibase\DataModel\Deserializers\StatementListDeserializer
+ *
+ * @licence GNU GPL v2+
+ * @author Bene* < benestar.wikimedia@gmail.com >
+ */
+class StatementListDeserializerTest extends DeserializerBaseTest {
+
+	public function buildDeserializer() {
+		$statement = new Statement( new Claim( new PropertyNoValueSnak( 42 ) ) );
+		$statement->setGuid( 'test' );
+
+		$statementDeserializerMock = $this->getMock( '\Deserializers\Deserializer' );
+		$statementDeserializerMock->expects( $this->any() )
+			->method( 'deserialize' )
+			->with( $this->equalTo( array(
+				'mainsnak' => array(
+					'snaktype' => 'novalue',
+					'property' => 'P42'
+				),
+				'type' => 'statement',
+				'rank' => 'normal'
+			) ) )
+			->will( $this->returnValue( $statement ) );
+
+		return new StatementListDeserializer( $statementDeserializerMock );
+	}
+
+	public function deserializableProvider() {
+		return array(
+			array(
+				array()
+			),
+			array(
+				array(
+					'P42' => array(
+						array(
+							'mainsnak' => array(
+								'snaktype' => 'novalue',
+								'property' => 'P42'
+							),
+							'type' => 'statement',
+							'rank' => 'normal'
+						)
+					)
+				)
+			),
+			array(
+				array(
+					'P42' => array(
+						array(
+							'mainsnak' => array(
+								'snaktype' => 'novalue',
+								'property' => 'P42'
+							),
+							'type' => 'statement',
+							'rank' => 'normal'
+						),
+						array(
+							'mainsnak' => array(
+								'snaktype' => 'novalue',
+								'property' => 'P42'
+							),
+							'type' => 'statement',
+							'rank' => 'normal'
+						)
+					)
+				)
+			),
+		);
+	}
+
+	public function nonDeserializableProvider() {
+		return array(
+			array(
+				42
+			),
+			array(
+				array(
+					'id' => 'P10'
+				)
+			),
+			array(
+				array(
+					'type' => '42'
+				)
+			),
+		);
+	}
+
+	public function deserializationProvider() {
+		$statement = new Statement( new Claim( new PropertyNoValueSnak( 42 ) ) );
+		$statement->setGuid( 'test' );
+
+		return array(
+			array(
+				new StatementList(),
+				array()
+			),
+			array(
+				new StatementList( array(
+					$statement
+				) ),
+				array(
+					'P42' => array(
+						array(
+							'mainsnak' => array(
+								'snaktype' => 'novalue',
+								'property' => 'P42'
+							),
+							'type' => 'statement',
+							'rank' => 'normal'
+						)
+					)
+				)
+			),
+		);
+	}
+}
---
+++ lib/packages/wikibase/data-model-serialization/tests/unit/Serializers/StatementListSerializerTest.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace Tests\Wikibase\DataModel\Serializers;
+
+use stdClass;
+use Wikibase\DataModel\Claim\Claim;
+use Wikibase\DataModel\Serializers\StatementListSerializer;
+use Wikibase\DataModel\Snak\PropertyNoValueSnak;
+use Wikibase\DataModel\Statement\Statement;
+use Wikibase\DataModel\Statement\StatementList;
+
+/**
+ * @covers Wikibase\DataModel\Serializers\StatementListSerializer
+ *
+ * @licence GNU GPL v2+
+ * @author Bene* < benestar.wikimedia@gmail.com >
+ */
+class StatementListSerializerTest extends SerializerBaseTest {
+
+	protected function buildSerializer() {
+		$statement = new Statement( new Claim( new PropertyNoValueSnak( 42 ) ) );
+		$statement->setGuid( 'test' );
+
+		$statementSerializerMock = $this->getMock( '\Serializers\Serializer' );
+		$statementSerializerMock->expects( $this->any() )
+			->method( 'serialize' )
+			->with( $this->equalTo( $statement ) )
+			->will( $this->returnValue( array(
+				'mainsnak' => array(
+					'snaktype' => 'novalue',
+					'property' => 'P42'
+				),
+				'type' => 'statement',
+				'rank' => 'normal'
+			) ) );
+
+		return new StatementListSerializer( $statementSerializerMock, false );
+	}
+
+	public function serializableProvider() {
+		$statement = new Statement( new Claim( new PropertyNoValueSnak( 42 ) ) );
+		$statement->setGuid( 'test' );
+
+		return array(
+			array(
+				new StatementList()
+			),
+			array(
+				new StatementList( array(
+					$statement
+				) )
+			),
+		);
+	}
+
+	public function nonSerializableProvider() {
+		return array(
+			array(
+				5
+			),
+			array(
+				array()
+			),
+			array(
+				new Statement( new PropertyNoValueSnak( 42 ) )
+			),
+		);
+	}
+
+	public function serializationProvider() {
+		$statement = new Statement( new Claim( new PropertyNoValueSnak( 42 ) ) );
+		$statement->setGuid( 'test' );
+
+		return array(
+			array(
+				array(),
+				new StatementList()
+			),
+			array(
+				array(
+					'P42' => array(
+						array(
+							'mainsnak' => array(
+								'snaktype' => 'novalue',
+								'property' => 'P42'
+							),
+							'type' => 'statement',
+							'rank' => 'normal'
+						)
+					)
+				),
+				new StatementList( array(
+					$statement
+				) )
+			),
+		);
+	}
+
+	public function testStatementListSerializerWithOptionObjectsForMaps() {
+		$statement = new Statement( new Claim( new PropertyNoValueSnak( 42 ) ) );
+		$statement->setGuid( 'test' );
+		$statementSerializerMock = $this->getMock( '\Serializers\Serializer' );
+		$statementSerializerMock->expects( $this->any() )
+			->method( 'serialize' )
+			->with( $this->equalTo( $statement ) )
+			->will( $this->returnValue( array(
+				'mockedsuff' => array(),
+				'type' => 'statement',
+			) ) );
+		$serializer = new StatementListSerializer( $statementSerializerMock, true );
+
+		$statementList = new StatementList( array( $statement ) );
+
+		$serial = new stdClass();
+		$serial->P42 = array( array(
+			'mockedsuff' => array(),
+			'type' => 'statement',
+		) );
+		$this->assertEquals( $serial, $serializer->serialize( $statementList ) );
+	}
+
+}
