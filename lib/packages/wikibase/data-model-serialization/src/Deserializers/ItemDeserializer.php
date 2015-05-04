1c30b32394bdd62041ed3765363bda3a75f72a92
Bene <benestar.wikimedia@gmail.com>
2015-05-04 20:28:26 +0200

    Remove EntityDeserializer abstract base class
    This class was never exposed in public but only used for code sharing.
    All relevant code has been moved out into separate deserializers so this
    can now be safely deleted.
    
    Remember that this is not a breaking change as EntityDeserializer was
    not a public class but only an implementation detail.

--- lib/packages/wikibase/data-model-serialization/src/Deserializers/ItemDeserializer.php
+++ lib/packages/wikibase/data-model-serialization/src/Deserializers/ItemDeserializer.php
@@ -3,6 +3,8 @@
 namespace Wikibase\DataModel\Deserializers;
 
 use Deserializers\Deserializer;
+use Deserializers\Exceptions\DeserializationException;
+use Deserializers\TypedObjectDeserializer;
 use Wikibase\DataModel\Entity\Item;
 use Wikibase\DataModel\SiteLinkList;
 
@@ -12,7 +14,22 @@ use Wikibase\DataModel\SiteLinkList;
  * @licence GNU GPL v2+
  * @author Thomas Pellissier Tanon
  */
-class ItemDeserializer extends EntityDeserializer {
+class ItemDeserializer extends TypedObjectDeserializer {
+
+	/**
+	 * @var Deserializer
+	 */
+	private $entityIdDeserializer;
+
+	/**
+	 * @var Deserializer
+	 */
+	private $fingerprintDeserializer;
+
+	/**
+	 * @var Deserializer
+	 */
+	private $statementListDeserializer;
 
 	/**
 	 * @var Deserializer
@@ -22,26 +39,64 @@ class ItemDeserializer extends EntityDeserializer {
 	/**
 	 * @param Deserializer $entityIdDeserializer
 	 * @param Deserializer $fingerprintDeserializer
-	 * @param Deserializer $claimsDeserializer
+	 * @param Deserializer $statementListDeserializer
 	 * @param Deserializer $siteLinkDeserializer
 	 */
 	public function __construct(
 		Deserializer $entityIdDeserializer,
 		Deserializer $fingerprintDeserializer,
-		Deserializer $claimsDeserializer,
+		Deserializer $statementListDeserializer,
 		Deserializer $siteLinkDeserializer
 	) {
-		parent::__construct( 'item', $entityIdDeserializer, $fingerprintDeserializer, $claimsDeserializer );
+		parent::__construct( 'item', 'type' );
 
+		$this->entityIdDeserializer = $entityIdDeserializer;
+		$this->fingerprintDeserializer = $fingerprintDeserializer;
+		$this->statementListDeserializer = $statementListDeserializer;
 		$this->siteLinkDeserializer = $siteLinkDeserializer;
 	}
 
-	protected function getPartiallyDeserialized( array $serialization ) {
+	/**
+	 * @see Deserializer::deserialize
+	 *
+	 * @param array $serialization
+	 *
+	 * @return Item
+	 * @throws DeserializationException
+	 */
+	public function deserialize( $serialization ) {
+		$this->assertCanDeserialize( $serialization );
+
+		return $this->getDeserialized( $serialization );
+	}
+
+	private function getDeserialized( array $serialization ) {
 		$item = new Item();
 
+		$item->setFingerprint( $this->fingerprintDeserializer->deserialize( $serialization ) );
+
+		$this->setIdFromSerialization( $serialization, $item );
+		$this->setStatementListFromSerialization( $serialization, $item );
 		$this->setSiteLinksFromSerialization( $item->getSiteLinkList(), $serialization );
 
 		return $item;
+	}
+
+	private function setIdFromSerialization( array $serialization, Item $item ) {
+		if ( !array_key_exists( 'id', $serialization ) ) {
+			return;
+		}
+
+		$item->setId( $this->entityIdDeserializer->deserialize( $serialization['id'] ) );
+	}
+
+	private function setStatementListFromSerialization( array $serialization, Item $item ) {
+		if ( !array_key_exists( 'claims', $serialization ) ) {
+			return;
+		}
+
+		$statements = $this->statementListDeserializer->deserialize( $serialization['claims'] );
+		$item->setStatements( $statements );
 	}
 
 	private function setSiteLinksFromSerialization(
@@ -51,6 +106,7 @@ class ItemDeserializer extends EntityDeserializer {
 		if ( !array_key_exists( 'sitelinks', $serialization ) ) {
 			return;
 		}
+
 		$this->assertAttributeIsArray( $serialization, 'sitelinks' );
 
 		foreach( $serialization['sitelinks'] as $siteLinksSerialization ) {
--- lib/packages/wikibase/data-model-serialization/src/Deserializers/PropertyDeserializer.php
+++ lib/packages/wikibase/data-model-serialization/src/Deserializers/PropertyDeserializer.php
@@ -3,6 +3,8 @@
 namespace Wikibase\DataModel\Deserializers;
 
 use Deserializers\Deserializer;
+use Deserializers\Exceptions\DeserializationException;
+use Deserializers\TypedObjectDeserializer;
 use Wikibase\DataModel\Entity\Property;
 
 /**
@@ -11,23 +13,83 @@ use Wikibase\DataModel\Entity\Property;
  * @licence GNU GPL v2+
  * @author Thomas Pellissier Tanon
  */
-class PropertyDeserializer extends EntityDeserializer {
+class PropertyDeserializer extends TypedObjectDeserializer {
+
+	/**
+	 * @var Deserializer
+	 */
+	private $entityIdDeserializer;
+
+	/**
+	 * @var Deserializer
+	 */
+	private $fingerprintDeserializer;
+
+	/**
+	 * @var Deserializer
+	 */
+	private $statementListDeserializer;
 
 	/**
 	 * @param Deserializer $entityIdDeserializer
 	 * @param Deserializer $fingerprintDeserializer
-	 * @param Deserializer $claimsDeserializer
+	 * @param Deserializer $statementListDeserializer
 	 */
-	public function __construct( Deserializer $entityIdDeserializer, Deserializer $fingerprintDeserializer, Deserializer $claimsDeserializer ) {
-		parent::__construct( 'property', $entityIdDeserializer, $fingerprintDeserializer, $claimsDeserializer );
+	public function __construct(
+		Deserializer $entityIdDeserializer,
+		Deserializer $fingerprintDeserializer,
+		Deserializer $statementListDeserializer
+	) {
+		parent::__construct( 'property', 'type' );
+
+		$this->entityIdDeserializer = $entityIdDeserializer;
+		$this->fingerprintDeserializer = $fingerprintDeserializer;
+		$this->statementListDeserializer = $statementListDeserializer;
 	}
 
-	protected function getPartiallyDeserialized( array $serialization ) {
+	/**
+	 * @see Deserializer::deserialize
+	 *
+	 * @param array $serialization
+	 *
+	 * @return Property
+	 * @throws DeserializationException
+	 */
+	public function deserialize( $serialization ) {
+		$this->assertCanDeserialize( $serialization );
+
+		return $this->getDeserialized( $serialization );
+	}
+
+	private function getDeserialized( array $serialization ) {
 		$this->requireAttribute( $serialization, 'datatype' );
 		$this->assertAttributeInternalType( $serialization, 'datatype', 'string' );
+
 		$property = Property::newFromType( $serialization['datatype'] );
 
+		$property->setFingerprint( $this->fingerprintDeserializer->deserialize( $serialization ) );
+
+		$this->setIdFromSerialization( $serialization, $property );
+		$this->setStatementListFromSerialization( $serialization, $property );
+
 		return $property;
+	}
+
+	private function setIdFromSerialization( array $serialization, Property $property ) {
+		if ( !array_key_exists( 'id', $serialization ) ) {
+			return;
+		}
+
+		$property->setId( $this->entityIdDeserializer->deserialize( $serialization['id'] ) );
+	}
+
+	private function setStatementListFromSerialization( array $serialization, Property $property ) {
+		if ( !array_key_exists( 'claims', $serialization ) ) {
+			return;
+		}
+
+		$statements = $this->statementListDeserializer->deserialize( $serialization['claims'] );
+		$property->setStatements( $statements );
 	}
 
 }
