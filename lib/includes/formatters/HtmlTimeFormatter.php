feb6ba6496ecd1cbe5ac04cb40faf4855166f21e
daniel <daniel.kinzler@wikimedia.de>
2015-05-06 18:05:17 +0200

    Adjust time formatter heuristics for calendar display.
    The calendar should be shown if it's different from the
    one that the parser would guess, as of
    https://github.com/DataValues/Time/pull/66
    
    Bug: 75272
    Change-Id: Ia0d7b1548972b6f81538c8f5182f8e014bfc1a36

--- lib/includes/formatters/HtmlTimeFormatter.php
+++ lib/includes/formatters/HtmlTimeFormatter.php
@@ -15,6 +15,7 @@ use ValueFormatters\ValueFormatterBase;
  * @license GNU GPL v2+
  * @author Adrian Lang < adrian.lang@wikimedia.de >
  * @author Thiemo Mättig
+ * @author Daniel Kinzler
  */
 class HtmlTimeFormatter extends ValueFormatterBase {
 
@@ -70,16 +71,36 @@ class HtmlTimeFormatter extends ValueFormatterBase {
 	 * @return bool
 	 */
 	private function calendarNameNeeded( TimeValue $value ) {
-		preg_match( '/^[-+]\d+/', $value->getTime(), $matches );
-		$year = $matches[0];
+		// We assume this is an ISO-ish timestamp.
+		preg_match( '/^([-+]?0*\d+)-/', $value->getTime(), $m );
+		$guessedCalendar = $this->getDefaultCalendar( $m[1] );
 
-		// This is how the original JavaScript UI decided this:
-		// year <= 1581 && calendar === 'Gregorian' ||
-		// year > 1581 && year < 1930 ||
-		// year >= 1930 && calendar === 'Julian'
-		return $value->getPrecision() >= TimeValue::PRECISION_DAY && (
-			$year <= 1581 || $value->getCalendarModel() !== TimeFormatter::CALENDAR_GREGORIAN
-		);
+		// Always show the calendar if it's different from the "guessed" default.
+		if ( $value->getCalendarModel() !== $guessedCalendar ) {
+			return true;
+		}
+
+		// Otherwise, the calendar is "unsurprising", so don't show it.
+		return false;
+	}
+
+	/**
+	 * This guesses the most likely calendar model based on the given TimeValue,
+	 * ignoring the calendar given in the TimeValue. This should always implement the
+	 * exact same heuristic as IsoTimestampParser::getCalendarModel().
+	 *
+	 * @see IsoTimestampParser::getCalendarModel()
+	 *
+	 * @param string|int $year The year as a decimal string with a mandatory leading sign
+	 *        and no leading zeros.
+	 *
+	 * @return string Calendar URI
+	 */
+	private function getDefaultCalendar( $year ) {
+		// The Gregorian calendar was introduced in October 1582,
+		// so we'll default to Julian for all years before 1583.
+		// NOTE: PHP will limit overly large values to PHP_INT_MAX. No overflow or wrap-around occurs.
+		return (int)$year < 1583 ? TimeFormatter::CALENDAR_JULIAN : TimeFormatter::CALENDAR_GREGORIAN;
 	}
 
 	/**
