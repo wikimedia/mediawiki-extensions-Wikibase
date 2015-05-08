a963aef285b7347214fed87c8716a563d11f797f
daniel <daniel.kinzler@wikimedia.de>
2015-05-08 19:11:47 +0200

    Track label usage with fallback from Lua.
    This change removed code that has become redundant by moving usage
    tracking away from the rendering logic into decorators for the
    lookup logic.
    
    In addition, this change includes several modifications to test cases
    to ensure that the SnakFormatter decorator is applied and configured
    correctly.
    
    Bug: T93056
    Change-Id: I89abc34fe17d9be4377e65ef90f584836a47446f

--- client/tests/phpunit/includes/DataAccess/Scribunto/SnakSerializationRendererTest.php
+++ client/tests/phpunit/includes/DataAccess/Scribunto/SnakSerializationRendererTest.php
@@ -2,17 +2,13 @@
 
 namespace Wikibase\Client\Tests\DataAccess\Scribunto;
 
+use DataValues\DataValue;
+use DataValues\StringValue;
 use Language;
 use PHPUnit_Framework_TestCase;
-use DataValues\StringValue;
-use DataValues\DataValue;
-use Wikibase\DataModel\Entity\EntityIdValue;
 use Wikibase\Client\DataAccess\Scribunto\SnakSerializationRenderer;
-use Wikibase\Client\Usage\EntityUsage;
-use Wikibase\Client\Usage\HashUsageAccumulator;
-use Wikibase\Client\Usage\UsageAccumulator;
 use Wikibase\Client\WikibaseClient;
-use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Entity\EntityIdValue;
 use Wikibase\DataModel\Entity\PropertyId;
 use Wikibase\DataModel\Snak\PropertyValueSnak;
 use Wikibase\Lib\Serializers\SnakSerializer;
@@ -51,7 +47,7 @@ class SnakSerializationRendererTest extends PHPUnit_Framework_TestCase {
 	 *
 	 * @return SnakSerializationRenderer
 	 */
-	private function getSnakRenderer( UsageAccumulator $usageAccumulator ) {
+	private function getSnakRenderer() {
 		$wikibaseClient = WikibaseClient::getDefaultInstance();
 
 		$snakFormatter = $this->getMock( 'Wikibase\Lib\SnakFormatter' );
@@ -73,35 +69,15 @@ class SnakSerializationRendererTest extends PHPUnit_Framework_TestCase {
 			$snakFormatter,
 			$snakDeserializer,
 			Language::factory( 'en' ),
-			$snaksDeserializer,
-			$usageAccumulator
+			$snaksDeserializer
 		);
 	}
 
 	public function testRenderSnak() {
 		$snakSerialization = $this->getSnakSerialization( new StringValue( 'foo bar foo' ) );
-		$usageAccumulator = new HashUsageAccumulator();
-		$snakRenderer = $this->getSnakRenderer( $usageAccumulator );
+		$snakRenderer = $this->getSnakRenderer();
 
 		$this->assertSame( 'foo bar foo', $snakRenderer->renderSnak( $snakSerialization ) );
-
-		$this->assertCount( 0, $usageAccumulator->getUsages() );
-	}
-
-	public function testRenderSnak_usage() {
-		$Q42 = new ItemId( 'Q42' );
-		$snakSerialization = $this->getSnakSerialization( new EntityIdValue( $Q42 ) );
-		$usageAccumulator = new HashUsageAccumulator();
-		$snakRenderer = $this->getSnakRenderer( $usageAccumulator );
-
-		$this->assertSame( 'Q42', $snakRenderer->renderSnak( $snakSerialization ) );
-		$usages = $usageAccumulator->getUsages();
-
-		$this->assertCount( 1, $usages );
-		$this->assertEquals(
-			new EntityUsage( $Q42, EntityUsage::LABEL_USAGE, 'en' ),
-			array_shift( $usages )
-		);
 	}
 
 	public function provideRenderSnaks() {
@@ -125,36 +101,9 @@ class SnakSerializationRendererTest extends PHPUnit_Framework_TestCase {
 	 * @dataProvider provideRenderSnaks
 	 */
 	public function testRenderSnaks( $expected, array $snaksSerialization ) {
-		$usageAccumulator = new HashUsageAccumulator();
-		$snakRenderer = $this->getSnakRenderer( $usageAccumulator );
+		$snakRenderer = $this->getSnakRenderer();
 
 		$this->assertSame( $expected, $snakRenderer->renderSnaks( $snaksSerialization ) );
-
-		$this->assertCount( 0, $usageAccumulator->getUsages() );
-	}
-
-	public function testRenderSnaks_usage() {
-		$Q42 = new ItemId( 'Q42' );
-		$Q43 = new ItemId( 'Q43' );
-		$snaksSerialization = array( array() );
-		$snaksSerialization[0][] = $this->getSnakSerialization( new EntityIdValue( $Q42 ) );
-		$snaksSerialization[0][] = $this->getSnakSerialization( new EntityIdValue( $Q43 ) );
-
-		$usageAccumulator = new HashUsageAccumulator();
-		$snakRenderer = $this->getSnakRenderer( $usageAccumulator );
-
-		$this->assertSame( 'Q42, Q43', $snakRenderer->renderSnaks( $snaksSerialization ) );
-		$usages = $usageAccumulator->getUsages();
-
-		$this->assertCount( 2, $usages );
-		$this->assertEquals(
-			new EntityUsage( $Q42, EntityUsage::LABEL_USAGE, 'en' ),
-			array_shift( $usages )
-		);
-		$this->assertEquals(
-			new EntityUsage( $Q43, EntityUsage::LABEL_USAGE, 'en' ),
-			array_shift( $usages )
-		);
 	}
 
 }
