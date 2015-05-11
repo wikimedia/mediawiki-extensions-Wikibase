f116fc095f74bc25d7c3a15a09042b63b5273dd5
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2015-05-11 11:46:12 +0200

    Fix, simplify and clean-up DeferredDecodingEntityHolder
    This patch fixes some actual issues:
    * Subsequent calls to getEntity with different $expectedClass
      parameters skipped the type check.
    * It was possible to use a mismatching $entityType constructor
      parameter.
    * The getEntity() documentation was wrong.
    
    Other changes:
    * I found the $format parameter dubious, renamed to $contentFormat.
    * Avoided the deprecated "Entity" class name in documentation.
    
    Change-Id: I1afbcf074e0c5c65470a2339cc3de6d7ff355f19

--- repo/includes/content/DeferredDecodingEntityHolder.php
+++ repo/includes/content/DeferredDecodingEntityHolder.php
@@ -13,6 +13,7 @@ use Wikibase\Lib\Store\EntityContentDataCodec;
  *
  * @license GPL 2+
  * @author Daniel Kinzler
+ * @author Thiemo Mättig
  */
 class DeferredDecodingEntityHolder implements EntityHolder {
 
@@ -32,9 +33,9 @@ class DeferredDecodingEntityHolder implements EntityHolder {
 	private $blob;
 
 	/**
-	 * @var string
+	 * @var string Serialization format to decode the blob, typically CONTENT_FORMAT_JSON.
 	 */
-	private $format;
+	private $contentFormat;
 
 	/**
 	 * @var string
@@ -42,26 +43,32 @@ class DeferredDecodingEntityHolder implements EntityHolder {
 	private $entityType;
 
 	/**
-	 * @var EntityId
+	 * @var EntityId|null
 	 */
-	private $entityId;
+	private $entityId = null;
 
 	/**
 	 * @param EntityContentDataCodec $codec
 	 * @param string $blob
-	 * @param string $format
-	 * @param $entityType
-	 * @param EntityId $entityId
+	 * @param string $contentFormat Serialization format to decode the blob, typically CONTENT_FORMAT_JSON.
+	 * @param string $entityType
+	 * @param EntityId|null $entityId
 	 *
 	 * @throws InvalidArgumentException
 	 */
-	public function __construct( EntityContentDataCodec $codec, $blob, $format, $entityType, EntityId $entityId = null ) {
+	public function __construct(
+		EntityContentDataCodec $codec,
+		$blob,
+		$contentFormat,
+		$entityType,
+		EntityId $entityId = null
+	) {
 		if ( !is_string( $blob ) ) {
 			throw new InvalidArgumentException( '$blob must be a string' );
 		}
 
-		if ( !is_string( $format ) ) {
-			throw new InvalidArgumentException( '$format must be a string' );
+		if ( !is_string( $contentFormat ) ) {
+			throw new InvalidArgumentException( '$contentFormat must be a string' );
 		}
 
 		if ( !is_string( $entityType ) ) {
@@ -70,7 +77,7 @@ class DeferredDecodingEntityHolder implements EntityHolder {
 
 		$this->codec = $codec;
 		$this->blob = $blob;
-		$this->format = $format;
+		$this->contentFormat = $contentFormat;
 		$this->entityType = $entityType;
 		$this->entityId = $entityId;
 	}
@@ -78,25 +85,36 @@ class DeferredDecodingEntityHolder implements EntityHolder {
 	/**
 	 * @see EntityHolder::getEntityId
 	 *
-	 * This implements lazy initialization of the entity: when called for the first time,
-	 * this method will call getEntity() on the EntityHolder passed to the constructor,
-	 * and then calls copy() on the entity returned. The resulting copy is returned.
-	 * Subsequent calls will return the same entity.
+	 * This implements lazy deserialization of the blob passed to the constructor.
 	 *
 	 * @param string $expectedClass The class with which the result is expected to be compatible.
-	 * Defaults to Entity.
+	 * Defaults to EntityDocument.
 	 *
 	 * @throws RuntimeException If the entity held by this EntityHolder is not compatible with $expectedClass.
 	 * @return EntityDocument
 	 */
 	public function getEntity( $expectedClass = 'Wikibase\DataModel\Entity\EntityDocument' ) {
 		if ( !$this->entity ) {
-			$this->entity = $this->codec->decodeEntity( $this->blob, $this->format );
+			$this->entity = $this->codec->decodeEntity( $this->blob, $this->contentFormat );
 
-			if ( !( $this->entity instanceof $expectedClass ) ) {
-				throw new RuntimeException( 'Deferred decoding resulted in an incompatible entity. ' .
-					'Expected ' . $expectedClass . ', got ' . get_class(  ) );
+			if ( !( $this->entity instanceof EntityDocument ) ) {
+				throw new RuntimeException( 'Deferred decoding resulted in an incompatible entity, '
+					. 'expected EntityDocument, got ' . gettype( $this->entity ) );
+			} elseif ( $this->entity->getType() !== $this->entityType ) {
+				throw new RuntimeException( 'Deferred decoding resulted in an incompatible entity, '
+					. 'expected ' . $this->entityType . ', got ' . $this->entity->getType() );
+			} elseif ( $this->entity->getId() === null ) {
+				throw new RuntimeException( 'Deferred decoding resulted in an incompatible entity, '
+					. 'expected an entity id to be set, got null' );
+			} elseif ( $this->entityId && !$this->entity->getId()->equals( $this->entityId ) ) {
+				throw new RuntimeException( 'Deferred decoding resulted in an incompatible entity, '
+					. 'expected ' . $this->entityId . ', got ' . $this->entity->getId() );
 			}
+		}
+
+		if ( !( $this->entity instanceof $expectedClass ) ) {
+			throw new RuntimeException( 'Deferred decoding resulted in an incompatible entity, '
+				. 'expected ' . $expectedClass . ', got ' . get_class( $this->entity ) );
 		}
 
 		return $this->entity;
@@ -105,7 +123,8 @@ class DeferredDecodingEntityHolder implements EntityHolder {
 	/**
 	 * @see EntityHolder::getEntityId
 	 *
-	 * @return EntityId|null
+	 * @throws RuntimeException If the entity held by this EntityHolder does not have an id.
+	 * @return EntityId
 	 */
 	public function getEntityId() {
 		if ( !$this->entityId ) {
--- repo/tests/phpunit/includes/content/DeferredDecodingEntityHolderTest.php
+++ repo/tests/phpunit/includes/content/DeferredDecodingEntityHolderTest.php
@@ -6,6 +6,7 @@ use Wikibase\Content\DeferredDecodingEntityHolder;
 use Wikibase\Content\EntityHolder;
 use Wikibase\DataModel\Entity\BasicEntityIdParser;
 use Wikibase\DataModel\Entity\Entity;
+use Wikibase\DataModel\Entity\EntityId;
 use Wikibase\DataModel\Entity\Item;
 use Wikibase\DataModel\Entity\ItemId;
 use Wikibase\Lib\Store\EntityContentDataCodec;
@@ -20,6 +21,7 @@ use Wikibase\Repo\WikibaseRepo;
  *
  * @licence GNU GPL v2+
  * @author Daniel Kinzler
+ * @author Thiemo Mättig
  */
 class DeferredDecodingEntityHolderTest extends \PHPUnit_Framework_TestCase {
 
@@ -35,24 +37,26 @@ class DeferredDecodingEntityHolderTest extends \PHPUnit_Framework_TestCase {
 
 	/**
 	 * @param Entity $entity
+	 * @param string|null $expectedEntityType
+	 * @param EntityId|null $expectedEntityId
 	 *
 	 * @return EntityHolder
 	 */
-	private function newHolder( Entity $entity ) {
+	private function newHolder( Entity $entity, $expectedEntityType = null, EntityId $expectedEntityId = null ) {
 		$codec = new EntityContentDataCodec(
 			new BasicEntityIdParser(),
 			WikibaseRepo::getDefaultInstance()->getInternalEntitySerializer(),
 			WikibaseRepo::getDefaultInstance()->getInternalEntityDeserializer()
 		);
-
 		$blob = $codec->encodeEntity( $entity, CONTENT_FORMAT_JSON );
 
 		return new DeferredDecodingEntityHolder(
 			$codec,
 			$blob,
 			CONTENT_FORMAT_JSON,
-			$entity->getType(),
-			$entity->getId() );
+			$expectedEntityType ?: $entity->getType(),
+			$expectedEntityId
+		);
 	}
 
 	public function testGetEntity() {
@@ -63,6 +67,54 @@ class DeferredDecodingEntityHolderTest extends \PHPUnit_Framework_TestCase {
 		$this->assertNotSame( $entity, $actual );
 		$this->assertEquals( $entity->getId(), $actual->getId() );
 		$this->assertTrue( $entity->equals( $actual ) );
+	}
+
+	public function testGetEntityWithExpectedClass() {
+		$entity = $this->newEntity();
+		$holder = $this->newHolder( $entity );
+
+		$actual = $holder->getEntity( 'Wikibase\DataModel\Entity\Item' );
+		$this->assertEquals( $entity, $actual );
+	}
+
+	public function testGivenEntityWithoutId_getEntityThrowsException() {
+		$holder = $this->newHolder( new Item() );
+
+		$this->setExpectedException( 'RuntimeException' );
+		$holder->getEntity();
+	}
+
+	public function testGivenMismatchingClassName_secondGetEntityCallThrowsException() {
+		$item = $this->newEntity();
+		$holder = $this->newHolder( $item );
+
+		$holder->getEntity( 'Wikibase\DataModel\Entity\Item' );
+		$this->setExpectedException( 'RuntimeException' );
+		$holder->getEntity( 'Wikibase\DataModel\Entity\Property' );
+	}
+
+	public function testGivenMismatchingEntityType_getEntityThrowsException() {
+		$item = $this->newEntity();
+		$holder = $this->newHolder( $item, 'property' );
+
+		$this->setExpectedException( 'RuntimeException' );
+		$holder->getEntity();
+	}
+
+	public function testGivenMismatchingEntityType_getEntityWithExpectedClassThrowsException() {
+		$item = $this->newEntity();
+		$holder = $this->newHolder( $item, 'property' );
+
+		$this->setExpectedException( 'RuntimeException' );
+		$holder->getEntity( 'Wikibase\DataModel\Entity\Property' );
+	}
+
+	public function testGivenMismatchingIds_getEntityThrowsException() {
+		$item = $this->newEntity();
+		$holder = $this->newHolder( $item, 'item', new ItemId( 'Q42' ) );
+
+		$this->setExpectedException( 'RuntimeException' );
+		$holder->getEntity();
 	}
 
 	public function testGetEntityType() {
@@ -79,6 +131,21 @@ class DeferredDecodingEntityHolderTest extends \PHPUnit_Framework_TestCase {
 
 		$actual = $holder->getEntityId();
 		$this->assertEquals( $entity->getId(), $actual );
+	}
+
+	public function testGivenMismatchingIds_getEntityIdReturnsGivenId() {
+		$item = $this->newEntity();
+		$holder = $this->newHolder( $item, 'item', new ItemId( 'Q42' ) );
+
+		$actual = $holder->getEntityId();
+		$this->assertEquals( new ItemId( 'Q42' ), $actual );
+	}
+
+	public function testGivenEntityWithoutId_getEntityIdThrowsException() {
+		$holder = $this->newHolder( new Item() );
+
+		$this->setExpectedException( 'RuntimeException' );
+		$holder->getEntityId();
 	}
 
 }
