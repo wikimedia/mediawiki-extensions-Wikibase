e199cb56ad3d5dc73cbb497fdf4e6cb7ec8f6711
Marius Hoch <hoo@online.de>
2015-05-21 06:35:04 +0200

    Make wbmergeitems respect the bot parameter
    Change-Id: I6627b56c5ca357057481357ccde61feef77e3bac

--- repo/includes/api/MergeItems.php
+++ repo/includes/api/MergeItems.php
@@ -125,8 +125,9 @@ class MergeItems extends ApiBase {
 			if ( $ignoreConflicts === null ) {
 				$ignoreConflicts = array();
 			}
+			$bot = isset( $params['bot'] );
 
-			$this->mergeItems( $fromId, $toId, $ignoreConflicts, $summary );
+			$this->mergeItems( $fromId, $toId, $ignoreConflicts, $summary, $bot );
 		} catch ( EntityIdParsingException $ex ) {
 			$this->errorReporter->dieException( $ex, 'invalid-entity-id' );
 		} catch ( ItemMergeException $ex ) {
@@ -139,9 +140,10 @@ class MergeItems extends ApiBase {
 	 * @param ItemId $toId
 	 * @param array $ignoreConflicts
 	 * @param string $summary
+	 * @param bool $bot
 	 */
-	private function mergeItems( ItemId $fromId, ItemId $toId, array $ignoreConflicts, $summary ) {
-		list( $newRevisionFrom, $newRevisionTo ) = $this->interactor->mergeItems( $fromId, $toId, $ignoreConflicts, $summary );
+	private function mergeItems( ItemId $fromId, ItemId $toId, array $ignoreConflicts, $summary, $bot ) {
+		list( $newRevisionFrom, $newRevisionTo ) = $this->interactor->mergeItems( $fromId, $toId, $ignoreConflicts, $summary, $bot );
 
 		$this->resultBuilder->setValue( null, 'success', 1 );
 
