6dc3cc991ce75fa21166c0cf29f791c39a153f43
frimelle <lucie.aimee@googlemail.com>
2015-05-31 16:28:03 +0200

    [WIP] Merging items creates redirect
    Add the creation of redirects when items are merged, adjust and improve the tests.
    No tests for the redirect creation yet (specials, api & interactor), change messages; work in progress.
    
    Bug: T59745
    Change-Id: I5093a09455b5dffa7c1c3f98c240fe356a3756ff

--- repo/includes/Interactors/ItemMergeInteractor.php
+++ repo/includes/Interactors/ItemMergeInteractor.php
@@ -17,6 +17,7 @@ use Wikibase\Lib\Store\StorageException;
 use Wikibase\Repo\Store\EntityPermissionChecker;
 use Wikibase\Summary;
 use Wikibase\SummaryFormatter;
+use Wikibase\Repo\Interactors\RedirectCreationInteractor;
 
 /**
  * @since 0.5
@@ -24,6 +25,7 @@ use Wikibase\SummaryFormatter;
  * @licence GNU GPL v2+
  * @author Adam Shorland
  * @author Daniel Kinzler
+ * @author Lucie-Aimée Kaffee
  *
  * @todo allow merging of specific parts of an item only (eg. sitelinks,aliases,claims)
  * @todo allow optional redirect creation after merging
@@ -61,6 +63,11 @@ class ItemMergeInteractor {
 	private $user;
 
 	/**
+	* @var RedirectCreationInteractor
+	*/
+	private $interactorRedirect;
+
+	/**
 	 * @param MergeChangeOpsFactory $changeOpFactory
 	 * @param EntityRevisionLookup $entityRevisionLookup
 	 * @param EntityStore $entityStore
@@ -74,7 +81,8 @@ class ItemMergeInteractor {
 		EntityStore $entityStore,
 		EntityPermissionChecker $permissionChecker,
 		SummaryFormatter $summaryFormatter,
-		User $user
+		User $user,
+		RedirectCreationInteractor $interactorRedirect
 	) {
 
 		$this->changeOpFactory = $changeOpFactory;
@@ -83,6 +91,7 @@ class ItemMergeInteractor {
 		$this->permissionChecker = $permissionChecker;
 		$this->summaryFormatter = $summaryFormatter;
 		$this->user = $user;
+		$this->interactorRedirect = $interactorRedirect;
 	}
 
 	/**
@@ -120,7 +129,7 @@ class ItemMergeInteractor {
 	}
 
 	/**
-	 * Merges the content of the first item into the second.
+	 * Merges the content of the first item into the second and creates a redirect if the first item is empty after the merge.
 	 *
 	 * @param ItemId $fromId
 	 * @param ItemId $toId
@@ -156,8 +165,34 @@ class ItemMergeInteractor {
 		} catch ( ChangeOpException $e ) {
 			throw new ItemMergeException( $e->getMessage(), 'failed-modify', $e );
 		}
+		
+		$result = $this->attemptSaveMerge( $fromEntity, $toEntity, $summary, $bot );
+		
+		$redirected = false;
+		
+		if ( $this->checkEmpty( $fromId ) ) {
+			$this->createRedirect( $fromId, $toId, $bot );
+			$redirected = true;
+		}
+		
+		array_push( $result, $redirected );
+		return $result;
+	}
 
-		return $this->attemptSaveMerge( $fromEntity, $toEntity, $summary, $bot );
+	/**
+	 * @param ItemId $fromId
+	 * @param ItemId $toId
+	 * @param bool $bot
+	 */
+	private function createRedirect( ItemId $fromId, ItemId $toId, $bot ) {
+		$this->interactorRedirect->createRedirect( $fromId, $toId, $bot );
+	}
+
+	/**
+	 * EntityId $entityId
+	 */
+	private function checkEmpty( EntityId $entityId ) {
+		return $this->loadEntity( $entityId )->isEmpty();
 	}
 
 	private function loadEntity( EntityId $entityId ) {
@@ -177,6 +212,11 @@ class ItemMergeInteractor {
 		}
 	}
 
+	/**
+	 * @param EntityDocument $fromEntity
+	 * @param EntityDocument $toEntity
+	 * @throws ItemMergeException
+	 */
 	private function validateEntities( EntityDocument $fromEntity, EntityDocument $toEntity ) {
 		if ( !( $fromEntity instanceof Item && $toEntity instanceof Item ) ) {
 			throw new ItemMergeException( 'One or more of the entities are not items', 'not-item' );
@@ -206,9 +246,10 @@ class ItemMergeInteractor {
 	 * @param Item $toItem
 	 * @param string|null $summary
 	 * @param bool $bot
+	 * @param bool $redirected
 	 *
-	 * @return array A list of exactly two EntityRevision objects. The first one represents
-	 * the modified source item, the second one represents the modified target item.
+	 * @return array A list of exactly two EntityRevision objects and a boolean for the status of the redirect.
+	 * The first one represents the modified source item, the second one represents the modified target item.
 	 */
 	private function attemptSaveMerge( Item $fromItem, Item $toItem, $summary, $bot ) {
 		$toSummary = $this->getSummary( 'to', $toItem->getId(), $summary );
