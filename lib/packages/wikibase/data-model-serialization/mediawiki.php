16a3ec6b171b562ba87b707e0b4202c5268cef66
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2015-06-08 20:43:28 +0200

    Update mediawiki.php

--- lib/packages/wikibase/data-model-serialization/mediawiki.php
+++ lib/packages/wikibase/data-model-serialization/mediawiki.php
@@ -4,7 +4,7 @@ if ( defined( 'MEDIAWIKI' ) ) {
 	$GLOBALS['wgExtensionCredits']['wikibase']['WikibaseDataModelSerialization'] = array(
 		'path' => __DIR__,
 		'name' => 'Wikibase DataModel Serialization',
-		'version' => '1.4',
+		'version' => '1.4.0',
 		'author' => array(
 			'[https://www.mediawiki.org/wiki/User:Jeroen_De_Dauw Jeroen De Dauw]',
 		),
