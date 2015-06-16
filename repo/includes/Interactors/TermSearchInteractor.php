3438a2b8d9711ce0495ec677762b0be99b48c415
addshore <addshorewiki@gmail.com>
2015-06-16 11:52:14 +0100

    Implement TermSearchInteractor
    Also adds a convenience method to WikibaseRepo
    
    As stated in the bug this will be used in:
     - SearchEntites (api module)
     - SpecialItemDisambiguation
    
    Bug: T90692
    Change-Id: I06714ad2d793e7927a2eb07715e8853a2c170715

---
+++ repo/includes/Interactors/TermSearchInteractor.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Wikibase\Repo\Interactors;
+
+/**
+ * Interface for searching for terms
+ *
+ * @since 0.5
+ *
+ * @licence GNU GPL v2+
+ * @author Adam Shorland
+ */
+interface TermSearchInteractor {
+
+	/**
+	 * @since 0.5
+	 *
+	 * @param string $text Term text to search for
+	 * @param string[] $languageCodes Language codes to search in
+	 * @param string $entityType Type of Entity to return
+	 * @param string[] $termTypes Types of Term to return, array of Wikibase\TermIndexEntry::TYPE_*
+	 *
+	 * @returns array[] array of arrays containing the following:
+	 *          ['entityId'] => EntityId EntityId
+	 *          ['matchedTerm'] => Term MatchedTerm
+	 *          ['displayTerms'] => Term[] array with possible keys Wikibase\TermIndexEntry::TYPE_*
+	 */
+	public function searchForTerms( $text, array $languageCodes, $entityType, array $termTypes );
+
+}
