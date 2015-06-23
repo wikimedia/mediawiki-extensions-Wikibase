wikimedia/mediawiki-extensions-Wikibase

commit 179afd6e50dcd6806a56be8d97ee4c2c97d3cd40

Fix "wikibase.special.entitiesWithout"
Adopt to "new" suggester data format, disable
autocompletion.

Bug: T96037
Change-Id: Ie1d4a69b3fd42f46cd3dea34569472298c5c9c2b

repo/resources/wikibase.special/wikibase.special.entitiesWithout.js
@@ -11,14 +11,12 @@
 
 		if( $.uls ) {
 			$.each( $.uls.data.getAutonyms(), function( key, value ) {
-				languages.push( {
-					label: value + ' (' + key + ')',
-					value: value + ' (' + key + ')'
-				} );
+				languages.push( value + ' (' + key + ')' );
 			} );
 		}
 
 		var $languageSelector = $( '#wb-entitieswithoutpage-language' );
+		$languageSelector.attr( 'languageSelector', 'off' );
 		$languageSelector.suggester( { source: languages } );
 
 		$( '#wb-entitieswithoutpage-form' ).submit( function() {

commit 53f4011e7d0bbd65cf298cf58b19a61d5203bcef

Fix copy and paste mistake in wikibase.special.entitiesWithout.js
Introduced in 179afd6e50dcd6806a56be8d97ee4c2c97d3cd40

Change-Id: Ib186b9fd7decb662894f268e360d99ec10ed65d3

repo/resources/wikibase.special/wikibase.special.entitiesWithout.js
@@ -16,7 +16,7 @@
 		}
 
 		var $languageSelector = $( '#wb-entitieswithoutpage-language' );
-		$languageSelector.attr( 'languageSelector', 'off' );
+		$languageSelector.attr( 'autocomplete', 'off' );
 		$languageSelector.suggester( { source: languages } );
 
 		$( '#wb-entitieswithoutpage-form' ).submit( function() {