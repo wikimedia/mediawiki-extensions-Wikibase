9671f2fc7fa76b284613400d1f06140a42a86964
addshore <addshorewiki@gmail.com>
2015-06-29 15:46:55 +0200

    Implement TermListDeserializer
    Fixes #147

---
+++ lib/packages/wikibase/data-model-serialization/src/Deserializers/TermListDeserializer.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Wikibase\DataModel\Deserializers;
+
+use Deserializers\Deserializer;
+use Deserializers\Exceptions\DeserializationException;
+use Wikibase\DataModel\Term\TermList;
+
+/**
+ * @since 1.5
+ *
+ * @author Adam Shorland
+ */
+class TermListDeserializer implements Deserializer {
+
+	/**
+	 * @param Deserializer $termDeserializer
+	 */
+	public function __construct( Deserializer $termDeserializer ) {
+		$this->termDeserializer = $termDeserializer;
+	}
+
+	/**
+	 * @param mixed $serialization
+	 *
+	 * @return TermList
+	 * @throws DeserializationException
+	 */
+	public function deserialize( $serialization ) {
+		$this->assertCanDeserialize( $serialization );
+
+		return $this->getDeserialized( $serialization );
+	}
+
+	/**
+	 * @param array $serialization
+	 *
+	 * @return TermList
+	 */
+	private function getDeserialized( $serialization ) {
+		$termList = new TermList();
+		foreach( $serialization as $termSerialization ) {
+			$termList->setTerm( $this->termDeserializer->deserialize( $termSerialization ) );
+		}
+		return $termList;
+	}
+
+	/**
+	 * @param array $serialization
+	 */
+	private function assertCanDeserialize( $serialization ) {
+		if ( !is_array( $serialization ) ) {
+			throw new DeserializationException( 'The term list serialization should be an array' );
+		}
+	}
+
+}
---
+++ lib/packages/wikibase/data-model-serialization/tests/unit/Deserializers/TermListDeserializerTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Tests\Wikibase\DataModel\Deserializers;
+
+use Deserializers\Deserializer;
+use Wikibase\DataModel\Deserializers\TermDeserializer;
+use Wikibase\DataModel\Deserializers\TermListDeserializer;
+use Wikibase\DataModel\Term\Term;
+use Wikibase\DataModel\Term\TermList;
+
+/**
+ * @covers Wikibase\DataModel\Deserializers\TermListDeserializer
+ *
+ * @licence GNU GPL v2+
+ * @author Adam Shorland
+ */
+class TermListDeserializerTest extends DeserializerBaseTest {
+
+	/**
+	 * @return Deserializer
+	 */
+	public function buildDeserializer() {
+		return new TermListDeserializer( New TermDeserializer() );
+	}
+
+	/**
+	 * @return array[] things that are deserialized by the deserializer
+	 */
+	public function deserializableProvider() {
+		return array(
+			array(
+				'en' => array( 'language' => 'en', 'value' => 'FooBar' ),
+			),
+			array(
+				'en' => array( 'language' => 'en', 'value' => 'FooBar' ),
+				'de' => array( 'language' => 'de', 'value' => 'ItsALydia' ),
+			),
+		);
+	}
+
+	/**
+	 * @return array[] things that aren't deserialized by the deserializer
+	 */
+	public function nonDeserializableProvider() {
+		return array(
+			array(
+				'en' => array( 'language' => 'en', 'value' => 'FooBar', 'source' => 'fr' ),
+			),
+			array(
+				'en' => array( 'language' => 'en' ),
+			),
+		);
+	}
+
+	/**
+	 * @return array[] an array of array( object deserialized, serialization )
+	 */
+	public function deserializationProvider() {
+		return array(
+			array(
+				new TermList( array() ),
+				array(),
+			),
+			array(
+				new TermList( array( new Term( 'en', 'Lama' ) ) ),
+				array( 'en' => array( 'language' => 'en', 'value' => 'Lama' ) ),
+			),
+			array(
+				new TermList( array(
+					new Term( 'en', 'Lama' ),
+					new Term( 'de', 'Delama' ),
+				) ),
+				array(
+					'en' => array( 'language' => 'en', 'value' => 'Lama' ),
+					'de' => array( 'language' => 'de', 'value' => 'Delama' ),
+				),
+			),
+		);
+	}
+}
