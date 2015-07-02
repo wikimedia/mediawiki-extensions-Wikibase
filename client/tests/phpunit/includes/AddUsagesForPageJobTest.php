5ace033993af2c035f488904590e724aae86273c
daniel <daniel.kinzler@wikimedia.de>
2015-07-02 21:59:41 +0200

    On page views, update usage tracking via the job queue.
    In some cases, we need to update the usage tracking table during normal
    page views. In this case, we should not write to the database directly,
    but schedule a job instead.
    
    Bug: T103429
    Change-Id: Ied1db3f2ab802ac7d7638b63844598374bd59812

---
+++ client/tests/phpunit/includes/AddUsagesForPageJobTest.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace Wikibase\Client\Test\Store;
+
+use Title;
+use Wikibase\Client\Store\AddUsagesForPageJob;
+use Wikibase\Client\Usage\EntityUsage;
+use Wikibase\DataModel\Entity\BasicEntityIdParser;
+use Wikibase\DataModel\Entity\ItemId;
+
+/**
+ * @covers Wikibase\Client\Store\AddUsagesForPageJob
+ *
+ * @group Wikibase
+ * @group WikibaseClient
+ * @group WikibaseUsageTracking
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ */
+class AddUsagesForPageJobTest extends \PHPUnit_Framework_TestCase {
+
+	public function provideConstructor_failure() {
+		$pageId = 17;
+		$usageQ5X = new EntityUsage( new ItemId( 'Q5' ), 'X' );
+		$usages = array( $usageQ5X->asArray() );
+		$touched = '20150101000000';
+
+		return array(
+			'empty' => array( array() ),
+
+			'$pageId is missing' => array( array(
+				'usages' => $usages,
+				'touched' => $touched,
+			) ),
+			'$pageId is not an int' => array( array(
+				'pageId' => 'foo',
+				'usages' => $usages,
+				'touched' => $touched,
+			) ),
+			'$pageId is zero' => array( array(
+				'pageId' => 0,
+				'usages' => $usages,
+				'touched' => $touched,
+			) ),
+
+			'$usages is missing' => array( array(
+				'pageId' => $pageId,
+				'touched' => $touched,
+			) ),
+			'$usages is not an array' => array( array(
+				'pageId' => $pageId,
+				'usages' => 'xxx',
+				'touched' => $touched,
+			) ),
+			'$usages is empty' => array( array(
+				'pageId' => $pageId,
+				'usages' => array(),
+				'touched' => $touched,
+			) ),
+			'$usages contains crap' => array( array(
+				'pageId' => $pageId,
+				'usages' => array( 1, 2, 3 ),
+				'touched' => $touched,
+			) ),
+
+			'$touched is missing' => array( array(
+				'pageId' => $pageId,
+				'usages' => $usages,
+			) ),
+			'$touched is not a string' => array( array(
+				'pageId' => $pageId,
+				'usages' => $usages,
+				'touched' => 23,
+			) ),
+			'$touched is empty' => array( array(
+				'pageId' => $pageId,
+				'usages' => $usages,
+				'touched' => '',
+			) ),
+		);
+	}
+
+	/**
+	 * @dataProvider provideConstructor_failure
+	 * @param array $params
+	 */
+	public function testConstructor_failure( $params ) {
+		$this->setExpectedException( 'InvalidArgumentException' );
+
+		$title = Title::makeTitle( NS_MAIN, 'Foo' );
+		new AddUsagesForPageJob( $title, $params );
+	}
+
+	public function testRun() {
+		$usageQ5X = new EntityUsage( new ItemId( 'Q5' ), 'X' );
+		$params = array(
+			'pageId' => 17,
+			'usages' => array( $usageQ5X->asArray() ),
+			'touched' => '20150101000000',
+		);
+
+		$usageUpdater = $this->getMockBuilder( 'Wikibase\Client\Store\UsageUpdater' )
+			->disableOriginalConstructor()
+			->getMock();
+
+		$usageUpdater->expects( $this->once() )
+			->method( 'addUsagesForPage' )
+			->with(
+				$params['pageId'],
+				array( $usageQ5X ),
+				$params['touched']
+			);
+
+		$title = Title::makeTitle( NS_MAIN, 'Foo' );
+		$job = new AddUsagesForPageJob( $title, $params );
+		$job->overrideServices( $usageUpdater, new BasicEntityIdParser() );
+
+		$job->run();
+	}
+
+	public function testNewSpec() {
+		$usageQ5X = new EntityUsage( new ItemId( 'Q5' ), 'X' );
+
+		$title = Title::makeTitle( NS_MAIN, 'Foo' );
+		$title->resetArticleID( 17 );
+
+		$touched = '20150101000000';
+		$usages = array( $usageQ5X );
+
+		$spec = AddUsagesForPageJob::newSpec( $title, $usages, $touched );
+
+		$params = array(
+			'pageId' => $title->getArticleID(),
+			'usages' => array( $usageQ5X->asArray() ),
+			'touched' => '20150101000000',
+		);
+
+		$this->assertEquals( 'wikibase-addUsagesForPage', $spec->getType() );
+		$this->assertEquals( $title->getFullText(), $spec->getTitle()->getFullText() );
+		$this->assertEquals( $params, $spec->getParams() );
+	}
+
+}
