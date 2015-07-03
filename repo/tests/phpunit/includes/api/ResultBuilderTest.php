17301831ed70b8acb1a0ff5e5037b105ac7ec671
addshore <addshorewiki@gmail.com>
2015-07-03 19:41:30 +0200

    Use DM Serializetion in RB for references
    Change-Id: If89d508e3e27b5e1be7b69922aaa163c2d32e125

--- repo/tests/phpunit/includes/api/ResultBuilderTest.php
+++ repo/tests/phpunit/includes/api/ResultBuilderTest.php
@@ -65,8 +65,8 @@ class ResultBuilderTest extends \PHPUnit_Framework_TestCase {
 		$mockPropertyDataTypeLookup = $this->getMock( '\Wikibase\DataModel\Entity\PropertyDataTypeLookup' );
 		$mockPropertyDataTypeLookup->expects( $this->any() )
 			->method( 'getDataTypeIdForProperty' )
-			->will( $this->returnCallback( function( $propertyId ) {
-				return 'DtIdFor_' . $propertyId;
+			->will( $this->returnCallback( function( PropertyId $propertyId ) {
+				return 'DtIdFor_' . $propertyId->getSerialization();
 			} ) );
 
 		// @todo inject EntityFactory and SiteStore
@@ -82,6 +82,7 @@ class ResultBuilderTest extends \PHPUnit_Framework_TestCase {
 			$libSerializerFactory,
 			$serializerFactory,
 			new MockSiteStore(),
+			$mockPropertyDataTypeLookup,
 			$indexedMode
 		);
 
@@ -861,31 +862,73 @@ class ResultBuilderTest extends \PHPUnit_Framework_TestCase {
 		);
 	}
 
-	public function testAddReference() {
-		$result = $this->getDefaultResult();
-		$reference = new Reference( new SnakList( array( new PropertyValueSnak( new PropertyId( 'P12' ), new StringValue( 'stringVal' ) ) ) ) );
-		$hash = $reference->getHash();
-		$expected = array(
-			'reference' => array(
-				'hash' => $hash,
-				'snaks' => array(
-					'P12' => array(
-						array(
-							'snaktype' => 'value',
-							'property' => 'P12',
-							'datavalue' => array(
-								'value' => 'stringVal',
-								'type' => 'string',
+	public function provideAddReference() {
+		return array(
+			array(
+				false,
+				array(
+					'reference' => array(
+						'hash' => 'de52176deca6dd967b2a4122c96766089c1f793a',
+						'snaks' => array(
+							'P12' => array(
+								array(
+									'snaktype' => 'value',
+									'property' => 'P12',
+									'datavalue' => array(
+										'value' => 'stringVal',
+										'type' => 'string',
+									),
+									'datatype' => 'DtIdFor_P12',
+								)
 							),
-							'datatype' => 'DtIdFor_P12',
-						)
+						),
+						'snaks-order' => array( 'P12' ),
 					),
-				),
-				'snaks-order' => array( 'P12' ),
+				)
+			),
+			array(
+				true,
+				array(
+					'reference' => array(
+						'hash' => 'de52176deca6dd967b2a4122c96766089c1f793a',
+						'snaks' => array(
+							array(
+								'id' => 'P12',
+								array(
+									'snaktype' => 'value',
+									'property' => 'P12',
+									'datavalue' => array(
+										'value' => 'stringVal',
+										'type' => 'string',
+									),
+									'datatype' => 'DtIdFor_P12',
+								),
+								'_element' => 'snak',
+							),
+							'_element' => 'property',
+						),
+						'snaks-order' => array(
+							'P12',
+							'_element' => 'property',
+						),
+					),
+				)
 			),
 		);
+	}
 
-		$resultBuilder = $this->getResultBuilder( $result );
+	/**
+	 * @dataProvider provideAddReference
+	 */
+	public function testAddReference( $isRawMode, $expected ) {
+		$result = $this->getDefaultResult();
+		$reference = new Reference(
+			new SnakList( array(
+				new PropertyValueSnak( new PropertyId( 'P12' ), new StringValue( 'stringVal' ) )
+			) )
+		);
+
+		$resultBuilder = $this->getResultBuilder( $result, null, $isRawMode );
 		$resultBuilder->addReference( $reference );
 
 		$data = $result->getResultData();
