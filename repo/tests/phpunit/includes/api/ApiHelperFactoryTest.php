ec545eb1631d7b97dd214e9cb2b761e8c954b3e2
addshore <addshorewiki@gmail.com>
2015-07-04 18:42:43 +0200

    Factor EntitySaveHelper out of ApiWikibase
    Change-Id: Ie426c1828a69623c9cced688eb7bf7992a8b437f

--- repo/tests/phpunit/includes/api/ApiHelperFactoryTest.php
+++ repo/tests/phpunit/includes/api/ApiHelperFactoryTest.php
@@ -23,12 +23,24 @@ class ApiHelperFactoryTest extends \PHPUnit_Framework_TestCase {
 		$exceptionLocalizer = $this->getMock( 'Wikibase\Lib\Localizer\ExceptionLocalizer' );
 		$dataTypeLookup = $this->getMock( 'Wikibase\DataModel\Entity\PropertyDataTypeLookup' );
 		$entityFactory = WikibaseRepo::getDefaultInstance()->getEntityFactory();
+		$summaryFormatter = $this->getMockBuilder( 'Wikibase\SummaryFormatter' )
+			->disableOriginalConstructor()->getMock();
+		$entityRevisionLookup = $this->getMock( 'Wikibase\Lib\Store\EntityRevisionLookup' );
+		$entityStore = $this->getMock( 'Wikibase\Lib\Store\EntityStore' );
+		$entityPermissionChecker = $this->getMock( 'Wikibase\Repo\Store\EntityPermissionChecker' );
+		$editFilterHookRunner = $this->getMockBuilder( 'Wikibase\Repo\Hooks\EditFilterHookRunner' )
+			->disableOriginalConstructor()->getMock();
 
 		return new ApiHelperFactory(
 			$titleLookup,
 			$exceptionLocalizer,
 			$dataTypeLookup,
-			$entityFactory
+			$entityFactory,
+			$summaryFormatter,
+			$entityRevisionLookup,
+			$entityStore,
+			$entityPermissionChecker,
+			$editFilterHookRunner
 		);
 	}
 
