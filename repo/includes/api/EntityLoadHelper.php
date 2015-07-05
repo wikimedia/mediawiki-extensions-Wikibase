95abfe706f01b14ad3cbf946188ea056f9ec2458
addshore <addshorewiki@gmail.com>
2015-07-05 13:29:12 +0200

    Factor EntityLoadHelper out of ApiWikibase
    This also:
     - adds this class to the ApiHelperFactory
     - Fully tests the method in EntityLoadHelper
     - leaves a wrapper method in ApiWikibase
    
    Another step toward getting rid of the base
    full of stuff :O
    
    Change-Id: Ib31e78c54885293c705a178314cbacdaa34e10f2

---
+++ repo/includes/api/EntityLoadHelper.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Wikibase\Api;
+
+use LogicException;
+use UsageException;
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\EntityRevision;
+use Wikibase\Lib\Store\BadRevisionException;
+use Wikibase\Lib\Store\EntityRevisionLookup;
+use Wikibase\Lib\Store\StorageException;
+use Wikibase\Lib\Store\UnresolvedRedirectException;
+
+/**
+ * Helper class for api modules to load entities.
+ *
+ * @since 0.5
+ * @licence GNU GPL v2+
+ * @author Adam Shorland
+ */
+class EntityLoadHelper {
+
+	/**
+	 * @var EntityRevisionLookup
+	 */
+	private $entityRevisionLookup;
+
+	/**
+	 * @var ApiErrorReporter
+	 */
+	private $errorReporter;
+
+	public function __construct(
+		EntityRevisionLookup $entityRevisionLookup,
+		ApiErrorReporter $errorReporter
+	) {
+		$this->entityRevisionLookup = $entityRevisionLookup;
+		$this->errorReporter = $errorReporter;
+	}
+
+	/**
+	 * Load the entity content of the given revision.
+	 *
+	 * Will fail by calling dieException() $this->errorReporter if the revision
+	 * cannot be found or cannot be loaded.
+	 *
+	 * @since 0.5
+	 *
+	 * @param EntityId $entityId EntityId of the page to load the revision for
+	 * @param int|string $revId revision to load. If not given, the current revision will be loaded.
+	 *
+	 * @throws UsageException
+	 * @throws LogicException
+	 *
+	 * @return EntityRevision
+	 */
+	public function loadEntityRevision(
+		EntityId $entityId,
+		$revId = EntityRevisionLookup::LATEST_FROM_MASTER
+	) {
+		try {
+			$revision = $this->entityRevisionLookup->getEntityRevision( $entityId, $revId );
+
+			if ( !$revision ) {
+				$this->errorReporter->dieError(
+					'Entity ' . $entityId->getSerialization() . ' not found',
+					'cant-load-entity-content' );
+			}
+
+			return $revision;
+		} catch ( UnresolvedRedirectException $ex ) {
+			$this->errorReporter->dieException( $ex, 'unresolved-redirect' );
+		} catch ( BadRevisionException $ex ) {
+			$this->errorReporter->dieException( $ex, 'nosuchrevid' );
+		} catch ( StorageException $ex ) {
+			$this->errorReporter->dieException( $ex, 'cant-load-entity-content' );
+		}
+
+		throw new LogicException( 'ApiErrorReporter::dieError did not throw a UsageException' );
+	}
+
+}
