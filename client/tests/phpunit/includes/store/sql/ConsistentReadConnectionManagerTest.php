93a4cc197d940f7a9e0c68835afe6924b2fc20c0
addshore <addshorewiki@gmail.com>
2015-07-10 11:30:40 +0100

    Fix Client tests due to core change with IDatabase interface
    Tests were broken due to commit with hash
    5682eb48fa1efa64d34c8cfcc4afbb5079d4fc0c
    and with changeid
    Ia83530820f185415725c1d5f54b5172121b4938f
    
    This change simply changes we way we mock
    the IDatabase interface.
    
    Change-Id: I0d8e939d0ad3cec7e7e2669c5bd5ed7bec01d670

--- client/tests/phpunit/includes/store/sql/ConsistentReadConnectionManagerTest.php
+++ client/tests/phpunit/includes/store/sql/ConsistentReadConnectionManagerTest.php
@@ -17,11 +17,7 @@ use Wikibase\Client\Store\Sql\ConsistentReadConnectionManager;
 class ConsistentReadConnectionManagerTest extends \PHPUnit_Framework_TestCase {
 
 	private function getConnectionMock() {
-		$connection = $this->getMockBuilder( 'IDatabase' )
-			->setMethods( array( 'startAtomic', 'endAtomic', 'rollback' ) )
-			->getMock();
-
-		return $connection;
+		return $this->getMock( 'IDatabase' );
 	}
 
 	private function getLoadBalancerMock() {
