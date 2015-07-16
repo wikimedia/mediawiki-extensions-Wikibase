cb2ec102de6a609141259e462c905c6a3a905836
addshore <addshorewiki@gmail.com>
2015-07-16 23:38:50 +0100

    Implement SerializationModifier
    This will be used:
     - In result builder to do things such as remove
       all reference snak hashes
     - In result builder to insert / inject datatypes
       into snaks (for now)
     - In result builder modify serialization for
       currently needed raw / XML mode
    
    Something like this could perhaps be factored out
    into a library as well (I couldn't find anything
    on packagist that did this yet) or used in more places
    than the api
    
    We could further improve this by instead of using
    random callbacks and a modifer object (or something)
    
    Change-Id: I21ca5e7a37d199ffd436dd19722fb5ee4efb695e

---
+++ repo/includes/api/SerializationModifier.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace Wikibase\Repo\Api;
+
+/**
+ * Class which can be used to easily modify serializations and arrays.
+ *
+ * This is in Repo\Api to be used to modify the Api output serialization.
+ * This could easily be used in other places if needed.
+ * This could also easily be factored out into a library.
+ *
+ * @since 0.5
+ * @author Adam Shorland
+ */
+class SerializationModifier {
+
+	/**
+	 * @param array $array the array to modify
+	 * @param null|string $path the path that we want to modify.
+	 *     Element keys should be seperated with / characters.
+	 *     * characters can be used to match all keys at a given level.
+	 *     null can be used to modify $array directly.
+	 *     Examples:
+	 *         null
+	 *         'foo/*'
+	 *         'root/entities/*\/statement/references/*\/snaks/*'
+	 * @param callback $callback
+	 *  Callback accepts 1 parameter which is the element to touch
+	 *  Callback should return the altered element
+	 *
+	 * @returns array the altered array
+	 */
+	public function modifyUsingCallback( array $array, $path, $callback ) {
+		$elementsToTouch = $this->getElementsMatchingPath(
+			$array,
+			$this->getPathParts( $path )
+		);
+
+		foreach ( $elementsToTouch as $key => $element ) {
+			$newElement = call_user_func( $callback, $element );
+			$this->setArrayValueAtKey( $array, $key, $newElement );
+		}
+
+		return $array;
+	}
+
+	/**
+	 * @param array $array
+	 * @param string $path
+	 * @param mixed $value
+	 */
+	private function setArrayValueAtKey( &$array, $path, $value ) {
+		$current = &$array;
+		$pathParts = $this->getPathParts( $path );
+		foreach ( $pathParts as $key ) {
+			$current = &$current[$key];
+		}
+		$current = $value;
+	}
+
+	/**
+	 * Method to get elements that match the path given.
+	 * This is called recursively along with getElementsForAllKeys and getElementsForKey
+	 * The number of calls depends on the depth of the array.
+	 *
+	 * @param array $array
+	 * @param string[] $pathElements
+	 * @param string $currentPath
+	 *
+	 * @return array
+	 */
+	private function getElementsMatchingPath( array $array, array $pathElements, $currentPath = '' ) {
+		$elements = array();
+
+		if ( empty( $pathElements ) ) {
+			$elements[$currentPath] = $array;
+		} else {
+			$currentKey = array_shift( $pathElements );
+
+			if ( $currentKey === '*' ) {
+				$elements = array_merge(
+					$elements,
+					$this->getElementsForAllKeys( $array, $pathElements, $currentPath )
+				);
+			} else {
+				$elements = array_merge(
+					$elements,
+					$this->getElementsForKey( $array, $pathElements, $currentPath, $currentKey )
+				);
+			}
+
+		}
+
+		return $elements;
+	}
+
+	/**
+	 * @param array $array
+	 * @param string[] $pathElements
+	 * @param string $currentPath
+	 *
+	 * @return array
+	 */
+	private function getElementsForAllKeys( array $array, array $pathElements, $currentPath ) {
+		$elements = array();
+
+		foreach ( array_keys( $array ) as $arrayKey ) {
+			$elements = array_merge(
+				$elements,
+				$this->getElementsForKey( $array, $pathElements, $currentPath, $arrayKey )
+			);
+		}
+
+		return $elements;
+	}
+
+	/**
+	 * @param array $array
+	 * @param string[] $pathElements
+	 * @param string $currentPath
+	 * @param string $key
+	 *
+	 * @return array
+	 */
+	private function getElementsForKey( array $array, array $pathElements, $currentPath, $key ) {
+		$elements = array();
+
+		if ( isset( $array[$key] ) ) {
+			$thisPath = $this->getJoinedPath( $currentPath, $key );
+
+			if ( is_array( $array[$key] ) ) {
+				$elements = array_merge(
+					$elements,
+					$this->getElementsMatchingPath( $array[$key], $pathElements, $thisPath )
+				);
+			} else {
+				$elements[$thisPath] = $array[$key];
+			}
+
+		}
+
+		return $elements;
+	}
+
+	/**
+	 * @param string $prefix
+	 * @param string $key
+	 *
+	 * @return string
+	 */
+	private function getJoinedPath( $prefix, $key ) {
+		if ( $prefix === '' ) {
+			return $key;
+		}
+
+		return $prefix . '/' . $key;
+	}
+
+	/**
+	 * @param null|string $path
+	 *
+	 * @return string[]
+	 */
+	private function getPathParts( $path ) {
+		if ( $path === null || $path === '' ) {
+			return array();
+		}
+
+		return explode( '/', $path );
+	}
+
+}
