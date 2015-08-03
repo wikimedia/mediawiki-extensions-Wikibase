1a337a883f7bbb46e6465a454dcb2c5bac7c51bf
jeroendedauw <jeroendedauw@gmail.com>
2015-08-03 21:48:06 +0200

    Remove file alias for dispatchChanges maintenance script
    Only merge if the cron jobs have been updated
    
    Change-Id: Id49d2c773afa7292b1e7c3dd5cbc0d0e0fb569c5

--- lib/maintenance/dispatchChanges.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-// Placeholder for backwards compatibility.
-// Remove once all scripts and cron jobs have been updated accordingly.
-require __DIR__ . '/../../repo/maintenance/dispatchChanges.php';
