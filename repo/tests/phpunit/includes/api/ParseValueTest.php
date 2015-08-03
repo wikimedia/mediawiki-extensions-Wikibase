4ff65368253a2c70a2c6d7fefeef8499663b8ce9
daniel <daniel.kinzler@wikimedia.de>
2015-08-03 18:31:13 +0200

    Simplify DataTypeFactory
    Requires new release of the DataTypes component,
    see https://github.com/wmde/DataTypes/pull/34
    
    Change-Id: I23f35b0bda8f20f502bb5b981e8ad438f04516b5

--- repo/tests/phpunit/includes/api/ParseValueTest.php
+++ repo/tests/phpunit/includes/api/ParseValueTest.php
@@ -50,9 +50,9 @@ class ParseValueTest extends \PHPUnit_Framework_TestCase {
 		);
 
 		$dataTypeFactory = new DataTypeFactory( array(
-			'string' => array( $this, 'newStringDataType' ),
-			'url' => array( $this, 'newStringDataType' ),
-			'globe-coordinate' => array( $this, 'newCoordinateDataType' ),
+			'string' => 'string',
+			'url' => 'string',
+			'globe-coordinate' => 'globecoordinate',
 		) );
 
 		$valueParserFactory = new ValueParserFactory( array(
@@ -83,12 +83,6 @@ class ParseValueTest extends \PHPUnit_Framework_TestCase {
 		return array( new RegexValidator( '/INVALID/', true, 'no-kittens' ) );
 	}
 
-	public function newStringDataType( $name ) {
-		return new DataType( $name, 'string', array() );
-	}
-
-	public function newCoordinateDataType( $name ) {
-		return new DataType( $name, 'globecoordinate', array() );
 	}
 
 	public function newNullParser() {
