a08cd4dee13f50130c8ead44a6d56539bbbe1df2
daniel <daniel.kinzler@wikimedia.de>
2015-08-03 19:48:51 +0200

    Switch ValueParser definition to $wgWikibaseDataTypeDefinitions
    Change-Id: I32e129badd42febd8d4c9055a444c2471399569a

--- repo/includes/ValueParserFactory.php
+++ repo/includes/ValueParserFactory.php
@@ -20,17 +20,17 @@ class ValueParserFactory {
 	/**
 	 * Maps parser id to ValueParser class or builder callback.
 	 *
-	 * @since 0.1
+	 * @since 0.5
 	 *
-	 * @var array
+	 * @var callable[]
 	 */
 	protected $parsers = array();
 
 	/**
-	 * @since 0.1
+	 * @since 0.5
 	 *
-	 * @param string|callable[] $valueParsers An associative array mapping parser ids to
-	 *        class names or callable builders.
+	 * @param callable[] $valueParsers An associative array mapping parser ids to
+	 *        factory functions.
 	 *
 	 * @throws InvalidArgumentException
 	 */
@@ -40,8 +40,8 @@ class ValueParserFactory {
 				throw new InvalidArgumentException( 'Parser id needs to be a string' );
 			}
 
-			if ( !is_string( $parserBuilder ) && !is_callable( $parserBuilder ) ) {
-				throw new InvalidArgumentException( 'Parser class needs to be a class name or callable' );
+			if ( !is_callable( $parserBuilder ) ) {
+				throw new InvalidArgumentException( 'Parser class needs to be a callable' );
 			}
 
 			$this->parsers[$parserId] = $parserBuilder;
@@ -57,24 +57,6 @@ class ValueParserFactory {
 	 */
 	public function getParserIds() {
 		return array_keys( $this->parsers );
-	}
-
-	/**
-	 * Returns the parser builder (class name or callable) for $parserId, or null if
-	 * no builder was registered for that id.
-	 *
-	 * @since 0.1
-	 *
-	 * @param string $parserId
-	 *
-	 * @return string|callable|null
-	 */
-	public function getParserBuilder( $parserId ) {
-		if ( array_key_exists( $parserId, $this->parsers ) ) {
-			return $this->parsers[$parserId];
-		}
-
-		return null;
 	}
 
 	/**
@@ -100,20 +82,15 @@ class ValueParserFactory {
 	}
 
 	/**
-	 * @param string|callable $builder Either a classname of an implementation of ValueParser,
-	 *        or a callable that returns a ValueParser. $options will be passed to the constructor
-	 *        or callable, respectively.
+	 * @param callable $builder A callable that returns a ValueParser.
+	 *        $options will be passed to the constructor or callable, respectively.
 	 * @param ParserOptions $options
 	 *
 	 * @throws LogicException if the builder did not create a ValueParser
 	 * @return ValueParser
 	 */
 	private function instantiateParser( $builder, ParserOptions $options ) {
-		if ( is_string( $builder ) ) {
-			$parser = new $builder( $options );
-		} else {
-			$parser = call_user_func( $builder, $options );
-		}
+		$parser = call_user_func( $builder, $options );
 
 		if ( !( $parser instanceof ValueParser ) ) {
 			throw new LogicException( "Invalid parser builder, did not create an instance of ValueParser." );
