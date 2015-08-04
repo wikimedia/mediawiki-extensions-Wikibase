99f8264ecd92b6383bb645880cda22d44f44997d
paladox <thomasmulhall410@yahoo.com>
2015-08-04 14:23:06 +0100

    Wikibase: Convet lib, client, repo to extension.json, empty php entry point
    Bug: T88258
    Change-Id: Idb57232ea453be433d414d1c11bf422f200fccf4

--- client/WikibaseClient.php
+++ client/WikibaseClient.php
@@ -38,21 +38,12 @@
 
 // @codingStandardsIgnoreFile
 
-if ( !defined( 'MEDIAWIKI' ) ) {
-	die( "Not an entry point.\n" );
-}
-
 if ( defined( 'WBC_VERSION' ) ) {
 	// Do not initialize more than once.
 	return;
 }
 
 define( 'WBC_VERSION', '0.5 alpha' );
-
-// Needs to be 1.26c because version_compare() works in confusing ways.
-if ( version_compare( $GLOBALS['wgVersion'], '1.26c', '<' ) ) {
-	die( "<b>Error:</b> Wikibase requires MediaWiki 1.26 or above.\n" );
-}
 
 define( 'WBC_DIR', __DIR__ );
 
@@ -66,134 +57,18 @@ if ( !defined( 'WBL_VERSION' ) ) {
 }
 
 call_user_func( function() {
-	global $wgExtensionCredits, $wgExtensionMessagesFiles, $wgHooks;
-	global $wgAPIMetaModules, $wgAPIPropModules, $wgSpecialPages, $wgResourceModules;
-	global $wgWBClientSettings, $wgRecentChangesFlags, $wgMessagesDirs;
-	global $wgJobClasses, $wgWBClientDataTypes;
-
-	$wgExtensionCredits['wikibase'][] = array(
-		'path' => __DIR__,
-		'name' => 'Wikibase Client',
-		'version' => WBC_VERSION,
-		'author' => array(
-			'The Wikidata team', // TODO: link?
-		),
-		'url' => 'https://www.mediawiki.org/wiki/Extension:Wikibase_Client',
-		'descriptionmsg' => 'wikibase-client-desc'
-	);
-
-	$wgWBClientDataTypes = require ( __DIR__ . '/../lib/WikibaseLib.datatypes.php' );
-	$clientDatatypes = require ( __DIR__ . '/WikibaseClient.datatypes.php' );
-
-	// merge WikibaseClient.datatypes.php into $wgWBClientDataTypes
-	foreach ( $clientDatatypes as $type => $clientDef ) {
-		$baseDef = isset( $wgWBClientDataTypes[$type] ) ? $wgWBClientDataTypes[$type] : array();
-		$wgWBClientDataTypes[$type] = array_merge( $baseDef, $clientDef );
+	if ( function_exists( 'wfLoadExtension' ) ) {
+		wfLoadExtension( 'WikibaseClient', __DIR__.'/extension.json' );
+		// Keep i18n globals so mergeMessageFileList.php doesn't break
+		$GLOBALS['wgMessagesDirs']['wikibaseclient'] = __DIR__ . '/i18n';
+		$GLOBALS['wgExtensionMessagesFiles']['Wikibaseclientalias'] = __DIR__ . '/WikibaseClient.i18n.alias.php';
+		$GLOBALS['wgExtensionMessagesFiles']['wikibaseclientmagic'] = __DIR__ . '/WikibaseClient.i18n.magic.php';
+		/* wfWarn(
+			'Deprecated PHP entry point used for Wikibase Client extension. Please use wfLoadExtension instead, ' .
+			'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+		); */
+		return;
+	} else {
+		die( 'This version of the Wikibase Client extension requires MediaWiki 1.25+' );
 	}
-
-	// i18n
-	$wgMessagesDirs['wikibaseclient'] = __DIR__ . '/i18n';
-	$wgExtensionMessagesFiles['Wikibaseclientalias'] = __DIR__ . '/WikibaseClient.i18n.alias.php';
-	$wgExtensionMessagesFiles['wikibaseclientmagic'] = __DIR__ . '/WikibaseClient.i18n.magic.php';
-
-	// Hooks
-	$wgHooks['UnitTestsList'][] = '\Wikibase\ClientHooks::registerUnitTests';
-	$wgHooks['BaseTemplateToolbox'][] = '\Wikibase\ClientHooks::onBaseTemplateToolbox';
-	$wgHooks['OldChangesListRecentChangesLine'][] = '\Wikibase\ClientHooks::onOldChangesListRecentChangesLine';
-	$wgHooks['OutputPageParserOutput'][] = '\Wikibase\Client\Hooks\SidebarHookHandlers::onOutputPageParserOutput';
-	$wgHooks['SkinTemplateGetLanguageLink'][] = '\Wikibase\Client\Hooks\SidebarHookHandlers::onSkinTemplateGetLanguageLink';
-	$wgHooks['ContentAlterParserOutput'][] = '\Wikibase\Client\Hooks\ParserOutputUpdateHookHandlers::onContentAlterParserOutput';
-	$wgHooks['SidebarBeforeOutput'][] = '\Wikibase\Client\Hooks\SidebarHookHandlers::onSidebarBeforeOutput';
-	$wgHooks['LinksUpdateComplete'][] = '\Wikibase\Client\Hooks\DataUpdateHookHandlers::onLinksUpdateComplete';
-	$wgHooks['ArticleDeleteComplete'][] = '\Wikibase\Client\Hooks\DataUpdateHookHandlers::onArticleDeleteComplete';
-	$wgHooks['ParserCacheSaveComplete'][] = '\Wikibase\Client\Hooks\DataUpdateHookHandlers::onParserCacheSaveComplete';
-	$wgHooks['ParserFirstCallInit'][] = '\Wikibase\ClientHooks::onParserFirstCallInit';
-	$wgHooks['MagicWordwgVariableIDs'][] = '\Wikibase\ClientHooks::onMagicWordwgVariableIDs';
-	$wgHooks['ParserGetVariableValueSwitch'][] = '\Wikibase\ClientHooks::onParserGetVariableValueSwitch';
-	$wgHooks['SkinTemplateOutputPageBeforeExec'][] = '\Wikibase\ClientHooks::onSkinTemplateOutputPageBeforeExec';
-	$wgHooks['SpecialMovepageAfterMove'][] = '\Wikibase\Client\Hooks\MovePageNotice::onSpecialMovepageAfterMove';
-	$wgHooks['GetPreferences'][] = '\Wikibase\ClientHooks::onGetPreferences';
-	$wgHooks['BeforePageDisplay'][] = '\Wikibase\ClientHooks::onBeforePageDisplay';
-	$wgHooks['BeforePageDisplay'][] = '\Wikibase\ClientHooks::onBeforePageDisplayAddJsConfig';
-	$wgHooks['ScribuntoExternalLibraries'][] = '\Wikibase\ClientHooks::onScribuntoExternalLibraries';
-	$wgHooks['InfoAction'][] = '\Wikibase\ClientHooks::onInfoAction';
-	$wgHooks['TitleMoveComplete'][] = '\Wikibase\Client\Hooks\UpdateRepoHookHandlers::onTitleMoveComplete';
-	$wgHooks['BaseTemplateAfterPortlet'][] = '\Wikibase\ClientHooks::onBaseTemplateAfterPortlet';
-	$wgHooks['GetBetaFeaturePreferences'][] = '\Wikibase\ClientHooks::onGetBetaFeaturePreferences';
-	$wgHooks['ArticleDeleteComplete'][] = '\Wikibase\Client\Hooks\UpdateRepoHookHandlers::onArticleDeleteComplete';
-	$wgHooks['ArticleDeleteAfterSuccess'][] = '\Wikibase\ClientHooks::onArticleDeleteAfterSuccess';
-	$wgHooks['ParserLimitReportFormat'][] = '\Wikibase\Client\Hooks\ParserLimitHookHandlers::onParserLimitReportFormat';
-	$wgHooks['ParserLimitReportPrepare'][] = '\Wikibase\Client\Hooks\ParserLimitHookHandlers::onParserLimitReportPrepare';
-	$wgHooks['FormatAutocomments'][] = '\Wikibase\ClientHooks::onFormat';
-
-	// recent changes / watchlist hooks
-	$wgHooks['ChangesListSpecialPageFilters'][] = '\Wikibase\Client\Hooks\ChangesListSpecialPageHooksHandler::onChangesListSpecialPageFilters';
-	$wgHooks['ChangesListSpecialPageQuery'][] = '\Wikibase\Client\Hooks\ChangesListSpecialPageHooksHandler::onChangesListSpecialPageQuery';
-
-	// update hooks
-	$wgHooks['LoadExtensionSchemaUpdates'][] = '\Wikibase\Client\Usage\Sql\SqlUsageTrackerSchemaUpdater::onSchemaUpdate';
-
-	// job classes
-	$wgJobClasses['wikibase-addUsagesForPage'] = 'Wikibase\Client\Store\AddUsagesForPageJob';
-
-	// api modules
-	$wgAPIMetaModules['wikibase'] = array(
-		'class' => 'Wikibase\ApiClientInfo',
-		'factory' => function( ApiQuery $apiQuery, $moduleName ) {
-			return new Wikibase\ApiClientInfo(
-				Wikibase\Client\WikibaseClient::getDefaultInstance()->getSettings(),
-				$apiQuery,
-				$moduleName
-			);
-		}
-	);
-
-	$wgAPIPropModules['pageterms'] = array(
-		'class' => 'Wikibase\Client\Api\PageTerms',
-		'factory' => function ( ApiQuery $query, $moduleName ) {
-			// FIXME: HACK: make pageterms work directly on entity pages on the repo.
-			// We should instead use an EntityIdLookup that combines the repo and the client
-			// implementation, see T115117.
-			// NOTE: when changing repo and/or client integration, remember to update the
-			// self-documentation of the API module in the "apihelp-query+pageterms-description"
-			// message and the PageTerms::getExamplesMessages() method.
-			if ( defined( 'WB_VERSION' ) ) {
-				$repo = \Wikibase\Repo\WikibaseRepo::getDefaultInstance();
-				$termIndex = $repo->getStore()->getTermIndex();
-				$entityIdLookup = $repo->getEntityContentFactory();
-			} else {
-				$client = \Wikibase\Client\WikibaseClient::getDefaultInstance();
-				$termIndex = $client->getStore()->getTermIndex();
-				$entityIdLookup = $client->getStore()->getEntityIdLookup();
-			}
-
-			return new Wikibase\Client\Api\PageTerms(
-				$termIndex,
-				$entityIdLookup,
-				$query,
-				$moduleName
-			);
-		}
-	);
-
-	// Special page registration
-	$wgSpecialPages['UnconnectedPages'] = 'Wikibase\Client\Specials\SpecialUnconnectedPages';
-	$wgSpecialPages['PagesWithBadges'] = 'Wikibase\Client\Specials\SpecialPagesWithBadges';
-	$wgHooks['wgQueryPages'][] = 'Wikibase\ClientHooks::onwgQueryPages';
-
-	// Resource loader modules
-	$wgResourceModules = array_merge(
-		$wgResourceModules,
-		include __DIR__ . '/resources/Resources.php'
-	);
-
-	$wgWBClientSettings = array_merge(
-		require __DIR__ . '/../lib/config/WikibaseLib.default.php',
-		require __DIR__ . '/config/WikibaseClient.default.php'
-	);
-
-	$wgRecentChangesFlags['wikibase-edit'] = array(
-		'letter' => 'wikibase-rc-wikibase-edit-letter',
-		'title' => 'wikibase-rc-wikibase-edit-title'
-	);
 } );
