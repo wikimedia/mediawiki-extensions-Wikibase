7d7eaf69232924259ea3f38484646afbbac05f32
paladox <thomasmulhall410@yahoo.com>
2015-08-04 14:23:06 +0100

    Wikibase: Convet lib, client, repo to extension.json, empty php entry point
    Bug: T88258
    Change-Id: Idb57232ea453be433d414d1c11bf422f200fccf4

--- Wikibase.php
+++ Wikibase.php
@@ -33,13 +33,21 @@
 if ( !array_key_exists( 'wgEnableWikibaseRepo', $GLOBALS ) || $GLOBALS['wgEnableWikibaseRepo'] ) {
 	require_once __DIR__ . '/repo/Wikibase.php';
 	if ( isset( $wgWikimediaJenkinsCI ) && $wgWikimediaJenkinsCI == true ) {
-		require_once __DIR__ . '/repo/ExampleSettings.php';
+		call_user_func(
+			function () {
+				require_once __DIR__ . '/repo/ExampleSettings.php';
+			}
+		);
 	}
 }
 
 if ( !array_key_exists( 'wgEnableWikibaseClient', $GLOBALS ) || $GLOBALS['wgEnableWikibaseClient'] ) {
 	require_once __DIR__ . '/client/WikibaseClient.php';
 	if ( isset( $wgWikimediaJenkinsCI ) && $wgWikimediaJenkinsCI == true ) {
-		require_once __DIR__ . '/client/ExampleSettings.php';
+		call_user_func(
+			function () {
+				require_once __DIR__ . '/client/ExampleSettings.php';
+			}
+		);
 	}
 }
