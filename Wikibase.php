72e4990eab72823dedd1d5d9f13776fab181bf7b
paladox <thomasmulhall410@yahoo.com>
2015-08-04 14:23:06 +0100

    [BREAKING CHANGE] Wikibase: Convet lib, client, repo to extension.json, empty php entry point
    Breaking Change:
    
    To install do the following in LocalSettings.php
    
    To use the new extension.json file do the following
    
    wfLoadExtension( 'Wikibase' );
    
    or to still use Wikibase.php as your entry point, do the following
    
    require_once __DIR__ . "/extensions/Wikibase.php";
    
    Wikibase.php or extension.json are now the default entry point, its no
    longer support to require WikibaseLib and then repo or client. It is now
    defined through The hooks file with a config.
    
    To enable repo which has lib included by default do the following in
    LocalSettings.php
    
    $wgEnableWikibaseRepo = true;
    
    or to set client which has lib as default do the following in
    LocalSettings.php
    
    $wgEnableWikibaseClient = true:
    
    Or for the config that sets bot, do the following in LocalSettings.php
    
    $wgEnableWikibaseBoth = true;
    
    This big change should make it easisar for the user to use Wikibase. Since
    he or she wont need to add all the required extensions in Wikibase. They
    only need to Require wikibase once then Use the configs.
    
    Bug: T88258
    Change-Id: Idb57232ea453be433d414d1c11bf422f200fccf4

--- Wikibase.php
+++ Wikibase.php
@@ -23,7 +23,7 @@
  */
 
 /**
- * Testing entry point. Do not use for production setups!
+ * As of MediaWiki 1.27 this is now the entry point.
  *
  * @see README.md
  * @see http://wikiba.se
@@ -31,18 +31,97 @@
  * @license GPL-2.0+
  */
 
-if ( !array_key_exists( 'wgEnableWikibaseRepo', $GLOBALS ) || $GLOBALS['wgEnableWikibaseRepo'] ) {
+if (
+	!array_key_exists( 'wgEnableWikibaseRepo', $GLOBALS ) || $GLOBALS['wgEnableWikibaseRepo'] ||
+	isset( $wgEnableWikibaseRepo ) && $wgEnableWikibaseRepo == true ||
+	isset( $GLOBALS['wgEnableWikibaseRepo'] ) && $GLOBALS['wgEnableWikibaseRepo'] == true
+) {
+	if ( (
+		!defined( 'WIKIBASE_DATAMODEL_VERSION' ) || !defined( 'Diff_VERSION' ) ||
+		!defined( 'DATAVALUES_VERSION' ) ) && is_readable( __DIR__ . '/vendor/autoload.php' )
+	) {
+		require_once __DIR__ . '/vendor/autoload.php';
+	}
+
+	require_once __DIR__ . '/lib/WikibaseLib.php';
+
+	if ( !defined( 'WIKIBASE_VIEW_VERSION' ) ) {
+		include_once __DIR__ . '/view/WikibaseView.php';
+	}
+
+	if ( !defined( 'WIKIBASE_VIEW_VERSION' ) ) {
+		throw new Exception( 'Wikibase depends on WikibaseView.' );
+	}
+
+	if ( !defined( 'PURTLE_VERSION' ) ) {
+		include_once __DIR__ . '/purtle/Purtle.php';
+	}
+
+	if ( !defined( 'PURTLE_VERSION' ) ) {
+		throw new Exception( 'Wikibase depends on Purtle.' );
+	}
+
 	require_once __DIR__ . '/repo/Wikibase.php';
+}
+
+if (
+	!array_key_exists( 'wgEnableWikibaseClient', $GLOBALS ) || $GLOBALS['wgEnableWikibaseClient'] ||
+	isset( $wgEnableWikibaseClient ) && $wgEnableWikibaseClient == true ||
+	isset( $GLOBALS['wgEnableWikibaseClient'] ) && $GLOBALS['wgEnableWikibaseClient'] == true
+) {
+	if ( (
+		!defined( 'WIKIBASE_DATAMODEL_VERSION' ) || !defined( 'Diff_VERSION' ) ||
+		!defined( 'DATAVALUES_VERSION' ) ) && is_readable( __DIR__ . '/vendor/autoload.php' )
+	) {
+		require_once __DIR__ . '/vendor/autoload.php';
+	}
+
+	wfLoadExtensions( array( 'Wikibase/lib', 'Wikibase/client' );
+
+	require_once __DIR__ . '/lib/WikibaseLib.php';
+
+	require_once __DIR__ . '/client/WikibaseClient.php';
 
 	if ( isset( $wgWikimediaJenkinsCI ) && $wgWikimediaJenkinsCI == true ) {
 		// Use example config for testing
-		require_once __DIR__ . '/repo/config/Wikibase.example.php';
+		require_once __DIR__ . '/client/config/WikibaseClient.example.php';
+		// TODO make this unncessary. Include hack to make testing work with the current code
+		require_once __DIR__ . '/client/config/WikibaseClient.jenkins.php';
 	}
 }
 
-if ( !array_key_exists( 'wgEnableWikibaseClient', $GLOBALS ) || $GLOBALS['wgEnableWikibaseClient'] ) {
-	require_once __DIR__ . '/client/WikibaseClient.php';
+if (
+	!array_key_exists( 'wgEnableWikibaseBoth', $GLOBALS ) || $GLOBALS['wgEnableWikibaseBoth'] ||
+	isset( $wgEnableWikibaseBoth ) && $wgEnableWikibaseBoth == true ||
+	isset( $GLOBALS['wgEnableWikibaseBoth'] ) && $GLOBALS['wgEnableWikibaseBoth'] == true
+) {
+	if ( (
+		!defined( 'WIKIBASE_DATAMODEL_VERSION' ) || !defined( 'Diff_VERSION' ) ||
+		!defined( 'DATAVALUES_VERSION' ) ) && is_readable( __DIR__ . '/vendor/autoload.php' )
+	) {
+		require_once __DIR__ . '/vendor/autoload.php';
+	}
 
+	require_once __DIR__ . '/lib/WikibaseLib.php';
+
+	if ( !defined( 'WIKIBASE_VIEW_VERSION' ) ) {
+		include_once __DIR__ . '/view/WikibaseView.php';
+	}
+
+	if ( !defined( 'WIKIBASE_VIEW_VERSION' ) ) {
+		throw new Exception( 'Wikibase depends on WikibaseView.' );
+	}
+
+	if ( !defined( 'PURTLE_VERSION' ) ) {
+		include_once __DIR__ . '/purtle/Purtle.php';
+	}
+
+	if ( !defined( 'PURTLE_VERSION' ) ) {
+		throw new Exception( 'Wikibase depends on Purtle.' );
+	}
+
+	require_once __DIR__ . '/repo/Wikibase.php';
+	require_once __DIR__ . '/client/WikibaseClient.php';
 	if ( isset( $wgWikimediaJenkinsCI ) && $wgWikimediaJenkinsCI == true ) {
 		// Use example config for testing
 		require_once __DIR__ . '/client/config/WikibaseClient.example.php';
