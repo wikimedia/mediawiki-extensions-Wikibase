13bf7003674c8c02eb6b0eb074149b372701982f
Marius Hoch <hoo@online.de>
2015-08-05 16:29:05 +0200

    Fix bool return value of SiteLinkTable::insertLinksInternal
    This method is supposed to return false in case anything goes
    wrong, not just die.
    We need this behavior for the rebuildItemsPerSite maintenance
    script which (tries) to re-insert all links.
    
    Change-Id: I4f47e7899a0df0142d134379edeb72ccf6f6094f

--- lib/includes/store/sql/SiteLinkTable.php
+++ lib/includes/store/sql/SiteLinkTable.php
@@ -151,8 +151,11 @@ class SiteLinkTable extends DBAccessBase implements SiteLinkStore, SiteLinkConfl
 					'ips_site_id' => $link->getSiteId(),
 					'ips_site_page' => $link->getPageName()
 				),
-				__METHOD__
+				__METHOD__,
+				array( 'IGNORE' )
 			);
+
+			$success = $success && $dbw->affectedRows() === 1;
 
 			if ( !$success ) {
 				break;
