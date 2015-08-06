b58198b28af3d02d66f8c1178f859d9b995af701
daniel <daniel.kinzler@wikimedia.de>
2015-08-06 23:54:10 +0200

    [WIP] split RDF mapping  for DataValues into individual classes.
    Bug: T110214
    Change-Id: I60ee41e74486fb8aa432d598d56ace693491a74b

---
+++ repo/includes/rdf/Values/CommonsMediaRdfBuilder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Wikibase\Rdf;
+
+use DataValues\DataValue;
+
+/**
+ * RDF mapping for commonsMedia DataValues.
+ *
+ * @since 0.5
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ * @author Stas Malyshev
+ */
+class CommonsMediaRdfBuilder extends ObjectValueRdfBuilder {
+
+	/**
+	 * @var RdfVocabulary
+	 */
+	private $vocabulary;
+
+	function __construct( RdfVocabulary $vocabulary ) {
+		$this->vocabulary = $vocabulary;
+	}
+
+	/**
+	 * @param DataValue $value
+	 *
+	 * @return string the object URI
+	 */
+	protected function getValueUri( DataValue $value ) {
+		return $this->vocabulary->getCommonsURI( $value->getValue() );
+	}
+}
