affa5930ba7c00f3bfe6b2a3a5740ca894aed7a9
addshore <addshorewiki@gmail.com>
2015-08-06 11:10:39 +0100

    Push dumpEntities members down
    Change-Id: I7eef9aa515b5dc3ea0ac4d9743cc53ba18db1ea8

--- repo/maintenance/dumpRdf.php
+++ repo/maintenance/dumpRdf.php
@@ -2,18 +2,74 @@
 
 namespace Wikibase;
 
+use SiteStore;
 use Title;
+use Wikibase\DataModel\Services\Lookup\PropertyDataTypeLookup;
 use Wikibase\Dumpers\DumpGenerator;
 use Wikibase\Dumpers\RdfDumpGenerator;
+use Wikibase\Lib\Store\EntityPrefetcher;
+use Wikibase\Lib\Store\EntityRevisionLookup;
 use Wikibase\Repo\Store\EntityPerPage;
+use Wikibase\Repo\WikibaseRepo;
 
 require_once __DIR__ . '/dumpEntities.php';
 
 class DumpRdf extends DumpScript {
 
+	/**
+	 * @var EntityRevisionLookup
+	 */
+	private $revisionLookup;
+
+	/**
+	 * @var EntityPrefetcher
+	 */
+	private $entityPrefetcher;
+
+	/**
+	 * @var SiteStore
+	 */
+	private $siteStore;
+
+	/**
+	 * @var PropertyDataTypeLookup
+	 */
+	private $propertyDatatypeLookup;
+
+	/**
+	 * @var Settings
+	 */
+	private $settings;
+
 	public function __construct() {
 		parent::__construct();
 		$this->addOption( 'format', "Set the dump format.", false, true );
+
+		$wikibaseRepo = WikibaseRepo::getDefaultInstance();
+		$this->setServices(
+			$wikibaseRepo->getStore()->newEntityPerPage(),
+			$wikibaseRepo->getStore()->getEntityPrefetcher(),
+			$wikibaseRepo->getSiteStore(),
+			$wikibaseRepo->getPropertyDataTypeLookup(),
+			$wikibaseRepo->getEntityRevisionLookup( 'uncached' ),
+			$wikibaseRepo->getSettings()
+		);
+	}
+
+	public function setServices(
+		EntityPerPage $entityPerPage,
+		EntityPrefetcher $entityPrefetcher,
+		SiteStore $siteStore,
+		PropertyDataTypeLookup $propertyDataTypeLookup,
+		EntityRevisionLookup $entityRevisionLookup,
+		SettingsArray $settings
+	) {
+		parent::setServices( $entityPerPage );
+		$this->entityPrefetcher = $entityPrefetcher;
+		$this->siteStore = $siteStore;
+		$this->propertyDatatypeLookup = $propertyDataTypeLookup;
+		$this->revisionLookup = $entityRevisionLookup;
+		$this->settings = $settings;
 	}
 
 	/**
