fbe6a2dcf6f74d802b9c9762024efa0bdbe730df
addshore <addshorewiki@gmail.com>
2015-08-07 13:18:54 +0100

    Add TypedValueFormatter & Exception
    This exception is new
    The formatter is a CP from Wikibase lib

---
+++ lib/packages/wikibase/data-model-services/src/TypedValue/TypedValueFormatter.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Wikibase\DataModel\Services\TypedValue;
+
+use DataValues\DataValue;
+
+/**
+ * Provides a string representation for a DataValue given its associated DataType.
+ *
+ * @since 0.4
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ */
+interface TypedValueFormatter {
+
+	/**
+	 * Formats the given DataValue.
+	 *
+	 * If $dataTypeId is given, it may be used as a hint for providing
+	 * more appropriate formatting.
+	 *
+	 * @param DataValue $value
+	 * @param string    $dataTypeId
+	 *
+	 * @throws TypedValueFormattingException
+	 * @return string
+	 */
+	public function formatValue( DataValue $value, $dataTypeId = null );
+
+}
