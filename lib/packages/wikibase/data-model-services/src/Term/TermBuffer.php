81bc2872700fa7fe2b705fc4e2e3c0f72d08221d
addshore <addshorewiki@gmail.com>
2015-08-07 15:01:35 +0100

    Add TermBuffer class
    This is a CP from Wikibase Lib

---
+++ lib/packages/wikibase/data-model-services/src/Term/TermBuffer.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Wikibase\DataModel\Services\Term;
+
+use Wikibase\DataModel\Entity\EntityId;
+
+/**
+ * A service interface for buffering terms.
+ *
+ * @since 1.1
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ */
+interface TermBuffer {
+
+	/**
+	 * Loads a set of terms into the buffer.
+	 * The source from which to fetch would typically be supplied to the buffer's constructor.
+	 *
+	 * @param EntityId[] $entityIds
+	 * @param string[]|null $termTypes The desired term types; null means all.
+	 * @param string[]|null $languageCodes The desired languages; null means all.
+	 */
+	public function prefetchTerms( array $entityIds, array $termTypes = null, array $languageCodes = null );
+
+	/**
+	 * Returns a term that was previously loaded by prefetchTerms.
+	 *
+	 * @param EntityId $entityId
+	 * @param string $termType
+	 * @param string $languageCode
+	 *
+	 * @return string|false|null The term, or false of that term is known to not exist,
+	 *         or null if the term was not yet requested via prefetchTerms().
+	 */
+	public function getPrefetchedTerm( EntityId $entityId, $termType, $languageCode );
+
+}
