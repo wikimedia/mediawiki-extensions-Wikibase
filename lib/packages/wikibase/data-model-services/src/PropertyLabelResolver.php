632365b9c1c6733192fc577839ab0c841c41b241
addshore <addshorewiki@gmail.com>
2015-08-07 15:04:42 +0100

    Add PropertyLabelResolver
    This is a CP from WikibaseLib

---
+++ lib/packages/wikibase/data-model-services/src/PropertyLabelResolver.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Wikibase\DataModel\Services;
+
+use Wikibase\DataModel\Entity\EntityId;
+
+/**
+ * Resolves property labels (which are unique per language) into entity IDs.
+ *
+ * Copyright © 24.04.13 by the authors listed below.
+ *
+ * @since 1.1
+ *
+ * @license GPL 2+
+ *
+ * @author daniel
+ */
+interface PropertyLabelResolver {
+
+	/**
+	 * @param string[] $labels the labels
+	 * @param string   $recache Flag, set to 'recache' to discard cached data and fetch fresh data
+	 *                 from the database.
+	 *
+	 * @return EntityId[] a map of strings from $labels to the corresponding entity ID.
+	 */
+	public function getPropertyIdsForLabels( array $labels, $recache = '' );
+
+}
