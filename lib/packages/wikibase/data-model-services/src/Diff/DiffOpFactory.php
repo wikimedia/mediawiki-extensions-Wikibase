8e552031cac53913d92f49c48f212b39df0bf3ed
addshore <addshorewiki@gmail.com>
2015-08-07 14:59:24 +0100

    Add DiffOpFactory
    This is a CP from WikibaseLib
    (with a minor alteration)

---
+++ lib/packages/wikibase/data-model-services/src/Diff/DiffOpFactory.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Wikibase;
+
+use Diff\DiffOp\DiffOp;
+use Diff\DiffOpFactory as DiffDiffOpFactory;
+use Wikibase\DataModel\Services\Diff\EntityDiff;
+
+/**
+ * Class for changes that can be represented as a Diff.
+ *
+ * @since 1.1
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ */
+class DiffOpFactory extends DiffDiffOpFactory {
+
+	public function newFromArray( array $diffOp ) {
+		$this->assertHasKey( 'type', $diffOp );
+
+		// see EntityDiff::getType() and ItemDiff::getType()
+		if ( preg_match( '!^diff/(.*)$!', $diffOp['type'], $matches ) ) {
+			$itemType = $matches[1];
+			$this->assertHasKey( 'operations', $diffOp );
+
+			$operations = $this->createOperations( $diffOp['operations'] );
+			$diff = EntityDiff::newForType( $itemType, $operations );
+
+			return $diff;
+		}
+
+		return parent::newFromArray( $diffOp );
+	}
+
+	/**
+	 * Converts a list of diff operations represented by arrays into a list of
+	 * DiffOp objects.
+	 *
+	 * @todo: pull this up into DiffOpFactory
+	 *
+	 * @param array $data the input data
+	 * @return DiffOp[] The diff ops
+	 */
+	private function createOperations( array $data ) {
+		$operations = array();
+
+		foreach ( $data as $key => $operation ) {
+			$operations[$key] = $this->newFromArray( $operation );
+		}
+
+		return $operations;
+	}
+
+}
