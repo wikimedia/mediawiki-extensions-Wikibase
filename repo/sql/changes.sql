77dafa83b67608698f42a2d3b755f0c9748ab465
Marius Hoch <hoo@online.de>
2015-08-08 13:43:51 +0200

    Change wb_changes.change_info to MEDIUMBLOB
    Bug: T108246
    Change-Id: Ibc0a02cbc9fc63226876e23c986f09f4e2a42c79

--- repo/sql/changes.sql
+++ repo/sql/changes.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS /*_*/wb_changes (
   change_object_id           varbinary(14)       NOT NULL, -- The full id of the object (ie item, query) the change affects
   change_revision_id         INT unsigned        NOT NULL, -- The id of the revision on the repo that made the change
   change_user_id             INT unsigned        NOT NULL, -- The id of the user on the repo that made the change
-  change_info                BLOB                NOT NULL -- Holds additional info about the change, inc diff and stuff
+  change_info                MEDIUMBLOB          NOT NULL -- Holds additional info about the change, inc diff and stuff
 ) /*$wgDBTableOptions*/;
 
 CREATE INDEX /*i*/wb_changes_change_type ON /*_*/wb_changes (change_type);
