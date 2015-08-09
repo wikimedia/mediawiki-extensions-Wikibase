bca47597739b13d5bfd249f7dce23d8d5481ea36
jeroendedauw <jeroendedauw@gmail.com>
2015-08-09 19:47:41 +0200

    Add EntityRetrievingDataTypeLookup, moved from Wikibase Lib
    EntityRetrievingDataTypeLookup is a copy, no changed appart from the NS.
    
    EntityRetrievingDataTypeLookupTest was slightly altered: it now uses
    InMemoryEntityLookup instead of MockRepository

---
+++ lib/packages/wikibase/data-model-services/tests/unit/Lookup/EntityRetrievingDataTypeLookupTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Wikibase\DataModel\Services\Tests\Lookup;
+
+use Wikibase\DataModel\Entity\Property;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\DataModel\Services\Fixtures\InMemoryEntityLookup;
+use Wikibase\DataModel\Services\Lookup\EntityLookup;
+use Wikibase\DataModel\Services\Lookup\EntityRetrievingDataTypeLookup;
+
+/**
+ * @covers Wikibase\DataModel\Services\Lookup\EntityRetrievingDataTypeLookup
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class EntityRetrievingDataTypeLookupTest extends \PHPUnit_Framework_TestCase {
+
+	private $propertiesAndTypes = array(
+		'P1' => 'NyanData all the way across the sky',
+		'P42' => 'string',
+		'P1337' => 'percentage',
+		'P9001' => 'positive whole number',
+	);
+
+	/**
+	 * @return EntityLookup
+	 */
+	private function newEntityLookup() {
+		$lookup = new InMemoryEntityLookup();
+
+		foreach ( $this->propertiesAndTypes as $propertyId => $dataTypeId ) {
+			$property = Property::newFromType( $dataTypeId );
+			$property->setId( new PropertyId( $propertyId ) );
+
+			$lookup->addEntity( $property );
+		}
+
+		return $lookup;
+	}
+
+	public function getDataTypeForPropertyProvider() {
+		$argLists = array();
+
+		foreach ( $this->propertiesAndTypes as $propertyId => $dataTypeId ) {
+			$argLists[] = array(
+				new PropertyId( $propertyId ),
+				$dataTypeId
+			);
+		}
+
+		return $argLists;
+	}
+
+	/**
+	 * @dataProvider getDataTypeForPropertyProvider
+	 *
+	 * @param PropertyId $propertyId
+	 * @param string $expectedDataType
+	 */
+	public function testGetDataTypeForProperty( PropertyId $propertyId, $expectedDataType ) {
+		$lookup = new EntityRetrievingDataTypeLookup( $this->newEntityLookup() );
+
+		$actualDataType = $lookup->getDataTypeIdForProperty( $propertyId );
+
+		$this->assertSame( $expectedDataType, $actualDataType );
+	}
+
+	// TODO: tests for not found
+
+}
