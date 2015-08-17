94f885dd2b5ab9dfa2e50323a12e2be2d6d40c49
addshore <addshorewiki@gmail.com>
2015-08-17 13:29:06 +0200

    Add RestrictedEntityLookup from Wikibase Lib

---
+++ lib/packages/wikibase/data-model-services/src/Lookup/RestrictedEntityLookup.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Wikibase\DataModel\Services\Lookup;
+
+use InvalidArgumentException;
+use Wikibase\DataModel\Entity\EntityDocument;
+use Wikibase\DataModel\Entity\EntityId;
+
+/**
+ * EntityLookup that counts how many entities have been loaded through it and throws
+ * an exception once to many entities have been loaded.
+ *
+ * This is needed to limit the number of entities that can be loaded via some
+ * user controlled features, like entity access in Lua.
+ *
+ * @since 1.2
+ *
+ * @license GNU GPL v2+
+ * @author Marius Hoch < hoo@online.de >
+ */
+class RestrictedEntityLookup implements EntityLookup {
+
+	/**
+	 * @var EntityLookup
+	 */
+	private $entityLookup;
+
+	/**
+	 * @var int
+	 */
+	private $entityAccessLimit;
+
+	/**
+	 * @var bool[] Entity id serialization => bool
+	 */
+	private $entitiesAccessed = array();
+
+	/**
+	 * @var int
+	 */
+	private $entityAccessCount = 0;
+
+	/**
+	 * @param EntityLookup $entityLookup
+	 * @param int $entityAccessLimit
+	 */
+	public function __construct( EntityLookup $entityLookup, $entityAccessLimit ) {
+		if ( !is_int( $entityAccessLimit ) ) {
+			throw new InvalidArgumentException( '$entityAccessLimit must be an integer' );
+		}
+
+		$this->entityLookup = $entityLookup;
+		$this->entityAccessLimit = $entityAccessLimit;
+	}
+
+	/**
+	 * @see EntityLookup::getEntity
+	 *
+	 * @param EntityId $entityId
+	 *
+	 * @throws EntityNotFoundException
+	 * @return EntityDocument
+	 */
+	public function getEntity( EntityId $entityId ) {
+		$entityIdSerialization = $entityId->getSerialization();
+
+		if ( !array_key_exists( $entityIdSerialization, $this->entitiesAccessed ) ) {
+			$this->entityAccessCount++;
+			$this->entitiesAccessed[$entityIdSerialization] = true;
+		}
+
+		if ( $this->entityAccessCount > $this->entityAccessLimit ) {
+			throw new EntityNotFoundException(
+				$entityId,
+				'To many entities loaded, must not load more than ' . $this->entityAccessLimit . ' entities.'
+			);
+		}
+
+		return $this->entityLookup->getEntity( $entityId );
+	}
+
+	/**
+	 * @see EntityLookup::hasEntity
+	 *
+	 * @param EntityId $entityId
+	 *
+	 * @return bool
+	 */
+	public function hasEntity( EntityId $entityId ) {
+		return $this->entityLookup->hasEntity( $entityId );
+	}
+
+	/**
+	 * Returns the number of entities already loaded via this object.
+	 *
+	 * @since 1.2
+	 *
+	 * @return int
+	 */
+	public function getEntityAccessCount() {
+		return $this->entityAccessCount;
+	}
+
+	/**
+	 * Whether an entity has been accessed before via this RestrictedEntityLookup.
+	 *
+	 * @since 1.2
+	 *
+	 * @param EntityId $entityId
+	 *
+	 * @return bool
+	 */
+	public function entityHasBeenAccessed( EntityId $entityId ) {
+		$entityIdSerialization = $entityId->getSerialization();
+
+		return array_key_exists( $entityIdSerialization, $this->entitiesAccessed );
+	}
+
+}
---
+++ lib/packages/wikibase/data-model-services/tests/unit/Lookup/RestrictedEntityLookupTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Wikibase\DataModel\Services\Tests\Lookup;
+
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Services\Lookup\EntityLookup;
+use Wikibase\DataModel\Services\Lookup\RestrictedEntityLookup;
+
+/**
+ * @covers Wikibase\DataModel\Services\Lookup\RestrictedEntityLookup
+ *
+ * @licence GNU GPL v2+
+ * @author Marius Hoch
+ */
+class RestrictedEntityLookupTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * @return \PHPUnit_Framework_MockObject_MockObject|EntityLookup
+	 */
+	private function getEntityLookup() {
+		$entityLookup = $this->getMock( 'Wikibase\DataModel\Services\Lookup\EntityLookup' );
+
+		$entityLookup->expects( $this->any() )
+			->method( 'hasEntity' )
+			->will( $this->returnValue( true ) );
+
+		$entityLookup->expects( $this->any() )
+			->method( 'getEntity' )
+			->will( $this->returnCallback( function( EntityId $entityId ) {
+				return $entityId->getSerialization();
+			} ) );
+
+		return $entityLookup;
+	}
+
+	public function testHasEntity() {
+		$lookup = new RestrictedEntityLookup( $this->getEntityLookup(), 200 );
+
+		$this->assertTrue( $lookup->hasEntity( new ItemId( 'Q22' ) ) );
+	}
+
+	public function testGetEntityAccessCount() {
+		$lookup = new RestrictedEntityLookup( $this->getEntityLookup(), 200 );
+
+		for ( $i = 1; $i < 6; $i++ ) {
+			$lookup->getEntity( new ItemId( 'Q' . $i ) );
+		}
+		// Q3 has already been loaded, thus doesn't count
+		$lookup->getEntity( new ItemId( 'Q3' ) );
+
+		$this->assertSame( 5, $lookup->getEntityAccessCount() );
+	}
+
+	public function testGetEntity() {
+		$lookup = new RestrictedEntityLookup( $this->getEntityLookup(), 200 );
+
+		for ( $i = 1; $i < 6; $i++ ) {
+			$this->assertSame(
+				'Q' . $i,
+				$lookup->getEntity( new ItemId( 'Q' . $i ) )
+			);
+		}
+	}
+
+	/**
+	 * @expectedException \Wikibase\DataModel\Services\Lookup\EntityNotFoundException
+	 */
+	public function testGetEntity_exception() {
+		$lookup = new RestrictedEntityLookup( $this->getEntityLookup(), 3 );
+
+		for ( $i = 1; $i < 6; $i++ ) {
+			$lookup->getEntity( new ItemId( 'Q' . $i ) );
+		}
+	}
+
+	public function testHasEntityBeenAccessed() {
+		$lookup = new RestrictedEntityLookup( $this->getEntityLookup(), 200 );
+		$lookup->getEntity( new ItemId( 'Q2' ) );
+
+		$this->assertTrue( $lookup->entityHasBeenAccessed( new ItemId( 'Q2' ) ) );
+		$this->assertFalse( $lookup->entityHasBeenAccessed( new ItemId( 'Q42' ) ) );
+	}
+
+}
