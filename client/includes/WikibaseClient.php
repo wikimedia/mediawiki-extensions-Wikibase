37c13791709ff20bbda0547a348c0609ef47715f
frimelle <lucie.aimee@googlemail.com>
2015-08-19 17:27:56 +0200

    Add BufferingTermLookup to getTermLookup in WikibaseClient
    DEPLOY: Keep an eye on terms table query performance!
    
    Change-Id: I67762454c38e2dc9f79e8fb21b49f9db779f2442

--- client/includes/WikibaseClient.php
+++ client/includes/WikibaseClient.php
@@ -57,6 +57,7 @@ use Wikibase\Lib\WikibaseContentLanguages;
 use Wikibase\Lib\WikibaseDataTypeBuilders;
 use Wikibase\Lib\WikibaseSnakFormatterBuilders;
 use Wikibase\Lib\WikibaseValueFormatterBuilders;
+use Wikibase\Store\BufferingTermLookup;
 use Wikibase\NamespaceChecker;
 use Wikibase\SettingsArray;
 use Wikibase\StringNormalizer;
@@ -131,6 +132,11 @@ final class WikibaseClient {
 	 * @var OutputFormatSnakFormatterFactory|null
 	 */
 	private $snakFormatterFactory = null;
+
+	/**
+	 * @var TermLookup|null
+	 */
+	private $termLookup;
 
 	/**
 	 * @var OutputFormatValueFormatterFactory|null
@@ -221,10 +227,24 @@ final class WikibaseClient {
 	}
 
 	/**
+	 * @return TermBuffer
+	 */
+	public function getTermBuffer() {
+		return $this->getTermLookup();
+	}
+
+	/**
 	 * @return TermLookup
 	 */
-	private function getTermLookup() {
-		return new EntityRetrievingTermLookup( $this->getEntityLookup() );
+	public function getTermLookup() {
+		if ( !$this->termLookup ) {
+			$this->termLookup = new BufferingTermLookup(
+				$this->getStore()->getTermIndex(),
+				1000 // @todo: configure buffer size
+			);
+		}
+
+		return $this->termLookup;
 	}
 
 	/**
