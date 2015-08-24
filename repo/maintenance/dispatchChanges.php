f94073baff5235d1de72c4b50afb85e65dc4c1b0
aude <aude.wiki@gmail.com>
2015-08-24 14:23:40 +0200

    Fix typo in dispatchChanges options description
    Change-Id: I30717ba69ecbfebb1bda112840f17cc050513f54

--- repo/maintenance/dispatchChanges.php
+++ repo/maintenance/dispatchChanges.php
@@ -48,7 +48,7 @@ class DispatchChanges extends Maintenance {
 		$this->addOption( 'idle-delay', "Seconds to sleep when idle. Default: 10", false, true );
 		$this->addOption( 'dispatch-interval', "How often to dispatch to each target wiki. "
 					. "Default: every 60 seconds", false, true );
-		$this->addOption( 'lock-grace-interval', "Seconds after wich to probe for orphaned locks. "
+		$this->addOption( 'lock-grace-interval', "Seconds after which to probe for orphaned locks. "
 					. "Default: 60", false, true );
 		$this->addOption( 'randomness', "Number of least current target wikis to pick from at random. "
 					. "Default: 10.", false, true );
