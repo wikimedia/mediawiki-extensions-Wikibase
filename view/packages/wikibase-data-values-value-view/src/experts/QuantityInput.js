1434fac4d27d618999b9d321ca8da40ddb22df1a
Daniel Kinzler <daniel.kinzler@wikimedia.de>
2015-08-27 14:14:02 +0200

    applyRounding: false

--- view/packages/wikibase-data-values-value-view/src/experts/QuantityInput.js
+++ view/packages/wikibase-data-values-value-view/src/experts/QuantityInput.js
@@ -50,7 +50,8 @@
 		valueCharacteristics: function() {
 			return {
 				unit: this._unitSelector && this._unitSelector.getConceptUri() || null,
-				applyUnit: false
+				applyUnit: false,
+				applyRounding: false
 			};
 		},
 
