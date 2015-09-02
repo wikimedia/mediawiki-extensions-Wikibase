dcac873f1418179f5812800229b89259d2b5bc60
jeroendedauw <jeroendedauw@gmail.com>
2015-09-02 23:46:38 +0200

    Make WikiPageEntityRedirectLookup match its interfaces contract
    Added several TODOs and FIXMEs for things that still need to be done.
    To be clear: these things where wrong before this commit so are not new issues.
    
    Change-Id: I174d0f2dc791e1ff70c43b9f9710a51f2f617ed8

--- lib/tests/phpunit/MockRepository.php
+++ lib/tests/phpunit/MockRepository.php
@@ -16,6 +16,7 @@ use Wikibase\DataModel\Entity\PropertyId;
 use Wikibase\DataModel\Entity\BasicEntityIdParser;
 use Wikibase\DataModel\Services\Lookup\EntityLookup;
 use Wikibase\DataModel\Services\Lookup\EntityRedirectLookup;
+use Wikibase\DataModel\Services\Lookup\EntityRedirectLookupException;
 use Wikibase\DataModel\Services\Lookup\PropertyDataTypeLookup;
 use Wikibase\DataModel\Services\Lookup\PropertyDataTypeLookupException;
 use Wikibase\DataModel\SiteLink;
@@ -778,8 +779,9 @@ class MockRepository implements
 	 * @param EntityId $entityId
 	 * @param string $forUpdate
 	 *
-	 * @return EntityId|null|false The ID of the redirect target, or null if $entityId
-	 *         does not refer to a redirect, or false if $entityId is not known.
+	 * @return EntityId|null The ID of the redirect target, or null if $entityId
+	 *         does not refer to a redirect
+	 * @throws EntityRedirectLookupException
 	 */
 	public function getRedirectForEntityId( EntityId $entityId, $forUpdate = '' ) {
 		$key = $entityId->getSerialization();
@@ -792,7 +794,7 @@ class MockRepository implements
 			return null;
 		}
 
-		return false;
+		throw new EntityRedirectLookupException( $entityId );
 	}
 
 }
