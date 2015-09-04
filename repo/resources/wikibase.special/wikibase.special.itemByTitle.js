6883e573d48322f38bb82a551de256c721608025
Ricordisamoa <ricordisamoa@openmailbox.org>
2015-09-04 12:11:27 +0200

    Convert SpecialItemByTitle to HTMLForm in 'ooui' mode
    Bug: T48248
    Change-Id: Ic6e10a007ffb68a8ba9dc150360293e426090a65

--- repo/resources/wikibase.special/wikibase.special.itemByTitle.js
+++ repo/resources/wikibase.special/wikibase.special.itemByTitle.js
@@ -22,13 +22,15 @@
 				siteList.push( sites[ siteId ].getName() + ' (' + siteId + ')' );
 			}
 		}
-		$( '#wb-itembytitle-sitename' )
+
+		var $input = OO.ui.infuse( $( '#wb-itembytitle-sitename' ) ).$input;
+
+		$input
 		.attr( 'autocomplete', 'off' )
 		.suggester( { source: siteList } );
 		// Hackety hack hack...
 		// On submit, replace human readable value like "English (en)" with actual sitename ("enwiki")
 		$( '#wb-itembytitle-form1' ).submit( function() {
-			var $input = $( '#wb-itembytitle-sitename' );
 			var langID = String( $input.val().replace( /.*\(|\).*/gi, '' ) );
 			if ( wb.sites.getSite( langID ).getId() !== undefined ) {
 				$input.val( wb.sites.getSite( langID ).getId() );
