989623de470ec9017d2d1d1c50221162bd95be87
addshore <addshorewiki@gmail.com>
2015-09-09 10:12:24 +0200

    Re add JSON entity keys
    This broke due to:
    https://gerrit.wikimedia.org/r/#/c/227686
    
    This is a similar sort of thing that we ran
    into with sitelinks and terms.
    
    Integration tests have been added in a followup
    on master
    
    Change-Id: I16cff8a7392a67a2292cb9e53cbd97ad53b16632

--- repo/includes/api/ResultBuilder.php
+++ repo/includes/api/ResultBuilder.php
@@ -310,7 +310,13 @@ class ResultBuilder {
 
 		$this->appendValue( array( 'entities' ), $sourceEntityIdSerialization, $record, 'entity' );
 		if ( $this->addMetaData ) {
-			$this->result->addArrayType( array( 'entities' ), 'array' );
+			$this->result->addArrayType( array( 'entities' ), 'kvp', 'id' );
+			$this->result->addValue(
+				array( 'entities' ),
+				ApiResult::META_KVP_MERGE,
+				true,
+				ApiResult::OVERRIDE
+			);
 		}
 	}
 
@@ -1028,6 +1034,13 @@ class ResultBuilder {
 
 		if ( $this->addMetaData ) {
 			$this->result->addIndexedTagName( 'entities', 'entity' );
+			$this->result->addArrayType( array( 'entities' ), 'kvp', 'id' );
+			$this->result->addValue(
+				array( 'entities' ),
+				ApiResult::META_KVP_MERGE,
+				true,
+				ApiResult::OVERRIDE
+			);
 		}
 
 		$this->missingEntityCounter--;
