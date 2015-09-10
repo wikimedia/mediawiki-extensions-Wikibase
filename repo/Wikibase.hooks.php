5c49324ba2882df8727878a4d54ee042b3d636ca
daniel <daniel.kinzler@wikimedia.de>
2015-09-10 19:04:35 +0200

    Deduplicate messages used to localize edit summaries.
    Localizable edit summaries were split by entity type. This change
    introduces a fallback that allows summary messages to be defined
    for all entity types, and be overwritten for specific entity types.
    
    This removes redundant messages, and it allows summaries to be
    localized without knowledge of which entity type was edited.
    
    Change-Id: I4d50aedcedafa3d230c70620d9619fdd7b6dd588

--- repo/Wikibase.hooks.php
+++ repo/Wikibase.hooks.php
@@ -833,7 +833,7 @@ final class RepoHooks {
 			StubUserLang::unstub( $wgLang );
 		}
 
-		$formatter = new AutoCommentFormatter( $wgLang, $messagePrefix );
+		$formatter = new AutoCommentFormatter( $wgLang, array( 'wikibase-entity', $messagePrefix ) );
 		$formattedComment = $formatter->formatAutoComment( $auto );
 
 		if ( is_string( $formattedComment ) ) {
