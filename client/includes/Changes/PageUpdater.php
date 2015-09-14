6ec067a63ccd22a45afd03bf2c283bdccbbb523e
daniel <daniel.kinzler@wikimedia.de>
2015-09-14 15:05:17 +0200

    Replace ExternalRecentChange with RecentChangeFactory.
    This consolidates the code for constructing RC entries from EntityChange
    objects in a single place.
    
    Change-Id: I8edc4177d24a8f8c6b10b84107c3ee119a21bee6

--- client/includes/Changes/PageUpdater.php
+++ client/includes/Changes/PageUpdater.php
@@ -3,6 +3,7 @@
 namespace Wikibase\Client\Changes;
 
 use Title;
+use Wikibase\EntityChange;
 
 /**
  * Service interface for triggering different kinds of page updates
@@ -51,8 +52,8 @@ interface PageUpdater {
 	 * @since 0.5
 	 *
 	 * @param Title[] $titles
-	 * @param array $attribs
+	 * @param EntityChange $change
 	 */
-	public function injectRCRecords( array $titles, array $attribs );
+	public function injectRCRecords( array $titles, EntityChange $change );
 
 }
