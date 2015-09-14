5d1b610ef85f848fcfb9b0c627d8d555368094f9
Adrian Heine <adrian.heine@wikimedia.de>
2015-09-14 09:49:22 +0200

    Fix file name for WikibaseStringValueNormalizer
    Change-Id: Ic16a23ac6459da9ecfb43062e6f49eb176d34c6d

---
+++ repo/includes/parsers/WikibaseStringValueNormalizer.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Wikibase\Repo\Parsers;
+
+use InvalidArgumentException;
+use ValueParsers\Normalizers\StringNormalizer;
+
+/**
+ * Adapter implementing ValueParsers\Normalizers\StringNormalizer based on \Wikibase\StringNormalize.
+ * Used to perform string normalization in StringParser.
+ *
+ * @since 0.5
+ *
+ * @license GPL 2+
+ * @author Daniel Kinzler
+ */
+class WikibaseStringValueNormalizer implements StringNormalizer {
+
+	/**
+	 * @var \Wikibase\StringNormalizer
+	 */
+	private $normalizer;
+
+	/**
+	 * @param \Wikibase\StringNormalizer $normalizer
+	 */
+	public function __construct( \Wikibase\StringNormalizer $normalizer ) {
+		$this->normalizer = $normalizer;
+	}
+
+	/**
+	 * Trims leading and trailing whitespace and performs unicode normalization
+	 * by calling Wikibase\StringNormalizer::trimToNFC().
+	 *
+	 * @see StringNormalizer::normalize()
+	 * @see Wikibase\StringNormalizer::trimToNFC()
+	 *
+	 * @param string $value the value to normalize
+	 *
+	 * @throws InvalidArgumentException if $value is not a string
+	 * @return string the normalized value
+	 */
+	public function normalize( $value ) {
+		return $this->normalizer->trimToNFC( $value );
+	}
+
+}
