b8fa8608de348fbc183be4763c2b69d7b929fc89
Adrian Heine <adrian.heine@wikimedia.de>
2015-09-15 08:45:06 +0200

    Use ViewFactory for building children of entityviews
    This change introduces ViewFactory methods for creating entitytermsview,
    sitelinkgrouplistview and statementgrouplistview widgets. These methods are
    passed to the different entityviews so that they can create the widgets without
    having to pass them all their dependencies.
    
    It improves separation between the entityviews and their immediate children,
    since the entityviews don't have to know how to construct the child views.
    It also allows to inject different implementations, for example in tests.
    
    This is a first step for T75380.
    
    Change-Id: I996c57f6fd6a1c6a4575bb82cbcbb7726eca4328

--- view/tests/qunit/jquery/wikibase/jquery.wikibase.propertyview.tests.js
+++ view/tests/qunit/jquery/wikibase/jquery.wikibase.propertyview.tests.js
@@ -12,17 +12,9 @@
  */
 var createPropertyview = function( options, $node ) {
 	options = $.extend( {
-		entityStore: new wb.store.EntityStore(),
-		entityChangersFactory: {
-			getAliasesChanger: function() { return 'I am an AliasesChanger'; },
-			getDescriptionsChanger: function() { return 'I am a DescriptionsChanger'; },
-			getLabelsChanger: function() { return 'I am a LabelsChanger'; }
-		},
-		api: 'I am an Api',
-		valueViewBuilder: 'I am a valueview builder',
-		dataTypeStore: 'I am a DataTypeStore',
 		value: new wb.datamodel.Property( 'P1', 'someDataType' ),
-		languages: 'en'
+		entityTermsViewBuilder: function() {},
+		statementGroupListViewBuilder: function() {}
 	}, options || {} );
 
 	$node = $node || $( '<div/>' ).appendTo( 'body' );
@@ -57,13 +49,6 @@ QUnit.test( 'Create & destroy', function( assert ) {
 		'Throwing error when trying to initialize widget without a value.'
 	);
 
-	assert.throws(
-		function() {
-			createPropertyview( { languages: null } );
-		},
-		'Throwing error when trying to initialize widget without a language.'
-	);
-
 	var $propertyview = createPropertyview(),
 		propertyview = $propertyview.data( 'propertyview' );
 
@@ -79,13 +64,6 @@ QUnit.test( 'Create & destroy', function( assert ) {
 		'Destroyed widget.'
 	);
 
-	$propertyview = createPropertyview( { languages: ['ku'] } );
-	propertyview = $propertyview.data( 'propertyview' );
-
-	assert.ok(
-		propertyview instanceof $.wikibase.propertyview,
-		'Created widget with a language.'
-	);
 } );
 
 }( jQuery, wikibase, QUnit ) );
