892c94e806a21a1832261efc054f93e67603a204
Adrian Heine <adrian.heine@wikimedia.de>
2015-09-16 09:39:48 +0200

    Remove unused batching from EntityStore
    Change-Id: Ie71052c244889d7f12c370afea39fbb4a3164e8f

--- view/resources/wikibase/store/store.EntityStore.js
+++ view/resources/wikibase/store/store.EntityStore.js
@@ -1,16 +1,14 @@
 /**
  * @licence GNU GPL v2+
- * @author Adrian Lang < adrian.lang@wikimedia.de >
+ * @author Adrian Heine < adrian.heine@wikimedia.de >
  */
-( function( wb, $ ) {
+( function( util, wb, $ ) {
 	'use strict';
 
 	var MODULE = wb.store;
 
 	/**
 	 * Entity store managing wikibase.datamodel.Entity objects.
-	 *
-	 * Subclasses have to implement at least one of get, getMultiple or getMultipleRaw.
 	 *
 	 * @constructor
 	 * @abstract
@@ -29,57 +27,8 @@
 		 *         - {wikibase.store.FetchedContent|undefined|null}
 		 *         No rejected parameters.
 		 */
-		get: function( entityId ) {
-			var deferred = $.Deferred();
+		get: util.abstractMember
 
-			this.getMultiple( [ entityId ] )
-			.done( function( entities ) {
-				deferred.resolve( entities[ 0 ] );
-			} )
-			// FIXME: Evaluate failing promise
-			.fail( deferred.reject );
-
-			return deferred.promise();
-		},
-
-		/**
-		 * Returns a promise resolving to an array with elements entity, undefined or null.
-		 * @since 0.5
-		 *
-		 * @param {string[]} entityIds
-		 * @return {jQuery.Promise}
-		 *         Resolved parameters:
-		 *         - {wikibase.store.FetchedContent|undefined|null[]}
-		 *         No rejected parameters.
-		 */
-		getMultiple: function( entityIds ) {
-			var deferred = $.Deferred();
-
-			$.when.apply( $, this.getMultipleRaw( entityIds ) )
-			.done( function( /*…*/ ) {
-				deferred.resolve( $.makeArray( arguments ) );
-			} )
-			// FIXME: Evaluate failing promise
-			.fail( function() {
-				deferred.reject();
-			} );
-
-			return deferred.promise();
-		},
-
-		/**
-		 * Returns an array of promises resolving to entity, undefined or null.
-		 * @since 0.5
-		 *
-		 * @param {string[]} entityIds
-		 * @return {jQuery.Promise[]}
-		 *         Resolved parameters:
-		 *         - {wikibase.store.FetchedContent|undefined|null}
-		 *         No rejected parameters.
-		 */
-		getMultipleRaw: function( entityIds ) {
-			return $.map( entityIds, $.proxy( this.get, this ) );
-		}
 	} );
 
-}( wikibase, jQuery ) );
+}( util, wikibase, jQuery ) );
