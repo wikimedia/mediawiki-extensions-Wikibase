aae10a0495c6ea348457d483b50f0c977f3d4c26
Adrian Heine <adrian.heine@wikimedia.de>
2015-09-16 09:39:48 +0200

    Remove unused batching from EntityStore
    Change-Id: Ie71052c244889d7f12c370afea39fbb4a3164e8f

--- view/resources/wikibase/store/store.CombiningEntityStore.js
+++ view/resources/wikibase/store/store.CombiningEntityStore.js
@@ -8,88 +8,25 @@
 	var MODULE = wb.store;
 
 	/**
-	 * Like jQuery.when, but does not differentiate between fail and success;
-	 * it just waits for all promises to resolve one way or the other and then
-	 * resolves itself.
+	 * Passes item to the handlers one after the other until it has a resolution
 	 */
-	function whenFinished( arr ) {
+	function asyncFallback( item, handlers ) {
 		var deferred = $.Deferred(),
-			returnsExpected = arr.length;
-
-		$.each( arr, function( i, promise ) {
-			promise.always( function() {
-				if( --returnsExpected <= 0 ) {
-					deferred.resolve();
-				}
-			} );
-		} );
-
-		return deferred.promise();
-	}
-
-	/**
-	 * An asynchronous reduce which fails when the first handler fails
-	 */
-	function asyncReduce( arr, callback, initialValue ) {
-		var deferred = $.Deferred(),
-			index = 0,
-			previousValue = initialValue;
+			index = 0;
 
 		function tryNext() {
-			if( arr.length <= index ) {
-				deferred.resolve( previousValue );
+			if( handlers.length <= index ) {
+				deferred.reject();
 				return;
 			}
-			callback( previousValue, arr[ index++ ] ).fail( deferred.reject )
-			.done( function( newState ) {
-				previousValue = newState;
-				tryNext();
-			} );
+			handlers[ index++ ]( item )
+			.fail( tryNext )
+			.done( deferred.resolve );
 		}
 
 		window.setTimeout( tryNext, 0 );
 
 		return deferred.promise();
-	}
-
-	/**
-	 * Passes all items from arr to the handlers one after the other until it has a resolution
-	 * for every item
-	 */
-	function asyncMapFallback( arr, arrHandlers ) {
-		var deferreds = $.map( arr, function() { return $.Deferred(); } );
-		asyncReduce( arrHandlers, function( state, arrHandler ) {
-			var deferred = $.Deferred();
-			if( state.unresolvedArr.length === 0 ) {
-				return deferred.reject( state ).promise();
-			}
-
-			var nextUnresolvedArr = [];
-			var nextUnresolvedDeferreds = [];
-			var promises = arrHandler( state.unresolvedArr );
-			$.each( promises, function( i, promise ) {
-				promise.done( state.unresolvedDeferreds[ i ].resolve )
-				.fail( function() {
-					nextUnresolvedArr.push( state.unresolvedArr[ i ] );
-					nextUnresolvedDeferreds.push( state.unresolvedDeferreds[ i ] );
-				} );
-			} );
-			whenFinished( promises ).done( function() {
-				deferred.resolve( {
-					unresolvedArr: nextUnresolvedArr,
-					unresolvedDeferreds: nextUnresolvedDeferreds
-				} );
-			} );
-
-			return deferred.promise();
-		}, {
-			unresolvedArr: arr,
-			unresolvedDeferreds: deferreds
-		} );
-
-		return $.map( deferreds, function( deferred ) {
-			return deferred.promise();
-		} );
 	}
 
 	/**
@@ -113,12 +50,12 @@
 		_stores: null,
 
 		/**
-		 * @see wikibase.store.EntityStore.getMultipleRaw
+		 * @see wikibase.store.EntityStore.get
 		 */
-		getMultipleRaw: function( entityIds ) {
-			return asyncMapFallback( entityIds, $.map( this._stores, function( store ) {
-				return function( entityIds ) {
-					return store.getMultipleRaw( entityIds );
+		get: function( entityId ) {
+			return asyncFallback( entityId, $.map( this._stores, function( store ) {
+				return function( entityId ) {
+					return store.get( entityId );
 				};
 			} ) );
 		}
