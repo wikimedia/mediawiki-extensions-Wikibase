2a3d2f5ca8bcb412b1c347823e58034d1b96e783
Bene <benestar.wikimedia@gmail.com>
2015-09-16 17:22:03 +0200

    Create wbsearch api query module
    Using action=query&list=wbsearch one can search for entities. The same
    module can also be used as a generator for other queries.
    
    Bug: T111209
    Change-Id: I5008581cc770f38fa73fee758055485946e35d89

--- repo/Wikibase.php
+++ repo/Wikibase.php
@@ -81,7 +81,7 @@ if ( !defined( 'PURTLE_VERSION' ) ) {
 
 call_user_func( function() {
 	global $wgExtensionCredits, $wgGroupPermissions, $wgExtensionMessagesFiles, $wgMessagesDirs;
-	global $wgAPIModules, $wgSpecialPages, $wgHooks, $wgAvailableRights;
+	global $wgAPIModules, $wgAPIListModules, $wgSpecialPages, $wgHooks, $wgAvailableRights;
 	global $wgWBRepoSettings, $wgResourceModules, $wgValueParsers, $wgJobClasses;
 	global $wgWBRepoDataTypes;
 
@@ -164,6 +164,7 @@ call_user_func( function() {
 	$wgAPIModules['wbparsevalue'] = 'Wikibase\Repo\Api\ParseValue';
 	$wgAPIModules['wbavailablebadges'] = 'Wikibase\Repo\Api\AvailableBadges';
 	$wgAPIModules['wbcreateredirect'] = 'Wikibase\Repo\Api\CreateRedirect';
+	$wgAPIListModules['wbsearch'] = 'Wikibase\Repo\Api\QuerySearchEntities';
 
 	// Special page registration
 	$wgSpecialPages['NewItem'] = 'Wikibase\Repo\Specials\SpecialNewItem';
