9c882bb245274a9ae605355a615bb6305257e04d
Adrian Heine <adrian.heine@wikimedia.de>
2015-09-28 13:09:56 +0200

    Use ViewFactory for building statementviews in statementlistview
    This change introduces a `ViewFactory` method for building `ListItemAdapter`s
    for `statementview` widgets. This `ListItemAdapter` is passed to the
    `statementlistview` so that it can create the widgets without having to
    pass them all their dependencies.
    
    This change leaves `listview` as an implementation detail of
    `statementlistview`. This implementation detail is publicly represented by
    having to pass in a `listview.ListItemAdapter`.
    
    It improves separation between the `statementlistview` and `statementview`,
    since the `statementlistview` doesn't have to know how to construct a
    `statementview` anymore. It also allows to inject a different implementation,
    for example in tests.
    
    This is a fourth step for T75380.
    
    Bug: T75380
    Change-Id: I293e6f745fc96f1dab9951e21af613ede8471501

---
+++ view/tests/qunit/getMockListItemAdapter.js
@@ -0,0 +1,36 @@
+/**
+ * @licence GNU GPL v2+
+ * @author Adrian Heine <adrian.heine@wikimedia.de>
+ */
+( function( $, wb ) {
+'use strict';
+
+function simpleWidget( name, fn ) {
+	var widget = function( options, element ) {
+		$.Widget.apply( this, arguments );
+		this._createWidget( options, element );
+		fn.apply( this );
+	};
+
+	widget.prototype = new $.Widget();
+
+	widget.prototype.widgetFullName = name;
+	widget.prototype.widgetName = name;
+	widget.prototype.widgetEventPrefix = name;
+	widget.prototype.value = function() {};
+
+	return widget;
+}
+
+wb.tests.getMockListItemAdapter = function( widgetName, initFn ) {
+	var Widget = simpleWidget( widgetName, initFn );
+
+	return new $.wikibase.listview.ListItemAdapter( {
+		listItemWidget: Widget,
+		newItemOptionsFn: function( value ) {
+			return { value: value };
+		}
+	} );
+};
+
+}( jQuery, wikibase ) );
--- view/tests/qunit/jquery/wikibase/jquery.wikibase.statementlistview.tests.js
+++ view/tests/qunit/jquery/wikibase/jquery.wikibase.statementlistview.tests.js
@@ -5,6 +5,20 @@
 ( function( $, wb, QUnit ) {
 'use strict';
 
+var statementviewListItemAdapter = wb.tests.getMockListItemAdapter(
+	'statementview',
+	function() {
+		var _value = this.options.value;
+		this.startEditing = function() {};
+		this.value = function( newValue ) {
+			if ( arguments.length ) {
+				_value = newValue;
+			}
+			return _value;
+		};
+	}
+);
+
 /**
  * @param {Object} [options={}]
  * @param {jQuery} [$node]
@@ -12,27 +26,8 @@
  */
 var createStatementlistview = function( options, $node ) {
 	options = $.extend( {
-		claimGuidGenerator: 'I am a ClaimGuidGenerator',
-		entityIdPlainFormatter: {
-			format: function ( entityId ) {
-				return $.Deferred().resolve( entityId ).promise();
-			}
-		},
-		entityStore: {
-			get: function () {
-				return $.Deferred().resolve().promise();
-			}
-		},
-		valueViewBuilder: 'I am a ValueViewBuilder',
-		entityChangersFactory: {
-			getClaimsChanger: function() {
-				return 'I am a ClaimsChanger';
-			},
-			getReferencesChanger: function() {
-				return 'I am a ReferencesChanger';
-			}
-		},
-		dataTypeStore: 'I am a DataTypeStore',
+		claimsChanger: 'I am a ClaimsChanger',
+		listItemAdapter: statementviewListItemAdapter,
 		value: new wb.datamodel.StatementList()
 	}, options || {} );
 
