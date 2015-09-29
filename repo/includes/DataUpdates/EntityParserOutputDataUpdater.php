2b1b0936c517dbbc9d5d7b29ef6b802ebcc21960
aude <aude.wiki@gmail.com>
2015-09-29 17:57:34 +0200

    Refactor ReferencedEntitiesFinder etc. into ParserOutputDataUpdates [WIP]
    Instead of looping through the array of Snaks several times,
    we can use the visitor pattern to loop once and extract
    data needed (e.g. referenced entities) for ParserOutput.
    
    Made this work on the Statement level, as some data updates need
    to consider things like ranks or are only concerned with main snak.
    
    ReferencedEntitiesFinder etc. have always had a higher performance
    cost, so this probably helps and would be the way to
    also extract coordinates for GeoData.
    
    @todo add tests for everything
    
    Bug: T114220
    Change-Id: I7b03cc001d708ea18812d7c9a191d05362e150cc

---
+++ repo/includes/DataUpdates/EntityParserOutputDataUpdater.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace Wikibase\Repo\DataUpdates;
+
+use ParserOutput;
+use Wikibase\DataModel\Entity\EntityDocument;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\DataModel\SiteLinkList;
+use Wikibase\DataModel\Snak\Snak;
+use Wikibase\DataModel\Statement\StatementList;
+use Wikibase\DataModel\Statement\StatementListProvider;
+
+/**
+ * @todo have ItemParserOutputDataUpdate, etc. instead.
+ *
+ * @since 0.5
+ *
+ * @license GNU GPL v2+
+ * @author Katie Filbert < aude.wiki@gmail.com >
+ */
+class EntityParserOutputDataUpdater {
+
+	/**
+	 * @param ParserOutputDataUpdate[] $dataUpdates
+	 */
+	public function __construct( array $dataUpdates ) {
+		$this->dataUpdates = $dataUpdates;
+	}
+
+	/**
+	 * @param EntityDocument $entity
+	 */
+	public function processEntity( EntityDocument $entity ) {
+		if ( $entity instanceof StatementListProvider ) {
+			$this->processStatements( $entity->getStatements() );
+		}
+
+		if ( $entity instanceof Item ) {
+			$this->processSiteLinks( $entity->getSiteLinkList() );
+		}
+	}
+
+	/**
+	 * @param StatementList $statements
+	 */
+	private function processStatements( StatementList $statements ) {
+		$statementDataUpdates = $this->getStatementDataUpdates();
+
+		if ( empty( $statementDataUpdates ) ) {
+			return;
+		}
+
+		foreach ( $statements as $statement ) {
+			foreach ( $statementDataUpdates as $statementDataUpdate ) {
+				$snakDataUpdate->processStatement( $statement );
+			}
+		}
+	}
+
+	/**
+	 * @param SiteLinkList $siteLinks
+	 */
+	private function processSiteLinks( SiteLinkList $siteLinks ) {
+		$siteLinkDataUpdates = $this->getSiteLinkDataUpdates();
+
+		if ( empty( $siteLinkDataUpdates ) ) {
+			return;
+		}
+
+		// process things like badges
+		foreach ( $siteLinks as $siteLink ) {
+			foreach ( $siteLinkDataUpdates as $siteLinkDataUpdate ) {
+				$siteLinkDataUpdate->processSiteLink( $siteLink );
+			}
+		}
+	}
+
+	/**
+	 * @param ParserOutput $parserOutput
+	 */
+	public function updateParserOutput( ParserOutput $parserOutput ) {
+		foreach ( $this->dataUpdates as $snakDataUpdate ) {
+			$snakDataUpdate->updateParserOutput( $parserOutput );
+		}
+	}
+
+	/**
+	 * @return SiteLinkDataUpdate[]
+	 */
+	private function getSiteLinkDataUpdates() {
+		$siteLinkDataUpdates = array();
+
+		foreach ( $this->dataUpdates as $dataUpdate ) {
+			if ( $dataUpdate instanceof SiteLinkDataUpdate ) {
+				$siteLinkDataUpdates[] = $dataUpdate;
+			}
+		}
+
+		return $siteLinkDataUpdates;
+	}
+
+	/**
+	 * @return StatementDataUpdate[]
+	 */
+	private function getStatementDataUpdates() {
+		$snakDataUpdates = array();
+
+		foreach ( $this->dataUpdates as $dataUpdate ) {
+			if ( $dataUpdate instanceof StatementDataUpdate ) {
+				$snakDataUpdates[] = $dataUpdate;
+			}
+		}
+
+		return $snakDataUpdates;
+	}
+
+}
