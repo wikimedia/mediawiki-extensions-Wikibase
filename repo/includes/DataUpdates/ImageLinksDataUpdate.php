902fe74c5982fd53c03350620a1c1be78d0859a1
aude <aude.wiki@gmail.com>
2015-09-29 17:57:34 +0200

    Refactor ReferencedEntitiesFinder etc. into ParserOutputDataUpdates [WIP]
    Instead of looping through the array of Snaks several times,
    we can use the visitor pattern to loop once and extract
    data needed (e.g. referenced entities) for ParserOutput.
    
    Made this work on the Statement level, as some data updates need
    to consider things like ranks or are only concerned with main snak.
    
    ReferencedEntitiesFinder etc. have always had a higher performance
    cost, so this probably helps and would be the way to
    also extract coordinates for GeoData.
    
    @todo add tests for everything
    
    Bug: T114220
    Change-Id: I7b03cc001d708ea18812d7c9a191d05362e150cc

---
+++ repo/includes/DataUpdates/ImageLinksDataUpdate.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Wikibase\Repo\DataUpdates;
+
+use DataValues\DataValue;
+use ParserOutput;
+use Wikibase\DataModel\Entity\EntityDocument;
+use Wikibase\DataModel\Snak\PropertyValueSnak;
+use Wikibase\DataModel\Snak\Snak;
+use Wikibase\Lib\Store\PropertyDataTypeMatcher;
+
+/**
+ * Register commonsMedia values as used images in ParserOutput.
+ *
+ * @since 0.5
+ *
+ * @license GNU GPL v2+
+ * @author Bene* < benestar.wikimedia@gmail.com >
+ * @author Katie Filbert < aude.wiki@gmail.com >
+ */
+class ImageLinksDataUpdate implements StatementDataUpdate {
+
+	/**
+	 * @var PropertyDataTypeMatcher
+	 */
+	private $propertyDataTypeMatcher;
+
+	/**
+	 * @var DataValue[]
+	 */
+	private $usedImages = array();
+
+	/**
+	 * @param PropertyDataTypeMatcher $propertyDataTypeMatcher
+	 */
+	public function __construct( PropertyDataTypeMatcher $propertyDataTypeMatcher ) {
+		$this->propertyDataTypeMatcher = $propertyDataTypeMatcher;
+	}
+
+	/**
+	 * Add DataValue to list of used images if Snak property data type is commonsMedia.
+	 *
+	 * @param Statement $statement
+	 */
+	public function processStatement( Statement $statement ) {
+		$snaks = $statement->getAllSnaks( $statement );
+		$this->extractUsedImagesFromSnaks( $snaks );
+	}
+
+	/**
+	 * @param Snak[] $snaks
+	 */
+	private function extractUsedImagesFromSnaks( array $snaks ) {
+		foreach ( $snaks as $snak ) {
+			if ( $snak instanceof PropertyValueSnak &&
+				$this->propertyDataTypeMatcher->isMatchingDataType(
+				$snak->getPropertyId(),
+					'commonsMedia'
+				)
+			) {
+				$dataValue = $snak->getDataValue();
+				$this->usedImages[$dataValue->getHash()] = $dataValue;
+			}
+		}
+	}
+
+	/**
+	 * Treat CommonsMedia values as file transclusions
+	 *
+	 * @param ParserOutput $parserOutput
+	 */
+	public function updateParserOutput( ParserOutput $parserOutput ) {
+		foreach ( $this->usedImages as $image ) {
+			$value = $image->getValue();
+			if ( is_string( $value ) ) {
+				$parserOutput->addImage( str_replace( ' ', '_', $value ) );
+			}
+		}
+	}
+
+}
