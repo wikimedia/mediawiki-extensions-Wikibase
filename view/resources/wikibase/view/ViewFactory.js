5b184aa1a27cf179bdb79d715c5ba153936bfc18
Adrian Heine <adrian.heine@wikimedia.de>
2015-09-30 09:50:55 +0200

    Use ViewFactory for building snaklistviews in referenceview
    This change uses the available `ViewFactory` method for building
    `ListItemAdapter`s for `snaklistview` widgets. This `ListItemAdapter` is
    passed to the `referenceview` so that it can create the widgets without having
    to pass them all their dependencies.
    
    This change leaves `listview` as an implementation detail of `referenceview`.
    This implementation detail is publicly represented by having to pass in a
    `listview.ListItemAdapter`.
    
    It improves separation between the `referenceview` and `snaklistview`,
    since the `referenceview` doesn't have to know how to construct a
    `snaklistview` anymore. It also allows to inject a different implementation,
    for example in tests.
    
    This is a sixth step for T75380.
    
    Change-Id: Ifd76d6238e5fed78916abd25fbb409129e3497e0

--- view/resources/wikibase/view/ViewFactory.js
+++ view/resources/wikibase/view/ViewFactory.js
@@ -331,15 +331,12 @@
 					value: value || null,
 					statementGuid: statementGuid,
 					dataTypeStore: this._dataTypeStore,
-					entityIdHtmlFormatter: this._entityIdHtmlFormatter,
-					entityIdPlainFormatter: this._entityIdPlainFormatter,
-					entityStore: this._entityStore,
-					referencesChanger: this._entityChangersFactory.getReferencesChanger(),
-					valueViewBuilder: this._getValueViewBuilder()
+					listItemAdapter: this.getListItemAdapterForSnakListView(),
+					referencesChanger: this._entityChangersFactory.getReferencesChanger()
 				};
 			}, this )
 		} );
-	}
+	};
 
 	/**
 	 * Construct a `ListItemAdapter` for `snaklistview`s
