5e4f6655daff1bb5dda0447e64af34af892ce5e3
Jan Zerebecki <jan.wikimedia@zerebecki.de>
2015-10-05 17:37:22 +0200

    Add composer test and npm test entry point
    Change-Id: Ic705507c794e698b8a3a7e18476b2d19bcbb510a

---
+++ view/packages/wikibase-data-values-value-view/Gruntfile.js
@@ -0,0 +1,42 @@
+/*jshint node:true */
+module.exports = function ( grunt ) {
+	'use strict';
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-jscs' );
+
+	grunt.initConfig( {
+		jshint: {
+			options: {
+				jshintrc: true
+			},
+			all: [
+				'*.js',
+				'modules/**/*.js'
+			]
+		},
+		jscs: {
+			src: '<%= jshint.all %>'
+		},
+		banana: {
+			options: {
+				requireCompleteMessageDocumentation: false,
+				disallowUnusedDocumentation: false,
+				disallowUnusedTranslations: false,
+				disallowDuplicateTranslations: false
+			},
+			all: 'i18n/'
+		},
+		jsonlint: {
+			all: [
+				'*.json',
+				'**/*.json',
+				'!node_modules/**'
+			]
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'jshint', 'jscs', 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'default', 'test' );
+};
