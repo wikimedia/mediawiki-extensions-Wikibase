598183de6c87e46a140e1f88ec1b51662c99b661
Marius Hoch <hoo@online.de>
2015-10-06 17:12:05 +0200

    Add regression tests for DatabaseChangeTransmitter
    In order to prepare for refactoring it to not depend
    on the ORM stuff.
    
    Change-Id: I3e91ae79147ef3abf1ccc02f6cc41a5f8c3c88fe

--- repo/tests/phpunit/includes/Notifications/DatabaseChangeTransmitterTest.php
+++ repo/tests/phpunit/includes/Notifications/DatabaseChangeTransmitterTest.php
@@ -2,6 +2,7 @@
 
 namespace Wikibase\Tests\Repo;
 
+use Diff\DiffOp\Diff\Diff;
 use Wikibase\DataModel\Entity\ItemId;
 use Wikibase\EntityChange;
 use Wikibase\Repo\Notifications\DatabaseChangeTransmitter;
@@ -18,14 +19,54 @@ use Wikibase\Repo\WikibaseRepo;
  *
  * @licence GNU GPL v2+
  * @author Daniel Kinzler
+ * @author Marius Hoch
  */
 class DatabaseChangeTransmitterTest extends \MediaWikiTestCase {
 
-	public function testTransmitChange() {
+	public function transmitChangeProvider() {
 		$wikibaseRepo = WikibaseRepo::getDefaultInstance();
 		$factory = $wikibaseRepo->getEntityChangeFactory();
-		$change = $factory->newForEntity( EntityChange::ADD, new ItemId( 'Q21389475' ) );
-		$change->setField( 'time', wfTimestamp( TS_MW ) );
+
+		$time = wfTimestamp( TS_MW );
+
+		$change1 = $factory->newForEntity( EntityChange::ADD, new ItemId( 'Q21389475' ) );
+		$change1->setField( 'time', $time );
+
+		$change2 = $factory->newForEntity( EntityChange::REMOVE, new ItemId( 'Q42' ) );
+		$change2->setField( 'time', $time );
+		$change2->setDiff( new Diff() );
+
+		return array(
+			array(
+				array(
+					'change_type' => 'wikibase-item~add',
+					'change_time' => $time,
+					'change_object_id' => 'q21389475',
+					'change_revision_id' => '0',
+					'change_user_id' => '0',
+					'change_info' => '[]',
+				),
+				$change1
+			),
+			array(
+				array(
+					'change_type' => 'wikibase-item~remove',
+					'change_time' => $time,
+					'change_object_id' => 'q42',
+					'change_revision_id' => '0',
+					'change_user_id' => '0',
+					'change_info' => '{"diff":{"type":"diff","isassoc":null,"operations":[]}}',
+				),
+				$change2
+			)
+		);
+	}
+
+	/**
+	 * @dataProvider transmitChangeProvider
+	 */
+	public function testTransmitChange( array $expected, EntityChange $change ) {
+		$wikibaseRepo = WikibaseRepo::getDefaultInstance();
 
 		$db = wfGetDB( DB_MASTER );
 		$tableName = $wikibaseRepo->getStore()->getChangesTable()->getName();
@@ -36,9 +77,15 @@ class DatabaseChangeTransmitterTest extends \MediaWikiTestCase {
 		$channel = new DatabaseChangeTransmitter();
 		$channel->transmitChange( $change );
 
-		$count = $db->selectField( $tableName, 'count(*)', array(), __METHOD__ );
+		$res = $db->select( $tableName, '*', array(), __METHOD__ );
 
-		$this->assertEquals( 1, $count, 'row count' );
+		$this->assertEquals( 1, $res->numRows(), 'row count' );
+
+		$row = (array)$res->current();
+		$this->assertTrue( is_numeric( $row['change_id'] ) );
+
+		unset( $row['change_id'] );
+		$this->assertEquals( $expected, $row );
 	}
 
 }
