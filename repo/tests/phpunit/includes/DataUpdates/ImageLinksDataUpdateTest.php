e363258994caab6bea5d9d0dd8ff654a5a89d781
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2015-10-09 11:35:11 +0200

    Introduce PropertyDataTypeMatcher and use in DataUpdates
    The PropertyDataTypeMatcher class is an untouched copy from I7b03cc0
    (PS14).
    
    Bug: T114220
    Change-Id: Ic0974d067e81468ac9d6bda9f47f06f4b6d37095

--- repo/tests/phpunit/includes/DataUpdates/ImageLinksDataUpdateTest.php
+++ repo/tests/phpunit/includes/DataUpdates/ImageLinksDataUpdateTest.php
@@ -4,6 +4,7 @@ namespace Wikibase\Repo\Tests\DataUpdates;
 
 use DataValues\StringValue;
 use PHPUnit_Framework_TestCase;
+use Wikibase\DataModel\Entity\PropertyId;
 use Wikibase\DataModel\Snak\PropertyValueSnak;
 use Wikibase\DataModel\Statement\StatementList;
 use Wikibase\Repo\DataUpdates\ImageLinksDataUpdate;
@@ -20,18 +21,37 @@ use Wikibase\Repo\DataUpdates\ImageLinksDataUpdate;
 class ImageLinksDataUpdateTest extends PHPUnit_Framework_TestCase {
 
 	/**
+	 * @return ImageLinksDataUpdate
+	 */
+	private function newInstance() {
+		$matcher = $this->getMockBuilder( 'Wikibase\Lib\Store\PropertyDataTypeMatcher' )
+			->disableOriginalConstructor()
+			->getMock();
+		$matcher->expects( $this->any() )
+			->method( 'isMatchingDataType' )
+			->will( $this->returnCallback( function( PropertyId $id, $type ) {
+				return $id->getSerialization() === 'P1';
+			} ) );
+
+		return new ImageLinksDataUpdate( $matcher );
+	}
+
+	/**
 	 * @param StatementList $statements
 	 * @param string $string
+	 * @param int $propertyId
 	 */
-	private function addStatement( StatementList $statements, $string ) {
-		$statements->addNewStatement( new PropertyValueSnak( 1, new StringValue( $string ) ) );
+	private function addStatement( StatementList $statements, $string, $propertyId = 1 ) {
+		$statements->addNewStatement(
+			new PropertyValueSnak( $propertyId, new StringValue( $string ) )
+		);
 	}
 
 	/**
 	 * @dataProvider imageLinksProvider
 	 */
 	public function testGetImageLinks( StatementList $statements, array $expected ) {
-		$instance = new ImageLinksDataUpdate();
+		$instance = $this->newInstance();
 		$actual = $instance->getImageLinks( $statements );
 		$this->assertSame( $expected, $actual );
 	}
@@ -46,7 +66,7 @@ class ImageLinksDataUpdateTest extends PHPUnit_Framework_TestCase {
 		$parserOutput->expects( $this->exactly( count( $expected ) ) )
 			->method( 'addImage' );
 
-		$instance = new ImageLinksDataUpdate();
+		$instance = $this->newInstance();
 		foreach ( $statements as $statement ) {
 			$instance->processStatement( $statement );
 		}
@@ -57,6 +77,7 @@ class ImageLinksDataUpdateTest extends PHPUnit_Framework_TestCase {
 		$set1 = new StatementList();
 		$this->addStatement( $set1, '1.jpg' );
 		$this->addStatement( $set1, '' );
+		$this->addStatement( $set1, 'no image property', 2 );
 
 		$set2 = new StatementList();
 		$this->addStatement( $set2, '2a.jpg' );
