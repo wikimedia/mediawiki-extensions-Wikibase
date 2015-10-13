32f27674d467bf29779eaaef6388aefbaf5a6a28
addshore <addshorewiki@gmail.com>
2015-10-13 11:58:16 +0100

    Mock ChangesList instead of constructing.
    No need to actually make one of these.
    Also this class may be going abstract in core.
    
    Change-Id: Ice8e443657e19699444653463c0814132a18989c

--- repo/tests/phpunit/includes/Hooks/LabelPrefetchHookHandlersTest.php
+++ repo/tests/phpunit/includes/Hooks/LabelPrefetchHookHandlersTest.php
@@ -133,7 +133,8 @@ class LabelPrefetchHookHandlersTest extends \PHPUnit_Framework_TestCase {
 		$context->setRequest( new FauxRequest() );
 		$context->setTitle( new Title( NS_SPECIAL, 'Watchlist' ) );
 
-		$changesList = new ChangesList( $context );
+		/** @var ChangesList $changesList */
+		$changesList = $this->getMock( 'ChangesList' );
 
 		$linkBeginHookHandler->doChangesListInitRows(
 			$changesList,
