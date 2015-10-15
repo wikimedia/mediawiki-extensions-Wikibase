9b994ccb2a84261116997da1a500c52216861f16
Marius Hoch <hoo@online.de>
2015-10-15 22:56:09 +0200

    Add a changePropertyDataType maintenance script
    This uses an English only edit summary for the edits
    being made, but that is probably good enough for such
    a one-off task.
    
    Bug: T95686
    Change-Id: I92ff59debf75096810d47045f11ff6906b2a7322

---
+++ repo/includes/PropertyDataTypeChanger.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Wikibase\Repo;
+
+use DataTypes\DataTypeFactory;
+use InvalidArgumentException;
+use Wikibase\DataModel\Entity\Property;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\Lib\Store\EntityStore;
+use Wikibase\Lib\Store\EntityRevisionLookup;
+use Wikibase\Lib\Store\StorageException;
+use User;
+
+/**
+ * Class for changing a property's data type.
+ * Please be aware of the implication such an operation has.
+ *
+ * @since 0.5
+ *
+ * @license GNU GPL v2+
+ * @author Marius Hoch
+ */
+class PropertyDataTypeChanger {
+
+	/**
+	 * @var EntityStore
+	 */
+	private $entityStore;
+
+	/**
+	 * @var EntityRevisionLookup
+	 */
+	private $entityRevisionLookup;
+
+	/**
+	 * @var DataTypeFactory
+	 */
+	private $dataTypeFactory;
+
+	/**
+	 * @param EntityRevisionLookup $entityRevisionLookup
+	 * @param EntityStore $entityStore
+	 * @param DataTypeFactory $dataTypeFactory
+	 */
+	public function __construct( EntityRevisionLookup $entityRevisionLookup, EntityStore $entityStore, DataTypeFactory $dataTypeFactory ) {
+		$this->entityRevisionLookup = $entityRevisionLookup;
+		$this->entityStore = $entityStore;
+		$this->dataTypeFactory = $dataTypeFactory;
+	}
+
+	/**
+	 * @param PropertyId $propertyId
+	 * @param User $user User to attribute the changes made to.
+	 * @param string $dataTypeId
+	 *
+	 * @throws InvalidArgumentException
+	 * @throws StorageException
+	 */
+	public function changeDataType( PropertyId $propertyId, User $user, $dataTypeId ) {
+		$entityRevision = $this->entityRevisionLookup->getEntityRevision(
+			$propertyId,
+			EntityRevisionLookup::LATEST_FROM_MASTER
+		);
+
+		if ( $entityRevision === null ) {
+			throw new StorageException( "Could not load property: " . $propertyId->getSerialization() );
+		}
+
+		/* @var $property Property */
+		$property = $entityRevision->getEntity();
+
+		$oldDataTypeId = $property->getDataTypeId();
+		$this->assertDataTypesCompatible( $oldDataTypeId, $dataTypeId );
+
+		$property->setDataTypeId( $dataTypeId );
+
+		$this->entityStore->saveEntity(
+			$property,
+			'Changing DataType from ' . $oldDataTypeId . ' to ' . $dataTypeId,
+			$user,
+			EDIT_UPDATE,
+			$entityRevision->getRevisionId()
+		);
+	}
+
+	/**
+	 * @param string $oldTypeId
+	 * @param string $newTypeId
+	 *
+	 * @throws InvalidArgumentException
+	 */
+	private function assertDataTypesCompatible( $oldTypeId, $newTypeId ) {
+		$oldType = $this->dataTypeFactory->getType( $oldTypeId );
+		$newType = $this->dataTypeFactory->getType( $newTypeId );
+
+		if ( $oldType->getDataValueType() !== $newType->getDataValueType() ) {
+			throw new InvalidArgumentException( "New and old DataType need to have the same data value type." );
+		}
+	}
+
+}
---
+++ repo/tests/phpunit/includes/PropertyDataTypeChangerTest.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace Wikibase\Repo\Test;
+
+use DataTypes\DataType;
+use DataTypes\DataTypeFactory;
+use PHPUnit_Framework_TestCase;
+use Wikibase\DataModel\Entity\Property;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\EntityRevision;
+use Wikibase\Lib\Store\EntityStore;
+use Wikibase\Lib\Store\StorageException;
+use Wikibase\Repo\PropertyDataTypeChanger;
+
+/**
+ * @covers Wikibase\Repo\PropertyDataTypeChanger;
+ *
+ * @since 0.5
+ *
+ * @group WikibaseRepo
+ * @group Wikibase
+ *
+ * @license GNU GPL v2+
+ * @author Marius Hoch
+ */
+class PropertyDataTypeChangerTest extends PHPUnit_Framework_TestCase {
+
+	public function testChangeDataType_success() {
+		$propertyId = new PropertyId( 'P42' );
+
+		$expectedProperty = new Property( $propertyId, null, 'shinydata' );
+
+		$entityStore = $this->getMock( 'Wikibase\Lib\Store\EntityStore' );
+		$entityStore->expects( $this->once() )
+			->method( 'saveEntity' )
+			->with(
+				$expectedProperty,
+				'Changing DataType from rustydata to shinydata',
+				$this->isInstanceOf( 'User' ),
+				EDIT_UPDATE, 6789
+			)
+			->will( $this->returnValue( new EntityRevision( $expectedProperty, 6790 ) ) );
+
+		$propertyDataTypeChanger = $this->getPropertyDataTypeChanger( $entityStore );
+		$propertyDataTypeChanger->changeDataType( $propertyId, $this->getMock( 'User' ), 'shinydata' );
+	}
+
+	public function testChangeDataType_propertyNotFound() {
+		$propertyId = new PropertyId( 'P43' );
+
+		$entityStore = $this->getMock( 'Wikibase\Lib\Store\EntityStore' );
+
+		$propertyDataTypeChanger = $this->getPropertyDataTypeChanger( $entityStore );
+
+		$this->setExpectedException(
+			'Wikibase\Lib\Store\StorageException',
+			"Could not load property: P43"
+		);
+		$propertyDataTypeChanger->changeDataType( $propertyId, $this->getMock( 'User' ), 'shinydata' );
+	}
+
+	public function testChangeDataType_saveFailed() {
+		$propertyId = new PropertyId( 'P42' );
+
+		$expectedProperty = new Property( $propertyId, null, 'shinydata' );
+		$storageException = new StorageException( 'whatever' );
+
+		$entityStore = $this->getMock( 'Wikibase\Lib\Store\EntityStore' );
+		$entityStore->expects( $this->once() )
+			->method( 'saveEntity' )
+			->with(
+				$expectedProperty,
+				'Changing DataType from rustydata to shinydata',
+				$this->isInstanceOf( 'User' ),
+				EDIT_UPDATE, 6789
+			)
+			->will( $this->throwException( $storageException ) );
+
+		$propertyDataTypeChanger = $this->getPropertyDataTypeChanger( $entityStore );
+
+		$this->setExpectedException( 'Wikibase\Lib\Store\StorageException' );
+		$propertyDataTypeChanger->changeDataType( $propertyId, $this->getMock( 'User' ), 'shinydata' );
+	}
+
+	public function testChangeDataType_mismatchingDataValueTypes() {
+		$propertyId = new PropertyId( 'P42' );
+
+		$entityStore = $this->getMock( 'Wikibase\Lib\Store\EntityStore' );
+
+		$propertyDataTypeChanger = $this->getPropertyDataTypeChanger( $entityStore );
+
+		$this->setExpectedException(
+			'InvalidArgumentException',
+			"New and old DataType need to have the same data value type."
+		);
+		$propertyDataTypeChanger->changeDataType( $propertyId, $this->getMock( 'User' ), 'otherdatatype' );
+	}
+
+	private function getPropertyDataTypeChanger( EntityStore $entityStore ) {
+		$entityRevisionLookup = $this->getMock( 'Wikibase\Lib\Store\EntityRevisionLookup' );
+
+		$entityRevisionLookup->expects( $this->once() )
+			->method( 'getEntityRevision' )
+			->will( $this->returnCallback( function( PropertyId $propertyId ) {
+				if ( $propertyId->getSerialization() === 'P42' ) {
+					$property = new Property(
+						new PropertyId( 'P42' ),
+						null,
+						'rustydata'
+					);
+
+					return new EntityRevision( $property, 6789, '20151015195144' );
+				} else {
+					return null;
+				}
+			} ) );
+
+		return new PropertyDataTypeChanger( $entityRevisionLookup, $entityStore, $this->getDataTypeFactory() );
+	}
+
+	private function getDataTypeFactory() {
+		$dataTypes = array();
+		$dataTypes[] = new DataType( 'rustydata', 'kittens' );
+		$dataTypes[] = new DataType( 'shinydata', 'kittens' );
+		$dataTypes[] = new DataType( 'otherdatatype', 'puppies' );
+
+		return DataTypeFactory::newFromTypes( $dataTypes );
+	}
+
+}
