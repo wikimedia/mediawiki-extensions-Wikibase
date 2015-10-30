d297af33aa2f7269e57f49322ee84a202d15ce37
Marius Hoch <hoo@online.de>
2015-10-30 21:54:00 +0100

    Create SkinTemplateOutputPageBeforeExec hook handler
    In order to pull that code out of ClientHooks and
    to make it testable.
    
    Change-Id: Ica79284cf49b2d942b18f4355b4d27936b75c74a

--- client/WikibaseClient.php
+++ client/WikibaseClient.php
@@ -108,7 +108,8 @@ call_user_func( function() {
 	$wgHooks['ParserFirstCallInit'][] = '\Wikibase\ClientHooks::onParserFirstCallInit';
 	$wgHooks['MagicWordwgVariableIDs'][] = '\Wikibase\ClientHooks::onMagicWordwgVariableIDs';
 	$wgHooks['ParserGetVariableValueSwitch'][] = '\Wikibase\ClientHooks::onParserGetVariableValueSwitch';
-	$wgHooks['SkinTemplateOutputPageBeforeExec'][] = '\Wikibase\ClientHooks::onSkinTemplateOutputPageBeforeExec';
+	$wgHooks['SkinTemplateOutputPageBeforeExec'][] =
+		'\Wikibase\Client\Hooks\SkinTemplateOutputPageBeforeExecHandler::onSkinTemplateOutputPageBeforeExec';
 	$wgHooks['SpecialMovepageAfterMove'][] = '\Wikibase\Client\Hooks\MovePageNotice::onSpecialMovepageAfterMove';
 	$wgHooks['SpecialWatchlistQuery'][] = '\Wikibase\ClientHooks::onSpecialWatchlistQuery';
 	$wgHooks['SpecialRecentChangesQuery'][] = '\Wikibase\ClientHooks::onSpecialRecentChangesQuery';
