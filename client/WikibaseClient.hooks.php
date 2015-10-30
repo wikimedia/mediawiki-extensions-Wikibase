f8aa01858342a8159c3849ab76f6b5ffbf83bedb
Marius Hoch <hoo@online.de>
2015-10-30 21:54:00 +0100

    Create SkinTemplateOutputPageBeforeExec hook handler
    In order to pull that code out of ClientHooks and
    to make it testable.
    
    Change-Id: Ica79284cf49b2d942b18f4355b4d27936b75c74a

--- client/WikibaseClient.hooks.php
+++ client/WikibaseClient.hooks.php
@@ -10,7 +10,6 @@ use IContextSource;
 use Message;
 use OutputPage;
 use Parser;
-use QuickTemplate;
 use RecentChange;
 use Skin;
 use SpecialRecentChanges;
@@ -28,7 +27,6 @@ use Wikibase\Client\Hooks\SpecialWatchlistQueryHandler;
 use Wikibase\Client\RecentChanges\ChangeLineFormatter;
 use Wikibase\Client\RecentChanges\ExternalChangeFactory;
 use Wikibase\Client\RecentChanges\RecentChangesFilterOptions;
-use Wikibase\Client\RepoItemLinkGenerator;
 use Wikibase\Client\WikibaseClient;
 use Wikibase\DataModel\Entity\EntityId;
 use Wikibase\Lib\AutoCommentFormatter;
@@ -337,66 +335,6 @@ final class ClientHooks {
 
 		$actionName = Action::getActionName( $skin->getContext() );
 		$beforePageDisplayHandler->addModules( $out, $actionName );
-
-		return true;
-	}
-
-	/**
-	 * Displays a list of links to pages on the central wiki at the end of the language box.
-	 *
-	 * @since 0.1
-	 *
-	 * @param Skin $skin
-	 * @param QuickTemplate $template
-	 *
-	 * @return bool
-	 */
-	public static function onSkinTemplateOutputPageBeforeExec( Skin &$skin, QuickTemplate &$template ) {
-		$title = $skin->getContext()->getTitle();
-		$wikibaseClient = WikibaseClient::getDefaultInstance();
-
-		if ( !self::isWikibaseEnabled( $title->getNamespace() ) ) {
-			// shorten out
-			return true;
-		}
-
-		$repoLinker = $wikibaseClient->newRepoLinker();
-		$entityIdParser = $wikibaseClient->getEntityIdParser();
-
-		$siteGroup = $wikibaseClient->getLangLinkSiteGroup();
-
-		$languageUrls = $template->get( 'language_urls' );
-		$hasLangLinks = $languageUrls !== false && !empty( $languageUrls );
-
-		$langLinkGenerator = new RepoItemLinkGenerator(
-			WikibaseClient::getDefaultInstance()->getNamespaceChecker(),
-			$repoLinker,
-			$entityIdParser,
-			$siteGroup,
-			$wikibaseClient->getSettings()->getSetting( 'siteGlobalID' )
-		);
-
-		$action = Action::getActionName( $skin->getContext() );
-
-		$noExternalLangLinks = $skin->getOutput()->getProperty( 'noexternallanglinks' );
-		$prefixedId = $skin->getOutput()->getProperty( 'wikibase_item' );
-
-		$editLink = $langLinkGenerator->getLink( $title, $action, $hasLangLinks, $noExternalLangLinks, $prefixedId );
-
-		// there will be no link in some situations, like add links widget disabled
-		if ( $editLink ) {
-			$template->set( 'wbeditlanglinks', $editLink );
-		}
-
-		// Needed to have "Other languages" section display, so we can add "add links".
-		// Only force the section to display if we are going to actually add such a link:
-		// Where external langlinks aren't suppressed and where action == 'view'.
-		if ( $languageUrls === false && $title->exists()
-			&& ( $noExternalLangLinks === null || !in_array( '*', $noExternalLangLinks ) )
-			&& $action === 'view'
-		) {
-			$template->set( 'language_urls', array() );
-		}
 
 		return true;
 	}
