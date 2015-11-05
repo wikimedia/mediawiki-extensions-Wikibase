a4665bc9955860ee73e205e90348898cfaeed758
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2015-11-05 20:25:06 +0100

    Add missing SqlStoreTest
    Ok, ok, this is only a factory. But it pops up in the coverage report
    as the top 1 "project risk" with an insane high number. This is easy to
    fix and you probably can not argue that this class should not have these
    tests.
    
    Change-Id: Iaa7d5027f22dd07e05d69f7db76d7b2724dc00ca

---
+++ repo/tests/phpunit/includes/store/sql/SqlStoreTest.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Wikibase\Repo\Tests\Store\Sql;
+
+use Wikibase\SqlStore;
+
+/**
+ * @covers Wikibase\SqlStore
+ *
+ * @group Wikibase
+ * @group WikibaseStore
+ * @group WikibaseRepo
+ *
+ * @licence GNU GPL v2+
+ * @author Thiemo Mättig
+ */
+class SqlIdGeneratorTest extends \MediaWikiTestCase {
+
+	public function newInstance() {
+		$contentCodec = $this->getMockBuilder( 'Wikibase\Lib\Store\EntityContentDataCodec' )
+			->disableOriginalConstructor()
+			->getMock();
+
+		return new SqlStore(
+			$contentCodec,
+			$this->getMock( 'Wikibase\DataModel\Entity\EntityIdParser' ),
+			$this->getMock( 'Wikibase\Store\EntityIdLookup' ),
+			$this->getMock( '\Wikibase\Lib\Store\EntityTitleLookup' )
+		);
+	}
+
+	public function testGetTermIndex() {
+		$service = $this->newInstance()->getTermIndex();
+		$this->assertInstanceOf( 'Wikibase\TermIndex', $service );
+	}
+
+	public function testGetLabelConflictFinder() {
+		$service = $this->newInstance()->getLabelConflictFinder();
+		$this->assertInstanceOf( 'Wikibase\Lib\Store\LabelConflictFinder', $service );
+	}
+
+	public function testNewIdGenerator() {
+		$service = $this->newInstance()->newIdGenerator();
+		$this->assertInstanceOf( 'Wikibase\IdGenerator', $service );
+	}
+
+	public function testNewSiteLinkStore() {
+		$service = $this->newInstance()->newSiteLinkStore();
+		$this->assertInstanceOf( 'Wikibase\Lib\Store\SiteLinkStore', $service );
+	}
+
+	public function testNewEntityPerPage() {
+		$service = $this->newInstance()->newEntityPerPage();
+		$this->assertInstanceOf( 'Wikibase\Repo\Store\EntityPerPage', $service );
+	}
+
+	public function testGetEntityRedirectLookup() {
+		$service = $this->newInstance()->getEntityRedirectLookup();
+		$this->assertInstanceOf( 'Wikibase\DataModel\Services\Lookup\EntityRedirectLookup', $service );
+	}
+
+	public function testGetEntityLookup() {
+		$factory = $this->newInstance();
+		$service = $this->newInstance()->getEntityLookup();
+		$this->assertInstanceOf( 'Wikibase\DataModel\Services\Lookup\EntityLookup', $service );
+	}
+
+	public function testGetEntityStoreWatcher() {
+		$service = $this->newInstance()->getEntityStoreWatcher();
+		$this->assertInstanceOf( 'Wikibase\Lib\Store\EntityStoreWatcher', $service );
+	}
+
+	public function testGetEntityStore() {
+		$service = $this->newInstance()->getEntityStore();
+		$this->assertInstanceOf( 'Wikibase\Lib\Store\EntityStore', $service );
+	}
+
+	public function testGetEntityRevisionLookup() {
+		$service = $this->newInstance()->getEntityRevisionLookup();
+		$this->assertInstanceOf( 'Wikibase\Lib\Store\EntityRevisionLookup', $service );
+	}
+
+	public function testGetEntityInfoBuilderFactory() {
+		$service = $this->newInstance()->getEntityInfoBuilderFactory();
+		$this->assertInstanceOf( 'Wikibase\Lib\Store\EntityInfoBuilderFactory', $service );
+	}
+
+	public function testGetPropertyInfoStore() {
+		$service = $this->newInstance()->getPropertyInfoStore();
+		$this->assertInstanceOf( 'Wikibase\PropertyInfoStore', $service );
+	}
+
+	public function testGetSiteLinkConflictLookup() {
+		$service = $this->newInstance()->getSiteLinkConflictLookup();
+		$this->assertInstanceOf( 'Wikibase\Repo\Store\SiteLinkConflictLookup', $service );
+	}
+
+	public function testGetEntityPrefetcher() {
+		$service = $this->newInstance()->getEntityPrefetcher();
+		$this->assertInstanceOf( 'Wikibase\Lib\Store\Sql\PrefetchingWikiPageEntityMetaDataAccessor', $service );
+	}
+
+	public function testGetChangeLookup() {
+		$service = $this->newInstance()->getChangeLookup();
+		$this->assertInstanceOf( 'Wikibase\Lib\Store\ChangeLookup', $service );
+	}
+
+	public function testGetChangeStore() {
+		$service = $this->newInstance()->getChangeStore();
+		$this->assertInstanceOf( 'Wikibase\Repo\Store\Sql\SqlChangeStore', $service );
+	}
+
+}
