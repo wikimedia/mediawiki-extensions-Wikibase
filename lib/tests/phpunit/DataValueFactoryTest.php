7a9053d709e6bbf0f83089e83200414966aa7944
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2015-11-06 17:11:29 +0100

    Add missing DataValueFactoryTest
    Change-Id: I128f06aebd3e1ab2837dd8096bee80ff1cf34498

---
+++ lib/tests/phpunit/DataValueFactoryTest.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Wikibase\Lib\Test;
+
+use DataValues\DataValueFactory;
+use DataValues\UnDeserializableValue;
+use Deserializers\Exceptions\DeserializationException;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * @covers DataValues\DataValueFactory
+ *
+ * @group WikibaseLib
+ * @group Wikibase
+ *
+ * @licence GNU GPL v2+
+ * @author Thiemo Mättig
+ */
+class DataValueFactoryTest extends PHPUnit_Framework_TestCase {
+
+	public function newInstance() {
+		$deserializer = $this->getMock( 'Deserializers\Deserializer' );
+		$deserializer->expects( $this->any() )
+			->method( 'deserialize' )
+			->will( $this->returnCallback( function( array $data ) {
+				if ( $data['type'] === 'string' ) {
+					return 'success';
+				}
+				throw new DeserializationException();
+			} ) );
+
+		return new DataValueFactory( $deserializer );
+	}
+
+	public function testGivenUnknownType_newDataValueFails() {
+		$this->setExpectedException( 'InvalidArgumentException' );
+		$this->newInstance()->newDataValue( 'unknown', '' );
+	}
+
+	public function testGivenKnownType_newDataValueSucceeds() {
+		$value = $this->newInstance()->newDataValue( 'string', '' );
+		$this->assertSame( 'success', $value );
+	}
+
+	public function testGivenUnknownType_tryNewDataValueFails() {
+		$this->setExpectedException( 'InvalidArgumentException' );
+		$this->newInstance()->tryNewDataValue( 'unknown', '' );
+	}
+
+	public function testGivenValidArguments_tryNewDataValueSucceeds() {
+		$value = $this->newInstance()->tryNewDataValue( 'string', '' );
+		$this->assertSame( 'success', $value );
+	}
+
+	public function testGivenNoType_newFromArrayFails() {
+		$this->setExpectedException( 'DataValues\IllegalValueException' );
+		$this->newInstance()->newFromArray( array() );
+	}
+
+	public function testGivenNoValue_newFromArrayFails() {
+		$this->setExpectedException( 'DataValues\IllegalValueException' );
+		$this->newInstance()->newFromArray( array( 'type' => 'unknown' ) );
+	}
+
+	public function testGivenUnknownType_newFromArrayFails() {
+		$this->setExpectedException( 'InvalidArgumentException' );
+		$this->newInstance()->newFromArray( array(
+			'type' => 'unknown',
+			'value' => '',
+		) );
+	}
+
+	public function testGivenKnownType_newFromArraySucceeds() {
+		$value = $this->newInstance()->newFromArray( array(
+			'type' => 'string',
+			'value' => '',
+		) );
+		$this->assertSame( 'success', $value );
+	}
+
+	public function testGivenNoType_tryNewFromArrayReturnsWithError() {
+		$value = $this->newInstance()->tryNewFromArray( array() );
+		$expected = new UnDeserializableValue( null, null, 'No type specified' );
+		$this->assertEquals( $expected, $value );
+	}
+
+	public function testGivenNoValue_tryNewFromArrayReturnsWithError() {
+		$value = $this->newInstance()->tryNewFromArray( array( 'type' => 'unknown' ) );
+		$expected = new UnDeserializableValue( null, 'unknown', 'No value data' );
+		$this->assertEquals( $expected, $value );
+	}
+
+	public function testGivenUnknownType_tryNewFromArrayFails() {
+		$this->setExpectedException( 'InvalidArgumentException' );
+		$this->newInstance()->tryNewFromArray( array(
+			'type' => 'unknown',
+			'value' => '',
+		) );
+	}
+
+	public function testGivenKnownType_tryNewFromArraySucceeds() {
+		$value = $this->newInstance()->tryNewFromArray( array(
+			'type' => 'string',
+			'value' => '',
+		) );
+		$this->assertSame( 'success', $value );
+	}
+
+}
