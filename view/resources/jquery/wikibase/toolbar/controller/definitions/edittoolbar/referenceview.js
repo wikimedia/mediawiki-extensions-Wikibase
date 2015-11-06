5d0a44721d42791150bb949193097b3017a1700c
jk <jonas.kress@wikimedia.de>
2015-11-06 14:26:31 +0100

    Remove referenceview edit mode handling
    Bug: T117702
    Change-Id: I227c5ff2ac8842ea0e6b8f3490bdfa572997aa1a

--- view/resources/jquery/wikibase/toolbar/controller/definitions/edittoolbar/referenceview.js
+++ view/resources/jquery/wikibase/toolbar/controller/definitions/edittoolbar/referenceview.js
@@ -12,108 +12,32 @@ $.wikibase.toolbarcontroller.definition( 'edittoolbar', {
 	selector: ':' + $.wikibase.referenceview.prototype.namespace
 		+ '-' + $.wikibase.referenceview.prototype.widgetName,
 	events: {
-		referenceviewcreate: function( event ) {
+		referenceviewafterstartediting: function( event ) {
 			var $referenceview = $( event.target ),
 				referenceview = $referenceview.data( 'referenceview' ),
 				options = {},
-				$container = $referenceview.find( '.wikibase-toolbar-container' );
-
-			if ( !$container.length ) {
 				$container = $( '<div/>' ).appendTo(
 					$referenceview.find( '.wikibase-referenceview-heading' )
 				);
-			}
 
 			options.$container = $container;
 
-			function removeFromListView() {
-				var $statementview = $referenceview.closest( ':wikibase-statementview' ),
-					statementview = $statementview.data( 'statementview' );
+			var $statementview = $referenceview.closest( ':wikibase-statementview' ),
+			statementview = $statementview.data( 'statementview' );
 
+			function removeFromListView() {
 				statementview._referencesListview.removeItem( $referenceview );
 			}
 
-			if ( !referenceview.options.statementGuid || !referenceview.value() ) {
-				var $statementview = $referenceview.closest( ':wikibase-statementview' ),
-					statementview = $statementview.data( 'statementview' );
-				if ( !statementview.isInEditMode() ) {
-					options.label = mw.msg( 'wikibase-cancel' );
-				}
-				$referenceview.removetoolbar( options )
-				.on( 'removetoolbarremove.removetoolbar', function( event ) {
-					removeFromListView();
-				} );
-
-				return;
+			if ( ( !referenceview.options.statementGuid || !referenceview.value() ) && !statementview.isInEditMode() ) {
+				options.label = mw.msg( 'wikibase-cancel' );
 			}
-
-			options.getHelpMessage = function() {
-				return $.Deferred().resolve( referenceview.options.helpMessage ).promise();
-			};
-
-			var edittoolbar = $referenceview.edittoolbar( options ).data( 'edittoolbar' );
-
-			var guid = referenceview.options.statementGuid;
-			var referencesChanger = referenceview.options.referencesChanger;
-			var controller = new wikibase.view.ToolbarViewController(
-				{
-					remove: function( reference ) {
-						return referencesChanger.removeReference( guid, reference );
-					},
-					save: function( reference ) {
-						return referencesChanger.setReference( guid, reference );
-					}
-				},
-				edittoolbar,
-				referenceview
-			);
-			edittoolbar.setController( controller );
-			edittoolbar.option(
-				'onRemove',
-				function() {
-					controller.remove().done( function() {
-						removeFromListView();
-					} );
-				}
-			);
-
-			$referenceview.on( 'keydown.edittoolbar', function( event ) {
-				if ( referenceview.option( 'disabled' ) ) {
-					return;
-				}
-				if ( event.keyCode === $.ui.keyCode.ESCAPE ) {
-					controller.stopEditing( true );
-				} else if ( event.keyCode === $.ui.keyCode.ENTER ) {
-					controller.stopEditing( false );
-				}
+			$referenceview.removetoolbar( options )
+			.on( 'removetoolbarremove.removetoolbar', function( event ) {
+				removeFromListView();
 			} );
-		},
-		referenceviewdisable: function( event ) {
-			var $referenceview = $( event.target ),
-				referenceview = $referenceview.data( 'referenceview' );
 
-			if ( !referenceview ) {
-				return;
-			}
-
-			var disable = referenceview.option( 'disabled' ),
-				edittoolbar = $referenceview.data( 'edittoolbar' );
-
-			if ( !edittoolbar ) {
-				return;
-			}
-
-			var btnSave = edittoolbar.getButton( 'save' ),
-				enableSave = ( referenceview.isValid() && !referenceview.isInitialValue() );
-
-			edittoolbar.option( 'disabled', disable );
-			if ( !disable ) {
-				btnSave.option( 'disabled', !enableSave );
-			}
 		}
-
-		// Destroying the referenceview will destroy the toolbar. Trying to destroy the toolbar
-		// in parallel will cause interference.
 	}
 } );
 
