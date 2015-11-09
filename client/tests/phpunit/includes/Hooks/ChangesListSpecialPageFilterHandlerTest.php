2e66a628422d7dacec9a31162a58d59c062f3c63
aude <aude.wiki@gmail.com>
2015-11-10 02:08:39 +0800

    Switch to use ChangesListSpecialPageFilters hook in client
    Not in scope of this patch is to rename the recent changes
    option name etc. While this might seem simple, it needs a
    migration strategy to convert existing preferences and
    b/w compat for the old filter name and should be done
    carefully in a separate patch.
    
    Bug: T64798
    Change-Id: I66d345e4355accf44f93ce8c6d765c41cebbcc3b

---
+++ client/tests/phpunit/includes/Hooks/ChangesListSpecialPageFilterHandlerTest.php
@@ -0,0 +1,226 @@
+<?php
+
+namespace Wikibase\Client\Tests\Hooks;
+
+use DerivativeContext;
+use FauxRequest;
+use RequestContext;
+use Wikibase\Client\Hooks\ChangesListSpecialPageFilterHandler;
+
+/**
+ * @covers Wikibase\Client\Hooks\ChangesListSpecialPageFilterHandler
+ *
+ * @group WikibaseClientHooks
+ * @group WikibaseClient
+ * @group Wikibase
+ */
+class ChangesListSpecialPageFilterHandlerTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * @dataProvider filterNotAddedWhenUsingEnhancedChangesProvider
+	 */
+	public function testFilterNotAddedWhenUsingEnhancedChanges(
+		array $requestParams,
+		array $userPreferences,
+		$pageName,
+		$message
+	) {
+		$hookHandler = new ChangesListSpecialPageFilterHandler(
+			$this->getRequest( $requestParams ),
+			$this->getUser( $userPreferences ),
+			$pageName,
+			true
+		);
+
+		$filters = array();
+		$hookHandler->addFilterIfEnabled( $filters );
+
+		$this->assertSame( array(), $filters, $message );
+	}
+
+	public function filterNotAddedWhenUsingEnhancedChangesProvider() {
+		return array(
+			array(
+				array(),
+				array( 'usenewrc' => 1 ),
+				'Watchlist',
+				'enhanced default pref for Watchlist'
+			),
+			array(
+				array(),
+				array( 'usenewrc' => 1 ),
+				'RecentChanges',
+				'enhanced default pref for RecentChanges'
+			),
+			array(
+				array( 'enhanced' => 1 ),
+				array( 'usenewrc' => 0 ),
+				'Watchlist',
+				'enhanced not default but has enhanced=1 req param'
+			),
+			array(
+				array( 'enhanced' => 1 ),
+				array( 'usenewrc' => 0 ),
+				'RecentChanges',
+				'enhanced not default but has enhanced=1 req param'
+			),
+			array(
+				array( 'enhanced' => 1 ),
+				array( 'usenewrc' => 1 ),
+				'Watchlist',
+				'enhanced default and has enhanced=1 req param'
+			),
+			array(
+				array( 'enhanced' => 1 ),
+				array( 'usenewrc' => 1 ),
+				'RecentChangesLinked',
+				'enhanced default and has enhanced=1 req param'
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider filterAddedWhenNotUsingEnhancedChangesProvider
+	 */
+	public function testFilterAddedWhenNotUsingEnhancedChanges_notShowWikibaseEditsByDefault(
+		array $requestParams,
+		array $userPreferences,
+		$expectedFilterName,
+		$specialPageName
+	) {
+		$hookHandler = new ChangesListSpecialPageFilterHandler(
+			$this->getRequest( $requestParams ),
+			$this->getUser( $userPreferences ),
+			$specialPageName,
+			true
+		);
+
+		$filters = array();
+		$hookHandler->addFilterIfEnabled( $filters );
+
+		$expected = array(
+			$expectedFilterName => array(
+				'msg' => 'wikibase-rc-hide-wikidata',
+				'default' => true
+			)
+		);
+
+		$this->assertSame( $expected, $filters );
+	}
+
+	public function filterAddedWhenNotUsingEnhancedChangesProvider() {
+		return array(
+			array(
+				array(),
+				array( 'usenewrc' => 0 ),
+				'hideWikibase',
+				'Watchlist'
+			),
+			array(
+				array( 'enhanced' => 0 ),
+				array( 'usenewrc' => 1 ),
+				'hidewikidata',
+				'RecentChanges'
+			),
+			array(
+				array(),
+				array( 'usenewrc' => 0 ),
+				'hidewikidata',
+				'RecentChangesLinked'
+			)
+		);
+	}
+
+	/**
+	 * @dataProvider filterAddedInNonEnhanced_withPrefToShowWikibaseEditsByDefaultProvider
+	 */
+	public function testFilterAddedInNonEnhanced_withPrefToShowWikibaseEditsByDefault(
+		array $userPreferences,
+		$expectedFilterName,
+		$specialPageName
+	) {
+		$hookHandler = new ChangesListSpecialPageFilterHandler(
+			$this->getRequest( array() ),
+			$this->getUser( $userPreferences ),
+			$specialPageName,
+			true
+		);
+
+		$filters = array();
+		$hookHandler->addFilterIfEnabled( $filters );
+
+		$expected = array(
+			$expectedFilterName => array(
+				'msg' => 'wikibase-rc-hide-wikidata',
+				'default' => false
+			)
+		);
+
+		$this->assertSame( $expected, $filters );
+	}
+
+	public function filterAddedInNonEnhanced_withPrefToShowWikibaseEditsByDefaultProvider() {
+		return array(
+			array(
+				array( 'wlshowwikibase' => 1, 'usenewrc' => 0 ),
+				'hideWikibase',
+				'Watchlist'
+			),
+			array(
+				array( 'rcshowwikidata' => 1, 'usenewrc' => 0 ),
+				'hidewikidata',
+				'RecentChanges'
+			)
+		);
+	}
+
+	/**
+	 * @dataProvider filterNotAddedWhenExternalRecentChangesDisabledProvider() {
+	 */
+	public function testFilterNotAddedWhenExternalRecentChangesDisabled( $specialPageName ) {
+		$hookHandler = new ChangesListSpecialPageFilterHandler(
+			$this->getRequest( array() ),
+			$this->getUser( array( 'usenewrc' => 0 ) ),
+			$specialPageName,
+			false
+		);
+
+		$filters = array();
+		$hookHandler->addFilterIfEnabled( $filters );
+
+		$this->assertSame( array(), $filters );
+	}
+
+	public function filterNotAddedWhenExternalRecentChangesDisabledProvider() {
+		return array(
+			array( 'Watchlist' ),
+			array( 'RecentChanges' ),
+			array( 'RecentChangesLinked' )
+		);
+	}
+
+	private function getRequest( array $requestParams ) {
+		return new FauxRequest( $requestParams );
+	}
+
+	private function getUser( array $options ) {
+		$user = $this->getMockBuilder( 'User' )
+			->disableOriginalConstructor()
+			->getMock();
+
+		$user->expects( $this->any() )
+			->method( 'getOption' )
+			->will( $this->returnCallback( function( $optionName ) use ( $options ) {
+				foreach ( $options as $key => $value ) {
+					if ( $optionName === $key ) {
+						return $value;
+					}
+				}
+
+				return null;
+			} ) );
+
+		return $user;
+	}
+
+}
