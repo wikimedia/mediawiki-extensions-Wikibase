0e5fc1c575f6aa0946a6cbc36e6495c4af5c695e
daniel <daniel.kinzler@wikimedia.de>
2015-11-09 19:24:17 +0100

    [WIP] Split SimpleValueRdfBuilder and ComplexValueRdfBuilder
    This replaces the monolithic implementation of the RDF mapping
    for data values with individual classes for each data type.
    
    TODO: missing tests for complex types
    
    Change-Id: I6d9b99d657e3dcd1de9072ba5582c3f05a99523b

---
+++ repo/includes/rdf/Values/GlobeCoordinateRdfBuilder.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Wikibase\Rdf\Values;
+
+use DataValues\Geo\Values\GlobeCoordinateValue;
+use Wikibase\DataModel\Snak\PropertyValueSnak;
+use Wikibase\Rdf\ValueSnakRdfBuilder;
+use Wikibase\Rdf\RdfVocabulary;
+use Wikimedia\Purtle\RdfWriter;
+
+/**
+ * RDF mapping for GlobeCoordinateValue.
+ *
+ * @todo: FIXME: test me!
+ *
+ * @since 0.5
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ * @author Stas Malyshev
+ */
+class GlobeCoordinateRdfBuilder implements ValueSnakRdfBuilder {
+
+	/**
+	 * @var ComplexValueRdfHelper|null
+	 */
+	private $complexValueHelper;
+
+	/**
+	 * @param ComplexValueRdfHelper|null $complexValueHelper
+	 */
+	function __construct( ComplexValueRdfHelper $complexValueHelper = null ) {
+		$this->complexValueHelper = $complexValueHelper;
+	}
+
+	/**
+	 * Adds specific value
+	 *
+	 * @param RdfWriter $writer
+	 * @param string $propertyValueNamespace Property value relation namespace
+	 * @param string $propertyValueLName Property value relation name
+	 * @param string $dataType Property data type
+	 * @param PropertyValueSnak $snak
+	 */
+	public function addValue(
+		RdfWriter $writer,
+		$propertyValueNamespace,
+		$propertyValueLName,
+		$dataType,
+		PropertyValueSnak $snak
+	) {
+		/** @var GlobeCoordinateValue $value */
+		$value = $snak->getDataValue();
+		$point = "Point({$value->getLatitude()} {$value->getLongitude()})";
+		$writer->say( $propertyValueNamespace, $propertyValueLName )
+			->value( $point, RdfVocabulary::NS_GEO, "wktLiteral" );
+
+		if ( $this->complexValueHelper !== null ) {
+			$this->addValueNode( $writer, $propertyValueNamespace, $propertyValueLName, $dataType, $value );
+		}
+	}
+
+	/**
+	 * Adds a value node representing all details of $value
+	 *
+	 * @todo FIXME test me
+	 *
+	 * @param RdfWriter $writer
+	 * @param string $propertyValueNamespace Property value relation namespace
+	 * @param string $propertyValueLName Property value relation name
+	 * @param string $dataType Property data type
+	 * @param GlobeCoordinateValue $value
+	 */
+	private function addValueNode(
+		RdfWriter $writer,
+		$propertyValueNamespace,
+		$propertyValueLName,
+		$dataType,
+		GlobeCoordinateValue $value
+	) {
+		$valueLName = $this->complexValueHelper->attachValueNode(
+			$writer,
+			$propertyValueNamespace,
+			$propertyValueLName,
+			$dataType,
+			$value
+		);
+
+		if ( $valueLName === null ) {
+			// The value node is already present in the output, don't create it again!
+			return;
+		}
+
+		$valueWriter = $this->complexValueHelper->getValueNodeWriter();
+
+		$valueWriter->say( RdfVocabulary::NS_ONTOLOGY, 'geoLatitude' )
+			->value( $value->getLatitude(), 'xsd', 'decimal' );
+
+		$valueWriter->say( RdfVocabulary::NS_ONTOLOGY, 'geoLongitude' )
+			->value( $value->getLongitude(), 'xsd', 'decimal' );
+
+		$valueWriter->say( RdfVocabulary::NS_ONTOLOGY, 'geoPrecision' )
+			->value( $value->getPrecision(), 'xsd', 'decimal' );
+
+		$valueWriter->say( RdfVocabulary::NS_ONTOLOGY, 'geoGlobe' )
+			->is( trim( $value->getGlobe() ) );
+	}
+
+}
---
+++ repo/includes/rdf/Values/ObjectValueRdfBuilder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Wikibase\Rdf\Values;
+
+use DataValues\DataValue;
+use Wikibase\DataModel\Snak\PropertyValueSnak;
+use Wikibase\Rdf\ValueSnakRdfBuilder;
+use Wikimedia\Purtle\RdfWriter;
+
+/**
+ * RDF mapping for DataValues that map to a resource (object) URI..
+ *
+ * @since 0.5
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ * @author Stas Malyshev
+ */
+class ObjectValueRdfBuilder implements ValueSnakRdfBuilder {
+
+	/**
+	 * Adds specific value
+	 *
+	 * @param RdfWriter $writer
+	 * @param string $propertyValueNamespace Property value relation namespace
+	 * @param string $propertyValueLName Property value relation name
+	 * @param string $dataType Property data type
+	 * @param PropertyValueSnak $snak
+	 */
+	public function addValue(
+		RdfWriter $writer,
+		$propertyValueNamespace,
+		$propertyValueLName,
+		$dataType,
+		PropertyValueSnak $snak
+	) {
+		$uri = $this->getValueUri( $snak->getDataValue() );
+
+		$writer->say( $propertyValueNamespace, $propertyValueLName )->is( $uri );
+	}
+
+	/**
+	 * @param DataValue $value
+	 *
+	 * @return string the object URI
+	 */
+	protected function getValueUri( DataValue $value ) {
+		return trim( $value->getValue() );
+	}
+}
