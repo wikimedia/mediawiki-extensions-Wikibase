7f43580e46d8bf7510baba589629612757e1af18
daniel <daniel.kinzler@wikimedia.de>
2015-11-09 19:24:17 +0100

    Split SimpleValueRdfBuilder and ComplexValueRdfBuilder
    This replaces the monolithic implementation of the RDF mapping
    for data values with individual classes for each data type.
    
    NOTE: SimpleValueRdfBuilderTest and ComplexValueRdfBuilderTest
    are kept to make sure that all previous test cases still pass.
    They should now be redundant though, and can be removed in
    a future patch.
    
    Bug: T118500
    Change-Id: I6d9b99d657e3dcd1de9072ba5582c3f05a99523b

---
+++ repo/includes/rdf/Values/EntityIdRdfBuilder.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Wikibase\Rdf\Values;
+
+use Wikibase\DataModel\Snak\PropertyValueSnak;
+use Wikibase\Rdf\ValueSnakRdfBuilder;
+use Wikibase\Rdf\EntityMentionListener;
+use Wikibase\Rdf\RdfVocabulary;
+use Wikimedia\Purtle\RdfWriter;
+
+/**
+ * RDF mapping for wikibase-entity DataValues.
+ *
+ * @since 0.5
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ * @author Stas Malyshev
+ */
+class EntityIdRdfBuilder implements ValueSnakRdfBuilder {
+
+	/**
+	 * @var EntityMentionListener
+	 */
+	private $mentionedEntityTracker;
+
+	/**
+	 * @param RdfVocabulary $vocabulary
+	 * @param EntityMentionListener $mentionedEntityTracker
+	 */
+	public function __construct( RdfVocabulary $vocabulary, EntityMentionListener $mentionedEntityTracker ) {
+		$this->vocabulary = $vocabulary;
+
+		$this->mentionedEntityTracker = $mentionedEntityTracker;
+	}
+
+	/**
+	 * Adds specific value
+	 *
+	 * @param RdfWriter $writer
+	 * @param string $propertyValueNamespace Property value relation namespace
+	 * @param string $propertyValueLName Property value relation name
+	 * @param string $dataType Property data type
+	 * @param PropertyValueSnak $snak
+	 */
+	public function addValue(
+		RdfWriter $writer,
+		$propertyValueNamespace,
+		$propertyValueLName,
+		$dataType,
+		PropertyValueSnak $snak
+	) {
+		$value = $snak->getDataValue();
+		$entityId = $value->getValue()->getEntityId();
+		$entityLName = $this->vocabulary->getEntityLName( $entityId );
+		$writer->say( $propertyValueNamespace, $propertyValueLName )->is( RdfVocabulary::NS_ENTITY, $entityLName );
+
+		$this->mentionedEntityTracker->entityReferenceMentioned( $entityId );
+	}
+
+}
---
+++ repo/tests/phpunit/includes/rdf/Values/GlobeCoordinateRdfBuilderTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Wikibase\Test\Rdf;
+
+use DataValues\Geo\Values\LatLongValue;
+use DataValues\GlobeCoordinateValue;
+use DataValues\StringValue;
+use Wikibase\DataModel\Entity\Property;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\Rdf\HashDedupeBag;
+use Wikibase\Rdf\RdfVocabulary;
+use Wikibase\Rdf\Values\ComplexValueRdfHelper;
+use Wikibase\Rdf\Values\GlobeCoordinateRdfBuilder;
+use Wikimedia\Purtle\NTriplesRdfWriter;
+use Wikibase\DataModel\Snak\PropertyValueSnak;
+
+/**
+ * @covers Wikibase\Rdf\Values\GlobeCoordinateRdfBuilder
+ *
+ * @group Wikibase
+ * @group WikibaseRepo
+ * @group WikibaseRdf
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ */
+class GlobeCoordinateRdfBuilderTest extends \PHPUnit_Framework_TestCase {
+
+	public function provideAddValue() {
+		$value = new GlobeCoordinateValue(
+			new LatLongValue( 12.25, -45.5 ),
+			0.025,
+			'https://www.wikidata.org/entity/Q2'
+		);
+
+		$snak = new PropertyValueSnak( new PropertyId( 'P7' ), $value );
+
+		return array(
+			'simple' => array(
+				$snak,
+				false,
+				array(
+					'<http://www/Q1> <http://acme/statement/P7> "Point(12.25 -45.5)"^^<http://acme/geo/wktLiteral> .',
+				)
+			),
+			'complex' => array(
+				$snak,
+				true,
+				array(
+					'<http://www/Q1> <http://acme/statement/P7> "Point(12.25 -45.5)"^^<http://acme/geo/wktLiteral> .',
+					'<http://www/Q1> <http://acme/statement/value/P7> <http://acme/value/7901049a90a3b6a6cbbae50dc76c2da9> .',
+					'<http://acme/value/7901049a90a3b6a6cbbae50dc76c2da9> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://acme/onto/GlobecoordinateValue> .',
+					'<http://acme/value/7901049a90a3b6a6cbbae50dc76c2da9> <http://acme/onto/geoLatitude> "12.25"^^<http://www.w3.org/2001/XMLSchema#decimal> .',
+					'<http://acme/value/7901049a90a3b6a6cbbae50dc76c2da9> <http://acme/onto/geoLongitude> "-45.5"^^<http://www.w3.org/2001/XMLSchema#decimal> .',
+					'<http://acme/value/7901049a90a3b6a6cbbae50dc76c2da9> <http://acme/onto/geoPrecision> "0.025"^^<http://www.w3.org/2001/XMLSchema#decimal> .',
+					'<http://acme/value/7901049a90a3b6a6cbbae50dc76c2da9> <http://acme/onto/geoGlobe> <https://www.wikidata.org/entity/Q2> .',
+				)
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider provideAddValue
+	 */
+	public function testAddValue( PropertyValueSnak $snak, $complex, array $expected ) {
+		$vocab = new RdfVocabulary( 'http://acme.com/item/', 'http://acme.com/data/' );
+
+		$snakWriter = new NTriplesRdfWriter();
+		$snakWriter->prefix( 'www', "http://www/" );
+		$snakWriter->prefix( 'acme', "http://acme/" );
+		$snakWriter->prefix( RdfVocabulary::NSP_CLAIM_VALUE, "http://acme/statement/value/" );
+		$snakWriter->prefix( RdfVocabulary::NSP_CLAIM_STATEMENT, "http://acme/statement/" );
+		$snakWriter->prefix( RdfVocabulary::NS_VALUE, "http://acme/value/" );
+		$snakWriter->prefix( RdfVocabulary::NS_ONTOLOGY, "http://acme/onto/" );
+		$snakWriter->prefix( RdfVocabulary::NS_GEO, "http://acme/geo/" );
+
+		if ( $complex ) {
+			$valueWriter = $snakWriter->sub();
+			$helper = new ComplexValueRdfHelper( $vocab, $valueWriter, new HashDedupeBag() );
+		} else {
+			$helper = null;
+		}
+
+		$builder = new GlobeCoordinateRdfBuilder( $helper );
+
+		$snakWriter->start();
+		$snakWriter->about( 'www', 'Q1' );
+
+		$builder->addValue(
+			$snakWriter,
+			RdfVocabulary::NSP_CLAIM_STATEMENT,
+			$vocab->getEntityLName( $snak->getPropertyid() ),
+			'DUMMY',
+			$snak
+		);
+
+		$triples = trim( $snakWriter->drain() );
+		$this->assertEquals( join( "\n", $expected ), $triples );
+	}
+
+}
