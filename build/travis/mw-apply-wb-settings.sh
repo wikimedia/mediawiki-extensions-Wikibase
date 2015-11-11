e664a3d82ad30b68aaee338fe1a2cb6fd4e16522
Jan Zerebecki <jan.wikimedia@zerebecki.de>
2015-11-11 16:51:57 +0100

    Load example config when under WM CI.
    Bug: T112572
    Change-Id: I5766cd9092df5b62b7908790fda66a95168bd46b

--- build/travis/mw-apply-wb-settings.sh
+++ build/travis/mw-apply-wb-settings.sh
@@ -5,32 +5,34 @@ set -x
 cd ../phase3
 
 function apply_client_settings {
-  echo '$wgEnableWikibaseRepo = false;' >> LocalSettings.php
   echo '$wgEnableWikibaseClient = true;' >> LocalSettings.php
-  echo 'require_once __DIR__ . "/extensions/Wikibase/client/WikibaseClient.php";' >> LocalSettings.php
-  echo 'require_once __DIR__ . "/extensions/Wikibase/client/ExampleSettings.php";' >> LocalSettings.php
   echo '$wgWBClientSettings["siteGlobalID"] = "enwiki";' >> LocalSettings.php
   echo 'require_once __DIR__ . "/extensions/Scribunto/Scribunto.php";' >> LocalSettings.php
 }
 
 function apply_repo_settings {
   echo '$wgEnableWikibaseRepo = true;' >> LocalSettings.php
-  echo '$wgEnableWikibaseClient = false;' >> LocalSettings.php
-  echo 'require_once __DIR__ . "/extensions/Wikibase/repo/Wikibase.php";' >> LocalSettings.php
-  echo 'require_once __DIR__ . "/extensions/Wikibase/repo/ExampleSettings.php";' >> LocalSettings.php
 }
 
-function apply_common_settings {
+function apply_common_before_settings {
   echo 'error_reporting(E_ALL| E_STRICT);' >> LocalSettings.php
   echo 'ini_set("display_errors", 1);' >> LocalSettings.php
+  echo '$wgWikimediaJenkinsCI = true;' >> LocalSettings.php
   echo '$wgShowExceptionDetails = true;' >> LocalSettings.php
   echo '$wgDevelopmentWarnings = true;' >> LocalSettings.php
   echo '$wgLanguageCode = "'$LANG'";' >> LocalSettings.php
   echo '$wgDebugLogFile = "mw-debug.log";' >> LocalSettings.php
   echo 'require_once __DIR__ . "/extensions/cldr/cldr.php";' >> LocalSettings.php
+  echo '$wgEnableWikibaseClient = false;' >> LocalSettings.php
+  echo '$wgEnableWikibaseRepo = false;' >> LocalSettings.php
 }
 
-apply_common_settings
+function apply_common_after_settings {
+  echo 'require_once __DIR__ . "/extensions/Wikibase/Wikibase.php";' >> LocalSettings.php
+}
+
+
+apply_common_before_settings
 
 if [ "$WB" = "repo" ]
 then
@@ -42,3 +44,5 @@ else
   apply_repo_settings
   apply_client_settings
 fi
+
+apply_common_after_settings
