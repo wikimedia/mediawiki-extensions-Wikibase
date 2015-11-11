a193aefefb3909e1c0d92c14a5c9694caf7313d1
jk <jonas.kress@wikimedia.de>
2015-11-11 15:42:40 +0100

    Add icons to text in toolbars
    Bug: T87757
    Change-Id: Ifd00e0ec966a359e60e16bb813ec17e470d36077

--- view/resources/jquery/wikibase/toolbar/jquery.wikibase.edittoolbar.js
+++ view/resources/jquery/wikibase/toolbar/jquery.wikibase.edittoolbar.js
@@ -197,8 +197,7 @@ $.widget( 'wikibase.edittoolbar', PARENT, {
 		}
 
 		$subToolbar.toolbar( {
-			$content: $content,
-			renderItemSeparators: true
+			$content: $content
 		} );
 	},
 
@@ -394,7 +393,6 @@ $.widget( 'wikibase.edittoolbar', PARENT, {
 
 		this._$tooltipAnchor = $( '<span/>', {
 			'class': 'mw-help-field-hint',
-			style: 'display:inline-block;text-decoration:none;width:8px;', // TODO: Get rid of inline styles.
 			html: '&nbsp;' // TODO find nicer way to hack Webkit browsers to display tooltip image (see also css)
 		} ).toolbaritem();
 
