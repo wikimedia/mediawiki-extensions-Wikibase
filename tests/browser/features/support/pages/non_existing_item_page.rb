69544e7f6b58105f9c8640d0fa15930e865232a3
Jan Zerebecki <jan.wikimedia@zerebecki.de>
2015-11-16 15:22:58 +0100

    [DNM] add browsertest to be run by jenkins
    copied from WikidataBrowserTests.git
    
    Change-Id: If4e1a88adca38ea7c719dc1398c71f0747f4b7e4

---
+++ tests/browser/features/support/pages/non_existing_item_page.rb
@@ -0,0 +1,16 @@
+# -*- encoding : utf-8 -*-
+# Wikidata UI tests
+#
+# Author:: Tobias Gritschacher (tobias.gritschacher@wikimedia.de)
+# License:: GNU GPL v2+
+#
+# page object for a non existing item
+
+class NonExistingItemPage
+  include PageObject
+  page_url ENV['WIKIDATA_REPO_URL'] + ENV['ITEM_NAMESPACE'] + ENV['ITEM_ID_PREFIX'] + 'xy'
+
+  h1(:first_heading, id: 'firstHeading')
+  link(:special_log_link, css: 'div#mw-content-text > div > p > span > a:nth-child(1)')
+  link(:special_create_new_item_link, css: 'div#mw-content-text > div > p > a:nth-child(2)')
+end
