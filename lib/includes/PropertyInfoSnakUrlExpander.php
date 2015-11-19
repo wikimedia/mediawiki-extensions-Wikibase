78f42e526e202500066c85341987ba7b5860b2a8
daniel <daniel.kinzler@wikimedia.de>
2015-11-19 18:42:18 +0100

    Introduce SnakUrlExpander
    SnakUrlExpander takes a snak and returns a URL (or URI).
    
    PropertyInfoSnakUrlExpander looks up a URL pattern
    in a PropertyInfoStore and fills in a string value
    from the snak to generate the URL.
    
    SnakUrlExpander is intended to be used by formatters to generate
    links for external IDs, and by RDF mappigns to generate URIs for
    external IDs. It may also be used to generate derived values
    representing these URLs and URIs for serialization.
    
    Bug: T112762
    Change-Id: Ib5e0ec2188731a6c648a806d0f7717e47084a50a

---
+++ lib/includes/PropertyInfoSnakUrlExpander.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Wikibase\Lib;
+
+use Wikibase\DataModel\Snak\PropertyValueSnak;
+use Wikimedia\Assert\Assert;
+
+/**
+ * SnakUrlExpander that uses an PropertyInfoProvider to find
+ * a URL pattern for expanding a Snak's value into an URL.
+ *
+ * @since 0.5
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ */
+class PropertyInfoSnakUrlExpander implements SnakUrlExpander {
+
+	/**
+	 * @var PropertyInfoProvider
+	 */
+	private $infoProvider;
+
+	/**
+	 * @param PropertyInfoProvider $infoProvider
+	 */
+	public function __construct( PropertyInfoProvider $infoProvider ) {
+		$this->infoProvider = $infoProvider;
+	}
+
+	/**
+	 * @see SnakUrlExpander::expandUrl
+	 *
+	 * @param PropertyValueSnak $snak
+	 *
+	 * @return string|null A URL or URI derived from the Snak, or null if no such URL
+	 *         could be determined.
+	 */
+	public function expandUrl( PropertyValueSnak $snak ) {
+		$propertyId = $snak->getPropertyId();
+		$value = $snak->getDataValue();
+
+		Assert::parameterType( 'DataValues\StringValue', $value, '$snak->getDataValue()' );
+
+		$pattern = $this->infoProvider->getPropertyInfo( $propertyId );
+
+		if ( $pattern === null ) {
+			return null;
+		}
+
+		$id = urlencode( $value->getValue() );
+		$url = str_replace( '$1', $id, $pattern );
+		return $url;
+	}
+
+}
