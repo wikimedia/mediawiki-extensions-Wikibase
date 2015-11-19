20b5502d7715a0726bd442e07de19d5c6252dcc0
daniel <daniel.kinzler@wikimedia.de>
2015-11-19 16:25:39 +0100

    Introduce EscapingSnakFormatter.
    An escaping mechanism is needed when registering callbacks
    that can produce SnakFormatters for different target formats.
    
    Change-Id: Iffdf8aac7449639b5f84d8c95adc1c3c92cc0952

---
+++ lib/tests/phpunit/formatters/EscapingSnakFormatterTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Wikibase\Lib\Test;
+
+use DataValues\StringValue;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\DataModel\Snak\PropertyValueSnak;
+use Wikibase\Lib\EscapingSnakFormatter;
+use Wikibase\Lib\PropertyValueSnakFormatter;
+use Wikibase\Lib\SnakFormatter;
+
+/**
+ * @covers Wikibase\Lib\EscapingSnakFormatter
+ *
+ * @group SnakFormatters
+ * @group DataValueExtensions
+ * @group WikibaseLib
+ * @group Wikibase
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ */
+class EscapingSnakFormatterTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * @param string $output
+	 *
+	 * @return PropertyValueSnakFormatter
+	 */
+	private function getSnakFormatter( $output ) {
+		$formatter = $this->getMock( 'Wikibase\Lib\SnakFormatter' );
+
+		$formatter->expects( $this->any() )
+			->method( 'formatSnak' )
+			->will( $this->returnValue( $output ) );
+
+		$formatter->expects( $this->any() )
+			->method( 'getFormat' )
+			->will( $this->returnValue( SnakFormatter::FORMAT_PLAIN ) );
+
+		return $formatter;
+	}
+
+	public function testFormatSnak() {
+		$formatter = new EscapingSnakFormatter(
+			SnakFormatter::FORMAT_HTML,
+			$this->getSnakFormatter( '<foo>' ),
+			'htmlspecialchars'
+		);
+
+		$p1 = new PropertyId( 'P77' );
+		$snak = new PropertyValueSnak( $p1, new StringValue( 'DUMMY' ) );
+		$this->assertEquals( '&lt;foo&gt;', $formatter->formatSnak( $snak ) );
+	}
+
+	public function testGetFormat() {
+		$formatter = new EscapingSnakFormatter(
+			'text/whatever',
+			$this->getSnakFormatter( '<foo>' ),
+			'htmlspecialchars'
+		);
+
+		$this->assertEquals( 'text/whatever', $formatter->getFormat() );
+	}
+
+}
