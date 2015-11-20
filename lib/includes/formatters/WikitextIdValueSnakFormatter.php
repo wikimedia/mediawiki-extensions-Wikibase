8900136d7780c59accab861a0247bed56649cb2e
daniel <daniel.kinzler@wikimedia.de>
2015-11-20 18:57:22 +0100

    Introduce SnakFormatters for ID snaks.
    The new formatters are not yet wired up to be used anywhere.
    For that, we still need Iff9f832b to be merged.
    
    Bug: T112760
    Change-Id: I5eb7bf7afc18b006cdac387e8872ef8cc0d51cc3

---
+++ lib/includes/formatters/WikitextIdValueSnakFormatter.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Wikibase\Lib\Formatters;
+
+use DataValues\UnDeserializableValue;
+use ValueFormatters\FormattingException;
+use ValueFormatters\ValueFormatter;
+use Wikibase\DataModel\Snak\PropertyValueSnak;
+use Wikibase\DataModel\Snak\Snak;
+use Wikibase\Lib\SnakFormatter;
+use Wikibase\Lib\SnakUrlExpander;
+use Wikimedia\Assert\Assert;
+use Wikimedia\Assert\ParameterTypeException;
+
+/**
+ * A formatter for WikitextExternalIdentifierFormatter that contain a StringValue that is
+ * interpreted as an external identifier. The identifier is rendered as an wikitext link to some
+ * resource about the wikitext. The link is created based on a URL pattern associated with the
+ * snak's PropertyID via a SnakUrlExpander.
+ *
+ * @license GPL 2+
+ * @author Daniel Kinzler
+ */
+class WikitextExternalIdentifierFormatter implements SnakFormatter {
+
+	/**
+	 * @var SnakUrlExpander
+	 */
+	private $urlExpander;
+
+	/**
+	 * @param SnakUrlExpander $urlExpander
+	 */
+	public function __construct(
+		SnakUrlExpander $urlExpander
+	) {
+		$this->urlExpander = $urlExpander;
+	}
+
+	/**
+	 * Formats the given Snak as an wikitext link to an authoritative resource.
+	 * The URL of that link is determined using a SnakUrlExpander.
+	 * If the snak could not be expanded into a URL, the identifier is returned as simple text.
+	 *
+	 * @param Snak $snak
+	 *
+	 * @throws ParameterTypeException if $snak is not a PropertyValueSnak, or if $snak->getValue()
+	 * does not return a StringValue.
+	 * @return string Text in the format indicated by getFormat()
+	 */
+	public function formatSnak( Snak $snak ) {
+		Assert::parameterType( 'Wikibase\DataModel\Snak\PropertyValueSnak', $snak, '$snak' );
+		/** @var PropertyValueSnak $snak */
+
+		$id = $snak->getDataValue()->getValue();
+		$url = $this->urlExpander->expandUrl( $snak );
+
+		if ( $url === null ) {
+			return wfEscapeWikiText( $id );
+		} else {
+			return '[' . $url . ' ' . wfEscapeWikiText( $id ) . ']';
+		}
+	}
+
+	/**
+	 * @see SnakFormatter::getFormat
+	 *
+	 * @return string SnakFormatter::FORMAT_HTML
+	 */
+	public function getFormat() {
+		return SnakFormatter::FORMAT_HTML;
+	}
+
+}
