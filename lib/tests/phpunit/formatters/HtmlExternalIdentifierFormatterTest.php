aa74088c81805e306b8dcca825219d7e3511230d
daniel <daniel.kinzler@wikimedia.de>
2015-11-20 18:57:22 +0100

    Introduce SnakFormatters for ID snaks.
    The new formatters are not yet wired up to be used anywhere.
    For that, we still need Iff9f832b to be merged.
    
    Bug: T112760
    Change-Id: I5eb7bf7afc18b006cdac387e8872ef8cc0d51cc3

---
+++ lib/tests/phpunit/formatters/HtmlExternalIdentifierFormatterTest.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Wikibase\Lib\Formatters\Test;
+
+use DataValues\StringValue;
+use DataValues\UnDeserializableValue;
+use ValueFormatters\ValueFormatter;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\DataModel\Snak\PropertyNoValueSnak;
+use Wikibase\DataModel\Snak\PropertyValueSnak;
+use Wikibase\Lib\Formatters\HtmlExternalIdentifierFormatter;
+use Wikibase\Lib\SnakFormatter;
+use Wikibase\Lib\SnakUrlExpander;
+
+/**
+ * @covers Wikibase\Lib\Formatters\HtmlExternalIdentifierFormatter
+ *
+ * @group ValueFormatters
+ * @group DataValueExtensions
+ * @group WikibaseLib
+ * @group Wikibase
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ */
+class HtmlExternalIdentifierFormatterTest extends \PHPUnit_Framework_TestCase {
+
+	public function provideFormatSnak() {
+		$formatterUrlExpander = $this->getMock( 'Wikibase\Lib\SnakUrlExpander' );
+
+		$formatterUrlExpander->expects( $this->any() )
+			->method( 'expandUrl' )
+			->will( $this->returnCallback( function( PropertyValueSnak $snak ) {
+				$id = $snak->getDataValue()->getValue();
+
+				if ( $snak->getPropertyId()->getSerialization() === 'P1' ) {
+					return 'http://acme.test/stuff/' . urlencode( $id );
+				} else {
+					return null;
+				}
+			} ) );
+
+		return array(
+			'formatter URL' => array(
+				$formatterUrlExpander,
+				new PropertyValueSnak( new PropertyId( 'P1' ), new StringValue( 'abc&123' ) ),
+				'<a class="wb-external-id" href="http://acme.test/stuff/abc%26123">abc&amp;123</a>'
+			),
+			'unknown property' => array(
+				$formatterUrlExpander,
+				new PropertyValueSnak( new PropertyId( 'P2' ), new StringValue( 'abc&123' ) ),
+				'<span class="wb-external-id">abc&amp;123</span>'
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider provideFormatSnak
+	 */
+	public function testFormatSnak(
+		SnakUrlExpander $urlExpander,
+		PropertyValueSnak $snak,
+		$expected
+	) {
+		$formatter = new HtmlExternalIdentifierFormatter( $urlExpander );
+		$text = $formatter->formatSnak( $snak );
+		$this->assertEquals( $expected, $text );
+	}
+
+	public function provideFormatSnak_ParameterTypeException() {
+		return array(
+			'bad snak type' => array(
+				new PropertyNoValueSnak( new PropertyId( 'P7' ) )
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider provideFormatSnak_ParameterTypeException
+	 */
+	public function testFormatSnak_ParameterTypeException( $snak ) {
+		$urlExpander = $this->getMock( 'Wikibase\Lib\SnakUrlExpander' );
+		$formatter = new HtmlExternalIdentifierFormatter( $urlExpander );
+
+		$this->setExpectedException( 'Wikimedia\Assert\ParameterTypeException' );
+		$formatter->formatSnak( $snak );
+	}
+
+	public function testGetFormat() {
+		$urlExpander = $this->getMock( 'Wikibase\Lib\SnakUrlExpander' );
+		$formatter = new HtmlExternalIdentifierFormatter( $urlExpander );
+
+		$this->assertSame( SnakFormatter::FORMAT_HTML, $formatter->getFormat() );
+	}
+
+}
