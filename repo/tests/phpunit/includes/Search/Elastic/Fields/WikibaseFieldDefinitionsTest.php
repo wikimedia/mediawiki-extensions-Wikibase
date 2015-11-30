c30e3e3e967e571bfbadc7748b339d825c18f34c
aude <aude.wiki@gmail.com>
2015-11-30 17:14:03 +0100

    Introduce hook handlers for CirrusSearch
    We use the CirrusSearchMappingConfig and the
    CirrusSearchBuildDocumentParse hooks to add
    fields that contain a count of site links and
    count of statements.
    
    These fields can potentially be considered when
    ranking search results and boost some items with
    a high number of site links and/or statements.
    
    Bug: T119066
    Change-Id: I34cce7281d10cc1c4176ebed6b9a3b90e10fe1da

---
+++ repo/tests/phpunit/includes/Search/Elastic/Fields/WikibaseFieldDefinitionsTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Wikibase\Test;
+
+use Wikibase\Repo\Search\Elastic\Fields\WikibaseFieldDefinitions;
+
+/**
+ * @covers Wikibase\Repo\Search\Elastic\Fields\WikibaseFieldDefinitions
+ *
+ * @group WikibaseElastic
+ * @group WikibaseRepo
+ * @group Wikibase
+ *
+ * @licence GNU GPL v2+
+ * @author Katie Filbert < aude.wiki@gmail.com >
+ */
+class WikibaseFieldDefinitionsTest extends \PHPUnit_Framework_TestCase {
+
+	public function testGetFields() {
+		$wikibaseFieldDefinitions = new WikibaseFieldDefinitions();
+		$fields = $wikibaseFieldDefinitions->getFields();
+
+		$expectedFieldNames = array( 'sitelink_count', 'statement_count' );
+
+		$this->assertSame( $expectedFieldNames, array_keys( $fields ) );
+	}
+
+	public function testGetFields_instanceOfSearchIndexField() {
+		$wikibaseFieldDefinitions = new WikibaseFieldDefinitions();
+
+		foreach ( $wikibaseFieldDefinitions->getFields() as $fieldName => $field ) {
+			$this->assertInstanceOf(
+				'Wikibase\Repo\Search\Elastic\Fields\SearchIndexField',
+				$field,
+				"$fieldName must be instance of SearchIndexField"
+			);
+		}
+	}
+
+}
