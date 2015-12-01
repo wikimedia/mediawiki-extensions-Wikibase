6cc6ad3c3ca8d14c040fd0bc7710a9f268c27727
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2015-12-01 11:21:17 +0100

    Add missing ClientEntitySerializerTest
    I'm also moving the class to a PSR4 compilant directory. Note that
    this does not really change anything because the namespace is untouched.
    
    Change-Id: Id18033846326b8d7638f690c48e2df8e87fdb399

---
+++ client/tests/phpunit/includes/Serializer/ClientEntitySerializerTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Wikibase\Client\Tests\Serializer;
+
+use PHPUnit_Framework_TestCase;
+use Wikibase\Client\Serializer\ClientEntitySerializer;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\DataModel\Snak\PropertyNoValueSnak;
+
+/**
+ * @covers Wikibase\Client\Serializer\ClientEntitySerializer
+ *
+ * @group Wikibase
+ * @group WikibaseClient
+ *
+ * @licence GNU GPL v2+
+ * @author Thiemo Mättig
+ */
+class ClientEntitySerializerTest extends PHPUnit_Framework_TestCase {
+
+	private function newInstance() {
+		$fallbackChain = $this->getMockBuilder( 'Wikibase\LanguageFallbackChain' )
+			->disableOriginalConstructor()
+			->getMock();
+		$fallbackChain->expects( $this->any() )
+			->method( 'extractPreferredValue' )
+			->will( $this->returnValue( array( 'source' => '<SOURCE>' ) ) );
+
+		$dataTypeLookup = $this->getMock(
+			'Wikibase\DataModel\Services\Lookup\PropertyDataTypeLookup'
+		);
+		$dataTypeLookup->expects( $this->any() )
+			->method( 'getDataTypeIdForProperty' )
+			->will( $this->returnValue( '<DATATYPE>' ) );
+
+		return new ClientEntitySerializer(
+			$dataTypeLookup,
+			array( 'en' ),
+			array( 'en' => $fallbackChain )
+		);
+	}
+
+	public function testSerialize() {
+		$item = new Item();
+		$item->setLabel( 'de', 'German' );
+		$item->setDescription( 'de', 'German' );
+		$item->setAliases( 'de', array( 'German' ) );
+		$item->setAliases( 'en', array( 'English' ) );
+		$item->getStatements()->addNewStatement( new PropertyNoValueSnak( 1 ) );
+
+		$instance = $this->newInstance();
+		$serialization = $instance->serialize( $item );
+
+		$expected = array(
+			'type' => 'item',
+			'labels' => array(
+				'en' => array( 'source-language' => '<SOURCE>' ),
+			),
+			'descriptions' => array(
+				'en' => array( 'source-language' => '<SOURCE>' ),
+			),
+			'aliases' => array(
+				'en' => array( array( 'language' => 'en', 'value' => 'English' ) ),
+			),
+			'claims' => array(
+				'P1' => array( array(
+					'mainsnak' => array(
+						'snaktype' => 'novalue',
+						'property' => 'P1',
+						'datatype' => '<DATATYPE>',
+					),
+					'type' => 'statement',
+					'rank' => 'normal'
+				) ),
+			),
+		);
+		$this->assertSame( $expected, $serialization );
+	}
+
+}
