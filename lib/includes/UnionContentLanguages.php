44add9c398101b3419a6a24ffc638d7bb81a7574
Adrian Heine <adrian.heine@wikimedia.de>
2015-12-07 14:38:46 +0100

    Allow special language codes in monolingual text values
    Names for these language codes are taken from CLDR, if present. The special
    languace codes are not yet supported in the language selector on monolingual
    text values.
    
    Bug: T72205
    Change-Id: I8f948f4888097d0fb6faaedbaa56fd27eed5b224

---
+++ lib/includes/UnionContentLanguages.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Wikibase\Lib;
+
+/**
+ * Provide languages supported as content languages based on two ContentLanguages
+ *
+ * @author Adrian Heine < adrian.heine@wikimedia.de >
+ */
+class UnionContentLanguages implements ContentLanguages {
+
+	/**
+	 * @var ContentLanguages $a
+	 */
+	private $a = null;
+
+	/**
+	 * @var ContentLanguages $b
+	 */
+	private $b = null;
+
+	/**
+	 * @var string[]|null Array of language codes
+	 */
+	private $languageCodes = null;
+
+	/**
+	 * @param ContentLanguages $a
+	 * @param ContentLanguages $b
+	 */
+	public function __construct( ContentLanguages $a, ContentLanguages $b ) {
+		$this->a = $a;
+		$this->b = $b;
+	}
+
+	/**
+	 * @return string[] Array of language codes supported as content language
+	 */
+	public function getLanguages() {
+		return $this->getLanguageCodes();
+	}
+
+	/**
+	 * @param string $languageCode
+	 *
+	 * @return bool
+	 */
+	public function hasLanguage( $languageCode ) {
+		return in_array( $languageCode, $this->getLanguageCodes() );
+	}
+
+	/**
+	 * @return string[] Array of language codes
+	 */
+	private function getLanguageCodes() {
+		if ( $this->languageCodes === null ) {
+			$this->languageCodes = $this->b->getLanguages() + $this->a->getLanguages();
+		}
+
+		return $this->languageCodes;
+	}
+
+}
