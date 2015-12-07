a9d3b4abc2329ee3b56969e2aa1d89a6cdb7f781
Adrian Heine <adrian.heine@wikimedia.de>
2015-12-07 14:38:46 +0100

    Allow special language codes in monolingual text values
    Names for these language codes are taken from CLDR, if present. The special
    languace codes are not yet supported in the language selector on monolingual
    text values.
    
    Bug: T72205
    Change-Id: I8f948f4888097d0fb6faaedbaa56fd27eed5b224

---
+++ lib/includes/StaticContentLanguages.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Wikibase\Lib;
+
+/**
+ * Provide languages supported as content languages based on a list
+ *
+ * @author Adrian Heine < adrian.heine@wikimedia.de >
+ */
+class StaticContentLanguages implements ContentLanguages {
+
+	/**
+	 * @var string[] Array of language codes
+	 */
+	private $languageCodes = null;
+
+	/**
+	 * @param string[] $languageCodes
+	 */
+	public function __construct( array $languageCodes ) {
+		$this->languageCodes = $languageCodes;
+	}
+
+	/**
+	 * @return string[] Array of language codes supported as content language
+	 */
+	public function getLanguages() {
+		return $this->languageCodes;
+	}
+
+	/**
+	 * @param string $languageCode
+	 *
+	 * @return bool
+	 */
+	public function hasLanguage( $languageCode ) {
+		return in_array( $languageCode, $this->languageCodes );
+	}
+
+}
