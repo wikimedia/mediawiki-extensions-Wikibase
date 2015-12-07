52763da5152215881bfc2e6af1e21231ae203f94
Adrian Heine <adrian.heine@wikimedia.de>
2015-12-07 14:38:46 +0100

    Allow special language codes in monolingual text values
    Names for these language codes are taken from CLDR, if present. The special
    languace codes are not yet supported in the language selector on monolingual
    text values.
    
    Bug: T72205
    Change-Id: I8f948f4888097d0fb6faaedbaa56fd27eed5b224

---
+++ lib/tests/phpunit/UnionContentLanguagesTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Wikibase\Lib\Test;
+
+use Wikibase\Lib\ContentLanguages;
+use Wikibase\Lib\UnionContentLanguages;
+
+/**
+ * @covers Wikibase\Lib\UnionContentLanguages
+ *
+ * @group WikibaseLib
+ * @group Wikibase
+ *
+ * @license GNU GPL v2+
+ * @author Adrian Heine < adrian.heine@wikimedia.de >
+ */
+class UnionContentLanguagesTest extends \MediaWikiTestCase {
+
+	/**
+	 * @dataProvider provideTestGetLanguages
+	 */
+	public function testGetLanguages( ContentLanguages $a, ContentLanguages $b, $expected ) {
+		$contentLanguages = new UnionContentLanguages( $a, $b );
+		$result = $contentLanguages->getLanguages();
+
+		$this->assertInternalType( 'array', $result );
+
+		$this->assertSame( $expected, $result );
+	}
+
+	public function provideTestGetLanguages() {
+		$empty = $this->getMockContentLanguages( array() );
+		$one = $this->getMockContentLanguages( array( 'one' ) );
+		$two = $this->getMockContentLanguages( array( 'one', 'two' ) );
+
+		return array(
+			array( $empty, $empty, array() ),
+			array( $empty, $one, array( 'one' ) ),
+			array( $one, $empty, array( 'one' ) ),
+			array( $one, $two, array( 'one', 'two' ) ),
+			array( $two, $one, array( 'one', 'two' ) ),
+		);
+	}
+
+	/**
+	 * @dataProvider provideTestHasLanguage
+	 */
+	public function testHasLanguage( ContentLanguages $a, ContentLanguages $b, $lang, $expected ) {
+		$contentLanguages = new UnionContentLanguages( $a, $b );
+		$result = $contentLanguages->hasLanguage( $lang );
+
+		$this->assertSame( $expected, $result );
+	}
+
+	public function provideTestHasLanguage() {
+		$empty = $this->getMockContentLanguages( array() );
+		$one = $this->getMockContentLanguages( array( 'one' ) );
+		$two = $this->getMockContentLanguages( array( 'one', 'two' ) );
+
+		return array(
+			array( $empty, $empty, 'one', false ),
+			array( $empty, $one, 'one', true ),
+			array( $empty, $one, 'two', false ),
+			array( $two, $one, 'one', true ),
+			array( $two, $one, 'two', true ),
+			array( $two, $one, 'three', false ),
+		);
+	}
+
+	private function getMockContentLanguages( $languages ) {
+		$contentLanguages = $this->getMock( 'Wikibase\Lib\ContentLanguages' );
+		$contentLanguages->expects( $this->any() )
+			->method( 'getLanguages' )
+			->will( $this->returnValue( $languages ) );
+		return $contentLanguages;
+	}
+
+}
