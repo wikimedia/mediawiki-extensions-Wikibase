8466fb518aba284b4eeeb2d7a1cfaac49bb674ed
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2015-12-09 10:31:40 +0100

    Cleaner autoload code in test bootstrap

--- lib/packages/wikibase/data-model-serialization/tests/bootstrap.php
+++ lib/packages/wikibase/data-model-serialization/tests/bootstrap.php
@@ -11,8 +11,11 @@ if ( !is_readable( __DIR__ . '/../vendor/autoload.php' ) ) {
 	die( 'You need to install this package with Composer before you can run the tests' );
 }
 
-$loader = require_once( __DIR__ . '/../vendor/autoload.php' );
-$loader->addClassMap( array(
+$classLoader = require __DIR__ . '/../vendor/autoload.php';
+
+$classLoader->addClassMap( array(
 	'Tests\Wikibase\DataModel\Serializers\SerializerBaseTest' => __DIR__ . '/unit/Serializers/SerializerBaseTest.php',
 	'Tests\Wikibase\DataModel\Deserializers\DeserializerBaseTest' => __DIR__ . '/unit/Deserializers/DeserializerBaseTest.php'
 ) );
+
+unset( $classLoader );
