b79b904f4c89be41ad0c5da4f06980e0edaa5d43
Marius Hoch <hoo@online.de>
2015-12-19 19:35:37 +0100

    Don't use htmlspecialchars to escape wikitext
    But rather use Message::plaintextParams which is
    supposed to do exactly this.
    
    Change-Id: I4044a35aa7e1393baddf2266ce988e8b9e42baf0

--- repo/includes/specials/SpecialSetLabelDescriptionAliases.php
+++ repo/includes/specials/SpecialSetLabelDescriptionAliases.php
@@ -256,9 +256,9 @@ class SpecialSetLabelDescriptionAliases extends SpecialModifyEntity {
 		if ( $this->languageCode === '' ) {
 			$this->languageCode = $this->getLanguage()->getCode();
 		} elseif ( !$this->isValidLanguageCode( $this->languageCode ) ) {
-			$msg = $this->msg( 'wikibase-wikibaserepopage-invalid-langcode',
-				htmlspecialchars( $this->languageCode )
-			);
+			$msg = $this->msg( 'wikibase-wikibaserepopage-invalid-langcode' )
+				->plaintextParams( $this->languageCode );
+
 			$this->showErrorHTML( $msg->parse() );
 			$this->languageCode = null;
 		}
