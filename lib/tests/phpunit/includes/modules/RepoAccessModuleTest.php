689e0fc888bb6c2619cc852fe3cb5c9050a237a5
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2015-12-21 14:26:55 +0100

    Add basic tests for all ResourceLoaderModule classes
    These integration tests do not do much and are supposed to be as
    trivial as possible. They help increasing code coverage.
    
    Change-Id: I4bb803bba2ad968ce03f2e4345a718e379dbab90

---
+++ lib/tests/phpunit/includes/modules/RepoAccessModuleTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Wikibase\Client\Tests\Modules;
+
+use PHPUnit_Framework_TestCase;
+use ResourceLoaderContext;
+use Wikibase\RepoAccessModule;
+
+/**
+ * @covers Wikibase\RepoAccessModule
+ *
+ * @licence GNU GPL v2+
+ * @author Thiemo Mättig
+ */
+class RepoAccessModuleTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * @return ResourceLoaderContext
+	 */
+	private function getContext() {
+		return $this->getMockBuilder( 'ResourceLoaderContext' )
+			->disableOriginalConstructor()
+			->getMock();
+	}
+
+	public function testGetScript() {
+		$module = new RepoAccessModule();
+		$script = $module->getScript( $this->getContext() );
+		$this->assertStringStartsWith( 'mediaWiki.config.set("wbRepo",', $script );
+		$this->assertStringEndsWith( ');', $script );
+	}
+
+}
