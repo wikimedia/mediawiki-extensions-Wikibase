2b41912a1f340a449abc3cfbc0a2e7a60c022205
Marius Hoch <hoo@online.de>
2015-12-23 21:23:46 +0100

    Delete at least one second worth of data in ChangePruner
    It's not too bad to go slightly over the batch size
    here as we're just going over it by one second worth
    of data (which is never going to be more than a thousand
    rows). A deletion of ~1500 rows (as the default batch
    size is 500) is not nice, but also not worrisome.
    
    Bug: T122336
    Change-Id: I37ced5f3fb3ed4a16632980b47f6f588c0839bef

--- repo/includes/ChangePruner.php
+++ repo/includes/ChangePruner.php
@@ -48,8 +48,8 @@ class ChangePruner {
 	 * @throws InvalidArgumentException
 	 */
 	public function __construct( $batchSize, $keepSeconds, $graceSeconds, $ignoreDispatch ) {
-		if ( !is_int( $batchSize ) || $batchSize < 0 ) {
-			throw new InvalidArgumentException( '$batchSize must be a non-negative integer' );
+		if ( !is_int( $batchSize ) || $batchSize <= 0 ) {
+			throw new InvalidArgumentException( '$batchSize must be a positive integer' );
 		}
 
 		if ( !is_int( $keepSeconds ) || $keepSeconds < 0 ) {
@@ -120,7 +120,12 @@ class ChangePruner {
 			}
 		}
 
-		return $this->limitCutoffTimestamp( wfTimestamp( TS_MW, $until ) );
+		$limitedTimestamp = $this->limitCutoffTimestamp( wfTimestamp( TS_MW, $until ) );
+
+		// Add one second just to make sure we delete at least one second worth of data
+		// as sometimes there are more edits in a single second than $this->batchSize
+		// (the peak on Wikidata is almost 550).
+		return wfTimestamp( TS_MW, wfTimestamp( TS_UNIX, $limitedTimestamp ) + 1 );
 	}
 
 	/**
@@ -139,7 +144,7 @@ class ChangePruner {
 			array( 'change_time < ' . $dbr->addQuotes( $until ) ),
 			__METHOD__,
 			array(
-				'OFFSET' => $this->batchSize,
+				'OFFSET' => $this->batchSize - 1,
 				'ORDER BY' => 'change_time ASC',
 			)
 		);
