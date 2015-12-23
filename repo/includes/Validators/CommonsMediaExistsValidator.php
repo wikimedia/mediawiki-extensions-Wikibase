c185e3b7b91dd90947884f2fb2336a20fa598e09
Marius Hoch <hoo@online.de>
2015-12-23 03:33:39 +0100

    Validate CommonsMedia file existence
    I've decided to implement a CachingCommonsMediaFileNameLookup
    in order to be able to use it for both validation and (later
    on) value parsing/ normalization without having to do more
    than one API query.
    
    Bug: T87263
    Depends-On: I008cadd29a2aa1f21098339b895c35a100959b04
    Change-Id: I65507d6ef974481c73d93353e4e9084ea77a6354

---
+++ repo/includes/Validators/CommonsMediaExistsValidator.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Wikibase\Repo\Validators;
+
+use DataValues\StringValue;
+use InvalidArgumentException;
+use ValueValidators\Error;
+use ValueValidators\Result;
+use ValueValidators\ValueValidator;
+use Wikibase\Repo\CachingCommonsMediaFileNameLookup;
+use Wikimedia\Assert\Assert;
+
+/**
+ * Validator for commons media values which checks whether the file in question
+ * exists. Doesn't check whether the name is normalized.
+ *
+ * @license GPL 2+
+ * @author Marius Hoch
+ */
+class CommonsMediaExistsValidator implements ValueValidator {
+
+	/**
+	 * @var CachingCommonsMediaFileNameLookup
+	 */
+	private $fileNameLookup;
+
+	/**
+	 * @param CachingCommonsMediaFileNameLookup $fileNameLookup
+	 */
+	public function __construct( CachingCommonsMediaFileNameLookup $fileNameLookup ) {
+		$this->fileNameLookup = $fileNameLookup;
+	}
+
+	/**
+	 * @see ValueValidator::validate()
+	 *
+	 * @param StringValue|string $value
+	 *
+	 * @return Result
+	 * @throws InvalidArgumentException
+	 */
+	public function validate( $value ) {
+		Assert::parameterType( 'string|DataValues\StringValue', $value, '$value' );
+
+		if ( $value instanceof StringValue ) {
+			$value = $value->getValue();
+		}
+
+		$actualName = $this->fileNameLookup->lookupFileName( $value );
+
+		$errors = array();
+
+		if ( $actualName === null ) {
+			$errors[] = Error::newError(
+				"File does not exist: " . $value,
+				null,
+				'no-such-media',
+				array( $value )
+			);
+		}
+
+		return empty( $errors ) ? Result::newSuccess() : Result::newError( $errors );
+	}
+
+	/**
+	 * @see ValueValidator::setOptions()
+	 *
+	 * @param array $options
+	 */
+	public function setOptions( array $options ) {
+		// Do nothing. This method shouldn't even be in the interface.
+	}
+
+}
