wikimedia/mediawiki-extensions-Wikibase

commit ea8c1a5634823fcc102feda973f5701744d1823a

Introduce $wgWBRepoDataTypes. Use it to define validators.
Bug: T104329
Bug: T110212
Change-Id: If7ac3e761bee81a61e6f8dfa4e84b966b66f1cb5

repo/WikibaseRepo.datatypes.php
@@ -1,0 +1,87 @@
+<?php
+/**
+ * Definition of data types for use with Wikibase.
+ * The array returned by the code below is supposed to be merged into $wgWBRepoDataTypes.
+ * It defines the formatters used by the repo to display data values of different types.
+ *
+ * @note: Keep in sync with lib/WikibaseLib.datatypes.php
+ *
+ * @note: This is bootstrap code, it is executed for EVERY request. Avoid instantiating
+ * objects or loading classes here!
+ *
+ * @note: 'validator-factory-callback' fields delegate to a global instance of
+ * ValidatorsBuilders.
+ *
+ * @see ValidatorsBuilders
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ */
+
+use Wikibase\Repo\WikibaseRepo;
+
+return call_user_func( function() {
+	// NOTE: 'validator-factory-callback' callbacks act as glue between the high level interface
+	// DataValueValidatorFactory and the low level factory for validators for well known data types,
+	// the ValidatorBuilders class.
+	// ValidatorBuilders should be used *only* here, program logic should use a
+	// DataValueValidatorFactory as returned by WikibaseRepo::getDataTypeValidatorFactory().
+
+	return array(
+		'commonsMedia' => array(
+			'validator-factory-callback' => function () {
+				$factory = WikibaseRepo::getDefaultValidatorBuilders();
+				return $factory->buildStringValidators();
+			},
+		),
+		'globe-coordinate' => array(
+			'validator-factory-callback' => function () {
+				$factory = WikibaseRepo::getDefaultValidatorBuilders();
+				return $factory->buildCoordinateValidators();
+			},
+		),
+		'monolingualtext' => array(
+			'validator-factory-callback' => function () {
+				$factory = WikibaseRepo::getDefaultValidatorBuilders();
+				return $factory->buildMonolingualTextValidators();
+			}
+		),
+		'quantity' => array(
+			'validator-factory-callback' => function () {
+				$factory = WikibaseRepo::getDefaultValidatorBuilders();
+				return $factory->buildQuantityValidators();
+			},
+		),
+		'string' => array(
+			'validator-factory-callback' => function () {
+				$factory = WikibaseRepo::getDefaultValidatorBuilders();
+				return $factory->buildStringValidators();
+			},
+		),
+		'time' => array(
+			'validator-factory-callback' => function () {
+				$factory = WikibaseRepo::getDefaultValidatorBuilders();
+				return $factory->buildTimeValidators();
+			},
+		),
+		'url' => array(
+			'validator-factory-callback' => function () {
+				$factory = WikibaseRepo::getDefaultValidatorBuilders();
+				return $factory->buildUrlValidators();
+			},
+		),
+		'wikibase-item' => array(
+			'validator-factory-callback' => function () {
+				$factory = WikibaseRepo::getDefaultValidatorBuilders();
+				return $factory->buildItemValidators();
+			},
+		),
+		'wikibase-property' => array(
+			'validator-factory-callback' => function () {
+				$factory = WikibaseRepo::getDefaultValidatorBuilders();
+				return $factory->buildPropertyValidators();
+			},
+		),
+	);
+
+});

client/WikibaseClient.php
@@ -67,7 +67,9 @@ call_user_func( function() {
 	global $wgExtensionCredits, $wgExtensionMessagesFiles, $wgHooks;
 	global $wgAPIMetaModules, $wgAPIPropModules, $wgSpecialPages, $wgResourceModules;
 	global $wgWBClientSettings, $wgRecentChangesFlags, $wgMessagesDirs;
-	global $wgJobClasses;
+	global $wgJobClasses, $wgWBClientDataTypes;
+
+	$wgWBClientDataTypes = require __DIR__ . '/../lib/WikibaseLib.datatypes.php';
 
 	$wgExtensionCredits['wikibase'][] = array(
 		'path' => __DIR__,

client/includes/WikibaseClient.php
@@ -6,6 +6,7 @@ use DataTypes\DataTypeFactory;
 use DataValues\Deserializers\DataValueDeserializer;
 use Deserializers\Deserializer;
 use Exception;
+use Hooks;
 use Language;
 use LogicException;
 use MediaWikiSite;
@@ -47,6 +48,7 @@ use Wikibase\InternalSerialization\DeserializerFactory as InternalDeserializerFa
 use Wikibase\LangLinkHandler;
 use Wikibase\LanguageFallbackChainFactory;
 use Wikibase\Lib\Changes\EntityChangeFactory;
+use Wikibase\Lib\DataTypeDefinitions;
 use Wikibase\Lib\FormatterLabelDescriptionLookupFactory;
 use Wikibase\Lib\LanguageNameLookup;
 use Wikibase\Lib\OutputFormatSnakFormatterFactory;
@@ -157,20 +159,26 @@ final class WikibaseClient {
 	private $restrictedEntityLookup = null;
 
 	/**
-	 * @since 0.4
-	 *
+	 * @var DataTypeDefinitions
+	 */
+	private $dataTypeDefinitions;
+
+	/**
 	 * @param SettingsArray $settings
 	 * @param Language $contentLanguage
+	 * @param DataTypeDefinitions $dataTypeDefinitions
 	 * @param SiteStore|null $siteStore
 	 */
 	public function __construct(
 		SettingsArray $settings,
 		Language $contentLanguage,
+		DataTypeDefinitions $dataTypeDefinitions,
 		SiteStore $siteStore = null
 	) {
 		$this->settings = $settings;
 		$this->contentLanguage = $contentLanguage;
 		$this->siteStore = $siteStore;
+		$this->dataTypeDefinitions = $dataTypeDefinitions;
 	}
 
 	/**
@@ -180,21 +188,7 @@ final class WikibaseClient {
 	 */
 	public function getDataTypeFactory() {
 		if ( $this->dataTypeFactory === null ) {
-			// Temporary hack, will be removed in a follow-up
-			$types = array(
-				'commonsMedia'      => 'string',
-				'globe-coordinate'  => 'globecoordinate',
-				'monolingualtext'   => 'monolingualtext',
-				'multilingualtext'  => 'multilingualtext',
-				'quantity'          => 'quantity',
-				'string'            => 'string',
-				'time'              => 'time',
-				'url'               => 'string',
-				'wikibase-item'     => 'wikibase-entityid',
-				'wikibase-property' => 'wikibase-entityid',
-			);
-
-			$this->dataTypeFactory = new DataTypeFactory( $types );
+			$this->dataTypeFactory = new DataTypeFactory( $this->dataTypeDefinitions->getValueTypes() );
 		}
 
 		return $this->dataTypeFactory;
@@ -352,9 +346,16 @@ final class WikibaseClient {
 	 * @return WikibaseClient
 	 */
 	private static function newInstance() {
-		global $wgContLang;
+		global $wgContLang, $wgWBClientSettings, $wgWBClientDataTypes;
 
-		return new self( new SettingsArray( $GLOBALS['wgWBClientSettings'] ), $wgContLang );
+		$dataTypeDefinitions = $wgWBClientDataTypes;
+		Hooks::run( 'WikibaseClientDataTypes', array( &$dataTypeDefinitions ) );
+
+		return new self(
+			new SettingsArray( $wgWBClientSettings ),
+			$wgContLang,
+			new DataTypeDefinitions( $dataTypeDefinitions )
+		);
 	}
 
 	/**

client/tests/phpunit/includes/WikibaseClientTest.php
@@ -7,6 +7,7 @@ use MediaWikiSite;
 use SiteStore;
 use Wikibase\Client\WikibaseClient;
 use Wikibase\DataModel\Entity\Item;
+use Wikibase\Lib\DataTypeDefinitions;
 use Wikibase\SettingsArray;
 use Wikibase\Test\MockSiteStore;
 
@@ -82,7 +83,7 @@ class WikibaseClientTest extends \PHPUnit_Framework_TestCase {
 		$settings->setSetting( 'siteGlobalID', 'enwiki' );
 		$settings->setSetting( 'languageLinkSiteGroup', 'wikipedia' );
 
-		$wikibaseClient = new WikibaseClient( $settings, Language::factory( 'en' ), $this->getSiteStore() );
+		$wikibaseClient = new WikibaseClient( $settings, Language::factory( 'en' ), new DataTypeDefinitions(), $this->getSiteStore() );
 
 		$returnValue = $wikibaseClient->getLangLinkHandler();
 		$this->assertInstanceOf( 'Wikibase\LangLinkHandler', $returnValue );
@@ -97,7 +98,7 @@ class WikibaseClientTest extends \PHPUnit_Framework_TestCase {
 	 * @dataProvider getLangLinkSiteGroupProvider
 	 */
 	public function testGetLangLinkSiteGroup( $expected, SettingsArray $settings, SiteStore $siteStore ) {
-		$client = new WikibaseClient( $settings, Language::factory( 'en' ), $siteStore );
+		$client = new WikibaseClient( $settings, Language::factory( 'en' ), new DataTypeDefinitions(), $siteStore );
 		$this->assertEquals( $expected, $client->getLangLinkSiteGroup() );
 	}
 
@@ -125,7 +126,7 @@ class WikibaseClientTest extends \PHPUnit_Framework_TestCase {
 	 * @dataProvider getSiteGroupProvider
 	 */
 	public function testGetSiteGroup( $expected, SettingsArray $settings, SiteStore $siteStore ) {
-		$client = new WikibaseClient( $settings, Language::factory( 'en' ), $siteStore );
+		$client = new WikibaseClient( $settings, Language::factory( 'en' ), new DataTypeDefinitions(), $siteStore );
 		$this->assertEquals( $expected, $client->getSiteGroup() );
 	}
 
@@ -251,7 +252,8 @@ class WikibaseClientTest extends \PHPUnit_Framework_TestCase {
 	private function getWikibaseClient() {
 		$settings = new SettingsArray( WikibaseClient::getDefaultInstance()->getSettings()->getArrayCopy() );
 		$sites = new MockSiteStore( array() );
-		return new WikibaseClient( $settings, Language::factory( 'en' ), $sites );
+		$dataTypeDefinitions = new DataTypeDefinitions();
+		return new WikibaseClient( $settings, Language::factory( 'en' ), $dataTypeDefinitions, $sites );
 	}
 
 }

docs/constraints.wiki
@@ -29,9 +29,9 @@ to be referencing an ''existing'' entity, but it is not a violation of
 data integrity if an entity contains a "broken" reference.
 
 Soft constraints are enforced by the respective ChangeOps. Snak
-validation is largely based on the validators provided by the DataType
-objects which are constructed in WikibaseDataTypeBuilders, while the
-validators for terms come fro the TermValidatorFactory.
+validation is based on the validators provided by a DataTypeValidatorFactory
+(see datatypes.wiki for details), while the validators for terms come fro the
+TermValidatorFactory.
 
 Note that some soft constraints only require local knowledge of the
 value in questions, while others are "global" in that they apply to

docs/datatypes.wiki
@@ -1,0 +1,52 @@
+This document describes the concept of data types as used by Wikibase.
+
+== Overview ==
+Data types in Wikibase are rather insubstantial: they are modelled by DataType objects,
+but such objects do not define any functionality of themselves. They merely act as a type safe
+ID for the data type.
+
+Data types are used to declare which kinds of values can be associated with a Property in a
+Snak. For each data type, the following things are defined:
+
+* the type of DataValue to use for values
+* a localized name and description of the data type
+* ValueValidators that impose constraints on the allowed values
+* A ValueParser for parsing user input
+* Formatters for rendering values of the given type to various target formats.
+* RDF mappings for representing values of the given type in RDF.
+
+
+== Data Type Definitions ==
+Data types are defined in the global $wgWBRepoDataTypes and $wgWBClientDataTypes arrays,
+respectively. These arrays are constructed at bootstrap time in Wikibase.php resp.
+WikibaseClient.php based on the information returned when including the files
+WikibaseLib.datatypes.php, Wikibase.datatypes.php, and WikibaseClient.datatypes.php,
+respectively.
+
+$wgWBRepoDataTypes and $wgWBClientDataTypes are associative arrays that map data type IDs to a
+data type definition record. Each such record has the following fields:
+* value-type (repo and client): the data value type ID identifying the low level value type to use for this data type. Logically, the value type defines the structure of the value, while the data type defines constraints on the value.
+* validator-factory-callback (repo only): a callable that acts as a factory for the list of validators that should be used to check any user supplied values of the given data type. The callable will be called without any arguments, and must return a list of ValueValidator objects.
+* parser-factory-callback (repo only): (PLANNED) a callable that acts as a factory for a ValueParser for this data type.
+* formatter-factory-callback (repo and client): (PLANNED) a callable that acts as a factory for ValueFormatters for use with this data type.
+* rdf-builder-factory-callback (repo only): (PLANNED) a callable that acts as a factory for DataValueRdfBuilder for use with this data type.
+
+Extensions that wish to register a data type should use the WikibaseRepoDataTypes
+resp. WikibaseClientDataTypes hooks to provide additional data type definitions.
+
+
+== Programmatic Access ==
+Information about data types can be accessed programmatically using the appropriate service objects.
+
+The data type definitions themselves are wrapped by a DataTypeDefinitions object; the DataType
+objects can be obtained from the DataTypeFactory service available via the getDataTypeFactory()
+method on WikibaseRepo and WikibaseClient.
+
+WikibaseRepo also defines the method getDataTypeValidatorFactory() which returns a
+DataTypeValidatorFactory for obtaining the validators for each data type.
+
+
+== Caveats ==
+* ValueParsers are still defined using the $wgValueParsers global.
+* the Methods getSnakFormatterFactory() does not yet use $wgWikibaseDataTypeDefinitions.
+* RDF mappings for the different data types are still hardcoded.

lib/WikibaseLib.datatypes.php
@@ -1,0 +1,27 @@
+<?php
+/**
+ * Definition of data types for use with Wikibase.
+ * The array returned by the code below is supposed to be merged into $wgWBRepoDataTypes
+ * resp. $wgWBClientDataTypes. The basic definition contains only the 'value-type' field.
+ *
+ * @note: When adding data types here, also add the corresponding information to
+ * repo/Wikibase.datatypes.php and client/WikibaseClient.datatypes.php
+ *
+ * @note: This is bootstrap code, it is executed for EVERY request. Avoid instantiating
+ * objects or loading classes here!
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ */
+
+return array(
+	'commonsMedia'      => array( 'value-type' => 'string' ),
+	'globe-coordinate'  => array( 'value-type' => 'globecoordinate' ),
+	'monolingualtext'   => array( 'value-type' => 'monolingualtext' ),
+	'quantity'          => array( 'value-type' => 'quantity' ),
+	'string'            => array( 'value-type' => 'string' ),
+	'time'              => array( 'value-type' => 'time' ),
+	'url'               => array( 'value-type' => 'string' ),
+	'wikibase-item'     => array( 'value-type' => 'wikibase-entityid' ),
+	'wikibase-property' => array( 'value-type' => 'wikibase-entityid' ),
+);

lib/includes/DataTypeDefinitions.php
@@ -1,0 +1,113 @@
+<?php
+
+namespace Wikibase\Lib;
+
+use Wikimedia\Assert\Assert;
+
+/**
+ * Service that manages data type definition. This is a registry that provides access to
+ * factory functions for various services associated with data types, such as validators,
+ * parsers, and formatters.
+ *
+ * DataTypeDefinitions provides a one-stop interface for defining data types. Each data type is defined
+ * using a "data type definition" array. A definition array has the following fields:
+ * - value-type: the value type used with the data type
+ * - validator-factory-callback: a callback for creating validators for the data type,
+ *   as used by BuilderBasedDataTypeValidatorFactory.
+ * @todo parser-factory-callback: a callback for instantiating a parser for the data type
+ * @todo formatter-factory-callback: a callback for instantiating a formatter for the data type
+ * @todo rdf-builder-factory-callback: a callback for instantiating a rdf mapping for the data type
+ *
+ * @see docs/datatypes.wiki
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ */
+class DataTypeDefinitions {
+
+	/**
+	 * @var array[]
+	 */
+	private $dataTypeDefinitions = array();
+
+	/**
+	 * @param array[] $dataTypeDefinitions An associative array mapping data type ids to data type
+	 * definitions. Data type definitions are associative arrays, refer to the class level
+	 * documentation.
+	 */
+	public function __construct( $dataTypeDefinitions = array() ) {
+		Assert::parameterElementType( 'array', $dataTypeDefinitions, '$dataTypeDefinitions' );
+
+		$this->registerDataTypes( $dataTypeDefinitions );
+	}
+
+	/**
+	 * Adds data type definitions. The new definitions are merged with the existing definitions.
+	 * If a data type in $dataTypeDefinitions was already defined, the old definition is not
+	 * replaced but the definitions are merged.
+	 *
+	 * @param array[] $dataTypeDefinitions An associative array mapping data type ids to data type
+	 * definitions. Data type definitions are associative arrays, refer to the class level
+	 * documentation.
+	 */
+	public function registerDataTypes( array $dataTypeDefinitions ) {
+		Assert::parameterElementType( 'array', $dataTypeDefinitions, '$dataTypeDefinitions' );
+
+		foreach ( $dataTypeDefinitions as $id => $def ) {
+			if ( isset( $this->dataTypeDefinitions[$id] ) ) {
+				$this->dataTypeDefinitions[$id] = array_merge(
+					$this->dataTypeDefinitions[$id],
+					$dataTypeDefinitions[$id]
+				);
+			} else {
+				$this->dataTypeDefinitions[$id] = $dataTypeDefinitions[$id];
+			}
+		}
+	}
+
+	/**
+	 * @return string[]
+	 */
+	public function getTypeIds() {
+		return array_keys( $this->dataTypeDefinitions );
+	}
+
+	/**
+	 * @param string $field
+	 *
+	 * @return array An associative array mapping data type IDs to the value of $field
+	 * given in the original data type definition provided to the constructor.
+	 */
+	private function getMapForDefinitionField( $field ) {
+		$fieldValues = array();
+
+		foreach ( $this->dataTypeDefinitions as $id => $def ) {
+			if ( isset( $def[$field] ) ) {
+				$fieldValues[$id] = $def[$field];
+			}
+		}
+
+		return $fieldValues;
+	}
+
+	/**
+	 * @return string[]
+	 */
+	public function getValueTypes() {
+		return $this->getMapForDefinitionField( 'value-type' );
+	}
+
+	/**
+	 * @see BuilderBasedDataTypeValidatorFactory
+	 *
+	 * @return callable[]
+	 */
+	public function getValidatorFactoryCallbacks() {
+		return $this->getMapForDefinitionField( 'validator-factory-callback' );
+	}
+
+	//TODO: getParserFactoryCallbacks()
+	//TODO: getFormatterFactoryCallbacks()
+	//TODO: getRdfBuilderFactoryCallbacks()
+
+}

commit 058a2ed6d9a08d9934e7d3c8d1bafeedc83bf9ef

Use Media validators for commons media
This apparently got broken by accident in
ea8c1a5634823fcc102feda973f5701744d1823a which changed
commons media to use the generic string validators.

This wont render items with (then) invalid statements
uneditable, only the invalid main Snaks and their
references and qualifiers wont be editable (they can
still be fixed or removed, but can't be altered without
also fixing the the value). I manually tested that.

Change-Id: I07fd4f72780c482e9f734c1bb507e09ba5293da0

repo/WikibaseRepo.datatypes.php
@@ -86,7 +86,7 @@ return call_user_func( function() {
 		'PT:commonsMedia' => array(
 			'validator-factory-callback' => function() {
 				$factory = WikibaseRepo::getDefaultValidatorBuilders();
-				return $factory->buildStringValidators();
+				return $factory->buildMediaValidators();
 			},
 			'parser-factory-callback' => $newStringParser,
 			'formatter-factory-callback' => function( $format, FormatterOptions $options ) {