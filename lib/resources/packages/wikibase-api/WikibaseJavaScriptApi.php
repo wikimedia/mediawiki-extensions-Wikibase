e19a4dc408b3fa378a04989f848b7355499a6430
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2015-12-28 17:23:14 +0100

    Avoid full qualified class names
    Plus a minor style fix. Both for consistency across the Wikibase code
    base.
    
    Change-Id: Ib69bbf098d4fca4d53cee075110690270616cbb1

--- lib/resources/packages/wikibase-api/WikibaseJavaScriptApi.php
+++ lib/resources/packages/wikibase-api/WikibaseJavaScriptApi.php
@@ -9,7 +9,7 @@ define( 'WIKIBASE_JAVASCRIPT_API_VERSION', '1.0.4' );
 
 // Include the composer autoloader if it is present.
 if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
-	include_once __DIR__ . '/vendor/autoload.php';
+	require_once __DIR__ . '/vendor/autoload.php';
 }
 
 if ( defined( 'MEDIAWIKI' ) ) {
