81263a619c9ef9af4ba9e241ec11580ea1598eff
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2016-01-07 11:56:25 +0100

    Do not fail hard on HistoryAction because of an undeserializable revision
    This fixes a very specific issue: When the newest revision of a page
    contains a JSON blob that can not be deserialized, the whole action fails
    hard with an exception. But why? There is only one minor detail where the
    deserialized entity (or redirect) is used: for the title on top of the
    history page. If this fails, we can simply fall back to the title of the
    wiki page.
    
    This came up while working on
    https://github.com/wmde/WikibaseDataModel/pull/593 (Do not allow
    self-redirects).
    
    This does *NOT* fix the issue that such a page with the newest revision
    being undeserializable can not be restored. This needs more investigation
    and a more complicated fix.
    
    Change-Id: Ie07d170053140daa8022431f451e0761c33f55af

---
+++ repo/tests/phpunit/includes/actions/HistoryEntityActionTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Wikibase\Test;
+
+use HashConfig;
+use MWContentSerializationException;
+use PHPUnit_Framework_MockObject_MockObject;
+use PHPUnit_Framework_TestCase;
+use Title;
+use User;
+use WebRequest;
+use Wikibase\HistoryEntityAction;
+
+/**
+ * @covers Wikibase\HistoryEntityAction
+ *
+ * @group Action
+ * @group Wikibase
+ * @group WikibaseAction
+ * @group WikibaseRepo
+ *
+ * @licence GNU GPL v2+
+ * @author Thiemo Mättig
+ */
+class HistoryEntityActionTest extends PHPUnit_Framework_TestCase {
+
+	private function getPage( $title ) {
+		$page = $this->getMockBuilder( 'Article' )
+			->disableOriginalConstructor()
+			->getMock();
+		$page->expects( $this->any() )
+			->method( 'getTitle' )
+			->will( $this->returnValue( Title::newFromText( $title ) ) );
+
+		return $page;
+	}
+
+	private function getOutput() {
+		$output = $this->getMockBuilder( 'OutputPage' )
+			->disableOriginalConstructor()
+			->getMock();
+
+		return $output;
+	}
+
+	private function getContext( PHPUnit_Framework_MockObject_MockObject $output ) {
+		$context = $this->getMock( 'IContextSource' );
+		$context->expects( $this->once() )
+			->method( 'getConfig' )
+			->will( $this->returnValue( new HashConfig( array(
+				'UseFileCache' => false,
+				'UseMediaWikiUIEverywhere' => false,
+			) ) ) );
+		$context->expects( $this->any() )
+			->method( 'getRequest' )
+			->will( $this->returnValue( new WebRequest() ) );
+		$context->expects( $this->any() )
+			->method( 'getUser' )
+			->will( $this->returnValue( new User() ) );
+		$context->expects( $this->any() )
+			->method( 'msg' )
+			->will( $this->returnCallback( function() {
+				return call_user_func_array( 'wfMessage', func_get_args() )->inLanguage( 'qqx' );
+			} ) );
+
+		$context->expects( $this->any() )
+			->method( 'getOutput' )
+			->will( $this->returnValue( $output ) );
+
+		$output->expects( $this->once() )
+			->method( 'getContext' )
+			->will( $this->returnValue( $context ) );
+
+		return $context;
+	}
+
+	public function testGivenUnDeserializableRevision_historyActionDoesNotFail() {
+		$page = $this->getPage( 'Page title' );
+		$page->expects( $this->once() )
+			->method( 'getPage' )
+			->will( $this->throwException( new MWContentSerializationException() ) );
+
+		$output = $this->getOutput();
+		$output->expects( $this->once() )
+			->method( 'setPageTitle' )
+			->with( '(history-title: Page title)' );
+
+		$action = new HistoryEntityAction( $page, $this->getContext( $output ) );
+		$action->show();
+	}
+
+}
