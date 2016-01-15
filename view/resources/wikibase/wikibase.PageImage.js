8aa4f46390150ae6388bf4343e03d2c0f6090c1b
jk <jonas.kress@wikimedia.de>
2016-01-15 16:49:20 +0100

    [WIP] Page image as image header
    Injects the cropped page image as image header via JS
    
    Bug: T119493
    Change-Id: Ic82f59713848902d63738546654a01c47d5ff695

---
+++ view/resources/wikibase/wikibase.PageImage.js
@@ -0,0 +1,144 @@
+var SmartCrop = SmartCrop || {};
+var mw = mw || {};
+
+/**
+ * @licence GNU GPL v2+
+ * @author Jonas Kress
+ */
+
+(function( wb, mw, $, SmartCrop ) {
+	'use strict';
+
+	/**
+	 * Offers access to the page image
+	 *
+	 * Uses pageimages and pageprops Api endpoints to retrieve the image URL
+	 *
+	 * @constructor
+	 *
+	 * @param {int} (optional) width
+	 * @param {int} (optional) height
+	 * @param {int} (optional) thumbnailSize
+	 * 		  Size of the thumbnail image which is loaded and cropped
+	 */
+	var SELF = wb.PageImage = function PageImage( width, height, thumbnailSize ) {
+
+		if ( width ) {
+			this._width = width;
+		}
+		if ( height ) {
+			this._height = height;
+		}
+
+		if( thumbnailSize ){
+			this._thumbnailSize = thumbnailSize;
+		}else{
+			this._thumbnailSize = 2 * Math.max( this._height, this._width );
+		}
+	};
+
+	SELF.prototype._width = 200;
+	SELF.prototype._height = 200;
+
+	SELF.prototype._thumbnailSize = 400;
+	SELF.prototype._minScale = 1;
+	SELF.prototype._crossOriginXmlUrl = 'https://upload.wikimedia.org/crossdomain.xml';
+	SELF.prototype._referenceUrl = 'https://commons.wikimedia.org/wiki/File:';
+
+	/**
+	 * Returns the page image as DOM element
+	 * @return {Object} jQuery.Promise Resolved after loading and cropping of image is done
+	 *         returning a DOM element.
+	 */
+	SELF.prototype.getPageImage = function() {
+		var deferred = $.Deferred(),
+			self = this;
+
+		self._getImageUrl().done( function( url, referenceUrl ){
+			self._loadImage( url ).done(
+				function( image ) {
+					self._getSmartCrop( image ).done(function( crop ) {
+						deferred.resolve(self._getMaskedImage( image, crop ), referenceUrl);
+					});
+				});
+		} );
+
+		return deferred.promise();
+	};
+
+	/**
+	 * @private
+	 **/
+	SELF.prototype._loadImage = function( url ) {
+		var deferred = $.Deferred();
+
+		var image = new Image();
+		image.onload = function() {
+			deferred.resolve( image );
+		};
+		image.crossOrigin = this._crossOriginXmlUrl;
+		image.src = url;
+
+		return deferred.promise();
+	};
+
+	/**
+	 * @private
+	 **/
+	SELF.prototype._getSmartCrop = function( image ) {
+		var deferred = $.Deferred();
+
+		SmartCrop.crop( image, {
+			width : this._width,
+			height : this._height,
+			minScale: this._minScale,
+		}, function( result ) {
+			deferred.resolve( result.topCrop );
+		});
+
+		return deferred.promise();
+	};
+
+	/**
+	 * @private
+	 **/
+	SELF.prototype._getMaskedImage = function( image, crop ) {
+
+		var canvas = $('<canvas/>')[0], ctx = canvas.getContext('2d');
+
+		canvas.width = this._width;
+		canvas.height = this._height;
+		ctx.drawImage( image, crop.x, crop.y, crop.width, crop.height, 0, 0,
+				canvas.width, canvas.height );
+
+		return canvas;
+	};
+
+	/**
+	 * @private
+	 **/
+	SELF.prototype._getImageUrl = function() {
+		var deferred = $.Deferred(),
+			self = this;
+
+		mw.loader.using( 'mediawiki.api', function() {
+			( new mw.Api()).get( {
+				action : 'query',
+				prop : 'pageimages|pageprops',
+				piprop: 'thumbnail',
+				pithumbsize: self._thumbnailSize,
+				titles: mw.config.get( 'wgPageName' )
+			} ).done( function( data ) {
+				var page = data.query.pages[Object.keys( data.query.pages )[0]];
+				if( page.thumbnail ){
+					deferred.resolve( page.thumbnail.source,
+							self._referenceUrl + page.pageprops.page_image );// jshint ignore:line
+				}
+
+			});
+		});
+
+		return deferred.promise();
+	};
+
+}(wikibase, mw, jQuery, SmartCrop));
