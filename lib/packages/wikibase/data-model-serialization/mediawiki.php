57a92293fc803354d23120bf938273a018b789a4
aude <aude.wiki@gmail.com>
2016-01-16 20:36:39 -0500

    Use extension registration for MediaWiki credits
    also fixes the broken license link on Special:Version

--- lib/packages/wikibase/data-model-serialization/mediawiki.php
+++ lib/packages/wikibase/data-model-serialization/mediawiki.php
@@ -1,16 +1,5 @@
 <?php
 
-if ( defined( 'MEDIAWIKI' ) ) {
-	$GLOBALS['wgExtensionCredits']['wikibase']['WikibaseDataModelSerialization'] = array(
-		'path' => __DIR__,
-		'name' => 'Wikibase DataModel Serialization',
-		'version' => '2.0.0',
-		'author' => array(
-			'[https://github.com/Tpt Thomas PT]',
-			'[https://www.mediawiki.org/wiki/User:Jeroen_De_Dauw Jeroen De Dauw]',
-		),
-		'url' => 'https://github.com/wmde/WikibaseDataModelSerialization',
-		'description' => 'Serializers and deserializers for the Wikibase DataModel',
-		'license-name' => 'GPL-2.0+'
-	);
+if ( defined( 'MEDIAWIKI' ) && function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'WikibaseDataModelSerialization', __DIR__ . '/mediawiki-extension.json' );
 }
