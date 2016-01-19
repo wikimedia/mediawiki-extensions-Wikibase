7e7cc5a51200930fddaa4901eb23f3c65ab34551
aude <aude.wiki@gmail.com>
2016-01-18 22:48:34 -0500

    Fix typos in docs/constraints.wiki and EditEntityTest
    Change-Id: I2bca46d703ed2458d4347598ebf0579c675428a2

--- repo/tests/phpunit/includes/api/EditEntityTest.php
+++ repo/tests/phpunit/includes/api/EditEntityTest.php
@@ -499,7 +499,7 @@ class EditEntityTest extends WikibaseApiTestCase {
 				'p' => array(
 					'site' => 'enwiki',
 					'title' => 'Berlin',
-					'data' => '{"lables":{"BLUB":{"language":"BLUB","value":"ImaLabel"}}}'
+					'data' => '{"labels":{"BLUB":{"language":"BLUB","value":"ImaLabel"}}}'
 				),
 				'e' => array( 'exception' => array( 'type' => 'UsageException', 'code' => 'not-recognized' ) ) ),
 			//@todo the error codes in the overly long string tests make no sense and should be corrected...
@@ -507,7 +507,7 @@ class EditEntityTest extends WikibaseApiTestCase {
 				'p' => array(
 					'site' => 'enwiki',
 					'title' => 'Berlin',
-					'data' => '{"lables":{"en":{"language":"en","value":"'
+					'data' => '{"labels":{"en":{"language":"en","value":"'
 						. TermTestHelper::makeOverlyLongString() . '"}}}'
 				),
 				'e' => array( 'exception' => array( 'type' => 'UsageException' ) ) ),
