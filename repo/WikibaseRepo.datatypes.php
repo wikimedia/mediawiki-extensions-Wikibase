4eb66b8edd177fd932a6e2f749b1f0a0bbc31f18
Bene <benestar.wikimedia@gmail.com>
2016-01-27 16:04:45 +0100

    Fix array keys for external identifier datatype
    The definition was wrong in WikibaseRepo where it was just PT:id instead
    of PT:external-id. Furthermore, in the SnakFormatter dispatching we need
    to add the PT prefix in order to get the correct formatter.
    
    Bug: T124902
    Change-Id: I1101538b245b7e9d79f77100abf73115cbdf81d9

--- repo/WikibaseRepo.datatypes.php
+++ repo/WikibaseRepo.datatypes.php
@@ -242,7 +242,7 @@ return call_user_func( function() {
 				return new ObjectUriRdfBuilder();
 			},
 		),
-		'PT:id' => array(
+		'PT:external-id' => array(
 			'validator-factory-callback' => function() {
 				$factory = WikibaseRepo::getDefaultValidatorBuilders();
 				return $factory->buildStringValidators();
