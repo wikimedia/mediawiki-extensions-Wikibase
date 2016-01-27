43db3b1c69f433f5ca995580552b8f219e681ae8
Bene <benestar.wikimedia@gmail.com>
2016-01-27 17:16:06 +0100

    Add external identifier support in ui
    This patch registers the new external identifier datatype in all necessary
    places. Furthermore, it enables rendering of it by passing the property id
    to the formatter.
    
    Bug: T124918
    Change-Id: Iab6305f9ba24dbfea1aa62e2a1c30c55a4613145

--- view/resources/wikibase/wikibase.ValueFormatterFactory.js
+++ view/resources/wikibase/wikibase.ValueFormatterFactory.js
@@ -6,12 +6,14 @@ var SELF = wb.ValueFormatterFactory = function() {
 };
 
 /**
- * Returns a ValueFormatter instance for the given DataType id and output type
+ * Returns a ValueFormatter instance for the given DataType ID or Property ID and output type.
  *
  * @param {string|null} dataTypeId
+ * @param {string|null} propertyId
  * @param {string} outputType
  * @return {valueFormatters.ValueFormatter}
  */
 SELF.prototype.getFormatter = util.abstractMember;
 
 }( util, wikibase ) );
+
