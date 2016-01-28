569467b6a9e50a5b6b0107ef320fd12efc37bbff
Adrian Heine <adrian.heine@wikimedia.de>
2016-01-28 16:03:22 +0100

    Remove bad language codes from mono languages
    In JS, we use a list of 391 language code (`mwfile`). In PHP, we have 416
    language codes (`mw`). That's why we have to remove more language codes from
    the PHP instance.
    
    Also, since `wikibase.WikibaseContentLanguages` is not yet used for terms, I
    hard-code the removal in that class instead of creating a JS implementation of
    `DifferenceContentLanguages`. I would like to defer that since we probably want
    to split out `LanguageNameLookup` beforehands as we did in PHP.
    
    Bug: T125063
    Change-Id: I8b203aae1c45efceb4459b62b166bab3c191afcd

--- repo/resources/wikibase.WikibaseContentLanguages.js
+++ repo/resources/wikibase.WikibaseContentLanguages.js
@@ -17,6 +17,17 @@ var SELF = MODULE.WikibaseContentLanguages = util.inherit(
 	PARENT,
 	function() {
 		this._languageMap = mw.config.get( 'wgULSLanguages' );
+		this._languageCodes = $.map( this._languageMap, function( val, key ) {
+			return key;
+		} );
+		this._languageCodes = $.grep( this._languageCodes, function( code ) {
+			// Make sure this is a subset of the language codes returned by
+			// WikibaseRepo::getMonolingualTextLanguages
+			// We don't want to have language codes in the suggester that are not
+			// supported by the backend. The other way round is currently acceptable,
+			// but will be fixed in T124758.
+			return [ 'de-formal', 'nl-informal', 'tokipona' ].indexOf( code ) === -1;
+		} );
 	}
 );
 
@@ -37,12 +48,6 @@ $.extend( SELF.prototype, {
 	 * @inheritdoc
 	 */
 	getAll: function() {
-		// Cache language codes
-		if ( !this._languageCodes && this._languageMap ) {
-			this._languageCodes = $.map( this._languageMap, function( val, key ) {
-				return key;
-			} );
-		}
 		return this._languageCodes;
 	},
 
