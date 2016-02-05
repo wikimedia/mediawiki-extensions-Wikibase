7184c1f0728832341d0d4784bc31cd3f4a56cdf0
jk <jonas.kress@wikimedia.de>
2016-02-05 16:23:12 +0100

    Introduces Wikibase performance mark and statistic
    Introduces a wrapper for window.performance, a performance statistic and
    a notification that shows the statistic.
    
    Bug: T126609
    Change-Id: Ib8ca57e6df6210d31fa51939eea2c8ce8febec03

---
+++ repo/resources/performance/Mark.js
@@ -0,0 +1,80 @@
+( function( wb, performance, $ ) {
+	'use strict';
+
+	var MODULE = wb.performance;
+
+	var MARK_START = '::START';
+	var MARK_END = '::END';
+
+	/**
+	 * Records a performance mark, which is a time value and a name
+	 *
+	 * @class wikibase.performance.Mark
+	 * @licence GNU GPL v2+
+	 *
+	 * @author Jonas Kress
+	 */
+	var SELF = MODULE.Mark = function() {
+	};
+
+	/**
+	 * @private
+	 **/
+	SELF.prototype._mark = function( name ) {
+		if ( !performance ) {
+			return;
+		}
+
+		performance.mark( name );
+	};
+
+	/**
+	 * Sets a start mark
+	 *
+	 * @param {string} name
+	 **/
+	SELF.prototype.addStart = function( name ) {
+		this._mark( name + MARK_START );
+	};
+
+	/**
+	 * Sets an end mark
+	 *
+	 * @param {string} name
+	 **/
+	SELF.prototype.addEnd = function( name ) {
+		this._mark( name + MARK_END );
+	};
+
+	/**
+	 * Get entries
+	 *
+	 * @return {object} List of performance marks with mark name as key
+	 **/
+	SELF.prototype.getAllMarks = function() {
+		var marks = {},
+			totals = {};
+
+		$.each( performance.getEntriesByType( 'mark' ), function() {
+			var markName = this.name.replace( MARK_START, '' ).replace( MARK_END, '' );
+
+			if ( !marks[markName] ) {
+				marks[markName] = this;
+
+			} else {
+				if ( !totals[ markName ] ) {
+					totals[ markName ] = { name: markName, durationTotal: 0, durations: [] };
+					totals[ markName ].name = markName;
+				}
+				var duration = this.startTime - marks[markName].startTime;
+				totals[ markName ].durationTotal += duration;
+				totals[ markName ].durations.push( duration );
+
+				delete marks[markName];
+			}
+		} );
+
+		return totals;
+	};
+
+}( wikibase, window.performance, jQuery ) );
