eaedc03cbb404abd90a23d5ecc8fd891e417d9db
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2016-02-11 14:33:56 +0100

    Add missing EntityRevisionTest
    Sure, this is super trivial. But believe it or not, PHPUnit's code
    coverage now reports this class as the top risk in the lib directory.
    
    Change-Id: I08a57ec5913e2542fb089ba2e29b1783e4893d37

---
+++ lib/tests/phpunit/store/EntityRevisionTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Wikibase\Lib\Tests\Store;
+
+use PHPUnit_Framework_TestCase;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\EntityRevision;
+
+/**
+ * @covers Wikibase\EntityRevision
+ *
+ * @group Wikibase
+ * @group WikibaseLib
+ * @group WikibaseStore
+ *
+ * @licence GNU GPL v2+
+ * @author Thiemo Mättig
+ */
+class EntityRevisionTest extends PHPUnit_Framework_TestCase {
+
+	public function testMinimalConstructorArguments() {
+		$entity = new Item();
+		$revision = new EntityRevision( $entity );
+
+		$this->assertSame( $entity, $revision->getEntity() );
+		$this->assertSame( 0, $revision->getRevisionId() );
+		$this->assertSame( '', $revision->getTimestamp() );
+	}
+
+	public function testAllConstructorArguments() {
+		$entity = new Item();
+		$revision = new EntityRevision( $entity, 42, '20150211000000' );
+
+		$this->assertSame( $entity, $revision->getEntity() );
+		$this->assertSame( 42, $revision->getRevisionId() );
+		$this->assertSame( '20150211000000', $revision->getTimestamp() );
+	}
+
+	/**
+	 * @dataProvider invalidConstructorArgumentsProvider
+	 */
+	public function testInvalidConstructorArguments( $revisionId, $mwTimestamp ) {
+		$this->setExpectedException( 'InvalidArgumentException' );
+		new EntityRevision( new Item(), $revisionId, $mwTimestamp );
+	}
+
+	public function invalidConstructorArgumentsProvider() {
+		return array(
+			array( null, '20150211000000' ),
+			array( '1', '20150211000000' ),
+			array( -1, '20150211000000' ),
+			array( 1, null ),
+			array( 1, 1423612800 ),
+			array( 1, '20150211' ),
+			array( 1, '2015-02-110000' ),
+		);
+	}
+
+}
