bb0737fa90f6f50160ef740978ebd9b148467816
Bene <benestar.wikimedia@gmail.com>
2016-02-16 23:54:27 +0100

    Create entity type registry
    This patch creates a registry for entity types like the one we already
    have for property data types. It provides an extension point for other
    pieces of software to hook into Wikibase and register their entity types.
    
    TODO: Documentation, Tests
    
    Bug: T76021
    Change-Id: Ib8ab73a25a05db1a5262ba7a29737f5f004791fc

---
+++ lib/WikibaseLib.entitytypes.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Definition of data types for use with Wikibase.
+ * The array returned by the code below is supposed to be merged into $wgWBRepoEntityTypes
+ * resp. $wgWBClientEntityTypes.
+ *
+ * @note: When adding entity types here, also add the corresponding information to
+ * repo/Wikibase.entitytypes.php and client/WikibaseClient.entitytypes.php
+ *
+ * @note: This is bootstrap code, it is executed for EVERY request. Avoid instantiating
+ * objects or loading classes here!
+ *
+ * @licence GNU GPL v2+
+ * @author Bene* < benestar.wikimedia@gmail.com >
+ */
+
+use Wikibase\DataModel\DeserializerFactory;
+use Wikibase\DataModel\SerializerFactory;
+use Wikibase\EntityChange;
+use Wikibase\ItemChange;
+
+return array(
+	'item' => array(
+		'serializer-factory-callback' => function( SerializerFactory $serializerFactory ) {
+			return $serializerFactory->newItemSerializer();
+		},
+		'deserializer-factory-callback' => function( DeserializerFactory $deserializerFactory ) {
+			return $deserializerFactory->newItemDeserializer();
+		},
+		'change-factory-callback' => function( array $fields ) {
+			return new ItemChange( $fields );
+		}
+	),
+	'property' => array(
+		'serializer-factory-callback' => function( SerializerFactory $serializerFactory ) {
+			return $serializerFactory->newPropertySerializer();
+		},
+		'deserializer-factory-callback' => function( DeserializerFactory $deserializerFactory ) {
+			return $deserializerFactory->newPropertyDeserializer();
+		},
+		'change-factory-callback' => function( array $fields ) {
+			return new EntityChange( $fields );
+		}
+	)
+);
