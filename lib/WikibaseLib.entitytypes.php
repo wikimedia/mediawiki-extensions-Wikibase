f85bdc1ccb3a6606e6b6d4a40472ec6df7adba93
Bene <benestar.wikimedia@gmail.com>
2016-02-16 23:54:27 +0100

    Create entity type registry
    This patch creates a registry for entity types like the one we already
    have for property data types. It provides an extension point for other
    pieces of software to hook into Wikibase and register their entity types.
    
    Bug: T76021
    Change-Id: Ib8ab73a25a05db1a5262ba7a29737f5f004791fc

---
+++ lib/WikibaseLib.entitytypes.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Definition of entity types for use with Wikibase.
+ * The array returned by the code below is supposed to be merged into $wgWBRepoEntityTypes
+ * resp. $wgWBClientEntityTypes.
+ *
+ * @note: This is bootstrap code, it is executed for EVERY request. Avoid instantiating
+ * objects or loading classes here!
+ *
+ * @licence GNU GPL v2+
+ * @author Bene* < benestar.wikimedia@gmail.com >
+ */
+
+use Wikibase\DataModel\DeserializerFactory;
+use Wikibase\DataModel\SerializerFactory;
+use Wikibase\EntityChange;
+use Wikibase\ItemChange;
+
+return array(
+	'item' => array(
+		'serializer-factory-callback' => function( SerializerFactory $serializerFactory ) {
+			return $serializerFactory->newItemSerializer();
+		},
+		'deserializer-factory-callback' => function( DeserializerFactory $deserializerFactory ) {
+			return $deserializerFactory->newItemDeserializer();
+		},
+		'change-factory-callback' => function( array $fields ) {
+			return new ItemChange( $fields );
+		}
+	),
+	'property' => array(
+		'serializer-factory-callback' => function( SerializerFactory $serializerFactory ) {
+			return $serializerFactory->newPropertySerializer();
+		},
+		'deserializer-factory-callback' => function( DeserializerFactory $deserializerFactory ) {
+			return $deserializerFactory->newPropertyDeserializer();
+		},
+		'change-factory-callback' => function( array $fields ) {
+			return new EntityChange( $fields );
+		}
+	)
+);
---
+++ lib/tests/phpunit/EntityTypesTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Wikibase\Lib\Tests;
+
+use PHPUnit_Framework_TestCase;
+
+/**
+ * @covers WikibaseLib.entitytypes.php
+ *
+ * @licence GNU GPL v2+
+ * @author Bene* < benestar.wikimedia@gmail.com >
+ */
+class EntityTypesTest extends PHPUnit_Framework_TestCase {
+
+	private function getRegistry() {
+		return require __DIR__  . '/../../WikibaseLib.entitytypes.php';
+	}
+
+	private function getSerializerFactroy( $entityType ) {
+		$serializerFactory = $this->getMockBuilder( 'Wikibase\DataModel\SerializerFactory' )
+			->disableOriginalConstructor()
+			->getMock();
+
+		$serializerFactory->expects( $this->once() )
+			->method( 'new' . $entityType . 'Serializer' )
+			->will( $this->returnValue( $this->getMock( 'Serializers\Serializer' ) ) );
+
+		return $serializerFactory;
+	}
+
+	private function getDeserializerFactroy( $entityType ) {
+		$deserializerFactory = $this->getMockBuilder( 'Wikibase\DataModel\DeserializerFactory' )
+			->disableOriginalConstructor()
+			->getMock();
+
+		$deserializerFactory->expects( $this->once() )
+			->method( 'new' . $entityType . 'Deserializer' )
+			->will( $this->returnValue( $this->getMock( 'Deserializers\Deserializer' ) ) );
+
+		return $deserializerFactory;
+	}
+
+	public function provideEntityTypes() {
+		return array(
+			array( 'item' ),
+			array( 'property' )
+		);
+	}
+
+	/**
+	 * @dataProvider provideEntityTypes
+	 */
+	public function testSerializerFactory( $entityType ) {
+		$registry = $this->getRegistry()[$entityType];
+		$serializerFactory = $this->getSerializerFactroy( $entityType );
+
+		$this->assertInstanceOf(
+			'Serializers\Serializer',
+			call_user_func( $registry['serializer-factory-callback'], $serializerFactory )
+		);
+	}
+
+	/**
+	 * @dataProvider provideEntityTypes
+	 */
+	public function testDeserializerFactory( $entityType ) {
+		$registry = $this->getRegistry()[$entityType];
+		$deserializerFactroy = $this->getDeserializerFactroy( $entityType );
+
+		$this->assertInstanceOf(
+			'Deserializers\Deserializer',
+			call_user_func( $registry['deserializer-factory-callback'], $deserializerFactroy )
+		);
+	}
+
+	/**
+	 * @dataProvider provideEntityTypes
+	 */
+	public function testChangeSerializer( $entityType ) {
+		$registry = $this->getRegistry()[$entityType];
+
+		$this->assertInstanceOf(
+			'Wikibase\EntityChange',
+			call_user_func( $registry['change-factory-callback'], array() )
+		);
+	}
+
+}
