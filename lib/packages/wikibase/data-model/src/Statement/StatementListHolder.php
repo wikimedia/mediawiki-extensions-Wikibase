57f1492c9cdbfa48cef72a55911739031d3bb80c
Bene <benestar.wikimedia@gmail.com>
2016-02-29 15:59:01 +0100

    Deprecate Holder interfaces
    As decided in T126152
    
    Bug: T128362

--- lib/packages/wikibase/data-model/src/Statement/StatementListHolder.php
+++ lib/packages/wikibase/data-model/src/Statement/StatementListHolder.php
@@ -6,6 +6,7 @@ namespace Wikibase\DataModel\Statement;
  * Interface for classes that contain a StatementList.
  *
  * @since 3.0
+ * @deprecated since 5.1
  *
  * @license GNU GPL v2+
  * @author Thiemo Mättig
