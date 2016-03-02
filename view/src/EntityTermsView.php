610b50ff871cc24405f917c3ea10456282489958
Adrian Heine <adrian.heine@wikimedia.de>
2016-03-02 15:39:53 +0100

    Re-use entitytermsview from PHP in JS
    Bug: T114877
    Change-Id: Ib5e9570a1cce9220f145e74e242f481f61cfb1ce

--- view/src/EntityTermsView.php
+++ view/src/EntityTermsView.php
@@ -227,8 +227,6 @@ class EntityTermsView {
 		$languageName = $this->languageNameLookup->getName( $languageCode );
 		$labels = $labelsProvider->getLabels();
 		$descriptions = $descriptionsProvider->getDescriptions();
-		$hasLabel = $labels->hasTermForLanguage( $languageCode );
-		$hasDescription = $descriptions->hasTermForLanguage( $languageCode );
 
 		return $this->templateFactory->render( 'wikibase-entitytermsforlanguageview',
 			'tr',
@@ -241,25 +239,34 @@ class EntityTermsView {
 				),
 				htmlspecialchars( $languageName )
 			),
-			$this->templateFactory->render( 'wikibase-labelview',
-				$hasLabel ? '' : 'wb-empty',
-				htmlspecialchars( $hasLabel
-					? $labels->getByLanguage( $languageCode )->getText()
-					: $this->textProvider->get( 'wikibase-label-empty' )
-				),
-				''
+			$this->getTermView(
+				$labels,
+				'wikibase-labelview', // Template
+				'wikibase-label-empty', // Text key
+				$languageCode
 			),
-			$this->templateFactory->render( 'wikibase-descriptionview',
-				$hasDescription ? '' : 'wb-empty',
-				htmlspecialchars( $hasDescription
-					? $descriptions->getByLanguage( $languageCode )->getText()
-					: $this->textProvider->get( 'wikibase-description-empty' )
-				),
-				'',
-				''
+			$this->getTermView(
+				$descriptions,
+				'wikibase-descriptionview', // Template
+				'wikibase-description-empty', // Text key
+				$languageCode
 			),
 			$aliasesProvider ? $this->getAliasesView( $aliasesProvider->getAliasGroups(), $languageCode ) : '',
 			''
+		);
+	}
+
+	private function getTermView( TermList $termList, $templateName, $emptyTextKey, $languageCode ) {
+		$hasTerm = $termList->hasTermForLanguage( $languageCode );
+		return $this->templateFactory->render( $templateName,
+			$hasTerm ? '' : 'wb-empty',
+			htmlspecialchars( $hasTerm
+				? $termList->getByLanguage( $languageCode )->getText()
+				: $this->textProvider->get( $emptyTextKey )
+			),
+			'',
+			'auto', // FIXME DirLookup
+			$hasTerm ? $languageCode : $this->textProvider->getLanguageOf( $emptyTextKey )
 		);
 	}
 
@@ -274,6 +281,8 @@ class EntityTermsView {
 			return $this->templateFactory->render( 'wikibase-aliasesview',
 				'wb-empty',
 				'',
+				'',
+				'auto', // FIXME DirLookup
 				''
 			);
 		} else {
@@ -289,7 +298,9 @@ class EntityTermsView {
 			return $this->templateFactory->render( 'wikibase-aliasesview',
 				'',
 				$aliasesHtml,
-				''
+				'',
+				'auto', // FIXME DirLookup
+				$languageCode
 			);
 		}
 	}
