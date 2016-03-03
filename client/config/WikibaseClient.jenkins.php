c1635191002ebf2e544ad73e09ddc2e87b6af206
daniel <daniel.kinzler@wikimedia.de>
2016-03-03 20:55:23 +0100

    Add a config override for siteGroup
    This necessarly deviates the config for CI from the the examples included for
    3rd party installs.
    
    Bug: T128779
    Change-Id: Ie4b2d8c6fa7d07f2180a76306959f89fe4adda87

---
+++ client/config/WikibaseClient.jenkins.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Configuration overrides for use in a Jenkins CI based testing environment.
+ *
+ * NOTE: Ideally, no such overrides should be needed, as tests should be self-contained.
+ *
+ * @since 0.5
+ *
+ * @license GPL-2.0+
+ * @author Daniel Kinzler
+ */
+
+// Force the siteGroup name, so we do not rely on looking up the
+// group in the sites table during testing.
+// NOTE: This can be removed once T126596 is implemented.
+$wgWBClientSettings['siteGroup'] = "mywikigroup";
