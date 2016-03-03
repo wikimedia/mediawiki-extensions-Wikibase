6108f28077286b578de152b26d9f1fc65f5fd5e6
daniel <daniel.kinzler@wikimedia.de>
2016-03-03 20:55:23 +0100

    Allow config overrides for Jenkins
    This change introduces a mechanism for specifying configuration
    specific to Jenkins runs on Wikimedia CI. This allows the config
    used for CI to differ from the examples included for 3rd party
    installs.
    
    Bug: T128779
    Change-Id: Ie4b2d8c6fa7d07f2180a76306959f89fe4adda87

---
+++ repo/config/Wikibase.jenkins.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Configuration for the Wikibase Repo extension for use with Jenkins CI.
+ *
+ * This file is NOT an entry point the Wikibase Repo extension. Use Wikibase.php.
+ * It should furthermore not be included from outside the extension.
+ *
+ * @since 0.5
+ *
+ * @license GPL-2.0+
+ * @author Daniel Kinzler
+ */
+
+if ( !defined( 'WB_VERSION' ) ) {
+	die( 'Not an entry point. Load Wikibase.php first.' );
+}
+
+// Load example settings:
+include __DIR__ . '/Wikibase.example.php';
+
+// Apply additional settings for Jenkins CI:
+
+// ...
