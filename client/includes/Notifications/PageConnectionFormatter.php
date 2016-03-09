7264b4d43be610847a6903395e65f37c2d13aa1d
matejsuchanek <matejsuchanek97@gmail.com>
2016-03-09 14:17:06 +0100

    [WIP] Send a notification when a page is connected to a repo item
    When a page is connected to the associated repository by a user different
    from the one who created the page, an Echo notification is sent to the user
    on the client wiki. One of the secondary links is customisable similarly to
    how welcome notifications are. The web notification is bundled.
    
    This is configurable per client and requires Echo extension enabled.
    
    The remaining point to be discussed is what icon should be associated with
    the notifications.
    
    Bug: T110604
    Change-Id: I2a3babd7f5bcc0cbf923d94b1ea3fe768457a95d

---
+++ client/includes/Notifications/PageConnectionFormatter.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Wikibase\Client\Notifications;
+
+class PageConnectionFormatter extends \EchoEditFormatter {
+
+	protected function processParam( $event, $param, $message, $user ) {
+		switch ( $param ) {
+			case 'item':
+				$extra = $event->getExtra();
+				$message->params( $extra['entityId']->getSerialization() );
+				break;
+			case 'page-count':
+				// for email subject
+				$count = \EchoNotificationController::getCappedNotificationCount(
+					$this->bundleData['other-page-count']
+				);
+				$message->numParams( $count );
+				break;
+			default:
+				parent::processParam( $event, $param, $message, $user );
+				break;
+		}
+	}
+
+	protected function generateBundleData( $event, $user, $type ) {
+		$data = $this->getRawBundleData( $event, $user, $type );
+		if ( !$data ) {
+			return;
+		}
+
+		$title = $event->getTitle();
+		if ( !$title || !$title->exists() ) {
+			return;
+		}
+
+		$otherTitles = [];
+		foreach ( $data as $bundledEvent ) {
+			$title = $bundledEvent->getTitle();
+			if ( $title->exists() ) {
+				$otherTitles[] = $title->getPrefixedText();
+			}
+		}
+
+		$count = count( array_unique( $otherTitles ) );
+		if ( $count > 0 ) {
+			$this->bundleData['use-bundle'] = true;
+			$this->bundleData['other-page-count'] = $count;
+		}
+	}
+
+}
