7399619148bb70afadb32242fc802c4230d11408
matejsuchanek <matejsuchanek97@gmail.com>
2016-03-09 14:17:06 +0100

    Send a notification when a page is connected to a repo item
    When a page is connected to the associated repository by a user different
    from the one who created the page, an Echo notification is sent to the user
    on the client wiki. One of the secondary links is customisable similarly to
    how welcome notifications are. The web notification is bundled.
    
    This is configurable per client and requires the Echo extension enabled.
    
    Bug: T110604
    Depends-On: I98dd21ffa
    Change-Id: I2a3babd7f5bcc0cbf923d94b1ea3fe768457a95d

---
+++ client/includes/Hooks/EchoNotificationsHandlers.php
@@ -0,0 +1,257 @@
+<?php
+
+namespace Wikibase\Client\Hooks;
+
+use Diff\DiffOp\DiffOp;
+use Diff\DiffOp\DiffOpAdd;
+use Diff\DiffOp\DiffOpChange;
+use EchoAttributeManager;
+use EchoEvent;
+use MWNamespace;
+use Title;
+use User;
+use Wikibase\Change;
+use Wikibase\Client\Notifications\PageConnectionPresentationModel;
+use Wikibase\Client\RepoLinker;
+use Wikibase\Client\WikibaseClient;
+use Wikibase\ItemChange;
+use WikiPage;
+
+/**
+ * Handlers for client Echo notifications
+ *
+ * @since 0.5
+ *
+ * @license GPL-2.0+
+ * @author Matěj Suchánek
+ */
+class EchoNotificationsHandlers {
+
+	/**
+	 * Type of notification
+	 */
+	const NOTIFICATION_TYPE = 'page-connection';
+
+	/**
+	 * @var RepoLinker
+	 */
+	private $repoLinker;
+
+	/**
+	 * @var string
+	 */
+	private $siteId;
+
+	/**
+	 * @var bool
+	 */
+	private $sendEchoNotification;
+
+	/**
+	 * @param RepoLinker $repoLinker
+	 * @param string $siteId
+	 * @param bool $sendEchoNotification
+	 */
+	public function __construct( RepoLinker $repoLinker, $siteId, $sendEchoNotification ) {
+		$this->repoLinker = $repoLinker;
+		$this->siteId = $siteId;
+		$this->sendEchoNotification = $sendEchoNotification;
+	}
+
+	/**
+	 * @return self
+	 */
+	public static function newFromGlobalState() {
+		$wikibaseClient = WikibaseClient::getDefaultInstance();
+		$settings = $wikibaseClient->getSettings();
+
+		return new self(
+			$wikibaseClient->newRepoLinker(),
+			$settings->getSetting( 'siteGlobalID' ),
+			$settings->getSetting( 'sendEchoNotification' )
+		);
+	}
+
+	/**
+	 * Handler for BeforeCreateEchoEvent hook
+	 * @see https://www.mediawiki.org/wiki/Notifications/Developer_guide
+	 * @see https://www.mediawiki.org/wiki/Extension:Echo/BeforeCreateEchoEvent
+	 *
+	 * @param array[] &$notifications
+	 * @param array[] &$notificationCategories
+	 * @param array[] &$icons
+	 */
+	public static function onBeforeCreateEchoEvent(
+		array &$notifications,
+		array &$notificationCategories,
+		array &$icons
+	) {
+		$notificationCategories['wikibase-action'] = [
+			'priority' => 5,
+			'tooltip' => 'echo-pref-tooltip-wikibase-action',
+		];
+
+		$notifications[self::NOTIFICATION_TYPE] = [
+			EchoAttributeManager::ATTR_LOCATORS => [
+				'EchoUserLocator::locateArticleCreator',
+			],
+			'category' => 'wikibase-action',
+			'group' => 'neutral',
+			'section' => 'alert',
+			'presentation-model' => PageConnectionPresentationModel::class,
+			'bundle' => [ 'web' => true, 'email' => false ],
+		];
+
+		$wikibaseClient = WikibaseClient::getDefaultInstance();
+		$settings = $wikibaseClient->getSettings();
+
+		$icon = $settings->getSetting( 'repoIcon' );
+		if ( !empty( $icon ) ) {
+			$icons[self::NOTIFICATION_TYPE] = $icon;
+		} else {
+			$icons[self::NOTIFICATION_TYPE] = $icons['placeholder'];
+		}
+	}
+
+	/**
+	 * Handler for EchoGetBundleRules hook
+	 * @see https://www.mediawiki.org/wiki/Notifications/Developer_guide#Bundled_notifications
+	 *
+	 * @param EchoEvent $event
+	 * @param string &$bundleString
+	 */
+	public static function onEchoGetBundleRules( EchoEvent $event, &$bundleString ) {
+		if ( $event->getType() === self::NOTIFICATION_TYPE ) {
+			$bundleString = self::NOTIFICATION_TYPE;
+		}
+	}
+
+	/**
+	 * Handler for UserGetDefaultOptions hook
+	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/UserGetDefaultOptions
+	 *
+	 * @param bool[] &$defaultOptions Array of preference keys and their default values.
+	 */
+	public static function onUserGetDefaultOptions( array &$defaultOptions ) {
+		$defaultOptions['echo-subscriptions-web-' . self::NOTIFICATION_TYPE] = true;
+	}
+
+	/**
+	 * Handler for WikibaseHandleChange hook
+	 * @see doWikibaseHandleChange
+	 *
+	 * @param Change $change
+	 */
+	public static function onWikibaseHandleChange( Change $change ) {
+		$self = self::newFromGlobalState();
+		$self->doWikibaseHandleChange( $change );
+	}
+
+	/**
+	 * @param Change $change
+	 *
+	 * @return bool
+	 */
+	public function doWikibaseHandleChange( Change $change ) {
+		if ( !( $change instanceof ItemChange ) ) {
+			return false;
+		}
+
+		if ( $this->sendEchoNotification !== true ) {
+			return false;
+		}
+
+		$siteLinkDiff = $change->getSiteLinkDiff();
+		if ( $siteLinkDiff->isEmpty() ) {
+			return false;
+		}
+
+		$siteId = $this->siteId;
+		if ( !isset( $siteLinkDiff[$siteId] ) || !isset( $siteLinkDiff[$siteId]['name'] ) ) {
+			return false;
+		}
+
+		$siteLinkDiffOp = $siteLinkDiff[$siteId]['name'];
+
+		$title = $this->getTitleForNotification( $siteLinkDiffOp );
+		if ( $title !== false ) {
+			$metadata = $change->getMetadata();
+			$entityId = $change->getEntityId();
+			$agent = User::newFromName( $metadata['user_text'], false );
+			EchoEvent::create( [
+				'agent' => $agent,
+				'extra' => [
+					// maybe also a diff link?
+					'url' => $this->repoLinker->getEntityUrl( $entityId ),
+				],
+				'title' => $title,
+				'type' => self::NOTIFICATION_TYPE
+			] );
+
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Determines whether the change was a real sitelink addition
+	 * and returns either title, or false
+	 *
+	 * @param DiffOp $siteLinkDiffOp
+	 *
+	 * @return Title|false
+	 */
+	private function getTitleForNotification( DiffOp $siteLinkDiffOp ) {
+		if ( $siteLinkDiffOp instanceof DiffOpAdd ) {
+			$new = $siteLinkDiffOp->getNewValue();
+			$newTitle = Title::newFromText( $new );
+			return $this->canNotifyForTitle( $newTitle ) ? $newTitle : false;
+		}
+
+		// if it's a sitelink change, make sure it wasn't triggered by a page move
+		if ( $siteLinkDiffOp instanceof DiffOpChange ) {
+			$new = $siteLinkDiffOp->getNewValue();
+			$newTitle = Title::newFromText( $new );
+
+			if ( !$this->canNotifyForTitle( $newTitle ) ) {
+				return false;
+			}
+
+			$old = $siteLinkDiffOp->getOldValue();
+			$oldTitle = Title::newFromText( $old );
+
+			// propably means that there was a page move
+			// without keeping the old title as redirect
+			if ( !$oldTitle->exists() ) {
+				return false;
+			}
+
+			// even if the old page is a redirect, make sure it redirects to the new title
+			if ( $oldTitle->isRedirect() ) {
+				$page = WikiPage::factory( $oldTitle );
+				$targetTitle = $page->getRedirectTarget();
+				if ( $targetTitle && $targetTitle->equals( $newTitle ) ) {
+					return false;
+				}
+			}
+
+			return $newTitle;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Whether it's reasonable to send a notification for the title
+	 *
+	 * @param Title $title
+	 *
+	 * @return bool
+	 */
+	private function canNotifyForTitle( Title $title ) {
+		return $title->exists() && !$title->isRedirect()
+			&& MWNamespace::isContent( $title->getNamespace() );
+	}
+
+}
---
+++ client/includes/Notifications/PageConnectionPresentationModel.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Wikibase\Client\Notifications;
+
+use EchoEvent;
+use EchoEventPresentationModel;
+use Title;
+
+/**
+ * Presentation model for Echo notifications
+ *
+ * @since 0.5
+ *
+ * @license GPL-2.0+
+ * @author Matěj Suchánek
+ */
+class PageConnectionPresentationModel extends EchoEventPresentationModel {
+
+	/**
+	 * @param EchoEvent
+	 * @return string
+	 */
+	public function callbackForBundleCount( EchoEvent $event ) {
+		return $event->getTitle()->getPrefixedText();
+	}
+
+	/**
+	 * @see EchoEventPresentationModel::getIconType()
+	 */
+	public function getIconType() {
+		return 'page-connection';
+	}
+
+	/**
+	 * @see EchoEventPresentationModel::canRender()
+	 */
+	public function canRender() {
+		return $this->event->getTitle()->exists();
+	}
+
+	/**
+	 * @see EchoEventPresentationModel::getHeaderMessage()
+	 */
+	public function getHeaderMessage() {
+		$count = $this->getNotificationCountForOutput(
+			false, // we need only other pages count
+			[ $this, 'callbackForBundleCount' ]
+		);
+
+		$truncated = $this->getTruncatedTitleText( $this->event->getTitle(), true );
+
+		if ( $count > 0 ) {
+			$msg = $this->getMessageWithAgent( "notification-bundle-header-{$this->type}" )
+				->params( $truncated )
+				->numParams( $count );
+		} else {
+			$msg = $this->getMessageWithAgent( "notification-header-{$this->type}" )
+				->params( $truncated );
+		}
+
+		return $msg;
+	}
+
+	/**
+	 * @see EchoEventPresentationModel::getSubjectMessageKey()
+	 */
+	protected function getSubjectMessageKey() {
+		return "notification-subject-{$this->type}";
+	}
+
+	/**
+	 * @see EchoEventPresentationModel::getSubjectMessage()
+	 */
+	public function getSubjectMessage() {
+		return parent::getSubjectMessage()->params( $this->getViewingUserForGender() );
+	}
+
+	/**
+	 * @see EchoEventPresentationModel::getPrimaryLink()
+	 */
+	public function getPrimaryLink() {
+		$title = $this->event->getTitle();
+		return [
+			'url' => $title->getFullURL(),
+			'label' => $title->getFullText()
+		];
+	}
+
+	/**
+	 * @see EchoEventPresentationModel::getSecondaryLinks()
+	 */
+	public function getSecondaryLinks() {
+		$extra = $this->event->getExtra();
+		$ret = [];
+
+		if ( $this->getBundleCount( true, [ $this, 'callbackForBundleCount' ] ) === 1 ) {
+			$ret[] = $this->getAgentLink();
+			$ret[] = [
+				'url' => $extra['url'],
+				'label' => $this->msg( 'notification-link-text-view-item' )->text(),
+				'description' => '',
+				'icon' => 'changes',
+				'prioritized' => true,
+			];
+		}
+
+		$message = $this->msg( 'notification-page-connection-link' );
+		if ( !$message->isDisabled() ) {
+			$title = Title::newFromText( $message->plain() );
+			if ( $title->exists() ) {
+				$ret[] = [
+					'url' => $title->getFullURL(),
+					'label' => $this->msg( 'echo-learn-more' )->text(),
+					'description' => '',
+					'icon' => 'help',
+					'prioritized' => false,
+				];
+			}
+		}
+
+		return $ret;
+	}
+
+}
