bb25e353374bd52302b4c0ecdc4c5e7a540b7d2e
matejsuchanek <matejsuchanek97@gmail.com>
2016-03-09 14:17:06 +0100

    Send a notification when a page is connected to a repo item
    When a page is connected to the associated repository by a user different
    from the one who created the page, an Echo notification is sent to the user
    on the client wiki. One of the secondary links is customisable similarly to
    how welcome notifications are. The web notification is bundled.
    
    This is configurable per client and requires the Echo extension enabled.
    
    Bug: T110604
    Depends-On: I98dd21ffa
    Change-Id: I2a3babd7f5bcc0cbf923d94b1ea3fe768457a95d

---
+++ client/tests/phpunit/includes/Notifications/PageConnectionPresentationModelTest.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Wikibase\Client\Tests\Notifications;
+
+use EchoEvent;
+use EchoEventPresentationModel;
+use MediaWikiTestCase;
+use Message;
+use Title;
+use User;
+use Wikibase\Client\Hooks\EchoNotificationsHandlers;
+use Wikibase\Client\Notifications\PageConnectionPresentationModel;
+use Wikibase\Client\RepoLinker;
+
+/**
+ * @covers Wikibase\Client\Notifications\PageConnectionPresentationModel
+ *
+ * @group Database
+ * @group Wikibase
+ * @group Wikibase Client
+ *
+ * @since 0.5
+ */
+class PageConnectionPresentationModelTest extends MediaWikiTestCase {
+
+	/**
+	 * @var RepoLinker
+	 */
+	private $repoLinker;
+
+	protected function setUp() {
+		parent::setUp();
+		// if Echo is not loaded, skip this test
+		if ( !class_exists( EchoEventPresentationModel::class ) ) {
+			$this->markTestSkipped( "Echo not loaded" );
+		}
+
+		$this->repoLinker = $this->getMockBuilder( RepoLinker::class )
+			->disableOriginalConstructor()
+			->getMock();
+		$this->repoLinker
+			->expects( $this->any() )
+			->method( 'getEntityUrl' )
+			->will( $this->returnValue( 'foo' ) );
+	}
+
+	/**
+	 * @param Title $title
+	 *
+	 * @return EchoEvent
+	 */
+	private function mockEvent( Title $title ) {
+		$agent = User::newFromName( 'Agent' );
+
+		$methods = [ 'getAgent', 'getBundleHash', 'getExtraParam', 'getTitle', 'getType' ];
+		$event = $this->getMockBuilder( EchoEvent::class )
+			->disableOriginalConstructor()
+			->setMethods( $methods )
+			->getMock();
+		$event->expects( $this->any() )
+			->method( 'getAgent' )
+			->will( $this->returnValue( $agent ) );
+		$event->expects( $this->any() )
+			->method( 'getBundleHash' )
+			->will( $this->returnValue( false ) );
+		$event->expects( $this->any() )
+			->method( 'getExtraParam' )
+			->will( $this->returnValue( 'dummy' ) );
+		$event->expects( $this->any() )
+			->method( 'getTitle' )
+			->will( $this->returnValue( $title ) );
+		$event->expects( $this->any() )
+			->method( 'getType' )
+			->will( $this->returnValue( EchoNotificationsHandlers::NOTIFICATION_TYPE ) );
+
+		return $event;
+	}
+
+	public function testPresentationModel() {
+		global $wgEchoNotifications, $wgEchoNotificationCategories, $wgEchoNotificationIcons;
+		$handlers = new EchoNotificationsHandlers(
+			$this->repoLinker, 'enwiki', true, false, 'repoSiteName'
+		);
+		$handlers->doBeforeCreateEchoEvent(
+			$wgEchoNotifications, $wgEchoNotificationCategories, $wgEchoNotificationIcons
+		);
+
+		$title = Title::newFromText( 'Connected_page' );
+		$event = $this->mockEvent( $title );
+		$this->assertTrue(
+			EchoEventPresentationModel::supportsPresentationModel( $event->getType() ),
+			"Failed asserting that Echo supports our notification events"
+		);
+
+		$user = User::newFromName( 'User' );
+		$model = EchoEventPresentationModel::factory( $event, 'en', $user );
+		$this->assertInstanceOf( PageConnectionPresentationModel::class, $model );
+
+		$this->assertFalse(
+			$model->canRender(),
+			"Failed asserting that the notification cannot be rendered"
+		);
+		$this->insertPage( 'Connected_page' );
+		$this->assertTrue(
+			$model->canRender(),
+			"Failed asserting that the notification can be rendered"
+		);
+
+		$this->assertArrayHasKey(
+			$model->getIconType(),
+			$wgEchoNotificationIcons,
+			"Failed asserting that our presentation model returns an existing icon type"
+		);
+
+		$msg = $model->getHeaderMessage();
+		$this->assertInstanceOf( Message::class, $msg );
+		$this->assertContains( $title->getPrefixedText(), $msg->text() );
+
+		$this->assertEquals(
+			[ 'url' => $title->getFullURL(), 'label' => $title->getFullText() ],
+			$model->getPrimaryLink(),
+			"Failed asserting that the primary link is same"
+		);
+
+		$this->assertInternalType( 'array', $model->getSecondaryLinks() );
+		$this->assertInstanceOf( Message::class, $model->getSubjectMessage() );
+
+		unset( $wgEchoNotifications[$handlers::NOTIFICATION_TYPE] );
+		unset( $wgEchoNotificationCategories['wikibase-action'] );
+		unset( $wgEchoNotificationIcons[$handlers::NOTIFICATION_TYPE] );
+	}
+
+}
