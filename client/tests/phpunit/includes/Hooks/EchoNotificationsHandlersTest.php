1e5d32f8fcab68fe3d3095ab29172bb9298a36dd
matejsuchanek <matejsuchanek97@gmail.com>
2016-03-09 14:17:06 +0100

    Send a notification when a page is connected to a repo item
    When a page is connected to the associated repository by a user different
    from the one who created the page, an Echo notification is sent to the user
    on the client wiki. One of the secondary links is customisable similarly to
    how welcome notifications are. The web notification is bundled.
    
    This is configurable per client and requires the Echo extension enabled.
    
    Bug: T110604
    Depends-On: I98dd21ffa
    Change-Id: I2a3babd7f5bcc0cbf923d94b1ea3fe768457a95d

---
+++ client/tests/phpunit/includes/Hooks/EchoNotificationsHandlersTest.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace Wikibase\Client\Tests\Hooks;
+
+use EchoEvent;
+use MediaWikiTestCase;
+use Wikibase\Client\Hooks\EchoNotificationsHandlers;
+use Wikibase\Client\RepoLinker;
+use Wikibase\Client\WikibaseClient;
+use Wikibase\Lib\Tests\Changes\TestChanges;
+use Wikibase\SettingsArray;
+
+/**
+ * @covers Wikibase\Client\Hooks\EchoNotificationsHandlers
+ *
+ * @group Database
+ * @group WikibaseClient
+ * @group Wikibase
+ */
+class EchoNotificationsHandlersTestCase extends MediaWikiTestCase {
+
+	/**
+	 * @var RepoLinker
+	 */
+	private $repoLinker;
+
+	protected function setUp() {
+		parent::setUp();
+		// if Echo is not loaded, skip this test
+		if ( !class_exists( EchoEvent::class ) ) {
+			$this->markTestSkipped( "Echo not loaded" );
+		}
+
+		$client = WikibaseClient::getDefaultInstance();
+		$this->repoLinker = $this->getMockBuilder( RepoLinker::class )
+			->disableOriginalConstructor()
+			->getMock();
+		$this->repoLinker
+			->expects( $this->any() )
+			->method( 'getEntityUrl' )
+			->will( $this->returnValue( 'foo' ) );
+	}
+
+	/**
+	 * @param SettingsArray $settings
+	 *
+	 * @return EchoNotificationsHandlers
+	 */
+	private function getHandlers( SettingsArray $settings ) {
+		return new EchoNotificationsHandlers(
+			$this->repoLinker,
+			$settings->getSetting( 'siteGlobalID' ),
+			$settings->getSetting( 'sendEchoNotification' )
+		);
+	}
+
+	public function testWikibaseHandleChange() {
+		/** @var ChangeRow[] $changes */
+		$changes = TestChanges::getChanges();
+
+		$settings = new SettingsArray();
+		$settings->setSetting( 'siteGlobalID', 'enwiki' );
+		$settings->setSetting( 'sendEchoNotification', true );
+
+		$handlers = $this->getHandlers( $settings );
+
+		$special = [
+			'change-dewiki-sitelink',
+			'change-enwiki-sitelink',
+			'set-enwiki-sitelink'
+		];
+		foreach ( $changes as $key => $change ) {
+			if ( in_array( $key, $special ) ) {
+				continue;
+			}
+			$this->assertFalse(
+				$handlers->doWikibaseHandleChange( $change ),
+				"Failed asserting that '$key' does not create an event"
+			);
+		}
+
+		$setEn = $changes['set-enwiki-sitelink'];
+		$changeEn = $changes['change-enwiki-sitelink'];
+
+		$this->assertFalse(
+			$handlers->doWikibaseHandleChange( $setEn ),
+			"Failed asserting that non-existing 'Emmy' does not create an event"
+		);
+
+		$this->insertPage( 'Emmy' );
+		$this->assertTrue(
+			$handlers->doWikibaseHandleChange( $setEn ),
+			"Failed asserting that 'Emmy' creates an event"
+		);
+
+		$settings->setSetting( 'siteGlobalID', 'dewiki' );
+		$handlers = $this->getHandlers( $settings );
+
+		$this->assertFalse(
+			$handlers->doWikibaseHandleChange( $setEn ),
+			"Failed asserting that 'dewiki' sitelink does not create an event"
+		);
+
+		$settings->setSetting( 'siteGlobalID', 'enwiki' );
+		$handlers = $this->getHandlers( $settings );
+
+		$this->assertFalse(
+			$handlers->doWikibaseHandleChange( $changeEn ),
+			"Failed asserting that non-existing 'Emmy2' does not create an event"
+		);
+
+		$this->insertPage( 'Emmy2' );
+		$this->assertTrue(
+			$handlers->doWikibaseHandleChange( $changeEn ),
+			"Failed asserting that 'Emmy2' creates an event"
+		);
+
+		$settings->setSetting( 'sendEchoNotification', false );
+		$handlers = $this->getHandlers( $settings );
+		$this->assertFalse(
+			$handlers->doWikibaseHandleChange( $setEn ),
+			"Failed asserting that configuration supresses creating an event"
+		);
+
+		$changeDe = $changes['change-dewiki-sitelink'];
+
+		$settings->setSetting( 'siteGlobalID', 'dewiki' );
+		$settings->setSetting( 'sendEchoNotification', true );
+		$handlers = $this->getHandlers( $settings );
+
+		$this->assertFalse(
+			$handlers->doWikibaseHandleChange( $changeDe ),
+			"Failed asserting that 'Dummy' does not create an event"
+		);
+
+		$this->insertPage( 'Dummy2' );
+		$this->assertFalse(
+			$handlers->doWikibaseHandleChange( $changeDe ),
+			"Failed asserting that 'Dummy2' does not create an event"
+		);
+
+		$this->insertPage( 'Dummy', '#REDIRECT [[Dummy2]]' );
+		$this->assertFalse(
+			$handlers->doWikibaseHandleChange( $changeDe ),
+			"Failed asserting that 'Dummy2' redirected to by 'Dummy' does not create an event"
+		);
+
+		$this->insertPage( 'Dummy' );
+		$this->assertTrue(
+			$handlers->doWikibaseHandleChange( $changeDe ),
+			"Failed asserting that 'Dummy2' creates an event"
+		);
+	}
+
+}
