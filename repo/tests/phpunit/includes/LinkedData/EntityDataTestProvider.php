a8faf6df5b71e869452df0603c2ddc3923038223
Stanislav Malyshev <smalyshev@wikimedia.org>
2016-03-16 16:54:42 -0700

    Switch coordinate order in WKT
    Change-Id: I9086f9125c5986321ff81ec65c5ca6c4eedb39dc
    Reference: http://www.geoapi.org/snapshot/javadoc/org/opengis/referencing/doc-files/WKT.html#AXIS
    Bug: T130049

--- repo/tests/phpunit/includes/LinkedData/EntityDataTestProvider.php
+++ repo/tests/phpunit/includes/LinkedData/EntityDataTestProvider.php
@@ -441,7 +441,7 @@ class EntityDataTestProvider {
 			array( 'id' => 'Q42', 'format' => 'ntriples', 'flavor' => 'full' ), // parameters
 			array(), // headers
 			'!^<http://data\.acme\.test/Q42> *'
-				. '<http://schema.org/softwareVersion> *"0\.0\.1" *\.$.*^'
+				. '<http://schema.org/softwareVersion> *"0\.0\.2" *\.$.*^'
 				. '<http://acme\.test/Q22> *'
 				. '<http://www\.w3\.org/2002/07/owl#sameAs> *'
 				. '<http://acme\.test/Q42> *.$!sm',
