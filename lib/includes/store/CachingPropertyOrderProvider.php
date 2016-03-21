abc2875c5478540beb805ec1e1541063b8cc409f
frimelle <lucie.aimee@googlemail.com>
2016-03-21 14:54:26 +0100

    Add a parser for the property page
    Parse a list on a wiki page to a PHP array
    
    Bug: T113952
    Change-Id: I5139acb8ac1dbb02af3c70af55762da1fab4e01d

---
+++ lib/includes/store/CachingPropertyOrderProvider.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Wikibase\Lib\Store;
+
+/**
+ * Interface for the MediaWikiPagePropertyOrderProvider
+ *
+ * @license GNU GPL v2+
+ * @author Lucie-Aimée Kaffee
+ */
+
+class CachingPropertyOrderProvider implements PropertyOrderProvider {
+
+	/**
+	 * @var PropertyOrderProvider
+	 */
+	private $propertyOrderProvider;
+
+	/**
+	 * The cache to use for caching entities.
+	 *
+	 * @var BagOStuff
+	 */
+	private $cache;
+
+	/**
+	 * @var int
+	 */
+	private $cacheDuration;
+
+	/**
+	 * @var string
+	 */
+	private $cacheKeyPrefix;
+
+	/**
+	 * @param PropertyOrderProvider $propertyOrderProvider
+	 * @param BagOStuff $cache
+	 * @param int $cacheDuration
+	 * @param string $cacheKeyPrefix
+	 */
+	public function __construct(
+		PropertyOrderProvider $propertyOrderProvider,
+		BagOStuff $cache,
+		$cacheDuration = 3600,
+		$cacheKeyPrefix = 'wikibase'
+	) {
+		$this->propertyOrderProvider = $propertyOrderProvider;
+		$this->cache = $cache;
+		$this->cacheDuration = $cacheDuration;
+		$this->cacheKeyPrefix = $cacheKeyPrefix;
+	}
+
+	/**
+	 * @return int[]|null
+	 */
+	public function getPropertyOrder() {
+		$propertyOrder = $this->propertyOrderProvider->getPropertyOrder();
+		if( $propertyOrder ) {
+			$this->cache->set(
+				null,
+				$propertyOrder,
+				$this->cacheDuration
+			);
+		}
+		return $propertyOrder;
+	}
+}
+
