21558c3c4fbc2c5c8c9c45c42f8b630e659c9d45
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2016-04-14 17:35:17 +0200

    Rename and rework classes for configuring JS deserializers
    This was originally meant to become Patch Set 9 in Ic058f8c, but it
    became to big.
    
    Bug: T132558
    Change-Id: I626a6ba29f82d045d4577bba79be51c7d6129a50

---
+++ repo/includes/Modules/EntityTypesConfigValueProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Wikibase\Repo\Modules;
+
+use Wikibase\Lib\EntityTypeDefinitions;
+
+/**
+ * @since 0.5
+ *
+ * @license GPL-2.0+
+ * @author Adrian Heine <adrian.heine@wikimedia.de>
+ * @author Thiemo Mättig
+ * @author Jonas Kress
+ */
+class EntityTypesConfigValueProvider implements MediaWikiConfigValueProvider {
+
+	/**
+	 * @var EntityTypeDefinitions
+	 */
+	private $entityTypeDefinitions;
+
+	public function __construct( EntityTypeDefinitions $entityTypeDefinitions ) {
+		$this->entityTypeDefinitions = $entityTypeDefinitions;
+	}
+
+	/**
+	 * @see MediaWikiJsConfigProvider::getKey
+	 *
+	 * @return string
+	 */
+	public function getKey() {
+		return 'wbEntityTypes';
+	}
+
+	/**
+	 * @see MediaWikiJsConfigProvider::getValue
+	 *
+	 * @return array
+	 */
+	public function getValue() {
+		return [
+			'types' => $this->entityTypeDefinitions->getEntityTypes(),
+			'deserializer-factory-functions'
+				=> $this->entityTypeDefinitions->getJsDeserializerFactoryFunctions()
+		];
+	}
+}
