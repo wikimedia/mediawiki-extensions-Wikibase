88c3764361674dcf658c9bc21b62a7179d4fa74d
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2016-04-26 11:39:13 +0200

    Remove unused code from DataValueFactory
    Change-Id: I085c0541aad6fe44887448386fe51018e3a28293

--- lib/includes/DataValueFactory.php
+++ lib/includes/DataValueFactory.php
@@ -7,12 +7,17 @@ use Deserializers\Exceptions\DeserializationException;
 use InvalidArgumentException;
 
 /**
+ * @since 0.1
+ *
  * @license GPL-2.0+
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  * @author Daniel Kinzler
  */
 class DataValueFactory {
 
+	/**
+	 * @var Deserializer
+	 */
 	private $deserializer;
 
 	public function __construct( Deserializer $dataValueDeserializer ) {
@@ -48,29 +53,6 @@ class DataValueFactory {
 	}
 
 	/**
-	 * Constructs and returns a new DataValue of specified type with the provided data,
-	 * using an UnDeserializableValue object to represent a bad data structure.
-	 *
-	 * @see DataValueFactory::newDataValue returns.
-	 *
-	 * @since 0.1
-	 *
-	 * @param string $dataValueType
-	 * @param mixed  $data
-	 *
-	 * @return DataValue
-	 */
-	public function tryNewDataValue( $dataValueType, $data ) {
-		try {
-			$value = $this->newDataValue( $dataValueType, $data );
-		} catch ( IllegalValueException $ex ) {
-			$value = new UnDeserializableValue( $data, $dataValueType, $ex->getMessage() );
-		}
-
-		return $value;
-	}
-
-	/**
 	 * Constructs a DataValue from its array representation.
 	 * This is what @see DataValue::toArray returns.
 	 *
@@ -95,42 +77,6 @@ class DataValueFactory {
 		}
 
 		return $this->newDataValue( $data['type'], $data['value'] );
-	}
-
-	/**
-	 * Constructs a DataValue from it's array representation, using an UnDeserializableValue
-	 * when the given data is invalid.
-	 *
-	 * @see DataValueFactory::newFromArray returns.
-	 *
-	 * @since 0.1
-	 *
-	 * @param array $data
-	 *
-	 * @return DataValue
-	 */
-	public function tryNewFromArray( array $data ) {
-		if ( !array_key_exists( 'type', $data ) ) {
-			$valueData = array_key_exists( 'value', $data ) ? $data['value'] : null;
-			return new UnDeserializableValue( $valueData, null, 'No type specified' );
-		}
-
-		if ( $data['type'] === null || $data['type'] === '' ) {
-			$valueData = array_key_exists( 'value', $data ) ? $data['value'] : null;
-			return new UnDeserializableValue( $valueData, $data['type'], 'No type specified' );
-		}
-
-		if ( !array_key_exists( 'value', $data ) ) {
-			return new UnDeserializableValue( null, $data['type'], 'No value data' );
-		}
-
-		try {
-			$value = $this->newFromArray( $data );
-		} catch ( IllegalValueException $ex ) {
-			$value = new UnDeserializableValue( $data['value'], $data['type'], $ex->getMessage() );
-		}
-
-		return $value;
 	}
 
 }
