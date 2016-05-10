4b35dfb8a9f6c8ddc821bee93da0a86051a03407
Adrian Heine <adrian.heine@wikimedia.de>
2016-05-10 13:12:43 +0200

    Extract FallbackHtmlIndicator
    Bug: T98041
    Change-Id: I7433a00aa6aab3daeb6c6c38476ef3be6ebb91f6

--- lib/includes/Formatters/EntityIdHtmlLinkFormatter.php
+++ lib/includes/Formatters/EntityIdHtmlLinkFormatter.php
@@ -22,9 +22,9 @@ use Wikibase\Lib\Store\EntityTitleLookup;
 class EntityIdHtmlLinkFormatter extends EntityIdLabelFormatter {
 
 	/**
-	 * @var LanguageNameLookup
+	 * @var LanguageFallbackIndicator
 	 */
-	private $languageNameLookup;
+	private $languageFallbackIndicator;
 
 	/**
 	 * @var EntityTitleLookup
@@ -44,7 +44,7 @@ class EntityIdHtmlLinkFormatter extends EntityIdLabelFormatter {
 		parent::__construct( $labelDescriptionLookup );
 
 		$this->entityTitleLookup = $entityTitleLookup;
-		$this->languageNameLookup = $languageNameLookup;
+		$this->languageFallbackIndicator = new LanguageFallbackIndicator( $languageNameLookup );
 	}
 
 	/**
@@ -91,7 +91,7 @@ class EntityIdHtmlLinkFormatter extends EntityIdLabelFormatter {
 		);
 
 		if ( $term instanceof TermFallback ) {
-			$fallbackIndicatorHtml = $this->getHtmlForFallbackIndicator( $term );
+			$fallbackIndicatorHtml = $this->languageFallbackIndicator->getHtml( $term );
 
 			if ( $term->getActualLanguageCode() !== $term->getLanguageCode() ) {
 				$attributes['lang'] = $term->getActualLanguageCode();
@@ -120,62 +120,6 @@ class EntityIdHtmlLinkFormatter extends EntityIdLabelFormatter {
 
 		$separator = wfMessage( 'word-separator' )->text();
 		return $entityId->getSerialization() . $separator . $undefinedInfo;
-	}
-
-	private function getHtmlForFallbackIndicator( TermFallback $term ) {
-		$requestedLanguage = $term->getLanguageCode();
-		$actualLanguage = $term->getActualLanguageCode();
-		$sourceLanguage = $term->getSourceLanguageCode();
-
-		// FIXME: TermFallback should either return equal values or null
-		$sourceLanguage = $sourceLanguage === null ? $actualLanguage : $sourceLanguage;
-
-		$isInRequestedLanguage = $actualLanguage === $requestedLanguage;
-		$isInSourceLanguage = $actualLanguage === $sourceLanguage;
-
-		if ( $isInRequestedLanguage && $isInSourceLanguage ) {
-			// This is neither a fallback nor a transliteration
-			return '';
-		}
-
-		$sourceLanguageName = $this->languageNameLookup->getName( $sourceLanguage );
-		$actualLanguageName = $this->languageNameLookup->getName( $actualLanguage );
-
-		// Generate indicator text
-		if ( $isInSourceLanguage ) {
-			$text = $sourceLanguageName;
-		} else {
-			$text = wfMessage(
-				'wikibase-language-fallback-transliteration-hint',
-				$sourceLanguageName,
-				$actualLanguageName
-			)->text();
-		}
-
-		// Generate HTML class names
-		$classes = 'wb-language-fallback-indicator';
-		if ( !$isInSourceLanguage ) {
-			$classes .= ' wb-language-fallback-transliteration';
-		}
-		if ( !$isInRequestedLanguage
-				&& $this->getBaseLanguage( $actualLanguage ) === $this->getBaseLanguage( $requestedLanguage )
-		) {
-			$classes .= ' wb-language-fallback-variant';
-		}
-
-		$attributes = array( 'class' => $classes );
-
-		$html = Html::element( 'sup', $attributes, $text );
-		return $html;
-	}
-
-	/**
-	 * @param string $languageCode
-	 *
-	 * @return string
-	 */
-	private function getBaseLanguage( $languageCode ) {
-		return preg_replace( '/-.*/', '', $languageCode );
 	}
 
 }
