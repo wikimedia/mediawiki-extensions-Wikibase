e860f666dad4c9cc39f474e22c4c34bbb97fdb48
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2016-05-11 14:37:40 +0200

    Make ChangeOpStatement work with StatementListProvider w/o index param
    As discussed in T133853 the index feature is not used in our code base.
    This patch makes the ChangeOp work with MediaInfo as long as there is no
    custom index provided.
    
    This does not block T134882. We can still track usage of the index
    feature. At the moment it can not be used on non-MediaInfo entity types
    anyway.
    
    Bug: T134884
    Change-Id: I37563c102dc5f94f73e6583ad6b446f8b46fa368

--- repo/includes/ChangeOp/ChangeOpStatement.php
+++ repo/includes/ChangeOp/ChangeOpStatement.php
@@ -7,12 +7,15 @@ use OutOfBoundsException;
 use ValueValidators\Result;
 use Wikibase\DataModel\ByPropertyIdArray;
 use Wikibase\DataModel\Entity\EntityDocument;
+use Wikibase\DataModel\Entity\EntityId;
 use Wikibase\DataModel\Services\Statement\GuidGenerator;
 use Wikibase\DataModel\Services\Statement\StatementGuidParser;
 use Wikibase\DataModel\Services\Statement\StatementGuidValidator;
 use Wikibase\DataModel\Statement\Statement;
 use Wikibase\DataModel\Statement\StatementList;
 use Wikibase\DataModel\Statement\StatementListHolder;
+use Wikibase\DataModel\Statement\StatementListProvider;
+use Wikibase\Repo\Tests\ChangeOp\StatementListProviderDummy;
 use Wikibase\Repo\Validators\SnakValidator;
 use Wikibase\Summary;
 
@@ -65,7 +68,7 @@ class ChangeOpStatement extends ChangeOpBase {
 	 * @param StatementGuidValidator $guidValidator
 	 * @param StatementGuidParser $guidParser
 	 * @param SnakValidator $snakValidator
-	 * @param int|null $index Where the claim should be placed among the other claims.
+	 * @param int|null $index Where the statement should be placed among the other statements.
 	 *
 	 * @throws InvalidArgumentException
 	 */
@@ -77,8 +80,8 @@ class ChangeOpStatement extends ChangeOpBase {
 		SnakValidator $snakValidator,
 		$index = null
 	) {
-		if ( !is_int( $index ) && $index !== null ) {
-			throw new InvalidArgumentException( '$index must be an integer or null' );
+		if ( $index !== null && ( !is_int( $index ) || $index < 0 ) ) {
+			throw new InvalidArgumentException( '$index must be an non-negative integer or null' );
 		}
 
 		$this->statement = $statement;
@@ -99,54 +102,79 @@ class ChangeOpStatement extends ChangeOpBase {
 	 * @throws ChangeOpException
 	 */
 	public function apply( EntityDocument $entity, Summary $summary = null ) {
-		if ( !( $entity instanceof StatementListHolder ) ) {
-			throw new InvalidArgumentException( '$entity must be a StatementListHolder' );
+		$entityId = $entity->getId();
+
+		if ( !( $entity instanceof StatementListProvider ) ) {
+			throw new InvalidArgumentException( '$entity must be a StatementListProvider' );
 		}
 
 		if ( $this->statement->getGuid() === null ) {
-			$this->statement->setGuid( $this->guidGenerator->newGuid( $entity->getId() ) );
+			$this->statement->setGuid( $this->guidGenerator->newGuid( $entityId ) );
 		}
 
+		$this->validateStatementGuid( $entityId );
+
+		if ( $this->index !== null ) {
+			if ( !( $entity instanceof StatementListHolder ) ) {
+				throw new ChangeOpException( 'Setting an index is not supported on this entity type' );
+			}
+
+			$this->applyStatementToEntity( $entity, $summary );
+		} else {
+			$oldIndex = $this->removeStatement( $entity->getStatements(), $summary );
+			// TODO: Use StatementList::addStatement( …, $index ). This will require DataModel 6.1.
+			if ( $oldIndex !== null ) {
+				$this->addStatementAtIndex( $entity->getStatements(), $oldIndex );
+			} else {
+				$entity->getStatements()->addStatement( $this->statement );
+			}
+		}
+	}
+
+	/**
+	 * @param EntityId $entityId
+	 *
+	 * @throws ChangeOpException
+	 */
+	private function validateStatementGuid( EntityId $entityId ) {
 		$guid = $this->guidParser->parse( $this->statement->getGuid() );
 
-		if ( $this->guidValidator->validate( $guid->getSerialization() ) === false ) {
-			throw new ChangeOpException( "Claim does not have a valid GUID" );
-		} elseif ( !$entity->getId()->equals( $guid->getEntityId() ) ) {
-			throw new ChangeOpException( "Claim GUID invalid for given entity" );
+		if ( !$this->guidValidator->validate( $guid->getSerialization() ) ) {
+			throw new ChangeOpException( 'Statement does not have a valid GUID' );
 		}
 
-		$this->applyStatementToEntity( $entity, $summary );
+		if ( !$guid->getEntityId()->equals( $entityId ) ) {
+			throw new ChangeOpException( 'Statement GUID invalid for given entity' );
+		}
 	}
 
 	/**
 	 * @param StatementListHolder $entity
 	 * @param Summary|null $summary
-	 *
-	 * @throws InvalidArgumentException
 	 */
 	private function applyStatementToEntity( StatementListHolder $entity, Summary $summary = null ) {
-		$this->removeStatement( $entity->getStatements(), $summary );
-		$statements = $this->addStatement( $entity->getStatements() );
+		$oldIndex = $this->removeStatement( $entity->getStatements(), $summary );
+		$newIndex = $this->index !== null ? $this->index : $oldIndex;
+		$statements = $this->addStatementToGroup( $entity->getStatements(), $newIndex );
 		$entity->setStatements( new StatementList( $statements ) );
 	}
 
 	/**
 	 * @param StatementList $statements
 	 * @param Summary|null $summary
+	 *
+	 * @return int|null
 	 */
 	private function removeStatement( StatementList $statements, Summary $summary = null ) {
 		$guid = $this->statement->getGuid();
 		$index = 0;
+		$oldIndex = null;
 		$oldStatement = null;
 
 		foreach ( $statements->toArray() as $statement ) {
 			if ( $statement->getGuid() === $guid ) {
+				$oldIndex = $index;
 				$oldStatement = $statement;
-
-				if ( $this->index === null ) {
-					$this->index = $index;
-				}
-
 				$statements->removeStatementsWithGuid( $guid );
 				break;
 			}
@@ -160,12 +188,14 @@ class ChangeOpStatement extends ChangeOpBase {
 			$this->checkMainSnakUpdate( $oldStatement );
 			$this->updateSummary( $summary, 'update' );
 		}
+
+		return $oldIndex;
 	}
 
 	/**
 	 * Checks that the update of the main snak is permissible.
 	 *
-	 * This checks that the main snaks of the old and the new claim
+	 * This checks that the main snaks of the old and the new statement
 	 * refer to the same property.
 	 *
 	 * @param Statement $oldStatement
@@ -186,22 +216,53 @@ class ChangeOpStatement extends ChangeOpBase {
 
 	/**
 	 * @param StatementList $statements
+	 * @param int $newIndex
 	 *
-	 * @throws ChangeOpException
-	 * @return Statement[]
+	 * @throws InvalidArgumentException
 	 */
-	private function addStatement( StatementList $statements ) {
-		// If we fail with the user supplied index and the index is greater than or equal 0
-		// presume the user wants to have the index at the end of the list.
-		if ( $this->index < 0 ) {
-			throw new ChangeOpException( 'Can not add claim at given index: '. $this->index );
+	private function addStatementAtIndex( StatementList $statements, $newIndex ) {
+		$index = 0;
+		$replacements = [];
+
+		foreach ( $statements->toArray() as $statement ) {
+			if ( $index >= $newIndex ) {
+				$guid = $statement->getGuid();
+
+				if ( $guid === null ) {
+					throw new InvalidArgumentException( 'Unexpected statement with no GUID set' );
+				}
+
+				$replacements[$guid] = $statement;
+			}
+
+			$index++;
 		}
 
+		foreach ( $replacements as $guid => $statement ) {
+			$statements->removeStatementsWithGuid( $guid );
+		}
+
+		$statements->addStatement( $this->statement );
+
+		foreach ( $replacements as $statement ) {
+			$statements->addStatement( $statement );
+		}
+	}
+
+	/**
+	 * @param StatementList $statements
+	 * @param int $index
+	 *
+	 * @return Statement[]
+	 */
+	private function addStatementToGroup( StatementList $statements, $index ) {
+		// If we fail with the user supplied index and the index is greater than or equal 0
+		// presume the user wants to have the index at the end of the list.
 		$indexedStatements = new ByPropertyIdArray( $statements->toArray() );
 		$indexedStatements->buildIndex();
 
 		try {
-			$indexedStatements->addObjectAtIndex( $this->statement, $this->index );
+			$indexedStatements->addObjectAtIndex( $this->statement, $index );
 		} catch ( OutOfBoundsException $ex ) {
 			$statements->addStatement( $this->statement );
 			return $statements->toArray();
