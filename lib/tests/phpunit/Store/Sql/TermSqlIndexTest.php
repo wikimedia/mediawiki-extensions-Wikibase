9f641ccb6383038916d0b143b45366619a415a47
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2016-05-26 11:07:09 +0200

    Runtime optimizations and more narrow interfaces in TermSqlIndex
    This is a small set of trivial, non-controversial refactorings, split
    from I0f7e2e3 to make it easier to review.
    
    Key changes:
    * Calculate weight not from the fingerprint but from the more narrow
      LabelsProvider.
    * Do not calculate weight in the loop. This does nothing but wasting time.
    * Nothing this class does makes any sense when no ID is set (term table
      entries with no ID are useless), so fail as early as possible. Note
      that it would fail anyway two lines below when assuming getId returns
      an EntityId.
    
    In case you wonder: saveTermsOfEntity is called in EntityHandler,
    everything else is class private, even the methods that are declared
    public for technical reasons.
    
    Bug: T134735
    Change-Id: I6e888ce3bf63660789ebbaac131a954e4850acef

--- lib/tests/phpunit/Store/Sql/TermSqlIndexTest.php
+++ lib/tests/phpunit/Store/Sql/TermSqlIndexTest.php
@@ -252,12 +252,14 @@ class TermSqlIndexTest extends TermIndexTest {
 	}
 
 	public function getEntityTermsProvider() {
+		$id = new ItemId( 'Q999' );
+
 		$fingerprint = new Fingerprint();
 		$fingerprint->setLabel( 'en', 'kittens!!!:)' );
 		$fingerprint->setDescription( 'es', 'es un gato!' );
 		$fingerprint->setAliasGroup( 'en', array( 'kitten-alias' ) );
 
-		$item = new Item( new ItemId( 'Q999' ) );
+		$item = new Item( $id );
 		$item->setFingerprint( $fingerprint );
 
 		$expectedTerms = array(
@@ -284,10 +286,15 @@ class TermSqlIndexTest extends TermIndexTest {
 			) )
 		);
 
+		$entityWithoutTerms = $this->getMock( EntityDocument::class );
+		$entityWithoutTerms->expects( $this->any() )
+			->method( 'getId' )
+			->will( $this->returnValue( $id ) );
+
 		return array(
 			array( $expectedTerms, $item ),
-			array( array(), new Item() ),
-			array( array(), $this->getMock( EntityDocument::class ) )
+			array( array(), new Item( $id ) ),
+			array( array(), $entityWithoutTerms)
 		);
 	}
 
