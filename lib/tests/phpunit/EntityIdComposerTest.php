762d04c522e3531f89924a75cffa1ddf0eeaaf73
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2016-05-26 15:02:49 +0200

    Introduce EntityIdComposer and "entity-id-composer"
    This is meant as a clean implementation for the hack you can see in
    https://gerrit.wikimedia.org/r/#/c/290461/1/lib/includes/Store/Sql/SqlEntityInfoBuilder.php
    
    This goes along with https://github.com/wmde/WikibaseDataModel/pull/670
    but is not an exact mirror of the Int32EntityId interface proposed there.
    This composer is much more general and also allows constructing all kinds of
    entity IDs that contain fixed parts that never change for that entity type
    (e.g. "File:Filename.jpg" where "File:" is a fixed prefix, but the suffix
    is not a number).
    
    The new stuff is not yet used in this patch. It's meant to replace all
    usages of LegacyIdInterpreter::newIdFromTypeAndNumber as well as remaining
    …::newFromNumber when it makes sense.
    
    Bug: T136294
    Change-Id: I9b541240ccbfb22425ab48b7de9c635be14dfc6d

---
+++ lib/tests/phpunit/EntityIdComposerTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Wikibase\Lib\Tests;
+
+use InvalidArgumentException;
+use PHPUnit_Framework_TestCase;
+use UnexpectedValueException;
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\Lib\EntityIdComposer;
+
+/**
+ * @covers Wikibase\Lib\EntityIdComposer
+ *
+ * @licence GNU GPL v2+
+ * @author Thiemo Mättig
+ */
+class EntityIdComposerTest extends PHPUnit_Framework_TestCase {
+
+	private function getComposer() {
+		return new EntityIdComposer( [
+			'numeric-item' => function( $uniquePart ) {
+				return new ItemId( 'Q' . $uniquePart );
+			},
+			'custom-item' => function( $uniquePart ) {
+				return new ItemId( 'Q100' . $uniquePart );
+			},
+		] );
+	}
+
+	public function invalidConstructorArgumentProvider() {
+		$callable = function( $uniquePart ) {
+		};
+
+		return [
+			[ [ 0 => $callable ] ],
+			[ [ '' => $callable ] ],
+			[ [ 'string' => null ] ],
+			[ [ 'string' => 'not a callable' ] ],
+		];
+	}
+
+	/**
+	 * @dataProvider invalidConstructorArgumentProvider
+	 */
+	public function testGivenInvalidComposer_constructorFails( $composers ) {
+		$this->setExpectedException( InvalidArgumentException::class );
+		new EntityIdComposer( $composers );
+	}
+
+	public function testGivenInvalidCallback_buildFails() {
+		$composer = new EntityIdComposer( [
+			'item' => function( $uniquePart ) {
+				return null;
+			},
+		] );
+		$this->setExpectedException( UnexpectedValueException::class );
+		$composer->composeEntityId( 'item', 1 );
+	}
+
+	public function validUniquePartProvider() {
+		return [
+			'Items are always supported' => [ 'item', 1, new ItemId( 'Q1' ) ],
+			'Properties are always supported' => [ 'property', 2, new PropertyId( 'P2' ) ],
+
+			'int' => [ 'numeric-item', 3, new ItemId( 'Q3' ) ],
+			'float' => [ 'numeric-item', 4.0, new ItemId( 'Q4' ) ],
+			'string' => [ 'numeric-item', '5', new ItemId( 'Q5' ) ],
+
+			'custom' => [ 'custom-item', 6, new ItemId( 'Q1006' ) ],
+		];
+	}
+
+	/**
+	 * @dataProvider validUniquePartProvider
+	 */
+	public function testGivenValidFragment_buildSucceeds( $entityType, $uniquePart, EntityId $expected ) {
+		$id = $this->getComposer()->composeEntityId( $entityType, $uniquePart );
+		$this->assertEquals( $expected, $id );
+	}
+
+	public function invalidUniquePartProvider() {
+		return [
+			[ null, 1 ],
+			[ 'unknown', 2 ],
+			[ 'item', null ],
+			[ 'item', new ItemId( 'Q4' ) ],
+		];
+	}
+
+	/**
+	 * @dataProvider invalidUniquePartProvider
+	 */
+	public function testGivenInvalidFragment_buildFails( $entityType, $uniquePart ) {
+		$composer = $this->getComposer();
+		$this->setExpectedException( InvalidArgumentException::class );
+		$composer->composeEntityId( $entityType, $uniquePart );
+	}
+
+}
