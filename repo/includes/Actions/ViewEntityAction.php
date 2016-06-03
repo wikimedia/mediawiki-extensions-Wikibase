9f9567b652fd84f84764d758d63907a581f61e31
daniel <daniel.kinzler@wikimedia.de>
2016-06-03 23:56:35 +0200

    remove code dublication for "missing page" message.
    Change-Id: Ifaac47cc66b49517326c858b25b1d7f02b2f21fe

--- repo/includes/Actions/ViewEntityAction.php
+++ repo/includes/Actions/ViewEntityAction.php
@@ -27,14 +27,6 @@ abstract class ViewEntityAction extends ViewAction {
 	 * Parent is doing $this->checkCanExecute( $this->getUser() )
 	 */
 	public function show() {
-		if ( !$this->page->exists() ) {
-			// @fixme could use ShowMissingArticle hook instead.
-			// Article checks for missing / deleted revisions and either
-			// shows appropriate error page or deleted revision, if permission allows.
-			$this->displayMissingEntity();
-			return;
-		}
-
 		$this->showEntityPage();
 	}
 
@@ -121,73 +113,6 @@ abstract class ViewEntityAction extends ViewAction {
 	private function setHTMLTitle( OutputPage $outputPage, $titleText ) {
 		// Prevent replacing {{...}} by using rawParams() instead of params():
 		$outputPage->setHTMLTitle( $this->msg( 'pagetitle' )->rawParams( $titleText ) );
-	}
-
-	/**
-	 * Displays there is no entity for the current page.
-	 */
-	private function displayMissingEntity() {
-		$title = $this->getTitle();
-		$oldid = $this->page->getOldID();
-
-		$out = $this->getOutput();
-
-		$out->setPageTitle( $title->getPrefixedText() );
-
-		// TODO: Factor the "show stuff for missing page" code out from Article::showMissingArticle,
-		//       so it can be re-used here. The below code is copied & modified from there...
-
-		Hooks::run( 'ShowMissingArticle', array( $this->page ) );
-
-		# Show delete and move logs
-		LogEventsList::showLogExtract( $out, array( 'delete', 'move' ), $title, '',
-			array(
-				'lim' => 10,
-				'conds' => array( "log_action != 'revision'" ),
-				'showIfEmpty' => false,
-				'msgKey' => array( 'moveddeleted-notice' )
-			)
-		);
-
-		$this->send404Code();
-
-		$hookResult = Hooks::run( 'BeforeDisplayNoArticleText', array( $this ) );
-
-		// XXX: ...end of stuff stolen from Article::showMissingArticle
-
-		if ( $hookResult ) {
-			// Show error message
-			if ( $oldid ) {
-				$text = wfMessage( 'missing-article',
-					$this->getTitle()->getPrefixedText(),
-					wfMessage( 'missingarticle-rev', $oldid )->plain() )->plain();
-			} else {
-				/** @var EntityHandler $entityHandler */
-				$entityHandler = ContentHandler::getForTitle( $this->getTitle() );
-				$entityCreationPage = $entityHandler->getSpecialPageForCreation();
-
-				$text = wfMessage( 'wikibase-noentity' )->plain();
-
-				if ( $entityCreationPage !== null
-					&& $this->getTitle()->quickUserCan( 'create', $this->getUser() )
-					&& $this->getTitle()->quickUserCan( 'edit', $this->getUser() )
-				) {
-					/*
-					 * add text with link to special page for creating an entity of that type if possible and
-					 * if user has the rights for it
-					 */
-					$createEntityPage = SpecialPage::getTitleFor( $entityCreationPage );
-					$text .= ' ' . wfMessage(
-						'wikibase-noentity-createone',
-						$createEntityPage->getPrefixedText() // TODO: might be nicer to use an 'action=create' instead
-					)->plain();
-				}
-			}
-
-			$text = "<div class='noarticletext'>\n$text\n</div>";
-
-			$out->addWikiText( $text );
-		}
 	}
 
 	private function send404Code() {
