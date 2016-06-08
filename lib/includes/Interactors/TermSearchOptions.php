b92b7ee09f1ab48eed4932065778bf85590fa4cd
aude <aude.wiki@gmail.com>
2016-06-08 16:11:38 -0400

    Split search options from TermIndexSearchInteractor
    We need these for ElasticSearch-based searches.
    
    Change-Id: I71c3f33e203872da30f3ef135a6af3d3425e3ab8

---
+++ lib/includes/Interactors/TermSearchOptions.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Wikibase\Lib\Interactors;
+
+use Wikibase\DataModel\Term\Term;
+use Wikimedia\Assert\Assert;
+
+/**
+ * @since 0.5
+ *
+ * @license GPL-2.0+
+ * @author Addshore
+ * @author Katie Filbert < aude.wiki@gmail.com >
+ */
+class TermSearchOptions {
+
+	const HARD_LIMIT = 5000;
+
+	/**
+	 * @var bool do a case sensitive search
+	 */
+	private $isCaseSensitive = false;
+
+	/**
+	 * @var bool do a prefix search
+	 */
+	private $isPrefixSearch = false;
+
+	/**
+	 * @var bool use language fallback in the search
+	 */
+	private $useLanguageFallback = true;
+
+	/**
+	 * @var int
+	 */
+	private $limit = self::HARD_LIMIT;
+
+	/**
+	 * @return int
+	 */
+	public function getLimit() {
+		return $this->limit;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function getIsCaseSensitive() {
+		return $this->isCaseSensitive;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function getIsPrefixSearch() {
+		return $this->isPrefixSearch;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function getUseLanguageFallback() {
+		return $this->useLanguageFallback;
+	}
+
+	/**
+	 * @param int $limit Hard upper limit of 5000
+	 */
+	public function setLimit( $limit ) {
+		Assert::parameterType( 'integer', $limit, '$limit' );
+		Assert::parameter( $limit > 0, '$limit', 'Must be positive' );
+		if ( $limit > self::HARD_LIMIT ) {
+			$limit = self::HARD_LIMIT;
+		}
+		$this->limit = $limit;
+	}
+
+	/**
+	 * @param bool $caseSensitive
+	 */
+	public function setIsCaseSensitive( $caseSensitive ) {
+		Assert::parameterType( 'boolean', $caseSensitive, '$caseSensitive' );
+		$this->isCaseSensitive = $caseSensitive;
+	}
+
+	/**
+	 * @param bool $prefixSearch
+	 */
+	public function setIsPrefixSearch( $prefixSearch ) {
+		Assert::parameterType( 'boolean', $prefixSearch, '$prefixSearch' );
+		$this->isPrefixSearch = $prefixSearch;
+	}
+
+	/**
+	 * @param bool $useLanguageFallback
+	 */
+	public function setUseLanguageFallback( $useLanguageFallback ) {
+		Assert::parameterType( 'boolean', $useLanguageFallback, '$useLanguageFallback' );
+		$this->useLanguageFallback = $useLanguageFallback;
+	}
+
+}
