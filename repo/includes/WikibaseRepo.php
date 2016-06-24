d59ff763c07411ff42e01406b6f67ae118ba79f6
Marius Hoch <hoo@online.de>
2016-06-24 17:33:23 +0200

    Don't pass string as constructor argument for MediaWikiPageNameNormalizer
    Not needed, breaks adding an actual constructor.
    
    Change-Id: I65295c0e09edbd4abfc9c2d94a2c3d7687990e41
    (cherry picked from commit 0038bfa7629cdedc9c1222088c1278f5a2f52391)

--- repo/includes/WikibaseRepo.php
+++ repo/includes/WikibaseRepo.php
@@ -1737,7 +1737,7 @@ class WikibaseRepo {
 	public function getCachingCommonsMediaFileNameLookup() {
 		if ( $this->cachingCommonsMediaFileNameLookup === null ) {
 			$this->cachingCommonsMediaFileNameLookup = new CachingCommonsMediaFileNameLookup(
-				new MediaWikiPageNameNormalizer( 'https://commons.wikimedia.org/w/api.php' ),
+				new MediaWikiPageNameNormalizer(),
 				new HashBagOStuff()
 			);
 		}
