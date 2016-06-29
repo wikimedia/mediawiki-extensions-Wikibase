af9b972d83f3e948303219d6790267d69f069941
Stanislav Malyshev <smalyshev@wikimedia.org>
2016-06-28 22:30:36 -0700

    Unit conversion support for full statements
    Additional statements go as follows:
    
    For statement:
    wds:STATEMENT psv:P6 wdv:cb213eea7a0b90d1d7f65c6eabfab9da;
              psn:P6 wdv:7bd19eac1a7dee69b4b0e27b2d622bf2
    
    Same for prv/prn and pqv/pqn.
    
    For value:
    wdv:cb213eea7a0b90d1d7f65c6eabfab9da wikibase:quantityNormalized wdv:7bd19eac1a7dee69b4b0e27b2d622bf2 .
    
    Where wdv:cb213eea7a0b90d1d7f65c6eabfab9da is original value,
    wdv:7bd19eac1a7dee69b4b0e27b2d622bf2 is normalized one.
    
    Values can normalize to themselves, but unitless value do not have
    normalized values.
    
    Change-Id: Icff586125010534ed048f20c78fdfbd912e1d1a4
    Bug: T117031

---
+++ lib/includes/Units/JsonUnitStorage.php
@@ -0,0 +1,50 @@
+<?php
+namespace Wikibase\Lib;
+
+/**
+ * JSON based unit conversion storage.
+ * Storage format:
+ * Json map, each element is 'Qsource': [ <factor>, 'QstandardUnit' ]
+ * Example:
+{
+"Q103510": [ "100000", "Q44395" ],
+"Q130964": [ "4.19", "Q25269" ],
+"Q182429": [ "1", "Q182429" ]
+}
+ * Q182429 here is a primary unit since source and standard are the same.
+ * Primary units must have factor of 1.
+ * Another acceptable format is:
+ * "Q103510": { "factor": "100000", "unit": "Q44395" },
+ */
+class JsonUnitStorage extends BaseUnitStorage {
+
+	/**
+	 * Filename of the source file
+	 * @var string
+	 */
+	private $sourceFile;
+
+	/**
+	 * JsonUnitStorage constructor.
+	 * @param string $fileName Filename of the storage file.
+	 */
+	public function __construct( $fileName ) {
+		$this->sourceFile = $fileName;
+	}
+
+	/**
+	 * Load data from concrete storage
+	 * @return array|null
+	 */
+	protected function loadStorageData() {
+		if ( !is_readable( $this->sourceFile ) ) {
+			return null;
+		}
+		$data = file_get_contents( $this->sourceFile );
+		if ( !$data ) {
+			return null;
+		}
+		return json_decode( $data, true );
+	}
+
+}
