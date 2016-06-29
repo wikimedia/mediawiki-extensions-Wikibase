380f931be5cee62145f754e0d788e640b050cb18
Stanislav Malyshev <smalyshev@wikimedia.org>
2016-06-28 22:30:36 -0700

    [WIP] Unit conversion support
    Change-Id: Icff586125010534ed048f20c78fdfbd912e1d1a4

---
+++ lib/tests/phpunit/Units/UnitConverterTest.php
@@ -0,0 +1,121 @@
+<?php
+namespace Wikibase\Test;
+
+use DataValues\DecimalValue;
+use DataValues\QuantityValue;
+use PHPUnit_Framework_TestCase;
+use Wikibase\Lib\UnitConverter;
+use Wikibase\Lib\UnitStorage;
+
+/**
+ * @covers Wikibase\Lib\UnitConverter
+ *
+ * @group Wikibase
+ *
+ * @license GPL-2.0+
+ */
+class UnitConverterTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * @var UnitConverter
+	 */
+	private $uc;
+
+	public function setUp() {
+	}
+
+	public function getConverterPairs() {
+		return [
+			[ 'Q1', '+16', 'http://acme.test/Q2', '+1975.2' ],
+			[ 'Q1', '-16', 'http://acme.test/Q2', '-1975.2' ],
+			[ 'Q1', '+1.6', 'http://acme.test/Q2', '+197.52' ],
+			[ 'Q1', '+0.16', 'http://acme.test/Q2', '+19.752' ],
+			[ 'Q1', '+0.1600', 'http://acme.test/Q2', '+19.752' ],
+			[ 'Q1', '-0.00000000000000000016', 'http://acme.test/Q2', '-0.000000000000000019752' ],
+			[
+				'Q1',
+				'+1600000000000000000000000000000000000000000000',
+				'http://acme.test/Q2',
+				'+197520000000000000000000000000000000000000000000'
+			],
+			[ 'Q1', '+160', 'http://acme.test/Q2', '+19752' ],
+			[ 'Q1', '+1600', 'http://acme.test/Q2', '+197520' ],
+			[ 'Q1', '+0.131', 'http://acme.test/Q2', '+16.17195' ],
+		];
+	}
+
+	private function getConverter( $fromUnit, $result ) {
+		$mockStorage = $this->getMock( UnitStorage::class );
+		$mockStorage->method( 'getConversion' )->with( $fromUnit )->willReturn( $result );
+		return new UnitConverter( $mockStorage, 'http://acme.test/' );
+	}
+
+	/**
+	 * @dataProvider getConverterPairs
+	 * @param $fromUnit
+	 * @param $fromValue
+	 * @param $toUnit
+	 * @param $toValue
+	 */
+	public function testConvert( $fromUnit, $fromValue, $toUnit, $toValue ) {
+		$uc = $this->getConverter( $fromUnit, [ '123.45', 'Q2' ] );
+
+		$decimal = new DecimalValue( $fromValue );
+		$q = new QuantityValue( $decimal, $fromUnit, $decimal, $decimal );
+		$qConverted = $uc->toStandardUnits( $q );
+
+		$this->assertEquals( $toValue, $qConverted->getAmount()->getValue(), 'Wrong amount' );
+		$this->assertEquals( $toUnit, $qConverted->getUnit(), 'Wrong unit' );
+	}
+
+	public function testConvertPrefixes() {
+		$uc = $this->getConverter( 'Q123', [ '123.45', 'Q345' ] );
+		$decimal = new DecimalValue( '+0.111' );
+		$q = new QuantityValue( $decimal, 'http://acme.test/Q123', $decimal, $decimal );
+		$qConverted = $uc->toStandardUnits( $q );
+
+		$this->assertEquals( '+13.70295', $qConverted->getAmount()->getValue(), 'Wrong amount' );
+		$this->assertEquals( 'http://acme.test/Q345', $qConverted->getUnit(), 'Wrong unit' );
+
+	}
+
+	public function testBounds() {
+		$uc = $this->getConverter( 'Q123', [ '123.45', 'Q345' ] );
+		$decimal = new DecimalValue( '+0.111' );
+		$low = new DecimalValue( '-0.111' );
+		$up = new DecimalValue( '+0.222' );
+		$q = new QuantityValue( $decimal, 'http://acme.test/Q123', $up, $low );
+		$qConverted = $uc->toStandardUnits( $q );
+
+		$this->assertEquals( '+13.70295', $qConverted->getAmount()->getValue(), 'Wrong amount' );
+		$this->assertEquals( 'http://acme.test/Q345', $qConverted->getUnit(), 'Wrong unit' );
+		$this->assertEquals( '+27.4059', $qConverted->getUpperBound()->getValue(),
+			'Wrong upper bound' );
+		$this->assertEquals( '-13.70295', $qConverted->getLowerBound()->getValue(),
+			'Wrong lower bound' );
+	}
+
+	public function getBadConversions() {
+		return [
+			[ null ],
+		    [ [ '1', 'Q123' ] ],
+			[ [ '-1', 'Q123' ] ],
+			[ [ '0', 'Q123' ] ],
+			[ [ '0.0', 'Q123' ] ],
+		];
+	}
+
+	/**
+	 * Cases where no conversion should happen
+	 * @dataProvider getBadConversions
+	 * @param $conv Conversion data
+	 */
+	public function testNoConversion( $conv ) {
+		$uc = $this->getConverter( 'Q123', $conv );
+
+		$decimal = new DecimalValue( '+42' );
+		$q = new QuantityValue( $decimal, 'http://acme.test/Q123', $decimal, $decimal );
+		$qConverted = $uc->toStandardUnits( $q );
+		$this->assertSame( $q, $qConverted );
+	}
+}
---
+++ repo/maintenance/SPARQLClient.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * Simple SPARQL client
+ */
+class SPARQLClient {
+
+	/**
+	 * Construct.
+	 * @param string $url SPARQL Endpoint
+	 * @param string $baseURL RDF base URL - common prefix in entity URIs
+	 */
+	public function __construct( $url, $baseURL ) {
+		$this->endpoint = $url;
+		$this->baseURL = $baseURL;
+	}
+
+	/**
+	 * Query SPARQL endpoint
+	 * @param string $sparql query
+	 * @param bool   $rawData Whether to return only values or full data objects
+	 * @return array|false
+	 */
+	public function query( $sparql, $rawData = false ) {
+		$result =
+			Http::get( $this->endpoint . '?' .
+			           http_build_query( [ "query" => $sparql, "format" => "json" ] ), [ ],
+				__METHOD__ );
+		if ( !$result ) {
+			return false;
+		}
+		$data = json_decode( $result, true );
+		if ( !$data ) {
+			return false;
+		}
+
+		return $this->extractData( $data, $rawData );
+	}
+
+	/**
+	 * Get list of IDs satisfying the query
+	 * @param string $sparql query
+	 * @param string $item variable name designating the needed element
+	 * @return array|false
+	 */
+	public function getIDs( $sparql, $item ) {
+		$data = $this->query( $sparql, false );
+		if ( $data ) {
+			return array_map( function ( $row ) use ( $item ) {
+				return str_replace( $this->baseURL, '', $row[$item] );
+			}, $data );
+		}
+		return [ ];
+	}
+
+	/**
+	 * Extract data from SPARQL response format
+	 * @param array $data SPARQL result
+	 * @param bool  $rawData Whether to return only values or full data objects
+	 * @return array
+	 */
+	private function extractData( $data, $rawData = false ) {
+		$result = [ ];
+		if ( $data && !empty( $data['results'] ) ) {
+			$vars = $data['head']['vars'];
+			$resrow = [ ];
+			foreach ( $data['results']['bindings'] as $row ) {
+				foreach ( $vars as $var ) {
+					if ( !isset( $row[$var] ) ) {
+						$resrow[$var] = null;
+						continue;
+					}
+					if ( $rawData ) {
+						$resrow[$var] = $row[$var];
+					} else {
+						$resrow[$var] = $row[$var]['value'];
+					}
+				}
+				$result[] = $resrow;
+			}
+		}
+		return $result;
+	}
+}
