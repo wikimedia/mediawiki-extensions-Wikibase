8347430d0b1ba8e1c69532fe05f2401c70a10553
Adrian Heine <adrian.heine@wikimedia.de>
2016-07-05 14:03:09 +0200

    Remove itemremoved event
    Change-Id: Id1e93bcdc176af663e70aafb3aed91809a951806

--- view/tests/qunit/jquery/wikibase/jquery.wikibase.referenceview.tests.js
+++ view/tests/qunit/jquery/wikibase/jquery.wikibase.referenceview.tests.js
@@ -5,6 +5,26 @@
 ( function( $, wb, QUnit ) {
 	'use strict';
 
+	var listItemAdapter = wb.tests.getMockListItemAdapter(
+		'snaklistview',
+		function() {
+			this.enterNewItem = function() {
+				return $.Deferred().resolve( {
+					data: function() {
+						return { focus: function() {} };
+					}
+				} ).promise();
+			};
+			this.isValid = function() {
+				return false;
+			};
+			this.stopEditing = function() {};
+			this.value = function() {
+				return this.options.value;
+			};
+		}
+	);
+
 	/**
 	 * Generates a referenceview widget suitable for testing.
 	 *
@@ -23,25 +43,9 @@
 					destroy: function() {}
 				};
 			},
-			listItemAdapter: wb.tests.getMockListItemAdapter(
-				'snaklistview',
-				function() {
-					this.enterNewItem = function() {
-						return $.Deferred().resolve( {
-							data: function() {
-								return { focus: function() {} };
-							}
-						} ).promise();
-					};
-					this.isValid = function() {
-						return false;
-					};
-					this.stopEditing = function() {};
-					this.value = function() {
-						return this.options.value;
-					};
-				}
-			)
+			getListItemAdapter: function() {
+				return listItemAdapter;
+			}
 		}, options );
 
 		return $( '<div/>' )
