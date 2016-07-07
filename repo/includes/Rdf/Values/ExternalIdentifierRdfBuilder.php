360098de1eff9cb3755359d64d263ec1b3385976
daniel <daniel.kinzler@wikimedia.de>
2016-07-07 19:08:11 +0200

    RDF mapping for external identifiers.
    Bug: T121274
    Change-Id: I44a203bddfece0d275c31d089a1172ab71891dae

---
+++ repo/includes/Rdf/Values/ExternalIdentifierRdfBuilder.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Wikibase\Rdf\Values;
+
+use DataValues\StringValue;
+use Wikibase\DataModel\Snak\PropertyValueSnak;
+use Wikibase\Lib\Store\PropertyInfoProvider;
+use Wikibase\Rdf\RdfVocabulary;
+use Wikibase\Rdf\ValueSnakRdfBuilder;
+use Wikimedia\Purtle\RdfWriter;
+
+/**
+ * RDF mapping for StringValues that are interpreted as external identifiers.
+ * URIs for the external identifier are generated based on a URI pattern associated with
+ * the respective property.
+ *
+ * @since 0.5
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ * @author Stas Malyshev
+ */
+class ExternalIdentifierRdfBuilder implements ValueSnakRdfBuilder {
+
+	/**
+	 * @var PropertyInfoProvider
+	 */
+	private $uriPatternProvider;
+
+	/**
+	 * @param PropertyInfoProvider $uriPatternProvider
+	 */
+	public function __construct(
+		PropertyInfoProvider $uriPatternProvider
+	) {
+		$this->uriPatternProvider = $uriPatternProvider;
+	}
+
+	/**
+	 * @param RdfWriter $writer
+	 * @param string $propertyValueNamespace Property value relation namespace
+	 * @param string $propertyValueLName Property value relation name
+	 * @param string $dataType Property data type
+	 * @param PropertyValueSnak $snak
+	 */
+	public function addValue(
+		RdfWriter $writer,
+		$propertyValueNamespace,
+		$propertyValueLName,
+		$dataType,
+		PropertyValueSnak $snak
+	) {
+		$id = $this->getValueId( $snak->getDataValue() );
+		$uriPattern = $this->uriPatternProvider->getPropertyInfo( $snak->getPropertyId() );
+
+		$writer->say( $propertyValueNamespace, $propertyValueLName )->value( $id );
+
+		$predicate = RdfVocabulary::$normalizedPropertyValueNamespace[$propertyValueNamespace];
+		if ( $uriPattern !== null && $predicate !== null ) {
+			$uri = str_replace( '$1', wfUrlencode( $id ), $uriPattern );
+			$writer->say( $predicate, $propertyValueLName )->is( $uri );
+		}
+	}
+
+	/**
+	 * @param StringValue $value
+	 *
+	 * @return string the external ID
+	 */
+	private function getValueId( StringValue $value ) {
+		return trim( strval( $value->getValue() ) );
+	}
+
+}
