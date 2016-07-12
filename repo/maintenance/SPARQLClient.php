190f812b939135eef2f158980332c1de8ae0cbcf
Stanislav Malyshev <smalyshev@wikimedia.org>
2016-07-11 17:41:09 -0700

    Tools for creating unit conversion config
    Change-Id: I7845e1fb3b5f862799462428481d63ab8e7afdd6
    Bug: T117032

---
+++ repo/maintenance/SPARQLClient.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Wikibase\Repo\Maintenance;
+
+use MWHttpRequest;
+
+/**
+ * Simple SPARQL client
+ */
+class SPARQLClient {
+
+	/**
+	 * Query timeout
+	 */
+	const TIMEOUT = 300;
+
+	/**
+	 * Construct.
+	 * @param string $url SPARQL Endpoint
+	 * @param string $baseURL RDF base URL - common prefix in entity URIs
+	 */
+	public function __construct( $url, $baseURL ) {
+		$this->endpoint = $url;
+		$this->baseURL = $baseURL;
+	}
+
+	/**
+	 * Query SPARQL endpoint
+	 * @param string $sparql query
+	 * @param bool   $rawData Whether to return only values or full data objects
+	 * @return array List of results, one row per array element
+	 *               Each row will contain fields indexed by variable name.
+	 * @throws SPARQLException
+	 */
+	public function query( $sparql, $rawData = false ) {
+		$url = $this->endpoint . '?' . http_build_query( [ "query" => $sparql, "format" => "json" ] );
+		$options = [ 'method' => 'GET', 'timeout' => self::TIMEOUT ];
+		$request = MWHttpRequest::factory( $url, $options, __METHOD__ );
+		$status = $request->execute();
+		if ( !$status->isOK() ) {
+			$errors = implode( "\n", $status->getErrorsByType( 'error' ) );
+			throw new SPARQLException( "HTTP error: $errors" );
+		}
+		$result = $request->getContent();
+		$data = json_decode( $result, true );
+		if ( !$data ) {
+			throw new SPARQLException( "HTTP request failed, response:\n$result" );
+		}
+
+		return $this->extractData( $data, $rawData );
+	}
+
+	/**
+	 * Get list of IDs satisfying the query
+	 * @param string $sparql query
+	 * @param string $item variable name designating the needed element
+	 * @return string[]|false List of IDs from query
+	 */
+	public function getIDs( $sparql, $item ) {
+		$data = $this->query( $sparql, false );
+		if ( $data ) {
+			return array_map( function ( $row ) use ( $item ) {
+				return str_replace( $this->baseURL, '', $row[$item] );
+			}, $data );
+		}
+		return [];
+	}
+
+	/**
+	 * Extract data from SPARQL response format
+	 * @param array $data SPARQL result
+	 * @param bool  $rawData Whether to return only values or full data objects
+	 * @return array List of results, one row per element.
+	 */
+	private function extractData( $data, $rawData = false ) {
+		$result = [];
+		if ( $data && !empty( $data['results'] ) ) {
+			$vars = $data['head']['vars'];
+			$resrow = [];
+			foreach ( $data['results']['bindings'] as $row ) {
+				foreach ( $vars as $var ) {
+					if ( !isset( $row[$var] ) ) {
+						$resrow[$var] = null;
+						continue;
+					}
+					if ( $rawData ) {
+						$resrow[$var] = $row[$var];
+					} else {
+						$resrow[$var] = $row[$var]['value'];
+					}
+				}
+				$result[] = $resrow;
+			}
+		}
+		return $result;
+	}
+
+}
