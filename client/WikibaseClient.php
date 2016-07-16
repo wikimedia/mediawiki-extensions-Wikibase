9ff9b8ecc1f062518d85e97afbbfc3283cf822ef
Amir Sarabadani <Ladsgroup@gmail.com>
2016-07-16 14:47:41 +0430

    Introduce prop=wbcentityusage in API to expose wbc_entity_usage
    Bug: T103091
    Change-Id: Ic69ef8e4d6ebeecb423234f2aac8e3209e466c51

--- client/WikibaseClient.php
+++ client/WikibaseClient.php
@@ -195,6 +195,8 @@ call_user_func( function() {
 		}
 	);
 
+	$wgAPIPropModules['wbcentityusage'] = Wikibase\Client\Api\ApiQueryEntityUsage::class;
+
 	// Special page registration
 	$wgSpecialPages['UnconnectedPages'] = 'Wikibase\Client\Specials\SpecialUnconnectedPages';
 	$wgSpecialPages['PagesWithBadges'] = 'Wikibase\Client\Specials\SpecialPagesWithBadges';
