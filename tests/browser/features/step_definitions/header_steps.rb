86a24f50b15ce8fda2bcd3a0ee5ce0240e5510de
Tobias Gritschacher <tobias.gritschacher@wikimedia.de>
2016-07-22 10:32:44 +0200

    [WIP] Move everything from the WikidataBrowserTests repository
    This moves all browsertests to Wikibase.git. Once this is done,
    WikidataBrowserTests.git can be deleted.
    Also https://integration.wikimedia.org/ci/job/selenium-Wikidata/
    should then be removed in favor of 
    https://integration.wikimedia.org/ci/job/selenium-Wikibase/
    
    Bug: T118727
    Change-Id: Ieb7c345475fba836511dd7b9555fa6af12b97dfd

---
+++ tests/browser/features/step_definitions/header_steps.rb
@@ -0,0 +1,51 @@
+# -*- encoding : utf-8 -*-
+# Wikidata UI tests
+#
+# Author:: Tobias Gritschacher (tobias.gritschacher@wikimedia.de)
+# Author:: Christoph Fischer (christoph.fischer@wikimedia.de)
+# License:: GNU GPL v2+
+#
+# tests for the header section
+
+When(/^I click the header edit button$/) do
+  on(ItemPage).edit_header_link_element.when_visible.click
+end
+
+When(/^I click the header cancel button$/) do
+  on(ItemPage).cancel_header_link_element.when_visible.click
+end
+
+When(/^I click the header save button$/) do
+  on(ItemPage) do |page|
+    page.save_header_link_element.when_visible.click
+    page.wait_for_api_callback
+  end
+end
+
+When(/^I click the EntityTermsView toggler$/) do
+  on(ItemPage).terms_view_toggler_element.when_visible.click
+end
+
+Then(/^Header edit button should be there$/) do
+  expect(on(ItemPage).edit_header_link_element.when_visible).to be_visible
+end
+
+Then(/^Header edit button should not be there$/) do
+  expect(on(ItemPage).edit_header_link_element.when_not_visible).not_to be_visible
+end
+
+Then(/^Header cancel button should be there$/) do
+  expect(on(ItemPage).cancel_header_link_element.when_visible).to be_visible
+end
+
+Then(/^Header cancel button should not be there$/) do
+  expect(on(ItemPage).cancel_header_link_element.when_not_visible).not_to be_visible
+end
+
+Then(/^Header save button should be there$/) do
+  expect(on(ItemPage).save_header_link_element.when_visible).to be_visible
+end
+
+Then(/^Header save button should not be there$/) do
+  expect(on(ItemPage).save_header_link_element.when_not_visible).not_to be_visible
+end
