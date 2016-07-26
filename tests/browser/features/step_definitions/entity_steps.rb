86624f7feb300c5a06ecf80eed4483e77a155b2f
Tobias Gritschacher <tobias.gritschacher@wikimedia.de>
2016-07-26 16:21:29 +0200

    [WIP] Trying out some more scenarios for integration
    Change-Id: Ie1bd00edf74e7c7c2115a986bf1bcf4f62dfea09

--- tests/browser/features/step_definitions/entity_steps.rb
+++ tests/browser/features/step_definitions/entity_steps.rb
@@ -7,8 +7,9 @@
 # basic steps for entities
 
 Given(/^I am logged in to the repo$/) do
-  lenv = MediawikiSelenium::Environment.load_default
-  visit(RepoLoginPage).login_with(lenv.user, lenv.password)
+  as_user(:b) do
+    visit(RepoLoginPage).login_with(user(:b), password(:b))
+  end
 end
 
 Given(/^I am not logged in to the repo$/) do
@@ -74,7 +75,14 @@ Given(/^I navigate to property handle (.*)$/) do |handle|
 end
 
 Given(/^I have the following properties with datatype:$/) do |props|
-  @properties = visit(PropertyPage).create_properties(props)
+  property_data = on(PropertyPage).create_property_data(props)
+  wb_api = MediawikiApi::Wikidata::WikidataClient.new URL.repo_api
+
+  as_user(:b) do
+    wb_api.log_in(user(:b), password(:b))
+  end
+
+  @properties = on(PropertyPage).create_properties(property_data, wb_api)
 end
 
 Given(/^I have the following items:$/) do |handles|
@@ -99,9 +107,12 @@ Given(/^I am on an item page with empty label and description$/) do
 end
 
 Given(/^The following sitelinks do not exist:$/) do |sitelinks|
-  lenv = MediawikiSelenium::Environment.load_default
   wb_api = MediawikiApi::Wikidata::WikidataClient.new URL.repo_api
-  wb_api.log_in(lenv.user, lenv.password)
+
+  as_user(:b) do
+    wb_api.log_in(user(:b), password(:b))
+  end
+
   sitelinks.raw.each do |sitelink|
     if wb_api.sitelink_exists?(sitelink[0], sitelink[1])
       wb_api.remove_sitelink({ site_id: sitelink[0], title: sitelink[1] }, sitelink[0])
