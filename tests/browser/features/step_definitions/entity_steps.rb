f2945ff52421d5d4697c16e7a1c05a16cfd1d27c
Tobias Gritschacher <tobias.gritschacher@wikimedia.de>
2016-07-26 16:21:29 +0200

    [WIP] Trying out some more scenarios for integration
    Change-Id: Ie1bd00edf74e7c7c2115a986bf1bcf4f62dfea09

--- tests/browser/features/step_definitions/entity_steps.rb
+++ tests/browser/features/step_definitions/entity_steps.rb
@@ -99,14 +99,15 @@ Given(/^I am on an item page with empty label and description$/) do
 end
 
 Given(/^The following sitelinks do not exist:$/) do |sitelinks|
-  lenv = MediawikiSelenium::Environment.load_default
-  wb_api = MediawikiApi::Wikidata::WikidataClient.new URL.repo_api
-  wb_api.log_in(lenv.user, lenv.password)
-  sitelinks.raw.each do |sitelink|
-    if wb_api.sitelink_exists?(sitelink[0], sitelink[1])
-      wb_api.remove_sitelink({ site_id: sitelink[0], title: sitelink[1] }, sitelink[0])
+  as_user(:b) {
+    wb_api = MediawikiApi::Wikidata::WikidataClient.new URL.repo_api
+    wb_api.log_in(user(:b), password(:b))
+    sitelinks.raw.each do |sitelink|
+      if wb_api.sitelink_exists?(sitelink[0], sitelink[1])
+        wb_api.remove_sitelink({ site_id: sitelink[0], title: sitelink[1] }, sitelink[0])
+      end
     end
-  end
+  }
 end
 
 Then(/^An error message should be displayed$/) do
