d8103ca581e397efee52dc5409b81da55d4290b8
Marius Hoch <hoo@online.de>
2016-07-29 12:48:58 +0200

    Add Lua integration tests for pages that are not linked to an Item
    To test the behavior of functions that work with the Item linked with
    the current page, if there is no linked Item.
    
    Change-Id: Ide068fe26bb539a6bbd60bbe955f97d8bcc6e2ce

---
+++ client/tests/phpunit/includes/DataAccess/Scribunto/LuaWikibaseLibraryNoLinkedEntityTests.lua
@@ -0,0 +1,30 @@
+--[[
+	Tests for pages that are not connected to any Item.
+
+	@license GNU GPL v2+
+	@author Marius Hoch < hoo@online.de >
+]]
+
+local testframework = require 'Module:TestFramework'
+
+local tests = {
+	-- Integration tests
+
+	{ name = "mw.wikibase.getEntityObject returns nil", func = mw.wikibase.getEntityObject,
+	  expect = { nil }
+	},
+	{ name = "mw.wikibase.getEntityUrl returns nil", func = mw.wikibase.getEntityUrl,
+	  expect = { nil }
+	},
+	{ name = "mw.wikibase.label returns nil", func = mw.wikibase.label,
+	  expect = { nil }
+	},
+	{ name = "mw.wikibase.description returns nil", func = mw.wikibase.description,
+	  expect = { nil }
+	},
+	{ name = "mw.wikibase.description returns nil", func = mw.wikibase.description,
+	  expect = { nil }
+	},
+}
+
+return testframework.getTestProvider( tests )
