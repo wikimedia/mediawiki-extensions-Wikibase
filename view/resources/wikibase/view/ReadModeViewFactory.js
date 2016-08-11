1961571a752c26f9765fa708cff8899e15d343cb
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2016-08-11 18:08:15 +0200

    Introduce ReadModeViewFactory for diffs and old revisions
    Bug: T129836
    Change-Id: I0df8481402125cf71840dcbe9bead136c11ead6f

---
+++ view/resources/wikibase/view/ReadModeViewFactory.js
@@ -0,0 +1,18 @@
+wikibase.view.ReadModeViewFactory = ( function( wb ) {
+'use strict';
+
+var PARENT = wb.view.ViewFactory;
+
+var SELF = util.inherit( 'ReadModeViewFactory', PARENT, {} );
+
+SELF.prototype.getStatementGroupListView = function( entity, $statementgrouplistview ) {
+	/* Skip constructing statement views entirely */
+};
+
+SELF.prototype.getSitelinkGroupListView = function( sitelinkSet, $sitelinkgrouplistview ) {
+	/* Skip constructing sitelink views entirely */
+};
+
+return SELF;
+
+}( wikibase ) );
