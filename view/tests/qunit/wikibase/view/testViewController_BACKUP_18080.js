fe7f0a1db0d5e98a6cb28606dae307e21aa54f35
frimelle <lucie.kaffee@wikimedia.de>
2016-09-05 17:20:03 +0200

    Move EntityPerPage class to Lib
    Needed for the new SpecialPage of the ArticlePlaceholder
    
    Change-Id: Ib8a33c672c04f5ffd224975f1be1e8aad52f04c0

---
+++ view/tests/qunit/wikibase/view/testViewController_BACKUP_18080.js
@@ -0,0 +1,66 @@
+wikibase.view.testViewController = ( function( QUnit, wb ) {
+'use strict';
+
+return {
+	all: function( constructor, getInstance ) {
+		this.constructorTests( constructor, getInstance );
+		this.methodTests( getInstance );
+	},
+
+	constructorTests: function( constructor, getInstance ) {
+		QUnit.test( 'implements wb.view.ViewController', function( assert ) {
+			var controller = getInstance();
+
+			assert.ok( controller instanceof constructor );
+			assert.ok( controller instanceof wb.view.ViewController );
+		} );
+	},
+
+	methodTests: function( getInstance ) {
+		QUnit.test( 'has non-abstract startEditing method', function( assert ) {
+			var controller = getInstance();
+
+			controller.startEditing();
+
+			assert.ok( true );
+		} );
+
+		QUnit.test( 'has non-abstract stopEditing method', function( assert ) {
+			var controller = getInstance();
+
+			controller.stopEditing();
+
+			assert.ok( true );
+		} );
+
+		QUnit.test( 'has non-abstract cancelEditing method', function( assert ) {
+			var controller = getInstance();
+
+			controller.cancelEditing();
+
+			assert.ok( true );
+		} );
+
+		QUnit.test( 'has non-abstract setError method', function( assert ) {
+			var controller = getInstance();
+
+			controller.setError();
+
+			assert.ok( true );
+		} );
+<<<<<<< 0c985dc4a5bd4316712cd5777dfe2edfe0bfd6ed
+
+		QUnit.test( 'has non-abstract remove method', function( assert ) {
+			var controller = getInstance();
+
+			controller.remove();
+
+			assert.ok( true );
+		} );
+=======
+>>>>>>> Introduce ViewController
+	}
+
+};
+
+}( QUnit, wikibase ) );
