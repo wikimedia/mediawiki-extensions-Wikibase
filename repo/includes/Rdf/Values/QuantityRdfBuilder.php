f5d3a9c5eeca569b0a79c2d7bdbc74b1bc353a6f
Stanislav Malyshev <smalyshev@wikimedia.org>
2016-09-23 18:52:06 -0700

    Script to produce RDF mappings for new normalized units
    Change-Id: Id3fc3423857d7ad28ab161e6f80a8a7804e6d9cf
    Bug: T145426

--- repo/includes/Rdf/Values/QuantityRdfBuilder.php
+++ repo/includes/Rdf/Values/QuantityRdfBuilder.php
@@ -62,19 +62,80 @@ class QuantityRdfBuilder implements ValueSnakRdfBuilder {
 		//FIXME: this is meaningless without a unit identifier!
 
 		if ( $this->complexValueHelper !== null ) {
-			$this->addValueNode( $writer, $propertyValueNamespace, $propertyValueLName, $dataType, $value );
+			$valueLName = $this->addValueNode(
+				$writer,
+				$propertyValueNamespace,
+				$propertyValueLName,
+				$dataType,
+				$value
+			);
+
+			// Can we convert units? This condition may become more complex in the future,
+			// but should keep checks for all prerequisites being set.
+			// FIXME: make this depend on flavor
+			if ( $valueLName && $this->unitConverter != null ) {
+				$newValue = $this->unitConverter->toStandardUnits( $value );
+
+				if ( $newValue ) {
+					if ( $newValue->equals( $value ) ) {
+						$this->complexValueHelper->attachValueNode(
+							$writer,
+							$propertyValueNamespace,
+							$propertyValueLName,
+							$dataType,
+							$value,
+							true
+						);
+
+						// The unnormalize value is always its own normalization.
+						$this->linkNormalizedValue( $valueLName, $valueLName );
+					} else {
+						$normLName = $this->addValueNode(
+							$writer,
+							$propertyValueNamespace,
+							$propertyValueLName,
+							$dataType,
+							$newValue,
+							true
+						);
+
+						// The normalized value is always its own normalization.
+						$this->linkNormalizedValue( $normLName, $normLName );
+
+						// Connect the normalized value to the unnormalized value
+						$this->linkNormalizedValue( $valueLName, $normLName );
+					}
+				}
+			}
 		}
+	}
+
+	/**
+	 * Connects a normalized value node to its base node via the quantityNormalized predicate.
+	 *
+	 * @param string $valueLName
+	 * @param string $normLName
+	 */
+	private function linkNormalizedValue( $valueLName, $normLName ) {
+		$valueWriter = $this->complexValueHelper->getValueNodeWriter();
+		$valueWriter->about( RdfVocabulary::NS_VALUE, $valueLName )
+			->say( RdfVocabulary::NS_ONTOLOGY, 'quantityNormalized' )
+			->is( RdfVocabulary::NS_VALUE, $normLName );
 	}
 
 	/**
 	 * Adds a value node representing all details of $value
 	 *
-	 * @param RdfWriter     $writer
-	 * @param string        $propertyValueNamespace Property value relation namespace
-	 * @param string        $propertyValueLName Property value relation name
-	 * @param string        $dataType Property data type
+	 * @param RdfWriter $writer
+	 * @param string $propertyValueNamespace Property value relation namespace
+	 * @param string $propertyValueLName Property value relation name
+	 * @param string $dataType Property data type
 	 * @param QuantityValue $value
-	 * @param bool          $normalized Is this a normalized value?
+	 * @param bool $normalized Is this a normalized value?
+	 *
+	 * @return string|null The LName of the value node (in the RdfVocabulary::NS_VALUE namespace),
+	 *  or null if the value node should not be processed (generally, because it already has
+	 *  been processed).
 	 */
 	private function addValueNode(
 		RdfWriter $writer,
@@ -93,15 +154,22 @@ class QuantityRdfBuilder implements ValueSnakRdfBuilder {
 			$normalized
 		);
 
-		if ( $valueLName === null ) {
-			// The value node is already present in the output, don't create it again!
-			return;
+		// If the value node is already present in the output, don't create it again!
+		if ( $valueLName !== null ) {
+			$this->writeQuantityValue( $value );
 		}
 
-		// Can we convert units? This condition may become more complex in the future,
-		// but should keep checks for all prerequisites being set.
-		$convertUnits = ( $this->unitConverter != null );
+		return $valueLName;
+	}
 
+	/**
+	 * Write data for the value.
+	 * This expects the current subject of the RDF writer to be the value node.
+	 * No instance-of statement is written about the value.
+	 *
+	 * @param QuantityValue $value
+	 */
+	public function writeQuantityValue( QuantityValue $value ) {
 		$valueWriter = $this->complexValueHelper->getValueNodeWriter();
 
 		$valueWriter->say( RdfVocabulary::NS_ONTOLOGY, 'quantityAmount' )
@@ -119,31 +187,7 @@ class QuantityRdfBuilder implements ValueSnakRdfBuilder {
 			$unitUri = RdfVocabulary::ONE_ENTITY;
 		}
 
-		$valueWriter->say( RdfVocabulary::NS_ONTOLOGY, 'quantityUnit' )
-			->is( $unitUri );
-
-		if ( $convertUnits && $normalized ) {
-			// Normalized value is it's own normalization
-			$valueWriter->say( RdfVocabulary::NS_ONTOLOGY, 'quantityNormalized' )
-				->is( RdfVocabulary::NS_VALUE, $valueLName );
-		}
-
-		// FIXME: make this depend on flavor
-		// We exclude unitless ones here because same property should not have
-		// both unitless and united quantity, and they are unlikely to be in the same query.
-		if ( !$normalized && $convertUnits && $unitUri !== RdfVocabulary::ONE_ENTITY ) {
-			$newValue = $this->unitConverter->toStandardUnits( $value );
-
-			if ( $newValue ) {
-				// Add normalized node
-				$normLName = $newValue->getHash();
-				$valueWriter->say( RdfVocabulary::NS_ONTOLOGY, 'quantityNormalized' )
-					->is( RdfVocabulary::NS_VALUE, $normLName );
-
-				$this->addValueNode( $writer, $propertyValueNamespace, $propertyValueLName,
-					$dataType, $newValue, true );
-			}
-		}
+		$valueWriter->say( RdfVocabulary::NS_ONTOLOGY, 'quantityUnit' )->is( $unitUri );
 	}
 
 }
