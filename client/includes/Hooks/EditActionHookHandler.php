4eae9d4eecfb5217305da1c225cc2ed5ce3c4d7d
Amir Sarabadani <Ladsgroup@gmail.com>
2016-10-01 02:12:03 +0330

    [WIP] Add EditActionHookHandler in clinet to inject entity usage data
    What needs to be done:
    - Get javascript working
    - CI tests
    
    Bug: T144921
    Change-Id: I1f1fad785545be475f1db8c12d1647ec449a29d7

---
+++ client/includes/Hooks/EditActionHookHandler.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace Wikibase\Client\Hooks;
+
+use EditPage;
+use Html;
+use IContextSource;
+use Wikibase\Client\RepoLinker;
+use Wikibase\Client\Usage\EntityUsage;
+use Wikibase\Client\Usage\UsageLookup;
+use Wikibase\DataModel\Entity\EntityIdParser;
+use Wikibase\Lib\Store\LanguageFallbackLabelDescriptionLookupFactory;
+
+/**
+ * @since 0.5
+ *
+ * @license GPL-2.0+
+ * @author Amir Sarabadani < ladsgroup@gmail.com >
+ */
+class EditActionHookHandler {
+
+	/**
+	 * @var UsageLookup
+	 */
+	private $usageLookup;
+
+	/**
+	 * @var LanguageFallbackLabelDescriptionLookupFactory
+	 */
+	private $labelDescriptionLookupFactory;
+
+	/**
+	 * @var EntityIdParser
+	 */
+	private $idParser;
+
+	/**
+	 * @var IContextSource
+	 */
+	private $context;
+
+	public function __construct(
+		RepoLinker $repoLinker,
+		UsageLookup $usageLookup,
+		LanguageFallbackLabelDescriptionLookupFactory $labelDescriptionLookupFactory,
+		EntityIdParser $idParser,
+		IContextSource $context
+	) {
+		$this->repoLinker = $repoLinker;
+		$this->usageLookup = $usageLookup;
+		$this->labelDescriptionLookupFactory = $labelDescriptionLookupFactory;
+		$this->idParser = $idParser;
+		$this->context = $context;
+	}
+
+	/**
+	 * @param EditPage $editor
+	 */
+	public function handle( EditPage $editor ) {
+		// Check if there is usage to show
+		$title = $editor->getTitle();
+		$usage = $this->usageLookup->getUsagesForPage( $title->getArticleID() );
+
+		if ( $usage ) {
+			$header = $this->getHeader();
+			$usageOutput = $this->formatEntityUsage( $usage );
+			$output = Html::rawElement(
+				'div', [ 'class' => 'wikibase-entity-usage' ],
+				$header . "\n" . $usageOutput
+			);
+			$editor->editFormTextAfterTools .= $output;
+		}
+	}
+
+	/**
+	 * @param string[] $rowAspects
+	 * @param IContextSource $context
+	 *
+	 * @return string HTML
+	 */
+	private function formatAspects( $rowAspects, IContextSource $context ) {
+		$aspects = [];
+
+		foreach ( $rowAspects as $aspect ) {
+			$aspects[] = $context->msg(
+				'wikibase-pageinfo-entity-usage-' . $aspect[0], $aspect[1] )->parse();
+		}
+
+		return $context->getLanguage()->commaList( $aspects );
+	}
+
+	/**
+	 * @param EntityUsage[] $usage
+	 * @return string HTML
+	 */
+	private function formatEntityUsage( array $usage ) {
+		$usageAspectsByEntity = [];
+		$entityIds = [];
+		foreach ( $usage as $key => $entityUsage ) {
+			$entityId = $entityUsage->getEntityId()->getSerialization();
+			$entityIds[$entityId] = $entityUsage->getEntityId();
+			if ( !isset( $usageAspectsByEntity[$entityId] ) ) {
+				$usageAspectsByEntity[$entityId] = [];
+			}
+			$usageAspectsByEntity[$entityId][] = [
+				$entityUsage->getAspect(),
+				$entityUsage->getModifier()
+			];
+		}
+		$output = '';
+		$entityNames = array_map(
+			function( $entityId ) {
+				return $this->idParser->parse( $entityId );
+			},
+			array_keys( $usageAspectsByEntity )
+		);
+		$labelLookup = $this->labelDescriptionLookupFactory->newLabelDescriptionLookup(
+			$this->context->getLanguage(),
+			$entityNames
+		);
+		foreach ( $usageAspectsByEntity as $entityId => $aspects ) {
+			$label = $labelLookup->getLabel( $entityIds[$entityId] );
+			$text = $label === null ? $entityId : $label->getText();
+
+			$aspectContent = $this->formatAspects( $aspects, $this->context );
+
+			$output .= Html::rawElement( 'li', [],
+				$this->repoLinker->buildEntityLink(
+					$entityIds[$entityId],
+					[ 'external' ],
+					$text
+				) . ': ' . $aspectContent
+			);
+		}
+		$output = Html::rawElement( 'ul', [ 'class' => 'mw-editfooter-list mw-collapsible'],
+			$output );
+		return $output;
+	}
+
+	/**
+	 * @return string HTML
+	 */
+	private function getHeader() {
+		$element = Html::rawelement( 'div', [
+			'class' => 'wikibase-entityusage-explanation ' .
+			             'mw-editfooter-toggler'
+		], $this->context->msg( 'wikibase-entityusage-explanation' )->parseAsBlock()
+		);
+		return $element;
+	}
+
+}
