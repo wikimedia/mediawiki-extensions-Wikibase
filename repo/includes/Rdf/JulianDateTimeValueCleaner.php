d6f5080ca43a4a7064c7a7e7eb558141a73bee54
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2016-10-05 11:28:09 +0200

    Cleaner JulianDateTimeValueCleaner implementation
    This is mostly, but not exclusively refactoring. This also adds a test
    and fixes an actual bug: The base class never checked if it is ok to
    convert something that is not a Gregorian date. Note that this edge case
    is currently not tested because of the "$calendar ===
    TimeValue::CALENDAR_GREGORIAN" in the tests. But I did not wanted to make
    this patch bigger.
    
    Bug: T146356
    Change-Id: Ib23582019a567310dfb87fbd1b51c17999bacc0f

--- repo/includes/Rdf/JulianDateTimeValueCleaner.php
+++ repo/includes/Rdf/JulianDateTimeValueCleaner.php
@@ -13,6 +13,7 @@ use DataValues\TimeValue;
  *
  * @license GPL-2.0+
  * @author Stas Malyshev
+ * @author Thiemo Mättig
  */
 class JulianDateTimeValueCleaner extends DateTimeValueCleaner {
 
@@ -22,57 +23,54 @@ class JulianDateTimeValueCleaner extends DateTimeValueCleaner {
 	 *
 	 * @param TimeValue $value
 	 *
-	 * @return string|null
+	 * @return string|null Value compatible with xsd:dateTime type, null if the input is an illegal
+	 *  XSD 1.0 timestamp
 	 */
 	public function getStandardValue( TimeValue $value ) {
-		$calendar = $value->getCalendarModel();
-		if ( $calendar == TimeValue::CALENDAR_GREGORIAN ) {
-			return $this->cleanupGregorianValue( $value->getTime(), $value->getPrecision() );
-		} elseif ( $calendar == TimeValue::CALENDAR_JULIAN ) {
-			$precision = $value->getPrecision();
+		try {
 			// If we are less precise than a day, no point to convert
 			// Julian to Gregorian since we don't have enough information to do it anyway
-			if ( $precision >= TimeValue::PRECISION_DAY ) {
-				return $this->julianDateValue( $value->getTime(), $precision );
-			} else {
-				return $this->cleanupGregorianValue( $value->getTime(), $precision );
+			if ( $value->getCalendarModel() === TimeValue::CALENDAR_JULIAN
+				&& $value->getPrecision() >= TimeValue::PRECISION_DAY
+			) {
+				// There is a certain range of years PHP can convert; assume everything else was a
+				// mistake and meant to be a Gregorian date
+				return $this->julianDateValue( $value->getTime() )
+					?: $this->cleanupGregorianValue( $value->getTime(), $value->getPrecision() );
 			}
+		} catch ( IllegalValueException $e ) {
+			return null;
 		}
-		return null;
+
+		return parent::getStandardValue( $value );
 	}
 
 	/**
 	 * Get Julian date value and return it as Gregorian date
 	 *
 	 * @param string $dateValue
-	 * @param int $precision
 	 *
-	 * @return string|null Value compatible with xsd:dateTime type, null if we failed to parse
+	 * @throws IllegalValueException if the input is an illegal XSD 1.0 timestamp
+	 * @return string|null Value compatible with xsd:dateTime type, null if conversion is not
+	 *  possible
 	 */
-	private function julianDateValue( $dateValue, $precision ) {
-		try {
-			list( $minus, $y, $m, $d, $time ) = $this->parseDateValue( $dateValue );
-		} catch ( IllegalValueException $e ) {
-			return null;
-		}
+	private function julianDateValue( $dateValue ) {
+		list( $minus, $y, $m, $d, $time ) = $this->parseDateValue( $dateValue );
 
 		$y = $minus ? -$y : $y + 0;
-		if ( !is_int( $y ) || $y < -4713 ) {
-			// Fall back to Gregorian when Julian to Gregorian conversion is not possible.
-			return $this->cleanupGregorianValue( $dateValue, $precision );
+		// There are some weird overflows happening in the PHP functions before and after this year.
+		if ( !is_int( $y ) || $y < -4713 || $y > 1465072 ) {
+			return null;
 		}
 
 		$jd = juliantojd( $m, $d, $y );
 		if ( $jd == 0 ) {
-			// Fall back to Gregorian when Julian to Gregorian conversion is not possible.
-			return $this->cleanupGregorianValue( $dateValue, $precision );
+			return null;
 		}
 
-		// PHP API for Julian/Gregorian conversions is kind of awful
 		$gregorian = jdtogregorian( $jd );
 		if ( $gregorian === '0/0/0' ) {
-			// Fall back to Gregorian when Julian to Gregorian conversion is not possible.
-			return $this->cleanupGregorianValue( $dateValue, $precision );
+			return null;
 		}
 
 		list( $m, $d, $y ) = explode( '/', $gregorian );
