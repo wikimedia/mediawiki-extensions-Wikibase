wikimedia/mediawiki-extensions-Wikibase

commit 4f5fd0e02071c6d7a58786ea4aedbcca60271b5c

Drop wbc_entity_usage.eu_touched
No longer needed after T125838 has been implemented and
usage tracking without eu_touched proved to work out.

Manually tested with MySQL.

Bug: T132628
Change-Id: I765924489838a36ffa869e0227e120e65def78b4

client/includes/Usage/Sql/SqlUsageTrackerSchemaUpdater.php
@@ -65,11 +65,11 @@ class SqlUsageTrackerSchemaUpdater {
 				$this->dbUpdater->modifyExtensionField( $table, 'eu_aspect', $script );
 			}
 
-			$script = $this->getUpdateScriptPath( 'entity_usage-add-touched', $db->getType() );
-			$this->dbUpdater->addExtensionField( $table, 'eu_touched', $script );
-
 			$script = $this->getUpdateScriptPath( 'entity_usage-drop-entity_type', $db->getType() );
 			$this->dbUpdater->dropExtensionField( $table, 'eu_entity_type', $script );
+
+			$script = $this->getUpdateScriptPath( 'entity_usage-drop-touched', $db->getType() );
+			$this->dbUpdater->dropExtensionField( $table, 'eu_touched', $script );
 		}
 	}
 

client/sql/entity_usage-add-touched.sql
@@ -1,3 +1,0 @@
--- Add a column for tracking page.page_touched, to detect outdated entries in wbc_entity_usage.
-ALTER TABLE /*_*/wbc_entity_usage
-  ADD COLUMN eu_touched BINARY(14) NOT NULL DEFAULT '';

client/sql/entity_usage-drop-touched.sql
@@ -1,0 +1,4 @@
+-- get rid of the eu_touched column
+
+ALTER TABLE /*_*/wbc_entity_usage
+  DROP COLUMN eu_touched;

client/sql/entity_usage.sql
@@ -2,8 +2,7 @@ CREATE TABLE IF NOT EXISTS /*_*/wbc_entity_usage (
   eu_row_id         BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   eu_entity_id      VARBINARY(255) NOT NULL, -- the ID of the entity being used
   eu_aspect         VARBINARY(37) NOT NULL,  -- the aspect of the entity. See EntityUsage::XXX_USAGE for possible values.
-  eu_page_id        INT NOT NULL,            -- the ID of the page that uses the entities.
-  eu_touched        BINARY(14) NOT NULL DEFAULT '' -- timestamp corresponding to page.page_touched
+  eu_page_id        INT NOT NULL             -- the ID of the page that uses the entities.
 ) /*$wgDBTableOptions*/;
 
 -- record one usage per page per aspect of an entity

docs/usagetracking.wiki
@@ -38,7 +38,6 @@ as a links table, just like templatelinks or imagelinks. It has the following fi
   eu_entity_id      VARBINARY(255) NOT NULL  -- the ID of the entity being used
   eu_aspect         VARBINARY(37) NOT NULL   -- the aspect of the entity. See EntityUsage::XXX_USAGE for possible values.
   eu_page_id        INT NOT NULL             -- the ID of the page that uses the entities.
-  eu_touched        BINARY(14) NOT NULL DEFAULT '' -- timestamp corresponding to page.page_touched (currently unused)
 
 The following indexes are provided for efficient access:
 

commit 5e158a5aa0878edb6bed40d9d5b4389ec7540a1a

Special handling for dropping eu_touched on SQLite
4f5fd0e02071c6d7a58786ea4aedbcca60271b5c broke sqlite support.

This patch fixes the support.

sqlite does not support any drop references so you need to work around to
drop columbs in sqlite per
http://stackoverflow.com/questions/5938048/delete-column-from-sqlite-table

Bug: T147300
Change-Id: Ia56db833f4e11adeb8a932d6b7d05056eba6a042

client/includes/Usage/Sql/SqlUsageTrackerSchemaUpdater.php
@@ -68,8 +68,13 @@ class SqlUsageTrackerSchemaUpdater {
 			$script = $this->getUpdateScriptPath( 'entity_usage-drop-entity_type', $db->getType() );
 			$this->dbUpdater->dropExtensionField( $table, 'eu_entity_type', $script );
 
-			$script = $this->getUpdateScriptPath( 'entity_usage-drop-touched', $db->getType() );
-			$this->dbUpdater->dropExtensionField( $table, 'eu_touched', $script );
+			if ( $db->getType() === 'sqlite' ) {
+				$script = $this->getUpdateScriptPath( 'entity_usage-drop-touched.sqlite', $db->getType() );
+				$this->dbUpdater->dropExtensionField( $table, 'eu_touched', $script );
+			} else {
+				$script = $this->getUpdateScriptPath( 'entity_usage-drop-touched', $db->getType() );
+				$this->dbUpdater->dropExtensionField( $table, 'eu_touched', $script );
+			}
 		}
 	}
 

client/sql/entity_usage-drop-touched.sqlite.sql
@@ -1,0 +1,24 @@
+-- get rid of the eu_touched column
+
+-- Workaround for sqlite - T147300
+
+CREATE TABLE IF NOT EXISTS /*_*/wbc_entity_usage_tmp (
+  eu_row_id         BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  eu_entity_id      VARBINARY(255) NOT NULL, -- the ID of the entity being used
+  eu_aspect         VARBINARY(37) NOT NULL,  -- the aspect of the entity. See EntityUsage::XXX_USAGE for possible values.
+  eu_page_id        INT NOT NULL             -- the ID of the page that uses the entities.
+) /*$wgDBTableOptions*/;
+
+INSERT INTO /*_*/wbc_entity_usage_tmp
+  SELECT eu_row_id, eu_entity_id, eu_aspect, eu_page_id
+  FROM /*_*/wbc_entity_usage;
+
+DROP TABLE /*_*/wbc_entity_usage;
+
+ALTER TABLE /*_*/wbc_entity_usage_tmp RENAME TO /*_*/wbc_entity_usage;
+
+-- record one usage per page per aspect of an entity
+CREATE UNIQUE INDEX /*i*/eu_entity_id ON /*_*/wbc_entity_usage ( eu_entity_id, eu_aspect, eu_page_id );
+
+-- look up (and especially, delete) usage entries by page id
+CREATE INDEX /*i*/eu_page_id ON /*_*/wbc_entity_usage ( eu_page_id, eu_entity_id ) ;