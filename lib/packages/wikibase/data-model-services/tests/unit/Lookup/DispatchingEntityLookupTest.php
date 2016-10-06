4c52f34f4a10c21e07e1812b1d2442bfe7004a70
Leszek Manicki <leszek.manicki@wikimedia.de>
2016-10-06 14:20:50 +0200

    Add DispatchingEntityLookup
    DispatchingEntityLookup delegates lookup
    to the repository-specific EntityLookup
    based on the name of the repository an EntityId belongs to.

---
+++ lib/packages/wikibase/data-model-services/tests/unit/Lookup/DispatchingEntityLookupTest.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace Wikibase\DataModel\Services\Tests\Lookup;
+
+use Exception;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\DataModel\Services\Fixtures\FakeEntityDocument;
+use Wikibase\DataModel\Services\Lookup\DispatchingEntityLookup;
+use Wikibase\DataModel\Services\Lookup\EntityLookup;
+use Wikibase\DataModel\Services\Lookup\EntityLookupException;
+use Wikibase\DataModel\Services\Lookup\InMemoryEntityLookup;
+use Wikibase\DataModel\Services\Lookup\UnknownForeignRepositoryException;
+use Wikimedia\Assert\ParameterAssertionException;
+
+/**
+ * @covers Wikibase\DataModel\Services\Lookup\DispatchingEntityLookup
+ *
+ * @license GPL-2.0+
+ */
+class DispatchingEntityLookupTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * @dataProvider provideInvalidForeignLookups
+	 */
+	public function testGivenInvalidForeignLookups_exceptionIsThrown( array $lookups ) {
+		$this->setExpectedException( ParameterAssertionException::class );
+		new DispatchingEntityLookup( $lookups );
+	}
+
+	public function provideInvalidForeignLookups() {
+		return [
+			'no lookups given' => [ [] ],
+			'not an implementation of EntityLookup given as a lookup' => [
+				[ '' => new ItemId( 'Q123' ) ],
+			],
+			'non-string keys' => [
+				[ '' => new InMemoryEntityLookup(), 100 => new InMemoryEntityLookup(), ],
+			],
+			'repo name containing colon' => [
+				[ '' => new InMemoryEntityLookup(),	'fo:oo' => new InMemoryEntityLookup(), ],
+			],
+		];
+	}
+
+	public function testGivenExistingEntityId_getEntityReturnsTheEntity() {
+		$localLookup = new InMemoryEntityLookup();
+		$localLookup->addEntity( new FakeEntityDocument( new ItemId( 'Q1' ) ) );
+		$fooLookup = new InMemoryEntityLookup();
+		$fooLookup->addEntity( new FakeEntityDocument( new PropertyId( 'foo:P11' ) ) );
+
+		$dispatchingLookup = new DispatchingEntityLookup( [ '' => $localLookup, 'foo' => $fooLookup ] );
+
+		$expected = new FakeEntityDocument( new ItemId( 'Q1' ) );
+		$actual = $dispatchingLookup->getEntity( new ItemId( 'Q1' ) );
+		$this->assertTrue( $actual->equals( $expected ) );
+		$this->assertTrue( $actual->getId()->equals( new ItemId( 'Q1' ) ) );
+
+		$expected = new FakeEntityDocument( new PropertyId( 'foo:P11' ) );
+		$actual = $dispatchingLookup->getEntity( new PropertyId( 'foo:P11' ) );
+		$this->assertTrue( $actual->equals( $expected ) );
+		$this->assertTrue( $actual->getId()->equals( new PropertyId( 'foo:P11' ) ) );
+	}
+
+	public function testGivenNotExistingEntityIdFromKnownRepository_getEntityReturnsNull() {
+		$localLookup = new InMemoryEntityLookup();
+		$fooLookup = new InMemoryEntityLookup();
+		$dispatchingLookup = new DispatchingEntityLookup( [ '' => $localLookup, 'foo' => $fooLookup ] );
+		$this->assertNull( $dispatchingLookup->getEntity( new ItemId( 'Q1' ) ) );
+		$this->assertNull( $dispatchingLookup->getEntity( new ItemId( 'foo:Q19' ) ) );
+	}
+
+	/**
+	 * @param Exception $exception
+	 * @return \PHPUnit_Framework_MockObject_MockObject|EntityLookup
+	 */
+	private function getExceptionThrowingLookup( Exception $exception ) {
+		$lookup = $this->getMock( EntityLookup::class );
+		$lookup->expects( $this->any() )
+			->method( $this->anything() )
+			->willThrowException( $exception );
+		return $lookup;
+	}
+
+	public function testLookupExceptionsAreNotCaughtInGetEntity() {
+		$lookup = $this->getExceptionThrowingLookup( new EntityLookupException( new ItemId( 'Q321' ) ) );
+
+		$dispatchingLookup = new DispatchingEntityLookup( [ '' => $lookup ] );
+
+		$this->setExpectedException( EntityLookupException::class );
+		$dispatchingLookup->getEntity( new ItemId( 'Q321' ) );
+	}
+
+	public function testGivenEntityIdFromUnknownRepository_getEntityThrowsException() {
+		$dispatchingLookup = new DispatchingEntityLookup( [ '' => $this->getMock( EntityLookup::class ), ] );
+
+		$this->setExpectedException( UnknownForeignRepositoryException::class );
+
+		$dispatchingLookup->getEntity( new ItemId( 'foo:Q1' ) );
+	}
+
+	public function testGivenExistingEntityId_hasEntityReturnsTrue() {
+		$localLookup = new InMemoryEntityLookup();
+		$localLookup->addEntity( new FakeEntityDocument( new ItemId( 'Q1' ) ) );
+		$fooLookup = new InMemoryEntityLookup();
+		$fooLookup->addEntity( new FakeEntityDocument( new PropertyId( 'foo:P11' ) ) );
+
+		$dispatchingLookup = new DispatchingEntityLookup( [ '' => $localLookup, 'foo' => $fooLookup ] );
+
+		$this->assertTrue( $dispatchingLookup->hasEntity( new ItemId( 'Q1' ) ) );
+		$this->assertTrue( $dispatchingLookup->hasEntity( new PropertyId( 'foo:P11' ) ) );
+	}
+
+	public function testGivenNotExistingEntityIdFromKnownRepository_getEntityReturnsFalse() {
+		$localLookup = new InMemoryEntityLookup();
+		$fooLookup = new InMemoryEntityLookup();
+
+		$dispatchingLookup = new DispatchingEntityLookup( [ '' => $localLookup, 'foo' => $fooLookup ] );
+
+		$this->assertFalse( $dispatchingLookup->hasEntity( new ItemId( 'Q1' ) ) );
+		$this->assertFalse( $dispatchingLookup->hasEntity( new ItemId( 'foo:Q19' ) ) );
+	}
+
+	public function testLookupExceptionsAreNotCaughtInHasEntity() {
+		$lookup = $this->getExceptionThrowingLookup( new EntityLookupException( new ItemId( 'Q321' ) ) );
+
+		$dispatchingLookup = new DispatchingEntityLookup( [ '' => $lookup ] );
+
+		$this->setExpectedException( EntityLookupException::class );
+		$dispatchingLookup->hasEntity( new ItemId( 'Q321' ) );
+	}
+
+	public function testGivenEntityIdFromUnknownRepository_hasEntityThrowsException() {
+		$dispatchingLookup = new DispatchingEntityLookup( [ '' => $this->getMock( EntityLookup::class ), ] );
+
+		$this->setExpectedException( UnknownForeignRepositoryException::class );
+
+		$dispatchingLookup->hasEntity( new ItemId( 'foo:Q1' ) );
+	}
+
+}
