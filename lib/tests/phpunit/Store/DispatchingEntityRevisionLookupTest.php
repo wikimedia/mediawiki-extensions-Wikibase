6b65025365504339af35e3a34e55cd9e62b19016
Leszek Manicki <leszek.manicki@wikimedia.de>
2016-10-07 14:10:21 +0200

    Add DispatchingEntityRevisionLookup
    DispatchingEntityLookup picks the right EntityRevisionLookup
    object to perform the lookup depending on the repository
    the input EntityId object belongs to.
    
    Bug: T146990
    Change-Id: Idd3efea6ec1fbd2df3c13a91db8e11ced12097ae

---
+++ lib/tests/phpunit/Store/DispatchingEntityRevisionLookupTest.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace Wikibase\Lib\Tests\Store;
+
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\EntityRevision;
+use Wikibase\Lib\Store\DispatchingEntityRevisionLookup;
+use Wikibase\Lib\Store\EntityRevisionLookup;
+use Wikibase\Lib\Store\StorageException;
+use Wikimedia\Assert\ParameterAssertionException;
+
+/**
+ * @covers Wikibase\Lib\Store\DispatchingEntityRevisionLookup
+ *
+ * @group WikibaseLib
+ * @group WikibaseStore
+ * @group Wikibase
+ *
+ * @license GPL-2.0+
+ */
+class DispatchingEntityRevisionLookupTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * @return \PHPUnit_Framework_MockObject_MockObject|EntityRevisionLookup
+	 */
+	private function getDummyEntityRevisionLookup() {
+		return $this->getMock( EntityRevisionLookup::class );
+	}
+
+	public function testGivenExistingRevision_getEntityRevisionReturnsIt() {
+		$itemId = new ItemId( 'Q123' );
+		$item = new Item( $itemId );
+
+		$localLookup = $this->getDummyEntityRevisionLookup();
+		$localLookup->expects( $this->any() )
+			->method( 'getEntityRevision' )
+			->with( $itemId )
+			->willReturn( new EntityRevision( $item, 123 ) );
+
+		$foreignItemId = new ItemId( 'foo:Q303' );
+		$foreignItem = new Item( $foreignItemId );
+
+		$fooLookup = $this->getDummyEntityRevisionLookup();
+		$fooLookup->expects( $this->any() )
+			->method( 'getEntityRevision' )
+			->with( $foreignItemId )
+			->willReturn( new EntityRevision( $foreignItem, 100 ) );
+
+		$dispatchingLookup = new DispatchingEntityRevisionLookup(
+			array( '' => $localLookup, 'foo' => $fooLookup, )
+		);
+
+		$revision = $dispatchingLookup->getEntityRevision( new ItemId( 'Q123' ) );
+		$foreignRevision = $dispatchingLookup->getEntityRevision( new ItemId( 'foo:Q303' ) );
+
+		$this->assertTrue( $revision->getEntity()->equals( $item ) );
+		$this->assertTrue( $revision->getEntity()->getId()->equals( $itemId ) );
+		$this->assertEquals( 123, $revision->getRevisionId() );
+
+		$this->assertTrue( $foreignRevision->getEntity()->equals( $foreignItem ) );
+		$this->assertTrue( $foreignRevision->getEntity()->getId()->equals( $foreignItemId ) );
+		$this->assertEquals( 100, $foreignRevision->getRevisionId() );
+	}
+
+	public function testGivenNotExistingEntityIdFromKnownRepository_getEntityRevisionReturnsNull() {
+		$localLookup = $this->getDummyEntityRevisionLookup();
+		$localLookup->expects( $this->any() )
+			->method( 'getEntityRevision' )
+			->with( $this->anything() )
+			->willReturn( null );
+
+		$fooLookup = $this->getDummyEntityRevisionLookup();
+		$fooLookup->expects( $this->any() )
+			->method( 'getEntityRevision' )
+			->with( $this->anything() )
+			->willReturn( null );
+
+		$dispatchingLookup = new DispatchingEntityRevisionLookup(
+			array( '' => $localLookup, 'foo' => $fooLookup, )
+		);
+
+		$this->assertNull( $dispatchingLookup->getEntityRevision( new ItemId( 'Q124' ) ) );
+		$this->assertNull( $dispatchingLookup->getEntityRevision( new ItemId( 'foo:Q808' ) ) );
+	}
+
+	public function testGivenForeignEntityFromUnknownRepository_getEntityRevisionReturnsNull() {
+		$localLookup = $this->getDummyEntityRevisionLookup();
+		$localLookup->expects( $this->never() )->method( 'getEntityRevision' );
+
+		$dispatchingLookup = new DispatchingEntityRevisionLookup( array( '' => $localLookup, ) );
+
+		$this->assertNull( $dispatchingLookup->getEntityRevision( new ItemId( 'foo:Q123' ) ) );
+	}
+
+	public function testGivenExistingEntityId_getLatestRevisionIdReturnsTheId() {
+		$localLookup = $this->getDummyEntityRevisionLookup();
+		$localLookup->expects( $this->any() )
+			->method( 'getLatestRevisionId' )
+			->with( new ItemId( 'Q123' ) )
+			->willReturn( 123 );
+
+		$fooLookup = $this->getDummyEntityRevisionLookup();
+		$fooLookup->expects( $this->any() )
+			->method( 'getLatestRevisionId' )
+			->with( new ItemId( 'foo:Q303' ) )
+			->willReturn( 100 );
+
+		$dispatchingLookup = new DispatchingEntityRevisionLookup(
+			array( '' => $localLookup, 'foo' => $fooLookup, )
+		);
+
+		$this->assertEquals( 123, $dispatchingLookup->getLatestRevisionId( new ItemId( 'Q123' ) ) );
+		$this->assertEquals( 100, $dispatchingLookup->getLatestRevisionId( new ItemId( 'foo:Q303' ) ) );
+	}
+
+	public function testGivenNotExistingEntityIdFromKnownRepository_getLatestRevisionIdReturnsFalse() {
+		$localLookup = $this->getDummyEntityRevisionLookup();
+		$localLookup->expects( $this->any() )
+			->method( 'getLatestRevisionId' )
+			->with( $this->anything() )
+			->willReturn( false );
+
+		$fooLookup = $this->getDummyEntityRevisionLookup();
+		$fooLookup->expects( $this->any() )
+			->method( 'getLatestRevisionId' )
+			->with( $this->anything() )
+			->willReturn( false );
+
+		$dispatchingLookup = new DispatchingEntityRevisionLookup(
+			array( '' => $localLookup, 'foo' => $fooLookup, )
+		);
+
+		$this->assertFalse( $dispatchingLookup->getLatestRevisionId( new ItemId( 'Q123' ) ) );
+		$this->assertFalse( $dispatchingLookup->getLatestRevisionId( new ItemId( 'foo:Q303' ) ) );
+	}
+
+	public function testGivenForeignEntityFromUnknownRepository_getLatestRevisionIdReturnsFalse() {
+		$localLookup = $this->getDummyEntityRevisionLookup();
+		$localLookup->expects( $this->never() )->method( 'getLatestRevisionId' );
+
+		$dispatchingLookup = new DispatchingEntityRevisionLookup( array( '' => $localLookup, ) );
+
+		$this->assertFalse( $dispatchingLookup->getLatestRevisionId( new ItemId( 'foo:Q123' ) ) );
+	}
+
+	public function testLookupExceptionsAreNotCaught() {
+		$localLookup = $this->getDummyEntityRevisionLookup();
+		$localLookup->expects( $this->any() )
+			->method( $this->anything() )
+			->willThrowException( new StorageException( 'No such revision for entity Q123: 124' ) );
+
+		$dispatchingLookup = new DispatchingEntityRevisionLookup( array( '' => $localLookup, ) );
+
+		$this->setExpectedException( StorageException::class );
+		$dispatchingLookup->getEntityRevision( new ItemId( 'Q123' ), 124 );
+	}
+
+	/**
+	 * @dataProvider provideInvalidForeignLookups
+	 */
+	public function testGivenInvalidForeignLookups_exceptionIsThrown( array $lookups ) {
+		$this->setExpectedException( ParameterAssertionException::class );
+		new DispatchingEntityRevisionLookup( $lookups );
+	}
+
+	public function provideInvalidForeignLookups() {
+		return array(
+			'no lookups given' => array( array() ),
+			'not an implementation of EntityRevisionLookup given as a lookup' => array(
+				array( '' => new ItemId( 'Q123' ) ),
+			),
+			'non-string keys' => array(
+				array(
+					'' => $this->getDummyEntityRevisionLookup(),
+					100 => $this->getDummyEntityRevisionLookup(),
+				),
+			),
+			'repo name containing colon' => array(
+				array(
+					'' => $this->getDummyEntityRevisionLookup(),
+					'fo:oo' => $this->getDummyEntityRevisionLookup(),
+				),
+			),
+		);
+	}
+
+}
