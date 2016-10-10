d9c8988c8f7274fb1c62bdd3a555d8f5a4af21e1
Marius Hoch <hoo@online.de>
2016-10-10 17:15:41 +0200

    Re-Implement SqlEntityIdPager without wb_entity_per_page
    Bug: T140888
    Change-Id: Ie3efe0c07bee0dc23cd96a15857f864f2f8b3dab

--- repo/includes/Store/Sql/SqlEntityIdPager.php
+++ repo/includes/Store/Sql/SqlEntityIdPager.php
@@ -3,10 +3,13 @@
 namespace Wikibase\Repo\Store\Sql;
 
 use InvalidArgumentException;
+use ResultWrapper;
 use Wikibase\DataModel\Entity\EntityId;
-use Wikibase\DataModel\Entity\Int32EntityId;
-use Wikibase\Lib\EntityIdComposer;
+use Wikibase\DataModel\Entity\EntityIdParser;
+use Wikibase\DataModel\Entity\EntityIdParsingException;
+use Wikibase\Lib\Store\EntityNamespaceLookup;
 use Wikibase\Repo\Store\EntityIdPager;
+use Wikimedia\Assert\Assert;
 
 /**
  * SqlEntityIdPager is a cursor for iterating over the EntityIds stored in
@@ -19,9 +22,14 @@ use Wikibase\Repo\Store\EntityIdPager;
 class SqlEntityIdPager implements EntityIdPager {
 
 	/**
-	 * @var EntityIdComposer
+	 * @var EntityNamespaceLookup
 	 */
-	private $entityIdComposer;
+	private $entityNamespaceLookup;
+
+	/**
+	 * @var EntityIdParser
+	 */
+	private $entityIdParser;
 
 	/**
 	 * @var string|null
@@ -29,26 +37,33 @@ class SqlEntityIdPager implements EntityIdPager {
 	private $entityType;
 
 	/**
-	 * @var EntityId|null
-	 */
-	private $position = null;
-
-	/**
 	 * @var string
 	 */
 	private $redirectMode;
 
 	/**
-	 * @param EntityIdComposer $entityIdComposer
+	 * Last page_id selected.
+	 *
+	 * @var int
+	 */
+	private $position = 0;
+
+	/**
+	 * @param EntityNamespaceLookup $entityNamespaceLookup
+	 * @param EntityIdParser $entityIdParser
 	 * @param null|string $entityType The desired entity type, or null for any type.
 	 * @param string $redirectMode A EntityIdPager::XXX_REDIRECTS constant (default is NO_REDIRECTS).
 	 */
 	public function __construct(
-		EntityIdComposer $entityIdComposer,
+		EntityNamespaceLookup $entityNamespaceLookup,
+		EntityIdParser $entityIdParser,
 		$entityType = null,
 		$redirectMode = EntityIdPager::NO_REDIRECTS
 	) {
-		$this->entityIdComposer = $entityIdComposer;
+		Assert::parameterType( 'string|null', $entityType, '$entityType' );
+
+		$this->entityNamespaceLookup = $entityNamespaceLookup;
+		$this->entityIdParser = $entityIdParser;
 		$this->entityType = $entityType;
 		$this->redirectMode = $redirectMode;
 	}
@@ -69,99 +84,64 @@ class SqlEntityIdPager implements EntityIdPager {
 	 * be empty if there are no more entities to list from the given offset.
 	 */
 	public function fetchIds( $limit ) {
-		$ids = $this->listEntities( $this->entityType, $limit, $this->position, $this->redirectMode );
+		Assert::parameter( is_int( $limit ) && $limit > 0, '$limit', '$limit must be a positive integer' );
 
-		if ( !empty( $ids ) ) {
-			$this->position = end( $ids );
-			reset( $ids );
-		}
+		$dbr = wfGetDB( DB_REPLICA );
+		$rows = $dbr->select(
+			'page',
+			[ 'page_id', 'page_title' ],
+			$this->getWhere(),
+			__METHOD__,
+			[
+				'ORDER BY' => 'page_id ASC',
+				'LIMIT' => $limit
+			]
+		);
 
-		return $ids;
+		return $this->getEntityIdsFromRows( $rows );
 	}
 
 	/**
-	 * @param null|string $entityType The entity type to look for.
-	 * @param int $limit The maximum number of IDs to return.
-	 * @param EntityId|null $after Only return entities with IDs greater than this.
-	 * @param string $redirects A XXX_REDIRECTS constant (default is NO_REDIRECTS).
-	 *
-	 * @throws InvalidArgumentException
-	 * @return EntityId[]
+	 * @return array
 	 */
-	private function listEntities( $entityType, $limit, EntityId $after = null, $redirects = self::NO_REDIRECTS ) {
-		if ( $entityType === null ) {
-			$where = array();
-			//NOTE: needs to be id/type, not type/id, according to the definition of the relevant
-			//      index in wikibase.sql: wb_entity_per_page (epp_entity_id, epp_entity_type);
-			$orderBy = array( 'epp_entity_id', 'epp_entity_type' );
-		} elseif ( !is_string( $entityType ) ) {
-			throw new InvalidArgumentException( '$entityType must be a string (or null)' );
+	private function getWhere() {
+		$where = [ 'page_id > ' . (int)$this->position ];
+
+		if ( $this->entityType === null ) {
+			$where['page_namespace'] = $this->entityNamespaceLookup->getEntityNamespaces();
 		} else {
-			$where = array( 'epp_entity_type' => $entityType );
-			// NOTE: If the type is fixed, don't use the type in the order;
-			// before changing this, check index usage.
-			$orderBy = array( 'epp_entity_id' );
+			$where['page_namespace'] = $this->entityNamespaceLookup->getEntityNamespace(
+				$this->entityType
+			);
 		}
 
-		if ( $redirects === self::NO_REDIRECTS ) {
-			$where[] = 'epp_redirect_target IS NULL';
-		} elseif ( $redirects === self::ONLY_REDIRECTS ) {
-			$where[] = 'epp_redirect_target IS NOT NULL';
+		if ( $this->redirectMode === self::NO_REDIRECTS ) {
+			$where['page_is_redirect'] = 0;
+		} elseif ( $this->redirectMode === self::ONLY_REDIRECTS ) {
+			$where['page_is_redirect'] = 1;
 		}
 
-		if ( !is_int( $limit ) || $limit < 1 ) {
-			throw new InvalidArgumentException( '$limit must be a positive integer' );
-		}
-
-		$dbr = wfGetDB( DB_REPLICA );
-
-		if ( $after ) {
-			if ( !( $after instanceof Int32EntityId ) ) {
-				throw new InvalidArgumentException( '$after must be an Int32EntityId' );
-			}
-
-			$numericId = (int)$after->getNumericId();
-
-			if ( $entityType === null ) {
-				// Ugly. About time we switch to qualified, string based IDs!
-				// NOTE: this must be consistent with the sort order, see above!
-				$where[] = '( ( epp_entity_type > ' . $dbr->addQuotes( $after->getEntityType() )
-					. ' AND epp_entity_id = ' . $numericId . ' )'
-					. ' OR epp_entity_id > ' . $numericId . ' )';
-			} else {
-				$where[] = 'epp_entity_id > ' . $numericId;
-			}
-		}
-
-		$rows = $dbr->select(
-			'wb_entity_per_page',
-			array( 'entity_type' => 'epp_entity_type', 'entity_id' => 'epp_entity_id' ),
-			$where,
-			__METHOD__,
-			array(
-				'ORDER BY' => $orderBy,
-				// MySQL tends to use the epp_redirect_target key which has a very low selectivity
-				'USE INDEX' => 'wb_epp_entity',
-				'LIMIT' => $limit
-			)
-		);
-
-		$ids = $this->getEntityIdsFromRows( $rows );
-		return $ids;
+		return $where;
 	}
 
+	/**
+	 * @param ResultWrapper $rows
+	 *
+	 * @return EntityId[]
+	 */
 	private function getEntityIdsFromRows( $rows ) {
-		$entities = array();
+		$entityIds = [];
 
 		foreach ( $rows as $row ) {
 			try {
-				$entities[] = $this->entityIdComposer->composeEntityId( $row->entity_type, $row->entity_id );
-			} catch ( InvalidArgumentException $ex ) {
-				wfLogWarning( 'Unsupported entity type "' . $row->entity_type . '"' );
+				$this->position = (int)$row->page_id;
+				$entityIds[] = $this->entityIdParser->parse( $row->page_title );
+			} catch ( EntityIdParsingException $ex ) {
+				wfLogWarning( 'Unexpected entity id serialization "' . $row->page_title . '"' );
 			}
 		}
 
-		return $entities;
+		return $entityIds;
 	}
 
 }
