83467ca92383b1d41c12ac28364d246bc0849d17
Leszek Manicki <leszek.manicki@wikimedia.de>
2016-10-12 13:24:44 +0200

    [DNM] Integration test for Ib23a348034acc8f8d072d96b36dafb9717c1c421
    Change-Id: I44647dd1804b43bc497909e8f7f3cfee38d323e3

--- client/tests/phpunit/includes/Specials/SpecialEntityUsageTest.php
+++ client/tests/phpunit/includes/Specials/SpecialEntityUsageTest.php
@@ -6,7 +6,9 @@ use FakeResultWrapper;
 use RequestContext;
 use SpecialPageFactory;
 use SpecialPageTestBase;
+use Title;
 use Wikibase\Client\Specials\SpecialEntityUsage;
+use WikiPage;
 
 /**
  * @covers Wikibase\Client\Specials\SpecialEntityUsage
@@ -21,50 +23,96 @@ use Wikibase\Client\Specials\SpecialEntityUsage;
  */
 class SpecialEntityUsageTest extends SpecialPageTestBase {
 
-	public function reallyDoQueryMock() {
-		$rows = [
-			(object)[
-				'value' => 11,
-				'namespace' => 0,
-				'title' => 'Tehran',
-				'aspects' => 'S|O|L.fa',
-				'eu_page_id' => 11,
-				'eu_entity_id' => 'Q3',
+	protected function setUp() {
+		$this->tablesUsed = array_unique(
+			array_merge( $this->tablesUsed, [ 'page', 'wbc_entity_usage' ] )
+		);
+		parent::setUp();
+	}
+
+	private function setupEntityUsageFixtures() {
+		$db = wfGetDB( DB_MASTER );
+		$pages = [
+			[
+				'page_title' => 'Vienna',
+				'page_namespace' => 0,
+				'page_id' => 11,
+			],
+			[
+				'page_title' => 'Berlin',
+				'page_namespace' => 0,
+				'page_id' => 22,
 			],
 		];
-
-		return new FakeResultWrapper( $rows );
+		$entityUsage = [
+			[
+				'eu_page_id' => 11,
+				'eu_entity_id' => 'Q3',
+				'eu_aspect' => 'S'
+			],
+			[
+				'eu_page_id' => 11,
+				'eu_entity_id' => 'Q3',
+				'eu_aspect' => 'O'
+			],
+			[
+				'eu_page_id' => 22,
+				'eu_entity_id' => 'Q3',
+				'eu_aspect' => 'L.de'
+			],
+		];
+		foreach ( $pages as $pageData ) {
+			$page = WikiPage::factory( Title::makeTitle( $pageData['page_namespace'], $pageData['page_title'] ) );
+			$page->insertOn( $db, $pageData['page_id'] );
+		}
+		foreach ( $entityUsage as $item ) {
+			$db->insert(
+				'wbc_entity_usage',
+				$item
+			);
+		}
 	}
 
 	/**
 	 * @return SpecialEntityUsage
 	 */
 	protected function newSpecialPage() {
-		$specialPage = $this->getMockBuilder( SpecialEntityUsage::class )
-			->setMethods( [ 'reallyDoQuery' ] )
-			->getMock();
-
-		$specialPage->expects( $this->any() )
-			->method( 'reallyDoQuery' )
-			->will( $this->returnValue( $this->reallyDoQueryMock() ) );
-
-		return $specialPage;
+		return new SpecialEntityUsage();
 	}
 
 	public function testExecuteWithValidParam() {
-		list( $result, ) = $this->executeSpecialPage( 'Q3' );
-		$aspects = [
+		if ( wfGetDB( DB_SLAVE )->getType() === 'mysql' ) {
+			$this->markTestSkipped( 'MySQL does not allow selfjoins on temporary tables' );
+		}
+
+		$this->setupEntityUsageFixtures();
+
+		$aspectsVienna = [
 			wfMessage( 'wikibase-pageinfo-entity-usage-S' )->parse(),
 			wfMessage( 'wikibase-pageinfo-entity-usage-O' )->parse(),
-			wfMessage( 'wikibase-pageinfo-entity-usage-L', 'fa' )->parse(),
 		];
-		$aspectList = RequestContext::getMain()->getLanguage()->commaList( $aspects );
+		$aspectsBerlin = [
+			wfMessage( 'wikibase-pageinfo-entity-usage-L', 'de' )->parse(),
+		];
+		$aspectsAll = array_merge( $aspectsVienna, $aspectsBerlin );
+		$lang = RequestContext::getMain()->getLanguage();
+		$aspectListVienna = $lang->commaList( $aspectsVienna );
+		$aspectListBerlin = $lang->commaList( $aspectsBerlin );
+		$aspectListAll = $lang->commaList( $aspectsAll );
 
-		$this->assertContains( 'Tehran', $result );
-		$this->assertNotContains( '<p class="error"', $result );
 		$expected = SpecialPageFactory::getLocalNameFor( 'EntityUsage', 'Q3' );
+
+		list( $result, ) = $this->executeSpecialPage( 'Q3' );
+
+		$this->assertNotContains( '<p class="error"', $result );
 		$this->assertContains( $expected, $result );
-		$this->assertContains( $aspectList, $result );
+
+		$this->assertContains( 'Vienna', $result );
+		$this->assertContains( 'Berlin', $result );
+		$this->assertContains( $aspectListVienna, $result );
+		$this->assertContains( $aspectListBerlin, $result );
+
+		$this->assertNotContains( $aspectListAll, $result );
 	}
 
 	public function testExecuteWithInvalidParam() {
