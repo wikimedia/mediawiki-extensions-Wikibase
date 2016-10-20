dd7fcf96423ccbf6910c3032673e7f51d6783053
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2016-10-20 17:50:52 +0200

    Split EntityTitleLookup interface
    Change-Id: I866457e21a990c3455e04797c384daff09b3d7a2

--- repo/includes/Content/EntityContentFactory.php
+++ repo/includes/Content/EntityContentFactory.php
@@ -14,9 +14,9 @@ use Wikibase\DataModel\Entity\EntityId;
 use Wikibase\DataModel\Entity\EntityIdParsingException;
 use Wikibase\DataModel\Entity\EntityRedirect;
 use Wikibase\EntityContent;
-use Wikibase\Lib\Store\EntityTitleLookup;
 use Wikibase\Lib\Store\StorageException;
 use Wikibase\Repo\Store\EntityPermissionChecker;
+use Wikibase\Repo\Store\RepoEntityTitleLookup;
 use Wikibase\Store\EntityIdLookup;
 use Wikimedia\Assert\Assert;
 
@@ -30,7 +30,7 @@ use Wikimedia\Assert\Assert;
  * @author Daniel Kinzler
  * @author Bene* < benestar.wikimedia@gmail.com >
  */
-class EntityContentFactory implements EntityTitleLookup, EntityIdLookup, EntityPermissionChecker {
+class EntityContentFactory implements RepoEntityTitleLookup, EntityIdLookup, EntityPermissionChecker {
 
 	/**
 	 * @var string[] Entity type ID to content model ID mapping.
