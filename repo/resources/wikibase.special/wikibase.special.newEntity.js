d3c5800ca3df432dec3ab35be11e22177061409c
Julia Himmel <himmel.julia@gmail.com>
2016-10-29 14:28:28 +0200

    Disallow double submit of newEntity form
    Bug: T147143
    Change-Id: Ibc52e79b6a383f4372b1ec8732da452ef88febc9

---
+++ repo/resources/wikibase.special/wikibase.special.newEntity.js
@@ -0,0 +1,10 @@
+( function ( $ ) {
+  'use strict';
+
+  var form = $( 'form#mw-newentity-form1' );
+
+  form.submit( function () {
+    $( this ).find( "button[type='submit']" ).prop( 'disabled', true );
+  } );
+
+} )( jQuery );
