745d004deb0f7f5198c40ff778bee4c121d970f4
Leszek Manicki <leszek.manicki@wikimedia.de>
2016-11-01 18:14:04 +0100

    Add functions to assert a value is a valid repository name, or an array indexed by valid repository names

---
+++ lib/packages/wikibase/data-model/tests/unit/Assert/RepositoryNameAssertTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Wikibase\DataModel\Tests\Assert;
+
+use Wikibase\DataModel\Assert\RepositoryNameAssert;
+use Wikimedia\Assert\ParameterAssertionException;
+
+/**
+ * @covers Wikibase\DataModel\Assert\RepositoryNameAssert
+ *
+ * @license GPL-2.0+
+ */
+class RepositoryNameAssertTest extends \PHPUnit_Framework_TestCase {
+
+	public function provideInvalidRepositoryNames() {
+		return [
+			[ 'fo:o' ],
+			[ 'foo:' ],
+			[ ':foo' ],
+			[ ':' ],
+			[ 123 ],
+			[ null ],
+			[ false ],
+			[ [ 'foo' ] ],
+		];
+	}
+
+	/**
+	 * @dataProvider provideInvalidRepositoryNames
+	 */
+	public function testGivenInvalidValue_assertParameterIsValidRepositoryNameFails( $value ) {
+		$this->setExpectedException( ParameterAssertionException::class );
+		RepositoryNameAssert::assertParameterIsValidRepositoryName( $value, 'test' );
+	}
+
+	public function provideValidRepositoryNames() {
+		return [
+			[ '' ],
+			[ 'foo' ],
+			[ '123' ],
+		];
+	}
+
+	/**
+	 * @dataProvider provideValidRepositoryNames
+	 */
+	public function testGivenValidValue_assertParameterIsValidRepositoryNamePasses( $value ) {
+		RepositoryNameAssert::assertParameterIsValidRepositoryName( $value, 'test' );
+	}
+
+	public function provideInvalidRepositoryNameIndexedArrays() {
+		return [
+			[ 'foo' ],
+			[ [ 0 => 'foo' ] ],
+			[ [ 'fo:0' => 'bar' ] ],
+			[ [ 'foo:' => 'bar' ] ],
+			[ [ ':foo' => 'bar' ] ],
+		];
+	}
+
+	/**
+	 * @dataProvider provideInvalidRepositoryNameIndexedArrays
+	 */
+	public function testGivenInvalidValue_assertParameterKeysAreValidRepositoryNamesFails( $values ) {
+		$this->setExpectedException( ParameterAssertionException::class );
+		RepositoryNameAssert::assertParameterKeysAreValidRepositoryNames( $values, 'test' );
+	}
+
+	public function provideValidRepositoryNameIndexedArrays() {
+		return [
+			[ [ 'foo' => 'bar' ] ],
+			[ [ '' => 'bar' ] ],
+			[ [ '' => 'bar', 'foo' => 'baz' ] ],
+			[ [] ],
+		];
+	}
+
+	/**
+	 * @dataProvider provideValidRepositoryNameIndexedArrays
+	 */
+	public function testGivenValidValue_assertParameterKeysAreValidRepositoryNamesPasses( array $values ) {
+		RepositoryNameAssert::assertParameterKeysAreValidRepositoryNames( $values, 'test' );
+	}
+
+}
