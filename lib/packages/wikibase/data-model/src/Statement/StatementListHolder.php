1f431b752ca9dfce601f512559a0de2632f45604
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2016-11-07 15:14:15 +0100

    Remove "will be removed in 6.0"

--- lib/packages/wikibase/data-model/src/Statement/StatementListHolder.php
+++ lib/packages/wikibase/data-model/src/Statement/StatementListHolder.php
@@ -6,8 +6,8 @@ namespace Wikibase\DataModel\Statement;
  * Interface for classes that contain a StatementList.
  *
  * @since 3.0
- * @deprecated since 5.1, will be removed in 6.0 in favor of StatementListProvider, which will then
- *  give the guarantee to return an object by reference. Changes to that object change the entity.
+ * @deprecated since 5.1, will be removed in favor of StatementListProvider, which
+ *  gives the guarantee to return an object by reference. Changes to that object change the entity.
  *
  * @license GPL-2.0+
  * @author Thiemo Mättig
