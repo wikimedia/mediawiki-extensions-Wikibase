6212626254383de88897d55ee13ee5c2ddada1ac
Leszek Manicki <leszek.manicki@wikimedia.de>
2016-11-09 14:54:59 +0100

    Add ForeignEntityRevisionLookupFactory providing a WikiPageEntityMetaDataLookup for the given foreign repository
    Bug: T150330
    Change-Id: Iac2de8d73fbc7e49ad2de358d69cc29456da25a5

---
+++ lib/includes/Store/ForeignEntityRevisionLookupFactory.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Wikibase\Lib\Store;
+
+use DataValues\Deserializers\DataValueDeserializer;
+use Serializers\Serializer;
+use Wikibase\DataModel\Entity\EntityIdParser;
+use Wikibase\DataModel\Services\EntityId\PrefixMappingEntityIdParserFactory;
+use Wikibase\DataModel\Services\Lookup\UnknownForeignRepositoryException;
+use Wikibase\InternalSerialization\DeserializerFactory;
+use Wikibase\Lib\Store\Sql\PrefetchingWikiPageEntityMetaDataAccessor;
+use Wikibase\Lib\Store\Sql\WikiPageEntityMetaDataLookup;
+
+/**
+ * A factory providing the WikiPageEntityMetaDataLookup instance configured for the given foreign repository.
+ *
+ * @license GPL-2.0+
+ */
+class ForeignEntityRevisionLookupFactory {
+
+	/**
+	 * @var PrefixMappingEntityIdParserFactory
+	 */
+	private $parserFactory;
+
+	/**
+	 * @var Serializer
+	 */
+	private $entitySerializer;
+
+	/**
+	 * @var DataValueDeserializer
+	 */
+	private $dataValueDeserializer;
+
+	/**
+	 * @var EntityNamespaceLookup
+	 */
+	private $entityNamespaceLookup;
+
+	/**
+	 * @var string
+	 */
+	private $maxBlobSize;
+
+	/**
+	 * @var string[]
+	 */
+	private $databaseNames;
+
+	/**
+	 * @param PrefixMappingEntityIdParserFactory $parserFactory
+	 * @param DataValueDeserializer $dataValueDeserializer
+	 * @param EntityIdParser $idParser Parser used to create an EntityId with a foreign repository prefix stripped
+	 * @param EntityNamespaceLookup $entityNamespaceLookup
+	 * @param string $maxBlobSize The maximum size of a blob allowed in serialization/deserialization,
+	 *               @see EntityContentDataCodec
+	 * @param string[] $databaseNames Associative array mapping repository names (prefixes) to database names
+	 */
+	public function __construct(
+		PrefixMappingEntityIdParserFactory $parserFactory,
+		Serializer $entitySerializer,
+		DataValueDeserializer $dataValueDeserializer,
+		EntityNamespaceLookup $entityNamespaceLookup,
+		$maxBlobSize,
+		array $databaseNames
+	) {
+		$this->parserFactory = $parserFactory;
+		$this->entitySerializer = $entitySerializer;
+		$this->dataValueDeserializer = $dataValueDeserializer;
+		$this->entityNamespaceLookup = $entityNamespaceLookup;
+		$this->maxBlobSize = $maxBlobSize;
+		$this->databaseNames = $databaseNames;
+	}
+
+	/**
+	 * @param string $repositoryName
+	 *
+	 * @return WikiPageEntityRevisionLookup
+	 *
+	 * @throws UnknownForeignRepositoryException
+	 */
+	public function getLookup( $repositoryName ) {
+		if ( !array_key_exists( $repositoryName, $this->databaseNames ) ) {
+			throw new UnknownForeignRepositoryException( 'No database configured for repository: ' . $repositoryName );
+		}
+
+		$prefixMappingIdParser = $this->parserFactory->getIdParser( $repositoryName );
+		$entityDeserializerFactory = new DeserializerFactory(
+			$this->dataValueDeserializer,
+			$prefixMappingIdParser,
+			null
+		);
+		$codec = new EntityContentDataCodec(
+			$prefixMappingIdParser,
+			$this->entitySerializer,
+			$entityDeserializerFactory->newEntityDeserializer(),
+			$this->maxBlobSize
+		);
+
+		$metaDataLookup = new PrefetchingWikiPageEntityMetaDataAccessor(
+			new WikiPageEntityMetaDataLookup(
+				$this->entityNamespaceLookup,
+				$this->databaseNames[$repositoryName],
+				$repositoryName
+			)
+		);
+
+		return new WikiPageEntityRevisionLookup(
+			$codec,
+			$metaDataLookup,
+			$this->databaseNames[$repositoryName]
+		);
+	}
+
+}
