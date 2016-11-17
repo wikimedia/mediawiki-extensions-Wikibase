f1e94edcfa18ea2199d26d947ff83f7ea83ddfb0
Leszek Manicki <leszek.manicki@wikimedia.de>
2016-11-17 16:35:21 +0100

    Add DispatchingTermSearchInteractor
    Bug: T150848
    Change-Id: I83171117333030f8724ef3f5688d0b7351f7175a

---
+++ lib/includes/Interactors/DispatchingTermSearchInteractor.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Wikibase\Lib\Interactors;
+
+use Wikimedia\Assert\Assert;
+use Wikimedia\Assert\ParameterAssertionException;
+
+/**
+ * Picks a TermSearchInteractor configured for the particular entity type when searching for entities.
+ *
+ * @license GPL-2.0+
+ *
+ * TODO: rename to DispatchingByEntityTypeTermSearchInteractor ?
+ */
+class DispatchingTermSearchInteractor implements TermSearchInteractor {
+
+	/**
+	 * @var TermSearchInteractor[]
+	 */
+	private $interactors = [];
+
+	/**
+	 * @param TermSearchInteractor[] $interactors Associative array mapping entity types (strings) to TermSearchInteractor instances
+	 *
+	 * @throws ParameterAssertionException
+	 */
+	public function __construct( array $interactors ) {
+		Assert::parameterElementType( TermSearchInteractor::class, $interactors, '$interactors' );
+		Assert::parameterElementType( 'string', array_keys( $interactors ), 'array_keys( $interactors )' );
+		$this->interactors = $interactors;
+	}
+
+	/**
+	 * @see TermSearchInteractor::searchForEntities
+	 *
+	 * @param string $text
+	 * @param string $languageCode
+	 * @param string $entityType
+	 * @param string[] $termTypes
+	 *
+	 * @return TermSearchResult[] Returns an empty array also when there is no TermSearchInteractor configured for $entityType
+	 */
+	public function searchForEntities( $text, $languageCode, $entityType, array $termTypes ) {
+		$interactor = $this->getInteractorForEntityType( $entityType );
+
+		if ( $interactor === null ) {
+			return [];
+		}
+
+		return $interactor->searchForEntities( $text, $languageCode, $entityType, $termTypes );
+	}
+
+	/**
+	 * @param string $entityType
+	 *
+	 * @return TermSearchInteractor|null
+	 */
+	private function getInteractorForEntityType( $entityType ) {
+		return isset( $this->interactors[$entityType] ) ? $this->interactors[$entityType] : null;
+	}
+
+}
