054c5113359febabddd30e214ae5d1ff176473f7
Leszek Manicki <leszek.manicki@wikimedia.de>
2016-11-17 16:35:21 +0100

    Add DispatchingTermSearchInteractor
    Bug: T150848
    Change-Id: I83171117333030f8724ef3f5688d0b7351f7175a

---
+++ lib/tests/phpunit/Interactors/DispatchingTermSearchInteractorTest.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace Wikibase\Test\Interactors;
+
+use PHPUnit_Framework_TestCase;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\DataModel\Term\Term;
+use Wikibase\Lib\Interactors\DispatchingTermSearchInteractor;
+use Wikibase\Lib\Interactors\TermSearchInteractor;
+use Wikibase\Lib\Interactors\TermSearchResult;
+use Wikibase\TermIndexEntry;
+use Wikimedia\Assert\ParameterAssertionException;
+
+/**
+ * @covers Wikibase\Lib\Interactors\DispatchingTermSearchInteractor
+ *
+ * @group Wikibase
+ * @group WikibaseLib
+ *
+ * @license GPL-2.0+
+ */
+class DispatchingTermSearchInteractorTest extends PHPUnit_Framework_TestCase  {
+
+	public function provideInvalidInteractorConfig() {
+		return [
+			'non-TermSearchInteractor values' => [
+				[ 'item' => 'foo' ]
+			],
+			'non-string keys' => [
+				[ 0 => $this->getTermSearchInteractor( [] ) ]
+			]
+		];
+	}
+
+	/**
+	 * @dataProvider provideInvalidInteractorConfig
+	 */
+	public function testGivenInvalidInteractorConfig_exceptionIsThrown( array $interactors ) {
+		$this->setExpectedException( ParameterAssertionException::class );
+		new DispatchingTermSearchInteractor( $interactors );
+	}
+
+	private function getTermSearchInteractor( array $resultsByEntityType ) {
+		$interactor = $this->getMockBuilder( TermSearchInteractor::class )->getMock();
+		$interactor->expects( $this->any() )
+			->method( 'searchForEntities' )
+			->will( $this->returnCallback(
+				function ( $text, $language, $entityType ) use ( $resultsByEntityType ) {
+					return isset( $resultsByEntityType[$entityType] ) ? $resultsByEntityType[$entityType] : [];
+				}
+			) );
+		return $interactor;
+	}
+
+	public function testSearchForEntities_usesTheInteractorConfiguredForEntityType() {
+		$interactorOne = $this->getTermSearchInteractor( [
+			'item' => [
+				new TermSearchResult(
+					new Term( 'en', 'foot' ),
+					'label',
+					new ItemId( 'Q123' )
+				),
+				new TermSearchResult(
+					new Term( 'en', 'fox' ),
+					'label',
+					new ItemId( 'Q321' )
+				),
+			],
+			'property' => [
+				new TermSearchResult(
+					new Term( 'en', 'founder' ),
+					'label',
+					new PropertyId( 'P123' )
+				),
+			]
+		] );
+		$interactorTwo = $this->getTermSearchInteractor( [
+			'property' => [
+				new TermSearchResult(
+					new Term( 'en', 'follows' ),
+					'label',
+					new PropertyId( 'baz:P322' )
+				),
+				new TermSearchResult(
+					new Term( 'en', 'followed by' ),
+					'label',
+					new PropertyId( 'baz:P567' )
+				),
+			]
+		] );
+
+		$interactor = new DispatchingTermSearchInteractor( [
+			'item' => $interactorOne,
+			'property' => $interactorTwo,
+		] );
+
+		$this->assertEquals(
+			[
+				new TermSearchResult(
+					new Term( 'en', 'foot' ),
+					'label',
+					new ItemId( 'Q123' )
+				),
+				new TermSearchResult(
+					new Term( 'en', 'fox' ),
+					'label',
+					new ItemId( 'Q321' )
+				),
+			],
+			$interactor->searchForEntities( 'fo', 'en', 'item', [ TermIndexEntry::TYPE_LABEL ] )
+		);
+		$this->assertEquals(
+			[
+				new TermSearchResult(
+					new Term( 'en', 'follows' ),
+					'label',
+					new PropertyId( 'baz:P322' )
+				),
+				new TermSearchResult(
+					new Term( 'en', 'followed by' ),
+					'label',
+					new PropertyId( 'baz:P567' )
+				),
+			],
+			$interactor->searchForEntities( 'fo', 'en', 'property', [ TermIndexEntry::TYPE_LABEL ] )
+		);
+	}
+
+}
