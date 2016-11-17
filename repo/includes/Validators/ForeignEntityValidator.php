f92ed77d10eb626146579b73c5bbd0ae16ea9b36
Jakob Warkotsch <j.warkotsch@gmail.com>
2016-11-17 14:48:51 +0100

    Add ForeignEntityValidator.
    Ensures that
      * the entity's repository name is known to the local wiki
      * the corresponding repository supports the entitiy's type
    
    Bug: T150655
    Change-Id: I253742a26b563b8173c5dfe1d21321fa30df307e

---
+++ repo/includes/Validators/ForeignEntityValidator.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Wikibase\Repo\Validators;
+
+use ValueValidators\Error;
+use ValueValidators\Result;
+use Wikibase\DataModel\Assert\RepositoryNameAssert;
+use Wikibase\DataModel\Entity\EntityDocument;
+use Wikimedia\Assert\Assert;
+
+/**
+ * @license GPL-2.0+
+ */
+class ForeignEntityValidator implements EntityValidator {
+
+	/**
+	 * @var array
+	 */
+	private $foreignRepositorySettings;
+
+	/**
+	 * @param array $foreignRepositorySettings
+	 */
+	public function __construct( array $foreignRepositorySettings ) {
+		RepositoryNameAssert::assertParameterKeysAreValidRepositoryNames(
+			$foreignRepositorySettings,
+			'$foreignRepositorySettings'
+		);
+		Assert::parameterElementType( 'array', $foreignRepositorySettings, '$foreignRepositorySettings' );
+
+		$this->foreignRepositorySettings = $foreignRepositorySettings;
+	}
+
+	/**
+	 * Ensures an entity's repository name is known and
+	 * the corresponding repository supports the entity's type.
+	 *
+	 * @param EntityDocument $entity
+	 *
+	 * @return Result
+	 */
+	public function validateEntity( EntityDocument $entity ) {
+		if ( !$entity->getId()->isForeign() ) {
+			return Result::newSuccess();
+		}
+
+		if ( !$this->isKnownRepositoryName( $entity->getId()->getRepositoryName() ) ) {
+			return Result::newError( [
+				Error::newError(
+					'Unknown repository name: ' . $entity->getId()->getRepositoryName(),
+					null,
+					'unknown-repository-name',
+					[ $entity ]
+				)
+			] );
+		}
+
+		if ( !$this->hasSupportedEntityType( $entity ) ) {
+			return Result::newError( [
+				Error::newError(
+					'Unsupported entity type: ' . $entity->getType()
+					. ' for repository ' . $entity->getId()->getRepositoryName(),
+					null,
+					'unsupported-entity-type',
+					[ $entity ]
+				)
+			] );
+		}
+
+		return Result::newSuccess();
+	}
+
+	private function isKnownRepositoryName( $repository ) {
+		return in_array( $repository, array_keys( $this->foreignRepositorySettings ) );
+	}
+
+	private function hasSupportedEntityType( EntityDocument $entity ) {
+		$repository = $entity->getId()->getRepositoryName();
+
+		if ( isset( $this->foreignRepositorySettings[$repository]['supportedEntityTypes'] ) ) {
+			return in_array(
+				$entity->getType(),
+				$this->foreignRepositorySettings[$repository]['supportedEntityTypes']
+			);
+		}
+
+		return false;
+	}
+
+}
