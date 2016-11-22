5ce0e2b5228c9a963a486a49ab4055971fa30b85
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2016-11-22 11:22:43 +0100

    Remove termWeight field from TermIndexEntry
    This patch does *not* drop the term_weight field from the terms table.
    But the field is not exposed any more via the TermIndexEntry class.
    This makes the field a private implementation detail of TermSqlIndex.
    The weight is still calculated, stored and used for ordering, but not
    exposed any more. It was not used anyway outside of TermSqlIndex.
    
    Change-Id: I25cf98c4dc72c52aae5bd5545066a2f577b4fb2c

--- lib/tests/phpunit/TermIndexEntryTest.php
+++ lib/tests/phpunit/TermIndexEntryTest.php
@@ -29,7 +29,6 @@ class TermIndexEntryTest extends PHPUnit_Framework_TestCase {
 					'termType' => TermIndexEntry::TYPE_LABEL,
 					'termLanguage' => 'en',
 					'termText' => 'foo',
-					'termWeight' => 1.234,
 				)
 			),
 			array( // #1
@@ -65,7 +64,6 @@ class TermIndexEntryTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals( isset( $fields['termType'] ) ? $fields['termType'] : null, $term->getType() );
 		$this->assertEquals( isset( $fields['termLanguage'] ) ? $fields['termLanguage'] : null, $term->getLanguage() );
 		$this->assertEquals( isset( $fields['termText'] ) ? $fields['termText'] : null, $term->getText() );
-		$this->assertEquals( isset( $fields['termWeight'] ) ? $fields['termWeight'] : null, $term->getWeight() );
 	}
 
 	public function testClone() {
