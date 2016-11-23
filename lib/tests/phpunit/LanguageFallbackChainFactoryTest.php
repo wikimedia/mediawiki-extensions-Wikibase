a8b5b6b6002ce5ec5cc93b9ef613f5e0e69cc32f
Fomafix <fomafix@googlemail.com>
2016-11-23 17:58:43 +0100

    Replace tests with language codes 'sr-ec' and 'sr-el'
    The language codes 'sr-ec' and 'sr-el' get replaced with the BCP 47 conform
    language codes 'sr-cyrl' and 'sr-latn' in core by
    I75da0af35a2066e7963e50c56c99daf1e07c55e6.
    To fullfil the integration tests for extensions first the old language codes
    in the tests must get replaced.
    
    This change replaces 'sr-ec' and 'sr-el' with 'kk-cyrl' and 'kk-latn'.
    
    Bug: T117845
    Change-Id: Idca04cd5b1f9b362f89a7b7b3581c5f5736b88c5

--- lib/tests/phpunit/LanguageFallbackChainFactoryTest.php
+++ lib/tests/phpunit/LanguageFallbackChainFactoryTest.php
@@ -233,10 +233,10 @@ class LanguageFallbackChainFactoryTest extends MediaWikiTestCase {
 			) ),
 			array( 'ii', LanguageFallbackChainFactory::FALLBACK_OTHERS, array( 'zh-cn', 'zh-hans', 'en' ) ),
 
-			array( 'sr', LanguageFallbackChainFactory::FALLBACK_SELF | LanguageFallbackChainFactory::FALLBACK_VARIANTS, array(
-				'sr',
-				array( 'sr', 'sr-ec' ),
-				array( 'sr', 'sr-el' ),
+			array( 'kk', LanguageFallbackChainFactory::FALLBACK_SELF | LanguageFallbackChainFactory::FALLBACK_VARIANTS, array(
+				'kk',
+				array( 'kk', 'kk-cyrl' ),
+				array( 'kk', 'kk-latn' ),
 			) ),
 		);
 	}
