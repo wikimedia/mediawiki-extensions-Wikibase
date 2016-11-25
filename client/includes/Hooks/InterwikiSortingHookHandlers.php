fc73f53b669b43b4bc2b11a71fd917c4d8b80e38
aude <aude.wiki@gmail.com>
2016-11-25 12:38:53 -0500

    Split InterwikiSorting hook handlers from ParserOutputUpdateHookHandlers
    In preparation to remove these and use InterwikiSorting extension
    instead.
    
    The new hook handlers also work with the new settings that
    InterwikiSorting has, as well as the old wikibase configs.
    
    Next steps are to add config to mediawiki-config (T111023)
    and then after this code is deployed, then we can change
    the config variables used there to the new ones and
    remove support for the old configurations.
    
    Bug: T151652
    Change-Id: I5b6fd2602f5d6f08114a5877dfc872b9d5db672c

---
+++ client/includes/Hooks/InterwikiSortingHookHandlers.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace Wikibase\Client\Hooks;
+
+use Config;
+use Content;
+use MediaWiki\MediaWikiServices;
+use ParserOutput;
+use Title;
+use Wikibase\Client\WikibaseClient;
+use Wikibase\InterwikiSorter;
+use Wikibase\NamespaceChecker;
+use Wikibase\NoLangLinkHandler;
+use Wikibase\SettingsArray;
+
+/**
+ * @since 0.5
+ *
+ * @license GPL-2.0+
+ */
+class InterwikiSortingHookHandlers {
+
+	/**
+	 * @var InterwikiSorter
+	 */
+	private $interwikiSorter;
+
+	/**
+	 * @var NamespaceChecker
+	 */
+	private $namespaceChecker;
+
+	/**
+	 * @var bool
+	 */
+	private $alwaysSort;
+
+	/**
+	 * @return InterwikiSortingConfig
+	 */
+	public static function newFromGlobalState() {
+		$wikibaseClient = WikibaseClient::getDefaultInstance();
+		$namespaceChecker = $wikibaseClient->getNamespaceChecker();
+
+		$config = MediaWikiServices::getInstance()->getMainConfig();
+
+		if ( $config->has( 'InterwikiSortingSort' ) ) {
+			return self::newFromInterwikiSortingConfig( $config, $namespaceChecker );
+		}
+
+		$settings = $wikibaseClient->getSettings();
+
+		return self::newFromWikibaseConfig( $settings, $namespaceChecker );
+	}
+
+	/**
+	 * @param Config $config
+	 * @param NamespaceChecker $namespaceChecker
+	 *
+	 * @return InterwikiSortingHookHandlers
+	 */
+	public static function newFromInterwikiSortingConfig(
+		Config $config,
+		NamespaceChecker $namespaceChecker
+	) {
+		$interwikiSorter = new InterwikiSorter(
+			$config->get( 'InterwikiSortingSort' ),
+			$config->get( 'InterwikiSortingInterwikiSortOrders' ),
+			$config->get( 'InterwikiSortingSortPrepend' )
+		);
+
+		return new InterwikiSortingHookHandlers(
+			$interwikiSorter,
+			$namespaceChecker,
+			$config->get( 'InterwikiSortingAlwaysSort' )
+		);
+	}
+
+	/**
+	 * @param SettingsArray $settings
+	 * @param NamespaceChecker $namespaceChecker
+	 *
+	 * @return InterwikiSortingHookHandlers
+	 */
+	public static function newFromWikibaseConfig(
+		SettingsArray $settings,
+		NamespaceChecker $namespaceChecker
+	) {
+		$interwikiSorter = new InterwikiSorter(
+			$settings->getSetting( 'sort' ),
+			$settings->getSetting( 'interwikiSortOrders' ),
+			$settings->getSetting( 'sortPrepend' )
+		);
+
+		return new InterwikiSortingHookHandlers(
+			$interwikiSorter,
+			$namespaceChecker,
+			$settings->getSetting( 'alwaysSort' )
+		);
+	}
+
+	/**
+	 * Static handler for the ContentAlterParserOutput hook.
+	 *
+	 * @param Content $content
+	 * @param Title $title
+	 * @param ParserOutput $parserOutput
+	 *
+	 * @return bool
+	 */
+	public static function onContentAlterParserOutput(
+		Content $content,
+		Title $title,
+		ParserOutput $parserOutput
+	) {
+		$handler = self::newFromGlobalState();
+		$handler->doContentAlterParserOutput( $title, $parserOutput );
+
+		return true;
+	}
+
+	/**
+	 * @param InterwikiSorter $sorter
+	 * @param NamespaceChecker $namespaceChecker
+	 * @param bool $alwaysSort
+	 */
+	public function __construct(
+		InterwikiSorter $sorter,
+		NamespaceChecker $namespaceChecker,
+		$alwaysSort
+	) {
+		$this->interwikiSorter = $sorter;
+		$this->namespaceChecker = $namespaceChecker;
+		$this->alwaysSort = $alwaysSort;
+	}
+
+	/**
+	 * Hook runs after internal parsing
+	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/ContentAlterParserOutput
+	 *
+	 * @param Title $title
+	 * @param ParserOutput $parserOutput
+	 *
+	 * @return void
+	 */
+	public function doContentAlterParserOutput( Title $title, ParserOutput $parserOutput ) {
+		if ( !$this->namespaceChecker->isWikibaseEnabled( $title->getNamespace() ) ) {
+			return;
+		}
+
+		if ( $this->alwaysSort || !$this->hasNoExternalLangLinks( $parserOutput ) ) {
+			$interwikiLinks = $parserOutput->getLanguageLinks();
+			$sortedLinks = $this->interwikiSorter->sortLinks( $interwikiLinks );
+			$parserOutput->setLanguageLinks( $sortedLinks );
+		}
+	}
+
+	/**
+	 * @param ParserOutput $parserOutput
+	 *
+	 * @return bool
+	 */
+	private function hasNoExternalLangLinks( ParserOutput $parserOutput ) {
+		$noExternalLangLinks = NoLangLinkHandler::getNoExternalLangLinks( $parserOutput );
+
+		if ( in_array( '*', $noExternalLangLinks ) ) {
+			return true;
+		}
+
+		return false;
+	}
+
+}
