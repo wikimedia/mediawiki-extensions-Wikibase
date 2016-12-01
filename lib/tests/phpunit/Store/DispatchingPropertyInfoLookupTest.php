838f87cb66a61e33cb9bb603be8747844d99bc75
Jakob Warkotsch <j.warkotsch@gmail.com>
2016-12-01 16:49:42 +0100

    Add DispatchingPropertyInfoLookup
    This service implements PropertyInfoLookup. It dispatches calls to
    getPropertyInfo to the respective repository-specific service and
    combines the results of all injected PropertyInfoLookups for
    getPropertyInfoForDataType and getAllPropertyInfo.
    
    Bug: T152103
    Change-Id: Iadb1539a749a602b62ab65d8d745b594d831bc0b

---
+++ lib/tests/phpunit/Store/DispatchingPropertyInfoLookupTest.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Wikibase\Lib\Tests\Store;
+
+use InvalidArgumentException;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\Lib\Store\DispatchingPropertyInfoLookup;
+use Wikibase\Lib\Store\PropertyInfoLookup;
+use Wikibase\PropertyInfoStore;
+
+/**
+ * @covers Wikibase\Lib\Store\DispatchingPropertyInfoLookup
+ *
+ * @group WikibaseLib
+ * @group WikibaseStore
+ * @group Wikibase
+ *
+ * @license GPL-2.0+
+ */
+class DispatchingPropertyInfoLookupTest extends \PHPUnit_Framework_TestCase {
+
+	private $localPropertyInfo;
+	private $fooPropertyInfo;
+
+	public function setUp() {
+		$this->localPropertyInfo = [
+			'P23' => [ PropertyInfoStore::KEY_DATA_TYPE => 'string' ],
+			'P42' => [ PropertyInfoStore::KEY_DATA_TYPE => 'commonsMedia', 'foo' => 'bar' ]
+		];
+		$this->fooPropertyInfo = [
+			'foo:P123' => [ PropertyInfoStore::KEY_DATA_TYPE => 'string', 'foo' => 'bar' ],
+			'foo:P42' => [ PropertyInfoStore::KEY_DATA_TYPE => 'commonsMedia' ]
+		];
+	}
+
+	public function testGivenUnknownRepository_getPropertyInfoReturnsNull() {
+		$lookup = new DispatchingPropertyInfoLookup( [
+			'' => $this->getPropertyInfoLookup( $this->localPropertyInfo ),
+			'foo' => $this->getPropertyInfoLookup( $this->fooPropertyInfo ),
+		] );
+
+		$this->assertNull( $lookup->getPropertyInfo( new PropertyId( 'bar:P123' ) ) );
+	}
+
+	public function testGivenDifferentPropertyIds_getPropertyInfoDispatchesAccordingToRepository() {
+		$lookup = new DispatchingPropertyInfoLookup( [
+			'' => $this->getPropertyInfoLookup( $this->localPropertyInfo ),
+			'foo' => $this->getPropertyInfoLookup( $this->fooPropertyInfo ),
+		] );
+
+		$this->assertSame(
+			$this->localPropertyInfo['P23'],
+			$lookup->getPropertyInfo( new PropertyId( 'P23' ) )
+		);
+		$this->assertSame(
+			$this->fooPropertyInfo['foo:P42'],
+			$lookup->getPropertyInfo( new PropertyId( 'foo:P42' ) )
+		);
+	}
+
+	public function testGivenMultiplePropertyInfoLookups_getAllPropertyInfoCombinesResults() {
+		$lookup = new DispatchingPropertyInfoLookup( [
+			'' => $this->getPropertyInfoLookup( $this->localPropertyInfo ),
+			'foo' => $this->getPropertyInfoLookup( $this->fooPropertyInfo ),
+		] );
+
+		$this->assertSame(
+			array_merge( $this->localPropertyInfo, $this->fooPropertyInfo ),
+			$lookup->getAllPropertyInfo()
+		);
+	}
+
+	public function testGivenMultiplePropertyInfoLookups_getPropertyInfoForDataTypeCombinesResults() {
+		$lookup = new DispatchingPropertyInfoLookup( [
+			'' => $this->getPropertyInfoLookup( $this->localPropertyInfo ),
+			'foo' => $this->getPropertyInfoLookup( $this->fooPropertyInfo ),
+		] );
+
+		$this->assertSame(
+			[ 'P23' => $this->localPropertyInfo['P23'], 'foo:P123' => $this->fooPropertyInfo['foo:P123'] ],
+			$lookup->getPropertyInfoForDataType( 'string' )
+		);
+		$this->assertSame(
+			[ 'P42' => $this->localPropertyInfo['P42'], 'foo:P42' => $this->fooPropertyInfo['foo:P42'] ],
+			$lookup->getPropertyInfoForDataType( 'commonsMedia' )
+		);
+	}
+
+	private function getPropertyInfoLookup( array $infos ) {
+		$lookup = new MockPropertyInfoStore();
+
+		foreach ( $infos as $id => $info ) {
+			$lookup->setPropertyInfo( new PropertyId( $id ), $info );
+		}
+
+		return $lookup;
+	}
+
+	/**
+	 * @dataProvider provideInvalidForeignLookups
+	 */
+	public function testGivenInvalidPropertyInfoLookup_exceptionIsThrown( $lookups ) {
+		$this->setExpectedException( InvalidArgumentException::class );
+		new DispatchingPropertyInfoLookup( $lookups );
+	}
+
+	public function provideInvalidForeignLookups() {
+		return [
+			'no lookups given' => [ [] ],
+			'not an implementation of PropertyInfoLookup given as a lookup' => [
+				[ '' => new PropertyId( 'P123' ) ],
+			],
+			'non-string keys' => [
+				[
+					'' => $this->getMock( PropertyInfoLookup::class ),
+					100 => $this->getMock( PropertyInfoLookup::class ),
+				],
+			],
+			'repo name containing colon' => [
+				[
+					'' => $this->getMock( PropertyInfoLookup::class ),
+					'fo:oo' => $this->getMock( PropertyInfoLookup::class ),
+				],
+			],
+		];
+	}
+
+}
