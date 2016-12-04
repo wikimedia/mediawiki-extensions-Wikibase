b3799783d5973d0441dffacf8cdc5371e9cbc5a9
Geoffrey Mon <geofbot@gmail.com>
2016-12-04 15:16:09 -0500

    Add a tracking category to pages with unresolvable property labels
    Unit test updated to check if tracking category has been added.
    
    Bug: T50799
    Change-Id: I94d1155d285877e144169789ca6f4ac2417a2147

--- client/includes/DataAccess/PropertyParserFunction/LanguageAwareRenderer.php
+++ client/includes/DataAccess/PropertyParserFunction/LanguageAwareRenderer.php
@@ -4,7 +4,9 @@ namespace Wikibase\Client\DataAccess\PropertyParserFunction;
 
 use InvalidArgumentException;
 use Language;
+use ParserOutput;
 use Status;
+use Title;
 use Wikibase\Client\DataAccess\StatementTransclusionInteractor;
 use Wikibase\Client\PropertyLabelNotResolvedException;
 use Wikibase\DataModel\Entity\EntityId;
@@ -35,15 +37,31 @@ class LanguageAwareRenderer implements StatementGroupRenderer {
 	private $statementTransclusionInteractor;
 
 	/**
+	 * @var ParserOutput
+	 */
+	private $parserOutput;
+
+	/**
+	 * @var Title
+	 */
+	private $title;
+
+	/**
 	 * @param Language $language
 	 * @param StatementTransclusionInteractor $statementTransclusionInteractor
+	 * @param ParserOutput $parserOutput
+	 * @param Title $title
 	 */
 	public function __construct(
 		Language $language,
-		StatementTransclusionInteractor $statementTransclusionInteractor
+		StatementTransclusionInteractor $statementTransclusionInteractor,
+		ParserOutput $parserOutput,
+		Title $title
 	) {
 		$this->language = $language;
 		$this->statementTransclusionInteractor = $statementTransclusionInteractor;
+		$this->parserOutput = $parserOutput;
+		$this->title = $title;
 	}
 
 	/**
@@ -52,7 +70,7 @@ class LanguageAwareRenderer implements StatementGroupRenderer {
 	 *
 	 * @return string
 	 */
-	public function render( EntityId $entityId, $propertyLabelOrId ) {
+	public function render(	EntityId $entityId, $propertyLabelOrId ) {
 		try {
 			$status = Status::newGood(
 				$this->statementTransclusionInteractor->render(
@@ -61,6 +79,8 @@ class LanguageAwareRenderer implements StatementGroupRenderer {
 				)
 			);
 		} catch ( PropertyLabelNotResolvedException $ex ) {
+			$this->parserOutput->addTrackingCategory( 'unresolved-property-category', $this->title );
+
 			// @fixme use ExceptionLocalizer
 			$status = $this->getStatusForException( $propertyLabelOrId, $ex->getMessage() );
 		} catch ( EntityAccessLimitException $ex ) {
