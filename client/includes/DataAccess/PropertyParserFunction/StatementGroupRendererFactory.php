b1f7052d46c476bfc5475458460f910734530748
Geoffrey Mon <geofbot@gmail.com>
2016-12-04 15:16:09 -0500

    Add a tracking category to pages with unresolvable property labels
    Unit test updated to check if tracking category has been added.
    
    Bug: T50799
    Change-Id: I94d1155d285877e144169789ca6f4ac2417a2147

--- client/includes/DataAccess/PropertyParserFunction/StatementGroupRendererFactory.php
+++ client/includes/DataAccess/PropertyParserFunction/StatementGroupRendererFactory.php
@@ -86,13 +86,17 @@ class StatementGroupRendererFactory {
 			// Use the user's language.
 			// Note: This splits the parser cache.
 			$targetLanguage = $parser->getOptions()->getUserLangObj();
-			return $this->newLanguageAwareRenderer( $type, $targetLanguage, $usageAccumulator );
+			$renderer = $this->newLanguageAwareRenderer( $type, $targetLanguage, $usageAccumulator );
+			$renderer->setParser( $parser );
+			return $renderer;
 		} elseif ( $this->useVariants( $parser ) ) {
 			$variants = $parser->getConverterLanguage()->getVariants();
 			return $this->newVariantsAwareRenderer( $type, $variants, $usageAccumulator );
 		} else {
 			$targetLanguage = $parser->getTargetLanguage();
-			return $this->newLanguageAwareRenderer( $type, $targetLanguage, $usageAccumulator );
+			$renderer = $this->newLanguageAwareRenderer( $type, $targetLanguage, $usageAccumulator );
+			$renderer->setParser( $parser );
+			return $renderer;
 		}
 	}
 
@@ -102,7 +106,6 @@ class StatementGroupRendererFactory {
 	 * @param UsageAccumulator $usageAccumulator
 	 *
 	 * @return LanguageAwareRenderer
-	 * @throws MWException
 	 */
 	private function newLanguageAwareRenderer(
 		$type,
