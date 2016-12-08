19a06ec9a310934249b81c7edb4646b1faa90f25
Leszek Manicki <leszek.manicki@wikimedia.de>
2016-12-08 18:41:09 +0100

    [WIP] Add DispatchingServiceFactory
    TODO:
     - needs adapthing when adding more services
     - document new settings in docs/options.wiki
    
    Bug: T148141
    Change-Id: I1cd10515e8eb713efba1000e987ece03e652e96c

---
+++ client/includes/DispatchingServiceFactory.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Wikibase\Client;
+
+use Wikibase\Client\Store\RepositoryServiceContainer;
+use Wikibase\DataModel\Entity\EntityIdParser;
+use Wikibase\DataModel\Services\EntityId\PrefixMappingEntityIdParserFactory;
+use Wikibase\Lib\Serialization\RepositorySpecificDataValueDeserializerFactory;
+use Wikibase\Lib\Store\EntityNamespaceLookup;
+use Wikibase\Lib\Store\RepositorySpecificEntityRevisionLookupFactory;
+use Wikibase\SettingsArray;
+
+/**
+ * TODO: rename the class? it is not really a dispatching service factory in its current form
+ *
+ * @license GPL-2.0+
+ */
+class DispatchingServiceFactory {
+
+	private $repositoryNames;
+
+	/**
+	 * @var RepositorySpecificEntityRevisionLookupFactory
+	 */
+	private $entityRevisionLookupFactory;
+
+	/**
+	 * @var RepositoryServiceContainer[]
+	 */
+	private $repositoryServiceContainers = [];
+
+	public function __construct(
+		EntityIdParser $entityIdParser,
+		EntityNamespaceLookup $entityNamespaceLookup,
+		SettingsArray $clientSettings
+	) {
+		$entityIdParserFactory = new PrefixMappingEntityIdParserFactory( $entityIdParser, [] ); // TODO: read mapping from settings
+		$this->entityRevisionLookupFactory = new RepositorySpecificEntityRevisionLookupFactory(
+			$entityIdParserFactory,
+			new ForbiddenSerializer( 'Entity serialization is not supported on the client!' ),
+			new RepositorySpecificDataValueDeserializerFactory( $entityIdParserFactory ),
+			// TODO: rebase on top of Ibc74446062799f0a3b5419808875e06a682b0812
+			// should also get deserializer factory callbacks injected
+			$entityNamespaceLookup,
+			$clientSettings->getSetting( 'maxSerializedEntitySize' ) * 1024,
+			$this->getDatabaseNameMap( $clientSettings )
+		);
+
+		$this->repositoryNames = array_merge(
+			[ '' ],
+			array_keys( $clientSettings->getSetting( 'foreignRepositories' ) )
+		);
+
+		$this->initRepositoryServiceContainers( $clientSettings );
+	}
+
+	/**
+	 * @param SettingsArray $settings
+	 * @return string[]
+	 */
+	private function getDatabaseNameMap( SettingsArray $settings ) {
+		$names = [ '' => $settings->getSetting( 'repoDatabase' ) ];
+
+		$foreignRepositorySettings = $settings->getSetting( 'foreignRepositories' );
+		foreach ( $foreignRepositorySettings as $repositoryName => $repositorySettings ) {
+			$names[$repositoryName] = $repositorySettings['repoDatabase'];
+		}
+
+		return $names;
+	}
+
+	private function initRepositoryServiceContainers( SettingsArray $settings ) {
+		foreach ( $this->repositoryNames as $repositoryName ) {
+			$this->repositoryServiceContainers[$repositoryName] = new RepositoryServiceContainer(
+				$repositoryName,
+				$settings->getSetting( 'RepositoryServiceWiringFiles' ),
+				[ $this->entityRevisionLookupFactory ]
+			);
+		}
+	}
+
+	public function getServiceMap( $service ) {
+		$serviceMap = [];
+		foreach ( $this->repositoryNames as $repositoryName ) {
+			$serviceMap[$repositoryName] =  $this->repositoryServiceContainers[$repositoryName]->getService( $service );
+		}
+		return $serviceMap;
+	}
+
+}
