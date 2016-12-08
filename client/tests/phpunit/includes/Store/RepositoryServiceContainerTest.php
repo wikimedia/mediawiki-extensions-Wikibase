377f2ddc0192190242ff7618cc19fbafb60f21f1
Leszek Manicki <leszek.manicki@wikimedia.de>
2016-12-08 18:39:59 +0100

    [WIP] Add RepositoryServiceContainer and the wiring file
    TODO:
     - add more services
    
    Bug: T148141
    Change-Id: Ie28f41133e092157a7a4b0bc08003c69238e860d

---
+++ client/tests/phpunit/includes/Store/RepositoryServiceContainerTest.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Wikibase\Client\Tests\Store;
+
+use HashSiteStore;
+use Language;
+use Wikibase\Client\Store\RepositoryServiceContainer;
+use Wikibase\Client\WikibaseClient;
+use Wikibase\DataModel\DeserializerFactory;
+use Wikibase\Lib\DataTypeDefinitions;
+use Wikibase\Lib\EntityTypeDefinitions;
+use Wikibase\Lib\Store\EntityRevisionLookup;
+use Wikibase\SettingsArray;
+use Wikibase\TermIndex;
+
+/**
+ * @covers Wikibase\Client\Store\RepositoryServiceContainer
+ *
+ * @group Wikibase
+ * @group WikibaseClient
+ *
+ * @license GPL-2.0+
+ */
+class RepositoryServiceContainerTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * @return WikibaseClient
+	 */
+	private function getWikibaseClient() {
+		$settings = WikibaseClient::getDefaultInstance()->getSettings()->getArrayCopy();
+		$settings['foreignRepositories'] = [
+			'foo' => [ 'repoDatabase' => 'foowiki', 'prefixMapping' => [ 'bar' => 'xyz' ] ]
+		];
+
+		return new WikibaseClient(
+			new SettingsArray( $settings ),
+			Language::factory( 'en' ),
+			new DataTypeDefinitions( [] ),
+			$this->getEntityTypeDefinitions(),
+			new HashSiteStore()
+		);
+	}
+
+	/**
+	 * Provides a dummy entity type definitions to satisfy asserts of RepositorySpecificEntityRevisionLookupFactory
+	 *
+	 * @return EntityTypeDefinitions
+	 */
+	private function getEntityTypeDefinitions() {
+		return new EntityTypeDefinitions( [
+			'item' => [
+				'deserializer-factory-callback' => function( DeserializerFactory $deserializerFactory ) {
+					return $deserializerFactory->newItemDeserializer();
+				}
+			],
+		] );
+	}
+
+	/**
+	 * @return RepositoryServiceContainer
+	 */
+	private function getRepositoryServiceContainer() {
+		return new RepositoryServiceContainer(
+			'foowiki',
+			'foo',
+			$this->getWikibaseClient(),
+			[ __DIR__ . '/../../../../includes/Store/RepositoryServiceWiring.php' ] // TODO: horrible!
+		);
+	}
+
+	public function provideServices() {
+		return [
+			[ 'EntityRevisionLookup', EntityRevisionLookup::class ],
+			[ 'TermIndex', TermIndex::class ]
+		];
+	}
+
+	/**
+	 * @dataProvider provideServices
+	 */
+	public function testGetService( $serviceName, $expectedClass ) {
+		$repositoryServiceContainer = $this->getRepositoryServiceContainer();
+
+		$serviceOne = $repositoryServiceContainer->getService( $serviceName );
+		$serviceTwo = $repositoryServiceContainer->getService( $serviceName );
+
+		$this->assertInstanceOf( $expectedClass, $serviceOne );
+		$this->assertInstanceOf( $expectedClass, $serviceTwo );
+
+		$this->assertSame( $serviceOne, $serviceTwo );
+	}
+
+	public function testGetServiceNames() {
+		$repositoryServiceContainer = $this->getRepositoryServiceContainer();
+
+		$this->assertEquals(
+			[ 'EntityRevisionLookup', 'TermIndex' ],
+			$repositoryServiceContainer->getServiceNames()
+		);
+	}
+
+	public function testGetRepositoryName() {
+		$repositoryServiceContainer = $this->getRepositoryServiceContainer();
+
+		$this->assertEquals( 'foo', $repositoryServiceContainer->getRepositoryName() );
+	}
+
+	public function testGetDatabaseName() {
+		$repositoryServiceContainer = $this->getRepositoryServiceContainer();
+
+		$this->assertEquals( 'foowiki', $repositoryServiceContainer->getDatabaseName() );
+	}
+
+}
