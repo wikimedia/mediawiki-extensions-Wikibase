b4913d15229aaf081f6c33c94eb56b6e5b789f16
Leszek Manicki <leszek.manicki@wikimedia.de>
2016-12-08 18:41:09 +0100

    [WIP] Add DispatchingServiceFactory and the wiring file
    TODO:
     - add more services
     - document new settings in docs/options.wiki
    
    Bug: T148141
    Change-Id: I1cd10515e8eb713efba1000e987ece03e652e96c

---
+++ client/includes/DispatchingServiceWiring.php
@@ -0,0 +1,34 @@
+<?php
+
+use Wikibase\Client\DispatchingServiceFactory;
+use Wikibase\Lib\Store\DispatchingEntityRevisionLookup;
+use Wikibase\Lib\Store\DispatchingTermBuffer;
+use Wikibase\Store\BufferingTermLookup;
+use Wikibase\TermIndex;
+
+/**
+ * @license GPL-2.0+
+ */
+
+return [
+
+	'EntityRevisionLookup' => function( DispatchingServiceFactory $dispatchingServiceFactory ) {
+		return new DispatchingEntityRevisionLookup(
+			$dispatchingServiceFactory->getServiceMap( 'EntityRevisionLookup' )
+		);
+	},
+
+	'TermBuffer' => function( DispatchingServiceFactory $dispatchingServiceFactory ) {
+		$termIndexes = $dispatchingServiceFactory->getServiceMap( 'TermIndex' );
+
+		$buffers = array_map(
+			function( TermIndex $termIndex ) {
+				return new BufferingTermLookup( $termIndex, 1000 ); // TODO: customize buffer sizes?
+			},
+			$termIndexes
+		);
+
+		return new DispatchingTermBuffer( $buffers );
+	}
+
+];
