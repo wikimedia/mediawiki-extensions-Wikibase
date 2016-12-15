1375467d91728dfcea4c8c9edcc55545674af65a
albe <aleksey.bekh-ivanov@wikimedia.de>
2016-12-15 14:12:45 +0100

    Add tests for SpecialNewItem and SpecialNewProperty pages
    Task: T150205
    Change-Id: If9187d02d16ee7177d49c0d5768655f8242af47f

---
+++ repo/tests/phpunit/includes/Specials/HtmlAssertionHelpers.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace Wikibase\Repo\Tests\Specials;
+
+/**
+ * @license GPL-2.0+
+ */
+trait HtmlAssertionHelpers {
+
+	/**
+	 * @param string $html
+	 * @param string $name
+	 */
+	protected function assertHtmlContainsInputWithName( $html, $name ) {
+		$matcher = [
+			'tag' => 'input',
+			'attributes' => [
+				'name' => $name,
+			],
+		];
+		$this->assertTag(
+			$matcher,
+			$html,
+			"Failed to find input element with name '{$name}'"
+		);
+	}
+
+	/**
+	 * @param string $html
+	 * @param string $name
+	 * @param string $value
+	 */
+	protected function assertHtmlContainsInputWithNameAndValue( $html, $name, $value ) {
+		$matcher = [
+			'tag' => 'input',
+			'attributes' => [
+				'name' => $name,
+				'value' => $value,
+			],
+		];
+		$this->assertTag(
+			$matcher,
+			$html,
+			"Failed to find input element with name '{$name}' and value '{$value}'"
+		);
+	}
+
+	/**
+	 * @param string $html
+	 * @param string $name
+	 */
+	protected function assertHtmlContainsSelectWithName( $html, $name ) {
+		$matcher = [
+			'tag' => 'select',
+			'attributes' => [
+				'name' => $name,
+			],
+		];
+		$this->assertTag(
+			$matcher,
+			$html,
+			"Failed to find select element with name '{$name}' in html:\n\n {$html}"
+		);
+	}
+
+	/**
+	 * @param string $html
+	 * @param string $name
+	 * @param string $value
+	 */
+	protected function assertHtmlContainsSelectWithNameAndSelectedValue( $html, $name, $value ) {
+		$matcher = [
+			'tag' => 'select',
+			'attributes' => [
+				'name' => $name,
+			],
+			'child' => [
+				'tag' => 'option',
+				'attributes' => [
+					'value' => $value,
+					'selected' => 'selected',
+				],
+			],
+		];
+		$this->assertTag(
+			$matcher,
+			$html,
+			"Failed to find select element with name '{$name}' and selected value '{$value}'" .
+			" in html:\n\n {$html}"
+		);
+	}
+
+	/**
+	 * @param string $html
+	 */
+	protected function assertHtmlContainsSubmitControl( $html ) {
+		$matchers = [
+			'button submit' => [
+				'tag' => 'button',
+				'attributes' => [
+					'type' => 'submit',
+				],
+			],
+			'input submit' => [
+				'tag' => 'input',
+				'attributes' => [
+					'type' => 'submit',
+				],
+			],
+		];
+
+		foreach ( $matchers as $matcher ) {
+			try {
+				$this->assertTag( $matcher, $html, "Failed to find submit element" );
+
+				return;
+			} catch ( \PHPUnit_Framework_ExpectationFailedException $exception ) {
+				continue;
+			}
+		}
+
+		$this->fail( "Failed to find submit element" );
+	}
+
+	protected function assertHtmlContainsErrorMessage( $html, $messageText ) {
+		$matcher = [
+			'tag' => 'div',
+			'class' => 'error',
+			'content' => 'regexp: /' . preg_quote( $messageText, '/' ) . '/ui',
+		];
+		$this->assertTag(
+			$matcher,
+			$html,
+			"Failed to find error message with text '{$messageText}' in html:\n\n {$html}"
+		);
+	}
+
+	/**
+	 * @param array $matcher Associative array of structure required by $options argument
+	 * 				of {@see \PHPUnit_Util_XML::findNodes}
+	 * @param string $htmlOrXml
+	 * @param string $message
+	 * @param bool $isHtml
+	 *
+	 *  @see \MediaWikiTestCase::assertTag
+	 */
+	abstract protected function assertTag( $matcher, $htmlOrXml, $message = '', $isHtml = true );
+
+	/**
+	 * @param string $message
+	 *
+	 * @see \PHPUnit_Framework_Assert::fail
+	 */
+	abstract public function fail( $message = '' );
+
+}
--- repo/tests/phpunit/includes/Specials/SpecialNewPropertyTest.php
+++ repo/tests/phpunit/includes/Specials/SpecialNewPropertyTest.php
@@ -3,6 +3,10 @@
 namespace Wikibase\Repo\Tests\Specials;
 
 use SpecialPageTestBase;
+use Wikibase\DataModel\Entity\EntityDocument;
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\DataModel\Entity\Property;
+use Wikibase\DataModel\Entity\PropertyId;
 use Wikibase\Repo\Specials\SpecialNewProperty;
 
 /**
@@ -23,65 +27,208 @@ use Wikibase\Repo\Specials\SpecialNewProperty;
  * @author John Erling Blad < jeblad@gmail.com >
  * @author Addshore
  */
-class SpecialNewPropertyTest extends SpecialPageTestBase {
+class SpecialNewPropertyTest extends SpecialNewEntityTest {
 
 	protected function newSpecialPage() {
 		return new SpecialNewProperty();
 	}
 
-	public function testExecute() {
-		//TODO: Verify that more of the output is correct.
-		//TODO: Verify that item creation works via a faux post request
+	public function testAllNecessaryFormFieldsArePresent_WhenRendered() {
 
-		$this->setMwGlobals( 'wgGroupPermissions', array( '*' => array( 'property-create' => true ) ) );
+		list( $html ) = $this->executeSpecialPage();
 
-		$matchers['label'] = array(
-			'tag' => 'div',
-			'attributes' => array(
-				'id' => 'wb-newentity-label',
-			),
-			'child' => array(
-				'tag' => 'input',
-				'attributes' => array(
-					'name' => 'label',
-				)
-			) );
-		$matchers['description'] = array(
-			'tag' => 'div',
-			'attributes' => array(
-				'id' => 'wb-newentity-description',
-			),
-			'child' => array(
-				'tag' => 'input',
-				'attributes' => array(
-					'name' => 'description',
-				)
-			) );
-		$matchers['submit'] = array(
-			'tag' => 'div',
-			'attributes' => array(
-				'id' => 'wb-newentity-submit',
-			),
-			'child' => array(
-				'tag' => 'button',
-				'attributes' => array(
-					'type' => 'submit',
-					'name' => 'submit',
-				)
-			) );
+		$this->assertHtmlContainsInputWithName( $html, 'lang' );
+		$this->assertHtmlContainsInputWithName( $html, 'label' );
+		$this->assertHtmlContainsInputWithName( $html, 'description' );
+		$this->assertHtmlContainsInputWithName( $html, 'aliases' );
+		$this->assertHtmlContainsSelectWithName( $html, 'datatype' );
+		$this->assertHtmlContainsSubmitControl( $html );
+	}
 
-		list( $output, ) = $this->executeSpecialPage( '' );
-		foreach ( $matchers as $key => $matcher ) {
-			$this->assertTag( $matcher, $output, "Failed to match html output with tag '{$key}''" );
+	public function testLabelAndDescriptionAndDataTypeValuesAreSetAccordingToSubpagePath_WhenRendered() {
+		$subPagePart1 = 'LabelText';
+		$subPagePart2 = 'DescriptionText';
+		$subPagePart3 = 'url';
+		$subPage = "{$subPagePart1}/{$subPagePart2}/{$subPagePart3}";
+
+		list( $html ) = $this->executeSpecialPage( $subPage );
+
+		$this->assertHtmlContainsInputWithNameAndValue( $html, 'label', $subPagePart1 );
+		$this->assertHtmlContainsInputWithNameAndValue( $html, 'description', $subPagePart2 );
+		$this->assertHtmlContainsSelectWithNameAndSelectedValue( $html, 'datatype', $subPagePart3 );
+	}
+
+	public function testFailsAndDisplaysAnError_WhenTryToCreateSecondPropertyWithTheSameLabel() {
+		$formData = [
+			'lang' => 'en',
+			'label' => 'label',
+			'description' => '',
+			'aliases' => '',
+			'datatype' => 'string',
+		];
+		$this->executeSpecialPage( '', new \FauxRequest( $formData, true ) );
+
+		list( $html ) = $this->executeSpecialPage( '', new \FauxRequest( $formData, true ) );
+
+		$this->assertHtmlContainsErrorMessage( $html, "already has label" );
+	}
+
+	/**
+	 * @return array[][]
+	 */
+	public function provideValidEntityCreationRequests() {
+		$labelIndex = 1;
+
+		return [
+			'only label is set' => [
+				[
+					'lang' => 'en',
+					'label' => 'label' . $labelIndex ++,
+					'description' => '',
+					'aliases' => '',
+					'datatype' => 'string',
+				],
+			],
+			'another language' => [
+				[
+					'lang' => 'fr',
+					'label' => 'label' . $labelIndex ++,
+					'description' => '',
+					'aliases' => '',
+					'datatype' => 'string',
+				],
+			],
+			'only description is set' => [
+				[
+					'lang' => 'en',
+					'label' => '',
+					'description' => 'desc',
+					'aliases' => '',
+					'datatype' => 'string',
+				],
+			],
+			'single alias' => [
+				[
+					'lang' => 'en',
+					'label' => '',
+					'description' => '',
+					'aliases' => 'alias',
+					'datatype' => 'string',
+				],
+			],
+			'multiple aliases' => [
+				[
+					'lang' => 'en',
+					'label' => '',
+					'description' => '',
+					'aliases' => 'alias1|alias2|alias3',
+					'datatype' => 'string',
+				],
+			],
+			'another datatype is set' => [
+				[
+					'lang' => 'en',
+					'label' => 'label' . $labelIndex ++,
+					'description' => '',
+					'aliases' => '',
+					'datatype' => 'url',
+				],
+			],
+			'all input is present' => [
+				[
+					'lang' => 'en',
+					'label' => 'label' . $labelIndex,
+					'description' => 'desc',
+					'aliases' => 'a1|a2',
+					'datatype' => 'url',
+				],
+			],
+		];
+	}
+
+	/**
+	 * Data provider method
+	 *
+	 * @return array[]
+	 */
+	public function provideInvalidEntityCreationRequests() {
+		return [
+			'unknown language' => [
+				[
+					'lang' => 'some-wierd-language',
+					'label' => 'label-that-does-not-exist-1',
+					'description' => '',
+					'aliases' => '',
+					'datatype' => 'string',
+				],
+				'language code was not recognized',
+			],
+			'unknown datatype' => [
+				[
+					'lang' => 'en',
+					'label' => 'label-that-does-not-exist-2',
+					'description' => '',
+					'aliases' => '',
+					'datatype' => 'unknown-datatype',
+				],
+				'Invalid data type specified',
+			],
+//			'bad user token' => [  // TODO Probably should be implemented
+//				[
+//				],
+//				'try again',
+//			],
+//			'all fields are empty' => [  // TODO Probably should be implemented
+//				[
+//				],
+//				'???'
+//			],
+		];
+	}
+
+	/**
+	 * @param string $url
+	 * @return EntityId
+	 */
+	protected function extractEntityIdFromUrl( $url ) {
+		$itemIdSerialization = preg_replace( '@^.*(P\d+)$@', '$1', $url );
+
+		return new PropertyId( $itemIdSerialization );
+	}
+
+	/**
+	 * @param array $form
+	 * @param EntityDocument $entity
+	 * @return void
+	 * @throws \Exception
+	 */
+	protected function assertEntityMatchesFormData( array $form, EntityDocument $entity ) {
+		$this->assertInstanceOf( Property::class, $entity );
+		/** @var Property $entity */
+
+		$language = $form['lang'];
+		if ( $form['label'] !== '' ) {
+			$this->assertSame(
+				$form['label'],
+				$entity->getLabels()->getByLanguage( $language )->getText()
+			);
 		}
 
-		list( $output, ) = $this->executeSpecialPage( 'LabelText/DescriptionText' );
-		$matchers['label']['child'][0]['attributes']['value'] = 'LabelText';
-		$matchers['description']['child'][0]['attributes']['value'] = 'DescriptionText';
-
-		foreach ( $matchers as $key => $matcher ) {
-			$this->assertTag( $matcher, $output, "Failed to match html output with tag '{$key}''" );
+		if ( $form['description'] !== '' ) {
+			$this->assertSame(
+				$form['description'],
+				$entity->getDescriptions()->getByLanguage( $language )->getText()
+			);
 		}
+
+		if ( $form['aliases'] !== '' ) {
+			$this->assertArrayEquals(
+				explode( '|', $form['aliases'] ),
+				$entity->getAliasGroups()->getByLanguage( $language )->getAliases()
+			);
+		}
+
+		$this->assertEquals( $form['datatype'], $entity->getDataTypeId() );
 	}
 
 }
