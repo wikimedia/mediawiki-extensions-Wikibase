349a47352d34564b2d320d9aa0babc7dd09c16c5
Aleksey Bekh-Ivanov (WMDE) <aleksey.bekh-ivanov@wikimedia.de>
2016-12-16 16:58:19 +0100

    Refactoring SpecialNewEntityAndSubclasses
      * Restructured SpecialNewEntity (*Property and *Item accordingly)
      * Changed some errors display type
      * Added test `all fields are empty` for item and property
      * Added NonExistentPage test for Item creation
      * Added assert for OOUI error message
      * Moved entity specific code to each special page
      * Removed 'lang' attribute - it's redundant and also not rendered anyway
      * Refactored tests to use FIELD_* constants of special pages
    
    Change-Id: I7f717f3eea2c0bff812aa9d03b4d00c6e123f23c

--- repo/includes/Specials/SpecialNewEntity.php
+++ repo/includes/Specials/SpecialNewEntity.php
@@ -4,11 +4,10 @@ namespace Wikibase\Repo\Specials;
 
 use Html;
 use HTMLForm;
-use InvalidArgumentException;
+use OutputPage;
 use Status;
 use Wikibase\CopyrightMessageBuilder;
 use Wikibase\DataModel\Entity\EntityDocument;
-use Wikibase\DataModel\Term\FingerprintProvider;
 use Wikibase\Lib\LanguageNameLookup;
 use Wikibase\Repo\WikibaseRepo;
 use Wikibase\Summary;
@@ -25,6 +24,7 @@ use Wikibase\View\LanguageDirectionalityLookup;
  * @author John Erling Blad < jeblad@gmail.com >
  * @author Bene* < benestar.wikimedia@gmail.com >
  * @author Adrian Heine <adrian.heine@wikimedia.de>
+ * @author Aleksey Bekh-Ivanov < aleksey.bekh-ivanov@wikimedia.de >
  */
 abstract class SpecialNewEntity extends SpecialWikibaseRepoPage {
 
@@ -36,39 +36,14 @@ abstract class SpecialNewEntity extends SpecialWikibaseRepoPage {
 	protected $parts = null;
 
 	/**
-	 * @var string|null
-	 */
-	private $label;
-
-	/**
-	 * @var string|null
-	 */
-	private $description;
-
-	/**
 	 * @var string[]
 	 */
-	private $aliases = [];
-
-	/**
-	 * @var string
-	 */
-	private $contentLanguageCode;
-
-	/**
-	 * @var string[]
-	 */
-	private $languageCodes;
+	protected $languageCodes;
 
 	/**
 	 * @var SpecialPageCopyrightView
 	 */
 	private $copyrightView;
-
-	/**
-	 * @var LanguageDirectionalityLookup
-	 */
-	private $languageDirectionalityLookup;
 
 	/**
 	 * @var LanguageNameLookup
@@ -92,7 +67,6 @@ abstract class SpecialNewEntity extends SpecialWikibaseRepoPage {
 			$settings->getSetting( 'dataRightsText' )
 		);
 		$this->languageCodes = $wikibaseRepo->getTermsLanguages()->getLanguages();
-		$this->languageDirectionalityLookup = $wikibaseRepo->getLanguageDirectionalityLookup();
 		$this->languageNameLookup = $wikibaseRepo->getLanguageNameLookup();
 	}
 
@@ -115,142 +89,25 @@ abstract class SpecialNewEntity extends SpecialWikibaseRepoPage {
 		$this->checkReadOnly();
 
 		$this->parts = ( $subPage === '' ? [] : explode( '/', $subPage ) );
-		$this->prepareArguments();
+
+		$form = $this->createForm();
+
+		$form->prepareForm();
+
+		/** @var Status|false $submitStatus `false` if form was not submitted */
+		$submitStatus = $form->tryAuthorizedSubmit();
+
+		if ( $submitStatus && $submitStatus->isGood() ) {
+			$this->redirectToEntityPage( $submitStatus->getValue() );
+
+			return;
+		}
 
 		$out = $this->getOutput();
 
-		$uiLanguageCode = $this->getLanguage()->getCode();
+		$this->displayBeforeForm( $out );
 
-		if ( $this->getRequest()->wasPosted()
-			&& $this->getUser()->matchEditToken( $this->getRequest()->getVal( 'wpEditToken' ) )
-		) {
-			if ( $this->hasSufficientArguments() ) {
-				$entity = $this->createEntity();
-
-				$status = $this->modifyEntity( $entity );
-
-				if ( $status->isGood() ) {
-					$summary = new Summary( 'wbeditentity', 'create' );
-					$summary->setLanguage( $uiLanguageCode );
-					$summary->addAutoSummaryArgs( $this->label, $this->description );
-
-					$status = $this->saveEntity(
-						$entity,
-						$summary,
-						$this->getRequest()->getVal( 'wpEditToken' ),
-						EDIT_NEW
-					);
-
-					$out = $this->getOutput();
-
-					if ( !$status->isOK() ) {
-						$out->addHTML( '<div class="error">' );
-						$out->addWikiText( $status->getWikiText() );
-						$out->addHTML( '</div>' );
-					} elseif ( $entity !== null ) {
-						$title = $this->getEntityTitle( $entity->getId() );
-						$entityUrl = $title->getFullURL();
-						$this->getOutput()->redirect( $entityUrl );
-					}
-				} else {
-					$out->addHTML( '<div class="error">' );
-					$out->addHTML( $status->getHTML() );
-					$out->addHTML( '</div>' );
-				}
-			}
-		}
-
-		$this->getOutput()->addModuleStyles( [ 'wikibase.special' ] );
-
-		foreach ( $this->getWarnings() as $warning ) {
-			$out->addHTML( Html::element( 'div', [ 'class' => 'warning' ], $warning ) );
-		}
-
-		$this->createForm( $this->getLegend(), $this->additionalFormElements() );
-	}
-
-	/**
-	 * Tries to extract argument values from web request or of the page's sub-page parts
-	 *
-	 * Trimming argument values from web request.
-	 *
-	 * @since 0.1
-	 */
-	protected function prepareArguments() {
-		$label = $this->getRequest()->getVal(
-			'label',
-			isset( $this->parts[0] ) ? $this->parts[0] : ''
-		);
-		$this->label = $this->stringNormalizer->trimToNFC( $label );
-
-		$description = $this->getRequest()->getVal(
-			'description',
-			isset( $this->parts[1] ) ? $this->parts[1] : ''
-		);
-		$this->description = $this->stringNormalizer->trimToNFC( $description );
-
-		$aliases = $this->getRequest()->getVal( 'aliases' );
-		$explodedAliases = $aliases === null ? [] : explode( '|', $aliases );
-		foreach ( $explodedAliases as $alias ) {
-			$alias = $this->stringNormalizer->trimToNFC( $alias );
-
-			if ( $alias !== '' ) {
-				$this->aliases[] = $alias;
-			}
-		}
-
-		$this->contentLanguageCode = $this->getRequest()->getVal( 'lang', $this->getLanguage()->getCode() );
-	}
-
-	/**
-	 * Checks whether required arguments are set sufficiently
-	 *
-	 * @since 0.1
-	 *
-	 * @return bool
-	 */
-	protected function hasSufficientArguments() {
-		return $this->label !== ''
-			|| $this->description !== ''
-			|| $this->aliases !== [];
-	}
-
-	/**
-	 * @since 0.1
-	 *
-	 * @return EntityDocument Created entity of correct subtype
-	 */
-	abstract protected function createEntity();
-
-	/**
-	 * Attempt to modify entity
-	 *
-	 * @since 0.1
-	 *
-	 * @param EntityDocument $entity
-	 *
-	 * @throws InvalidArgumentException
-	 * @return Status
-	 */
-	protected function modifyEntity( EntityDocument $entity ) {
-		if ( !( $entity instanceof FingerprintProvider ) ) {
-			throw new InvalidArgumentException( '$entity must be a FingerprintProvider' );
-		}
-
-		$fingerprint = $entity->getFingerprint();
-		$status = Status::newGood();
-
-		$languageCode = $this->contentLanguageCode;
-		if ( !in_array( $languageCode, $this->languageCodes ) ) {
-			$status->error( 'wikibase-newitem-not-recognized-language' );
-			return $status;
-		}
-
-		$fingerprint->setLabel( $languageCode, $this->label );
-		$fingerprint->setDescription( $languageCode, $this->description );
-		$fingerprint->setAliasGroup( $languageCode, $this->aliases );
-
-		return $status;
+		$form->displayForm( $submitStatus ?: Status::newGood() );
 	}
 
 	/**
@@ -258,7 +115,7 @@ abstract class SpecialNewEntity extends SpecialWikibaseRepoPage {
 	 *
 	 * @return string[]
 	 */
-	private function getLanguageOptions() {
+	protected function getLanguageOptions() {
 		$languageOptions = [];
 		foreach ( $this->languageCodes as $code ) {
 			$languageName = $this->languageNameLookup->getName( $code );
@@ -270,88 +127,7 @@ abstract class SpecialNewEntity extends SpecialWikibaseRepoPage {
 	/**
 	 * @return array[]
 	 */
-	protected function additionalFormElements() {
-		$this->getOutput()->addModules( 'wikibase.special.languageLabelDescriptionAliases' );
-
-		$langCode = $this->contentLanguageCode;
-		$langDir = $this->languageDirectionalityLookup->getDirectionality( $this->contentLanguageCode );
-		return [
-			'lang' => [
-				'name' => 'lang',
-				'options' => $this->getLanguageOptions(),
-				'default' => $langCode,
-				'type' => 'combobox',
-				'id' => 'wb-newentity-language',
-				'label-message' => 'wikibase-newentity-language'
-			],
-			'label' => [
-				'name' => 'label',
-				'default' => $this->label ?: '',
-				'type' => 'text',
-				'id' => 'wb-newentity-label',
-				'lang' => $langCode,
-				'dir' => $langDir,
-				'placeholder' => $this->msg(
-					'wikibase-label-edit-placeholder'
-				)->text(),
-				'label-message' => 'wikibase-newentity-label'
-			],
-			'description' => [
-				'name' => 'description',
-				'default' => $this->description ?: '',
-				'type' => 'text',
-				'id' => 'wb-newentity-description',
-				'lang' => $langCode,
-				'dir' => $langDir,
-				'placeholder' => $this->msg(
-					'wikibase-description-edit-placeholder'
-				)->text(),
-				'label-message' => 'wikibase-newentity-description'
-			],
-			'aliases' => [
-				'name' => 'aliases',
-				'default' => $this->aliases ? implode( '|', $this->aliases ) : '',
-				'type' => 'text',
-				'id' => 'wb-newentity-aliases',
-				'lang' => $langCode,
-				'dir' => $langDir,
-				'placeholder' => $this->msg(
-					'wikibase-aliases-edit-placeholder'
-				)->text(),
-				'label-message' => 'wikibase-newentity-aliases'
-			]
-		];
-	}
-
-	/**
-	 * Building the HTML form for creating a new item.
-	 *
-	 * @param string|null $legend initial value for the label input box
-	 * @param array[] $additionalFormElements initial value for the description input box
-	 */
-	private function createForm( $legend = null, array $additionalFormElements ) {
-		$this->addCopyrightText();
-
-		$this->getOutput()->addModules( 'wikibase.special.newEntity' );
-
-		HTMLForm::factory( 'ooui', $additionalFormElements, $this->getContext() )
-			->setId( 'mw-newentity-form1' )
-			->setSubmitID( 'wb-newentity-submit' )
-			->setSubmitName( 'submit' )
-			->setSubmitTextMsg( 'wikibase-newentity-submit' )
-			->setWrapperLegendMsg( $legend )
-			->setSubmitCallback( function () {// no-op
-			} )->show();
-	}
-
-	/**
-	 * @todo could factor this out into a special page form builder and renderer
-	 */
-	private function addCopyrightText() {
-		$html = $this->copyrightView->getHtml( $this->getLanguage(), 'wikibase-newentity-submit' );
-
-		$this->getOutput()->addHTML( $html );
-	}
+	abstract protected function getFormFields();
 
 	/**
 	 * @since 0.1
@@ -368,5 +144,92 @@ abstract class SpecialNewEntity extends SpecialWikibaseRepoPage {
 	 * @return string[] Warnings that should be presented to the user
 	 */
 	abstract protected function getWarnings();
+
+	/**
+	 * @return HTMLForm
+	 */
+	private function createForm() {
+		return HTMLForm::factory( 'ooui', $this->getFormFields(), $this->getContext() )
+			->setId( 'mw-newentity-form1' )
+			->setSubmitID( 'wb-newentity-submit' )
+			->setSubmitName( 'submit' )
+			->setSubmitTextMsg( 'wikibase-newentity-submit' )
+			->setWrapperLegendMsg( $this->getLegend() )
+			->setSubmitCallback(
+				function ( $data, HTMLForm $form ) {
+					$validationStatus = $this->validateFormData( $data );
+					if ( !$validationStatus->isGood() ) {
+						return $validationStatus;
+					}
+
+					$entity = $this->createEntityFromFormData( $data );
+
+					$summary = $this->createSummary( $entity );
+
+					$saveStatus = $this->saveEntity(
+						$entity,
+						$summary,
+						$form->getRequest()->getVal( 'wpEditToken' ),
+						EDIT_NEW
+					);
+
+					if ( !$saveStatus->isGood() ) {
+						return $saveStatus;
+					}
+
+					return Status::newGood( $entity );
+				}
+			);
+	}
+
+	/**
+	 * @param array $formData
+	 *
+	 * @return EntityDocument
+	 */
+	abstract protected function createEntityFromFormData( array $formData );
+
+	/**
+	 * @param array $formData
+	 *
+	 * @return Status
+	 */
+	abstract protected function validateFormData( array $formData );
+
+	/**
+	 * @param EntityDocument $entity
+	 *
+	 * @return Summary
+	 */
+	abstract protected function createSummary( $entity );
+
+	/**
+	 * @return string
+	 */
+	private function getCopyrightText() {
+		return $this->copyrightView->getHtml( $this->getLanguage(), 'wikibase-newentity-submit' );
+	}
+
+	/**
+	 * @param OutputPage $output
+	 */
+	protected function displayBeforeForm( OutputPage $output ) {
+		$output->addModules( 'wikibase.special.newEntity' );
+
+		$output->addHTML( $this->getCopyrightText() );
+
+		foreach ( $this->getWarnings() as $warning ) {
+			$output->addHTML( Html::element( 'div', [ 'class' => 'warning' ], $warning ) );
+		}
+	}
+
+	/**
+	 * @param EntityDocument $entity
+	 */
+	private function redirectToEntityPage( EntityDocument $entity ) {
+		$title = $this->getEntityTitle( $entity->getId() );
+		$entityUrl = $title->getFullURL();
+		$this->getOutput()->redirect( $entityUrl );
+	}
 
 }
