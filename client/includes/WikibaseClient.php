2e29929dbbf5d62d013465908feb0fc47c0a209a
Gergő Tisza <gtisza@wikimedia.org>
2016-12-20 02:39:46 +0000

    Remove warning about missing site ID
    Seems to interfere with qunit somehow.
    
    Bug: T153729
    Bug: T153597
    Change-Id: I1a52ce07943f20ccd63f5d357c5e006cd721da64
    (cherry picked from commit 64a6e066509c3aa86ff71dafad17630079726253)

--- client/includes/WikibaseClient.php
+++ client/includes/WikibaseClient.php
@@ -713,8 +713,6 @@ final class WikibaseClient {
 			$site = $this->getSiteStore()->getSite( $siteId );
 
 			if ( !$site ) {
-				wfWarn( 'Cannot find site ' . $siteId . ' in sites table' );
-
 				return true;
 			}
 
