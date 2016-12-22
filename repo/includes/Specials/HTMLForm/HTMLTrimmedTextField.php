8708921a43e26f92da84adb664cc1d4b0d4d65c0
Aleksey Bekh-Ivanov (WMDE) <aleksey.bekh-ivanov@wikimedia.de>
2016-12-22 16:47:25 +0100

    Introduce trimmed text form field
     Simplifies form creation - no need to define filter callback
    
    Change-Id: I7d3188a6780f44813614f75337f1d54dcde44608

---
+++ repo/includes/Specials/HTMLForm/HTMLTrimmedTextField.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Wikibase\Repo\Specials\HTMLForm;
+
+use HTMLTextField;
+use Wikibase\StringNormalizer;
+
+class HTMLTrimmedTextField extends HTMLTextField {
+
+	/**
+	 * @var StringNormalizer
+	 */
+	private $stringNormalizer;
+
+	public function __construct( array $params ) {
+		parent::__construct( $params );
+
+		$this->stringNormalizer = new StringNormalizer();
+	}
+
+	public function filter( $value, $alldata ) {
+		$filteredValue = $this->stringNormalizer->trimToNFC( $value );
+
+		return parent::filter( $filteredValue, $alldata );
+	}
+
+}
