e35851339cf3f48a4a4933005efa273dc263f0c1
Aleksey Bekh-Ivanov (WMDE) <aleksey.bekh-ivanov@wikimedia.de>
2016-12-23 15:12:03 +0100

    Introduced form field for content language
    Change-Id: I1c432364143a346693f1e6cbc5085e83c7ecd2d6

--- repo/includes/Specials/SpecialNewProperty.php
+++ repo/includes/Specials/SpecialNewProperty.php
@@ -9,6 +9,7 @@ use Wikibase\DataModel\Term\Term;
 use Wikibase\DataTypeSelector;
 use Wikibase\Repo\Specials\HTMLForm\HTMLAliasesField;
 use Wikibase\Repo\Specials\HTMLForm\HTMLTrimmedTextField;
+use Wikibase\Repo\Specials\HTMLForm\HTMLContentLanguageField;
 use Wikibase\Repo\WikibaseRepo;
 use Wikibase\Summary;
 
@@ -72,19 +73,8 @@ class SpecialNewProperty extends SpecialNewEntity {
 		$formFields = [
 			self::FIELD_LANG => [
 				'name' => self::FIELD_LANG,
-				'options' => $this->getLanguageOptions(),
-				'default' => $langCode,
-				'type' => 'combobox',
+				'class' => HTMLContentLanguageField::class,
 				'id' => 'wb-newentity-language',
-				'filter-callback' => [ $this->stringNormalizer, 'trimToNFC' ],
-				'validation-callback' => function ( $language ) {
-					if ( !in_array( $language, $this->languageCodes ) ) {
-						return [ $this->msg( 'wikibase-newitem-not-recognized-language' )->text() ];
-					}
-
-					return true;
-				},
-				'label-message' => 'wikibase-newentity-language'
 			],
 			self::FIELD_LABEL => [
 				'name' => self::FIELD_LABEL,
