da6a41ff1f6de750c612ce37b4af9b4ed1fd7a11
Aleksey Bekh-Ivanov (WMDE) <aleksey.bekh-ivanov@wikimedia.de>
2016-12-23 15:12:03 +0100

    Introduced form field for content language
    Change-Id: I1c432364143a346693f1e6cbc5085e83c7ecd2d6

---
+++ repo/includes/Specials/HTMLForm/HTMLContentLanguageField.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Wikibase\Repo\Specials\HTMLForm;
+
+use Doctrine\Instantiator\Exception\InvalidArgumentException;
+use HTMLComboboxField;
+use Language;
+
+class HTMLContentLanguageField extends HTMLComboboxField {
+
+	public function __construct( array $params ) {
+		$defaultParameters = [
+			'label-message' => 'wikibase-content-language-edit-label',
+		];
+
+		if (isset($params['parent']) && $params['parent'] instanceof \IContextSource) {
+			/** @var \IContextSource $form */
+			$form = $params['parent'];
+			$params['default'] = $form->getLanguage()->getCode();
+		}
+
+		if ( isset( $params['options'] )
+			 || isset( $params['options-message'] )
+			 || isset( $params['options-messages'] )
+		) {
+			throw new InvalidArgumentException(
+				"Can not set options for content language field. It already has it's own options"
+			);
+		}
+		$params['options'] = $this->getLanguageOptions();
+
+		parent::__construct( array_merge( $defaultParameters, $params ) );
+	}
+
+	/**
+	 * Get options for language selector
+	 *
+	 * @return string[]
+	 */
+	private function getLanguageOptions() {
+		$languageOptions = [];
+		foreach ( Language::fetchLanguageNames() as $code => $languageName ) {
+			$languageOptions["$languageName ($code)"] = $code;
+		}
+
+		return $languageOptions;
+	}
+
+	public function validate( $value, $alldata ) {
+		$options = $this->getOptions();
+		if ( array_search( $value, $options ) === false ) {
+			return $this->msg( 'wikibase-newitem-not-recognized-language' );
+		}
+
+		return parent::validate( $value, $alldata );
+	}
+
+}
