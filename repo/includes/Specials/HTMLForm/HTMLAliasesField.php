19cf6376f350c76caa55b2b68acc8aa07dc4f6f8
Aleksey Bekh-Ivanov (WMDE) <aleksey.bekh-ivanov@wikimedia.de>
2016-12-24 19:32:38 +0100

    Introduced form field for aliases
    Change-Id: Iaa94f1a33ba491255f1fdf809faa1be460677e42

---
+++ repo/includes/Specials/HTMLForm/HTMLAliasesField.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Wikibase\Repo\Specials\HTMLForm;
+
+use HTMLTextField;
+use Wikibase\StringNormalizer;
+
+/**
+ * Class representing generic alias field
+ *
+ * @license GPL-2.0+
+ */
+class HTMLAliasesField extends HTMLTextField {
+	const DELIMITER = '|';
+
+	/**
+	 * @var StringNormalizer
+	 */
+	private $stringNormalizer;
+
+	private $defaultParameters = [
+		'placeholder-message' => 'wikibase-aliases-edit-placeholder',
+		'label-message' => 'wikibase-aliases-edit-label',
+	];
+
+	/**
+	 * Can be used without label and placeholder - has some predefined values. <br>
+	 * Doesn't accept `filter-callback` parameter. <br>
+	 * Doesn't accept `type` parameter. <br>
+	 *
+	 * @inheritdoc
+	 *
+	 * @see \HTMLForm There is detailed description of $info array
+	 */
+	public function __construct( array $params ) {
+		if ( isset( $params['filter-callback'] ) ) {
+			throw new \InvalidArgumentException(
+				"Cannot use `filter-callback` for aliases field. It already has its own filtering"
+			);
+		}
+
+		if ( isset( $params['type'] ) ) {
+			throw new \InvalidArgumentException(
+				"Can not use `type` for aliases field"
+			);
+		}
+
+		$params['type'] = 'text';
+
+		parent::__construct( array_merge( $this->defaultParameters, $params ) );
+
+		$this->stringNormalizer = new StringNormalizer();
+	}
+
+	public function filter( $value, $alldata ) {
+		$aliases = explode( self::DELIMITER, $value );
+		$aliases = array_map( [ $this->stringNormalizer, 'trimToNFC' ], $aliases );
+
+		return array_values( array_filter( $aliases ) );
+	}
+
+	public function validate( $value, $alldata ) {
+		if ( isset( $this->mParams['required'] )
+			 && $this->mParams['required'] !== false
+			 && $value === []
+		) {
+			return $this->msg( 'htmlform-required' );
+		}
+
+		return parent::validate( $value, $alldata );
+	}
+
+	public function getOOUI( $value ) {
+		$value = $this->arrayToString( $value );
+
+		return parent::getOOUI( $value );
+	}
+
+	public function getTableRow( $value ) {
+		$value = $this->arrayToString( $value );
+
+		return parent::getTableRow( $value );
+	}
+
+	public function getDiv( $value ) {
+		$value = $this->arrayToString( $value );
+
+		return parent::getDiv( $value );
+	}
+
+	public function getRaw( $value ) {
+		$value = $this->arrayToString( $value );
+
+		return parent::getRaw( $value );
+	}
+
+	public function getVForm( $value ) {
+		$value = $this->arrayToString( $value );
+
+		return parent::getVForm( $value );
+	}
+
+	public function getInline( $value ) {
+		$value = $this->arrayToString( $value );
+
+		return parent::getInline( $value );
+	}
+
+	private function arrayToString( $value ) {
+		return implode( self::DELIMITER, $value );
+	}
+
+}
