46f6251fbb73e792ee3edb3e7e58375829383cfb
Aleksey Bekh-Ivanov (WMDE) <aleksey.bekh-ivanov@wikimedia.de>
2016-12-25 00:18:08 +0100

    Introduced form field for content language
    Change-Id: I1c432364143a346693f1e6cbc5085e83c7ecd2d6

---
+++ repo/includes/Specials/HTMLForm/HTMLContentLanguageField.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Wikibase\Repo\Specials\HTMLForm;
+
+use InvalidArgumentException;
+use HTMLComboboxField;
+use Wikibase\Lib\LanguageNameLookup;
+use Wikibase\Repo\WikibaseRepo;
+
+class HTMLContentLanguageField extends HTMLComboboxField {
+
+	public function __construct( array $params ) {
+		$defaultParameters = [
+			'label-message' => 'wikibase-content-language-edit-label',
+		];
+
+		if ( isset( $params['parent'] ) && $params['parent'] instanceof \IContextSource ) {
+			/** @var \IContextSource $form */
+			$form = $params['parent'];
+			$params['default'] = $form->getLanguage()->getCode();
+		}
+
+		if ( isset( $params['options'] )
+			 || isset( $params['options-message'] )
+			 || isset( $params['options-messages'] )
+		) {
+			throw new InvalidArgumentException(
+				"Cannot set options for content language field. It already has it's own options"
+			);
+		}
+
+		$wikibaseRepo = WikibaseRepo::getDefaultInstance();
+		$contentLanguages = $wikibaseRepo->getTermsLanguages();
+		$params['options'] = $this->constructOptions(
+			$contentLanguages->getLanguages(),
+			$wikibaseRepo->getLanguageNameLookup()
+		);
+
+		parent::__construct( array_merge( $defaultParameters, $params ) );
+	}
+
+	private function constructOptions( array $languageCodes, LanguageNameLookup $lookup ) {
+		$languageOptions = [];
+		foreach ( $languageCodes as $code ) {
+			$languageName = $lookup->getName( $code );
+			$languageOptions["$languageName ($code)"] = $code;
+		}
+
+		return $languageOptions;
+	}
+
+	public function validate( $value, $alldata ) {
+		$options = $this->getOptions();
+		if ( array_search( $value, $options ) === false ) {
+			return $this->msg( 'wikibase-content-language-edit-not-recognized-language' );
+		}
+
+		return parent::validate( $value, $alldata );
+	}
+
+}
