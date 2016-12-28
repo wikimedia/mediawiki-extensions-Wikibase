bc05c86504e82002bf3fee48f17e3281df1355fb
Amir Sarabadani <Ladsgroup@gmail.com>
2016-12-28 06:16:01 +0330

    Introduce ChangeOpDeserializer interface
    Bug: T153950
    Change-Id: Ia6f8e6848a65f640085fbb5e20fb41ff74fcd146

---
+++ repo/includes/ChangeOp/ChangeOpDeserializer.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Wikibase\Repo\ChangeOp;
+
+/**
+ * Interface for services that construct ChangeOps from a JSON style array structure
+ *
+ * @license GPL-2.0+
+ * @author Amir Sarabadani <ladsgroup@gmail.com>
+ */
+interface ChangeOpDeserializer {
+
+	/**
+	 * @param array $data an array of data to deserialize. For example:
+	 *        [ 'label' => [ 'zh' => [ 'remove' ], 'de' => [ 'value' => 'Foo' ] ] ]
+	 * @return ChangeOp
+	 */
+	public function deserialize( array $serialization );
+
+}
