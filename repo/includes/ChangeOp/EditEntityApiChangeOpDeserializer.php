3a64d0e822023e7ff500d68e1062f6f9090523d6
Amir Sarabadani <Ladsgroup@gmail.com>
2016-12-28 06:16:01 +0330

    Intorduce EditEntityApiChangeOpDeserializer interface
    Bug: T153950
    Change-Id: Ia6f8e6848a65f640085fbb5e20fb41ff74fcd146

---
+++ repo/includes/ChangeOp/EditEntityApiChangeOpDeserializer.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Wikibase\Repo\ChangeOp;
+
+/**
+ * Deserializer for EditEntity API
+ *
+ * @license GPL-2.0+
+ * @author Amir Sarabadani <ladsgroup@gmail.com>
+ */
+interface EditEntityApiChangeOpDeserializer {
+
+	/**
+	 * @param array $data an array of data to apply. For example:
+	 *        [ 'label' => [ 'zh' => [ 'remove' ], 'de' => [ 'value' => 'Foo' ] ] ]
+	 * @param ChangeOps $changeOps ChangeOps to apploy
+	 * @param EntityDocument $entity entity to run validations. For example if it
+	 *        implements LabelsProvider when applying labels.
+	 * @return ChangeOps
+	 */
+	public function deserialize( array $data, ChangeOps $changeOps, EntityDocument $entity );
+
+}
