b4ea3ddd73c061006397b0120184c037e0344e68
addshore <addshorewiki@gmail.com>
2017-01-03 20:04:12 +0000

    Add Phan config
    See: https://www.mediawiki.org/wiki/Continuous_integration/Phan
    
    Change-Id: Ia496cdac4989bcab47c2bbb33754b582982c3db1

---
+++ tests/phan/stubs/babel.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Minimal set of classes necessary to fulfill needs of parts of Wikibase relying on
+ * the Babel extension.
+ * @codingStandardsIgnoreFile
+ */
+
+class Babel {
+	/**
+	 * @param User $user
+	 * @param string $level Minimal level as given in $wgBabelCategoryNames
+	 * @return string[] List of language codes
+	 */
+	public static function getUserLanguages( User $user, $level = null ) {
+	}
+}
