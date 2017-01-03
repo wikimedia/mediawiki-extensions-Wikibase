68d38402cb0cc20aa014889a5d9191f457a4089f
addshore <addshorewiki@gmail.com>
2017-01-03 20:04:12 +0000

    Add Phan config
    See: https://www.mediawiki.org/wiki/Continuous_integration/Phan
    
    Change-Id: Ia496cdac4989bcab47c2bbb33754b582982c3db1

---
+++ tests/phan/stubs/cirrussearch.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Minimal set of classes necessary to fulfill needs of parts of Wikibase relying on
+ * the CirrusSearch extension.
+ * @codingStandardsIgnoreFile
+ */
+
+class CirrusSearch {
+}
