bb74fdfd98e89fdb35bbedb325de067eac76b96f
jeroendedauw <jeroendedauw@gmail.com>
2017-01-07 02:59:31 +0100

    Make if predicates more explicit
    Change-Id: I8f0b80475d265e6ad7a3c625c2fa0d2cb569f378

--- repo/includes/Hooks/InfoActionHookHandler.php
+++ repo/includes/Hooks/InfoActionHookHandler.php
@@ -125,12 +125,12 @@ class InfoActionHookHandler {
 	 */
 	private function formatSubscription( $subscription, Title $title ) {
 		$site = $this->siteLookup->getSite( $subscription );
-		if ( !$site ) {
+		if ( $site === null ) {
 			return $subscription;
 		}
 
 		$url = $site->getPageUrl( 'Special:EntityUsage/' . $title->getText() );
-		if ( !$url ) {
+		if ( $url === false ) {
 			return $subscription;
 		}
 		$element = Html::element( 'a',
