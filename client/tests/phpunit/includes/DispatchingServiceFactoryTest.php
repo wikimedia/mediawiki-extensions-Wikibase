d0ccaa0e633c3e40c64e4e77e0ec633c27133aa8
Leszek Manicki <leszek.manicki@wikimedia.de>
2017-01-18 14:31:08 +0100

    Make DispatchingServiceFactory implement EntityStoreWatcher
    Prefetching EntityMetaDataAccessor used by EntityRevisionLookup
    can be watched by EntityWatchers, so that the cache
    is updated accordingly when prefetched entities are changed.
    For instance Repo's SqlStore instances do this.
    
    DispatchingServiceFactory should also allow its users
    to update prefetcher's cache when entity is updated.
    In order to do that DispatchingServiceFactory implements
    EntityStoreWatcher interface. When cache update request
    comes to DispatchingServiceFactory it delegates the request
    to the prefetching meta data accessor configured
    for the particular repository.
    
    Each repository's EntityRevisionLookup relies on
    the prefetcher mentioned above, so whenever underlying
    cache is purged, it is also reflected in results
    EntityRevisionLookup returns.
    
    Bug: T155633
    Change-Id: Id5dfc9ee9ae36a43f63b10e3ab195c66e0265af8

--- client/tests/phpunit/includes/DispatchingServiceFactoryTest.php
+++ client/tests/phpunit/includes/DispatchingServiceFactoryTest.php
@@ -2,11 +2,18 @@
 
 namespace Wikibase\Client\Tests;
 
+use DataValues\Deserializers\DataValueDeserializer;
 use Wikibase\Client\DispatchingServiceFactory;
 use Wikibase\Client\Store\RepositoryServiceContainer;
 use Wikibase\Client\Store\RepositoryServiceContainerFactory;
 use Wikibase\Client\WikibaseClient;
+use Wikibase\DataModel\Entity\BasicEntityIdParser;
+use Wikibase\DataModel\Entity\EntityRedirect;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\EntityRevision;
 use Wikibase\Lib\Store\EntityRevisionLookup;
+use Wikibase\Lib\Store\Sql\PrefetchingWikiPageEntityMetaDataAccessor;
 
 /**
  * @covers Wikibase\Client\DispatchingServiceFactory
@@ -48,15 +55,12 @@ class DispatchingServiceFactoryTest extends \PHPUnit_Framework_TestCase {
 	/**
 	 * @return DispatchingServiceFactory
 	 */
-	private function getDispatchingServiceFactory() {
+	private function getDispatchingServiceFactory( RepositoryServiceContainerFactory $containerFactory ) {
 		$client = WikibaseClient::getDefaultInstance();
 		$settings = $client->getSettings();
 		$settings->setSetting( 'foreignRepositories', [ 'foo' => [ 'repoDatabase' => 'foowiki' ] ] );
 
-		$factory = new DispatchingServiceFactory(
-			$this->getRepositoryServiceContainerFactory(),
-			[ '', 'foo' ]
-		);
+		$factory = new DispatchingServiceFactory( $containerFactory, [ '', 'foo' ] );
 
 		$factory->defineService( 'EntityRevisionLookup', function() {
 			return $this->getMock( EntityRevisionLookup::class );
@@ -66,7 +70,7 @@ class DispatchingServiceFactoryTest extends \PHPUnit_Framework_TestCase {
 	}
 
 	public function testGetServiceNames() {
-		$factory = $this->getDispatchingServiceFactory();
+		$factory = $this->getDispatchingServiceFactory( $this->getRepositoryServiceContainerFactory() );
 
 		$this->assertEquals(
 			[ 'EntityRevisionLookup' ],
@@ -75,7 +79,7 @@ class DispatchingServiceFactoryTest extends \PHPUnit_Framework_TestCase {
 	}
 
 	public function testGetServiceMap() {
-		$factory = $this->getDispatchingServiceFactory();
+		$factory = $this->getDispatchingServiceFactory( $this->getRepositoryServiceContainerFactory() );
 
 		$serviceMap = $factory->getServiceMap( 'EntityRevisionLookup' );
 
@@ -87,7 +91,7 @@ class DispatchingServiceFactoryTest extends \PHPUnit_Framework_TestCase {
 	}
 
 	public function testGetService() {
-		$factory = $this->getDispatchingServiceFactory();
+		$factory = $this->getDispatchingServiceFactory( $this->getRepositoryServiceContainerFactory() );
 
 		$serviceOne = $factory->getService( 'EntityRevisionLookup' );
 		$serviceTwo = $factory->getService( 'EntityRevisionLookup' );
@@ -95,6 +99,87 @@ class DispatchingServiceFactoryTest extends \PHPUnit_Framework_TestCase {
 		$this->assertInstanceOf( EntityRevisionLookup::class, $serviceOne );
 		$this->assertInstanceOf( EntityRevisionLookup::class, $serviceTwo );
 		$this->assertSame( $serviceOne, $serviceTwo );
+	}
+
+	/**
+	 * @param string|false $dbName
+	 * @param string $repositoryName
+	 *
+	 * @return RepositoryServiceContainer
+	 */
+	private function getRepositoryServiceContainer( $dbName, $repositoryName ) {
+		return new RepositoryServiceContainer(
+			$dbName,
+			$repositoryName,
+			new BasicEntityIdParser(),
+			new DataValueDeserializer( [] ),
+			WikibaseClient::getDefaultInstance()
+		);
+	}
+
+	/**
+	 * @param string $event
+	 *
+	 * @return RepositoryServiceContainerFactory
+	 */
+	private function getRepositoryServiceContainerFactoryForEventTest( $event ) {
+		$localMetaDataAccessor = $this->getMockBuilder( PrefetchingWikiPageEntityMetaDataAccessor::class )
+			->disableOriginalConstructor()
+			->getMock();
+		$localMetaDataAccessor->expects( $this->never() )->method( $event );
+
+		$fooMetaDataAccessor = $this->getMockBuilder( PrefetchingWikiPageEntityMetaDataAccessor::class )
+			->disableOriginalConstructor()
+			->getMock();
+		$fooMetaDataAccessor->expects( $this->atLeastOnce() )->method( $event );
+
+		$localServiceContainer = $this->getRepositoryServiceContainer( false, '' );
+		$localServiceContainer->defineService( 'PrefetchingEntityMetaDataAccessor', function () use ( $localMetaDataAccessor ) {
+			return $localMetaDataAccessor;
+		} );
+
+		$fooServiceContainer = $this->getRepositoryServiceContainer( 'foo', 'foowiki' );
+		$fooServiceContainer->defineService( 'PrefetchingEntityMetaDataAccessor', function () use ( $fooMetaDataAccessor ) {
+			return $fooMetaDataAccessor;
+		} );
+
+		$containerFactory = $this->getMockBuilder( RepositoryServiceContainerFactory::class )
+			->disableOriginalConstructor()
+			->getMock();
+
+		$containerFactory->expects( $this->any() )
+			->method( 'getContainer' )
+			->will(
+				$this->returnCallback( function ( $container ) use ( $localServiceContainer, $fooServiceContainer ) {
+					return $container === '' ? $localServiceContainer : $fooServiceContainer;
+				} )
+			);
+
+		return $containerFactory;
+	}
+
+	public function testEntityUpdatedDelegatesEventToRepositorySpecificWatcher() {
+		$factory = $this->getDispatchingServiceFactory(
+			$this->getRepositoryServiceContainerFactoryForEventTest( 'entityUpdated' )
+		);
+
+		$factory->entityUpdated( new EntityRevision( new Item( new ItemId( 'foo:Q123' ) ) ) );
+	}
+
+	public function testEntityDeletedDelegatesEventToRepositorySpecificWatcher() {
+		$factory = $this->getDispatchingServiceFactory(
+			$this->getRepositoryServiceContainerFactoryForEventTest( 'entityDeleted' )
+		);
+
+		$factory->entityDeleted( new ItemId( 'foo:Q123' ) );
+	}
+
+	public function testRedirectUpdatedDelegatesEventToRepositorySpecificWatcher() {
+		$factory = $this->getDispatchingServiceFactory(
+			$this->getRepositoryServiceContainerFactoryForEventTest( 'redirectUpdated' )
+		);
+
+		$factory->redirectUpdated( new EntityRedirect( new ItemId( 'foo:Q123' ), new ItemId( 'foo:Q321' ) ), 100 );
 	}
 
 }
