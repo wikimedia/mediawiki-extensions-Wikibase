da3519969fd8ba38e017b228d9d71eb42f0e9b62
Leszek Manicki <leszek.manicki@wikimedia.de>
2017-01-18 14:31:08 +0100

    Make DispatchingServiceFactory implement EntityStoreWatcher
    Prefetching EntityMetaDataAccessor used by EntityRevisionLookup
    can be watched by EntityWatchers, so that the cache
    is updated accordingly when prefetched entities are changed.
    For instance Repo's SqlStore instances do this.
    
    DispatchingServiceFactory should also allow its users
    to update prefetcher's cache when entity is updated.
    In order to do that DispatchingServiceFactory implements
    EntityStoreWatcher interface. When cache update request
    comes to DispatchingServiceFactory it delegates the request
    to the prefetching meta data accessor configured
    for the particular repository.
    
    Each repository's EntityRevisionLookup relies on
    the prefetcher mentioned above, so whenever underlying
    cache is purged, it is also reflected in results
    EntityRevisionLookup returns.
    
    Bug: T155633
    Change-Id: Id5dfc9ee9ae36a43f63b10e3ab195c66e0265af8

--- client/includes/DispatchingServiceFactory.php
+++ client/includes/DispatchingServiceFactory.php
@@ -4,20 +4,26 @@ namespace Wikibase\Client;
 
 use MediaWiki\Services\ServiceContainer;
 use Wikibase\Client\Store\RepositoryServiceContainerFactory;
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\DataModel\Entity\EntityRedirect;
+use Wikibase\DataModel\Services\Entity\EntityPrefetcher;
 use Wikibase\DataModel\Services\Term\TermBuffer;
+use Wikibase\EntityRevision;
 use Wikibase\Lib\Store\EntityRevisionLookup;
+use Wikibase\Lib\Store\EntityStoreWatcher;
 use Wikibase\Lib\Store\PropertyInfoLookup;
+use Wikibase\Lib\Store\Sql\PrefetchingWikiPageEntityMetaDataAccessor;
 
 /**
  * A factory/locator of services dispatching the action to services configured for the
  * particular input, based on the repository the particular input entity belongs to.
  * Dispatching services provide a way of using entities from multiple repositories.
  *
- * Services are defined by loading a wiring array(s), or by using defineService method.
+ * Services are defined by loading wiring array(s), or by using defineService method.
  *
  * @license GPL-2.0+
  */
-class DispatchingServiceFactory extends ServiceContainer implements EntityDataRetrievalServiceFactory {
+class DispatchingServiceFactory extends ServiceContainer implements EntityDataRetrievalServiceFactory, EntityStoreWatcher {
 
 	/**
 	 * @var string[]
@@ -53,6 +59,61 @@ class DispatchingServiceFactory extends ServiceContainer implements EntityDataRe
 			$serviceMap[$repositoryName] = $this->repositoryServiceContainerFactory->getContainer( $repositoryName )->getService( $service );
 		}
 		return $serviceMap;
+	}
+
+	/**
+	 * @param string $repositoryName
+	 * @return PrefetchingWikiPageEntityMetaDataAccessor|null
+	 */
+	private function getPrefetchingMetaDataAccessorForRepository( $repositoryName ) {
+		/** @var PrefetchingWikiPageEntityMetaDataAccessor[] $entityPrefetchers */
+		$metaDataAccessors = $this->getServiceMap( 'PrefetchingEntityMetaDataAccessor' );
+
+		return isset( $metaDataAccessors[$repositoryName] ) ? $metaDataAccessors[$repositoryName] : null;
+	}
+
+	/**
+	 * @see EntityStoreWatcher::entityUpdated
+	 *
+	 * @param EntityRevision $entityRevision
+	 */
+	public function entityUpdated( EntityRevision $entityRevision ) {
+		$metaDataAccessor = $this->getPrefetchingMetaDataAccessorForRepository(
+			$entityRevision->getEntity()->getId()->getRepositoryName()
+		);
+
+		if ( $metaDataAccessor !== null ) {
+			$metaDataAccessor->entityUpdated( $entityRevision );
+		}
+	}
+
+	/**
+	 * @see EntityStoreWatcher::entityDeleted
+	 *
+	 * @param EntityId $entityId
+	 */
+	public function entityDeleted( EntityId $entityId ) {
+		$metaDataAccessor = $this->getPrefetchingMetaDataAccessorForRepository( $entityId->getRepositoryName() );
+
+		if ( $metaDataAccessor !== null ) {
+			$metaDataAccessor->entityDeleted( $entityId );
+		}
+	}
+
+	/**
+	 * @see EntityStoreWatcher::redirectUpdated
+	 *
+	 * @param EntityRedirect $entityRedirect
+	 * @param int $revisionId
+	 */
+	public function redirectUpdated( EntityRedirect $entityRedirect, $revisionId ) {
+		$metaDataAccessor = $this->getPrefetchingMetaDataAccessorForRepository(
+			$entityRedirect->getEntityId()->getRepositoryName()
+		);
+
+		if ( $metaDataAccessor !== null ) {
+			$metaDataAccessor->redirectUpdated( $entityRedirect, $revisionId );
+		}
 	}
 
 	/**
--- client/tests/phpunit/includes/Store/RepositoryServiceWiringTest.php
+++ client/tests/phpunit/includes/Store/RepositoryServiceWiringTest.php
@@ -6,6 +6,7 @@ use DataValues\Deserializers\DataValueDeserializer;
 use Wikibase\Client\Store\RepositoryServiceContainer;
 use Wikibase\Client\WikibaseClient;
 use Wikibase\DataModel\Entity\EntityIdParser;
+use Wikibase\DataModel\Services\Entity\EntityPrefetcher;
 use Wikibase\DataModel\Services\EntityId\PrefixMappingEntityIdParser;
 use Wikibase\Lib\Store\EntityRevisionLookup;
 use Wikibase\Lib\Store\PropertyInfoLookup;
@@ -39,6 +40,7 @@ class RepositoryServiceWiringTest extends \PHPUnit_Framework_TestCase {
 	public function provideServices() {
 		return [
 			[ 'EntityRevisionLookup', EntityRevisionLookup::class ],
+			[ 'PrefetchingEntityMetaDataAccessor', EntityPrefetcher::class ],
 			[ 'PropertyInfoLookup', PropertyInfoLookup::class ],
 			[ 'TermIndex', TermIndex::class ],
 		];
@@ -59,7 +61,7 @@ class RepositoryServiceWiringTest extends \PHPUnit_Framework_TestCase {
 		$container = $this->getRepositoryServiceContainer();
 
 		$this->assertEquals(
-			[ 'EntityRevisionLookup', 'PropertyInfoLookup', 'TermIndex' ],
+			[ 'EntityRevisionLookup', 'PrefetchingEntityMetaDataAccessor', 'PropertyInfoLookup', 'TermIndex' ],
 			$container->getServiceNames()
 		);
 	}
