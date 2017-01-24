e6a2c562c755128a1ab0e00ac9adcc6a37ee637c
Stanislav Malyshev <smalyshev@wikimedia.org>
2017-01-24 14:38:08 -0800

    Refactor search fields: use ContentHandler instead of hooks
    This makes existing fields indepenedent from Elastic and using
    standard field definitions from SearchEngine.
    Also removes usage of hooks since ContentHandler classes now
    can perform this function.
    
    NOTE: next patches will build on this one.
    
    Change-Id: If9e945a7730a2d9797b8b57ce08dec6b882f588b

---
+++ repo/tests/phpunit/includes/Search/Elastic/Fields/WikibaseNumericFieldTest.php
@@ -0,0 +1,47 @@
+<?php
+namespace Wikibase\Repo\Tests\Search\Elastic\Fields;
+
+use DummySearchIndexFieldDefinition;
+use PHPUnit_Framework_TestCase;
+use Wikibase\DataModel\Entity\EntityDocument;
+use Wikibase\Repo\Search\Elastic\Fields\WikibaseNumericField;
+
+/**
+ * Base class for testing numeric fields.
+ * @package Wikibase\Repo\Tests\Search\Elastic\Fields
+ */
+abstract class WikibaseNumericFieldTest extends PHPUnit_Framework_TestCase {
+
+	public function testGetMapping() {
+		$field = $this->getFieldObject();
+		$searchEngine = $this->getMockBuilder( 'SearchEngine' )->getMock();
+
+		$searchEngine->expects( $this->any() )
+			->method( 'makeSearchFieldMapping' )
+			->will( $this->returnCallback( function ( $name, $type ) {
+				return new DummySearchIndexFieldDefinition( $name, $type );
+			} ) );
+
+		$mapping = $field->getMapping( $searchEngine, get_class( $field ) )
+			->getMapping( $searchEngine );
+		$this->assertEquals( \SearchIndexField::INDEX_TYPE_INTEGER, $mapping['type'] );
+		$this->assertEquals( get_class( $field ), $mapping['name'] );
+	}
+
+	/**
+	 * @dataProvider getFieldDataProvider
+	 */
+	public function testGetFieldData( $expected, EntityDocument $entity ) {
+		$labelCountField = $this->getFieldObject();
+
+		$this->assertSame( $expected, $labelCountField->getFieldData( $entity ) );
+	}
+
+	abstract public function getFieldDataProvider();
+
+	/**
+	 * @return WikibaseNumericField
+	 */
+	abstract protected function getFieldObject();
+
+}
