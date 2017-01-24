350355a76ae4f44b23476bc41c7baa1139795d29
Stanislav Malyshev <smalyshev@wikimedia.org>
2017-01-24 14:38:08 -0800

    Refactor search fields: use ContentHandler instead of hooks
    This makes existing fields indepenedent from Elastic and using
    standard field definitions from SearchEngine.
    Also removes usage of hooks since ContentHandler classes now
    can perform this function.
    
    NOTE: next patches will build on this one.
    
    Change-Id: If9e945a7730a2d9797b8b57ce08dec6b882f588b

---
+++ repo/includes/Search/Elastic/Fields/WikibaseNumericField.php
@@ -0,0 +1,24 @@
+<?php
+namespace Wikibase\Repo\Search\Elastic\Fields;
+
+use SearchEngine;
+
+/**
+ * Generic numeric field
+ * @package Wikibase\Repo\Search\Elastic\Fields
+ */
+abstract class WikibaseNumericField implements SearchIndexField {
+
+	/**
+	 * @param SearchEngine $engine
+	 * @param string       $name
+	 * @return \SearchIndexField
+	 */
+	public function getMapping( SearchEngine $engine, $name ) {
+		return $engine->makeSearchFieldMapping(
+			$name,
+			\SearchIndexField::INDEX_TYPE_INTEGER
+		);
+	}
+
+}
