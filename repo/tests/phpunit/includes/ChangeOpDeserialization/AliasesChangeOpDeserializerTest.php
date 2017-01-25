862df97d22201a5bde20a5a7b7864c925ddc8552
Jakob Warkotsch <j.warkotsch@gmail.com>
2017-01-25 17:27:47 +0100

    [WIP] Add ChangeOpDeserializers for labels, aliases, and descriptions
    ChangeOp initialization logic is mainly copied from Api\EditEntity.
    
    Note: Comments below should be removed once not relevant!
    
    Moved out of I2977607b4b02bc56183cfa5a9a9a0dc57432964d to make
    review easier. Note: this change is a part of a bigger chain
    of patches. Refer to related patches to see how this fits
    a bigger picture.
    
    TODOs:
     - this should be referring some ticket but which one? T154288 or
       some more specific one?
    
    Change-Id: I07e829fadd2b38c232e1f865dfca2765d4b6620d

---
+++ repo/tests/phpunit/includes/ChangeOpDeserialization/AliasesChangeOpDeserializerTest.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Wikibase\Repo\Tests\ChangeOp;
+
+use RuntimeException;
+use Wikibase\ChangeOp\ChangeOps;
+use Wikibase\ChangeOp\FingerprintChangeOpFactory;
+use Wikibase\Repo\Api\ApiErrorReporter;
+use Wikibase\Repo\ChangeOpDeserialization\AliasesChangeOpDeserializer;
+use Wikibase\Repo\ChangeOpDeserialization\TermChangeOpSerializationValidator;
+use Wikibase\Repo\Tests\ChangeOpDeserialization\ChangeOpDeserializationTestHelper;
+use Wikibase\Repo\WikibaseRepo;
+
+/**
+ * @covers Wikibase\Repo\ChangeOpDeserialization\AliasesChangeOpDeserializer
+ *
+ * @group Wikibase
+ *
+ * @license GPL-2.0+
+ */
+class AliasesChangeOpDeserializerTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * @var ChangeOpDeserializationTestHelper
+	 */
+	private $testHelper;
+
+	public function setUp() {
+		$this->testHelper = new ChangeOpDeserializationTestHelper( $this );
+	}
+
+	public function testGivenAliasesFieldNotAnArray_createEntityChangeOpThrowsError() {
+		$this->setExpectedException( RuntimeException::class, 'not-recognized-array' );
+		$this->newAliasesChangeOpDeserializer(
+			$this->testHelper->getApiErrorReporter( true ),
+			$this->getFingerPrintChangeOpFactory()
+		)->createEntityChangeOp( [ 'aliases' => null ] );
+	}
+
+	public function testGivenInvalidLanguage_createEntityChangeOpThrowsError() {
+		$this->setExpectedException( RuntimeException::class, 'inconsistent-language' );
+		$this->newAliasesChangeOpDeserializer(
+			$this->testHelper->getApiErrorReporter( true ),
+			$this->getFingerPrintChangeOpFactory()
+		)->createEntityChangeOp( [ 'aliases' => [ 'en' => [ 'language' => 'de', 'value' => 'foo' ] ] ] );
+	}
+
+	public function testGivenChangeRequestWithNewAliase_callsNewSetAliaseOp() {
+		$factory = $this->getFingerPrintChangeOpFactory();
+		$factory->expects( $this->once() )
+			->method( 'newSetAliasesOp' )
+			->willReturn( new ChangeOps() );
+
+		$this->newAliasesChangeOpDeserializer( $this->testHelper->getApiErrorReporter( false ), $factory )
+			->createEntityChangeOp( [ 'aliases' => [ 'en' => [ 'language' => 'en', 'value' => 'foo' ] ] ] );
+	}
+
+	public function testGivenChangeRequestWithRemove_callsNewRemoveAliasesOp() {
+		$factory = $this->getFingerPrintChangeOpFactory();
+		$factory->expects( $this->once() )
+			->method( 'newRemoveAliasesOp' )
+			->willReturn( new ChangeOps() );
+
+		$this->newAliasesChangeOpDeserializer( $this->testHelper->getApiErrorReporter( false ), $factory )
+			->createEntityChangeOp(
+				[ 'aliases' => [
+					'en' => [ 'language' => 'en', 'value' => 'foo', 'remove' => '' ] ]
+				]
+			);
+	}
+
+	public function testGivenChangeRequestWithAdd_callsNewRemoveAliasesOp() {
+		$factory = $this->getFingerPrintChangeOpFactory();
+		$factory->expects( $this->once() )
+			->method( 'newAddAliasesOp' )
+			->willReturn( new ChangeOps() );
+
+		$this->newAliasesChangeOpDeserializer( $this->testHelper->getApiErrorReporter( false ), $factory )
+			->createEntityChangeOp(
+				[ 'aliases' => [
+					'en' => [ 'language' => 'en', 'value' => 'foo', 'add' => '' ] ]
+				]
+			);
+	}
+
+	private function newAliasesChangeOpDeserializer(
+		ApiErrorReporter $errorReporter,
+		FingerprintChangeOpFactory $factory
+	) {
+		return new AliasesChangeOpDeserializer(
+			$errorReporter,
+			$factory,
+			$this->getStringNormalizer(),
+			$this->getTermChangeOpValidator( $errorReporter )
+		);
+	}
+
+	private function getStringNormalizer() {
+		return WikibaseRepo::getDefaultInstance()->getStringNormalizer();
+	}
+
+	private function getFingerPrintChangeOpFactory() {
+		return $this->getMockBuilder( FingerprintChangeOpFactory::class )
+			->disableOriginalConstructor()
+			->getMock();
+	}
+
+	private function getTermChangeOpValidator( ApiErrorReporter $errorReporter ) {
+		return new TermChangeOpSerializationValidator(
+			WikibaseRepo::getDefaultInstance()->getTermsLanguages(),
+			$errorReporter
+		);
+	}
+
+}
