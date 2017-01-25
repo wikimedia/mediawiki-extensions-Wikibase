a123cb11ad256c59e9345f523b0438ec5054a8fc
Stanislav Malyshev <smalyshev@wikimedia.org>
2017-01-24 17:33:22 -0800

    Generalize field definitions for Items and Properties
    NOTE: Labels & descriptions not handled yet here, this will be next patch.
    
    NOTE: The change in EntityHandler will require updating MediaInfo handler too.
    
    Bug: T125500
    Change-Id: I781113f489267b092998a27c4ed76240db20f9ac

--- repo/tests/phpunit/includes/Content/EntityHandlerTest.php
+++ repo/tests/phpunit/includes/Content/EntityHandlerTest.php
@@ -485,6 +485,8 @@ abstract class EntityHandlerTest extends \MediaWikiTestCase {
 		$this->assertFalse( $this->getHandler()->supportsCategories() );
 	}
 
+	abstract protected function getExpectedFields();
+
 	public function testFieldsForSearchIndex() {
 		$handler = $this->getHandler();
 
@@ -497,8 +499,7 @@ abstract class EntityHandlerTest extends \MediaWikiTestCase {
 			} ) );
 
 		$fields = $handler->getFieldsForSearchIndex( $searchEngine );
-		$expectedFields = [ 'label_count', 'sitelink_count', 'statement_count' ];
-		foreach ( $expectedFields as $expected ) {
+		foreach ( $this->getExpectedFields() as $expected ) {
 			$this->assertInstanceOf( \SearchIndexField::class, $fields[$expected] );
 			$mapping = $fields[$expected]->getMapping( $searchEngine );
 			$this->assertEquals( $expected, $mapping['name'] );
