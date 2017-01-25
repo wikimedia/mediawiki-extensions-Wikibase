b95fcc51d81b3a09e3c48d0f4bc90d3c9c0801b8
Jakob Warkotsch <j.warkotsch@gmail.com>
2017-01-25 17:00:45 +0100

    [WIP] Add TermChangeOpValidator
    Note: Comments below should be removed once not relevant!
    
    Moved out of I2977607b4b02bc56183cfa5a9a9a0dc57432964d to make
    review easier. Note: this change is a part of a bigger chain
    of patches. Refer to related patches to see how this fits
    a bigger picture.
    
    TODOs:
     - numeric $langCode part not implemented yet.
     - this should be referring some ticket but which one? T154288 or
       some more specific one?
    
    Change-Id: Ia5141947523b4fcc94e388eb86a0701a7cbed2d5

---
+++ repo/includes/Validators/TermChangeOpSerializationValidator.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Wikibase\Repo\Validators;
+
+use ApiUsageException;
+use Wikibase\Lib\ContentLanguages;
+use Wikibase\Repo\Api\ApiErrorReporter;
+
+/**
+ * TODO: add class description
+ *
+ * @license GPL-2.0+
+ */
+class TermChangeOpSerializationValidator {
+
+	/**
+	 * @var ApiErrorReporter
+	 */
+	private $errorReporter;
+
+	/**
+	 * @var ContentLanguages
+	 */
+	private $termsLanguages;
+
+	public function __construct( ApiErrorReporter $errorReporter, ContentLanguages $termsLanguages ) {
+		$this->errorReporter = $errorReporter;
+		$this->termsLanguages = $termsLanguages;
+	}
+
+	/**
+	 * @param string[] $arg The argument array to verify
+	 * @param int|string $langCode The language code used in the value part
+	 *
+	 * @throws ApiUsageException
+	 */
+	public function validateMultilangArgs( $arg, $langCode ) {
+		$this->assertArray( $arg, 'An array was expected, but not found in the json for the '
+			. "langCode $langCode" );
+
+		if ( !array_key_exists( 'language', $arg ) ) {
+			$this->errorReporter->dieError(
+				"'language' was not found in the label or description json for $langCode",
+				'missing-language' );
+		}
+
+		$this->assertString( $arg['language'], 'A string was expected, but not found in the json '
+			. "for the langCode $langCode and argument 'language'" );
+		if ( !is_numeric( $langCode ) ) {
+			if ( $langCode !== $arg['language'] ) {
+				$this->errorReporter->dieError(
+					"inconsistent language: $langCode is not equal to {$arg['language']}",
+					'inconsistent-language' );
+			}
+		}
+
+		if ( !$this->termsLanguages->hasLanguage( $arg['language'] ) ) {
+			$this->errorReporter->dieError( 'Unknown language: ' . $arg['language'], 'not-recognized-language' );
+		}
+
+		if ( !array_key_exists( 'remove', $arg ) ) {
+			$this->assertString( $arg['value'], 'A string was expected, but not found in the json '
+				. "for the langCode $langCode and argument 'value'" );
+		}
+	}
+
+	/**
+	 * @param mixed $value
+	 * @param string $message
+	 */
+	private function assertArray( $value, $message ) {
+		$this->assertType( 'array', $value, $message );
+	}
+
+	/**
+	 * @param mixed $value
+	 * @param string $message
+	 */
+	private function assertString( $value, $message ) {
+		$this->assertType( 'string', $value, $message );
+	}
+
+	/**
+	 * @param string $type
+	 * @param mixed $value
+	 * @param string $message
+	 */
+	private function assertType( $type, $value, $message ) {
+		if ( gettype( $value ) !== $type ) {
+			$this->errorReporter->dieError( $message, 'not-recognized-' . $type );
+		}
+	}
+
+}
---
+++ repo/tests/phpunit/includes/Validators/TermChangeOpSerializationValidatorTest.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Wikibase\Repo\Tests\Validators;
+
+use RuntimeException;
+use Wikibase\Lib\StaticContentLanguages;
+use Wikibase\Repo\Api\ApiErrorReporter;
+use Wikibase\Repo\Validators\TermChangeOpSerializationValidator;
+
+/**
+ * @covers Wikibase\Repo\Validators\TermChangeOpValidator
+ *
+ * @group Wikibase
+ * @group WikibaseValidators
+ *
+ * @license GPL-2.0+
+ */
+class TermChangeOpSerializationValidatorTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * @dataProvider multilangArgsProvider
+	 */
+	public function testValidateMultilangArgs( $arg, $langCode, $errorCode ) {
+		$validator = new TermChangeOpSerializationValidator(
+			$this->getApiErrorReporter( $errorCode ),
+			$this->getContentLanguages()
+		);
+
+		if ( $errorCode !== false ) {
+			$this->setExpectedException( RuntimeException::class, $errorCode );
+		}
+
+		$validator->validateMultilangArgs( $arg, $langCode );
+	}
+
+	public function multilangArgsProvider() {
+		return [
+			'no language key' => [ [], 'en', 'missing-language' ],
+			'language not a string (bool)' => [ [ 'language' => false ], 'en', 'not-recognized-string' ],
+			'language not a string (int)' => [ [ 'language' => 3 ], 'en', 'not-recognized-string' ],
+			'language not a string (null)' => [ [ 'language' => null ], 'en', 'not-recognized-string' ],
+			'arg lang not matching langCode' => [
+				[ 'language' => 'en', 'value' => 'foo' ],
+				'de',
+				'inconsistent-language'
+			],
+			'unknown language' => [
+				[ 'language' => 'xx', 'value' => 'foo' ],
+				'xx',
+				'not-recognized-language'
+			],
+			'valid' => [
+				[ 'language' => 'en', 'value' => 'foo' ],
+				'en',
+				false
+			],
+			'valid numeric langcode' => [
+				[ 'language' => 'en', 'value' => 'foo' ],
+				'123',
+				false
+			],
+			'valid remove' => [
+				[ 'language' => 'en', 'remove' => true ],
+				'en',
+				false
+			],
+		];
+	}
+
+	private function getContentLanguages() {
+		return new StaticContentLanguages( [ 'en', 'de', 'fr' ] );
+	}
+
+	/**
+	 * TODO: Refactor to mock class or into test helper
+	 *
+	 * @param bool $expectsError
+	 *
+	 * @return ApiErrorReporter
+	 */
+	private function getApiErrorReporter( $expectsError = false ) {
+		$errorReporter = $this->getMockBuilder( ApiErrorReporter::class )
+			->disableOriginalConstructor()
+			->getMock();
+
+		if ( !$expectsError ) {
+			$errorReporter->expects( $this->never() )
+				->method( 'dieError' );
+		} else {
+			$errorReporter->expects( $this->once() )
+				->method( 'dieError' )
+				->willReturnCallback( function( $description, $errorCode ) {
+					throw new RuntimeException( $errorCode );
+				} );
+		}
+
+		return $errorReporter;
+	}
+
+}
