8ef19feba954d7a0368785ff818493bef02c0a14
Jakob Warkotsch <j.warkotsch@gmail.com>
2017-01-25 17:56:57 +0100

    Add factory of ChangeOpDeserializers
    Factory provides instances of deserializers dedicated
    to change requests of fields of items and properties:
    labels, aliases, descriptions, statements (claims),
    and site links.
    
    Change-Id: I851ac59e250960d389fd7ef76f1e78a60f037ee2

---
+++ repo/tests/phpunit/includes/ChangeOp/Deserialization/ChangeOpDeserializerFactoryTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Wikibase\Repo\Tests\ChangeOp\Deserialization;
+
+use Wikibase\Repo\ChangeOp\Deserialization\AliasesChangeOpDeserializer;
+use Wikibase\Repo\ChangeOp\Deserialization\ChangeOpDeserializerFactory;
+use Wikibase\Repo\ChangeOp\Deserialization\ClaimsChangeOpDeserializer;
+use Wikibase\Repo\ChangeOp\Deserialization\DescriptionsChangeOpDeserializer;
+use Wikibase\Repo\ChangeOp\Deserialization\LabelsChangeOpDeserializer;
+use Wikibase\Repo\ChangeOp\Deserialization\SiteLinkBadgeChangeOpSerializationValidator;
+use Wikibase\Repo\ChangeOp\Deserialization\SiteLinksChangeOpDeserializer;
+use Wikibase\Repo\ChangeOp\Deserialization\TermChangeOpSerializationValidator;
+use Wikibase\Repo\SiteLinkTargetProvider;
+use Wikibase\Repo\WikibaseRepo;
+
+/**
+ * @covers Wikibase\Repo\ChangeOp\Deserialization\ChangeOpDeserializerFactory
+ *
+ * @group Wikibase
+ *
+ * @license GPL-2.0+
+ */
+class ChangeOpDeserializerFactoryTest extends \PHPUnit_Framework_TestCase {
+
+	public function testGetLabelsChangeOpDeserializer() {
+		$this->assertInstanceOf(
+			LabelsChangeOpDeserializer::class,
+			$this->newWikibaseChangeOpDeserializerFactory()->getLabelsChangeOpDeserializer()
+		);
+	}
+
+	public function testGetDescriptionsChangeOpDeserializer() {
+		$this->assertInstanceOf(
+			DescriptionsChangeOpDeserializer::class,
+			$this->newWikibaseChangeOpDeserializerFactory()->getDescriptionsChangeOpDeserializer()
+		);
+	}
+
+	public function testGetAliasesChangeOpDeserializer() {
+		$this->assertInstanceOf(
+			AliasesChangeOpDeserializer::class,
+			$this->newWikibaseChangeOpDeserializerFactory()->getAliasesChangeOpDeserializer()
+		);
+	}
+
+	public function testGetClaimsChangeOpDeserializer() {
+		$this->assertInstanceOf(
+			ClaimsChangeOpDeserializer::class,
+			$this->newWikibaseChangeOpDeserializerFactory()->getClaimsChangeOpDeserializer()
+		);
+	}
+
+	public function testGetSiteLinksChangeOpDeserializer() {
+		$this->assertInstanceOf(
+			SiteLinksChangeOpDeserializer::class,
+			$this->newWikibaseChangeOpDeserializerFactory()->getSiteLinksChangeOpDeserializer()
+		);
+	}
+
+	private function newWikibaseChangeOpDeserializerFactory() {
+		$wikibaseRepo = WikibaseRepo::getDefaultInstance();
+		$changeOpFactoryProvider = $wikibaseRepo->getChangeOpFactoryProvider();
+
+		return new ChangeOpDeserializerFactory(
+			$changeOpFactoryProvider->getFingerprintChangeOpFactory(),
+			$changeOpFactoryProvider->getStatementChangeOpFactory(),
+			$changeOpFactoryProvider->getSiteLinkChangeOpFactory(),
+			new TermChangeOpSerializationValidator( $wikibaseRepo->getTermsLanguages() ),
+			new SiteLinkBadgeChangeOpSerializationValidator(
+				$wikibaseRepo->getEntityTitleLookup(),
+				[]
+			),
+			$wikibaseRepo->getExternalFormatStatementDeserializer(),
+			new SiteLinkTargetProvider( $wikibaseRepo->getSiteLookup(), [] ),
+			$wikibaseRepo->getEntityIdParser(),
+			$wikibaseRepo->getStringNormalizer(),
+			[]
+		);
+	}
+
+}
