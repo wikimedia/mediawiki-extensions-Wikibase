4ba027d944e264adf01da909efcb70a1e9d5fa68
Jakob Warkotsch <j.warkotsch@gmail.com>
2017-01-25 17:27:47 +0100

    [WIP] Add ChangeOpDeserializers for labels, aliases, and descriptions
    Note: Comments below should be removed once not relevant!
    
    Moved out of I2977607b4b02bc56183cfa5a9a9a0dc57432964d to make
    review easier. Note: this change is a part of a bigger chain
    of patches. Refer to related patches to see how this fits
    a bigger picture.
    
    TODOs:
     - names: plural vs singular Label*s*ChangeOpDeserializer
     - unit tests missing
     - this should be referring some ticket but which one? T154288 or
       some more specific one?
    
    Change-Id: I07e829fadd2b38c232e1f865dfca2765d4b6620d

---
+++ repo/includes/ChangeOpDeserializers/DescriptionsChangeOpDeserializer.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Wikibase\Repo\ChangeOpDeserializers;
+
+use Wikibase\ChangeOp\ChangeOp;
+use Wikibase\ChangeOp\ChangeOps;
+use Wikibase\ChangeOp\FingerprintChangeOpFactory;
+use Wikibase\Repo\Api\ApiErrorReporter;
+use Wikibase\Repo\ChangeOp\ChangeOpDeserializer;
+use Wikibase\Repo\Validators\TermChangeOpSerializationValidator;
+use Wikibase\StringNormalizer;
+
+/**
+ * Constructs ChangeOps for description change requests
+ *
+ * @license GPL-2.0+
+ */
+class DescriptionsChangeOpDeserializer implements ChangeOpDeserializer {
+
+	/**
+	 * @var ApiErrorReporter
+	 */
+	private $errorReporter;
+
+	/**
+	 * @var FingerprintChangeOpFactory
+	 */
+	private $fingerprintChangeOpFactory;
+
+	/**
+	 * @var StringNormalizer
+	 */
+	private $stringNormalizer;
+
+	/**
+	 * @var TermChangeOpSerializationValidator
+	 */
+	private $validator;
+
+	public function __construct(
+		ApiErrorReporter $errorReporter,
+		FingerprintChangeOpFactory $fingerprintChangeOpFactory,
+		StringNormalizer $stringNormalizer,
+		TermChangeOpSerializationValidator $validator
+	) {
+		$this->errorReporter = $errorReporter;
+		$this->fingerprintChangeOpFactory = $fingerprintChangeOpFactory;
+		$this->stringNormalizer = $stringNormalizer;
+		$this->validator = $validator;
+	}
+
+	/**
+	 * @see ChangeOpDeserializer::createEntityChangeOp
+	 *
+	 * @param array[] $changeRequest
+	 *
+	 * @return ChangeOp
+	 */
+	public function createEntityChangeOp( array $changeRequest ) {
+		$this->validateDescriptionList( $changeRequest['descriptions'] );
+
+		$changeOps = new ChangeOps();
+
+		foreach ( $changeRequest['descriptions'] as $langCode => $arg ) {
+			$this->validator->validateMultilangArgs( $arg, $langCode );
+
+			$language = $arg['language'];
+			$newDescription = ( array_key_exists( 'remove', $arg ) ? '' :
+				$this->stringNormalizer->trimToNFC( $arg['value'] ) );
+
+			if ( $newDescription === '' ) {
+				$changeOps->add( $this->fingerprintChangeOpFactory->newRemoveDescriptionOp( $language ) );
+			} else {
+				$changeOps->add( $this->fingerprintChangeOpFactory->newSetDescriptionOp( $language, $newDescription ) );
+			}
+		}
+
+		return $changeOps;
+	}
+
+	private function validateDescriptionList( $descriptions ) {
+		if ( !is_array( $descriptions ) ) {
+			$this->errorReporter->dieError( 'List of descriptions must be an array', 'not-recognized-array' );
+		}
+	}
+
+}
---
+++ repo/tests/phpunit/includes/ChangeOpDeserializers/DescriptionsChangeOpDeserializerTest.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Wikibase\Repo\Tests\ChangeOp;
+
+use RuntimeException;
+use Wikibase\ChangeOp\ChangeOps;
+use Wikibase\ChangeOp\FingerprintChangeOpFactory;
+use Wikibase\Repo\Api\ApiErrorReporter;
+use Wikibase\Repo\ChangeOpDeserializers\DescriptionsChangeOpDeserializer;
+use Wikibase\Repo\Validators\TermChangeOpSerializationValidator;
+use Wikibase\Repo\WikibaseRepo;
+
+/**
+ * @covers DescriptionsChangeOpDeserializer
+ *
+ * @group Wikibase
+ *
+ * @license GPL-2.0+
+ */
+class DescriptionsChangeOpDeserializerTest extends \PHPUnit_Framework_TestCase {
+
+	public function testGivenDescriptionsFieldNotAnArray_createEntityChangeOpThrowsError() {
+		$this->setExpectedException( RuntimeException::class, 'not-recognized-array' );
+		$this->newDescriptionChangeOpDeserializer(
+			$this->getApiErrorReporter( true ),
+			$this->getFingerPrintChangeOpFactory()
+		)->createEntityChangeOp( [ 'descriptions' => null ] );
+	}
+
+	public function testGivenInvalidLanguage_createEntityChangeOpThrowsError() {
+		$this->setExpectedException( RuntimeException::class, 'inconsistent-language' );
+		$this->newDescriptionChangeOpDeserializer(
+			$this->getApiErrorReporter( true ),
+			$this->getFingerPrintChangeOpFactory()
+		)->createEntityChangeOp( [ 'descriptions' => [ 'en' => [ 'language' => 'de', 'value' => 'foo' ] ] ] );
+	}
+
+	public function testGivenChangeRequestWithNewDescription_callsNewSetDescriptionOp() {
+		$factory = $this->getFingerPrintChangeOpFactory();
+		$factory->expects( $this->once() )
+			->method( 'newSetDescriptionOp' )
+			->willReturn( new ChangeOps() );
+
+		$this->newDescriptionChangeOpDeserializer( $this->getApiErrorReporter( false ), $factory )
+			->createEntityChangeOp( [ 'descriptions' => [ 'en' => [ 'language' => 'en', 'value' => 'foo' ] ] ] );
+	}
+
+	public function testGivenChangeRequestWithRemove_callsNewRemoveDescriptionOp() {
+		$factory = $this->getFingerPrintChangeOpFactory();
+		$factory->expects( $this->once() )
+			->method( 'newRemoveDescriptionOp' )
+			->willReturn( new ChangeOps() );
+
+		$this->newDescriptionChangeOpDeserializer( $this->getApiErrorReporter( false ), $factory )
+			->createEntityChangeOp( [ 'descriptions' => [ 'en' => [ 'language' => 'en', 'remove' => true ] ] ] );
+	}
+
+	private function newDescriptionChangeOpDeserializer(
+		ApiErrorReporter $errorReporter,
+		FingerprintChangeOpFactory $factory
+	) {
+		return new DescriptionsChangeOpDeserializer(
+			$errorReporter,
+			$factory,
+			$this->getStringNormalizer(),
+			$this->getTermChangeOpValidator( $errorReporter )
+		);
+	}
+
+	private function getStringNormalizer() {
+		return WikibaseRepo::getDefaultInstance()->getStringNormalizer();
+	}
+
+	private function getFingerPrintChangeOpFactory() {
+		return $this->getMockBuilder( FingerprintChangeOpFactory::class )
+			->disableOriginalConstructor()
+			->getMock();
+	}
+
+	private function getTermChangeOpValidator( ApiErrorReporter $errorReporter ) {
+		return new TermChangeOpSerializationValidator(
+			$errorReporter,
+			WikibaseRepo::getDefaultInstance()->getTermsLanguages()
+		);
+	}
+
+	/**
+	 * TODO: Refactor into mock class or test helper
+	 *
+	 * @param bool $expectsError
+	 *
+	 * @return ApiErrorReporter
+	 */
+	private function getApiErrorReporter( $expectsError = false ) {
+		$errorReporter = $this->getMockBuilder( ApiErrorReporter::class )
+			->disableOriginalConstructor()
+			->getMock();
+
+		if ( !$expectsError ) {
+			$errorReporter->expects( $this->never() )
+				->method( 'dieError' );
+		} else {
+			$errorReporter->expects( $this->once() )
+				->method( 'dieError' )
+				->willReturnCallback( function( $description, $errorCode ) {
+					throw new RuntimeException( $errorCode );
+				} );
+		}
+
+		return $errorReporter;
+	}
+
+}
