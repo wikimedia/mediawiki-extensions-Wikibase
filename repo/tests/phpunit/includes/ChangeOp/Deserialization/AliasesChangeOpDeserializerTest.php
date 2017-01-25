7fe699971d23d2e644ff48f634356d2a5a456e2a
Jakob Warkotsch <j.warkotsch@gmail.com>
2017-01-25 17:27:47 +0100

    Add ChangeOpDeserializers for labels, aliases, and descriptions
    ChangeOp initialization logic is mainly copied from Api\EditEntity.
    
    Change-Id: I07e829fadd2b38c232e1f865dfca2765d4b6620d

---
+++ repo/tests/phpunit/includes/ChangeOp/Deserialization/AliasesChangeOpDeserializerTest.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Wikibase\Repo\Tests\ChangeOp\Deserialization;
+
+use Wikibase\ChangeOp\ChangeOp;
+use Wikibase\ChangeOp\ChangeOps;
+use Wikibase\ChangeOp\FingerprintChangeOpFactory;
+use Wikibase\DataModel\Entity\EntityDocument;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\Lib\StaticContentLanguages;
+use Wikibase\Repo\ChangeOp\Deserialization\AliasesChangeOpDeserializer;
+use Wikibase\Repo\ChangeOp\Deserialization\TermChangeOpSerializationValidator;
+use Wikibase\StringNormalizer;
+
+/**
+ * @covers Wikibase\Repo\ChangeOp\Deserialization\AliasesChangeOpDeserializer
+ *
+ * @group Wikibase
+ *
+ * @license GPL-2.0+
+ */
+class AliasesChangeOpDeserializerTest extends \PHPUnit_Framework_TestCase {
+
+	public function testGivenAliasesFieldNotAnArray_createEntityChangeOpThrowsError() {
+		TermChangeOpDeserializationAssert::assertThrowsChangeOpDeserializationException(
+			function() {
+				$this->newAliasesChangeOpDeserializer(
+					$this->getFingerPrintChangeOpFactory()
+				)->createEntityChangeOp( [ 'aliases' => null ] );
+			},
+			'not-recognized-array'
+		);
+	}
+
+	public function testGivenInvalidLanguage_createEntityChangeOpThrowsError() {
+		TermChangeOpDeserializationAssert::assertThrowsChangeOpDeserializationException(
+			function() {
+				$this->newAliasesChangeOpDeserializer(
+					$this->getFingerPrintChangeOpFactory()
+				)->createEntityChangeOp( [ 'aliases' => [ 'en' => [ 'language' => 'de', 'value' => 'foo' ] ] ] );
+			},
+			'inconsistent-language'
+		);
+	}
+
+	public function testGivenChangeRequestWithNewAliases_callsNewSetAliasesOp() {
+		$factory = $this->getFingerPrintChangeOpFactory();
+		$factory->expects( $this->once() )
+			->method( 'newSetAliasesOp' )
+			->willReturn( new ChangeOps() );
+
+		$this->newAliasesChangeOpDeserializer( $factory )
+			->createEntityChangeOp( [ 'aliases' => [ 'en' => [ 'language' => 'en', 'value' => 'foo' ] ] ] );
+	}
+
+	public function testGivenChangeRequestWithRemove_callsNewRemoveAliasesOp() {
+		$factory = $this->getFingerPrintChangeOpFactory();
+		$factory->expects( $this->once() )
+			->method( 'newRemoveAliasesOp' )
+			->willReturn( new ChangeOps() );
+
+		$this->newAliasesChangeOpDeserializer( $factory )
+			->createEntityChangeOp(
+				[ 'aliases' => [
+					'en' => [ 'language' => 'en', 'value' => 'foo', 'remove' => '' ] ]
+				]
+			);
+	}
+
+	public function testGivenChangeRequestWithAdd_createsChangeOpsWithChangeOpAddingAliases() {
+		$changeOpAddingAliases = $this->getMock( ChangeOp::class );
+		$changeOpAddingAliases->wasApplied = false;
+		$changeOpAddingAliases->method( 'validate' )->willReturn( true );
+		$changeOpAddingAliases->method( 'apply' )
+			->willReturnCallback(
+				function () use ( $changeOpAddingAliases ) {
+					$changeOpAddingAliases->wasApplied = true;
+				}
+			);
+		$factory = $this->getFingerPrintChangeOpFactory();
+		$factory->method( 'newAddAliasesOp' )->willReturn( $changeOpAddingAliases );
+
+		$resultChangeOp = $this->newAliasesChangeOpDeserializer( $factory )
+			->createEntityChangeOp(
+				[ 'aliases' => [
+					'en' => [ 'language' => 'en', 'value' => 'foo', 'add' => '' ] ]
+				]
+			);
+
+		$resultChangeOp->apply( $this->getMock( EntityDocument::class ) );
+		self::assertTrue( $changeOpAddingAliases->wasApplied );
+	}
+
+	private function newAliasesChangeOpDeserializer(
+		FingerprintChangeOpFactory $factory
+	) {
+		return new AliasesChangeOpDeserializer(
+			$factory,
+			$this->getStringNormalizer(),
+			$this->getTermChangeOpValidator()
+		);
+	}
+
+	private function getStringNormalizer() {
+		return new StringNormalizer();
+	}
+
+	private function getFingerPrintChangeOpFactory() {
+		return $this->getMockBuilder( FingerprintChangeOpFactory::class )
+			->disableOriginalConstructor()
+			->getMock();
+	}
+
+	private function getTermChangeOpValidator() {
+		return new TermChangeOpSerializationValidator( new StaticContentLanguages( [ 'en', 'de' ] ) );
+	}
+
+}
