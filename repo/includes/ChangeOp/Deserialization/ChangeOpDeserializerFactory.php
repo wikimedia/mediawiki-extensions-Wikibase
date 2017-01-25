7d0026d2ed14798eaa5e62324a479a025d0da29d
Jakob Warkotsch <j.warkotsch@gmail.com>
2017-01-25 17:56:57 +0100

    Add factory of ChangeOpDeserializers
    Factory provides instances of deserializers dedicated
    to change requests of fields of items and properties:
    labels, aliases, descriptions, statements (claims),
    and site links.
    
    Change-Id: I851ac59e250960d389fd7ef76f1e78a60f037ee2

---
+++ repo/includes/ChangeOp/Deserialization/ChangeOpDeserializerFactory.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace Wikibase\Repo\ChangeOp\Deserialization;
+
+use Deserializers\Deserializer;
+use Wikibase\ChangeOp\FingerprintChangeOpFactory;
+use Wikibase\ChangeOp\SiteLinkChangeOpFactory;
+use Wikibase\ChangeOp\StatementChangeOpFactory;
+use Wikibase\DataModel\Entity\EntityIdParser;
+use Wikibase\Repo\SiteLinkTargetProvider;
+use Wikibase\StringNormalizer;
+
+/**
+ * Factory providing ChangeOpDeserializers for fields of items and properties,
+ * such as label, description, alias, claim and sitelink.
+ *
+ * @license GPL-2.0+
+ */
+class ChangeOpDeserializerFactory {
+
+	/**
+	 * @var FingerprintChangeOpFactory
+	 */
+	private $fingerprintChangeOpFactory;
+
+	/**
+	 * @var StatementChangeOpFactory
+	 */
+	private $statementChangeOpFactory;
+
+	/**
+	 * @var SiteLinkChangeOpFactory
+	 */
+	private $siteLinkChangeOpFactory;
+
+	/**
+	 * @var TermChangeOpSerializationValidator
+	 */
+	private $termChangeOpSerializationValidator;
+
+	/**
+	 * @var SiteLinkChangeOpSerializationValidator
+	 */
+	private $siteLinkChangeOpSerializationValidator;
+
+	/**
+	 * @var Deserializer
+	 */
+	private $statementDeserializer;
+
+	/**
+	 * @var SiteLinkTargetProvider
+	 */
+	private $siteLinkTargetProvider;
+
+	/**
+	 * @var EntityIdParser
+	 */
+	private $entityIdParser;
+
+	/**
+	 * @var StringNormalizer
+	 */
+	private $stringNormalizer;
+
+	/**
+	 * @var string[]
+	 */
+	private $siteLinkGroups;
+
+	/**
+	 * @param FingerprintChangeOpFactory $fingerprintChangeOpFactory
+	 * @param StatementChangeOpFactory $statementChangeOpFactory
+	 * @param SiteLinkChangeOpFactory $siteLinkChangeOpFactory
+	 * @param TermChangeOpSerializationValidator $termChangeOpSerializationValidator
+	 * @param SiteLinkChangeOpSerializationValidator $siteLinkChangeOpSerializationValidator
+	 * @param Deserializer $statementDeserializer
+	 * @param SiteLinkTargetProvider $siteLinkTargetProvider
+	 * @param EntityIdParser $entityIdParser
+	 * @param StringNormalizer $stringNormalizer
+	 * @param string[] $siteLinkGroups
+	 */
+	public function __construct(
+		FingerprintChangeOpFactory $fingerprintChangeOpFactory,
+		StatementChangeOpFactory $statementChangeOpFactory,
+		SiteLinkChangeOpFactory $siteLinkChangeOpFactory,
+		TermChangeOpSerializationValidator $termChangeOpSerializationValidator,
+		SiteLinkChangeOpSerializationValidator $siteLinkChangeOpSerializationValidator,
+		Deserializer $statementDeserializer,
+		SiteLinkTargetProvider $siteLinkTargetProvider,
+		EntityIdParser $entityIdParser,
+		StringNormalizer $stringNormalizer,
+		array $siteLinkGroups
+	) {
+		$this->fingerprintChangeOpFactory = $fingerprintChangeOpFactory;
+		$this->statementChangeOpFactory = $statementChangeOpFactory;
+		$this->siteLinkChangeOpFactory = $siteLinkChangeOpFactory;
+		$this->termChangeOpSerializationValidator = $termChangeOpSerializationValidator;
+		$this->siteLinkChangeOpSerializationValidator = $siteLinkChangeOpSerializationValidator;
+		$this->statementDeserializer = $statementDeserializer;
+		$this->siteLinkTargetProvider = $siteLinkTargetProvider;
+		$this->entityIdParser = $entityIdParser;
+		$this->stringNormalizer = $stringNormalizer;
+		$this->siteLinkGroups = $siteLinkGroups;
+	}
+
+	/**
+	 * @return LabelsChangeOpDeserializer
+	 */
+	public function getLabelsChangeOpDeserializer() {
+		return new LabelsChangeOpDeserializer(
+			$this->fingerprintChangeOpFactory,
+			$this->stringNormalizer,
+			$this->termChangeOpSerializationValidator
+		);
+	}
+
+	/**
+	 * @return DescriptionsChangeOpDeserializer
+	 */
+	public function getDescriptionsChangeOpDeserializer() {
+		return new DescriptionsChangeOpDeserializer(
+			$this->fingerprintChangeOpFactory,
+			$this->stringNormalizer,
+			$this->termChangeOpSerializationValidator
+		);
+	}
+
+	/**
+	 * @return AliasesChangeOpDeserializer
+	 */
+	public function getAliasesChangeOpDeserializer() {
+		return new AliasesChangeOpDeserializer(
+			$this->fingerprintChangeOpFactory,
+			$this->stringNormalizer,
+			$this->termChangeOpSerializationValidator
+		);
+	}
+
+	/**
+	 * @return ClaimsChangeOpDeserializer
+	 */
+	public function getClaimsChangeOpDeserializer() {
+		return new ClaimsChangeOpDeserializer(
+			$this->statementDeserializer,
+			$this->statementChangeOpFactory
+		);
+	}
+
+	/**
+	 * @return SiteLinksChangeOpDeserializer
+	 */
+	public function getSiteLinksChangeOpDeserializer() {
+		return new SiteLinksChangeOpDeserializer(
+			$this->siteLinkChangeOpSerializationValidator,
+			$this->siteLinkChangeOpFactory,
+			$this->siteLinkTargetProvider,
+			$this->entityIdParser,
+			$this->stringNormalizer,
+			$this->siteLinkGroups
+		);
+	}
+
+}
