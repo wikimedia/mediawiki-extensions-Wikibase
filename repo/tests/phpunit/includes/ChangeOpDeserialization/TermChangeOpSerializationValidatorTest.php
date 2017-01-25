1eb09772efe72f37ff3a11467271c481462ef1ac
Jakob Warkotsch <j.warkotsch@gmail.com>
2017-01-25 17:00:45 +0100

    [WIP] Add TermChangeOpSerializationValidator
    - method validateMultiLangArgs is copied from Api\EditEntity
    
    Note: Comments below should be removed once not relevant!
    
    Moved out of I2977607b4b02bc56183cfa5a9a9a0dc57432964d to make
    review easier. Note: this change is a part of a bigger chain
    of patches. Refer to related patches to see how this fits
    a bigger picture.
    
    TODOs:
     - this should be referring some ticket but which one? T154288 or
       some more specific one?
    
    Change-Id: Ia5141947523b4fcc94e388eb86a0701a7cbed2d5

---
+++ repo/tests/phpunit/includes/ChangeOpDeserialization/TermChangeOpSerializationValidatorTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Wikibase\Repo\Tests\ChangeOpDeserialization;
+
+use RuntimeException;
+use Wikibase\Lib\StaticContentLanguages;
+use Wikibase\Repo\ChangeOpDeserialization\TermChangeOpSerializationValidator;
+
+/**
+ * @covers TermChangeOpSerializationValidator
+ *
+ * @group Wikibase
+ * @group WikibaseValidators
+ *
+ * @license GPL-2.0+
+ */
+class TermChangeOpSerializationValidatorTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * @var ChangeOpDeserializationTestHelper
+	 */
+	private $testHelper;
+
+	public function setUp() {
+		$this->testHelper = new ChangeOpDeserializationTestHelper( $this );
+	}
+
+	/**
+	 * @dataProvider multilangArgsProvider
+	 */
+	public function testValidateMultilangArgs( $arg, $langCode, $errorCode ) {
+		$validator = new TermChangeOpSerializationValidator(
+			$this->getContentLanguages(),
+			$this->testHelper->getApiErrorReporter( $errorCode )
+		);
+
+		if ( $errorCode !== false ) {
+			$this->setExpectedException( RuntimeException::class, $errorCode );
+		}
+
+		$validator->validateMultilangArgs( $arg, $langCode );
+	}
+
+	public function multilangArgsProvider() {
+		return [
+			'no language key' => [ [], 'en', 'missing-language' ],
+			'language not a string (bool)' => [ [ 'language' => false ], 'en', 'not-recognized-string' ],
+			'language not a string (int)' => [ [ 'language' => 3 ], 'en', 'not-recognized-string' ],
+			'language not a string (null)' => [ [ 'language' => null ], 'en', 'not-recognized-string' ],
+			'arg lang not matching langCode' => [
+				[ 'language' => 'en', 'value' => 'foo' ],
+				'de',
+				'inconsistent-language'
+			],
+			'unknown language' => [
+				[ 'language' => 'xx', 'value' => 'foo' ],
+				'xx',
+				'not-recognized-language'
+			],
+			'valid' => [
+				[ 'language' => 'en', 'value' => 'foo' ],
+				'en',
+				false
+			],
+			'valid numeric langcode' => [
+				[ 'language' => 'en', 'value' => 'foo' ],
+				'123',
+				false
+			],
+			'valid int langcode' => [
+				[ 'language' => 'en', 'value' => 'foo' ],
+				123,
+				false
+			],
+			'valid remove' => [
+				[ 'language' => 'en', 'remove' => '' ],
+				'en',
+				false
+			],
+		];
+	}
+
+	private function getContentLanguages() {
+		return new StaticContentLanguages( [ 'en', 'de', 'fr' ] );
+	}
+
+}
