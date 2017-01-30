39ad13a164cd1b45bd3f68949a243d79405fad5a
Amir Sarabadani <Ladsgroup@gmail.com>
2017-01-30 21:09:25 +0330

    Inject some of MergeItems dependencies
    Bug: T156650
    Change-Id: I3c6009368b9fcb5a4e0fc2967bb8bfe546326b9e

--- repo/Wikibase.php
+++ repo/Wikibase.php
@@ -294,7 +294,19 @@ call_user_func( function() {
 			);
 		}
 	];
-	$wgAPIModules['wbmergeitems'] = Wikibase\Repo\Api\MergeItems::class;
+	$wgAPIModules['wbmergeitems'] = [
+		'class' => Wikibase\Repo\Api\MergeItems::class,
+		'factory' => function( ApiMain $mainModule, $moduleName ) {
+			$wikibaseRepo = \Wikibase\Repo\WikibaseRepo::getDefaultInstance();
+
+			return new Wikibase\Repo\Api\MergeItems(
+				$mainModule,
+				$moduleName,
+				$wikibaseRepo->getEntityIdParser(),
+				$wikibaseRepo->newItemMergeInteractor( $mainModule->getContext() )
+			);
+		}
+	];
 	$wgAPIModules['wbformatvalue'] = [
 		'class' => Wikibase\Repo\Api\FormatSnakValue::class,
 		'factory' => function( ApiMain $mainModule, $moduleName ) {
