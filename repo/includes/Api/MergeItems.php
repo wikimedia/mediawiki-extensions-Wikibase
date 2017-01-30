bdebc4400b28622af92677a38aee086dd8ccc6af
Amir Sarabadani <Ladsgroup@gmail.com>
2017-01-30 21:09:25 +0330

    Inject some of MergeItems dependencies
    Bug: T156650
    Change-Id: I3c6009368b9fcb5a4e0fc2967bb8bfe546326b9e

--- repo/includes/Api/MergeItems.php
+++ repo/includes/Api/MergeItems.php
@@ -47,36 +47,30 @@ class MergeItems extends ApiBase {
 	private $resultBuilder;
 
 	/**
+	 * @see ApiBase::__construct
+	 *
 	 * @param ApiMain $mainModule
 	 * @param string $moduleName
-	 * @param string $modulePrefix
-	 *
-	 * @see ApiBase::__construct
+	 * @param EntityIdParser $idParser
+	 * @param ItemMergeInteractor $interactor
+	 * @param ApiErrorReporter $errorReporter
 	 */
-	public function __construct( ApiMain $mainModule, $moduleName, $modulePrefix = '' ) {
-		parent::__construct( $mainModule, $moduleName, $modulePrefix );
-
-		$wikibaseRepo = WikibaseRepo::getDefaultInstance();
-		$apiHelperFactory = $wikibaseRepo->getApiHelperFactory( $this->getContext() );
-
-		$this->setServices(
-			$wikibaseRepo->getEntityIdParser(),
-			$apiHelperFactory->getErrorReporter( $this ),
-			$apiHelperFactory->getResultBuilder( $this ),
-			$wikibaseRepo->newItemMergeInteractor( $this->getContext() )
-		);
-	}
-
-	public function setServices(
+	public function __construct(
+		ApiMain $mainModule,
+		$moduleName,
 		EntityIdParser $idParser,
-		ApiErrorReporter $errorReporter,
-		ResultBuilder $resultBuilder,
-		ItemMergeInteractor $interactor
+		ItemMergeInteractor $interactor,
+		ApiErrorReporter $errorReporter
 	) {
+		parent::__construct( $mainModule, $moduleName );
+
+		$apiHelperFactory = WikibaseRepo::getDefaultInstance()->getApiHelperFactory( $this->getContext() );
+
 		$this->idParser = $idParser;
-		$this->errorReporter = $errorReporter;
-		$this->resultBuilder = $resultBuilder;
 		$this->interactor = $interactor;
+
+		$this->errorReporter = $errorReporter;
+		$this->resultBuilder = $apiHelperFactory->getResultBuilder( $this );
 	}
 
 	/**
