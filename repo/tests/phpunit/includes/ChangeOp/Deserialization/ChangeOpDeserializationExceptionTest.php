b52119530bb477b6a81954a43d49df95a08d456b
Jakob Warkotsch <j.warkotsch@gmail.com>
2017-01-31 18:00:40 +0100

    Clean up TermChangeOpSerializationValidator
    - removed ApiErrorReporter dependency
    - added ChangeOpDeserializationException
    - TermChangeOpSerializationValidator throws an exception,
      API class(es) using ChangeOpDeserializers should catch
      ChangeOpDeserializationExceptions and pass them
      accordingly to ApiErrorReporter to present errors
      to API users
    - improved method and argument names
    
    Change-Id: If7f7acd8b5e35db468ca199e5519239088fa4ce9

---
+++ repo/tests/phpunit/includes/ChangeOp/Deserialization/ChangeOpDeserializationExceptionTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Wikibase\Repo\Tests\ChangeOp\Deserialization;
+
+use Wikibase\Repo\ChangeOp\Deserialization\ChangeOpDeserializationException;
+
+/**
+ * @covers Wikibase\Repo\ChangeOp\Deserialization\ChangeOpDeserializationException
+ *
+ * @group Wikibase
+ *
+ * @license GPL-2.0+
+ */
+class ChangeOpDeserializationExceptionTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * @dataProvider exceptionMessageProvider
+	 */
+	public function testGetMessage( $message ) {
+		$exception = new ChangeOpDeserializationException( $message, 'error-bar' );
+
+		$this->assertSame( $message, $exception->getMessage() );
+	}
+
+	public function exceptionMessageProvider() {
+		return [ [ 'foo' ], [ 'bar' ] ];
+	}
+
+	/**
+	 * @dataProvider errorCodeProvider
+	 */
+	public function testGetCode( $errorCode ) {
+		$exception = new ChangeOpDeserializationException( 'foo', $errorCode );
+
+		$this->assertSame( $errorCode, $exception->getErrorCode() );
+	}
+
+	public function errorCodeProvider() {
+		return [ [ 'error-foo' ], [ 'error-bar' ] ];
+	}
+
+}
