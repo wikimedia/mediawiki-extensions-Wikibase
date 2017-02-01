d804f88c5d5394d496056be85a98687aaf584795
Stanislav Malyshev <smalyshev@wikimedia.org>
2017-02-01 15:15:32 -0800

    Add prefix search for labels
    This patch implements wbsearchentities API via ElasticSearch.
    $wgEntitySearchUseCirrus should be enabled to switch to using ES.
    
    Change-Id: I831220363bbe3e94ee3888134d3eb3d92d06a264
    Bug: T125500

---
+++ repo/includes/Api/EntitySearcher.php
@@ -0,0 +1,25 @@
+<?php
+namespace Wikibase\Repo\Api;
+
+use Wikibase\Lib\Interactors\TermSearchResult;
+
+/**
+ * Generic interface for searching entities.
+ */
+interface EntitySearcher {
+
+	/**
+	 * Get entities matching the search term.
+	 *
+	 * @param string $text
+	 * @param string $languageCode
+	 * @param string $entityType
+	 * @param int $limit
+	 * @param bool $strictLanguage
+	 *
+	 * @return TermSearchResult[] Key: string Serialized EntityId
+	 */
+	public function getRankedSearchResults( $text, $languageCode, $entityType, $limit,
+	                                        $strictLanguage );
+
+}
