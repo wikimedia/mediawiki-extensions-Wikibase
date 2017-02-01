10ec5530b86a05a55c49b9d40bcd78a4ef372f3a
Stanislav Malyshev <smalyshev@wikimedia.org>
2017-02-01 15:15:32 -0800

    Add prefix search for labels
    This patch implements wbsearchentities API via ElasticSearch.
    $wgEntitySearchUseCirrus should be enabled to switch to using ES.
    
    Change-Id: I831220363bbe3e94ee3888134d3eb3d92d06a264
    Bug: T125500

---
+++ repo/includes/Api/WikibasePrefixSearcher.php
@@ -0,0 +1,79 @@
+<?php
+namespace Wikibase\Repo\Api;
+
+use CirrusSearch\Connection;
+use CirrusSearch\Searcher;
+use Elastica\Query;
+use Elastica\Query\BoolQuery;
+use Elastica\Query\Match;
+use Elastica\Query\MultiMatch;
+use MediaWiki\MediaWikiServices;
+
+/**
+ * Searcher class for performing Wikibase prefix search.
+ * @see \CirrusSearch\Searcher
+ */
+class WikibasePrefixSearcher extends Searcher {
+	/**
+	 * @var Query\AbstractQuery
+	 */
+	private $query;
+
+	/**
+	 * WikibasePrefixSearcher constructor.
+	 * @param int $offset
+	 * @param int $limit
+	 * @param Query\AbstractQuery $query Search query.
+	 */
+	public function __construct( $offset, $limit, Query\AbstractQuery $query ) {
+		$config =
+			MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'CirrusSearch' );
+		$connection = new Connection( $config );
+		$this->query = $query;
+		parent::__construct( $connection, $offset, $limit, $config );
+	}
+
+	/**
+	 * Build search query object.
+	 * @return \Elastica\Search
+	 */
+	protected function buildSearch() {
+		$this->getSearchContext()->addSyntaxUsed( 'wikibase_prefix', PHP_INT_MAX );
+
+		$indexType = $this->connection->pickIndexTypeForNamespaces( $this->getSearchContext()->getNamespaces() );
+		$pageType = $this->connection->getPageType( $this->indexBaseName, $indexType );
+
+		$queryOptions = [
+			\Elastica\Search::OPTION_TIMEOUT => $this->config->getElement( 'CirrusSearchSearchShardTimeout',
+				'default' ),
+		];
+		$searchQuery = new Query();
+		$searchQuery->setQuery( $this->query );
+
+		$searchQuery->setParam( '_source', $this->resultsType->getSourceFiltering() );
+		$searchQuery->setParam( 'fields', $this->resultsType->getFields() );
+
+		$highlight = $this->searchContext->getHighlight( $this->resultsType );
+		if ( $highlight ) {
+			$searchQuery->setHighlight( $highlight );
+		}
+		$searchQuery->setParam( 'rescore', $this->searchContext->getRescore() );
+
+		return $pageType->createSearch( $searchQuery, $queryOptions );
+	}
+
+	/**
+	 * Perform prefix search for Wikibase entities.
+	 * @return array
+	 */
+	public function performSearch() {
+		$result = $this->searchOne();
+
+		if ( $result->isOK() ) {
+			return $result->getValue();
+		}
+		// TODO: can we do any error reporting upstream here?
+		return [];
+	}
+
+}
