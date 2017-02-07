7f39bb9c9d6cf6b57698ccf8307b1948479cde9d
hall1467 <hall1467@umn.edu>
2017-02-07 16:09:27 -0600

    Adding logging functionality for statements
    Bug: T151717
    
    Change-Id: I57e12454a3935238a596e9732316748fbc53a194

---
+++ client/sql/statement_usage.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS /*_*/wbc_statement_usage (
+  su_entity_id               VARBINARY(255) NOT NULL, -- the ID of the entity being used
+  su_property_id             VARBINARY(255) NOT NULL, -- the property ID of the main snak associated with the statement used from the entity
+  su_page_id                 INT UNSIGNED NOT NULL,   -- the ID of the page that uses the entities
+  su_property_exists         Boolean,                 -- true if entity table has value for property
+
+ ) /*$wgDBTableOptions*/;
+
+-- record one usage per page per property associated with a statement of an entity
+CREATE UNIQUE INDEX /*i*/su_entity_id ON /*_*/wbc_statement_usage ( su_entity_id, su_property_id, su_page_id );
+
+-- create primary key
+ALTER TABLE wbc_statement_usage ADD PRIMARY KEY su_entity_id;
