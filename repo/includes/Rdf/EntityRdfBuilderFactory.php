cc721b72451aa07cc05dea37cd237e73478ac5a5
Amir Sarabadani <Ladsgroup@gmail.com>
2017-02-07 12:34:09 +0330

    Introduce EntityRdfBuilderFactory interface
    Bug: T157311
    Change-Id: Ifdd14b9bac203c10c5f5a03d766ec63e15acfe2f

---
+++ repo/includes/Rdf/EntityRdfBuilderFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Wikibase\Rdf;
+
+use Wikibase\DataModel\Entity\EntityDocument;
+use Wikimedia\Purtle\RdfWriter;
+
+/**
+ * Interface for EntityRdfBuilderFactories
+ *
+ * @license GPL-2.0+
+ * @author Amir Sarabadani <ladsgroup@gmail.com>
+ */
+interface EntityRdfBuilderFactory {
+
+	/**
+	 * Return an array of Rdf builders for parts of the entity
+	 *
+	 * @param EntityDocument $entity Entity to do santiy check and don't run Rdf builder where it's not supposed to
+	 * @param RdfWriter $writer
+	 * @param RdfVocabulary $vocabulary
+	 * @param int $flavorFlags
+	 * @return EntityRdfBuilder[]
+	 */
+	public function getBuilders(
+		EntityDocument $entity,
+		RdfWriter $writer,
+		RdfVocabulary $vocabulary,
+		$flavorFlags
+	);
+
+}
