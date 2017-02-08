5d570475bc7ff9df509df15316c541a4ce9050eb
Leszek Manicki <leszek.manicki@wikimedia.de>
2017-02-08 18:42:02 +0100

    Introduce RepositoryDefinitions
    RepositoryDefinitions is a service providing access to settings
    of configured repository (both "local" repository and possible
    "foreign" repositories).
    
    Definitions are provided in the form of map from repository
    names (strings, with empty-string key standing for the local
    repository) to an array of repository-specific settings.
    
    RepositoryDefinitions will be instantiazed by Client and/or
    Repo based on the configuration of the Wikibase instance,
    and passed to services requiring the knowledge on the
    repository settings (e.g. DispatchingServiceFactory,
    RepositoryServiceContainerFactory, etc).
    
    Bug: T157570
    Change-Id: Id23db330f20d0e3bcd62669607db1b12e70049b6

---
+++ lib/tests/phpunit/RepositoryDefinitionsTest.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace Wikibase\Lib\Tests;
+
+use InvalidArgumentException;
+use Wikibase\Lib\RepositoryDefinitions;
+
+/**
+ * @covers Wikibase\Lib\RepositoryDefinitions
+ *
+ * @group Wikibase
+ *
+ * @license GPL-2.0+
+ */
+class RepositoryDefinitionsTest extends \PHPUnit_Framework_TestCase {
+
+	public function provideInvalidConstructorArguments() {
+		return [
+			'repository name containing colon' => [ [ 'fo:o' => [] ] ],
+			'repository definition not an array' => [ [ '' => 'string' ] ],
+			'no settings for the local repository' => [ [ 'foo' => [ 'database' => 'foodb' ] ] ],
+			'no database key in repository definition' => [ [ '' => [ 'entity-types' => [], 'prefix-mapping' => [] ] ] ],
+			'no entity-types key in repository definition' => [ [ '' => [ 'database' => 'xyz', 'prefix-mapping' => [] ] ] ],
+			'no prefix-mapping key in repository definition' => [ [ '' => [ 'database' => 'xyz', 'entity-types' => [] ] ] ],
+		];
+	}
+
+	/**
+	 * @dataProvider provideInvalidConstructorArguments
+	 */
+	public function testGivenInvalidArguments_constructorThrowsException( array $args ) {
+		$this->setExpectedException( InvalidArgumentException::class );
+
+		new RepositoryDefinitions( $args );
+	}
+
+	/**
+	 * @return array
+	 */
+	private function getCompleteRepositoryDefinitionArray() {
+		return [
+			'' => [
+				'database' => false,
+				'entity-types' => [ 'item', 'property' ],
+				'prefix-mapping' => [],
+			],
+			'media' => [
+				'database' => 'foowiki',
+				'entity-types' => [ 'mediainfo' ],
+				'prefix-mapping' => [],
+			],
+			'lexeme' => [
+				'database' => 'bazwiki',
+				'entity-types' => [ 'lexeme' ],
+				'prefix-mapping' => [ 'foo' => 'media' ],
+			],
+		];
+	}
+
+	public function testGetRepositoryNames() {
+		$definitions = new RepositoryDefinitions( $this->getCompleteRepositoryDefinitionArray() );
+
+		$this->assertEquals( [ '', 'media', 'lexeme' ], $definitions->getRepositoryNames() );
+	}
+
+	public function testGetDatabaseNames() {
+		$definitions = new RepositoryDefinitions( $this->getCompleteRepositoryDefinitionArray() );
+
+		$this->assertEquals(
+			[ '' => false, 'media' => 'foowiki', 'lexeme' => 'bazwiki' ],
+			$definitions->getDatabaseNames()
+		);
+	}
+
+	public function testGetPrefixMappings() {
+		$definitions = new RepositoryDefinitions( $this->getCompleteRepositoryDefinitionArray() );
+
+		$this->assertEquals(
+			[ '' => [], 'media' => [], 'lexeme' => [ 'foo' => 'media' ] ],
+			$definitions->getPrefixMappings()
+		);
+	}
+
+	public function testGetEntityTypesPerRepository() {
+		$definitions = new RepositoryDefinitions( $this->getCompleteRepositoryDefinitionArray() );
+
+		$this->assertEquals(
+			[
+				'' => [ 'item', 'property' ],
+				'media' => [ 'mediainfo' ],
+				'lexeme' => [ 'lexeme' ],
+			],
+			$definitions->getEntityTypesPerRepository()
+		);
+	}
+
+	public function testGetEntityTypeToRepositoryMapping() {
+		$definitions = new RepositoryDefinitions( $this->getCompleteRepositoryDefinitionArray() );
+
+		$this->assertEquals(
+			[ 'item' => '', 'property' => '', 'mediainfo' => 'media', 'lexeme' => 'lexeme' ],
+			$definitions->getEntityTypeToRepositoryMapping()
+		);
+	}
+
+	public function testGivenSameEntityTypeDefinedForMultitpleRepos_languageLevelErrorIsRaised() {
+		$this->setExpectedException( \PHPUnit_Framework_Error::class );
+
+		$irrelevantDefinitions = [ 'database' => 'foo', 'prefix-mapping' => [] ];
+
+		new RepositoryDefinitions( [
+			'' => array_merge( $irrelevantDefinitions, [ 'entity-types' => [ 'item', 'property' ] ] ),
+			'media' => array_merge( $irrelevantDefinitions, [ 'entity-types' => [ 'item', 'mediainfo' ] ] ),
+		] );
+	}
+
+}
