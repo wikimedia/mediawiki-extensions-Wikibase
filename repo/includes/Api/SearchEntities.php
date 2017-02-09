3bec484a01dd2999bb29a1826e9672481f7f39bc
Matěj Suchánek <matejsuchanek97@gmail.com>
2017-02-09 14:02:41 +0000

    Return the property datatype in wbsearchentities
    Bug: T45069
    Change-Id: Idd586f66cfcee5f407ba7903ab61b25b71dec71f

--- repo/includes/Api/SearchEntities.php
+++ repo/includes/Api/SearchEntities.php
@@ -4,6 +4,8 @@ namespace Wikibase\Repo\Api;
 
 use ApiBase;
 use ApiMain;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\DataModel\Services\Lookup\PropertyDataTypeLookup;
 use Wikibase\Lib\ContentLanguages;
 use Wikibase\Lib\Interactors\TermSearchResult;
 use Wikibase\Lib\Store\EntityTitleLookup;
@@ -48,12 +50,18 @@ class SearchEntities extends ApiBase {
 	private $conceptBaseUri;
 
 	/**
+	 * @var PropertyDataTypeLookup
+	 */
+	private $propertyDataTypeLookup;
+
+	/**
 	 * @param ApiMain $mainModule
 	 * @param string $moduleName
 	 * @param EntityTitleLookup $entityTitleLookup
 	 * @param ContentLanguages $termLanguages
 	 * @param string[] $entityTypes
 	 * @param string $conceptBaseUri
+	 * @param PropertyDataTypeLookup $propertyDataTypeLookup
 	 *
 	 * @see ApiBase::__construct
 	 */
@@ -64,7 +72,8 @@ class SearchEntities extends ApiBase {
 		EntityTitleLookup $entityTitleLookup,
 		ContentLanguages $termLanguages,
 		array $entityTypes,
-		$conceptBaseUri
+		$conceptBaseUri,
+		PropertyDataTypeLookup $propertyDataTypeLookup
 	) {
 		parent::__construct( $mainModule, $moduleName, '' );
 
@@ -73,6 +82,7 @@ class SearchEntities extends ApiBase {
 		$this->termsLanguages = $termLanguages;
 		$this->entityTypes = $entityTypes;
 		$this->conceptBaseUri = $conceptBaseUri;
+		$this->propertyDataTypeLookup = $propertyDataTypeLookup;
 	}
 
 	/**
@@ -120,6 +130,11 @@ class SearchEntities extends ApiBase {
 			'title' => $title->getPrefixedText(),
 			'pageid' => $title->getArticleID()
 		);
+
+		if ( $entityId instanceof PropertyId ) {
+			$entry['datatype'] = $this->propertyDataTypeLookup
+				->getDataTypeIdForProperty( $match->getEntityId() );
+		}
 
 		$displayLabel = $match->getDisplayLabel();
 
