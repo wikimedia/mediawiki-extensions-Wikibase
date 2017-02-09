6ee4e5cdad21b925206352f6d2fd1846da7eae04
Stanislav Malyshev <smalyshev@wikimedia.org>
2017-02-09 13:52:17 -0800

    Add indexing for descriptions
    Descriptions follow the same pattern as labels, 'descriptions'
    field includes per-language fields. Only one description per language.
    
    Bug: T125500
    Change-Id: I459c619daf8f7e6c47c1a486428f0ca38f7c8faf
    Depends-On: Ic1cc1581f07381224d3f4fcba4feb1eac7085057

---
+++ repo/includes/Search/Elastic/Fields/DescriptionsField.php
@@ -0,0 +1,70 @@
+<?php
+namespace Wikibase\Repo\Search\Elastic\Fields;
+
+use SearchEngine;
+use SearchIndexFieldDefinition;
+use Wikibase\DataModel\Entity\EntityDocument;
+use Wikibase\DataModel\Term\DescriptionsProvider;
+
+/**
+ * Field which contains per-language specific descriptions.
+ */
+class DescriptionsField implements WikibaseIndexField {
+
+	/**
+	 * List of available languages
+	 * @var string[]
+	 */
+	private $languages;
+
+	public function __construct( $languages ) {
+		$this->languages = $languages;
+	}
+
+	/**
+	 * @param SearchEngine $engine
+	 * @param string $name
+	 * @return null|\SearchIndexField
+	 */
+	public function getMappingField( SearchEngine $engine, $name ) {
+		$field = $engine->makeSearchFieldMapping( $name, \SearchIndexField::INDEX_TYPE_NESTED );
+		if ( !( $field instanceof SearchIndexFieldDefinition ) ) {
+			return null;
+		}
+
+		$field->setMergeCallback( function ( $self, $that ) use ( $field ) {
+			return $field;
+		} );
+
+		$subfields = [];
+		/**
+		 * @var SearchIndexFieldDefinition
+		 */
+		foreach ( $this->languages as $language ) {
+			$subfields[$language] = $engine->makeSearchFieldMapping( "$name.$language",
+				\SearchIndexField::INDEX_TYPE_TEXT );
+		}
+		$field->setSubfields( $subfields );
+
+		return $field;
+	}
+
+	/**
+	 * @param EntityDocument $entity
+	 *
+	 * @return mixed Get the value of the field to be indexed when a page/document
+	 *               is indexed. This might be an array with nested data, if the field
+	 *               is defined with nested type or an int or string for simple field types.
+	 */
+	public function getFieldData( EntityDocument $entity ) {
+		if ( !( $entity instanceof DescriptionsProvider ) ) {
+			return [];
+		}
+		$data = [];
+		foreach ( $entity->getDescriptions() as $language => $desc ) {
+			$data[$language] = $desc->getText();
+		}
+		return $data;
+	}
+
+}
