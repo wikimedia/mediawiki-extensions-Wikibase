0fdc17497825c268278275f228814e585e899667
Leszek Manicki <leszek.manicki@wikimedia.de>
2017-02-10 15:36:35 +0100

    Introduce DispatchingEntityInfoBuilderFactory
    Factory instantiates DispatchingEntityInfoBuilders
    configured for a given list of entity IDs.
    
    This allows performing bulk entity info queries
    across multiple repositories.
    
    Bug: T157363
    Change-Id: Ic2a1ec741355ba6ef982e19319528d1bef6f75be

---
+++ lib/includes/Store/DispatchingEntityInfoBuilderFactory.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Wikibase\Lib\Store;
+
+use Wikibase\DataModel\Assert\RepositoryNameAssert;
+use Wikibase\DataModel\Entity\EntityId;
+use Wikimedia\Assert\Assert;
+
+/**
+ * Factory of DispatchingEntityInfoBuilders configured for a list of entity IDs.
+ * Builders used in the DispatchingEntityInfoBuilder created by the factory
+ * are created depending on to which repositories requested entities belong to.
+ *
+ * @license GPL-2.0+
+ */
+class DispatchingEntityInfoBuilderFactory implements EntityInfoBuilderFactory {
+
+	/**
+	 * @var EntityInfoBuilderFactory[]
+	 */
+	private $builderFactories;
+
+	/**
+	 * @param EntityInfoBuilderFactory[] $builderFactories
+	 */
+	public function __construct( array $builderFactories ) {
+		Assert::parameter( $builderFactories !== [], '$builderFactories', 'must not be empty' );
+		foreach ( $builderFactories as $repositoryName => $factory ) {
+			Assert::parameterType( EntityInfoBuilderFactory::class, $factory, '$factory' );
+			RepositoryNameAssert::assertParameterIsValidRepositoryName( $repositoryName, '$repositoryName' );
+		}
+
+		$this->builderFactories = $builderFactories;
+	}
+
+	/**
+	 * Returns a new DispatchingEntityInfoBuilder for gathering information about the
+	 * Entities specified by the given IDs.
+	 *
+	 * @param EntityId[] $entityIds
+	 *
+	 * @return DispatchingEntityInfoBuilder
+	 */
+	public function newEntityInfoBuilder( array $entityIds ) {
+		// TODO: should this filter $entityIds and only pass IDs from a given repository, to this repository's
+		// builder factory? Possibly skipping builders with no entities related. What should this
+		// then return when $entityIds is an empty array?
+		$builders = [];
+		foreach ( $this->builderFactories as $repositoryName => $factory ) {
+			$builders[$repositoryName] = $factory->newEntityInfoBuilder( $entityIds );
+		}
+
+		return new DispatchingEntityInfoBuilder( $builders );
+	}
+
+}
