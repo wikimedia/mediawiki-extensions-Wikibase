3ddba783c69d44ae402cc727248ade6628f8edb9
Tobias Gritschacher <tobias.gritschacher@wikimedia.de>
2017-02-14 14:35:50 +0100

    Login when creating new items in browsertests
    Bug: T157050
    Change-Id: I34412e37a5ccc8d5fc37dc657b195653e4c64543

--- tests/browser/features/support/pages/item_page.rb
+++ tests/browser/features/support/pages/item_page.rb
@@ -7,54 +7,54 @@
 # page object for item page
 
 class ItemPage
-  include PageObject
-  include EntityPage
+	include PageObject
+	include EntityPage
 
-  def create_item_data(handles, empty = false)
-    item_data = {}
-    handles.raw.each do |handle|
-      handle = handle[0]
-      label = empty ? '' : generate_random_string(8)
-      description = empty ? '' : generate_random_string(20)
-      data = '{"labels":{"en":{"language":"en","value":"' + label +
-             '"}},"descriptions":{"en":{"language":"en","value":"' + description + '"}}}'
-      item_data[handle] = data
-    end
+	def create_item_data(handles, empty = false)
+		item_data = {}
+		handles.raw.each do |handle|
+			handle = handle[0]
+			label = empty ? '' : generate_random_string(8)
+			description = empty ? '' : generate_random_string(20)
+			data = '{"labels":{"en":{"language":"en","value":"' + label +
+					'"}},"descriptions":{"en":{"language":"en","value":"' + description + '"}}}'
+			item_data[handle] = data
+		end
 
-    item_data
-  end
+		item_data
+	end
 
-  def create_item(data)
-    wb_api = MediawikiApi::Wikidata::WikidataClient.new URL.repo_api
-    resp = wb_api.create_item(data)
+	def create_item(data, wb_api)
+		wb_api = MediawikiApi::Wikidata::WikidataClient.new URL.repo_api
+		resp = wb_api.create_item(data)
 
-    id = resp['entity']['id']
+		id = resp['entity']['id']
 
-    if resp['entity']['labels'].length > 0 && resp['entity']['labels']['en']
-      label_en = resp['entity']['labels']['en']['value']
-    else
-      label_en = ''
-    end
+		if resp['entity']['labels'].length > 0 && resp['entity']['labels']['en']
+			label_en = resp['entity']['labels']['en']['value']
+		else
+			label_en = ''
+		end
 
-    if resp['entity']['descriptions'].length > 0 && resp['entity']['descriptions']['en']
-      description_en = resp['entity']['descriptions']['en']['value']
-    else
-      description_en = ''
-    end
+		if resp['entity']['descriptions'].length > 0 && resp['entity']['descriptions']['en']
+			description_en = resp['entity']['descriptions']['en']['value']
+		else
+			description_en = ''
+		end
 
-    url = URL.repo_url(ENV['ITEM_NAMESPACE'] + id)
+		url = URL.repo_url(ENV['ITEM_NAMESPACE'] + id)
 
-    { 'id' => id, 'url' => url, 'label' => label_en, 'description' => description_en }
-  end
+		{ 'id' => id, 'url' => url, 'label' => label_en, 'description' => description_en }
+	end
 
-  def create_items(handles, empty = false)
-    item_data = create_item_data(handles, empty)
-    items = {}
-    item_data.each do |handle, data|
-      item = create_item(data)
-      items[handle] = item
-    end
+	def create_items(handles, wb_api, empty = false)
+		item_data = create_item_data(handles, empty)
+		items = {}
+		item_data.each do |handle, data|
+			item = create_item(data, wb_api)
+			items[handle] = item
+		end
 
-    items
-  end
+		items
+	end
 end
