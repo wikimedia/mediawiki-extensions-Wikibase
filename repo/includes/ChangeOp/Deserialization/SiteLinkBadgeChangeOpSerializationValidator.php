9fc6efd1ffd69bcc6fd68353489ea9f57390fb6d
Leszek Manicki <leszek.manicki@wikimedia.de>
2017-02-21 10:27:18 +0100

    Add ChangeOpDeserializer for site link changes
    ChangeOp initialization logic is mainly copied from Api\EditEntity.
    
    SiteLinkChangeOpSerializationValidator is introduced with the code
    validating the structure of the single site link change request
    (copied from Api\EditEntity::checkSiteLinks).
    
    SiteLinkBadgeChangeOpSerializationValidator is also introduced
    with the code copied from Api\ModifyEntity::parseSiteLinkBadges.
    
    Bug: T154288
    Change-Id: I7855ca9f7710ce4ba208924469ab213b81b08221

---
+++ repo/includes/ChangeOp/Deserialization/SiteLinkBadgeChangeOpSerializationValidator.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Wikibase\Repo\ChangeOp\Deserialization;
+
+use InvalidArgumentException;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\Lib\Store\EntityTitleLookup;
+
+/**
+ * Validates the structure of the site link's badge change request.
+ *
+ * @license GPL-2.0+
+ */
+class SiteLinkBadgeChangeOpSerializationValidator {
+
+	/**
+	 * @var EntityTitleLookup
+	 */
+	private $titleLookup;
+
+	/**
+	 * @var string[]
+	 */
+	private $allowedBadgeItemIds;
+
+	/**
+	 * @param EntityTitleLookup $titleLookup
+	 * @param string[] $allowedBadgeItemIds
+	 */
+	public function __construct( EntityTitleLookup $titleLookup, array $allowedBadgeItemIds ) {
+		$this->titleLookup = $titleLookup;
+		$this->allowedBadgeItemIds = $allowedBadgeItemIds;
+	}
+
+	/**
+	 * @param string[] $serialization
+	 *
+	 * @throws ChangeOpDeserializationException
+	 */
+	public function validateBadgeSerialization( array $serialization ) {
+		foreach ( $serialization as $badgeSerialization ) {
+			if ( !is_string( $badgeSerialization ) ) {
+				throw new ChangeOpDeserializationException(
+					'Badges: a string was expected, but not found',
+					'not-recognized-string'
+				);
+			}
+
+			try {
+				// TODO: this should be rather using EntityIdParser
+				$badgeId = new ItemId( $badgeSerialization );
+			} catch ( InvalidArgumentException $ex ) {
+				throw new ChangeOpDeserializationException( 'Badges: could not parse "' . $badgeSerialization
+					. '", the id is invalid', 'invalid-entity-id' );
+				continue;
+			}
+
+			if ( !in_array( $badgeId->getSerialization(), $this->allowedBadgeItemIds ) ) {
+				throw new ChangeOpDeserializationException( 'Badges: item "' . $badgeSerialization . '" is not a badge',
+					'not-badge' );
+			}
+
+			$itemTitle = $this->titleLookup->getTitleForId( $badgeId );
+
+			if ( is_null( $itemTitle ) || !$itemTitle->exists() ) {
+				throw new ChangeOpDeserializationException(
+					'Badges: no item found matching id "' . $badgeSerialization . '"',
+					'no-such-entity'
+				);
+			}
+		}
+	}
+
+}
