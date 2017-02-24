588ce2d20c31b3e7e8248e15faaabfd2108bc9a1
Aleksey Bekh-Ivanov (WMDE) <aleksey.bekh-ivanov@wikimedia.de>
2017-02-24 17:34:24 +0100

    Made GeoShape configurable
    A step towards making GeoShape view configurable
    Bug: T160069
    
    Change-Id: I1bff331d464147cd7ab4fc6833ae0cf805d8e3fe

--- view/resources/wikibase/view/ViewFactory.js
+++ view/resources/wikibase/view/ViewFactory.js
@@ -37,6 +37,7 @@
 	 *        Required for showing the user interface in the correct language and for showing terms
 	 *        in all languages requested by the user.
 	 * @param {string|null} [vocabularyLookupApiUrl=null]
+	 * @param {string} commonsApiUrl
 	 */
 	var SELF = MODULE.ViewFactory = function ViewFactory(
 		structureEditorFactory,
@@ -50,12 +51,14 @@
 		messageProvider,
 		parserStore,
 		userLanguages,
-		vocabularyLookupApiUrl
+		vocabularyLookupApiUrl,
+		commonsApiUrl
 	) {
 		if ( ( !structureEditorFactory || !structureEditorFactory.getAdder )
 			|| ( !messageProvider || !messageProvider.getMessage )
 			|| !$.isArray( userLanguages )
-			|| ( vocabularyLookupApiUrl && typeof vocabularyLookupApiUrl !== 'string' )
+			|| ( vocabularyLookupApiUrl && typeof vocabularyLookupApiUrl !== 'string'
+			|| !commonsApiUrl )
 		) {
 			throw new Error( 'Required parameter(s) not specified properly' );
 		}
@@ -74,6 +77,7 @@
 		this._userLanguages = userLanguages;
 		this._vocabularyLookupApiUrl = vocabularyLookupApiUrl || null;
 		this._eventSingletonManager = new $.util.EventSingletonManager();
+		this._commonsApiUrl = commonsApiUrl;
 	};
 
 	/**
@@ -153,6 +157,12 @@
 	 * @private
 	 */
 	SELF.prototype._vocabularyLookupApiUrl = null;
+
+	/**
+	 * @property {string}
+	 * @private
+	 */
+	SELF.prototype._commonsApiUrl = null;
 
 	/**
 	 * Construct a suitable view for the given entity on the given DOM element
@@ -616,7 +626,8 @@
 			this._userLanguages && this._userLanguages[0],
 			this._messageProvider,
 			this._contentLanguages,
-			this._vocabularyLookupApiUrl
+			this._vocabularyLookupApiUrl,
+			this._commonsApiUrl
 		);
 	};
 
