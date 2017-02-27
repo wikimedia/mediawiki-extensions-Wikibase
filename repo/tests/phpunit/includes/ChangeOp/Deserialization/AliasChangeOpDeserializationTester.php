71861cab7c6bcdb4c92a4b9093c45e845b3b9246
Jakob Warkotsch <j.warkotsch@gmail.com>
2017-02-27 14:13:55 +0100

    Make test methods of ChangeOpDeserializerTests reusable.
    Adds the following traits to be reused in entity ChangeOpDeserializer
    tests as well as in tests of each of the field's ChangeOpDeserializers
    respectively:
      * AliasChangeOpDeserializationTest
      * ClaimsChangeOpDeserializationTest
      * DescriptionsChangeOpDeserializationTest
      * LabelsChangeOpDeserializationTest
    
    Change-Id: I5b3dc0eddea5a6e236cb14885d65e63d5866a310

---
+++ repo/tests/phpunit/includes/ChangeOp/Deserialization/AliasChangeOpDeserializationTester.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Wikibase\Repo\Tests\ChangeOp\Deserialization;
+
+use Wikibase\Summary;
+
+/**
+ * Set of test methods that can be reused in AliasesChangeOpDeserializerTest and tests for
+ * ChangeOpDeserializers of entities that have aliases.
+ * Using this trait requires classes to implement getEntity and getChangeOpDeserializer methods.
+ *
+ * @license GPL-2.0+
+ */
+trait AliasChangeOpDeserializationTester {
+
+	public function testGivenChangeRequestSettingAliasesToItemWithNoAlias_addsAlias() {
+		$entity = $this->getEntity();
+		$alias = 'foo';
+		$changeOp = $this->getChangeOpDeserializer()->createEntityChangeOp( [
+			'aliases' => [ 'en' => [ 'language' => 'en', 'value' => $alias ] ]
+		] );
+
+		$changeOp->apply( $entity, new Summary() );
+		$this->assertSame( $entity->getAliasGroups()->getByLanguage( 'en' )->getAliases(), [ $alias ] );
+	}
+
+	public function testGivenChangeRequestSettingAliases_overridesExistingAliases() {
+		$entity = $this->getEntityWithExistingAliases();
+		$newAlias = 'foo';
+		$changeOp = $this->getChangeOpDeserializer()->createEntityChangeOp( [
+			'aliases' => [ 'en' => [ 'language' => 'en', 'value' => $newAlias ] ]
+		] );
+
+		$changeOp->apply( $entity, new Summary() );
+		$this->assertSame( $entity->getAliasGroups()->getByLanguage( 'en' )->getAliases(), [ $newAlias ] );
+	}
+
+	public function testGivenChangeRequestRemovingAllExistingEnAliases_enAliasGroupDoesNotExist() {
+		$entity = $this->getEntityWithExistingAliases();
+		$existingAliases = $entity->getAliasGroups()->getByLanguage( 'en' )->getAliases();
+		$changeOp = $this->getChangeOpDeserializer()->createEntityChangeOp( [
+			'aliases' => array_map( function( $alias ) {
+				return [ 'language' => 'en', 'value' => $alias, 'remove' => '' ];
+			}, $existingAliases )
+		] );
+
+		$changeOp->apply( $entity, new Summary() );
+		$this->assertFalse( $entity->getAliasGroups()->hasGroupForLanguage( 'en' ) );
+	}
+
+	public function testGivenChangeRequestAddingAlias_addsAlias() {
+		$entity = $this->getEntityWithExistingAliases();
+		$newAlias = 'foo';
+		$existingAliases = $entity->getAliasGroups()->getByLanguage( 'en' )->getAliases();
+		$changeOp = $this->getChangeOpDeserializer()->createEntityChangeOp( [
+			'aliases' => [
+				'en' => [ 'language' => 'en', 'value' => $newAlias, 'add' => '' ]
+			]
+		] );
+
+		$changeOp->apply( $entity, new Summary() );
+		$this->assertSame(
+			array_merge( $existingAliases, [ $newAlias ] ),
+			$entity->getAliasGroups()->getByLanguage( 'en' )->getAliases()
+		);
+	}
+
+	private function getEntityWithExistingAliases() {
+		$existingEnAliases = [ 'en-existingAlias1', 'en-existingAlias2' ];
+		$entity = $this->getEntity();
+		$entity->setAliases( 'en', $existingEnAliases );
+
+		return $entity;
+	}
+
+}
---
+++ repo/tests/phpunit/includes/ChangeOp/Deserialization/ClaimsChangeOpDeserializationTester.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace Wikibase\Repo\Tests\ChangeOp\Deserialization;
+
+use DataValues\StringValue;
+use Wikibase\DataModel\Entity\EntityDocument;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\DataModel\Services\Statement\GuidGenerator;
+use Wikibase\DataModel\Snak\PropertyNoValueSnak;
+use Wikibase\DataModel\Snak\PropertyValueSnak;
+use Wikibase\DataModel\Statement\Statement;
+use Wikibase\DataModel\Statement\StatementList;
+use Wikibase\Repo\WikibaseRepo;
+use Wikibase\Summary;
+
+/**
+ * Set of test methods that can be reused in ClaimsChangeOpDeserializerTest and tests for
+ * ChangeOpDeserializers of entities that have claims.
+ * Using this trait requires classes to implement getEntity and getChangeOpDeserializer methods.
+ *
+ * @license GPL-2.0+
+ */
+trait ClaimsChangeOpDeserializationTester {
+
+	/**
+	 * @dataProvider setStatementProvider
+	 */
+	public function testGivenNewStatementChangeRequest_setsStatement( $changeRequest, EntityDocument $entity, $property ) {
+		$changeOp = $this->getChangeOpDeserializer()->createEntityChangeOp( $changeRequest );
+		$changeOp->apply( $entity, new Summary() );
+
+		$this->assertFalse(
+			$entity->getStatements()->getByPropertyId( $property )->isEmpty()
+		);
+	}
+
+	public function setStatementProvider() {
+		$property = new PropertyId( 'P7' );
+		$statement = new Statement( new PropertyNoValueSnak( $property ) );
+		$statementSerialization = $this->getStatementSerializer()->serialize( $statement );
+		$entity = $this->getEntity();
+
+		return [
+			'numeric index format' => [ [ 'claims' => [ $statementSerialization ] ], $entity, $property ],
+			'associative format' => [ [ 'claims' => [ 'P7' => [ $statementSerialization ] ] ], $entity, $property ],
+		];
+	}
+
+	/**
+	 * @dataProvider deleteStatementProvider
+	 */
+	public function testGivenRemoveChangeRequest_removesStatement( $changeRequest, EntityDocument $entity, $property ) {
+		$changeOp = $this->getChangeOpDeserializer()->createEntityChangeOp( $changeRequest );
+		$changeOp->apply( $entity, new Summary() );
+
+		$this->assertTrue( $entity->getStatements()->getByPropertyId( $property )->isEmpty() );
+	}
+
+	public function deleteStatementProvider() {
+		$property = new PropertyId( 'P7' );
+		$statement = new Statement( new PropertyNoValueSnak( $property ) );
+		$statement->setGuid( 'test-guid' );
+		$entity = $this->getEntity();
+		$entity->setStatements( new StatementList( [ $statement ] ) );
+
+		return [
+			'numeric index format' => [
+				[ 'claims' => [
+					[ 'remove' => '', 'id' => $statement->getGuid() ]
+				] ],
+				$entity,
+				$property
+			],
+			'associative format' => [
+				[ 'claims' => [
+					'P7' => [ [ 'remove' => '', 'id' => $statement->getGuid() ] ]
+				] ],
+				$entity->copy(),
+				$property
+			],
+		];
+	}
+
+	/**
+	 * @dataProvider editStatementProvider
+	 */
+	public function testGivenEditChangeRequest_statementGetsChanged( $changeRequest, EntityDocument $entity ) {
+		$changeOp = $this->getChangeOpDeserializer()->createEntityChangeOp( $changeRequest );
+		$changeOp->apply( $entity, new Summary() );
+
+		$this->assertCount( 1, $entity->getStatements()->toArray() );
+		$this->assertSame(
+			'bar',
+			$entity->getStatements()->toArray()[0]
+				->getMainSnak()
+				->getDataValue()
+				->getValue()
+		);
+	}
+
+	public function editStatementProvider() {
+		$property = new PropertyId( 'P7' );
+		$statement = new Statement( new PropertyValueSnak( $property, new StringValue( 'foo' ) ) );
+		$entity = $this->getEntity();
+		$statement->setGuid( ( new GuidGenerator() )->newGuid( $entity->getId() ) );
+		$entity->setStatements( new StatementList( [ $statement ] ) );
+		$statementSerialization = $this->getStatementSerializer()->serialize( $statement );
+		$statementSerialization['mainsnak']['datavalue']['value'] = 'bar';
+
+		return [
+			'numeric index format' => [
+				[ 'claims' => [ $statementSerialization ] ],
+				$entity
+			],
+			'associative format' => [
+				[ 'claims' => [ 'P7' => [ $statementSerialization ] ] ],
+				$entity
+			],
+		];
+	}
+
+	private function getStatementSerializer() {
+		return WikibaseRepo::getDefaultInstance()->getStatementSerializer();
+	}
+
+}
