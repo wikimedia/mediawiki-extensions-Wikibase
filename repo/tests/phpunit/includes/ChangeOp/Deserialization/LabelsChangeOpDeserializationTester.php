701306a3c50e6e2cc14e8cf5831c127e4b715cc7
Jakob Warkotsch <j.warkotsch@gmail.com>
2017-02-27 14:13:55 +0100

    Make test methods of ChangeOpDeserializerTests reusable.
    Adds the following traits to be reused in entity ChangeOpDeserializer
    tests as well as in tests of each of the field's ChangeOpDeserializers
    respectively:
      * AliasChangeOpDeserializationTest
      * ClaimsChangeOpDeserializationTest
      * DescriptionsChangeOpDeserializationTest
      * LabelsChangeOpDeserializationTest
    
    Change-Id: I5b3dc0eddea5a6e236cb14885d65e63d5866a310

---
+++ repo/tests/phpunit/includes/ChangeOp/Deserialization/LabelsChangeOpDeserializationTester.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Wikibase\Repo\Tests\ChangeOp\Deserialization;
+
+use Wikibase\Summary;
+
+/**
+ * Set of test methods that can be reused in LabelsChangeOpDeserializerTest and tests for
+ * ChangeOpDeserializers of entities that have labels.
+ * Using this trait requires classes to implement getEntity and getChangeOpDeserializer methods.
+ *
+ * @license GPL-2.0+
+ */
+trait LabelsChangeOpDeserializationTester {
+
+	public function testGivenChangeRequestWithLabel_addsLabel() {
+		$entity = $this->getEntity();
+		$label = 'foo';
+		$changeOp = $this->getChangeOpDeserializer()->createEntityChangeOp(
+			[ 'labels' => [ 'en' => [ 'language' => 'en', 'value' => $label ] ] ]
+		);
+
+		$changeOp->apply( $entity, new Summary() );
+		$this->assertSame( $label, $entity->getLabels()->getByLanguage( 'en' )->getText() );
+	}
+
+	public function testGivenChangeRequestWithNewLabel_overridesExistingLabel() {
+		$entity = $this->getEntityWithEnLabel();
+		$newLabel = 'foo';
+		$changeOp = $this->getChangeOpDeserializer()->createEntityChangeOp(
+			[ 'labels' => [ 'en' => [ 'language' => 'en', 'value' => $newLabel ] ] ]
+		);
+
+		$changeOp->apply( $entity, new Summary() );
+		$this->assertSame( $newLabel, $entity->getLabels()->getByLanguage( 'en' )->getText() );
+	}
+
+	public function testGivenChangeRequestWithRemove_removesLabel() {
+		$entity = $this->getEntityWithEnLabel();
+		$changeOp = $this->getChangeOpDeserializer()->createEntityChangeOp(
+			[ 'labels' => [ 'en' => [ 'language' => 'en', 'remove' => '' ] ] ]
+		);
+
+		$changeOp->apply( $entity, new Summary() );
+		$this->assertFalse( $entity->getLabels()->hasTermForLanguage( 'en' ) );
+	}
+
+	public function testGivenChangeRequestWithEmptyLabel_removesLabel() {
+		$entity = $this->getEntityWithEnLabel();
+		$changeOp = $this->getChangeOpDeserializer()->createEntityChangeOp(
+			[ 'labels' => [ 'en' => [ 'language' => 'en', 'value' => '' ] ] ]
+		);
+
+		$changeOp->apply( $entity, new Summary() );
+		$this->assertFalse( $entity->getLabels()->hasTermForLanguage( 'en' ) );
+	}
+
+	private function getEntityWithEnLabel() {
+		$entity = $this->getEntity();
+		$entity->setLabel( 'en', 'en-label' );
+
+		return $entity;
+	}
+
+}
