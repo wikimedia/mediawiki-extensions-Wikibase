ee42b829f42d02fc0b339f24ef0399526f21c2d2
Leszek Manicki <leszek.manicki@wikimedia.de>
2017-02-27 18:03:52 +0100

    If search term is a entity ID add known repository prefixes
    If entity ID is given, prefixes of repositories configured
    as providing entities of the given type are prepended to
    the given search term, and a resulting entity ID is search
    in respective repositories.
    
    In particular, it is possible to search for entity ID
    provided by the foreign repository without providing
    the repository prefix in the search term.
    
    If multiple repositories provided entity of the given type,
    and accordingly prefixed matches were found in several
    repositories, all relevant matches will be returned.
    As of January 2017 single entity type is only provided by
    single repository but the code in EntitySearchHelper
    would be able to handle the multi-repo case if this
    was changed.
    
    Bug: T158169
    Change-Id: I2405dcdc2cedaf17b1df363bf7ea2918a338b909

--- repo/includes/WikibaseRepo.php
+++ repo/includes/WikibaseRepo.php
@@ -264,7 +264,7 @@ class WikibaseRepo {
 	/**
 	 * @var RepositoryDefinitions
 	 */
-	private $repositoryDefinitons;
+	private $repositoryDefinitions;
 
 	/**
 	 * @var ValueSnakRdfBuilderFactory
@@ -401,7 +401,7 @@ class WikibaseRepo {
 			$this->getVocabularyBaseUri(),
 			$this->getMonolingualTextLanguages(),
 			$this->getCachingCommonsMediaFileNameLookup(),
-			$this->repositoryDefinitons->getEntityTypesPerRepository(),
+			$this->repositoryDefinitions->getEntityTypesPerRepository(),
 			new MediaWikiPageNameNormalizer(),
 			$this->settings->getSetting( 'geoShapeStorageApiEndpointUrl' )
 		);
@@ -514,7 +514,7 @@ class WikibaseRepo {
 		$this->settings = $settings;
 		$this->dataTypeDefinitions = $dataTypeDefinitions;
 		$this->entityTypeDefinitions = $entityTypeDefinitions;
-		$this->repositoryDefinitons = $repositoryDefinitions;
+		$this->repositoryDefinitions = $repositoryDefinitions;
 		$this->entityDataRetrievalServiceFactory = $entityDataRetrievalServiceFactory;
 	}
 
@@ -1262,7 +1262,7 @@ class WikibaseRepo {
 	 *  entity types from the configured foreign repositories.
 	 */
 	public function getEnabledEntityTypes() {
-		return $this->repositoryDefinitons->getAllEntityTypes();
+		return $this->repositoryDefinitions->getAllEntityTypes();
 	}
 
 	/**
@@ -1864,6 +1864,13 @@ class WikibaseRepo {
 		}
 
 		return $this->entityRdfBuilderFactory;
+	}
+
+	/**
+	 * @return RepositoryDefinitions
+	 */
+	public function getRepositoryDefinitions() {
+		return $this->repositoryDefinitions;
 	}
 
 }
