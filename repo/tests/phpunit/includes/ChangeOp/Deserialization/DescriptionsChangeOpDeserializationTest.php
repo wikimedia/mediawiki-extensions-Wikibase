089e0ad9634d8413226da8b7c7bb8c106f23b74e
Jakob Warkotsch <j.warkotsch@gmail.com>
2017-02-27 14:13:55 +0100

    Make test methods of ChangeOpDeserializerTests reusable.
    Adds the following traits to be reused in entity ChangeOpDeserializer
    tests as well as in tests of each of the field's ChangeOpDeserializers
    respectively:
      * AliasChangeOpDeserializationTest
      * ClaimsChangeOpDeserializationTest
      * DescriptionsChangeOpDeserializationTest
      * LabelsChangeOpDeserializationTest
    
    Change-Id: I5b3dc0eddea5a6e236cb14885d65e63d5866a310

---
+++ repo/tests/phpunit/includes/ChangeOp/Deserialization/DescriptionsChangeOpDeserializationTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Wikibase\Repo\Tests\ChangeOp\Deserialization;
+
+use Wikibase\Summary;
+
+/**
+ * Set of test methods that can be reused in DescriptionsChangeOpDeserializerTest and tests for
+ * ChangeOpDeserializers of entities that have descriptions.
+ * Using this trait requires classes to implement getEntity and getChangeOpDeserializer methods.
+ *
+ * @group Wikibase
+ *
+ * @license GPL-2.0+
+ */
+trait DescriptionsChangeOpDeserializationTest {
+
+	public function testGivenChangeRequestWithDescription_addsDescription() {
+		$entity = $this->getEntity();
+		$description = 'foo';
+		$changeOp = $this->getChangeOpDeserializer()->createEntityChangeOp(
+			[
+				'descriptions' => [ 'en' => [ 'language' => 'en', 'value' => $description ] ]
+			]
+		);
+
+		$changeOp->apply( $entity, new Summary() );
+		$this->assertSame( $description, $entity->getDescriptions()->getByLanguage( 'en' )->getText() );
+	}
+
+	public function testGivenChangeRequestWithNewDescription_overridesExistingDescription() {
+		$entity = $this->getEntityWithEnDescription();
+		$newDescription = 'foo';
+		$changeOp = $this->getChangeOpDeserializer()->createEntityChangeOp(
+			[ 'descriptions' => [ 'en' => [ 'language' => 'en', 'value' => $newDescription ] ] ]
+		);
+
+		$changeOp->apply( $entity, new Summary() );
+		$this->assertSame( $newDescription, $entity->getDescriptions()->getByLanguage( 'en' )->getText() );
+	}
+
+	public function testGivenChangeRequestWithRemove_removesDescription() {
+		$entity = $this->getEntityWithEnDescription();
+		$changeOp = $this->getChangeOpDeserializer()->createEntityChangeOp(
+			[ 'descriptions' => [ 'en' => [ 'language' => 'en', 'remove' => '' ] ] ]
+		);
+
+		$changeOp->apply( $entity, new Summary() );
+		$this->assertFalse( $entity->getDescriptions()->hasTermForLanguage( 'en' ) );
+	}
+
+	public function testGivenChangeRequestWithEmptyValue_removesDescription() {
+		$entity = $this->getEntityWithEnDescription();
+		$changeOp = $this->getChangeOpDeserializer()->createEntityChangeOp(
+			[ 'descriptions' => [ 'en' => [ 'language' => 'en', 'value' => '' ] ] ]
+		);
+
+		$changeOp->apply( $entity, new Summary() );
+		$this->assertFalse( $entity->getDescriptions()->hasTermForLanguage( 'en' ) );
+	}
+
+	private function getEntityWithEnDescription() {
+		$entity = $this->getEntity();
+		$entity->setDescription( 'en', 'en-description' );
+
+		return $entity;
+	}
+
+}
