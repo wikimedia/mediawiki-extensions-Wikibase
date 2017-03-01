821b67a766a9ffc6c1a1a57ee650a1fbe4d4a01d
Jakob Warkotsch <j.warkotsch@gmail.com>
2017-03-01 17:39:00 +0100

    Reuse badge serialization validation in SetSiteLink.
    Removes parseSiteLinkBadges from ModifyEntity which is instead done
    using SiteLinkBadgeChangeOpSerializationValidator which was introduced
    for sitelink ChangeOp deserialization.
    
    Bug: T158864
    Change-Id: Iaad515029301887b5117480af1fe7ff8d479e062

--- repo/includes/WikibaseRepo.php
+++ repo/includes/WikibaseRepo.php
@@ -849,6 +849,13 @@ class WikibaseRepo {
 		);
 	}
 
+	public function getSiteLinkBadgeChangeOpSerializationValidator() {
+		return new SiteLinkBadgeChangeOpSerializationValidator(
+			$this->getEntityTitleLookup(),
+			array_keys( $this->settings->getSetting( 'badgeItems' ) )
+		);
+	}
+
 	/**
 	 * @return EntityChangeOpProvider
 	 */
@@ -869,10 +876,7 @@ class WikibaseRepo {
 			$changeOpFactoryProvider->getStatementChangeOpFactory(),
 			$changeOpFactoryProvider->getSiteLinkChangeOpFactory(),
 			new TermChangeOpSerializationValidator( $this->getTermsLanguages() ),
-			new SiteLinkBadgeChangeOpSerializationValidator(
-				$this->getEntityTitleLookup(),
-				array_keys( $this->settings->getSetting( 'badgeItems' ) )
-			),
+			$this->getSiteLinkBadgeChangeOpSerializationValidator(),
 			$this->getExternalFormatStatementDeserializer(),
 			new SiteLinkTargetProvider(
 				$this->getSiteLookup(),
