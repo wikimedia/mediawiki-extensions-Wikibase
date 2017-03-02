6d2a349ef56ea77fc96924fba836e6e12166bb4e
Amir Sarabadani <Ladsgroup@gmail.com>
2017-03-02 12:38:10 +0330

    Add EntityRdfBuilderFactory
    Bug: T157311
    Change-Id: Id5920889655136cf06800ca92d06edda4551813e

---
+++ repo/includes/Rdf/EntityRdfBuilderFactory.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Wikibase\Rdf;
+
+use Wikimedia\Assert\Assert;
+use Wikimedia\Purtle\RdfWriter;
+
+/**
+ * Factory for EntityRdfBuilder based on factory callbacks.
+ * For use with EntityTypeDefinitions.
+ *
+ * @license GPL-2.0+
+ * @author Amir Sarabadani <ladsgroup@gmail.com>
+ */
+class EntityRdfBuilderFactory {
+
+	/**
+	 * @var callable[]
+	 */
+	private $factoryCallbacks;
+
+	/**
+	 * @param callable[] $factoryCallbacks Factory callback functions as returned by
+	 *        EntityTypeDefinitions::getRdfBuilderFactoryCallbacks(). Callbacks will be invoked
+	 *        with the signature ($mode, RdfVocabulary, RdfWrite, EntityMentionListener, DedupeBag)
+	 *        and must return a EntityRdfBuilder (or null).
+	 */
+	public function __construct( array $factoryCallbacks ) {
+		Assert::parameterElementType( 'callable', $factoryCallbacks, '$factoryCallbacks' );
+
+		$this->factoryCallbacks = $factoryCallbacks;
+	}
+
+	/**
+	 * Returns an EntityRdfBuilder for reified value output.
+	 *
+	 * @param int                   $flavorFlags Flavor flags to use for the entity rdf builder
+	 * @param RdfVocabulary         $vocabulary
+	 * @param RdfWriter             $writer
+	 * @param EntityMentionListener $mentionedEntityTracker
+	 * @param DedupeBag             $dedupe
+	 * @return EntityRdfBuilder[]
+	 */
+	public function getEntityRdfBuilder(
+		$flavorFlags,
+		RdfVocabulary $vocabulary,
+		RdfWriter $writer,
+		EntityMentionListener $mentionedEntityTracker,
+		DedupeBag $dedupe
+	) {
+		return $this->createEntityRdfBuilders(
+			$flavorFlags,
+			$vocabulary,
+			$writer,
+			$mentionedEntityTracker,
+			$dedupe
+		);
+
+	}
+
+	/**
+	 * @param int                   $flavorFlags Flavor flags to use for the entity rdf builder
+	 * @param RdfVocabulary         $vocabulary
+	 * @param RdfWriter             $writer
+	 * @param EntityMentionListener $mentionedEntityTracker
+	 * @param DedupeBag             $dedupe
+	 *
+	 * @return EntityRdfBuilder[]
+	 */
+	private function createEntityRdfBuilders(
+		$flavorFlags,
+		RdfVocabulary $vocabulary,
+		RdfWriter $writer,
+		EntityMentionListener $mentionedEntityTracker,
+		DedupeBag $dedupe
+	) {
+		$builders = [];
+
+		foreach ( $this->factoryCallbacks as $key => $callback ) {
+			$builders[$key] = call_user_func(
+				$callback,
+				$flavorFlags,
+				$vocabulary,
+				$writer,
+				$mentionedEntityTracker,
+				$dedupe
+			);
+		}
+
+		return $builders;
+	}
+
+}
