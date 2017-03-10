20d9f356728353986563d0068619775de1446ea3
daniel <daniel.kinzler@wikimedia.de>
2017-03-10 21:47:49 +0100

    Factor access to settings out of top level factories.
    This allows the repo and client components to access each other's
    configuration without using eachother's code.
    
    Change-Id: If559e8616bab1dfeb6b9ec2d8a2b325573f26d60

---
+++ lib/tests/phpunit/WikibaseSettingsTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Wikibase\Lib\Tests;
+
+use MWException;
+use OutOfBoundsException;
+use PHPUnit_Framework_TestCase;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\DataModel\Term\Term;
+use Wikibase\WikibaseSettings;
+use Wikimedia\Assert\ParameterAssertionException;
+
+/**
+ * @covers Wikibase\WikibaseSettings
+ *
+ * @group Wikibase
+ *
+ * @license GPL-2.0+
+ * @author Daniel Kinzler <daniel.kinzler@wikimedia.de>
+ */
+class WikibaseSettingsTest extends PHPUnit_Framework_TestCase {
+
+	public function testGetSettings() {
+		if ( WikibaseSettings::isClientEnabled() ) {
+			$this->assertNotNull( WikibaseSettings::getSettings( 'Client' ) );
+		}
+
+		if ( WikibaseSettings::isRepoEnabled() ) {
+			$this->assertNotNull( WikibaseSettings::getSettings( 'Repo' ) );
+		}
+	}
+
+	public function testGetSettings_fails_givenUnknownConfigName() {
+		$this->setExpectedException( OutOfBoundsException::class );
+
+		$this->assertNotNull( WikibaseSettings::getSettings( 'SomethingThatDoesNotExist' ) );
+	}
+
+	public function testGetRepoSettings() {
+		if ( WikibaseSettings::isRepoEnabled() ) {
+			$this->assertNotNull( WikibaseSettings::getRepoSettings() );
+		} else {
+			$this->setExpectedException( MWException::class );
+			WikibaseSettings::getRepoSettings();
+		}
+	}
+
+	public function testGetClientSettings() {
+		if ( WikibaseSettings::isClientEnabled() ) {
+			$this->assertNotNull( WikibaseSettings::getClientSettings() );
+		} else {
+			$this->setExpectedException( MWException::class );
+			WikibaseSettings::getClientSettings();
+		}
+	}
+
+}
