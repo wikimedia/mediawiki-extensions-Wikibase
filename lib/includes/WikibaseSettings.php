99bcdfdf3c36f21192481864891c67551f2b38c1
daniel <daniel.kinzler@wikimedia.de>
2017-03-10 21:47:49 +0100

    Factor access to settings out of top level factories.
    This allows the repo and client components to access each other's
    configuration without using eachother's code.
    
    Change-Id: If559e8616bab1dfeb6b9ec2d8a2b325573f26d60

---
+++ lib/includes/WikibaseSettings.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Wikibase;
+
+use Hooks;
+use MWException;
+use OutOfBoundsException;
+
+/**
+ * WikibaseSettings is a static access point to Wikibase settings defined as global state
+ * (typically in LocalSettings.php).
+ *
+ * @note WikibaseSettings is intended for internal use by bootstrapping code. Application service
+ * logic should have individual settings injected, static entry points to application logic should
+ * use top level factory methods such as WikibaseRepo::getRepoSettings() and
+ * WikibaseClient::getClientSettings().
+ *
+ * @todo Move this to a separate component.
+ *
+ * @license GPL 2+
+ * @author Daniel Kinzler
+ */
+class WikibaseSettings {
+
+	/**
+	 * Returns settings for a wikibase component based on global state.
+	 * This is intended to be used to access settings specified in LocalSettings.php.
+	 *
+	 * @param string $var The name of a global variable.
+	 *
+	 * @return SettingsArray
+	 */
+	private static function getSettings( $var ) {
+		if ( !isset( $GLOBALS[$var] ) ) {
+			throw new OutOfBoundsException( 'No such global configuration variable: ' . $var );
+		}
+
+		if ( !is_array( $GLOBALS[$var] ) ) {
+			throw new OutOfBoundsException( 'Not a Wikibase configuration array: ' . $var );
+		}
+
+		$settings = $GLOBALS[$var];
+
+		return new SettingsArray( $settings );
+	}
+
+	/**
+	 * @note This runs the WikibaseEntityNamespaces hook to allow extensions to modify
+	 *       the 'entityNamepsaces' setting.
+	 *
+	 * @throws MWException
+	 *
+	 * @returns SettingsArray
+	 */
+	public static function getRepoSettings() {
+		if ( !self::isRepoEnabled() ) {
+			throw new MWException( 'Cannot access repo settings: Wikibase Repository component is not enabled!' );
+		}
+
+		$settings = self::getSettings( 'wgWBRepoSettings' );
+		$settings->setSetting( 'entityNamespaces', self::buildEntityNamespaceConfigurations( $settings ) );
+		return $settings;
+	}
+
+	/**
+	 * @throws MWException
+	 *
+	 * @returns SettingsArray
+	 */
+	public static function getClientSettings() {
+		if ( !self::isClientEnabled() ) {
+			throw new MWException( 'Cannot access client settings: Wikibase Client component is not enabled!' );
+		}
+
+		return self::getSettings( 'wgWBClientSettings' );
+	}
+
+	/**
+	 * @returns true if and only if the Wikibase client component is enabled on this wiki.
+	 */
+	public static function isClientEnabled() {
+		return defined( 'WBC_VERSION' );
+	}
+
+	/**
+	 * @returns true if and only if the Wikibase repository component is enabled on this wiki.
+	 */
+	public static function isRepoEnabled() {
+		return defined( 'WB_VERSION' );
+	}
+
+	/**
+	 * @throws MWException in case of a misconfiguration
+	 * @return int[] An array mapping entity type identifiers to namespace numbers.
+	 */
+	private static function buildEntityNamespaceConfigurations( SettingsArray $settings ) {
+		if ( !$settings->hasSetting( 'entityNamespaces' ) ) {
+			throw new MWException( 'Wikibase: Incomplete configuration: '
+				. '$wgWBRepoSettings[\'entityNamespaces\'] has to be set to an '
+				. 'array mapping entity types to namespace IDs. '
+				. 'See Wikibase.example.php for details and examples.' );
+		}
+
+		$namespaces = $settings->getSetting( 'entityNamespaces' );
+		Hooks::run( 'WikibaseEntityNamespaces', [ &$namespaces ] );
+		return $namespaces;
+	}
+
+}
