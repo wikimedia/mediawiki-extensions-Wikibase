fe946802389f238a485486b7ecda85f97c904b8a
Bekh-Ivanov Aleksey <bekh6ex@users.noreply.github.com>
2017-03-14 18:51:54 +0100

    Add autoload-dev composer section (#724)
    * Add autoload-dev composer section
    
    * Remove redundant code from bootstrap for tests

--- lib/packages/wikibase/data-model/tests/bootstrap.php
+++ lib/packages/wikibase/data-model/tests/bootstrap.php
@@ -18,9 +18,4 @@ if ( !is_readable( __DIR__ . '/../vendor/autoload.php' ) ) {
 	die( 'You need to install this package with Composer before you can run the tests' );
 }
 
-$classLoader = require __DIR__ . '/../vendor/autoload.php';
-
-$classLoader->addPsr4( 'Wikibase\\DataModel\\Tests\\', __DIR__ . '/unit/' );
-$classLoader->addPsr4( 'Wikibase\\DataModel\\Fixtures\\', __DIR__ . '/fixtures/' );
-
-unset( $classLoader );
+require __DIR__ . '/../vendor/autoload.php';
