8cd1dd7749967c65479c7dfdf5668861624ee9e7
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2017-03-15 12:39:40 +0100

    Update Wikibase DataModel to 7.0.0
    Change-Id: Id5e83ec591346b205cc099a48b44a92a4c518e41

--- lib/tests/phpunit/Changes/EntityChangeTest.php
+++ lib/tests/phpunit/Changes/EntityChangeTest.php
@@ -296,7 +296,7 @@ class EntityChangeTest extends ChangeRowTest {
 			'mainsnak' => array(
 				'snaktype' => 'novalue',
 				'property' => 'P1',
-				'hash' => '2d7ef41c913ec99eb249645e154e77670090db68',
+				'hash' => 'any hash',
 			),
 			'type' => 'statement',
 			'rank' => 'normal',
@@ -311,6 +311,7 @@ class EntityChangeTest extends ChangeRowTest {
 
 		$json = $change->getSerializedInfo();
 		$array = json_decode( $json, true );
+		$array['diff']['newvalue']['mainsnak']['hash'] = 'any hash';
 		$this->assertSame( $expected, $array['diff']['newvalue'] );
 	}
 
--- repo/tests/phpunit/includes/Rdf/FullStatementRdfBuilderTest.php
+++ repo/tests/phpunit/includes/Rdf/FullStatementRdfBuilderTest.php
@@ -157,7 +157,7 @@ class FullStatementRdfBuilderTest extends \PHPUnit_Framework_TestCase {
 
 	public function provideAddEntity_seen() {
 		return array(
-			array( 'Q7', 'Q7_all_refs_seen', array( '569a639de6dc67beb02644abfcf55534cb2f51ce' ) ),
+			array( 'Q7', 'Q7_all_refs_seen', array( '647b6894edee42d5195b24ad4ffb4722771fcfdf' ) ),
 		);
 	}
 
