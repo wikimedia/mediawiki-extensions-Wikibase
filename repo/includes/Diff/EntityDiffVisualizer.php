ccdeb89101cf808fb27bb2680c615f29a1731c8f
Amir Sarabadani <ladsgroup@gmail.com>
2017-03-22 22:26:47 +0430

    Introduce EntityDiffVisualizer interface and use it in BasicEntityDiffVisualizer
    Bug: T160656
    Change-Id: I2b41d5c0ad0098dfeac0f479ed479494ddc61b7a

---
+++ repo/includes/Diff/EntityDiffVisualizer.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Wikibase\Repo\Diff;
+
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\Repo\Content\EntityContentDiff;
+
+/**
+ * Interface for different visualizing diffs.
+ *
+ * @license GPL-2.0+
+ * @author Amir Sarabadani <ladsgroup@gmail.com>
+ */
+interface EntityDiffVisualizer {
+
+	/**
+	 * This method should get an EntityContentDiff object and turn it into HTML
+	 * EntityId itself is going to be passed to give proper context (e.g. dynamic dispatching)
+	 *
+	 * @param EntityContentDiff $diff
+	 * @return string HTML
+	 */
+	public function visualizeEntityContentDiff( EntityContentDiff $diff );
+
+}
