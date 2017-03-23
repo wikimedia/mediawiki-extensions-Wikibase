8c6af7627e75efe1e5d29427478b5b8204c085ef
daniel <daniel.kinzler@wikimedia.de>
2017-03-23 18:26:01 +0100

    Use uncritical floatvalues for RDF test data
    RDF data includes hashes of references. Hashes depend on the serialized form
    of values, and the serialized form of floats is unstable, because it depends
    on a PHP ini setting, serialize_precision.
    
    Bug: T161231
    Change-Id: Ie443ae51678baa389224be55832ed73664537254

--- repo/tests/phpunit/includes/Rdf/FullStatementRdfBuilderTest.php
+++ repo/tests/phpunit/includes/Rdf/FullStatementRdfBuilderTest.php
@@ -157,7 +157,7 @@ class FullStatementRdfBuilderTest extends \PHPUnit_Framework_TestCase {
 
 	public function provideAddEntity_seen() {
 		return array(
-			array( 'Q7', 'Q7_all_refs_seen', array( '569a639de6dc67beb02644abfcf55534cb2f51ce' ) ),
+			array( 'Q7', 'Q7_all_refs_seen', array( 'd2412760c57cacd8c8f24d9afde3b20c87161cca' ) ),
 		);
 	}
 
