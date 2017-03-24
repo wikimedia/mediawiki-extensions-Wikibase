968af214eb0b2e9fcb63a85e24f607f7834a83e9
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2017-03-24 12:25:32 +0100

    Change bad ASCII to UTF-8 validation in terms/value validators
    We need to BACKPORT this. Not necesarrily the tests. I also believe the
    changes to ValidatorBuilders are not deployed yet. So the only file that
    needs backporting is TermValidatorFactory.
    
    Bug: T161263
    Change-Id: If129df5364afd03848d40658f605923d252e4a69

--- repo/tests/phpunit/includes/Validators/TermValidatorFactoryTest.php
+++ repo/tests/phpunit/includes/Validators/TermValidatorFactoryTest.php
@@ -111,6 +111,7 @@ class TermValidatorFactoryTest extends \PHPUnit_Framework_TestCase {
 		return [
 			'Space' => [ 'x x', true ],
 			'Unicode support' => [ 'Äöü', true ],
+			'T161263' => [ 'Ӆ', true ],
 
 			// Length checks
 			'To short' => [ '', false ],
@@ -130,7 +131,7 @@ class TermValidatorFactoryTest extends \PHPUnit_Framework_TestCase {
 			'U+000B: Vertical tab' => [ "x\x0Bx", false ],
 			'U+000C: Form feed' => [ "x\fx", false ],
 			'U+000D: Return' => [ "x\rx", false ],
-			'U+0085: Next line' => [ "x\x85x", false ],
+			'U+0085: Next line' => [ "x\xC2\x85x", false ],
 		];
 	}
 
