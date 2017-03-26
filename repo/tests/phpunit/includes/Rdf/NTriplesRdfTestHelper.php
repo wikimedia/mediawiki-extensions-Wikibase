b1045cffd501fe6805895c867721e5f7b6cbda8b
daniel <daniel.kinzler@wikimedia.de>
2017-03-26 21:56:49 +0200

    Improve RDF test helpers.
    This patch allows all RDF tests to match against a set of files,
    instead of a single file. The idea is to modularize test data.
    
    See the follow-up changes for the intended use.
    
    Change-Id: I3b6ffe8bce3fa98951bea0e51954d63126d39be7

--- repo/tests/phpunit/includes/Rdf/NTriplesRdfTestHelper.php
+++ repo/tests/phpunit/includes/Rdf/NTriplesRdfTestHelper.php
@@ -2,10 +2,13 @@
 
 namespace Wikibase\Repo\Tests\Rdf;
 
+use InvalidArgumentException;
 use PHPUnit_Framework_Assert;
+use PHPUnit_Framework_AssertionFailedError;
+use RuntimeException;
 
 /**
- * Utility class to load, normalize and compare N-Triples in RDF builder tests.
+ * Utility class to normalize and compare N-Triples in RDF builder tests.
  *
  * @see https://en.wikipedia.org/wiki/N-Triples
  *
@@ -14,6 +17,49 @@ use PHPUnit_Framework_Assert;
  * @author Thiemo Mättig
  */
 class NTriplesRdfTestHelper {
+
+	/**
+	 * @var RdfBuilderTestData|null
+	 */
+	private $testData;
+
+	/**
+	 * @var bool
+	 */
+	private $allBlanksEqual = false;
+
+	public function __construct( RdfBuilderTestData $testData = null ) {
+		$this->testData = $testData;
+	}
+
+	/**
+	 * @return boolean whether all blank nodes are considered equal
+	 */
+	public function getAllBlanksEqual() {
+		return $this->allBlanksEqual;
+	}
+
+	/**
+	 * Setting all blank nodes to be equal allows tests to be robust against changes in the
+	 * numbering of blank nodes. However, it also means that no test can reöly on the identity
+	 * of a blank node.
+	 *
+	 * @param boolean $allBlanksEqual whether all blank nodes are considered equal
+	 */
+	public function setAllBlanksEqual( $allBlanksEqual ) {
+		$this->allBlanksEqual = $allBlanksEqual;
+	}
+
+	/**
+	 * @return RdfBuilderTestData|null
+	 */
+	public function getTestData() {
+		if ( !$this->testData ) {
+			throw new RuntimeException( 'No RdfBuilderTestData provided to constructor' );
+		}
+
+		return $this->testData;
+	}
 
 	/**
 	 * @param string[]|string $nTriples
@@ -26,6 +72,16 @@ class NTriplesRdfTestHelper {
 			$nTriples = explode( "\n", rtrim( $nTriples, "\n" ) );
 		}
 
+		if ( $this->allBlanksEqual ) {
+			$nTriples = array_map(
+				function ( $line ) {
+					return preg_replace( '/_:\w+/', '_:#####', $line );
+				},
+				$nTriples
+			);
+		}
+
+		$nTriples = array_unique( $nTriples );
 		sort( $nTriples );
 
 		return $nTriples;
@@ -50,6 +106,80 @@ class NTriplesRdfTestHelper {
 		// directly looks a bit odd in code, but produces meaningful output, especially if the input
 		// was sorted.
 		PHPUnit_Framework_Assert::assertEquals( $missing, $extra, $message );
+	}
+
+	/**
+	 * Compares the actual data to the expected data, and records any differences by creating
+	 * new files in the test data directory, with the suffixes .extra and .missing.
+	 *
+	 * @note This method is intended to be used when updating test data, or manually
+	 * investigating test failures. Code using this method is typically not checked in.
+	 *
+	 * @param string|string[] $dataSetNames
+	 * @param string|string[] $actual
+	 */
+	public function recordNTriplesDatasetDifferences( $dataSetNames, $actual ) {
+		$testData = $this->getTestData();
+
+		$dataSetNames = (array)$dataSetNames;
+		$joinedName = join( '-', $dataSetNames );
+
+		$expected = $testData->getNTriples( $dataSetNames );
+		$expected = $this->normalizeNTriples( $expected );
+		$actual = $this->normalizeNTriples( $actual );
+
+		// Comparing $expected and $actual directly would show triples that are present in both but
+		// shifted in position. That makes the output hard to read. Calculating the $missing and
+		// $extra sets helps.
+		$extra = array_diff( $actual, $expected );
+		$missing = array_diff( $expected, $actual );
+
+		if ( !empty( $extra ) ) {
+			$testData->putTestData( $joinedName, $extra, '.extra' );
+		}
+
+		if ( !empty( $missing ) ) {
+			$testData->putTestData( $joinedName, $missing, '.missing' );
+		}
+	}
+
+	/**
+	 * Creates a test data file from the given triples. The file will be created in the
+	 * test data directory.
+	 *
+	 * @note This method is intended to be used when updating test data, or manually
+	 * investigating test failures. Code using this method is typically not checked in.
+	 *
+	 * @param string|string[] $dataSetNames
+	 * @param string|string[] $triples
+	 * @param string $suffix File name suffix, including the leading dot. None per default.
+	 */
+	public function createNTriplesDataset( $dataSetNames, $triples, $suffix = '' ) {
+		$testData = $this->getTestData();
+
+		$dataSetNames = (array)$dataSetNames;
+		$joinedName = join( '-', $dataSetNames );
+
+		$testData->putTestData( $joinedName, $triples, $suffix );
+	}
+
+	/**
+	 * @param string|string[] $dataSetNames
+	 * @param string|string[] $actual
+	 * @param string $message
+	 */
+	public function assertNTriplesEqualsDataset( $dataSetNames, $actual, $message = null ) {
+		$testData = $this->getTestData();
+
+		$dataSetNames = (array)$dataSetNames;
+		$prettyName = join( '+', $dataSetNames );
+
+		if ( $message === null ) {
+			$message = "Data set $prettyName";
+		}
+
+		$expected = $testData->getNTriples( $dataSetNames );
+		$this->assertNTriplesEquals( $expected, $actual, $message );
 	}
 
 }
