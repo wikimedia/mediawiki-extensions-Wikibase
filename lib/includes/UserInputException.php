46ce2779c00059409dfcf99105c79be5568b716d
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2017-03-26 17:46:27 +0200

    Always escape user input in UserInputException
    I played around with different possible ways to fix this. I started
    adding wfEscapeWikiText to all calls of the UserInputException as well
    as MessageException constructors. Then I realized that *all*
    UserInputException have the exact same problem. I decided to change the
    interface of the class to expect unescaped user input (this fits very well
    to the name of the class), and do the escaping in the constructor.
    
    The bug describes an attack vector that allows injecting arbitrary
    Wikitext. It does not allow injecting arbitrary HTML or JavaScript.
    
    This patch is intentionally minified, so we can easily backport it.
    
    Bug: T161418
    Change-Id: If6e79745ec79ba3574694601a0a75fd93f0246a8

--- lib/includes/UserInputException.php
+++ lib/includes/UserInputException.php
@@ -2,6 +2,8 @@
 
 namespace Wikibase\Lib;
 
+use Exception;
+
 /**
  * Used in special pages and elsewhere to handle user input errors,
  * allow them to bubble up to presentation layer and contain message
@@ -11,5 +13,19 @@ namespace Wikibase\Lib;
  * @author Katie Filbert < aude.wiki@gmail.com >
  */
 class UserInputException extends MessageException {
+
+	/**
+	 * @see MessageException::__construct
+	 *
+	 * @param string $key
+	 * @param array $params List of parameters, depending on the message. Since this is a "user
+	 *  input" exception, callers are expected to pass in unescaped user input. This class will take
+	 *  care of proper wikitext escaping.
+	 * @param string $message
+	 * @param Exception|null $previous
+	 */
+	public function __construct( $key, array $params, $message, Exception $previous = null ) {
+		parent::__construct( $key, array_map( 'wfEscapeWikiText', $params ), $message, $previous );
+	}
 
 }
