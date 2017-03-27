02477cfa742f154907c09a86b56afd66b63765cb
daniel <daniel.kinzler@wikimedia.de>
2017-03-27 22:37:10 +0200

    Consolidate RDF test data.
    This patch removes redundancy from the RDF test data by splitting the test files
    into re-usable parts.
    
    The intent is to reduce the effort needed to adjust test data files when extending
    or changing the RDF binding.
    
    REVIEW NOTE: Note that no application logic changed, and the unit tests only changed
    in how they load the test data. Since the actual output has not changed and the tests
    still pass, a line-by-line review of the test data should not be needed.
    
    Change-Id: I340c9bfa96160606dc828e69984e82a00d8da9e3

--- repo/tests/phpunit/includes/Rdf/RdfBuilderTest.php
+++ repo/tests/phpunit/includes/Rdf/RdfBuilderTest.php
@@ -115,13 +115,13 @@ class RdfBuilderTest extends \MediaWikiTestCase {
 
 	public function provideAddEntity() {
 		$rdfTests = [
-			[ 'Q1', 'Q1_simple' ],
-			[ 'Q2', 'Q2_labels' ],
-			[ 'Q3', 'Q3_links' ],
-			[ 'Q4', 'Q4_claims' ],
+			[ 'Q1', 'Q1_info' ],
+			[ 'Q2', [ 'Q2_meta', 'Q2_version', 'Q2_stub', 'Q2_aliases' ] ],
+			[ 'Q3', [ 'Q3_meta', 'Q3_version', 'Q3_sitelinks' ] ],
+			[ 'Q4', [ 'Q4_meta', 'Q4_version', 'Q4_statements', 'Q4_direct', 'Q4_values' ] ],
 			[ 'Q5', 'Q5_badges' ],
-			[ 'Q6', 'Q6_qualifiers' ],
-			[ 'Q7', 'Q7_references' ],
+			[ 'Q6', [ 'Q6_meta', 'Q6_version', 'Q6_statements', 'Q6_qualifiers', 'Q6_values', 'Q6_referenced' ] ],
+			[ 'Q7', [ 'Q7_meta', 'Q7_version', 'Q7_statements', 'Q7_reference_refs', 'Q7_references', 'Q7_values' ] ],
 			[ 'Q8', 'Q8_baddates' ],
 		];
 
@@ -134,15 +134,9 @@ class RdfBuilderTest extends \MediaWikiTestCase {
 	public function testAddEntity( $entityName, $dataSetName ) {
 		$entity = $this->getEntityData( $entityName );
 
-		$builder =
-			$this->newRdfBuilder( RdfProducer::PRODUCE_ALL_STATEMENTS |
-			                      RdfProducer::PRODUCE_TRUTHY_STATEMENTS |
-			                      RdfProducer::PRODUCE_QUALIFIERS |
-			                      RdfProducer::PRODUCE_REFERENCES | RdfProducer::PRODUCE_SITELINKS |
-			                      RdfProducer::PRODUCE_VERSION_INFO |
-			                      RdfProducer::PRODUCE_FULL_VALUES );
+		$builder = $this->newRdfBuilder( RdfProducer::PRODUCE_ALL );
 		$builder->addEntity( $entity );
-		$builder->addEntityRevisionInfo( $entity->getId(), 42, "2014-11-04T03:11:05Z" );
+		$builder->addEntityRevisionInfo( $entity->getId(), 42, "2013-10-04T03:31:05Z" );
 
 		$this->helper->assertNTriplesEqualsDataset( $dataSetName, $builder->getRDF() );
 	}
@@ -166,7 +160,8 @@ class RdfBuilderTest extends \MediaWikiTestCase {
 			                      RdfProducer::PRODUCE_FULL_VALUES );
 		$builder->addEntityStub( $entity );
 
-		$this->helper->assertNTriplesEqualsDataset( $dataSetName, $builder->getRDF() );
+		$triples = $builder->getRDF();
+		$this->helper->assertNTriplesEqualsDataset( $dataSetName, $triples );
 	}
 
 	public function testAddEntityRedirect() {
@@ -184,29 +179,59 @@ class RdfBuilderTest extends \MediaWikiTestCase {
 	public function getProduceOptions() {
 		$produceTests = [
 			[
+				'Q4',
+				RdfProducer::PRODUCE_ALL_STATEMENTS,
+				[ 'Q4_meta', 'Q4_statements' ]
+			],
+			[
+				'Q4',
+				RdfProducer::PRODUCE_TRUTHY_STATEMENTS,
+				[ 'Q4_meta', 'Q4_direct' ]
+			],
+			[
+				'Q6',
+				RdfProducer::PRODUCE_ALL_STATEMENTS,
+				[ 'Q6_meta', 'Q6_statements' ]
+			],
+			[
 				'Q6',
 				RdfProducer::PRODUCE_ALL_STATEMENTS | RdfProducer::PRODUCE_QUALIFIERS,
-				'Q6_with_qualifiers'
+				[ 'Q6_meta', 'Q6_statements', 'Q6_qualifiers' ]
+			],
+			[
+				'Q7',
+				RdfProducer::PRODUCE_ALL_STATEMENTS,
+				[ 'Q7_meta', 'Q7_statements' ]
 			],
 			[
 				'Q7',
 				RdfProducer::PRODUCE_ALL_STATEMENTS | RdfProducer::PRODUCE_REFERENCES,
-				'Q7_refs'
+				[ 'Q7_meta', 'Q7_statements', 'Q7_reference_refs', 'Q7_references' ]
+			],
+			[
+				'Q3',
+				RdfProducer::PRODUCE_SITELINKS,
+				[ 'Q3_meta', 'Q3_sitelinks' ]
 			],
 			[
 				'Q4',
 				RdfProducer::PRODUCE_ALL_STATEMENTS | RdfProducer::PRODUCE_PROPERTIES,
-				'Q4_props'
+				[ 'Q4_meta', 'Q4_statements', 'Q4_props' ]
 			],
 			[
 				'Q4',
 				RdfProducer::PRODUCE_ALL_STATEMENTS | RdfProducer::PRODUCE_FULL_VALUES,
-				'Q4_values'
+				[ 'Q4_meta', 'Q4_values', 'Q4_statements' ]
+			],
+			[
+				'Q1',
+				RdfProducer::PRODUCE_VERSION_INFO,
+				'Q1_info'
 			],
 			[
 				'Q4',
 				RdfProducer::PRODUCE_TRUTHY_STATEMENTS | RdfProducer::PRODUCE_RESOLVED_ENTITIES,
-				'Q4_resolved'
+				[ 'Q4_meta', 'Q4_direct', 'Q4_referenced' ]
 			],
 			[
 				'Q10',
@@ -228,7 +253,9 @@ class RdfBuilderTest extends \MediaWikiTestCase {
 		$builder->addEntity( $entity );
 		$builder->addEntityRevisionInfo( $entity->getId(), 42, "2013-10-04T03:31:05Z" );
 		$builder->resolveMentionedEntities( $this->getTestData()->getMockRepository() );
-		$this->helper->assertNTriplesEqualsDataset( $dataSetName, $builder->getRDF() );
+
+		$triples = $builder->getRDF();
+		$this->helper->assertNTriplesEqualsDataset( $dataSetName, $triples );
 	}
 
 	public function testDumpHeader() {
