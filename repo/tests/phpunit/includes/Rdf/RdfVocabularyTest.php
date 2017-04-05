c2161c337819c1eece3acabf5fc6bab471bf6ff8
Leszek Manicki <leszek.manicki@wikimedia.de>
2017-04-05 11:45:12 +0200

    Consider foreign entities in RDF mapping
    Namespaces used in clauses related to foreign entities now
    contain the repository name (prefix) of the repository
    the entity belongs to.
    
    Repository prefix is also dropped from the "local name" of
    the foreign entity.
    
    Bug: T161592
    Change-Id: I6acb00b23425bdce4dd03227415e593595b3c1cc

--- repo/tests/phpunit/includes/Rdf/RdfVocabularyTest.php
+++ repo/tests/phpunit/includes/Rdf/RdfVocabularyTest.php
@@ -5,6 +5,7 @@ namespace Wikibase\Repo\Tests\Rdf;
 use DataValues\StringValue;
 use OutOfBoundsException;
 use PHPUnit_Framework_TestCase;
+use Wikibase\DataModel\Entity\ItemId;
 use Wikibase\DataModel\Entity\Property;
 use Wikibase\DataModel\Entity\PropertyId;
 use Wikibase\DataModel\Snak\PropertyNoValueSnak;
@@ -24,8 +25,8 @@ class RdfVocabularyTest extends PHPUnit_Framework_TestCase {
 
 	private function newInstance() {
 		return new RdfVocabulary(
-			'<BASE>',
-			'<DATA>',
+			[ '' => '<BASE>', 'foo' => '<BASE-foo>' ],
+			[ '' => '<DATA>', 'foo' => '<DATA-foo>' ],
 			array( 'German' => 'de' ),
 			array( 'acme' => 'http://acme.test/vocab/ACME' )
 		);
@@ -67,6 +68,18 @@ class RdfVocabularyTest extends PHPUnit_Framework_TestCase {
 		$this->assertSame( 'P1', $actual );
 	}
 
+	public function testGetEntityLName_foreignEntity() {
+		$entityId = new PropertyId( 'foo:P1' );
+		$actual = $this->newInstance()->getEntityLName( $entityId );
+		$this->assertSame( 'P1', $actual );
+	}
+
+	public function testGetEntityLName_foreignEntityMultiplePrefixes() {
+		$entityId = new PropertyId( 'foo:bar:P1' );
+		$actual = $this->newInstance()->getEntityLName( $entityId );
+		$this->assertSame( 'bar--P1', $actual );
+	}
+
 	public function testGetEntityTypeName() {
 		$actual = $this->newInstance()->getEntityTypeName( 'type' );
 		$this->assertSame( 'Type', $actual );
@@ -78,6 +91,8 @@ class RdfVocabularyTest extends PHPUnit_Framework_TestCase {
 		$this->assertContainsOnly( 'string', $actual );
 		$this->assertContains( '<BASE>', $actual );
 		$this->assertContains( '<DATA>', $actual );
+		$this->assertContains( '<BASE-foo>', $actual );
+		$this->assertContains( '<DATA-foo>', $actual );
 	}
 
 	public function testGetNamespaceURI() {
@@ -93,6 +108,176 @@ class RdfVocabularyTest extends PHPUnit_Framework_TestCase {
 
 		$this->setExpectedException( OutOfBoundsException::class );
 		$vocab->getNamespaceURI( 'NonExistingNamespaceForGetNamespaceUriTest' );
+	}
+
+	public function testGetEntityNamespace() {
+		$vocabulary = $this->newInstance();
+
+		$this->assertEquals( RdfVocabulary::NS_ENTITY, $vocabulary->getEntityNamespace( new ItemId( 'Q12' ) ) );
+		$this->assertEquals( RdfVocabulary::NS_ENTITY . '-foo', $vocabulary->getEntityNamespace( new ItemId( 'foo:Q12' ) ) );
+	}
+
+	public function testGetDataNamespace() {
+		$vocabulary = $this->newInstance();
+
+		$this->assertEquals( RdfVocabulary::NS_DATA, $vocabulary->getDataNamespace( new ItemId( 'Q12' ) ) );
+		$this->assertEquals( RdfVocabulary::NS_DATA . '-foo', $vocabulary->getDataNamespace( new ItemId( 'foo:Q12' ) ) );
+	}
+
+	public function testGetClaimPropertyNamespace() {
+		$vocabulary = $this->newInstance();
+
+		$this->assertEquals(
+			RdfVocabulary::NSP_CLAIM,
+			$vocabulary->getClaimPropertyNamespace( new PropertyId( 'P22' ) )
+		);
+		$this->assertEquals(
+			RdfVocabulary::NSP_CLAIM . '-foo',
+			$vocabulary->getClaimPropertyNamespace( new PropertyId( 'foo:P22' ) )
+		);
+	}
+
+	public function testGetClaimStatementPropertyNamespace() {
+		$vocabulary = $this->newInstance();
+
+		$this->assertEquals(
+			RdfVocabulary::NSP_CLAIM_STATEMENT,
+			$vocabulary->getClaimStatementPropertyNamespace( new PropertyId( 'P22' ) )
+		);
+		$this->assertEquals(
+			RdfVocabulary::NSP_CLAIM_STATEMENT . '-foo',
+			$vocabulary->getClaimStatementPropertyNamespace( new PropertyId( 'foo:P22' ) )
+		);
+	}
+
+	public function testGetClaimValuePropertyNamespace() {
+		$vocabulary = $this->newInstance();
+
+		$this->assertEquals(
+			RdfVocabulary::NSP_CLAIM_VALUE,
+			$vocabulary->getClaimValuePropertyNamespace( new PropertyId( 'P22' ) )
+		);
+		$this->assertEquals(
+			RdfVocabulary::NSP_CLAIM_VALUE . '-foo',
+			$vocabulary->getClaimValuePropertyNamespace( new PropertyId( 'foo:P22' ) )
+		);
+	}
+
+	public function testGetClaimNormalizedValuePropertyNamespace() {
+		$vocabulary = $this->newInstance();
+
+		$this->assertEquals(
+			RdfVocabulary::NSP_CLAIM_VALUE_NORM,
+			$vocabulary->getClaimNormalizedValuePropertyNamespace( new PropertyId( 'P22' ) )
+		);
+		$this->assertEquals(
+			RdfVocabulary::NSP_CLAIM_VALUE_NORM . '-foo',
+			$vocabulary->getClaimNormalizedValuePropertyNamespace( new PropertyId( 'foo:P22' ) )
+		);
+	}
+
+	public function testGetDirectClaimPropertyNamespace() {
+		$vocabulary = $this->newInstance();
+
+		$this->assertEquals(
+			RdfVocabulary::NSP_DIRECT_CLAIM,
+			$vocabulary->getDirectClaimPropertyNamespace( new PropertyId( 'P22' ) )
+		);
+		$this->assertEquals(
+			RdfVocabulary::NSP_DIRECT_CLAIM . '-foo',
+			$vocabulary->getDirectClaimPropertyNamespace( new PropertyId( 'foo:P22' ) )
+		);
+	}
+
+	public function testGetQualifierPropertyNamespace() {
+		$vocabulary = $this->newInstance();
+
+		$this->assertEquals(
+			RdfVocabulary::NSP_QUALIFIER,
+			$vocabulary->getQualifierPropertyNamespace( new PropertyId( 'P22' ) )
+		);
+		$this->assertEquals(
+			RdfVocabulary::NSP_QUALIFIER . '-foo',
+			$vocabulary->getQualifierPropertyNamespace( new PropertyId( 'foo:P22' ) )
+		);
+	}
+
+	public function testGetQualifierValuePropertyNamespace() {
+		$vocabulary = $this->newInstance();
+
+		$this->assertEquals(
+			RdfVocabulary::NSP_QUALIFIER_VALUE,
+			$vocabulary->getQualifierValuePropertyNamespace( new PropertyId( 'P22' ) )
+		);
+		$this->assertEquals(
+			RdfVocabulary::NSP_QUALIFIER_VALUE . '-foo',
+			$vocabulary->getQualifierValuePropertyNamespace( new PropertyId( 'foo:P22' ) )
+		);
+	}
+
+	public function testGetQualifierNormalizedValuePropertyNamespace() {
+		$vocabulary = $this->newInstance();
+
+		$this->assertEquals(
+			RdfVocabulary::NSP_QUALIFIER_VALUE_NORM,
+			$vocabulary->getQualifierNormalizedValuePropertyNamespace( new PropertyId( 'P22' ) )
+		);
+		$this->assertEquals(
+			RdfVocabulary::NSP_QUALIFIER_VALUE_NORM . '-foo',
+			$vocabulary->getQualifierNormalizedValuePropertyNamespace( new PropertyId( 'foo:P22' ) )
+		);
+	}
+
+	public function testGetReferencePropertyNamespace() {
+		$vocabulary = $this->newInstance();
+
+		$this->assertEquals(
+			RdfVocabulary::NSP_REFERENCE,
+			$vocabulary->getReferencePropertyNamespace( new PropertyId( 'P22' ) )
+		);
+		$this->assertEquals(
+			RdfVocabulary::NSP_REFERENCE . '-foo',
+			$vocabulary->getReferencePropertyNamespace( new PropertyId( 'foo:P22' ) )
+		);
+	}
+
+	public function testGetReferenceValuePropertyNamespace() {
+		$vocabulary = $this->newInstance();
+
+		$this->assertEquals(
+			RdfVocabulary::NSP_REFERENCE_VALUE,
+			$vocabulary->getReferenceValuePropertyNamespace( new PropertyId( 'P22' ) )
+		);
+		$this->assertEquals(
+			RdfVocabulary::NSP_REFERENCE_VALUE . '-foo',
+			$vocabulary->getReferenceValuePropertyNamespace( new PropertyId( 'foo:P22' ) )
+		);
+	}
+
+	public function testGetReferenceNormalizedValuePropertyNamespace() {
+		$vocabulary = $this->newInstance();
+
+		$this->assertEquals(
+			RdfVocabulary::NSP_REFERENCE_VALUE_NORM,
+			$vocabulary->getReferenceNormalizedValuePropertyNamespace( new PropertyId( 'P22' ) )
+		);
+		$this->assertEquals(
+			RdfVocabulary::NSP_REFERENCE_VALUE_NORM . '-foo',
+			$vocabulary->getReferenceNormalizedValuePropertyNamespace( new PropertyId( 'foo:P22' ) )
+		);
+	}
+
+	public function testGetNoValuePropertyNamespace() {
+		$vocabulary = $this->newInstance();
+
+		$this->assertEquals(
+			RdfVocabulary::NSP_NOVALUE,
+			$vocabulary->getNoValuePropertyNamespace( new PropertyId( 'P22' ) )
+		);
+		$this->assertEquals(
+			RdfVocabulary::NSP_NOVALUE . '-foo',
+			$vocabulary->getNoValuePropertyNamespace( new PropertyId( 'foo:P22' ) )
+		);
 	}
 
 	public function testGetOntologyURI() {
