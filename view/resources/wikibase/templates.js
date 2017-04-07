3534a1a5f2dc1140f00aceb8adf63e8e5499dcc4
Aleksey Bekh-Ivanov (WMDE) <aleksey.bekh-ivanov@wikimedia.de>
2017-04-07 16:13:32 +0200

    Fix JS template rendering
    There were two problems:
    * not all attributes of template's root element were copied to the element
    * if template's root element contains text, then text won't be copied
      to the element contents
    
    Change-Id: I30fa2747597829adf0d116c4ede1cf8841a292fe

--- view/resources/wikibase/templates.js
+++ view/resources/wikibase/templates.js
@@ -310,12 +310,16 @@
 		// Copy template's root node children and classes to given root:
 		this.addClass( $template.prop( 'class' ) );
 
-		// Copy dir attribute if set:
-		if ( $template.prop( 'dir' ) !== '' ) {
-			this.prop( 'dir', $template.prop( 'dir' ) );
+		var templateAttrs = $template.getAttrs();
+		for ( var attrName in templateAttrs ) {
+			if ( attrName === 'class' ) {
+				continue;
+			}
+
+			this.attr( attrName, templateAttrs[attrName] );
 		}
 
-		this.empty().append( $template.children() );
+		this.empty().append( $template.contents() );
 
 		return this;
 	};
