86fac38e1883e8dd1781ea732e3f20abdaf4a82f
aude <aude.wiki@gmail.com>
2017-04-09 14:25:41 -0400

    Replace use of MockTermIndex in TermPropertyLabelResolverTest
    also converted to short array syntax here
    
    Bug: T162555
    Change-Id: I44d3e743a5f6f9f57f295556540e9c8b797fcd11

--- lib/tests/phpunit/Store/TermPropertyLabelResolverTest.php
+++ lib/tests/phpunit/Store/TermPropertyLabelResolverTest.php
@@ -3,9 +3,9 @@
 namespace Wikibase\Lib\Tests\Store;
 
 use HashBagOStuff;
-use Wikibase\DataModel\Entity\ItemId;
 use Wikibase\DataModel\Entity\PropertyId;
 use Wikibase\DataModel\Services\Term\PropertyLabelResolver;
+use Wikibase\TermIndex;
 use Wikibase\TermIndexEntry;
 use Wikibase\TermPropertyLabelResolver;
 
@@ -21,15 +21,132 @@ use Wikibase\TermPropertyLabelResolver;
 class TermPropertyLabelResolverTest extends \MediaWikiTestCase {
 
 	/**
-	 * @param string $lang
+	 * @dataProvider provideGetPropertyIdsForLabels
+	 */
+	public function testGetPropertyIdsForLabels(
+		$languageCode,
+		array $terms,
+		array $labels,
+		array $expected
+	) {
+		$resolver = $this->getResolver( $languageCode, $terms );
+
+		// check we are getting back the expected map of labels to IDs
+		$actual = $resolver->getPropertyIdsForLabels( $labels );
+		$this->assertEquals( $expected, $actual );
+
+		// check again, so we also hit the "stuff it cached" code path
+		$actual = $resolver->getPropertyIdsForLabels( $labels );
+		$this->assertEquals( $expected, $actual );
+	}
+
+	public function provideGetPropertyIdsForLabels() {
+		return [
+			[ // #0
+				'de',
+				$this->getTermIndexEntries( 'de' ),
+				[], // labels
+				[], // expected
+			],
+			[ // #1
+				'de',
+				$this->getTermIndexEntries( 'de' ),
+				[ // labels
+					'Eins',
+					'Zwei'
+				],
+				[ // expected
+					'Eins' => new PropertyId( 'P1' ),
+					'Zwei' => new PropertyId( 'P2' ),
+				]
+			],
+			[ // #2
+				'de',
+				$this->getTermIndexEntries( 'de' ),
+				[ // labels
+					'Drei',
+					'Vier'
+				],
+				[ // expected
+					'Drei' => new PropertyId( 'P3' ),
+				]
+			],
+			[ // #3
+				'en',
+				$this->getTermIndexEntries( 'en' ),
+				[ // labels
+					'Eins',
+					'Zwei'
+				],
+				[] // expected
+			],
+			[ // #4
+				'en',
+				$this->getTermIndexEntries( 'en' ),
+				[ // labels
+					'One',
+					'Two',
+					'Three',
+					'Four'
+				],
+				[ // expected
+					'One' => new PropertyId( 'P1' ),
+				]
+			],
+		];
+	}
+
+	private function getTermIndexEntries( $languageCode ) {
+		$terms = [
+			'de' => [
+				new TermIndexEntry( [
+					'termType' => 'label',
+					'termLanguage' => 'de',
+					'entityId' => new PropertyId( 'P1' ),
+					'termText' => 'Eins',
+				] ),
+				new TermIndexEntry( [
+					'termType' => 'label',
+					'termLanguage' => 'de',
+					'entityId' => new PropertyId( 'P2' ),
+					'termText' => 'Zwei',
+				] ),
+				new TermIndexEntry( [
+					'termType' => 'label',
+					'termLanguage' => 'de',
+					'entityId' => new PropertyId( 'P3' ),
+					'termText' => 'Drei',
+				] ),
+				new TermIndexEntry( [
+					'termType' => 'label',
+					'termLanguage' => 'de',
+					'entityId' => new PropertyId( 'P4' ),
+					'termText' => 'vier', // lower case
+				] )
+			],
+			'en' => [
+				new TermIndexEntry( [
+					'termType' => 'label',
+					'termLanguage' => 'en',
+					'entityId' => new PropertyId( 'P1' ),
+					'termText' => 'One',
+				] ),
+			]
+		];
+
+		return $terms[$languageCode];
+	}
+
+	/**
+	 * @param string $languageCode
 	 * @param TermIndexEntry[] $terms
 	 *
 	 * @return PropertyLabelResolver
 	 */
-	public function getResolver( $lang, array $terms ) {
+	private function getResolver( $languageCode, array $terms ) {
 		$resolver = new TermPropertyLabelResolver(
-			$lang,
-			new MockTermIndex( $terms ),
+			$languageCode,
+			$this->getMockTermIndex( $terms ),
 			new HashBagOStuff(),
 			3600,
 			'testrepo:WBL\0.5alpha'
@@ -38,129 +155,15 @@ class TermPropertyLabelResolverTest extends \MediaWikiTestCase {
 		return $resolver;
 	}
 
-	/**
-	 * @dataProvider provideGetPropertyIdsForLabels
-	 */
-	public function testGetPropertyIdsForLabels( $lang, array $terms, array $labels, array $expected ) {
-		$resolver = $this->getResolver( $lang, $terms );
+	private function getMockTermIndex( array $terms ) {
+		$termIndex = $this->getMockBuilder( TermIndex::class )
+			->disableOriginalConstructor()
+			->getMock();
 
-		// check we are getting back the expected map of labels to IDs
-		$actual = $resolver->getPropertyIdsForLabels( $labels );
-		$this->assertArrayEquals( $expected, $actual, false, true );
+		$termIndex->expects( $this->any() )
+			->method( 'getMatchingTerms' )
+			->will( $this->returnValue( $terms ) );
 
-		// check again, so we also hit the "stuff it cached" code path
-		$actual = $resolver->getPropertyIdsForLabels( $labels );
-		$this->assertArrayEquals( $expected, $actual, false, true );
+		return $termIndex;
 	}
-
-	public function provideGetPropertyIdsForLabels() {
-		$terms = array(
-			// de
-			new TermIndexEntry( array(
-				'termType' => 'label',
-				'termLanguage' => 'de',
-				'entityId' => new PropertyId( 'P1' ),
-				'termText' => 'Eins',
-			) ),
-			new TermIndexEntry( array(
-				'termType' => 'label',
-				'termLanguage' => 'de',
-				'entityId' => new PropertyId( 'P2' ),
-				'termText' => 'Zwei',
-			) ),
-			new TermIndexEntry( array(
-				'termType' => 'label',
-				'termLanguage' => 'de',
-				'entityId' => new PropertyId( 'P3' ),
-				'termText' => 'Drei',
-			) ),
-			new TermIndexEntry( array(
-				'termType' => 'label',
-				'termLanguage' => 'de',
-				'entityId' => new PropertyId( 'P4' ),
-				'termText' => 'vier', // lower case
-			) ),
-
-			// en
-			new TermIndexEntry( array(
-				'termType' => 'label',
-				'termLanguage' => 'en',
-				'entityId' => new PropertyId( 'P1' ),
-				'termText' => 'One',
-			) ),
-			new TermIndexEntry( array(
-				'termType' => 'label',
-				'termLanguage' => 'en',
-				'entityId' => new ItemId( 'Q2' ), // not a property
-				'termText' => 'Two',
-			) ),
-			new TermIndexEntry( array(
-				'termType' => 'alias', // not a label
-				'termLanguage' => 'en',
-				'entityId' => new PropertyId( 'P3' ),
-				'termText' => 'Three',
-			) ),
-			new TermIndexEntry( array(
-				'termType' => 'description', // not a label
-				'termLanguage' => 'en',
-				'entityId' => new PropertyId( 'P4' ),
-				'termText' => 'Four',
-			) ),
-		);
-
-		return array(
-			array( // #0
-				'de',
-				$terms,
-				array(), // labels
-				array(), // expected
-			),
-			array( // #1
-				'de',
-				$terms,
-				array( // labels
-					'Eins',
-					'Zwei'
-				),
-				array( // expected
-					'Eins' => new PropertyId( 'P1' ),
-					'Zwei' => new PropertyId( 'P2' ),
-				)
-			),
-			array( // #2
-				'de',
-				$terms,
-				array( // labels
-					'Drei',
-					'Vier'
-				),
-				array( // expected
-					'Drei' => new PropertyId( 'P3' ),
-				)
-			),
-			array( // #3
-				'en',
-				$terms,
-				array( // labels
-					'Eins',
-					'Zwei'
-				),
-				array() // expected
-			),
-			array( // #4
-				'en',
-				$terms,
-				array( // labels
-					'One',
-					'Two',
-					'Three',
-					'Four'
-				),
-				array( // expected
-					'One' => new PropertyId( 'P1' ),
-				)
-			),
-		);
-	}
-
 }
