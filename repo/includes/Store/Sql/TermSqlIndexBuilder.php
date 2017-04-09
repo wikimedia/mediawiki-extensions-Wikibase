fb11702a4c91d24c2888a44e19a31c7219052bd0
aude <aude.wiki@gmail.com>
2017-04-09 17:22:47 -0400

    Script for rebuilding wb_terms table (e.g. w/ term_full_entity_id)
    Making this a more generic script for rebuilding the table.
    
    Bug: T162533
    Change-Id: Iae7690be42e96867494c21c8ba71483825dfdf10

---
+++ repo/includes/Store/Sql/TermSqlIndexBuilder.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace Wikibase\Repo\Store\Sql;
+
+use Wikibase\DataModel\Entity\Int32EntityId;
+use Wikibase\Lib\Reporting\ObservableMessageReporter;
+use Wikibase\Lib\Store\EntityRevisionLookup;
+use Wikibase\Lib\Store\Sql\TermSqlIndex;
+use Wikimedia\Rdbms\LBFactory;
+
+class TermSqlIndexBuilder {
+
+	const TABLE_NAME = 'wb_terms';
+
+	/**
+	 * @var LBFactory
+	 */
+	private $loadBalancerFactory;
+
+	/**
+	 * @var TermSqlIndex
+	 */
+	private $termSqlIndex;
+
+	/**
+	 * @var SqlEntityIdPagerFactory
+	 */
+	private $entityIdPagerFactory;
+
+	/**
+	 * @var EntityRevisionLookup
+	 */
+	private $entityRevisionLookup;
+
+	/**
+	 * @var ObservableMessageReporter
+	 */
+	private $reporter;
+
+	/**
+	 * @var int
+	 */
+	private $batchSize;
+
+	/**
+	 * @var string|null
+	 */
+	private $entityType = null;
+
+	/**
+	 * @var int|null
+	 */
+	private $fromId = null;
+
+	/**
+	 * @param LBFactory $loadBalancerFactory
+	 * @param TermSqlIndex $termSqlIndex
+	 * @param SqlEntityIdPagerFactory $entityIdPagerFactory
+	 * @param EntityRevisionLookup $entityRevisionLookup
+	 * @param ObservableMessageReporter $reporter
+	 * @param int $batchSize
+	 * @param int|null $fromId
+	 */
+	public function __construct(
+		LBFactory $loadBalancerFactory,
+		TermSqlIndex $termSqlIndex,
+		SqlEntityIdPagerFactory $entityIdPagerFactory,
+		EntityRevisionLookup $entityRevisionLookup,
+		ObservableMessageReporter $reporter,
+		$batchSize = 1000,
+		$fromId = null
+	) {
+		$this->loadBalancerFactory = $loadBalancerFactory;
+		$this->termSqlIndex = $termSqlIndex;
+		$this->entityIdPagerFactory = $entityIdPagerFactory;
+		$this->entityRevisionLookup = $entityRevisionLookup;
+		$this->reporter = $reporter;
+		$this->batchSize = $batchSize;
+		$this->fromId = $fromId;
+	}
+
+	/**
+	 * @param string $entityType
+	 */
+	public function setEntityType( $entityType ) {
+		$this->entityType = $entityType;
+	}
+
+	public function rebuild() {
+		foreach ( $this->getEntityTypes() as $entityType ) {
+			$this->rebuildForEntityType( $entityType );
+		}
+
+		$this->reporter->reportMessage( 'Done' );
+	}
+
+	/**
+	 * @param string $entityType
+	 */
+	private function rebuildForEntityType( $entityType ) {
+		$idPager = $this->entityIdPagerFactory->newSqlEntityIdPager( $entityType );
+		$lastIdProcessed = null;
+
+		if ( $this->fromId !== null ) {
+			$idPager->setPosition( $this->fromId );
+		}
+
+		while ( true ) {
+			$this->loadBalancerFactory->waitForReplication();
+
+			$entityIds = $idPager->fetchIds( $this->batchSize );
+
+			if ( !$entityIds ) {
+				break;
+			}
+
+			foreach ( $entityIds as $entityId ) {
+				if ( isset( $numericStartId ) && ! ( $entityId instanceof Int32EntityId ) ) {
+					throw new \RuntimeException( '$startId only works for entity types with ' .
+						'numeric ids. EntityId of type ' . $entityId->getEntityType() . ' found.'
+					);
+				}
+
+				if ( isset( $numericStartId ) && $entityId->getNumericId() < $numericStartId ) {
+					continue;
+				}
+
+				$entityRevision = $this->entityRevisionLookup->getEntityRevision( $entityId );
+				$success = $this->termSqlIndex->saveTermsOfEntity( $entityRevision->getEntity() );
+				$lastIdProcessed = $entityId;
+
+				if ( !$success ) {
+					wfLogWarning( 'Failed to save terms of entity: ' .
+					              $entityId->getSerialization() );
+				}
+			}
+
+			if ( $lastIdProcessed !== null ) {
+				$serializedId = $entityId->getSerialization();
+				$this->reporter->reportMessage( "Updated to $serializedId" );
+			}
+		}
+	}
+
+	/**
+	 * @return string[]
+	 */
+	private function getEntityTypes() {
+		if ( $this->entityType !== null ) {
+			$entityTypes = [ $this->entityType ];
+		} else {
+			$entityTypes = $this->getAllEntityTypes();
+		}
+
+		return $entityTypes;
+	}
+
+	/**
+	 * @return string[]
+	 */
+	private function getAllEntityTypes() {
+		$dbw = $this->loadBalancerFactory->getMainLB()->getConnection( DB_MASTER );
+		$rows = $dbw->select( self::TABLE_NAME, 'DISTINCT term_entity_type', [], __METHOD__ );
+
+		$entityTypes = [];
+
+		foreach ( $rows as $row ) {
+			$entityTypes[] = $row->term_entity_type;
+		}
+
+		return $entityTypes;
+	}
+
+}
---
+++ repo/maintenance/rebuildTermSqlIndex.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace Wikibase;
+
+use LoggedUpdateMaintenance;
+use MediaWiki\MediaWikiServices;
+use Wikibase\DataModel\Entity\EntityIdParser;
+use Wikibase\DataModel\Entity\Int32EntityId;
+use Wikibase\Lib\EntityIdComposer;
+use Wikibase\Lib\Reporting\ObservableMessageReporter;
+use Wikibase\Lib\Store\Sql\TermSqlIndex;
+use Wikibase\Repo\Store\Sql\SqlEntityIdPagerFactory;
+use Wikibase\Repo\Store\Sql\TermSqlIndexBuilder;
+use Wikibase\Repo\WikibaseRepo;
+
+$basePath = getenv( 'MW_INSTALL_PATH' ) !== false ? getenv( 'MW_INSTALL_PATH' ) : __DIR__ . '/../../../..';
+
+require_once $basePath . '/maintenance/Maintenance.php';
+
+/**
+ * @license GPL-2.0+
+ * @author Katie Filbert < aude.wiki@gmail.com >
+ */
+class RebuildTermSqlIndex extends LoggedUpdateMaintenance {
+
+	public function __construct() {
+		parent::__construct();
+
+		$this->addDescription( 'Populate the term_full_entity_id column in the wb terms table.' );
+
+		$this->addOption( 'batch-size', "Number of rows to update per batch (Default: 1000)",
+			false, true );
+		$this->addOption( 'entity-type', "Only rebuild terms for specified entity type (e.g. 
+		'item', 'property')", false, true );
+		$this->addOption( 'from-id', "First row (page id) to start updating from", false, true );
+	}
+
+	/**
+	 * @see LoggedUpdateMaintenance::doDBUpdates
+	 * @todo Should be a separate script, though also something that can run as a DB update
+	 *
+	 * @return bool
+	 */
+	public function doDBUpdates() {
+		if ( !defined( 'WB_VERSION' ) ) {
+			$this->error( "You need to have Wikibase enabled in order to use this "
+				. "maintenance script!\n\n", 1 );
+		}
+
+		$builder = $this->getTermIndexBuilder();
+		$builder->rebuild();
+
+		$this->output( "Done.\n" );
+
+		return true;
+	}
+
+	private function getTermIndexBuilder() {
+		$batchSize = (int)$this->getOption( 'batch-size', 1000 );
+		$entityType = $this->getOption( 'entity-type', null );
+		$fromId = (int)$this->getOption( 'from-id', null );
+
+		$wikibaseRepo = WikibaseRepo::getDefaultInstance();
+		$idParser = $wikibaseRepo->getEntityIdParser();
+		$entityIdComposer = $wikibaseRepo->getEntityIdComposer();
+
+		$sqlEntityIdPagerFactory = new SqlEntityIdPagerFactory(
+			$wikibaseRepo->getEntityNamespaceLookup(),
+			$idParser
+		);
+
+		$termSqlIndex = $this->getTermSqlIndex( $entityIdComposer, $idParser );
+
+		$builder = new TermSqlIndexBuilder(
+			MediaWikiServices::getInstance()->getDBLoadBalancerFactory(),
+			$termSqlIndex,
+			$sqlEntityIdPagerFactory,
+			$wikibaseRepo->getEntityRevisionLookup( 'uncached' ),
+			$this->getReporter(),
+			$batchSize,
+			$fromId
+		);
+
+		if ( $entityType !== null ) {
+			// TODO validate
+			$builder->setEntityType( $entityType );
+		}
+
+		return $builder;
+	}
+
+	/**
+	 * @param EntityIdComposer $entityIdComposer
+	 * @param EntityIdParser $entityIdParser
+	 * @return TermSqlIndex
+	 */
+	private function getTermSqlIndex(
+		EntityIdComposer $entityIdComposer,
+		EntityIdParser $entityIdParser
+	) {
+		return new TermSqlIndex(
+			new StringNormalizer(),
+			$entityIdComposer,
+			$entityIdParser,
+			false,
+			'',
+			true
+		);
+	}
+
+	/**
+	 * @return ObservableMessageReporter
+	 */
+	private function getReporter() {
+		$reporter = new ObservableMessageReporter();
+		$reporter->registerReporterCallback(
+			array( $this, 'report' )
+		);
+
+		return $reporter;
+	}
+
+	/**
+	 * @see LoggedUpdateMaintenance::getUpdateKey
+	 *
+	 * @return string
+	 */
+	public function getUpdateKey() {
+		return 'Wikibase\PopulateTermFullEntityId';
+	}
+
+	/**
+	 * Outputs a message vis the output() method.
+	 *
+	 * @param string $msg
+	 */
+	public function report( $msg ) {
+		$this->output( "$msg\n" );
+	}
+
+}
+
+$maintClass = RebuildTermSqlIndex::class;
+require_once RUN_MAINTENANCE_IF_MAIN;
