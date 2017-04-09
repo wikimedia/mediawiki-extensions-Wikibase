18504545acb6c374b7d27fbf1bed9c258d74ac1f
aude <aude.wiki@gmail.com>
2017-04-09 17:22:47 -0400

    Script for populating term_full_entity_id column in wb_terms [WIP]
    Bug: T162533
    Change-Id: Iae7690be42e96867494c21c8ba71483825dfdf10

---
+++ repo/tests/phpunit/includes/Store/Sql/TermFullEntityIdBuilderTest.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Wikibase\Repo\Tests\Store\Sql;
+
+use MediaWiki\MediaWikiServices;
+use Wikibase\DataModel\Entity\BasicEntityIdParser;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\Lib\Reporting\ObservableMessageReporter;
+use Wikibase\Repo\Store\Sql\TermFullEntityIdBuilder;
+use Wikibase\Repo\WikibaseRepo;
+
+/**
+ * @covers Wikibase\Wikibase\Repo\Store\Sql\TermFullEntityIdBuilder
+ *
+ * @group Wikibase
+ * @group Database
+ * @group medium
+ *
+ * @license GPL-2.0+
+ * @author Katie Filbert < aude.wiki@gmail.com >
+ */
+class TermFullEntityIdBuilderTest extends \MediaWikiTestCase {
+
+	protected function setUp() {
+		$this->tablesUsed[] = 'wb_terms';
+
+		parent::setUp();
+	}
+
+	public function testRebuild() {
+		$termSqlIndex = WikibaseRepo::getDefaultInstance()->getStore()->getTermIndex();
+
+		foreach ( $this->getItems() as $item ) {
+			$termSqlIndex->saveTermsOfEntity( $item );
+		}
+
+		$this->clearFullEntityIdColumn();
+		$this->assertFullEntityIdIsNull();
+
+		$termFullEntityIdBuilder = new TermFullEntityIdBuilder(
+			MediaWikiServices::getInstance()->getDBLoadBalancerFactory(),
+			WikibaseRepo::getDefaultInstance()->getEntityIdComposer(),
+			new BasicEntityIdParser(),
+			$this->getMockBuilder( ObservableMessageReporter::class )->getMock(),
+			2,
+			false
+		);
+
+		$termFullEntityIdBuilder->rebuild();
+
+		$this->assertTermIndexRowsHaveFullEntityId( 'Q111', 5 );
+		$this->assertTermIndexRowsHaveFullEntityId( 'Q112', 2 );
+	}
+
+	private function assertTermIndexRowsHaveFullEntityId( $entityIdString, $numRows ) {
+		$rows = $this->db->select(
+			TermFullEntityIdBuilder::TABLE_NAME,
+			'*',
+			[ 'term_full_entity_id' => $entityIdString ],
+			__METHOD__
+		);
+
+		$this->assertSame( $numRows, $rows->numRows() );
+
+		foreach ( $rows as $row ) {
+			$this->assertSame( $entityIdString, $row->term_full_entity_id );
+		}
+	}
+
+	/**
+	 * @return Item[]
+	 */
+	private function getItems() {
+		$items = [];
+
+		$item = new Item();
+		$item->setId( new ItemId( 'Q111' ) );
+		$item->setLabel( 'en', 'cat' );
+		$item->setDescription( 'en', 'feline' );
+		$item->setLabel( 'de', 'Katze' );
+		$item->setLabel( 'es', 'gato' );
+		$item->setLabel( 'fr', 'chat' );
+
+		$items[] = $item;
+
+		$item2 = new Item();
+		$item2->setId( new ItemId( 'Q112' ) );
+		$item2->setLabel( 'en', 'dog' );
+		$item2->setLabel( 'es', 'perro' );
+
+		$items[] = $item2;
+
+		return $items;
+	}
+
+	private function clearFullEntityIdColumn() {
+		$this->db->update(
+			TermFullEntityIdBuilder::TABLE_NAME,
+			[ 'term_full_entity_id' => null ],
+			[],
+			__METHOD__
+		);
+	}
+
+	private function assertFullEntityIdIsNull() {
+		$rows = $this->db->select(
+			TermFullEntityIdBuilder::TABLE_NAME,
+			'*',
+			[],
+			__METHOD__
+		);
+
+		foreach ( $rows as $row ) {
+			$this->assertNull( $row->term_full_entity_id );
+		}
+	}
+
+}
