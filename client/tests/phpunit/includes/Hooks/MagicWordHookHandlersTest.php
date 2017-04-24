bccb4967c0cfe71a34c4a4853a7e8088235aa94a
Marius Hoch <hoo@online.de>
2017-04-24 13:56:39 +0200

    Add a test for noexternallanglinks to MagicWordHookHandlersTest
    Related to https://wikitech.wikimedia.org/wiki/Incident_documentation/20170323-wikibase
    
    Change-Id: I5b082d3793eb1db8d56b26ae7062c854c0314bf3

--- client/tests/phpunit/includes/Hooks/MagicWordHookHandlersTest.php
+++ client/tests/phpunit/includes/Hooks/MagicWordHookHandlersTest.php
@@ -6,6 +6,8 @@ use Language;
 use Wikibase\Client\Hooks\MagicWordHookHandlers;
 use MediaWikiTestCase;
 use Parser;
+use ParserOutput;
+use Title;
 use Wikibase\SettingsArray;
 use Wikimedia\TestingAccessWrapper;
 
@@ -67,9 +69,7 @@ class MagicWordHookHandlersTest extends MediaWikiTestCase {
 		];
 	}
 
-	// TODO: Test noexternallanglinks
-
-	public function testDoParserGetVariableValueSwitch() {
+	public function testDoParserGetVariableValueSwitch_wbreponame() {
 		$parser = $this->getMockBuilder( Parser::class )
 			->getMock();
 
@@ -96,6 +96,34 @@ class MagicWordHookHandlersTest extends MediaWikiTestCase {
 		$this->assertEquals(
 			'Client for the Wikibase extension',
 			$ret
+		);
+	}
+
+	public function testDoParserGetVariableValueSwitch_noexternallanglinks() {
+		$parser = $this->getMockBuilder( Parser::class )
+			->getMock();
+
+		$out = new ParserOutput();
+		$parser->method( 'getOutput' )
+			->willReturn( $out );
+		$parser->method( 'getTitle' )
+			->willReturn( Title::newMainPage() );
+
+		$handler = TestingAccessWrapper::newFromObject(
+			new MagicWordHookHandlers( new SettingsArray() )
+		);
+
+		$ret = null;
+		$cache = [];
+		$word = 'noexternallanglinks';
+		call_user_func_array(
+			[ $handler, 'doParserGetVariableValueSwitch' ],
+			[ &$parser, &$cache, &$word, &$ret ]
+		);
+
+		$this->assertType(
+			'string',
+			$out->getProperty( 'noexternallanglinks' )
 		);
 	}
 
