c90573b848f8f7a4e9e14ac13207ba783fdf9941
Leszek Manicki <leszek.manicki@wikimedia.de>
2017-04-26 11:04:22 +0200

    [WIP] Use eslint for linting JavaScript code
    Bug: T165843
    Change-Id: I7be44baeaf83a16505adef5ff229a0a4ba1a346a

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,16 @@
 /* jshint node: true, strict: false */
 module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-eslint' );
 	grunt.loadNpmTasks( 'grunt-jscs' );
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-stylelint' );
 
 	grunt.initConfig( {
+		eslint: {
+			all: '.'
+		},
 		jshint: {
 			options: {
 				jshintrc: true
@@ -43,5 +47,5 @@ module.exports = function ( grunt ) {
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jshint', 'jscs', 'jsonlint', 'banana', 'stylelint' ] );
+	grunt.registerTask( 'test', [ 'eslint', 'jshint', 'jscs', 'jsonlint', 'banana', 'stylelint' ] );
 };
