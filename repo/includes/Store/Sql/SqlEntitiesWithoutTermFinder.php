d0a746f3ee394933f9dcb6714844e9f0f8b88f80
Amir Sarabadani <ladsgroup@gmail.com>
2017-05-05 18:00:00 +0430

    [WIP] Use two reading mode in SqlEntitiesWithoutTermFinder
    Bug: T162673
    Change-Id: Ia6c6f10a62899bbddb53ec173df92faababe3bde

--- repo/includes/Store/Sql/SqlEntitiesWithoutTermFinder.php
+++ repo/includes/Store/Sql/SqlEntitiesWithoutTermFinder.php
@@ -39,18 +39,28 @@ class SqlEntitiesWithoutTermFinder implements EntitiesWithoutTermFinder {
 	private $entityTypeToPrefixMap;
 
 	/**
+	 * Whether it's possible to read from term_full_entity_id
+	 *
+	 * @var bool
+	 */
+	private $canReadFullEntityIdColumn;
+
+	/**
 	 * @param EntityIdParser $entityIdParser
 	 * @param EntityNamespaceLookup $entityNamespaceLookup
 	 * @param string[] $entityTypeToPrefixMap Maps (supported) entity types to their prefix (before the numerical part).
+	 * @param bool $canReadFullEntityIdColumn Whether it can read term_entity_full_id or not
 	 */
 	public function __construct(
 		EntityIdParser $entityIdParser,
 		EntityNamespaceLookup $entityNamespaceLookup,
-		array $entityTypeToPrefixMap
+		array $entityTypeToPrefixMap,
+		$canReadFullEntityIdColumn = true
 	) {
 		$this->entityIdParser = $entityIdParser;
 		$this->entityNamespaceLookup = $entityNamespaceLookup;
 		$this->entityTypeToPrefixMap = $entityTypeToPrefixMap;
+		$this->canReadFullEntityIdColumn = $canReadFullEntityIdColumn;
 
 		Assert::parameterElementType( 'string', $entityTypeToPrefixMap, '$entityTypeToPrefixMap' );
 	}
@@ -147,12 +157,16 @@ class SqlEntitiesWithoutTermFinder implements EntitiesWithoutTermFinder {
 	 * @return string
 	 */
 	private function getConditionsForEntityType( IDatabase $dbr, $entityType ) {
-		$prefix = $dbr->addQuotes( $this->entityTypeToPrefixMap[ $entityType ] );
 		$conditions = [
-			'term_entity_id = ' . $dbr->strreplace( 'page_title', "$prefix", "''" ),
 			'term_entity_type' => $entityType,
 			'page_namespace' => $this->entityNamespaceLookup->getEntityNamespace( $entityType )
 		];
+		if ( $this->canReadFullEntityIdColumn ) {
+			$conditions['term_full_entity_id'] = 'page_title';
+		} else {
+			$prefix = $dbr->addQuotes( $this->entityTypeToPrefixMap[ $entityType ] );
+			$conditions[] = 'term_entity_id = ' . $dbr->strreplace( 'page_title', "$prefix", "''" );
+		}
 
 		return $dbr->makeList( $conditions, IDatabase::LIST_AND );
 	}
