ae368a16fa6d4927d2695f6abfd45d1cbc1e7945
Leszek Manicki <leszek.manicki@wikimedia.de>
2017-05-11 15:22:21 +0200

    Add WikibaseServices interface and the "dispatching-aware" implementation
    Initially the interface of WikibaseServices is equal to the one of
    DispatchingServiceFactory/EntityDataRetrievalServiceFactory.
    
    MultipleRepositoryAwareWikibaseServices implements WikibaseServices
    and uses the DispatchingServiceFactory instance to provide
    "dispatching" instances of services.
    
    Bug: T165039
    Bug: T165041
    Change-Id: I39316c1a2bc2de5540dbb570356e60a218b8e345

---
+++ data-access/src/WikibaseServices.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Wikibase\DataAccess;
+
+use Wikibase\DataModel\Services\Entity\EntityPrefetcher;
+use Wikibase\DataModel\Services\Term\TermBuffer;
+use Wikibase\Lib\Interactors\TermSearchInteractorFactory;
+use Wikibase\Lib\Store\EntityInfoBuilderFactory;
+use Wikibase\Lib\Store\EntityRevisionLookup;
+use Wikibase\Lib\Store\PropertyInfoLookup;
+
+/**
+ * Interface of the top-level container/factory of data access services.
+ *
+ * @license GPL-2.0+
+ */
+interface WikibaseServices {
+
+	/**
+	 * @return EntityInfoBuilderFactory
+	 */
+	public function getEntityInfoBuilderFactory();
+
+	/**
+	 * @return EntityPrefetcher
+	 */
+	public function getEntityPrefetcher();
+
+	/**
+	 * Note: Instance returned is not guaranteed to be a caching decorator.
+	 * Callers should take care of caching themselves.
+	 *
+	 * @return EntityRevisionLookup
+	 */
+	public function getEntityRevisionLookup();
+
+	/**
+	 * Note: Instance returned is not guaranteed to be a caching decorator.
+	 * Callers should take care of caching themselves.
+	 *
+	 * @return PropertyInfoLookup
+	 */
+	public function getPropertyInfoLookup();
+
+	/**
+	 * @return TermBuffer
+	 */
+	public function getTermBuffer();
+
+	/**
+	 * @return TermSearchInteractorFactory
+	 */
+	public function getTermSearchInteractorFactory();
+
+}
