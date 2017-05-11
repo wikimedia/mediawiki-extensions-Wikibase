5eca8c93e5d45797509147c5c7591bc8872de749
Leszek Manicki <leszek.manicki@wikimedia.de>
2017-05-11 16:56:28 +0200

    Use WikibaseServices in Client and Repo instead of EntityDataRetrievalServiceFactory
    WikibaseServices currently only provide services that
    EntityDataRetrievalServiceFactory also used to provide.
    Dependency between Repo and Client in case of using
    "dispatching" services allowing the access to entity data
    from multiple repositories is NOT removed in this change.
    
    This also makes WikibaseServices provide an implementation of
    EntityStoreWatcher interface, which can be registered as
    a watcher to changes done to the entity.
    
    MultipleRepositoryAwareWikibaseServices returns DispatchingServiceFactory,
    that already implements EntityStoreWatcher, as a watcher.
    
    Bug: T165047
    Change-Id: Iae1bf00a6b02e8656cf97d27371d9c5ee670ad60

--- client/includes/Store/Sql/DirectSqlStore.php
+++ client/includes/Store/Sql/DirectSqlStore.php
@@ -20,7 +20,7 @@ use Wikibase\Client\Usage\SubscriptionManager;
 use Wikibase\Client\Usage\UsageLookup;
 use Wikibase\Client\Usage\UsageTracker;
 use Wikibase\Client\WikibaseClient;
-use Wikibase\DataAccess\EntityDataRetrievalServiceFactory;
+use Wikibase\DataAccess\WikibaseServices;
 use Wikibase\DataModel\Entity\EntityIdParser;
 use Wikibase\DataModel\Services\Lookup\EntityLookup;
 use Wikibase\DataModel\Services\Lookup\RedirectResolvingEntityLookup;
@@ -110,9 +110,9 @@ class DirectSqlStore implements ClientStore {
 	private $entityRevisionLookup = null;
 
 	/**
-	 * @var EntityDataRetrievalServiceFactory
+	 * @var WikibaseServices
 	 */
-	private $entityDataRetrievalServices = null;
+	private $wikibaseServices = null;
 
 	/**
 	 * @var PropertyLabelResolver|null
@@ -185,7 +185,7 @@ class DirectSqlStore implements ClientStore {
 	 * @param EntityIdParser $entityIdParser
 	 * @param EntityIdComposer $entityIdComposer
 	 * @param EntityNamespaceLookup $entityNamespaceLookup
-	 * @param EntityDataRetrievalServiceFactory $entityDataRetrievalServiceFactory
+	 * @param WikibaseServices $wikibaseServices
 	 * @param string|bool $repoWiki The symbolic database name of the repo wiki or false for the
 	 * local wiki.
 	 * @param string $languageCode
@@ -196,7 +196,7 @@ class DirectSqlStore implements ClientStore {
 		EntityIdParser $entityIdParser,
 		EntityIdComposer $entityIdComposer,
 		EntityNamespaceLookup $entityNamespaceLookup,
-		EntityDataRetrievalServiceFactory $entityDataRetrievalServiceFactory,
+		WikibaseServices $wikibaseServices,
 		$repoWiki = false,
 		$languageCode
 	) {
@@ -205,7 +205,7 @@ class DirectSqlStore implements ClientStore {
 		$this->entityIdParser = $entityIdParser;
 		$this->entityIdComposer = $entityIdComposer;
 		$this->entityNamespaceLookup = $entityNamespaceLookup;
-		$this->entityDataRetrievalServices = $entityDataRetrievalServiceFactory;
+		$this->wikibaseServices = $wikibaseServices;
 		$this->repoWiki = $repoWiki;
 		$this->languageCode = $languageCode;
 
@@ -351,7 +351,7 @@ class DirectSqlStore implements ClientStore {
 		// NOTE: Keep cache key in sync with SqlStore::newEntityRevisionLookup in WikibaseRepo
 		$cacheKeyPrefix = $this->cacheKeyPrefix . ':WikiPageEntityRevisionLookup';
 
-		$dispatchingLookup = $this->entityDataRetrievalServices->getEntityRevisionLookup();
+		$dispatchingLookup = $this->wikibaseServices->getEntityRevisionLookup();
 
 		// Lower caching layer using persistent cache (e.g. memcached).
 		$persistentCachingLookup = new CachingEntityRevisionLookup(
@@ -460,7 +460,7 @@ class DirectSqlStore implements ClientStore {
 	 */
 	public function getPropertyInfoLookup() {
 		if ( $this->propertyInfoLookup === null ) {
-			$propertyInfoLookup = $this->entityDataRetrievalServices->getPropertyInfoLookup();
+			$propertyInfoLookup = $this->wikibaseServices->getPropertyInfoLookup();
 			$cacheKey = $this->cacheKeyPrefix . ':CacheAwarePropertyInfoStore';
 
 			$this->propertyInfoLookup = new CachingPropertyInfoLookup(
@@ -478,7 +478,7 @@ class DirectSqlStore implements ClientStore {
 	 * @return PrefetchingWikiPageEntityMetaDataAccessor
 	 */
 	public function getEntityPrefetcher() {
-		return $this->entityDataRetrievalServices->getEntityPrefetcher();
+		return $this->wikibaseServices->getEntityPrefetcher();
 	}
 
 	/**
