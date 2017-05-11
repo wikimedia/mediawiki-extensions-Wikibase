79c3f06203f585882500ff918a746c468104947e
Aleksey Bekh-Ivanov (WMDE) <aleksey.bekh-ivanov@wikimedia.de>
2017-05-11 21:36:40 +0200

    [WIP] Replacement for 'wbEntity' config variable To be able to migrate from 'wbEntity' in future replacing it with an API call.
    Decisions:
    * Using promise as soon as in future it'll be API call
    * Provide user with native promise (if possible) instead of jQuery one.
      jQuery guys might again change something in the future and break stuff.
      Native promises are supported almost in all modern browsers,
      so user-script developer probably will see `Promise` type in his/her
      dev-tools console, unless one uses IE <= v11.
    * Another reason to use native promises is the fact that jQuery swallows
      errors in the promises and developer won't be able to find out about
      an error happened in callback. Native implementation displays error
      in console if error wasn't handeled and promise chain is "over".
    * Monkey-patched internal "wbEntity" property declaration
      in `mw.config.values` to display usage warning.
      With this approach, in worst case if internal implementation
      of `mw.config` changes we'll just stop displaying warning.
    * To avoid problem with user-scripts changing shared entity we
      make it read only without ability to change or add a property.
    
    Bug: T85499
    Change-Id: I9f36e8fe55af4150dc938819ad30f8cdd7689fc0

---
+++ repo/resources/wikibase.currentEntity.js
@@ -0,0 +1,83 @@
+module.exports = (function ( $, mw ) {
+	var wbEntity;
+	if ( mw.config.exists( 'wbEntity' ) ) {
+		wbEntity = mw.config.get( 'wbEntity' );
+		if ( mw.config.values && mw.config.values.wbEntity ) {
+			Object.defineProperty( mw.config.values, 'wbEntity', {
+				enumerable: true,
+				get: function () {
+					console.warn(
+						'Configuration variable "wbEntity" is deprecated. ' +
+						'Use module "wikibase.currentEntity" instead.'
+					);
+					return wbEntity;
+				}
+			} );
+		}
+	}
+
+
+	var result = (typeof wbEntity === 'undefined') ? createPendingPromise() : createResolvedPromise( wbEntity );
+
+	return result
+		.then( JSON.parse )
+		.then( deepFreeze );
+
+	function nativePromisesSupported() {
+		return typeof Promise !== "undefined" && Promise.toString().indexOf( "[native code]" ) !== -1;
+	}
+
+	function createResolvedPromise( value ) {
+		if ( nativePromisesSupported() ) {
+			return Promise.resolve( value );
+		} else {
+			return $.Deferred().resolve( value ).promise();
+		}
+	}
+
+	function createPendingPromise() {
+		if ( nativePromisesSupported() ) {
+			return new Promise( function () {} );
+		} else {
+			return $.Deferred().promise();
+		}
+	}
+
+	/**
+	 * Copied from https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Object/freeze
+	 * @param obj
+	 * @return {Object}
+	 */
+	function deepFreeze( obj ) {
+		// Retrieve the property names defined on obj
+		var propNames = Object.getOwnPropertyNames( obj );
+
+		// Freeze properties before freezing self
+		propNames.forEach( function ( name ) {
+			var prop = obj[ name ];
+
+			// Freeze prop if it is an object
+			if ( typeof prop === 'object' && prop !== null )
+				deepFreeze( prop );
+		} );
+
+		// Freeze self (no-op if already frozen)
+		return Object.freeze( obj );
+	}
+
+})( $, mw );
+
+/**
+Usage example:
+
+mw.loader.using( [ 'wikibase.currentEntity' ] ).then( function ( require ) {
+	'use strict';
+	// This code will be fired only if and when 'wikibase.currentEntity' is loaded
+	require( 'wikibase.currentEntity' ).then( function ( entity ) {
+			console.log( entity );
+			return entity;
+		}
+	);
+} );
+
+*/
