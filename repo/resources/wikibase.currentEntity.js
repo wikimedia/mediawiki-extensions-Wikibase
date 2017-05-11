ff6c32860afc0182975e4e29ab05699c5a71b3ad
Aleksey Bekh-Ivanov (WMDE) <aleksey.bekh-ivanov@wikimedia.de>
2017-05-11 21:36:40 +0200

    [WIP] Replacement for 'wbEntity' config variable To be able to migrate from 'wbEntity' in future replacing it with an API call.
    Decisions:
    * Using mw.hook - standard way to hook into Mediawiki javascript code
    * Monkey-patched internal "wbEntity" property declaration
      in `mw.config.values` to display usage warning.
      With this approach, in worst case if internal implementation
      of `mw.config` changes we'll just stop displaying warning.
    * To avoid problem with user-scripts changing shared entity we
      make it read only without ability to change or add a property.
    
    Bug: T85499
    Change-Id: I9f36e8fe55af4150dc938819ad30f8cdd7689fc0

---
+++ repo/resources/wikibase.currentEntity.js
@@ -0,0 +1,75 @@
+module.exports = (function ( $, mw ) {
+	'use strict';
+
+	var wbEntity;
+	if ( mw.config.exists( 'wbEntity' ) ) {
+		wbEntity = mw.config.get( 'wbEntity' );
+		if ( mw.config.values && mw.config.values.wbEntity ) {
+			Object.defineProperty( mw.config.values, 'wbEntity', {
+				enumerable: true,
+				get: function () {
+					console.warn(
+						'Configuration variable "wbEntity" is deprecated. ' +
+						'Use module "wikibase.currentEntity" instead.'
+					);
+					return wbEntity;
+				}
+			} );
+		}
+	}
+
+	if (wbEntity) {
+		mw.hook( 'wikibase.entity' ).fire( deepFreeze( JSON.parse( wbEntity ) ) );
+	}
+
+	/**
+	 * Copied from https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Object/freeze
+	 * @param obj
+	 * @return {Object}
+	 */
+	function deepFreeze( obj ) {
+		// Retrieve the property names defined on obj
+		var propNames = Object.getOwnPropertyNames( obj );
+
+		// Freeze properties before freezing self
+		propNames.forEach( function ( name ) {
+			var prop = obj[ name ];
+
+			// Freeze prop if it is an object
+			if ( typeof prop === 'object' && prop !== null )
+				deepFreeze( prop );
+		} );
+
+		// Freeze self (no-op if already frozen)
+		return Object.freeze( obj );
+	}
+
+})( $, mw );
+
+/**
+ Usage example:
+	 mw.hook( 'wikibase.entity' ).add( function (entity) {
+		'use strict';
+		console.log( entity );
+	 } );
+
+ Can be easily converted to promise.
+
+ Native:
+	 new Promise( function ( resolve ) {
+			mw.hook( 'wikibase.entity' ).add( function ( entity ) {
+				resolve( entity );
+			} );
+		} ).then( function ( entity ) {
+			console.log( entity );
+		} );
+
+ jQuery:
+	 $.Deferred( function ( deferred ) {
+			mw.hook( 'wikibase.entity' ).add( function ( entity ) {
+				deferred.resolve( entity );
+			} );
+		} ).then( function ( entity ) {
+			console.log( entity );
+		} );
+ */
