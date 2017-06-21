e0801f483bf2eedb9eeaab63ce9d95c47bdd73ae
Leszek Manicki <leszek.manicki@wikimedia.de>
2017-06-21 13:45:50 +0200

    Only load entity data TermSqlIndexBuilder when doing full rebuild
    Makes TermSqlIndexBuilder only load entity data when
    full rebuild of the term index is requested.
    
    This should make the "default" rebuild faster, as it
    will only check the contents of wb_terms to detect possible
    unpopulated ID fields and/or duplicate term entries,
    without needing to load all the data for each
    processed entity.
    
    RebuildTermSqlIndex maintenance script also gets the
    respective option, which is turned off by default.
    
    This also allows to switch deduplicating of entity terms
    off on TermSqlIndexBuilder. DatabaseSchemaUpdater skips
    it, so when running update script only populating
    of full entity ID column will take place. This should
    make running update.php faster.
    
    Bug: T162533
    Change-Id: Ia0409ebfd7c905b178c8ebe17945226a845e010c

--- repo/maintenance/rebuildTermSqlIndex.php
+++ repo/maintenance/rebuildTermSqlIndex.php
@@ -21,7 +21,7 @@ require_once $basePath . '/maintenance/Maintenance.php';
  * @license GPL-2.0+
  * @author Katie Filbert < aude.wiki@gmail.com >
  */
-class RebuildTermSqlIndex extends Maintenance {
+class RebuildTermSqlIndex extends Maintenance  {
 
 	public function __construct() {
 		parent::__construct();
@@ -38,6 +38,9 @@ class RebuildTermSqlIndex extends Maintenance {
 			'entity-type', "Only rebuild terms for specified entity type (e.g. 'item', 'property')",
 			false,
 			true
+		);
+		$this->addOption(
+			'rebuild-all-terms', 'Rebuilds all terms of the entity (requires loading data of each processed entity)'
 		);
 		$this->addOption( 'from-id', "First row (page id) to start updating from", false, true );
 	}
@@ -57,6 +60,7 @@ class RebuildTermSqlIndex extends Maintenance {
 	private function getTermIndexBuilder() {
 		$batchSize = (int)$this->getOption( 'batch-size', 1000 );
 		$fromId = $this->getOption( 'from-id', null );
+		$rebuildAllEntityTerms = $this->getOption( 'rebuild-all-terms', false );
 
 		$wikibaseRepo = WikibaseRepo::getDefaultInstance();
 		$idParser = $wikibaseRepo->getEntityIdParser();
@@ -83,11 +87,15 @@ class RebuildTermSqlIndex extends Maintenance {
 			$this->getEntityTypes(),
 			$this->getReporter(),
 			$this->getErrorReporter(),
-			$batchSize
+			$batchSize,
+			$repoSettings->getSetting( 'readFullEntityIdColumn' )
 		);
 
 		if ( $fromId !== null ) {
 			$builder->setFromId( (int)$fromId );
+		}
+		if ( $rebuildAllEntityTerms ) {
+			$builder->setRebuildAllEntityTerms();
 		}
 
 		return $builder;
