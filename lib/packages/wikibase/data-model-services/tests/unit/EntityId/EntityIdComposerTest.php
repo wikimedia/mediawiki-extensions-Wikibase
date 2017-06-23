6fb704e5927a166bf7e22a0a4bfdef0392a6c02e
Amir Sarabadani <Ladsgroup@gmail.com>
2017-06-23 10:26:20 +0000

    Move EntityIdComposer from Wikibase to DataModelServices (#182)
    * Move EntityIdComposer from Wikibase to DataModelServices
    
    * Fix phpmd errors
    
    * Add @since tag and remove @group

---
+++ lib/packages/wikibase/data-model-services/tests/unit/EntityId/EntityIdComposerTest.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Wikibase\DataModel\Services\Tests\EntityId;
+
+use InvalidArgumentException;
+use PHPUnit_Framework_TestCase;
+use UnexpectedValueException;
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Services\EntityId\EntityIdComposer;
+
+/**
+ * @covers Wikibase\DataModel\Services\EntityId\EntityIdComposer
+ *
+ * @licence GNU GPL v2+
+ * @author Thiemo Mättig
+ */
+class EntityIdComposerTest extends PHPUnit_Framework_TestCase {
+
+	private function getComposer() {
+		return new EntityIdComposer( [
+			'numeric-item' => function( $repositoryName, $uniquePart ) {
+				return new ItemId( 'Q' . $uniquePart );
+			},
+			'custom-item' => function( $repositoryName, $uniquePart ) {
+				return new ItemId( 'Q100' . $uniquePart );
+			},
+		] );
+	}
+
+	public function invalidConstructorArgumentProvider() {
+		$callable = function() {
+		};
+
+		return [
+			[ [ 0 => $callable ] ],
+			[ [ '' => $callable ] ],
+			[ [ 'string' => null ] ],
+			[ [ 'string' => 'not a callable' ] ],
+		];
+	}
+
+	/**
+	 * @dataProvider invalidConstructorArgumentProvider
+	 */
+	public function testGivenInvalidComposer_constructorFails( $composers ) {
+		$this->setExpectedException( InvalidArgumentException::class );
+		new EntityIdComposer( $composers );
+	}
+
+	public function testGivenInvalidCallback_buildFails() {
+		$composer = new EntityIdComposer( [
+			'item' => function() {
+				return null;
+			},
+		] );
+		$this->setExpectedException( UnexpectedValueException::class );
+		$composer->composeEntityId( '', 'item', 1 );
+	}
+
+	public function validUniquePartProvider() {
+		return [
+			'int' => [ 'numeric-item', 3, new ItemId( 'Q3' ) ],
+			'float' => [ 'numeric-item', 4.0, new ItemId( 'Q4' ) ],
+			'string' => [ 'numeric-item', '5', new ItemId( 'Q5' ) ],
+
+			'custom' => [ 'custom-item', 6, new ItemId( 'Q1006' ) ],
+		];
+	}
+
+	/**
+	 * @dataProvider validUniquePartProvider
+	 */
+	public function testGivenValidFragment_buildSucceeds( $entityType, $uniquePart, EntityId $expected ) {
+		$id = $this->getComposer()->composeEntityId( '', $entityType, $uniquePart );
+		$this->assertEquals( $expected, $id );
+	}
+
+	public function invalidUniquePartProvider() {
+		return [
+			[ null, 1 ],
+			[ 'unknown', 2 ],
+			[ 'numeric-item', null ],
+			[ 'numeric-item', new ItemId( 'Q4' ) ],
+		];
+	}
+
+	/**
+	 * @dataProvider invalidUniquePartProvider
+	 */
+	public function testGivenInvalidFragment_buildFails( $entityType, $uniquePart ) {
+		$composer = $this->getComposer();
+		$this->setExpectedException( InvalidArgumentException::class );
+		$composer->composeEntityId( '', $entityType, $uniquePart );
+	}
+
+}
