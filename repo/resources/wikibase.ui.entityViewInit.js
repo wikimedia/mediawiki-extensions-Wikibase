9d49a381dfcb128ecc516425129357b7fa786b02
Aleksey Bekh-Ivanov (WMDE) <aleksey.bekh-ivanov@wikimedia.de>
2017-06-26 15:41:17 +0200

    Migrate EntityInitializer to use "wikibase.entityPage.entityLoaded" hook
    Change-Id: I4e780816004cca54859d43d74ccf85bd3889b273

--- repo/resources/wikibase.ui.entityViewInit.js
+++ repo/resources/wikibase.ui.entityViewInit.js
@@ -352,7 +352,13 @@
 		mwPerformance.mark( 'wbInitStart' );
 
 		var $entityview = $( '.wikibase-entityview' );
-		var entityInitializer = new wb.EntityInitializer( 'wbEntity' );
+		var entityPromise = $.Deferred( function ( deferred ) {
+			mw.hook( 'wikibase.entityPage.entityLoaded' ).add( function ( entity ) {
+				deferred.resolve( entity );
+			} );
+		} ).promise();
+
+		var entityInitializer = new wb.EntityInitializer( entityPromise );
 		var canEdit = isEditable();
 
 		entityInitializer.getEntity().done( function ( entity ) {
