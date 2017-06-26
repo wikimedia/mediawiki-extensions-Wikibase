526dd683a47571ccf30a176f593b3a91722d8fb6
Leszek Manicki <leszek.manicki@wikimedia.de>
2017-06-26 17:22:51 +0200

    [WIP] Add WikiPageEntityStorePermissionChecker for consistent permission checks
    WikiPageEntityStorePermissionChecker is meant a central place to check
    permission for editing entity wiki pages.
    
    TODO: follow ups using it EditEntity, dropping permission
    checker part of EntityContentFactory etc, removing other
    permission checking code from other places.
    
    TODOs:
     - Isn't the new code changing some of the previous behaviour/logic?
     - How to structure tests better? All test cases needed?
     - Anything special needed for foreign entities?
    
    Bug: T166586
    Change-Id: Ifb78d909cacb9e63168898ed35fd0f286ad711e1

---
+++ repo/includes/Store/WikiPageEntityStorePermissionChecker.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace Wikibase\Repo\Store;
+
+use Status;
+use Title;
+use User;
+use Wikibase\DataModel\Entity\EntityDocument;
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\Lib\Store\EntityNamespaceLookup;
+use Wikibase\Lib\Store\EntityTitleLookup;
+
+/**
+ * Checks permissions to do actions on the entity based on MediaWiki page permissions.
+ *
+ * @license GPL-2.0+
+ */
+class WikiPageEntityStorePermissionChecker implements EntityPermissionChecker {
+
+	/**
+	 * @var EntityNamespaceLookup
+	 */
+	private $namespaceLookup;
+
+	/**
+	 * @var EntityTitleLookup
+	 */
+	private $titleLookup;
+
+	public function __construct( EntityNamespaceLookup $namespaceLookup, EntityTitleLookup $titleLookup ) {
+		$this->namespaceLookup = $namespaceLookup;
+		$this->titleLookup = $titleLookup;
+	}
+
+	public function getPermissionStatusForEntity( User $user, $permission, EntityDocument $entity, $quick = '' ) {
+		$id = $entity->getId();
+		$status = null;
+
+		if ( $id === null ) {
+			$entityType = $entity->getType();
+
+			if ( $permission === 'edit' ) {
+				// for editing a non-existing page, check the createpage permission
+				return $this->getPermissionStatusForEntityType( $user, 'createpage', $entityType, $quick );
+			}
+
+			return $this->getPermissionStatusForEntityType( $user, $permission, $entityType, $quick );
+		}
+
+		return $this->getPermissionStatusForEntityId( $user, $permission, $id, $quick );
+	}
+
+	public function getPermissionStatusForEntityId( User $user, $permission, EntityId $entityId, $quick = '' ) {
+		$title = $this->titleLookup->getTitleForId( $entityId );
+
+		if ( $title === null ) {
+			if ( $permission === 'edit' ) {
+				return $this->getPermissionStatusForEntityType(
+					$user,
+					'createpage',
+					$entityId->getEntityType(),
+					$quick
+				);
+			}
+
+			return $this->getPermissionStatusForEntityType(
+				$user,
+				$permission,
+				$entityId->getEntityType(),
+				$quick
+			);
+		}
+
+		return $this->checkPermission( $user, $permission, $title, $entityId->getEntityType(), $quick );
+	}
+
+	public function getPermissionStatusForEntityType( User $user, $permission, $type, $quick = '' ) {
+		$title = $this->getPageTitleInEntityNamespace( $type );
+
+		if ( $permission === 'edit' ) {
+			// Note: No entity ID given, assuming creating new entity, i.e. create permissions will be checked
+			return $this->checkPermission(
+				$user,
+				'createpage',
+				$title,
+				$type,
+				$quick
+			);
+		}
+
+		return $this->checkPermission( $user, $permission, $title, $type, $quick );
+	}
+
+	/**
+	 * @param string $entityType
+	 *
+	 * @return Title
+	 */
+	private function getPageTitleInEntityNamespace( $entityType ) {
+		$namespace = $this->namespaceLookup->getEntityNamespace( $entityType ); // TODO: can be null!
+
+		return Title::makeTitle( $namespace, '/' );
+	}
+
+	private function checkPermission( User $user, $permission, Title $title, $entityType, $quick ='' ) {
+		$status = Status::newGood();
+
+		$permissions = $this->getMediaWikiPermissionsToCheck( $permission, $entityType );
+
+		foreach ( $permissions as $permission ) {
+			$partialStatus = $this->getPermissionStatus( $user, $permission, $title, $quick );
+			$status->merge( $partialStatus );
+		}
+
+		return $status;
+	}
+
+	private function getMediaWikiPermissionsToCheck( $permission, $entityType ) {
+		global $wgAvailableRights;
+
+		if ( $permission === 'edit' ) {
+			return [ 'read', 'edit' ]; // TODO: need to check read permission?
+		}
+
+		$hasEntityTypeCreateRight = in_array( $entityType . '-create', $wgAvailableRights );
+
+		if ( $permission === 'createpage' || ( $hasEntityTypeCreateRight && $permission === $entityType . '-create' ) ) {
+			$permissions = [ 'read', 'edit', 'createpage' ]; // TODO: need to check read permission?
+			if ( $hasEntityTypeCreateRight ) {
+				$permissions[] = $entityType . '-create';
+			}
+			return $permissions;
+		}
+
+		return [ $permission ];
+	}
+
+	private function getPermissionStatus( User $user, $permission, Title $title, $quick = '' ) {
+		$status = Status::newGood();
+
+		$errors = $title->getUserPermissionsErrors( $permission, $user, $quick !== 'quick' );
+
+		if ( $errors ) {
+			$status->setResult( false );
+			foreach ( $errors as $error ) {
+				call_user_func_array( [ $status, 'fatal' ], $error );
+			}
+		}
+
+		return $status;
+	}
+
+}
