ebf07d71a26e22bce0ff1e157490ec6ced7866d8
Aleksey Bekh-Ivanov (WMDE) <aleksey.bekh-ivanov@wikimedia.de>
2017-06-29 16:43:24 +0200

    Introduce `NewStatement` builder for tests
    Change-Id: I0c6f491541a9c120545e02dfee5cdc10b838e709

--- repo/tests/phpunit/includes/NewItem.php
+++ repo/tests/phpunit/includes/NewItem.php
@@ -2,17 +2,8 @@
 
 namespace Wikibase\Repo\Tests;
 
-use DataValues\DataValue;
-use DataValues\StringValue;
-use InvalidArgumentException;
-use Wikibase\DataModel\Entity\EntityId;
-use Wikibase\DataModel\Entity\EntityIdValue;
 use Wikibase\DataModel\Entity\Item;
 use Wikibase\DataModel\Entity\ItemId;
-use Wikibase\DataModel\Entity\PropertyId;
-use Wikibase\DataModel\Snak\PropertyNoValueSnak;
-use Wikibase\DataModel\Snak\PropertySomeValueSnak;
-use Wikibase\DataModel\Snak\PropertyValueSnak;
 use Wikibase\DataModel\Snak\Snak;
 use Wikibase\DataModel\Statement\Statement;
 
@@ -196,87 +187,19 @@ class NewItem {
 	}
 
 	/**
-	 * @param Statement|Snak $statement
+	 * @param NewStatement|Statement|Snak $statement
 	 *
 	 * @return self
 	 */
 	public function andStatement( $statement ) {
 		$copy = clone $this;
-		if ( $statement instanceof Snak ) {
+		if ( $statement instanceof NewStatement ) {
+			$statement = $statement->build();
+		} elseif ( $statement instanceof Snak ) {
 			$statement = new Statement( $statement );
 		}
 		$copy->statements[] = clone $statement;
 		return $copy;
-	}
-
-	/**
-	 * @see andPropertyNoValueSnak
-	 */
-	public static function withPropertyNoValueSnak( $propertyId ) {
-		return ( new self() )->andPropertyNoValueSnak( $propertyId );
-	}
-
-	/**
-	 * @param PropertyId|string $propertyId
-	 *
-	 * @return self
-	 */
-	public function andPropertyNoValueSnak( $propertyId ) {
-		if ( !( $propertyId instanceof PropertyId ) ) {
-			$propertyId = new PropertyId( $propertyId );
-		}
-
-		return $this->andStatement( new PropertyNoValueSnak( $propertyId ) );
-	}
-
-	/**
-	 * @see andPropertySomeValueSnak
-	 */
-	public static function withPropertySomeValueSnak( $propertyId ) {
-		return ( new self() )->andPropertySomeValueSnak( $propertyId );
-	}
-
-	/**
-	 * @param PropertyId|string $propertyId
-	 *
-	 * @return self
-	 */
-	public function andPropertySomeValueSnak( $propertyId ) {
-		if ( !( $propertyId instanceof PropertyId ) ) {
-			$propertyId = new PropertyId( $propertyId );
-		}
-
-		return $this->andStatement( new PropertySomeValueSnak( $propertyId ) );
-	}
-
-	/**
-	 * @see andPropertyValueSnak
-	 */
-	public static function withPropertyValueSnak( $propertyId, $dataValue ) {
-		return ( new self() )->andPropertyValueSnak( $propertyId, $dataValue );
-	}
-
-	/**
-	 * @param PropertyId|string $propertyId
-	 * @param DataValue|EntityId|string $dataValue If not a DataValue object, the builder tries to
-	 *  guess the type and turns it into a DataValue object.
-	 *
-	 * @return self
-	 */
-	public function andPropertyValueSnak( $propertyId, $dataValue ) {
-		if ( !( $propertyId instanceof PropertyId ) ) {
-			$propertyId = new PropertyId( $propertyId );
-		}
-
-		if ( $dataValue instanceof EntityId ) {
-			$dataValue = new EntityIdValue( $dataValue );
-		} elseif ( is_string( $dataValue ) ) {
-			$dataValue = new StringValue( $dataValue );
-		} elseif ( !( $dataValue instanceof DataValue ) ) {
-			throw new InvalidArgumentException( 'Unsupported $dataValue type' );
-		}
-
-		return $this->andStatement( new PropertyValueSnak( $propertyId, $dataValue ) );
 	}
 
 }
