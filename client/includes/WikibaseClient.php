b9a6026488e19e8d38b6934154b9d52e43f1df01
Amir Sarabadani <ladsgroup@gmail.com>
2017-07-09 10:17:26 +0430

    Wait for replication after making changes in WikiPageUpdater
    Bug: T164173
    Change-Id: If195a35e9cf4eb245457a26213b1c9db4566c744

--- client/includes/WikibaseClient.php
+++ client/includes/WikibaseClient.php
@@ -1161,6 +1161,7 @@ final class WikibaseClient {
 		$pageUpdater = new WikiPageUpdater(
 			JobQueueGroup::singleton(),
 			$this->getRecentChangeFactory(),
+			MediaWikiServices::getInstance()->getDBLoadBalancerFactory(),
 			$this->getStore()->getRecentChangesDuplicateDetector(),
 			MediaWikiServices::getInstance()->getStatsdDataFactory()
 		);
