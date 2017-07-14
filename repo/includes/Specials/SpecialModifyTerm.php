0a445c07004d0ed6f76a5004bb54e84e24caaa38
Amir Sarabadani <ladsgroup@gmail.com>
2017-07-15 02:33:47 +0430

    Add hidden field for base revision id in editing entities special pages
    This later will be needed when detecting edit conflicts
    
    Change-Id: Ic754ae6f0c850511a54910fdfc31ac8538c5f835

--- repo/includes/Specials/SpecialModifyTerm.php
+++ repo/includes/Specials/SpecialModifyTerm.php
@@ -240,7 +240,12 @@ abstract class SpecialModifyTerm extends SpecialModifyEntity {
 					'type' => 'hidden',
 					'default' => 'remove'
 				],
-				'value' => $valueinput
+				'value' => $valueinput,
+				'revid' => [
+					'name' => 'revid',
+					'type' => 'hidden',
+					'default' => $this->entityRevision->getRevisionId(),
+				],
 			];
 		} else {
 			// Messages:
