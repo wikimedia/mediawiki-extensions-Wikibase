761200c3f79511e21180bae22fc395f95f12ba7e
Leszek Manicki <leszek.manicki@wikimedia.de>
2017-07-17 17:39:58 +0200

    API classes require permissions defined by ChangeOps they use
    In particular wbeditentity requires entity term editing
    permission if the request changes entity terms.
    
    Bug: T170673
    Change-Id: I88b1fb2d47ee9098f4e7ceec4e87f0b3f57dae63

--- repo/includes/Api/EditEntity.php
+++ repo/includes/Api/EditEntity.php
@@ -241,11 +241,12 @@ class EditEntity extends ModifyEntity {
 	 * @see ModifyEntity::modifyEntity
 	 *
 	 * @param EntityDocument &$entity
+	 * @param ChangeOp $changeOp
 	 * @param array $preparedParameters
 	 *
 	 * @return Summary
 	 */
-	protected function modifyEntity( EntityDocument &$entity, array $preparedParameters ) {
+	protected function modifyEntity( EntityDocument &$entity, ChangeOp $changeOp, array $preparedParameters ) {
 		$data = $preparedParameters['data'];
 
 		$exists = $this->entityExists( $entity->getId() );
@@ -259,9 +260,7 @@ class EditEntity extends ModifyEntity {
 			$entity->setDataTypeId( $data['datatype'] );
 		}
 
-		$changeOps = $this->getChangeOp( $data, $entity );
-
-		$this->applyChangeOp( $changeOps, $entity );
+		$this->applyChangeOp( $changeOp, $entity );
 
 		$this->buildResult( $entity );
 		return $this->getSummary( $preparedParameters );
@@ -303,16 +302,17 @@ class EditEntity extends ModifyEntity {
 	}
 
 	/**
-	 * @param array $changeRequest an array of data to apply. For example:
-	 *        [ 'label' => [ 'zh' => [ 'remove' ], 'de' => [ 'value' => 'Foo' ] ] ]
+	 * @param array $preparedParameters
 	 * @param EntityDocument $entity
 	 *
 	 * @throws ApiUsageException
 	 * @return ChangeOp
 	 */
-	private function getChangeOp( array $changeRequest, EntityDocument $entity ) {
+	protected function getChangeOp( array $preparedParameters, EntityDocument $entity ) {
+		$data = $preparedParameters['data'];
+
 		try {
-			return $this->entityChangeOpProvider->newEntityChangeOp( $entity->getType(), $changeRequest );
+			return $this->entityChangeOpProvider->newEntityChangeOp( $entity->getType(), $data );
 		} catch ( ChangeOpDeserializationException $exception ) {
 			$this->errorReporter->dieException( $exception, $exception->getErrorCode() );
 		}
