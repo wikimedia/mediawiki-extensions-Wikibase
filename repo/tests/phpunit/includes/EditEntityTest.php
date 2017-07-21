d5d1f4ee640eed87ea065a084bff2aee335abf29
daniel <daniel.kinzler@wikimedia.de>
2017-07-21 19:33:05 +0200

    Simplify edit conflict detection in EditEntity.
    This change does two things:
    * Always check for "late" conflicts: when a new revision was added while
      the edit was in progress, always fail.
    
    * If no base revision is given, always use the latest revision as base
      revision. That removes the "no base revision" mode.
    
    Change-Id: I514a4acfa4e6ead356a5a7f0480a172f2086e012

--- repo/tests/phpunit/includes/EditEntityTest.php
+++ repo/tests/phpunit/includes/EditEntityTest.php
@@ -364,17 +364,7 @@ class EditEntityTest extends MediaWikiTestCase {
 		];
 	}
 
-	public function provideAttemptSaveWithLateConflict() {
-		return [
-			[ true, true ],
-			[ false, false ],
-		];
-	}
-
-	/**
-	 * @dataProvider provideAttemptSaveWithLateConflict
-	 */
-	public function testAttemptSaveWithLateConflict( $baseRevId, $expectedConflict ) {
+	public function testAttemptSaveWithLateConflict() {
 		$repo = $this->getMockRepository();
 
 		$user = $this->getUser( 'EditEntityTestUser' );
@@ -390,10 +380,8 @@ class EditEntityTest extends MediaWikiTestCase {
 		$entity->setLabel( 'en', 'Trust' );
 
 		$titleLookup = $this->getEntityTitleLookup();
-		$editEntity = $this->makeEditEntity( $repo, $entity->getId(), $titleLookup, $user, $baseRevId );
+		$editEntity = $this->makeEditEntity( $repo, $entity->getId(), $titleLookup, $user, true );
 		$editEntity->getLatestRevision(); // make sure EditEntity has page and revision
-
-		$this->assertEquals( $baseRevId !== false, $editEntity->doesCheckForEditConflicts(), 'doesCheckForEditConflicts()' );
 
 		// create independent Entity instance for the same entity, and modify and save it
 		$entity2 = new Item( new ItemId( 'Q42' ) );
@@ -414,16 +402,16 @@ class EditEntityTest extends MediaWikiTestCase {
 			$statusMessage = "Status ($id): " . $status->getWikiText();
 		}
 
-		$this->assertNotEquals( $expectedConflict, $status->isOK(),
-			"Saving should have failed late if and only if a base rev was provided.\n$statusMessage" );
+		$this->assertFalse( $status->isOK(),
+			"Saving should have failed late\n$statusMessage" );
 
-		$this->assertEquals( $expectedConflict, $editEntity->hasError(),
-			"Saving should have failed late if and only if a base rev was provided.\n$statusMessage" );
+		$this->assertTrue( $editEntity->hasError(),
+			"Saving should have failed late\n$statusMessage" );
 
-		$this->assertEquals( $expectedConflict, $status->hasMessage( 'edit-conflict' ),
-			"Saving should have failed late if and only if a base rev was provided.\n$statusMessage" );
+		$this->assertTrue( $status->hasMessage( 'edit-conflict' ),
+			"Saving should have failed late\n$statusMessage" );
 
-		$this->assertEquals( $expectedConflict, $editEntity->showErrorPage(),
+		$this->assertTrue( $editEntity->showErrorPage(),
 			"If and only if there was an error, an error page should be shown.\n$statusMessage" );
 	}
 
