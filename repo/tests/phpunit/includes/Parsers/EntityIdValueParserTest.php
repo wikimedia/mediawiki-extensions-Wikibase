be7ef81674544299fd85f13d726c777e6f5b5912
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2017-08-02 14:48:52 +0200

    Remove DataValues Common <0.3 compatibility code
    I added this to be sure the code is still compatible with older DataValues
    Common versions, but we require >=0.3 for a while now. We just forgot to
    remove this.
    
    Change-Id: I075a1ff1ad8926f1131659965e55b3f8db8b2fc6

--- repo/tests/phpunit/includes/Parsers/EntityIdValueParserTest.php
+++ repo/tests/phpunit/includes/Parsers/EntityIdValueParserTest.php
@@ -23,13 +23,6 @@ use Wikibase\Repo\Parsers\EntityIdValueParser;
 class EntityIdValueParserTest extends StringValueParserTest {
 
 	/**
-	 * @deprecated since DataValues Common 0.3, just use getInstance.
-	 */
-	protected function getParserClass() {
-		throw new \LogicException( 'Should not be called, use getInstance' );
-	}
-
-	/**
 	 * @see ValueParserTestBase::getInstance
 	 *
 	 * @return EntityIdValueParser
