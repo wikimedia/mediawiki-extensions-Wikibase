98d6b73c3c11fa6b600d989129c095b2c3ebfc4f
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2017-08-10 16:55:00 -0400

    Add monolingual wikitext formatter
    With PS6 this patch became a lot simpler because the unused options
    have already been removed in Id885f29.
    
    Change-Id: Ie820a6da323f5165f1f8dbdd1e170d3f00d4548a

---
+++ lib/tests/phpunit/Formatters/MonolingualWikitextFormatterTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Wikibase\Lib\Tests\Formatters;
+
+use DataValues\MonolingualTextValue;
+use Wikibase\Lib\Formatters\MonolingualWikitextFormatter;
+
+/**
+ * @covers Wikibase\Lib\Formatters\MonolingualWikitextFormatter
+ *
+ * @group ValueFormatters
+ * @group Wikibase
+ *
+ * @license GPL-2.0-or-later
+ * @author Thiemo Kreuz
+ */
+class MonolingualWikitextFormatterTest extends \MediaWikiTestCase {
+
+	/**
+	 * @dataProvider monolingualWikitextProvider
+	 */
+	public function testFormat( MonolingualTextValue $value, $expected ) {
+		$formatter = new MonolingualWikitextFormatter();
+		$wikitext = $formatter->format( $value );
+		$this->assertSame( $expected, $wikitext );
+	}
+
+	public function monolingualWikitextProvider() {
+		return [
+			'formatting' => [
+				new MonolingualTextValue( 'de', 'Hallo Welt' ),
+				'<span lang="de">Hallo Welt</span>'
+			],
+			'wikitext escaping' => [
+				new MonolingualTextValue( 'de', '[[Hallo&Welt]]' ),
+				'<span lang="de">&#91;&#91;Hallo&#38;Welt&#93;&#93;</span>'
+			],
+			'code injection' => [
+				new MonolingualTextValue( 'de', '<script>alert("Hallo Welt")</script>' ),
+				'<span lang="de">&#60;script&#62;alert(&#34;Hallo Welt&#34;)&#60;/script&#62;</span>'
+			],
+		];
+	}
+
+}
