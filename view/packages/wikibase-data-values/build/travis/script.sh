88db9af0fa69f1905c205ed872fc9761a5782de9
Leszek Manicki <leszek.manicki@wikimedia.de>
2017-08-11 13:30:55 -0400

    Add package.json and run eslint checks using npm test

--- view/packages/wikibase-data-values/build/travis/script.sh
+++ view/packages/wikibase-data-values/build/travis/script.sh
@@ -2,13 +2,6 @@
 
 set -x
 
-if [[ $RUNJOB == eslint ]]; then
-	npm install eslint
-	npm install --save eslint-config-wikimedia
-	./node_modules/.bin/eslint .
-	exit $?
-fi
-
 if [[ $RUNJOB == qunit ]]; then
 	cd lib/TestRunner
 	phantomjs runTests.phantom.js ../../tests/runTests.html
