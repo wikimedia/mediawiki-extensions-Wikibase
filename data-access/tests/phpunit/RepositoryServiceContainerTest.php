61377bb8db34fe65006edb708a2ea4dc43d0e45c
Leszek Manicki <leszek.manicki@wikimedia.de>
2017-08-22 13:52:32 +0200

    Do not pass the WikibaseClient instance to RepositoryServiceContainer
    RepositoryServiceContainer gets the array of callbacks instantiating
    entity deserializers in the constructor, instead of WikibaseClient
    being passed.
    
    RepositoryServiceContainerFactory gets the instance of
    EntityTypeDefinitions that is then used the get the array
    of deserializer "factory" callbacks.
    
    Note this does not yet entirely remove the dependency of data access
    component on the Client component.
    
    Change-Id: I6338881ae4140894b2cb869f0216431dec32c87b

--- data-access/tests/phpunit/RepositoryServiceContainerTest.php
+++ data-access/tests/phpunit/RepositoryServiceContainerTest.php
@@ -66,7 +66,7 @@ class RepositoryServiceContainerTest extends \PHPUnit_Framework_TestCase {
 			new DataValueDeserializer( [] ),
 			new GenericServices( $client->getEntityNamespaceLookup(), new EntityTypeDefinitions( [] ) ),
 			new DataAccessSettings( 0, false ),
-			$client
+			[]
 		);
 	}
 
