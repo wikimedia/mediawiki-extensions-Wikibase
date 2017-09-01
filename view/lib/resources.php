e625eb48d3278f9742acbdb9dc5e262b87f0ee06
Leszek Manicki <leszek.manicki@wikimedia.de>
2017-09-01 17:51:41 +0200

    [WIP][DNM][TEST][GOAT] Mount JavaScript "libraries" as git submodules
    Background: there are several JS libraries installed using composer
    in Wikibase. They contain some PHP code but it is mostly (if not
    only) for registering ResourceLoader modules.
    They are not real extensions neither PHP libraries.
    
    Instead of installing those using composer, try making each of
    them a git submodule in some directory. Then move the RL module
    definitons from those libs into Wikibase main git repo (as said
    before, those libs are not really extensions and have nothing
    to do with MW actually).
    
    This patch takes the first such lib as a proof-of-concept wild
    test. None of other libs does not depend on this one, so it
    could be dropped from composer.json and is not installed by
    Composer).
    
    RL modules related to tests (present in those libs too) are
    intentionally NOT moved. Those libs should care of running
    their tests themselves (WIP, see TODO below).
    
    This seems to work locally (as in JS stuff loads and does not
    seem to break UI). If browser tests etc pass, that might
    be a possible way to actually KILL the JS part of the
    Wikidata build step.
    
    TODOs (list hugely imcomplete):
     - think the structure of dirs (where put those libs, how organize
       the RL modules files, etc)
     - clean up those JS libs: make them run their JS tests themselves
       (WIP, close to done), remove all RL and other PHP content from them
     - turn those fake PHP libs into npm packages (or so)
     - possibly tests files of those libs should not be included
       in the git submodules (if there is a way to skip them)
     - document the relevant somewhere (how to switch git submodules to
       a new release, as they're not intended to be using their
       masters)
     - anything needed to be done to installation process (or commands
       run on CI) so those modules are initialized properly on clone,
       pull, etc?
     - probably 600 things more...
    
    Change-Id: I62e07ce35488fe7d828d483d6eddb134b72da7dd

---
+++ view/lib/resources.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license GPL-2.0+
+ */
+return call_user_func( function() {
+        return array_merge(
+                include __DIR__ . '/resources/wikibase-serialization-javascript.php'
+        );
+} );
