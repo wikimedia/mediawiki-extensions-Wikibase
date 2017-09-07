764889833fd101c28d385f5b98b5977fd8553c9e
Stanislav Malyshev <smalyshev@wikimedia.org>
2017-09-07 16:08:45 -0700

    [WIP] Index statements on items
    Statements are indexed in the field 'statements' and are represented
    as 'property:value' e.g. P31:Q4167410.
    
    Bug: T175199
    Change-Id: I880808d70ed5350d39c81e1f41560dccfd982d00

---
+++ repo/includes/Search/Elastic/Fields/StatementProviderFieldDefinitions.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Wikibase\Repo\Search\Elastic\Fields;
+
+use Wikibase\Lib\DataTypeDefinitions;
+
+/**
+ * Fields for an object that has statements.
+ */
+class StatementProviderFieldDefinitions implements FieldDefinitions {
+
+	/**
+	 * List of properties to index.
+	 * @var string[]
+	 */
+	private $properties;
+
+	/**
+	 * @var callable[]
+	 */
+	private $definitions;
+
+	/**
+	 * StatementProviderFieldDefinitions constructor.
+	 * @param string[] $properties List of properties to index
+	 * @param callable[] $definitions
+	 */
+	public function __construct( $properties, $definitions ) {
+		$this->properties = $properties;
+	}
+
+	/**
+	 * Get the list of definitions
+	 * @return WikibaseIndexField[] key is field name, value is WikibaseIndexField
+	 */
+	public function getFields() {
+		$fields['statements'] = new StatementsField( $this->properties, $this->definitions );
+		$fields['statement_count'] = new StatementCountField();
+		return $fields;
+	}
+
+}
