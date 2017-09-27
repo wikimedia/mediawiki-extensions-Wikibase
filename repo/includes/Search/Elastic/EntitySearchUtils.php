55f9727951e0732769f399488e7bfc54a616384a
Stanislav Malyshev <smalyshev@wikimedia.org>
2017-09-26 17:47:58 -0700

    Add fulltext search support for Wikidata labels
    This patch defines new fulltext search builder, result set
    and result types. These cover both API and desktop fulltext searches.
    
    The patch uses onShowSearchHit and onShowSearchHitTitle to display
    search results with highlighting. The result would display label and
    description for the entity with highlighting as appropriate.
    
    Note that search code won't use LinkBegin code anymore, but it does
    reuse some of the code in LinkBegin handler.
    
    The results will be displayed in this format:
    
    Label (Q123) : Additional match
    Description
    N statements, N sitelinks - DATE
    
    Where "additional match" is optional and is displayed if the match did
    not happen in neither main label nor description.
    
    Bug: T178851
    Depends-On: Ibecea9f4f697e1788029adfbaa5e1e4ba28a6880
    Change-Id: I9609c78dfe16241424ab38bb6da37d3ae27b0e1b

---
+++ repo/includes/Search/Elastic/EntitySearchUtils.php
@@ -0,0 +1,71 @@
+<?php
+namespace Wikibase\Repo\Search\Elastic;
+
+use Elastica\Query\ConstantScore;
+use Elastica\Query\Match;
+use Wikibase\DataModel\Entity\EntityIdParser;
+use Wikibase\DataModel\Entity\EntityIdParsingException;
+
+/**
+ * Utilities useful for entity searches.
+ */
+final class EntitySearchUtils {
+
+	/**
+	 * Create constant score query for a field.
+	 * @param string $field
+	 * @param string|double $boost
+	 * @param string $text
+	 * @return ConstantScore
+	 */
+	public static function makeConstScoreQuery( $field, $boost, $text ) {
+		$csquery = new ConstantScore();
+		$csquery->setFilter( new Match( $field, $text ) );
+		$csquery->setBoost( $boost );
+		return $csquery;
+	}
+
+	/**
+	 * If the text looks like ID, normalize it to ID title
+	 * Cases handled:
+	 * - q42
+	 * - (q42)
+	 * - leading/trailing spaces
+	 * - http://www.wikidata.org/entity/Q42
+	 * @param string $text
+	 * @param EntityIdParser $idParser
+	 * @return string Normalized ID or original string
+	 */
+	public static function normalizeId( $text, EntityIdParser $idParser ) {
+		// TODO: this is a bit hacky, better way would be to make the field case-insensitive
+		// or add new subfiled which is case-insensitive
+		$text = strtoupper( str_replace( [ '(', ')' ], '', trim( $text ) ) );
+		$id = self::parseOrNull( $text, $idParser );
+		if ( $id ) {
+			return $id->getSerialization();
+		}
+		if ( preg_match( '/\b(\w+)$/', $text, $matches ) && $matches[1] ) {
+			$id = self::parseOrNull( $matches[1], $idParser );
+			if ( $id ) {
+				return $id->getSerialization();
+			}
+		}
+		return $text;
+	}
+
+	/**
+	 * Parse entity ID or return null
+	 * @param $text
+	 * @param EntityIdParser $idParser
+	 * @return null|\Wikibase\DataModel\Entity\EntityId
+	 */
+	public static function parseOrNull( $text, EntityIdParser $idParser ) {
+		try {
+			$id = $idParser->parse( $text );
+		} catch ( EntityIdParsingException $ex ) {
+			return null;
+		}
+		return $id;
+	}
+
+}
