4e28a911ce1717b3e96e1eefb9031d1550bc2590
aude <aude.wiki@gmail.com>
2017-10-05 03:55:25 -0400

    Split Echo hook handlers based on services needed
    BeforeCreateEchoEvent runs when the Echo extension is
    initialized, so on every page load.
    
    All this hook needs is a couple settings and no services.
    Other echo hooks require some services that are heavy to
    instantiate.
    
    This patch splits the hook handlers into two classes,
    depending on what services/dependencies they need so we
    avoid instantiating unnecessary services on page load.
    
    Bug: T177311
    Change-Id: I3c13b6d3a88a9db706db9b6a7c753080994b8830

---
+++ client/includes/Hooks/EchoSetupHookHandlers.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Wikibase\Client\Hooks;
+
+use EchoAttributeManager;
+use EchoUserLocator;
+use Wikibase\Client\Notifications\PageConnectionPresentationModel;
+use Wikibase\WikibaseSettings;
+
+/**
+ * Handlers for hooks (e.g. BeforeCreateEchoEvent) called when Echo extension
+ * is initialized, so on every page load.
+ *
+ * @license GPL-2.0+
+ * @author Matěj Suchánek
+ * @author Katie Filbert < aude.wiki@gmail.com >
+ */
+class EchoSetupHookHandlers {
+
+	/**
+	 * @var bool
+	 */
+	private $sendEchoNotification;
+
+	/**
+	 * @var array|false
+	 */
+	private $echoIcon;
+
+	/**
+	 * @param bool $sendEchoNotification
+	 * @param array|false $echoIcon
+	 */
+	public function __construct( $sendEchoNotification, $echoIcon ) {
+		$this->sendEchoNotification = $sendEchoNotification;
+		$this->echoIcon = $echoIcon;
+	}
+
+	/**
+	 * @return self
+	 */
+	public static function newFromGlobalState() {
+		$settings = WikibaseSettings::getClientSettings();
+
+		return new self(
+			$settings->getSetting( 'sendEchoNotification' ),
+			$settings->getSetting( 'echoIcon' )
+		);
+	}
+
+	/**
+	 * Handler for BeforeCreateEchoEvent hook
+	 * @see https://www.mediawiki.org/wiki/Extension:Echo/BeforeCreateEchoEvent
+	 * @see doBeforeCreateEchoEvent
+	 *
+	 * @param array[] &$notifications
+	 * @param array[] &$notificationCategories
+	 * @param array[] &$icons
+	 */
+	public static function onBeforeCreateEchoEvent(
+		array &$notifications,
+		array &$notificationCategories,
+		array &$icons
+	) {
+		$self = self::newFromGlobalState();
+		$self->doBeforeCreateEchoEvent( $notifications, $notificationCategories, $icons );
+	}
+
+	/**
+	 * @see https://www.mediawiki.org/wiki/Notifications/Developer_guide
+	 *
+	 * @param array[] &$notifications
+	 * @param array[] &$notificationCategories
+	 * @param array[] &$icons
+	 */
+	public function doBeforeCreateEchoEvent(
+		array &$notifications,
+		array &$notificationCategories,
+		array &$icons
+	) {
+		if ( $this->sendEchoNotification !== true ) {
+			return;
+		}
+
+		$notificationCategories['wikibase-action'] = [
+			'priority' => 5,
+			'tooltip' => 'echo-pref-tooltip-wikibase-action',
+		];
+
+		$notifications[EchoNotificationsHandlers::NOTIFICATION_TYPE] = [
+			EchoAttributeManager::ATTR_LOCATORS => [
+				EchoUserLocator::class . '::locateArticleCreator',
+			],
+			'category' => 'wikibase-action',
+			'group' => 'neutral',
+			'section' => 'message',
+			'presentation-model' => PageConnectionPresentationModel::class,
+			'bundle' => [ 'web' => true, 'email' => false ],
+		];
+
+		if ( !empty( $this->echoIcon ) ) {
+			$icons[EchoNotificationsHandlers::NOTIFICATION_TYPE] = $this->echoIcon;
+		} else {
+			preg_match( '+/extensions/(.*)+', __DIR__, $remoteExtPath );
+			$iconPath = $remoteExtPath[1] . '/../../resources/images/echoIcon.svg';
+			$icons[EchoNotificationsHandlers::NOTIFICATION_TYPE] = [ 'path' => $iconPath ];
+		}
+	}
+
+}
--- client/tests/phpunit/includes/Notifications/PageConnectionPresentationModelTest.php
+++ client/tests/phpunit/includes/Notifications/PageConnectionPresentationModelTest.php
@@ -9,9 +9,8 @@ use Message;
 use Title;
 use User;
 use Wikibase\Client\Hooks\EchoNotificationsHandlers;
-use Wikibase\Client\NamespaceChecker;
+use Wikibase\Client\Hooks\EchoSetupHookHandlers;
 use Wikibase\Client\Notifications\PageConnectionPresentationModel;
-use Wikibase\Client\RepoLinker;
 
 /**
  * @covers Wikibase\Client\Notifications\PageConnectionPresentationModel
@@ -61,34 +60,12 @@ class PageConnectionPresentationModelTest extends MediaWikiTestCase {
 		return $event;
 	}
 
-	/**
-	 * @return RepoLinker
-	 */
-	private function getRepoLinker() {
-		$repoLinker = $this->getMockBuilder( RepoLinker::class )
-			->disableOriginalConstructor()
-			->getMock();
-		$repoLinker
-			->expects( $this->any() )
-			->method( 'getEntityUrl' )
-			->will( $this->returnValue( 'foo' ) );
-
-		return $repoLinker;
-	}
-
 	public function testPresentationModel() {
 		global $wgEchoNotifications, $wgEchoNotificationCategories, $wgEchoNotificationIcons;
 
-		$namespaceChecker = $this->getMockBuilder( NamespaceChecker::class )
-			->disableOriginalConstructor()
-			->getMock();
-		$handlers = new EchoNotificationsHandlers(
-			$this->getRepoLinker(),
-			$namespaceChecker,
-			'enwiki',
+		$handlers = new EchoSetupHookHandlers(
 			true,
-			false,
-			'repoSiteName'
+			false
 		);
 		$handlers->doBeforeCreateEchoEvent(
 			$wgEchoNotifications, $wgEchoNotificationCategories, $wgEchoNotificationIcons
@@ -134,9 +111,9 @@ class PageConnectionPresentationModelTest extends MediaWikiTestCase {
 		$this->assertInternalType( 'array', $model->getSecondaryLinks() );
 		$this->assertInstanceOf( Message::class, $model->getSubjectMessage() );
 
-		unset( $wgEchoNotifications[$handlers::NOTIFICATION_TYPE] );
+		unset( $wgEchoNotifications[EchoNotificationsHandlers::NOTIFICATION_TYPE] );
 		unset( $wgEchoNotificationCategories['wikibase-action'] );
-		unset( $wgEchoNotificationIcons[$handlers::NOTIFICATION_TYPE] );
+		unset( $wgEchoNotificationIcons[EchoNotificationsHandlers::NOTIFICATION_TYPE] );
 	}
 
 }
