9c87874d0ea71f29d63faba1d836271ee021b0cc
Leszek Manicki <leszek.manicki@wikimedia.de>
2017-10-06 17:01:06 +0200

    Remove MediaWiki ResourceLoader module definitions

--- view/packages/wikibase-serialization/init.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define( 'WIKIBASE_SERIALIZATION_JAVASCRIPT_VERSION', '3.0.0' );
-
-if ( defined( 'MEDIAWIKI' ) ) {
-	call_user_func( function() {
-		require_once __DIR__ . '/init.mw.php';
-	} );
-}
