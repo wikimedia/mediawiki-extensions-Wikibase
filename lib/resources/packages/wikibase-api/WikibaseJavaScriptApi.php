019eb01091dc7aa170b85e6538410f299f6a4a4f
Leszek Manicki <leszek.manicki@wikimedia.de>
2017-10-13 13:44:00 +0200

    Removed Composer files
    Bug: T178088
    Change-Id: Ib148caa762a11484dee48fedb3f39c779997930d

--- lib/resources/packages/wikibase-api/WikibaseJavaScriptApi.php
+++ lib/resources/packages/wikibase-api/WikibaseJavaScriptApi.php
@@ -7,11 +7,6 @@ if ( defined( 'WIKIBASE_JAVASCRIPT_API_VERSION' ) ) {
 
 define( 'WIKIBASE_JAVASCRIPT_API_VERSION', '2.2.2' );
 
-// Include the composer autoloader if it is present.
-if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
-	require_once __DIR__ . '/vendor/autoload.php';
-}
-
 if ( defined( 'MEDIAWIKI' ) ) {
 	call_user_func( function () {
 		require_once __DIR__ . '/init.mw.php';
