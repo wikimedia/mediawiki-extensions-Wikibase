707dfb8891292b0d4b0fdc5ab3d4ad4a721e3be2
Stanislav Malyshev <smalyshev@wikimedia.org>
2017-10-16 16:45:05 -0700

    Enable configurable boosting certain statement values.
    Can be configured e.g. thusly:
    $wgWBRepoSettings['entitySearch']['statementBoost'] = [ 'P31=Q4167410' => '-1' ];
    
    Bug: T148411
    Change-Id: I874d2d00a83476cfcb1db0e06ad8a0c0fee9d05b
    Depends-On: Ibfd5505405499cb2dfed7006573ce0d735b68691

--- repo/config/ElasticSearchRescoreProfiles.php
+++ repo/config/ElasticSearchRescoreProfiles.php
@@ -20,4 +20,20 @@ return [
 			],
 		]
 	],
+	// Profile that uses both entity weight end statement-based boosts
+	'wikibase_prefix_boost' => [
+		'i18n_msg' => 'wikibase-rescore-profile-prefix',
+		'supported_namespaces' => 'all',
+		'rescore' => [
+			[
+				'window' => 8192,
+				'window_size_override' => 'EntitySearchRescoreWindowSize',
+				'query_weight' => 1.0,
+				'rescore_query_weight' => 1.0,
+				'score_mode' => 'total',
+				'type' => 'function_score',
+				'function_chain' => 'entity_weight_boost'
+			],
+		]
+	],
 ];
