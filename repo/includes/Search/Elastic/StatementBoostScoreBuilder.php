44b29f45dc4b0e4fa32190b5bf3419a626ce79f8
Stanislav Malyshev <smalyshev@wikimedia.org>
2017-10-16 16:45:05 -0700

    Enable configurable boosting certain statement values.
    Can be configured e.g. thusly:
    $wgWBRepoSettings['entitySearch']['statementBoost'] = [ 'P31=Q4167410' => '-1' ];
    
    Bug: T148411
    Change-Id: I874d2d00a83476cfcb1db0e06ad8a0c0fee9d05b
    Depends-On: Ibfd5505405499cb2dfed7006573ce0d735b68691

---
+++ repo/includes/Search/Elastic/StatementBoostScoreBuilder.php
@@ -0,0 +1,40 @@
+<?php
+namespace Wikibase\Repo\Search\Elastic;
+
+use CirrusSearch\Search\FunctionScoreBuilder;
+use CirrusSearch\Search\SearchContext;
+use CirrusSearch\Search\TermBoostScoreBuilder;
+use Elastica\Query\FunctionScore;
+use Wikibase\Repo\WikibaseRepo;
+
+/**
+ * Boost function implementation for statement values.
+ * @package Wikibase\Repo\Search\Elastic
+ */
+class StatementBoostScoreBuilder extends FunctionScoreBuilder {
+	/**
+	 * @var TermBoostScoreBuilder
+	 */
+	private $termBuilder;
+
+	/**
+	 * @param SearchContext $context
+	 * @param float $weight
+	 * @param WikibaseRepo $repo
+	 */
+	public function __construct( SearchContext $context, $weight, WikibaseRepo $repo ) {
+		parent::__construct( $context, $weight );
+		$settings = $repo->getSettings()->getSetting( 'entitySearch' );
+		$this->termBuilder = new TermBoostScoreBuilder( $context, $weight,
+				[ 'statement_keywords' => $settings['statementBoost'] ] );
+	}
+
+	/**
+	 * Append functions to the function score $container
+	 *
+	 * @param FunctionScore $container
+	 */
+	public function append( FunctionScore $container ) {
+		$this->termBuilder->append( $container );
+	}
+}
