07da43bfc170c00e6988b740b229557368615b32
Leszek Manicki <leszek.manicki@wikimedia.de>
2017-10-18 09:25:38 +0200

    Install value-view lib as git submodule, not as composer dependency
    Bug: T178576
    Change-Id: I4ac2518123148699173a45c18693494d63a200bb

---
+++ view/lib/i18n.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license GPL-2.0+
+ */
+return call_user_func( function() {
+	global $wgMessagesDirs;
+
+	$wgMessagesDirs['WikibaseView'] = array_merge(
+		$wgMessagesDirs['WikibaseView'],
+		 [ __DIR__ . '/wikibase-data-values-value-view/i18n' ]
+	);
+} );
