38bcbf4f7dbfc73b7a7a26bb25f6b647661e07b8
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2017-10-19 16:30:19 +0200

    Minor cleanups to StatementBoostScoreBuilder and related
    This fixes all issues I had with I874d2d0. If this get's a +2, I'm fine
    with I874d2d0 being merged as it is.
    
    Bug: T148411
    Change-Id: Ieb92fefa5dc017be3039250a75a9ea71b7bc809d

--- repo/includes/Search/Elastic/StatementBoostScoreBuilder.php
+++ repo/includes/Search/Elastic/StatementBoostScoreBuilder.php
@@ -1,16 +1,20 @@
 <?php
+
 namespace Wikibase\Repo\Search\Elastic;
 
 use CirrusSearch\Search\FunctionScoreBuilder;
 use CirrusSearch\Search\SearchContext;
 use CirrusSearch\Search\TermBoostScoreBuilder;
 use Elastica\Query\FunctionScore;
-use Wikibase\Repo\WikibaseRepo;
 
 /**
  * Boost function implementation for statement values.
+ *
+ * @license GPL-2.0+
+ * @author Stas Malyshev
  */
 class StatementBoostScoreBuilder extends FunctionScoreBuilder {
+
 	/**
 	 * @var TermBoostScoreBuilder
 	 */
@@ -19,11 +23,16 @@ class StatementBoostScoreBuilder extends FunctionScoreBuilder {
 	/**
 	 * @param SearchContext $context
 	 * @param float $weight
-	 * @param array[] $settings Boost settings: [ 'P31=Q123' => SCORE, ... ]
+	 * @param array $settings Boost settings: [ 'P31=Q123' => SCORE, ... ]
 	 */
 	public function __construct( SearchContext $context, $weight, array $settings ) {
 		parent::__construct( $context, $weight );
-		$this->termBuilder = new TermBoostScoreBuilder( $context, $weight, [ 'statement_keywords' => $settings ] );
+
+		$this->termBuilder = new TermBoostScoreBuilder(
+			$context,
+			$weight,
+			[ 'statement_keywords' => $settings ]
+		);
 	}
 
 	/**
