49971721b1492ab57f4f206897c966bcebaee8b0
Leszek Manicki <leszek.manicki@wikimedia.de>
2017-10-19 17:27:32 +0200

    Install data values js lib as a git submodule, not as a composer dependency
    Bug: T178580
    Change-Id: I4a44c76825c4b9ba446463ecc609f28a80756039

--- view/lib/resources.php
+++ view/lib/resources.php
@@ -5,6 +5,7 @@
  */
 return call_user_func( function() {
 	return array_merge(
+		include __DIR__ . '/resources/wikibase-data-values.php',
 		include __DIR__ . '/resources/wikibase-serialization.php',
 		include __DIR__ . '/resources/wikibase-api.php',
 		include __DIR__ . '/resources/wikibase-data-values-value-view.php'
---
+++ view/lib/resources/wikibase-data-values/src/valueParsers/resources.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @license GPL-2.0+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ * @author Daniel Werner < daniel.werner@wikimedia.de >
+ *
+ * @codeCoverageIgnoreStart
+ */
+return call_user_func( function() {
+	$dir = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR
+		. '..' . DIRECTORY_SEPARATOR . 'wikibase-data-values' . DIRECTORY_SEPARATOR . 'lib'
+		.  DIRECTORY_SEPARATOR . 'valueParsers';
+
+	preg_match( '+' . preg_quote( DIRECTORY_SEPARATOR ) . '(?:vendor|extensions)'
+		. preg_quote( DIRECTORY_SEPARATOR ) . '.*+', $dir, $remoteExtPath );
+
+	$moduleTemplate = [
+		'localBasePath' => $dir,
+		'remoteExtPath' => '..' . $remoteExtPath[0],
+	];
+
+	return [
+		'valueParsers' => $moduleTemplate + [
+				'scripts' => [
+					'valueParsers.js',
+				],
+			],
+
+		'valueParsers.ValueParser' => $moduleTemplate + [
+				'scripts' => [
+					'parsers/ValueParser.js',
+				],
+				'dependencies' => [
+					'util.inherit',
+					'valueParsers',
+				],
+			],
+
+		'valueParsers.ValueParserStore' => $moduleTemplate + [
+				'scripts' => [
+					'ValueParserStore.js',
+				],
+				'dependencies' => [
+					'valueParsers',
+				],
+			],
+
+		'valueParsers.parsers' => $moduleTemplate + [
+				'scripts' => [
+					'parsers/BoolParser.js',
+					'parsers/FloatParser.js',
+					'parsers/IntParser.js',
+					'parsers/NullParser.js',
+					'parsers/StringParser.js',
+				],
+				'dependencies' => [
+					'dataValues.values',
+					'util.inherit',
+					'valueParsers.ValueParser',
+				],
+			],
+	];
+} );
