748784395a6ffff338aa87c1eeda664f28558110
Leszek Manicki <leszek.manicki@wikimedia.de>
2017-10-19 17:27:32 +0200

    Install data values js lib as a git submodule, not as a composer dependency
    Bug: T178580
    Change-Id: I4a44c76825c4b9ba446463ecc609f28a80756039

---
+++ view/lib/resources/wikibase-data-values/lib/resources.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @license GPL-2.0+
+ * @author Daniel Werner < daniel.werner@wikimedia.de >
+ * @author H. Snater < mediawiki@snater.com >
+ *
+ * @codeCoverageIgnoreStart
+ */
+return call_user_func( function() {
+	$dir = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR
+		. '..' . DIRECTORY_SEPARATOR . 'wikibase-data-values' . DIRECTORY_SEPARATOR . 'lib';
+
+	preg_match( '+' . preg_quote( DIRECTORY_SEPARATOR ) . '(?:vendor|extensions)'
+		. preg_quote( DIRECTORY_SEPARATOR ) . '.*+', $dir, $remoteExtPath );
+
+	$moduleTemplate = [
+		'localBasePath' => $dir,
+		'remoteExtPath' => '..' . $remoteExtPath[0]
+	];
+
+	return [
+		'globeCoordinate.js' => $moduleTemplate + [
+				'scripts' => [
+					'globeCoordinate/globeCoordinate.js',
+					'globeCoordinate/globeCoordinate.GlobeCoordinate.js',
+				],
+			],
+
+		'util.inherit' => $moduleTemplate + [
+				'scripts' => [
+					'util/util.inherit.js',
+				],
+			],
+	];
+} );
