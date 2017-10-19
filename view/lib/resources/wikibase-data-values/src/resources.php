e1bbd472f3783ce00bbd170fa8eb7eb2ebcf6bcc
Leszek Manicki <leszek.manicki@wikimedia.de>
2017-10-19 17:27:32 +0200

    Install data values js lib as a git submodule, not as a composer dependency
    Bug: T178580
    Change-Id: I4a44c76825c4b9ba446463ecc609f28a80756039

---
+++ view/lib/resources/wikibase-data-values/src/resources.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * @license GPL-2.0+
+ * @author Daniel Werner < daniel.werner@wikimedia.de >
+ * @author H. Snater < mediawiki@snater.com >
+ *
+ * @codeCoverageIgnoreStart
+ */
+return call_user_func( function() {
+	$dir = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR
+		. '..' . DIRECTORY_SEPARATOR . 'wikibase-data-values' . DIRECTORY_SEPARATOR . 'src';
+
+	preg_match( '+' . preg_quote( DIRECTORY_SEPARATOR ) . '(?:vendor|extensions)'
+		. preg_quote( DIRECTORY_SEPARATOR ) . '.*+', $dir, $remoteExtPath );
+
+	$moduleTemplate = [
+		'localBasePath' => $dir,
+		'remoteExtPath' => '..' . $remoteExtPath[0],
+	];
+
+	$modules = [
+		'dataValues' => $moduleTemplate + [
+				'scripts' => [
+					'dataValues.js',
+				],
+			],
+
+		'dataValues.DataValue' => $moduleTemplate + [
+				'scripts' => [
+					'DataValue.js',
+				],
+				'dependencies' => [
+					'dataValues',
+					'util.inherit',
+				],
+			],
+
+		'dataValues.values' => $moduleTemplate + [
+				'scripts' => [
+					// Note: The order here is relevant, scripts should be places after the ones they
+					//  depend on.
+					'values/BoolValue.js',
+					'values/DecimalValue.js',
+					'values/GlobeCoordinateValue.js',
+					'values/MonolingualTextValue.js',
+					'values/MultilingualTextValue.js',
+					'values/StringValue.js',
+					'values/NumberValue.js',
+					'values/TimeValue.js',
+					'values/QuantityValue.js',
+					'values/UnknownValue.js',
+					'values/UnDeserializableValue.js',
+				],
+				'dependencies' => [
+					'dataValues.DataValue',
+					'dataValues.TimeValue',
+					'globeCoordinate.js', // required by GlobeCoordinateValue
+					'util.inherit',
+				],
+			],
+
+		'dataValues.TimeValue' => $moduleTemplate + [
+				'scripts' => [
+					'values/TimeValue.js',
+				],
+				'dependencies' => [
+					'dataValues.DataValue',
+					'util.inherit',
+				],
+			],
+
+	];
+
+	$modules = array_merge(
+		$modules,
+		include __DIR__ . '/valueFormatters/resources.php',
+		include __DIR__ . '/valueParsers/resources.php'
+	);
+
+	return $modules;
+} );
