44746e88e7e30e240959a19500ba64a473435958
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2017-10-23 09:25:45 +0200

    Properly react on unknown languages in LanguageDirectionalityLookup
    Instead of throwing a MWException (which is not documented anywhere, not
    even in Language::factory), return null. This is fine for all callers.
    No caller would do anything with an exception, other than turning it
    into null anyway.
    
    Bug: T138724
    Change-Id: Ia96cdc61947f18409e334b4fa2c1b8589a52a142

---
+++ repo/tests/phpunit/includes/MediaWikiLanguageDirectionalityLookupTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Wikibase\Repo\Tests;
+
+use PHPUnit_Framework_TestCase;
+use Wikibase\Repo\MediaWikiLanguageDirectionalityLookup;
+
+/**
+ * @covers \Wikibase\Repo\MediaWikiLanguageDirectionalityLookup
+ *
+ * @license GPL-2.0+
+ * @author Thiemo Mättig
+ */
+class MediaWikiLanguageDirectionalityLookupTest extends PHPUnit_Framework_TestCase {
+
+	public function provideLanguageCodes() {
+		return [
+			'Known LTR language' => [ 'en', 'ltr' ],
+			'Known RTL language' => [ 'fa', 'rtl' ],
+			'Unknown code' => [ 'unknown', 'ltr' ],
+			'Invalid code' => [ '<invalid>', null ],
+		];
+	}
+
+	/**
+	 * @dataProvider provideLanguageCodes
+	 */
+	public function testGetDirectionality( $languageCode, $expected ) {
+		$lookup = new MediaWikiLanguageDirectionalityLookup();
+		$this->assertSame( $expected, $lookup->getDirectionality( $languageCode ) );
+	}
+
+}
