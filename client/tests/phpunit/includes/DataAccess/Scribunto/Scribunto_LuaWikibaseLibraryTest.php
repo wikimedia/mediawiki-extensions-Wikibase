21da0ad1cf9c6635796086a16a194c5627be9c88
Ladsgroup <Ladsgroup@gmail.com>
2017-11-17 13:42:05 +0000

    xkill - lazy track labels/sitelinks/claims
    This reverts I11676ed, which reverted Iac5ac53.
    
    Bug: T179923
    Change-Id: I6731897b67aa2e1ff996fa0d520e150c5da79a69

--- client/tests/phpunit/includes/DataAccess/Scribunto/Scribunto_LuaWikibaseLibraryTest.php
+++ client/tests/phpunit/includes/DataAccess/Scribunto/Scribunto_LuaWikibaseLibraryTest.php
@@ -70,6 +70,13 @@ class Scribunto_LuaWikibaseLibraryTest extends Scribunto_LuaWikibaseLibraryTestC
 		];
 	}
 
+	public function fineGrainedLuaTrackingProvider() {
+		return [
+			[ true, [ 'Q885588#L', 'Q885588#T' ] ],
+			[ false, [ 'Q32488#X', 'Q885588#L', 'Q885588#T' ] ],
+		];
+	}
+
 	public function testConstructor() {
 		$engine = $this->getEngine();
 		$luaWikibaseLibrary = new Scribunto_LuaWikibaseLibrary( $engine );
@@ -278,8 +285,12 @@ class Scribunto_LuaWikibaseLibraryTest extends Scribunto_LuaWikibaseLibraryTestC
 		$this->assertSame( $allowDataAccessInUserLanguage, $cacheSplit );
 	}
 
-	public function testRenderSnak_languageFallback() {
+	/**
+	 * @dataProvider fineGrainedLuaTrackingProvider
+	 */
+	public function testRenderSnak_languageFallback( $fineGrainedTracking, $expectedUsage ) {
 		$this->setAllowDataAccessInUserLanguage( true );
+		$this->setFineGrainedLuaTracking( $fineGrainedTracking );
 		$cacheSplit = false;
 		$lang = Language::factory( 'ku' );
 
@@ -294,7 +305,8 @@ class Scribunto_LuaWikibaseLibraryTest extends Scribunto_LuaWikibaseLibraryTestC
 
 		// All languages in the fallback chain from 'ku' to 'ku-latn' count as "used".
 		$usage = $luaWikibaseLibrary->getUsageAccumulator()->getUsages();
-		$this->assertSame( [ 'Q32488#X', 'Q885588#L', 'Q885588#T' ], array_keys( $usage ) );
+
+		$this->assertSame( $expectedUsage, array_keys( $usage ) );
 
 		$this->assertSame( true, $cacheSplit );
 	}
@@ -491,6 +503,14 @@ class Scribunto_LuaWikibaseLibraryTest extends Scribunto_LuaWikibaseLibraryTestC
 	private function setAllowDataAccessInUserLanguage( $value ) {
 		$settings = WikibaseClient::getDefaultInstance()->getSettings();
 		$settings->setSetting( 'allowDataAccessInUserLanguage', $value );
+	}
+
+	/**
+	 * @param bool $value
+	 */
+	private function setFineGrainedLuaTracking( $value ) {
+		$settings = WikibaseClient::getDefaultInstance()->getSettings();
+		$settings->setSetting( 'fineGrainedLuaTracking', $value );
 	}
 
 }
