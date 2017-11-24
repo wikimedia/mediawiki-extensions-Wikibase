9cf9490a396a58984be4b7e0bd222cde6bf253a5
Amir Sarabadani <ladsgroup@gmail.com>
2017-11-24 21:07:22 +0100

    Transmit compact diff instead of suppressed diff
    Bug: T113468
    Change-Id: I83c611866750f646a0591f3d4d5c873615af0998

--- lib/includes/Changes/ItemChange.php
+++ lib/includes/Changes/ItemChange.php
@@ -2,8 +2,12 @@
 
 namespace Wikibase;
 
+use Diff\DiffOpAdd;
+use Diff\DiffOpChange;
 use Diff\DiffOp\Diff\Diff;
+use Diff\DiffOpRemove;
 use Wikibase\DataModel\Services\Diff\ItemDiff;
+use Wikibase\Lib\Changes\EntityDiffChangedAspects;
 
 /**
  * @license GPL-2.0+
@@ -16,22 +20,54 @@ class ItemChange extends EntityChange {
 	 * @return Diff
 	 */
 	public function getSiteLinkDiff() {
+		if ( !array_key_exists( 'diff', $this->getInfo() ) ) {
+			if ( !array_key_exists( 'compactDiff', $this->getInfo() ) ) {
+				return $this->sendEmptyDiff( $this );
+			}
+
+			$aspects = $this->getAspectsDiff();
+			if ( !( $aspects instanceof EntityDiffChangedAspects ) ) {
+				return $this->sendEmptyDiff( $aspects );
+			}
+			return $this->getDiffFromSiteLinkChanges( $aspects->getSiteLinkChanges() );
+
+		}
 		$diff = $this->getDiff();
-
 		if ( !( $diff instanceof ItemDiff ) ) {
-			// This shouldn't happen, but we should be robust against corrupt, incomplete
-			// or obsolete instances in the database, etc.
-
-			$cls = $diff === null ? 'null' : get_class( $diff );
-
-			wfLogWarning(
-				'Cannot get sitelink diff from ' . $cls . '. Change #' . $this->getId()
-				. ", type " . $this->getType() );
-
-			return new Diff();
+			return $this->sendEmptyDiff( $diff );
 		} else {
 			return $diff->getSiteLinkDiff();
 		}
+	}
+
+	private function getDiffFromSiteLinkChanges( array $siteLinkChanges ) {
+		$siteLinkDiff = [];
+		foreach ( $siteLinkChanges as $wiki => $change ) {
+			if ( $change[0] === $change[1] ) {
+				continue;
+			}
+			if ( $change[0] === null && $change[1] !== null ) {
+				$siteLinkDiff[$wiki] = [ 'name' => new DiffOpAdd( $change[1] ) ];
+			} elseif ( $change[0] !== null && $change[1] === null ) {
+				$siteLinkDiff[$wiki] = [ 'name' => new DiffOpRemove( $change[0] ) ];
+			} else {
+				$siteLinkDiff[$wiki] = [ 'name' => new DiffOpChange( $change[0], $change[1] ) ];
+			}
+		}
+
+		return new Diff( $siteLinkDiff, true );
+	}
+
+	private function sendEmptyDiff( $obj ) {
+		// This shouldn't happen, but we should be robust against corrupt, incomplete
+		// or obsolete instances in the database, etc.
+
+		$cls = $obj === null ? 'null' : get_class( $obj );
+		wfLogWarning(
+			'Cannot get sitelink diff from ' . $cls . '. Change #' . $this->getId()
+			. ", type " . $this->getType() );
+
+		return new Diff();
 	}
 
 }
