5ff53494d71f4788da4270e2b3cfc2b407d4330b
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2017-12-04 11:24:33 +0100

    Test for HTML anchors on Special:ListDatatypes
    Bug: T181371
    Change-Id: If7e1e18138190c7ee7c24e3cd0d8c5bb322dd360

--- repo/tests/phpunit/includes/Specials/SpecialListDatatypesTest.php
+++ repo/tests/phpunit/includes/Specials/SpecialListDatatypesTest.php
@@ -2,7 +2,6 @@
 
 namespace Wikibase\Repo\Tests\Specials;
 
-use Language;
 use MediaWiki\MediaWikiServices;
 use SpecialPageTestBase;
 use Wikibase\Repo\Specials\SpecialListDatatypes;
@@ -23,9 +22,6 @@ class SpecialListDatatypesTest extends SpecialPageTestBase {
 	protected function setUp() {
 		parent::setUp();
 
-		$this->setMwGlobals( [
-			'wgContLang' => Language::factory( 'qqx' )
-		] );
 		$services = MediaWikiServices::getInstance();
 		$services->resetServiceForTesting( 'TitleFormatter' );
 		$services->resetServiceForTesting( 'TitleParser' );
@@ -48,13 +44,13 @@ class SpecialListDatatypesTest extends SpecialPageTestBase {
 	public function testExecute() {
 		// This also tests that there is no fatal error, that the restriction handling is working
 		// and doesn't block. That is, the default should let the user execute the page.
-		list( $output, ) = $this->executeSpecialPage( '' );
+		list( $output, ) = $this->executeSpecialPage( '', null, 'qqx' );
 
 		$this->assertInternalType( 'string', $output );
 		$this->assertContains( 'wikibase-listdatatypes-summary', $output );
 		$this->assertContains( 'wikibase-listdatatypes-intro', $output );
 
-		$this->assertContains( 'wikibase-item', $output );
+		$this->assertContains( 'id="wikibase-item"', $output );
 		$this->assertContains( 'wikibase-listdatatypes-listproperties', $output );
 		$this->assertContains( 'Special:ListProperties/wikibase-item', $output );
 	}
