5a5f3f347d6d99600a9edb3d9efeeadeab7f161f
Leszek Manicki <leszek.manicki@wikimedia.de>
2017-12-20 18:27:58 +0100

    [WIP] Switch to load extension using MediaWiki's extension registration
    TODOs:
     - travis config not tested
     - default/example values of sharedCacheKeyPrefix
       setting?!
    
    Bug: T88258
    Change-Id: I4dcf695423bbbd5469337ee84051930b87ab2c2b

--- view/lib/i18n.php
+++ view/lib/i18n.php
@@ -4,10 +4,7 @@
  * @license GPL-2.0+
  */
 return call_user_func( function() {
-	global $wgMessagesDirs;
-
-	$wgMessagesDirs['WikibaseView'] = array_merge(
-		$wgMessagesDirs['WikibaseView'],
-		[ __DIR__ . '/wikibase-data-values-value-view/i18n' ]
-	);
+	return [
+		__DIR__ . '/wikibase-data-values-value-view/i18n',
+	];
 } );
