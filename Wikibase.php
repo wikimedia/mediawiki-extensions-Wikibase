ec5a683ec9a7280a562a4601d58aa85f322e9b47
Leszek Manicki <leszek.manicki@wikimedia.de>
2017-12-20 18:27:58 +0100

    [WIP][DNM] Switch to load extension using MediaWiki's extension registration
    TODOs:
     - do not write down class/method names in JSON file, have
       everything in the code? (easier maintenance)
     - what about View?
     - travis config
     - remove wikibase.php entry point?
     - remove wgEnableWikibaseX globals?
     - default/example values of sharedCacheKeyPrefix
       setting is currently broken?!
    
    Bug: TODO
    Change-Id: I4dcf695423bbbd5469337ee84051930b87ab2c2b

--- Wikibase.php
+++ Wikibase.php
@@ -30,9 +30,9 @@
  *
  * @license GPL-2.0+
  */
-
 if ( !array_key_exists( 'wgEnableWikibaseRepo', $GLOBALS ) || $GLOBALS['wgEnableWikibaseRepo'] ) {
-	require_once __DIR__ . '/repo/Wikibase.php';
+	wfLoadExtension( 'WikibaseLib', __DIR__ . '/lib/extension.json' );
+	wfLoadExtension( 'WikibaseRepo', __DIR__ . '/repo/extension.json' );
 
 	if ( isset( $wgWikimediaJenkinsCI ) && $wgWikimediaJenkinsCI == true ) {
 		// Use example config for testing
@@ -41,7 +41,8 @@ if ( !array_key_exists( 'wgEnableWikibaseRepo', $GLOBALS ) || $GLOBALS['wgEnable
 }
 
 if ( !array_key_exists( 'wgEnableWikibaseClient', $GLOBALS ) || $GLOBALS['wgEnableWikibaseClient'] ) {
-	require_once __DIR__ . '/client/WikibaseClient.php';
+	wfLoadExtension( 'WikibaseLib', __DIR__ . '/lib/extension.json' );
+	wfLoadExtension( 'Wikibase Client', __DIR__ . '/client/extension.json' );
 
 	if ( isset( $wgWikimediaJenkinsCI ) && $wgWikimediaJenkinsCI == true ) {
 		// Use example config for testing
