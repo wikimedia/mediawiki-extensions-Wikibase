2bf21e11f7b002d1f8a557b5eefa721d8d3f12b4
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2018-01-26 17:49:38 +0100

    Remove all not-needed & references from hook handler functions
    In some cases the variable is not even used. In many cases the variable
    is used, but exclusively read, and not changed. The & does not do
    anything in these cases but:
    * It makes the code look more complex than it is.
    * We want to remove some of these problematic pass-by-ref ampersands
      from Hooks::run() calls, but this can only be done if no hook handler
      *expects* stuff to be passed by reference any more.
    
    Change-Id: Iaaf923e10814dbab086dc795d4ede6323e527a7d

--- lib/WikibaseLib.hooks.php
+++ lib/WikibaseLib.hooks.php
@@ -33,11 +33,11 @@ final class LibHooks {
 	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/ResourceLoaderTestModules
 	 *
 	 * @param array &$testModules
-	 * @param ResourceLoader &$resourceLoader
+	 * @param ResourceLoader $resourceLoader
 	 *
 	 * @return boolean
 	 */
-	public static function registerQUnitTests( array &$testModules, ResourceLoader &$resourceLoader ) {
+	public static function registerQUnitTests( array &$testModules, ResourceLoader $resourceLoader ) {
 		$testModules['qunit'] = array_merge(
 			$testModules['qunit'],
 			include __DIR__ . '/tests/qunit/resources.php'
