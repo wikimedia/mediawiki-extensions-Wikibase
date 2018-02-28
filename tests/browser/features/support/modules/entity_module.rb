2e5f066aed27ca26781ad0318e46f937b05e7303
Leszek Manicki <leszek.manicki@wikimedia.de>
2018-02-28 17:33:16 +0100

    Browser tests: fix the bug in entity_module::page_index_updated
    Bug: T188507
    Change-Id: I6f5f8bea2556189107a8779f774309fc527c6d4e

--- tests/browser/features/support/modules/entity_module.rb
+++ tests/browser/features/support/modules/entity_module.rb
@@ -151,7 +151,7 @@ module EntityPage
 
   def page_index_updated(api, page_titles)
     response = api.action(:query, prop: 'cirrusdoc|revisions', titles: page_titles.join('|'), rvprop: 'ids', token_type: false)
-    response['query']['pages'].each_value do |_, page_data|
+    response['query']['pages'].each_value do |page_data|
       if !page_data.key?('cirrusdoc') || !page_data.key?('revisions')
         return false
       end
