f6e348a2c960e00ed6a74ed29b4428525c465b7d
Timo Tijhof <krinklemail@gmail.com>
2018-03-13 17:25:23 -0700

    [WIP] Module registration: Keep global var handling central
    For the most part, it was already working this way; each resource file
    returns an array to be merged by the includer into a larger array, until
    eventually at the end it is add it all to the wgResourceModules and
    wgMessagesDirs global variables from the main entry point file of
    one of the four virtual MediaWiki extensions:
    
    - Wikibase Client (client/WikibaseClient.php)
    - Wikibase Repo (repo/Wikibase.php)
    - Wikibase Lib (lib/WikibaseLib.php)
    - Wikibase View (view/WikibaseView.php)
    
    However, there were a few spurious cases in view/ that were interacting
    with these globals directly. This commit fixes those to more closely
    resemble the other four virtual extensions.
    
    * Convert to array-returning files.
    * Moving the final merge to the entry point (view/WikibaseView.php).
    * Remove view/init.mw.php (essentially renamed to view/WikibaseView.php).
    
    Bug: T189538
    Change-Id: I2cd1db24b3e2d174b4262ddff69445e89bc064e5

--- view/WikibaseView.php
+++ view/WikibaseView.php
@@ -15,8 +15,83 @@ if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
 // Load autoload info as long as extension classes are not PSR-4-autoloaded
 require_once __DIR__  . '/autoload.php';
 
-if ( defined( 'MEDIAWIKI' ) ) {
-	call_user_func( function() {
-		require_once __DIR__ . '/init.mw.php';
-	} );
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die( 'Not an entry point.' );
 }
+
+$GLOBALS['wgExtensionCredits']['wikibase'][] = [
+	'path' => __DIR__,
+	'name' => 'Wikibase View',
+	'author' => [
+		'The Wikidata team',
+	],
+	'url' => 'https://phabricator.wikimedia.org/diffusion/EWBA/browse/master/view/',
+	'description' => 'View component for the Wikibase Repository',
+	'license-name' => 'GPL-2.0-or-later'
+];
+
+$GLOBALS['wgResourceModules'] = array_merge(
+		$GLOBALS['wgResourceModules'],
+		include __DIR__ . '/lib/resources.php',
+		include __DIR__ . '/resources/resources.php'
+);
+
+$GLOBALS['wgHooks']['ResourceLoaderTestModules'][] = function(
+	array &$testModules,
+	ResourceLoader &$resourceLoader
+) {
+	$testModules['qunit'] = array_merge(
+		$testModules['qunit'],
+		include __DIR__ . '/lib/resources.test.php',
+		include __DIR__ . '/tests/qunit/resources.php'
+	);
+};
+
+$GLOBALS['wgMessagesDirs']['WikibaseView'] = include __DIR__ . '/lib/i18n.php';
+
+$GLOBALS['wgHooks']['UnitTestsList'][] = function( array &$paths ) {
+	$paths[] = __DIR__ . '/tests/phpunit';
+};
+
+/**
+ * Register ResourceLoader modules with dynamic dependencies.
+ *
+ * @param ResourceLoader $resourceLoader
+ *
+ * @return bool
+ */
+$GLOBALS['wgHooks']['ResourceLoaderRegisterModules'][] = function( ResourceLoader $resourceLoader ) {
+	preg_match( '+' . preg_quote( DIRECTORY_SEPARATOR ) . '(?:vendor|extensions)'
+		. preg_quote( DIRECTORY_SEPARATOR ) . '.*+', __DIR__, $remoteExtPath );
+
+	$moduleTemplate = [
+		'localBasePath' => __DIR__,
+		'remoteExtPath' => '..' . $remoteExtPath[0],
+	];
+
+	$modules = [
+		'jquery.util.getDirectionality' => $moduleTemplate + [
+			'scripts' => [
+				'resources/jquery/jquery.util.getDirectionality.js',
+			],
+		],
+		'wikibase.getLanguageNameByCode' => $moduleTemplate + [
+			'scripts' => [
+				'resources/wikibase/wikibase.getLanguageNameByCode.js',
+			],
+			'dependencies' => [
+				'wikibase',
+			],
+		],
+	];
+
+	$isUlsLoaded = ExtensionRegistry::getInstance()->isLoaded( 'UniversalLanguageSelector' );
+	if ( $isUlsLoaded ) {
+		$modules['jquery.util.getDirectionality']['dependencies'][] = 'ext.uls.mediawiki';
+		$modules['wikibase.getLanguageNameByCode']['dependencies'][] = 'ext.uls.mediawiki';
+	}
+
+	$resourceLoader->register( $modules );
+
+	return true;
+};
