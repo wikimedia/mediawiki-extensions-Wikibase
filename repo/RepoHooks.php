54b17c481098834723a098a15c44774a23f0bdc1
Stanislav Malyshev <smalyshev@gmail.com>
2018-03-20 14:11:22 -0700

    Actually enable the query builder hook
    Without it, the code still uses old query builder.
    
    Bug: T178851
    Change-Id: I32834113b38218628aae55cd7245bb122f69cd49

--- repo/RepoHooks.php
+++ repo/RepoHooks.php
@@ -1131,7 +1131,7 @@ final class RepoHooks {
 			EntitySearchElastic::CONTEXT_WIKIBASE_FULLTEXT, 'cirrusWBProfile' );
 
 		// Determine the default rescore profile to use for fulltext search
-		$defaultFTRescore = EntitySearchElastic::DEFAULT_RESCORE_PROFILE;
+		$defaultFTRescore = 'wikibase';
 		if ( isset( $entitySearchConfig['defaultFulltextRescoreProfile'] ) ) {
 			// If set in config use it
 			$defaultFTRescore = $entitySearchConfig['defaultFulltextRescoreProfile'];
