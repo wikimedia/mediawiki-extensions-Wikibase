0ba25d17c7013554a778e9b7156a83b1da6ef5bb
Jakob Warkotsch <j.warkotsch@gmail.com>
2018-04-04 12:40:53 +0200

    Introduce ViewFactoryFactory overridable by extensions.
    The newly introduced ViewFactoryFactory can be overridden by
    extensions which allows them to change the behavior of the
    ViewFactory independent of whether the page is editable or not.
    
    Bug: T190921
    Change-Id: Ica2a13d2f09af7027fe8dcdb5c5a07129407759f

---
+++ view/tests/qunit/wikibase/view/ViewFactoryFactory.tests.js
@@ -0,0 +1,57 @@
+( function ( $, sinon, QUnit, wb, ViewFactoryFactory ) {
+	'use strict';
+
+	QUnit.module( 'wikibase.view.ViewFactoryFactory' );
+
+	QUnit.test( 'returns ControllerViewFactory when editable', function ( assert ) {
+		assert.expect( 2 );
+		var factory = new ViewFactoryFactory(),
+			controllerViewStub = sinon.stub( wikibase.view, 'ControllerViewFactory' ),
+			result = factory.getViewFactory( true, [] );
+
+		sinon.assert.calledWithNew( controllerViewStub );
+		assert.ok( result instanceof controllerViewStub );
+
+		controllerViewStub.restore();
+	});
+
+
+	QUnit.test( 'returns ReadModeViewFactory when not editable', function ( assert ) {
+		assert.expect( 2 );
+		var factory = new ViewFactoryFactory(),
+			readModeViewFactory = sinon.stub( wikibase.view, 'ReadModeViewFactory' ),
+			result = factory.getViewFactory( false, [] );
+
+		sinon.assert.calledWithNew( readModeViewFactory );
+		assert.ok( result instanceof readModeViewFactory );
+
+		readModeViewFactory.restore();
+	});
+
+	QUnit.test( 'ControllerViewFactory is called with correct arguments', function ( assert ) {
+		assert.expect( 1 );
+		var factory = new ViewFactoryFactory(),
+			controllerViewStub = sinon.stub( wikibase.view, 'ControllerViewFactory' );
+
+		factory.getViewFactory( true, [ 1, 2, 3 ] );
+
+		assert.ok( controllerViewStub.calledWith( 1, 2, 3 ) );
+
+		controllerViewStub.restore();
+	} );
+
+
+
+	QUnit.test( 'ReadModeViewFactory is called with correct arguments', function ( assert ) {
+		assert.expect( 1 );
+		var factory = new ViewFactoryFactory(),
+			readModeViewStub = sinon.stub( wikibase.view, 'ReadModeViewFactory' );
+
+		factory.getViewFactory( false, [ 1, 2, 3 ] );
+
+		assert.ok( readModeViewStub.calledWith( 3 ) );
+
+		readModeViewStub.restore();
+	} );
+
+}( jQuery, sinon, QUnit, wikibase, wikibase.view.ViewFactoryFactory ) );
