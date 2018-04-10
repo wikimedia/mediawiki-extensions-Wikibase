69f412776c36b744880f0e4bee4574daa2af817c
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2018-04-10 17:50:59 +0200

    Add maintenance script to clear term_search_key+term_weight
    This maintenance script can be used to clear the term_search_key and
    term_weight columns of the wb_terms database if they’re not being used
    anymore. The rebuildTermSqlIndex with the --rebuild-all-terms option
    enabled has the same effect if the repo is configured not to use the
    search keys, but this script is much simpler and more efficient for its
    single purpose.
    
    Based on rebuildTermSqlIndex and TermSqlIndexBuilder, but simplified.
    For example, we don’t need an SqlEntityIdPager – we implement paging on
    the term_row_id ourselves.
    
    Bug: T191631
    Change-Id: I2c539a39f6a6a206e46b7ef98e3eabafc7d758e2

---
+++ repo/includes/Store/Sql/TermSqlIndexSearchFieldsClearer.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace Wikibase;
+
+use Wikibase\Lib\Reporting\MessageReporter;
+use Wikibase\Lib\Reporting\NullMessageReporter;
+use Wikimedia\Assert\Assert;
+use Wikimedia\Rdbms\IDatabase;
+use Wikimedia\Rdbms\LBFactory;
+
+/**
+ * Clears search-related fields in the SQL terms table.
+ *
+ * @license GPL-2.0-or-later
+ * @author Lucas Werkmeister
+ */
+class TermSqlIndexSearchFieldsClearer {
+
+	const TABLE_NAME = 'wb_terms';
+
+	/**
+	 * @var LBFactory
+	 */
+	private $loadBalancerFactory;
+
+	/**
+	 * @var int
+	 */
+	private $sleep;
+
+	/**
+	 * @var MessageReporter
+	 */
+	private $progressReporter;
+
+	/**
+	 * @var MessageReporter
+	 */
+	private $errorReporter;
+
+	/**
+	 * @var int
+	 */
+	private $batchSize = 1000;
+
+	/**
+	 * @var int|null
+	 */
+	private $fromId = null;
+
+	/**
+	 * @param LBFactory $loadBalancerFactory
+	 * @param int $sleep Sleep time between each batch
+	 */
+	public function __construct(
+		LBFactory $loadBalancerFactory,
+		$sleep = 0
+	) {
+		$this->loadBalancerFactory = $loadBalancerFactory;
+		$this->sleep = $sleep;
+
+		$this->progressReporter = new NullMessageReporter();
+		$this->errorReporter = new NullMessageReporter();
+	}
+
+	public function setProgressReporter( MessageReporter $reporter ) {
+		$this->progressReporter = $reporter;
+	}
+
+	public function setErrorReporter( MessageReporter $reporter ) {
+		$this->errorReporter = $reporter;
+	}
+
+	/**
+	 * @param int $size
+	 */
+	public function setBatchSize( $size ) {
+		Assert::parameterType( 'integer', $size, 'size' );
+
+		$this->batchSize = $size;
+	}
+
+	/**
+	 * @param int $fromId
+	 */
+	public function setFromId( $fromId ) {
+		Assert::parameterType( 'integer', $fromId, 'fromId' );
+
+		$this->fromId = $fromId;
+	}
+
+	public function clear() {
+		$ticket = $this->loadBalancerFactory->getEmptyTransactionTicket( __METHOD__ );
+		$loadBalancer = $this->loadBalancerFactory->getMainLB();
+
+		while ( true ) {
+			$dbr = $loadBalancer->getConnection( DB_REPLICA );
+			$dbw = $loadBalancer->getConnection( DB_MASTER );
+
+			$lastRow = $this->clearBatch( $dbr, $dbw, (int)$this->fromId, $this->batchSize );
+
+			$loadBalancer->reuseConnection( $dbw );
+			$loadBalancer->reuseConnection( $dbr );
+
+			$this->loadBalancerFactory->commitAndWaitForReplication( __METHOD__, $ticket );
+
+			if ( $lastRow === false ) {
+				break;
+			} else {
+				$this->fromId = $lastRow + 1;
+				$this->progressReporter->reportMessage( 'Cleared up to row ' . $lastRow );
+			}
+
+			if ( $this->sleep > 0 ) {
+				sleep( $this->sleep );
+			}
+		}
+
+		$this->progressReporter->reportMessage( 'Done clearing search fields' );
+	}
+
+	/**
+	 * @param IDatabase $dbr database connection for reading
+	 * @param IDatabase $dbw database connection for writing
+	 * @param int $fromId start with this row ID
+	 * @param int $batchSize clear up to this many rows
+	 * @return int|bool the last row ID processed, or false if there were no rows left to clear
+	 */
+	public function clearBatch( IDatabase $dbr, IDatabase $dbw, $fromId, $batchSize ) {
+		$rows = $dbr->selectFieldValues(
+			self::TABLE_NAME,
+			'term_row_id',
+			[
+				'term_row_id >= ' . (int)$fromId,
+			],
+			__METHOD__,
+			[
+				'ORDER BY term_row_id',
+				'LIMIT' => (int)$batchSize,
+			]
+		);
+
+		if ( $rows !== [] ) {
+			$dbw->update(
+				self::TABLE_NAME,
+				[
+					'term_search_key' => '',
+					'term_weight' => 0.0,
+				],
+				[
+					'term_row_id' => $rows,
+				],
+				__METHOD__
+			);
+
+			return (int)end( $rows );
+		} else {
+			return false;
+		}
+	}
+
+}
