af92829c92992afeca8e3d41d8f190b6f483acbd
Cormac Parle <cparle@wikimedia.org>
2018-04-19 16:04:54 +0100

    Add haswbstatement CirrusSearch query feature
    Add haswbstatement feature to allow searching the `statement_keywords` field in the search index
    
    Bug: T192345
    Change-Id: I179233a91c937227c13eab0dfba7b552d7661e05

---
+++ repo/includes/Search/Elastic/Query/HasWbStatementFeature.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace Wikibase\Repo\Search\Elastic\Query;
+
+use CirrusSearch\Query\SimpleKeywordFeature;
+use CirrusSearch\Search\SearchContext;
+use CirrusSearch\WarningCollector;
+use Elastica\Query\BoolQuery;
+use Elastica\Query\Match;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\Repo\Search\Elastic\Fields\StatementsField;
+
+/**
+ * Handles the search keyword 'haswbstatement:'
+ *
+ * Allows the user to search for pages/items that have wikibase statements associated with them.
+ *
+ * If a file page has the statement 'wikidata:P180=wikidata:Q527' (meaning 'depicts sky') associated
+ * with it then it can be found by including 'haswbstatement:wikidata:P180=wikidata:Q527' in the
+ * search query.
+ *
+ * If a file page has the statement 'wikidata:P2014=79802' (meaning 'MoMA artwork id 79802')
+ * associated with it then it can be found by including 'haswbstatement:wikidata:P2014=79802' in the
+ * search query.
+ *
+ * Statements can be combined using logical OR by separating them with a | character in a single
+ * haswbstatement query e.g. 'haswbstatement:P999=Q888|P999=Q777'
+ *
+ * Statements can be combined using logical AND by using two separate haswbstatement queries e.g.
+ * 'haswbstatement:P999=Q888 haswbstatement:P999=Q777'
+ *
+ * Note that NOT ALL STATEMENTS ARE INDEXED. Searching for a statement about a property that has
+ * not been indexed will give an empty result set.
+ *
+ * @uses CirrusSearch
+ * @todo Move to WikibaseCirrusSearch extension
+ * @see https://phabricator.wikimedia.org/T190022
+ */
+class HasWbStatementFeature extends SimpleKeywordFeature {
+
+	/**
+	 * @var array Names of foreign repos from $wgWBRepoSettings
+	 */
+	private $foreignRepoNames;
+
+	/**
+	 * HasWbStatementFeature constructor.
+	 * @param array $foreignRepoNames Array of names of foreign repos from $wgWBRepoSettings
+	 */
+	public function __construct( array $foreignRepoNames ) {
+		$this->foreignRepoNames = $foreignRepoNames;
+	}
+
+	/**
+	 * @return string[]
+	 */
+	protected function getKeywords() {
+		return [ 'haswbstatement' ];
+	}
+
+	/**
+	 * @param SearchContext $context
+	 * @param string $key The keyword
+	 * @param string $value The value attached to the keyword with quotes stripped
+	 * @param string $quotedValue The original value in the search string, including quotes if used
+	 * @param bool $negated Is the search negated? Not used to generate the returned AbstractQuery,
+	 *  that will be negated as necessary. Used for any other building/context necessary.
+	 * @return array Two element array, first an AbstractQuery or null to apply to the
+	 *  query. Second a boolean indicating if the quotedValue should be kept in the search
+	 *  string.
+	 */
+	protected function doApply( SearchContext $context, $key, $value, $quotedValue, $negated ) {
+		$parsedValue = $this->parseValue(
+			$key,
+			$value,
+			$quotedValue,
+			'',
+			'',
+			$context
+		);
+		if ( count( $parsedValue[ 'statements' ] ) == 0 ) {
+			$context->setResultsPossible( false );
+			return [ null, false ];
+		}
+
+		return [ $this->matchStatements( $parsedValue[ 'statements' ] ), false ];
+	}
+
+	/**
+	 * Builds an OR between many statements about the wikibase item
+	 *
+	 * @param string[] $statements statements to match
+	 * @return \Elastica\Query\BoolQuery
+	 */
+	private function matchStatements( array $statements ) {
+		$query = new BoolQuery();
+		foreach ( $statements as $statement ) {
+			$query->addShould( new Match( StatementsField::NAME, [ 'query' => $statement ] ) );
+		}
+		return $query;
+	}
+
+	/**
+	 * @param string $key
+	 * @param string $value
+	 * @param string $quotedValue
+	 * @param string $valueDelimiter
+	 * @param string $suffix
+	 * @param WarningCollector $warningCollector
+	 * @return array|false|null
+	 */
+	public function parseValue(
+		$key,
+		$value,
+		$quotedValue,
+		$valueDelimiter,
+		$suffix,
+		WarningCollector $warningCollector
+	) {
+		$validStatements = [];
+		$statementStrings = explode( '|', $value );
+		foreach ( $statementStrings as $statementString ) {
+			if ( $this->isStatementStringValid( $statementString ) ) {
+				$validStatements[] = $statementString;
+			}
+		}
+		if ( count( $validStatements ) == 0 ) {
+			$warningCollector->addWarning(
+				'cirrussearch-haswbstatement-feature-no-valid-statements',
+				$key
+			);
+		}
+		return [ 'statements' => $validStatements ];
+	}
+
+	/**
+	 * Check that a statement string is valid. A valid string is a P-id followed by an equals and
+	 * optionally suffixed with a foreign repo name followed by a colon
+	 *
+	 * The following strings are valid:
+	 * Wikidata:P180=Wikidata:Q537 (assuming 'Wikidata' is a valid foreign repo name)
+	 * P2014=79802
+	 *
+	 * The following strings are invalid:
+	 * NON_EXISTENT_FOREIGN_REPO_NAME:P123=Wikidata:Q537
+	 * PA=Q888
+	 * PF=1234567
+	 *
+	 * @param $statementString
+	 * @return bool
+	 */
+	private function isStatementStringValid( $statementString ) {
+		//Strip delimiters, anchors and pattern modifiers from PropertyId::PATTERN
+		$propertyIdPattern = preg_replace(
+			'/([^\sa-zA-Z0-9\\\])(\^|\\\A)?(.*?)(\$|\\\z|\\\Z)?\\1[a-zA-Z]*/',
+			'$3',
+			PropertyId::PATTERN
+		);
+		$validStatementStringPattern = '/^' .
+			'((' . implode( '|', $this->foreignRepoNames ) .'):)?' .
+			$propertyIdPattern .
+			StatementsField::STATEMENT_SEPARATOR .
+			'/i';
+
+		return (bool)preg_match(
+			$validStatementStringPattern,
+			$statementString
+		);
+	}
+
+}
---
+++ repo/tests/phpunit/includes/Search/Elastic/Query/HasWbStatementFeatureTest.php
@@ -0,0 +1,237 @@
+<?php
+
+namespace Wikibase\Repo\Tests\Search\Elastic\Query;
+
+use CirrusSearch\Query\BaseSimpleKeywordFeatureTest;
+use CirrusSearch\Search\SearchContext;
+use Wikibase\Repo\Search\Elastic\Query\HasWbStatementFeature;
+
+/**
+ * @covers \Wikibase\Repo\Search\Elastic\Query\HasWbStatementFeature
+ *
+ * @group WikibaseElastic
+ * @group Wikibase
+ */
+class HasWbStatementFeatureTest extends BaseSimpleKeywordFeatureTest {
+
+	public function applyProvider() {
+		return [
+			'single statement entity' => [
+				'expected' => [ 'bool' => [
+					'should' => [
+						[ 'match' => [
+							'statement_keywords' => [
+								'query' => 'P999=Q888',
+							],
+						] ]
+					]
+				] ],
+				'search string' => 'haswbstatement:P999=Q888',
+				'foreignRepoNames' => [],
+			],
+			'single statement string' => [
+				'expected' => [ 'bool' => [
+					'should' => [
+						[ 'match' => [
+							'statement_keywords' => [
+								'query' => 'P999=12345',
+							],
+						] ]
+					]
+				] ],
+				'search string' => 'haswbstatement:P999=12345',
+				'foreignRepoNames' => [],
+			],
+			'single statement federated' => [
+				'expected' => [ 'bool' => [
+					'should' => [
+						[ 'match' => [
+							'statement_keywords' => [
+								'query' => 'Federated:P999=Federated:Q888',
+							],
+						] ]
+					]
+				] ],
+				'search string' => 'haswbstatement:Federated:P999=Federated:Q888',
+				'foreignRepoNames' => [ 'Federated' ],
+			],
+			'multiple statements' => [
+				'expected' => [ 'bool' => [
+					'should' => [
+						[ 'match' => [
+							'statement_keywords' => [
+								'query' => 'Federated:P999=Q888',
+							],
+						] ],
+						[ 'match' => [
+							'statement_keywords' => [
+								'query' => 'P777=someString',
+							],
+						] ]
+					]
+				] ],
+				'search string' => 'haswbstatement:Federated:P999=Q888|P777=someString',
+				'foreignRepoNames' => [ 'Federated' ],
+			],
+			'some data invalid' => [
+				'expected' => [ 'bool' => [
+					'should' => [
+						[ 'match' => [
+							'statement_keywords' => [
+								'query' => 'P999=Q888',
+							],
+						] ],
+					]
+				] ],
+				'search string' => 'haswbstatement:INVALID|P999=Q888',
+				'foreignRepoNames' => [],
+			],
+			'invalid foreign repo name rejected' => [
+				'expected' => [ 'bool' => [
+					'should' => [
+						[ 'match' => [
+							'statement_keywords' => [
+								'query' => 'Federated:P999=Q888',
+							],
+						] ],
+					]
+				] ],
+				'search string' => 'haswbstatement:INVALID_FOREIGN_REPO:P999=P777|' .
+					'Federated:P999=Q888',
+				'foreignRepoNames' => [ 'Federated' ],
+			],
+			'all data invalid' => [
+				'expected' => null,
+				'search string' => 'haswbstatement:INVALID',
+				'foreignRepoNames' => [],
+			],
+		];
+	}
+
+	/**
+	 * @dataProvider applyProvider
+	 */
+	public function testApply( array $expected = null, $term, $foreignRepoNames ) {
+		$context = $this->mockContextExpectingAddFilter( $expected );
+		$context->expects( $this->exactly(
+				$expected === null ? 1 : 0
+			) )
+			->method( 'setResultsPossible' )
+			->with( false );
+
+		$feature = new HasWbStatementFeature( $foreignRepoNames );
+		$feature->apply( $context, $term );
+	}
+
+	public function applyNoDataProvider() {
+		return [
+			'empty data' => [
+				null,
+				'haswbstatement:',
+			],
+			'no data' => [
+				null,
+				'',
+			],
+		];
+	}
+
+	/**
+	 * @dataProvider applyNoDataProvider
+	 */
+	public function testApplyNoData( array $expected = null, $term ) {
+		$context = $this->mockContextExpectingAddFilter( $expected );
+
+		$feature = new HasWbStatementFeature( [ 'P999' ] );
+		$feature->apply( $context, $term );
+	}
+
+	public function testInvalidStatementWarning() {
+		$this->assertWarnings(
+			new HasWbStatementFeature( [ 'P999' ] ),
+			[ [ 'cirrussearch-haswbstatement-feature-no-valid-statements', 'haswbstatement' ] ],
+			'haswbstatement:INVALID'
+		);
+	}
+
+	/**
+	 * @dataProvider parseProvider
+	 */
+	public function testParseValue( $foreignRepoNames, $value, $expected, $warningExpected ) {
+		$warningCollector = $this->getMockBuilder( SearchContext::class )
+			->disableOriginalConstructor()
+			->getMock();
+		$warningCollector->expects( $warningExpected ? $this->once() : $this->never() )
+			->method( 'addWarning' );
+
+		$feature = new HasWbStatementFeature( $foreignRepoNames );
+		$parsedValue = $feature->parseValue(
+			'',
+			$value,
+			'',
+			'',
+			'',
+			$warningCollector
+		);
+		$this->assertEquals( [ 'statements' => $expected ], $parsedValue );
+	}
+
+	public function parseProvider() {
+		return [
+			'empty value' => [
+				'foreignRepoNames' => [],
+				'value' => '',
+				'expected' => [],
+				'warningExpected' => true,
+			],
+			'invalid value' => [
+				'foreignRepoNames' => [],
+				'value' => 'xyz=12345',
+				'expected' => [],
+				'warningExpected' => true,
+			],
+			'invalid federated value' => [
+				'foreignRepoNames' => [ 'Wikidata' ],
+				'value' => 'Wikisource:P123=12345',
+				'expected' => [],
+				'warningExpected' => true,
+			],
+			'single value Q-id' => [
+				'foreignRepoNames' => [],
+				'value' => 'P999=Q888',
+				'expected' => [ 'P999=Q888' ],
+				'warningExpected' => false,
+			],
+			'single value other id' => [
+				'foreignRepoNames' => [],
+				'value' => 'P999=AB123',
+				'expected' => [ 'P999=AB123' ],
+				'warningExpected' => false,
+			],
+			'single value federated' => [
+				'foreignRepoNames' => [ 'Wikidata' ],
+				'value' => 'Wikidata:P999=Wikidata:Q888',
+				'expected' => [ 'Wikidata:P999=Wikidata:Q888' ],
+				'warningExpected' => false,
+			],
+			'multiple values' => [
+				'foreignRepoNames' => [ 'Wikidata', 'Wikisource' ],
+				'value' => 'Wikidata:P999=Wikidata:Q888|Wikisource:P777=12345',
+				'expected' => [
+					'Wikidata:P999=Wikidata:Q888',
+					'Wikisource:P777=12345',
+				],
+				'warningExpected' => false,
+			],
+			'multiple values, not all valid' => [
+				'foreignRepoNames' => [ 'Wikidata' ],
+				'value' => 'Wikidata:P999=Wikidata:Q888|Wikisource:P777=12345',
+				'expected' => [
+					'Wikidata:P999=Wikidata:Q888',
+				],
+				'warningExpected' => false,
+			],
+		];
+	}
+
+}
