6630c60310a7973a18fb1a867cfc41c48042a33e
Cormac Parle <cparle@wikimedia.org>
2018-05-02 14:50:20 +0100

    Skip tests if cirrus search not present
    Bug: T192345
    Change-Id: I99d5765e980f37b62de06d585e9706752b0ed8bb

--- repo/tests/phpunit/includes/Search/Elastic/Query/HasWbStatementFeatureTest.php
+++ repo/tests/phpunit/includes/Search/Elastic/Query/HasWbStatementFeatureTest.php
@@ -2,8 +2,12 @@
 
 namespace Wikibase\Repo\Tests\Search\Elastic\Query;
 
-use CirrusSearch\Query\BaseSimpleKeywordFeatureTest;
+use CirrusSearch\InterwikiResolver;
+use CirrusSearch\Query\KeywordFeature;
+use CirrusSearch\Search\Escaper;
 use CirrusSearch\Search\SearchContext;
+use CirrusSearch\SearchConfig;
+use MediaWiki\MediaWikiServices;
 use Wikibase\Repo\Search\Elastic\Query\HasWbStatementFeature;
 
 /**
@@ -12,7 +16,62 @@ use Wikibase\Repo\Search\Elastic\Query\HasWbStatementFeature;
  * @group WikibaseElastic
  * @group Wikibase
  */
-class HasWbStatementFeatureTest extends BaseSimpleKeywordFeatureTest {
+class HasWbStatementFeatureTest extends \MediaWikiTestCase {
+
+	public function setUp() {
+		parent::setUp();
+
+		if ( !class_exists( \CirrusSearch::class ) ) {
+			$this->markTestSkipped( 'CirrusSearch needed.' );
+		} else {
+			MediaWikiServices::getInstance()
+				->resetServiceForTesting( InterwikiResolver::SERVICE );
+		}
+	}
+
+	/**
+	 * @return SearchContext
+	 */
+	protected function mockContext() {
+		$context = $this->getMockBuilder( SearchContext::class )
+			->disableOriginalConstructor()
+			->getMock();
+		$context->expects( $this->any() )->method( 'getConfig' )->willReturn( new SearchConfig() );
+		$context->expects( $this->any() )->method( 'escaper' )->willReturn(
+			new Escaper( 'en', true )
+		);
+
+		return $context;
+	}
+
+	protected function mockContextExpectingAddFilter( array $expectedQuery = null ) {
+		$context = $this->mockContext();
+		if ( $expectedQuery === null ) {
+			$context->expects( $this->never() )
+				->method( 'addFilter' );
+		} else {
+			$context->expects( $this->once() )
+				->method( 'addFilter' )
+				->with( $this->callback( function ( $query ) use ( $expectedQuery ) {
+					$this->assertEquals( $expectedQuery, $query->toArray() );
+					return true;
+				} ) );
+		}
+
+		return $context;
+	}
+
+	protected function assertWarnings( KeywordFeature $feature, $expected, $term ) {
+		$warnings = [];
+		$context = $this->mockContext();
+		$context->expects( $this->any() )
+			->method( 'addWarning' )
+			->will( $this->returnCallback( function () use ( &$warnings ) {
+				$warnings[] = array_filter( func_get_args() );
+			} ) );
+		$feature->apply( $context, $term );
+		$this->assertEquals( $expected, $warnings );
+	}
 
 	public function applyProvider() {
 		return [
