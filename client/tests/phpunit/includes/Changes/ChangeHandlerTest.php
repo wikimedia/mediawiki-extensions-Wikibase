cd623b19b6300743538668fed77d12344c1e3009
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2018-05-29 17:23:23 +0200

    Don’t use NS_MAIN as default function argument
    This seems to cause errors in CI.
    
    Bug: T195868
    Change-Id: I09a9ca53f7438000e3e50334875b03081daf8cb6

--- client/tests/phpunit/includes/Changes/ChangeHandlerTest.php
+++ client/tests/phpunit/includes/Changes/ChangeHandlerTest.php
@@ -240,7 +240,7 @@ class ChangeHandlerTest extends MediaWikiTestCase {
 
 		$titleFactory->expects( $this->any() )
 			->method( 'newFromText' )
-			->will( $this->returnCallback( function( $text, $defaultNs = NS_MAIN ) use ( $pageIdsByTitle ) {
+			->will( $this->returnCallback( function( $text, $defaultNs = 0 /* TODO T195868 */ ) use ( $pageIdsByTitle ) {
 				$title = Title::newFromText( $text, $defaultNs );
 
 				if ( !$title ) {
