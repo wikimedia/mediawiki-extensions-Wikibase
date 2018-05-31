de6745693a0ed44fe401b1f5658ae8c73bf9370e
Amir Sarabadani <ladsgroup@gmail.com>
2018-05-31 17:02:19 +0200

    Include Wikibase dispatch lag in API "maxlag" enforcing
    Depends-On: I8a396d07c2d5764a680d0e5536cd1f99edbe141e
    Bug: T194950
    Change-Id: I814f592657fa7957fbe916e3e3032c4e6e967b49

--- repo/RepoHooks.php
+++ repo/RepoHooks.php
@@ -1187,4 +1187,38 @@ final class RepoHooks {
 		$extraFeatures[] = new HasWbStatementFeature( $foreignRepoNames );
 	}
 
+	/**
+	 * Handler for the ApiMaxLagInfo
+	 *
+	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/ApiMaxLagInfo
+	 *
+	 * @param array &$lagInfo
+	 */
+	public static function onApiMaxLagInfo( &$lagInfo ) {
+
+		$dispatchLagToMaxLagFactor = WikibaseRepo::getDefaultInstance()->getSettings()->getSetting(
+			'dispatchLagToMaxLagFactor'
+		);
+
+		if ( $dispatchLagToMaxLagFactor <= 0 ) {
+			return;
+		}
+
+		$stats = new DispatchStats();
+		$stats->load();
+		$stalest = $stats->getStalest();
+
+		if ( $stalest ) {
+			$maxDispatchLag = $stalest->chd_lag / (float)$dispatchLagToMaxLagFactor;
+			if ( $maxDispatchLag > $lagInfo['lag'] ) {
+				$lagInfo = [
+					'host' => $stalest->chd_site,
+					'lag' => $maxDispatchLag,
+					'type' => 'wikibase-dispatching',
+					'dispatchLag' => $stalest->chd_lag,
+				];
+			}
+		}
+	}
+
 }
