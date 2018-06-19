d671d6538944ea2ccd088ca1b1f9ed1f9db34b9a
Aleksey Bekh-Ivanov (WMDE) <aleksey.bekh-ivanov@wikimedia.de>
2018-06-19 12:42:38 +0200

    DNM Don't access wb_terms table in ItemIdHtmlLinkFormatter
    Change-Id: I018c5ccfc0971f2a2a66b38c0d19d16e7b96d457

--- lib/tests/phpunit/Formatters/WikibaseValueFormatterBuildersTest.php
+++ lib/tests/phpunit/Formatters/WikibaseValueFormatterBuildersTest.php
@@ -21,6 +21,7 @@ use Wikibase\DataModel\Entity\EntityIdValue;
 use Wikibase\DataModel\Entity\ItemId;
 use Wikibase\DataModel\Entity\ItemIdParser;
 use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\DataModel\Services\Lookup\EntityLookup;
 use Wikibase\DataModel\Services\Lookup\LabelDescriptionLookup;
 use Wikibase\DataModel\Services\Lookup\TermLookup;
 use Wikibase\DataModel\Term\Term;
@@ -28,6 +29,7 @@ use Wikibase\LanguageFallbackChainFactory;
 use Wikibase\Lib\FormatterLabelDescriptionLookupFactory;
 use Wikibase\Lib\LanguageNameLookup;
 use Wikibase\Lib\SnakFormatter;
+use Wikibase\Lib\Store\EntityRevisionLookup;
 use Wikibase\Lib\Store\EntityTitleLookup;
 use Wikibase\Lib\WikibaseValueFormatterBuilders;
 
@@ -94,6 +96,9 @@ class WikibaseValueFormatterBuildersTest extends MediaWikiTestCase {
 			new ItemIdParser(),
 			self::GEO_SHAPE_STORAGE_FRONTEND_URL,
 			self::TABULAR_DATA_STORAGE_FRONTEND_URL,
+			0,
+			$this->createMock( EntityLookup::class ),
+			$this->createMock( EntityRevisionLookup::class ),
 			$entityTitleLookup
 		);
 	}
