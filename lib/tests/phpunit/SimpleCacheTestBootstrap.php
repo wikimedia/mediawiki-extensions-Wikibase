068684b4d33f2d0b7b5c5d97dc9234e8766a42b5
Aleksey Bekh-Ivanov (WMDE) <aleksey.bekh-ivanov@wikimedia.de>
2018-06-20 15:35:16 +0200

    Implementation of `Psr\SimpleCache\CacheInterface` using `BagOStuff`
    Change-Id: I2d9e0c621583379fd44a4b5aeb87820b0d5fdd37

---
+++ lib/tests/phpunit/SimpleCacheTestBootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Cache\IntegrationTests;
+
+use PHPUnit\Framework\TestCase;
+
+if ( !class_exists( 'Cache\IntegrationTests\SimpleCacheTest' ) ) {
+	abstract class SimpleCacheTest extends TestCase {
+		public function setUp() {
+			$this->markTestSkipped( 'Cache\IntegrationTests not installed.' );
+		}
+	}
+}
