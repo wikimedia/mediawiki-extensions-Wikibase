91de0235c35af9f08189e3513682e0afe7709ba8
Aleksey Bekh-Ivanov (WMDE) <aleksey.bekh-ivanov@wikimedia.de>
2018-06-20 15:35:16 +0200

    Implementation of `Psr\SimpleCache\CacheInterface` using `BagOStuff`
    Change-Id: I2d9e0c621583379fd44a4b5aeb87820b0d5fdd37

---
+++ lib/tests/phpunit/SimpleCacheWithBagOStuffTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Wikibase\Lib\Tests;
+
+use Cache\IntegrationTests\SimpleCacheTest;
+use HashBagOStuff;
+use Psr\SimpleCache\CacheInterface;
+use Wikibase\Lib\SimpleCacheWithBagOStuff;
+
+class SimpleCacheWithBagOStuffTest extends SimpleCacheTest {
+
+	protected $skippedTests = [
+		'testClear' => 'Not possible to implement for BagOStuff'
+	];
+
+	/**
+	 * @return CacheInterface that is used in the tests
+	 */
+	public function createSimpleCache() {
+		return new SimpleCacheWithBagOStuff( new HashBagOStuff(), 'somePrefix' );
+	}
+
+	public function testUsesPrefixWhenSetting() {
+		$inner = new HashBagOStuff();
+
+		$prefix = 'somePrefix_';
+		$simpleCache = new SimpleCacheWithBagOStuff( $inner, $prefix );
+
+		$simpleCache->set( 'test', 'value' );
+		$this->assertNotFalse( $inner->get( 'somePrefix_test' ) );
+	}
+
+	public function testUsesPrefixWhenSettingMultiple() {
+		$inner = new HashBagOStuff();
+
+		$prefix = 'somePrefix_';
+		$simpleCache = new SimpleCacheWithBagOStuff( $inner, $prefix );
+
+		$simpleCache->setMultiple( [ 'test' => 'value' ] );
+		$this->assertNotFalse( $inner->get( 'somePrefix_test' ) );
+	}
+
+	public function testGivenPrefixContainsForbiddenCharacters_ConstructorThrowsException() {
+		$prefix = '@somePrefix_';
+		$inner = new HashBagOStuff();
+
+		$this->expectException( \Exception::class );
+		new SimpleCacheWithBagOStuff( $inner, $prefix );
+	}
+
+}
