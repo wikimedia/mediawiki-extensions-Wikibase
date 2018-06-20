38b9da294e1e7d0c1de66dd7fe3798c6231e376a
Aleksey Bekh-Ivanov (WMDE) <aleksey.bekh-ivanov@wikimedia.de>
2018-06-20 15:35:16 +0200

    Implementation of `Psr\SimpleCache\CacheInterface` using `BagOStuff`
    Depends-On: Ie01859ad79e1b4c9a4d69dd0c6669a83d1e50e6d
    Change-Id: I2d9e0c621583379fd44a4b5aeb87820b0d5fdd37

---
+++ lib/tests/phpunit/SimpleCacheWithBagOStuffTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Cache\IntegrationTests {
+
+	use PHPUnit\Framework\TestCase;
+
+	if ( !class_exists( 'Cache\IntegrationTests\SimpleCacheTest' ) ) {
+		abstract class SimpleCacheTest extends TestCase {}
+	}
+}
+
+namespace Wikibase\Lib\Tests {
+
+	use Cache\IntegrationTests\SimpleCacheTest;
+	use HashBagOStuff;
+	use Psr\SimpleCache\CacheInterface;
+	use Wikibase\Lib\SimpleCacheWithBagOStuff;
+
+	/**
+	 * @group Wikibase
+	 *
+	 * @license GPL-2.0-or-later
+	 */
+	class SimpleCacheWithBagOStuffTest extends SimpleCacheTest {
+
+		use \PHPUnit4And6Compat;
+
+		protected $skippedTests = [
+			'testClear' => 'Not possible to implement for BagOStuff'
+		];
+
+		/**
+		 * @return CacheInterface that is used in the tests
+		 */
+		public function createSimpleCache() {
+			return new SimpleCacheWithBagOStuff( new HashBagOStuff(), 'somePrefix' );
+		}
+
+		public function testUsesPrefixWhenSetting() {
+			$inner = new HashBagOStuff();
+
+			$prefix = 'somePrefix_';
+			$simpleCache = new SimpleCacheWithBagOStuff( $inner, $prefix );
+
+			$simpleCache->set( 'test', 'value' );
+			$this->assertNotFalse( $inner->get( 'somePrefix_test' ) );
+		}
+
+		public function testUsesPrefixWhenSettingMultiple() {
+			$inner = new HashBagOStuff();
+
+			$prefix = 'somePrefix_';
+			$simpleCache = new SimpleCacheWithBagOStuff( $inner, $prefix );
+
+			$simpleCache->setMultiple( [ 'test' => 'value' ] );
+			$this->assertNotFalse( $inner->get( 'somePrefix_test' ) );
+		}
+
+		public function testGivenPrefixContainsForbiddenCharacters_ConstructorThrowsException() {
+			$prefix = '@somePrefix_';
+			$inner = new HashBagOStuff();
+
+			$this->setExpectedException( \InvalidArgumentException::class );
+			new SimpleCacheWithBagOStuff( $inner, $prefix );
+		}
+
+	}
+}
---
+++ lib/tests/phpunit/bootstrap/SimpleCacheTestBootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+// phpcs:disable
+//
+//namespace Cache\IntegrationTests {
+//
+//	use PHPUnit\Framework\TestCase;
+//
+//	if ( !class_exists( 'Cache\IntegrationTests\SimpleCacheTest' ) ) {
+//		abstract class SimpleCacheTest extends TestCase {
+//			public function setUp() {
+//				$this->markTestSkipped( 'Cache\IntegrationTests not installed.' );
+//			}
+//		}
+//	}
+//}
