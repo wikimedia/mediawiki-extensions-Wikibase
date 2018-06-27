38f92b8024602e2ffe52c3ea348debe1ffece2ea
Leszek Manicki <leszek.manicki@wikimedia.de>
2018-06-27 11:55:34 +0200

    Added CachingFallbackLabelDescriptionLookup
    Co-authored with Aleksey Bekh-Ivanov
    
    Bug: T196882
    Change-Id: If16a711ace4ddf01ddf58bc948c71d4df06127ff

---
+++ lib/includes/Store/CachingFallbackLabelDescriptionLookup.php
@@ -0,0 +1,187 @@
+<?php
+
+namespace  Wikibase\Lib\Store;
+
+use Psr\SimpleCache\CacheInterface;
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\DataModel\Services\Lookup\LabelDescriptionLookup;
+use Wikibase\DataModel\Services\Lookup\LabelDescriptionLookupException;
+use Wikibase\DataModel\Term\TermFallback;
+use Wikibase\LanguageFallbackChain;
+
+/**
+ * @license GPL-2.0-or-later
+ */
+class CachingFallbackLabelDescriptionLookup implements LabelDescriptionLookup {
+
+	// TODO: make private constant when possible (PHP 7.1+)
+	/* private */ const LABEL = 'label';
+	/* private */ const DESCRIPTION = 'description';
+
+	const FIELD_LANGUAGE = 'language';
+	const FIELD_VALUE = 'value';
+	const FIELD_REQUEST_LANGUAGE = 'requestLanguage';
+	const FIELD_SOURCE_LANGUAGE = 'sourceLanguage';
+
+	const SERIALIZATION_NULL = 'null';
+
+	/**
+	 * @var CacheInterface
+	 */
+	private $cache;
+
+	/**
+	 * @var EntityRevisionLookup
+	 */
+	private $revisionLookup;
+
+	/**
+	 * @var LabelDescriptionLookup
+	 */
+	private $labelDescriptionLookup;
+
+	/**
+	 * @var LanguageFallbackChain
+	 */
+	private $languageFallbackChain;
+
+	/**
+	 * @var int
+	 */
+	private $cacheTtlInSeconds = 3600;
+
+	/**
+	 * @param CacheInterface $cache
+	 * @param EntityRevisionLookup $revisionLookup
+	 * @param LabelDescriptionLookup $labelDescriptionLookup
+	 * @param LanguageFallbackChain $languageFallbackChain
+	 * @param int $cacheTtlInSeconds
+	 */
+	public function __construct(
+		CacheInterface $cache,
+		EntityRevisionLookup $revisionLookup,
+		LabelDescriptionLookup $labelDescriptionLookup,
+		LanguageFallbackChain $languageFallbackChain,
+		$cacheTtlInSeconds
+	) {
+		$this->cache = $cache;
+		$this->revisionLookup = $revisionLookup;
+		$this->labelDescriptionLookup = $labelDescriptionLookup;
+		$this->languageFallbackChain = $languageFallbackChain;
+		$this->cacheTtlInSeconds = $cacheTtlInSeconds;
+	}
+
+	/**
+	 * @param EntityId $entityId
+	 *
+	 * @throws LabelDescriptionLookupException
+	 * @return TermFallback|null
+	 */
+	public function getDescription( EntityId $entityId ) {
+		$languageCodes = $this->languageFallbackChain->getFetchLanguageCodes();
+
+		$languageCode = $languageCodes[0];
+		$description = $this->getTerm( $entityId, $languageCode, self::DESCRIPTION );
+
+		if ( $this->shouldCache( $entityId, $languageCode, self::DESCRIPTION ) ) {
+			$this->cacheTerm( $entityId, $languageCode, self::DESCRIPTION, $description );
+		}
+
+		return $description;
+	}
+
+	/**
+	 * @param EntityId $entityId
+	 *
+	 * @throws LabelDescriptionLookupException
+	 * @return TermFallback|null
+	 */
+	public function getLabel( EntityId $entityId ) {
+		$languageCodes = $this->languageFallbackChain->getFetchLanguageCodes();
+		$languageCode = $languageCodes[0];
+		$label = $this->getTerm( $entityId, $languageCode, self::LABEL );
+
+		if ( $this->shouldCache( $entityId, $languageCode, self::LABEL ) ) {
+			$this->cacheTerm( $entityId, $languageCode, self::LABEL, $label );
+		}
+
+		return $label;
+	}
+
+	private function getTerm( EntityId $entityId, $languageCode, $termName = self::LABEL ) {
+		$cacheKey = $this->getCacheKey( $entityId, $languageCode, $termName );
+		$result = $this->cache->get( $cacheKey );
+		if ( !$result ) {
+			$term = $termName === self::LABEL
+				? $this->labelDescriptionLookup->getLabel( $entityId )
+				: $this->labelDescriptionLookup->getDescription( $entityId );
+
+			return $term;
+		}
+
+		$term = $this->unserialize( $result );
+
+		return $term;
+	}
+
+	private function shouldCache( EntityId $entityId, $languageCode, $termName ) {
+		$cacheKey = $this->getCacheKey( $entityId, $languageCode, $termName );
+		return !$this->cache->has( $cacheKey );
+	}
+
+	private function cacheTerm( EntityId $entityId, $languageCode, $termName, TermFallback $term = null ) {
+		$cacheKey = $this->getCacheKey( $entityId, $languageCode, $termName );
+
+		$serialization = $this->serialize( $term );
+
+		$this->cache->set( $cacheKey, $serialization, $this->cacheTtlInSeconds );
+	}
+
+	/**
+	 * @param TermFallback|null $termFallback
+	 * @return array|null
+	 */
+	private function serialize( TermFallback $termFallback = null ) {
+		if ( $termFallback === null ) {
+			return null;
+		}
+
+		return [
+			self::FIELD_LANGUAGE => $termFallback->getActualLanguageCode(),
+			self::FIELD_VALUE => $termFallback->getText(),
+			self::FIELD_REQUEST_LANGUAGE => $termFallback->getLanguageCode(),
+			self::FIELD_SOURCE_LANGUAGE => $termFallback->getSourceLanguageCode(),
+		];
+	}
+
+	/**
+	 * @param array|null $serialized
+	 * @return null|TermFallback
+	 */
+	private function unserialize( $serialized ) {
+		if ( $serialized === null ) {
+			return null;
+		}
+
+		$termData = $serialized;
+		return new TermFallback(
+			$termData[self::FIELD_REQUEST_LANGUAGE],
+			$termData[self::FIELD_VALUE],
+			$termData[self::FIELD_LANGUAGE],
+			$termData[self::FIELD_SOURCE_LANGUAGE]
+		);
+	}
+
+	/**
+	 * @param EntityId $entityId
+	 * @param string $languageCode
+	 * @param string $termName
+	 * @return string
+	 */
+	private function getCacheKey( EntityId $entityId, $languageCode, $termName ) {
+		$revisionId = $this->revisionLookup->getLatestRevisionId( $entityId );
+
+		return "{$entityId->getSerialization()}_{$revisionId}_{$languageCode}_{$termName}";
+	}
+
+}
---
+++ lib/tests/phpunit/Store/CachingFallbackLabelDescriptionLookupTest.php
@@ -0,0 +1,303 @@
+<?php
+
+namespace Wikibase\Lib\Tests\Store;
+
+use Prophecy\Argument;
+use Psr\SimpleCache\CacheInterface;
+use PHPUnit\Framework\TestCase;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Services\Lookup\LabelDescriptionLookup;
+use Wikibase\DataModel\Term\TermFallback;
+use Wikibase\LanguageFallbackChain;
+use Wikibase\Lib\Store\CachingFallbackLabelDescriptionLookup;
+use Wikibase\Lib\Store\EntityRevisionLookup;
+
+/**
+ * @covers \Wikibase\Lib\Store\CachingFallbackLabelDescriptionLookup
+ *
+ * @group Wikibase
+ *
+ * @license GPL-2.0-or-later
+ */
+class CachingFallbackLabelDescriptionLookupTest extends TestCase {
+
+	/*private */ const TEST_LABEL = 'tomato';
+	/*private */ const TEST_DESCRIPTION = 'The edible berry of the plant Solanum lycopersicum';
+
+	public function testGivenNoLabelInCache_getLabelPassesRequestToInnerLookup() {
+		$cache = $this->prophesize( CacheInterface::class );
+
+		$itemId = new ItemId( 'Q123' );
+
+		$ldLookup = $this->prophesize( LabelDescriptionLookup::class );
+		$ldLookup->getLabel( $itemId )
+			->willReturn( new TermFallback( 'en', self::TEST_LABEL, 'en', 'en' ) );
+
+		$ttlInSeconds = 10;
+
+		$lookup = new CachingFallbackLabelDescriptionLookup(
+			$cache->reveal(),
+			$this->newRevisionLookup(),
+			$ldLookup->reveal(),
+			$this->newFallbackChain(),
+			$ttlInSeconds
+		);
+
+		$this->assertSame( self::TEST_LABEL, $lookup->getLabel( $itemId )->getText() );
+	}
+
+	public function testGetLabelWritesLabelToCache() {
+		$cache = $this->prophesize( CacheInterface::class );
+
+		$itemId = new ItemId( 'Q123' );
+
+		$ldLookup = $this->prophesize( LabelDescriptionLookup::class );
+		$ldLookup->getLabel( $itemId )
+			->willReturn( new TermFallback( 'en', self::TEST_LABEL, 'en', 'en' ) );
+
+		$ttlInSeconds = 10;
+
+		$lookup = new CachingFallbackLabelDescriptionLookup(
+			$cache->reveal(),
+			$this->newRevisionLookup( 99 ),
+			$ldLookup->reveal(),
+			$this->newFallbackChain(),
+			$ttlInSeconds
+		);
+
+		$lookup->getLabel( $itemId );
+
+		$cache->set(
+			'Q123_99_en_label',
+			 [
+				'language' => 'en',
+				'value' => self::TEST_LABEL,
+				'requestLanguage' => 'en',
+				'sourceLanguage' => 'en',
+			],
+			$ttlInSeconds
+		)->shouldHaveBeenCalled();
+	}
+
+	public function testGivenEntryInCacheExists_getLabelUsesCachedValue() {
+		$cache = $this->prophesize( CacheInterface::class );
+		$cache->get( 'Q123_99_en_label' )->willReturn( [
+			'language' => 'en',
+			'value' => self::TEST_LABEL,
+			'requestLanguage' => 'en',
+			'sourceLanguage' => 'en',
+		] );
+		$cache->has( Argument::any() )->willReturn( true );
+
+		$itemId = new ItemId( 'Q123' );
+
+		$ldLookup = $this->prophesize( LabelDescriptionLookup::class );
+
+		$ttlInSeconds = 10;
+
+		$lookup = new CachingFallbackLabelDescriptionLookup(
+			$cache->reveal(),
+			$this->newRevisionLookup( 99 ),
+			$ldLookup->reveal(),
+			$this->newFallbackChain(),
+			$ttlInSeconds
+		);
+
+		$this->assertSame( self::TEST_LABEL, $lookup->getLabel( $itemId )->getText() );
+
+		$ldLookup->getLabel( Argument::any() )->shouldNotHaveBeenCalled();
+	}
+
+	public function testGivenNoLabelFound_nullEntryWrittenToCache() {
+		$cache = $this->prophesize( CacheInterface::class );
+
+		$itemId = new ItemId( 'Q123' );
+
+		$ldLookup = $this->prophesize( LabelDescriptionLookup::class );
+		$ldLookup->getLabel( Argument::any() )->willReturn( null );
+
+		$ttlInSeconds = 10;
+
+		$lookup = new CachingFallbackLabelDescriptionLookup(
+			$cache->reveal(),
+			$this->newRevisionLookup( 99 ),
+			$ldLookup->reveal(),
+			$this->newFallbackChain(),
+			$ttlInSeconds
+		);
+
+		$lookup->getLabel( $itemId );
+
+		$cache->set( 'Q123_99_en_label', null, $ttlInSeconds )->shouldHaveBeenCalled();
+	}
+
+	public function testGivenNullEntryInCache_getLabelReturnsCachedNull() {
+		$cache = $this->prophesize( CacheInterface::class );
+		$cache->get( 'Q123_99_en_label' )->willReturn( null );
+		$cache->has( Argument::any() )->willReturn( true );
+
+		$itemId = new ItemId( 'Q123' );
+
+		$ldLookup = $this->prophesize( LabelDescriptionLookup::class );
+
+		$ttlInSeconds = 10;
+
+		$lookup = new CachingFallbackLabelDescriptionLookup(
+			$cache->reveal(),
+			$this->newRevisionLookup( 99 ),
+			$ldLookup->reveal(),
+			$this->newFallbackChain(),
+			$ttlInSeconds
+		);
+
+		$this->assertNull( $lookup->getLabel( $itemId ) );
+
+		$ldLookup->getLabel()->shouldNotHaveBeenCalled();
+	}
+
+	public function testGivenNoDescriptionInCache_getDescriptionPassesRequestToInnerLookup() {
+		$cache = $this->prophesize( CacheInterface::class );
+
+		$itemId = new ItemId( 'Q123' );
+
+		$ldLookup = $this->prophesize( LabelDescriptionLookup::class );
+		$ldLookup->getDescription( $itemId )
+			->willReturn( new TermFallback( 'en', self::TEST_DESCRIPTION, 'en', 'en' ) );
+
+		$ttlInSeconds = 10;
+
+		$lookup = new CachingFallbackLabelDescriptionLookup(
+			$cache->reveal(),
+			$this->newRevisionLookup(),
+			$ldLookup->reveal(),
+			$this->newFallbackChain(),
+			$ttlInSeconds
+		);
+
+		$this->assertSame( self::TEST_DESCRIPTION, $lookup->getDescription( $itemId )->getText() );
+	}
+
+	public function testGetDescriptionWritesDescriptionToCache() {
+		$cache = $this->prophesize( CacheInterface::class );
+
+		$itemId = new ItemId( 'Q123' );
+
+		$ldLookup = $this->prophesize( LabelDescriptionLookup::class );
+		$ldLookup->getDescription( $itemId )
+			->willReturn( new TermFallback( 'en', self::TEST_DESCRIPTION, 'en', 'en' ) );
+
+		$ttlInSeconds = 10;
+
+		$lookup = new CachingFallbackLabelDescriptionLookup(
+			$cache->reveal(),
+			$this->newRevisionLookup( 99 ),
+			$ldLookup->reveal(),
+			$this->newFallbackChain(),
+			$ttlInSeconds
+		);
+
+		$lookup->getDescription( $itemId );
+
+		$cache->set(
+			'Q123_99_en_description',
+			[
+				'language' => 'en',
+				'value' => self::TEST_DESCRIPTION,
+				'requestLanguage' => 'en',
+				'sourceLanguage' => 'en',
+			],
+			$ttlInSeconds
+		)->shouldHaveBeenCalled();
+	}
+
+	public function testGivenEntryInCacheExists_getDescriptionUsesCachedValue() {
+		$cache = $this->prophesize( CacheInterface::class );
+		$cache->get( 'Q123_99_en_description' )->willReturn( [
+			'language' => 'en',
+			'value' => self::TEST_DESCRIPTION,
+			'requestLanguage' => 'en',
+			'sourceLanguage' => 'en',
+		] );
+		$cache->has( Argument::any() )->willReturn( true );
+
+		$itemId = new ItemId( 'Q123' );
+
+		$ldLookup = $this->prophesize( LabelDescriptionLookup::class );
+
+		$ttlInSeconds = 10;
+
+		$lookup = new CachingFallbackLabelDescriptionLookup(
+			$cache->reveal(),
+			$this->newRevisionLookup( 99 ),
+			$ldLookup->reveal(),
+			$this->newFallbackChain(),
+			$ttlInSeconds
+		);
+
+		$this->assertSame( self::TEST_DESCRIPTION, $lookup->getDescription( $itemId )->getText() );
+
+		$ldLookup->getDescription( Argument::any() )->shouldNotHaveBeenCalled();
+	}
+
+	public function testGivenNoDescriptionFound_nullEntryWrittenToCache() {
+		$cache = $this->prophesize( CacheInterface::class );
+
+		$itemId = new ItemId( 'Q123' );
+
+		$ldLookup = $this->prophesize( LabelDescriptionLookup::class );
+		$ldLookup->getDescription( Argument::any() )->willReturn( null );
+
+		$ttl = 10;
+
+		$lookup = new CachingFallbackLabelDescriptionLookup(
+			$cache->reveal(),
+			$this->newRevisionLookup( 99 ),
+			$ldLookup->reveal(),
+			$this->newFallbackChain(),
+			$ttl
+		);
+
+		$lookup->getDescription( $itemId );
+
+		$cache->set( 'Q123_99_en_description', null, $ttl )->shouldHaveBeenCalled();
+	}
+
+	public function testGivenNullEntryInCache_getDescriptionReturnsCachedNull() {
+		$cache = $this->prophesize( CacheInterface::class );
+		$cache->get( 'Q123_99_en_description' )->willReturn( null );
+		$cache->has( Argument::any() )->willReturn( true );
+
+		$itemId = new ItemId( 'Q123' );
+
+		$ldLookup = $this->prophesize( LabelDescriptionLookup::class );
+
+		$ttlInSeconds = 10;
+
+		$lookup = new CachingFallbackLabelDescriptionLookup(
+			$cache->reveal(),
+			$this->newRevisionLookup( 99 ),
+			$ldLookup->reveal(),
+			$this->newFallbackChain(),
+			$ttlInSeconds
+		);
+
+		$this->assertNull( $lookup->getDescription( $itemId ) );
+
+		$ldLookup->getDescription()->shouldNotHaveBeenCalled();
+	}
+
+	private function newRevisionLookup( $revisionIdToReturn = 0 ) {
+		$revLookup = $this->prophesize( EntityRevisionLookup::class );
+		if ( $revisionIdToReturn ) {
+			$revLookup->getLatestRevisionId( Argument::any() )->willReturn( $revisionIdToReturn );
+		}
+		return $revLookup->reveal();
+	}
+
+	private function newFallbackChain() {
+		$fallbackChain = $this->prophesize( LanguageFallbackChain::class );
+		$fallbackChain->getFetchLanguageCodes()->willReturn( [ 'en' ] );
+		return $fallbackChain->reveal();
+	}
+
+}
