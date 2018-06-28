c8f022e6c6f83b14f7b9530eba8db0834bb543e0
Jakob Warkotsch <j.warkotsch@gmail.com>
2018-06-28 16:26:14 +0200

    Refactor ParserOutputDataUpdater
    The goal is to have a more generic ParserOutputDataUpdater interface for
    objects that work with entities and update the ParserOutput accordingly.
    
    * refactoredEntityParserOutputDataUpdaterCollection
      * renamed from EntityParserOutputDataUpdater
      * more generic (no longer knows about sitelinks and statements)
    
    * introduced EntityParserOutputDataUpdater as an interface
    * wrapped existing StatementDataUpdaters in EntityParserOutputDataUpdater
      adapters
    
    Bug: T198295
    Change-Id: Ifaa77e34fb25b80eaf8f9b30d47ecae6d862cbd9

---
+++ repo/tests/phpunit/includes/ParserOutput/EntityStatementDataUpdaterAdapterTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Wikibase\Repo\Tests\ParserOutput;
+
+use PHPUnit\Framework\TestCase;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\DataModel\Statement\Statement;
+use Wikibase\DataModel\Statement\StatementList;
+use Wikibase\Repo\ParserOutput\EntityStatementDataUpdaterAdapter;
+use Wikibase\Repo\ParserOutput\StatementDataUpdater;
+
+/**
+ * @covers Wikibase\Repo\ParserOutput\EntityStatementDataUpdaterAdapter
+ *
+ * @group Wikibase
+ *
+ * @license GPL-2.0-or-later
+ */
+class EntityStatementDataUpdaterAdapterTest extends TestCase {
+
+	public function testProcessEntityCallsProcessStatementForEachStatement() {
+		$statement1 = $this->getMockStatement();
+		$statement2 = $this->getMockStatement();
+		$item = new Item( null, null, null, new StatementList( [ $statement1, $statement2 ] ) );
+
+		$statementDataUpdater = $this->getMockStatementDataUpdater();
+		$statementDataUpdater->expects( $this->exactly( 2 ) )
+			->method( 'processStatement' )
+			->withConsecutive(
+				[ $statement1 ],
+				[ $statement2 ]
+			);
+		$adapter = new EntityStatementDataUpdaterAdapter( $statementDataUpdater );
+		$adapter->processEntity( $item );
+	}
+
+	public function testUpdateParserOutputIsDelegated() {
+		$parserOutput = new \ParserOutput();
+		$statementDataUpdater = $this->getMockStatementDataUpdater();
+		$statementDataUpdater->expects( $this->once() )
+			->method( 'updateParserOutput' )
+			->with( $parserOutput );
+
+		$adapter = new EntityStatementDataUpdaterAdapter( $statementDataUpdater );
+
+		$adapter->updateParserOutput( $parserOutput );
+	}
+
+	private function getMockStatementDataUpdater() {
+		return $this->getMockBuilder( StatementDataUpdater::class )
+			->getMock();
+	}
+
+	private function getMockStatement() {
+		return $this->getMockBuilder( Statement::class )
+			->disableOriginalConstructor()
+			->getMock();
+	}
+
+}
