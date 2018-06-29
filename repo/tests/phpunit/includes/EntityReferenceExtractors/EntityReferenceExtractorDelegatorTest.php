b216faea8428bb238fc825e3f41cfed688769dad
Jakob Warkotsch <j.warkotsch@gmail.com>
2018-06-29 17:34:03 +0200

    Make EntityReferenceExtractors configurable per entity type
    TODO:
    * test all the new things
    * sensible fallback for the factory
    * is there a complete integration test? surprisingly few tests broke
    
    Bug: T198295
    Change-Id: I8b254007279fc115924a1faca8a0202e15dbbf6d

---
+++ repo/tests/phpunit/includes/EntityReferenceExtractors/EntityReferenceExtractorDelegatorTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Wikibase\Repo\Tests\Interactors;
+
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\Repo\EntityReferenceExtractors\EntityReferenceExtractor;
+use Wikibase\Repo\EntityReferenceExtractors\EntityReferenceExtractorDelegator;
+
+/**
+ * @covers Wikibase\Repo\EntityReferenceExtractors\EntityReferenceExtractorDelegator
+ *
+ * @group Wikibase
+ *
+ * @license GPL-2.0-or-later
+ */
+class EntityReferenceExtractorDelegatorTest extends TestCase {
+
+	/**
+	 * @dataProvider nonCallableArrayProvider
+	 *
+	 * @expectedException InvalidArgumentException
+	 */
+	public function testGivenNonCallables_throwsException( $nonCallables ) {
+		new EntityReferenceExtractorDelegator( $nonCallables );
+	}
+
+	public function testGivenEntityReferenceExtractorsForEntityType_extractEntityIdsDelegates() {
+		$entity = new Item();
+		$expected = [ new ItemId( 'Q123' ) ];
+		$mockEntityReferenceExtractor = $this->getMockBuilder( EntityReferenceExtractor::class )->getMock();
+		$mockEntityReferenceExtractor->expects( $this->once() )
+			->method( 'extractEntityIds' )
+			->with( $entity )
+			->willReturn( $expected );
+		$delegator = new EntityReferenceExtractorDelegator( [
+			'property' => function () {
+				// not called
+			},
+			'item' => function () use ( $mockEntityReferenceExtractor ) {
+				return $mockEntityReferenceExtractor;
+			},
+		] );
+
+		$this->assertSame(
+			$expected,
+			$delegator->extractEntityIds( $entity )
+		);
+	}
+
+	public function nonCallableArrayProvider() {
+		return [
+			[ [ 'string', 'string' ] ],
+			[ [ 1, 2, 3 ] ],
+			[ [ function () {
+			}, null ] ],
+		];
+	}
+
+}
