d9980d1db350f98ef1549b05dadb833f4173db9b
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2018-07-05 16:03:44 +0100

    WIP - Move Wikibase API Selenium to Wikibase
    Change-Id: I75aec22d9fe38aab9d85c4d1e4dc39f66f8885e2

---
+++ repo/tests/selenium/wikibase.api.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const MWBot = require( 'mwbot' ),
+	bot = new MWBot( {
+		apiUrl: browser.options.baseUrl + '/api.php'
+	} );
+
+class WikibaseApi {
+
+	/**
+	 * Create an item
+	 *
+	 * @param {string} [label] Optional English label of the item
+	 * @param {object} [data] Optional data to populate the item with
+	 * @return {Promise}
+	 */
+	createItem( label, data) {
+		let labels = {};
+		if ( label ) {
+			labels = {
+					en: {
+						language: 'en',
+						value: label
+					}
+			};
+		}
+
+		let itemData = Object.assign({}, { labels }, data )
+
+		return bot.getEditToken()
+			.then( () => {
+				return new Promise( ( resolve, reject ) => {
+					bot.request( {
+						action: 'wbeditentity',
+						'new': 'item',
+						data: JSON.stringify( itemData ),
+						token: bot.editToken
+					} ).then( ( response ) => {
+						resolve( response.entity.id );
+					}, reject );
+				} );
+			} );
+	}
+
+	createProperty ( datatype, data ) {
+		let propertyData = {};
+
+		propertyData = Object.assign({}, { datatype }, data);
+
+		return bot.getEditToken()
+			.then( () => {
+			return new Promise( ( resolve, reject ) => {
+				bot.request( {
+				action: 'wbeditentity',
+				new: 'property',
+				data: JSON.stringify( propertyData ),
+				token: bot.editToken
+			} ).then( ( response ) => {
+				resolve( response.entity.id );
+				}, reject );
+			} );
+		} );
+	}
+
+}
+
+module.exports = new WikibaseApi();
