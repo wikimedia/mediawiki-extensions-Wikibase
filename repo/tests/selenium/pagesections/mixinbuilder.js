3e852761cd42a7a910de344ddd5db239f331ef0d
Pablo Grass <pablo.grass@wikimedia.de>
2018-07-10 12:27:23 +0200

    selenium: ComponentInteraction mixin, convenient way to add
    Add a mixin providing interaction for (ooui) form components.
    Also provides a helper to add multiple mixins to page objects in a more
    readable fashion.
    
    Bug: T194771
    Change-Id: I3f87f794877d1f2451dcbe0cde77578af422f30c

---
+++ repo/tests/selenium/pagesections/mixinbuilder.js
@@ -0,0 +1,16 @@
+'use strict';
+
+class MixinBuilder {
+
+	constructor( superclass ) {
+		this.superclass = superclass;
+	}
+
+	with( ...mixins ) {
+		return mixins.reduce( ( c, mixin ) => mixin( c ), this.superclass );
+	}
+}
+
+module.exports = {
+	mix: ( superclass ) => new MixinBuilder( superclass )
+};
