c0bed0dfd0c640175b44fbf65e2097ce2991c665
Aleksey Bekh-Ivanov (WMDE) <aleksey.bekh-ivanov@wikimedia.de>
2018-07-10 14:56:38 +0200

    Count number of `$entityIds` being prefetched
    This is needed to understand real impact of prefetching approach
    
    Bug: T198868
    Change-Id: I91536e24fbde1b12076f43b28fe69cc7f929f1b9

--- lib/includes/Store/BufferingTermLookup.php
+++ lib/includes/Store/BufferingTermLookup.php
@@ -115,6 +115,15 @@ class BufferingTermLookup extends EntityTermLookupBase implements PrefetchingTer
 			'wikibase.repo.wb_terms.select.BufferingTermLookup_prefetchTerms'
 		);
 
+		$closest2Power = round( log( count( $entityIds ), 2 ) );
+		$low = ceil( pow( 2, $closest2Power - 0.5 ) );
+		$high = floor( pow( 2, $closest2Power + 0.5 ) );
+		$idCount = $low === $high ? $low : "{$low}-{$high}";
+
+		MediaWikiServices::getInstance()->getStatsdDataFactory()->increment(
+			'wikibase.repo.wb_terms.BufferingTermLookup_prefetchTerms.idCount.' . $idCount
+		);
+
 		if ( empty( $entityIds ) ) {
 			return;
 		}
