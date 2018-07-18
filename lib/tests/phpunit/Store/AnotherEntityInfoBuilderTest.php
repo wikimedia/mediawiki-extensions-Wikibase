b1860629a8612d9dff38ea6dba694f330aff5a2b
Leszek Manicki <leszek.manicki@wikimedia.de>
2018-07-18 17:12:55 +0200

    DNM Attempt to make EntityInfoBuilder not using wb_terms
    Depends on gerrit change 441203
    (SimpleCacheWithBagOStuff)
    
    Bug: T198868
    Change-Id: I2c0116db23e419dd0a16556df02ea2bdf704b5a7

---
+++ lib/tests/phpunit/Store/AnotherEntityInfoBuilderTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Wikibase\Lib\Tests\Store;
+
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Services\Lookup\EntityRetrievingTermLookup;
+use Wikibase\DataModel\Services\Lookup\InMemoryEntityLookup;
+use Wikibase\DataModel\Services\Lookup\RedirectResolvingEntityLookup;
+use Wikibase\DataModel\Services\Lookup\UnresolvedEntityRedirectException;
+use Wikibase\Lib\SimpleCacheWithBagOStuff;
+use Wikibase\Lib\Store\AnotherEntityInfoBuilder;
+use Wikibase\Lib\Store\CachingLabelDescriptionLookupForBatch;
+use Wikibase\Lib\Store\EntityRetrievingLabelDescriptionLookupForBatch;
+use Wikibase\Lib\Store\EntityRevisionCache;
+
+/**
+ * @license GPL-2.0-or-later
+ *
+ * @covers \Wikibase\Lib\Store\AnotherEntityInfoBuilder
+ *
+ * @group Wikibase
+ */
+class AnotherEntityInfoBuilderTest extends EntityInfoBuilderTestCase {
+
+	protected function newEntityInfoBuilder() {
+		$entityLookup = new InMemoryEntityLookup();
+		foreach ( $this->getKnownEntities() as $entity ) {
+			$entityLookup->addEntity( $entity );
+		}
+		foreach ( $this->getKnownRedirects() as $fromId => $toId ) {
+			$entityLookup->addException( new UnresolvedEntityRedirectException( new ItemId( $fromId ), $toId ) );
+		}
+
+		return new AnotherEntityInfoBuilder(
+			new CachingLabelDescriptionLookupForBatch(
+				new EntityRevisionCache( new \HashBagOStuff() ),
+				new SimpleCacheWithBagOStuff( new \HashBagOStuff(), 'test' ),
+				new EntityRetrievingLabelDescriptionLookupForBatch(
+					new EntityRetrievingTermLookup(
+						new RedirectResolvingEntityLookup( $entityLookup )
+					)
+				)
+			)
+		);
+	}
+
+}
