93f7cf5e178fba6920bf422764703134b4f9b52b
Pablo Grass <pablo.grass@wikimedia.de>
2018-07-26 13:55:28 +0200

    ChangeOpsMerge: abstract cross-referencing validation
    Abstract out the validator that ensures that statement lists do not
    contain references between the entities they are attached to.
    Will soon be needed for Lexeme Forms, too.
    
    Bug: T198106
    Change-Id: Ieff928ee0b9d0fc46ddd120753850ef83b0f7039

---
+++ repo/includes/Merge/Validator/NoCrossReferencingStatements.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Wikibase\Repo\Merge\Validator;
+
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\DataModel\Entity\EntityIdValue;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\DataModel\Snak\PropertyValueSnak;
+use Wikibase\DataModel\Snak\Snak;
+use Wikibase\DataModel\Statement\Statement;
+use Wikibase\DataModel\Statement\StatementListProvider;
+
+/**
+ * @license GPL-2.0-or-later
+ */
+class NoCrossReferencingStatements {
+
+	private $violations = [];
+
+	/**
+	 * TODO Reasonably hint against both EntityDocument (for id) and StatementListProvider
+	 * e.g. StatementListBearingEntityDocument
+	 *
+	 * @param StatementListProvider $source
+	 * @param StatementListProvider $target
+	 * @return bool
+	 */
+	public function validate( StatementListProvider $source, StatementListProvider $target ) {
+		$this->violations = [];
+
+		foreach ( $target->getStatements()->toArray() as $toStatement ) {
+			$this->checkStatementHasLink( $toStatement, $source->getId() );
+		}
+
+		foreach ( $source->getStatements()->toArray() as $fromStatement ) {
+			$this->checkStatementHasLink( $fromStatement, $target->getId() );
+		}
+
+		return empty( $this->violations );
+	}
+
+	/**
+	 * @return PropertyId[] Properties used to link across
+	 */
+	public function getViolations() {
+		return $this->violations;
+	}
+
+	private function checkStatementHasLink( Statement $statement, EntityId $id ) {
+		// TODO will be getAllSnaks() in next step
+		$snaks = [ $statement->getMainSnak() ];
+
+		foreach ( $snaks as $snak ) {
+			$this->checkSnakIsLink( $snak, $id );
+		}
+	}
+
+	private function checkSnakIsLink( Snak $snak, EntityId $id ) {
+		if ( !( $snak instanceof PropertyValueSnak ) ) {
+			return;
+		}
+
+		$dataValue = $snak->getDataValue();
+
+		if ( !( $dataValue instanceof EntityIdValue ) ) {
+			return;
+		}
+
+		if ( $dataValue->getEntityId()->equals( $id ) ) {
+			$this->violations[] = $snak->getPropertyId();
+		}
+	}
+
+}
