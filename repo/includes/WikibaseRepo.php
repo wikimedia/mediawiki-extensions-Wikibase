307a2b4bf9ab7cff8b79893b49472e060fef3cf1
addshore <addshorewiki@gmail.com>
2018-07-27 14:58:08 +0100

    EntityTypeDefinitions, fulltext-search-context can be callables
    Change-Id: If5f37dd124dcf612e8d735168bd595103acc59c5

--- repo/includes/WikibaseRepo.php
+++ repo/includes/WikibaseRepo.php
@@ -2092,7 +2092,13 @@ class WikibaseRepo {
 	 * @return string[]
 	 */
 	public function getFulltextSearchTypes() {
-		return $this->entityTypeDefinitions->getFulltextSearchTypes();
+		$searchTypes = $this->entityTypeDefinitions->getFulltextSearchTypes();
+		foreach ( $searchTypes as $key => $value ) {
+			if ( is_callable( $value ) ) {
+				$searchTypes[$key] = $value();
+			}
+		}
+		return $searchTypes;
 	}
 
 }
