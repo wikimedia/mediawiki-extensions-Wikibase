9b429df4ecdb683fd6d748d99eb8246aa21988d6
daniel <daniel.kinzler@wikimedia.de>
2018-08-02 19:36:00 +0200

    WIP MCR Allow alternative slots to be configured for entities
    Slots will be configured in the entityNamespace setting (or by extensions via
    the WikibaseEntityNamespaces hook), by using the syntax <namespace-id>@<slot-name>
    instead of giving the namespace ID as an integer.
    
    TODO: tests
    
    Bug: T194729
    Change-Id: Ia66f8ffcc74a4786acdab813abe4d52dbd59e82e

--- lib/includes/Store/Sql/WikiPageEntityMetaDataLookup.php
+++ lib/includes/Store/Sql/WikiPageEntityMetaDataLookup.php
@@ -236,9 +236,18 @@ class WikiPageEntityMetaDataLookup extends DBAccessBase implements WikiPageEntit
 
 		wfDebugLog( __CLASS__, __FUNCTION__ . ": Looking up revision $revisionId of " . $entityId );
 
+		$fields = $this->selectFields();
+
+		// Attach the appropriate role name.
+		// This could as well come from the database, if the query was written accordingly.
+		$roleName = $this->entityNamespaceLookup->getEntitySlotRole(
+			$entityId->getEntityType()
+		);
+		$fields['role_name'] = $db->addQuotes( $roleName );
+
 		$row = $db->selectRow(
 			[ 'revision', 'page' ],
-			$this->selectFields(),
+			$fields,
 			[ 'rev_id' => $revisionId ],
 			__METHOD__,
 			[],
@@ -348,7 +357,15 @@ class WikiPageEntityMetaDataLookup extends DBAccessBase implements WikiPageEntit
 			$result[$serializedId] = false;
 
 			if ( isset( $rows[$idLocalPart] ) ) {
-				$result[$serializedId] = $rows[$idLocalPart];
+				$row = $rows[$idLocalPart];
+
+				// Attach the appropriate role name.
+				// This could as well come from the database, if the query was written accordingly.
+				$row->role_name = $this->entityNamespaceLookup->getEntitySlotRole(
+					$entityId->getEntityType()
+				);
+
+				$result[$serializedId] = $row;
 			}
 		}
 
