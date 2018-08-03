fde08563201ee1115ab4f81a64f7bfd3b5e7f162
Pablo Grass <pablo.grass@wikimedia.de>
2018-08-03 14:32:08 +0200

    travis: build using composer-merge
    I9d535473ea30dcd15b3e60244673d770177e608f made the use of the composer
    merge plugin mandatory.
    
    Bug: T201161
    Change-Id: Icca56623cd77b7fedc9da9f6b392d60720caf91d

--- build/travis/install.sh
+++ build/travis/install.sh
@@ -18,19 +18,7 @@ wget https://github.com/wikimedia/mediawiki/archive/$MW.tar.gz
 tar -zxf $MW.tar.gz
 mv mediawiki-$MW phase3
 
-cd phase3
-composer self-update
-composer install
-
-# Try composer install again... this tends to fail from time to time
-if [ $? -gt 0 ]; then
-	composer install
-fi
-
-mysql -e 'create database its_a_mw;'
-php maintenance/install.php --dbtype $DBTYPE --dbuser root --dbname its_a_mw --dbpath $(pwd) --pass nyan TravisWiki admin
-
-cd extensions
+cd phase3/extensions
 
 if [ "$WB" != "repo" ]; then
 	git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/Scribunto.git --depth 1
@@ -39,11 +27,12 @@ git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/cldr --depth 1
 
 cp -r $originalDirectory Wikibase
 
-cd Wikibase
+cd phase3
 
-composer install --prefer-source
+cp $originalDirectory/build/travis/composer.local.json composer.local.json
 
-# Try composer install again... this tends to fail from time to time
-if [ $? -gt 0 ]; then
-	composer install --prefer-source
-fi
+composer self-update
+travis_retry composer install
+
+mysql -e 'create database its_a_mw;'
+php maintenance/install.php --dbtype $DBTYPE --dbuser root --dbname its_a_mw --dbpath $(pwd) --pass nyan TravisWiki admin
