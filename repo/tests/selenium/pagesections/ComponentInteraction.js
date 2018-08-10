7577d27e52bb429fd0a354825ec1e5270029370c
Pablo Grass <pablo.grass@wikimedia.de>
2018-08-10 10:33:16 +0200

    ComponentInteraction: set value on combobox
    Add method to set the value of a combobox element.
    https://doc.wikimedia.org/oojs-ui/master/js/#!/api/OO.ui.ComboBoxInputWidget
    
    Bug: T201337
    Change-Id: I6d462c6e714ecd7c582bb28530a2eb8ec110a6c8

--- repo/tests/selenium/pagesections/ComponentInteraction.js
+++ repo/tests/selenium/pagesections/ComponentInteraction.js
@@ -4,7 +4,9 @@ let ComponentInteraction = Base => class extends Base {
 
 	static get OOUI_SELECTORS() {
 		return {
-			OPTION_WIDGET: '.oo-ui-optionWidget'
+			OPTION_WIDGET: '.oo-ui-optionWidget',
+			OPTION_WIDGET_SELECTED: 'oo-ui-optionWidget-selected',
+			OVERLAY: '.oo-ui-defaultOverlay'
 		};
 	}
 
@@ -12,6 +14,20 @@ let ComponentInteraction = Base => class extends Base {
 		element.$( 'input' ).setValue( value );
 		element.waitForVisible( this.constructor.OOUI_SELECTORS.OPTION_WIDGET );
 		element.$( this.constructor.OOUI_SELECTORS.OPTION_WIDGET ).click();
+	}
+
+	setValueOnComboboxElement( element, value ) {
+		element.$( 'input' ).setValue( value );
+		browser.waitUntil( () => {
+			return (
+				browser.isVisible(
+					this.constructor.OOUI_SELECTORS.OVERLAY +
+					' ' +
+					this.constructor.OOUI_SELECTORS.OPTION_WIDGET_SELECTED
+				)
+			);
+		} );
+		browser.keys( 'Escape' ); // close suggestion overlay
 	}
 };
 
