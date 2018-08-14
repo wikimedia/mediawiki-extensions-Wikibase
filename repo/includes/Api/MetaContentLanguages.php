d7c5c01a5d8a38c74bf185e3cad5797121e58bf9
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2018-08-14 18:16:37 +0200

    WIP: Add API to query content languages
    This new API module (action=query&meta=wikibasecontentlanguages) exposes
    the valid language codes known to Wikibase in various contexts, e. g.
    for the terms of an item or property, or for monolingual text values.
    
    TODO:
    * Add i18n messages
    * Add examples
    * Add tests
    
    Bug: T198202
    Change-Id: I46b99ea2e300107fb9fdb76cfbac5cfac0d25ca0

---
+++ repo/includes/Api/MetaContentLanguages.php
@@ -0,0 +1,135 @@
+<?php
+
+
+namespace Wikibase\Repo\Api;
+
+use ApiQuery;
+use ApiQueryBase;
+use Language;
+use Wikibase\Lib\ContentLanguages;
+
+/**
+ * @license GPL-2.0-or-later
+ */
+class MetaContentLanguages extends ApiQueryBase {
+
+	/**
+	 * @var ContentLanguages[]
+	 */
+	private $contentLanguages;
+
+	/**
+	 * @var bool
+	 */
+	private $expectKnownLanguageNames;
+
+	/**
+	 * @param array $contentLanguages associative array from contexts
+	 * to {@link ContentLanguage} objects
+	 * @param bool $expectKnownLanguageNames whether we should expect MediaWiki
+	 * to know a language name for any language code that occurs in the content languages
+	 * (if true, warnings will be raised for any language without known language name)
+	 * @param ApiQuery $queryModule
+	 * @param string $moduleName
+	 */
+	public function __construct(
+		array $contentLanguages,
+		$expectKnownLanguageNames,
+		ApiQuery $queryModule,
+		string $moduleName
+	) {
+		parent::__construct( $queryModule, $moduleName, 'wbcl' );
+		$this->contentLanguages = $contentLanguages;
+		$this->expectKnownLanguageNames = $expectKnownLanguageNames;
+	}
+
+	/**
+	 * @see ApiBase::execute
+	 */
+	public function execute() {
+		$params = $this->extractRequestParams();
+		$result = $this->getResult();
+
+		$context = $params['context'];
+		$contentLanguagesForContext = $this->contentLanguages[$context];
+		$languageCodes = $contentLanguagesForContext->getLanguages();
+
+		$props = $params['props'];
+		$includeCode = in_array( 'code', $props );
+		$includeAutonym = in_array( 'autonym', $props );
+		$includeName = in_array( 'name', $props );
+
+		foreach ( $languageCodes as $languageCode ) {
+			$path = [
+				$this->getQuery()->getModuleName(),
+				$this->getModuleName(),
+				$languageCode,
+			];
+
+			if ( $includeCode ) {
+				$result->addValue( $path, 'code', $languageCode );
+			}
+
+			if ( $includeAutonym ) {
+				$autonym = Language::fetchLanguageName( $languageCode );
+				if ( $autonym === '' ) {
+					$autonym = null;
+				}
+				$result->addValue( $path, 'autonym', $autonym );
+			}
+
+			if ( $includeName ) {
+				$userLanguageCode = $this->getLanguage()->getCode();
+				$name = Language::fetchLanguageName( $languageCode, $userLanguageCode );
+				if ( $name === '' ) {
+					if ( $this->expectKnownLanguageNames ) {
+						wfLogWarning( 'No name known for language ' . $languageCode );
+					}
+					$name = null;
+				}
+				$result->addValue( $path, 'name', $name );
+			}
+		}
+	}
+
+	/**
+	 * @see ApiQueryBase::getCacheMode
+	 *
+	 * @param array $params
+	 * @return string
+	 */
+	public function getCacheMode( $params ) {
+		return 'public';
+	}
+
+	/**
+	 * @see ApiBase::getAllowedParams
+	 */
+	protected function getAllowedParams() {
+		return [
+			'context' => [
+				self::PARAM_DFLT => 'term',
+				self::PARAM_TYPE => array_keys( $this->contentLanguages ),
+			],
+			'props' => [
+				self::PARAM_DFLT => 'code',
+				self::PARAM_ISMULTI => true,
+				self::PARAM_TYPE => [
+					'code',
+					'autonym',
+					'name',
+				],
+			]
+		];
+	}
+
+	/**
+	 * @see ApiBase::getExamplesMessages()
+	 */
+	protected function getExamplesMessages() {
+		// TODO
+		return [
+		];
+	}
+
+}
