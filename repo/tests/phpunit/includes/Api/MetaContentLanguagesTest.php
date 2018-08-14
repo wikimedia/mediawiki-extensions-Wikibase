1dc784c7308f84e57660650ea679601a852dd058
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2018-08-14 18:16:37 +0200

    Add API to query content languages
    This new API module (action=query&meta=wbcontentlanguages) exposes the
    valid language codes known to Wikibase in various contexts, e. g.  for
    the terms of an item or property, or for monolingual text values.
    
    Bug: T198202
    Change-Id: I46b99ea2e300107fb9fdb76cfbac5cfac0d25ca0

---
+++ repo/tests/phpunit/includes/Api/MetaContentLanguagesTest.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Wikibase\Repo\Tests\Api;
+
+use ApiMain;
+use ApiQuery;
+use FauxRequest;
+use Language;
+use PHPUnit\Framework\TestCase;
+use PHPUnit_Framework_Error_Warning;
+use RequestContext;
+use Wikibase\Lib\StaticContentLanguages;
+use Wikibase\Repo\Api\MetaContentLanguages;
+
+/**
+ * @covers \Wikibase\Repo\Api\MetaContentLanguages
+ *
+ * @group API
+ * @group Wikibase
+ * @group WikibaseAPI
+ *
+ * @license GPL-2.0-or-later
+ */
+class MetaContentLanguagesTest extends TestCase {
+
+	/**
+	 * @dataProvider provideParamsAndExpectedResults
+	 */
+	public function testExecute( array $params, array $expectedResults ) {
+		$query = $this->getQuery( $params );
+		$api = new MetaContentLanguages(
+			$this->getContentLanguages(),
+			false,
+			$query,
+			'wbcontentlanguages'
+		);
+
+		$api->execute();
+		$apiResult = $api->getResult();
+		$results = $apiResult->getResultData()['query']['wbcontentlanguages'];
+
+		$this->assertSame( $expectedResults, $results );
+	}
+
+	/**
+	 * @expectedException PHPUnit_Framework_Error_Warning
+	 */
+	public function testExecute_warnsAboutUnknownLanguageNames() {
+		$query = $this->getQuery( [ 'wbclprop' => 'name' ] );
+		$api = new MetaContentLanguages(
+			[ 'term' => new StaticContentLanguages( [ 'unknown' ] ) ],
+			true,
+			$query,
+			'wbcontentlanguages'
+		);
+
+		$api->execute();
+	}
+
+	/**
+	 * @param array $params
+	 * @return ApiQuery
+	 */
+	private function getQuery( array $params ): ApiQuery {
+		$context = new RequestContext();
+		$context->setLanguage( 'de' );
+		$context->setRequest( new FauxRequest( $params ) );
+		$main = new ApiMain( $context );
+		$query = new ApiQuery( $main, 'query' );
+
+		return $query;
+	}
+
+	private function getContentLanguages() {
+		return [
+			'term' => new StaticContentLanguages( [ 'en', 'de', 'es' ] ),
+			'test' => new StaticContentLanguages( [ 'en', 'mis', 'und' ] ),
+		];
+	}
+
+	public function provideParamsAndExpectedResults() {
+		yield 'default' => [
+			[],
+			[
+				'en' => [ 'code' => 'en' ],
+				'de' => [ 'code' => 'de' ],
+				'es' => [ 'code' => 'es' ],
+			],
+		];
+
+		yield 'test context, with autonyms' => [
+			[ 'wbclcontext' => 'test', 'wbclprop' => 'code|autonym' ],
+			[
+				'en' => [ 'code' => 'en', 'autonym' => 'English' ],
+				'mis' => [ 'code' => 'mis', 'autonym' => null ],
+				'und' => [ 'code' => 'und', 'autonym' => null ],
+			],
+		];
+
+		$haveCldr = Language::fetchLanguageName( 'en', 'de' ) === 'Englisch';
+		yield 'test context, with language names' => [
+			[ 'wbclcontext' => 'test', 'wbclprop' => 'name' ],
+			[
+				'en' => [ 'name' => $haveCldr ? 'Englisch' : 'English' ],
+				'mis' => [ 'name' => $haveCldr ? 'nicht unterstützte Sprache' : null ],
+				'und' => [ 'name' => $haveCldr ? 'Unbekannte Sprache' : null ],
+			],
+		];
+	}
+
+}
