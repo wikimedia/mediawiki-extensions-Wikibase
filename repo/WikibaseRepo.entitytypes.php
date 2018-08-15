8744ffb80f763d3650c60cc6d3e0ca85f9587d22
Stanislav Malyshev <smalyshev@gmail.com>
2018-08-15 15:22:12 -0700

    Move field definitions to search-field-definitions handler
    This allows to override this code and ultimately move it to
    a separate extension. Extensions like WikibaseLexeme and MediaInfo
    have to be updated too.
    
    Bug: T190022
    Change-Id: Ifca67d6a7fb7498690d97aeb5f5d22b3ffffbe53

--- repo/WikibaseRepo.entitytypes.php
+++ repo/WikibaseRepo.entitytypes.php
@@ -21,6 +21,7 @@ use Wikibase\DataModel\Entity\Item;
 use Wikibase\DataModel\Entity\Property;
 use Wikibase\DataModel\SerializerFactory;
 use Wikibase\DataModel\Services\EntityId\EntityIdFormatter;
+use Wikibase\DataModel\Services\Lookup\InProcessCachingDataTypeLookup;
 use Wikibase\DataModel\Services\Lookup\LabelDescriptionLookup;
 use Wikibase\Lib\LabelsProviderEntityIdHtmlLinkFormatter;
 use Wikibase\Repo\Diff\BasicEntityDiffVisualizer;
@@ -39,7 +40,13 @@ use Wikibase\Repo\EntityReferenceExtractors\EntityReferenceExtractorCollection;
 use Wikibase\Repo\EntityReferenceExtractors\SiteLinkBadgeItemReferenceExtractor;
 use Wikibase\Repo\EntityReferenceExtractors\StatementEntityReferenceExtractor;
 use Wikibase\Repo\Hooks\Formatters\DefaultEntityLinkFormatter;
+use Wikibase\Repo\Search\Elastic\Fields\DescriptionsProviderFieldDefinitions;
+use Wikibase\Repo\Search\Elastic\Fields\ItemFieldDefinitions;
+use Wikibase\Repo\Search\Elastic\Fields\LabelsProviderFieldDefinitions;
+use Wikibase\Repo\Search\Elastic\Fields\PropertyFieldDefinitions;
+use Wikibase\Repo\Search\Elastic\Fields\StatementProviderFieldDefinitions;
 use Wikibase\Repo\WikibaseRepo;
+use Wikibase\SettingsArray;
 use Wikibase\View\EditSectionGenerator;
 use Wikibase\View\EntityTermsView;
 use Wikibase\View\FingerprintableEntityMetaTagsCreator;
@@ -126,6 +133,19 @@ return [
 				$entityIdFormatter,
 				$basicEntityDiffVisualizer
 			);
+		},
+		'search-field-definitions' => function ( array $languageCodes, SettingsArray $searchSettings ) {
+			$repo = WikibaseRepo::getDefaultInstance();
+			return new ItemFieldDefinitions( [
+				new LabelsProviderFieldDefinitions( $languageCodes ),
+				new DescriptionsProviderFieldDefinitions( $languageCodes,
+					$searchSettings->getSetting( 'entitySearch' ) ),
+				StatementProviderFieldDefinitions::newFromSettings(
+					new InProcessCachingDataTypeLookup( $repo->getPropertyDataTypeLookup() ),
+					$repo->getDataTypeDefinitions()->getSearchIndexDataFormatterCallbacks(),
+					$searchSettings
+				)
+			] );
 		},
 		'entity-search-callback' => function ( WebRequest $request ) {
 			// FIXME: this code should be split into extension for T190022
@@ -240,6 +260,19 @@ return [
 				$vocabulary,
 				$writer
 			);
+		},
+		'search-field-definitions' => function ( array $languageCodes, SettingsArray $searchSettings ) {
+			$repo = WikibaseRepo::getDefaultInstance();
+			return new PropertyFieldDefinitions( [
+				new LabelsProviderFieldDefinitions( $languageCodes ),
+				new DescriptionsProviderFieldDefinitions( $languageCodes,
+					$searchSettings->getSetting( 'entitySearch' ) ),
+				StatementProviderFieldDefinitions::newFromSettings(
+					new InProcessCachingDataTypeLookup( $repo->getPropertyDataTypeLookup() ),
+					$repo->getDataTypeDefinitions()->getSearchIndexDataFormatterCallbacks(),
+					$searchSettings
+				)
+			] );
 		},
 		'entity-search-callback' => function ( WebRequest $request ) {
 			// FIXME: this code should be split into extension for T190022
