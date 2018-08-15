c80702583ba50b0d1b73f15437e7201a46e9254e
Stanislav Malyshev <smalyshev@gmail.com>
2018-08-15 15:22:12 -0700

    Move field definitions to search-field-definitions handler
    This allows to override this code and ultimately move it to
    a separate extension. Extensions like WikibaseLexeme and MediaInfo
    have to be updated too.
    
    Bug: T190022
    Change-Id: Ifca67d6a7fb7498690d97aeb5f5d22b3ffffbe53

---
+++ repo/includes/Search/Elastic/Fields/NoFieldDefinitions.php
@@ -0,0 +1,15 @@
+<?php
+namespace Wikibase\Repo\Search\Elastic\Fields;
+
+/**
+ * Class for empty field definitions
+ */
+class NoFieldDefinitions implements FieldDefinitions {
+
+	/**
+	 * Get the list of definitions
+	 */
+	public function getFields() {
+		return [];
+	}
+}
