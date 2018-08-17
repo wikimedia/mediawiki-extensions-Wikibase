f7913019da636d2ff240d05ea952b34ae5f5dc1d
Erik Bernhardson <ebernhardson@wikimedia.org>
2018-08-17 15:10:33 -0700

    Search: transform and createEmpty must return same type
    Various code expects that the results of transformElasticsearchResult
    and createEmptyResult return the same kind of thing.
    
    Bug: T201360
    Change-Id: Ifc8d7475428c07a9ac56f1fa6ac7bcbd94b1adcb

--- tests/phan/stubs/cirrussearch.php
+++ tests/phan/stubs/cirrussearch.php
@@ -249,6 +249,9 @@ namespace CirrusSearch\Search {
 	class ResultSet {
 	}
 
+	class EmptyResultSet extends ResultSet {
+	}
+
 	interface ResultsType {
 		/**
 		 * @return false|string|array corresponding to Elasticsearch source filtering syntax
