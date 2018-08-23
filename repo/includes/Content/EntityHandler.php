8fe834d3eaf6c56bb2772791742f93f320a69774
Gergő Tisza <tgr.huwiki@gmail.com>
2018-08-23 14:39:53 +0200

    Handle mcrundo action
    Bug: T200216
    Change-Id: Iedd9bf6c057e8b396a575bab700b15bd38b32cc9

--- repo/includes/Content/EntityHandler.php
+++ repo/includes/Content/EntityHandler.php
@@ -542,27 +542,25 @@ abstract class EntityHandler extends ContentHandler {
 	/**
 	 * @see ContentHandler::getUndoContent
 	 *
-	 * @param Revision $latestRevision The current text
-	 * @param Revision $newerRevision The revision to undo
-	 * @param Revision $olderRevision Must be an earlier revision than $undo
+	 * @param Revision|EntityContent $latest The current text
+	 * @param Revision|EntityContent $newer The revision to undo
+	 * @param Revision|EntityContent $older Must be an earlier revision than $newer
+	 * @param bool $undoIsLatest Set to true if $newer is from the current revision (since 1.32)
 	 *
 	 * @return Content|bool Content on success, false on failure
 	 */
-	public function getUndoContent(
-		Revision $latestRevision,
-		Revision $newerRevision,
-		Revision $olderRevision
-	) {
-		/**
-		 * @var EntityContent $latestContent
-		 * @var EntityContent $newerContent
-		 * @var EntityContent $olderContent
-		 */
-		$latestContent = $latestRevision->getContent();
-		$newerContent = $newerRevision->getContent();
-		$olderContent = $olderRevision->getContent();
+	public function getUndoContent( $latest, $newer, $older, $undoIsLatest = false ) {
+		$latestContent = ( $latest instanceof Revision ) ? $latest->getContent() : $latest;
+		$newerContent = ( $newer instanceof Revision ) ? $newer->getContent() : $newer;
+		$olderContent = ( $older instanceof Revision ) ? $older->getContent() : $older;
+		Assert::parameterType( Revision::class . '|' . EntityContent::class, $latest, '$latest' );
+		Assert::parameterType( Revision::class . '|' . EntityContent::class, $newer, '$newer' );
+		Assert::parameterType( Revision::class . '|' . EntityContent::class, $older, '$older' );
+		if ( $latest instanceof Revision && $newer instanceof Revision ) {
+			$undoIsLatest = ( $latest->getId() === $newer->getId() );
+		}
 
-		if ( $latestRevision->getId() === $newerRevision->getId() ) {
+		if ( $undoIsLatest ) {
 			// no patching needed, just roll back
 			return $olderContent;
 		}
