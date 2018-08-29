ea5571546eaab39b825226c31851b749cfb7578f
addshore <addshorewiki@gmail.com>
2018-08-29 08:27:55 +0100

    Introduce EntityDocumentView interface
    This is an alternative to Ia69220d91 & I828e29103e
    which change the visibility of various EntityView properties
    for use by WikibaseMediaInfo.
    
    This new interface could also be used by WikibaseLexeme.
    
    Bug tagged as the realtes WikibaseMediaInfo ticket.
    
    In the future it would be good to renmove the labels/description
    /aliases specific stuff from EntityView and perhaps kill EntityView
    all together.
    
    The Lexeme Experience team were recently looking at getTitle from
    EntityView so it would be good to have them review this ticket and
    add some thoughts.
    
    Bug: T202287
    Change-Id: I84dd340aafc3dffd988c1456bf0d80995cd3cb65

---
+++ view/src/EntityDocumentView.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Wikibase\View;
+
+use Wikibase\DataModel\Entity\EntityDocument;
+
+/**
+ * Interface for creating views for all different kinds of Wikibase\DataModel\Entity\EntityDocument.
+ *
+ * @license GPL-2.0-or-later
+ */
+interface EntityDocumentView {
+
+	/**
+	 * Builds and returns the HTML representing a whole WikibaseEntity.
+	 *
+	 * @param EntityDocument $entity the entity to render
+	 *
+	 * @return string HTML
+	 */
+	public function getHtml( EntityDocument $entity );
+
+	/**
+	 * Returns the html used for the title of the page.
+	 * @see ParserOutput::setDisplayTitle
+	 *
+	 * @param EntityDocument $entity
+	 *
+	 * @return string HTML
+	 */
+	public function getTitleHtml( EntityDocument $entity );
+
+}
