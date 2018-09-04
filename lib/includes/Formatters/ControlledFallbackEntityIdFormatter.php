ea5ba973b1ad48936b59e4d4f90b431ad46d08fc
Aleksey Bekh-Ivanov (WMDE) <aleksey.bekh-ivanov@wikimedia.de>
2018-09-04 13:16:10 +0200

    Track new ItemId formatter usages
    Change-Id: If4009636d21d076c7a5e377ea2419ae21221fdeb

--- lib/includes/Formatters/ControlledFallbackEntityIdFormatter.php
+++ lib/includes/Formatters/ControlledFallbackEntityIdFormatter.php
@@ -2,11 +2,13 @@
 
 namespace Wikibase\Lib\Formatters;
 
+use Liuggio\StatsdClient\Factory\StatsdDataFactoryInterface;
 use Psr\Log\LoggerAwareTrait;
 use Psr\Log\NullLogger;
 use Wikibase\DataModel\Entity\EntityId;
 use Wikibase\DataModel\Entity\Int32EntityId;
 use Wikibase\DataModel\Services\EntityId\EntityIdFormatter;
+use Wikimedia\Assert\Assert;
 
 /**
  * Wrapper class giving ability to replace EntityIdFormatter in production in a controlled manner.
@@ -35,41 +37,80 @@ class ControlledFallbackEntityIdFormatter implements EntityIdFormatter {
 	private $fallbackFormatter;
 
 	/**
+	 * @var StatsdDataFactoryInterface
+	 */
+	private $statsdDataFactory;
+
+	/**
+	 * @var string
+	 */
+	private $statsPrefix;
+
+	/**
 	 * @param int $maxEntityId
 	 * @param EntityIdFormatter $targetFormatter
 	 * @param EntityIdFormatter $fallbackFormatter
+	 * @param StatsdDataFactoryInterface $statsdDataFactory
+	 * @param string $statsPrefix
 	 */
 	public function __construct(
 		$maxEntityId,
 		EntityIdFormatter $targetFormatter,
-		EntityIdFormatter $fallbackFormatter
+		EntityIdFormatter $fallbackFormatter,
+		StatsdDataFactoryInterface $statsdDataFactory,
+		$statsPrefix
 	) {
+		Assert::parameterType( 'string', $statsPrefix, '$statsPrefix' );
+
 		$this->maxEntityId = $maxEntityId;
 		$this->targetFormatter = $targetFormatter;
 		$this->fallbackFormatter = $fallbackFormatter;
 		$this->logger = new NullLogger();
+		$this->statsdDataFactory = $statsdDataFactory;
+		$this->statsPrefix = $statsPrefix;
 	}
 
 	public function formatEntityId( EntityId $value ) {
 		if ( $value instanceof Int32EntityId && $value->getNumericId() <= $this->maxEntityId ) {
 			try {
-				return $this->targetFormatter->formatEntityId( $value );
+				$formatEntityId = $this->targetFormatter->formatEntityId( $value );
+				$this->statsdDataFactory->increment( $this->statsPrefix . 'targetFormatterCalled' );
+				return $formatEntityId;
 			} catch ( \Exception $e ) { //TODO: Catch Throwable once we move to php7
-				$this->logger->critical(
-					'Failed to format entity ID. Using fallback formatter.'
-					. ' Error: {exception_message}',
-					[
-						'entityId' => $value->getSerialization(),
-						'exception' => $e,
-						'exception_message' => $e->getMessage(),
-					]
-				);
+				$this->logTargetFormatterFailure( $value, $e );
 
-				return $this->fallbackFormatter->formatEntityId( $value );
+				return $this->formatUsingFallbackFormatter( $value );
 			}
 		}
 
-		return $this->fallbackFormatter->formatEntityId( $value );
+		return $this->formatUsingFallbackFormatter( $value );
+	}
+
+	/**
+	 * @param EntityId $value
+	 * @return string
+	 */
+	private function formatUsingFallbackFormatter( EntityId $value ) {
+		$formatEntityId = $this->fallbackFormatter->formatEntityId( $value );
+		$this->statsdDataFactory->increment( $this->statsPrefix . 'fallbackFormatterCalled' );
+		return $formatEntityId;
+	}
+
+	/**
+	 * @param EntityId $value
+	 * @param \Exception $e
+	 */
+	private function logTargetFormatterFailure( EntityId $value, \Exception $e ) {
+		$this->logger->critical(
+			'Failed to format entity ID. Using fallback formatter.'
+			. ' Error: {exception_message}',
+			[
+				'entityId' => $value->getSerialization(),
+				'exception' => $e,
+				'exception_message' => $e->getMessage(),
+			]
+		);
+		$this->statsdDataFactory->increment( $this->statsPrefix . 'targetFormatterFailed' );
 	}
 
 }
