9167ef1077ba2d3c4ae3b6bc627d5ab445a6444b
Stanislav Malyshev <smalyshev@gmail.com>
2018-09-05 13:43:31 -0700

    Add phrase rescoring to queries
    Bug: T163642
    Change-Id: I4abe146d7d7c5d833643aab450ab6623b1715607

--- repo/config/EntitySearchProfiles.php
+++ repo/config/EntitySearchProfiles.php
@@ -15,6 +15,8 @@ return [
 			'fallback-folded'   => 0.005,
 			'fallback-partial'  => 0.03,
 			'fallback-discount' => 0.1,
+			'phrase-all'        => 0.001,
+			'phrase-plain'      => 0.1,
 		]
 	],
 ];
