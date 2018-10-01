b1189b3dcd5045f95b1b7b4f2037f86c45148604
addshore <addshorewiki@gmail.com>
2018-10-01 11:37:12 +0100

    MCR: Introduce PageTableEntityQuery & Implementation
    TODO:
     - Test for EntityIdLocalPartPageTableEntityQuery
    
    Bug: T194729
    Change-Id: I694117e8a8f16f992cce9d1dc41fd969261ed135

---
+++ lib/includes/Store/Sql/PageTableEntityQuery.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Wikibase\Lib\Store\Sql;
+
+use Wikibase\DataModel\Entity\EntityId;
+use Wikimedia\Rdbms\IDatabase;
+
+/**
+ * Interface to run a query to find an entity of given ID within the mediawiki page table
+ * and also map resulting rows back to the entity IDs they relate to.
+ *
+ * @license GPL-2.0-or-later
+ */
+interface PageTableEntityQuery {
+
+	/**
+	 * @param array $fields Fields to select
+	 * @param array $tables Tables to use
+	 * @param array $joins Joins to use
+	 * @param EntityId[] $entityIds EntityIds to select
+	 * @param IDatabase $db DB to query on
+	 * @return array of rows with keys of their entity ID serializations
+	 */
+	public function selectRows(
+		array $fields,
+		array $tables,
+		array $joins,
+		array $entityIds,
+		IDatabase $db
+	);
+
+}
