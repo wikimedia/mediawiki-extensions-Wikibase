469b292b6fe03e38d075cf57750605f300bc4c09
addshore <addshorewiki@gmail.com>
2018-10-01 11:37:12 +0100

    MCR: Introduce PageTableEntityQueryHelper
    This allows MediaInfo to avoid extending our class accross an extension
    boundary. Instead MediaInfo can implement this interface and
    use our standard WikiPageEntityMetaDataLookup class.
    
    Bug: T194729
    Change-Id: I694117e8a8f16f992cce9d1dc41fd969261ed135

---
+++ repo/tests/phpunit/includes/Store/Sql/EntityIdLocalPartPageTableEntityQueryHelperTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Wikibase\Repo\Tests\Store\Sql;
+
+use ArrayObject;
+use MediaWiki\Storage\NameTableStore;
+use PHPUnit\Framework\TestCase;
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\Lib\Store\EntityNamespaceLookup;
+use Wikibase\Lib\Store\Sql\EntityIdLocalPartPageTableEntityQueryHelper;
+
+/**
+ * @group Wikibase
+ * @group WikibaseRepo
+ * @license GPL-2.0-or-later
+ */
+class EntityIdLocalPartPageTableEntityQueryHelperTest extends TestCase {
+
+	/**
+	 * @param string $type
+	 * @param string $localPart
+	 * @return EntityId
+	 */
+	private function getMockEntityId( $type, $localPart ) {
+		$id = $this->prophesize( EntityId::class );
+		$id->getLocalPart()->willReturn( $localPart );
+		$id->getEntityType()->willReturn( $type );
+		return $id->reveal();
+	}
+
+	public function testGetQueryInfo() {
+		$slotRoleStore = $this->prophesize( NameTableStore::class );
+		$slotRoleStore->getId( 'otherSlot' )->willReturn( 76 );
+
+		$helper = new EntityIdLocalPartPageTableEntityQueryHelper(
+			new EntityNamespaceLookup( [ 'item' => 0, 'other' => 2 ], [ 'other' => 'otherSlot' ] ),
+			$slotRoleStore->reveal()
+		);
+
+		$queryInfo = $helper->getQueryInfo(
+			[ $this->getMockEntityId( 'item', 'Q1' ), $this->getMockEntityId( 'other', 'ooo123' ) ],
+			wfGetDB( DB_REPLICA )
+		);
+
+		$this->assertEquals(
+			[
+				"(page_title = 'Q1' AND page_namespace = '0') OR (page_title = 'ooo123' AND page_namespace = '2' AND slot_role_id = '76')",
+				[
+					'slots' => [
+						'INNER JOIN',
+						'page_latest=slot_revision_id'
+					]
+				],
+				[ 'page_title' ]
+			],
+			$queryInfo
+		);
+	}
+
+	public function testMapRowsToEntityIds() {
+		$helper = new EntityIdLocalPartPageTableEntityQueryHelper(
+			new EntityNamespaceLookup( [] ),
+			$this->prophesize( NameTableStore::class )->reveal()
+		);
+
+		$rows = [
+			(object)[ 'page_title' => 'Q1' ],
+			(object)[ 'page_title' => 'U7' ],
+		];
+
+		$mappedRows = $helper->mapRowsToEntityIds( new ArrayObject( $rows ) );
+
+		$this->assertEquals(
+			[ 'Q1' => $rows[0], 'U7' => $rows[1] ],
+			$mappedRows
+		);
+	}
+
+}
