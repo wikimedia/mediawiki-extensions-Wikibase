995696ffed140c72abfca45011b05b17ef81d428
Aryeh Gregor <ayg@aryeh.name>
2018-10-02 19:46:39 +0300

    Don't directly access members of Language
    This is needed to prevent breaking when LanguageFactory lands.
    
    Change-Id: I8d67fb41c79c9e3aa57cecad27d8e2a75bcef35a

--- repo/tests/phpunit/includes/Parsers/MwTimeIsoParserTest.php
+++ repo/tests/phpunit/includes/Parsers/MwTimeIsoParserTest.php
@@ -4,6 +4,7 @@ namespace Wikibase\Repo\Tests\Parsers;
 
 use DataValues\TimeValue;
 use Language;
+use LanguageFactory;
 use PHPUnit4And6Compat;
 use ValueParsers\ParserOptions;
 use ValueParsers\ValueParser;
@@ -39,15 +40,23 @@ class MwTimeIsoParserTest extends StringValueParserTest {
 	protected function setUp() {
 		parent::setUp();
 
-		// We don't have control over object instantiation, but
-		// need this in order to test with some staged messages.
-		Language::$mLangObjCache['es'] = $this->getLanguage();
+		if ( class_exists( LanguageFactory::class ) ) {
+			$stub = $this->createMock( LanguageFactory::class );
+			$stub->method( 'get' )->willReturn( $this->getLanguage() );
+			$this->setService( 'LanguageFactory', $stub );
+		} else {
+			// Back-compat code, remove once LanguageFactory lands
+			Language::$mLangObjCache['es'] = $this->getLanguage();
+		}
 	}
 
 	protected function tearDown() {
-		parent::tearDown();
+		if ( !class_exists( LanguageFactory::class ) ) {
+			// Back-compat code, remove once LanguageFactory lands
+			unset( Language::$mLangObjCache['es'] );
+		}
 
-		unset( Language::$mLangObjCache['es'] );
+		parent::tearDown();
 	}
 
 	private function getLanguage() {
