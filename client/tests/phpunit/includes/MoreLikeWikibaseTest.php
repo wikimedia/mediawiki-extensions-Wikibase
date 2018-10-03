c8f117dc6d7ff9e7fa2eb5b3601ccbf70b787d1e
Stanislav Malyshev <smalyshev@gmail.com>
2018-10-03 16:19:20 -0700

    Make Wikibase Client create wikibase_item field
    Bug: T154447
    Change-Id: I2b638ab78b039b869f4d060ae3e8dd5f8a838690

---
+++ client/tests/phpunit/includes/MoreLikeWikibaseTest.php
@@ -0,0 +1,45 @@
+<?php
+namespace Wikibase\Client\Tests;
+
+use CirrusSearch;
+use CirrusSearch\Query\MoreLikeFeatureTest;
+
+// Not happy about this, don't have better idea yet.
+if ( !class_exists( CirrusSearch::class ) ) {
+	return;
+}
+
+/**
+ * Test for morelikewithwikibase feature.
+ *
+ * Reuses MoreLikeFeatureTest but with different data.
+ * @covers \Wikibase\Client\MoreLikeWikibase
+ */
+class MoreLikeWikibaseTest extends MoreLikeFeatureTest {
+	public function applyProvider() {
+		return [
+			'single page morelike w/wikibase' => [
+				'morelikewithwikibase:Some page',
+				( new \Elastica\Query\BoolQuery() )
+					->addFilter( new \Elastica\Query\Exists( 'wikibase_item' ) )
+					->addMust( ( new \Elastica\Query\MoreLikeThis() )
+						->setParams( [
+							'min_doc_freq' => 2,
+							'max_doc_freq' => null,
+							'max_query_terms' => 25,
+							'min_term_freq' => 2,
+							'min_word_length' => 0,
+							'max_word_length' => 0,
+							'minimum_should_match' => '30%',
+						] )
+						->setFields( [ 'text' ] )
+						->setLike( [
+							[ '_id' => '12345' ],
+						] )
+					),
+				true,
+			]
+		];
+	}
+
+}
