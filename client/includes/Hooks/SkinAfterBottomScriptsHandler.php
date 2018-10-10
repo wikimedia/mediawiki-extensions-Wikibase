de3fb0fa42f83da5cce3d5074692a086c10eeb53
Stephen Niedzielski <stephen@niedzielski.com>
2018-10-09 20:38:12 -0600

    WIP: Update: add schema
    Bug: T198946
    Change-Id: I02bc43f16cc5308b2613d1cc441df8232a055dbb

---
+++ client/includes/Hooks/SkinAfterBottomScriptsHandler.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Wikibase\Client\Hooks;
+
+use HTML;
+use Title;
+
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\Client\RepoLinker;
+
+/**
+ * @license GPL-2.0-or-later
+ */
+class SkinAfterBottomScriptsHandler {
+	/**
+	 * @var RepoLinker
+	 */
+	private $repoLinker;
+
+	public function __construct( RepoLinker $repoLinker ) {
+		$this->repoLinker = $repoLinker;
+	}
+
+	/**
+	 * @param string &$html
+	 * @param Title $title
+	 * @param EntityDocument $entity
+	 */
+	public function addSchema( &$html, Title $title, EntityId $entityId ) {
+		$schema = [
+			'@type' => 'schema:Article',
+			'name' => $title->getText(),
+			'url' => $title->getFullURL(),
+			'sameAs' => [ $this->repoLinker->getCanonicalEntityUrl( $entityId ) ],
+		];
+
+		$html .= Html::openElement( 'script', [ 'type' => 'application/ld+json' ] );
+		$html .= json_encode( $schema );
+		$html .= Html::closeElement( 'script' );
+
+		return true;
+	}
+
+}
