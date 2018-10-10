b50bd98b94cea1c5d06f2072f4d7c50b852cc5b6
Stephen Niedzielski <stephen@niedzielski.com>
2018-10-09 20:38:12 -0600

    Update: add schema to main namespace pages for SEO
    Add a JSON-LD schema to main namespace pages for search engine
    optimization. The pretty-printed addition looks like:
    
      <script type="application/ld+json">
      {
        "@type": "schema:Article",
        "name": "Douglas Adams",
        "url": "https://de.wikipedia.org/wiki/Douglas_Adams",
        "sameAs": ["https://www.wikidata.org/wiki/Q42"]
      }
      </script>
    
    And is placed towards the end of the page in an inlined body script tag.
    The schema is interpreted without error by the Google Structured Data
    Testing Tool[0].
    
    The new schema can be enabled per namespace by configuring the
    correspondingly new schemaNamespaces option. The default configuration
    is disabled (empty).
    
    [0] https://search.google.com/structured-data/testing-tool/u/0/
    
    Bug: T198946
    Change-Id: I02bc43f16cc5308b2613d1cc441df8232a055dbb

---
+++ client/includes/Hooks/SkinAfterBottomScriptsHandler.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Wikibase\Client\Hooks;
+
+use HTML;
+use Title;
+
+use Wikibase\Client\RepoLinker;
+use Wikibase\DataModel\Entity\EntityId;
+
+/**
+ * @license GPL-2.0-or-later
+ */
+class SkinAfterBottomScriptsHandler {
+	/**
+	 * @var RepoLinker
+	 */
+	private $repoLinker;
+
+	public function __construct( RepoLinker $repoLinker ) {
+		$this->repoLinker = $repoLinker;
+	}
+
+	/**
+	 * @param Title $title
+	 * @param EntityId $entityId
+	 * @return string
+	 */
+	public function createSchema( Title $title, EntityId $entityId ) {
+		$schema = [
+			'@type' => 'schema:Article',
+			'name' => $title->getText(),
+			'url' => $title->getFullURL( '', false, PROTO_CANONICAL ),
+			'sameAs' => [ $this->repoLinker->getEntityUrl( $entityId ) ],
+		];
+
+		$html = Html::openElement( 'script', [ 'type' => 'application/ld+json' ] );
+		$html .= json_encode( $schema );
+		$html .= Html::closeElement( 'script' );
+		return $html;
+	}
+
+}
