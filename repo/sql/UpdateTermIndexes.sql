f77e01649613d02de17c102e9491eb457f1a91aa
Leszek Manicki <leszek.manicki@wikimedia.de>
2018-10-12 17:31:47 +0200

    Replaced text_term SQL index with wb_terms_text
    Making wb_terms indexes match the ones
    existing on the production database, part 3.
    
    Bug: T204837
    
    Change-Id: Ifc42858abc212eb83d09536d62ee69a9cfa60a64

--- repo/sql/UpdateTermIndexes.sql
+++ repo/sql/UpdateTermIndexes.sql
@@ -2,19 +2,12 @@
 DROP INDEX /*i*/wb_terms_entity_type ON /*_*/wb_terms;
 DROP INDEX /*i*/wb_terms_language ON /*_*/wb_terms;
 DROP INDEX /*i*/wb_terms_type ON /*_*/wb_terms;
-DROP INDEX /*i*/wb_terms_text ON /*_*/wb_terms;
 DROP INDEX /*i*/wb_terms_search_key ON /*_*/wb_terms;
 
 -- Indexes and comments below adopted from the suggestions Sean Pringle made
 -- at https://phabricator.wikimedia.org/T47529#518941 based on a
 -- live analysis of queries on wikidata.org in January 2014.
 -- NOTE: keep these in sync with Wikibase.sql
-
--- When any wb_terms query includes a search on term_text greater than
--- four or five leading characters a simple index on term_text and
--- language is often better than the proposed composite indexes. Note
--- that MariaDB still uses the entire key length even with LIKE '...%' on term_text.
-CREATE INDEX /*i*/term_text ON /*_*/wb_terms (term_text, term_language);
 
 -- Same idea as above for terms_search_key (for normalized/insensitive matches).
 CREATE INDEX /*i*/term_search_key ON /*_*/wb_terms (term_search_key, term_language);
