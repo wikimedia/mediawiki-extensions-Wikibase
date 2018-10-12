ecc67c8b9f4f48c4e57f55fc3b4a24c1ddddd89b
Leszek Manicki <leszek.manicki@wikimedia.de>
2018-10-12 17:07:31 +0200

    Removed no longer needed indexes from UpdateTermIndexes.sql
    Those two have apparently now only be added for a second,
    to be dropped seconds later in DropNotFullEntityIdTermIndexes.sql.
    
    This clean up makes it easier to reason about other indexes
    that are supposed to be adjusted on the course of T85414.
    
    Change-Id: I82fdac409eac28508a492e5da54beee6e675d56a

--- repo/sql/UpdateTermIndexes.sql
+++ repo/sql/UpdateTermIndexes.sql
@@ -11,8 +11,9 @@ DROP INDEX /*i*/wb_terms_search_key ON /*_*/wb_terms;
 -- live analysis of queries on wikidata.org in January 2014.
 -- NOTE: keep these in sync with Wikibase.sql
 
+-- terms_entity is removed down the update chian, so don't bother adding it in the first place.
 -- Some wb_terms queries use term_entity_id=N which is good selectivity.
-CREATE INDEX /*i*/term_entity ON /*_*/wb_terms (term_entity_id);
+-- CREATE INDEX /*i*/term_entity ON /*_*/wb_terms (term_entity_id);
 
 -- When any wb_terms query includes a search on term_text greater than
 -- four or five leading characters a simple index on term_text and
@@ -23,5 +24,6 @@ CREATE INDEX /*i*/term_text ON /*_*/wb_terms (term_text, term_language);
 -- Same idea as above for terms_search_key (for normalized/insensitive matches).
 CREATE INDEX /*i*/term_search_key ON /*_*/wb_terms (term_search_key, term_language);
 
+-- term_search is removed down the update chian, so don't bother adding it in the first place.
 -- This index has good selectivity while still allowing ICP for short string values.
-CREATE INDEX /*i*/term_search ON /*_*/wb_terms (term_language, term_entity_id, term_type, term_search_key(16));
+-- CREATE INDEX /*i*/term_search ON /*_*/wb_terms (term_language, term_entity_id, term_type, term_search_key(16));
