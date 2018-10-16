7fad4c21152e908d5255d5159177a57186b969a0
Jakob Warkotsch <j.warkotsch@gmail.com>
2018-10-16 16:07:48 +0200

    Remove item and property specific code from Entityview
    Change-Id: Iba7293af994a87c93a98e6fb29d7422fd3483331

--- view/src/EntityView.php
+++ view/src/EntityView.php
@@ -3,14 +3,10 @@
 namespace Wikibase\View;
 
 use Wikibase\DataModel\Entity\EntityDocument;
-use Wikibase\DataModel\Term\AliasesProvider;
-use Wikibase\DataModel\Term\DescriptionsProvider;
-use Wikibase\DataModel\Term\LabelsProvider;
 use Wikibase\View\Template\TemplateFactory;
 
 /**
  * Base class for creating views for all different kinds of Wikibase\DataModel\Entity\EntityDocument.
- * This class provides some help for EntityDocuments that are also Labels/Descriptions/AliasesProviders.
  * If you want a simple interface instead see EntityDocumentView.
  *
  * @license GPL-2.0-or-later
@@ -21,11 +17,6 @@ abstract class EntityView implements EntityDocumentView {
 	 * @var TemplateFactory
 	 */
 	protected $templateFactory;
-
-	/**
-	 * @var EntityTermsView
-	 */
-	private $entityTermsView;
 
 	/**
 	 * @var LanguageDirectionalityLookup
@@ -39,17 +30,14 @@ abstract class EntityView implements EntityDocumentView {
 
 	/**
 	 * @param TemplateFactory $templateFactory
-	 * @param EntityTermsView $entityTermsView
 	 * @param LanguageDirectionalityLookup $languageDirectionalityLookup
 	 * @param string $languageCode
 	 */
 	public function __construct(
 		TemplateFactory $templateFactory,
-		EntityTermsView $entityTermsView,
 		LanguageDirectionalityLookup $languageDirectionalityLookup,
 		$languageCode
 	) {
-		$this->entityTermsView = $entityTermsView;
 		$this->languageDirectionalityLookup = $languageDirectionalityLookup;
 		$this->languageCode = $languageCode;
 
@@ -87,15 +75,7 @@ abstract class EntityView implements EntityDocumentView {
 	 *
 	 * @return string HTML
 	 */
-	public function getTitleHtml( EntityDocument $entity ) {
-		if ( $entity instanceof LabelsProvider ) {
-			return $this->entityTermsView->getTitleHtml(
-				$entity->getId()
-			);
-		}
-
-		return '';
-	}
+	abstract public function getTitleHtml( EntityDocument $entity );
 
 	/**
 	 * Builds and returns the HTML to be put into the main container of an entity's HTML structure.
@@ -114,28 +94,5 @@ abstract class EntityView implements EntityDocumentView {
 	 * @return string HTML
 	 */
 	abstract protected function getSideHtml( EntityDocument $entity );
-
-	/**
-	 * Builds and returns the HTML for the entity's fingerprint.
-	 *
-	 * @param EntityDocument $entity
-	 *
-	 * @return string HTML
-	 */
-	protected function getHtmlForTerms( EntityDocument $entity ) {
-		$id = $entity->getId();
-
-		if ( $entity instanceof LabelsProvider && $entity instanceof DescriptionsProvider ) {
-			return $this->entityTermsView->getHtml(
-				$this->languageCode,
-				$entity->getLabels(),
-				$entity->getDescriptions(),
-				$entity instanceof AliasesProvider ? $entity->getAliasGroups() : null,
-				$id
-			);
-		}
-
-		return '';
-	}
 
 }
