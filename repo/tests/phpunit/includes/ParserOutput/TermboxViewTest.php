71601bd0a9eaed32c57c709db262795f1abbf6c8
Jakob Warkotsch <j.warkotsch@gmail.com>
2018-10-25 18:35:33 +0200

    Render new termbox for mobile devices
    Bug: T206863
    Change-Id: I65e085be0421b58adc615a98bc778ee3e928a1a1

---
+++ repo/tests/phpunit/includes/ParserOutput/TermboxViewTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Wikibase\Repo\Tests\ParserOutput;
+
+use MediaWiki\Http\HttpRequestFactory;
+use MWHttpRequest;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use PHPUnit4And6Compat;
+use Wikibase\DataModel\Term\TermList;
+use Wikibase\LanguageFallbackChain;
+use Wikibase\Repo\ParserOutput\TermboxView;
+use Wikibase\SettingsArray;
+
+/**
+ * @covers \Wikibase\Repo\ParserOutput\TermboxView
+ *
+ * @group Wikibase
+ *
+ * @license GPL-2.0-or-later
+ */
+class TermboxViewTest extends TestCase {
+
+	use PHPUnit4And6Compat;
+
+	/** private */
+	const SSR_URL = 'https://ssr/termbox';
+
+	public function testGetHtml() {
+		$termbox = $this->newTermbox();
+
+		$termbox->getHtml(
+			'en',
+			new TermList( [] ),
+			new TermList( [] )
+		);
+	}
+
+	private function newTermbox(): TermboxView {
+		return new TermboxView(
+			new LanguageFallbackChain( [] ),
+			$this->newHttpRequestFactory(),
+			$this->newSettingsWithSsrUrl()
+		);
+	}
+
+	/**
+	 * @return MockObject|HttpRequestFactory
+	 */
+	private function newHttpRequestFactory(): MockObject {
+		$requestFactory = $this->createMock( HttpRequestFactory::class );
+		$requestFactory->expects( $this->once() )
+			->method( 'create' )
+			->with( self::SSR_URL )
+			->willReturn( $this->newHttpRequest() );
+
+		return $requestFactory;
+	}
+
+	/**
+	 * @return MockObject|SettingsArray
+	 */
+	private function newSettingsWithSsrUrl(): MockObject {
+		$settings = $this->createMock( SettingsArray::class );
+		$settings->expects( $this->once() )
+			->method( 'getSetting' )
+			->with( 'ssrServerUrl' )
+			->willReturn( self::SSR_URL );
+
+		return $settings;
+	}
+
+	/**
+	 * @return MockObject|MWHttpRequest
+	 */
+	private function newHttpRequest(): MockObject {
+		$req = $this->createMock( MWHttpRequest::class );
+		$req->expects( $this->once() )->method( 'execute' );
+
+		return $req;
+	}
+
+}
