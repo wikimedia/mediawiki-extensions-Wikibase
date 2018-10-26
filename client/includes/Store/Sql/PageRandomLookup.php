1fe158556a379d42de21acf87e3ed4402634e373
Stephen Niedzielski <stephen@niedzielski.com>
2018-10-26 09:26:09 -0700

    WIP: add page split testing
    Bug: T206868
    Change-Id: I6eb59214835054fbaa42145da716e59f911e58df

---
+++ client/includes/Store/Sql/PageRandomLookup.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Wikibase\Client\Store\Sql;
+
+use Wikimedia\Rdbms\LoadBalancer;
+
+/**
+ * Lookup of page.page_random based on page ID.
+ *
+ * @license GPL-2.0-or-later
+ */
+class PageRandomLookup {
+
+	/**
+	 * @var LoadBalancer
+	 */
+	private $loadBalancer;
+
+	public function __construct( LoadBalancer $loadBalancer ) {
+		$this->loadBalancer = $loadBalancer;
+	}
+
+	/**
+	 * @param int $pageId
+	 *
+	 * @return float|null
+	 */
+	public function getPageRandom( $pageId ) {
+		$db = $this->loadBalancer->getConnection( DB_REPLICA );
+
+		$pageRandom = $db->selectField( 'page', 'page_random', [ 'page_id' => $pageId ] );
+
+		$this->loadBalancer->reuseConnection( $db );
+
+		if ( $pageRandom === false ) {
+			return null;
+		}
+
+		return (float)$pageRandom;
+	}
+
+}
