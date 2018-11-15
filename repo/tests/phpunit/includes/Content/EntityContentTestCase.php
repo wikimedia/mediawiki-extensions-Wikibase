2e6cdf3bd85b009994eb6697b68b94cbfe5109f0
addshore <addshorewiki@gmail.com>
2018-11-15 17:48:45 +0000

    EntityContent::collectValues, add some basic test coverage
    This method had 0 test coverage and I'm about to change it.
    So lets add some tests...
    
    Bug: T204109
    Change-Id: Ie41bb5c7d3ac3b64c811e692af03ed54c0abd856

--- repo/tests/phpunit/includes/Content/EntityContentTestCase.php
+++ repo/tests/phpunit/includes/Content/EntityContentTestCase.php
@@ -19,6 +19,7 @@ use Wikibase\EntityContent;
 use Wikibase\Lib\Store\EntityStore;
 use Wikibase\Repo\Content\EntityContentDiff;
 use Wikibase\Repo\WikibaseRepo;
+use Wikimedia\TestingAccessWrapper;
 use WikiPage;
 
 /**
@@ -509,6 +510,76 @@ abstract class EntityContentTestCase extends \MediaWikiTestCase {
 		} else {
 			$this->assertNotNull( $content->getRedirectTarget() );
 		}
+	}
+
+	public function provideCollectValues() {
+		return [
+			'simple array, nothing ignored' => [
+				[
+					'key1' => 'val1',
+					'key2' => 'val2',
+				],
+				[],
+				[
+					'val1',
+					'val2',
+				],
+			],
+			'simple array, 1 key ignored' => [
+				[
+					'key1' => 'val1',
+					'key2' => 'val2',
+					'key3' => 'val3',
+				],
+				[ 'key2' ],
+				[
+					'val1',
+					'val3',
+				],
+			],
+			'multi dimension array, nothing ignored' => [
+				[
+					'key1' => 'val1',
+					'key2' => [
+						'val2',
+						'key3' => 'val3',
+						'key4' => [ 'val4' ]
+					]
+				],
+				[],
+				[
+					'val1',
+					'val2',
+					'val3',
+					'val4',
+				],
+			],
+			'multi dimension array, 2 ignored ignored' => [
+				[
+					'key1' => 'val1',
+					'key2' => [
+						'val2',
+						'key3' => 'val3',
+						'key4' => [ 'val4' ]
+					]
+				],
+				[ 'key1', 'key4' ],
+				[
+					'val2',
+					'val3',
+				],
+			],
+		];
+	}
+
+	/**
+	 * @covers \Wikibase\EntityContent::collectValues
+	 * @dataProvider provideCollectValues
+	 */
+	public function testCollectValues( $data, $ignore, $expected ) {
+		$content = TestingAccessWrapper::newFromClass( EntityContent::class );
+		$result = $content->collectValues( $data, $ignore );
+		$this->assertEquals( $expected, $result );
 	}
 
 }
