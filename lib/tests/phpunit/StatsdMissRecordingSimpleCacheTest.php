44a2081060b62ec33a958d61d469794763c782e3
addshore <addshorewiki@gmail.com>
2018-11-15 08:35:58 +0000

    StatsdMissRecordingSimpleCacheTest hit test cases
    Change-Id: I9cd9d03e0d144420ed43824d26a3de7790c9acaa

--- lib/tests/phpunit/StatsdMissRecordingSimpleCacheTest.php
+++ lib/tests/phpunit/StatsdMissRecordingSimpleCacheTest.php
@@ -60,4 +60,42 @@ class StatsdMissRecordingSimpleCacheTest extends \PHPUnit\Framework\TestCase {
 		$this->assertEquals( [ 'key1' => 'd1', 'key2' => 'd1' ], $result );
 	}
 
+	public function testGetDoesNotIncrementsMetricOnHit() {
+		$stats = $this->getMockForAbstractClass( StatsdDataFactoryInterface::class );
+		$stats->expects( $this->never() )
+			->method( 'increment' );
+
+		// Inner cache that returns the default that has been passed to the get method (cache miss)
+		$innerCache = $this->getMockForAbstractClass( CacheInterface::class );
+		$innerCache->expects( $this->once() )
+			->method( 'get' )
+			->with( 'key' )
+			->willReturn( 'cached value' );
+
+		$sot = new StatsdMissRecordingSimpleCache( $innerCache, $stats, 'statsKey' );
+		$result = $sot->get( 'key', 'default value' );
+		$this->assertEquals( 'cached value', $result );
+	}
+
+	public function testGetMultipleDoesNotIncrementMetricsOnHit() {
+		// Stats expects to be incremented once
+		$stats = $this->getMockForAbstractClass( StatsdDataFactoryInterface::class );
+		$stats->expects( $this->once() )
+			->method( 'increment' )
+			->with( 'statsKey', 1 );
+
+		// Inner cache that returns the default that has been passed to the get method (cache miss)
+		$innerCache = $this->getMockForAbstractClass( CacheInterface::class );
+		$innerCache->expects( $this->once() )
+			->method( 'getMultiple' )
+			->with( [ 'key1', 'key2' ] )
+			->willReturnCallback( function( $a, $default ) {
+				return [ 'key1' => 'cachehit', 'key2' => $default ];
+			} );
+
+		$sot = new StatsdMissRecordingSimpleCache( $innerCache, $stats, 'statsKey' );
+		$result = $sot->getMultiple( [ 'key1', 'key2' ], 'd1' );
+		$this->assertEquals( [ 'key1' => 'cachehit', 'key2' => 'd1' ], $result );
+	}
+
 }
