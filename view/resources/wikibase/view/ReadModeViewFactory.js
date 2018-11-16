772e870d28446616d60b9229e1863870021a4e92
Michael Große <michael.grosse@wikimedia.de>
2018-11-16 18:35:15 +0100

    Fix hidden references on protected items
    This fixes a bug that unprivileged users cannot see the references on
    protected pages. The root cause for the bug is that the references are
    initially collapsed and the code that initialises the toggling of the
    references is part of the widget-chain that also handles the editing of
    statements. This widget used to be not loaded at all when in
    read-only-mode.
    
    Now all the widgets for the editing are loaded when in read-only-mode,
    but the method to add the edit-buttons is disabled.
    
    This is a hack. But since all this code is intended to soon™️  be
    replaced by a vuejs implementation, it seemed to be overkill to create a
    new jQueryUI-widget hierarchy that properly separates widgets for
    read-only-mode and widgets for edit-possible-mode.
    
    Todo:
    * Tests
    
    Bug: T186006
    Change-Id: Id05bfcc2196c88451ee52b9f6084e3031acfacfe

--- view/resources/wikibase/view/ReadModeViewFactory.js
+++ view/resources/wikibase/view/ReadModeViewFactory.js
@@ -5,12 +5,14 @@ wikibase.view.ReadModeViewFactory = ( function ( wb ) {
 
 	var SELF = util.inherit( 'ReadModeViewFactory', PARENT, {} );
 
-	SELF.prototype.getStatementGroupListView = function ( entity, $statementgrouplistview ) {
-		/* Skip constructing statement views entirely */
-	};
-
 	SELF.prototype.getSitelinkGroupListView = function ( sitelinkSet, $sitelinkgrouplistview ) {
 		/* Skip constructing sitelink views entirely */
+	};
+
+	SELF.prototype._getAdderWithStartEditing = function () {
+		return function() {
+			return null;
+		};
 	};
 
 	return SELF;
