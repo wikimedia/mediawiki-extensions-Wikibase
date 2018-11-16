2e9a6bf2908c753f1aa24c6c7d4d2326c05ff6cb
addshore <addshorewiki@gmail.com>
2018-11-16 08:51:50 +0000

    ArrayValueCollector, Improve the performance
    While testing this against the data of Q56596767 on wikidata.org this
    change improves the time spent on the getTextForFilters call that calls
    collectValues from around 0.8s to 0.015s. The improvement holds true for
    php71 and also hhvm.
    
    This is a dramatic performance increase.
    getTextForFilters is called twice on edits, for both the old and new
    entity.
    
    Bug: T204109
    Change-Id: I974f8f36608d350427c83d4e4f1213bbead69816

--- repo/includes/ArrayValueCollector.php
+++ repo/includes/ArrayValueCollector.php
@@ -16,16 +16,30 @@ class ArrayValueCollector {
 	 * @return array The values found in the array structure.
 	 */
 	public static function collectValues( array $data, array $ignore = [] ) {
-		$values = [];
-
 		$erongi = array_flip( $ignore );
+		return self::collectValuesInternal( $data, $erongi );
+	}
+
+	/**
+	 * Recursively collects values from nested arrays.
+	 *
+	 * This function can be called tens of thousands of times, so try to keep it as lean as possible.
+	 * The passing of $values by reference through the calls is a performance improvement.
+	 *
+	 * @param array $data The array structure to process.
+	 * @param array $flippedIgnoreArray An array with keys that should be flipped
+	 * @param array &$values Values from a previous call.
+	 *
+	 * @return array The values found in the array structure.
+	 */
+	private static function collectValuesInternal( array $data, array $flippedIgnoreArray, array &$values = [] ) {
 		foreach ( $data as $key => $value ) {
-			if ( isset( $erongi[$key] ) ) {
+			if ( isset( $flippedIgnoreArray[$key] ) ) {
 				continue;
 			}
 
 			if ( is_array( $value ) ) {
-				$values = array_merge( $values, self::collectValues( $value, $ignore ) );
+				self::collectValuesInternal( $value, $flippedIgnoreArray, $values );
 			} else {
 				$values[] = $value;
 			}
