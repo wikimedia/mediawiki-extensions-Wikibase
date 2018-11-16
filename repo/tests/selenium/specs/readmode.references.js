3fa46e1b63b35f76cbea02112475ab0e3ea77895
Michael Große <michael.grosse@wikimedia.de>
2018-11-16 18:35:15 +0100

    Fix hidden references on protected items
    This fixes a bug that unprivileged users cannot see the references on
    protected pages. The root cause for the bug is that the references are
    initially collapsed and the code that initialises the toggling of the
    references is part of the widget-chain that also handles the editing of
    statements. This widget used to be not loaded at all when in
    read-only-mode.
    
    Now all the widgets for the editing are loaded when in read-only-mode,
    but the method to add the edit-buttons is disabled.
    
    This is a hack. But since all this code is intended to soon™️  be
    replaced by a vuejs implementation, it seemed to be overkill to create a
    new jQueryUI-widget hierarchy that properly separates widgets for
    read-only-mode and widgets for edit-possible-mode.
    
    Bug: T186006
    Change-Id: Id05bfcc2196c88451ee52b9f6084e3031acfacfe

---
+++ repo/tests/selenium/specs/readmode.references.js
@@ -0,0 +1,31 @@
+const Util = require( 'wdio-mediawiki/Util' ),
+	ItemPage = require( '../pageobjects/item.page' ),
+	WikibaseApi = require( '../wikibase.api' );
+
+describe( 'WikibaseReferenceOnProtectedPage', function () {
+
+	it( 'can expand collapsed references on a protected page as unprivileged user', function () {
+		let itemId, propertyId;
+
+		browser.call( () => {
+			return WikibaseApi.createItem( Util.getTestString( 'T186006-' ) )
+				.then( ( id ) => {
+					itemId = id;
+				} );
+		} );
+		browser.call( () => {
+			return WikibaseApi.getProperty( 'string' )
+				.then( ( id ) => {
+					propertyId = id;
+				} );
+		} );
+
+		ItemPage.open( itemId );
+		ItemPage.addMainStatement( propertyId, 'main value' );
+		ItemPage.addReferenceToNthStatementOfStatementGroup( 0, propertyId, propertyId, 'reference value 1-1' );
+		ItemPage.protectPage( itemId );
+
+		browser.waitForVisible( '.wikibase-statementview-references-container .wikibase-statementview-references-heading a.ui-toggler' );
+
+	} );
+} );
