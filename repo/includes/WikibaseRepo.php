9dd067fbb440c8585d3fedaf82e258deb81b1756
addshore <addshorewiki@gmail.com>
2018-11-19 16:12:11 +0000

    Inject IdGenerator into SqlStore
    There was a TODO to inject the ID blacklist, this
    takes it a step further and injects the whole id
    generator.
    
    Eventually this will have to be cleaned up to come up
    to speed with the WikibaseServices and multi repo aware
    services etc, but we are not there yet afaik, especially
    not for writing to repos.
    
    Moving he IdGenerator construction to WikibaseRepo top
    level factory will mor easily allow me to make the version
    of IdGenerator used configurable so we can easily try out
    the UpsertSqlIdGenerator and not have it ride the deployment
    train.
    
    Bug: T194299
    Change-Id: I156a6f8da4ecbad63b622b0f2c3df82b7a2ad281

--- repo/includes/WikibaseRepo.php
+++ repo/includes/WikibaseRepo.php
@@ -10,6 +10,7 @@ use ObjectCache;
 use Psr\SimpleCache\CacheInterface;
 use Wikibase\DataModel\Entity\EntityIdParsingException;
 use Wikibase\DataModel\Entity\Property;
+use Wikibase\IdGenerator;
 use Wikibase\Lib\Changes\CentralIdLookupFactory;
 use Wikibase\Lib\DataTypeFactory;
 use DataValues\DataValueFactory;
@@ -168,6 +169,7 @@ use Wikibase\Repo\Validators\ValidatorErrorLocalizer;
 use Wikibase\Repo\View\RepoSpecialPageLinker;
 use Wikibase\Repo\View\WikibaseHtmlSnakFormatterFactory;
 use Wikibase\SettingsArray;
+use Wikibase\SqlIdGenerator;
 use Wikibase\SqlStore;
 use Wikibase\Store;
 use Wikibase\Store\EntityIdLookup;
@@ -1014,6 +1016,13 @@ class WikibaseRepo {
 		return $this->settings;
 	}
 
+	public function newIdGenerator() : IdGenerator {
+		return new SqlIdGenerator(
+			MediaWikiServices::getInstance()->getDBLoadBalancer(),
+			$this->getSettings()->getSetting( 'idBlacklist' )
+		);
+	}
+
 	/**
 	 * @return Store
 	 */
@@ -1026,6 +1035,7 @@ class WikibaseRepo {
 				$this->getEntityIdLookup(),
 				$this->getEntityTitleLookup(),
 				$this->getEntityNamespaceLookup(),
+				$this->newIdGenerator(),
 				$this->getWikibaseServices()
 			);
 		}
