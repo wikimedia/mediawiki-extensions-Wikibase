9506c3fdaea3f1cd151f57e5b4580b9bc776c312
Jeroen De Dauw <jeroendedauw@gmail.com>
2018-12-01 12:01:27 +0100

    Replace generic code with item/property specific code
    A follow up in Lexeme will be needed to add a Lexeme specific updater
    
    This is for https://phabricator.wikimedia.org/T204066
    
    Change-Id: I7add121fe1159d881b6bd697a9a13bdf8e7cc3d0

--- repo/tests/phpunit/includes/ParserOutput/EntityParserOutputDataUpdaterCollectionTest.php
+++ repo/tests/phpunit/includes/ParserOutput/EntityParserOutputDataUpdaterCollectionTest.php
@@ -6,11 +6,11 @@ use InvalidArgumentException;
 use ParserOutput;
 use PHPUnit4And6Compat;
 use Wikibase\DataModel\Entity\Item;
-use Wikibase\Repo\ParserOutput\EntityParserOutputDataUpdater;
+use Wikibase\Repo\ParserOutput\EntityParserOutputUpdater;
 use Wikibase\Repo\ParserOutput\EntityParserOutputDataUpdaterCollection;
 
 /**
- * @covers \Wikibase\Repo\ParserOutput\EntityParserOutputDataUpdater
+ * @covers \Wikibase\Repo\ParserOutput\EntityParserOutputUpdater
  *
  * @group Wikibase
  *
@@ -24,12 +24,12 @@ class EntityParserOutputDataUpdaterCollectionTest extends \PHPUnit\Framework\Tes
 		$entity = new Item();
 		$parserOutput = new ParserOutput();
 
-		$dataUpdater1 = $this->getMock( EntityParserOutputDataUpdater::class );
+		$dataUpdater1 = $this->getMock( EntityParserOutputUpdater::class );
 		$dataUpdater1->expects( $this->once() )
 			->method( 'updateParserOutput' )
 			->with( $parserOutput, $entity );
 
-		$dataUpdater2 = $this->getMock( EntityParserOutputDataUpdater::class );
+		$dataUpdater2 = $this->getMock( EntityParserOutputUpdater::class );
 		$dataUpdater2->expects( $this->once() )
 			->method( 'updateParserOutput' )
 			->with( $parserOutput, $entity );
