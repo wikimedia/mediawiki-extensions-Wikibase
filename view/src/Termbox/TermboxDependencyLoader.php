a5787e3da52235e6a668b0baca1e4af92a948230
bitPogo <matthias.geisler@wikimedia.de>
2018-12-18 21:36:00 +0100

    Introduce message-resource-hook for termbox
    Bug: T212250
    Change-Id: I075b9ad03d101b0b45dc8b087793ca13450a0d68

---
+++ view/src/Termbox/TermboxDependencyLoader.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Wikibase\View\Termbox;
+
+/** 
+ * @license GPL-2.0-or-later
+ */
+class TermboxDependencyLoader {
+
+	/**
+	 *
+	 * @return string[]
+	 */
+	public static function getMessages( $file ) {
+		$JSON = self::readFileAsJSON( $file );
+		if ( array_key_exists( 'messages', $JSON ) ) {
+			return $JSON[ 'messages' ];
+		} else {
+			return [];
+		}
+	}
+
+	/**
+	 *
+	 *
+	 * @return string[] | null
+	 */
+	private static function readFileAsJSON( $file ) {
+		if ( !is_readable( $file ) ) {
+			return [];
+		}
+
+		$toParse = trim( file_get_contents( $file ) );
+		if ( empty( $toParse ) ) {
+			return [];
+		}
+
+		$JSON = json_decode( $toParse, true );
+		if ( $JSON === null ) {
+			return [];
+		} else {
+			return $JSON;
+		}
+	}
+
+}
