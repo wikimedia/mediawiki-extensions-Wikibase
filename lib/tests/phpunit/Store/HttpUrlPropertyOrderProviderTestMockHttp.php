e8e9329424d22f6f7885d12f28f2f181d6bb7c36
Derick Alangi <alangiderick@gmail.com>
2019-01-11 11:10:04 +0100

    Fix ::get() compatibility issue with Http
    As HttpUrlPropertyOrderProviderTestMockHttp extends class Http, and
    type hintings involved, methods of both classes should be compatible.
    
    Depends-On: I2554ffa311587b66d93bfd026eada9c516d5c093
    Change-Id: I4b45e79d35252d13f714f3271b87301ca515121a

--- lib/tests/phpunit/Store/HttpUrlPropertyOrderProviderTestMockHttp.php
+++ lib/tests/phpunit/Store/HttpUrlPropertyOrderProviderTestMockHttp.php
@@ -21,7 +21,7 @@ class HttpUrlPropertyOrderProviderTestMockHttp extends Http {
 	 */
 	public static $response;
 
-	public static function get( $url, $options = [], $caller = __METHOD__ ) {
+	public static function get( $url, array $options = [], $caller = __METHOD__ ) {
 		PHPUnit_Framework_Assert::assertSame( 'page-url', $url );
 		PHPUnit_Framework_Assert::assertInternalType( 'array', $options );
 		PHPUnit_Framework_Assert::assertInternalType( 'string', $caller );
