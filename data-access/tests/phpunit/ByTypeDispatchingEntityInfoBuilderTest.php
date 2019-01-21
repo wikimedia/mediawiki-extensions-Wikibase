8296cc891d88b0186cbbcf9e475060b9be80b6db
Leszek Manicki <leszek.manicki@wikimedia.de>
2019-01-21 15:41:07 +0100

    Added ByTypeDispatchingEntityInfoBuilder
    Bug: T214557
    Change-Id: Ief7a6c2a2152c52122527837be455c6353fa3bc5

---
+++ data-access/tests/phpunit/ByTypeDispatchingEntityInfoBuilderTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Wikibase\DataAccess\Tests;
+
+use PHPUnit4And6Compat;
+use Wikibase\DataAccess\ByTypeDispatchingEntityInfoBuilder;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\Lib\Store\EntityInfo;
+use Wikibase\Lib\Store\EntityInfoBuilder;
+
+/**
+ * @covers \Wikibase\DataAccess\ByTypeDispatchingEntityInfoBuilder
+ *
+ * @group Wikibase
+ *
+ * @license GPL-2.0-or-later
+ */
+class ByTypeDispatchingEntityInfoBuilderTest extends \PHPUnit_Framework_TestCase {
+
+	use PHPUnit4And6Compat;
+
+	public function testCollectEntityInfoPassesRequestToBuildersDefinedForRelevantEntityTypes() {
+		$itemId = new ItemId( 'Q1' );
+		$propertyId = new PropertyId( 'P1' );
+
+		$itemInfoBuilder = $this->createMock( EntityInfoBuilder::class );
+		$itemInfoBuilder->expects( $this->atLeastOnce() )
+			->method( 'collectEntityInfo' )
+			->with( [ $itemId ] )
+			->willReturn( new EntityInfo( [] ) );
+
+		$propertyInfoBuilder = $this->createMock( EntityInfoBuilder::class );
+		$propertyInfoBuilder->expects( $this->atLeastOnce() )
+			->method( 'collectEntityInfo' )
+			->with( [ $propertyId ] )
+			->willReturn( new EntityInfo( [] ) );
+
+		$builder = new ByTypeDispatchingEntityInfoBuilder( [ 'item' => $itemInfoBuilder, 'property' => $propertyInfoBuilder ] );
+
+		$builder->collectEntityInfo( [ $itemId, $propertyId ], [ 'en' ] );
+	}
+
+	public function testCollectEntityInfoMergesResultsFromAllBuilders() {
+		$itemId = new ItemId( 'Q1' );
+		$propertyId = new PropertyId( 'P1' );
+
+		$itemInfoBuilder = $this->createMock( EntityInfoBuilder::class );
+		$itemInfoBuilder->method( 'collectEntityInfo' )
+			->willReturn( new EntityInfo( [ 'Q1' => [ 'id' => 'Q1', 'type' => 'item' ] ] ) );
+
+		$propertyInfoBuilder = $this->createMock( EntityInfoBuilder::class );
+		$propertyInfoBuilder->method( 'collectEntityInfo' )
+			->willReturn( new EntityInfo( [ 'P1' => [ 'id' => 'P1', 'type' => 'property' ] ] ) );
+
+		$builder = new ByTypeDispatchingEntityInfoBuilder( [ 'item' => $itemInfoBuilder, 'property' => $propertyInfoBuilder ] );
+
+		$this->assertEquals(
+			new EntityInfo( [
+				'Q1' => [ 'id' => 'Q1', 'type' => 'item' ],
+				'P1' => [ 'id' => 'P1', 'type' => 'property' ],
+			] ),
+			$builder->collectEntityInfo( [ $itemId, $propertyId ], [ 'en' ] )
+		);
+	}
+
+}
