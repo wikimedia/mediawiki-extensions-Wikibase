02efb8cd9ad76bde8d0e3ab641133858e30a2370
Lucas Werkmeister (WMDE) <lucas.werkmeister@wikimedia.de>
2019-01-21 16:10:58 +0000

    Revert "Temporarily skip PhanUndeclaredClassMethod phan check"
    This reverts commit d0db1cf6bd7de360c0dc61028ae6f0e3e332b69e.
    
    With Iaf051b1ff8 and I7dfe3917e2 merged, this should no longer be
    necessary.
    
    Bug: T214016
    Change-Id: I851e1b2e0f726d751c13f83805bfa576611fc56e

--- tests/phan/config.php
+++ tests/phan/config.php
@@ -350,10 +350,6 @@ return [
 		"PhanUndeclaredVariable",
 		// approximate error count: 1
 		"PhanTypeArraySuspicious",
-		// https://phabricator.wikimedia.org/T214016
-		"PhanUndeclaredClassMethod",
-		"PhanUndeclaredClass",
-		"PhanUndeclaredExtendedClass",
 	],
 
 	/**
