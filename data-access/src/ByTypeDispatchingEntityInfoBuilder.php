957ba689e3bbcd58aabaad7b4358e3522d72de78
Leszek Manicki <leszek.manicki@wikimedia.de>
2019-01-21 15:41:07 +0100

    Added ByTypeDispatchingEntityInfoBuilder
    Bug: T214557
    Change-Id: Ief7a6c2a2152c52122527837be455c6353fa3bc5

---
+++ data-access/src/ByTypeDispatchingEntityInfoBuilder.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Wikibase\DataAccess;
+
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\Lib\Store\EntityInfo;
+use Wikibase\Lib\Store\EntityInfoBuilder;
+use Wikimedia\Assert\Assert;
+
+/**
+ * @license GPL-2.0-or-later
+ */
+class ByTypeDispatchingEntityInfoBuilder implements EntityInfoBuilder {
+
+	/**
+	 * @var EntityInfoBuilder[]
+	 */
+	private $builders;
+
+	/**
+	 * @param EntityInfoBuilder[] $builders
+	 */
+	public function __construct( array $builders ) {
+		Assert::parameterElementType( EntityInfoBuilder::class, $builders, '$builders' );
+		Assert::parameterElementType( 'string', array_keys( $builders ), 'keys of $builders' );
+
+		$this->builders = $builders;
+	}
+
+	/**
+	 * @param EntityId[] $entityIds
+	 * @param string[] $languageCodes
+	 * @return EntityInfo
+	 */
+	public function collectEntityInfo( array $entityIds, array $languageCodes ) {
+		$entityIdsByType = [];
+		foreach ( $entityIds as $entityId ) {
+			$entityIdsByType[$entityId->getEntityType()][] = $entityId;
+		}
+
+		$info = [];
+		foreach ( $entityIdsByType as $type => $ids ) {
+			if ( array_key_exists( $type, $this->builders ) ) {
+				$info = array_merge(
+					$info,
+					$this->builders[$type]->collectEntityInfo( $ids, $languageCodes )->asArray()
+				);
+			}
+		}
+
+		return new EntityInfo( $info );
+	}
+
+}
