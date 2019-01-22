136562d0ec0d34ed442bfb85e244166b33336195
Leszek Manicki <leszek.manicki@wikimedia.de>
2019-01-22 15:53:22 +0100

    Added ByTypeDispatchingEntityPrefetcher
    Change-Id: Idee1766c251d8456d7d261f77f308030c1e86bd1

---
+++ data-access/src/ByTypeDispatchingEntityPrefetcher.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Wikibase\DataAccess;
+
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\DataModel\Services\Entity\EntityPrefetcher;
+
+/**
+ * @license GPL-2.0-or-later
+ */
+class ByTypeDispatchingEntityPrefetcher implements EntityPrefetcher {
+
+	/**
+	 * @var EntityPrefetcher[]
+	 */
+	private $prefetchers;
+
+	public function __construct( array $prefetchers ) {
+		$this->prefetchers = $prefetchers;
+	}
+
+	/**
+	 * @param EntityId[] $entityIds
+	 */
+	public function prefetch( array $entityIds ) {
+		$groupedEntityIds = [];
+		foreach ( $entityIds as $id ) {
+			$groupedEntityIds[$id->getEntityType()][] = $id;
+		}
+		foreach ( $groupedEntityIds as $type => $ids ) {
+			$prefetcher = $this->getServiceForEntityType( $type );
+			if ( $prefetcher !== null ) {
+				$prefetcher->prefetch( $ids );
+			}
+		}
+	}
+
+	public function purge( EntityId $entityId ) {
+		$prefetcher = $this->getServiceForEntityType( $entityId->getEntityType() );
+		if ( $prefetcher !== null ) {
+			$prefetcher->purge( $entityId );
+		}
+	}
+
+	public function purgeAll() {
+		foreach ( $this->prefetchers as $prefetcher ) {
+			$prefetcher->purgeAll();
+		}
+	}
+
+	/**
+	 * @param string $entityType
+	 * @return EntityPrefetcher|null
+	 */
+	private function getServiceForEntityType( $entityType ) {
+		if ( array_key_exists( $entityType, $this->prefetchers ) ) {
+			return $this->prefetchers[$entityType];
+		}
+		return null;
+	}
+
+}
