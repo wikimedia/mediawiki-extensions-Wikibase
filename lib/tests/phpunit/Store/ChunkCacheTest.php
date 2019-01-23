2b8df2272cd950de28ce5757930f3682fba5ea68
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2019-01-23 18:12:40 +0100

    Test pruning behavior of ChunkCache
    Previously, the tests in ChunkCacheTest only checked that it did not
    corrupt the data: a “cache” that didn’t cache anything at all would
    still have passed them. Rectify this by verifying that accesses to the
    backing store happen in the correct sequence. (Note that we currently
    don’t assert that backing store accesses are actually triggered by the
    correct cache accesses, but hopefully this should be enough to catch
    most bugs.)
    
    Change-Id: I53b5344526b079443a563da57dc9bc1197213679

--- lib/tests/phpunit/Store/ChunkCacheTest.php
+++ lib/tests/phpunit/Store/ChunkCacheTest.php
@@ -2,6 +2,7 @@
 
 namespace Wikibase\Lib\Tests\Store;
 
+use Wikibase\Lib\Store\ChunkAccess;
 use Wikibase\Lib\Store\ChunkCache;
 
 /**
@@ -26,59 +27,70 @@ class ChunkCacheTest extends \MediaWikiTestCase {
 		return $data;
 	}
 
-	protected static function makeCacheAction( $start, $length, $info ) {
+	/**
+	 * Instruct the test to perform a cache access.
+	 *
+	 * @param int $start The start address of the chunk to load.
+	 * @param int $length The length of the chunk to load.
+	 * @param array $expectedAccess List of [ $start, $length ] accesses to the backing store.
+	 * @param string $info Description.
+	 * @return array
+	 */
+	protected static function makeCacheAction( $start, $length, array $expectedAccess, $info ): array {
 		$data = self::getTestData();
 
 		return [
 			'start' => $start,
 			'length' => $length,
 			'expected' => array_slice( $data, $start, $length ),
-			'info' => $info
+			'expectedAccess' => $expectedAccess,
+			'info' => $info,
 		];
 	}
 
 	public function provideLoadChunk() {
 		return [
-			[ // #0: basic loading
+			'basic loading' => [
 				10,  // chunkSize
 				50, // maxSize
 				[
-					self::makeCacheAction( 0, 4, 'start at the start' ),
-					self::makeCacheAction( 10, 4, 'start at ten' ),
-					self::makeCacheAction( 98, 5, 'exceed end' ),
+					self::makeCacheAction( 0, 4, [ [ 0, 10 ] ], 'start at the start' ),
+					self::makeCacheAction( 10, 4, [ [ 10, 10 ] ], 'start at ten' ),
+					self::makeCacheAction( 98, 5, [ [ 98, 10 ] ], 'exceed end' ),
 				]
 			],
 
-			[ // #1: matching & loading
-				10,  // chunkSize
+			'matching & loading' => [
+				4,  // chunkSize
 				50, // maxSize
 				[
-					self::makeCacheAction( 20, 4, 'start in the middle' ),
+					self::makeCacheAction( 20, 4, [ [ 20, 4 ] ], 'start in the middle' ),
 
-					self::makeCacheAction( 16, 4, 'fit block before' ),
-					self::makeCacheAction( 24, 4, 'fit block after' ),
+					self::makeCacheAction( 16, 4,  [ [ 16, 4 ] ], 'fit block before' ),
+					self::makeCacheAction( 24, 4,  [ [ 24, 4 ] ], 'fit block after' ),
 
-					self::makeCacheAction( 14, 4, 'overlap block before' ),
-					self::makeCacheAction( 26, 4, 'overlap block after' ),
+					self::makeCacheAction( 14, 4, [ [ 14, 2 ] ], 'overlap block before' ),
+					self::makeCacheAction( 26, 4, [ [ 28, 4 ] ], 'overlap block after' ),
 
-					self::makeCacheAction( 7, 4, 'detached block before' ),
-					self::makeCacheAction( 33, 4, 'detached block after' ),
+					self::makeCacheAction( 7, 4,  [ [ 7, 4 ] ], 'detached block before' ),
+					self::makeCacheAction( 33, 4,  [ [ 33, 4 ] ], 'detached block after' ),
 
-					self::makeCacheAction( 21, 2, 'single chunk match' ),
-					self::makeCacheAction( 18, 8, 'multi chunk match' ),
+					self::makeCacheAction( 21, 2,  [], 'single chunk match' ),
+					self::makeCacheAction( 18, 8,  [], 'multi chunk match' ),
 				]
 			],
 
-			[ // #2: pruning
+			'pruning' => [
 				3, // chunkSize
 				7, // maxSize
 				[
-					self::makeCacheAction( 3, 3, 'first chunk fits' ),
-					self::makeCacheAction( 0, 3, 'second chunk fits' ),
-					self::makeCacheAction( 2, 4, 'third chunk is a hit' ),
-					self::makeCacheAction( 16, 4, 'fourth chunk triggers prune' ),
-					self::makeCacheAction( 22, 4, 'fifth chunk triggers prune' ),
-					self::makeCacheAction( 26, 4, 'sixth chunk triggers prune' ),
+					self::makeCacheAction( 3, 3, [ [ 3, 3 ] ], 'first chunk fits' ),
+					self::makeCacheAction( 0, 3, [ [ 0, 3 ] ], 'second chunk fits' ),
+					self::makeCacheAction( 2, 4, [], 'third chunk is a hit' ),
+					self::makeCacheAction( 16, 4, [ [ 16, 3 ], [ 19, 3 ] ], 'fourth chunk triggers prune' ),
+					self::makeCacheAction( 22, 4, [ [ 22, 3 ], [ 25, 3 ] ], 'fifth chunk triggers prune' ),
+					self::makeCacheAction( 26, 4, [ [ 28, 3 ] ], 'sixth chunk triggers prune' ),
+					self::makeCacheAction( 2, 4, [ [ 2, 3 ], [ 5, 3 ] ], 'third chunk is no longer a hit' ),
 				]
 			],
 
@@ -91,7 +103,18 @@ class ChunkCacheTest extends \MediaWikiTestCase {
 	public function testLoadChunk( $chunkSize, $maxSize, $sequence ) {
 		$data = self::getTestData();
 
-		$store = new MockChunkAccess( $data );
+		$realStore = new MockChunkAccess( $data );
+		$store = $this->getMock( ChunkAccess::class );
+		$store->method( 'loadChunk' )
+			->withConsecutive( ...array_merge( ...array_map(
+				function ( $action ) {
+					return $action['expectedAccess'];
+				},
+				$sequence
+			) ) )
+			->willReturnCallback( [ $realStore, 'loadChunk' ] );
+		$store->method( 'getRecordId' )
+			->willReturnCallback( [ $realStore, 'getRecordId' ] );
 		$cache = new ChunkCache( $store, $chunkSize, $maxSize );
 
 		foreach ( $sequence as $action ) {
