b235be646a7d5ab6a3be10d617339815bbeace91
Leszek Manicki <leszek.manicki@wikimedia.de>
2019-01-23 16:46:13 +0100

    Added SingleEntitySourceServices and MultipleEntitySourceServices
    Initially, both provide EntityRevisionLookup only.
    
    Bug: T214557
    Change-Id: I90bd7e8e9faf9e2bea1bf08be8c608f51fbc5ac0

---
+++ data-access/src/MultipleEntitySourceServices.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Wikibase\DataAccess;
+
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\DataModel\Entity\EntityRedirect;
+use Wikibase\Lib\Store\EntityRevision;
+use Wikibase\Lib\Store\EntityStoreWatcher;
+
+/**
+ * @license GPL-2.0-or-later
+ */
+class MultipleEntitySourceServices implements EntityStoreWatcher {
+
+	/**
+	 * @var EntitySourceDefinitions
+	 */
+	private $entitySourceDefinitions;
+
+	/**
+	 * @var SingleEntitySourceServices[]
+	 */
+	private $singleSourceServices;
+
+	private $entityRevisionLookup = null;
+
+	public function __construct( EntitySourceDefinitions $entitySourceDefinitions, array $singleSourceServices ) {
+		$this->entitySourceDefinitions = $entitySourceDefinitions;
+		$this->singleSourceServices = $singleSourceServices;
+	}
+
+	public function getEntityRevisionLookup() {
+		if ( $this->entityRevisionLookup === null ) {
+			$lookupsPerType = [];
+
+			/** @var EntitySource $source */
+			foreach ( $this->entitySourceDefinitions->getEntityTypeToSourceMapping() as $entityType => $source ) {
+				$lookupsPerType[$entityType] = $this->singleSourceServices[$source->getSourceName()]->getEntityRevisionLookup();
+			}
+
+			$this->entityRevisionLookup = new ByTypeDispatchingEntityRevisionLookup( $lookupsPerType );
+		}
+
+		return $this->entityRevisionLookup;
+	}
+
+	public function getEntityStoreWatcher() {
+		return $this;
+	}
+
+	public function entityUpdated( EntityRevision $entityRevision ) {
+		$source = $this->entitySourceDefinitions->getSourceForEntityType( $entityRevision->getEntity()->getType() );
+		if ( $source !== null ) {
+			$this->singleSourceServices[$source->getSourceName()]->entityUpdated( $entityRevision );
+		}
+	}
+
+	public function redirectUpdated( EntityRedirect $entityRedirect, $revisionId ) {
+		$source = $this->entitySourceDefinitions->getSourceForEntityType( $entityRedirect->getEntityId()->getEntityType() );
+		if ( $source !== null ) {
+			$this->singleSourceServices[$source->getSourceName()]->redirectUpdated( $entityRedirect, $revisionId );
+		}
+	}
+
+	public function entityDeleted( EntityId $entityId ) {
+		$source = $this->entitySourceDefinitions->getSourceForEntityType( $entityId->getEntityType() );
+		if ( $source !== null ) {
+			$this->singleSourceServices[$source->getSourceName()]->entityDeleted( $entityId );
+		}
+	}
+
+}
