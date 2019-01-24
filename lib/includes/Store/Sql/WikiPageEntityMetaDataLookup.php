3d206df790bbf3c16e4a7857c30996e1dba51f1e
Leszek Manicki <leszek.manicki@wikimedia.de>
2019-01-24 14:11:13 +0100

    DNM Disabled sanity/integrity checks related to ID prefixes
    Those made wbsearchentities API work
    
    Change-Id: I0b10c645a1364aab105d7f8c49740497773462e9

--- lib/includes/Store/Sql/WikiPageEntityMetaDataLookup.php
+++ lib/includes/Store/Sql/WikiPageEntityMetaDataLookup.php
@@ -197,6 +197,7 @@ class WikiPageEntityMetaDataLookup extends DBAccessBase implements WikiPageEntit
 	 * @return bool
 	 */
 	private function isEntityIdFromRightRepository( EntityId $entityId ) {
+		return true;
 		return $entityId->getRepositoryName() === $this->repositoryName;
 	}
 
