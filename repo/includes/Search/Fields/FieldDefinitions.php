2f2f2b2aecdf85fea7f9241fe3167bd775c4a4c6
Stanislav Malyshev <smalyshev@gmail.com>
2019-01-25 17:00:20 -0800

    Move basic fields definitions to Wikibase space
    These definitions stay in Wikibase after ElasticSearch code is migrated out.
    They will reside in Wikibase\Repo\Search\Fields namespace, which
    is not linked to any Cirrus/ElasticSearch functionality.
    
    Bug: T190022
    Change-Id: I4d4515d6f74a79cd26192a6268ccce633288bb7b

---
+++ repo/includes/Search/Fields/FieldDefinitions.php
@@ -0,0 +1,20 @@
+<?php
+namespace Wikibase\Repo\Search\Fields;
+
+/**
+ * This is a collection of field definitions.
+ * This interface should be implemented by specific definition
+ * classes which know which fields they deal with.
+ *
+ * @license GPL-2.0-or-later
+ * @author Stas Malyshev
+ */
+interface FieldDefinitions {
+
+	/**
+	 * Get the list of definitions
+	 * @return WikibaseIndexField[] key is field name, value is WikibaseIndexField
+	 */
+	public function getFields();
+
+}
