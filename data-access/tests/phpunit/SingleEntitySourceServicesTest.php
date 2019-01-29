fc2307d5caef2dd44a105ea0955d38e982e7d741
addshore <addshorewiki@gmail.com>
2019-01-29 13:02:40 +0000

    Followup docs & tests for *EntitySourceServices
    Change-Id: I349a12066efc18f8d3125314361434af4a6bd9bb

---
+++ data-access/tests/phpunit/SingleEntitySourceServicesTest.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Wikibase\DataAccess\Tests;
+
+use DataValues\Deserializers\DataValueDeserializer;
+use MediaWiki\Storage\NameTableStore;
+use PHPUnit4And6Compat;
+use Wikibase\DataAccess\DataAccessSettings;
+use Wikibase\DataAccess\EntitySource;
+use Wikibase\DataAccess\GenericServices;
+use Wikibase\DataAccess\SingleEntitySourceServices;
+use Wikibase\DataModel\Entity\BasicEntityIdParser;
+use Wikibase\Lib\EntityTypeDefinitions;
+use Wikibase\Lib\Store\EntityRevisionLookup;
+use Wikimedia\Assert\ParameterElementTypeException;
+
+/**
+ * @covers \Wikibase\DataAccess\SingleEntitySourceServices
+ *
+ * @license GPL-2.0-or-later
+ */
+class SingleEntitySourceServicesTest extends \PHPUnit\Framework\TestCase {
+	use PHPUnit4And6Compat;
+
+	public function testValidConstruction() {
+		$this->newSingleEntitySourceServices();
+		$this->assertTrue( true );
+	}
+
+	public function provideSimpleServiceGetters() {
+		return [
+			[ 'getEntityRevisionLookup', EntityRevisionLookup::class, true ]
+		];
+	}
+
+	/**
+	 * @dataProvider provideSimpleServiceGetters
+	 */
+	public function testSimpleServiceGetters( $function, $expected, $expectSame ) {
+		$services = $this->newSingleEntitySourceServices();
+
+		$serviceOne = $services->$function();
+		$serviceTwo = $services->$function();
+
+		$this->assertInstanceOf( $expected, $serviceOne );
+
+		if ( $expectSame ) {
+			$this->assertSame( $serviceOne, $serviceTwo );
+		} else {
+			$this->assertNotSame( $serviceOne, $serviceTwo );
+		}
+	}
+
+	public function testInvalidConstruction_deserializeFactoryCallbacks() {
+		$this->setExpectedException( ParameterElementTypeException::class );
+		new SingleEntitySourceServices(
+			$this->newGenericServices(),
+			new BasicEntityIdParser(),
+			new DataValueDeserializer( [] ),
+			$this->getMockNameTableStore(),
+			new DataAccessSettings( 10, true, false ),
+			new EntitySource( 'source', 'sourcedb', [] ),
+			[ null ],
+			[]
+		);
+	}
+
+	public function testInvalidConstruction_entityMetaDataAccessorCallbacks() {
+		$this->setExpectedException( ParameterElementTypeException::class );
+		new SingleEntitySourceServices(
+			$this->newGenericServices(),
+			new BasicEntityIdParser(),
+			new DataValueDeserializer( [] ),
+			$this->getMockNameTableStore(),
+			new DataAccessSettings( 10, true, false ),
+			new EntitySource( 'source', 'sourcedb', [] ),
+			[],
+			[ null ]
+		);
+	}
+
+	public function newSingleEntitySourceServices() {
+		return new SingleEntitySourceServices(
+			$this->newGenericServices(),
+			new BasicEntityIdParser(),
+			new DataValueDeserializer( [] ),
+			$this->getMockNameTableStore(),
+			new DataAccessSettings( 10, true, false ),
+			new EntitySource( 'source', 'sourcedb', [] ),
+			[],
+			[]
+		);
+	}
+
+	public function newGenericServices() {
+		return new GenericServices(
+			new EntityTypeDefinitions( [] ),
+			[],
+			[]
+		);
+	}
+
+	/**
+	 * @return \PHPUnit_Framework_MockObject_MockObject|NameTableStore
+	 */
+	private function getMockNameTableStore() {
+		$m = $this->getMockBuilder( NameTableStore::class );
+		return $m->disableOriginalConstructor()->getMock();
+	}
+
+	// TODO test entityUpdated
+	// TODO test redirectUpdated
+	// TODO test entityDeleted
+
+}
