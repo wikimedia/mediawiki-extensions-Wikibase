4fb668dd671074b527b6f5f37399245822db63bb
addshore <addshorewiki@gmail.com>
2019-01-29 13:02:40 +0000

    Followup docs & tests for *EntitySourceServices
    Change-Id: I349a12066efc18f8d3125314361434af4a6bd9bb

---
+++ data-access/tests/phpunit/MultipleEntitySourceServicesTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Wikibase\DataAccess\Tests;
+
+use PHPUnit4And6Compat;
+
+/**
+ * @covers \Wikibase\DataAccess\MultipleEntitySourceServicesTest
+ */
+class MultipleEntitySourceServicesTest extends \PHPUnit\Framework\TestCase {
+	use PHPUnit4And6Compat;
+
+	// TODO test me
+
+}
