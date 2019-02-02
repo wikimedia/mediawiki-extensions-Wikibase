f436f6a974af504f5662fce03f08f03965b6c327
Leszek Manicki <leszek.manicki@wikimedia.de>
2019-02-02 19:54:52 +0100

    Added EntitySource to WikiPageEntityMetaDataLookup
    Lookup checks whether it can "process"
    input entity IDs based on either the repository prefix
    of the ID, or the entity type (vs what types are
    configured in the entity source the service "handles").
    
    The right algorithm is chosen based on the Wikibase configuration.
    Currently Wikibase only uses the repository prefix
    based access.
    
    Bug: T214557
    Depends-On: Ibc5e2bf8fb7a2ea0e135f2a3e09957bff5de2bd3
    Change-Id: I764ab61c72d668d44831533e872d5bd12d7d637a

--- repo/tests/phpunit/includes/Store/Sql/WikiPageEntityMetaDataLookupTest.php
+++ repo/tests/phpunit/includes/Store/Sql/WikiPageEntityMetaDataLookupTest.php
@@ -6,10 +6,14 @@ use InvalidArgumentException;
 use MediaWiki\MediaWikiServices;
 use MediaWikiTestCase;
 use stdClass;
+use Wikibase\DataAccess\DataAccessSettings;
+use Wikibase\DataAccess\EntitySource;
+use Wikibase\DataAccess\UnusableEntitySource;
 use Wikibase\DataModel\Entity\EntityId;
 use Wikibase\DataModel\Entity\EntityRedirect;
 use Wikibase\DataModel\Entity\Item;
 use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Entity\PropertyId;
 use Wikibase\DataModel\Services\Lookup\EntityLookupException;
 use Wikibase\Lib\Store\EntityRevision;
 use Wikibase\Lib\Store\EntityNamespaceLookup;
@@ -35,6 +39,9 @@ use Wikimedia\Rdbms\IDatabase;
  * @author Marius Hoch < hoo@online.de >
  */
 class WikiPageEntityMetaDataLookupTest extends MediaWikiTestCase {
+
+	const REPOSITORY_PREFIX_BASED_FEDERATION = false;
+	const ENTITY_SOURCE_BASED_FEDERATION = true;
 
 	/**
 	 * @var EntityRevision[]
@@ -97,7 +104,41 @@ class WikiPageEntityMetaDataLookupTest extends MediaWikiTestCase {
 			new EntityIdLocalPartPageTableEntityQuery(
 				$nsLookup,
 				$slotRoleStore
-			)
+			),
+			new UnusableEntitySource(),
+			new DataAccessSettings( 100, false, false, DataAccessSettings::USE_REPOSITORY_PREFIX_BASED_FEDERATION )
+		);
+	}
+
+	private function newMetaDataLookupForSourceBasedFederation( $namespaceLookup = null ) {
+		if ( $namespaceLookup === null ) {
+			$namespaceLookup = $this->getEntityNamespaceLookup();
+		}
+
+		return new WikiPageEntityMetaDataLookup(
+			$namespaceLookup,
+			new EntityIdLocalPartPageTableEntityQuery(
+				$namespaceLookup,
+				MediaWikiServices::getInstance()->getSlotRoleStore()
+			),
+			$this->newEntitySource(),
+			new DataAccessSettings( 100, false, false, DataAccessSettings::USE_ENTITY_SOURCE_BASED_FEDERATION )
+		);
+	}
+
+	private function newEntitySource() {
+		$irrelevantItemNamespaceId = 100;
+		$irrelevantPropertyNamespaceId = 200;
+		$irrelevantItemSlotName = 'main';
+		$irrelevantPropertySlotName = 'main';
+
+		return new EntitySource(
+			'testsource',
+			false,
+			[
+				'item' => [ 'namespaceId' => $irrelevantItemNamespaceId, 'slot' => $irrelevantItemSlotName ],
+				'property' => [ 'namespaceId' => $irrelevantPropertyNamespaceId, 'slot' => $irrelevantPropertySlotName ],
+			]
 		);
 	}
 
@@ -119,6 +160,39 @@ class WikiPageEntityMetaDataLookupTest extends MediaWikiTestCase {
 					$nsLookup,
 					MediaWikiServices::getInstance()->getSlotRoleStore()
 				),
+				new UnusableEntitySource(),
+				new DataAccessSettings( 100, false, false, DataAccessSettings::USE_REPOSITORY_PREFIX_BASED_FEDERATION ),
+			] )
+			->setMethods( [ 'getConnection' ] )
+			->getMock();
+
+		$lookup->expects( $this->exactly( $getConnectionCount ) )
+			->method( 'getConnection' )
+			->will( $this->returnCallback( function( $id ) use ( $selectCount, $selectRowCount ) {
+				$db = $realDB = wfGetDB( DB_MASTER );
+
+				if ( $id === DB_REPLICA ) {
+					// This is a (fake) lagged database connection.
+					$db = $this->getLaggedDatabase( $realDB, $selectCount, $selectRowCount );
+				}
+
+				return $db;
+			} ) );
+
+		return $lookup;
+	}
+
+	private function newLookupWithLaggedConnectionForSourceBasedFederation( $selectCount, $selectRowCount, $getConnectionCount ) {
+		$nsLookup = $this->getEntityNamespaceLookup();
+		$lookup = $this->getMockBuilder( WikiPageEntityMetaDataLookup::class )
+			->setConstructorArgs( [
+				$nsLookup,
+				new EntityIdLocalPartPageTableEntityQuery(
+					$nsLookup,
+					MediaWikiServices::getInstance()->getSlotRoleStore()
+				),
+				$this->newEntitySource(),
+				new DataAccessSettings( 100, false, false, DataAccessSettings::USE_REPOSITORY_PREFIX_BASED_FEDERATION ),
 			] )
 			->setMethods( [ 'getConnection' ] )
 			->getMock();
@@ -185,12 +259,40 @@ class WikiPageEntityMetaDataLookupTest extends MediaWikiTestCase {
 		$this->assertSame( 'main', $result->role_name );
 	}
 
+	public function testLoadRevisionInformationById_latest_entitySourceBasedFederation() {
+		$entityRevision = $this->data[0];
+
+		$result = $this->newMetaDataLookupForSourceBasedFederation()
+			->loadRevisionInformationByRevisionId( $entityRevision->getEntity()->getId(), $entityRevision ->getRevisionId() );
+
+		$this->assertEquals( $entityRevision->getRevisionId(), $result->rev_id );
+		$this->assertEquals( $entityRevision->getRevisionId(), $result->page_latest );
+		$this->assertSame( 'main', $result->role_name );
+	}
+
 	public function testLoadRevisionInformationById_masterFallback() {
 		$entityRevision = $this->data[0];
 
 		// Make sure we have two calls to getConnection: One that asks for a
 		// replica and one that asks for the master.
 		$lookup = $this->getLookupWithLaggedConnection( 0, 1, 2 );
+
+		$result = $lookup->loadRevisionInformationByRevisionId(
+			$entityRevision->getEntity()->getId(),
+			$entityRevision ->getRevisionId(),
+			EntityRevisionLookup::LATEST_FROM_REPLICA_WITH_FALLBACK
+		);
+
+		$this->assertEquals( $entityRevision->getRevisionId(), $result->rev_id );
+		$this->assertEquals( $entityRevision->getRevisionId(), $result->page_latest );
+	}
+
+	public function testLoadRevisionInformationById_masterFallback_entitySourceBasedFederation() {
+		$entityRevision = $this->data[0];
+
+		// Make sure we have two calls to getConnection: One that asks for a
+		// replica and one that asks for the master.
+		$lookup = $this->newLookupWithLaggedConnectionForSourceBasedFederation( 0, 1, 2 );
 
 		$result = $lookup->loadRevisionInformationByRevisionId(
 			$entityRevision->getEntity()->getId(),
@@ -218,10 +320,40 @@ class WikiPageEntityMetaDataLookupTest extends MediaWikiTestCase {
 		$this->assertFalse( $result );
 	}
 
+	public function testLoadRevisionInformationById_noFallback_entitySourceBasedFederation() {
+		$entityRevision = $this->data[0];
+
+		// Should do only one getConnection call.
+		$lookup = $this->newLookupWithLaggedConnectionForSourceBasedFederation( 0, 1, 1 );
+
+		$result = $lookup->loadRevisionInformationByRevisionId(
+			$entityRevision->getEntity()->getId(),
+			$entityRevision ->getRevisionId(),
+			EntityRevisionLookup::LATEST_FROM_REPLICA
+		);
+
+		// No fallback: Lagged data is omitted.
+		$this->assertFalse( $result );
+	}
+
 	public function testLoadRevisionInformationById_old() {
 		$entityRevision = $this->data[2];
 
 		$result = $this->getWikiPageEntityMetaDataLookup()
+			->loadRevisionInformationByRevisionId(
+				$entityRevision->getEntity()->getId(),
+				$entityRevision ->getRevisionId() - 1 // There were two edits to this item in sequence
+			);
+
+		$this->assertEquals( $entityRevision->getRevisionId() - 1, $result->rev_id );
+		// Page latest should reflect that this is not the latest revision
+		$this->assertEquals( $entityRevision->getRevisionId(), $result->page_latest );
+	}
+
+	public function testLoadRevisionInformationById_old_entitySourceBasedFederation() {
+		$entityRevision = $this->data[2];
+
+		$result = $this->newMetaDataLookupForSourceBasedFederation()
 			->loadRevisionInformationByRevisionId(
 				$entityRevision->getEntity()->getId(),
 				$entityRevision ->getRevisionId() - 1 // There were two edits to this item in sequence
@@ -244,8 +376,30 @@ class WikiPageEntityMetaDataLookupTest extends MediaWikiTestCase {
 		$this->assertFalse( $result );
 	}
 
+	public function testLoadRevisionInformationById_wrongRevision_entitySourceBasedFederation() {
+		$entityRevision = $this->data[2];
+
+		$result = $this->newMetaDataLookupForSourceBasedFederation()
+			->loadRevisionInformationByRevisionId(
+				$entityRevision->getEntity()->getId(),
+				$entityRevision ->getRevisionId() * 2 // Doesn't exist
+			);
+
+		$this->assertFalse( $result );
+	}
+
 	public function testLoadRevisionInformationById_notFound() {
 		$result = $this->getWikiPageEntityMetaDataLookup()
+			->loadRevisionInformationByRevisionId(
+				new ItemId( 'Q823487354' ),
+				823487354
+			);
+
+		$this->assertFalse( $result );
+	}
+
+	public function testLoadRevisionInformationById_notFound_entitySourceBasedFederation() {
+		$result = $this->newMetaDataLookupForSourceBasedFederation()
 			->loadRevisionInformationByRevisionId(
 				new ItemId( 'Q823487354' ),
 				823487354
@@ -302,6 +456,26 @@ class WikiPageEntityMetaDataLookupTest extends MediaWikiTestCase {
 		$this->assertSame( 'main', $result[$key]->role_name );
 	}
 
+	public function testLoadRevisionInformation_entitySourceBasedFederation() {
+		$entityIds = [
+			$this->data[0]->getEntity()->getId(),
+			$this->data[1]->getEntity()->getId(),
+			new ItemId( 'Q823487354' ), // Doesn't exist
+			$this->data[2]->getEntity()->getId()
+		];
+
+		$result = $this->newMetaDataLookupForSourceBasedFederation()
+			->loadRevisionInformation(
+				$entityIds,
+				EntityRevisionLookup::LATEST_FROM_REPLICA
+			);
+
+		$this->assertRevisionInformation( $entityIds, $result );
+
+		$key = $entityIds[0]->getSerialization();
+		$this->assertSame( 'main', $result[$key]->role_name );
+	}
+
 	public function testLoadRevisionInformation_masterFallback() {
 		$entityIds = [
 			$this->data[0]->getEntity()->getId(),
@@ -322,10 +496,42 @@ class WikiPageEntityMetaDataLookupTest extends MediaWikiTestCase {
 		$this->assertRevisionInformation( $entityIds, $result );
 	}
 
+	public function testLoadRevisionInformation_masterFallback_entitySourceBasedFederation() {
+		$entityIds = [
+			$this->data[0]->getEntity()->getId(),
+			$this->data[1]->getEntity()->getId(),
+			new ItemId( 'Q823487354' ), // Doesn't exist
+			$this->data[2]->getEntity()->getId()
+		];
+
+		// Make sure we have two calls to getConnection: One that asks for a
+		// replica and one that asks for the master.
+		$lookup = $this->newLookupWithLaggedConnectionForSourceBasedFederation( 1, 0, 2 );
+
+		$result = $lookup->loadRevisionInformation(
+			$entityIds,
+			EntityRevisionLookup::LATEST_FROM_REPLICA_WITH_FALLBACK
+		);
+
+		$this->assertRevisionInformation( $entityIds, $result );
+	}
+
 	public function testLoadRevisionInformation_unknownNamespace() {
 		$entityId = $this->data[0]->getEntity()->getId();
 		$namespaceLookup = new EntityNamespaceLookup( [] );
 		$metaDataLookup = $this->getWikiPageEntityMetaDataLookup( $namespaceLookup );
+
+		$this->setExpectedException( EntityLookupException::class );
+		$metaDataLookup->loadRevisionInformation(
+			[ $entityId ],
+			EntityRevisionLookup::LATEST_FROM_REPLICA
+		);
+	}
+
+	public function testLoadRevisionInformation_unknownNamespace_entitySourceBasedFederation() {
+		$entityId = $this->data[0]->getEntity()->getId();
+		$namespaceLookup = new EntityNamespaceLookup( [] );
+		$metaDataLookup = $this->newMetaDataLookupForSourceBasedFederation( $namespaceLookup );
 
 		$this->setExpectedException( EntityLookupException::class );
 		$metaDataLookup->loadRevisionInformation(
@@ -357,6 +563,55 @@ class WikiPageEntityMetaDataLookupTest extends MediaWikiTestCase {
 		);
 	}
 
+	/**
+	 * @expectedException InvalidArgumentException
+	 */
+	public function testGivenEntityFromOtherSource_loadRevisionInformationThrowsException() {
+		$lookup = $this->newLookupForEntitySourceProvidingItemsOnly();
+
+		$lookup->loadRevisionInformation(
+			[ new PropertyId( 'P123' ) ],
+			EntityRevisionLookup::LATEST_FROM_REPLICA
+		);
+	}
+
+	/**
+	 * @expectedException InvalidArgumentException
+	 */
+	public function testGivenEntityFromOtherSource_loadRevisionInformationByRevisionIdThrowsException() {
+		$lookup = $this->newLookupForEntitySourceProvidingItemsOnly();
+
+		$lookup->loadRevisionInformationByRevisionId(
+			new PropertyId( 'P123' ),
+			1,
+			EntityRevisionLookup::LATEST_FROM_REPLICA
+		);
+	}
+
+	private function newLookupForEntitySourceProvidingItemsOnly() {
+		$irrelevantItemNamespaceId = 100;
+		$irrelevantItemSlotName = 'main';
+
+		$itemSource = new EntitySource(
+			'testsource',
+			false,
+			[
+				'item' => [ 'namespaceId' => $irrelevantItemNamespaceId, 'slot' => $irrelevantItemSlotName ],
+			]
+		);
+
+		$namespaceLookup = $this->getEntityNamespaceLookup();
+		return new WikiPageEntityMetaDataLookup(
+			$namespaceLookup,
+			new EntityIdLocalPartPageTableEntityQuery(
+				$namespaceLookup,
+				MediaWikiServices::getInstance()->getSlotRoleStore()
+			),
+			$itemSource,
+			new DataAccessSettings( 100, false, false, DataAccessSettings::USE_ENTITY_SOURCE_BASED_FEDERATION )
+		);
+	}
+
 	public function testGivenEntityIdWithRepositoryPrefix_loadRevisionInformationStripsPrefix() {
 		$revision = $this->data[0];
 		$unprefixedId = $revision->getEntity()->getId()->getSerialization();
@@ -369,6 +624,8 @@ class WikiPageEntityMetaDataLookupTest extends MediaWikiTestCase {
 				$nsLookup,
 				MediaWikiServices::getInstance()->getSlotRoleStore()
 			),
+			new UnusableEntitySource(),
+			new DataAccessSettings( 100, false, false, DataAccessSettings::USE_REPOSITORY_PREFIX_BASED_FEDERATION ),
 			false,
 			'foo'
 		);
@@ -400,6 +657,8 @@ class WikiPageEntityMetaDataLookupTest extends MediaWikiTestCase {
 				$nsLookup,
 				MediaWikiServices::getInstance()->getSlotRoleStore()
 			),
+			new UnusableEntitySource(),
+			new DataAccessSettings( 100, false, false, DataAccessSettings::USE_REPOSITORY_PREFIX_BASED_FEDERATION ),
 			false,
 			'foo'
 		);
@@ -461,6 +720,23 @@ class WikiPageEntityMetaDataLookupTest extends MediaWikiTestCase {
 		$this->assertLatestRevisionIds( $entityIds, $result );
 	}
 
+	public function testLoadLatestRevisionIds_entitySourceBasedFederation() {
+		$entityIds = [
+			$this->data[0]->getEntity()->getId(),
+			$this->data[1]->getEntity()->getId(),
+			new ItemId( 'Q823487354' ), // Doesn't exist
+			$this->data[2]->getEntity()->getId()
+		];
+
+		$result = $this->newMetaDataLookupForSourceBasedFederation()
+			->loadLatestRevisionIds(
+				$entityIds,
+				EntityRevisionLookup::LATEST_FROM_REPLICA
+			);
+
+		$this->assertLatestRevisionIds( $entityIds, $result );
+	}
+
 	public function testLoadLatestRevisionIds_masterFallback() {
 		$entityIds = [
 			$this->data[0]->getEntity()->getId(),
@@ -472,6 +748,26 @@ class WikiPageEntityMetaDataLookupTest extends MediaWikiTestCase {
 		// Make sure we have two calls to getConnection: One that asks for a
 		// replica and one that asks for the master.
 		$lookup = $this->getLookupWithLaggedConnection( 1, 0, 2 );
+
+		$result = $lookup->loadLatestRevisionIds(
+			$entityIds,
+			EntityRevisionLookup::LATEST_FROM_REPLICA_WITH_FALLBACK
+		);
+
+		$this->assertLatestRevisionIds( $entityIds, $result );
+	}
+
+	public function testLoadLatestRevisionIds_masterFallback_entitySourceBasedFederation() {
+		$entityIds = [
+			$this->data[0]->getEntity()->getId(),
+			$this->data[1]->getEntity()->getId(),
+			new ItemId( 'Q823487354' ), // Doesn't exist
+			$this->data[2]->getEntity()->getId()
+		];
+
+		// Make sure we have two calls to getConnection: One that asks for a
+		// replica and one that asks for the master.
+		$lookup = $this->newLookupWithLaggedConnectionForSourceBasedFederation( 1, 0, 2 );
 
 		$result = $lookup->loadLatestRevisionIds(
 			$entityIds,
@@ -493,10 +789,34 @@ class WikiPageEntityMetaDataLookupTest extends MediaWikiTestCase {
 		);
 	}
 
+	public function testLoadLatestRevisionIds_unknownNamespace_entitySourceBasedFederation() {
+		$entityId = $this->data[0]->getEntity()->getId();
+		$namespaceLookup = new EntityNamespaceLookup( [] );
+		$metaDataLookup = $this->newMetaDataLookupForSourceBasedFederation( $namespaceLookup );
+
+		$this->setExpectedException( EntityLookupException::class );
+		$result = $metaDataLookup->loadLatestRevisionIds(
+			[ $entityId ],
+			EntityRevisionLookup::LATEST_FROM_REPLICA
+		);
+	}
+
 	public function testLoadLatestRevisionIds_noResultForRedirect() {
 		$entityId = $this->redirectId;
 
 		$result = $this->getWikiPageEntityMetaDataLookup()
+			->loadLatestRevisionIds(
+				[ $entityId ],
+				EntityRevisionLookup::LATEST_FROM_REPLICA
+			);
+
+		$this->assertSame( [ $entityId->getSerialization() => false ], $result );
+	}
+
+	public function testLoadLatestRevisionIds_noResultForRedirect_entitySourceBasedFederation() {
+		$entityId = $this->redirectId;
+
+		$result = $this->newMetaDataLookupForSourceBasedFederation()
 			->loadLatestRevisionIds(
 				[ $entityId ],
 				EntityRevisionLookup::LATEST_FROM_REPLICA
@@ -512,6 +832,17 @@ class WikiPageEntityMetaDataLookupTest extends MediaWikiTestCase {
 
 		$lookup->loadLatestRevisionIds(
 			[ new ItemId( 'foo:Q123' ) ],
+			EntityRevisionLookup::LATEST_FROM_REPLICA
+		);
+	}
+
+	public function testGivenEntityFromOtherSource_loadLatestRevisionIdsThrowsException() {
+		$lookup = $this->newLookupForEntitySourceProvidingItemsOnly();
+
+		$this->expectException( InvalidArgumentException::class );
+
+		$lookup->loadLatestRevisionIds(
+			[ new PropertyId( 'P123' ) ],
 			EntityRevisionLookup::LATEST_FROM_REPLICA
 		);
 	}
