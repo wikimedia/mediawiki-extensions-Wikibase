0f690d47b5c7811c3c3394fcc6a1c93608b7b36c
Leszek Manicki <leszek.manicki@wikimedia.de>
2019-02-04 23:10:22 +0100

    Removed ClientStore::getTermIndex
    As no longer used
    
    Change-Id: I39148280a9986e7e1f5db1f20f1a3f4389ddff0f

--- client/includes/Store/ClientStore.php
+++ client/includes/Store/ClientStore.php
@@ -60,11 +60,6 @@ interface ClientStore {
 	public function getEntityRevisionLookup();
 
 	/**
-	 * @return TermIndex
-	 */
-	public function getTermIndex();
-
-	/**
 	 * @return EntityIdLookup
 	 */
 	public function getEntityIdLookup();
