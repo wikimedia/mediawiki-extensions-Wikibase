2bfbc099d5ac05f6f057ab56ad5fbd7398b2fac9
Leszek Manicki <leszek.manicki@wikimedia.de>
2019-02-04 22:59:52 +0100

    Move created of PropertyLabelResolver from ClientStore to WikibaseClient
    To avoid it being bound to "local" repo's database
    
    Change-Id: I6be7bc7aa1042f686e1bbfe2e43365569e4f3505

--- client/tests/phpunit/includes/DataAccess/Scribunto/Scribunto_LuaWikibaseLibraryTestCase.php
+++ client/tests/phpunit/includes/DataAccess/Scribunto/Scribunto_LuaWikibaseLibraryTestCase.php
@@ -128,6 +128,8 @@ abstract class Scribunto_LuaWikibaseLibraryTestCase extends Scribunto_LuaEngineT
 	}
 
 	protected function setUp() {
+		$this->tablesUsed[] = 'wb_terms';
+
 		parent::setUp();
 		self::doMock();
 
@@ -140,6 +142,20 @@ abstract class Scribunto_LuaWikibaseLibraryTestCase extends Scribunto_LuaEngineT
 		);
 
 		$this->setContentLang( 'de' );
+
+		$db = wfGetDB( DB_MASTER );
+		$db->insert(
+			'wb_terms',
+			[
+				'term_full_entity_id' => 'P342',
+				'term_entity_id' => 342,
+				'term_entity_type' => 'property',
+				'term_language' => 'de',
+				'term_type' => 'label',
+				'term_text' => 'LuaTestStringProperty',
+				'term_search_key' => 'fooo'
+			]
+		);
 	}
 
 	protected function tearDown() {
