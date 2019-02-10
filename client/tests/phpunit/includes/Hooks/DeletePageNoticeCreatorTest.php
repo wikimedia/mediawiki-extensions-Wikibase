7afc7cc574567356ea0ba58c4cac221689748c03
Leszek Manicki <leszek.manicki@wikimedia.de>
2019-02-10 13:14:20 +0100

    Made RepoLinker aware of entity sources
    Bug: T214557
    Change-Id: I3442a2b906b5b87e9993a302c97242a88dcd608c

--- client/tests/phpunit/includes/Hooks/DeletePageNoticeCreatorTest.php
+++ client/tests/phpunit/includes/Hooks/DeletePageNoticeCreatorTest.php
@@ -5,6 +5,8 @@ namespace Wikibase\Client\Tests\Hooks;
 use Title;
 use Wikibase\Client\Hooks\DeletePageNoticeCreator;
 use Wikibase\Client\RepoLinker;
+use Wikibase\DataAccess\DataAccessSettings;
+use Wikibase\DataAccess\EntitySourceDefinitions;
 use Wikibase\DataModel\Entity\ItemId;
 use Wikibase\Lib\Store\SiteLinkLookup;
 
@@ -32,7 +34,14 @@ class DeletePageNoticeCreatorTest extends \MediaWikiTestCase {
 		$articlePath = '/wiki/$1';
 		$scriptPath = '';
 
-		return new RepoLinker( $baseUrl, $conceptBaseUri, $articlePath, $scriptPath );
+		return new RepoLinker(
+			new DataAccessSettings( 100, false, false, DataAccessSettings::USE_REPOSITORY_PREFIX_BASED_FEDERATION ),
+			new EntitySourceDefinitions( [] ),
+			$baseUrl,
+			$conceptBaseUri,
+			$articlePath,
+			$scriptPath
+		);
 	}
 
 	/**
