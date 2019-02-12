99a1447db66bb2e75113e6b9178d4448c1eab42e
Leszek Manicki <leszek.manicki@wikimedia.de>
2019-02-12 21:15:53 +0100

    Moved out logic on creating entity source definitions from backwards compatible settings out of WikibaseRepo/Client
    Bug: T214557
    Change-Id: Ib1fb6ad5e2c630f96b4bdf2d97cf45f2c2095331

--- repo/includes/WikibaseRepo.php
+++ repo/includes/WikibaseRepo.php
@@ -24,7 +24,6 @@ use IContextSource;
 use InvalidArgumentException;
 use Language;
 use MWException;
-use MWNamespace;
 use MediaWiki\Logger\LoggerFactory;
 use MediaWiki\MediaWikiServices;
 use MediaWiki\Site\MediaWikiPageNameNormalizer;
@@ -44,7 +43,6 @@ use ValueFormatters\ValueFormatter;
 use Wikibase\ByIdDispatchingItemTermStore;
 use Wikibase\DataAccess\AliasTermBuffer;
 use Wikibase\DataAccess\DataAccessSettings;
-use Wikibase\DataAccess\EntitySource;
 use Wikibase\DataAccess\EntitySourceDefinitions;
 use Wikibase\DataAccess\EntitySourceDefinitionsConfigParser;
 use Wikibase\DataAccess\GenericServices;
@@ -468,90 +466,8 @@ class WikibaseRepo {
 			return $configParser->newDefinitionsFromConfigArray( $settings->getSetting( 'entitySources' ), $entityTypeDefinitions );
 		}
 
-		$localEntityNamespaces = $settings->getSetting( 'entityNamespaces' );
-		$localDatabaseName = $settings->getSetting( 'changesDatabase' );
-		$localConceptBaseUri = $settings->getSetting( 'conceptBaseUri' );
-
-		$sources = [];
-
-		$localEntityNamespaceSlotData = [];
-		foreach ( $localEntityNamespaces as $entityType => $namespaceSlot ) {
-			list( $namespaceId, $slot ) = self::splitNamespaceAndSlot( $namespaceSlot );
-			$localEntityNamespaceSlotData[$entityType] = [
-				'namespaceId' => $namespaceId,
-				'slot' => $slot,
-			];
-		}
-		$sources[] = new EntitySource(
-			'local',
-			$localDatabaseName,
-			$localEntityNamespaceSlotData,
-			$localConceptBaseUri,
-			'wd', // TODO: make configurable
-			'', // TODO: make configurable
-			''
-		);
-
-		$foreignRepositories = $settings->getSetting( 'foreignRepositories' );
-
-		if ( isset( $foreignRepositories[''] ) ) {
-			throw new MWException( 'A WikibaseRepo cannot have a foreign repo configured with '
-				. 'the empty prefix, since the empty prefix always refers to the local repo.' );
-		}
-
-		foreach ( $foreignRepositories as $repository => $repositorySettings ) {
-			$namespaceSlotData = [];
-			foreach ( $repositorySettings['entityNamespaces'] as $entityType => $namespaceSlot ) {
-				list( $namespaceId, $slot ) = self::splitNamespaceAndSlot( $namespaceSlot );
-				$namespaceSlotData[$entityType] = [
-					'namespaceId' => $namespaceId,
-					'slot' => $slot,
-				];
-			}
-			$sources[] = new EntitySource(
-				$repository,
-				$repositorySettings['repoDatabase'],
-				$namespaceSlotData,
-				$repositorySettings['baseUri'],
-				$repository, // TODO: make configurable
-				$repository, // TODO: make configurable
-				$repository // TODO: this is a "magic" default/assumption
-			);
-		}
-
-		return new EntitySourceDefinitions( $sources, $entityTypeDefinitions );
-	}
-
-	private static function splitNamespaceAndSlot( $namespaceAndSlot ) {
-		if ( is_int( $namespaceAndSlot ) ) {
-			return [ $namespaceAndSlot, 'main' ];
-		}
-
-		if ( !preg_match( '!^(\w*)(/(\w+))?!', $namespaceAndSlot, $m ) ) {
-			throw new InvalidArgumentException(
-				'Bad namespace/slot specification: an integer namespace index, or a canonical'
-				. ' namespace name, or have the form <namespace>/<slot-name>.'
-				. ' Found ' . $namespaceAndSlot
-			);
-		}
-
-		if ( is_numeric( $m[1] ) ) {
-			$ns = intval( $m[1] );
-		} else {
-			$ns = MWNamespace::getCanonicalIndex( strtolower( $m[1] ) );
-		}
-
-		if ( !is_int( $ns ) ) {
-			throw new InvalidArgumentException(
-				'Bad namespace specification: must be either an integer or a canonical'
-				. ' namespace name. Found ' . $m[1]
-			);
-		}
-
-		return [
-			$ns,
-			$m[3] ?? 'main'
-		];
+		$parser = new EntitySourceDefinitionsLegacyRepoSettingsParser();
+		return $parser->newDefinitionsFromSettings( $settings, $entityTypeDefinitions );
 	}
 
 	/**
