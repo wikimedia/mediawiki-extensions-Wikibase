0ee83028ec33082aabc4cc74907e89a4810352d2
Pablo Grass <pablo.grass@wikimedia.de>
2019-02-21 14:55:12 +0100

    nkaaaaaaaaaaaa

--- repo/tests/phpunit/includes/ParserOutput/TermboxViewTest.php
+++ repo/tests/phpunit/includes/ParserOutput/TermboxViewTest.php
@@ -10,6 +10,7 @@ use Wikibase\DataModel\Entity\ItemId;
 use Wikibase\DataModel\Term\TermList;
 use Wikibase\LanguageFallbackChain;
 use Wikibase\LanguageFallbackChainFactory;
+use Wikibase\Lib\Store\EntityRevision;
 use Wikibase\Repo\ParserOutput\TextInjector;
 use Wikibase\View\EntityTermsView;
 use Wikibase\View\LocalizedTextProvider;
@@ -29,6 +30,8 @@ use Wikibase\View\ViewPlaceHolderEmitter;
 class TermboxViewTest extends TestCase {
 
 	use PHPUnit4And6Compat;
+
+	private $entityRevisionLookup;
 
 	public function testGetHtmlReturnsPlaceholderMarker() {
 		$marker = 'termbox-marker';
@@ -78,12 +81,14 @@ class TermboxViewTest extends TestCase {
 			$textInjector
 		);
 
-		$this->assertContains( $markers, $termbox->getPlaceholders( new Item( new ItemId( 'Q123' ) ), $languageCode ) );
+		$this->assertContains( $markers, $termbox->getPlaceholders( new Item( new ItemId( 'Q123' ) ), 4711, $languageCode ) );
 	}
 
 	public function testPlaceHolderWithMarkupWithClientStringResponse_returnsContent() {
 		$language = 'en';
-		$item = new Item( new ItemId( 'Q42' ) );
+		$itemId = new ItemId( 'Q42' );
+		$item = new Item( $itemId );
+		$revision = 4711;
 		$editLinkUrl = '/edit/Q42';
 		$response = 'termbox says hi';
 
@@ -97,7 +102,7 @@ class TermboxViewTest extends TestCase {
 		$renderer = $this->newTermboxRenderer();
 		$renderer->expects( $this->once() )
 			->method( 'getContent' )
-			->with( $item->getId(), $language, $editLinkUrl, $fallbackChain )
+			->with( $item->getId(), $revision, $language, $editLinkUrl, $fallbackChain )
 			->willReturn( $response );
 
 		$placeholders = $this->newTermbox(
@@ -107,6 +112,7 @@ class TermboxViewTest extends TestCase {
 			$fallbackChainFactory
 		)->getPlaceholders(
 			$item,
+			$revision,
 			$language
 		);
 
@@ -119,6 +125,7 @@ class TermboxViewTest extends TestCase {
 	public function testPlaceHolderWithMarkupWithClientThrowingException_returnsErrorValue() {
 		$language = 'en';
 		$item = new Item( new ItemId( 'Q42' ) );
+		$revision = 4711;
 
 		$renderer = $this->newTermboxRenderer();
 		$renderer->expects( $this->once() )
@@ -127,6 +134,27 @@ class TermboxViewTest extends TestCase {
 
 		$placeholders = $this->newTermbox( $renderer, $this->newLocalizedTextProvider(), $this->newSpecialPageLinker() )->getPlaceholders(
 			$item,
+			$revision,
+			$language
+		);
+		$this->assertSame(
+			ViewPlaceHolderEmitter::ERRONEOUS_PLACEHOLDER_VALUE,
+			$placeholders[ TermboxView::TERMBOX_MARKUP ]
+		);
+	}
+
+	public function testPlaceHolderWithMarkupWithUnsavedRevision_returnsErrorValue() {
+		$language = 'en';
+		$item = new Item( new ItemId( 'Q42' ) );
+		$revision = EntityRevision::UNSAVED_REVISION;
+
+		$renderer = $this->newTermboxRenderer();
+		$renderer->expects( $this->never() )
+			->method( 'getContent' );
+
+		$placeholders = $this->newTermbox( $renderer, $this->newLocalizedTextProvider(), $this->newSpecialPageLinker() )->getPlaceholders(
+			$item,
+			$revision,
 			$language
 		);
 		$this->assertSame(
