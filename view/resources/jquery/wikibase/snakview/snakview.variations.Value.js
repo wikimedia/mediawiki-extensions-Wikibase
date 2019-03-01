0557ebf163e3632c53cce63f1737132ef8b0ba7d
alaasarhan <alaa.sarhan@wikimedia.de>
2019-03-01 16:05:01 +0100

    Warn to hard refresh page when datatypes in client side seems outdated
    Bug: T216728
    Change-Id: If4a3c8fb289aed87e042f37c5bc5f13c601c8bab

--- view/resources/jquery/wikibase/snakview/snakview.variations.Value.js
+++ view/resources/jquery/wikibase/snakview/snakview.variations.Value.js
@@ -188,6 +188,27 @@
 						propertyId = fetchedProperty ? fetchedProperty.getId() : null;
 
 					if ( fetchedProperty && !dataType ) {
+						// Note: Could not find a link between the caching problem and localStorage
+						// and this issue (T216728), meaning the following clear doesn't seem to be
+						// necessary in most cases.
+						// However, clearing resourceloader storage prior to a hard refresh sounded
+						// a good idea at the time if odds that this is causing an issue under
+						// some edge circumstances.
+						mw.loader.store.clear();
+
+						mw.notify(
+							mw.msg(
+								'wikibase-refresh-for-missing-datatype',
+								fetchedProperty.getDataTypeId()
+							),
+							{
+								autoHide: false,
+								title: mw.message( 'wikibase-outdated-client-script' ),
+								type: 'warn',
+								tag: 'wikibase-outdated-datatypes'
+							}
+						);
+
 						mw.log.warn(
 							'Found property ' + fetchedProperty.getId() + ' in entityStore but couldn\'t find ' +
 							'the datatype "' + fetchedProperty.getDataTypeId() + '" in dataTypeStore. ' +
