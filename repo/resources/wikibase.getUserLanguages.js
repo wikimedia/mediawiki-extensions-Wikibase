41d5df97b3730029af64fd43b54d2c16e947d1ac
Jakob Warkotsch <j.warkotsch@gmail.com>
2019-03-13 18:55:00 +0100

    Extract getUserLanguages from entityViewInit for reuse
    Bug: T218111
    Change-Id: I24ded3322897ee32c37380b404378c3652a68d61

---
+++ repo/resources/wikibase.getUserLanguages.js
@@ -0,0 +1,28 @@
+/**
+ * @license GPL-2.0-or-later
+ */
+( function ( wb ) {
+	'use strict';
+
+	/**
+	 * relocated from repo/resources/wikibase.ui.entityViewInit.js
+	 * @return {string[]} An ordered list of languages the user wants to use, the first being her
+	 * preferred language, and thus the UI language (currently wgUserLanguage).
+	 */
+
+	wb.getUserLanguages = function () {
+		var userLanguages = mw.config.get( 'wbUserSpecifiedLanguages' ),
+			isUlsDefined = mw.uls && $.uls && $.uls.data,
+			languages;
+
+		if ( !userLanguages.length && isUlsDefined ) {
+			languages = mw.uls.getFrequentLanguageList().slice( 1, 4 );
+		} else {
+			languages = userLanguages.slice();
+			languages.splice( userLanguages.indexOf( mw.config.get( 'wgUserLanguage' ) ), 1 );
+		}
+
+		languages.unshift( mw.config.get( 'wgUserLanguage' ) );
+		return languages;
+	};
+}( wikibase ) );
---
+++ view/tests/qunit/wikibase/wikibase.getUserLanguages.tests.js
@@ -0,0 +1,96 @@
+( function ( wb, sinon ) {
+	'use strict';
+
+	QUnit.module( 'wikibase.getUserLanguages' );
+
+	QUnit.test( 'getUserLanguages provides languages of babel if they are defined', function ( assert ) {
+		var removeUls = false;
+		var removeData = false;
+		var mwConfigStub = sinon.stub( mw.config, 'get' );
+		var mwUlsConfigStub = sinon.stub( mw.uls, 'getFrequentLanguageList' );
+		var ulsLanguages = [ 'de', 'en', 'gr', 'zh' ];
+		var languages = [ 'de', 'he', 'fr' ];
+
+		if ( ! $.uls ) {
+			removeUls = true;
+			$.uls = {};
+		}
+
+		if ( !$.uls.data ) {
+			removeData = true;
+			$.uls.data = 'ignoreMe';
+		}
+
+		mwConfigStub.withArgs( 'wbUserSpecifiedLanguages' ).returns( languages );
+		mwConfigStub.withArgs( 'wgUserLanguage' ).returns( 'de' );
+		mwUlsConfigStub.returns( ulsLanguages );
+
+		assert.propEqual( languages, wb.getUserLanguages() );
+
+		if ( removeData ) {
+			delete $.uls.data;
+		}
+
+		if ( removeUls ) {
+			delete $.uls;
+		}
+	} );
+
+	QUnit.test( 'getUserLanguages uses uls languages if babel is not defined', function ( assert ) {
+		var removeUls = false;
+		var removeData = false;
+		var mwConfigStub = sinon.stub( mw.config, 'get' );
+		var mwUlsConfigStub = sinon.stub( mw.uls, 'getFrequentLanguageList' );
+		var ulsLanguages = [ 'de', 'en', 'gr', 'zh' ];
+		var languages = [];
+
+		if ( ! $.uls ) {
+			removeUls = true;
+			$.uls = {};
+		}
+
+		if ( !$.uls.data ) {
+			removeData = true;
+			$.uls.data = 'ignoreMe';
+		}
+
+		mwConfigStub.withArgs( 'wbUserSpecifiedLanguages' ).returns( languages );
+		mwConfigStub.withArgs( 'wgUserLanguage' ).returns( 'de' );
+		mwUlsConfigStub.returns( ulsLanguages );
+
+		assert.propEqual( ulsLanguages, wb.getUserLanguages() );
+
+		if ( removeData ) {
+			delete $.uls.data;
+		}
+
+		if ( removeUls ) {
+			delete $.uls;
+		}
+	} );
+
+	QUnit.test( 'getUserLanguages returns just the given userLanguage if uls languages and babel is not defined', function ( assert ) {
+		var restoreData = false;
+		var restore;
+		var mwConfigStub = sinon.stub( mw.config, 'get' );
+		var mwUlsConfigStub = sinon.stub( mw.uls, 'getFrequentLanguageList' );
+		var ulsLanguages = [];
+		var languages = [];
+
+		if ( $.uls.data ) {
+			restoreData = true;
+			restore = $.uls.data;
+		}
+
+		mwConfigStub.withArgs( 'wbUserSpecifiedLanguages' ).returns( languages );
+		mwConfigStub.withArgs( 'wgUserLanguage' ).returns( 'de' );
+		mwUlsConfigStub.returns( ulsLanguages );
+
+		assert.propEqual( [ 'de' ], wb.getUserLanguages() );
+
+		if ( restoreData ) {
+			$.uls.data = restore;
+		}
+	} );
+
+}( wikibase, sinon ) );
