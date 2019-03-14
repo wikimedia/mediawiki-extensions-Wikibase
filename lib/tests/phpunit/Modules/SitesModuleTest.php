1de73ac2f70125f3681a32a1fab19f77d80ab405
Timo Tijhof <krinklemail@gmail.com>
2019-03-14 20:52:33 +0000

    Use enableModuleContentVersion() for Wikibase\lib\SitesModule (take 2)
    This re-applies commit 6a7aa53dc944, reverting e1b60c7b1f065.
    
    Original rationale:
    
      Remove the need to maintain two separate code paths, one to
      compute the script, and one to compute the module version hash.
    
      Instead, let ResourceLoader do the module versioning based on the
      content of the script. When setting enableModuleContentVersion(),
      then module->getVersionHash() will call getScript() instead of
      getDefinitionSummary().
    
    Changes:
    
    * Remove now-redundant use of Memcached for the module hash.
    
    * Remove now-unused internal methods.
    
    * Add explicit use of Memcached via getWithSetCallback for the generated
      JavaScript code, which we don't want to recompute too often. This is
      effectively the same as before, except that it no longer applies only to
      the HTTP request for the 'startup' module. It now *also* applies to the
      HTTP request for the 'mw.config.values.wbSiteDetails' module itself.
      Previously it was actually computing both values, which kind of defeated
      the purpose of the cache. This happened because even on the main module
      request, we still need the version as well (for the E-Tag header).
    
    * Update the tests accordingly.
    
      For the Worker class testGetDefinitionSummary_caching becomes
      testGetSpecialSiteLinkGroups_caching.
    
      For the Module class, testGetDefinitionSummary was moved to it
      from the WorkerTest suite, as testGetVersionHash.
    
    Difference from original attempt:
    
     I orignally introduced the getWithSet caching call in
     computeSiteDetails() which was a mistake, because that's for
     each individual site object, not for the combined script of all
     known MW sites. As such, it was caching the first iteration of
     the for-loop, and then re-using it for all other sites.
    
     Now, the caching is applied to the generated script.
    
     I've updated the test case to actually create a mock with
     more than 1 site, so that this bug is exposed.
    
     The original patch would have failed with this new test,
     as it would result in "mywiki" values being used for "otherwiki"
     as well.
    
     I also fixed another bug in the original code, which is that
     it did not vary the cache by language code, despite the code using
     it as parameter to make decisions. This is now fixed as well.
    
    Bug: T219549
    Change-Id: I4e63853e352ed3d083590c4cf789de08468c4ba5

--- lib/tests/phpunit/Modules/SitesModuleTest.php
+++ lib/tests/phpunit/Modules/SitesModuleTest.php
@@ -2,10 +2,15 @@
 
 namespace Wikibase\Lib\Tests\Modules;
 
-use Language;
+use HashBagOStuff;
+use HashSiteStore;
+use MediaWikiSite;
 use PHPUnit4And6Compat;
 use ResourceLoaderContext;
 use Wikibase\SitesModule;
+use Wikibase\SettingsArray;
+use Wikibase\Lib\SitesModuleWorker;
+use Wikimedia\TestingAccessWrapper;
 
 /**
  * @covers \Wikibase\SitesModule
@@ -28,9 +33,7 @@ class SitesModuleTest extends \PHPUnit\Framework\TestCase {
 
 		$context->expects( $this->any() )
 			->method( 'getLanguage' )
-			->will( $this->returnCallback( function() {
-				return Language::factory( 'en' );
-			} ) );
+			->willReturn( 'en' );
 
 		return $context;
 	}
@@ -42,12 +45,70 @@ class SitesModuleTest extends \PHPUnit\Framework\TestCase {
 		$this->assertStringEndsWith( '});', $script );
 	}
 
-	public function testGetDefinitionSummary() {
-		$module = new SitesModule();
-		$summary = $module->getDefinitionSummary( $this->getContext() );
-		$this->assertInternalType( 'array', $summary );
-		$this->assertArrayHasKey( 0, $summary );
-		$this->assertArrayHasKey( 'dataHash', $summary[0] );
+	public function testGetVersionHash() {
+		$workerLists = $this->getWorkersForVersionHash();
+		$hashesByName = [];
+
+		/** @var SitesModuleWorker[] $workers */
+		foreach ( $workerLists as $name => $workers ) {
+			$hashes = [];
+			foreach ( $workers as $worker ) {
+				$module = new SitesModule();
+				$moduleAccess = TestingAccessWrapper::newFromObject( $module );
+				$moduleAccess->worker = $worker;
+
+				$hashes[] = $module->getVersionHash( $this->getContext() );
+			}
+			$this->assertSame(
+				array_unique( $hashes ),
+				[ $hashes[0] ],
+				'the same version hash for equivalent settings'
+			);
+
+			$hashesByName[ $name ] = $hashes;
+		}
+
+		$this->assertSame(
+			array_keys( $workerLists ),
+			array_keys( $hashesByName ),
+			'different settings lead to same hash'
+		);
+	}
+
+	private function getWorkersForVersionHash() {
+		$site = new MediaWikiSite();
+		$site->setGlobalId( 'siteid' );
+		$site->setGroup( 'allowedgroup' );
+
+		$site2 = new MediaWikiSite();
+		$site2->setGlobalId( 'site2id' );
+		$site2->setGroup( 'allowedgroup' );
+
+		return [
+			'empty workers' => [
+				$this->newSitesModuleWorker( [], [] ),
+				$this->newSitesModuleWorker( [], [] ),
+			],
+			'single site' => [
+				$this->newSitesModuleWorker( [ $site ], [] ),
+				$this->newSitesModuleWorker( [ $site ], [] ),
+			],
+			'single site with configured group' => [
+				$this->newSitesModuleWorker( [ $site ], [ 'allowedgroup' ] ),
+				$this->newSitesModuleWorker( [ $site ], [ 'allowedgroup' ] )
+			],
+		];
+	}
+
+	private function newSitesModuleWorker( array $sites, array $groups ) {
+		return new SitesModuleWorker(
+			new SettingsArray( [
+				'siteLinkGroups' => $groups,
+				'specialSiteLinkGroups' => []
+			] ),
+			new HashSiteStore( $sites ),
+			new HashBagOStuff()
+		);
 	}
 
 }
