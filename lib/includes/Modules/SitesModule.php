1efcd14396cdbf985009696a3c4113d1f683a3d8
Timo Tijhof <krinklemail@gmail.com>
2019-03-28 23:28:51 +0000

    Use module 'factory' for dependency injection to SitesModule class
    The 'SitesModule' class was basically an empty shell around
    SitesModuleWorker for the purposes of dependency injection.
    
    Do this using the 'factory' function instead, and move the rest
    of the code and tests over.
    
    Bug: T219549
    Change-Id: I1114fe80727f4a443132dcdd588a5d3951db835d

--- lib/includes/Modules/SitesModule.php
+++ lib/includes/Modules/SitesModule.php
@@ -2,10 +2,14 @@
 
 namespace Wikibase;
 
-use MediaWiki\MediaWikiServices;
+use BagOStuff;
+use MediaWikiSite;
+use ResourceLoader;
 use ResourceLoaderContext;
 use ResourceLoaderModule;
-use Wikibase\Lib\SitesModuleWorker;
+use Site;
+use SiteLookup;
+use Wikibase\Lib\LanguageNameLookup;
 
 /**
  * @license GPL-2.0-or-later
@@ -16,16 +20,29 @@ use Wikibase\Lib\SitesModuleWorker;
 class SitesModule extends ResourceLoaderModule {
 
 	/**
-	 * @var SitesModuleWorker
+	 * How many seconds the result of getSiteDetails() is cached.
 	 */
-	private $worker;
+	const SITE_DETAILS_TTL = 600; // 10 minutes
 
-	public function __construct() {
-		$this->worker = new SitesModuleWorker(
-			Settings::singleton(),
-			MediaWikiServices::getInstance()->getSiteStore(),
-			MediaWikiServices::getInstance()->getLocalServerObjectCache()
-		);
+	/**
+	 * @var SettingsArray
+	 */
+	private $settings;
+
+	/**
+	 * @var SiteLookup
+	 */
+	private $siteLookup;
+
+	/**
+	 * @var BagOStuff
+	 */
+	private $cache;
+
+	public function __construct( SettingsArray $settings, SiteLookup $siteLookup, BagOStuff $cache ) {
+		$this->settings = $settings;
+		$this->siteLookup = $siteLookup;
+		$this->cache = $cache;
 	}
 
 	/**
@@ -34,17 +51,120 @@ class SitesModule extends ResourceLoaderModule {
 	 * @see ResourceLoaderModule::getScript
 	 *
 	 * @param ResourceLoaderContext $context
-	 *
-	 * @return string
+	 * @return string JavaScript Code
 	 */
 	public function getScript( ResourceLoaderContext $context ) {
-		return $this->worker->getScript( $context->getLanguage() );
+		$languageCode = $context->getLanguage();
+		return $this->cache->getWithSetCallback(
+			$this->cache->makeKey( 'wikibase-sites-module', 'script', $languageCode ),
+			self::SITE_DETAILS_TTL,
+			function () use ( $languageCode ) {
+					return $this->makeScript( $languageCode );
+			}
+		);
 	}
 
 	/** @return bool */
 	public function enableModuleContentVersion() {
-		// Let getVersionHash() invoke getScript() and hash that directly.
+		// Let ResourceLoaderModule::getVersionHash() invoke getScript() and hash that.
 		return true;
+	}
+
+	/**
+	 * @param string $languageCode
+	 * @return string JavaScript Code
+	 */
+	protected function makeScript( $languageCode ) {
+		$groups = $this->settings->getSetting( 'siteLinkGroups' );
+		$specialGroups = $this->settings->getSetting( 'specialSiteLinkGroups' );
+		$specialPos = array_search( 'special', $groups );
+		if ( $specialPos !== false ) {
+			// The "special" group actually maps to multiple groups
+			array_splice( $groups, $specialPos, 1, $specialGroups );
+		}
+
+		$siteDetails = [];
+		/**
+		 * @var MediaWikiSite $site
+		 */
+		foreach ( $this->siteLookup->getSites() as $site ) {
+			if ( $this->shouldSiteBeIncluded( $site, $groups ) ) {
+				$siteDetails[$site->getGlobalId()] = $this->computeSiteDetails(
+					$site,
+					$specialGroups,
+					$languageCode
+				);
+			}
+		}
+
+		return ResourceLoader::makeConfigSetScript( [ 'wbSiteDetails' => $siteDetails ] );
+	}
+
+	/**
+	 * @param MediaWikiSite $site
+	 * @param string[] $specialGroups
+	 * @param string $languageCode
+	 * @return string[]
+	 */
+	private function computeSiteDetails( MediaWikiSite $site, array $specialGroups, $languageCode ) {
+		$languageNameLookup = new LanguageNameLookup();
+
+		// FIXME: quickfix to allow a custom site-name / handling for the site groups which are
+		// special according to the specialSiteLinkGroups setting
+		$group = $site->getGroup();
+		if ( in_array( $group, $specialGroups ) ) {
+			$languageName = $this->getSpecialSiteLanguageName( $site, $languageCode );
+			$groupName = 'special';
+		} else {
+			$languageName = $languageNameLookup->getName( $site->getLanguageCode() );
+			$groupName = $group;
+		}
+
+		// Use protocol relative URIs, as it's safe to assume that all wikis support the same protocol
+		list( $pageUrl, $apiUrl ) = preg_replace(
+			"/^https?:/i",
+			'',
+			[
+				$site->getPageUrl(),
+				$site->getFileUrl( 'api.php' )
+			]
+		);
+
+		return [
+				'shortName' => $languageName,
+				'name' => $languageName, // use short name for both, for now
+				'id' => $site->getGlobalId(),
+				'pageUrl' => $pageUrl,
+				'apiUrl' => $apiUrl,
+				'languageCode' => $site->getLanguageCode(),
+				'group' => $groupName
+		];
+	}
+
+	/**
+	 * @param Site $site
+	 * @param string $languageCode
+	 * @return string
+	 */
+	private function getSpecialSiteLanguageName( Site $site, $languageCode ) {
+		$siteId = $site->getGlobalId();
+		$messageKey = 'wikibase-sitelinks-sitename-' . $siteId;
+
+		// @note: inLanguage needs to be called before exists and parse. See: T127872.
+		$languageNameMsg = wfMessage( $messageKey )->inLanguage( $languageCode );
+
+		return $languageNameMsg->exists() ? $languageNameMsg->parse() : $siteId;
+	}
+
+	/**
+	 * Whether it's needed to add a Site to the JS variable.
+	 *
+	 * @param Site $site
+	 * @param string[] $groups
+	 * @return bool
+	 */
+	private function shouldSiteBeIncluded( Site $site, array $groups ) {
+		return $site->getType() === Site::TYPE_MEDIAWIKI && in_array( $site->getGroup(), $groups );
 	}
 
 }
