a36b7e11ecdefa1911e313a956678f16fd6be1b9
Jeroen De Dauw <jeroendedauw@gmail.com>
2019-04-11 10:22:34 +0200

    Turn EntityTermStoreWriter into extract of TermIndex
    Bug: T219296
    
    Non-breaking version of
    https://gerrit.wikimedia.org/r/c/mediawiki/extensions/Wikibase/+/500734
    
    Change-Id: Ib8ffc69947e366ed18a373a0aeeca39dbc4cba6f

--- lib/includes/Store/EntityTermStoreWriter.php
+++ lib/includes/Store/EntityTermStoreWriter.php
@@ -2,6 +2,7 @@
 
 namespace Wikibase\Lib\Store;
 
+use InvalidArgumentException;
 use Wikibase\DataModel\Entity\EntityDocument;
 use Wikibase\DataModel\Entity\EntityId;
 
@@ -11,21 +12,23 @@ use Wikibase\DataModel\Entity\EntityId;
 interface EntityTermStoreWriter {
 
 	/**
-	 * Saves the terms of the provided entity in the term store.
+	 * Saves the terms of the provided entity in the term cache.
 	 *
-	 * @param EntityDocument $entity
+	 * @param EntityDocument $entity Must have an ID, and optionally any combination of terms as
+	 *  declared by the TermIndexEntry::TYPE_... constants.
 	 *
-	 * @return boolean true on success, false otherwise.
+	 * @throws InvalidArgumentException when $entity does not have an ID.
+	 * @return boolean Success indicator
 	 */
-	public function saveTerms( EntityDocument $entity );
+	public function saveTermsOfEntity( EntityDocument $entity );
 
 	/**
-	 * Deletes the terms of the provided entity from the term store.
+	 * Deletes the terms of the provided entity from the term cache.
 	 *
 	 * @param EntityId $entityId
 	 *
-	 * @return boolean true on success, false otherwise.
+	 * @return boolean Success indicator
 	 */
-	public function deleteTerms( EntityId $entityId );
+	public function deleteTermsOfEntity( EntityId $entityId );
 
 }
