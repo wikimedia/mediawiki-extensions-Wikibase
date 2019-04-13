27ca2547e88e959ddfe029a6a23707b32e208aaa
rosalie <perside.rosalie@wikimedia.de>
2019-04-14 00:40:46 +0200

    Add data value type to Special:ListDatatypes
    Add data value to data type on the ListDatatype Special page,
    now display as <Datatype (data Value)>
    
    Bug: T210598
    Change-Id: Id6ba54a52b5042c5a2f13fb373365f7444e0d706

--- repo/includes/Specials/SpecialListDatatypes.php
+++ repo/includes/Specials/SpecialListDatatypes.php
@@ -52,24 +52,27 @@ class SpecialListDatatypes extends SpecialWikibasePage {
 		// 'wikibase-listdatatypes-external-id-head'
 		// 'wikibase-listdatatypes-external-id-body'
 
-		foreach ( $this->getDataTypeIds() as $dataTypeId ) {
-			$this->getOutput()->addHTML( $this->getHtmlForDataTypeId( $dataTypeId ) );
+		foreach ( $this->getDataTypeIdsAndValues() as $dataTypeId => $valueType ) {
+			$this->getOutput()->addHTML( $this->getHtmlForDataTypeId( $dataTypeId, $valueType ) );
 		}
 
 		$this->getOutput()->addHTML( Html::closeElement( 'dl' ) );
 	}
 
-	protected function getDataTypeIds() {
-		return WikibaseRepo::getDefaultInstance()->getDataTypeFactory()->getTypeIds();
+	protected function getDataTypeIdsAndValues() {
+		return WikibaseRepo::getDefaultInstance()->getDataTypeDefinitions()->getValueTypes();
 	}
 
-	protected function getHtmlForDataTypeId( $dataTypeId ) {
+	protected function getHtmlForDataTypeId( $dataTypeId, $valueType ) {
 		$baseKey = 'wikibase-listdatatypes-' . mb_strtolower( $dataTypeId );
 
 		return Html::rawElement(
 			'dt',
 			[ 'id' => $dataTypeId ],
 			$this->msg( $baseKey . '-head' )->parse()
+				. Html::element( 'span', [],
+				' ' . wfMessage( 'parentheses' )->plaintextParams( $valueType )->parse()
+				)
 		)
 		. Html::rawElement( 'dd', [],
 			$this->msg( $baseKey . '-body' )->parse()
