386cedac1dd88942b00a61c67519c6da21abac39
alaasarhan <alaa.sarhan@wikimedia.de>
2019-04-30 12:32:32 +0200

    Add EntityTermsStoreSchemaAccess NormalizedTermStoreSchemaAccess interfaces
    These interfaces abstract normalized terms schema access
    from mediawiki dbal's implementation of wikibase-term-store
    
    Bug: T221706
    Change-Id: Iaa2a3bd9d40334adb8461748cc21d2c96cf4f833

---
+++ lib/includes/Store/Sql/MediaWikiTermStore/EntityTermStoreSchemaAccess.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Wikibase\Lib\Store\Sql\MediaWikiTermStore;
+
+use Wikibase\DataModel\Entity\EntityId;
+
+/**
+ * Accessor to entity terms stored in schema.
+ *
+ * Consumers can set or unset entity terms through this accessor
+ * for a specific entity, identified by its id and type.
+ */
+interface EntityTermStoreSchemaAccess {
+
+	/**
+	 * set terms for an entity in entity term store
+	 *
+	 * @param EntityId $entityId
+	 * @param array $termsArray array containing terms per type per language:
+	 * 	[
+	 *		'type' => [
+	 *			[ 'language' => 'term' | [ 'term1', 'term2', ... ] ], ...
+	 *		], ...
+	 *  ]
+	 */
+	public function setTerms( EntityId $entityId, array $termsArray );
+
+	/**
+	 * clear terms of any type or language on a property
+	 *
+	 * @param EntityId $entityId
+	 */
+	public function unsetTerms( EntityId $entityId );
+
+}
