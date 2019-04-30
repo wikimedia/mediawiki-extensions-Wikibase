d84e59cf84c572bfa70d78bc709b4a572a4131c1
alaasarhan <alaa.sarhan@wikimedia.de>
2019-04-30 12:35:01 +0200

    Add InMemorySchemaAccess test helper class
    Bug: T221706
    Change-Id: If33a943be5848c507a2294f8b131f075d14c7bd8

---
+++ lib/tests/phpunit/Store/Sql/MediaWikiTermStore/InMemoryNormalizedTermStoreAccess.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Wikibase\Lib\Tests\Store\Sql\MediaWikiTermStore;
+
+use Wikibase\Lib\Store\Sql\MediaWikiTermStore\NormalizedTermStoreSchemaAccess;
+
+class InMemoryNormalizedTermStoreAccess implements NormalizedTermStoreSchemaAccess {
+
+	private $terms;
+	private $lastId = 0;
+
+	/**
+	 * @inheritDoc
+	 */
+	public function acquireTermIds( array $termsArray ) {
+		$ids = [];
+
+		foreach ( $termsArray as $type => $termsOfType ) {
+			foreach ( $termsOfType as $lang => $terms ) {
+				$terms = (array)$terms;
+
+				foreach ( $terms as $term ) {
+					if ( isset( $this->terms[$type][$lang][$term] ) ) {
+						$ids[] = $this->terms[$type][$lang][$term];
+					} else {
+						$this->terms[$type][$lang][$term] = ++$this->lastId;
+						$ids[] = $this->lastId;
+					}
+				}
+			}
+		}
+
+		return $ids;
+	}
+
+}
