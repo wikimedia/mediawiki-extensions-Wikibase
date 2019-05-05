0908e16298a84ebad4de937ba2aa27db84d3f46c
Jeroen De Dauw <jeroendedauw@gmail.com>
2019-05-05 10:47:32 +0200

    Script to populate new item term persistence
    I accidentally did most of this task as part of
    https://gerrit.wikimedia.org/r/c/mediawiki/extensions/Wikibase/+/507954,
    so this is esentially a re-submit in more sensible context.
    
    Bug: T221564
    Change-Id: I32f5fb035dce869a36ab326f29b9c9d541e5b0cb

---
+++ repo/includes/Store/ItemTermsRebuilder.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Wikibase\Repo\Store;
+
+use Exception;
+use Onoi\MessageReporter\MessageReporter;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\DataModel\Services\Lookup\ItemLookup;
+use Wikibase\TermStore\ItemTermStore;
+use Wikimedia\Rdbms\LBFactory;
+
+/**
+ * @license GPL-2.0-or-later
+ */
+class ItemTermsRebuilder {
+
+	private $itemTermStore;
+	private $itemIds;
+	private $progressReporter;
+	private $errorReporter;
+	private $loadBalancerFactory;
+	private $itemLookup;
+	private $batchSize;
+	private $batchSpacingInSeconds;
+
+	public function __construct(
+		ItemTermStore $itemTermStore,
+		$itemIdIterable,
+		MessageReporter $progressReporter,
+		MessageReporter $errorReporter,
+		LBFactory $loadBalancerFactory,
+		ItemLookup $itemLookup,
+		$batchSize,
+		$batchSpacingInSeconds
+	) {
+		$this->itemTermStore = $itemTermStore;
+		$this->itemIds = $itemIdIterable;
+		$this->progressReporter = $progressReporter;
+		$this->errorReporter = $errorReporter;
+		$this->loadBalancerFactory = $loadBalancerFactory;
+		$this->itemLookup = $itemLookup;
+		$this->batchSize = $batchSize;
+		$this->batchSpacingInSeconds = $batchSpacingInSeconds;
+	}
+
+	public function rebuild() {
+		$ticket = $this->loadBalancerFactory->getEmptyTransactionTicket( __METHOD__ );
+
+		foreach ( $this->getIdBatches() as $itemIds ) {
+			$this->progressReporter->reportMessage(
+				'Rebuilding ' . $itemIds[0] . ' till ' . end( $itemIds )
+			);
+
+			$this->rebuildTermsForBatch( $itemIds );
+
+			$this->loadBalancerFactory->commitAndWaitForReplication( __METHOD__, $ticket );
+
+			if ( $this->batchSpacingInSeconds > 0 ) {
+				sleep( $this->batchSpacingInSeconds );
+			}
+		}
+	}
+
+	private function getIdBatches() {
+		$idsInBatch = [];
+
+		foreach ( $this->itemIds as $itemId ) {
+			$idsInBatch[] = $itemId;
+
+			if ( count( $idsInBatch ) >= $this->batchSize ) {
+				yield $idsInBatch;
+				$idsInBatch = [];
+			}
+		}
+
+		if ( $idsInBatch !== [] ) {
+			yield $idsInBatch;
+		}
+	}
+
+	private function rebuildTermsForBatch( $itemIds ) {
+		foreach ( $itemIds as $itemId ) {
+			// TODO: catch errors
+			$item = $this->itemLookup->getItemForId( $itemId );
+
+			if ( $item !== null ) {
+				$this->saveTerms( $item );
+			}
+		}
+	}
+
+	private function saveTerms( Item $item ) {
+		try {
+			$this->itemTermStore->storeTerms( $item->getId(), $item->getFingerprint() );
+		} catch ( Exception $ex ) {
+			$this->loadBalancerFactory->rollbackMasterChanges( __METHOD__ );
+			$this->errorReporter->reportMessage(
+				'Failed to save terms of item: ' . $item->getId()->getSerialization()
+			);
+		}
+	}
+
+}
