5cd429efd3b10c09516555170603c1dce07ace81
Michael Große <michael.grosse@wikimedia.de>
2019-05-14 19:26:09 +0200

    Use correct validator and provide error messages
    The ChangeOpAliases should use AliasValidator to produce the correct
    error message keys.
    
    The validator can produce error for labels, descriptions and aliases
    that are too short. There should be translations available.
    
    Bug: T223311
    Change-Id: Ib6052834be54d5cc6c1cddaea98d23b73c6f5c47

--- repo/includes/ChangeOp/ChangeOpAliases.php
+++ repo/includes/ChangeOp/ChangeOpAliases.php
@@ -124,7 +124,7 @@ class ChangeOpAliases extends ChangeOpBase {
 	 */
 	public function validate( EntityDocument $entity ) {
 		$languageValidator = $this->termValidatorFactory->getLanguageValidator();
-		$termValidator = $this->termValidatorFactory->getLabelValidator( $entity->getType() );
+		$termValidator = $this->termValidatorFactory->getAliasValidator( $entity->getType() );
 
 		// check that the language is valid
 		$result = $languageValidator->validate( $this->languageCode );
