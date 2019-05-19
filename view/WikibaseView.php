181a771b53e85e7f44c1329a54cced95add9d913
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2019-05-19 22:59:13 +0200

    Convert WikibaseView custom resource modules to extension registration
    This can’t be done directly in extension.json, so we use the callback
    functionality, which is kind of a generic cop-out for anything that
    extension.json can’t (yet) handle. Putting that callback function inside
    the Hooks class and calling it onRegistration() seems to be the usual
    convention, according to a cursory code search.
    
    Since there are no more parts of WikibaseView.php left to be ported, the
    comment above the wfLoadExtension() call is removed. However, completely
    getting rid of the entry point isn’t easy – ExtensionProcessor complains
    if you load the same extension twice (i. e. from repo and client), and
    guarding the wfLoadExtension() call with ExtensionRegistry::isLoaded()
    doesn’t seem to work, probably because that function still returns false
    while the extension is being loaded, so for now the require_once is the
    only thing guarding us against loading WikibaseView twice. I suspect
    that when we fully switch to extension registration, we’ll require users
    to load WikibaseView in LocalSettings.php explicitly instead of doing it
    for them in the repository or client extensions.
    
    Bug: T223214
    Change-Id: I3b2a3ebc853fe67cc76847766de29707c604baea

--- view/WikibaseView.php
+++ view/WikibaseView.php
@@ -4,11 +4,4 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	die( 'Not an entry point.' );
 }
 
-// load parts already converted to extension registration
 wfLoadExtension( 'WikibaseView', __DIR__ . '/../extension-view-wip.json' );
-
-$GLOBALS['wgResourceModules'] = array_merge(
-	$GLOBALS['wgResourceModules'],
-	require __DIR__ . '/lib/resources.php',
-	require __DIR__ . '/resources/resources.php'
-);
