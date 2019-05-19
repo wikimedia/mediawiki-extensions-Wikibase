b197b6e2d25ca92ea7647aaec80309f0d80e493e
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2019-05-19 22:23:49 +0200

    Convert WikibaseView to PSR-4 autoloading
    This requires MediaWiki 1.31 or later, so add a corresponding
    restriction to the requires field. This is mostly pro forma, since all
    of Wikibase more or less requires the latest MediaWiki version, and
    WikibaseRepository explicitly requires MediaWiki >= 1.31 already.
    
    Bug: T223214
    Change-Id: Ib9042653ac9f47ded3055e93822addfa2692e89d

--- view/WikibaseView.php
+++ view/WikibaseView.php
@@ -9,9 +9,6 @@ define( 'WIKIBASE_VIEW_VERSION', '0.1-dev' );
 // load parts already converted to extension registration
 wfLoadExtension( 'WikibaseView', __DIR__ . '/../extension-view-wip.json' );
 
-// Load autoload info as long as extension classes are not PSR-4-autoloaded
-require_once __DIR__  . '/autoload.php';
-
 $GLOBALS['wgResourceModules'] = array_merge(
 	$GLOBALS['wgResourceModules'],
 	require __DIR__ . '/lib/resources.php',
