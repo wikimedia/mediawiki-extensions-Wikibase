b3e243303d86f153a719fc74da7ba4a9d0ab07fa
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2019-05-24 16:30:09 +0200

    Add SqlTypeIdsStore implementation
    This provides an implementation of TypeIdsAcquirer and TypeIdsResolver
    backed by a NameTableStore (which means it’s automatically cached all
    over the place, because updates to wbt_type should be very rare).
    
    For the tests, we register the schema update file from repo/ in a test
    in lib/; this isn’t very nice, but we want to keep the SQL files in repo
    because the tables should only exist there, even though they’ll be read
    from clients as well (which is why the implementation lives in lib/).
    
    Bug: T224036
    Change-Id: I16e73a5a392413d20547cc928d5504326b28fba4

---
+++ lib/tests/phpunit/Store/Sql/Terms/SqlTypeIdsStoreTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Wikibase\Lib\Tests\Store\Sql\Terms;
+
+use HashBagOStuff;
+use MediaWikiTestCase;
+use WANObjectCache;
+use Wikibase\Lib\Store\Sql\Terms\SqlTypeIdsStore;
+use Wikimedia\Rdbms\ILoadBalancer;
+use Wikimedia\Rdbms\IMaintainableDatabase;
+
+/**
+ * @covers \Wikibase\Lib\Store\Sql\Terms\SqlTypeIdsStore
+ *
+ * @group Wikibase
+ * @group Database
+ *
+ * @license GPL-2.0-or-later
+ */
+class SqlTypeIdsStoreTest extends MediaWikiTestCase {
+
+	/** @var SqlTypeIdsStore */
+	private $typeIdsStore;
+
+	protected function getSchemaOverrides( IMaintainableDatabase $db ) {
+		return [
+			'scripts' => [
+				__DIR__ . '/../../../../../../repo/sql/AddNormalizedTermsTablesDDL.sql',
+			],
+			'create' => [
+				'wby_type',
+			],
+		];
+	}
+
+	protected function setUp() {
+		parent::setUp();
+		$this->tablesUsed[] = 'wbt_type';
+
+		$loadBalancer = $this->createMock( ILoadBalancer::class );
+		$loadBalancer->method( 'getConnection' )
+			->willReturn( $this->db );
+		$cache = new WANObjectCache( [ 'cache' => new HashBagOStuff() ] );
+		$this->typeIdsStore = new SqlTypeIdsStore(
+			$loadBalancer,
+			$cache
+		);
+	}
+
+	public function testAcquireTypeIds_sameNamesTwice_returnsSameIds() {
+		$ids1 = $this->typeIdsStore->acquireTypeIds( [ 'label', 'description' ] );
+		$ids2 = $this->typeIdsStore->acquireTypeIds( [ 'description', 'label' ] );
+
+		ksort( $ids1 );
+		ksort( $ids2 );
+
+		$this->assertSame( $ids1, $ids2 );
+	}
+
+	public function testAcquireTypeIdsAndResolveTypeIds() {
+		$ids = $this->typeIdsStore->acquireTypeIds( [ 'label', 'description' ] );
+		$names = $this->typeIdsStore->resolveTypeIds( array_values( $ids ) );
+
+		$this->assertSame( $ids, array_flip( $names ) );
+	}
+
+}
