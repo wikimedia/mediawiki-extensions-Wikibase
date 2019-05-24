e448c66138b6aa63a98646b8aed80b9ffaceb288
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2019-05-24 15:54:40 +0200

    Add StaticTypeIdsStore implementation
    This implementation can be used when no flexible type storage is
    necessary, and hard-coding the required IDs is deemed sufficient:
    
        $typeIdsStore = new StaticTypeIdsStore( [
            'label' => 1,
    	'description' => 2,
    	'alias' => 3,
        ] );
    
    Copied from mediawiki-term-store as of ddb6c0a8b9 [1]. The namespace was
    updated for the new location and the docstrings were changed to add the
    @license tag.
    
    [1]: https://github.com/wmde/mediawiki-term-store/tree/ddb6c0a8b92ce6f59f54508adeab8996dc3ede58
    
    Change-Id: Iec8322e4c6585f5947438055bb3e721bafcbe3ee

---
+++ lib/includes/Store/Sql/Terms/StaticTypeIdsStore.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Wikibase\Lib\Store\Sql\Terms;
+
+use DomainException;
+
+/**
+ * A type IDs acquirer and resolver that only encapsulates access to a static array of IDs.
+ *
+ * @license GPL-2.0-or-later
+ */
+class StaticTypeIdsStore implements TypeIdsAcquirer, TypeIdsResolver {
+
+	/** @var int[] */
+	private $typeIdsByName;
+
+	/** @var string[] */
+	private $typeNamesById;
+
+	/**
+	 * @param int[] $types Array from type name to type ID.
+	 */
+	public function __construct( array $types ) {
+		$this->typeIdsByName = $types;
+		$this->typeNamesById = array_flip( $types );
+	}
+
+	public function acquireTypeIds( array $types ): array {
+		$ret = [];
+		foreach ( $types as $typeName ) {
+			if ( array_key_exists( $typeName, $this->typeIdsByName ) ) {
+				$ret[$typeName] = $this->typeIdsByName[$typeName];
+			} else {
+				throw new DomainException( 'Unknown type ' . $typeName . ' not supported!' );
+			}
+		}
+		return $ret;
+	}
+
+	public function resolveTypeIds( array $typeIds ): array {
+		$ret = [];
+		foreach ( $typeIds as $typeId ) {
+			if ( array_key_exists( $typeId, $this->typeNamesById ) ) {
+				$ret[$typeId] = $this->typeNamesById[$typeId];
+			}
+		}
+		return $ret;
+	}
+
+}
