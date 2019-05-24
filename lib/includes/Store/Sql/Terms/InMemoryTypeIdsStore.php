30693b895d648c40155a7d76823dd51be2e1c39e
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2019-05-24 15:54:09 +0200

    Add InMemoryTypeIdsStore implementation
    This implementation is mainly useful for tests.
    
    Copied from mediawiki-term-store as of ddb6c0a8b9 [1]. The namespace was
    updated for the new location and the docstrings were changed to add the
    @license tag.
    
    [1]: https://github.com/wmde/mediawiki-term-store/tree/ddb6c0a8b92ce6f59f54508adeab8996dc3ede58
    
    Change-Id: Ie683c7d318e223f87ee95cd3939b97ae4707aca5

---
+++ lib/includes/Store/Sql/Terms/InMemoryTypeIdsStore.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Wikibase\Lib\Store\Sql\Terms;
+
+/**
+ * Acquires and resolves unique and constant ids of types, stored in memory.
+ *
+ * @license GPL-2.0-or-later
+ */
+class InMemoryTypeIdsStore implements TypeIdsAcquirer, TypeIdsResolver {
+	private $types = [];
+	private $lastId = 0;
+
+	public function acquireTypeIds( array $types ): array {
+		$ids = [];
+		foreach ( $types as $type ) {
+			if ( !isset( $this->types[$type] ) ) {
+				$this->types[$type] = ++$this->lastId;
+			}
+			$ids[$type] = $this->types[$type];
+		}
+
+		return $ids;
+	}
+
+	public function resolveTypeIds( array $typeIds ): array {
+		$types = [];
+		foreach ( $this->types as $typeName => $typeId ) {
+			if ( in_array( $typeId, $typeIds ) ) {
+				$types[$typeId] = $typeName;
+			}
+		}
+		return $types;
+	}
+
+}
