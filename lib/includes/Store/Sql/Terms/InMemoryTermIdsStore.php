bc7cc5717b2aff44e3cd4ec8e55bc67e52a22e23
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2019-05-24 18:06:20 +0200

    Add InMemoryTermIdsStore implementation
    This implementation is mainly useful for tests.
    
    Copied from mediawiki-term-store as of ddb6c0a8b9 [1]. The namespace was
    updated for the new location, the docstrings were changed to add the
    @license tag, and the code was updated for the TermIdsCleaner rename.
    
    [1]: https://github.com/wmde/mediawiki-term-store/tree/ddb6c0a8b92ce6f59f54508adeab8996dc3ede58
    
    Change-Id: I4425fd4dbc4be417e84d1b9422c401c1055b1173

---
+++ lib/includes/Store/Sql/Terms/InMemoryTermIdsStore.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Wikibase\Lib\Store\Sql\Terms;
+
+/**
+ * @license GPL-2.0-or-later
+ */
+class InMemoryTermIdsStore implements TermIdsAcquirer, TermIdsResolver, TermIdsCleaner {
+
+	private $terms = [];
+	private $lastId = 0;
+
+	public function acquireTermIds( array $termsArray ): array {
+		$ids = [];
+
+		foreach ( $termsArray as $type => $termsOfType ) {
+			foreach ( $termsOfType as $lang => $terms ) {
+				$terms = (array)$terms;
+
+				foreach ( $terms as $term ) {
+					if ( !isset( $this->terms[$type][$lang][$term] ) ) {
+						$this->terms[$type][$lang][$term] = ++$this->lastId;
+					}
+
+					$ids[] = $this->terms[$type][$lang][$term];
+				}
+			}
+		}
+
+		return $ids;
+	}
+
+	public function resolveTermIds( array $termIds ): array {
+		$terms = [];
+		foreach ( $this->terms as $type => $termsOfType ) {
+			foreach ( $termsOfType as $lang => $termsOfLang ) {
+				foreach ( $termsOfLang as $term => $id ) {
+					if ( in_array( $id, $termIds ) ) {
+						$terms[$type][$lang][] = $term;
+					}
+				}
+			}
+		}
+		return $terms;
+	}
+
+	public function cleanTermIds( array $termIds ) {
+		$termIdsAsKeys = array_flip( $termIds );
+		foreach ( $this->terms as $type => &$termsOfType ) {
+			foreach ( $termsOfType as $lang => &$termsOfLang ) {
+				foreach ( $termsOfLang as $term => $id ) {
+					if ( array_key_exists( $id, $termIdsAsKeys ) ) {
+						unset( $termsOfLang[$term] );
+					}
+				}
+				if ( $termsOfLang === [] ) {
+					unset( $termsOfType[$lang] );
+				}
+			}
+			if ( $termsOfType === [] ) {
+				unset( $this->terms[$type] );
+			}
+		}
+	}
+
+}
