033351efd481cf1608d75cba206a828fabab0a39
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2019-05-27 15:38:57 +0200

    Add DatabaseTermIdsAcquirer and helpers
    This class is a database-backed implementation of the TermIdsAcquirer
    interface (Resolver and Cleaner will follow). It’s not fully safe
    against race conditions with the Cleaner yet. The terms use an in-memory
    SQLite instance for now.
    
    Copied from mediawiki-term-store as of ddb6c0a8b9 [1]. The namespace was
    updated for the new location, the docstrings were changed to add the
    @license and @covers tags, appropriate /*comments*/ were added to the
    test SQL file, and the reference to TermStoreSchemaUpdater was removed
    so that we don’t have to copy that class (yet).
    
    [1]: https://github.com/wmde/mediawiki-term-store/tree/ddb6c0a8b92ce6f59f54508adeab8996dc3ede58
    
    Change-Id: I123b568568fc75ae59194563c938a3bc8136cc6b

---
+++ lib/tests/phpunit/Store/Sql/Terms/DatabaseTermIdsAcquirerTest.php
@@ -0,0 +1,294 @@
+<?php
+
+namespace Wikibase\Lib\Tests\Store\Sql\Terms;
+
+use PHPUnit\Framework\TestCase;
+use Wikibase\TermStore\MediaWiki\PackagePrivate\DatabaseTermIdsAcquirer;
+use Wikibase\TermStore\MediaWiki\PackagePrivate\InMemoryTypeIdsStore;
+use Wikibase\TermStore\MediaWiki\Tests\Util\FakeLoadBalancer;
+use Wikimedia\Rdbms\DatabaseSqlite;
+use Wikimedia\Rdbms\IDatabase;
+use Wikimedia\Rdbms\ILoadBalancer;
+
+/**
+ * @covers \Wikibase\Lib\Store\Sql\Terms\DatabaseTermIdsAcquirer
+ *
+ * @group Wikibase
+ *
+ * @license GPL-2.0-or-later
+ */
+class DatabaseTermIdsAcquirerTest extends TestCase {
+
+	/**
+	 * @var IDatabase
+	 */
+	private $db;
+
+	/**
+	 * @var ILoadBalancer
+	 */
+	private $loadBalancer;
+
+	public function setUp() {
+		$this->db = DatabaseSqlite::newStandaloneInstance( ':memory:' );
+		$this->db->sourceFile(
+			__DIR__ . '/../../../../../../repo/sql/AddNormalizedTermsTablesDDL.sql' );
+		$this->loadBalancer = new FakeLoadBalancer( [
+			'dbr' => $this->db,
+		] );
+	}
+
+	public function testAcquireTermIdsReturnsArrayOfIdsForAllTerms() {
+		$typeIdsAcquirer = new InMemoryTypeIdsStore();
+
+		$dbTermIdsAcquirer = new DatabaseTermIdsAcquirer(
+			$this->loadBalancer,
+			$typeIdsAcquirer
+		);
+
+		$termsArray = [
+			'label' => [
+				'en' => 'same',
+				'de' => 'same',
+			],
+			'description' => [ 'en' => 'same' ],
+			'alias' => [
+				'en' => [ 'same', 'same', 'another', 'yet another' ]
+			]
+		];
+
+		$acquiredTermIds = $dbTermIdsAcquirer->acquireTermIds( $termsArray );
+
+		$this->assertInternalType( 'array', $acquiredTermIds );
+		$this->assertCount( 7, $acquiredTermIds );
+	}
+
+	public function testAcquireTermIdsStoresTermsInDatabase() {
+		$typeIdsAcquirer = new InMemoryTypeIdsStore();
+		$alreadyAcquiredTypeIds = $typeIdsAcquirer->acquireTypeIds(
+			[ 'label', 'description', 'alias' ]
+		);
+
+		$dbTermIdsAcquirer = new DatabaseTermIdsAcquirer(
+			$this->loadBalancer,
+			$typeIdsAcquirer
+		);
+
+		$termsArray = [
+			'label' => [
+				'en' => 'same',
+				'de' => 'same',
+			],
+			'description' => [ 'en' => 'same' ],
+			'alias' => [
+				'en' => [ 'same', 'same', 'another', 'yet another' ]
+			]
+		];
+
+		$acquiredTermIds = $dbTermIdsAcquirer->acquireTermIds( $termsArray );
+
+		$this->assertTermsArrayExistInDb( $termsArray, $alreadyAcquiredTypeIds );
+	}
+
+	public function testAcquireTermIdsStoresOnlyUniqueTexts() {
+		$typeIdsAcquirer = new InMemoryTypeIdsStore();
+
+		$dbTermIdsAcquirer = new DatabaseTermIdsAcquirer(
+			$this->loadBalancer,
+			$typeIdsAcquirer
+		);
+
+		$termsArray = [
+			'label' => [
+				'en' => 'same',
+				'de' => 'same',
+			],
+			'description' => [ 'en' => 'same' ],
+			'alias' => [
+				'en' => [ 'same', 'same', 'another', 'yet another' ]
+			]
+		];
+
+		$acquiredTermIds = $dbTermIdsAcquirer->acquireTermIds( $termsArray );
+
+		$this->assertSame(
+			3,
+			$this->db->selectRowCount( 'wbt_text', '*' )
+		);
+	}
+
+	public function testAcquireTermIdsStoresOnlyUniqueTextInLang() {
+		$typeIdsAcquirer = new InMemoryTypeIdsStore();
+
+		$dbTermIdsAcquirer = new DatabaseTermIdsAcquirer(
+			$this->loadBalancer,
+			$typeIdsAcquirer
+		);
+
+		$termsArray = [
+			'label' => [
+				'en' => 'same',
+				'de' => 'same',
+			],
+			'description' => [ 'en' => 'same' ],
+			'alias' => [
+				'en' => [ 'same', 'same', 'another', 'yet another' ]
+			]
+		];
+
+		$acquiredTermIds = $dbTermIdsAcquirer->acquireTermIds( $termsArray );
+
+		$this->assertSame(
+			4,
+			$this->db->selectRowCount( 'wbt_text_in_lang', '*' )
+		);
+	}
+
+	public function testAcquireTermIdsStoresOnlyUniqueTermInLang() {
+		$typeIdsAcquirer = new InMemoryTypeIdsStore();
+
+		$dbTermIdsAcquirer = new DatabaseTermIdsAcquirer(
+			$this->loadBalancer,
+			$typeIdsAcquirer
+		);
+
+		$termsArray = [
+			'label' => [
+				'en' => 'same',
+				'de' => 'same',
+			],
+			'description' => [ 'en' => 'same' ],
+			'alias' => [
+				'en' => [ 'same', 'same', 'another', 'yet another' ]
+			]
+		];
+
+		$acquiredTermIds = $dbTermIdsAcquirer->acquireTermIds( $termsArray );
+
+		$this->assertSame(
+			6,
+			$this->db->selectRowCount( 'wbt_term_in_lang', '*' )
+		);
+	}
+
+	public function testAcquireTermIdsReusesExistingTerms() {
+		$termsArray = [
+			'label' => [
+				'en' => 'same',
+				'de' => 'same',
+			],
+			'description' => [ 'en' => 'same' ],
+			'alias' => [
+				'en' => [ 'same', 'same', 'another', 'yet another' ]
+			]
+		];
+
+		// We will populate DB with two terms that both have
+		// text "same". One is of type "label" in language "en",
+		// and the other is of type "alias" in language "en.
+		//
+		// TermIdsAcquirer should then reuse those terms for the given
+		// termsArray above, meaning thoese pre-inserted terms will
+		// appear (their ids) in the returned array from
+		// TermIdsAcquirer::acquireTermIds( $termsArray )
+		$typeIdsAcquirer = new InMemoryTypeIdsStore();
+		$alreadyAcquiredTypeIds = $typeIdsAcquirer->acquireTypeIds(
+			[ 'label', 'description', 'alias' ]
+		);
+
+		$this->db->insert( 'wbt_text', [ 'wbx_text' => 'same' ] );
+		$sameTextId = $this->db->insertId();
+
+		$this->db->insert(
+			'wbt_text_in_lang',
+			[ 'wbxl_text_id' => $sameTextId, 'wbxl_language' => 'en' ]
+		);
+		$enSameTextInLangId = $this->db->insertId();
+
+		$this->db->insert(
+			'wbt_term_in_lang',
+			[ 'wbtl_text_in_lang_id' => $enSameTextInLangId,
+			  'wbtl_type_id' => $alreadyAcquiredTypeIds['label'] ]
+		);
+		$labelEnSameTermInLangId = (string)$this->db->insertId();
+
+		$this->db->insert(
+			'wbt_term_in_lang',
+			[ 'wbtl_text_in_lang_id' => $enSameTextInLangId,
+			  'wbtl_type_id' => $alreadyAcquiredTypeIds['alias'] ]
+		);
+		$aliasEnSameTermInLangId = (string)$this->db->insertId();
+
+		$dbTermIdsAcquirer = new DatabaseTermIdsAcquirer(
+			$this->loadBalancer,
+			$typeIdsAcquirer
+		);
+
+		$acquiredTermIds = $dbTermIdsAcquirer->acquireTermIds( $termsArray );
+
+		$this->assertCount( 7, $acquiredTermIds );
+
+		// We will assert that the returned ids of acquired terms contains
+		// one occurence of the term id for en label "same" that already existed in db,
+		// and two occurences of the term id for en alias "same" that already existed
+		// in db.
+		$this->assertCount(
+			1,
+			array_filter(
+				$acquiredTermIds,
+				function ( $id ) use ( $labelEnSameTermInLangId ) {
+					return $id === $labelEnSameTermInLangId;
+				}
+			)
+		);
+		$this->assertCount(
+			2,
+			array_filter(
+				$acquiredTermIds,
+				function ( $id ) use ( $aliasEnSameTermInLangId ) {
+					return $id === $aliasEnSameTermInLangId;
+				}
+			)
+		);
+	}
+
+	private function assertTermsArrayExistInDb( $termsArray, $typeIds ) {
+		foreach ( $termsArray as $type => $textsPerLang ) {
+			foreach ( $textsPerLang as $lang => $texts ) {
+				foreach ( (array)$texts as $text ) {
+					$textId = $this->db->selectField(
+						'wbt_text',
+						'wbx_id',
+						[ 'wbx_text' => $text ]
+					);
+
+					$this->assertNotEmpty(
+						$textId,
+						"Expected record for text '$text' is not in wbt_text"
+					);
+
+					$textInLangId = $this->db->selectField(
+						'wbt_text_in_lang',
+						'wbxl_id',
+						[ 'wbxl_language' => $lang, 'wbxl_text_id' => $textId ]
+					);
+
+					$this->assertNotEmpty(
+						$textInLangId,
+						"Expected text '$text' in language '$lang' is not in wbt_text_in_lang"
+					);
+
+					$this->assertNotEmpty(
+						$this->db->selectField(
+							'wbt_term_in_lang',
+							'wbtl_id',
+							[ 'wbtl_type_id' => $typeIds[$type], 'wbtl_text_in_lang_id' => $textInLangId ]
+						),
+						"Expected $type '$text' in language '$lang' is not in wbt_term_in_lang"
+					);
+				}
+			}
+		}
+	}
+
+}
