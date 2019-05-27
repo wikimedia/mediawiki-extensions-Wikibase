0f393d721696015cf2f61473485fcaa2618a7ab2
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2019-05-27 19:43:43 +0200

    WIP: Add DatabasePropertyTermStore
    First stab at an implementation of PropertyTermStore using the database.
    When I manually tested it in maintenance/shell.php (with only a single
    label), getTerms() seemed to work, as did storeTerms() as long as there
    were no previous terms to clean up, but cleanup/deletion failed:
    
    > Wikimedia/Rdbms/DBTransactionError with message 'Transaction round stage must be 'approved' (not 'cursory')'
    > --
    > 0:  () at includes/libs/rdbms/loadbalancer/LoadBalancer.php:1578
    > 1:  Wikimedia\Rdbms\LoadBalancer->assertTransactionRoundStage() at includes/libs/rdbms/loadbalancer/LoadBalancer.php:1416
    > 2:  Wikimedia\Rdbms\LoadBalancer->commitMasterChanges() at extensions/Wikibase/lib/includes/Store/Sql/Terms/DatabaseTermIdsCleaner.php:61
    
    Having the transaction round in the cleaner seems a bit odd anyways, now
    that I think of it – I think that needs to be in
    DatabasePropertyTermStore::cleanTermsIfUnused()?
    
    That said, this failure does not occur in the unit tests, so perhaps
    it’s related to the special way in which transactions work in
    maintenance scripts.
    
    Also, I didn’t bother moving the cleanup to the post-response stage yet,
    and some of the tests would have to be adjusted for that. The tests can
    also probably be expanded some more.
    
    Change-Id: If82e18caca8a1aa4536940c9a1c42adc1341a94b

---
+++ lib/tests/phpunit/Store/Sql/Terms/DatabasePropertyTermStoreTest.php
@@ -0,0 +1,214 @@
+<?php
+
+namespace Wikibase\Lib\Tests\Store\Sql\Terms;
+
+use MediaWikiTestCase;
+use WANObjectCache;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\DataModel\Term\Fingerprint;
+use Wikibase\DataModel\Term\Term;
+use Wikibase\DataModel\Term\TermList;
+use Wikibase\Lib\Store\Sql\Terms\DatabasePropertyTermStore;
+use Wikibase\Lib\Store\Sql\Terms\DatabaseTermIdsAcquirer;
+use Wikibase\Lib\Store\Sql\Terms\DatabaseTermIdsCleaner;
+use Wikibase\Lib\Store\Sql\Terms\DatabaseTermIdsResolver;
+use Wikibase\Lib\Store\Sql\Terms\SqlTypeIdsStore;
+use Wikibase\TermStore\MediaWiki\Tests\Util\FakeLoadBalancer;
+use Wikimedia\Rdbms\IMaintainableDatabase;
+
+/**
+ * @covers \Wikibase\Lib\Store\Sql\Terms\DatabasePropertyTermStore
+ *
+ * @group Database
+ * @group Wikibase
+ *
+ * @license GPL-2.0-or-later
+ */
+class DatabasePropertyTermStoreTest extends MediaWikiTestCase {
+
+	/** @var DatabasePropertyTermStore */
+	private $propertyTermStore;
+
+	/** @var PropertyId */
+	private $p1;
+
+	/** @var Fingerprint */
+	private $fingerprint1;
+
+	/** @var Fingerprint */
+	private $fingerprint2;
+
+	/** @var Fingerprint */
+	private $fingerprintEmpty;
+
+	protected function setUp() {
+		parent::setUp();
+		$this->tablesUsed[] = 'wbt_type';
+		$this->tablesUsed[] = 'wbt_text';
+		$this->tablesUsed[] = 'wbt_text_in_lang';
+		$this->tablesUsed[] = 'wbt_term_in_lang';
+		$this->tablesUsed[] = 'wbt_property_terms';
+
+		$loadBalancer = new FakeLoadBalancer( [
+			'dbr' => $this->db,
+		] );
+		$typeIdsStore = new SqlTypeIdsStore(
+			$loadBalancer,
+			WANObjectCache::newEmpty()
+		);
+		$this->propertyTermStore = new DatabasePropertyTermStore(
+			$loadBalancer,
+			new DatabaseTermIdsAcquirer(
+				$loadBalancer,
+				$typeIdsStore
+			),
+			new DatabaseTermIdsResolver(
+				$typeIdsStore,
+				$loadBalancer
+			),
+			new DatabaseTermIdsCleaner(
+				$loadBalancer
+			)
+		);
+		$this->p1 = new PropertyId( 'P1' );
+		$this->fingerprint1 = new Fingerprint(
+			new TermList( [ new Term( 'en', 'some label' ) ] ),
+			new TermList( [ new Term( 'en', 'description' ) ] )
+		);
+		$this->fingerprint2 = new Fingerprint(
+			new TermList( [ new Term( 'en', 'another label' ) ] ),
+			new TermList( [ new Term( 'en', 'description' ) ] )
+		);
+		$this->fingerprintEmpty = new Fingerprint();
+	}
+
+	protected function getSchemaOverrides( IMaintainableDatabase $db ) {
+		return [
+			'scripts' => [
+				__DIR__ . '/../../../../../../repo/sql/AddNormalizedTermsTablesDDL.sql',
+			],
+			'create' => [
+				'wbt_item_terms',
+				'wbt_property_terms',
+				'wbt_term_in_lang',
+				'wbt_text_in_lang',
+				'wbt_text',
+				'wbt_type',
+			],
+		];
+	}
+
+	public function testStoreAndGetTerms() {
+		$this->propertyTermStore->storeTerms(
+			$this->p1,
+			$this->fingerprint1
+		);
+
+		$fingerprint = $this->propertyTermStore->getTerms( $this->p1 );
+
+		$this->assertEquals( $this->fingerprint1, $fingerprint );
+	}
+
+	public function testGetTermsWithoutStore() {
+		$fingerprint = $this->propertyTermStore->getTerms( $this->p1 );
+
+		$this->assertTrue( $fingerprint->isEmpty() );
+	}
+
+	public function testStoreEmptyAndGetTerms() {
+		$this->propertyTermStore->storeTerms(
+			$this->p1,
+			$this->fingerprintEmpty
+		);
+
+		$fingerprint = $this->propertyTermStore->getTerms( $this->p1 );
+
+		$this->assertTrue( $fingerprint->isEmpty() );
+	}
+
+	public function testDeleteTermsWithoutStore() {
+		$this->propertyTermStore->deleteTerms( $this->p1 );
+		$this->assertTrue( true, 'did not throw an error' );
+	}
+
+	public function testStoreSameFingerprintTwiceAndGetTerms() {
+		$this->propertyTermStore->storeTerms(
+			$this->p1,
+			$this->fingerprint1
+		);
+		$this->propertyTermStore->storeTerms(
+			$this->p1,
+			$this->fingerprint1
+		);
+
+		$fingerprint = $this->propertyTermStore->getTerms( $this->p1 );
+
+		$this->assertEquals( $this->fingerprint1, $fingerprint );
+	}
+
+	public function testStoreTwoFingerprintsAndGetTerms() {
+		$this->propertyTermStore->storeTerms(
+			$this->p1,
+			$this->fingerprint1
+		);
+		$this->propertyTermStore->storeTerms(
+			$this->p1,
+			$this->fingerprint2
+		);
+
+		$fingerprint = $this->propertyTermStore->getTerms( $this->p1 );
+
+		$this->assertEquals( $this->fingerprint2, $fingerprint );
+	}
+
+	public function testStoreAndDeleteAndGetTerms() {
+		$this->propertyTermStore->storeTerms(
+			$this->p1,
+			$this->fingerprint1
+		);
+
+		$this->propertyTermStore->deleteTerms( $this->p1 );
+
+		$fingerprint = $this->propertyTermStore->getTerms( $this->p1 );
+
+		$this->assertTrue( $fingerprint->isEmpty() );
+	}
+
+	public function testStoreTermsCleansUpRemovedTerms() {
+		$this->propertyTermStore->storeTerms(
+			$this->p1,
+			new Fingerprint(
+				new TermList( [ new Term( 'en', 'The real name of UserName is John Doe' ) ] )
+			)
+		);
+		$this->propertyTermStore->storeTerms(
+			$this->p1,
+			$this->fingerprintEmpty
+		);
+
+		$this->assertSelect(
+			'wbt_text',
+			'wbx_text',
+			[ 'wbx_text' => 'The real name of UserName is John Doe' ],
+			[ /* empty */ ]
+		);
+	}
+
+	public function testDeleteTermsCleansUpRemovedTerms() {
+		$this->propertyTermStore->storeTerms(
+			$this->p1,
+			new Fingerprint(
+				new TermList( [ new Term( 'en', 'The real name of UserName is John Doe' ) ] )
+			)
+		);
+		$this->propertyTermStore->deleteTerms( $this->p1 );
+
+		$this->assertSelect(
+			'wbt_text',
+			'wbx_text',
+			[ 'wbx_text' => 'The real name of UserName is John Doe' ],
+			[ /* empty */ ]
+		);
+	}
+
+}
