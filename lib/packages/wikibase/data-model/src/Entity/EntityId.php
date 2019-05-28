5561fd2b7258d401e1f2ce70fdd08f7f064f42d7
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2019-05-28 18:54:28 +0200

    Optimize EntityId::isForeign()
    When this method was first introduced, getRepositoryName() would split
    the serialization each time it was called, and using strpos() in
    isForeign() was an optimization to avoid that cost. However, since #769
    (commit 7623cfef8d), the serialization is only split once in the
    constructor, so checking strpos each time instead of deferring to the
    already populated $repositoryName is probably slightly more expensive.
    
    As a very minor optimization, we directly access $this->repositoryName
    instead of calling getRepositoryName(). Subclasses that for some reason
    override getRepositoryName() to do something different must override
    isForeign() as well.

--- lib/packages/wikibase/data-model/src/Entity/EntityId.php
+++ lib/packages/wikibase/data-model/src/Entity/EntityId.php
@@ -164,15 +164,14 @@ abstract class EntityId implements Comparable, Serializable {
 	}
 
 	/**
-	 * Returns true iff EntityId::getRepoName returns a non-empty string.
+	 * Returns true iff EntityId::getRepositoryName returns a non-empty string.
 	 *
 	 * @since 6.2
 	 *
 	 * @return bool
 	 */
 	public function isForeign() {
-		// not actually using EntityId::getRepoName for performance reasons
-		return strpos( $this->serialization, ':' ) > 0;
+		return $this->repositoryName !== '';
 	}
 
 	/**
