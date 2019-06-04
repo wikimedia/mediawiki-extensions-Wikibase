37b653acb86a8f8402e5b64b1fd0527031148d0d
mvolz <marielle.volz@gmail.com>
2019-06-04 13:56:25 +0100

    Add temp configuration variable for tabs
    Add temporary configuration variable
    for enabling tabs in references.
    
    This allows the anticipated feature
    to be temporarily enabled on testwikidata
    only config and be disabled on wikidata.
    
    This change only adds the config and is
    not yet currently used.
    
    To enable set:
    
    $wgWBRepoSettings['enableRefTabs'] = true;
    
    in LocalSettings.php.
    
    Sets variable available to the view via
    mw.config.get('wbRefTabsEnabled') and returns
    true if set to true, false if not set or is
    false.
    
    Corresponding operations config change at
    Iafd8b7221f73
    
    Bug:T199197
    Change-Id: Iafd8b7221f73526f810b67427a7c8d1f93157a50

---
+++ lib/includes/Modules/ViewModule.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Wikibase;
+
+use ResourceLoader;
+use ResourceLoaderContext;
+use ResourceLoaderModule;
+
+/**
+ * JavaScript variables needed to change the view
+ *
+ * @license GPL-2.0-or-later
+ * @author M. Volz
+ */
+class ViewModule extends ResourceLoaderModule {
+
+	protected $targets = [ 'desktop' ];
+
+	/**
+	 * This one lets the client JavaScript know whether or not
+	 * to enable tabs
+	 * @see ResourceLoaderModule::getScript
+	 *
+	 * @param ResourceLoaderContext $context
+	 *
+	 * @return string
+	 */
+	public function getScript( ResourceLoaderContext $context ) {
+
+		$wbRefTabsEnabled;
+
+		$settings = Settings::singleton();
+
+		if ( $settings->hasSetting( 'enableRefTabs' ) ) {
+			$wbRefTabsEnabled = $settings->getSetting( 'enableRefTabs' );
+		} else {
+			$wbRefTabsEnabled = false;
+		}
+
+		return ResourceLoader::makeConfigSetScript( [ 'wbRefTabsEnabled' => $wbRefTabsEnabled ] );
+	}
+
+}
