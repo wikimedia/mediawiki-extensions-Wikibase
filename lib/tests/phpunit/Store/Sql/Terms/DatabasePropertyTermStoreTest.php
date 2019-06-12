a6b358cbacef657bd3e1ff2f323a2a4ee643acb6
Marius Hoch <hoo@online.de>
2019-06-12 19:16:00 +0200

    TermIdsResolver: Allow filtering terms by type and lang
    Contains Ifa97e4886b645e8a3c107e77a7e9bd1fa82832c9 at 17591c2
    
    Bug: T225003
    Change-Id: I4c8aa96d75bb6039de2cd0fe7c05081f8ff9fc39

--- lib/tests/phpunit/Store/Sql/Terms/DatabasePropertyTermStoreTest.php
+++ lib/tests/phpunit/Store/Sql/Terms/DatabasePropertyTermStoreTest.php
@@ -66,6 +66,7 @@ class DatabasePropertyTermStoreTest extends MediaWikiTestCase {
 			),
 			new DatabaseTermIdsResolver(
 				$typeIdsStore,
+				$typeIdsStore,
 				$loadBalancer
 			),
 			new DatabaseTermIdsCleaner(
