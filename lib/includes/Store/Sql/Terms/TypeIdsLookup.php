cbf6b85682a1be3529d91d34357f286c236e5b6e
alaasarhan <alaa.sarhan@wikimedia.de>
2019-06-13 15:11:29 +0200

    Add TypeIdsLookup interface and implement it in DatabaseTypeIdsStore.
    Bug: T225718
    Change-Id: Ifa97e4886b645e8a3c107e77a7e9bd1fa82832c9

---
+++ lib/includes/Store/Sql/Terms/TypeIdsLookup.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Wikibase\Lib\Store\Sql\Terms;
+
+interface TypeIdsLookup {
+
+	/**
+	 * Lookup type ids for given type names.
+	 *
+	 * @param string[] $types
+	 * @return int[] array type names to type ids
+	 */
+	public function lookupTypeIds( array $types ): array;
+}
