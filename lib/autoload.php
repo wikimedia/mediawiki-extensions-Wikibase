6b276a931cd0e1fb40bcc9a8441c14af6cfaa0dc
alaasarhan <alaa.sarhan@wikimedia.de>
2019-06-14 18:33:39 +0200

    Add CachedDatabasePropertyLabelResolver implementation using new store.
    Bug: T225003
    Change-Id: I7badbd8a9151fc045c0abc25b6ca65e4ff66d500

--- lib/autoload.php
+++ lib/autoload.php
@@ -165,6 +165,7 @@ $wgAutoloadClasses += [
 	'Wikibase\\Lib\\Store\\Sql\\SiteLinkTable' => __DIR__ . '/includes/Store/Sql/SiteLinkTable.php',
 	'Wikibase\\Lib\\Store\\Sql\\SqlEntityInfoBuilder' => __DIR__ . '/includes/Store/Sql/SqlEntityInfoBuilder.php',
 	'Wikibase\\Lib\\Store\\Sql\\TermSqlIndex' => __DIR__ . '/includes/Store/Sql/TermSqlIndex.php',
+	'Wikibase\\Lib\\Store\\Sql\\Terms\\CachedDatabasePropertyLabelResolver' => __DIR__ . '/includes/Store/Sql/Terms/CachedDatabasePropertyLabelResolver.php',
 	'Wikibase\\Lib\\Store\\Sql\\Terms\\DatabaseItemTermStore' => __DIR__ . '/includes/Store/Sql/Terms/DatabaseItemTermStore.php',
 	'Wikibase\\Lib\\Store\\Sql\\Terms\\DatabasePropertyTermStore' => __DIR__ . '/includes/Store/Sql/Terms/DatabasePropertyTermStore.php',
 	'Wikibase\\Lib\\Store\\Sql\\Terms\\DatabaseTermIdsAcquirer' => __DIR__ . '/includes/Store/Sql/Terms/DatabaseTermIdsAcquirer.php',
