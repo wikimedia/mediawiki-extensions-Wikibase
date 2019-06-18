7aa3b68d13f89a2e47194238de87abb838862afb
Pablo Grass <pablo.grass@wikimedia.de>
2019-06-18 15:15:45 +0200

    bridge: use stable file names for dist files
    Bug: T225949
    Change-Id: I2193414d95e9f9cd3a02df853f7e520d7a5cb5ac

---
+++ client/data-bridge/vue.config.js
@@ -0,0 +1,20 @@
+const filePrefix = 'data-bridge.';
+
+module.exports = {
+	configureWebpack: () => ( {
+		output: {
+			filename: `${filePrefix}[name].js`
+		}
+	} ),
+	chainWebpack: config => {
+		config.optimization.delete( 'splitChunks' );
+
+		if ( process.env.NODE_ENV === 'production' ) {
+			config.plugin( 'extract-css' )
+				.tap( ( [ options, ...args ] ) => [
+					Object.assign( {}, options, { filename: `${filePrefix}[name].css` } ),
+					...args,
+				] );
+		}
+	}
+}
