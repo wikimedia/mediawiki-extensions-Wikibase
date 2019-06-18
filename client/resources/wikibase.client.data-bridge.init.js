bbc280b2fcdf6cd458b0ead7c88bc46b598cb464
Michael Große <michael.grosse@wikimedia.de>
2019-06-18 16:11:33 +0200

    Implement basic link counter
    This implements the link count in javascript that doesn't need a build
    step. It might be a consideration to have only the app itself in
    typescript and vue and load that only after some eligible links have
    been found.
    
    This is behind the feature flag as wikibase.client.data-bridge.init is
    only loaded if the feature flag is true.
    
    Bug: T225930
    Change-Id: Id9da39634611b53b43aa620e55f624638f05df26

---
+++ client/resources/wikibase.client.data-bridge.init.js
@@ -0,0 +1,6 @@
+$( function () {
+	var $validLinks = $( 'a' ).filter( function ( index, element ) {
+		return element.href.match( /^https:\/\/www\.wikidata\.org\/wiki\/(Q[1-9][0-9]*).*#(P[1-9][0-9]*)/ );
+	} );
+	console.log( 'Number of links potentially usable for wikidata bridge: ' + $validLinks.length );
+} );
