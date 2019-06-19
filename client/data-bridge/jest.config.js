1232ba1862cb0b9af02e07a517e3ce0ddce166b3
Michael Große <michael.grosse@wikimedia.de>
2019-06-19 12:56:23 +0200

    bridge: Add hello world jest testing infrastructure
    This adds the basic testing infrastructure to run jest tests. This is
    more ore less exactly what `vue create data-bridge` produces when run
    with unit-tests and jest and dedicated config files.
    
    Bug: T225950
    Change-Id: Ic90267e839c82ab0bd762c5dcfa2a7e68cb49aff

---
+++ client/data-bridge/jest.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+	moduleFileExtensions: [
+		'js',
+		'jsx',
+		'json',
+		'vue',
+		'ts',
+		'tsx'
+	],
+	moduleNameMapper: {
+		'^@/(.*)$': '<rootDir>/src/$1'
+	},
+	snapshotSerializers: [
+		'jest-serializer-vue'
+	],
+	testMatch: [
+		'**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
+	],
+	testURL: 'http://localhost/',
+	transform: {
+		'^.+\\.vue$': 'vue-jest',
+		'.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+		'^.+\\.tsx?$': 'ts-jest'
+	},
+	transformIgnorePatterns: [
+		'/node_modules/'
+	],
+	watchPlugins: [
+		'jest-watch-typeahead/filename',
+		'jest-watch-typeahead/testname'
+	]
+};
