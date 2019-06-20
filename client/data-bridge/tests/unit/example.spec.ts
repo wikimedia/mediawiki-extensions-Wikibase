a9f7bd7d7f959e9269cacd761c3ca2d42b42b7d6
Michael Große <michael.grosse@wikimedia.de>
2019-06-20 13:51:02 +0200

    bridge: move vue code to presentation dir for clearer separation of concerns
    This eases the cognitive load when considering what is (preferably
    logicless) presentation code, what is part of the business logic and
    what is part of the API that talks to Wikidata, etc.
    
    Bug: T226489
    Change-Id: If0ba2aaa2d1c75fe4840d70730c5fa845f25ad48

--- client/data-bridge/tests/unit/example.spec.ts
+++ client/data-bridge/tests/unit/example.spec.ts
@@ -1,5 +1,5 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import HelloWorld from '@/presentation/components/HelloWorld.vue';
 
 describe( 'HelloWorld.vue', () => {
 	it( 'renders props.msg when passed', () => {
