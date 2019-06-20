5b79cf4594c513eeb4ce673bd8b4c642862f0134
bitPogo <matthias.geisler@wikimedia.de>
2019-06-20 16:07:30 +0200

    Introduces lintrules
    This patch is based on the current ruleset of termbox (including
    I6c635edb9c75cc79031bcda5d0c7619ba2218a4d ) and adds a eslint config as
    well as a stylelint config.
    This patch also inlcudes I22115b36ea4864ff98761c5ba9e842316b1e5329.
    
    Bug: T225952
    Change-Id: I2df9b5e31d90546db458e9c8dfabb0d3601b2434

---
+++ client/data-bridge/.eslintrc.js
@@ -0,0 +1,72 @@
+module.exports = {
+	extends: [
+		'wikimedia',
+		'wikimedia/node',
+		'wikimedia/client',
+		'wikimedia/language/es6',
+		'wikimedia/language/es2017',
+		'plugin:vue/strongly-recommended',
+	],
+	plugins: [
+		'@typescript-eslint',
+		'filenames',
+	],
+	parser: 'vue-eslint-parser',
+	parserOptions: {
+		parser: '@typescript-eslint/parser',
+		sourceType: 'module',
+		ecmaFeatures: {
+			impliedStrict: true,
+		},
+	},
+	root: true,
+	rules: {
+		'function-paren-newline': [ 'error', 'consistent' ],
+		'@typescript-eslint/type-annotation-spacing': [ 'error' ],
+		'filenames/match-exported': 'error',
+		'object-shorthand': [ 'error', 'always' ],
+		'@typescript-eslint/explicit-member-accessibility': [ 'error', { 'accessibility': 'explicit' } ],
+
+		// problematic in TypeScript / ES6
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': [ 'error', { 'argsIgnorePattern': '^_' } ],
+		'no-undef': 'error',
+
+		// diverging from Wikimedia rule set
+		'max-len': [ 'error', 120 ],
+		'comma-dangle': [ 'error', {
+			arrays: 'always-multiline',
+			objects: 'always-multiline',
+			imports: 'always-multiline',
+			exports: 'always-multiline',
+			functions: 'always-multiline',
+		} ],
+		'operator-linebreak': 'off',
+		'quote-props': 'off',
+		'valid-jsdoc': 'off',
+
+		'vue/html-indent': [ 'error', 'tab' ],
+		'vue/max-attributes-per-line': [ 'error', {
+			singleline: 3,
+			multiline: {
+				max: 1,
+				allowFirstLine: false,
+			},
+		} ],
+
+		'no-restricted-properties': 'off',
+
+		/* remove the following if
+		 * https://github.com/wikimedia/eslint-config-wikimedia/pull/171
+		 * is active in eslint-config-wikimedia
+		 */
+		'prefer-const': 'error',
+	},
+	overrides: {
+		files: [ '**/*.ts' ],
+		parser: 'vue-eslint-parser',
+		rules: {
+			'no-undef': 'off',
+		},
+	},
+};
