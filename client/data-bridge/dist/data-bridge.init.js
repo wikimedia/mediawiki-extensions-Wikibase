a789412ae317a017fcfc8f7fa90c7737857d5a6e
Michael Große <michael.grosse@wikimedia.de>
2019-06-24 13:08:18 +0200

    bridge: Make linkcounter somewhat testable
    This patch is an attempt to make parts of the initial js testable with
    jest. It is understood as a basis for discussion. If it is to be
    accepted, then it might make sense to squash it into the parent commit.
    
    The approach implemented here has some drawbacks:
    * The included file has to be referenced in its compiled version
    * The included file has to be listed in the package.json
    * The included file has to be listed in the Resources.php
    * we are mixing include syntax
    
    The alternatives seem to be either omitting testing or using the full
    webpack functionality to bundle the scripts with all the overhead that
    this incurs. Are there other options?
    
    Bug: T225930
    Change-Id: Icb25a3d5bc409176996dfba57f9c576d122cfe6d

--- client/data-bridge/dist/data-bridge.init.js
+++ client/data-bridge/dist/data-bridge.init.js
@@ -1,15 +1,12 @@
+function countLinks() {
+    var selectLinks = require('./selectLinks');
+    var validLinks = selectLinks();
+    // eslint-disable-next-line no-console
+    console.log("Number of links potentially usable for data bridge: " + validLinks.length);
+}
 if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', countLinks);
 }
 else {
     countLinks();
-}
-function countLinks() {
-    var linkRegexp = /^https:\/\/www\.wikidata\.org\/wiki\/(Q[1-9][0-9]*).*#(P[1-9][0-9]*)/;
-    var validLinks = Array.from(document.querySelectorAll('a[href]'))
-        .filter(function (element) {
-        return element.href.match(linkRegexp);
-    });
-    // eslint-disable-next-line no-console
-    console.log('Number of links potentially usable for wikidata bridge: ' + validLinks.length);
 }
