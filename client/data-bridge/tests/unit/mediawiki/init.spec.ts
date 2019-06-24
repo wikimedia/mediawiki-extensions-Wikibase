f540ff7a05fb2cb0bf5a832e17ddb39ddaa4039d
bitPogo <matthias.geisler@wikimedia.de>
2019-06-24 15:27:53 +0200

    bridge: Rudimentary bridge app
    This patch introduces a very simple implentation of the app entry point.
    It is loads by ResourceLoader, iff a valid link has been detected.
    
    Bug: T226485
    Bug: T226609
    Change-Id: I0dc347859f96bd11751aa999d3887819413a4f4c

---
+++ client/data-bridge/tests/unit/mediawiki/init.spec.ts
@@ -0,0 +1,41 @@
+import init from '@/mediawiki/init';
+import * as linker from '@/mediawiki/selectLinks';
+import MwWindow from '@/@types/mediawiki/MwWindow';
+
+function mockMwEnv( using: () => Promise<any> ): void {
+	( window as MwWindow ).mw = {
+		loader: {
+			using,
+		},
+	};
+}
+
+describe( 'init', () => {
+	it( 'loads `wikibase.client.data-bridge.app`, if it found supported links', () => {
+		const using = jest.fn( () => {
+			return new Promise<void>( ( resolve ) => resolve() );
+		} );
+
+		mockMwEnv( using );
+		const mock = jest.spyOn( linker, 'filterLinksByHref' ); // spy on otherFn
+		mock.mockReturnValue( [
+			{ href: ' https://www.wikidata.org/wiki/Q123#P321' },
+		] as any );
+
+		init();
+		expect( using ).toBeCalledTimes( 1 );
+		expect( using ).toBeCalledWith( 'wikibase.client.data-bridge.app' );
+
+	} );
+
+	it( 'loads does nothing if no supported links are found', () => {
+		const using = jest.fn();
+		mockMwEnv( using );
+		const mock = jest.spyOn( linker, 'filterLinksByHref' );
+		mock.mockReturnValue( [] );
+
+		init();
+		expect( using ).toBeCalledTimes( 0 );
+
+	} );
+} );
