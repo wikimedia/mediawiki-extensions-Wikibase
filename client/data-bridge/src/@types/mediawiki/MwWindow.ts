c3b66580df1e73ac29393a6abcc453676ae911d8
bitPogo <matthias.geisler@wikimedia.de>
2019-06-24 15:27:53 +0200

    bridge: Rudimentary bridge app
    This patch introduces a very simple implentation of the app entry point.
    It is loads by ResourceLoader, iff a valid link has been detected.
    
    Bug: T226485
    Bug: T226609
    
    Change-Id: I0dc347859f96bd11751aa999d3887819413a4f4c

---
+++ client/data-bridge/src/@types/mediawiki/MwWindow.ts
@@ -0,0 +1,13 @@
+interface ResourceLoader {
+	using: ( module: string|string[] ) => Promise<any>;
+}
+
+interface MediaWiki {
+	loader: ResourceLoader;
+}
+
+interface MwWindow extends Window {
+	mw: MediaWiki;
+}
+
+export default MwWindow;
