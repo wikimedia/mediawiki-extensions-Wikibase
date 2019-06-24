6d32070c3ef9af9f1963c9f71647b1846b85f9cb
Michael Große <michael.grosse@wikimedia.de>
2019-06-24 15:27:53 +0200

    Rudimentary bridge app
    This patch introduces a very simple implentation of the app entry point.
    It is loads by ResourceLoader, iff a valid link has been detected.
    
    Bug: T226485
    Bug: T226609
    
    Change-Id: I0dc347859f96bd11751aa999d3887819413a4f4c

--- client/data-bridge/src/main.ts
+++ client/data-bridge/src/main.ts
@@ -1,8 +1,2 @@
-import Vue from 'vue';
-import App from './App.vue';
-
-Vue.config.productionTip = false;
-
-new Vue( {
-	render: ( h ) => h( App ),
-} ).$mount( '#app' );
+// eslint-disable-next-line no-console
+console.log( 'hello from wikidata-data-bridge.' );
