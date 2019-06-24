2782514c509424761f8fc1debb2ebe65e673eebd
Michael Große <michael.grosse@wikimedia.de>
2019-06-24 13:08:18 +0200

    bridge: Make linkcounter somewhat testable
    This patch is an attempt to make parts of the initial js testable with
    jest. It is understood as a basis for discussion. If it is to be
    accepted, then it might make sense to squash it into the parent commit.
    
    The approach implemented here has some drawbacks:
    * The included file has to be referenced in its compiled version
    * The included file has to be listed in the package.json
    * The included file has to be listed in the Resources.php
    * we are mixing include syntax
    
    The alternatives seem to be either omitting testing or using the full
    webpack functionality to bundle the scripts with all the overhead that
    this incurs. Are there other options?
    
    Bug: T225930
    Change-Id: Icb25a3d5bc409176996dfba57f9c576d122cfe6d

---
+++ client/data-bridge/tests/unit/mediawiki/linkSelection.spec.ts
@@ -0,0 +1,13 @@
+const selectLinks = require( '../../../src/mediawiki/selectLinks' );
+
+test( 'foo', () => {
+	document.body.innerHTML =
+		'<div>' +
+		' <a rel="nofollow" class="external text" href="https://www.wikidata.org/wiki/Q4115189#P31">a link to be selected</a>' +
+		' <a href="/mediawiki/index.php?title=Page_with_sitelink_to_item" title="Page with sitelink to item">a link to be not selected</a>' +
+		'</div>';
+
+	const actualSelectedLinks = selectLinks();
+
+	expect( actualSelectedLinks.length ).toBe( 1 );
+} );
