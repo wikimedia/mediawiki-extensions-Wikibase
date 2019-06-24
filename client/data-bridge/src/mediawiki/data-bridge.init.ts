d3730ff3631c717aad5e3134ad42c9d2af2d26b6
Michael Große <michael.grosse@wikimedia.de>
2019-06-24 15:27:53 +0200

    bridge: make init js testable -- the webpack way
    This is an alternative to Icb25a3d5bc409176996dfba57f9c576d122cfe6d that
    realizes the bundling with webpack.
    
    Pros:
    * much simpler config
    * consistent imports with the rest of the app
    * all internal details are hidden resource loader / Resource.php
    
    Cons:
    * much much larger filesize for init js in dist/
    ** can we somehow reduce this with clever webpack options?
    
    Change-Id: Ie8bd0798b3c26a0022d309e6498706452346bb49

--- client/data-bridge/src/mediawiki/data-bridge.init.ts
+++ client/data-bridge/src/mediawiki/data-bridge.init.ts
@@ -1,15 +1,13 @@
+import selectLinks from './selectLinks';
+
+function countLinks() {
+	const validLinks = selectLinks();
+	// eslint-disable-next-line no-console
+	console.log( `Number of links potentially usable for data bridge: ${validLinks.length}` );
+}
+
 if ( document.readyState === 'loading' ) {
 	document.addEventListener( 'DOMContentLoaded', countLinks );
 } else {
 	countLinks();
-}
-
-function countLinks(): void {
-	const linkRegexp = /^https:\/\/www\.wikidata\.org\/wiki\/(Q[1-9][0-9]*).*#(P[1-9][0-9]*)/;
-	const validLinks = Array.from( document.querySelectorAll( 'a[href]' ) )
-		.filter( function ( element: Element ): boolean {
-			return !!( element as HTMLAnchorElement ).href.match( linkRegexp );
-		} );
-	// eslint-disable-next-line no-console
-	console.log( `Number of links potentially usable for data bridge: ${ validLinks.length }` );
 }
