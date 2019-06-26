30c0717e9eef328d6c4af25dbb1a31074ec2e546
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2019-06-26 18:42:14 +0200

    bridge: load link href regexp from config
    This is the first step towards making this configurable, so that it
    works for different wikis – the init code looks up the regexp in
    mw.config, and the same code that causes the ResourceLoader to be loaded
    also stores the regexp there (still hard-coded for now). The value is an
    object, not just a plain string, because we’ll need at least one other
    setting (the repo API URL) later.
    
    The declaration of the `mw` global is restricted to selectLinks.ts
    because other code should probably not access it.
    
    Bug: T226494
    Change-Id: I4d28943c782f9a95666a4d7f62962dcf3e5a9877

--- client/data-bridge/src/mediawiki/selectLinks.ts
+++ client/data-bridge/src/mediawiki/selectLinks.ts
@@ -1,11 +1,13 @@
+declare var mw: any;
+
 export function selectLinks(): HTMLAnchorElement[] {
 	return Array.from( document.querySelectorAll( 'a[href]' ) );
 }
 
 export function filterLinksByHref( selectedLinks: HTMLAnchorElement[] ): HTMLAnchorElement[] {
-	// Todo: create some configuration for this link --> T226494
-	const linkRegexp = /^https:\/\/www\.wikidata\.org\/wiki\/(Q[1-9][0-9]*).*#(P[1-9][0-9]*)/;
-	return selectedLinks.filter( ( element: HTMLAnchorElement ): boolean => {
+	const dataBridgeConfig = mw.config.get( 'wbDataBridgeConfig' ),
+		linkRegexp = new RegExp( dataBridgeConfig.hrefRegExp );
+	return selectedLinks.filter( function ( element: HTMLAnchorElement ): boolean {
 		return !!element.href.match( linkRegexp );
 	} );
 }
