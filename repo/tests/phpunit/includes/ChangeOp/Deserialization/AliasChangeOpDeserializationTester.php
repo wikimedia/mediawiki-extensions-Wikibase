20140fc23129f278d8b542d71f7bd4b9077c772e
rosalie <perside.rosalie@wikimedia.de>
2019-06-26 15:11:19 +0200

    Remove feature flag for the bugfix for wbeditentity setting aliases to empty array
    Feature flag was added here:
    I7ac01018b43487ed11b1468fff1386682e3db698
    
    Bug: T223305
    Change-Id: Ia72409b5e58ffd8a7c986423f1d35ee9d109d01c

--- repo/tests/phpunit/includes/ChangeOp/Deserialization/AliasChangeOpDeserializationTester.php
+++ repo/tests/phpunit/includes/ChangeOp/Deserialization/AliasChangeOpDeserializationTester.php
@@ -5,7 +5,6 @@ namespace Wikibase\Repo\Tests\ChangeOp\Deserialization;
 use Wikibase\DataModel\Entity\EntityDocument;
 use Wikibase\DataModel\Term\AliasesProvider;
 use Wikibase\Repo\ChangeOp\ChangeOpDeserializer;
-use Wikibase\Repo\WikibaseRepo;
 use Wikibase\Summary;
 
 /**
@@ -40,35 +39,12 @@ trait AliasChangeOpDeserializationTester {
 
 	public function testGivenChangeRequestSettingAliasesToEmpty_enAliasGroupDoesNotExist() {
 		$entity = $this->getEntityWithExistingAliases();
-		$repoSettings = WikibaseRepo::getDefaultInstance()
-			->getSettings();
-		$originalSetting = $repoSettings->getSetting( 'featureFlagWbeditentitySetEmptyAliases' );
-		$repoSettings->setSetting( 'featureFlagWbeditentitySetEmptyAliases', true );
 		$changeOp = $this->getChangeOpDeserializer()->createEntityChangeOp( [
 			'aliases' => [ 'en' => [] ],
 		] );
-		$repoSettings->setSetting( 'featureFlagWbeditentitySetEmptyAliases', $originalSetting );
 
 		$changeOp->apply( $entity, new Summary() );
 		$this->assertFalse( $entity->getAliasGroups()->hasGroupForLanguage( 'en' ) );
-	}
-
-	public function testGivenChangeRequestSettingAliasesToEmpty_AliasesAreNotChanged_WithFeatureFlagFalse() {
-		$entity = $this->getEntityWithExistingAliases();
-		$preExistingAliases = $entity->getAliasGroups()->getByLanguage( 'en' )->getAliases();
-
-		$repoSettings = WikibaseRepo::getDefaultInstance()
-			->getSettings();
-		$originalSetting = $repoSettings->getSetting( 'featureFlagWbeditentitySetEmptyAliases' );
-		$repoSettings->setSetting( 'featureFlagWbeditentitySetEmptyAliases', false );
-		$changeOp = $this->getChangeOpDeserializer()->createEntityChangeOp( [
-			'aliases' => [ 'en' => [] ],
-		] );
-		$repoSettings->setSetting( 'featureFlagWbeditentitySetEmptyAliases', $originalSetting );
-
-		$changeOp->apply( $entity, new Summary() );
-
-		$this->assertSame( $preExistingAliases, $entity->getAliasGroups()->getByLanguage( 'en' )->getAliases() );
 	}
 
 	public function testGivenChangeRequestRemovingAllExistingEnAliases_enAliasGroupDoesNotExist() {
