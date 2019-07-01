85630e2576dfe31e7d5bba0f2ae03553a795ea6e
Noa Rave <noa.rave@wikimedia.de>
2019-07-01 10:35:13 +0200

    Browser test for old revisions of Wikibase item pages to check there are no edit links
    Bug: T95406
    Depends-on: I1ebd2fbbf8c176a8856d10a4992a702676ba5f16
    Change-Id: Ia0c97383b8f9632f649d2e31d92f75d613816fcd

--- repo/tests/selenium/wdio-wikibase/pageobjects/item.page.js
+++ repo/tests/selenium/wdio-wikibase/pageobjects/item.page.js
@@ -7,18 +7,22 @@ const Page = require( 'wdio-mediawiki/Page' ),
 	PageMixture = MixinBuilder.mix( Page ).with( MainStatementSection, ComponentInteraction );
 
 class ItemPage extends PageMixture {
-
 	static get ITEM_WIDGET_SELECTORES() {
 		return {
 			MAIN_STATEMENTS: 'div.wikibase-entityview-main > .wikibase-statementgrouplistview',
 			ADD_STATEMENT: 'div.wikibase-addtoolbar > .wikibase-toolbar-button-add > a',
 			SAVE_BUTTON: '.wikibase-toolbar-button-save',
-			EDIT: ',wikibase-toolbar-button-edit',
+			EDIT: '.wikibase-toolbar-container .wikibase-toolbar-button-edit',
 			PROPERTY_INPUT: '.ui-entityselector-input',
 			VALUE_INPUT: '.valueview-input',
+			ITEM_DESCRIPTION_INPUT: '.wikibase-descriptionview-text .wikibase-descriptionview-input',
 			QUALIFIERS: '.wikibase-statementview-qualifiers .listview-item',
 			REFERENCES: '.wikibase-statementview-references',
-			NTH_ELEMENT: '.wikibase-listview > .listview-item'
+			NTH_ELEMENT: '.wikibase-listview > .listview-item',
+			RECENT_CHANGES: '#n-recentchanges',
+			LASTCHANGE_HISTORY: '.mw-changeslist-last .mw-changeslist-history',
+			REVISION: '#pagehistory',
+			REVISION_DATE_LINK: '.after .mw-changeslist-date'
 		};
 	}
 
@@ -42,6 +46,26 @@ class ItemPage extends PageMixture {
 		return $( this.constructor.ITEM_WIDGET_SELECTORES.VALUE_INPUT );
 	}
 
+	get editButton() {
+		return $( this.constructor.ITEM_WIDGET_SELECTORES.EDIT );
+	}
+
+	get saveButton() {
+		return $( this.constructor.ITEM_WIDGET_SELECTORES.SAVE_BUTTON );
+	}
+
+	get recentChanges() {
+		return $( this.constructor.ITEM_WIDGET_SELECTORES.RECENT_CHANGES );
+	}
+
+	get lastChangeHistory() {
+		return $( this.constructor.ITEM_WIDGET_SELECTORES.LASTCHANGE_HISTORY );
+	}
+
+	get descriptionInputField() {
+		return $( this.constructor.ITEM_WIDGET_SELECTORES.ITEM_DESCRIPTION_INPUT );
+	}
+
 	getNthQualifierPropertyInput( statement, qualifierIndex ) {
 		let qualifier = statement.$$( this.constructor.ITEM_WIDGET_SELECTORES.QUALIFIERS )[ qualifierIndex ];
 		return qualifier.$( this.constructor.ITEM_WIDGET_SELECTORES.PROPERTY_INPUT );
@@ -60,6 +84,25 @@ class ItemPage extends PageMixture {
 	getNthReferenceValueInput( statement, referenceIndex ) {
 		let reference = statement.$$( this.constructor.ITEM_WIDGET_SELECTORES.REFERENCES )[ referenceIndex ];
 		return reference.$( this.constructor.ITEM_WIDGET_SELECTORES.VALUE_INPUT );
+	}
+
+	editItemDescription( description ) {
+
+		this.editButton.waitForExist( 3000 );
+		browser.pause( 100 );
+		this.editButton.click();
+		this.descriptionInputField.waitForExist();
+		this.descriptionInputField.setValue( description );
+		this.saveButton.waitForExist( 1000 );
+		this.saveButton.click();
+	}
+
+	goToPreviousRevision() {
+		this.recentChanges.click();
+		this.lastChangeHistory.waitForExist( 1000 );
+		this.lastChangeHistory.click();
+		let revisionList = $( this.constructor.ITEM_WIDGET_SELECTORES.REVISION );
+		revisionList.$( this.constructor.ITEM_WIDGET_SELECTORES.REVISION_DATE_LINK ).click();
 	}
 
 	isSaveButtonEnabled() {
