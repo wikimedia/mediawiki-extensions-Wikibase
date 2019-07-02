6d395f9834867287bfc0abf29df765a5a492598d
Amir Sarabadani <Ladsgroup@gmail.com>
2019-07-02 10:58:18 +0200

    Move trivial unit tests to the new structure
    Bug: T226911
    Bug: T87781
    Change-Id: Ib4411ca2e12ceecaa258fd0d9f962df6d3316806

---
+++ repo/tests/phpunit/unit/includes/ParserOutput/ParserOutputJsConfigBuilderTest.php
@@ -0,0 +1,178 @@
+<?php
+
+namespace Wikibase\Repo\Tests\ParserOutput;
+
+use DataValues\Deserializers\DataValueDeserializer;
+use DataValues\Serializers\DataValueSerializer;
+use DataValues\StringValue;
+use Wikibase\DataModel\DeserializerFactory;
+use Wikibase\DataModel\Entity\BasicEntityIdParser;
+use Wikibase\DataModel\Entity\EntityDocument;
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Entity\Property;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\DataModel\SerializerFactory;
+use Wikibase\DataModel\Snak\PropertyValueSnak;
+use Wikibase\DataModel\Statement\StatementListProvider;
+use Wikibase\DataModel\Term\LabelsProvider;
+use Wikibase\Repo\ParserOutput\ParserOutputJsConfigBuilder;
+
+/**
+ * @covers \Wikibase\Repo\ParserOutput\ParserOutputJsConfigBuilder
+ *
+ * @group Wikibase
+ *
+ * @license GPL-2.0-or-later
+ * @author Katie Filbert < aude.wiki@gmail.com >
+ */
+class ParserOutputJsConfigBuilderTest extends \MediaWikiUnitTestCase {
+
+	private function newEntitySerializer() {
+		$serializerFactory = new SerializerFactory(
+			new DataValueSerializer(),
+			SerializerFactory::OPTION_SERIALIZE_MAIN_SNAKS_WITHOUT_HASH +
+			SerializerFactory::OPTION_SERIALIZE_REFERENCE_SNAKS_WITHOUT_HASH
+		);
+
+		return $serializerFactory->newEntitySerializer();
+	}
+
+	public function testBuildConfigItem() {
+		$item = new Item( new ItemId( 'Q5881' ) );
+		$this->addLabels( $item );
+		$mainSnakPropertyId = $this->addStatements( $item );
+
+		$configBuilder = new ParserOutputJsConfigBuilder( $this->newEntitySerializer() );
+		$configVars = $configBuilder->build( $item );
+
+		$this->assertWbEntityId( 'Q5881', $configVars );
+
+		$this->assertWbEntity(
+			$this->getSerialization( $item, $mainSnakPropertyId ),
+			$configVars
+		);
+
+		$this->assertSerializationEqualsEntity(
+			$item,
+			json_decode( $configVars['wbEntity'], true )
+		);
+	}
+
+	public function testBuildConfigProperty() {
+		$property = new Property( new PropertyId( 'P330' ), null, 'string' );
+		$this->addLabels( $property );
+		$mainSnakPropertyId = $this->addStatements( $property );
+
+		$configBuilder = new ParserOutputJsConfigBuilder( $this->newEntitySerializer() );
+		$configVars = $configBuilder->build( $property );
+
+		$this->assertWbEntityId( 'P330', $configVars );
+
+		$expectedSerialization = $this->getSerialization( $property, $mainSnakPropertyId );
+		$expectedSerialization['datatype'] = 'string';
+
+		$this->assertWbEntity( $expectedSerialization, $configVars );
+
+		$this->assertSerializationEqualsEntity(
+			$property,
+			json_decode( $configVars['wbEntity'], true )
+		);
+	}
+
+	public function assertWbEntityId( $expectedId, array $configVars ) {
+		$this->assertEquals(
+			$expectedId,
+			$configVars['wbEntityId'],
+			'wbEntityId'
+		);
+	}
+
+	public function assertWbEntity( array $expectedSerialization, array $configVars ) {
+		$this->assertEquals(
+			$expectedSerialization,
+			json_decode( $configVars['wbEntity'], true ),
+			'wbEntity'
+		);
+	}
+
+	public function assertSerializationEqualsEntity( EntityDocument $entity, $serialization ) {
+		$deserializerFactory = new DeserializerFactory(
+			new DataValueDeserializer( [ 'string' => StringValue::class ] ),
+			new BasicEntityIdParser()
+		);
+
+		$unserializedEntity = $deserializerFactory->newEntityDeserializer()->deserialize( $serialization );
+
+		$this->assertTrue(
+			$unserializedEntity->equals( $entity ),
+			'unserialized entity equals entity'
+		);
+	}
+
+	private function addLabels( LabelsProvider $entity ) {
+		$termList = $entity->getLabels();
+		$termList->setTextForLanguage( 'en', 'Cake' );
+		$termList->setTextForLanguage( 'de', 'Kuchen' );
+	}
+
+	private function addStatements( StatementListProvider $statementListProvider ) {
+		$propertyId = new PropertyId( 'P794' );
+
+		$statementListProvider->getStatements()->addNewStatement(
+			new PropertyValueSnak( $propertyId, new StringValue( 'kittens!' ) ),
+			null,
+			null,
+			$this->makeGuid( $statementListProvider->getId() )
+		);
+
+		return $propertyId;
+	}
+
+	private function makeGuid( EntityId $entityId ) {
+		return $entityId->getSerialization() . '$muahahaha';
+	}
+
+	private function getSerialization( EntityDocument $entity, PropertyId $propertyId ) {
+		$serialization = [
+			'id' => $entity->getId()->getSerialization(),
+			'type' => $entity->getType(),
+			'labels' => [
+				'de' => [
+					'language' => 'de',
+					'value' => 'Kuchen'
+				],
+				'en' => [
+					'language' => 'en',
+					'value' => 'Cake'
+				]
+			],
+			'descriptions' => [],
+			'aliases' => [],
+			'claims' => [
+				$propertyId->getSerialization() => [
+					[
+						'id' => $this->makeGuid( $entity->getId() ),
+						'mainsnak' => [
+							'snaktype' => 'value',
+							'property' => $propertyId->getSerialization(),
+							'datavalue' => [
+								'value' => 'kittens!',
+								'type' => 'string'
+							],
+						],
+						'type' => 'statement',
+						'rank' => 'normal',
+					],
+				],
+			],
+		];
+
+		if ( $entity instanceof Item ) {
+			$serialization['sitelinks'] = [];
+		}
+		return $serialization;
+	}
+
+}
---
+++ repo/tests/phpunit/unit/includes/Store/StoreTest.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace Wikibase\Repo\Tests\Store;
+
+use Wikibase\DataAccess\EntitySource;
+use Wikibase\DataAccess\WikibaseServices;
+use Wikibase\IdGenerator;
+use Wikibase\Lib\Store\Sql\EntityChangeLookup;
+use Wikibase\Lib\Store\EntityNamespaceLookup;
+use Wikibase\Repo\Store\EntityTitleStoreLookup;
+use Wikibase\Lib\Store\LabelConflictFinder;
+use Wikibase\Lib\Store\SiteLinkLookup;
+use Wikibase\Repo\Store\ChangeStore;
+use Wikibase\Repo\Store\EntitiesWithoutTermFinder;
+use Wikibase\Repo\Store\ItemsWithoutSitelinksFinder;
+use Wikibase\Repo\Store\SiteLinkConflictLookup;
+use Wikibase\Repo\WikibaseRepo;
+use Wikibase\SqlStore;
+use Wikibase\Store;
+use Wikibase\Store\EntityIdLookup;
+use Wikibase\TermIndex;
+
+/**
+ * @covers \Wikibase\Store
+ *
+ * @group Wikibase
+ * @group WikibaseStore
+ * @group Database
+ *
+ * @group medium
+ *
+ * @license GPL-2.0-or-later
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class StoreTest extends \MediaWikiUnitTestCase {
+
+	public function instanceProvider() {
+		$wikibaseRepo = WikibaseRepo::getDefaultInstance();
+
+		$instances = [
+			new SqlStore(
+				$wikibaseRepo->getEntityChangeFactory(),
+				$wikibaseRepo->getEntityIdParser(),
+				$wikibaseRepo->getEntityIdComposer(),
+				$this->getMock( EntityIdLookup::class ),
+				$this->getMock( EntityTitleStoreLookup::class ),
+				new EntityNamespaceLookup( [] ),
+				$this->getMock( IdGenerator::class ),
+				$this->getMock( WikibaseServices::class ),
+				new EntitySource( 'testsource', 'testdb', [], '', '' )
+			)
+		];
+
+		return [ $instances ];
+	}
+
+	/**
+	 * @dataProvider instanceProvider
+	 * @param Store $store
+	 */
+	public function testRebuild( Store $store ) {
+		$store->rebuild();
+		$this->assertTrue( true );
+	}
+
+	/**
+	 * @dataProvider instanceProvider
+	 */
+	public function testNewSiteLinkStore( Store $store ) {
+		$this->assertInstanceOf( SiteLinkLookup::class, $store->newSiteLinkStore() );
+	}
+
+	/**
+	 * @dataProvider instanceProvider
+	 */
+	public function testNewEntitiesWithoutTermFinder( Store $store ) {
+		$this->assertInstanceOf( EntitiesWithoutTermFinder::class, $store->newEntitiesWithoutTermFinder() );
+	}
+
+	/**
+	 * @dataProvider instanceProvider
+	 */
+	public function testItemsWithoutSitelinksFinder( Store $store ) {
+		$this->assertInstanceOf( ItemsWithoutSitelinksFinder::class, $store->newItemsWithoutSitelinksFinder() );
+	}
+
+	/**
+	 * @dataProvider instanceProvider
+	 */
+	public function testNewTermCache( Store $store ) {
+		$this->assertInstanceOf( TermIndex::class, $store->getTermIndex() );
+	}
+
+	/**
+	 * @dataProvider instanceProvider
+	 */
+	public function testGetLabelConflictFinder( Store $store ) {
+		$this->assertInstanceOf( LabelConflictFinder::class, $store->getLabelConflictFinder() );
+	}
+
+	/**
+	 * @dataProvider instanceProvider
+	 */
+	public function testGetEntityChangeLookup( Store $store ) {
+		$this->assertInstanceOf( EntityChangeLookup::class, $store->getEntityChangeLookup() );
+	}
+
+	/**
+	 * @dataProvider instanceProvider
+	 */
+	public function testGetChangeStore( Store $store ) {
+		$this->assertInstanceOf( ChangeStore::class, $store->getChangeStore() );
+	}
+
+	/**
+	 * @dataProvider instanceProvider
+	 */
+	public function testGetSiteLinkConflictLookup( Store $store ) {
+		$this->assertInstanceOf(
+			SiteLinkConflictLookup::class,
+			$store->getSiteLinkConflictLookup()
+		);
+	}
+
+	public function testLookupCacheConstantsHaveDistinctValues() {
+		$constants = [
+			Store::LOOKUP_CACHING_ENABLED,
+			Store::LOOKUP_CACHING_DISABLED,
+			Store::LOOKUP_CACHING_RETRIEVE_ONLY
+		];
+		$this->assertSame( count( $constants ), count( array_unique( $constants ) ) );
+	}
+
+}
