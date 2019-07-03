7707ca6b9c81b4dc474fa1ea513e6d7dd58da8c5
Tulsi Bhagat <Tulsibhagat50@gmail.com>
2019-07-03 12:11:26 +0545

    Enable monolingual text lanugage for rmf (Finnish Kalo)
    Bug: T226701
    Change-Id: Ia6ae09fac52a974b307b97f61dcdbaa55a25cfff

--- lib/includes/WikibaseContentLanguages.php
+++ lib/includes/WikibaseContentLanguages.php
@@ -113,6 +113,7 @@ class WikibaseContentLanguages {
 					'quc', // T155376
 					'qya', // T185194
 					'rar', // T155427
+					'rmf', // T226701
 					'shy', // T184783
 					'sia', // T217521
 					'sjd', // T188596
