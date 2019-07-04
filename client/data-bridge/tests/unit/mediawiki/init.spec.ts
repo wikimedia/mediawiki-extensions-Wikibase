5693a22cc25511a12dcff6d6f41aa3555b41e4bc
Michael Große <michael.grosse@wikimedia.de>
2019-07-04 17:08:13 +0200

    bridge: Integrate the new selector class
    Bug: T227177
    Change-Id: Ifa5c28ecfc1b3e259ca9c668b459b5484873f4f5

--- client/data-bridge/tests/unit/mediawiki/init.spec.ts
+++ client/data-bridge/tests/unit/mediawiki/init.spec.ts
@@ -1,36 +1,45 @@
 import init from '@/mediawiki/init';
-import * as linker from '@/mediawiki/selectLinks';
+import BridgeDomElementsSelector from '@/mediawiki/BridgeDomElementsSelector';
 import MwWindow from '@/@types/mediawiki/MwWindow';
 
-function mockMwEnv( using: () => Promise<any> ): void {
+function mockMwEnv( using: () => Promise<any>, get: () => any, warn: () => void ): void {
 	( window as MwWindow ).mw = {
 		loader: {
 			using,
 		},
 		config: {
-			get: jest.fn(),
+			get,
 		},
 		log: {
 			deprecate: jest.fn(),
 			error: jest.fn(),
-			warn: jest.fn(),
+			warn,
 		},
 	};
 }
 
+jest.mock( '@/mediawiki/BridgeDomElementsSelector', function () {
+	return jest.fn().mockImplementation( () => {} );
+} );
+
 describe( 'init', () => {
+
 	it( 'loads `wikibase.client.data-bridge.app`, if it found supported links', () => {
 		const app = { launch: jest.fn() },
 			require = jest.fn( () => app ),
 			using = jest.fn( () => {
 				return new Promise( ( resolve ) => resolve( require ) );
 			} );
+		const get = (): any => ( {
+			hrefRegExp: 'https://www\\.wikidata\\.org/wiki/(Q[1-9][0-9]*).*#(P[1-9][0-9]*)',
+		} );
+		mockMwEnv( using, get, jest.fn() );
 
-		mockMwEnv( using );
-		const mock = jest.spyOn( linker, 'filterLinksByHref' );
-		mock.mockReturnValue( [
-			{ href: 'https://www.wikidata.org/wiki/Q123#P321' },
-		] as any );
+		( BridgeDomElementsSelector as jest.Mock ).mockImplementation( () => {
+			return {
+				selectElementsToOverload: () => [ {} ],
+			};
+		} );
 
 		return init().then( () => {
 			expect( using ).toBeCalledTimes( 1 );
@@ -42,12 +51,32 @@ describe( 'init', () => {
 
 	it( 'loads does nothing if no supported links are found', () => {
 		const using = jest.fn();
-		mockMwEnv( using );
-		const mock = jest.spyOn( linker, 'filterLinksByHref' );
-		mock.mockReturnValue( [] );
+		const get = (): any => ( {
+			hrefRegExp: 'https://www\\.wikidata\\.org/wiki/(Q[1-9][0-9]*).*#(P[1-9][0-9]*)',
+		} );
+		mockMwEnv( using, get, jest.fn() );
+
+		( BridgeDomElementsSelector as jest.Mock ).mockImplementation( () => {
+			return {
+				selectElementsToOverload: () => [],
+			};
+		} );
 
 		init();
 		expect( using ).toBeCalledTimes( 0 );
 
+	} );
+
+	it( 'warns on missing hrefRegExp', () => {
+		const using = jest.fn();
+		const get = (): any => ( {
+			hrefRegExp: null,
+		} );
+		const warn = jest.fn();
+		mockMwEnv( using, get, warn );
+
+		init();
+		expect( using ).toBeCalledTimes( 0 );
+		expect( warn ).toBeCalledTimes( 1 );
 	} );
 } );
