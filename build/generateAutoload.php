6be1640f4b036214c31b024ce2edaabd81fce23b
Amir Sarabadani <Ladsgroup@gmail.com>
2019-07-11 00:02:24 +0200

    Remove PSR-4-compliant parts from lib/autoload.php
    Load them as namespace, this cleans up $wgAutoLoadClasses.
    build/generateAutoload.php is adjusted so that it won’t try to add them
    back to the file either.
    
    Bug: T172368
    Change-Id: Id71ad76bc333ee983d1c9823ef2d78060b2d3e5c

--- build/generateAutoload.php
+++ build/generateAutoload.php
@@ -6,7 +6,9 @@
 namespace Wikibase\Build;
 
 use AutoloadGenerator;
+use ExtensionRegistry;
 use Maintenance;
+use UnexpectedValueException;
 
 require_once getenv( 'MW_INSTALL_PATH' ) !== false
 	? getenv( 'MW_INSTALL_PATH' ) . '/maintenance/Maintenance.php'
@@ -120,6 +122,28 @@ class GenerateWikibaseAutoload extends Maintenance {
 	private function generateAutoloadForComponent( $componentDir, array $dirs, array $files = [] ) {
 		$base = __DIR__ . '/../' . $componentDir;
 		$generator = new AutoloadGenerator( $base );
+
+		$extensionJsonPath = realpath( __DIR__ . '/../extension-' . $componentDir . '-wip.json' );
+		if ( $extensionJsonPath !== false ) {
+			$extensionJson = file_get_contents( $extensionJsonPath );
+			$extensionInfo = json_decode( $extensionJson, true );
+			$extensionJsonDir = dirname( $extensionJsonPath );
+
+			$autoloadClasses = [];
+			$autoloadNamespaces = [];
+			ExtensionRegistry::exportAutoloadClassesAndNamespaces(
+				$extensionJsonDir,
+				$extensionInfo,
+				$autoloadClasses,
+				$autoloadNamespaces
+			);
+
+			if ( $autoloadClasses !== [] ) {
+				throw new UnexpectedValueException( 'We do not use AutoloadClasses in extension.json!' );
+			}
+			$generator->setPsr4Namespaces( $autoloadNamespaces );
+		}
+
 		foreach ( $dirs as $componentDir ) {
 			$generator->readDir( $base . '/' . $componentDir );
 		}
