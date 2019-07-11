d72f9ada8c6628898292a080576ba986ea55d3a0
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2019-07-11 16:45:38 +0200

    Add termbox browser tests
    Bug: T225196
    Change-Id: If9d3b1ff9bfe306755af6133e53152e9e8e94cea

---
+++ repo/tests/selenium/specs/termbox.spec.js
@@ -0,0 +1,3 @@
+// Once T199116 is resolved this file is no longer needed and the termbox test suite can be loaded
+// via wdio.conf.js instead.
+require( '../../../../view/lib/wikibase-termbox/tests/selenium/specs/Termbox.spec.js' );
