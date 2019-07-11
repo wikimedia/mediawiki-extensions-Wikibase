89e8ebdbec0a2d25c00eaa0809e2e9bcb788b168
rosalie <perside.rosalie@wikimedia.de>
2019-07-11 19:52:07 +0200

    Add ChangeOpResult interface
    Make ChangeOp::apply() return ChangeOpResult by adding a ChangeOpResult
    interface that will be implemented (by e.g ChangeOpAliasesResult, ChangeOpLabelResult).
    Add a DummyChangeOpResult that will be returned by ChangeOP::apply() in WikibaseLexeme.
    See: Ie7cb0d819fa5b98421066c33af660c7b688b5b33
    
    Bug: T220696
    Change-Id: I3841ddc06a68dafd72b740decc7e145c8351f2a7

---
+++ repo/includes/ChangeOp/DummyChangeOpResult.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace Wikibase\Repo\ChangeOp;
+
+use Wikibase\DataModel\Entity\EntityId;
+
+/**
+ * Class DummyChangeOpResult has no result
+ */
+class DummyChangeOpResult implements ChangeOpResult {
+
+	private $entityId;
+
+	public function __construct( EntityId $entityId = null ) {
+		 $this->entityId = $entityId;
+	}
+
+	public function getEntityId() {
+		return $this->entityId;
+	}
+
+	public function isEntityChanged() {
+		 return false;
+	}
+
+}
