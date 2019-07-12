d06c05cc9b99e99d7b59d1bb9693620bc390b321
bitPogo <matthias.geisler@wikimedia.de>
2019-07-12 15:47:39 +0200

    bridge: plug Entity into Application
    This patch merges the Entity store chain and the Application store
    chain.
    The BRIDGE_INIT action is with this patch the main start point, from
    where other initalization actions are called.
    
    Bug: T227181
    Change-Id: Ib2e536fe2e946c2727144a9748e9ff9aa3a96be6

--- client/data-bridge/src/store/actions.ts
+++ client/data-bridge/src/store/actions.ts
@@ -7,13 +7,30 @@ import {
 	EDITFLOW_SET,
 	PROPERTY_TARGET_SET,
 } from '@/store/mutationTypes';
+import {
+	NS_ENTITY,
+} from '@/store/namespaces';
+import {
+	ENTITY_INIT,
+} from '@/store/entity/actionTypes';
+import namespacedStoreEvent from '@/store/namespacedStoreEvent';
 
 export const actions = {
 	[ BRIDGE_INIT ](
 		context: ActionContext<Application, any>,
-		payload: { editFlow: string, targetProperty: string },
-	): void {
+		payload: {
+			editFlow: string,
+			targetProperty: string,
+			targetEntity: string,
+		},
+	): Promise<void> {
 		context.commit( EDITFLOW_SET, payload.editFlow );
 		context.commit( PROPERTY_TARGET_SET, payload.targetProperty );
+		return Promise.resolve(
+			context.dispatch(
+				namespacedStoreEvent( NS_ENTITY, ENTITY_INIT ),
+				payload.targetEntity,
+			),
+		);
 	},
 };
--- client/data-bridge/src/store/index.ts
+++ client/data-bridge/src/store/index.ts
@@ -4,6 +4,10 @@ import Application from '@/store/Application';
 import { actions } from '@/store/actions';
 import { getters } from '@/store/getters';
 import { mutations } from '@/store/mutations';
+import createEntity from './entity';
+import {
+	NS_ENTITY,
+} from '@/store/namespaces';
 
 Vue.use( Vuex );
 
@@ -19,6 +23,9 @@ export function createStore(): Store<Application> {
 		getters,
 		mutations,
 		strict: process.env.NODE_ENV !== 'production',
+		modules: {
+			[ NS_ENTITY ]: createEntity(),
+		},
 	};
 
 	return new Store<Application>( storeBundle );
