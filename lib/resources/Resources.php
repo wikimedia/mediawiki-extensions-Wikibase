f96549797bb18bce6c62791a9e366c250af6f458
Amir Sarabadani <Ladsgroup@gmail.com>
2019-07-15 18:20:43 +0200

    Drop deprecated wikibase.RepoApi{,Error}
    Bug: T228074
    Change-Id: I8d2b6155cf5e4385ee6e35f93af2ce4292857b5c

--- lib/resources/Resources.php
+++ lib/resources/Resources.php
@@ -65,7 +65,6 @@ return call_user_func( function() {
 
 	$modules = array_merge(
 		$modules,
-		require __DIR__ . '/deprecated/resources.php',
 		require __DIR__ . '/jquery.wikibase/resources.php'
 	);
 
