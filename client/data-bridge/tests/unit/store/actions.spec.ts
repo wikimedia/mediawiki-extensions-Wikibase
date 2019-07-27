88bf1b7a998e2c0ca1b853f9809baba00f15f345
bitPogo <matthias.geisler@wikimedia.de>
2019-07-27 11:24:25 +0200

    bridge: clean up store test grouping
    This patch makes a minor changes - it make a more specific grouping of
    the testsets of store/actions.spec.
    
    Change-Id: I92b13fc3cf4f42158e37adc9db2f12d1f13926d1

--- client/data-bridge/tests/unit/store/actions.spec.ts
+++ client/data-bridge/tests/unit/store/actions.spec.ts
@@ -18,7 +18,6 @@ import ApplicationStatus from '@/store/ApplicationStatus';
 
 describe( 'root/actions', () => {
 	describe( BRIDGE_INIT, () => {
-
 		it( `commits to ${EDITFLOW_SET}`, () => {
 			const editFlow = 'Heraklid';
 			const context = newMockStore( {} );
@@ -67,31 +66,32 @@ describe( 'root/actions', () => {
 			} );
 		} );
 
-		it( `commits to ${APPLICATION_STATUS_SET} on successful entity lookup`, () => {
-			const context = newMockStore( {} );
+		describe( 'ApplicationStatus', () => {
+			it( `commits to ${APPLICATION_STATUS_SET} on successful entity lookup`, () => {
+				const context = newMockStore( {} );
 
-			return actions[ BRIDGE_INIT ]( context, {
-				editFlow: 'overwrite',
-				targetProperty: 'P123',
-				targetEntity: 'Q123',
-			} ).then( () => {
-				expect( context.commit ).toHaveBeenCalledWith( APPLICATION_STATUS_SET, ApplicationStatus.READY );
+				return actions[ BRIDGE_INIT ]( context, {
+					editFlow: 'overwrite',
+					targetProperty: 'P123',
+					targetEntity: 'Q123',
+				} ).then( () => {
+					expect( context.commit ).toHaveBeenCalledWith( APPLICATION_STATUS_SET, ApplicationStatus.READY );
+				} );
+			} );
+
+			it( `commits to ${APPLICATION_STATUS_SET} on fail entity lookup`, () => {
+				const context = newMockStore( {
+					dispatch: () => Promise.reject(),
+				} );
+
+				return actions[ BRIDGE_INIT ]( context, {
+					editFlow: 'overwrite',
+					targetProperty: 'P123',
+					targetEntity: 'Q123',
+				} ).catch( () => {
+					expect( context.commit ).toHaveBeenCalledWith( APPLICATION_STATUS_SET, ApplicationStatus.ERROR );
+				} );
 			} );
 		} );
-
-		it( `commits to ${APPLICATION_STATUS_SET} on fail entity lookup`, () => {
-			const context = newMockStore( {
-				dispatch: () => Promise.reject(),
-			} );
-
-			return actions[ BRIDGE_INIT ]( context, {
-				editFlow: 'overwrite',
-				targetProperty: 'P123',
-				targetEntity: 'Q123',
-			} ).catch( () => {
-				expect( context.commit ).toHaveBeenCalledWith( APPLICATION_STATUS_SET, ApplicationStatus.ERROR );
-			} );
-		} );
-
 	} );
 } );
