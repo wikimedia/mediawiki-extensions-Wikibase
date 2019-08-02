b1e1c8cd8521cae2ee1a43bde237cb97b2cb68ca
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2019-08-02 18:04:47 +0200

    Define $wgMessagesDirs in WikibaseView PHP entry point
    This seems to be necessary; see also change I85ecc207fc (commit
    92b545a98c), which does the same thing for WikibaseLib, and the links
    mentioned in its commit message.
    
    Bug: T229604
    Change-Id: I470a59670195c70613a105c5b0dd2788d057fa6e
    Cherry-Picked-From: c1333620179cc7b348b2848be8e6b857cbbf1e17

--- view/WikibaseView.php
+++ view/WikibaseView.php
@@ -5,3 +5,9 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 }
 
 wfLoadExtension( 'WikibaseView', __DIR__ . '/../extension-view-wip.json' );
+
+call_user_func( function() {
+	global $wgMessagesDirs;
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['WikibaseView'] = __DIR__ . '/lib/wikibase-data-values-value-view/i18n';
+} );
