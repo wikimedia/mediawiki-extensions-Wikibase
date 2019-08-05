32d38608ae8407caecaa85d25e3b4fb46ada467d
bitPogo <matthias.geisler@wikimedia.de>
2019-08-06 01:11:32 +0200

    bridge: Statement Actions
    This patch introduces the init action for the statement module.
    
    Bug: T228654
    Change-Id: I0d0205df3a06d479d6bc321e0abe9332cc8fc981

---
+++ client/data-bridge/tests/unit/store/entity/statements/actions.spec.ts
@@ -0,0 +1,49 @@
+import { actions } from '@/store/entity/statements/actions';
+import {
+	STATEMENTS_INIT,
+} from '@/store/entity/statements/actionTypes';
+import {
+	STATEMENTS_SET,
+} from '@/store/entity/statements/mutationTypes';
+import StatementMap from '@/datamodel/StatementMap';
+import newMockStore from '../../newMockStore';
+
+describe( 'statements/actions', () => {
+	describe( STATEMENTS_INIT, () => {
+		it( `commits to ${STATEMENTS_SET}`, () => {
+			const payload = {
+				entityId: 'Q42',
+				statements: {
+					P23: [ {
+						id: 'Q60$6f832804-4c3f-6185-38bd-ca00b8517765',
+						mainsnak: {
+							snaktype: 'value',
+							property: 'P23',
+							datatype: 'wikibase-item',
+							datavalue: {
+								value: {
+									'entity-type': 'item',
+									id: 'Q6342720',
+								},
+								type: 'wikibase-entityid',
+							},
+						},
+						type: 'statement',
+						rank: 'normal',
+					} ],
+				} as StatementMap,
+			};
+
+			const context = newMockStore( {} );
+			actions[ STATEMENTS_INIT ](
+				context,
+				payload,
+			);
+
+			expect( context.commit ).toHaveBeenCalledWith(
+				STATEMENTS_SET,
+				payload,
+			);
+		} );
+	} );
+} );
