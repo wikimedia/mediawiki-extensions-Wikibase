68cf238069659dfb515b9571e0953ed7ba0b7c07
Michael Große <michael.grosse@wikimedia.de>
2019-08-07 12:07:21 +0200

    bridge: collect coverage from all ts and vue files in src/
    This setting ensures that also files that are not covered by any tests
    show up in the coverage report.
    
    This setting does not cause coverage to be collected by itself. That has
    to still be invoked by using --coverage.
    
    https://jestjs.io/docs/en/configuration#collectcoveragefrom-array
    
    Change-Id: I180158235d9607d6ad7f6f2b8eac9c2e72e91a54

--- client/data-bridge/jest.config.js
+++ client/data-bridge/jest.config.js
@@ -31,4 +31,12 @@ module.exports = {
 		'jest-watch-typeahead/filename',
 		'jest-watch-typeahead/testname',
 	],
+	collectCoverageFrom: [
+		'src/**/*.{ts,vue}',
+		'!src/@types/**',
+		'!src/datamodel/**',
+		'!src/definitions/**',
+		'!src/mock-data/**',
+		'!src/mock-entry.ts',
+	],
 };
