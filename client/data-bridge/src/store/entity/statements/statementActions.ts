90e0648b699ede4ca952ca853b0c3079be751fcf
bitPogo <matthias.geisler@wikimedia.de>
2019-08-07 10:12:13 +0200

    bridge: wire Snak Unit Action with Statements
    This patch wire the snak unit actions against the statements module.
    
    Bug: T228654
    Bug: T228984
    Change-Id: I01990a6e8917e17fbda2884af3448334d3ca6ff9

---
+++ client/data-bridge/src/store/entity/statements/statementActions.ts
@@ -0,0 +1,25 @@
+import { ActionContext } from 'vuex';
+import Application from '@/store/Application';
+import {
+	STATEMENTS_INIT,
+} from '@/store/entity/statements/actionTypes';
+import {
+	STATEMENTS_SET,
+} from '@/store/entity/statements/mutationTypes';
+import StatementsState from '@/store/entity/statements/StatementsState';
+import StatementMap from '@/datamodel/StatementMap';
+
+export const statementActions = {
+	[ STATEMENTS_INIT ](
+		context: ActionContext<StatementsState, Application>,
+		payload: {
+			entityId: string;
+			statements: StatementMap;
+		},
+	): void {
+		context.commit( STATEMENTS_SET, {
+			entityId: payload.entityId,
+			statements: payload.statements,
+		} );
+	},
+};
