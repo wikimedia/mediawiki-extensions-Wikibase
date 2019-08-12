fa5e3144cfd2719e4a8707925ba14cf1e414634f
bitPogo <matthias.geisler@wikimedia.de>
2019-08-12 09:45:57 +0200

    bridge: Align newMockableEntityRevision
    This patch makes adds testset for newMockableEntityRevision similar to
    I767eb5c0f7bfbf00bfc5e30f8baa80488f874ce8 .
    It also makes a small change to newMockableEntityRevision, therefore it
    is now capable to be called without any arguments.
    
    Change-Id: I95a2a26bb1ac67e48dc18af54592dc279e151d31

---
+++ client/data-bridge/tests/unit/store/newMockableEntityRevision.spec.ts
@@ -0,0 +1,59 @@
+import newMockableEntityRevision from './newMockableEntityRevision';
+import Snak from '@/datamodel/Snak';
+import StatementMap from '@/datamodel/StatementMap';
+
+describe( 'newMockableEntityRevision', () => {
+	it( 'contains a entity', () => {
+		const entityRevision = newMockableEntityRevision();
+		expect( entityRevision.entity ).toBeDefined();
+	} );
+
+	describe( 'Entity', () => {
+		describe( 'id', () => {
+			it( 'is set to Q1 by default', () => {
+				const entityRevision = newMockableEntityRevision();
+				expect( entityRevision.entity.id ).toBe( 'Q1' );
+			} );
+
+			it( 'is set as passed', () => {
+				const id = 'Q4711';
+				const entityRevision = newMockableEntityRevision( { id } );
+				expect( entityRevision.entity.id ).toBe( id );
+			} );
+		} );
+
+		describe( 'statements', () => {
+			it( 'contains a empty by default', () => {
+				const entityRevision = newMockableEntityRevision();
+				expect( entityRevision.entity.statements ).toStrictEqual( {} );
+			} );
+
+			it( 'is set as passed', () => {
+				const statements: StatementMap = {
+					P23: [ {
+						type: 'statement',
+						id: 'Q60$6f832804-4c3f-6185-38bd-ca00b8517765',
+						rank: 'normal',
+						mainsnak: {} as Snak,
+					} ],
+				};
+
+				const entityRevision = newMockableEntityRevision( { statements } );
+				expect( entityRevision.entity.statements ).toBe( statements );
+			} );
+		} );
+	} );
+
+	describe( 'revisionId', () => {
+		it( 'is set to 0 by default', () => {
+			const entityRevision = newMockableEntityRevision();
+			expect( entityRevision.revisionId ).toBe( 0 );
+		} );
+
+		it( 'are set as passed', () => {
+			const revisionId = 23;
+			const entityRevision = newMockableEntityRevision( { revisionId } );
+			expect( entityRevision.revisionId ).toBe( revisionId );
+		} );
+	} );
+} );
