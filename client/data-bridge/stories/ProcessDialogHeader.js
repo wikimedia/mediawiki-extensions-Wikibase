9ed410911016dfb17d7ba14fd7bffe0fdde7df0d
Michael Große <michael.grosse@wikimedia.de>
2019-08-21 11:07:35 +0200

    bridge: Add Header component
    Relevant ooui styles and inspiration have been taken from:
    https://github.com/wikimedia/oojs-ui/blob/d3cefbbc4fa708069f6b86008f02e08e37e36c88/src/themes/wikimediaui/windows.less#L115
    https://www.mediawiki.org/wiki/OOUI/Windows/Process_Dialogs
    https://doc.wikimedia.org/oojs-ui/master/demos/?page=dialogs&theme=wikimediaui&direction=ltr&platform=desktop
    
    This uses a <div> instead of a semantic <header> tag because the claim
    has been made that a <header> should not contain navigational elements.
    
    The cyan background-color in the stories has been added in order to show
    that the buttons switched sides in the rtl-story for people who do not
    read Hebrew.
    
    Bug: T230328
    Change-Id: I6fb4a5a9bc6d0767d72ccb7698d9898a62c32eed

---
+++ client/data-bridge/stories/ProcessDialogHeader.js
@@ -0,0 +1,30 @@
+import { storiesOf } from '@storybook/vue';
+
+import ProcessDialogHeader from '@/presentation/components/ProcessDialogHeader';
+
+storiesOf( 'ProcessDialogHeader', module )
+	.add( 'without buttons', () => ( {
+		components: { ProcessDialogHeader },
+		template: '<ProcessDialogHeader />',
+	} ), { info: true } )
+	.add( 'with only mock primary button', () => ( {
+		components: { ProcessDialogHeader },
+		template: `<ProcessDialogHeader>
+<button style="background-color: cyan;">primary action</button>
+</ProcessDialogHeader>`,
+	} ), { info: true } )
+	.add( 'with mock buttons', () => ( {
+		components: { ProcessDialogHeader },
+		template: `<ProcessDialogHeader>
+<button style="background-color: cyan;">primary action</button>
+<template slot="safeAction"><button>safe action</button></template>
+</ProcessDialogHeader>`,
+	} ), { info: true } )
+	.add( 'rtl with mock buttons', () => ( {
+		components: { ProcessDialogHeader },
+		template: `<div dir="rtl"><ProcessDialogHeader title="גשר נתונים">
+<template><button style="background-color: cyan;">פעולה ראשונית</button></template>
+<template slot="safeAction"><button>פעולה בטוחה</button></template>
+</ProcessDialogHeader>
+</div>`,
+	} ), { info: true } );
