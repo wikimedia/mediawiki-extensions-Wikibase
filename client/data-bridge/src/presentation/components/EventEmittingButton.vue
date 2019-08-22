b62ebe6d06dab2c6d751cb0d9f0e8df26e8930a6
bitPogo <matthias.geisler@wikimedia.de>
2019-08-22 17:48:32 +0200

    bridge: copy EventEmittingButton
    Copy the primaryProgressive modification of termbox's EventEmittingButton
    as it stood at 537049d25bbe86165ae067ebfc2e3edfc22e912f.
    Tests were mended to only exercise the primaryProgressive
    modification - as it is the only valid one. No functional changes.
    
    Bug: T230326
    Change-Id: I191e05647d2ee032b7186ee1aed922b01b874f09

---
+++ client/data-bridge/src/presentation/components/EventEmittingButton.vue
@@ -0,0 +1,93 @@
+<template>
+	<a
+		class="wb-ui-event-emitting-button"
+		:class="`wb-ui-event-emitting-button--${type}`"
+		:href="href"
+		:title="message"
+		@click="click"
+	>
+		<span
+			class="wb-ui-event-emitting-button__text"
+		>{{ message }}</span>
+	</a>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import { Prop } from 'vue-property-decorator';
+
+const validTypes = [
+	'primaryProgressive',
+];
+
+@Component
+export default class EventEmittingButton extends Vue {
+	@Prop( {
+		required: true,
+		validator: ( type ) => validTypes.indexOf( type ) !== -1,
+	} )
+	public type!: string;
+
+	@Prop( { required: true, type: String } )
+	public message!: string;
+
+	@Prop( { required: false, default: '#', type: String } )
+	public href!: string;
+
+	@Prop( { required: false, default: true, type: Boolean } )
+	public preventDefault!: boolean;
+
+	public click( event: MouseEvent ) {
+		if ( this.preventDefault ) {
+			event.preventDefault();
+		}
+		this.$emit( 'click', event );
+	}
+}
+</script>
+<style lang="scss">
+$block: '.wb-ui-event-emitting-button';
+
+%textButton {
+	font-family: $font-family-sans;
+	display: inline-block;
+	cursor: pointer;
+	white-space: nowrap;
+	text-decoration: none;
+	font-weight: bold;
+}
+
+%framed {
+	border-width: 1px;
+	border-radius: 2px;
+	border-style: solid;
+	box-sizing: border-box;
+	padding: $padding-vertical-base $padding-horizontal-base;
+}
+
+#{$block} {
+	transition: background-color 100ms, color 100ms, border-color 100ms, box-shadow 100ms, filter 100ms;
+
+	&--primaryProgressive {
+		@extend %textButton;
+		@extend %framed;
+		background-color: $color-primary;
+		color: $color-base--inverted;
+		border-color: $color-primary;
+
+		&:hover {
+			background-color: $color-primary--hover;
+			border-color: $color-primary--hover;
+		}
+
+		&:active {
+			background-color: $color-primary--active;
+			border-color: $color-primary--active;
+		}
+
+		&:focus {
+			box-shadow: $box-shadow-primary--focus;
+		}
+	}
+}
+</style>
---
+++ client/data-bridge/tests/unit/presentation/components/EventEmittingButton.spec.ts
@@ -0,0 +1,62 @@
+import EventEmittingButton from '@/presentation/components/EventEmittingButton.vue';
+import { shallowMount } from '@vue/test-utils';
+
+function shallowMountWithProps( props = {} ): any {
+	return shallowMount( EventEmittingButton, {
+		propsData: {
+			type: 'primaryProgressive',
+			message: 'click me',
+			...props,
+		},
+	} );
+}
+describe( 'EventEmittingButton', () => {
+	it( 'emits an event on click', () => {
+		const wrapper = shallowMountWithProps();
+		wrapper.find( 'a' ).trigger( 'click' );
+		const clickEvent = wrapper.emitted( 'click' );
+		expect( clickEvent ).toBeTruthy();
+		const originalEvent: MouseEvent = clickEvent[ 0 ][ 0 ];
+		expect( originalEvent ).toBeInstanceOf( MouseEvent );
+		expect( originalEvent.defaultPrevented ).toBeTruthy();
+	} );
+
+	it( 'can be configured not to prevent default (href) event', () => {
+		const wrapper = shallowMountWithProps( {
+			preventDefault: false,
+		} );
+		wrapper.find( 'a' ).trigger( 'click' );
+		const clickEvent = wrapper.emitted( 'click' );
+		expect( clickEvent ).toBeTruthy();
+		const originalEvent: MouseEvent = clickEvent[ 0 ][ 0 ];
+		expect( originalEvent ).toBeInstanceOf( MouseEvent );
+		expect( originalEvent.defaultPrevented ).toBeFalsy();
+	} );
+
+	it( 'can have a link target', () => {
+		const href = '/edit/Q1';
+		const wrapper = shallowMountWithProps( { href } );
+		expect( wrapper.find( 'a' ).attributes( 'href' ) ).toBe( href );
+	} );
+
+	it( 'has a default href', () => {
+		const wrapper = shallowMountWithProps();
+		expect( wrapper.find( 'a' ).attributes( 'href' ) ).toBe( '#' );
+	} );
+
+	it( 'sets its class based on its `type` prop', () => {
+		const wrapper = shallowMountWithProps( { type: 'primaryProgressive' } );
+		expect( wrapper.classes() ).toContain( 'wb-ui-event-emitting-button--primaryProgressive' );
+	} );
+
+	it( 'throws for unknown type', () => {
+		expect( () => shallowMountWithProps( { type: 'potato' } ) ).toThrow();
+	} );
+
+	it( 'shows a message in its title attribute and text content', () => {
+		const message = 'click me';
+		const wrapper = shallowMountWithProps( { message } );
+		expect( wrapper.find( 'a' ).text() ).toBe( message );
+		expect( wrapper.find( 'a' ).attributes( 'title' ) ).toBe( message );
+	} );
+} );
