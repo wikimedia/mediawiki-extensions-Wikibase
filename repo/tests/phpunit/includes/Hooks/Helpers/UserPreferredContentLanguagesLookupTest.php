56d31251e3c41567687c92ed17349ab8ee66cfff
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2019-08-23 14:52:19 +0200

    Add service for looking up a user's preferred languages
    Introduces UserPreferredContentLanguagesLookup which returns a list of
    languages the user is likely to speak, also filtering out invalid
    Wikibase content languages, and falling back to the Wiki's default content
    languages in the event that there were no valid preferred languages.
    
    Bug: T227083
    Change-Id: I97ba6fc9fa0b5862de97efae64515be0cbb233ce

---
+++ repo/tests/phpunit/includes/Hooks/Helpers/UserPreferredContentLanguagesLookupTest.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Wikibase\Repo\Tests\Hooks\Helpers;
+
+use PHPUnit4And6Compat;
+use PHPUnit\Framework\TestCase;
+use User;
+use Wikibase\Lib\StaticContentLanguages;
+use Wikibase\Lib\UserLanguageLookup;
+use Wikibase\Repo\Hooks\Helpers\UserPreferredContentLanguagesLookup;
+
+/**
+ * @covers \Wikibase\Repo\Hooks\Helpers\UserPreferredContentLanguagesLookup
+ *
+ * @group Wikibase
+ *
+ * @license GPL-2.0-or-later
+ */
+class UserPreferredContentLanguagesLookupTest extends TestCase {
+	use PHPUnit4And6Compat;
+
+	private $userLanguageLookup;
+	private $contentLanguages;
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->userLanguageLookup = $this->createMock( UserLanguageLookup::class );
+		$this->userLanguageLookup->method( 'getAllUserLanguages' )->willReturn( [] );
+		$this->contentLanguages = new StaticContentLanguages( [ 'en', 'de', 'fr' ] );
+	}
+
+	public function testGivenValidUserInterfaceLanguage() {
+		$wikiDefaultLanguage = 'en';
+		$lookup = $this->newUserPreferredTermsLanguagesLookup( $wikiDefaultLanguage );
+
+		$this->assertSame( [ 'de' ], $lookup->getLanguages( 'de', $this->createMock( User::class ) ) );
+	}
+
+	public function testGivenAdditionalUserLanguages() {
+		$user = $this->createMock( User::class );
+		$this->userLanguageLookup = $this->createMock( UserLanguageLookup::class );
+		$this->userLanguageLookup->expects( $this->once() )
+			->method( 'getAllUserLanguages' )
+			->with( $user )
+			->willReturn( [ 'de', 'fr' ] );
+
+		$lookup = $this->newUserPreferredTermsLanguagesLookup();
+
+		$this->assertSame( [ 'en', 'de', 'fr' ], $lookup->getLanguages( 'en', $user ) );
+	}
+
+	public function testDoesNotReturnDuplicateLanguages() {
+		$user = $this->createMock( User::class );
+		$this->userLanguageLookup = $this->createMock( UserLanguageLookup::class );
+		$this->userLanguageLookup->expects( $this->once() )
+			->method( 'getAllUserLanguages' )
+			->with( $user )
+			->willReturn( [ 'en', 'fr' ] );
+
+		$lookup = $this->newUserPreferredTermsLanguagesLookup();
+
+		$this->assertEquals( [ 'en', 'fr' ], array_values( $lookup->getLanguages( 'en', $user ) ) );
+	}
+
+	public function testGivenNoValidUserLanguages_returnsWikiDefaultContentLanguage() {
+		$this->userLanguageLookup = $this->createMock( UserLanguageLookup::class );
+		$this->userLanguageLookup->expects( $this->once() )
+			->method( 'getAllUserLanguages' )
+			->willReturn( [ 'potato' ] );
+		$wikiDefaultLanguage = 'en';
+
+		$lookup = $this->newUserPreferredTermsLanguagesLookup( $wikiDefaultLanguage );
+
+		$this->assertSame(
+			[ $wikiDefaultLanguage ],
+			$lookup->getLanguages( 'kartoffel', $this->createMock( User::class ) )
+		);
+	}
+
+	public function testFiltersInvalidContentLanguages() {
+		$this->userLanguageLookup = $this->createMock( UserLanguageLookup::class );
+		$this->userLanguageLookup->expects( $this->once() )
+			->method( 'getAllUserLanguages' )
+			->willReturn( [ 'potato' ] );
+
+		$lookup = $this->newUserPreferredTermsLanguagesLookup( 'en' );
+
+		$this->assertSame(
+			[ 'en' ],
+			$lookup->getLanguages( 'en', $this->createMock( User::class ) )
+		);
+	}
+
+	/**
+	 * @return UserPreferredContentLanguagesLookup
+	 */
+	private function newUserPreferredTermsLanguagesLookup( $wikiDefaultLanguage = 'en' ) {
+		return new UserPreferredContentLanguagesLookup(
+			$this->contentLanguages, $this->userLanguageLookup, $wikiDefaultLanguage
+		);
+	}
+
+}
