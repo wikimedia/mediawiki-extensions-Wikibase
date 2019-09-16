c0352e9d0fe6f0f6429d70fc8189c9e4c4e8fca1
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2019-09-16 20:00:31 +0100

    Introduce Basic TaintedIcon
    Icon is currently missing localisation of the title text.
    
    Bug: T232815
    Change-Id: Ied0c68b181da4003914e1f9920f0cb126577552c

---
+++ view/lib/wikibase-tainted-ref/src/presentation/components/TaintedIcon.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="wb-tr-tainted-icon" title="This statement has some potential issues">
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'TaintedIcon'
+	};
+</script>
+
+<style lang="scss">
+	.wb-tr-tainted-icon {
+		min-width: 20px;
+		min-height: 30px;
+		background-color: transparent;
+		background-image: $svg-tainted-icon;
+		background-position: top left;
+		background-size: 20px 30px;
+		background-repeat: no-repeat;
+	}
+</style>
