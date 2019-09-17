d0ceb75665f63261430d43965c5499b7d76e7030
bitPogo <matthias.geisler@wikimedia.de>
2019-09-17 15:32:15 +0200

    bridge: service container test using data provider
    This patch simply wraps the repeating pattern of services.spec.ts into a
    loop.
    
    Change-Id: I5f277b80676232bb8e0702ef4e56aeb4a360ac93

--- client/data-bridge/tests/unit/services.spec.ts
+++ client/data-bridge/tests/unit/services.spec.ts
@@ -1,97 +1,40 @@
 import ServiceRepositories from '@/services/ServiceRepositories';
-import ReadingEntityRepository from '@/definitions/data-access/ReadingEntityRepository';
-import WritingEntityRepository from '@/definitions/data-access/WritingEntityRepository';
-import LanguageInfoRepository from '@/definitions/data-access/LanguageInfoRepository';
-import EntityLabelRepository from '@/definitions/data-access/EntityLabelRepository';
-import MessagesRepository from '@/definitions/data-access/MessagesRepository';
 
 function newServices(): ServiceRepositories {
 	return new ServiceRepositories();
 }
 
-function newMockReadingEntityRepository(): ReadingEntityRepository {
-	return {} as ReadingEntityRepository;
-}
-
-function newMockWritingEntityRepository(): WritingEntityRepository {
-	return {} as WritingEntityRepository;
-}
-
-function newMockLanguageInfoRepository(): LanguageInfoRepository {
-	return {} as LanguageInfoRepository;
-}
-
-function newMockEntityLabelRepository(): EntityLabelRepository {
-	return {} as EntityLabelRepository;
-}
-
-function newMockMessagesRepository(): MessagesRepository {
-	return {} as MessagesRepository;
-}
-
 describe( 'ServiceRepositories', () => {
-	describe( 'ReadingEntityRepository', () => {
+	describe.each( [ [
+		'ReadingEntityRepository',
+		'getReadingEntityRepository',
+		'setReadingEntityRepository',
+	], [
+		'WritingEntityRepository',
+		'getWritingEntityRepository',
+		'setWritingEntityRepository',
+	], [
+		'LanguageInfoRepository',
+		'getLanguageInfoRepository',
+		'setLanguageInfoRepository',
+	], [
+		'EntityLabelRepository',
+		'getEntityLabelRepository',
+		'setEntityLabelRepository',
+	], [
+		'MessagesRepository',
+		'getMessagesRepository',
+		'setMessagesRepository',
+	] ] )( '%s', ( _name: string, getter: string, setter: string ) => {
 		it( 'throws an error if it is not set', () => {
-			expect( () => newServices().getReadingEntityRepository() ).toThrow();
+			expect( () => ( newServices() as any )[ getter ]() ).toThrow();
 		} );
 
-		it( 'can set and get an ReadingEntityRepository', () => {
+		it( 'can set and get it', () => {
 			const services = newServices();
-			const mockReadingEntityRepository = newMockReadingEntityRepository();
-			services.setReadingEntityRepository( mockReadingEntityRepository );
-			expect( services.getReadingEntityRepository() ).toBe( mockReadingEntityRepository );
-		} );
-	} );
-
-	describe( 'WritingEntityRepository', () => {
-		it( 'throws an error if it is not set', () => {
-			expect( () => newServices().getWritingEntityRepository() ).toThrow();
-		} );
-
-		it( 'can set and get an WritingEntityRepository', () => {
-			const services = newServices();
-			const mockWritingEntityRepository = newMockWritingEntityRepository();
-
-			services.setWritingEntityRepository( mockWritingEntityRepository );
-			expect( services.getWritingEntityRepository() ).toBe( mockWritingEntityRepository );
-		} );
-	} );
-
-	describe( 'LanguageInfoRepository', () => {
-		it( 'throws an error if it is not set', () => {
-			expect( () => newServices().getLanguageInfoRepository() ).toThrow();
-		} );
-		it( 'can set and get an LanguageInfoRepository', () => {
-			const services = newServices();
-			const mockLanguageInfoRepository = newMockLanguageInfoRepository();
-			services.setLanguageInfoRepository( mockLanguageInfoRepository );
-			expect( services.getLanguageInfoRepository() ).toBe( mockLanguageInfoRepository );
-		} );
-	} );
-
-	describe( 'EntityLabelRepository', () => {
-		it( 'throws an error if it is not set', () => {
-			expect( () => newServices().getEntityLabelRepository() ).toThrow();
-		} );
-
-		it( 'can set and get an EntityLabelRepository', () => {
-			const services = newServices();
-			const mockEntityLabelRepository = newMockEntityLabelRepository();
-			services.setEntityLabelRepository( mockEntityLabelRepository );
-			expect( services.getEntityLabelRepository() ).toBe( mockEntityLabelRepository );
-		} );
-	} );
-
-	describe( 'MessagesRepository', () => {
-		it( 'throws an error if it is not set', () => {
-			expect( () => newServices().getMessagesRepository() ).toThrow();
-		} );
-
-		it( 'can set and get a MessagesRepository', () => {
-			const services = newServices();
-			const mockMessagesRepository = newMockMessagesRepository();
-			services.setMessagesRepository( mockMessagesRepository );
-			expect( services.getMessagesRepository() ).toBe( mockMessagesRepository );
+			const mockedRepository = {};
+			( services as any )[ setter ]( mockedRepository );
+			expect( ( services as any )[ getter ]() ).toBe( mockedRepository );
 		} );
 	} );
 } );
