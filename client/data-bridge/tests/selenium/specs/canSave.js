1fc0b6fb99e448787adcabebeee2c52013eaeade
Michael Große <michael.grosse@wikimedia.de>
2019-09-20 13:08:03 +0200

    bridge: Test tags via browser test
    Bug: T232588
    Change-Id: Idabcc96ad171b51909cb8083b8af06c862a80b21

--- client/data-bridge/tests/selenium/specs/canSave.js
+++ client/data-bridge/tests/selenium/specs/canSave.js
@@ -3,8 +3,54 @@ const assert = require( 'assert' ),
 	DataBridgePage = require( '../pageobjects/dataBridge.page' ),
 	WikibaseApi = require( 'wdio-wikibase/wikibase.api' );
 
+function createTag( tagname, reason ) {
+	let bot;
+	return Api.bot( browser.config.username, browser.config.password )
+		.then( ( mwBot ) => {
+			bot = mwBot;
+			return bot.getEditToken();
+		} ).then( ( botState ) => {
+			return bot.request( {
+				action: 'managetags',
+				operation: 'create',
+				tag: tagname,
+				reason,
+				token: botState.csrftoken,
+			} );
+		} );
+}
+
+function getLatestRevisionTags( title ) {
+	return Api.bot( browser.config.username, browser.config.password ).then( ( bot ) => {
+		return bot.request( {
+			titles: title,
+			action: 'query',
+			prop: 'revisions',
+			rvprop: 'tags',
+			formatversion: '2',
+		} ).then( ( response ) => {
+			return response.query.pages[ 0 ].revisions[ 0 ].tags;
+		} );
+	} );
+}
+
+function ensureTagExists( bridgeEditTag ) {
+	browser.call(
+		() => createTag( bridgeEditTag, 'created by selenium browser test' )
+			.catch( ( reason ) => {
+				if ( reason.code === 'tags-create-already-exists' ) {
+					return;
+				}
+				throw new Error( reason );
+			} )
+	);
+}
+
 describe( 'App', () => {
-	it( 'can save', () => {
+	it( 'can save with tag', () => {
+		const bridgeEditTag = 'Data Bridge';
+		ensureTagExists( bridgeEditTag );
+
 		const title = DataBridgePage.getDummyTitle();
 		const propertyId = browser.call( () => WikibaseApi.getProperty( 'string' ) );
 		const stringPropertyExampleValue = 'initialValue';
@@ -56,5 +102,11 @@ describe( 'App', () => {
 		const actualValueAtServer = entity.claims[ propertyId ][ 0 ].mainsnak.datavalue.value;
 
 		assert.strictEqual( actualValueAtServer, newValue );
+
+		const actualTags = browser.call(
+			() => getLatestRevisionTags( entity.title )
+				.catch( assert.fail )
+		);
+		assert.ok( actualTags.includes( bridgeEditTag ), `${JSON.stringify( actualTags )} doesn't include tag "${bridgeEditTag}"!` );
 	} );
 } );
