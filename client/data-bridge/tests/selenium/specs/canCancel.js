0195f2ce4919cee6830b43526731a4cf5a9f3def
bitPogo <matthias.geisler@wikimedia.de>
2019-09-26 18:50:01 +0200

    bridge: add selenium test for esc event
    This patch adds a testset for esc event behavior.
    
    Bug: T233793
    Change-Id: I36f2f356a74f096df9df5894d9d41d950d32a046

---
+++ client/data-bridge/tests/selenium/specs/canCancel.js
@@ -0,0 +1,41 @@
+const Api = require( 'wdio-mediawiki/Api' ),
+	DataBridgePage = require( '../pageobjects/dataBridge.page' ),
+	WikibaseApi = require( 'wdio-wikibase/wikibase.api' );
+
+describe( 'App', () => {
+	it( 'closes on Escape-key event', () => {
+		const title = DataBridgePage.getDummyTitle();
+		const propertyId = browser.call( () => WikibaseApi.getProperty( 'string' ) );
+		const stringPropertyExampleValue = 'initialValue';
+		const entityId = browser.call( () => WikibaseApi.createItem( 'data bridge browser test item', {
+			'claims': [ {
+				'mainsnak': {
+					'snaktype': 'value',
+					'property': propertyId,
+					'datavalue': { 'value': stringPropertyExampleValue, 'type': 'string' },
+				},
+				'type': 'statement',
+				'rank': 'normal',
+			} ],
+		} ) );
+		const editFlow = 'overwrite';
+		const content = `{|class="wikitable"
+|-
+| official website
+| {{#statements:${propertyId}|from=${entityId}}}&nbsp;<span data-bridge-edit-flow="${editFlow}">[https://example.org/wiki/Item:${entityId}?uselang=en#${propertyId} Edit this on Wikidata]</span>
+|}`;
+		browser.call( () => Api.edit( title, content, browser.config.username, browser.config.password ) );
+		DataBridgePage.open( title );
+		DataBridgePage.overloadedLink.click();
+		DataBridgePage.bridge.waitForDisplayed();
+
+		browser.waitUntil(
+			() => {
+				browser.keys( 'Escape' );
+				return !DataBridgePage.app.isExisting();
+			},
+			5000,
+			'App didn\'t close after push ESC key'
+		);
+	} );
+} );
