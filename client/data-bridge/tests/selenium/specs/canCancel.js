30ce6abc499a9cd122fb43271fd88feac5ececa7
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2019-10-02 13:57:54 +0200

    bridge: reuse duplicate code in can-cancel browser test
    This also has the advantage of creating only one test item and page,
    rather than two.
    
    Bug: T233796
    Change-Id: I027550414dfe6c0f3a277733f6bdcf14b42035d0

--- client/data-bridge/tests/selenium/specs/canCancel.js
+++ client/data-bridge/tests/selenium/specs/canCancel.js
@@ -3,8 +3,10 @@ const Api = require( 'wdio-mediawiki/Api' ),
 	WikibaseApi = require( 'wdio-wikibase/wikibase.api' );
 
 describe( 'App', () => {
-	it( 'closes on Escape-key event', () => {
-		const title = DataBridgePage.getDummyTitle();
+	let title;
+
+	before( 'set up test item and page', () => {
+		title = DataBridgePage.getDummyTitle();
 		const propertyId = browser.call( () => WikibaseApi.getProperty( 'string' ) );
 		const stringPropertyExampleValue = 'initialValue';
 		const entityId = browser.call( () => WikibaseApi.createItem( 'data bridge browser test item', {
@@ -25,10 +27,15 @@ describe( 'App', () => {
 | {{#statements:${propertyId}|from=${entityId}}}&nbsp;<span data-bridge-edit-flow="${editFlow}">[https://example.org/wiki/Item:${entityId}?uselang=en#${propertyId} Edit this on Wikidata]</span>
 |}`;
 		browser.call( () => Api.edit( title, content, browser.config.username, browser.config.password ) );
+	} );
+
+	beforeEach( 'open bridge', () => {
 		DataBridgePage.open( title );
 		DataBridgePage.overloadedLink.click();
 		DataBridgePage.bridge.waitForDisplayed();
+	} );
 
+	it( 'closes on Escape-key event', () => {
 		browser.waitUntil(
 			() => {
 				browser.keys( 'Escape' );
@@ -40,32 +47,8 @@ describe( 'App', () => {
 	} );
 
 	it( 'closes on clicking the cancel button', () => {
-		const title = DataBridgePage.getDummyTitle();
-		const propertyId = browser.call( () => WikibaseApi.getProperty( 'string' ) );
-		const stringPropertyExampleValue = 'initialValue';
-		const entityId = browser.call( () => WikibaseApi.createItem( 'data bridge browser test item', {
-			'claims': [ {
-				'mainsnak': {
-					'snaktype': 'value',
-					'property': propertyId,
-					'datavalue': { 'value': stringPropertyExampleValue, 'type': 'string' },
-				},
-				'type': 'statement',
-				'rank': 'normal',
-			} ],
-		} ) );
-		const editFlow = 'overwrite';
-		const content = `{|class="wikitable"
-|-
-| official website
-| {{#statements:${propertyId}|from=${entityId}}}&nbsp;<span data-bridge-edit-flow="${editFlow}">[https://example.org/wiki/Item:${entityId}?uselang=en#${propertyId} Edit this on Wikidata]</span>
-|}`;
-		browser.call( () => Api.edit( title, content, browser.config.username, browser.config.password ) );
-		DataBridgePage.open( title );
-		DataBridgePage.overloadedLink.click();
-		DataBridgePage.bridge.waitForDisplayed();
-
 		DataBridgePage.cancelButton.click();
+
 		DataBridgePage.app.waitForExist( 5000, true, 'App still exists in the DOM after clicking the cancel button' );
 	} );
 } );
