9490a0d54bf95db89806449d4d74775acb2d5819
Marius Hoch <hoo@online.de>
2019-10-11 15:28:39 +0200

    Launch tainted-ref on entityLoaded mw.hook
    Bug: T233083
    Change-Id: Ib3bb93b9071839460f724014b954b9df3392a53e

--- view/lib/wikibase-tainted-ref/src/init.ts
+++ view/lib/wikibase-tainted-ref/src/init.ts
@@ -6,6 +6,6 @@ export default async (): Promise<void> => {
 	if ( mwWindow.mw.config.get( 'wbTaintedReferencesEnabled' ) ) {
 		const require = await mwWindow.mw.loader.using( RL_COMMON_MODULE_NAME );
 		const app = require( RL_COMMON_MODULE_NAME );
-		app.launch();
+		mwWindow.mw.hook( 'wikibase.entityPage.entityView.rendered' ).add( app.launch );
 	}
 };
