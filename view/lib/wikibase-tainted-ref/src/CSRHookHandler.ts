7c97b1e904c24a9a051ce2f5009b950f8f3be2d1
Marius Hoch <hoo@online.de>
2019-10-17 19:04:53 +0200

    Add (un)taint buttons to CSR, hook up like MW hooks
    Change-Id: Ia46198fc3088158ba50d4258ec8a31f95896bbe9

--- view/lib/wikibase-tainted-ref/src/CSRHookHandler.ts
+++ view/lib/wikibase-tainted-ref/src/CSRHookHandler.ts
@@ -1,12 +1,27 @@
 import { HookHandler } from '@/HookHandler';
 import { Store } from 'vuex';
 import Application from '@/store/Application';
-import { STATEMENT_TAINTED_STATE_UNTAINT } from '@/store/actionTypes';
+import { STATEMENT_TAINTED_STATE_UNTAINT, STATEMENT_TAINTED_STATE_TAINT } from '@/store/actionTypes';
 
 export default class CSRHookHandler implements HookHandler {
 	public addStore( store: Store<Application> ): void {
-		setTimeout( () => {
-			store.dispatch( STATEMENT_TAINTED_STATE_UNTAINT, 'Q73$c14f0d0c-4b50-784a-48ef-3c8d55d2a03b' );
-		}, 2000 );
+		document.querySelectorAll( '.wikibase-statementview' ).forEach( ( element ) => {
+			const guid = element.getAttribute( 'id' );
+
+			const untaintLink: HTMLElement | null = element.querySelector( '.wikibase-toolbar-button-untaint' );
+			if ( untaintLink ) {
+				untaintLink.onclick = () => {
+					store.dispatch( STATEMENT_TAINTED_STATE_UNTAINT, guid );
+					return false;
+				};
+			}
+			const taintLink: HTMLElement | null = element.querySelector( '.wikibase-toolbar-button-taint' );
+			if ( taintLink ) {
+				taintLink.onclick = () => {
+					store.dispatch( STATEMENT_TAINTED_STATE_TAINT, guid );
+					return false;
+				};
+			}
+		} );
 	}
 }
