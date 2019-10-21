6982395e9fc699e07d1d2fc63a7e38f98101395b
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2019-10-21 15:11:56 +0100

    Add saved hook to HookHandler
    This patch wires up the MW save hook to the taint action via
    the HookHandler.
    
    Bug: T232814
    Change-Id: Ideac466b1f45659b67bc977b6bf482d3529416cd

--- view/lib/wikibase-tainted-ref/tests/unit/MWHookHandler.spec.ts
+++ view/lib/wikibase-tainted-ref/tests/unit/MWHookHandler.spec.ts
@@ -1,18 +1,44 @@
-import { STATEMENT_TAINTED_STATE_UNTAINT } from '@/store/actionTypes';
+import { STATEMENT_TAINTED_STATE_TAINT, STATEMENT_TAINTED_STATE_UNTAINT } from '@/store/actionTypes';
 import MWHookHandler from '@/MWHookHandler';
 import Vuex from 'vuex';
 import Vue from 'vue';
+import { Hook, HookRegistry } from '@/@types/mediawiki/MwWindow';
 
 Vue.use( Vuex );
 describe( 'MWHookHandler', () => {
-	it( `should dispatch ${STATEMENT_TAINTED_STATE_UNTAINT} with statement guid on hook firing`, () => {
-		const dummyHook = ( randomFunction: Function ): void => {
+	it( `should dispatch ${STATEMENT_TAINTED_STATE_UNTAINT} with statement guid on edit hook firing`, () => {
+		const dummyEditHook = ( randomFunction: Function ): Hook => {
 			randomFunction( 'gooGuid' );
+			return { add: jest.fn() };
 		};
-		const hookHandler = new MWHookHandler( dummyHook );
+
+		const mwHookRegistry: HookRegistry = jest.fn( ( _hookName: string ) => {
+			return { add: dummyEditHook };
+		} );
+
+		const hookHandler = new MWHookHandler( mwHookRegistry );
 		const store = new Vuex.Store( { state: { statementsTaintedState: {} } } );
 		store.dispatch = jest.fn();
 		hookHandler.addStore( store );
+		expect( mwHookRegistry ).toHaveBeenCalledWith( 'wikibase.statement.startEditing' );
 		expect( store.dispatch ).toHaveBeenCalledWith( STATEMENT_TAINTED_STATE_UNTAINT, 'gooGuid' );
+	} );
+
+	it( 'should dispatch ${STATEMENT_TAINTED_STATE_TAINT} with statement guid on save hook firing', () => {
+		const dummySaveHook = ( randomFunction: Function ): Hook => {
+			randomFunction( 'Q1', 'gooGuid' );
+			return { add: jest.fn() };
+		};
+
+		const mwHookRegistry = jest.fn( ( _hookName: string ) => {
+			return { add: dummySaveHook };
+		} );
+
+		const hookHandler = new MWHookHandler( mwHookRegistry );
+		const store = new Vuex.Store( { state: { statementsTaintedState: {} } } );
+		store.dispatch = jest.fn();
+		hookHandler.addStore( store );
+		expect( mwHookRegistry ).toHaveBeenCalledWith( 'wikibase.statement.saved' );
+		expect( store.dispatch ).toHaveBeenCalledWith( STATEMENT_TAINTED_STATE_TAINT, 'gooGuid' );
 	} );
 } );
