b24626b2c536889960610cf93a67a7ef63222202
Michael Große <michael.grosse@wikimedia.de>
2019-11-18 16:58:34 +0100

    bridge: Interface to determine if the user is allowed to edit item/page
    The title of the current page on the client and the title of the item on
    the repo is intended to be added in the constructor of the
    implementation.
    
    Bug: T237523
    Change-Id: I9e49a10cc30485845f41585b5e1c9d82cf92fa28

---
+++ client/data-bridge/src/definitions/ApplicationError.ts
@@ -0,0 +1,4 @@
+export interface ApplicationErrorBase {
+	type: string;
+	info?: object;
+}
---
+++ client/data-bridge/src/definitions/data-access/PageEditPermissionsRepository.ts
@@ -0,0 +1,46 @@
+import { ApplicationErrorBase } from '@/definitions/ApplicationError';
+
+export enum PageNotEditable {
+	BLOCKED_ON_PAGE = 'blocked_on_client_page',
+	BLOCKED_ON_ITEM = 'blocked_on_repo_item',
+	ITEM_FULLY_PROTECTED = 'protectedpage',
+	ITEM_SEMI_PROTECTED = 'semiprotectedpage',
+	ITEM_CASCADE_PROTECTED = 'cascadeprotected',
+	UNKNOWN = 'unknown',
+}
+
+interface BlockInfo {
+	blockedBy: string;
+	blockedById: number;
+	blockReason: string;
+	blockedTimestamp: string;
+	blockExpiry: string;
+	blockPartial: boolean;
+	currentIP: string;
+}
+
+export interface BlockReason extends ApplicationErrorBase {
+	type: typeof PageNotEditable.BLOCKED_ON_ITEM | typeof PageNotEditable.BLOCKED_ON_PAGE;
+	info: BlockInfo;
+}
+
+export interface ProtectedReason extends ApplicationErrorBase {
+	type: typeof PageNotEditable.ITEM_FULLY_PROTECTED
+	| typeof PageNotEditable.ITEM_SEMI_PROTECTED
+	| typeof PageNotEditable.ITEM_CASCADE_PROTECTED;
+}
+
+export interface UnknownReason extends ApplicationErrorBase {
+	type: typeof PageNotEditable.UNKNOWN;
+	info: {
+		code: string;
+		messageKey: string;
+		messageParams: string[];
+	};
+}
+
+export type MissingPermissionsError = BlockReason | ProtectedReason | UnknownReason;
+
+export interface PageEditPermissionsRepository {
+	isUserAllowedToEditPage(): MissingPermissionsError[];
+}
