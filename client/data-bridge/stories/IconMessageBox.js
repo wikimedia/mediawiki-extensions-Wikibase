4980b2afbf3c11bcb864b888a7c3af67c8028b39
bitPogo <matthias.geisler@wikimedia.de>
2019-11-22 13:30:22 +0100

    bridge: Add notice component
    This patch extends the already existing IconMessage from Termbox and
    modify it to satisfy the current constrains for the notice version as
    well the inline state.
    It also makes some correction regarding ooui.
    
    Bug: T237527
    Change-Id: Ibc68cffb54a6dc9170138ea4e2ccce4164ae7b8c

---
+++ client/data-bridge/stories/IconMessageBox.js
@@ -0,0 +1,30 @@
+import { storiesOf } from '@storybook/vue';
+import IconMessageBox from '@/presentation/components/IconMessageBox.vue';
+storiesOf( 'IconMessageBox', module )
+	.add( 'notice', () => ( {
+		components: { IconMessageBox },
+		template:
+			`<div>
+				<IconMessageBox type="notice">
+					Just to inform you...
+				</IconMessageBox>
+			</div>`,
+	} ), { info: true } )
+	.add( 'notice long', () => ( {
+		components: { IconMessageBox },
+		template:
+			`<div>
+				<IconMessageBox type="notice">
+					Just to inform you... Just to inform you... Just to inform you...Just to inform you... Just to inform you...Just to inform you... Just to inform you... Just to inform you...Just to inform you...Just to inform you...Just to inform you... Just to inform you... Just to inform you...Just to inform you...Just to inform you...Just to inform you... Just to inform you... Just to inform you...Just to inform you... Just to inform you...Just to inform you...Just to inform you... Just to inform you...Just to inform you... Just to inform you...Just to inform you... Just to inform you... Just to inform you...Just to inform you... Just to inform you...Just to inform you... Just to inform you... Just to inform you...Just to inform you... Just to inform you...Just to inform you... Just to inform you...  Just to inform you...Just to inform you... Just to inform you...Just to inform you...Just to inform you... 
+				</IconMessageBox>
+			</div>`,
+	} ), { info: true } )
+	.add( 'notice inline', () => ( {
+		components: { IconMessageBox },
+		template:
+			`<div>
+				<IconMessageBox type="notice" inline="true">
+					Just to inform you...
+				</IconMessageBox>
+			</div>`,
+	} ), { info: true } );
