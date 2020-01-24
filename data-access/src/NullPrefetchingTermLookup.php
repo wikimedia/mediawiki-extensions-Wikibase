1a5a7f81128ad19825430fbcdd80c4aca2b09fc2
addshore <addshorewiki@gmail.com>
2020-01-24 15:22:31 +0100

    Remove TermBuffer compat for data-model-services change
    Bug: T243596
    Depends-On: Ie52d6874543aa4c19dd356d0a1f61684a9abfe13
    Change-Id: I5ca0c75793b2f14c35b8f0a3ec2fcf00581747df

--- data-access/src/NullPrefetchingTermLookup.php
+++ data-access/src/NullPrefetchingTermLookup.php
@@ -17,17 +17,9 @@ use Wikibase\DataModel\Entity\EntityId;
 class NullPrefetchingTermLookup implements PrefetchingTermLookup {
 
 	/**
-	 * @todo $termTypes and $languageCodes can not be null with data-model-service ~5.0
-	 * Code calling this already always passes array here and the defaults should be removed soon
-	 * Leaving the defaults in this method allows us to stay compatible with ~4.0 and ~5.0
-	 * for a short period during migration and updates.
-	 *
 	 * @inheritDoc
 	 */
-	public function prefetchTerms( array $entityIds, array $termTypes = null, array $languageCodes = null ) {
-		if ( $termTypes === null || $languageCodes === null ) {
-			throw new \InvalidArgumentException( '$termTypes and $languageCodes can not be null' );
-		}
+	public function prefetchTerms( array $entityIds, array $termTypes, array $languageCodes ) {
 	}
 
 	/**
