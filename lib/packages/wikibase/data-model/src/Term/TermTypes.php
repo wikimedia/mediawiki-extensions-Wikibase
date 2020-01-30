9d16c2cd085e424ab638bc0cb7465c591183ce89
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2020-01-30 12:48:23 +0100

    Add TermTypes class with term type constants
    Useful when passing term types to services like TermBuffer.

---
+++ lib/packages/wikibase/data-model/src/Term/TermTypes.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Wikibase\DataModel\Term;
+
+/**
+ * Term types used on Items and Properties
+ *
+ * @since 9.3
+ *
+ * @license GPL-2.0-or-later
+ */
+final class TermTypes {
+
+	public const TYPE_LABEL = 'label';
+	public const TYPE_DESCRIPTION = 'description';
+	public const TYPE_ALIAS = 'alias';
+
+}
