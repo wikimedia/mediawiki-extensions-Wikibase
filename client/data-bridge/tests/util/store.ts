73d55d883ac3705f5cac06ec2370a3c454e135fc
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2020-02-03 11:19:53 +0100

    bridge: Add getters to test store
    Some components use getters instead of directly accessing the store
    state. Therefore, to unit test them, the test store needs to allow for
    mocked getters.
    
    Change-Id: Iea2c4bc89ec653641c9dd68b34fead4e6c7fb18f

--- client/data-bridge/tests/util/store.ts
+++ client/data-bridge/tests/util/store.ts
@@ -1,21 +1,25 @@
 import { rootModule } from '@/store';
 import { RootActions } from '@/store/actions';
+import { RootGetters } from '@/store/getters';
 import Application from '@/store/Application';
 import { BaseState } from '@/store/state';
 import { Store } from 'vuex';
 import {
 	Actions,
+	Getters,
 	createStore as smartCreateStore,
 } from 'vuex-smart-module';
 
 afterEach( () => {
 	rootModule.options.state = BaseState;
 	rootModule.options.actions = RootActions;
+	rootModule.options.getters = RootGetters;
 } );
 
-export function createTestStore( { state, actions }: {
+export function createTestStore( { state, actions, getters }: {
 	state?: Partial<Application>;
 	actions?: Partial<RootActions>;
+	getters?: Partial<RootGetters>;
 } = {} ): Store<any> {
 	if ( state !== undefined ) {
 		rootModule.options.state = class implements Partial<Application> {
@@ -27,6 +31,10 @@ export function createTestStore( { state, actions }: {
 	if ( actions !== undefined ) {
 		rootModule.options.actions = class extends Actions {} as new() => RootActions;
 		Object.assign( rootModule.options.actions.prototype, actions );
+	}
+	if ( getters !== undefined ) {
+		rootModule.options.getters = class extends Getters {} as new() => RootGetters;
+		Object.assign( rootModule.options.getters.prototype, getters );
 	}
 	return smartCreateStore( rootModule );
 }
