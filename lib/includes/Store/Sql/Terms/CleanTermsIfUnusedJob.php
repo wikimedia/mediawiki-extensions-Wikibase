61a5578206d30200e763ea5350ba4c3d99f96d13
addshore <addshorewiki@gmail.com>
2020-03-04 16:29:07 +0000

    Schedule 1 CleanTermsIfUnusedJob per ID to clean
    Bug: T244115
    Bug: T246898
    Change-Id: I596c3efaa3ee31c50323d782829f09773cda4c0e

--- lib/includes/Store/Sql/Terms/CleanTermsIfUnusedJob.php
+++ lib/includes/Store/Sql/Terms/CleanTermsIfUnusedJob.php
@@ -11,7 +11,15 @@ use Title;
  * A job to cleanup the wbt_* terms table rows when they may not be needed any more.
  * This job currently executes in a single transaction.
  *
- * @todo Execute the cleanup of each table in its own transaction
+ * Callers can choose to supply a single termInLang id to clean per job or multiple.
+ * Supplying multiple ids will naturally mean the Job and the transaction take longer
+ * and therefor locks will also be held for a longer period of time possibly causing lock contention.
+ * This started to show on wikidata.org in https://phabricator.wikimedia.org/T246898
+ * and thus the default was switched for create a single job per termInLang id.
+ * For smaller Wikibase installs with a lower edit rate it might make sense to instead optimize
+ * for fewer jobs, where lock contention is less of an issue.
+ *
+ * @todo Execute the cleanup of each table in its own transaction to further reduce lock contention
  *
  * @see @ref md_docs_storage_terms
  * @author Addshore
