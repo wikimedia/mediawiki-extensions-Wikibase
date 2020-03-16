ba9a91597d996e15c900a828db75a3ce1327291e
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2020-03-16 18:35:39 +0100

    Allow overriding property services for Federated Properties
    This uses the WikibaseRepoEntityTypes hook (also used by WikibaseLexeme
    and WikibaseCirrusSearch) to override property services as needed. The
    service overrides are defined in a separated file in
    `repo/WikibaseRepo.FederatedProperties.entitytypes.php`.
    
    Bug: T247071
    Change-Id: Ic758a61ce1394e73eca2bc65c9574da43809dce1

---
+++ repo/WikibaseRepo.FederatedProperties.entitytypes.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Definition of entity service overrides for Federated Properties.
+ * The array returned by the code below is supposed to be merged with the content of
+ * repo/WikibaseRepo.entitytypes.php.
+ *
+ * @note: This is bootstrap code, it is executed for EVERY request. Avoid instantiating
+ * objects or loading classes here!
+ *
+ * @see docs/entiytypes.wiki
+ *
+ * @license GPL-2.0-or-later
+ */
+return [
+	'property' => []
+];
