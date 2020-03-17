f6462d39590b8f226fec4bab65a3e161dde64456
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2020-03-17 15:53:25 +0100

    Add ReferenceFormatterFactory
    This factory can create a DataBridgeReferenceFormatter, and possibly
    other styles of reference formatters in the future.
    
    Because the specifics of creating SnakFormatters differ between repo and
    client, this class is client-specific. The alternative would have been
    to somehow inject finished SnakFormatters into the factory, but then
    using the factory would be more complicated and the factory itself would
    do even less, so I decided against it.
    
    The test isn’t great – it essentially tests that the factory runs
    exactly the code that it runs – but at least better than nothing.
    
    Bug: T247412
    Change-Id: Ia4bd257cd76d4f3f19f1cc6d0ca414419f9e8793

---
+++ client/includes/DataAccess/ReferenceFormatterFactory.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Wikibase\Client\DataAccess;
+
+use Language;
+use MessageLocalizer;
+use Wikibase\Client\Usage\UsageAccumulator;
+use Wikibase\Lib\Formatters\Reference\DataBridgeReferenceFormatter;
+use Wikibase\Lib\Formatters\Reference\ReferenceFormatter;
+use Wikibase\Lib\Formatters\Reference\WellKnownReferenceProperties;
+
+/**
+ * A factory for {@link ReferenceFormatter}s.
+ *
+ * @license GPL-2.0-or-later
+ */
+class ReferenceFormatterFactory {
+
+	/** @var DataAccessSnakFormatterFactory */
+	private $snakFormatterFactory;
+
+	/** @var WellKnownReferenceProperties */
+	private $properties;
+
+	public function __construct(
+		DataAccessSnakFormatterFactory $snakFormatterFactory,
+		WellKnownReferenceProperties $properties
+	) {
+		$this->snakFormatterFactory = $snakFormatterFactory;
+		$this->properties = $properties;
+	}
+
+	public function newDataBridgeReferenceFormatter(
+		MessageLocalizer $messageLocalizer,
+		Language $language,
+		UsageAccumulator $usageAccumulator
+	): ReferenceFormatter {
+		return new DataBridgeReferenceFormatter(
+			$this->snakFormatterFactory->newWikitextSnakFormatter(
+				$language,
+				$usageAccumulator,
+				'rich-wikitext'
+			),
+			$this->properties,
+			$messageLocalizer
+		);
+	}
+
+}
