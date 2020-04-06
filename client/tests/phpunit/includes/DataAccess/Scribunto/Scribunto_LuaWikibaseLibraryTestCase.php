f4901fdc93efd2fba0f0adb0b1f6c4c291cafc18
Michael Große <mic.grosse@googlemail.com>
2020-04-06 16:20:31 +0200

    Remove Scribunto testcase stub
    The test for WikibaseLexeme, ArticlePlaceholder and WikibaseMediaInfo
    have been adjusted to use the new location of the Scribunto testcase and
    the MockClientStore class.
    
    Depends-On: Ia3e08c1548aeb53cd117b7519738a1a92089493a
    Depends-On: I796de6ec2df853721c3f1228bba83192fdf7fe24
    Depends-On: Ie6761331caeff52a9790a5d8b4bcde25582613ee
    Change-Id: I00996f8cb3bffb498923712c7687ddb9eda9160b

--- client/tests/phpunit/includes/DataAccess/Scribunto/Scribunto_LuaWikibaseLibraryTestCase.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class_alias(
-	'Wikibase\Client\Tests\Integration\DataAccess\Scribunto\Scribunto_LuaWikibaseLibraryTestCase',
-	'Wikibase\Client\Tests\DataAccess\Scribunto\Scribunto_LuaWikibaseLibraryTestCase'
-);
--- client/tests/phpunit/includes/MockClientStore.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class_alias(
-	'Wikibase\Client\Tests\Mocks\MockClientStore',
-	'Wikibase\Client\Tests\MockClientStore'
-);
