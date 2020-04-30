73472c48befafddb12c15a51009a91003f1a97d3
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2020-04-30 17:25:37 +0200

    Make PHPCS/PHPCBF scripts more resilient
    Use --file-list to avoid parsing file names in the shell; this should
    work as long as no file name contains a line break. Also, spell out the
    -om options to git ls-files for readability, and only make git list PHP
    files in the first place, rather than having PHPCS/PHPCBF skip all other
    arguments. (Wikibase doesn’t have any *.inc files.)
    
    Change-Id: Ic21999656b6605ae01ff98ab6404ca50b1336dc5

--- build/phpcsGitModified.sh
+++ build/phpcsGitModified.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-# Runs phpcs across all files that have been modified according to git.
+# Runs phpcs across all PHP files that have been modified according to git.
 
-files=$(git ls-files -om --exclude-standard)
-phpcs -p -s $files
+phpcs -p -s --file-list=<(
+    git ls-files \
+        --modified \
+        --others \
+        --exclude-standard \
+        '*.php'
+    echo /dev/null # prevent empty file list
+)
