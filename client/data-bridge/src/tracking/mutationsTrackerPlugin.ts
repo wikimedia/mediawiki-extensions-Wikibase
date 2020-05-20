948fb9401164d7e66d0c89f5062658b2c379605e
Pablo Grass <pablo.grass@wikimedia.de>
2020-05-20 17:15:22 +0200

    bridge: mutationsTrackerPlugin: type guard as fixed function
    Make the type guard which determines addApplicationErrors mutations a
    normal function - no need for it to be variable.
    
    Also makes the type guard a guard for better readability.
    
    Change-Id: Ie8280c01dd87eae1ced0659ccddb72ad6a2452c5

--- client/data-bridge/src/tracking/mutationsTrackerPlugin.ts
+++ client/data-bridge/src/tracking/mutationsTrackerPlugin.ts
@@ -3,16 +3,18 @@ import BridgeTracker from '@/definitions/data-access/BridgeTracker';
 import { Store } from 'vuex';
 import Application from '@/store/Application';
 
-export default function mutationsTrackerPlugin( tracker: BridgeTracker ): ( store: Store<Application> ) => void {
-	const isAddApplicationErrorsMutation = ( type: string, _payload: unknown ): _payload is ApplicationError[] => {
-		return type === 'addApplicationErrors';
-	};
+function isAddApplicationErrorsMutation( type: string, _payload: unknown ): _payload is ApplicationError[] {
+	return type === 'addApplicationErrors';
+}
 
+export default function mutationsTrackerPlugin( tracker: BridgeTracker ): ( store: Store<Application> ) => void {
 	return ( store ): void => {
 		store.subscribe( ( { type, payload }: { type: string; payload: unknown } ): void => {
-			if ( isAddApplicationErrorsMutation( type, payload ) ) {
-				payload.forEach( ( error ) => tracker.trackError( error.type ) );
+			if ( !isAddApplicationErrorsMutation( type, payload ) ) {
+				return;
 			}
+
+			payload.forEach( ( error ) => tracker.trackError( error.type ) );
 		} );
 	};
 }
