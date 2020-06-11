1a4261ba07206c5c0bff59e23171923e2c6b48c2
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2020-06-11 10:55:39 +0200

    Fix entity id lookup for interwiki special page links
    This allows interwiki entity links to special pages like the ones from
    Wikimedia Commons to Wikidata in the shape of d:Special:EntityPage/Q42.
    It also ensures that the interwiki prefix of the link matches the source
    of the parsed entity id.
    
    Bug: T255078
    Change-Id: Ia3f907a50a9cbd9ad7e96fcc4fe22fb8eb2628be

--- repo/includes/WikibaseRepo.php
+++ repo/includes/WikibaseRepo.php
@@ -2529,7 +2529,12 @@ class WikibaseRepo {
 	}
 
 	public function getLinkTargetEntityIdLookup(): LinkTargetEntityIdLookup {
-		return new EntityLinkTargetEntityIdLookup( $this->getEntityNamespaceLookup(), $this->getEntityIdParser() );
+		return new EntityLinkTargetEntityIdLookup(
+			$this->getEntityNamespaceLookup(),
+			$this->getEntityIdParser(),
+			$this->entitySourceDefinitions,
+			$this->getLocalEntitySource()
+		);
 	}
 
 }
