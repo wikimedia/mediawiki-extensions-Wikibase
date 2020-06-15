c55e86b016505c07ee3c0a141956d50d23d0e5ad
Tobias Andersson <tobias.andersson@wikimedia.de>
2020-06-15 15:49:47 +0200

    FP: Handle api error on entity page
    Handle the api error on timeouts. Should return generic looking
    error page.
    
    Bug: T255010
    Change-Id: I9f995004a97711c085a9d93e81dbc6759cb8f967

---
+++ repo/includes/FederatedProperties/FederatedPropertiesEntityParserOutputGenerator.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Wikibase\Repo\FederatedProperties;
+
+use Language;
+use ParserOutput;
+use Wikibase\DataModel\Term\LabelsProvider;
+use Wikibase\Lib\Store\EntityRevision;
+use Wikibase\Repo\ParserOutput\EntityParserOutputGenerator;
+
+/**
+ * @license GPL-2.0-or-later
+ */
+class FederatedPropertiesEntityParserOutputGenerator implements EntityParserOutputGenerator {
+
+	private $inner;
+	private $languageCode;
+
+	/**
+	 * @param EntityParserOutputGenerator $inner
+	 * @param Language $language
+	 */
+	public function __construct(
+		EntityParserOutputGenerator $inner,
+		Language $language
+
+	) {
+		$this->inner = $inner;
+		$this->languageCode = $language->getCode();
+	}
+
+	/**
+	 * Creates the parser output for the given entity.
+	 *
+	 * @param EntityRevision $entityRevision
+	 * @param bool $generateHtml
+	 *
+	 * @return ParserOutput
+	 * @throws FederatedPropertiesError|FederatedPropertiesException
+	 */
+	public function getParserOutput(
+		EntityRevision $entityRevision,
+		$generateHtml = true
+	) {
+
+		// add wikibase styles in all cases, so we can format the link properly:
+		try {
+			$po = $this->inner->getParserOutput( $entityRevision, $generateHtml );
+		} catch ( FederatedPropertiesException $ex ) {
+			$entity = $entityRevision->getEntity();
+
+			if ( $entity instanceof LabelsProvider ) {
+				$ex = new FederatedPropertiesError(
+					$this->languageCode,
+					$entity,
+					'wikibase-federated-properties-source-wiki-api-error-message'
+				);
+			}
+			throw $ex;
+		}
+
+		return $po;
+	}
+
+}
---
+++ repo/includes/FederatedProperties/FederatedPropertiesError.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Wikibase\Repo\FederatedProperties;
+
+use ErrorPageError;
+use RawMessage;
+use Wikibase\DataModel\Term\LabelsProvider;
+use Wikibase\View\Template\TemplateFactory;
+
+/**
+ * An error page used for showing API errors for a specific entity using Federated Properties.
+ *
+ * @ingroup Wikibase
+ *
+ * @license GPL-2.0-or-later
+ */
+class FederatedPropertiesError extends ErrorPageError {
+
+	/**
+	 * @param LabelsProvider $entity
+	 * @param string $msg Message key (string) for error text
+	 * @param array $params Array with parameters to wfMessage()
+	 *
+	 * @suppress SecurityCheck-DoubleEscaped
+	 */
+	public function __construct( $languageCode, $entity, $msg, $params = [] ) {
+
+		$templateFactory = TemplateFactory::getDefaultInstance();
+		$errorBody = new RawMessage( '<div class="errorbox">$1</div>', [ wfMessage( $msg, $params )->parse() ] );
+
+		// @phan-suppress-next-line PhanUndeclaredMethod Phan is confused by intersection types
+		$entityId = $entity->getId()->getSerialization();
+		$hasLabel = $entity->getLabels()->hasTermForLanguage( $languageCode );
+		$labelText = '';
+
+		if ( $hasLabel ) {
+			$labelText = $entity->getLabels()->getByLanguage( $languageCode )->getText();
+		}
+
+		$idInParenthesesHtml = htmlspecialchars( wfMessage( 'parentheses', [ $entityId ] )->parse() );
+
+		$html = $templateFactory->render( 'wikibase-title',
+			!$hasLabel ? 'wb-empty' : '',
+			!$hasLabel ? htmlspecialchars( wfMessage( 'wikibase-label-empty' ) ) : $labelText,
+			$idInParenthesesHtml
+		);
+
+		parent::__construct( new RawMessage( $html ), $errorBody, [] );
+	}
+
+	public function report( $action = self::SEND_OUTPUT ) {
+		if ( self::isCommandLine() || defined( 'MW_API' ) ) {
+			parent::report();
+		} else {
+			global $wgOut;
+			$wgOut->addModuleStyles( [ 'wikibase.common' ] );
+			parent::report( $action );
+		}
+	}
+}
