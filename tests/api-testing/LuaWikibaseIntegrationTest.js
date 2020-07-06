36463300a8256e399de9923670bfd0de1414425d
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2020-07-06 14:33:11 +0200

    Rewrite LuaWikibaseIntegrationTest as API integration test
    We want to avoid the direct coupling to the WikibaseRepo class from this
    Client test, and chose to do that by rewriting it as an API integration
    test [1]. Most of the test characteristics can be retained (using API
    calls to create items, parse wikitext, and get entity usage), but since
    we have less control over the target wiki, we can’t port the getSitelink
    test (we don’t know any valid sitelink code – not even the repo wiki’s
    own code is guaranteed to be registered), and some tests have to be more
    lenient as to which result language they expect.
    
    Since the API integration tests aren’t voting in CI yet, this change
    doesn’t remove the PHP version of the test, but that will follow soon.
    
    [1]: https://www.mediawiki.org/wiki/MediaWiki_API_integration_tests
    
    Bug: T255885
    Change-Id: I62c520f8269f304b492390760d139c2ceea11752

---
+++ tests/api-testing/LuaWikibaseIntegrationTest.js
@@ -0,0 +1,150 @@
+'use strict';
+
+const { assert, action, utils } = require( 'api-testing' );
+const germanLabel = 'a-German-label-' + utils.uniq();
+const englishLabel = 'an-English-label-' + utils.uniq();
+const englishDescription = 'an-English-description-' + utils.uniq();
+
+describe( 'Lua Wikibase integration', () => {
+	let mindy;
+	let testItemId;
+	let module;
+
+	before( 'require extensions', async function () {
+		const requiredExtensions = [
+			'Scribunto',
+			'WikibaseRepository',
+			'WikibaseClient',
+		];
+		const installedExtensions = ( await action.getAnon().meta(
+			'siteinfo',
+			{ siprop: 'extensions' },
+			'extensions',
+		) ).map( ( extension ) => extension.name );
+		const missingExtensions = requiredExtensions.filter(
+			( requiredExtension ) => installedExtensions.indexOf( requiredExtension ) === -1,
+		);
+		if ( missingExtensions.length ) {
+			this.skip();
+		}
+	} );
+
+	before( 'set up admin', async () => {
+		mindy = await action.mindy();
+	} );
+
+	before( 'create test item', async () => {
+		const response = await mindy.action( 'wbeditentity', {
+			new: 'item',
+			token: await mindy.token( 'csrf' ),
+			data: JSON.stringify( {
+				labels: {
+					de: { language: 'de', value: germanLabel },
+					en: { language: 'en', value: englishLabel },
+				},
+				descriptions: {
+					en: { language: 'en', value: englishDescription },
+				},
+			} ),
+		}, 'POST' );
+		testItemId = response.entity.id;
+	} );
+
+	before( 'create test module', async () => {
+		module = utils.title( 'LuaWikibaseIntegrationTest-' );
+		await mindy.edit( `Module:${module}`, {
+			text: `
+local p = {}
+local dataValue = { type = 'wikibase-entityid', value = { ['entity-type'] = 'item', id = '${testItemId}' } }
+local snak = { datatype = 'wikibase-item', property = 'P435739845', snaktype = 'value', datavalue = dataValue }
+p.getLabel = function() return mw.wikibase.getLabel( '${testItemId}' ) end
+p.getLabelByLang = function() return mw.wikibase.getLabelByLang( '${testItemId}', 'en' ) end
+p.getEntity_labels = function() return mw.wikibase.getEntity( '${testItemId}' ).labels.de.value end
+p.getDescription = function() return mw.wikibase.getDescription( '${testItemId}' ) end
+p.formatValue = function() return mw.wikibase.formatValue( snak ) end
+return p
+`,
+			contentmodel: 'Scribunto',
+		} );
+	} );
+
+	it( 'getLabel can be invoked correctly', async () => {
+		const pageTitle = utils.title( 'WikibaseTestPageToParse-' );
+		await writeTextToPage( `{{#invoke:${module}|getLabel}}`, pageTitle );
+		const pageResponse = await parsePage( pageTitle );
+		assert.match( pageResponse.parse.text, new RegExp( englishLabel + '|' + germanLabel ) );
+		const usageAspects = await getUsageAspects( pageTitle );
+		assert.isNotEmpty( usageAspects );
+		for ( const usageAspect of usageAspects ) {
+			assert.match( usageAspect, /^L(\..*)?$/ );
+		}
+	} );
+
+	it( 'getLabelByLang can be invoked correctly', async () => {
+		const pageTitle = utils.title( 'WikibaseTestPageToParse-' );
+		await writeTextToPage( `{{#invoke:${module}|getLabelByLang}}`, pageTitle );
+		const pageResponse = await parsePage( pageTitle );
+		assert.equal( pageResponse.parse.text, `<p>${englishLabel}\n</p>` );
+		const usageAspects = await getUsageAspects( pageTitle );
+		assert.equal( usageAspects, 'L.en' );
+	} );
+
+	it( 'getEntity_labels can be invoked correctly', async () => {
+		const pageTitle = utils.title( 'WikibaseTestPageToParse-' );
+		await writeTextToPage( `{{#invoke:${module}|getEntity_labels}}`, pageTitle );
+		const response = await parsePage( pageTitle );
+		assert.equal( response.parse.text, `<p>${germanLabel}\n</p>` );
+		const usageAspects = await getUsageAspects( pageTitle );
+		assert.equal( usageAspects, 'L.de' );
+	} );
+
+	it( 'getDescription can be invoked correctly', async () => {
+		const pageTitle = utils.title( 'WikibaseTestPageToParse-' );
+		await writeTextToPage( `{{#invoke:${module}|getDescription}}`, pageTitle );
+		const response = await parsePage( pageTitle );
+		assert.equal( response.parse.text, `<p>${englishDescription}\n</p>` );
+		const usageAspects = await getUsageAspects( pageTitle );
+		assert.isNotEmpty( usageAspects );
+		for ( const usageAspect of usageAspects ) {
+			assert.match( usageAspect, /^D(\..*)?$/ );
+		}
+	} );
+
+	it( 'formatValue can be invoked correctly', async () => {
+		const pageTitle = utils.title( 'WikibaseTestPageToParse-' );
+		await writeTextToPage( `{{#invoke:${module}|formatValue}}`, pageTitle );
+		const response = await parsePage( pageTitle );
+		assert.match( response.parse.text, new RegExp( englishLabel + '|' + germanLabel ) );
+		const usageAspects = await getUsageAspects( pageTitle );
+		assert.include( usageAspects, 'T' );
+		const otherUsageAspects = usageAspects.filter( ( usageAspect ) => usageAspect !== 'T' );
+		assert.isNotEmpty( otherUsageAspects );
+		for ( const usageAspect of otherUsageAspects ) {
+			assert.match( usageAspect, /^L(\..*)?$/ );
+		}
+	} );
+
+	function writeTextToPage( text, pageTitle ) {
+		return action.getAnon().edit( pageTitle, { text }, 'post' );
+	}
+
+	function parsePage( pageTitle ) {
+		return action.getAnon().action( 'parse', {
+			page: pageTitle,
+			disablelimitreport: true,
+			formatversion: 2,
+			wrapoutputclass: '',
+		} );
+	}
+
+	async function getUsageAspects( pageTitle ) {
+		const usageResponse = await action.getAnon().action( 'query', {
+			prop: 'wbentityusage',
+			titles: pageTitle,
+			indexpageids: true,
+		} );
+		const pageId = usageResponse.query.pageids[ 0 ];
+		return usageResponse.query.pages[ pageId ].wbentityusage[ testItemId ].aspects;
+	}
+
+} );
---
+++ tests/api-testing/README.md
@@ -0,0 +1,6 @@
+# API integration tests
+
+These tests check that Wikibase works as expected through the API.
+See [mw:MediaWiki API integrationt tests][] for more information on how to run or write these tests.
+
+[mw:MediaWiki API integration tests]: https://www.mediawiki.org/wiki/MediaWiki_API_integration_tests
