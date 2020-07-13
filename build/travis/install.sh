638864cbde6bff53ffb987fc242ba83518ae59ce
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2020-07-13 18:39:12 +0200

    Travis: pick more random password
    MediaWiki now rejects overly short passwords. This one was randomly
    generated by `pwgen 20`.
    
    Bug: T257858
    Change-Id: Ie751501bad891436569cdd0ce52aa991e1d116fd

--- build/travis/install.sh
+++ build/travis/install.sh
@@ -34,4 +34,10 @@ if [ $? -gt 0 ]; then
 fi
 
 mysql -e 'create database its_a_mw;'
-php maintenance/install.php --dbtype $DBTYPE --dbuser root --dbname its_a_mw --dbpath $(pwd) --pass nyan TravisWiki admin
+php maintenance/install.php \
+    --dbtype $DBTYPE \
+    --dbuser root \
+    --dbname its_a_mw \
+    --dbpath $(pwd) \
+    --pass shie3Ekutaiy5Giebuwi \
+    TravisWiki admin
