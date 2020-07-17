94642aa4e79223cbfb08f7a9b72d8707490e7dc1
sihe <silvan.heintze@wikimedia.de>
2020-07-17 13:10:14 +0200

    Backport five FP changes
    All changes taken, with very slight changes, from previous
    cherry-picks by Tobias Andersson, while originally authored by:
    
    (3) sihe <silvan.heintze@wikimedia.de>
    (1) rosalie <perside.rosalie@wikimedia.de>
    (1) Tobias Andersson <tobias.andersson@wikimedia.de>
    
    This is the 1st commit message:
    
    FP: Add WDQS to the test system's ansible config
    
    This adds Wikidata Query Service to the Federated Properties test
    system by adding four containers (wdqs-frontend, wdqs, wdqs-updater
    and wdqs-proxy) to the docker-compose.yml file. Required EntitySource
    configurations are added to LocalSettings.php and a previously missing
    .htaccess file now enables redirects for the /entity/Qxx URI scheme.
    
    Bug: T257213
    Change-Id: I7e5ac4a66052547252fd64b0d0d186c31db387ef
    (cherry picked from commit 90116d3f7082743f9de1ac2f84e32bf062320ed3)
    
    This is the commit message #2:
    
    FP: Maintenance script for test instance #1
    
    We need to prepare one of the test instances created
    with a sample set of items to be used for testing federated properties.
    
    Bug: T255648
    Change-Id: I8fd206134dc66ac68047bf64c324db8ef2390c11
    (cherry picked from commit 1c5c7ee6abc93c78a60061af13f146bcff8278ba)
    
    This is the commit message #3:
    
    FP: Add eu-invasive-species to ansible test setup
    
    This adds configuration for the user testing system 'Invasive Species'
    to the ansible inventory and adds a few necessary fixes to the setup
    process.
    
    Bug: T256457
    Change-Id: I926f7c6653fbff4454073fef490fc92ae2da407d
    (cherry picked from commit 2424c0db4192bd11bff089f9740fb9edf4887c3c)
    
    This is the commit message #4:
    
    FP: Add sparql queries to documentation
    
    Bug: T246615
    Change-Id: I1a16f69b93adeea9fda3f522e878a15459e3cca0
    (cherry picked from commit ac254b978e1fe28d147a919ef45dd08f241bb3e1)
    
    This is the commit message #5:
    
    FP: Add opennext to ansible test setup
    
    This adds configuration for the user testing system 'OPEN!NEXT'
    to the ansible inventory.
    
    Bug: T256457
    Change-Id: If5eb093ed8ac24556efcbce4d2740ffa6ec3d7b6
    (cherry picked from commit 5e92960c3eed72ef6e3a6876bb2fca250d7e5850)

---
+++ repo/maintenance/importFederatedPropertiesSampleData.php
@@ -0,0 +1,73 @@
+<?php
+
+declare( strict_types = 1 );
+namespace Wikibase\Repo\Maintenance;
+
+use Maintenance;
+use User;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\Lib\Store\EntityStore;
+use Wikibase\Repo\WikibaseRepo;
+
+$basePath = getenv( 'MW_INSTALL_PATH' ) !== false ? getenv( 'MW_INSTALL_PATH' ) : __DIR__ . '/../../../mediawiki';
+
+require_once $basePath . '/maintenance/Maintenance.php';
+
+/**
+ * Import data from a data file to a Federated Properties Wikibase instance
+ * For an example file format to import, see repo/tests/phpunit/data/maintenance/federatedPropertiesTestDataFile.tsv
+ *
+ * @license GPL-2.0-or-later
+ */
+class ImportFederatedPropertiesSampleData extends Maintenance {
+
+	public function __construct() {
+		parent::__construct();
+		$this->addDescription( "Create new Items for federated properties test system." );
+		$this->addOption( 'dataFile', 'Data source for the new Items to create', true, true );
+		$this->addOption(
+			'delimiter',
+			'Character separating data across columns in the data file. default is tab.',
+			false,
+			true
+		);
+	}
+
+	public function execute() {
+		$dataFile = $this->getOption( 'dataFile' );
+		$lineDelimiter = $this->getOption( 'delimiter', "\t" );
+
+		$repo = WikibaseRepo::getDefaultInstance();
+		$entityStore = $repo->getEntityStore();
+
+		foreach ( $this->getDataToImport( $dataFile ) as $dataline ) {
+			if ( $dataline !== '' ) {
+				$dataArray = explode( $lineDelimiter, $dataline );
+				$this->storeNewItemWithTermData( $dataArray, $entityStore );
+			}
+		}
+		$this->output( 'Created new Items from data file:' . $dataFile . "\n" );
+	}
+
+	public function storeNewItemWithTermData( array $data, EntityStore $entityStore ) {
+		$item = new Item();
+		$item->setLabel( 'en', $data[0] );
+		$item->setDescription( 'en', $data[1] );
+
+		$entityStore->saveEntity( $item, 'Create new Item', User::newFromName( 'Maintenance script' ), EDIT_NEW );
+
+		return $item;
+	}
+
+	public function getDataToImport( $dataFileLocation ) {
+		$data = file_get_contents( $dataFileLocation );
+		if ( $data === '' ) {
+			$this->fatalError( 'Error: ' . $dataFileLocation . ' is an empty file' . "\n" );
+		}
+
+		return explode( "\n", $data );
+	}
+}
+
+$maintClass = ImportFederatedPropertiesSampleData::class;
+require_once RUN_MAINTENANCE_IF_MAIN;
