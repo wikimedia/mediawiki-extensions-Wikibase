07885b932ca3dc5ecd80f2272d9cc6085f1b6b8a
sihe <silvan.heintze@wikimedia.de>
2020-07-22 19:37:50 +0200

    Fix bug that causes wrong prefixes in RDF output
    With this fix, statement values in RDF output are correctly
    pre-prefixed, when using entitysource config and extra prefixes.
    
    Bug: T258507
    Change-Id: I5ec85deb5f83c5ebf6c111e6cfb68c9bc1d5e828

--- repo/tests/phpunit/includes/Rdf/Values/ComplexValueRdfHelperTest.php
+++ repo/tests/phpunit/includes/Rdf/Values/ComplexValueRdfHelperTest.php
@@ -47,10 +47,12 @@ class ComplexValueRdfHelperTest extends \PHPUnit\Framework\TestCase {
 		$snakWriter->prefix( 'www', "http://www/" );
 		$snakWriter->prefix( RdfVocabulary::NSP_CLAIM_VALUE, "http://acme/statement/value/" );
 		$snakWriter->prefix( RdfVocabulary::NS_VALUE, "http://acme/value/" );
+		$snakWriter->prefix( 'someAcmeSnakNamespace', "http://acme/someNamespace/" );
 
 		$valueWriter = new NTriplesRdfWriter();
 		$valueWriter->prefix( RdfVocabulary::NS_VALUE, "http://acme/value/" );
 		$valueWriter->prefix( RdfVocabulary::NS_ONTOLOGY, "http://acme/onto/" );
+		$valueWriter->prefix( 'someAcmeSnakNamespace', "http://acme/someNamespace/" );
 
 		$helper = new ComplexValueRdfHelper( $vocab, $valueWriter, new HashDedupeBag() );
 
@@ -86,10 +88,22 @@ class ComplexValueRdfHelperTest extends \PHPUnit\Framework\TestCase {
 		);
 		$this->assertNull( $lvalue, 'lvalue produced by adding a value a second time should be null' );
 
+		// do it a third time, with custom snakNamespace
+		$snakWriter->about( 'www', 'Q3' );
+		$helper->attachValueNode(
+			$snakWriter,
+			RdfVocabulary::NSP_CLAIM_STATEMENT,
+			'testing',
+			'DUMMY',
+			'someAcmeSnakNamespace',
+			$value
+		);
+
 		// check the triples written to the snak writer
 		$expected = [
 			'<http://www/Q1> <http://acme/statement/value/testing> <http://acme/value/e93b68fef814eb52e813bb72e6867432> .',
-			'<http://www/Q2> <http://acme/statement/value/testing> <http://acme/value/e93b68fef814eb52e813bb72e6867432> .'
+			'<http://www/Q2> <http://acme/statement/value/testing> <http://acme/value/e93b68fef814eb52e813bb72e6867432> .',
+			'<http://www/Q3> <http://acme/statement/value/testing> <http://acme/someNamespace/e93b68fef814eb52e813bb72e6867432> .'
 		];
 
 		$this->helper->assertNTriplesEquals( $expected, $snakWriter->drain() );
