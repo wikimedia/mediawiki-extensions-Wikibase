3db460e5e62c0faa337833930d647c3f9427a8f1
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2020-08-06 10:26:10 +0100

    Add empty WikibaseLib.php
    Reinstating this file to prevent breaking people's
    LocalSettings in a confusing way.
    
    Adding a warning to tell them not to do this.
    
    Bug: T259782
    Change-Id: I50120c7e979aa02b66f2cb4d79f21cf881496ecb

---
+++ lib/WikibaseLib.php
@@ -0,0 +1,4 @@
+<?php
+// This file is here for https://phabricator.wikimedia.org/T259782
+wfLogWarning( __FILE__ . ' is not an entry point and should not be required in your LocalSettings.php' .
+            '. It may be dropped soon.' );
