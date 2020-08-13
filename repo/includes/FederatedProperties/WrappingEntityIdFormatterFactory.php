65622b1f3cff04922cfcdbfb03cacc7d83e68dd9
addshore <addshorewiki@gmail.com>
2020-08-13 17:31:38 +0100

    FP: Show diff pages even when FP has connection issues
    This catches exceptions in a couple of different places and
    provides some less explosive defaults.
    1) When rendering the actual diff the label is fetched. Now this
       just displays the property ID instaed (with no link)
    2) Diff pages normally display the page below the diff output.
       This is now blank, as this needs ParserOutput which we currently
       do not render when there is no connection.
    
    Bug: T258173
    Change-Id: Iac3fca8d2da4115ff43a711c38c0549141665f76

---
+++ repo/includes/FederatedProperties/WrappingEntityIdFormatterFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace Wikibase\Repo\FederatedProperties;
+
+use Language;
+use Wikibase\View\EntityIdFormatterFactory;
+
+/**
+ * @license GPL-2.0-or-later
+ * @author Addshore
+ */
+class WrappingEntityIdFormatterFactory implements EntityIdFormatterFactory {
+
+	private $inner;
+
+	public function __construct( EntityIdFormatterFactory $inner ) {
+		$this->inner = $inner;
+	}
+
+	public function getOutputFormat() {
+		return $this->inner->getOutputFormat();
+	}
+
+	public function getEntityIdFormatter( Language $language ) {
+		return new FederatedPropertiesEntityIdFormatter(
+			$this->inner->getEntityIdFormatter( $language )
+		);
+	}
+
+}
