d6bcbafabcc43246a75ad476982188a9e7beb281
Tobias Andersson <tobias.andersson@wikimedia.de>
2020-08-20 10:37:46 +0200

    TypeError on commons Title::newFromText returning null
    Bug: T260853
    
    (cherry picked from commit 72e342204d9a04dcd29e332ca764ec8d152a9577)
    
    Change-Id: Iff8f03f7d287b5172001d988da24b940e375e4e0

--- repo/tests/phpunit/includes/Hooks/HtmlPageLinkRendererEndHookHandlerTest.php
+++ repo/tests/phpunit/includes/Hooks/HtmlPageLinkRendererEndHookHandlerTest.php
@@ -10,6 +10,7 @@ use SpecialPage;
 use Title;
 use Wikibase\Lib\Store\EntityUrlLookup;
 use Wikibase\Repo\WikibaseRepo;
+use Wikimedia\TestingAccessWrapper;
 
 /**
  * @covers \Wikibase\Repo\Hooks\HtmlPageLinkRendererEndHookHandler
@@ -334,5 +335,23 @@ class HtmlPageLinkRendererEndHookHandlerTest extends HtmlPageLinkRendererEndHook
 		);
 
 		$this->assertEquals( 'some-other-class', $customAttribs['class'] );
+	}
+
+	/**
+	 * @dataProvider linkTargetProvider
+	 */
+	public function testExtractForeignIdString( $linkTarget, $expectedOutput ) {
+		$wrapper = TestingAccessWrapper::newFromObject( $this->newInstance() );
+		$output = $wrapper->extractForeignIdString( $linkTarget );
+		$this->assertSame( $expectedOutput, $output );
+	}
+
+	public function linkTargetProvider() {
+		return [
+			'NS=MAIN, title=null' => [ Title::makeTitle( NS_MAIN, null ), null ], // T260853
+			'NS=SPECIAL, title=null' => [ Title::makeTitle( NS_SPECIAL, null ), null ],
+			'NS=SPECIAL, title=EntityPage/Q123' => [ Title::newFromText( 'Special:EntityPage/Q123' ), 'Q123' ],
+			'NS=MAIN, title=Special:EntityPage/Q123' => [ Title::newFromText( 'wikidata:Special:EntityPage/Q123' ), 'Q123' ],
+		];
 	}
 }
