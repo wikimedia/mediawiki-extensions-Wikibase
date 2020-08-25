cb02d8862539173093579517a30c010255ae4e83
addshore <addshorewiki@gmail.com>
2020-08-25 15:28:27 +0100

    FP: Split HtmlPageLinkRendererEndHook tests for FP into own file
    This makes it much clearer how much of this hook is currently
    tested for federated properties specific cases vs regular wikibase.
    
    This also fixed a small issue in testFederatedPropertiesFailure
    which was running some code in non fed props mode.
    
    Bug: T260230
    Change-Id: Ied52aedebbe7177dd6571b5b0effce545dc0684e

---
+++ repo/tests/phpunit/includes/FederatedProperties/Hooks/HtmlPageLinkRendererEndHookHandlerTest.php
@@ -0,0 +1,71 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace Wikibase\Repo\Test\Hooks\FederatedProperties;
+
+use RequestContext;
+use Title;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Entity\Property;
+use Wikibase\Repo\FederatedProperties\ApiRequestExecutionException;
+use Wikibase\Repo\Tests\Hooks\HtmlPageLinkRendererEndHookHandlerTestBase;
+
+/**
+ * @covers \Wikibase\Repo\Hooks\HtmlPageLinkRendererEndHookHandler
+ *
+ * @group Wikibase
+ *
+ * @license GPL-2.0-or-later
+ */
+class HtmlPageLinkRendererEndHookHandlerTest extends HtmlPageLinkRendererEndHookHandlerTestBase {
+
+	/**
+	 * @dataProvider validContextProvider
+	 */
+	public function testDoHtmlPageLinkRendererBegin( RequestContext $context ) {
+		$handler = $this->newInstance( 'foo', false, true, Property::ENTITY_TYPE );
+
+		$title = Title::makeTitle( WB_NS_PROPERTY, self::PROPERTY_WITH_LABEL );
+		$text = $title->getFullText();
+		$customAttribs = [];
+
+		$ret = $handler->doHtmlPageLinkRendererEnd(
+			$this->getLinkRenderer(), $title, $text, $customAttribs, $context );
+
+		$this->assertTrue( $ret );
+		$this->assertStringContainsString( 'fedprop', $customAttribs['class'] );
+		$this->assertContains( 'wikibase.federatedPropertiesLeavingSiteNotice', $context->getOutput()->getModules() );
+	}
+
+	public function testFederatedPropertiesFailure() {
+		$customAttribs = [ 'class' => 'new another' ];
+		$html = 'change-me';
+
+		$this->entityUrlLookup->expects( $this->once() )
+			->method( 'getLinkUrl' )
+			->with( new ItemId( 'Q1' ) )
+			->willThrowException( new ApiRequestExecutionException() );
+
+		$returnValue = $this->newInstance( 'foo', false, true )->doHtmlPageLinkRendererEnd(
+			$this->getLinkRenderer(),
+			$this->newTitle( self::ITEM_WITH_LABEL ),
+			$text,
+			$customAttribs,
+			$this->newContext(),
+			$html
+		);
+		$this->assertTrue( $returnValue );
+
+		// This will fallback to using the plain entity id as title and link text, and it will
+		// link via Special:EntityData (as we can't lookup namespaces).
+		$expectedAttribs = [
+			'href' => 'http://source.wiki/script/index.php?title=Special:EntityData/Q1',
+			'class' => 'another',
+			'title' => 'Q1'
+		];
+		$this->assertSame( 'Q1', $text );
+		$this->assertEquals( $expectedAttribs, $customAttribs );
+	}
+
+}
