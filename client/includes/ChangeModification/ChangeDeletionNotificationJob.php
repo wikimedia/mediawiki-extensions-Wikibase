85a5d96e91b4ae52f879e9250c4d76654de07698
Itamar Givon <itamar.givon@wikimedia.de>
2020-09-04 16:21:45 +0200

    Add a ChangeDeletionNotificationJob to ChangesModification NS
    - Restructure integration tests
    
    Depends-On: I656ee86671c6d4c9ba00a3cc6ecf123ac1cc04c3
    Change-Id: Ie988e30963562efc177b861f9729d65b43a55daa

---
+++ client/includes/ChangeModification/ChangeDeletionNotificationJob.php
@@ -0,0 +1,68 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace Wikibase\Client\ChangeModification;
+
+use MediaWiki\MediaWikiServices;
+use Title;
+use Wikimedia\Rdbms\ILBFactory;
+
+/**
+ * Job for notifying a client wiki of a batch of revision deletions on the repository.
+ *
+ * @license GPL-2.0-or-later
+ */
+class ChangeDeletionNotificationJob extends ChangeModificationNotificationJob {
+
+	private $batchSize;
+	private $lbFactory;
+
+	/**
+	 * Constructs a ChangeDeletionNotificationJob for the repo revisions given.
+	 *
+	 * @param ILBFactory $lbFactory
+	 * @param int $batchSize
+	 * @param array $params Contains the name of the repo, revisionIdentifiersJson to redact
+	 */
+	public function __construct( ILBFactory $lbFactory, int $batchSize, array $params = [] ) {
+		parent::__construct( 'ChangeDeletionNotification', $lbFactory->getMainLB(), $params );
+
+		$this->lbFactory = $lbFactory;
+		$this->batchSize = $batchSize;
+	}
+
+	/**
+	 * @param Title $unused
+	 * @param array $params
+	 * @return ChangeDeletionNotificationJob
+	 */
+	public static function newFromGlobalState( Title $unused, array $params ) {
+		$mwServices = MediaWikiServices::getInstance();
+
+		return new self(
+			$mwServices->getDBLoadBalancerFactory(),
+			$mwServices->getMainConfig()->get( 'UpdateRowsPerQuery' ),
+			$params
+		);
+	}
+
+	/**
+	 * @param int[] $relevantChanges
+	 */
+	protected function modifyChanges( array $relevantChanges ): void {
+
+		$dbw = $this->loadBalancer->getConnection( DB_MASTER );
+
+		foreach ( array_chunk( $relevantChanges, $this->batchSize ) as $rcIdBatch ) {
+			$dbw->delete(
+				'recentchanges',
+				[ 'rc_id' => $rcIdBatch ],
+				__METHOD__
+			);
+
+			$this->lbFactory->waitForReplication();
+		}
+	}
+
+}
