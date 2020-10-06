e702230515db5332fea35578f262d64925e54404
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2020-10-06 14:13:54 +0200

    Remove unused DataAccessSettings getters
    Bug: T264724
    Change-Id: Iba128c682bc823c692a7e27e759d8f87ef352b39

--- data-access/tests/phpunit/DataAccessSettingsTest.php
+++ data-access/tests/phpunit/DataAccessSettingsTest.php
@@ -3,7 +3,6 @@
 namespace Wikibase\DataAccess\Tests;
 
 use Wikibase\DataAccess\DataAccessSettings;
-use Wikibase\DataModel\Entity\Int32EntityId;
 
 /**
  * @covers \Wikibase\DataAccess\DataAccessSettings
@@ -20,23 +19,6 @@ class DataAccessSettingsTest extends \PHPUnit\Framework\TestCase {
 		);
 
 		$this->assertEquals( 1024, $settings->maxSerializedEntitySizeInBytes() );
-	}
-
-	public function testNormalizedPropertyTerms() {
-		$settings = new DataAccessSettings(
-			1
-		);
-
-		$this->assertTrue( $settings->useNormalizedPropertyTerms() );
-	}
-
-	public function testUseNormalizedItemTerms() {
-		$settings = new DataAccessSettings(
-			1
-		);
-
-		$this->assertTrue( $settings->useNormalizedItemTerms( 1 ) );
-		$this->assertTrue( $settings->useNormalizedItemTerms( Int32EntityId::MAX ) );
 	}
 
 }
