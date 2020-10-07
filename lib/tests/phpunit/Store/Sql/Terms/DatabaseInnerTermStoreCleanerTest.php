714deddb34ed9f2798ce7b11c497b2a653f9b788
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2020-10-07 13:58:43 +0200

    Refactor DatabaseInnerTermStoreCleanerTest further
    * Consistently use the plural for all three insert* methods (since they
      can all insert multiple records), and change “language” to “lang” to
      match the table name.
    
    * Consistently use the singular for all three assert* methods (since the
      method name refers to the table, not its contents).
    
    * Rename assertInDatabase to assertTableReturns to match the other
      three assert* methods. (Also, add missing parameter types.)
    
    * Rename parameters so the three insert* methods don’t have the same
      parameter name with different meanings, and add phpdoc.
    
    Bug: T264284
    Change-Id: Iea9e38328695770c2e9b960187b08a609060cfe9

--- lib/tests/phpunit/Store/Sql/Terms/DatabaseInnerTermStoreCleanerTest.php
+++ lib/tests/phpunit/Store/Sql/Terms/DatabaseInnerTermStoreCleanerTest.php
@@ -49,8 +49,8 @@ class DatabaseInnerTermStoreCleanerTest extends MediaWikiIntegrationTestCase {
 		$typeId = $this->db->insertId();
 
 		[ $text1Id, $text2Id ] = $this->insertTexts( [ 'a label', 'eine Bezeichnung' ] );
-		[ $textInLang1Id, $textInLang2Id ] = $this->insertTextsInLanguage( [ $text1Id => 'en', $text2Id => 'de' ] );
-		[ $termInLang1Id, $termInLang2Id ] = $this->insertTermInLang( [ $textInLang1Id => $typeId, $textInLang2Id => $typeId ] );
+		[ $textInLang1Id, $textInLang2Id ] = $this->insertTextsInLang( [ $text1Id => 'en', $text2Id => 'de' ] );
+		[ $termInLang1Id, $termInLang2Id ] = $this->insertTermsInLang( [ $textInLang1Id => $typeId, $textInLang2Id => $typeId ] );
 
 		$this->getCleaner()->cleanTermInLangIds( $this->db, $this->db, [ $termInLang1Id, $termInLang2Id ] );
 
@@ -73,10 +73,10 @@ class DatabaseInnerTermStoreCleanerTest extends MediaWikiIntegrationTestCase {
 		[ $text1Id, $text2Id ] = $this->insertTexts( [ 'some text', 'etwas Text' ] );
 
 		// insert into wbt_text_in_lang
-		[ $textInLang1Id, $textInLang2Id ] = $this->insertTextsInLanguage( [ $text1Id => 'en', $text2Id => 'de' ] );
+		[ $textInLang1Id, $textInLang2Id ] = $this->insertTextsInLang( [ $text1Id => 'en', $text2Id => 'de' ] );
 
 		// both texts are label & description in wbt_term_in_lang
-		[ $termInLang1Id, $termInLang2Id, $termInLang3Id, $termInLang4Id ] = $this->insertTermInLang(
+		[ $termInLang1Id, $termInLang2Id, $termInLang3Id, $termInLang4Id ] = $this->insertTermsInLang(
 			[
 				$textInLang1Id => [ $type1Id, $type2Id ],
 				$textInLang2Id => [ $type1Id, $type2Id ]
@@ -87,8 +87,8 @@ class DatabaseInnerTermStoreCleanerTest extends MediaWikiIntegrationTestCase {
 		$this->getCleaner()->cleanTermInLangIds( $this->db, $this->db, [ $termInLang1Id, $termInLang4Id ] );
 
 		// The two initial inserts remain
-		$this->assertTextsTableReturns( [ $text1Id, $text2Id ] );
-		$this->assertTextsInLangTableReturns( [ $textInLang1Id, $textInLang2Id ] );
+		$this->assertTextTableReturns( [ $text1Id, $text2Id ] );
+		$this->assertTextInLangTableReturns( [ $textInLang1Id, $textInLang2Id ] );
 
 		// the first and the last is removed from wbt_term_in_lang
 		$this->assertTermInLangTableReturns( [ $termInLang2Id, $termInLang3Id ] );
@@ -103,12 +103,12 @@ class DatabaseInnerTermStoreCleanerTest extends MediaWikiIntegrationTestCase {
 		[ $text1Id, $text2Id ] = $this->insertTexts( [ 'text', 'Text' ] );
 
 		// insert into wbt_text_in_lang
-		[ $textInLang1Id, $textInLang2Id ] = $this->insertTextsInLanguage( [ $text1Id => 'en', $text2Id => 'de' ] );
+		[ $textInLang1Id, $textInLang2Id ] = $this->insertTextsInLang( [ $text1Id => 'en', $text2Id => 'de' ] );
 		// text1 has one additional
-		[ $textInLang3Id ] = $this->insertTextsInLanguage( [ $text1Id => 'fr' ] );
+		[ $textInLang3Id ] = $this->insertTextsInLang( [ $text1Id => 'fr' ] );
 
 		// all terms are the same type
-		[ $termInLang1Id, $termInLang2Id, $termInLang3Id ] = $this->insertTermInLang(
+		[ $termInLang1Id, $termInLang2Id, $termInLang3Id ] = $this->insertTermsInLang(
 			[
 				$textInLang1Id => $typeId,
 				$textInLang2Id => $typeId,
@@ -120,11 +120,11 @@ class DatabaseInnerTermStoreCleanerTest extends MediaWikiIntegrationTestCase {
 		$this->getCleaner()->cleanTermInLangIds( $this->db, $this->db, [ $termInLang1Id ] );
 
 		// $textInLang1Id and $termInLang1Id gone,
-		$this->assertTextsInLangTableReturns( [ $textInLang2Id, $textInLang3Id ] );
+		$this->assertTextInLangTableReturns( [ $textInLang2Id, $textInLang3Id ] );
 		$this->assertTermInLangTableReturns( [ $termInLang2Id, $termInLang3Id ] );
 
 		// but $text1Id is still there because referenced by $termInLang3Id
-		$this->assertTextsTableReturns( [ $text1Id, $text2Id ] );
+		$this->assertTextTableReturns( [ $text1Id, $text2Id ] );
 	}
 
 	public function testCleanupOneText() {
@@ -136,14 +136,14 @@ class DatabaseInnerTermStoreCleanerTest extends MediaWikiIntegrationTestCase {
 		[ $text1Id, $text2Id ] = $this->insertTexts( [ 'text', 'Text' ] );
 
 		// insert into wbt_text_in_lang and term_in_lang
-		[ $textInLang1Id, $textInLang2Id ] = $this->insertTextsInLanguage( [ $text1Id => 'en', $text2Id => 'de' ] );
-		[ $termInLang1Id, $termInLang2Id ] = $this->insertTermInLang( [ $textInLang1Id => $typeId, $textInLang2Id => $typeId ] );
+		[ $textInLang1Id, $textInLang2Id ] = $this->insertTextsInLang( [ $text1Id => 'en', $text2Id => 'de' ] );
+		[ $termInLang1Id, $termInLang2Id ] = $this->insertTermsInLang( [ $textInLang1Id => $typeId, $textInLang2Id => $typeId ] );
 
 		$this->getCleaner()->cleanTermInLangIds( $this->db, $this->db, [ $termInLang1Id ] );
 
 		// $textId1, $textInLang1Id and $termInLang1Id gone
-		$this->assertTextsTableReturns( [ $text2Id ] );
-		$this->assertTextsInLangTableReturns( [ $textInLang2Id ] );
+		$this->assertTextTableReturns( [ $text2Id ] );
+		$this->assertTextInLangTableReturns( [ $textInLang2Id ] );
 		$this->assertTermInLangTableReturns( [ $termInLang2Id ] );
 	}
 
@@ -153,16 +153,16 @@ class DatabaseInnerTermStoreCleanerTest extends MediaWikiIntegrationTestCase {
 		$typeId = $this->db->insertId();
 
 		[ $text1Id, $text2Id ] = $this->insertTexts( [ 'a label', 'eine Bezeichnung' ] );
-		[ $textInLang1Id, $textInLang2Id ] = $this->insertTextsInLanguage( [ $text1Id => 'en', $text2Id => 'de' ] );
-		[ $termInLang1Id ] = $this->insertTermInLang( [ $textInLang1Id => $typeId ] );
+		[ $textInLang1Id, $textInLang2Id ] = $this->insertTextsInLang( [ $text1Id => 'en', $text2Id => 'de' ] );
+		[ $termInLang1Id ] = $this->insertTermsInLang( [ $textInLang1Id => $typeId ] );
 
 		// remove the first
 		$this->getCleaner()->cleanTermInLangIds( $this->db, $this->db, [ $termInLang1Id ] );
 
 		// $text2Id and $textInLang2Id are not used by any term_in_lang,
 		// but we should not attempt to clean them up
-		$this->assertTextsTableReturns( [ $text2Id ] );
-		$this->assertTextsInLangTableReturns( [ $textInLang2Id ] );
+		$this->assertTextTableReturns( [ $text2Id ] );
+		$this->assertTextInLangTableReturns( [ $textInLang2Id ] );
 		$this->assertTermInLangTableReturns( [] );
 	}
 
@@ -177,11 +177,11 @@ class DatabaseInnerTermStoreCleanerTest extends MediaWikiIntegrationTestCase {
 
 		[ $textId ] = $this->insertTexts( [ 'someText' ] );
 
-		[ $textInLangIdSingleUse1 ] = $this->insertTextsInLanguage( [ $textId => 'en' ] );
-		[ $textInLangIdSingleUse2 ] = $this->insertTextsInLanguage( [ $textId => 'de' ] );
-		[ $textInLangIdShared ] = $this->insertTextsInLanguage( [ $textId => 'fr' ] );
+		[ $textInLangIdSingleUse1 ] = $this->insertTextsInLang( [ $textId => 'en' ] );
+		[ $textInLangIdSingleUse2 ] = $this->insertTextsInLang( [ $textId => 'de' ] );
+		[ $textInLangIdShared ] = $this->insertTextsInLang( [ $textId => 'fr' ] );
 
-		[ $termInLangIdToDelete1, $termInLangIdToDelete2, $termInLangIdToDelete3, $termInLangIdToRemain ] = $this->insertTermInLang(
+		[ $termInLangIdToDelete1, $termInLangIdToDelete2, $termInLangIdToDelete3, $termInLangIdToRemain ] = $this->insertTermsInLang(
 			[
 				$textInLangIdSingleUse1 => $typeIdLabel,
 				$textInLangIdSingleUse2 => $typeIdLabel,
@@ -195,10 +195,10 @@ class DatabaseInnerTermStoreCleanerTest extends MediaWikiIntegrationTestCase {
 			$termInLangIdToDelete3,
 		] );
 
-		$this->assertTextsTableReturns( [ $textId ] );
+		$this->assertTextTableReturns( [ $textId ] );
 		$this->assertTermInLangTableReturns( [ $termInLangIdToRemain ] );
 		// This row should not be deleted, as it is still used by $termInLangIdToRemain
-		$this->assertTextsInLangTableReturns( [ $textInLangIdShared ] );
+		$this->assertTextInLangTableReturns( [ $textInLangIdShared ] );
 	}
 
 	public function testT237984_sharedTextIdsAreNotDeleted() {
@@ -209,15 +209,15 @@ class DatabaseInnerTermStoreCleanerTest extends MediaWikiIntegrationTestCase {
 		[ $textIdSingleUse, $textIdShared ] = $this->insertTexts( [ 'someText1', 'someText2' ] );
 
 		// insert a language for each, and one additional for the shared
-		[ $textInLangIdToDelete1, $textInLangIdToDelete2 ] = $this->insertTextsInLanguage(
+		[ $textInLangIdToDelete1, $textInLangIdToDelete2 ] = $this->insertTextsInLang(
 			[
 				$textIdSingleUse => 'en',
 				$textIdShared => 'de'
 			]
 		);
-		[ $textInLangIdToRemain3 ] = $this->insertTextsInLanguage( [ $textIdShared => 'fr' ] );
+		[ $textInLangIdToRemain3 ] = $this->insertTextsInLang( [ $textIdShared => 'fr' ] );
 
-		[ $termInLangIdToDelete1, $termInLangIdToDelete2, $termInLangIdToRemain3 ] = $this->insertTermInLang(
+		[ $termInLangIdToDelete1, $termInLangIdToDelete2, $termInLangIdToRemain3 ] = $this->insertTermsInLang(
 			[
 				$textInLangIdToDelete1 => $typeIdLabel,
 				$textInLangIdToDelete2 => $typeIdLabel,
@@ -229,10 +229,10 @@ class DatabaseInnerTermStoreCleanerTest extends MediaWikiIntegrationTestCase {
 
 		// This row should not be deleted, as it is still used by $textIdShared
 		$this->assertTermInLangTableReturns( [ $termInLangIdToRemain3 ] );
-		$this->assertTextsTableReturns( [ $textIdShared ] );
+		$this->assertTextTableReturns( [ $textIdShared ] );
 	}
 
-	private function assertInDatabase( array $elements, $table, $field ) {
+	private function assertTableReturns( array $elements, string $table, string $field ) {
 		$this->assertSelect(
 			$table,
 			$field,
@@ -247,18 +247,22 @@ class DatabaseInnerTermStoreCleanerTest extends MediaWikiIntegrationTestCase {
 		);
 	}
 
-	private function assertTextsTableReturns( array $elements ) {
-		$this->assertInDatabase( $elements, 'wbt_text', 'wbx_id' );
+	private function assertTextTableReturns( array $elements ) {
+		$this->assertTableReturns( $elements, 'wbt_text', 'wbx_id' );
 	}
 
-	private function assertTextsInLangTableReturns( array $elements ) {
-		$this->assertInDatabase( $elements, 'wbt_text_in_lang', 'wbxl_id' );
+	private function assertTextInLangTableReturns( array $elements ) {
+		$this->assertTableReturns( $elements, 'wbt_text_in_lang', 'wbxl_id' );
 	}
 
 	private function assertTermInLangTableReturns( array $elements ) {
-		$this->assertInDatabase( $elements, 'wbt_term_in_lang', 'wbtl_id' );
+		$this->assertTableReturns( $elements, 'wbt_term_in_lang', 'wbtl_id' );
 	}
 
+	/**
+	 * @param string[] $texts list of texts
+	 * @return int[] text IDs in the same order
+	 */
 	private function insertTexts( array $texts ): array {
 		$ids = [];
 		foreach ( $texts as $text ) {
@@ -269,9 +273,13 @@ class DatabaseInnerTermStoreCleanerTest extends MediaWikiIntegrationTestCase {
 		return $ids;
 	}
 
-	private function insertTextsInLanguage( array $texts ): array {
+	/**
+	 * @param string[] $textIds mapping from text ID to language code
+	 * @return int[] text_in_lang IDs in the same order
+	 */
+	private function insertTextsInLang( array $textIds ): array {
 		$ids = [];
-		foreach ( $texts as $textId => $language ) {
+		foreach ( $textIds as $textId => $language ) {
 			$this->db->insert( 'wbt_text_in_lang',
 				[ 'wbxl_language' => $language, 'wbxl_text_id' => $textId ] );
 			$ids[] = $this->db->insertId();
@@ -279,9 +287,14 @@ class DatabaseInnerTermStoreCleanerTest extends MediaWikiIntegrationTestCase {
 		return $ids;
 	}
 
-	private function insertTermInLang( array $texts ): array {
+	/**
+	 * @param (int|int[])[] $textInLangIds mapping from text_in_lang ID to type ID(s)
+	 * (to add several terms with the same text_in_lang ID, call the method multiple times)
+	 * @return int[] term_in_lang IDs in the same order
+	 */
+	private function insertTermsInLang( array $textInLangIds ): array {
 		$ids = [];
-		foreach ( $texts as $textInLangId => $typeId ) {
+		foreach ( $textInLangIds as $textInLangId => $typeId ) {
 			if ( !is_array( $typeId ) ) {
 				$typeId = [ $typeId ];
 			}
