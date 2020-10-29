18add39e2b42ea9500a776565159a68a140b07e5
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2020-10-29 15:15:59 +0000

    Introduce TermFallbackCacheFacade
    This facade is to be used to remove the responsibility of
    the mechanics of reading and writing the TermFallbackCache
    from the CachingFallbackLabelDescriptionLookup.
    
    There is also the intention of using this inside a new TermLookup
    backed by this cache.
    
    Bug: T266144
    Change-Id: Ifac650af31ee125cb3fc255c2279cd7d6aef1fbe

---
+++ lib/includes/FormatterCache/TermFallbackCacheFacade.php
@@ -0,0 +1,89 @@
+<?php
+
+declare( strict_types=1 );
+namespace Wikibase\Lib\FormatterCache;
+
+use Psr\SimpleCache\CacheInterface;
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\DataModel\Term\TermFallback;
+use Wikibase\Lib\Store\TermCacheKeyBuilder;
+
+/**
+ * TermFallbackCacheFacade is class to allow for simplified
+ * interaction with the shared cache used for storing
+ * TermFallback objects (also known as the formatter cache).
+ *
+ * The cache returns TermFallbackCacheFacade::NO_VALUE in the
+ * case there is no entry in the cache.
+ *
+ * Storing null values is also allowed as this indicates we
+ * have already checked the database for the term but found nothing.
+ *
+ * @license GPL-2.0-or-later
+ */
+class TermFallbackCacheFacade {
+
+	use TermFallbackSerializerTrait;
+	use TermCacheKeyBuilder;
+
+	public const NO_VALUE = false;
+
+	/**
+	 * @var CacheInterface
+	 */
+	private $cache;
+
+	/**
+	 * @var int
+	 */
+	private $cacheTtlInSeconds;
+
+	public function __construct(
+		CacheInterface $cache,
+		int $cacheTtlInSeconds
+	) {
+		$this->cache = $cache;
+		$this->cacheTtlInSeconds = $cacheTtlInSeconds;
+	}
+
+	/**
+	 * @param EntityId $targetEntityId
+	 * @param int $revisionId
+	 * @param string $languageCode
+	 * @param string $termType
+	 * @return false|TermFallback|null
+	 *
+	 * @example returning null indicates we have already checked the database and stored it's null response in the cache.
+	 * @example returning false indicates there is no cache entry.
+	 */
+	public function get( EntityId $targetEntityId, int $revisionId, string $languageCode, string $termType ) {
+		$cacheKey = $this->buildCacheKey( $targetEntityId, $revisionId, $languageCode, $termType );
+		$termFallback = $this->cache->get( $cacheKey, self::NO_VALUE );
+
+		if ( $termFallback === self::NO_VALUE ) {
+			return self::NO_VALUE;
+		}
+
+		return $this->unserialize( $termFallback );
+	}
+
+	/**
+	 * @param null|TermFallback $termFallback
+	 * @param EntityId $targetEntityId
+	 * @param int $revisionId
+	 * @param string $languageCode
+	 * @param string $termType
+	 */
+	public function set(
+		?TermFallback $termFallback,
+		EntityId $targetEntityId,
+		int $revisionId,
+		string $languageCode,
+		string $termType
+	): void {
+		$cacheKey = $this->buildCacheKey( $targetEntityId, $revisionId, $languageCode, $termType );
+		$serialization = $this->serialize( $termFallback );
+
+		$this->cache->set( $cacheKey, $serialization, $this->cacheTtlInSeconds );
+	}
+}
---
+++ lib/includes/FormatterCache/TermFallbackSerializerTrait.php
@@ -0,0 +1,39 @@
+<?php
+
+declare( strict_types=1 );
+namespace Wikibase\Lib\FormatterCache;
+
+use Wikibase\DataModel\Term\TermFallback;
+
+/**
+ * @license GPL-2.0-or-later
+ */
+trait TermFallbackSerializerTrait {
+
+	private function serialize( ?TermFallback $termFallback ): ?array {
+		if ( $termFallback === null ) {
+			return null;
+		}
+
+		return [
+			'language' => $termFallback->getActualLanguageCode(),
+			'value' => $termFallback->getText(),
+			'requestLanguage' => $termFallback->getLanguageCode(),
+			'sourceLanguage' => $termFallback->getSourceLanguageCode(),
+		];
+	}
+
+	private function unserialize( ?array $serialized ): ?TermFallback {
+		if ( $serialized === null ) {
+			return null;
+		}
+
+		$termData = $serialized;
+		return new TermFallback(
+			$termData['requestLanguage'],
+			$termData['value'],
+			$termData['language'],
+			$termData['sourceLanguage']
+		);
+	}
+}
