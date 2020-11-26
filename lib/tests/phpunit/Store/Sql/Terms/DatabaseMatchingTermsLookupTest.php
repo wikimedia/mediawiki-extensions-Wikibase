8db3c316cbdb07d0ff1878241c1dcd12edf36041
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2020-11-26 12:03:27 +0100

    Resurrect DatabaseMatchingTermsLookupTest
    Change I37fe78968d (commit c15fb47e44) accidentally left this test class
    devoid of any tests. Add them back and make them call getMatchingTerms()
    instead of getTopMatchingTerms().
    
    The code is mostly as of c15fb47e44’s parent commit, but I’ve taken the
    liberty to reformat it a bit, especially the data provider (use yield;
    use 'paramName' => instead of // $paramName). Also, I assume the class
    mentioned in @covers was always a copy+paste mistake – fix that.
    
    I’ve left the case-insensitive tests in place, even though they’re
    currently skipped, because the corresponding Phabricator task T242644
    has not been marked as declined yet, and it seems there’s still a chance
    that this might get implemented in the future.
    
    Change-Id: I2b2b2dacc171e76ee1a1dd95cde63d7fac1155bb

--- lib/tests/phpunit/Store/Sql/Terms/DatabaseMatchingTermsLookupTest.php
+++ lib/tests/phpunit/Store/Sql/Terms/DatabaseMatchingTermsLookupTest.php
@@ -15,7 +15,9 @@ use Wikibase\Lib\Store\Sql\Terms\DatabaseMatchingTermsLookup;
 use Wikibase\Lib\Store\Sql\Terms\DatabaseTermInLangIdsAcquirer;
 use Wikibase\Lib\Store\Sql\Terms\DatabaseTermInLangIdsResolver;
 use Wikibase\Lib\Store\Sql\Terms\DatabaseTypeIdsStore;
+use Wikibase\Lib\Store\TermIndexSearchCriteria;
 use Wikibase\Lib\StringNormalizer;
+use Wikibase\Lib\TermIndexEntry;
 use Wikibase\Lib\Tests\Store\Sql\Terms\Util\FakeLBFactory;
 use Wikibase\Lib\Tests\Store\Sql\Terms\Util\FakeLoadBalancer;
 use Wikimedia\Rdbms\DatabaseSqlite;
@@ -23,7 +25,7 @@ use Wikimedia\Rdbms\IDatabase;
 use Wikimedia\Rdbms\ILBFactory;
 
 /**
- * @covers \Wikibase\Lib\Store\Sql\Terms\DatabaseTermInLangIdsAcquirer
+ * @covers \Wikibase\Lib\Store\Sql\Terms\DatabaseMatchingTermsLookup
  *
  * @group Wikibase
  *
@@ -73,6 +75,137 @@ class DatabaseMatchingTermsLookupTest extends MediaWikiIntegrationTestCase {
 		$item2->setLabel( 'en', 'KITTENS should have mittens' );
 
 		return [ $item0, $item1, $item2 ];
+	}
+
+	/** @see testGetMatchingTerms */
+	public function provideGetMatchingTerms() {
+		[ $item0, $item1, $item2 ] = $this->getTestItems();
+
+		yield 'EXACT MATCH not prefix, case sensitive' => [
+			'entities' => [ $item0, $item1, $item2 ],
+			'criteria' => [
+				new TermIndexSearchCriteria( [
+					'termText' => 'Mittens',
+				] ),
+			],
+			'termTypes' => null,
+			'entityTypes' => null,
+			'options' => [
+				'prefixSearch' => false,
+				'caseSensitive' => true,
+			],
+			'expectedTermKeys' => [
+				'Q11/label.de:Mittens',
+			],
+		];
+		yield 'prefix, case sensitive' => [
+			'entities' => [ $item0, $item1, $item2 ],
+			'criteria' => [
+				new TermIndexSearchCriteria( [
+					'termText' => 'Mitte',
+				] ),
+			],
+			'termTypes' => null,
+			'entityTypes' => null,
+			'options' => [
+				'prefixSearch' => true,
+				'caseSensitive' => true,
+			],
+			'expectedTermKeys' => [
+				'Q11/label.de:Mittens',
+			],
+		];
+		yield 'prefixSearch and not caseSensitive' => [
+			'entities' => [ $item0, $item1, $item2 ],
+			'criteria' => [
+				new TermIndexSearchCriteria( [
+					'termText' => 'KiTTeNS',
+				] ),
+			],
+			'termTypes' => null,
+			'entityTypes' => null,
+			'options' => [
+				'prefixSearch' => true,
+				'caseSensitive' => false,
+			],
+			'expectedTermKeys' => [
+				'Q11/label.fr:kittens love mittens',
+				'Q22/label.en:KITTENS should have mittens',
+				// If not asking for top terms the below would normally also be expected
+				//'Q22/label.sv:kittens should have mittens',
+				'Q10/label.en:kittens',
+			],
+		];
+		yield 'prefixSearch and not caseSensitive LIMIT 1' => [
+			'entities' => [ $item0, $item1, $item2 ],
+			'criteria' => [
+				new TermIndexSearchCriteria( [
+					'termText' => 'KiTTeNS',
+				] ),
+			],
+			'termTypes' => null,
+			'entityTypes' => null,
+			'options' => [
+				'prefixSearch' => true,
+				'caseSensitive' => false,
+				'LIMIT' => 1,
+			],
+			'expectedTermKeys' => [
+				'Q11/label.fr:kittens love mittens',
+			],
+		];
+	}
+
+	/** @dataProvider provideGetMatchingTerms */
+	public function testGetMatchingTerms(
+		array $entities,
+		array $criteria,
+		$termTypes,
+		$entityTypes,
+		array $options,
+		array $expectedTermKeys
+	) {
+		if ( $options['caseSensitive'] === false ) {
+			$this->markTestSkipped( 'Case insensitive search is not supported yet: T242644' );
+		}
+		$lookup = $this->getMatchingTermsLookup();
+		$store = $this->getItemTermStoreWriter();
+
+		foreach ( $entities as $entity ) {
+			/** @var Item $entity */
+			$store->storeTerms( $entity->getId(), $entity->getFingerprint() );
+		}
+
+		$actual = $lookup->getMatchingTerms( $criteria, $termTypes, $entityTypes, $options );
+
+		$this->assertIsArray( $actual );
+
+		$actualTermKeys = array_map( [ $this, 'getTermKey' ], $actual );
+		$this->assertEquals( $expectedTermKeys, $actualTermKeys );
+	}
+
+	private function getTermKey( TermIndexEntry $term ) {
+		$key = '';
+		if ( $term->getEntityId() !== null ) {
+			$key .= $term->getEntityId()->getSerialization();
+		}
+
+		$key .= '/';
+		if ( $term->getTermType() !== null ) {
+			$key .= $term->getTermType();
+		}
+
+		$key .= '.';
+		if ( $term->getLanguage() !== null ) {
+			$key .= $term->getLanguage();
+		}
+
+		$key .= ':';
+		if ( $term->getText() !== null ) {
+			$key .= $term->getText();
+		}
+
+		return $key;
 	}
 
 	private function getMatchingTermsLookup() {
