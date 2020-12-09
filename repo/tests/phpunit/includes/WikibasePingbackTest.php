ed83daf671660b1f663cbfc195e329fd2195edd3
Tobias Andersson <tobias.andersson@wikimedia.de>
2020-12-09 15:48:43 +0100

    pingback: add more tests, inject requestfactory
    Bug: T269737
    Change-Id: I754b22c16a53ced59e75017497fea57e5a5426e6

--- repo/tests/phpunit/includes/WikibasePingbackTest.php
+++ repo/tests/phpunit/includes/WikibasePingbackTest.php
@@ -4,24 +4,75 @@ namespace Wikibase\Repo\Tests;
 
 use Config;
 use ExtensionRegistry;
-use PHPUnit\Framework\TestCase;
+use MediaWiki\Http\HttpRequestFactory;
+use MediaWikiIntegrationTestCase;
 use Psr\Log\LoggerInterface;
 use Wikibase\Lib\SettingsArray;
+use Wikibase\Lib\Tests\Store\Sql\Terms\Util\FakeLBFactory;
+use Wikibase\Lib\Tests\Store\Sql\Terms\Util\FakeLoadBalancer;
 use Wikibase\Repo\WikibasePingback;
 
 /**
  *
- * @covers WikibasePingback
+ * @covers \Wikibase\Repo\WikibasePingback
  * @group Wikibase
+ * @group Database
  *
  * @license GPL-2.0-or-later
  */
-class WikibasePingbackTest extends TestCase {
+class WikibasePingbackTest extends MediaWikiIntegrationTestCase {
+
+	public function testGetSystemInfo() {
+		$systemInfo = $this->getPingback()->getSystemInfo();
+		$this->assertIsArray( $systemInfo );
+	}
+
+	public function testSendPingback() {
+		$requestFactory = $this->createMock( HTTPRequestFactory::class );
+		$requestFactory->expects( $this->once() )
+			->method( 'post' )
+			->willReturn( true );
+
+		$pingback = $this->getPingback( $requestFactory );
+		$result = $pingback->sendPingback();
+
+		$this->assertTrue( $result );
+	}
 
 	public function testGetSystemInfo_getsListOfExtenstions() {
-		$config = $this->createMock( Config::class );
-		$logger = $this->createMock( LoggerInterface::class );
-		$extensions = $this->createMock( ExtensionRegistry::class );
+		$pingback = $this->getPingback();
+		$actual = $pingback->getSystemInfo()['extensions'];
+
+		$this->assertEquals( [ 'BBL', 'VE' ], $actual );
+	}
+
+	public function testGetSystemInfo_getsRepoSettings() {
+		$pingback = $this->getPingback();
+		$federationActual = $pingback->getSystemInfo()['federation'];
+		$termboxActual = $pingback->getSystemInfo()['termbox'];
+
+		$this->assertTrue( $federationActual );
+		$this->assertTrue( $termboxActual );
+	}
+
+	private function getPingback(
+		HttpRequestFactory $requestFactory = null,
+		Config $config = null,
+		LoggerInterface $logger = null,
+		ExtensionRegistry $extensions = null,
+		SettingsArray $wikibaseRepoSettings = null
+	): WikibasePingback {
+		$config = $config ?: $this->createMock( Config::class );
+		$logger = $logger ?: $this->createMock( LoggerInterface::class );
+		$extensions = $extensions ?: $this->createMock( ExtensionRegistry::class );
+		$wikibaseRepoSettings = $wikibaseRepoSettings ?: $this->createMock( SettingsArray::class );
+		$requestFactory = $requestFactory ?: $this->createMock( HTTPRequestFactory::class );
+		$loadBalancerFactory = new FakeLBFactory( [ 'lb' => new FakeLoadBalancer( [ 'dbr' => $this->db ] ) ] );
+
+		$wikibaseRepoSettings
+			->method( 'getSetting' )
+			->withConsecutive( [ 'federatedPropertiesEnabled' ], [ 'termboxEnabled' ] )
+			->willReturn( true );
 
 		$extensions->method( 'getAllThings' )
 			->willReturn( [
@@ -29,27 +80,13 @@ class WikibasePingbackTest extends TestCase {
 				'VisualEditor' => []
 			] );
 
-		$pingback = new WikibasePingback( $config, $logger, $extensions );
-		$actual = $pingback->getSystemInfo()['extensions'];
-
-		$this->assertEquals( [ 'BBL', 'VE' ], $actual );
-	}
-
-	public function testGetSystemInfo_getsRepoSettings() {
-		$config = $this->createMock( Config::class );
-		$logger = $this->createMock( LoggerInterface::class );
-		$wikibaseRepoSettings = $this->createMock( SettingsArray::class );
-
-		$wikibaseRepoSettings
-			->method( 'getSetting' )
-			->withConsecutive( [ 'federatedPropertiesEnabled' ], [ 'termboxEnabled' ] )
-			->willReturn( true );
-
-		$pingback = new WikibasePingback( $config, $logger, null, $wikibaseRepoSettings );
-		$federationActual = $pingback->getSystemInfo()['federation'];
-		$termboxActual = $pingback->getSystemInfo()['termbox'];
-
-		$this->assertTrue( $federationActual );
-		$this->assertTrue( $termboxActual );
+		return new WikibasePingback(
+			$config,
+			$logger,
+			$extensions,
+			$wikibaseRepoSettings,
+			$requestFactory,
+			$loadBalancerFactory
+		);
 	}
 }
