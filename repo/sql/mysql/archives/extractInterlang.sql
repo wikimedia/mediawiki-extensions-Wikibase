48d3c80fc413e28b4192aadc82517fbebdca181d
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2021-02-11 17:11:03 +0100

    Remove extractInterlang.sql
    This SQL file was added in the earliest days of Wikibase, in commit
    a08abb3630 (April 2012), together with an importInterlang.php
    maintenance script that would process its output. importInterlang.php is
    long gone – removed in change I21b8919fad (commit 69757b7ebb, May 2015)
    – but extractInterlang.sql has survived so far. Let’s get rid of it,
    since I’m certain it’s not useful or used anymore, and the SQL contains
    several antipatterns.
    
    Change-Id: Ieffaba736d0fe650e6fa0585eb8d3ecc40d354ca

--- repo/sql/mysql/archives/extractInterlang.sql
+++
@@ -1,5 +0,0 @@
--- Extracts interlanguage links in the format used by importInterlanguage.php
-select distinct page_title, ll_lang, ll_title
-from page, langlinks 
-where page_id = ll_from and page_namespace = 0
-order by 1, 2, 3;
