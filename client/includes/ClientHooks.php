953ed2d34f08686814c609a8e1d7069285e79818
Umherirrender <umherirrender_de.wp@web.de>
2021-03-03 01:30:07 +0100

    ClientHooks::SiteConfig: Unsupported operand types: array & int
    php8 is not happy when trying to bitwise an array with int
    in BagOStuff::fieldHasFlags due to harder checks on numeric operands
    https://wiki.php.net/rfc/saner-numeric-strings
    
    Change-Id: Ie9146c1136b7e392a22207b5305da4785151dd61

--- client/includes/ClientHooks.php
+++ client/includes/ClientHooks.php
@@ -252,10 +252,7 @@ final class ClientHooks {
 				];
 
 				return [ 'currentSite' => $currentSite ];
-			},
-			// @fixme These options only exist in WanObjectCache, but this code is using BagOStuff!
-			// @phan-suppress-next-line PhanTypeMismatchArgumentProbablyReal
-			[ 'lockTSE' => 10, 'pcTTL' => $cache::TTL_PROC_LONG ]
+			}
 		);
 	}
 
