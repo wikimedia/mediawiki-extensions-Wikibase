97e5c1716db75c84eb7a7432cc425f516ea241d7
Noa wmde <noa.rave@wikimedia.de>
2021-03-04 10:53:31 +0100

    GH Actions CI - specify server in db install command
    Change-Id: Ifc3d9c6b463ccf40cc2c9e9aab216ee036302b91

--- build/ci-scripts/install.sh
+++ build/ci-scripts/install.sh
@@ -37,9 +37,10 @@ if [ $? -gt 0 ]; then
 	composer install
 fi
 
-mysql -e 'create database its_a_mw;' -proot -h"127.0.0.1"
+mysql -e 'create database its_a_mw;' -uroot -proot -h"127.0.0.1"
 php maintenance/install.php \
     --dbtype $DBTYPE \
+    --dbserver 127.0.0.1
     --dbuser root \
     --dbname its_a_mw \
     --dbpath $(pwd) \
