2845ab8ed9c74ffeacb3d2068a4e85d40710d40e
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2021-03-05 16:43:54 +0100

    client: move CompactBaseDataModelSerializerFactory to service container
    The GenericServices version of the code has the two SerializerFactory
    constants hard-coded “to not fail phan”; in the service wiring code, I’m
    using the class constants again, since if Phan complains I think it’s
    better to suppress the warning than to inline the constants.
    
    Bug: T276535
    Change-Id: I1e375b4b241bd60f873f508344ffdd7701e2c4ac

---
+++ client/tests/phpunit/unit/includes/ServiceWiring/CompactBaseDataModelSerializerFactoryTest.php
@@ -0,0 +1,25 @@
+<?php
+declare( strict_types = 1 );
+
+namespace Wikibase\Client\Tests\Unit\ServiceWiring;
+
+use Wikibase\Client\Tests\Unit\ServiceWiringTestCase;
+use Wikibase\DataModel\SerializerFactory;
+
+/**
+ * @coversNothing
+ *
+ * @group Wikibase
+ *
+ * @license GPL-2.0-or-later
+ */
+class CompactBaseDataModelSerializerFactoryTest extends ServiceWiringTestCase {
+
+	public function testConstruction(): void {
+		$this->assertInstanceOf(
+			SerializerFactory::class,
+			$this->getService( 'WikibaseClient.CompactBaseDataModelSerializerFactory' )
+		);
+	}
+
+}
