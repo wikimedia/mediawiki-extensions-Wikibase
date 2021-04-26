bbf0c1e087fc4952e618370c0ea9767191c80fb0
addshore <addshorewiki@gmail.com>
2021-04-26 19:26:06 +0100

    bridge: Switch tests waiting for bridge to open from 10 -> 20s wait
    TLDR of the main investigation ticket leading to the 10 -> 20s
    change at T281122 is that sometimes the VMs that run our CI get
    slow.
    After looking at the most recent failures of this test case, a
    20s timeout instead of a 10s timeout would have lead to them all
    passing.
    
    If our CI ever speeds up perhaps we can decrease this again?
    But for now a longer timeout here and a more stable CI system
    will be a big win for everyone! (IMO)
    
    Bug: T281122
    Bug: T277205
    Bug: T276932
    Change-Id: I61c0d8f3d4808c05e90d79d45681005b51ff9ba9

--- client/data-bridge/tests/selenium/wdio.conf.js
+++ client/data-bridge/tests/selenium/wdio.conf.js
@@ -90,7 +90,7 @@ exports.config = {
 	logLevel: 'error',
 
 	// Default timeout for all waitFor* commands.
-	waitforTimeout: 10000,
+	waitforTimeout: 20000,
 
 	// Default timeout in milliseconds for request
 	// if Selenium Grid doesn't send response
