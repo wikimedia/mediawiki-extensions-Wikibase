b3a69eda1d6b8e560d9cda608d7a72f3bccc9086
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2021-06-02 13:22:14 +0100

    Add ItemStubRdfBuilder
    Covered with a simple unit test that is very permissive
    and simple expects the TermLookup to be used.
    
    Also covered with an integration test inspired by
    the one that currently covers the addEntityStub method
    on the TermsRdfBuilder
    
    An in memory implementation of a PrefetchingTermLookup
    similar to the FakePrefetchingTermLookup is introduced
    to avoid creating a very heavy mock but for use in tests
    only.
    
    Change-Id: I6efabfe4b2b0ccbab962a9091f1bd4cdbd80845b

---
+++ repo/tests/phpunit/includes/Rdf/ItemStubRdfBuilderIntegrationTest.php
@@ -0,0 +1,98 @@
+<?php
+
+declare( strict_types=1 );
+
+namespace Wikibase\Repo\Tests\Rdf;
+
+use MediaWikiIntegrationTestCase;
+use Wikibase\DataAccess\Tests\InMemoryPrefetchingTermLookup;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\Repo\Rdf\ItemStubRdfBuilder;
+use Wikimedia\Purtle\RdfWriter;
+
+/**
+ * @covers \Wikibase\Repo\Rdf\TermsRdfBuilder
+ *
+ * @group Wikibase
+ * @group WikibaseRdf
+ *
+ * @license GPL-2.0-or-later
+ */
+class ItemStubRdfBuilderIntegrationTest extends MediaWikiIntegrationTestCase {
+
+	/**
+	 * @var NTriplesRdfTestHelper
+	 */
+	private $helper;
+	private $termLookup;
+
+	protected function setUp(): void {
+		parent::setUp();
+
+		$this->helper = new NTriplesRdfTestHelper(
+			new RdfBuilderTestData(
+				__DIR__ . '/../../data/rdf/entities',
+				__DIR__ . '/../../data/rdf/TermsRdfBuilder'
+			)
+		);
+		$this->termLookup = new InMemoryPrefetchingTermLookup();
+	}
+
+	/**
+	 * Initialize repository data
+	 *
+	 * @return RdfBuilderTestData
+	 */
+	private function getTestData() {
+		return $this->helper->getTestData();
+	}
+
+	/**
+	 * @param RdfWriter $writer
+	 * @param string[]|null $languages
+	 *
+	 * @return ItemStubRdfBuilder
+	 */
+	private function newBuilder( RdfWriter $writer, array $languages = null ): ItemStubRdfBuilder {
+		$vocabulary = $this->getTestData()->getVocabulary();
+
+		$builder = new ItemStubRdfBuilder(
+			$this->termLookup,
+			$vocabulary,
+			$writer,
+			[],
+			$languages
+		);
+
+		return $builder;
+	}
+
+	private function assertNTriples( $dataSetName, RdfWriter $writer ) {
+		$actual = $writer->drain();
+		$this->helper->assertNTriplesEqualsDataset( $dataSetName, $actual );
+	}
+
+	public function provideAddEntityStub() {
+		return [
+			[ 'Q2', 'Q2_terms_stubs', [ 'en', 'ru' ] ],
+			[ 'Q2', 'Q2_terms_stubs_ru', [ 'ru' ] ],
+		];
+	}
+
+	/**
+	 * @dataProvider provideAddEntityStub
+	 */
+	public function testAddEntityStub( $entityName, $dataSetName, array $languages = null ) {
+		$entityId = new ItemId( $entityName );
+
+		$this->termLookup->setData( [
+			$this->getTestData()->getEntity( $entityName )
+		] );
+
+		$writer = $this->getTestData()->getNTriplesWriter();
+		$this->newBuilder( $writer, $languages )->addEntityStub( $entityId );
+
+		$this->assertNTriples( $dataSetName, $writer );
+	}
+
+}
