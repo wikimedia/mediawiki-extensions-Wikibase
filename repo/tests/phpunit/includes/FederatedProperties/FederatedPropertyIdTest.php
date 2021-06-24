65938ef831721433bde0b9dfd0e6444741f7036c
rosalie <perside.rosalie@wikimedia.de>
2021-06-24 12:24:46 +0200

    Introduce FederatedPropertyId
    This class was introduced in Repo since this is the only place we intend to use it for now.
    In the future we might want to move it to the packages in lib or to datamodel
    
    Bug: T285453
    Change-Id: I63383451bbece7af216fd2ab80980367870308af

---
+++ repo/tests/phpunit/includes/FederatedProperties/FederatedPropertyIdTest.php
@@ -0,0 +1,41 @@
+<?php
+declare( strict_types=1 );
+
+namespace Wikibase\Repo\Tests\FederatedProperties;
+
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use Wikibase\Repo\FederatedProperties\FederatedPropertyId;
+
+/**
+ * @group Wikibase
+ * @license GPL-2.0-or-later
+ */
+class FederatedPropertyIdTest extends TestCase {
+
+	public function testCreateAndSerializeId() {
+		$serialization = 'http://www.wikidata.org/entity/P31';
+		$id = new FederatedPropertyId( $serialization );
+		$this->assertEquals( $serialization, $id->serialize() );
+		$this->assertEquals( $serialization, $id->getSerialization() );
+	}
+
+	public function testCreationFailsWhenSerializationNotURI() {
+		$serialization = 'P31';
+		$this->expectException( InvalidArgumentException::class );
+		$id = new FederatedPropertyId( $serialization );
+	}
+
+	public function testCreationFailsIfURIhasNoPath() {
+		$serialization = 'http://www.wikidata.org';
+		$this->expectException( InvalidArgumentException::class );
+		$id = new FederatedPropertyId( $serialization );
+	}
+
+	public function testUnserializationWithValidSerialization() {
+		$serialization = 'http://www.wikidata.org/entity/P32';
+		$id = new FederatedPropertyId( 'http://www.wikidata.org/entity/P31' );
+		$id->unserialize( $serialization );
+		$this->assertEquals( $serialization, $id->getSerialization() );
+	}
+}
