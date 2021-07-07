da0dd9998a430c38deb5d9b4e18103d67dc02f15
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2021-07-07 13:37:03 +0200

    Add RepoNoBadUsageTest
    Repo still has some wfGetDB() calls – just allow them for now, we’ll get
    rid of them soon.
    
    Bug: T286209
    Change-Id: Ieaaac176bbcf2928e98b76bd02560e02552335a7

---
+++ repo/tests/phpunit/unit/RepoNoBadUsageTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace Wikibase\Repo\Tests\Unit;
+
+use Wikibase\Lib\Tests\NoBadUsageTest;
+
+/**
+ * @group Wikibase
+ *
+ * @license GPL-2.0-or-later
+ */
+class RepoNoBadUsageTest extends NoBadUsageTest {
+
+	protected function getBadPatternsWithAllowedUsages(): array {
+		return [
+			// don’t reference client in repo
+			'WikibaseClient::' => [
+				'maintenance/dispatchChanges.php' => 1, // guarded by isClientEnabled()
+			],
+			'Wikibase\\Client\\' => [
+				'maintenance/dispatchChanges.php' => 1, // see above
+			],
+			'Wikibase\\\\Client\\\\' => [],
+			// don’t use MediaWiki RDBMS – use our RDBMS instead (DomainDb etc.)
+			'/\b(get|I|)LBFactory(?:;)/' => [
+				'tests/phpunit/includes/GlobalStateFactoryMethodsResourceTest.php' => 1, // mock
+				'tests/phpunit/unit/ServiceWiringTestCase.php' => true, // mock
+			],
+			'/\b((get)?(DB)?|I|)LoadBalancer(Factory)?(?!::|;)/' => [
+				'WikibaseRepo.ServiceWiring.php' => 1, // RepoDomainDbFactory service wiring
+				'tests/phpunit/includes/GlobalStateFactoryMethodsResourceTest.php' => 1, // mock
+				'tests/phpunit/unit/ServiceWiringTestCase.php' => true, // mock
+			],
+			'wfGetDB' => [
+				// TODO fix these, this should really be an empty array
+				'includes/Store/Sql/WikiPageEntityStore.php' => 1,
+				'tests/' => true,
+			],
+			'wfGetLB' => [],
+		];
+	}
+
+	protected function getBaseDir(): string {
+		return __DIR__ . '/../../../';
+	}
+
+	protected function getThisFile(): string {
+		return __FILE__;
+	}
+
+}
