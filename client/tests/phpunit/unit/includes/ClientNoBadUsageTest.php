9d898a7378d1222dc98192d47f22b2f7313416fe
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2021-07-08 12:46:02 +0200

    Add ClientNoBadUsageTest
    Three of the tests that use LoadBalancer are for classes that directly
    get a connection manager injected; I feel like we should change those to
    inject some DomainDb (haven’t yet checked if Repo- or Client), but not
    right now.
    
    Bug: T286209
    Change-Id: I728e34cb23135ee12aef13ae11ed06771ef11071

---
+++ client/tests/phpunit/unit/includes/ClientNoBadUsageTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace Wikibase\Client\Tests\Unit;
+
+use Wikibase\Lib\Tests\NoBadUsageTest;
+
+/**
+ * @group Wikibase
+ *
+ * @license GPL-2.0-or-later
+ */
+class ClientNoBadUsageTest extends NoBadUsageTest {
+
+	protected function getBadPatternsWithAllowedUsages(): array {
+		return [
+			// don’t reference repo in client
+			'WikibaseRepo::' => [
+				'config/WikibaseClient.default.php' => 2, // both guarded by thisWikiIsTheRepo
+				'tests/phpunit/ClientDefaultsTest.php' => 1, // guarded by isRepoEnabled()
+			],
+			'Wikibase\\Repo\\' => [
+				'config/WikibaseClient.default.php' => 1, // see above
+				'tests/phpunit/ClientDefaultsTest.php' => 1, // see above
+			],
+			'Wikibase\\\\Repo\\\\' => [],
+			// don’t use MediaWiki RDBMS – use our RDBMS instead (DomainDb etc.)
+			'/\b(get|I|)LBFactory(?:;)/' => [
+				'tests/phpunit/integration/includes/GlobalStateFactoryMethodsResourceTest.php' => 1, // mock
+				'tests/phpunit/unit/includes/ServiceWiringTestCase.php' => true, // mock
+			],
+			'/\b((get)?(DB)?|I|)LoadBalancer(Factory)?(?!::|;)/' => [
+				'WikibaseClient.ServiceWiring.php' => 2, // RepoDomainDbFactory+ClientDomainDbFactory service wiring
+				'tests/phpunit/integration/includes/GlobalStateFactoryMethodsResourceTest.php' => 1, // mock
+				'tests/phpunit/integration/includes/RecentChanges/RecentChangesFinderTest.php' => true, // TODO migrate?
+				'tests/phpunit/integration/includes/Usage/Sql/SqlSubscriptionManagerTest.php' => true, // TODO migrate?
+				'tests/phpunit/integration/includes/Usage/Sql/SqlUsageTrackerTest.php' => true, // TODO migrate?
+				'tests/phpunit/integration/includes/ChangeModification/ChangeDeletionNotificationJobTest.php' => 1, // make DomainDb
+				'tests/phpunit/integration/includes/ChangeModification/ChangeVisibilityNotificationJobTest.php' => 1, // make DomainDb
+				'tests/phpunit/unit/includes/ServiceWiringTestCase.php' => true, // mock
+			],
+			'wfGetDB' => [
+				// TODO fix these, this should really be an empty array
+				'tests/' => true,
+			],
+			'wfGetLB' => [],
+		];
+	}
+
+	protected function getBaseDir(): string {
+		return __DIR__ . '/../../../../';
+	}
+
+	protected function getThisFile(): string {
+		return __FILE__;
+	}
+
+}
