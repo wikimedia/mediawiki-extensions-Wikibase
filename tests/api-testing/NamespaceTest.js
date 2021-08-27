21d5778daa20fc180255894392644b2391f954b3
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2021-08-27 15:20:45 +0200

    Extract requireExtensions() function for api-testing
    Maybe at some point we can share this beyond Wikibase, but for now just
    putting it in a file here is already better than copy+pasting it around.
    
    Change-Id: If95002e9ba11bf9b3ecaf1144fc0543dde91d666

--- tests/api-testing/NamespaceTest.js
+++ tests/api-testing/NamespaceTest.js
@@ -1,25 +1,13 @@
 'use strict';
 
 const { assert, action } = require( 'api-testing' );
+const { requireExtensions } = require( './utils.js' );
 
 describe( 'namespaces', () => {
 
-	before( 'require extensions', async function () {
-		const requiredExtensions = [
-			'WikibaseRepository',
-		];
-		const installedExtensions = ( await action.getAnon().meta(
-			'siteinfo',
-			{ siprop: 'extensions' },
-			'extensions',
-		) ).map( ( extension ) => extension.name );
-		const missingExtensions = requiredExtensions.filter(
-			( requiredExtension ) => installedExtensions.indexOf( requiredExtension ) === -1,
-		);
-		if ( missingExtensions.length ) {
-			this.skip();
-		}
-	} );
+	before( 'require extensions', requireExtensions( [
+		'WikibaseRepository',
+	] ) );
 
 	const testCases = [
 		[ 120, 'wikibase-item' ],
