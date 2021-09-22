c393ffbd7bc59e5dec973daa9e4eca093def2be4
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2021-09-22 12:31:11 +0200

    FP: allow enabling fed props without an api source
    When Federated Properties is enabled, property search does a combined
    search for local and federated properties. With no API source
    configured, the instantiation of ApiEntitySearchHelper errors prior to
    this patch.
    
    Bug: T291152
    Change-Id: Ie0d0e5105f2d732a007edb20e093922c1c3eda89

---
+++ repo/includes/FederatedProperties/NullEntitySearchHelper.php
@@ -0,0 +1,18 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace Wikibase\Repo\FederatedProperties;
+
+use Wikibase\Repo\Api\EntitySearchHelper;
+
+/**
+ * @license GPL-2.0-or-later
+ */
+class NullEntitySearchHelper implements EntitySearchHelper {
+
+	public function getRankedSearchResults( $text, $languageCode, $entityType, $limit, $strictLanguage ) {
+		return [];
+	}
+
+}
