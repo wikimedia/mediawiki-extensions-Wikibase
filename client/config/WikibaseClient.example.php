d502ce6ec2743251d5ed1b329f966fee90f85332
Amir Sarabadani <ladsgroup@gmail.com>
2021-09-24 23:32:32 +0200

    client: Remove default configs in the example file
    It is the same as the default values.
    
    This file is noop.
    
    Bug: T291617
    Change-Id: Ib0e301d3477afad9fd165761d23c944991bf2058

--- client/config/WikibaseClient.example.php
+++ client/config/WikibaseClient.example.php
@@ -1,20 +1,8 @@
 <?php
-
 /**
- * Example configuration for the Wikibase Client extension.
- *
- * This file is NOT an entry point the Wikibase Client extension. Use WikibaseClient.php.
- * It should furthermore not be included from outside the extension.
- *
- * @see docs/options.wiki
- *
- * @license GPL-2.0-or-later
- * @author Daniel Kinzler
+ * Deprecated: Do not load this file. It will be removed in the future.
  */
 
 if ( !defined( 'MEDIAWIKI' ) ) {
 	die( 'Not an entry point.' );
 }
-
-$wgWBClientSettings['injectRecentChanges'] = true;
-$wgWBClientSettings['showExternalRecentChanges'] = true;
