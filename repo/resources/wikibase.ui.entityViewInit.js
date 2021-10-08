7ec524b99a5976451299c7f782fc79f5b787e486
Addbot <addshorewiki+addbot-dependabot@gmail.com>
2021-10-08 15:43:34 +0000

    Bump eslint-config-wikimedia from 0.17.0 to 0.20.0
    And fix resulting errors.
    
    Bumps [eslint-config-wikimedia](https://github.com/wikimedia/eslint-config-wikimedia) from 0.17.0 to 0.20.0.
    - [Release notes](https://github.com/wikimedia/eslint-config-wikimedia/releases)
    - [Changelog](https://github.com/wikimedia/eslint-config-wikimedia/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/wikimedia/eslint-config-wikimedia/compare/v0.17.0...v0.20.0)
    
    ---
    updated-dependencies:
    - dependency-name: eslint-config-wikimedia
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Pull Request: https://github.com/wikimedia/Wikibase/pull/24
    Change-Id: I13c89f5d0dabb533f92344de8baaf559bad7d65c

--- repo/resources/wikibase.ui.entityViewInit.js
+++ repo/resources/wikibase.ui.entityViewInit.js
@@ -356,6 +356,7 @@
 
 	mw.hook( 'wikipage.content' ).add( function () {
 		// This is copied from startup.js in MediaWiki core.
+		// eslint-disable-next-line compat/compat
 		var mwPerformance = window.performance && performance.mark ? performance : {
 			mark: function () {}
 		};
