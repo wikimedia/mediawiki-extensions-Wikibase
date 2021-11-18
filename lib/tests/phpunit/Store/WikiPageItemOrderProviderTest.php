b67bb856836a99b150136044880fa1ceb25a6d65
Noa wmde <noa.rave@wikimedia.de>
2021-11-18 10:22:23 +0100

    Introduce WikiPageItemOrderProvider
    Its purpose is to provide an ordered list of
    Items based on the contents of a Wiki page.
    This was introduce to support the ordering
    of grammatical features in Lexeme based on
    MediaWiki:WikibaseLexeme-SortedGrammaticalFeatures (to
    be created and curated by the community).
    
    Bug: T232557
    Change-Id: Ie8a4a76c7fdcca019424edab96f6877e38c892b4

---
+++ lib/tests/phpunit/Store/WikiPageItemOrderProviderTest.php
@@ -0,0 +1,60 @@
+<?php
+declare( strict_types=1 );
+
+namespace Wikibase\Lib\Tests\Store;
+
+use MediaWikiIntegrationTestCase;
+use Title;
+use Wikibase\Lib\Store\WikiPageItemOrderProvider;
+use WikiPage;
+use WikitextContent;
+
+/**
+ * @covers \Wikibase\Lib\Store\WikiPageItemOrderProvider
+ *
+ * @group Wikibase
+ * @group Database
+ *
+ * @license GPL-2.0-or-later
+ * @author Noa Rave
+ */
+class WikiPageItemOrderProviderTest extends MediaWikiIntegrationTestCase {
+
+	protected function setUp(): void {
+		parent::setUp();
+		$this->tablesUsed[] = 'page';
+	}
+
+	public function provideGetItemOrder(): iterable {
+		return WikiPageItemOrderProviderTestHelper::provideGetItemOrder();
+	}
+
+	/**
+	 * @dataProvider provideGetItemOrder
+	 */
+	public function testGetItemOrder( string $text, ?array $expected ): void {
+		$this->makeWikiPage( 'MediaWiki:WikibaseLexeme-SortedGrammaticalFeaturesTest', $text );
+		$instance = new WikiPageItemOrderProvider( Title::newFromTextThrow( 'MediaWiki:WikibaseLexeme-SortedGrammaticalFeaturesTest' ) );
+		$itemOrder = $instance->getItemOrder();
+		$this->assertSame( $expected, $itemOrder );
+	}
+
+	private function makeWikiPage( string $name, string $text ): void {
+		$title = Title::newFromTextThrow( $name );
+		$wikiPage = WikiPage::factory( $title );
+		$wikiPage->doUserEditContent(
+			new WikitextContent( $text ),
+			$this->getTestUser()->getUser(),
+			'test'
+		);
+	}
+
+	public function testGetItemOrder_pageDoesNotExist(): void {
+		$instance = new WikiPageItemOrderProvider(
+			Title::newFromTextThrow( 'MediaWiki:WikibaseLexeme-SortedGrammaticalFeatures-Test-DoesNotExist' )
+		);
+		$itemOrder = $instance->getItemOrder();
+		$this->assertSame( null, $itemOrder );
+	}
+
+}
---
+++ lib/tests/phpunit/Store/WikiPageItemOrderProviderTestHelper.php
@@ -0,0 +1,104 @@
+<?php
+declare( strict_types=1 );
+
+namespace Wikibase\Lib\Tests\Store;
+
+/**
+ * Helper class for tests for WikiPageItemOrderProvider subclasses.
+ *
+ * @license GPL-2.0-or-later
+ * @author Noa Rave
+ */
+class WikiPageItemOrderProviderTestHelper {
+
+	public static function provideGetItemOrder(): iterable {
+		return [
+			'empty page' => [
+				'',
+				[]
+			],
+			'syntax that is not accepted' => [
+				"*\nQ1\n"
+				. "* Q2Q\n"
+				. " # Q3\n"
+				. " * Q4\n"
+				. "* Item:Q5\n"
+				. "* [[d:Project:Q6]]\n"
+				. "* d:Q7\n"
+				. "* {{q|8}}\n"
+				. "* {{q|P11}}",
+				[]
+			],
+			'simple match' => [
+				"* Q1 \n"
+				. "*Q133 \n"
+				. "* q5", // Testing for lower case item IDs
+				[ 'Q1' => 0, 'Q133' => 1, 'Q5' => 2 ]
+			],
+			'multiple bullets' => [
+				"* Q1 \n"
+				. "** Q2 \n",
+				[ 'Q1' => 0, 'Q2' => 1 ]
+			],
+			'ordered list' => [
+				"# Q1 \n"
+				. "# Q2 \n",
+				[ 'Q1' => 0, 'Q2' => 1 ]
+			],
+			'ordered with multiple pounds' => [
+				"# Q1 \n"
+				. "## Q2 \n",
+				[ 'Q1' => 0, 'Q2' => 1 ]
+			],
+			'mixed bullets and pounds' => [
+				"# Q1 \n"
+				. "#* Q2 \n",
+				[ 'Q1' => 0, 'Q2' => 1 ]
+			],
+			'strip multiline comment' => [
+				"* Q1 \n"
+				. "<!-- * Q133 \n"
+				. "* Q5 -->",
+				[ 'Q1' => 0 ]
+			],
+			'muliple comments' => [
+				"* Q1 \n"
+				. "<!-- * Q133 --> \n"
+				. "* <!-- Q5 -->",
+				[ 'Q1' => 0 ]
+			],
+			'bullet point glibberish' => [
+				"* Q1 \n"
+				. "* Q133 \n"
+				. "* Q5 Unicorns are all \n"
+				. "*  very beautiful!\n"
+				. "** This is a subheading",
+				[ 'Q1' => 0, 'Q133' => 1, 'Q5' => 2 ]
+			],
+			'additional text' => [
+				"* Q1 \n"
+				. "* Q133 \n"
+				. "* Q5 Unicorns are all \n"
+				. "very beautiful!",
+				[ 'Q1' => 0, 'Q133' => 1, 'Q5' => 2 ]
+			],
+			'wiki links' => [
+				"*\t[[Item:Q9]]\n"
+				. "* [[Item:Q8|Q1008]]\n"
+				. "* [[d:Item:Q7]]\n"
+				. "* [[P6|Q1006]]",
+				[ 'Q9' => 0, 'Q8' => 1, 'Q7' => 2 ]
+			],
+			'templates' => [
+				"* {{NiceTemplate|q7}}\n"
+				. "* {{NiceTemplate|q8}}\n"
+				. "* {{|Q9}}\n"
+				. "* {{Q|10}}\n"
+				. "* {{Q11}}\n"
+				. "* {{C0nfusingT3mpl4te|Q12}}\n",
+				[ 'Q7' => 0, 'Q8' => 1 ]
+			],
+		];
+	}
+
+}
