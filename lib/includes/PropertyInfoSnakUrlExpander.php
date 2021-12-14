3087b6054c41db1f5767d00cede186424e92647f
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2021-12-14 11:36:56 +0100

    SECURITY: Ignore formatter URLs with bad protocol
    Only use the formatter URL if it has one of three, known-good protocols:
    HTTPS, HTTP, or telephone number.
    
    Bug: T297570
    Change-Id: I37ece1dfdc80d38055067c9c4fa73ba591acd8bd
    Signed-off-by: toan <tobias.andersson@wikimedia.de>
    (cherry picked from commit c3a38b3bbd6973798a04928af64a8eb778cd1d45)

--- lib/includes/PropertyInfoSnakUrlExpander.php
+++ lib/includes/PropertyInfoSnakUrlExpander.php
@@ -17,6 +17,15 @@ use Wikimedia\Assert\Assert;
 class PropertyInfoSnakUrlExpander implements SnakUrlExpander {
 
 	/**
+	 * URL protocols (schemes) that are allowed in the pattern.
+	 */
+	private const ALLOWED_PROTOCOLS = [
+		'https' => null,
+		'http' => null,
+		'tel' => null,
+	];
+
+	/**
 	 * @var PropertyInfoProvider
 	 */
 	private $infoProvider;
@@ -50,8 +59,14 @@ class PropertyInfoSnakUrlExpander implements SnakUrlExpander {
 			'/',
 			rawurlencode( $value->getValue() )
 		);
+		$url = str_replace( '$1', $id, $pattern );
 
-		return str_replace( '$1', $id, $pattern );
+		$parsedUrl = wfParseUrl( $url );
+		if ( !$parsedUrl || !array_key_exists( $parsedUrl['scheme'], self::ALLOWED_PROTOCOLS ) ) {
+			return null;
+		}
+
+		return $url;
 	}
 
 }
