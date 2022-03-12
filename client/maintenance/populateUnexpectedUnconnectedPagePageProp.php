8f6cabd23328b5e8bfc699a31fa3003530a79b20
Marius Hoch <hoo@online.de>
2022-03-12 14:23:51 +0100

    Add populateUnexpectedUnconnectedPagePageProp maintenance script
    Fairly simple wrapper for UnexpectedUnconnectedPagePrimer.
    
    Change-Id: Ib1c6f3d2b9419e51d75f88506d3252157ee3bb4a

---
+++ client/maintenance/populateUnexpectedUnconnectedPagePageProp.php
@@ -0,0 +1,87 @@
+<?php
+
+declare( strict_types=1 );
+
+namespace Wikibase\Client;
+
+use Maintenance;
+use Onoi\MessageReporter\CallbackMessageReporter;
+use Wikibase\Client\Store\Sql\UnexpectedUnconnectedPagePrimer;
+use Wikibase\Lib\WikibaseSettings;
+
+$basePath = getenv( 'MW_INSTALL_PATH' ) !== false
+	? getenv( 'MW_INSTALL_PATH' )
+	: __DIR__ . '/../../../..';
+
+require_once $basePath . '/maintenance/Maintenance.php';
+
+/**
+ * Maintenance script for populating the 'unexpectedUnconnectedPage' page property.
+ *
+ * @license GPL-2.0-or-later
+ * @author Marius Hoch <mail@mariushoch.de>
+ */
+class PopulateUnexpectedUnconnectedPagePageProp extends Maintenance {
+
+	public function __construct() {
+		parent::__construct();
+
+		$this->addDescription( 'Populate the "unexpectedUnconnectedPage" page property.' );
+
+		$this->addOption( 'batch-size', "Number of rows to update per batch (1000 by default)", false, true );
+		$this->addOption(
+			'first-page-id',
+			'First page id to process, use 1 to start with the first page. ' .
+			'Use --last-page-id + 1 to continue a previous run.',
+			false,
+			true
+		);
+		$this->addOption(
+			'last-page-id',
+			'Page id of the last page to process.',
+			false,
+			true
+		);
+	}
+
+	public function execute(): bool {
+		if ( !WikibaseSettings::isClientEnabled() ) {
+			$this->output( "You need to have WikibaseClient enabled in order to use this maintenance script!\n\n" );
+			exit;
+		}
+
+		$reporter = new CallbackMessageReporter( [ $this, 'report' ] );
+
+		$batchSize = (int)$this->getOption( 'batch-size', 1000 );
+		$primer = new UnexpectedUnconnectedPagePrimer(
+			WikibaseClient::getClientDomainDbFactory()->newLocalDb(),
+			WikibaseClient::getNamespaceChecker(),
+			$batchSize
+		);
+		$primer->setProgressReporter( $reporter );
+
+		$firstPageId = $this->getOption( 'first-page-id' );
+		if ( $firstPageId ) {
+			$primer->setMinPageId( intval( $firstPageId ) );
+		}
+		$lastPageId = $this->getOption( 'last-page-id' );
+		if ( $lastPageId ) {
+			$primer->setMaxPageId( intval( $lastPageId ) );
+		}
+
+		$primer->insertPageProp();
+
+		return true;
+	}
+
+	/**
+	 * Outputs a message via the output() method.
+	 */
+	public function report( string $msg ): void {
+		$this->output( "$msg\n" );
+	}
+
+}
+
+$maintClass = PopulateUnexpectedUnconnectedPagePageProp::class;
+require_once RUN_MAINTENANCE_IF_MAIN;
---
+++ client/tests/phpunit/integration/maintenance/PopulateUnexpectedUnconnectedPagePagePropTest.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace Wikibase\Client\Tests\Maintenance;
+
+use MediaWiki\Tests\Maintenance\MaintenanceBaseTestCase;
+use Title;
+use Wikibase\Client\NamespaceChecker;
+use Wikibase\Client\PopulateUnexpectedUnconnectedPagePageProp;
+use Wikimedia\Rdbms\IDatabase;
+
+// files in maintenance/ are not autoloaded, so load explicitly
+require_once __DIR__ . '/../../../../maintenance/populateUnexpectedUnconnectedPagePageProp.php';
+
+/**
+ * @covers \Wikibase\Client\PopulateUnexpectedUnconnectedPagePageProp
+ *
+ * @group Wikibase
+ * @group Database
+ *
+ * @license GPL-2.0-or-later
+ * @author Marius Hoch <mail@mariushoch.de>
+ */
+class PopulateUnexpectedUnconnectedPagePagePropTest extends MaintenanceBaseTestCase {
+
+	protected function getMaintenanceClass() {
+		return PopulateUnexpectedUnconnectedPagePageProp::class;
+	}
+
+	protected function setUp(): void {
+		parent::setUp();
+
+		$this->overrideMwServices(
+			null,
+			[
+				'WikibaseClient.NamespaceChecker' => function() {
+					return new NamespaceChecker( [], [ $this->getDefaultWikitextNS() ] );
+				},
+			]
+		);
+
+		$this->tablesUsed[] = 'page_props';
+	}
+
+	public function addDBDataOnce() {
+		// Remove old stray pages.
+		$this->db->delete( 'page', IDatabase::ALL_ROWS, __METHOD__ );
+
+		$titles = [];
+		for ( $i = 1; $i < 5; $i++ ) {
+			$titles[$i] = Title::newFromTextThrow( "PopulateUnexpectedUnconnectedPagePagePropTest-$i", $this->getDefaultWikitextNS() );
+		}
+		$titles[] = Title::newFromTextThrow( "Page outside of a Wikibase NS", NS_TALK );
+		$titles[101] = Title::newFromTextThrow(
+			"PopulateUnexpectedUnconnectedPagePagePropTest-High-Page-id",
+			$this->getDefaultWikitextNS()
+		);
+		foreach ( $titles as $pageId => $title ) {
+			$page = $this->getServiceContainer()->getWikiPageFactory()->newFromTitle( $title );
+			$page->insertOn( $this->db, $pageId );
+		}
+	}
+
+	public function testExecute(): void {
+		$namespaceString = strval( $this->getDefaultWikitextNS() );
+		$namespaceFloat = $this->getDefaultWikitextNS() + 0.0;
+
+		$prior = [
+			[ '3', 'expectedUnconnectedPage', '', 0.0 ],
+			[ '2', 'unexpectedUnconnectedPage', $namespaceString, $namespaceFloat ],
+			[ '4', 'unrelated-page-prop', '', 0.0 ],
+		];
+		$expected = [
+			[ '1', 'unexpectedUnconnectedPage', $namespaceString, $namespaceFloat ],
+			[ '2', 'unexpectedUnconnectedPage', $namespaceString, $namespaceFloat ],
+			[ '3', 'expectedUnconnectedPage', '', 0.0 ],
+			[ '4', 'unexpectedUnconnectedPage', $namespaceString, $namespaceFloat ],
+			[ '4', 'unrelated-page-prop', '', 0.0 ],
+			[ '101', 'unexpectedUnconnectedPage', $namespaceString, $namespaceFloat ],
+		];
+		$this->insertPageProps( $prior );
+
+		$this->maintenance->loadWithArgv( [] );
+		$this->maintenance->execute();
+
+		$this->assertSelect(
+			'page_props',
+			[ 'pp_page', 'pp_propname', 'pp_value', 'pp_sortkey' ],
+			IDatabase::ALL_ROWS,
+			$expected
+		);
+	}
+
+	public function testExecute_paging(): void {
+		$namespaceInt = $this->getDefaultWikitextNS();
+		$namespaceString = strval( $namespaceInt );
+		$namespaceFloat = $namespaceInt + 0.0;
+
+		$this->insertPageProps( [
+			[ 1, 'expectedUnconnectedPage', '', 0.0 ],
+			// 2 is unexpected unconnected
+			[ 3, 'wikibase_item', '', 0.0 ],
+			// 4 is unexpected unconnected
+			// 101 is unexpected unconnected
+		] );
+
+		$argv = [ '--batch-size', 1, '--first-page-id', 3, '--last-page-id', 4 ];
+
+		$this->maintenance->loadWithArgv( $argv );
+		$this->maintenance->execute();
+		$this->assertSelect(
+			'page_props',
+			[ 'pp_page', 'pp_propname', 'pp_value', 'pp_sortkey' ],
+			IDatabase::ALL_ROWS,
+			[
+				[ 1, 'expectedUnconnectedPage', '', 0.0 ],
+				// 2 is excluded
+				[ 3, 'wikibase_item', '', 0.0 ],
+				[ 4, 'unexpectedUnconnectedPage', $namespaceString, $namespaceFloat ],
+				// 101 is excluded
+			]
+		);
+	}
+
+	/**
+	 * Insert given page props
+	 *
+	 * @param array[] $pageProps Array of 'pp_page', 'pp_propname', 'pp_value', 'pp_sortkey'
+	 */
+	private function insertPageProps( array $pageProps ): void {
+		$this->db->delete( 'page_props', IDatabase::ALL_ROWS, __METHOD__ );
+
+		$toInsert = [];
+		foreach ( $pageProps as $pageProp ) {
+			$toInsert[] = [
+				'pp_page' => $pageProp[0],
+				'pp_propname' => $pageProp[1],
+				'pp_value' => $pageProp[2],
+				'pp_sortkey' => $pageProp[3],
+			];
+		}
+
+		$this->db->insert( 'page_props', $toInsert, __METHOD__ );
+	}
+
+}
