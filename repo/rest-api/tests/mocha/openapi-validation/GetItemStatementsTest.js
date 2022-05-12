e9354da39dec3df77085f1a07eb60d45e62bd374
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2022-05-12 17:34:13 +0200

    REST: Add schema tests for GetItemStatements
    Bug: T307200
    Change-Id: Ibc8b64e175d3960c033261285a7177733b822e2c

---
+++ repo/rest-api/tests/mocha/openapi-validation/GetItemStatementsTest.js
@@ -0,0 +1,62 @@
+'use strict';
+
+// allow chai expectations
+/* eslint-disable no-unused-expressions */
+const { REST } = require( 'api-testing' );
+const SwaggerParser = require( '@apidevtools/swagger-parser' );
+const chai = require( 'chai' );
+const { createEntity, createSingleItem, createRedirectForItem } = require( '../helpers/entityHelper' );
+const expect = chai.expect;
+const chaiResponseValidator = require( 'chai-openapi-response-validator' ).default;
+const basePath = 'rest.php/wikibase/v0';
+const rest = new REST( basePath );
+
+describe( 'validate GET /entities/items/{id}/statements responses against OpenAPI spec', () => {
+	before( async () => {
+		const spec = await SwaggerParser.dereference( './specs/openapi.json' );
+		// dynamically add CI test system to the spec
+		spec.servers = [ { url: rest.req.app + basePath } ];
+		chai.use( chaiResponseValidator( spec ) );
+	} );
+
+	it( '200 OK response is valid for an Item with no statements', async () => {
+		const createEmptyItemResponse = await createEntity( 'item', {} );
+		const response = await rest.get( `/entities/items/${createEmptyItemResponse.entity.id}/statements` );
+
+		expect( response.status ).to.equal( 200 );
+		expect( response ).to.satisfyApiSpec;
+	} );
+
+	it( '200 OK response is valid for an Item with statements', async () => {
+		const createSingleItemResponse = await createSingleItem();
+		const response = await rest.get( `/entities/items/${createSingleItemResponse.entity.id}/statements` );
+
+		expect( response.status ).to.equal( 200 );
+		expect( response ).to.satisfyApiSpec;
+	} );
+
+	it( '308 Permanent Redirect response is valid for a redirected item', async () => {
+		const redirectTargetId = ( await createEntity( 'item', {} ) ).entity.id;
+		const redirectSourceId = await createRedirectForItem( redirectTargetId );
+
+		const response = await rest.get( `/entities/items/${redirectSourceId}/statements` );
+
+		expect( response.status ).to.equal( 308 );
+		expect( response ).to.satisfyApiSpec;
+	} );
+
+	it( '400 Bad Request response is valid for an invalid item ID', async () => {
+		const response = await rest.get( '/entities/items/X123/statements' );
+
+		expect( response.status ).to.equal( 400 );
+		expect( response ).to.satisfyApiSpec;
+	} );
+
+	it( '404 Not Found response is valid for a non-existing item', async () => {
+		const response = await rest.get( '/entities/items/Q99999/statements' );
+
+		expect( response.status ).to.equal( 404 );
+		expect( response ).to.satisfyApiSpec;
+	} );
+
+} );
