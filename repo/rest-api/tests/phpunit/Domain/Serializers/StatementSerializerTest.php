1aaee843da26b74c9eed25165e551742dfda2d9d
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2022-05-30 15:48:36 +0200

    REST: Add StatementSerializer with required fields
    Unlike the "legacy" StatementSerializer this one always returns the
    required fields (qualifiers, references, qualifiers-order) even if they
    are empty.
    
    Bug: T309519
    Change-Id: I5005cd699d3c0512cb08882a55754821818eb5c0

---
+++ repo/rest-api/tests/phpunit/Domain/Serializers/StatementSerializerTest.php
@@ -0,0 +1,69 @@
+<?php declare( strict_types=1 );
+
+namespace Wikibase\Repo\Tests\RestApi\Domain\Serializers;
+
+use PHPUnit\Framework\TestCase;
+use Wikibase\DataModel\Entity\NumericPropertyId;
+use Wikibase\DataModel\Reference;
+use Wikibase\DataModel\ReferenceList;
+use Wikibase\DataModel\Snak\PropertyNoValueSnak;
+use Wikibase\Repo\RestApi\Domain\Serializers\StatementSerializer;
+use Wikibase\Repo\RestApi\WbRestApi;
+use Wikibase\Repo\Tests\NewStatement;
+
+/**
+ * @covers \Wikibase\Repo\RestApi\Domain\Serializers\StatementSerializer
+ *
+ * @group Wikibase
+ *
+ * @license GPL-2.0-or-later
+ */
+class StatementSerializerTest extends TestCase {
+
+	public function testMinimalStatementContainsRequiredFields(): void {
+		$statement = NewStatement::noValueFor( 'P123' )
+			->withGuid( 'Q123$AAAAAAAA-BBBB-CCCC-DDDD-EEEEEEEEEEEE' )
+			->build();
+
+		$serialization = $this->newSerializer()->serialize( $statement );
+
+		$requiredFieldsWithExpectedValues = [
+			'id' => $statement->getGuid(),
+			'mainsnak' => [
+				'snaktype' => 'novalue',
+				'property' => $statement->getMainSnak()->getPropertyId(),
+				'hash' => $statement->getMainSnak()->getHash(),
+			],
+			'rank' => 'normal',
+			'qualifiers' => (object)[],
+			'qualifiers-order' => [],
+			'references' => [],
+		];
+
+		foreach ( $requiredFieldsWithExpectedValues as $field => $value ) {
+			$this->assertArrayHasKey( $field, $serialization );
+			$this->assertEquals( $value, $serialization[$field] );
+		}
+	}
+
+	public function testEmptyFieldDefaultsGetOverridden(): void {
+		$qualifierPropertyId = 'P321';
+		$statement = NewStatement::noValueFor( 'P123' )
+			->withQualifier( $qualifierPropertyId, 'Q666' )
+			->build();
+		$statement->setReferences( new ReferenceList( [
+			new Reference( [ new PropertyNoValueSnak( new NumericPropertyId( 'P123' ) ) ] )
+		] ) );
+
+		$serialization = $this->newSerializer()->serialize( $statement );
+
+		$this->assertObjectHasAttribute( $qualifierPropertyId, $serialization['qualifiers'] );
+		$this->assertNotEmpty( $serialization['qualifiers-order'] );
+		$this->assertNotEmpty( $serialization['references'] );
+	}
+
+	private function newSerializer(): StatementSerializer {
+		return new StatementSerializer( WbRestApi::getBaseDataModelSerializerFactory()->newStatementSerializer() );
+	}
+
+}
