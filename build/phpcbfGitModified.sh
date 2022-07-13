2190286c036e3f7467b0d37a8116871ed3d96ae3
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2022-07-13 13:21:52 +0200

    Check staged files in phpcs-modified scripts
    This previously only checked unstaged files.
    
    Change-Id: I4fe50f79aaf9858e28027f0877df82726c3fbd83

--- build/phpcbfGitModified.sh
+++ build/phpcbfGitModified.sh
@@ -2,10 +2,16 @@
 # Runs phpcbf across all PHP files that have been modified according to git.
 
 phpcbf --file-list=<(
+    # list unstaged modified files
     git ls-files \
         --modified \
         --others \
         --exclude-standard \
         '*.php'
-    echo /dev/null # prevent empty file list
+
+    # list staged modified files
+    git diff --name-only --cached
+
+    # prevent empty file list
+    echo /dev/null
 )
