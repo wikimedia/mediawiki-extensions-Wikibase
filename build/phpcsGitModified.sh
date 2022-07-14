4b70c23bbcd7e4a10965b53940aca7aea805dcb5
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2022-07-14 14:20:10 +0200

    Fix phpcs-modified scripts
    I4fe50f79aaf9858e28027f0877df82726c3fbd83 introduced an issue that
    caused them to fail when there were staged removed files. This issue
    was already present in the original version of the script, but only
    for unstaged deletions which may not be as common.
    
    phpcs-modified and fix-modified now no longer run for untracked files,
    which people didn't seem to care about anyway.
    
    Change-Id: Icf391999186f47580799cec210b5b8d400320260

--- build/phpcsGitModified.sh
+++ build/phpcsGitModified.sh
@@ -2,15 +2,8 @@
 # Runs phpcs across all PHP files that have been modified according to git.
 
 phpcs -p -s --file-list=<(
-    # list unstaged modified files
-    git ls-files \
-        --modified \
-        --others \
-        --exclude-standard \
-        '*.php'
-
-    # list staged modified files
-    git diff --name-only --cached
+    # list staged and unstaged modified files
+    git diff --name-only --diff-filter=d HEAD -- '*.php'
 
     # prevent empty file list
     echo /dev/null
