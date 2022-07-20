77c5d34e01389c2eea8ad38c5871f69f99bc5d2a
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2022-07-20 17:31:26 +0200

    REST: Create ReplaceStatementRouteHandler
    Bug: T312969
    Change-Id: I5d2014a70cd2904c73858b191c4edacb840fffaf

---
+++ repo/rest-api/tests/phpunit/RouteHandlers/ReplaceStatementRouteHandlerTest.php
@@ -0,0 +1,69 @@
+<?php declare( strict_types=1 );
+
+namespace Wikibase\Repo\Tests\RestApi\RouteHandlers;
+
+use MediaWiki\Rest\Handler;
+use MediaWiki\Rest\RequestData;
+use MediaWiki\Tests\Rest\Handler\HandlerTestTrait;
+use MediaWikiIntegrationTestCase;
+use Wikibase\Repo\RestApi\RouteHandlers\ReplaceStatementRouteHandler;
+use Wikibase\Repo\RestApi\UseCases\ErrorResponse;
+use Wikibase\Repo\RestApi\UseCases\ReplaceItemStatement\ReplaceItemStatement;
+
+/**
+ * @covers \Wikibase\Repo\RestApi\RouteHandlers\ReplaceStatementRouteHandler
+ *
+ * @group Wikibase
+ *
+ * @license GPL-2.0-or-later
+ */
+class ReplaceStatementRouteHandlerTest extends MediaWikiIntegrationTestCase {
+
+	use HandlerTestTrait;
+
+	public function testHandlesUnexpectedErrors(): void {
+		$useCase = $this->createStub( ReplaceItemStatement::class );
+		$useCase->method( 'execute' )->willThrowException( new \RuntimeException() );
+		$this->setService( 'WbRestApi.ReplaceItemStatement', $useCase );
+
+		$routeHandler = $this->newRouteHandlerWithValidRequest();
+		$this->validateHandler( $routeHandler );
+
+		$response = $routeHandler->execute();
+		$responseBody = json_decode( $response->getBody()->getContents() );
+		$this->assertSame( [ 'en' ], $response->getHeader( 'Content-Language' ) );
+		$this->assertSame( ErrorResponse::UNEXPECTED_ERROR, $responseBody->code );
+	}
+
+	public function testReadWriteAccess(): void {
+		$routeHandler = $this->newRouteHandlerWithValidRequest();
+
+		$this->assertTrue( $routeHandler->needsReadAccess() );
+		$this->assertTrue( $routeHandler->needsWriteAccess() );
+	}
+
+	private function newRouteHandlerWithValidRequest(): Handler {
+		$routeHandler = ReplaceStatementRouteHandler::factory();
+		$this->initHandler(
+			$routeHandler,
+			new RequestData( [
+					'method' => 'PUT',
+					'headers' => [ 'Content-Type' => 'application/json' ],
+					'pathParams' => [
+						ReplaceStatementRouteHandler::STATEMENT_ID_PATH_PARAM => 'Q1$1e63e3d9-4bd4-7671-706c-b745db23c3f1',
+					],
+					'bodyContents' => json_encode( [
+						'statement' => [
+							'type' => 'statement',
+							'mainsnak' => [
+								'snaktype' => 'novalue',
+								'property' => 'P1',
+							],
+						],
+					] )
+				]
+			)
+		);
+		return $routeHandler;
+	}
+}
