8cc15c7ee38e2c25c65f805daa60efe027270fed
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2022-08-02 18:57:28 +0200

    Add tests for sitelink to redirect behavior
    Test the different scenarios of this feature using the API. In order to
    let the tests control the configured badge items, add the two relevant
    settings to Wikibase.ci.php, where they add extra badge items (on top of
    whatever the user configured before) based on the request headers. Users
    who want to run the tests locally should include Wikibase.ci.php towards
    the end of their LocalSettings.php. (This also requires the change from
    require to require_once near the top of the file.)
    
    Bug: T314417
    Depends-On: I9b4c3f3cd65e63da8f7b3f089b05921f01ac6cb3
    Change-Id: I7f5bd94087a6528ba6a479921708de8af6157057

---
+++ tests/api-testing/SiteLinkRedirectBadgeTest.js
@@ -0,0 +1,244 @@
+'use strict';
+
+const { assert, action, utils } = require( 'api-testing' );
+const { requireExtensions } = require( './utils.js' );
+
+describe( 'sitelink redirect behavior with badges', () => {
+	let alice;
+	let siteId;
+	let goodArticleBadgeId;
+	let sitelinkToRedirectBadgeId;
+	let intentionalSitelinkToRedirectBadgeId;
+
+	before( 'require extensions', requireExtensions( [
+		'WikibaseRepository',
+		'WikibaseClient',
+	] ) );
+
+	before( 'set up user', async () => {
+		alice = await action.alice();
+	} );
+
+	before( 'get site ID', async () => {
+		siteId = ( await action.getAnon().meta(
+			'wikibase',
+			{ wbprop: 'siteid' },
+		) ).siteid;
+	} );
+
+	/**
+	 * Create an item with the given label and data.
+	 *
+	 * @param {string} enLabel The English label.
+	 * A unique suffix is automatically appended.
+	 * @param {Object} [data] Any other item data.
+	 * @return {Promise<string>} The item ID.
+	 */
+	async function createItem( enLabel, data = {} ) {
+		if ( !data.labels ) {
+			data.labels = {};
+		}
+		data.labels.en = { language: 'en', value: `${enLabel}-${utils.uniq()}` };
+
+		const response = await alice.action( 'wbeditentity', {
+			new: 'item',
+			token: await alice.token( 'csrf' ),
+			data: JSON.stringify( data ),
+		}, 'POST' );
+		return response.entity.id;
+	}
+
+	before( 'create badge items', async () => {
+		goodArticleBadgeId = await createItem( 'good article' );
+		sitelinkToRedirectBadgeId = await createItem( 'sitelink to redirect' );
+		intentionalSitelinkToRedirectBadgeId = await createItem( 'intentional sitelink to redirect' );
+
+		// set request headers for all requests; see repo/config/Wikibase.ci.php
+		alice.req.set( 'X-Wikibase-CI-Badges',
+			[ goodArticleBadgeId, sitelinkToRedirectBadgeId, intentionalSitelinkToRedirectBadgeId ]
+				.join( ', ' ) );
+		alice.req.set( 'X-Wikibase-CI-Redirect-Badges',
+			[ sitelinkToRedirectBadgeId, intentionalSitelinkToRedirectBadgeId ]
+				.join( ', ' ) );
+	} );
+
+	/**
+	 * Create a regular page that can be used as the target of a redirect.
+	 *
+	 * @param {string} title The page title.
+	 * A unique suffix is automatically appended,
+	 * and the page is always a subpage of the Alice user page.
+	 * @return {Promise<string>} The full title.
+	 */
+	async function createRedirectTarget( title ) {
+		const fullTitle = `User:${alice.username}/${title}-${utils.uniq()}`;
+		const response = await alice.edit( fullTitle, {
+			text: 'Redirect target for SiteLinkRedirectBadgeTest',
+			createonly: '1',
+		} );
+		return response.title;
+	}
+
+	/**
+	 * Create a redirect to the given target page.
+	 *
+	 * @param {string} title The redirect page title.
+	 * A unique suffix is automatically appended,
+	 * and the page is always a subpage of the Alice user page.
+	 * @param {string} target The full title of the target page,
+	 * typically returned by {@link createRedirectTarget}.
+	 * @return {Promise<string>} The full title.
+	 */
+	async function createRedirect( title, target ) {
+		const fullTitle = `User:${alice.username}/${title}-${utils.uniq()}`;
+		const response = await alice.edit( fullTitle, {
+			text: `#REDIRECT [[${target}]]`,
+			createonly: '1',
+		} );
+		return response.title;
+	}
+
+	// Redirects are NOT allowed as separate sitelinks
+	// if NO redirect badge is added in the same edit
+	it( 'disallows redirect sitelink without redirect badge', async () => {
+		const redirectTarget = await createRedirectTarget( 'target' ),
+			redirect = await createRedirect( 'redirect', redirectTarget ),
+			item = await createItem( 'item where we try to add a sitelink to a redirect' ),
+			itemLinkedToTarget = await createItem( 'item linked to redirect target', {
+				sitelinks: {
+					[ siteId ]: {
+						site: siteId,
+						title: redirectTarget,
+						badges: [],
+					},
+				},
+			} );
+
+		const error = await alice.actionError( 'wbsetsitelink', {
+			id: item,
+			linksite: siteId,
+			linktitle: redirect,
+			badges: goodArticleBadgeId,
+			token: await alice.token( 'csrf' ),
+		}, 'POST' );
+
+		assert.strictEqual( error.code, 'failed-save' );
+		const sitelinkConflict = error.messages.find( ( message ) =>
+			message.name === 'wikibase-validator-sitelink-conflict' );
+		assert.isNotNull( sitelinkConflict );
+		assert.include( sitelinkConflict.parameters[ 1 ], itemLinkedToTarget );
+	} );
+
+	// Redirects are allowed as separate sitelinks
+	// if a redirect badge is added in the same edit
+	it( 'allows redirect sitelink with redirect badge', async () => {
+		const redirectTarget = await createRedirectTarget( 'target' ),
+			redirect = await createRedirect( 'redirect', redirectTarget ),
+			item = await createItem( 'item where we add a sitelink to a redirect' );
+
+		const response = await alice.action( 'wbsetsitelink', {
+			id: item,
+			linksite: siteId,
+			linktitle: redirect,
+			badges: sitelinkToRedirectBadgeId,
+			token: await alice.token( 'csrf' ),
+		}, 'POST' );
+		assert.strictEqual( response.entity.sitelinks[ siteId ].title, redirect );
+	} );
+
+	// Redirect badges can be added to existing sitelinks even if they are redirects
+	it( 'allows adding redirect badge to redirect sitelink', async () => {
+		const redirectTarget = await createRedirectTarget( 'target' ),
+			redirect = await createRedirectTarget( 'redirect' ), // not yet a redirect
+			item = await createItem( 'item with sitelink to redirect', {
+				sitelinks: {
+					[ siteId ]: {
+						site: siteId,
+						title: redirect,
+						badges: [],
+					},
+				},
+			} );
+
+		await alice.edit( redirect, {
+			text: `#REDIRECT [[${redirectTarget}]]`,
+		} );
+
+		const response = await alice.action( 'wbsetsitelink', {
+			id: item,
+			linksite: siteId,
+			linktitle: redirect,
+			badges: sitelinkToRedirectBadgeId,
+			token: await alice.token( 'csrf' ),
+		}, 'POST' );
+		assert.strictEqual( response.entity.sitelinks[ siteId ].title, redirect );
+		assert.deepEqual( response.entity.sitelinks[ siteId ].badges,
+			[ sitelinkToRedirectBadgeId ] );
+	} );
+
+	// Redirect badges can NOT be removed from sitelinks
+	// if the redirected target is used as a sitelink for a different Item
+	it( 'disallows removing redirect badge from redirect sitelink', async () => {
+		const redirectTarget = await createRedirectTarget( 'target' ),
+			redirect = await createRedirect( 'redirect', redirectTarget ),
+			item = await createItem( 'item where we try to remove a redirect badge', {
+				sitelinks: {
+					[ siteId ]: {
+						site: siteId,
+						title: redirect,
+						badges: [ sitelinkToRedirectBadgeId ],
+					},
+				},
+			} ),
+			itemLinkedToTarget = await createItem( 'item linked to redirect target', {
+				sitelinks: {
+					[ siteId ]: {
+						site: siteId,
+						title: redirectTarget,
+						badges: [],
+					},
+				},
+			} );
+
+		const error = await alice.actionError( 'wbsetsitelink', {
+			id: item,
+			linksite: siteId,
+			linktitle: redirect,
+			badges: '',
+			token: await alice.token( 'csrf' ),
+		}, 'POST' );
+
+		assert.strictEqual( error.code, 'failed-save' );
+		const sitelinkConflict = error.messages.find( ( message ) =>
+			message.name === 'wikibase-validator-sitelink-conflict' );
+		assert.isNotNull( sitelinkConflict );
+		assert.include( sitelinkConflict.parameters[ 1 ], itemLinkedToTarget );
+	} );
+
+	// A redirect badge can always be switched with another redirect badge in the same edit
+	it( 'allows replacing redirect badge with another', async () => {
+		const redirectTarget = await createRedirectTarget( 'target' ),
+			redirect = await createRedirect( 'redirect', redirectTarget ),
+			item = await createItem( 'item where we replace a redirect badge', {
+				sitelinks: {
+					[ siteId ]: {
+						site: siteId,
+						title: redirect,
+						badges: [ sitelinkToRedirectBadgeId ],
+					},
+				},
+			} );
+
+		const response = await alice.action( 'wbsetsitelink', {
+			id: item,
+			linksite: siteId,
+			linktitle: redirect,
+			badges: intentionalSitelinkToRedirectBadgeId,
+			token: await alice.token( 'csrf' ),
+		}, 'POST' );
+		assert.strictEqual( response.entity.sitelinks[ siteId ].title, redirect );
+		assert.deepEqual( response.entity.sitelinks[ siteId ].badges,
+			[ intentionalSitelinkToRedirectBadgeId ] );
+	} );
+
+} );
