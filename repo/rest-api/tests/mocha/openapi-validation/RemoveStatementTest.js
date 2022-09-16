6daa69338a94855379b838a0ecf73530bd4e9d85
sihe <silvan.heintze@wikimedia.de>
2022-09-16 10:45:37 +0200

    REST: Combine e2e and schema tests for DELETE
    This combines both the e2e and schema tests for short and long DELETE
    routes into single files.
    
    Bug: T317777
    Change-Id: Iae201d8f9162aea361b0ac82e19d9951f97000ff

--- repo/rest-api/tests/mocha/openapi-validation/RemoveStatementTest.js
+++ repo/rest-api/tests/mocha/openapi-validation/RemoveStatementTest.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const chai = require( 'chai' );
-const { createSingleItem } = require( '../helpers/entityHelper' );
+const entityHelper = require( '../helpers/entityHelper' );
 const { RequestBuilder } = require( '../helpers/RequestBuilder' );
 const expect = chai.expect;
 
@@ -11,61 +11,97 @@ function newRemoveStatementRequestBuilder( statementId ) {
 		.withPathParam( 'statement_id', statementId );
 }
 
-describe( 'validate DELETE /entities/items/{item_id}/statements/{statement_id} responses', () => {
+function newRemoveItemStatementRequestBuilder( itemId, statementId ) {
+	return new RequestBuilder()
+		.withRoute( 'DELETE', '/entities/items/{item_id}/statements/{statement_id}' )
+		.withPathParam( 'item_id', itemId )
+		.withPathParam( 'statement_id', statementId );
+}
 
-	describe( 'create statements to delete', () => {
-		let newStatementId;
+describe( 'validate DELETE endpoints against OpenAPI definition', () => {
+	let testItemId;
+	let testPropertyId;
 
-		beforeEach( async () => {
-			const createSingleItemResponse = await createSingleItem();
-			newStatementId = Object.values( createSingleItemResponse.entity.claims )[ 0 ][ 0 ].id;
-		} );
+	before( async () => {
+		testItemId = ( await entityHelper.createEntity( 'item', {} ) ).entity.id;
+		testPropertyId = ( await entityHelper.createUniqueStringProperty() ).entity.id;
 
-		it( '200 OK response is valid', async () => {
-			const response = await newRemoveStatementRequestBuilder( newStatementId )
-				.makeRequest();
-
-			expect( response.status ).to.equal( 200 );
-			expect( response ).to.satisfyApiSpec;
-		} );
-
-		it( '412 Precondition Failed is valid for outdated revision id', async () => {
-			const response = await newRemoveStatementRequestBuilder( newStatementId )
-				.withHeader( 'If-Match', '"1"' )
-				.makeRequest();
-
-			expect( response.status ).to.equal( 412 );
-			expect( response ).to.satisfyApiSpec;
-		} );
 	} );
 
-	it( '400 Bad Request for invalid Statement ID', async () => {
-		const invalidStatementId = 'X123$AAAAAAAA-BBBB-CCCC-DDDD-EEEEEEEEEEEE';
-		const response = await newRemoveStatementRequestBuilder( invalidStatementId )
-			.makeRequest();
+	[
+		{
+			route: '/entities/items/{item_id}/statements/{statement_id}',
+			newRemoveRequestBuilder: ( statementId, patch ) =>
+				newRemoveItemStatementRequestBuilder( testItemId, statementId, patch )
+		},
+		{
+			route: '/statements/{statement_id}',
+			newRemoveRequestBuilder: newRemoveStatementRequestBuilder
+		}
+	].forEach( ( { route, newRemoveRequestBuilder } ) => {
+		describe( route, () => {
 
-		expect( response.status ).to.equal( 400 );
-		expect( response ).to.satisfyApiSpec;
-	} );
+			describe( 'DELETE on newly created statements', () => {
+				let testStatementId;
 
-	it( '404 Not Found response is valid for a non-existing statement', async () => {
-		const nonexistentStatement = 'Q1$AAAAAAAA-BBBB-CCCC-DDDD-EEEEEEEEEEEE';
+				beforeEach( async () => {
+					testStatementId = ( await new RequestBuilder()
+						.withRoute( 'POST', '/entities/items/{item_id}/statements' )
+						.withPathParam( 'item_id', testItemId )
+						.withHeader( 'content-type', 'application/json' )
+						.withJsonBodyParam(
+							'statement',
+							entityHelper.newStatementWithRandomStringValue( testPropertyId )
+						).makeRequest() ).body.id;
+				} );
 
-		const response = await newRemoveStatementRequestBuilder( nonexistentStatement )
-			.makeRequest();
+				it( '200 OK response is valid', async () => {
+					const response = await newRemoveRequestBuilder( testStatementId )
+						.makeRequest();
 
-		expect( response.status ).to.equal( 404 );
-		expect( response ).to.satisfyApiSpec;
-	} );
+					expect( response.status ).to.equal( 200 );
+					expect( response ).to.satisfyApiSpec;
+				} );
 
-	it( '415 Unsupported Media Type is valid for unknown Content-Type: headers', async () => {
-		const validStatementId = 'Q1$a787449b-4e36-a7fd-1842-0f38ee96cdb0';
-		const response = await newRemoveStatementRequestBuilder( validStatementId )
-			.withHeader( 'Content-Type', 'text/plain' )
-			.makeRequest();
+				it( '412 Precondition Failed is valid for outdated revision id', async () => {
+					const response = await newRemoveRequestBuilder( testStatementId )
+						.withHeader( 'If-Match', '"1"' )
+						.makeRequest();
 
-		expect( response.status ).to.equal( 415 );
-		expect( response ).to.satisfyApiSpec;
+					expect( response.status ).to.equal( 412 );
+					expect( response ).to.satisfyApiSpec;
+				} );
+			} );
+
+			it( '400 Bad Request for invalid Statement ID', async () => {
+				const invalidStatementId = 'X123$AAAAAAAA-BBBB-CCCC-DDDD-EEEEEEEEEEEE';
+				const response = await newRemoveRequestBuilder( invalidStatementId )
+					.makeRequest();
+
+				expect( response.status ).to.equal( 400 );
+				expect( response ).to.satisfyApiSpec;
+			} );
+
+			it( '404 Not Found response is valid for a non-existing statement', async () => {
+				const nonexistentStatementId = 'Q1$AAAAAAAA-BBBB-CCCC-DDDD-EEEEEEEEEEEE';
+
+				const response = await newRemoveRequestBuilder( nonexistentStatementId )
+					.makeRequest();
+
+				expect( response.status ).to.equal( 404 );
+				expect( response ).to.satisfyApiSpec;
+			} );
+
+			it( '415 Unsupported Media Type is valid for unknown Content-Type: headers', async () => {
+				const validStatementId = 'Q1$a787449b-4e36-a7fd-1842-0f38ee96cdb0';
+				const response = await newRemoveRequestBuilder( validStatementId )
+					.withHeader( 'Content-Type', 'text/plain' )
+					.makeRequest();
+
+				expect( response.status ).to.equal( 415 );
+				expect( response ).to.satisfyApiSpec;
+			} );
+		} );
 	} );
 
 } );
