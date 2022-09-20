377566cd9a3a89df97707ec3dd9c716535c8ec03
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2022-09-20 13:16:31 +0200

    Send language names to wikibase.getLanguageNameByCode()
    For wikis without ULS, this may be the first time we’re providing
    language names on the JS side at all: we can have the server-side code
    send them as a generated file of the ResourceLoader module.
    
    For wikis with ULS, the majority of the language names is already sent
    to the client as part of a ULS module. We don’t want to send this data
    twice, so we leave it out of the Wikibase data. The client-side code
    looks for language names in the ULS data, as before; however, it now
    only does that after first looking in the data we sent from Wikibase.
    
    getLanguageNameByCode() is effectively rewritten; the ULS logic is the
    same as before, but organized differently, so that you can very easily
    see the fallback chain in the main function: all the other functions
    have the same signature, taking a string language code and returning a
    string language name or null.
    
    This fixes the language name of 'mul', as well as many other language
    codes, in the classical / legacy termbox, as well as in other places
    using this function.
    
    Bug: T261851
    Bug: T311626
    Change-Id: Ic61b6146c338e69336cf6bbbb4525c62dbe9bb90

--- view/src/ViewHooks.php
+++ view/src/ViewHooks.php
@@ -4,8 +4,11 @@ namespace Wikibase\View;
 
 use ExtensionRegistry;
 use MediaWiki\Hook\UnitTestsListHook;
+use MediaWiki\Languages\LanguageNameUtils;
+use MediaWiki\MediaWikiServices;
 use MediaWiki\ResourceLoader\Hook\ResourceLoaderRegisterModulesHook;
 use MediaWiki\ResourceLoader\ResourceLoader;
+use ResourceLoaderContext;
 
 /**
  * File defining the hook handlers for the WikibaseView extension.
@@ -38,6 +41,8 @@ final class ViewHooks implements UnitTestsListHook, ResourceLoaderRegisterModule
 			'remoteExtPath' => 'Wikibase/view',
 		];
 
+		$isUlsLoaded = ExtensionRegistry::getInstance()->isLoaded( 'UniversalLanguageSelector' );
+
 		$modules = [
 			'jquery.util.getDirectionality' => $moduleTemplate + [
 				'scripts' => [
@@ -45,8 +50,31 @@ final class ViewHooks implements UnitTestsListHook, ResourceLoaderRegisterModule
 				],
 			],
 			'wikibase.getLanguageNameByCode' => $moduleTemplate + [
-				'scripts' => [
+				'packageFiles' => [
 					'resources/wikibase/wikibase.getLanguageNameByCode.js',
+					[
+						'name' => 'resources/wikibase/languageNames.json',
+						'callback' => static function (
+							ResourceLoaderContext $context
+						) use ( $isUlsLoaded ) {
+							$languageNameUtils = MediaWikiServices::getInstance()->getLanguageNameUtils();
+							$languageNames = $languageNameUtils->getLanguageNames(
+								$context->getLanguage(),
+								LanguageNameUtils::ALL
+							);
+
+							if ( $isUlsLoaded ) {
+								// remove all the supported languages, ULS already sends those
+								$supportedLanguageNames = $languageNameUtils->getLanguageNames(
+									$context->getLanguage(),
+									LanguageNameUtils::SUPPORTED
+								);
+								$languageNames = array_diff_key( $languageNames, $supportedLanguageNames );
+							}
+
+							return $languageNames;
+						},
+					],
 				],
 				'dependencies' => [
 					'wikibase',
@@ -55,7 +83,6 @@ final class ViewHooks implements UnitTestsListHook, ResourceLoaderRegisterModule
 			],
 		];
 
-		$isUlsLoaded = ExtensionRegistry::getInstance()->isLoaded( 'UniversalLanguageSelector' );
 		if ( $isUlsLoaded ) {
 			$modules['jquery.util.getDirectionality']['dependencies'][] = 'ext.uls.mediawiki';
 			$modules['wikibase.getLanguageNameByCode']['dependencies'][] = 'ext.uls.mediawiki';
