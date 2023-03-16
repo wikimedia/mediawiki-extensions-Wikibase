761904251505fbd233a20d512467c01334a4590f
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2023-03-16 13:22:25 +0100

    tests: Use ExtensionServicesTestBase
    This new base class in MediaWiki core lets us remove all the duplicate
    code between WikibaseRepoTest and WikibaseClientTest.
    
    Bug: T330628
    Change-Id: Iac961be786dd51caa3aa2a1a5af293793e9f0e32
    Depends-On: I6530947aa7c79a7a6437fc09bca8a721285beb15

--- repo/tests/phpunit/includes/WikibaseRepoTest.php
+++ repo/tests/phpunit/includes/WikibaseRepoTest.php
@@ -2,12 +2,7 @@
 
 namespace Wikibase\Repo\Tests;
 
-use MediaWikiIntegrationTestCase;
-use Psr\Container\ContainerInterface;
-use ReflectionClass;
-use ReflectionMethod;
-use ReflectionNamedType;
-use ReflectionType;
+use MediaWiki\Tests\ExtensionServicesTestBase;
 use Wikibase\Repo\WikibaseRepo;
 
 /**
@@ -17,89 +12,10 @@ use Wikibase\Repo\WikibaseRepo;
  *
  * @license GPL-2.0-or-later
  */
-class WikibaseRepoTest extends MediaWikiIntegrationTestCase {
+class WikibaseRepoTest extends ExtensionServicesTestBase {
 
-	/** @dataProvider provideMethods */
-	public function testMethodSignature( ReflectionMethod $method ): void {
-		$this->assertTrue( $method->isPublic(),
-			'service accessor must be public' );
-		$this->assertTrue( $method->isStatic(),
-			'service accessor must be static' );
-		$this->assertStringStartsWith( 'get', $method->getName(),
-			'service accessor must be a getter' );
-		$this->assertTrue( $method->hasReturnType(),
-			'service accessor must declare return type' );
-	}
+	protected string $className = WikibaseRepo::class;
 
-	/** @dataProvider provideMethods */
-	public function testMethodWithDefaultServiceContainer( ReflectionMethod $method ): void {
-		$methodName = $method->getName();
-		$serviceName = 'WikibaseRepo.' . substr( $methodName, 3 );
-		$expectedService = $this->createValue( $method->getReturnType() );
-		$this->setService( $serviceName, $expectedService );
-
-		$actualService = WikibaseRepo::$methodName();
-
-		$this->assertSame( $expectedService, $actualService,
-			'should return service from MediaWikiServices' );
-	}
-
-	/** @dataProvider provideMethods */
-	public function testMethodWithCustomServiceContainer( ReflectionMethod $method ): void {
-		$methodName = $method->getName();
-		$serviceName = 'WikibaseRepo.' . substr( $methodName, 3 );
-		$expectedService = $this->createValue( $method->getReturnType() );
-		$services = $this->createMock( ContainerInterface::class );
-		$services->expects( $this->once() )
-			->method( 'get' )
-			->with( $serviceName )
-			->willReturn( $expectedService );
-
-		$actualService = WikibaseRepo::$methodName( $services );
-
-		$this->assertSame( $expectedService, $actualService,
-			'should return service from injected container' );
-	}
-
-	public function provideMethods(): iterable {
-		$reflectionClass = new ReflectionClass( WikibaseRepo::class );
-		$methods = $reflectionClass->getMethods();
-
-		foreach ( $methods as $method ) {
-			if ( $method->isConstructor() ) {
-				continue;
-			}
-			yield $method->getName() => [ $method ];
-		}
-	}
-
-	private function createValue( ReflectionType $type ) {
-		// (in PHP 8.0, account for $type being a ReflectionUnionType here)
-		$this->assertInstanceOf( ReflectionNamedType::class, $type );
-		/** @var ReflectionNamedType $type */
-		if ( $type->allowsNull() ) {
-			return null;
-		}
-		if ( $type->isBuiltin() ) {
-			switch ( $type->getName() ) {
-				case 'bool':
-					return true;
-				case 'int':
-					return 0;
-				case 'float':
-					return 0.0;
-				case 'string':
-					return '';
-				case 'array':
-				case 'iterable':
-					return [];
-				case 'callable':
-					return 'is_null';
-				default:
-					$this->fail( "unknown builtin type {$type->getName()}" );
-			}
-		}
-		return $this->createMock( $type->getName() );
-	}
+	protected string $serviceNamePrefix = 'WikibaseRepo.';
 
 }
