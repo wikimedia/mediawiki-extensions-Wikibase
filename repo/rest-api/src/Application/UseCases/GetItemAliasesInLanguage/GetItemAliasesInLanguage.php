c644dfa1b3cf2cd002fa44a69c249c9b24da5d06
Muhammad Jaziraly <muhammad.jaziraly@wikimedia.de>
2023-05-30 13:50:42 +0200

    REST: Use revision data step in GetItemAliasesInLanguage
    Bug: T337307
    Change-Id: I1e28294966104717b554b5b9195a22120ed1af17

--- repo/rest-api/src/Application/UseCases/GetItemAliasesInLanguage/GetItemAliasesInLanguage.php
+++ repo/rest-api/src/Application/UseCases/GetItemAliasesInLanguage/GetItemAliasesInLanguage.php
@@ -3,26 +3,26 @@
 namespace Wikibase\Repo\RestApi\Application\UseCases\GetItemAliasesInLanguage;
 
 use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\Repo\RestApi\Application\UseCases\GetLatestItemRevisionMetadata;
 use Wikibase\Repo\RestApi\Application\UseCases\ItemRedirect;
 use Wikibase\Repo\RestApi\Application\UseCases\UseCaseError;
 use Wikibase\Repo\RestApi\Domain\Services\ItemAliasesInLanguageRetriever;
-use Wikibase\Repo\RestApi\Domain\Services\ItemRevisionMetadataRetriever;
 
 /**
  * @license GPL-2.0-or-later
  */
 class GetItemAliasesInLanguage {
 
-	private ItemRevisionMetadataRetriever $itemRevisionMetadataRetriever;
+	private GetLatestItemRevisionMetadata $getLatestRevisionMetadata;
 	private ItemAliasesInLanguageRetriever $itemAliasesInLanguageRetriever;
 	private GetItemAliasesInLanguageValidator $validator;
 
 	public function __construct(
-		ItemRevisionMetadataRetriever $itemRevisionMetadataRetriever,
+		GetLatestItemRevisionMetadata $getLatestRevisionMetadata,
 		ItemAliasesInLanguageRetriever $itemAliasesInLanguageRetriever,
 		GetItemAliasesInLanguageValidator $validator
 	) {
-		$this->itemRevisionMetadataRetriever = $itemRevisionMetadataRetriever;
+		$this->getLatestRevisionMetadata = $getLatestRevisionMetadata;
 		$this->itemAliasesInLanguageRetriever = $itemAliasesInLanguageRetriever;
 		$this->validator = $validator;
 	}
@@ -37,20 +37,7 @@ class GetItemAliasesInLanguage {
 
 		$itemId = new ItemId( $request->getItemId() );
 
-		$metaDataResult = $this->itemRevisionMetadataRetriever->getLatestRevisionMetadata( $itemId );
-
-		if ( !$metaDataResult->itemExists() ) {
-			throw new UseCaseError(
-				UseCaseError::ITEM_NOT_FOUND,
-				"Could not find an item with the ID: {$request->getItemId()}"
-			);
-		}
-
-		if ( $metaDataResult->isRedirect() ) {
-			throw new ItemRedirect(
-				$metaDataResult->getRedirectTarget()->getSerialization()
-			);
-		}
+		[ $revisionId, $lastModified ] = $this->getLatestRevisionMetadata->execute( $itemId );
 
 		$aliases = $this->itemAliasesInLanguageRetriever->getAliasesInLanguage( $itemId, $request->getLanguageCode() );
 
@@ -63,8 +50,8 @@ class GetItemAliasesInLanguage {
 
 		return new GetItemAliasesInLanguageResponse(
 			$aliases,
-			$metaDataResult->getRevisionTimestamp(),
-			$metaDataResult->getRevisionId(),
+			$lastModified,
+			$revisionId,
 		);
 	}
 }
