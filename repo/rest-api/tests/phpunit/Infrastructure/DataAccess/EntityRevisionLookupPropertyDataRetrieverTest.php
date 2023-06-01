a48638cb0337e0c5bd842d72b498112097aaccf8
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2023-06-01 16:37:53 +0200

    REST: Add PropertyDataRetriever
    Bug: T337849
    Change-Id: I62c6df1696ec2367cd2891fa18d1721f437185a1

---
+++ repo/rest-api/tests/phpunit/Infrastructure/DataAccess/EntityRevisionLookupPropertyDataRetrieverTest.php
@@ -0,0 +1,95 @@
+<?php declare( strict_types=1 );
+
+namespace Wikibase\Repo\Tests\RestApi\Infrastructure\DataAccess;
+
+use PHPUnit\Framework\TestCase;
+use Wikibase\DataModel\Entity\NumericPropertyId;
+use Wikibase\DataModel\Entity\Property;
+use Wikibase\DataModel\Services\Lookup\InMemoryDataTypeLookup;
+use Wikibase\DataModel\Statement\StatementList as DataModelStatementList;
+use Wikibase\DataModel\Term\AliasGroup;
+use Wikibase\DataModel\Term\AliasGroupList;
+use Wikibase\DataModel\Term\Fingerprint;
+use Wikibase\DataModel\Term\Term;
+use Wikibase\DataModel\Term\TermList;
+use Wikibase\DataModel\Tests\NewStatement;
+use Wikibase\Lib\Store\EntityRevision;
+use Wikibase\Lib\Store\EntityRevisionLookup;
+use Wikibase\Repo\RestApi\Domain\ReadModel\Aliases;
+use Wikibase\Repo\RestApi\Domain\ReadModel\Descriptions;
+use Wikibase\Repo\RestApi\Domain\ReadModel\Labels;
+use Wikibase\Repo\RestApi\Domain\ReadModel\StatementList;
+use Wikibase\Repo\RestApi\Domain\Services\StatementReadModelConverter;
+use Wikibase\Repo\RestApi\Infrastructure\DataAccess\EntityRevisionLookupPropertyDataRetriever;
+use Wikibase\Repo\WikibaseRepo;
+
+/**
+ * @covers \Wikibase\Repo\RestApi\Infrastructure\DataAccess\EntityRevisionLookupPropertyDataRetriever
+ *
+ * @group Wikibase
+ *
+ * @license GPL-2.0-or-later
+ */
+class EntityRevisionLookupPropertyDataRetrieverTest extends TestCase {
+
+	private EntityRevisionLookup $entityRevisionLookup;
+
+	protected function setUp(): void {
+		parent::setUp();
+
+		$this->entityRevisionLookup = $this->createStub( EntityRevisionLookup::class );
+	}
+
+	public function testGetPropertyData(): void {
+		$propertyId = new NumericPropertyId( 'P123' );
+		$expectedStatement = NewStatement::someValueFor( 'P321' )
+			->withGuid( 'Q123$AAAAAAAA-BBBB-CCCC-DDDD-EEEEEEEEEEEE' )
+			->build();
+		$property = new Property(
+			$propertyId,
+			new Fingerprint(
+				new TermList( [ new Term( 'en', 'potato' ) ] ),
+				new TermList( [ new Term( 'en', 'root vegetable' ) ] ),
+				new AliasGroupList( [ new AliasGroup( 'en', [ 'spud', 'tater' ] ) ] )
+			),
+			'string',
+			new DataModelStatementList( $expectedStatement )
+		);
+
+		$this->entityRevisionLookup = $this->createStub( EntityRevisionLookup::class );
+		$this->entityRevisionLookup->method( 'getEntityRevision' )
+			->willReturn( new EntityRevision( $property, 123, '20201010998877' ) );
+
+		$propertyData = $this->newRetriever()->getPropertyData( $propertyId );
+
+		$this->assertSame( $propertyId, $propertyData->getId() );
+		$this->assertEquals( Labels::fromTermList( $property->getLabels() ), $propertyData->getLabels() );
+		$this->assertEquals( Descriptions::fromTermList( $property->getDescriptions() ), $propertyData->getDescriptions() );
+		$this->assertEquals( Aliases::fromAliasGroupList( $property->getAliasGroups() ), $propertyData->getAliases() );
+		$this->assertEquals(
+			new StatementList( $this->newStatementReadModelConverter()->convert( $expectedStatement ) ),
+			$propertyData->getStatements()
+		);
+	}
+
+	public function testGivenPropertyDoesNotExist_getPropertyDataReturnsNull(): void {
+		$propertyId = new NumericPropertyId( 'P234' );
+		$this->entityRevisionLookup = $this->createStub( EntityRevisionLookup::class );
+		$this->entityRevisionLookup->method( 'getEntityRevision' )
+			->willReturn( null );
+
+		$this->assertNull( $this->newRetriever()->getPropertyData( $propertyId ) );
+	}
+
+	private function newRetriever(): EntityRevisionLookupPropertyDataRetriever {
+		return new EntityRevisionLookupPropertyDataRetriever(
+			$this->entityRevisionLookup,
+			$this->newStatementReadModelConverter()
+		);
+	}
+
+	private function newStatementReadModelConverter(): StatementReadModelConverter {
+		return new StatementReadModelConverter( WikibaseRepo::getStatementGuidParser(), new InMemoryDataTypeLookup() );
+	}
+
+}
