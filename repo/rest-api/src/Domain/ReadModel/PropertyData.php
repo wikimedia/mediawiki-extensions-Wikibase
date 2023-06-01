248d3cf02904b4c19ce0c074acc608e3165e89fd
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2023-06-01 11:31:51 +0200

    REST: Add PropertyData read model
    Bug: T337849
    Change-Id: I5a9b96035581921948a5969c9a64b5d2fed22b0c

---
+++ repo/rest-api/src/Domain/ReadModel/PropertyData.php
@@ -0,0 +1,60 @@
+<?php declare( strict_types=1 );
+
+namespace Wikibase\Repo\RestApi\Domain\ReadModel;
+
+use Wikibase\DataModel\Entity\Property as DataModelProperty;
+use Wikibase\DataModel\Entity\PropertyId;
+
+/**
+ * @license GPL-2.0-or-later
+ */
+class PropertyData {
+	public const TYPE = DataModelProperty::ENTITY_TYPE;
+
+	private PropertyId $id;
+	private string $dataType;
+	private Labels $labels;
+	private Descriptions $descriptions;
+	private Aliases $aliases;
+	private StatementList $statements;
+
+	public function __construct(
+		PropertyId $id,
+		string $dataType,
+		Labels $labels,
+		Descriptions $descriptions,
+		Aliases $aliases,
+		StatementList $statements
+	) {
+		$this->id = $id;
+		$this->dataType = $dataType;
+		$this->labels = $labels;
+		$this->descriptions = $descriptions;
+		$this->aliases = $aliases;
+		$this->statements = $statements;
+	}
+
+	public function getId(): PropertyId {
+		return $this->id;
+	}
+
+	public function getDataType(): string {
+		return $this->dataType;
+	}
+
+	public function getLabels(): Labels {
+		return $this->labels;
+	}
+
+	public function getDescriptions(): Descriptions {
+		return $this->descriptions;
+	}
+
+	public function getAliases(): Aliases {
+		return $this->aliases;
+	}
+
+	public function getStatements(): StatementList {
+		return $this->statements;
+	}
+}
