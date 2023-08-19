f20cf7fe5ee7307fc2bcf91f155a7f07bc710e64
Umherirrender <umherirrender_de.wp@web.de>
2023-08-19 14:54:12 +0200

    Use namespaced TitleFactory
    Bug: T321681
    Change-Id: I3ea6c358630bc1be37e45de46b4cb6a28e9c1eda

--- client/tests/phpunit/integration/includes/Specials/SpecialUnconnectedPagesTest.php
+++ client/tests/phpunit/integration/includes/Specials/SpecialUnconnectedPagesTest.php
@@ -4,6 +4,7 @@ namespace Wikibase\Client\Tests\Integration\Specials;
 
 use Iterator;
 use MediaWiki\Title\Title;
+use MediaWiki\Title\TitleFactory;
 use SpecialPageTestBase;
 use Wikibase\Client\NamespaceChecker;
 use Wikibase\Client\Specials\SpecialUnconnectedPages;
@@ -254,7 +255,7 @@ class SpecialUnconnectedPagesTest extends SpecialPageTestBase {
 
 		$namespaceChecker = new NamespaceChecker( [] );
 
-		$titleFactoryMock = $this->createMock( \TitleFactory::class );
+		$titleFactoryMock = $this->createMock( TitleFactory::class );
 
 		$titleFactoryMock->method( 'newFromID' )
 			->willReturn( null );
