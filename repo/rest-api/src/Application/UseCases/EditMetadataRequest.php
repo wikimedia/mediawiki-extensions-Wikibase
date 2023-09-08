a4b312ddfea30efeb8ffce9745b5fe2a706aa7c5
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2023-09-08 15:56:33 +0200

    REST: Add EditMetadataRequestValidatingDeserializer
    Bug: T345715
    Change-Id: Ib57fc68af2b8b0ed66d85bdfbfc05237f3645aed

---
+++ repo/rest-api/src/Application/UseCases/EditMetadataRequest.php
@@ -0,0 +1,16 @@
+<?php declare( strict_types=1 );
+
+namespace Wikibase\Repo\RestApi\Application\UseCases;
+
+/**
+ * @license GPL-2.0-or-later
+ */
+interface EditMetadataRequest {
+	public function getUsername(): ?string;
+
+	public function isBot(): bool;
+
+	public function getComment(): ?string;
+
+	public function getEditTags(): array;
+}
