858266da673bf65b3606da3e28a7b9efcbae4b0a
Silvan <silvan.heintze@wikimedia.de>
2023-09-14 10:57:23 +0200

    REST: Add OpenAPI spec tests for GetPropertyLabels
    Bug: T346303
    Change-Id: Ia20d3953ee25cdcf981864d5ccd301315b5a479d

---
+++ repo/rest-api/tests/mocha/openapi-validation/GetPropertyLabelsTest.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const { expect } = require( '../helpers/chaiHelper' );
+const { createEntity, createUniqueStringProperty } = require( '../helpers/entityHelper' );
+const { newGetPropertyLabelsRequestBuilder } = require( '../helpers/RequestBuilderFactory' );
+
+describe( 'validate GET /entities/properties/{id}/labels responses against OpenAPI spec', () => {
+
+	let testPropertyId;
+	let lastRevisionId;
+
+	before( async () => {
+		const createPropertyResponse = await createUniqueStringProperty();
+		testPropertyId = createPropertyResponse.entity.id;
+		lastRevisionId = createPropertyResponse.entity.lastrevid;
+	} );
+
+	it( '200 OK response is valid for a Property with two labels', async () => {
+		const response = await newGetPropertyLabelsRequestBuilder( testPropertyId ).makeRequest();
+
+		expect( response ).to.have.status( 200 );
+		expect( response ).to.satisfyApiSpec;
+	} );
+
+	it( '200 OK response is valid for a Property without labels', async () => {
+		const createPropertyResponse = await createEntity( 'property', {
+			descriptions: { en: { language: 'en', value: 'some description' } },
+			datatype: 'string'
+		} );
+
+		const response = await newGetPropertyLabelsRequestBuilder( createPropertyResponse.entity.id ).makeRequest();
+
+		expect( response ).to.have.status( 200 );
+		expect( response ).to.satisfyApiSpec;
+	} );
+
+	it( '304 Not Modified response is valid', async () => {
+		const response = await newGetPropertyLabelsRequestBuilder( testPropertyId )
+			.withHeader( 'If-None-Match', `"${lastRevisionId}"` )
+			.makeRequest();
+
+		expect( response ).to.have.status( 304 );
+		expect( response ).to.satisfyApiSpec;
+	} );
+
+	it( '400 Bad Request response is valid for an invalid property ID', async () => {
+		const response = await newGetPropertyLabelsRequestBuilder( 'X123' ).makeRequest();
+
+		expect( response ).to.have.status( 400 );
+		expect( response ).to.satisfyApiSpec;
+	} );
+
+	it( '404 Not Found response is valid for a non-existing property', async () => {
+		const response = await newGetPropertyLabelsRequestBuilder( 'P99999' ).makeRequest();
+
+		expect( response ).to.have.status( 404 );
+		expect( response ).to.satisfyApiSpec;
+	} );
+
+} );
