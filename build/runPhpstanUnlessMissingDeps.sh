9bcdcdac9b7dcfdace46bf917999b872f29b027f
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2023-09-26 11:46:21 +0200

    REST: Fix phpstan by creating LocalSettings.php
    Since Ib578644b8a4c0b64dca607afb9eb8204ca7fc660 phpunit/bootstrap.php
    needs to load LocalSettings.php. At the time we run phpstan there is no
    LocalSettings.php yet, so we create a minimal one loading only Wikibase
    repo.
    
    Change-Id: Idf54ec10d64df8703cd9f003ea01e3715688d487

--- build/runPhpstanUnlessMissingDeps.sh
+++ build/runPhpstanUnlessMissingDeps.sh
@@ -8,10 +8,28 @@ SCRIPT_DIR=$(dirname "$0")
 WB_DIR="$SCRIPT_DIR/.."
 CORE_DIR="$WB_DIR/../.."
 CORE_VENDOR_DIR="$CORE_DIR/vendor"
+LOCAL_SETTINGS_FILE="$CORE_DIR/LocalSettings.php"
 
 if [[ -d $CORE_VENDOR_DIR && -n "$(ls -A $CORE_VENDOR_DIR)" ]];
 then
-    $WB_DIR/vendor/bin/phpstan analyze -a $CORE_DIR/tests/phpunit/bootstrap.php
+	CREATED_LOCAL_SETTINGS=false
+	if [ ! -f $LOCAL_SETTINGS_FILE ]; then
+		# As of T227900, tests/phpunit/bootstrap.php which we use for class loading here, requires LocalSettings.php to
+		# exist. Since we're not actually running PHPUnit tests here, we just create a minimal LocalSettings.php loading
+		# Wikibase repo.
+		echo '<?php
+		$wgServer = "not-actually-needed-here";
+		wfLoadExtension( "WikibaseRepository", __DIR__ . "/extensions/Wikibase/extension-repo.json" );
+		require_once __DIR__ . "/extensions/Wikibase/repo/config/Wikibase.ci.php";' > $LOCAL_SETTINGS_FILE
+		CREATED_LOCAL_SETTINGS=true
+	fi
+
+	$WB_DIR/vendor/bin/phpstan analyze -a $CORE_DIR/tests/phpunit/bootstrap.php
+
+	# Delete LocalSettings.php if we created it. Following test code may depend on it not being there.
+	if [ "$CREATED_LOCAL_SETTINGS" = true ]; then
+		rm $LOCAL_SETTINGS_FILE
+	fi
 else
 	echo "Cannot run PHPStan because MediaWiki dependencies are not installed."
 fi
