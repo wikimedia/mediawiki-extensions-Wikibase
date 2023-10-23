52b98e1f8d4690135ac5d6e5e309d83f1b883d81
Ollie Shotton <oliver.shotton@wikimedia.de>
2023-10-23 09:37:08 +0100

    REST: Fix `JsonPatcher` return type
    We can't guarantee that the result from patching a JSON structure will result in an array (json object or list)
    
    Change-Id: I1c78acfdfc0712ee5d16091ac7d466ebe49dab43

--- repo/rest-api/src/Domain/Services/JsonPatcher.php
+++ repo/rest-api/src/Domain/Services/JsonPatcher.php
@@ -12,10 +12,13 @@ use Wikibase\Repo\RestApi\Domain\Services\Exceptions\PatchTestConditionFailedExc
 interface JsonPatcher {
 
 	/**
+	 * @return mixed usually this will be an array, but the type may change if the whole
+	 *               root element is modified by the client
+	 *
 	 * @throws InvalidArgumentException for an invalid patch
 	 * @throws PatchPathException if a path target provided in the patch does not exist
 	 * @throws PatchTestConditionFailedException if a "test" op in the patch fails
 	 */
-	public function patch( array $target, array $patch ): array;
+	public function patch( array $target, array $patch );
 
 }
