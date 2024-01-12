3367abe77bd47592ee96bb27c2cfbde2b6cfd907
dima koushha <dima.koushha@wikimedia.de>
2024-01-12 17:01:16 +0100

    REST: Handle bot flag and tags in DELETE siteLink
    Bug: T354861
    Change-Id: I3746ac51eff84a1a86192f05516134816f51ea07

--- repo/rest-api/src/Application/UseCases/RemoveItemSiteLink/RemoveItemSiteLinkRequest.php
+++ repo/rest-api/src/Application/UseCases/RemoveItemSiteLink/RemoveItemSiteLinkRequest.php
@@ -1,4 +1,4 @@
-<?php declare( strict_types=1 );
+<?php declare( strict_types = 1 );
 
 namespace Wikibase\Repo\RestApi\Application\UseCases\RemoveItemSiteLink;
 
@@ -9,10 +9,25 @@ class RemoveItemSiteLinkRequest {
 
 	private string $itemId;
 	private string $siteId;
+	private array $editTags;
+	private bool $isBot;
+	private ?string $comment;
+	private ?string $username;
 
-	public function __construct( string $itemId, string $siteId ) {
+	public function __construct(
+		string $itemId,
+		string $siteId,
+		array $editTags,
+		bool $isBot,
+		?string $comment,
+		?string $username
+	) {
 		$this->itemId = $itemId;
 		$this->siteId = $siteId;
+		$this->editTags = $editTags;
+		$this->isBot = $isBot;
+		$this->comment = $comment;
+		$this->username = $username;
 	}
 
 	public function getItemId(): string {
@@ -21,6 +36,22 @@ class RemoveItemSiteLinkRequest {
 
 	public function getSiteId(): string {
 		return $this->siteId;
+	}
+
+	public function getEditTags(): array {
+		return $this->editTags;
+	}
+
+	public function isBot(): bool {
+		return $this->isBot;
+	}
+
+	public function getComment(): ?string {
+		return $this->comment;
+	}
+
+	public function getUsername(): ?string {
+		return $this->username;
 	}
 
 }
