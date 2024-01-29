38a05be432fb1b49298132b1a825e88cee869f21
Ollie Shotton <oliver.shotton@wikimedia.de>
2024-01-29 15:13:27 +0000

    REST: Make `SiteLinks` extend `ArrayObject`
    instead of `ArrayIterator` to make it easier to get an individual `SiteLink` via it's site id
    
    Change-Id: I39f3ca068062e09e2cf1078ebdef44f15ee9182d

--- repo/rest-api/src/Domain/ReadModel/SiteLinks.php
+++ repo/rest-api/src/Domain/ReadModel/SiteLinks.php
@@ -2,15 +2,20 @@
 
 namespace Wikibase\Repo\RestApi\Domain\ReadModel;
 
-use ArrayIterator;
+use ArrayObject;
 
 /**
  * @license GPL-2.0-or-later
  */
-class SiteLinks extends ArrayIterator {
+class SiteLinks extends ArrayObject {
 
-	public function __construct( SiteLink ...$siteLinks ) {
-		parent::__construct( $siteLinks );
+	public function __construct( SiteLink ...$sitelinks ) {
+		parent::__construct(
+			array_combine(
+				array_map( fn( SiteLink $siteLink ) => $siteLink->getSite(), $sitelinks ),
+				$sitelinks
+			)
+		);
 	}
 
 }
