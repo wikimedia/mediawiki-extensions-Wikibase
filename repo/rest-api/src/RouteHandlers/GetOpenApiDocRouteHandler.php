67aef7b9ec38b34726ea3b8ce59997f06546b474
Silvan <silvan.heintze@wikimedia.de>
2024-02-12 13:29:41 +0100

    REST: Add route to serve OpenAPI doc
    Bug: T352294
    Change-Id: I715b866b37f2c7c8a8a4c7875716d60e85d50452

---
+++ repo/rest-api/src/RouteHandlers/GetOpenApiDocRouteHandler.php
@@ -0,0 +1,34 @@
+<?php declare( strict_types=1 );
+
+namespace Wikibase\Repo\RestApi\RouteHandlers;
+
+use MediaWiki\Rest\Handler;
+use MediaWiki\Rest\Response;
+use MediaWiki\Rest\SimpleHandler;
+use MediaWiki\Rest\StringStream;
+
+/**
+ * @license GPL-2.0-or-later
+ */
+class GetOpenApiDocRouteHandler extends SimpleHandler {
+
+	private const OPENAPI_FILE = __DIR__ . '/openapi.json';
+
+	public static function factory(): Handler {
+		return new self();
+	}
+
+	public function run(): Response {
+		$httpResponse = $this->getResponseFactory()->create();
+		$httpResponse->setHeader( 'Content-Type', 'application/json' );
+
+		$httpResponse->setBody( new StringStream( file_get_contents( self::OPENAPI_FILE ) ) );
+
+		return $httpResponse;
+	}
+
+	public function needsWriteAccess(): bool {
+		return false;
+	}
+
+}
