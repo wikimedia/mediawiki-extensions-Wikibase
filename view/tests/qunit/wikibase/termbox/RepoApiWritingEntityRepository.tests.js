4f64a7f453988759a4a06a2e56081dd9e276c593
Arthur Taylor <arthur.taylor@wikimedia.de>
2024-03-01 09:34:20 +0100

    Process temp user authentication redirects in API edit responses
    Termbox uses RepoApiWritingEntityRespository to save edits to the
    repo. With the introduction of Temp Users, an API edit request by
    an unauthenticated user may result in the creation of a temp user.
    In this case, the Termbox needs to redirect to the redirecturl
    returned from the API to complete the centralauth login for the
    user.
    
    Depends-On: I1b8389dc5cd5557e209a3c68b4fc3d38888466d1
    Bug: T357151
    Change-Id: I9370b7cc9e58254c55f4eb41ab7820e38359adce

--- view/tests/qunit/wikibase/termbox/RepoApiWritingEntityRepository.tests.js
+++ view/tests/qunit/wikibase/termbox/RepoApiWritingEntityRepository.tests.js
@@ -36,6 +36,44 @@
 
 	} );
 
+	QUnit.test( 'success with tempuser redirect', function ( assert ) {
+		const targetUrl = 'https://wiki.example';
+		const revisionId = 1234;
+		const testEntity = {
+			id: 'Q42',
+			labels: {
+				de: { language: 'de', value: 'Kartoffel' },
+				en: { language: 'en', value: 'potato' }
+			},
+			descriptions: {},
+			aliases: {},
+			lastrevid: revisionId
+		};
+
+		const stubRepoApi = {
+			editEntity( id, baseRevId, data ) {
+				return Promise.resolve( {
+					entity: testEntity,
+					tempuserredirect: targetUrl,
+					success: 1
+				} );
+			}
+		};
+		sinon.spy( stubRepoApi, 'editEntity' );
+
+		new RepoApiWritingEntityRepository( stubRepoApi )
+			.saveEntity( testEntity, revisionId )
+			.then( ( newRevision ) => {
+				assert.true( stubRepoApi.editEntity.calledWith( testEntity.id, revisionId, testEntity ) );
+				assert.deepEqual( newRevision, {
+					entity: testEntity,
+					revisionId: testEntity.lastrevid,
+					redirectUrl: targetUrl
+				} );
+			} );
+
+	} );
+
 	QUnit.test( 'request failed', function ( assert ) {
 		const done = assert.async();
 		const repoApiError = 'some-error-code';
