5445b365575e9169b5d3a6d5727650f16c3c8e23
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2024-04-16 16:12:04 +0200

    Remove unused dynamic property in WikibaseApiTestCase
    This was added in change I780e4969b6 (commit 6a58f36c4b), for no reason
    that I can see – perhaps just an accident. No usages found in
    codesearch, so let’s just remove it.
    
    Bug: T360560
    Change-Id: I22bed79d5e7ded6d9405e9e2972441730dd12d80

--- repo/tests/phpunit/includes/Api/WikibaseApiTestCase.php
+++ repo/tests/phpunit/includes/Api/WikibaseApiTestCase.php
@@ -79,7 +79,6 @@ abstract class WikibaseApiTestCase extends ApiTestCase {
 		$siteStore = new HashSiteStore( TestSites::getSites() );
 		$this->setService( 'SiteStore', $siteStore );
 		$this->setService( 'SiteLookup', $siteStore );
-		$this->sitesSetup = true;
 	}
 
 	/**
