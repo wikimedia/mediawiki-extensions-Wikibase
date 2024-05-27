42bed714378cdaf644b362820d3ee7af1f6b6db6
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2024-05-27 17:35:56 +0200

    Use ObjectCacheFactory in OtherProjectsSitesProvider service wiring
    This makes the OtherProjectsSitesProviderTest runnable as a pure unit
    test.
    
    Bug: T281708
    Change-Id: If2e6468700de70204d942d774702227ed4832281

--- client/tests/phpunit/unit/includes/ServiceWiring/OtherProjectsSitesProviderTest.php
+++ client/tests/phpunit/unit/includes/ServiceWiring/OtherProjectsSitesProviderTest.php
@@ -3,6 +3,8 @@ declare( strict_types = 1 );
 
 namespace Wikibase\Client\Tests\Unit\ServiceWiring;
 
+use BagOStuff;
+use ObjectCacheFactory;
 use Wikibase\Client\OtherProjectsSitesProvider;
 use Wikibase\Client\Tests\Unit\ServiceWiringTestCase;
 use Wikibase\Lib\SettingsArray;
@@ -24,6 +26,13 @@ class OtherProjectsSitesProviderTest extends ServiceWiringTestCase {
 				'specialSiteLinkGroups' => [],
 			] )
 		);
+		$objectCacheFactory = $this->createMock( ObjectCacheFactory::class );
+		$objectCacheFactory->expects( $this->once() )
+			->method( 'getLocalClusterInstance' )
+			->willReturn( $this->createMock( BagOStuff::class ) );
+		$this->serviceContainer->expects( $this->once() )
+			->method( 'getObjectCacheFactory' )
+			->willReturn( $objectCacheFactory );
 
 		$this->assertInstanceOf(
 			OtherProjectsSitesProvider::class,
