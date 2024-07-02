906f59b0f7bc7e2f2ca736233384e44be5bddbeb
Ollie Shotton <oliver.shotton@wikimedia.de>
2024-07-02 15:23:09 +0100

    REST: Remove unused setup.js file
    `setup.js` is not needed now that we are using `hooks.js`
    
    Bug: T368902
    Change-Id: I163093ed800f4f536ec138baf517b5a1af07cf7d

--- repo/rest-api/tests/mocha/openapi-validation/hooks.js
+++ repo/rest-api/tests/mocha/openapi-validation/hooks.js
@@ -1,10 +1,3 @@
-/**
- * Duplicate the setup from `setup.js`, but in a format that mocha can use as a
- * global beforeAll - https://mochajs.org/#root-hook-plugins
- *
- * We need this to support parallel execution of the openAPI tests.
- * See T368902
- */
 'use strict';
 
 const chai = require( 'chai' );
@@ -12,6 +5,11 @@ const SwaggerParser = require( '@apidevtools/swagger-parser' );
 const { REST } = require( 'api-testing' );
 const { default: chaiResponseValidator } = require( 'chai-openapi-response-validator' );
 
+/**
+ * mocha global beforeAll hook required to support parallel execution of OpenAPI tests.
+ * See https://mochajs.org/#root-hook-plugins
+ * See T368902
+ */
 exports.mochaHooks = {
 	beforeAll: [
 		async function () {
