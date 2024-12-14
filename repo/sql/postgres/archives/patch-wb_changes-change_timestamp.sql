0dea1809c14e6a9c7365ecbfbf491eb173875730
Daimona Eaytoy <daimona.wiki@gmail.com>
2024-12-14 04:19:45 +0100

    tests: Add structure test for abstract schema
    Use the new base class introduced in T381981.
    
    Also regenerate all the schema changes to get rid of drifts, as done in
    I3a3acc0935409d4601eb172664580b13d5fe2340.
    
    Depends-On: I8ae59ce6854922c2804d12986c870d3f0e0c3299
    Bug: T317180
    Change-Id: I4ee25a69017a11c8b489fdbff0441842cccf9543

--- repo/sql/postgres/archives/patch-wb_changes-change_timestamp.sql
+++ repo/sql/postgres/archives/patch-wb_changes-change_timestamp.sql
@@ -2,7 +2,7 @@
 -- Source: repo/sql/abstractSchemaChanges/patch-wb_changes-change_timestamp.json
 -- Do not modify this file directly.
 -- See https://www.mediawiki.org/wiki/Manual:Schema_changes
-ALTER TABLE  wb_changes ALTER change_time TYPE TIMESTAMPTZ;
-ALTER TABLE  wb_changes ALTER change_time
-DROP  DEFAULT;
-ALTER TABLE  wb_changes ALTER change_time TYPE TIMESTAMPTZ;
+ALTER TABLE wb_changes
+  ALTER change_time TYPE TIMESTAMPTZ;
+ALTER TABLE wb_changes
+  ALTER change_time TYPE TIMESTAMPTZ;
