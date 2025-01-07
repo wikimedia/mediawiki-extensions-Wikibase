a0ed2c663dfff726990edf8c49d2e48ef2ea2306
Jon Robson <jdlrobson@gmail.com>
2025-01-06 17:37:24 -0800

    Declare modules that are loaded on article views
    Tracking bundlesizes on modules that are loaded on page load, allows
    us to track overall site performance for article views.
    
    These budgets are set to null for now, as tracking and enforcing
    their size is optional per
    https://www.mediawiki.org/wiki/Performance_budgeting
    
    Bug: T360590
    Change-Id: I1f47161b3e2c5116a7a1622bed41fba0c44c5f07

---
+++ lib/tests/phpunit/WikibaseBundleSizeTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace Wikibase\Lib\Tests;
+
+use MediaWiki\Tests\Structure\BundleSizeTestBase;
+
+/**
+ * Track the size of ResourceLoader modules that repo or client load on page load.
+ *
+ * @group Wikibase
+ * @license GPL-2.0-or-later
+ * @coversNothing
+ */
+class WikibaseBundleSizeTest extends BundleSizeTestBase {
+
+	protected function setUp(): void {
+		parent::setUp();
+
+		$this->markTestSkippedIfExtensionNotLoaded( 'WikibaseRepository' );
+		$this->markTestSkippedIfExtensionNotLoaded( 'WikibaseClient' );
+	}
+
+	/** @inheritDoc */
+	public function getBundleSizeConfig(): string {
+		return dirname( __DIR__, 3 ) . '/bundlesize.config.json';
+	}
+
+}
