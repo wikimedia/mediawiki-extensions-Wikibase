32b41fabe5f4ea47774802d8a0ec2d8ea73dc35d
Itamar Givon <itamar.givon@wikimedia.de>
2025-02-06 20:15:37 +0100

    Search: Add initial tests and config for search
    This commit introduces initial Mocha tests and
    configurations for the search domain. It also adds
    an ESLint configuration specific to search and
    updates the `package.json` scripts to include
    linting and testing commands for the new domain.
    
    Bug: T384211
    Change-Id: I21b452a5e32f259120055114855f1fdd6bec8c10

---
+++ repo/domains/search/tests/mocha/api-testing/HelloSearch.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const { assert } = require( 'api-testing' );
+
+describe( 'Hello search e2e', () => {
+	it( 'passes', () => {
+		assert.ok( true );
+	} );
+} );
---
+++ repo/domains/search/tests/mocha/openapi-validation/HelloSearch.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const { assert } = require( 'api-testing' );
+
+describe( 'Hello search spec', () => {
+	it( 'passes', () => {
+		assert.ok( true );
+	} );
+} );
