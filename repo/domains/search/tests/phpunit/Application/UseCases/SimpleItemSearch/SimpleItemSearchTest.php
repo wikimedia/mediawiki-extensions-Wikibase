1ebb67f159697b34e95b514cd9cee5a317b74a3b
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2025-03-27 15:44:12 +0100

    Search: Add SimpleItemSearch validation test
    Change-Id: I526cea264da738e073492317a0fdf33a11281f06

--- repo/domains/search/tests/phpunit/Application/UseCases/SimpleItemSearch/SimpleItemSearchTest.php
+++ repo/domains/search/tests/phpunit/Application/UseCases/SimpleItemSearch/SimpleItemSearchTest.php
@@ -6,6 +6,7 @@ use PHPUnit\Framework\TestCase;
 use Wikibase\Repo\Domains\Search\Application\UseCases\SimpleItemSearch\SimpleItemSearch;
 use Wikibase\Repo\Domains\Search\Application\UseCases\SimpleItemSearch\SimpleItemSearchRequest;
 use Wikibase\Repo\Domains\Search\Application\UseCases\SimpleItemSearch\SimpleItemSearchValidator;
+use Wikibase\Repo\Domains\Search\Application\UseCases\UseCaseError;
 use Wikibase\Repo\Domains\Search\Domain\Model\ItemSearchResults;
 use Wikibase\Repo\Domains\Search\Domain\Services\ItemSearchEngine;
 
@@ -31,13 +32,31 @@ class SimpleItemSearchTest extends TestCase {
 
 		$this->assertEquals(
 			$expectedResults,
-			$this->newUseCase( $searchEngine )
+			$this->newUseCase( $this->createStub( SimpleItemSearchValidator::class ), $searchEngine )
 				->execute( new SimpleItemSearchRequest( $query, $language ) )
 				->getResults()
 		);
 	}
 
-	private function newUseCase( ItemSearchEngine $searchEngine ): SimpleItemSearch {
-		return new SimpleItemSearch( $this->createStub( SimpleItemSearchValidator::class ), $searchEngine );
+	public function testValidatesTheRequest(): void {
+		$request = $this->createStub( SimpleItemSearchRequest::class );
+		$expectedException = $this->createStub( UseCaseError::class );
+
+		$validator = $this->createMock( SimpleItemSearchValidator::class );
+		$validator->expects( $this->once() )
+			->method( 'validate' )
+			->with( $request )
+			->willThrowException( $expectedException );
+
+		try {
+			$this->newUseCase( $validator, $this->createStub( ItemSearchEngine::class ) )->execute( $request );
+			$this->fail( 'Expected exception was not thrown' );
+		} catch ( UseCaseError $e ) {
+			$this->assertSame( $expectedException, $e );
+		}
+	}
+
+	private function newUseCase( SimpleItemSearchValidator $validator, ItemSearchEngine $searchEngine ): SimpleItemSearch {
+		return new SimpleItemSearch( $validator, $searchEngine );
 	}
 }
