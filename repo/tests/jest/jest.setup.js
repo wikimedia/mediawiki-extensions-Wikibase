79b4801661a65fa252e928ea9d9b4a4915a9ca62
Audrey Penven <audrey.penven@wikimedia.de>
2025-04-02 11:57:25 +0200

    Add tests for ScopedTypeaheadSearch
    Adds some test coverage and relevant configuration for the
    ScopedTypeaheadSearch component. These tests cover the initialization
    of the component as well as interactions between the child components
    (CdxSelect and CdxTypeaheadSearch).
    
    Bug: T385790
    Change-Id: I8ed6d3c4c3ed17fa659b30005ac79a7317c1322a

---
+++ repo/tests/jest/jest.setup.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const { config } = require( '@vue/test-utils' );
+/**
+ * Mock for the calls to Core's $i18n plugin which returns a mw.Message object.
+ *
+ * @param {string} key The key of the message to parse.
+ * @param {...*} args Arbitrary number of arguments to be parsed.
+ * @return {Object} mw.Message-like object with .text() and .parse() methods.
+ */
+function $i18nMock( key, ...args ) {
+	function serializeArgs() {
+		return args.length ? `${ key }:[${ args.join( ',' ) }]` : key;
+	}
+	return {
+		text: () => serializeArgs(),
+		parse: () => serializeArgs()
+	};
+}
+// Mock Vue plugins in test suites.
+config.global.provide = {
+	i18n: $i18nMock
+};
+config.global.mocks = {
+	$i18n: $i18nMock
+};
+config.global.directives = {
+	'i18n-html': ( el, binding ) => {
+		el.innerHTML = `${ binding.arg } (${ binding.value })`;
+	}
+};
+
+function ApiMock() {}
+ApiMock.prototype.get = jest.fn();
+
+global.mw = {
+	Api: ApiMock,
+	msg: jest.fn( ( key ) => key )
+};
