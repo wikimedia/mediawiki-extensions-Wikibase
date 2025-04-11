ba61974e225b9cb470246a72ceffe0c5ac8e32d7
dima koushha <dima.koushha@wikimedia.de>
2025-04-11 16:56:08 +0200

    Search: Validate pagination query params for `SimplePropertySearch`
    Bug: T390694
    Change-Id: I277ee6452beed97829326a7b427a82c5385f77c6

--- repo/domains/search/src/Application/UseCases/SimplePropertySearch/SimplePropertySearchValidator.php
+++ repo/domains/search/src/Application/UseCases/SimplePropertySearch/SimplePropertySearchValidator.php
@@ -10,7 +10,12 @@ use Wikibase\Repo\Domains\Search\Application\Validation\SearchLanguageValidator;
  * @license GPL-2.0-or-later
  */
 class SimplePropertySearchValidator {
+
 	public const LANGUAGE_QUERY_PARAM = 'language';
+	public const LIMIT_QUERY_PARAM = 'limit';
+	public const OFFSET_QUERY_PARAM = 'offset';
+
+	private const MAX_LIMIT = 500;
 
 	private SearchLanguageValidator $languageValidator;
 
@@ -37,6 +42,21 @@ class SimplePropertySearchValidator {
 				default:
 					throw new LogicException( 'unknown validation error code ' . $validationError->getCode() );
 			}
+		}
+
+		$this->validateLimitAndOffset( $request );
+	}
+
+	private function validateLimitAndOffset( SimplePropertySearchRequest $request ): void {
+		$limit = $request->getLimit();
+		$offset = $request->getOffset();
+
+		if ( $limit < 0 || $limit > self::MAX_LIMIT ) {
+			throw UseCaseError::invalidQueryParameter( self::LIMIT_QUERY_PARAM );
+		}
+
+		if ( $offset < 0 ) {
+			throw UseCaseError::invalidQueryParameter( self::OFFSET_QUERY_PARAM );
 		}
 	}
 }
--- repo/domains/search/tests/phpunit/Application/UseCases/SimplePropertySearch/SimplePropertySearchValidatorTest.php
+++ repo/domains/search/tests/phpunit/Application/UseCases/SimplePropertySearch/SimplePropertySearchValidatorTest.php
@@ -2,6 +2,7 @@
 
 namespace Wikibase\Repo\Tests\Domains\Search\Application\UseCases\SimplePropertySearch;
 
+use Generator;
 use MediaWiki\MediaWikiServices;
 use PHPUnit\Framework\TestCase;
 use Wikibase\Repo\Domains\Search\Application\UseCases\SimplePropertySearch\SimplePropertySearchRequest;
@@ -24,18 +25,29 @@ use Wikibase\Repo\WikibaseRepo;
  */
 class SimplePropertySearchValidatorTest extends TestCase {
 
+	private const DEFAULT_LIMIT = 10;
+	private const DEFAULT_OFFSET = 0;
+
 	/**
 	 * @doesNotPerformAssertions
 	 */
 	public function testValidate_passes(): void {
 		$this->newUseCaseValidator()
-			->validate( new SimplePropertySearchRequest( 'search term', 'en', 10, 0 ) );
+			->validate( new SimplePropertySearchRequest( 'search term', 'en', self::DEFAULT_LIMIT, self::DEFAULT_OFFSET ) );
+	}
+
+	/**
+	 * @doesNotPerformAssertions
+	 */
+	public function testValidateWithoutLimitAndOffsetParams_passe(): void {
+		$this->newUseCaseValidator()
+			->validate( new SimplePropertySearchRequest( 'search term', 'en', null, null ) );
 	}
 
 	public function testGivenInvalidLanguageCode_throws(): void {
 		try {
 			$this->newUseCaseValidator()
-				->validate( new SimplePropertySearchRequest( 'search term', 'xyz', 10, 0 ) );
+				->validate( new SimplePropertySearchRequest( 'search term', 'xyz', self::DEFAULT_LIMIT, self::DEFAULT_OFFSET ) );
 
 			$this->fail( 'Expected exception was not thrown' );
 		} catch ( UseCaseError $e ) {
@@ -46,6 +58,43 @@ class SimplePropertySearchValidatorTest extends TestCase {
 				$e->getErrorContext()
 			);
 		}
+	}
+
+	/**
+	 * @dataProvider provideInvalidLimitAndOffset
+	 */
+	public function testGivenInvalidLimitAndOffset_throws(
+		UseCaseError $expectedError,
+		int $limit,
+		int $offset
+	): void {
+		try {
+			$this->newUseCaseValidator()
+				->validate( new SimplePropertySearchRequest( 'search term', 'en', $limit, $offset ) );
+			$this->fail( 'Expected exception was not thrown' );
+		} catch ( UseCaseError $e ) {
+			$this->assertEquals( $expectedError, $e );
+		}
+	}
+
+	public static function provideInvalidLimitAndOffset(): Generator {
+		yield 'invalid limit - negative limit' => [
+			UseCaseError::invalidQueryParameter( 'limit' ),
+			-1,
+			self::DEFAULT_OFFSET,
+		];
+
+		yield 'invalid limit - limit exceeds max (500)' => [
+			UseCaseError::invalidQueryParameter( 'limit' ),
+			501,
+			self::DEFAULT_OFFSET,
+		];
+
+		yield 'invalid offset - negative offset' => [
+			UseCaseError::invalidQueryParameter( 'offset' ),
+			self::DEFAULT_LIMIT,
+			-2,
+		];
 	}
 
 	private function newUseCaseValidator(): SimplePropertySearchValidator {
