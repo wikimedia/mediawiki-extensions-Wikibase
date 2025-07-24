ff7eac7ff6190844bfdca12bff2180122808b6b0
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2025-07-24 17:21:18 +0200

    GQL: Request labels in batch
    This reduces the number of DB queries to fetch labels. It combines the
    labels to fetch of the main item with the ones to fetch of statement
    properties. In theory, this could result in over-fetching if item labels
    are requested in different languages than the statement property labels,
    but in reality this is unlikely and probably not worth optimizing.
    
    Bug: T400146
    Change-Id: Ie92b5bfaad562649c284b6de41d4d533c4903f6a

--- repo/includes/GraphQLPrototype/LabelsResolver.php
+++ repo/includes/GraphQLPrototype/LabelsResolver.php
@@ -2,6 +2,7 @@
 
 namespace Wikibase\Repo\GraphQLPrototype;
 
+use GraphQL\Deferred;
 use GraphQL\Type\Definition\ResolveInfo;
 use Wikibase\DataAccess\PrefetchingTermLookup;
 use Wikibase\DataModel\Entity\BasicEntityIdParser;
@@ -12,19 +13,31 @@ use Wikibase\DataModel\Term\TermTypes;
  */
 class LabelsResolver {
 
+	private array $entityIdsBatch = [];
+	private array $languageCodesBatch = [];
+	private bool $hasPrefetched = false;
+
 	public function __construct( private PrefetchingTermLookup $termLookup ) {
 	}
 
-	public function fetchLabels( array $rootValue, ResolveInfo $info ): array {
+	public function fetchLabels( array $rootValue, ResolveInfo $info ): Deferred {
 		$entityId = ( new BasicEntityIdParser() )->parse( $rootValue['id'] );
 		$languageCodes = array_keys( $info->getFieldSelection() );
 
-		$this->termLookup->prefetchTerms(
-			[ $entityId ],
-			[ TermTypes::TYPE_LABEL ],
-			$languageCodes
-		);
+		$this->entityIdsBatch[] = $entityId;
+		$this->languageCodesBatch = array_merge( $this->languageCodesBatch, $languageCodes );
 
-		return $this->termLookup->getLabels( $entityId, $languageCodes );
+		return new Deferred( function() use( $entityId, $languageCodes ) {
+			if ( !$this->hasPrefetched ) {
+				$this->termLookup->prefetchTerms(
+					array_unique( $this->entityIdsBatch ),
+					[ TermTypes::TYPE_LABEL ],
+					array_unique( $this->languageCodesBatch )
+				);
+				$this->hasPrefetched = true;
+			}
+
+			return $this->termLookup->getLabels( $entityId, $languageCodes );
+		} );
 	}
 }
