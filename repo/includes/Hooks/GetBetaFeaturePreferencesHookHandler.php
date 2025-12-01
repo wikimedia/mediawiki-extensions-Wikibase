15fe51d6b429751cf836796b188144e8097b1f44
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2025-12-01 16:42:15 +0100

    Add image to wbui2025 (MEX) beta feature
    The file is [[File:Wikimedia Brand Guidelines Update 2022 - Mobile.svg]]
    [1] (Jasmina El Bouamraoui and Karabo Poppy Moletsane, CC0 1.0),
    optimized with svgo 4.0.0.
    
    Also use constructor property promotion and fix the link to the hook
    documentation (it was pointing to the wrong hook).
    
    [1]: https://commons.wikimedia.org/wiki/File:Wikimedia_Brand_Guidelines_Update_2022_-_Mobile.svg
    
    Bug: T411341
    Change-Id: Iecb54964d1d923e6a704242f9a77beb796e8601d

--- repo/includes/Hooks/GetBetaFeaturePreferencesHookHandler.php
+++ repo/includes/Hooks/GetBetaFeaturePreferencesHookHandler.php
@@ -4,13 +4,15 @@ declare( strict_types = 1 );
 
 namespace Wikibase\Repo\Hooks;
 
+use MediaWiki\Config\Config;
+use MediaWiki\MainConfigNames;
 use MediaWiki\User\User;
 use Wikibase\Lib\SettingsArray;
 use Wikibase\View\Wbui2025FeatureFlag;
 
 /**
  * Hook handler that registers beta features with the BetaFeatures extension.
- * @see https://www.mediawiki.org/wiki/Special:MyLanguage/Manual:Hooks/GetBetaFeatureDependencyHooks
+ * @see https://www.mediawiki.org/wiki/Special:MyLanguage/Extension:BetaFeatures/Hooks/GetBetaFeaturePreferences
  * @see \MediaWiki\Extension\BetaFeatures\Hooks\GetBetaFeaturePreferencesHook
  *
  * @license GPL-2.0-or-later
@@ -18,14 +20,17 @@ use Wikibase\View\Wbui2025FeatureFlag;
  */
 class GetBetaFeaturePreferencesHookHandler {
 
-	private bool $enableMobileEditingUIBetaFeature;
-
-	public function __construct( bool $enableMobileEditingUIBetaFeature ) {
-		$this->enableMobileEditingUIBetaFeature = $enableMobileEditingUIBetaFeature;
+	public function __construct(
+		private readonly string $extensionAssetsPath,
+		private readonly bool $enableMobileEditingUIBetaFeature,
+	) {
 	}
 
-	public static function factory( SettingsArray $settings ): self {
-		return new self( $settings->getSetting( 'tmpEnableMobileEditingUIBetaFeature' ) );
+	public static function factory( Config $mainConfig, SettingsArray $settings ): self {
+		return new self(
+			$mainConfig->get( MainConfigNames::ExtensionAssetsPath ),
+			$settings->getSetting( 'tmpEnableMobileEditingUIBetaFeature' ),
+		);
 	}
 
 	public function onGetBetaFeaturePreferences( User $user, array &$betaFeatures ): void {
@@ -39,6 +44,8 @@ class GetBetaFeaturePreferencesHookHandler {
 				'https://www.wikidata.org/wiki/Wikidata:Usability_and_usefulness/Item_editing_experience/Mobile_editing_of_statements',
 			'discussion-link' =>
 				'https://www.wikidata.org/wiki/Wikidata_talk:Usability_and_usefulness/Item_editing_experience/Mobile_editing_of_statements',
+			'screenshot' =>
+				"{$this->extensionAssetsPath}/Wikibase/repo/resources/wikibase.wbui2025/images/mobile.svg",
 		];
 	}
 
