4277f76965386734fd2d729df0c0190f97fb32db
Mahmoud <mahmoud.abdelsattar@wikimedia.de>
2026-01-06 13:35:59 +0100

    Styling for horizontally-scrollable snak values
    Introduces a gradient mask to fade out the overflow, and
    adds padding to value elements equal to the width of the
    mask.
    
    This works for LTR languages (where the overflow is on the
    right side), for the RTL values the musical notations
    are having the direction of the LTR (logic) as the
    musical notations can be read from left-to-right in
    all languages.
    
    Bug: T412128
    Change-Id: I16c09f8322f9ffff7f1c13a62ca8609842477aee

--- repo/resources/wikibase.wbui2025/components/snakValue.vue
+++ repo/resources/wikibase.wbui2025/components/snakValue.vue
@@ -29,7 +29,8 @@ module.exports = exports = defineComponent( {
 				'wikibase-wbui2025-time-value': this.snak.datatype === 'time',
 				'wikibase-wbui2025-globe-coordinate-value': this.snak.datatype === 'globe-coordinate',
 				'wikibase-wbui2025-tabular-data-value': this.snak.datatype === 'tabular-data',
-				'wikibase-wbui2025-geo-shape-value': this.snak.datatype === 'geo-shape'
+				'wikibase-wbui2025-geo-shape-value': this.snak.datatype === 'geo-shape',
+				'wikibase-wbui2025-musical-notation-value': this.snak.datatype === 'musical-notation'
 			};
 		},
 		indicatorsHtml() {
