f9a0081b99525c59646a0a20984046e56a51f77c
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2026-02-09 17:16:41 +0100

    GQL: Improve introspection query tracking
    In order to be able to separate successful introspection queries from
    successful item data queries in the metrics, we want to track
    introspection-only requests differently:
     * no field usage is tracked
     * mediawiki_wikibase_graphql_hit_total uses "introspection" as the
       value of the status label if a request contains a pure introspection
       query
    
    Bug: T416886
    Change-Id: I07f85d109cf5da6198a97ee993c7a24df521a21a

--- repo/domains/reuse/src/Infrastructure/GraphQL/Schema/Schema.php
+++ repo/domains/reuse/src/Infrastructure/GraphQL/Schema/Schema.php
@@ -14,54 +14,59 @@ use Wikibase\Repo\Domains\Reuse\Infrastructure\GraphQL\Resolvers\SearchItemsReso
  * @license GPL-2.0-or-later
  */
 class Schema extends GraphQLSchema {
+	public readonly array $fieldNames;
+
 	public function __construct(
 		ItemResolver $itemResolver,
 		SearchItemsResolver $searchItemsResolver,
 		private readonly Types $types,
 	) {
+		$fieldDefinitions = [
+			'item' => [
+				'type' => $this->types->getItemType(),
+				'args' => [
+					'id' => Type::nonNull( $this->types->getItemIdType() ),
+				],
+				'resolve' => fn( $rootValue, array $args, $context ) => $itemResolver
+					->resolveItem( $args['id'], $context ),
+				'complexity' => fn() => GraphQLService::LOAD_ITEM_COMPLEXITY,
+			],
+			'itemsById' => [
+				// @phan-suppress-next-line PhanUndeclaredInvokeInCallable
+				'type' => Type::nonNull( Type::listOf( $this->types->getItemType() ) ),
+				'args' => [
+					// @phan-suppress-next-line PhanUndeclaredInvokeInCallable
+					'ids' => Type::nonNull( Type::listOf( Type::nonNull( $this->types->getItemIdType() ) ) ),
+				],
+				'resolve' => fn( $rootValue, array $args, $context ) => $itemResolver
+					->resolveItems( $args['ids'], $context ),
+				'complexity' => fn( int $childrenComplexity, array $args ) => count( $args['ids'] ) *
+					GraphQLService::LOAD_ITEM_COMPLEXITY,
+			],
+			'searchItems' => [
+				'type' => Type::nonNull( $this->types->getItemSearchResultConnectionType() ),
+				'args' => [
+					'query' => Type::nonNull( $this->types->getItemSearchFilterType() ),
+					'first' => [
+						'type' => Type::nonNull( Type::int() ),
+						'defaultValue' => FacetedItemSearchRequest::DEFAULT_LIMIT,
+					],
+					'after' => Type::string(),
+				],
+				'resolve' => fn( $rootValue, array $args ) => $searchItemsResolver->resolve(
+					$args['query'],
+					$args['first'],
+					$args['after'] ?? null,
+				),
+				'complexity' => fn() => GraphQLService::SEARCH_ITEMS_COMPLEXITY,
+			],
+		];
+		$this->fieldNames = array_keys( $fieldDefinitions );
+
 		parent::__construct( [
 			'query' => new ObjectType( [
 				'name' => 'Query',
-				'fields' => [
-					'item' => [
-						'type' => $this->types->getItemType(),
-						'args' => [
-							'id' => Type::nonNull( $this->types->getItemIdType() ),
-						],
-						'resolve' => fn( $rootValue, array $args, $context ) => $itemResolver
-								->resolveItem( $args['id'], $context ),
-						'complexity' => fn() => GraphQLService::LOAD_ITEM_COMPLEXITY,
-					],
-					'itemsById' => [
-						// @phan-suppress-next-line PhanUndeclaredInvokeInCallable
-						'type' => Type::nonNull( Type::listOf( $this->types->getItemType() ) ),
-						'args' => [
-							// @phan-suppress-next-line PhanUndeclaredInvokeInCallable
-							'ids' => Type::nonNull( Type::listOf( Type::nonNull( $this->types->getItemIdType() ) ) ),
-						],
-						'resolve' => fn( $rootValue, array $args, $context ) => $itemResolver
-								->resolveItems( $args['ids'], $context ),
-						'complexity' => fn( int $childrenComplexity, array $args ) => count( $args['ids'] ) *
-							GraphQLService::LOAD_ITEM_COMPLEXITY,
-					],
-					'searchItems' => [
-						'type' => Type::nonNull( $this->types->getItemSearchResultConnectionType() ),
-						'args' => [
-							'query' => Type::nonNull( $this->types->getItemSearchFilterType() ),
-							'first' => [
-								'type' => Type::nonNull( Type::int() ),
-								'defaultValue' => FacetedItemSearchRequest::DEFAULT_LIMIT,
-							],
-							'after' => Type::string(),
-						],
-						'resolve' => fn( $rootValue, array $args ) => $searchItemsResolver->resolve(
-							$args['query'],
-							$args['first'],
-							$args['after'] ?? null,
-						),
-						'complexity' => fn() => GraphQLService::SEARCH_ITEMS_COMPLEXITY,
-					],
-				],
+				'fields' => $fieldDefinitions,
 			] ),
 		] );
 	}
