538ffd538db6eaf6567bf682cdd5ba303ecfe72e
dima koushha <dima.koushha@wikimedia.de>
2026-03-03 13:28:51 +0100

    GQL: Move trackErrors into the new GraphQLTracking class
    Bug: T418558
    Change-Id: Icb5d2f5a4363d0f31436c96867617cf16727238e

--- repo/domains/reuse/src/Infrastructure/GraphQL/GraphQLTracking.php
+++ repo/domains/reuse/src/Infrastructure/GraphQL/GraphQLTracking.php
@@ -2,7 +2,10 @@
 
 namespace Wikibase\Repo\Domains\Reuse\Infrastructure\GraphQL;
 
+use GraphQL\Error\Error;
 use GraphQL\Language\AST\DocumentNode;
+use Wikibase\Repo\Domains\Reuse\Infrastructure\GraphQL\Errors\GraphQLError;
+use Wikibase\Repo\Domains\Reuse\Infrastructure\GraphQL\Errors\GraphQLErrorType;
 use Wikibase\Repo\Domains\Reuse\Infrastructure\GraphQL\Schema\Schema;
 use Wikimedia\Stats\StatsFactory;
 
@@ -53,5 +56,38 @@ class GraphQLTracking {
 				->setLabel( 'field', $field )
 				->increment();
 		}
+	}
+
+	public function trackErrors( QueryComplexityRule $complexityRule, array $errors ): void {
+		$errorTypes = array_unique( array_map(
+			fn( Error $e ) => $this->getErrorType( $complexityRule, $e )->name,
+			$errors,
+		) );
+
+		foreach ( $errorTypes as $type ) {
+			$this->stats->getCounter( 'wikibase_graphql_error_total' )
+				->setLabel( 'type', $type )
+				->increment();
+		}
+	}
+
+	private function getErrorType( QueryComplexityRule $queryComplexityRule, Error $error ): GraphQLErrorType {
+		if ( $queryComplexityRule->wasChecked()
+			 && $queryComplexityRule->getQueryComplexity() > $queryComplexityRule->getMaxQueryComplexity() ) {
+			return GraphQLErrorType::QUERY_TOO_COMPLEX;
+		}
+
+		$previousError = $error->getPrevious();
+		if ( $previousError instanceof GraphQLError ) {
+			return $previousError->type;
+		}
+
+		// If there is no previous error, it means that the GraphQL engine itself rejected the query
+		// e.g. because the query was malformed, or an invalid field or operation name.
+		if ( $previousError === null ) {
+			return GraphQLErrorType::INVALID_QUERY;
+		}
+
+		return GraphQLErrorType::UNKNOWN;
 	}
 }
