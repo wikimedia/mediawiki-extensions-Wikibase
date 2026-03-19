731484980a59dca60a39a6d42888c638359f0983
nicholusmuwonge <nicholus.muwonge@wikimedia.de>
2026-03-19 16:29:12 +0100

    Add unit tests for EntityChangeNotificationJob
    - Add testHandlingBatchChange to verify batch update scenarios
    - Add testHandlingChangeWithEmptyChangesArray to test edge case
    - Improve test coverage for entity change notifications
    
    Change-Id: I22ae4884d3d6e46fe5d57224a892e6c87d3c4a4d

--- client/tests/phpunit/unit/includes/EntityChangeNotificationJobTest.php
+++ client/tests/phpunit/unit/includes/EntityChangeNotificationJobTest.php
@@ -10,6 +10,8 @@ use Wikibase\Client\Changes\ChangeHandler;
 use Wikibase\Client\EntityChangeNotificationJob;
 use Wikibase\DataModel\Entity\EntityIdParser;
 use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Entity\NumericPropertyId;
+use Wikibase\Lib\Changes\EntityChange;
 use Wikibase\Lib\Changes\ItemChange;
 
 /**
@@ -53,6 +55,90 @@ class EntityChangeNotificationJobTest extends TestCase {
 			new NullLogger(),
 			[
 				'changes' => [ $testItemChange->getFields() ],
+			]
+		);
+
+		$entityChangeNotificationJob->run();
+	}
+
+	public function testHandlingBatchChange() {
+		# Testing for a batch update scenario for an entity.
+		$testItemId = new ItemId( 'Q1' );
+		$testItemChange = new ItemChange( [
+			'time' => '20210906122813',
+			'info' => [], // some json
+			'user_id' => '43',
+			'revision_id' => '123',
+			'object_id' => 'Q1',
+			'type' => 'wikibase-item~update',
+		] );
+
+		$testStatementChange = new ItemChange( [
+			'time' => '20210906122813',
+			'info' => [], // some json
+			'user_id' => '43',
+			'revision_id' => '124',
+			'object_id' => 'Q1',
+			'type' => 'wikibase-item~update',
+		] );
+		$testItemChange->setEntityId( $testItemId );
+		$testStatementChange->setEntityId( $testItemId );
+
+		$testPropertyId = new NumericPropertyId( 'P18' );
+		$testPropertyChange = new EntityChange( [
+			'time' => '20210906122813',
+			'info' => [], // some json
+			'user_id' => '43',
+			'revision_id' => '125',
+			'object_id' => 'P18',
+			'type' => 'wikibase-property~update',
+		] );
+		$testPropertyChange->setEntityId( $testPropertyId );
+
+		$mockChangeHandler = $this->createMock( ChangeHandler::class );
+		$mockChangeHandler->expects( $this->once() )->method( 'handleChanges' )
+			->with(
+				[ $testItemChange, $testStatementChange, $testPropertyChange ],
+				[
+					'rootJobSignature' => null,
+					'rootJobTimestamp' => null,
+				]
+			);
+
+		$mockEntityIdParser = $this->createMock( EntityIdParser::class );
+		$mockEntityIdParser->expects( $this->exactly( 3 ) )->method( 'parse' )
+		->willReturn( $testItemId, $testItemId, $testPropertyId );
+
+		$entityChangeNotificationJob = new EntityChangeNotificationJob(
+			$mockChangeHandler,
+			$mockEntityIdParser,
+			new NullLogger(),
+			[
+				'changes' => [
+					$testItemChange->getFields(),
+					$testStatementChange->getFields(),
+					$testPropertyChange->getFields(),
+				],
+			]
+		);
+
+		$entityChangeNotificationJob->run();
+	}
+
+	public function testHandlingChangeWithEmptyChangesArray() {
+
+		$mockChangeHandler = $this->createMock( ChangeHandler::class );
+		$mockChangeHandler->expects( $this->never() )->method( 'handleChanges' );
+
+		$mockEntityIdParser = $this->createMock( EntityIdParser::class );
+		$mockEntityIdParser->expects( $this->never() )->method( 'parse' );
+
+		$entityChangeNotificationJob = new EntityChangeNotificationJob(
+			$mockChangeHandler,
+			$mockEntityIdParser,
+			new NullLogger(),
+			[
+				'changes' => [],
 			]
 		);
 
