6e29eb68ad47b00f401babe6b6112c687439f019
Kim Pham <kim.pham@wikimedia.de>
2026-05-29 15:25:14 +0200

    Run  api-testing sequentially in Github Actions
    Addresses the intermittent resource not found errors related to running the secondary CI api tests by disabling parallel processes.
    
    Bug: T420318
    Change-Id: I43989192e40d84dc1b7d71ba415b3a056a9c8002

--- repo/rest-api/.mocharc.js
+++ repo/rest-api/.mocharc.js
@@ -1,7 +1,8 @@
 'use strict';
 
+// parallel is enabled by default, but can be disabled by setting MOCHA_PARALLEL to 'false'
 module.exports = {
-	parallel: true,
+	parallel: process.env.MOCHA_PARALLEL !== 'false',
 	recursive: true,
 	ext: 'Test.js'
 };
