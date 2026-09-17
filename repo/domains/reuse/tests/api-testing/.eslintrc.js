2b7ab076d99387b5e448d90984f18092adb53c06
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2026-09-17 16:36:22 +0200

    REUSE: Address eslint issues instead of ignoring
    There were warnings in the CI logs that these ignore comments aren't
    needed. Removing them revealed that the linter wants single quotes,
    which is still an easy enough change.
    
    Change-Id: I72821aa2015be84d5bcd1576cc5c4f4c84eb8a42

--- repo/domains/reuse/tests/api-testing/.eslintrc.js
+++ repo/domains/reuse/tests/api-testing/.eslintrc.js
@@ -1,15 +1,14 @@
 'use strict';
 
-/* eslint-disable quotes */
 module.exports = {
 	extends: [
-		"wikimedia/mocha"
+		'wikimedia/mocha'
 	],
 	rules: {
-		"prefer-arrow-callback": "off",
-		"mocha/no-setup-in-describe": 0,
-		"mocha/no-skipped-tests": "error",
+		'prefer-arrow-callback': 'off',
+		'mocha/no-setup-in-describe': 0,
+		'mocha/no-skipped-tests': 'error',
 		// GraphQL introspection fields use double underscores (e.g. __type, __schema)
-		"no-underscore-dangle": "off"
+		'no-underscore-dangle': 'off'
 	}
 };
